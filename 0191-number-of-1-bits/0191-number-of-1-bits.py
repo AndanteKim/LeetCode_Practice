e07f1a566befa1f6b2e2ba47977c92a18f0c3533
Andrew Kim <ksb0724@utexas.edu>
2023-01-03 01:39:43 -0600

    Time: 43 ms (59.80%), Space: 13.9 MB (49.08%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -1,8 +1,8 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         steps = 0
-        while (n > 0):
-            steps += n & 1
-            n = n >> 1
+        while n != 0:
+            steps += 1
+            n &= n-1
         
         return steps
