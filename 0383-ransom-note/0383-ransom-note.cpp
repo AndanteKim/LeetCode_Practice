744ee3444ca10c6008b8fd73bf4054533f397be1
Andrew Kim <ksb0724@utexas.edu>
2023-01-03 00:47:48 -0600

    Time: 19 ms (77.83%), Space: 8.9 MB (41.90%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -3,8 +3,8 @@ public:
     bool canConstruct(string ransomNote, string magazine) {
         unordered_map<char, int> r_table, m_table;
         
-        for (const auto &c:ransomNote) r_table[c] += 1;
-        for (const auto&c:magazine) m_table[c] += 1;
+        for (const auto &c:ransomNote) ++r_table[c];
+        for (const auto&c:magazine) ++m_table[c];
         
         for (auto iter : r_table){
             if (iter.second > m_table[iter.first]) return false;
