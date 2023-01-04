65b5b549883a1ebc691ed308d4adce23ad326e44
Andrew Kim <ksb0724@utexas.edu>
2023-01-04 01:19:14 -0600

    Time: 309 ms (49.32%), Space: 59.5 MB (8.09%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+        ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
         int curr = INT_MAX, ans = -1, i = 0;
-        
         for (const auto &point : points){
             if (point[0] == x || point[1] == y){
                 if (curr > abs(point[0] - x) + abs(point[1] - y)){
