5c94be41c5d8fbb6d1f4e75869e3b0ecb6fdb091
Andrew Kim <ksb0724@utexas.edu>
2023-01-05 01:23:39 -0600

    Time: 71 ms (9.61%), Space: 13.8 MB (70.19%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,14 +1,14 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        lookup1, lookup2 = Counter(s1), Counter(s2)
         
-        if lookup1 != lookup2:
-            return False
-        else:
-            switch = 0
-            for c1, c2 in zip(s1,s2):
-                if c1 != c2:
-                    switch += 1
-            if switch == 2 or switch == 0:
-                return True
-        return False
+        if s1 == s2: return True
+        c1, c2, cnt = Counter(s1), Counter(s2), 0
+        
+        for i in range(len(s1)):
+            cnt += s1[i] != s2[i]
+            if cnt > 2:
+                return False
+            if c1[s1[i]] != c2[s1[i]]:
+                return False
+        return True
+    
