bc3209939840f68ddeafdfe146545def48b3c6d6
Andrew Kim <ksb0724@utexas.edu>
2023-01-05 00:17:52 -0600

    Time: 51 ms (36.21%), Space: 13.5 MB (46.90%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def canMakeArithmeticProgression(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: bool
+        """
+        arr.sort()
+        
+        diff = arr[1] - arr[0]
+        
+        for i in range(len(arr)-1):
+            if arr[i+1] - arr[i] != diff:
+                return False
+        return True
