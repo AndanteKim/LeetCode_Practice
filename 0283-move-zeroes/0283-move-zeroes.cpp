d14d61c370aeec2be4b2f9b6d0b80329e061dedb
Andrew Kim <ksb0724@utexas.edu>
2023-01-07 02:56:00 -0600

    Time: 19 ms (96.37%), Space: 19.3 MB (62.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        for (int lastNonZeroFoundAt = 0, curr = 0; curr < nums.size(); ++curr){
-            if (nums[curr] != 0){
-                swap(nums[curr], nums[lastNonZeroFoundAt++]);
-                
-            }
-            
+        int lastNonZeroFoundAt = 0, length = nums.size();
+        for (int i = 0; i < length; ++i){
+            if (nums[i] != 0) nums[lastNonZeroFoundAt++] = nums[i];
         }
+        
+        for (;lastNonZeroFoundAt < length; ++lastNonZeroFoundAt) nums[lastNonZeroFoundAt] = 0;
     }
 };
