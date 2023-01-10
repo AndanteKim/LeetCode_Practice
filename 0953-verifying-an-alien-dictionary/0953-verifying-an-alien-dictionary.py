7d7962e5b8149808ffb244b607f169d6321ef14e
Andrew Kim <ksb0724@utexas.edu>
2023-01-10 10:05:50 -0600

    Time: 37 ms (85.79%), Space: 13.9 MB (81.79%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,21 +1,13 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        lookup_order, prev = {o:idx for idx, o in enumerate(order)}, list()
-        for word in words:
-            flag, order_match = False, []
-            for c in word:
-                order_match.append(c)
-            if not prev:
-                prev = order_match
-            else:
-                for i in range(min(len(prev), len(order_match))):
-                    if lookup_order[prev[i]] < lookup_order[order_match[i]]:
-                        flag = True
-                        break
-                    elif lookup_order[prev[i]] > lookup_order[order_match[i]]:
-                        return False
-                if not flag and (len(prev) > len(order_match)):
-                    return False
-            prev = order_match
+        lookup_order = {o:idx for idx, o in enumerate(order)}
+        
+        for i in range(len(words)-1):
+            for j in range(len(words[i])):
+                if j >= len(words[i+1]): return False
+                
+                if words[i][j] != words[i+1][j]:
+                    if lookup_order[words[i][j]] > lookup_order[words[i+1][j]]: return False
+                    break
         
         return True
