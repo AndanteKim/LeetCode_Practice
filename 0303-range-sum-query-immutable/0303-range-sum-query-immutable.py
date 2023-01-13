f1040be98c62b91e75127965cf90ef3ef39c6746
Andrew Kim <ksb0724@utexas.edu>
2023-01-12 23:40:57 -0600

    Time: 170 ms (59.15%), Space: 17.7 MB (59.72%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -1,10 +1,15 @@
 class NumArray:
 
     def __init__(self, nums: List[int]):
-        self.nums = nums
-
+        self.nums, total, self.prefix_sum = nums, 0, (0, )
+        
+        for i in range(len(self.nums)):
+            self.prefix_sum += (total + self.nums[i], )
+            total += self.nums[i]
+        print(self.prefix_sum)
+        
     def sumRange(self, left: int, right: int) -> int:
-        return sum(self.nums[left:right+1])
+        return self.prefix_sum[right+1] - self.prefix_sum[left]
 
 
 # Your NumArray object will be instantiated and called as such:
