1bfb39971d567739a818f9cf03d810d45c32ed21
Andrew Kim <ksb0724@utexas.edu>
2023-01-12 23:41:32 -0600

    Time: 182 ms (54.37%), Space: 17.6 MB (59.72%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.py
@@ -6,7 +6,6 @@ class NumArray:
         for i in range(len(self.nums)):
             self.prefix_sum += (total + self.nums[i], )
             total += self.nums[i]
-        print(self.prefix_sum)
         
     def sumRange(self, left: int, right: int) -> int:
         return self.prefix_sum[right+1] - self.prefix_sum[left]
