05f1fb41f16284f08757a603889c0d216e32014b
Andrew Kim <ksb0724@utexas.edu>
2023-01-14 10:31:21 -0600

    Time: 6 ms (49.54%), Space: 6.3 MB (94.50%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,41 +1,30 @@
 class Solution {
 public:
-    void DFS(int src, array<array<int,26>,26>& adjMatrix, array<int, 26>& visited, vector<int>& component, int& minChar){
-        visited[src] = 1;
-        component.push_back(src);
-        minChar = min(minChar, src);
+    array<int, 26> representative;
+    int find(int x){
+        if (representative[x] == x) return x;
+        return representative[x] = find(representative[x]);
+    }
+    
+    void performUnion(int x, int y){
+        x = find(x);
+        y = find(y);
         
-        for (int i = 0; i < 26; ++i){
-            if (adjMatrix[src][i] && !visited[i]){
-                DFS(i, adjMatrix, visited, component, minChar);
-            }
-        }
+        if (x == y) return;
+        
+        if (x < y) representative[y] = x;
+        else representative[x] = y;
+        
     }
     
     string smallestEquivalentString(string s1, string s2, string baseStr) {
-        array<array<int, 26>, 26> adjMatrix = {0};
-        for (int i = 0; i < s1.size(); ++i){
-            adjMatrix[s1[i] - 'a'][s2[i] - 'a'] = 1;
-            adjMatrix[s2[i] - 'a'][s1[i] - 'a'] = 1;
-        }
+        for (int i = 0; i < 26; ++i) representative[i] = i;
         
-        array<int, 26> mappingChar = {0};
-        for (int i = 0; i < 26; ++i) mappingChar[i] = i;
-        
-        array<int, 26> visited = {0};
-        
-        for (int c = 0; c < 26; ++c){
-            if (!visited[c]){
-                vector<int> component;
-                int minChar = 27;
-            
-            DFS(c, adjMatrix, visited, component, minChar);
-            for (int vertex : component) mappingChar[vertex] = minChar;
-            }
-        }
+        for (int i = 0; i < s1.size(); ++i) performUnion(s1[i] - 'a', s2[i]-'a');
         
         string ans;
-        for (char c : baseStr) ans += (char)(mappingChar[c-'a'] + 'a');
+        for (char c : baseStr) ans += (char)(find(c - 'a') +'a');
+        
         return ans;
     }
 };
