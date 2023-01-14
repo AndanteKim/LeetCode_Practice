11a9bdf6aa90e3e209c27c9ec8d35cfa04b6ee4b
Andrew Kim <ksb0724@utexas.edu>
2023-01-14 07:19:44 -0600

    Time: 42 ms (85.63%), Space: 14 MB (56.90%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,40 +1,33 @@
 class Solution:
+    minChar = 2**31 - 1
+    def DFS(self, src: int, adjMatrix: List[List[int]], visited: List[int], component: List[int]):
+        visited[src] = 1
+        component.append(src)
+        self.minChar = min(self.minChar, src)
+        
+        for i in range(26):
+            if (adjMatrix[src][i] != None and visited[i] == None):
+                self.DFS(i, adjMatrix, visited, component)
+    
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        ans, match_up = "", dict()
-        
+        adjMatrix = [[None] * 26 for _ in range(26)]
         for i in range(len(s1)):
-            if s1[i] not in match_up.keys():
-                match_up[s1[i]] = {s1[i]}
-            
-                
-            if s2[i] not in match_up.keys():
-                match_up[s2[i]] = {s2[i]}
-                
-                
-            match_up[s2[i]].add(s1[i])
-            temp = copy.deepcopy(match_up)
-            for char in match_up[s1[i]]:
-                temp[s2[i]].add(char)
-                temp_char = temp[char]
-                for c in temp_char:
-                    temp[s2[i]].add(c)
-            match_up = copy.deepcopy(temp)
-            
-            match_up[s1[i]].add(s2[i])
-            temp = copy.deepcopy(match_up)
-            for char in match_up[s2[i]]:
-                temp[s1[i]].add(char)
-                temp_char = temp[char]
-                for c in temp_char:
-                    temp[s1[i]].add(c)
-            match_up = copy.deepcopy(temp)
-            
+            adjMatrix[ord(s1[i]) - ord('a')][ord(s2[i]) - ord('a')] = 1
+            adjMatrix[ord(s2[i]) - ord('a')][ord(s1[i]) - ord('a')] = 1
         
-        for key, value in match_up.items():
-            curr = len(value)
-            for char in value:
-                match_up[key] = max(match_up[key], match_up[char], key = len)
-        
+        mappingChar = [i for i in range(26)]
+        visited = [None] *26
+        for c in range(26):
+            if visited[c] == None:
+                component = []
+                self.minChar = 27
+                self.DFS(c, adjMatrix, visited, component)
+                
+                for vertex in component:
+                    mappingChar[vertex] = self.minChar
+                    
+        ans = ""
         for c in baseStr:
-            ans += c if c not in match_up.keys() else min(match_up[c])
+            ans += chr(mappingChar[ord(c) - ord('a')] + ord('a'))
+        
         return ans
