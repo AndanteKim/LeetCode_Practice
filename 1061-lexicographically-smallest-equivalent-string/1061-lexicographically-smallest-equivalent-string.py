f43a1b1e85a997c783bb005439b630d29f62a6a4
Andrew Kim <ksb0724@utexas.edu>
2023-01-14 07:59:44 -0600

    Time: 44 ms (81.03%), Space: 13.8 MB (100.00%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,33 +1,32 @@
 class Solution:
-    minChar = 2**31 - 1
-    def DFS(self, src: int, adjMatrix: List[List[int]], visited: List[int], component: List[int]):
-        visited[src] = 1
-        component.append(src)
-        self.minChar = min(self.minChar, src)
+    representative = [0] * 26
+    
+    def find(self, x: int) -> int:
+            if (self.representative[x] == x): return x
+            return self.find(self.representative[x])
         
-        for i in range(26):
-            if (adjMatrix[src][i] != None and visited[i] == None):
-                self.DFS(i, adjMatrix, visited, component)
+    def performUnion(self, x: int, y: int) -> None:
+        x = self.find(x)
+        y = self.find(y)
+        if x == y: return
+            
+        if (x < y):
+            self.representative[y] = x
+        else:
+            self.representative[x] = y
     
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        adjMatrix = [[None] * 26 for _ in range(26)]
-        for i in range(len(s1)):
-            adjMatrix[ord(s1[i]) - ord('a')][ord(s2[i]) - ord('a')] = 1
-            adjMatrix[ord(s2[i]) - ord('a')][ord(s1[i]) - ord('a')] = 1
-        
-        mappingChar = [i for i in range(26)]
-        visited = [None] *26
-        for c in range(26):
-            if visited[c] == None:
-                component = []
-                self.minChar = 27
-                self.DFS(c, adjMatrix, visited, component)
-                
-                for vertex in component:
-                    mappingChar[vertex] = self.minChar
-                    
         ans = ""
+        for i in range(26):
+            self.representative[i] = i
+        
+        for i in range(len(s1)):
+            self.performUnion(ord(s1[i]) - ord('a'), ord(s2[i]) - ord('a'))
+        
         for c in baseStr:
-            ans += chr(mappingChar[ord(c) - ord('a')] + ord('a'))
+            ans += chr(self.find(ord(c)-ord('a')) + ord('a'))
         
         return ans
+            
+        
+        
