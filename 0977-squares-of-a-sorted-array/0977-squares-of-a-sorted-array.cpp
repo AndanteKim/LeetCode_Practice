fa095cece5129ed00218f658fd29a2352b8d9900
Andrew Kim <ksb0724@utexas.edu>
2023-01-16 10:04:35 -0600

    Time: 38 ms (77.42%), Space: 26 MB (58.35%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), [](int &a, int &b) {return abs(a) < abs(b);});
-        for (int i = 0; i < nums.size(); ++i){
-            nums[i] *= nums[i];
+        int length = nums.size();
+        int left = 0, right = length - 1;
+        vector<int> result(length, 0);
+        for (int i = length-1; i >= 0; --i){
+            if (abs(nums[left]) < abs(nums[right])){
+                result[i] = nums[right] * nums[right];
+                --right;
+            }
+            else{
+                result[i] = nums[left] * nums[left];
+                ++left;
+            }
         }
-        return nums;
+        
+        return result;
     }
 };
