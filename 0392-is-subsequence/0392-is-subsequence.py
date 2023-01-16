e272254361b7d8e8f632e0f0d0d8ede12633e768
Andrew Kim <ksb0724@utexas.edu>
2023-01-16 05:51:02 -0600

    Time: 41 ms (54.13%), Space: 24.2 MB (5.79%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,14 +1,23 @@
 class Solution:
+    LEFT_BOUND, RIGHT_BOUND = 0, 0
+    s, t = "", ""
+    def rec_isSubsequence(self, left_index: str, right_index: str) -> bool:
+        #base cases
+        if left_index == self.LEFT_BOUND:
+            return True
+        if right_index == self.RIGHT_BOUND:
+            return False
+        
+        if self.s[left_index] == self.t[right_index]:
+            left_index += 1
+        right_index += 1
+        
+        return self.rec_isSubsequence(left_index, right_index)
+    
     def isSubsequence(self, s: str, t: str) -> bool:
-        length_s, length_t = len(s), len(t)
-        i, j = 0, 0
-        while i < length_s and j < length_t:
-            if s[i] == t[j]:
-                i += 1
-                j += 1
-            else:
-                j += 1
-                
-        return True if i == length_s else False
+        self.LEFT_BOUND, self.RIGHT_BOUND = len(s), len(t)
+        self.s, self.t = s, t
+        return self.rec_isSubsequence(0, 0)
+        
         
         
