9b9c5d60003a631acc2c73f7e7ba4995f4cddfe2
Andrew Kim <ksb0724@utexas.edu>
2023-01-16 06:52:56 -0600

    Time: 119 ms (5.19%), Space: 14.9 MB (5.79%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -1,19 +1,20 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        letter_indices_table = defaultdict(list)
-        for index, letter in enumerate(t):
-            letter_indices_table[letter].append(index)
+        source_len, target_len = len(s), len(t)
         
-        curr_match_index = -1
-        for letter in s:
-            if letter not in letter_indices_table:
-                return False
+        if source_len == 0:
+            return True
+        
+        dp = [ [0] * (target_len + 1) for _ in range(source_len + 1)]
+        
+        for col in range(1, target_len + 1):
+            for row in range(1, source_len + 1):
+                if s[row - 1] == t[col - 1]:
+                    dp[row][col] = dp[row-1][col-1] + 1
+                else:
+                    dp[row][col] = max(dp[row][col - 1], dp[row - 1][col])
             
-            indices_list = letter_indices_table[letter]
-            match_index = bisect.bisect_right(indices_list, curr_match_index)
-            if match_index != len(indices_list):
-                curr_match_index = indices_list[match_index]
-            else:
-                return False
+            if dp[source_len][col] == source_len:
+                return True
         
-        return True
+        return False
