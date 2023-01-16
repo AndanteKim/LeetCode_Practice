3457e96c7b9f0baf10a961adf97282ffa7571319
Andrew Kim <ksb0724@utexas.edu>
2023-01-16 00:08:24 -0600

    Time: 19 ms (74.71%), Space: 17.2 MB (45.20%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
-    bool doesIntervalsOverlap(vector<int> &a, vector<int> &b){
-        return min(a[1], b[1]) - max(a[0], b[0]) >= 0;
-    }
-    
-    vector<int> mergeIntervals(vector<int> &a, vector<int> &b){
-        return {min(a[0], b[0]), max(a[1], b[1])};
-    }
-    
-    void insertInterval(vector<vector<int>>& intervals, vector<int>& newInterval){
-        int index = upper_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin();
-        if (index != intervals.size()) intervals.insert(intervals.begin() + index, newInterval);
-        else intervals.push_back(newInterval);
-    }
-    
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        insertInterval(intervals, newInterval);
-        
+        int position = upper_bound(intervals.begin(), intervals.end(), newInterval) - intervals.begin();
+        intervals.insert(intervals.begin() + position, newInterval);
         vector<vector<int>> answer;
+        
         for (int i = 0; i < intervals.size(); ++i){
-            vector<int> currInterval = {intervals[i][0], intervals[i][1]};
-            while (i < intervals.size() && doesIntervalsOverlap(currInterval, intervals[i])){
-                currInterval = mergeIntervals(currInterval, intervals[i]);
-                ++i;
-            }
-            --i;
-            answer.push_back(currInterval);
+            if (answer.empty() || answer.back()[1] < intervals[i][0]) answer.push_back(intervals[i]);
+            else answer.back()[1] = max(answer.back()[1], intervals[i][1]);
         }
         return answer;
     }
