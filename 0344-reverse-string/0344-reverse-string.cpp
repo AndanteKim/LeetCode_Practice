4a4d262a734b38b62136ca9669d7dc2f635ddd65
Andrew Kim <ksb0724@utexas.edu>
2023-01-16 07:58:42 -0600

    Time: 32 ms (44.89%), Space: 23.2 MB (47.67%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
+    void helper(int i, int j, vector<char>& s){
+        if (i >= j) return;
+        if (i < j) swap(s[i], s[j]);
+        helper(i + 1, j - 1, s);
+    }
+    
     void reverseString(vector<char>& s) {
-        reverse(s.begin(), s.end());
+        helper(0, s.size()-1, s);
     }
 };
