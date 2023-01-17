32a05c82f33ee9f53a5539155930501844afc4c3
Andrew Kim <ksb0724@utexas.edu>
2023-01-17 00:26:09 -0600

    Time: 22 ms (89.87%), Space: 11.2 MB (52.66%) - LeetHub

--- 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
+++ 0926-flip-string-to-monotone-increasing/0926-flip-string-to-monotone-increasing.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     int minFlipsMonoIncr(string s) {
-        int ans = 0, num = 0;
+        int sum_cnt = 0, ans;
         
-        for (const auto& c:s){
-            if (c == '0')
-                ans = min(num, ans + 1);
-            else ++num;
+        for (const auto &c: s) if (c == '0') ++sum_cnt;
+        ans = sum_cnt;
+        for (const auto &c: s){
+            if (c == '0') {
+                --sum_cnt;
+                ans = min(ans, sum_cnt);
+            }
+            else ++sum_cnt;
         }
         
         return ans;
