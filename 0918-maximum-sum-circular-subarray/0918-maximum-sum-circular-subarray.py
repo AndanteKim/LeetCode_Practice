96e12b1683d95dfc01cb57b3f0bfce4fdf3d1ffb
Andrew Kim <ksb0724@utexas.edu>
2023-01-18 03:45:30 -0600

    Time: 573 ms (65.97%), Space: 19 MB (43.34%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,20 +1,12 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        n = len(nums)
-        right_max = [0] * n
-        right_max[n-1], suffix_sum = nums[n-1], nums[n-1]
-        for i in range(n-2, -1, -1):
-            suffix_sum += nums[i]
-            right_max[i] = max(right_max[i+1], suffix_sum)
-            
-        max_sum, special_sum = nums[0], nums[0]
-        suffix_sum, curMax = 0, 0
-        for i in range(n):
-            curMax = max(curMax, 0) + nums[i]
-            max_sum = max(max_sum, curMax)
-            suffix_sum += nums[i]
-            if i + 1 < n:
-                special_sum = max(special_sum, suffix_sum + right_max[i+1])
-            
-        return max(max_sum, special_sum)
+        cur_max, cur_min, sum, max_sum, min_sum = 0, 0, 0, nums[0], nums[0]
         
+        for num in nums:
+            cur_max = max(cur_max, 0) + num
+            max_sum = max(max_sum, cur_max)
+            cur_min = min(cur_min, 0) + num
+            min_sum = min(min_sum, cur_min)
+            sum += num;
+        
+        return max_sum if sum == min_sum else max(max_sum, sum - min_sum)
