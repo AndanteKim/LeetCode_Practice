ee1df99901469b777088e82dd987c4c769e4ad0b
Andrew Kim <ksb0724@utexas.edu>
2023-01-18 07:21:13 -0600

    Time: 148 ms (43.27%), Space: 67.7 MB (58.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
+    int findBestSubarray(vector<int> &nums, int left, int right){
         
-        // Kadane's algorithm
-        int best_sum = nums[0], current_sum = 0;
+        if (left > right) return numeric_limits<int>::min();
         
-        for (const auto num : nums){
-            current_sum = max(0, current_sum) + num;
-            best_sum = max(current_sum, best_sum);
+        int mid = (left + right) >> 1;
+        int curr = 0, best_left_sum = 0, best_right_sum = 0;
+        
+        for (int i = mid - 1; i >= left; --i){
+            curr += nums[i];
+            best_left_sum = max(best_left_sum, curr);
         }
-        return best_sum;
+        
+        curr = 0;
+        
+        for (int i = mid + 1; i <= right; ++i){
+            curr += nums[i];
+            best_right_sum = max(best_right_sum, curr);
+        }
+        
+        int best_combined_sum = nums[mid] + best_left_sum + best_right_sum;
+        
+        int left_half = findBestSubarray(nums, left, mid - 1);
+        int right_half = findBestSubarray(nums, mid + 1, right);
+        
+        return max(best_combined_sum, max(left_half, right_half));
+    }
+    
+    int maxSubArray(vector<int>& nums) {
+        return findBestSubarray(nums, 0, nums.size()-1);
     }
 };
