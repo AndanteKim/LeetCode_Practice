dc65f539dde0b259eface93ecc0b7f5d4d940def
Andrew Kim <ksb0724@utexas.edu>
2023-01-18 07:02:33 -0600

    Time: 2618 ms (5.01%), Space: 28.3 MB (43.74%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,9 +1,29 @@
 class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        cur_max, max_sum, total = 0, nums[0], 0
+    def findBestSubarray(self, nums: List[int], left: int, right: int) -> int:
         
-        for num in nums:
-            cur_max = max(cur_max, 0) + num
-            max_sum = max(max_sum, cur_max)
+        # base case: empty array
+        if left > right:
+            return -math.inf
+        
+        mid = (left + right) >> 1
+        curr = best_left_sum = best_right_sum = 0
+        
+        for i in range(mid - 1, left - 1, -1):
+            curr += nums[i]
+            best_left_sum = max(best_left_sum, curr)
+        
+        #Reset curr and iterate from the middle to the end
+        curr = 0
+        for i in range(mid + 1, right + 1):
+            curr += nums[i]
+            best_right_sum = max(best_right_sum, curr)
             
-        return max_sum
+        best_combined_sum = nums[mid] + best_left_sum + best_right_sum
+        left_half = self.findBestSubarray(nums, left, mid - 1)
+        right_half = self.findBestSubarray(nums, mid + 1, right)
+        
+        return max(best_combined_sum, left_half, right_half)
+    
+    def maxSubArray(self, nums: List[int]) -> int:
+        
+        return self.findBestSubarray(nums, 0, len(nums) - 1)
