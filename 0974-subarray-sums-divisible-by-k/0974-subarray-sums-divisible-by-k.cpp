42f3ff526a9c1960bf4e7a8f70d869ec0584f176
Andrew Kim <ksb0724@utexas.edu>
2023-01-19 00:37:51 -0600

    Time: 54 ms (84.73%), Space: 31.7 MB (28.63%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        int n = nums.size(), prefixMod = 0, answer = 0;
-        
-        vector<int>modGroups(k);
-        modGroups[0] = 1;
-        
-        for (const int &num : nums){
-            prefixMod = (prefixMod + (num % k) + k) % k;
-            answer += modGroups[prefixMod];
-            ++modGroups[prefixMod];
+        unordered_map<int, int> counters;
+        int running_sum = 0, ans = 0;
+        for (const int &num: nums){
+            running_sum += num;
+            if (running_sum < 0){
+                if ((k+(running_sum % k)) % k == 0) ++ans;
+                ans += counters[(k+(running_sum % k)) % k];
+                ++counters[(k+(running_sum % k)) % k];
+            }
+            else{
+                if (running_sum % k == 0) ++ans;
+                ans += counters[running_sum % k];
+                ++counters[running_sum % k];
+            }
+            
         }
         
-        return answer;
+        return ans;
     }
 };
