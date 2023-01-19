825683f994bd8b17732c719b87145827caa80305
Andrew Kim <ksb0724@utexas.edu>
2023-01-19 07:24:31 -0600

    Time: 1 ms (55.63%), Space: 7.4 MB (82.29%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int minStartValue(vector<int>& nums) {
-        int start_value = 1;
-        while (true){
-            int total = start_value;
+        int n = nums.size(), m = 100;
+        int left = 1, right = n * m + 1;
+        while (left < right){
+            int mid = (left + right) >> 1;
+            int total = mid;
             bool is_valid = true;
             
-            for (const int& num: nums) {
+            for (const int &num : nums){
                 total += num;
-                if (total < 1) {is_valid = false; break;}
+                if (total < 1) is_valid = false;
             }
             
-            if (is_valid) return start_value;
-            else ++start_value;
+            if (is_valid) right = mid;
+            else left = mid + 1;
         }
+        
+        return left;
     }
 };
