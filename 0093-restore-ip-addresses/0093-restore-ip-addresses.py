d7a7cf80029922609294582c4645d7ffb4142f7f
Andrew Kim <ksb0724@utexas.edu>
2023-01-21 10:08:16 -0600

    Time: 37 ms (80.65%), Space: 13.8 MB (79.81%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,38 +1,20 @@
 class Solution:
-    def valid(self, s: str, start: int, length: int):
-        return length == 1 or (s[start] != '0' and (length < 3 or s[start: start + length] <= "255"))
-    
-    def helper(self, s: str, startIndex: int, dots: List[int], ans: List[str]) -> None:
-        remainingLength = len(s) - startIndex
-        remainingNumberOfIntegers = 4 - len(dots)
-        
-        if (remainingLength > remainingNumberOfIntegers * 3 or remainingLength < remainingNumberOfIntegers):
-            return
-        
-        if len(dots) == 3:
-            if self.valid(s, startIndex, remainingLength):
-                temp, last = "", 0
-                for dot in dots:
-                    temp += s[last:last+dot]
-                    last += dot
-                    temp += '.'
-                temp += s[startIndex:]
-                
-                ans.append(temp)
-                
-            return
-        
-        for curPos in range(1, max(3, remainingLength)):
-            if curPos <= 3 and curPos <= remainingLength:
-                dots.append(curPos)
-                if self.valid(s, startIndex, curPos):
-                    self.helper(s, startIndex + curPos, dots, ans)
-                dots.pop()
-            else:
-                break
+    def valid(self, s: str, start: int, length: int) -> bool:
+        return length == 1 or (s[start] != '0' and (length < 3 or s[start:start+length] <= "255"))
     
     def restoreIpAddresses(self, s: str) -> List[str]:
-        ans = []
-        self.helper(s, 0, [], ans)
+        ans, length = [], len(s)
+        
+        for len1 in range(max(1, length - 9), min(3, length - 3)+1):
+            if not self.valid(s, 0, len1):
+                continue
+                
+            for len2 in range(max(1, length - len1 - 6), min(3, length - len1 - 2)+1):
+                if not self.valid(s, len1, len2):
+                    continue
+                for len3 in range(max(1, length - len1 - len2 - 3), min(3, length - len1 - len2 - 1)+1):
+                    if self.valid(s, len1 + len2, len3) and self.valid(s, len1 + len2 + len3, length - len1 - len2 - len3):
+                        ans.append(s[0:len1] + '.' + s[len1:len1+len2] + '.' + s[len1 + len2:len1 + len2 + len3] + '.' + s[len1 + len2 + len3:])
+        
         
         return ans
