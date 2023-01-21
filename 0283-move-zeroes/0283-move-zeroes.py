a3345b7c641614d562591088c7256c6d19a75289
Andrew Kim <ksb0724@utexas.edu>
2023-01-21 13:12:22 -0600

    Time: 347 ms (43.01%), Space: 15.6 MB (58.61%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,10 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        left, right, n = 0, 1, len(nums)
         
-        lastNonZeroFoundAt = 0
+        while right < n:
+            if nums[left] == 0 and nums[right] != 0:
+                nums[left], nums[right] = nums[right], nums[left]
+            while nums[left] != 0 and left < right:
+                left += 1
+            right += 1
         
-        for cur in range(len(nums)):
-            if nums[cur] != 0:
-                nums[lastNonZeroFoundAt], nums[cur] = nums[cur], nums[lastNonZeroFoundAt]
-                lastNonZeroFoundAt += 1
+            
