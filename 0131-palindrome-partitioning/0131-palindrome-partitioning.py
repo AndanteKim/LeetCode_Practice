f53ea55f006855e5bd9466dce93bccdab31f4d95
Andrew Kim <ksb0724@utexas.edu>
2023-01-22 02:16:06 -0600

    Time: 1840 ms (16.17%), Space: 28.9 MB (96.27%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -1,21 +1,18 @@
 class Solution:
-    def isPalindrome(self, s: str, low: int, high: int) -> bool:
-        while low < high:
-            if s[low] != s[high]:
-                return False
-            low += 1
-            high -= 1
-        return True
-    
-    def dfs(self, ans: List[List[str]], s: str, start: int, currentList: List[str]) -> None:
+    def dfs(self, ans: List[List[str]], s: str, start: int, currentList: List[str], dp: List[List[bool]]) -> None:
         if start >= len(s): ans.append(currentList.copy())
+        
         for end in range(start, len(s)):
-            if self.isPalindrome(s, start, end):
-                currentList.append(s[start: end + 1])
-                self.dfs(ans, s, end + 1, currentList)
+            if s[start] == s[end] and (end - start <= 2 or dp[start + 1][end - 1]):
+                dp[start][end] = True
+                currentList.append(s[start : end + 1])
+                self.dfs(ans, s, end + 1, currentList, dp)
                 currentList.pop()
     
+    
     def partition(self, s: str) -> List[List[str]]:
-        ans, currentList = [], []
-        self.dfs(ans, s, 0, currentList)
-        return ans
+        length = len(s)
+        dp = [[False] * length for _ in range(length)]
+        answer, currentList = [], []
+        self.dfs(answer, s, 0, currentList, dp)
+        return answer
