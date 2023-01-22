d53806269890652228dbc531f3b68791e03e5b94
Andrew Kim <ksb0724@utexas.edu>
2023-01-22 13:08:17 -0600

    Time: 59 ms (12.64%), Space: 13.9 MB (9.24%) - LeetHub

--- 1056-confusing-number/1056-confusing-number.py
+++ 1056-confusing-number/1056-confusing-number.py
@@ -1,14 +1,16 @@
 class Solution:
     def confusingNumber(self, n: int) -> bool:
         
-        invert_map = {"0":"0", "1":"1", "8":"8", "6":"9","9":"6"}
-        rotated_number = []
+        invert_map = {0:0, 1:1, 8:8, 6:9,9:6}
+        invert_number = 0
+        n_copy = n
         
-        for ch in str(n):
-            if ch not in invert_map:
+        while n_copy:
+            res = n_copy % 10
+            if res not in invert_map:
                 return False
-            rotated_number.append(invert_map[ch])
+            
+            invert_number = invert_number * 10 + invert_map[res]
+            n_copy //= 10
         
-        rotated_number = "".join(rotated_number)
-        
-        return int(rotated_number[::-1]) != n
+        return invert_number != n
