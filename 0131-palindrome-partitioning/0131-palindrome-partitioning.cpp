c56ef8b884caa3b34824c5470844464d406c5ac7
Andrew Kim <ksb0724@utexas.edu>
2023-01-22 02:40:46 -0600

    Time: 113 ms (86.80%), Space: 49.2 MB (96.39%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int low, int high){
-        while (low < high){
-            if (s[low++] != s[high--]) return false;
-        }
-        return true;
-    }
-    
-    void dfs(vector<vector<string>> &answer, string &s, int start, vector<string>& currentList){
+    void dfs(vector<vector<string>> &answer, string &s, int start, vector<string> &currentList, vector<vector<bool>> &dp){
         if (start >= s.size()) answer.push_back(currentList);
-        
         for (int end = start; end < s.size(); ++end){
-            if (isPalindrome(s, start, end)){
+            if(s[start] == s[end] && (end - start <= 2 || dp[start + 1][end - 1])){
+                dp[start][end] = true;
                 currentList.push_back(s.substr(start, end - start + 1));
-                dfs(answer, s, end + 1, currentList);
+                dfs(answer, s, end + 1, currentList, dp);
                 currentList.pop_back();
             }
         }
     }
     
     vector<vector<string>> partition(string s) {
+        int len = s.size();
+        vector<vector<bool>> dp(len, vector<bool> (len, false));
         vector<vector<string>> answer;
         vector<string> currentList;
-        dfs(answer, s, 0, currentList);
-        
+        dfs(answer, s, 0, currentList, dp);
         return answer;
     }
 };
