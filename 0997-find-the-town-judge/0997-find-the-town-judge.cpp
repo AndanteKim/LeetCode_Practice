3e2f0208342893f6b87c426d8b9748393183dc29
Andrew Kim <ksb0724@utexas.edu>
2023-01-23 02:19:47 -0600

    Time: 184 ms (83.22%), Space: 60.6 MB (95.56%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
         if (trust.size() < n - 1) return -1;
-        int indegree[n+1], outdegree[n+1];
-        fill_n(indegree, n+1, 0);
-        fill_n(outdegree, n+1, 0);
-        
+        int scores[n+1];
+        fill_n(scores, n+1, 0);
         
         for (const auto &t : trust){
-            ++outdegree[t[0]];
-            ++indegree[t[1]];
+            --scores[t[0]];
+            ++scores[t[1]];
         }
         
         for (int i = 1; i <= n; ++i){
-            if (indegree[i] == n - 1 && outdegree[i] == 0) return i;
+            if (scores[i] == n - 1) return i;
         }
         return -1;
     }
