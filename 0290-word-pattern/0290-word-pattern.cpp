af1d21476dc3f72890b7b325ad337402ed05c3e8
Andrew Kim <ksb0724@utexas.edu>
2023-01-23 08:11:42 -0600

    Time: 0 ms (100.00%), Space: 6.3 MB (97.00%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        unordered_map<char, string> map_char;
-        unordered_map<string, char> map_word;
+        unordered_map<string, int> map_index;
         stringstream ss(s);
         string word;
         vector<string> str;
@@ -11,16 +10,14 @@ public:
         if (str.size() != pattern.size()) return false;
         
         for (int i = 0; i < pattern.size(); ++i){
-            if (map_char.find(pattern[i]) == map_char.end()) {
-                if (map_word.find(str[i]) != map_word.end()) return false;
-                else{
-                    map_char[pattern[i]] = str[i];
-                    map_word[str[i]] = pattern[i];
-                }
-            }
-            else{
-                if (map_char[pattern[i]] != str[i]) return false;
-            }
+            string char_key = "char_";
+            char_key.push_back(pattern[i]);
+            string char_word = "word_" + str[i];
+            
+            if (map_index.find(char_key) == map_index.end()) map_index[char_key] = i;
+            if (map_index.find(char_word) == map_index.end()) map_index[char_word] = i;
+            
+            if (map_index[char_key] != map_index[char_word]) return false;
         }
         
         return true;
