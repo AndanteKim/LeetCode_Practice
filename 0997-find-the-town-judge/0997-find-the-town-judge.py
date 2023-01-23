a1f1329d087c985e9337013b6dd403a59d4b2ee7
Andrew Kim <ksb0724@utexas.edu>
2023-01-23 01:36:03 -0600

    Time: 760 ms (77.52%), Space: 18.8 MB (99.48%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -3,15 +3,14 @@ class Solution:
         if len(trust) < n - 1:
             return -1
         
-        indegree = [0] * (n+1)
-        outdegree = [0] * (n+1)
+        trust_scores = [0] * (n+1)
         
         for a, b in trust:
-            outdegree[a] += 1
-            indegree[b] += 1
+            trust_scores[a] -= 1
+            trust_scores[b] += 1
         
-        for i in range(1, n+1):
-            if indegree[i] == n - 1 and outdegree[i] == 0:
+        for i, score in enumerate(trust_scores[1:], start = 1):
+            if score == n - 1:
                 return i
             
         return -1
