a216bbe67a6e6c65591b2507e8134c899dd3c902
Andrew Kim <ksb0724@utexas.edu>
2023-01-23 01:29:19 -0600

    Time: 1270 ms (33.30%), Space: 18.9 MB (90.33%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,21 +1,17 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        ans = -1
-        trust_give = {i:set() for i in range(1, n+1)}
-        trust_receive = {i:set() for i in range(1, n+1)}
+        if len(trust) < n - 1:
+            return -1
         
-        for a_i, b_i in trust:
-            trust_give[a_i].add(b_i)
-            trust_receive[b_i].add(a_i)
+        indegree = [0] * (n+1)
+        outdegree = [0] * (n+1)
         
-        candidates = []
+        for a, b in trust:
+            outdegree[a] += 1
+            indegree[b] += 1
         
-        for key, val in trust_receive.items():
-            if len(val) == n-1:
-                candidates.append(key)
-        
-        for candidate in candidates:
-            if trust_give[candidate] == set():
-                ans = candidate
-        
-        return ans
+        for i in range(1, n+1):
+            if indegree[i] == n - 1 and outdegree[i] == 0:
+                return i
+            
+        return -1
