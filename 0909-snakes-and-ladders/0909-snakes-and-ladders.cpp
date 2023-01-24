4cc5a25d520994ccb43a05c940a1062f3c8b4495
Andrew Kim <ksb0724@utexas.edu>
2023-01-24 01:46:39 -0600

    Time: 34 ms (51.50%), Space: 13.4 MB (43.99%) - LeetHub

--- 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
+++ 0909-snakes-and-ladders/0909-snakes-and-ladders.cpp
@@ -2,30 +2,30 @@ class Solution {
 public:
     int snakesAndLadders(vector<vector<int>>& board) {
         int n = board.size(), label = 1;
-        vector<pair<int, int>> cells(n*n + 1);
+        vector<pair<int, int>> cells(n*n+1);
         vector<int> columns(n);
         iota(columns.begin(), columns.end(), 0);
-        for(int row = n-1; row >= 0; --row){
-            for(int col : columns){
+        for (int row = n-1; row >= 0; --row){
+            for (int col: columns){
                 cells[label++] = {row, col};
             }
             reverse(columns.begin(), columns.end());
         }
-        vector<int> dist(n*n + 1, -1);
-        queue<int> q;
-        dist[1] = 0;
-        q.push(1);
         
+        vector<int> dist(n * n +1, -1);
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+        dist[1] = 0;
+        q.emplace(0,1);
         while (!q.empty()){
-            int curr = q.front();
+            auto [d, curr] = q.top();
             q.pop();
+            if (d != dist[curr]) continue;
             for (int next = curr + 1; next <= min(curr + 6, n * n); ++next){
                 auto [row, col] = cells[next];
-                int destination = board[row][col] != -1 ? board[row][col] : next;
-                
-                if (dist[destination] == -1){
+                int destination = board[row][col] != -1? board[row][col] : next;
+                if (dist[destination] == -1 || dist[curr] + 1 < dist[destination]){
                     dist[destination] = dist[curr] + 1;
-                    q.push(destination);
+                    q.emplace(dist[destination], destination);
                 }
             }
         }
