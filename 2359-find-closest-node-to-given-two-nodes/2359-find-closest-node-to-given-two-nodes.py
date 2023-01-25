a6054b78434b3054f10f2e8986c70a0fa3903e68
Andrew Kim <ksb0724@utexas.edu>
2023-01-25 11:02:04 -0600

    Time: 2188 ms (28.16%), Space: 123.9 MB (28.16%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,30 +1,22 @@
 class Solution:
-    def bfs(self, startNode: int, edges: int, dist: List[int]) -> None:
-        n = len(edges)
-        q = deque([startNode])
+    def dfs(self, node: int, edges: List[int], dist: List[int], visit: List[bool]) -> None:
+        visit[node] = True
+        neighbor = edges[node]
         
-        visit = [False] * n
-        dist[startNode] = 0
+        if neighbor != -1 and not visit[neighbor]:
+            dist[neighbor] = 1 + dist[node]
+            self.dfs(neighbor, edges, dist, visit)
         
-        while q:
-            node = q.popleft()
-            
-            if visit[node]:
-                continue
-                
-            visit[node] = True
-            neighbor = edges[node]
-            if neighbor != -1 and not visit[neighbor]:
-                dist[neighbor] = 1 + dist[node]
-                q.append(neighbor)
         
 
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
         n = len(edges)
         dist1, dist2 = [float('inf')] * n, [float('inf')] * n
+        visit1, visit2 = [False] * n, [False] * n
+        dist1[node1], dist2[node2] = 0, 0
         
-        self.bfs(node1, edges, dist1)
-        self.bfs(node2, edges, dist2)
+        self.dfs(node1, edges, dist1, visit1)
+        self.dfs(node2, edges, dist2, visit2)
         
         minDistNode, minDistTillNow = -1, float('inf')
         
