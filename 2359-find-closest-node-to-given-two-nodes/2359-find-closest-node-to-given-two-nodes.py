a1b42872f69e3340762d8bd73aeb9ff8e1c5b920
Andrew Kim <ksb0724@utexas.edu>
2023-01-25 11:30:16 -0600

    Time: 2575 ms (26.44%), Space: 42.9 MB (53.45%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,29 +1,21 @@
 class Solution:
-    def dfs(self, node: int, edges: List[int], dist: List[int], visit: List[bool]) -> None:
-        visit[node] = True
-        neighbor = edges[node]
-        
-        if neighbor != -1 and not visit[neighbor]:
-            dist[neighbor] = 1 + dist[node]
-            self.dfs(neighbor, edges, dist, visit)
-        
-        
-
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
-        n = len(edges)
-        dist1, dist2 = [float('inf')] * n, [float('inf')] * n
-        visit1, visit2 = [False] * n, [False] * n
-        dist1[node1], dist2[node2] = 0, 0
+        def calc_dist(node):
+            dist = defaultdict(lambda: -1)
+            dist[node] = 0
+            
+            while edges[node] != -1 and dist[edges[node]] == -1:
+                dist[edges[node]] = dist[node] + 1
+                node = edges[node]
+            return dist
         
-        self.dfs(node1, edges, dist1, visit1)
-        self.dfs(node2, edges, dist2, visit2)
+        dist1, dist2 = calc_dist(node1), calc_dist(node2)
+        min_dist = float('inf')
+        res = - 1
         
-        minDistNode, minDistTillNow = -1, float('inf')
-        
-        for currNode in range(n):
-            if (minDistTillNow > max(dist1[currNode], dist2[currNode])):
-                minDistNode = currNode
-                minDistTillNow = max(dist1[currNode], dist2[currNode])
-        
-        return minDistNode
-        
+        for n in range(len(edges)):
+            max_dist = max(dist1[n], dist2[n])
+            if dist1[n] != -1 and dist2[n] != -1 and min_dist > max_dist:
+                min_dist = max_dist
+                res = n
+        return res
