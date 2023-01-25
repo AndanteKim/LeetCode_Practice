66f6a9fcebbf4d0967b98bf3798568ce4b884370
Andrew Kim <ksb0724@utexas.edu>
2023-01-25 11:46:54 -0600

    Time: 468 ms (33.52%), Space: 97.9 MB (69.53%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    void bfs(int startNode, vector<int> &edges, vector<int> &dist){
-        int n = edges.size();
-        
-        queue<int> q;
-        q.push(startNode);
-        vector<bool> visit(n);
-        dist[startNode] = 0;
-        
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            
-            if (visit[node]) continue;
-            
-            visit[node] = true;
-            int neighbor = edges[node];
-            if (neighbor != -1 && !visit[neighbor]){
-                dist[neighbor] = 1 + dist[node];
-                q.push(neighbor);
-            }
+    void dfs(int node, vector<int>& edges, vector<int>& dist, vector<bool>& visit){
+        visit[node] = true;
+        int neighbor = edges[node];
+        if (neighbor != -1 && !visit[neighbor]){
+            dist[neighbor] = dist[node] + 1;
+            dfs(neighbor, edges, dist, visit);
         }
+        
     }
     
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
-        
         vector<int> dist1(n, numeric_limits<int>::max()), dist2(n, numeric_limits<int>::max());
-        bfs(node1, edges, dist1);
-        bfs(node2, edges, dist2);
+        vector<bool> visit1(n), visit2(n);
+        dist1[node1] = 0, dist2[node2] = 0;
+        
+        dfs(node1, edges, dist1, visit1);
+        dfs(node2, edges, dist2, visit2);
         
         int minDistNode = -1, minDistTillNow = numeric_limits<int>::max();
-        
         for (int currNode = 0; currNode < n; ++currNode){
-            if (minDistTillNow > max(dist1[currNode], dist2[currNode])){
+            if(minDistTillNow > max(dist1[currNode], dist2[currNode])){
                 minDistNode = currNode;
                 minDistTillNow = max(dist1[currNode], dist2[currNode]);
             }
