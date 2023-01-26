16e1b070b755b895a894fb3155e8f52f7aee3612
Andrew Kim <ksb0724@utexas.edu>
2023-01-26 09:36:10 -0600

    Time: 236 ms, Memory: 15.3 MB - LeetHub

---
+++ missing-number/missing-number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        total = (n * (n+1)) // 2
+        
+        for num in nums:
+            total -= num
+        return total
