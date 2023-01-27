8e0f0005202b4c6c07bc4abdcb12c8174a90173f
Andrew Kim <ksb0724@utexas.edu>
2023-01-27 05:56:56 -0600

    Time: 208 ms (83.61%), Space: 42.8 MB (83.05%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,24 +1,32 @@
 class Solution {
+    bool dfs(const string& word, int length, vector<bool>& visited,
+             const unordered_set<string>& dictionary) {
+        if (length == word.length()) {
+            return true;
+        }
+        if (visited[length]) {
+            return false;
+        }
+        visited[length] = true;
+        for (int i = word.length() - (length == 0 ? 1 : 0); i > length; --i) {
+            if (dictionary.count(word.substr(length, i - length)) &&
+                dfs(word, i, visited, dictionary)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
-        // DP with hashmap
-        set<string> dictionary(words.begin(), words.end());
-        vector<string> ans;
-        
-        for (const string& word: words){
-            const int n = word.size();
-            vector<bool> dp(n+1);
-            dp[0] = true;
-            
-            for (int i = 1; i <= n; ++i){
-                for (int j = (i == n? 1 : 0); !dp[i] && j < i; ++j){
-                    dp[i] = dp[j] && dictionary.count(word.substr(j, i - j));
-                }
+        unordered_set<string> dictionary(words.begin(), words.end());
+        vector<string> answer;
+        for (const string& word : words) {
+            vector<bool> visited(word.length());
+            if (dfs(word, 0, visited, dictionary)) {
+                answer.push_back(word);
             }
-            
-            if (dp[n]) ans.push_back(word);
         }
-        
-        return ans;
+        return answer;
     }
 };
