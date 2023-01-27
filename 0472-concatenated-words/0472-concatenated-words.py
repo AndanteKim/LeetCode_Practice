438892375f885d151f8b1780785b369896dc9dab
Andrew Kim <ksb0724@utexas.edu>
2023-01-27 01:11:47 -0600

    Time: 348 ms (90.87%), Space: 16.6 MB (90.74%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.py
+++ 0472-concatenated-words/0472-concatenated-words.py
@@ -1,19 +1,22 @@
 class Solution:
+    def dfs(self, word: str, length: int, visited: List[bool], dictionary: Set[str]) -> bool:
+        if length == len(word):
+            return True
+        if visited[length]:
+            return False
+        visited[length] = True
+        start = 1 if length == 0 else 0
+        for i in range(len(word) - start, length, -1):
+            if word[length:i] in dictionary and self.dfs(word, i, visited, dictionary):
+                return True
+        return False
+    
     def findAllConcatenatedWordsInADict(self, words: List[str]) -> List[str]:
         
         dictionary = {word for word in words}
         ans = []
         for word in words:
-            n = len(word)
-            dp = [False] * (n + 1)
-            dp[0] = True
-            for i in range(1, n+1):
-                start = 1 if i == n else 0
-                for j in range(start, i):
-                    if dp[i]:
-                        continue
-                    dp[i] = dp[j] and word[j:i] in dictionary
-                    
-            if dp[n]:
+            visited = [False] * len(word)
+            if (self.dfs(word, 0, visited, dictionary)):
                 ans.append(word)
         return ans
