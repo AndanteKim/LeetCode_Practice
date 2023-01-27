a5397d4cdea47fa2cc8fcd6dcad4492df6205f6b
Andrew Kim <ksb0724@utexas.edu>
2023-01-27 06:15:41 -0600

    Time: 273 ms (64.25%), Space: 226 MB (23.84%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.cpp
+++ 0472-concatenated-words/0472-concatenated-words.cpp
@@ -1,26 +1,55 @@
 class Solution {
-    bool dfs(const string& word, int length, vector<bool>& visited, const unordered_set<string>& dictionary){
-        if (length == word.size()) return true;
-        
-        if (visited[length]) return false;
-        
-        visited[length] = true;
-        for (int i = word.size() - (length == 0? 1:0); i > length; --i){
-            if (dictionary.count(word.substr(length, i - length)) && dfs(word, i, visited, dictionary)) return true;
+    struct trie{
+        struct trie* arr[26];
+        bool end = false;
+        trie(){
+            memset(arr, 0, sizeof(arr));
+            end = false;
         }
-        return false;
+    };
+    
+    trie* root;
+    
+    void insert(string &s){
+        trie* node = root;
+        
+        for (int i = s.size()-1; i >= 0; --i){
+            char ch = s[i];
+            if (!node -> arr[ch - 'a']) node -> arr[ch - 'a'] = new trie();
+            
+            node = node -> arr[ch - 'a'];
+        }
+        node -> end = true;
     }
+    
 public:
     vector<string> findAllConcatenatedWordsInADict(vector<string>& words) {
+        sort(words.begin(), words.end(), [](const string &w1, string &w2) {return w1.size() < w2.size();});
         
-        unordered_set<string> dictionary(words.begin(), words.end());
-        vector<string> ans;
-        
-        for (const string& word: words){
-            vector<bool> visited(word.size());
-            if (dfs(word, 0, visited, dictionary)) ans.push_back(word);
+        vector<string> ret;
+        root = new trie();
+        for (string w: words){
+            if (w.size() == 0) continue;
+            
+            vector<bool> dp(w.size()+1);
+            dp[0] = true;
+            
+            for (int i = 0; i < w.size(); ++i){
+                trie *node = root;
+                for (int j = i; j >= 0; --j){
+                    if (node -> arr[w[j] - 'a'] == NULL) break;
+                    node = node -> arr[w[j] - 'a'];
+                    if (dp[j] && node -> end) {
+                        dp[i+1] = true;
+                        break;
+                    }
+                }
+            }
+            
+            if (dp[w.size()]) ret.push_back(w);
+            insert(w);
         }
         
-        return ans;
+        return ret;
     }
 };
