aa726ed29ec9429655f24cc343e9eb1fe34749ea
Andrew Kim <ksb0724@utexas.edu>
2023-01-28 02:18:51 -0600

    Time: 163 ms (84.95%), Space: 18.9 MB (75.92%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.py
@@ -1,29 +1,28 @@
 class SummaryRanges:
 
     def __init__(self):
-        self.values = set()
+        self.intervals = [[float('-inf'), float('-inf')], [float('inf'), float('inf')]]
 
     def addNum(self, value: int) -> None:
-        self.values.add(value)
+        i = bisect.bisect(self.intervals, [value])
+        
+        # ps: previous interval start, pe: previous interval end
+        # ns: next interval start, ne: next interval end
+        ps, pe = self.intervals[i-1]
+        ns, ne = self.intervals[i]
+        
+        if pe == value - 1 and ns == value + 1:
+            self.intervals = self.intervals[:i-1] + [[ps, ne]] + self.intervals[i+1:]
+        elif pe == value - 1:
+            self.intervals[i-1][1] = value
+        elif ns == value + 1:
+            self.intervals[i][0] = value
+        elif pe < value - 1 and ns > value + 1:
+            self.intervals = self.intervals[:i] + [[value, value]] + self.intervals[i:]
 
     def getIntervals(self) -> List[List[int]]:
-        if self.values == set():
-            return set()
+        return self.intervals[1:-1]
         
-        intervals = []
-        left = right = -1
-        
-        for value in sorted(self.values):
-            if left < 0:
-                left = right = value
-            elif value == right + 1:
-                right = value
-            else:
-                intervals.append([left, right])
-                left = right = value
-        intervals.append([left, right])
-        return intervals
-
 # Your SummaryRanges object will be instantiated and called as such:
 # obj = SummaryRanges()
 # obj.addNum(value)
