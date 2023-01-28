d31211d2393b8cf86fc157a9d0496a41f6a9f32f
Andrew Kim <ksb0724@utexas.edu>
2023-01-28 02:33:15 -0600

    Time: 70 ms (63.53%), Space: 33.2 MB (76.08%) - LeetHub

--- 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
+++ 0352-data-stream-as-disjoint-intervals/0352-data-stream-as-disjoint-intervals.cpp
@@ -1,26 +1,32 @@
 class SummaryRanges {
-    set<int> values;
+    map<int, int> intervals;
 public:
     SummaryRanges() {}
     
+    
     void addNum(int value) {
-        values.insert(value);
+        int left = value, right = value;
+        auto small_entry = intervals.upper_bound(value);
+        if (small_entry != intervals.begin()){
+            auto max_entry = small_entry;
+            --max_entry;
+            if (max_entry -> second >= value) return;
+            if (max_entry -> second == value - 1) left = max_entry -> first;
+        }
+        
+        if (small_entry != intervals.end() && small_entry -> first == value + 1){
+            right = small_entry -> second;
+            intervals.erase(small_entry);
+        }
+        intervals[left] = right;
     }
     
     vector<vector<int>> getIntervals() {
-        if (values.empty()) return {};
-        vector<vector<int>> intervals;
-        int left = -1, right = -1;
-        for (const int& value : values){
-            if (left < 0) left = right = value;
-            else if (value == right + 1) right = value;
-            else{
-                intervals.push_back({left, right});
-                left = right = value;
-            }
+        vector<vector<int>> answer;
+        for(const auto &p: intervals){
+            answer.push_back({p.first, p.second});
         }
-        intervals.push_back({left, right});
-        return intervals;
+        return answer;
     }
 };
 
