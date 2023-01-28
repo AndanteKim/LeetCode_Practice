ad2f3640fbe458430081efa96baa2da99db945f2
Andrew Kim <ksb0724@utexas.edu>
2023-01-28 10:01:31 -0600

    Time: 168 ms (61.32%), Space: 67.6 MB (78.26%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,14 +1,20 @@
 class Solution {
-public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        unordered_map<int, int> counts;
-        counts[0] = 1;
-        int ans = 0, curr = 0;
-        for (const auto& num : nums){
-            curr += num % 2;
-            ans += counts[curr - k];
-            ++counts[curr];
+    int atMost(vector<int>& nums, int k){
+        int ans = 0, left = 0;
+        
+        for (int right = 0; right < nums.size(); ++right){
+            k -= nums[right] & 1;
+            while (k < 0){
+                k += nums[left] & 1;
+                ++left;
+            }
+            ans += right - left + 1;
         }
         return ans;
+    }
+    
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        return atMost(nums, k) - atMost(nums, k-1);
     }
 };
