98f90b180baf1dfb778a11c7ce6d90c1a4b11ce6
Andrew Kim <ksb0724@utexas.edu>
2023-01-29 07:10:50 -0600

    Time: 448 ms (99.87%), Space: 134.4 MB (97.86%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
-        unordered_map<int, int> lost_lookup;
-        
-        for (const auto &match : matches) {
-            ++lost_lookup[match[1]];
-            lost_lookup[match[0]] += 0;
+        int lost_cnt[100001];
+        memset(lost_cnt, -1, sizeof(lost_cnt));
+        for (const vector<int>& match : matches) {
+            if (lost_cnt[match[0]] == -1) lost_cnt[match[0]] = 0;
+            if (lost_cnt[match[1]] == -1) lost_cnt[match[1]] = 1;
+            else ++lost_cnt[match[1]];
         }
         vector<int> zero_cnt, one_cnt;
-        
-        for (const auto&[loser, cnt] : lost_lookup) {
-            if (cnt == 0) zero_cnt.push_back(loser);
-            else if (cnt == 1) one_cnt.push_back(loser);
+        for (int i = 0; i <= 100000; ++i){
+            if (lost_cnt[i] == 0) zero_cnt.push_back(i);
+            else if (lost_cnt[i] == 1) one_cnt.push_back(i);
         }
-        sort(zero_cnt.begin(), zero_cnt.end());
-        sort(one_cnt.begin(), one_cnt.end());
         return {zero_cnt, one_cnt};
     }
 };
