b2ecb6c87ec2256f5d30180a30ff294d78aac7c6
Andrew Kim <ksb0724@utexas.edu>
2023-01-30 10:45:03 -0600

    Time: 44 ms (60.81%), Space: 19.5 MB (88.25%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,15 +1,21 @@
 class Solution {
+private:
+    string CountSort(string s){
+        int n = s.size(), counter[26] = {0};
+        
+        for (const char&c :s) ++counter[c - 'a'];
+        
+        string t;
+        for (int i = 0; i < 26; ++i) t += string(counter[i], i + 'a');
+        return t;
+    }
+    
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> groups;
-        for (const string& s: strs){
-            string key = s;
-            sort(key.begin(), key.end());
-            groups[key].push_back(s);
-        }
-        
+        for (const string& s: strs) groups[CountSort(s)].push_back(s);
         vector<vector<string>> ans;
-        for (const auto&[key, value] : groups) ans.push_back(value);    
+        for (const auto &[key, value] : groups) ans.push_back(value);  
         return ans;
     }
 };
