6ee6448c1c28a4cf4afd7b440660ab0145167ac0
Andrew Kim <ksb0724@utexas.edu>
2023-01-31 08:06:58 -0600

    Time: 25 ms (100.00%), Space: 20 MB (62.66%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,30 +1,36 @@
 class Solution {
-public:
-    int findMaxScore(vector<pair<int, int>>& ageScorePair){
-        int n = ageScorePair.size();
-        int answer = 0;
-        vector<int> dp(n);
-        for (int i = 0; i < n; ++i){
-            dp[i] = ageScorePair[i].second;
-            answer = max(answer, dp[i]);
+    int queryBit(vector<int> &BIT, int age){
+        int maxScore = INT_MIN;
+        for (int i = age; i > 0; i -= i & (-i)){
+            maxScore = max(maxScore, BIT[i]);
         }
-        
-        for (int i = 0; i < n; ++i){
-            for (int j = i-1; j >= 0; --j){
-                if (ageScorePair[i].second >= ageScorePair[j].second) dp[i] = max(dp[i], ageScorePair[i].second + dp[j]);
-            }
-            answer = max(answer, dp[i]);
-        }
-        return answer;
+        return maxScore;
     }
     
+    void updateBIT(vector<int> &BIT, int age, int currentBest){
+        for (int i = age; i < BIT.size(); i += i & (-i)) BIT[i] = max(BIT[i], currentBest);
+    }
+    
+public:
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
         vector<pair<int, int>> ageScorePair;
         for (int i = 0; i < scores.size(); ++i){
-            ageScorePair.push_back({ages[i], scores[i]});
+            ageScorePair.push_back({scores[i], ages[i]});
+        }
+        sort(ageScorePair.begin(), ageScorePair.end());
+        
+        int highestAge = 0;
+        for (const int& age : ages) highestAge = max(highestAge, age);
+        
+        vector<int> BIT(highestAge + 1, 0);
+        
+        int answer= INT_MIN;
+        for (const pair<int, int> ageScore: ageScorePair){
+            int currentBest = ageScore.first + queryBit(BIT, ageScore.second);
+            updateBIT(BIT, ageScore.second, currentBest);
+            answer = max(answer, currentBest);
         }
         
-        sort(ageScorePair.begin(), ageScorePair.end());
-        return findMaxScore(ageScorePair);
+        return answer;
     }
 };
