a0e6c86d7717d1b28e86d01597866ed987e4fcad
Andrew Kim <ksb0724@utexas.edu>
2023-01-31 08:01:11 -0600

    Time: 196 ms (70.13%), Space: 19.9 MB (62.66%) - LeetHub

--- 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
+++ 1626-best-team-with-no-conflicts/1626-best-team-with-no-conflicts.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int findMaxScore(vector<vector<int>> &dp, vector<pair<int, int>>& ageScorePair, int prev, int index){
-        if (index >= ageScorePair.size()) return 0;
+    int findMaxScore(vector<pair<int, int>>& ageScorePair){
+        int n = ageScorePair.size();
+        int answer = 0;
+        vector<int> dp(n);
+        for (int i = 0; i < n; ++i){
+            dp[i] = ageScorePair[i].second;
+            answer = max(answer, dp[i]);
+        }
         
-        if (dp[prev + 1][index] != -1) return dp[prev + 1][index];
-        
-        if (prev == -1 || ageScorePair[index].second >= ageScorePair[prev].second) return dp[prev + 1][index] = max(findMaxScore(dp, ageScorePair, prev, index + 1), ageScorePair[index].second + findMaxScore(dp, ageScorePair, index, index + 1));
-        return dp[prev + 1][index] = findMaxScore(dp, ageScorePair, prev, index + 1);
+        for (int i = 0; i < n; ++i){
+            for (int j = i-1; j >= 0; --j){
+                if (ageScorePair[i].second >= ageScorePair[j].second) dp[i] = max(dp[i], ageScorePair[i].second + dp[j]);
+            }
+            answer = max(answer, dp[i]);
+        }
+        return answer;
     }
     
     int bestTeamScore(vector<int>& scores, vector<int>& ages) {
@@ -16,8 +25,6 @@ public:
         }
         
         sort(ageScorePair.begin(), ageScorePair.end());
-        
-        vector<vector<int>> dp(scores.size(), vector<int>(scores.size(), -1));
-        return findMaxScore(dp, ageScorePair, -1, 0);
+        return findMaxScore(ageScorePair);
     }
 };
