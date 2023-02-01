200ae491181de5e04f462b47ba4406f4b576385b
Andrew Kim <ksb0724@utexas.edu>
2023-02-01 16:20:28 +0900

    Time: 3 ms (83.39%), Space: 7.1 MB (78.21%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-     bool valid(string str1, string str2, int k){
-        int len1 = str1.size(), len2 = str2.size();
-        if (len1 % k > 0 || len2 % k > 0) return false;
-        else{
-            string base = str1.substr(0, k);
-            int n1 = len1 / k, n2 = len2 / k;
-            return str1 == joinWords(base, n1) && str2 == joinWords(base, n2);
-        }
-    }
-    
-    string joinWords(string str, int k){
-        string ans = "";
-        for (int i = 0; i < k; ++i) ans += str;
-        return ans;
+    int gcd(int a, int b){
+        
+        if (a % b == 0) return b;
+        return gcd(b, a % b);
     }
     
     string gcdOfStrings(string str1, string str2) {
-        int m = str1.size(), n = str2.size();
-        for (int i = min(m, n); i >= 1; --i){
-            if (valid(str1, str2, i)) return str1.substr(0, i);
-        }
-        return "";
+        if (str1 + str2 != str2 + str1) return "";
+        
+        int max_length = gcd(str1.size(), str2.size());
+        return str1.substr(0, max_length);
     }
 };
