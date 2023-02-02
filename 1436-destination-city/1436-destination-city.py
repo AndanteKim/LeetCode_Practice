c457e9e2e690295452db5702f42391a0596dd2e0
Andrew Kim <ksb0724@utexas.edu>
2023-02-02 23:12:38 +0900

    Time: 50 ms (91.72%), Space: 14 MB (5.65%) - LeetHub

--- 1436-destination-city/1436-destination-city.py
+++ 1436-destination-city/1436-destination-city.py
@@ -1,8 +1,4 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        dic = {A_i : B_i for A_i, B_i in paths}
-        
-        current_location = paths[0][0]
-        while current_location in dic.keys():
-            current_location = dic[current_location]
-        return current_location
+        A, B = map(set, zip(*paths))
+        return (B-A).pop()
