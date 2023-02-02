2251937ae4f382718378b363353cdde7f03bc809
Andrew Kim <ksb0724@utexas.edu>
2023-02-02 16:14:58 +0900

    Time: 38 ms (74.83%), Space: 13.9 MB (77.66%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.py
@@ -1,17 +1,14 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        lexico_dict = {c:i for i, c in enumerate(order)}
-        
-        for i in range(len(words)-1):
-            word, next_word = words[i], words[i+1]
-            for j in range(len(word)):
-                if lexico_dict[word[j]] > lexico_dict[next_word[j]]:
-                    return False
-                elif word[j] == next_word[j]:
-                    if j == len(next_word) - 1 and len(word) != len(next_word):
-                        return False
-                    continue
-                else:
+        ordered_map = dict()
+        for idx, val in enumerate(order):
+            ordered_map[val] = idx
+            
+        for i in range(len(words) - 1):
+            for j in range(len(words[i])):
+                if j >= len(words[i + 1]): return False
+            
+                if words[i][j] != words[i+1][j]:
+                    if ordered_map[words[i][j]] > ordered_map[words[i+1][j]]: return False
                     break
         return True
-        
