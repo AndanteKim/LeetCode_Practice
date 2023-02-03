07399792788d586aa8c9f2971bd32bd907663b57
Andrew Kim <ksb0724@utexas.edu>
2023-02-03 15:53:40 +0900

    Time: 52 ms (31.31%), Space: 13.8 MB (96.19%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,7 +1,16 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        values_list = []
-        for value in Counter(arr).values():
-            values_list.append(value)
+        k = 1000
+        freq = [0] * (2 * k + 1)
         
-        return True if len(set(values_list)) == len(values_list) else False
+        for num in arr:
+            freq[num + k] += 1
+        
+        freq.sort()
+        
+        for i in range(2 * k):
+            if freq[i] and freq[i] == freq[i+1]:
+                return False
+        
+        return True
+        
