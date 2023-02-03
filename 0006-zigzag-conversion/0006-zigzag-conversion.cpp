ab9cba5ad95b32ce90bd1a5b16a9b1c79813a2b8
Andrew Kim <ksb0724@utexas.edu>
2023-02-03 12:08:43 +0900

    Time: 806 ms (5.02%), Space: 502.3 MB (5.08%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        int n = s.size(), sections = ceil(n / (double)(2 * numRows - 2));
+        int num_cols = sections * (numRows - 1);
+        
+        vector<vector<string>> matrix(numRows, vector<string>(num_cols, " "));
+        int curr_row = 0, curr_col = 0, curr_string_index = 0;
+        
+        while (curr_string_index < n){
+            // move down
+            while (curr_row < numRows && curr_string_index < n){
+                matrix[curr_row][curr_col] = s[curr_string_index];
+                ++curr_row; ++curr_string_index;
+            }        
+            
+            curr_row -= 2;
+            ++curr_col;
+            
+            // move up
+            while (curr_row > 0 && curr_col < num_cols && curr_string_index < n){
+                matrix[curr_row][curr_col] = s[curr_string_index];
+                --curr_row;
+                ++curr_string_index;
+                ++curr_col;
+            }
+        }
+        
+        string answer = "";
+        
+        for (const vector<string>& row : matrix){
+            for (const string& elem : row) {
+                if (elem == " ") continue;
+                answer += elem;
+            }
+        }
+        
+        return answer;
+    }
+};
