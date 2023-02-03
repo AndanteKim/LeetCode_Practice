e378ef6c4d5bdd1b88799f55f367a66c4fd19910
Andrew Kim <ksb0724@utexas.edu>
2023-02-03 12:14:00 +0900

    Time: 70 ms (17.61%), Space: 33.8 MB (14.30%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,37 +1,40 @@
-class Solution:
-    def convert(self, s: str, numRows: int) -> str:
-        if numRows == 1: return s
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        int n = s.size(), sections = ceil(n / (double)(2 * numRows - 2));
+        int num_cols = sections * (numRows - 1);
         
-        n = len(s)
-        sections = ceil(n / (2 * numRows - 2.0))
-        num_cols = sections * (numRows - 1)
+        vector<vector<char>> matrix(numRows, vector<char>(num_cols, ' '));
+        int curr_row = 0, curr_col = 0, curr_string_index = 0;
         
-        matrix = [[' '] * num_cols for _ in range(numRows)]
-        
-        curr_row, curr_col = 0, 0
-        curr_string_index = 0
-        
-        
-        while curr_string_index < n:
+        while (curr_string_index < n){
+            // move down
+            while (curr_row < numRows && curr_string_index < n){
+                matrix[curr_row][curr_col] = s[curr_string_index];
+                ++curr_row; ++curr_string_index;
+            }        
             
-            # Move down
-            while curr_row < numRows and curr_string_index < n:
-                matrix[curr_row][curr_col] = s[curr_string_index]
-                curr_row += 1
-                curr_string_index += 1
+            curr_row -= 2;
+            ++curr_col;
             
-            curr_row -= 2
-            curr_col += 1
+            // move up
+            while (curr_row > 0 && curr_col < num_cols && curr_string_index < n){
+                matrix[curr_row][curr_col] = s[curr_string_index];
+                --curr_row;
+                ++curr_string_index;
+                ++curr_col;
+            }
+        }
         
-            # Move up
-            while curr_row > 0 and curr_col < num_cols and curr_string_index < n:
-                matrix[curr_row][curr_col] = s[curr_string_index]
-                curr_row -= 1
-                curr_col += 1
-                curr_string_index += 1
+        string answer = "";
         
-        answer = ""
-        for row in matrix:
-            answer += "".join(row)
+        for (const vector<char>& row : matrix){
+            for (const char& elem : row) {
+                if (elem != ' ') answer += elem;
+            }
+        }
         
-        return answer.replace(" ", "")
+        return answer;
+    }
+};
