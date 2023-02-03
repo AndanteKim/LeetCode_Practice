e0b12bd6435afb99e3eec21e422a9e50c7f1368a
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 03:56:19 +0900

    Time: 0 ms (100.00%), Space: 8.5 MB (70.24%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        vector<vector<char>> bucket(s.size() + 1);
-        unordered_map<char, int> m;
         string ans;
+        int freq[256]{0};
+        priority_queue<pair<int, char>>pq;
+        for (const char&c:s) ++freq[c];
+        for (int i = 0; i < 256; ++i) if (freq[i]) pq.push(make_pair(freq[i], i));
         
-        for (const char &c : s) ++m[c];
-        
-        for (const auto&[key, value] : m){
-            bucket[value].push_back(key);
+        while (!pq.empty()){
+            ans.append(pq.top().first, pq.top().second);
+            pq.pop();
         }
         
-        for (int i = bucket.size() - 1; i > 0; --i){
-            for (int j = 0 ; j < bucket[i].size(); ++j){
-                for (int count = i; count > 0; --count) ans += bucket[i][j];
-            }
-        }
         return ans;
     }
 };
