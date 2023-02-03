fe379d6a055169e7ef4f3ed846e98b7ce4854a2b
Andrew Kim <ksb0724@utexas.edu>
2023-02-03 12:51:30 +0900

    Time: 3 ms (98.92%), Space: 8.3 MB (69.47%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -2,36 +2,22 @@ class Solution {
 public:
     string convert(string s, int numRows) {
         if (numRows == 1) return s;
-        int n = s.size(), sections = ceil(n / (double)(2 * numRows - 2));
-        int num_cols = sections * (numRows - 1);
-        
-        vector<vector<char>> matrix(numRows, vector<char>(num_cols, ' '));
-        int curr_row = 0, curr_col = 0, curr_string_index = 0;
-        
-        while (curr_string_index < n){
-            // move down
-            while (curr_row < numRows && curr_string_index < n){
-                matrix[curr_row][curr_col] = s[curr_string_index];
-                ++curr_row; ++curr_string_index;
-            }        
-            
-            curr_row -= 2;
-            ++curr_col;
-            
-            // move up
-            while (curr_row > 0 && curr_col < num_cols && curr_string_index < n){
-                matrix[curr_row][curr_col] = s[curr_string_index];
-                --curr_row;
-                ++curr_string_index;
-                ++curr_col;
-            }
-        }
         
         string answer = "";
+        int n = s.size(), chars_in_section = 2 * (numRows - 1);
         
-        for (const vector<char>& row : matrix){
-            for (const char& elem : row) {
-                if (elem != ' ') answer += elem;
+        for (int curr_row = 0; curr_row < numRows; ++curr_row){
+            int index = curr_row;
+            
+            while(index < n){
+                answer += s[index];
+                
+                if (curr_row != 0 && curr_row != numRows - 1){
+                    int chars_in_between = chars_in_section - (2 * curr_row);
+                    int second_index = index + chars_in_between;
+                    if (second_index < n) answer += s[second_index];
+                }
+                index += chars_in_section;
             }
         }
         
