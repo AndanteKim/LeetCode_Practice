2b9b7d8f8be7961ab241f51168dfab876eb64f20
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 17:36:38 +0900

    Time: 1331 ms (6.62%), Space: 81.7 MB (5.01%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<char> sort_s1(s1.begin(), s1.end());
-        sort(sort_s1.begin(), sort_s1.end());
-        
-        vector<char> sort_s2(s2.begin(), s2.end());
-        
         int l1 = s1.size(), l2 = s2.size();
+        
+        if (l1 > l2) return false;
+        
+        unordered_map<char, int> dic_s1;
+        for (const char&c : s1) ++dic_s1[c];
         for (int i = 0; i <= l2 - l1; ++i){
-            vector<char> sub_vec(sort_s2.begin() + i, sort_s2.begin() + i + l1);
-            sort(sub_vec.begin(), sub_vec.end());
-            if (sort_s1 == sub_vec) return true;
+            unordered_map<char, int> dic_s2;
+            for (int j = 0; j < l1; ++j) ++dic_s2[s2[i + j]];
+            
+            if (dic_s1 == dic_s2) return true;
         }
+        
         return false;
     }
 };
