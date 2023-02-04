1c5ca6fcbe449c9ffdd7b28b0a2376255b76d3f6
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 21:49:04 +0900

    Time: 271 ms (98.49%), Space: 15.7 MB (65.49%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,12 +1,17 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        indices = [-1] + [idx for idx, v in enumerate(nums) if v] + [len(nums)]
+        ans = 0
         
-        cnts = defaultdict(int)
-        cnts[0] = 1
-        ans = curr = 0
+        if goal == 0:
+            for i in range(len(indices) - 1):
+                w = indices[i+1] - indices[i] - 1
+                ans += (w * (w+1)) // 2
+            return ans
         
-        for num in nums:
-            curr += num
-            ans += cnts[curr - goal]
-            cnts[curr] += 1
+        for i in range(1, len(indices) - goal):
+            j = i + goal - 1
+            left = indices[i] - indices[i-1]
+            right = indices[j+1] - indices[j]
+            ans += left * right
         return ans
