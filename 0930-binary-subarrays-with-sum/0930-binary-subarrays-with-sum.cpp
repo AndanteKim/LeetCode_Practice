65d533191ea9a93b3c8a8266c9af5ca79d6b26f2
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 22:27:49 +0900

    Time: 42 ms (74.98%), Space: 28.7 MB (91.92%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        vector<int> indices;
-        indices.push_back(-1);
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i]) indices.push_back(i);
-        }
-        indices.push_back(nums.size());
-        int ans = 0;
+        int i_lo = 0, i_hi = 0, sum_lo = 0, sum_hi = 0, ans = 0;
         
-        if (goal == 0){
-            for (int i = 0; i < indices.size()-1; ++i){
-                int w = indices[i+1] - indices[i] - 1;
-                ans += (w * (w+1)) / 2;
+        for (int i = 0; i < nums.size(); ++i){
+            sum_lo += nums[i];
+            while (i_lo < i && sum_lo > goal){
+                sum_lo -= nums[i_lo];
+                ++i_lo;
             }
-            return ans;
-        }
-        
-        for (int i = 1; i < indices.size() - goal; ++i){
-            int j = i + goal - 1;
-            int left = indices[i] - indices[i-1];
-            int right = indices[j+1] - indices[j];
-            ans += right * left;
+            
+            sum_hi += nums[i];
+            while (i_hi < i && (sum_hi > goal || sum_hi == goal && !nums[i_hi])){
+                sum_hi -= nums[i_hi];
+                ++i_hi;
+            }
+            
+            if (sum_lo == goal) ans += i_hi - i_lo + 1;
         }
         
         return ans;
