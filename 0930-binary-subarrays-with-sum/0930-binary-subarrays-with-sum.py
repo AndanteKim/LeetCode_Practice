78da2739f6840bd79c72647890694dfd9273541b
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 22:04:35 +0900

    Time: 298 ms (79.56%), Space: 14.8 MB (88.27%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -1,17 +1,20 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        indices = [-1] + [idx for idx, v in enumerate(nums) if v] + [len(nums)]
+        i_lo = i_hi = 0
+        sum_lo = sum_hi = 0
         ans = 0
-        
-        if goal == 0:
-            for i in range(len(indices) - 1):
-                w = indices[i+1] - indices[i] - 1
-                ans += (w * (w+1)) // 2
-            return ans
-        
-        for i in range(1, len(indices) - goal):
-            j = i + goal - 1
-            left = indices[i] - indices[i-1]
-            right = indices[j+1] - indices[j]
-            ans += left * right
+        for j, x in enumerate(nums):
+            sum_lo += x
+            while i_lo < j and sum_lo > goal:
+                sum_lo -= nums[i_lo]
+                i_lo += 1
+            
+            sum_hi += x
+            while i_hi < j and (sum_hi > goal or sum_hi == goal and not nums[i_hi]):
+                sum_hi -= nums[i_hi]
+                i_hi += 1
+            
+            if sum_lo == goal:
+                ans += i_hi - i_lo + 1
+            
         return ans
