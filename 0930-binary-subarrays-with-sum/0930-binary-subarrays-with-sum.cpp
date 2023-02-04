3012d8b81c13c303e6a5c1d9aad8d112e82ca15b
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 22:21:55 +0900

    Time: 26 ms (97.44%), Space: 29.7 MB (63.64%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int, int> counts;
-        counts[0] = 1;
-        int ans = 0, curr = 0;
+        vector<int> indices;
+        indices.push_back(-1);
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i]) indices.push_back(i);
+        }
+        indices.push_back(nums.size());
+        int ans = 0;
         
-        for (const int& num : nums){
-            curr += num;
-            ans += counts[curr - goal];
-            ++counts[curr];
+        if (goal == 0){
+            for (int i = 0; i < indices.size()-1; ++i){
+                int w = indices[i+1] - indices[i] - 1;
+                ans += (w * (w+1)) / 2;
+            }
+            return ans;
+        }
+        
+        for (int i = 1; i < indices.size() - goal; ++i){
+            int j = i + goal - 1;
+            int left = indices[i] - indices[i-1];
+            int right = indices[j+1] - indices[j];
+            ans += right * left;
         }
         
         return ans;
