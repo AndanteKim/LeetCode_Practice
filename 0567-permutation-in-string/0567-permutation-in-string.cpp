c005772a2f321eacfd17c11fa183af5cb8135658
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 17:44:54 +0900

    Time: 4 ms (93.47%), Space: 7.4 MB (45.48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,19 +1,27 @@
 class Solution {
+    bool matches(vector<int> &s1, vector<int> &s2){
+        for (int i = 0; i < 26; ++i) if (s1[i] != s2[i]) return false;
+        return true;
+    }
+    
 public:
     bool checkInclusion(string s1, string s2) {
         int l1 = s1.size(), l2 = s2.size();
-        
         if (l1 > l2) return false;
         
-        unordered_map<char, int> dic_s1;
-        for (const char&c : s1) ++dic_s1[c];
-        for (int i = 0; i <= l2 - l1; ++i){
-            unordered_map<char, int> dic_s2;
-            for (int j = 0; j < l1; ++j) ++dic_s2[s2[i + j]];
-            
-            if (dic_s1 == dic_s2) return true;
+        vector<int> dic_s1(26, 0), dic_s2(26, 0);
+        
+        for (int i = 0; i < l1; ++i) {
+            ++dic_s1[s1[i] - 'a'];
+            ++dic_s2[s2[i] - 'a'];
         }
         
-        return false;
+        for (int i = 0; i < l2 - l1; ++i){
+            if (matches(dic_s1, dic_s2)) return true;
+            ++dic_s2[s2[i+l1] - 'a'];
+            --dic_s2[s2[i] - 'a'];
+        }
+        
+        return matches(dic_s1, dic_s2);
     }
 };
