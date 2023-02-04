01e69f6573fc768d2d567e254f0519d78b3dd2c6
Andrew Kim <ksb0724@utexas.edu>
2023-02-04 16:54:28 +0900

    Time: 68 ms (85.53%), Space: 13.9 MB (64.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,19 +1,24 @@
 class Solution:
+    def matches(self, s1: List[int], s2: List[int]) -> bool:
+        for i in range(26):
+            if s1[i] != s2[i]:
+                return False
+        return True
+    
     def checkInclusion(self, s1: str, s2: str) -> bool:
         l1, l2 = len(s1), len(s2)
         
         if l1 > l2: return False
         
-        dic_s1 = defaultdict(int)
+        dic_s1, dic_s2 = [0] * 26, [0] * 26
+        for i in range(l1):
+            dic_s1[ord(s1[i]) - ord('a')] += 1
+            dic_s2[ord(s2[i]) - ord('a')] += 1
         
-        for c in s1:
-            dic_s1[c] += 1
-        
-        for i in range(l2 - l1 + 1):
-            dic_s2 = defaultdict(int)
+        for i in range(l2 - l1):
+            if self.matches(dic_s1, dic_s2): return True
             
-            for j in range(l1):
-                dic_s2[s2[i+j]] += 1
-            if dic_s1 == dic_s2:
-                return True
-        return False
+            dic_s2[ord(s2[i + l1]) - ord('a')] += 1
+            dic_s2[ord(s2[i]) - ord('a')] -= 1
+        
+        return self.matches(dic_s1, dic_s2)
