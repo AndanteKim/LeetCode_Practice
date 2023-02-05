76f03eba4cf203c596ccc7a3bf8f1506c902e2ec
Andrew Kim <ksb0724@utexas.edu>
2023-02-05 03:36:00 -0600

    Time: 32 ms (71.67%), Space: 13.9 MB (60.59%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,14 +1,7 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        count_s, ans = Counter(s), ""
-        remainder_lexico = count_s.keys() - Counter(order).keys()
-        for c in order:
-            while count_s[c] > 0:
-                ans += c
-                count_s[c] -= 1
-        
-        for c in remainder_lexico:
-            while count_s[c] > 0:
-                ans += c
-                count_s[c] -= 1
-        return ans
+        cnt = Counter(s);
+        ans = [cnt.pop(c, 0) * c for c in order]
+        for c, v in cnt.items():
+            ans.append(c * v)
+        return ''.join(ans)
