eb7adaa91c63be4117d44483fdf374b5388f1972
Andrew Kim <ksb0724@utexas.edu>
2023-02-05 03:40:08 -0600

    Time: 38 ms (39.33%), Space: 13.8 MB (60.59%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.py
+++ 0791-custom-sort-string/0791-custom-sort-string.py
@@ -1,7 +1,4 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        cnt = Counter(s);
-        ans = [cnt.pop(c, 0) * c for c in order]
-        for c, v in cnt.items():
-            ans.append(c * v)
-        return ''.join(ans)
+        d = {k : i for i, k in enumerate(order)}
+        return ''.join(sorted(s, key = lambda x: d.get(x, len(s) + ord(x))))
