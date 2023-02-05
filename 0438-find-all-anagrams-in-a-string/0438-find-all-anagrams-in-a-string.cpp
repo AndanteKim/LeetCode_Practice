a0daed0613dcb0911de50276d4cd0032dfcb7654
Andrew Kim <ksb0724@utexas.edu>
2023-02-05 17:12:39 +0900

    Time: 25 ms (44.29%), Space: 8.6 MB (92.32%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -3,18 +3,18 @@ public:
     vector<int> findAnagrams(string s, string p) {
         int len_p = p.size(), left = 0;
         vector<int> ans;
-        unordered_map<char, int> p_elems, s_elems;
-        for (const char& c: p) ++p_elems[c];
+        vector<int> p_elems(26, 0), s_elems(26, 0);
+        for (const char& c: p) ++p_elems[c - 'a'];
         
         for (int right = 0; right < s.size(); ++right){
-            if (s_elems[s[right]] < p_elems[s[right]]) ++s_elems[s[right]];
+            if (s_elems[s[right] - 'a'] < p_elems[s[right] - 'a']) ++s_elems[s[right] - 'a'];
             else{
-                while (left < right && s_elems[s[right]] >= p_elems[s[right]]){
-                    if (s_elems[s[left]] > 0) --s_elems[s[left]];
+                while (left < right && s_elems[s[right] - 'a'] >= p_elems[s[right] - 'a']){
+                    if (s_elems[s[left] - 'a'] > 0) --s_elems[s[left] - 'a'];
                     ++left;
                 }
                 
-                if (p_elems[s[right]] > 0) ++s_elems[s[right]];
+                if (p_elems[s[right] - 'a'] > 0) ++s_elems[s[right] - 'a'];
             }
             if (p_elems == s_elems) ans.push_back(right - len_p + 1);
         }
