58b8937b401a2e592663cbc2d20478ee68b9d5ee
Andrew Kim <ksb0724@utexas.edu>
2023-02-05 03:19:04 -0600

    Time: 5 ms (19.81%), Space: 6.2 MB (88.59%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,31 +1,10 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        unordered_map<char, int> count_s;
-        set<char> s_list, o_list;
-        string ans = "";
-        for (const char &c: s){ 
-            ++count_s[c];
-            s_list.insert(c);
-        }
+        sort(s.begin(), s.end(), [&order](char lhs, char rhs) {
+            return order.find(lhs) < order.find(rhs);
+        });
         
-        for (const char &c : order) {
-            o_list.insert(c);
-            while (count_s[c] > 0) {
-                ans += c;
-                --count_s[c];
-            }
-        }
-        set<char> diff;
-        set_difference(s_list.begin(), s_list.end(), o_list.begin(), o_list.end(), inserter(diff, diff.end()));
-        
-        for (const char &c: diff){
-            while (count_s[c] > 0){
-                ans += c;
-                --count_s[c];
-            }
-        }
-        
-        return ans;
+        return s;
     }
 };
