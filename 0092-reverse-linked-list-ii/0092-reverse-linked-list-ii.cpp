a7523178aaf346b9433b8bde6e71fa9957e4d21e
Andrew Kim <ksb0724@utexas.edu>
2023-02-07 08:37:03 -0600

    Time: 6 ms (32.21%), Space: 7.4 MB (67.33%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -9,28 +9,33 @@
  * };
  */
 class Solution {
-    bool stop = false;
-    ListNode *left_head;
-    void recurseAndReverse(ListNode* right_head, int m, int n){
-        if (n == 1) return;
-        
-        right_head = right_head -> next;
-        if (m > 1) this -> left_head = this -> left_head -> next;
-        recurseAndReverse(right_head, m - 1, n - 1);
-        if (this -> left_head == right_head || right_head -> next == this -> left_head) this -> stop = true;
-        
-        if (!stop){
-            swap(this -> left_head -> val, right_head -> val);
-            this -> left_head = this -> left_head -> next;
-        }
-    }
-    
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if (head == nullptr) return nullptr;
-        ListNode *right_head = head;
-        this -> left_head = head;
-        recurseAndReverse(right_head, left, right);
+        
+        ListNode* curr = head, * prev = nullptr;
+        while (left > 1){
+            prev = curr;
+            curr = curr -> next;
+            --left;
+            --right;
+        }
+        
+        ListNode *tail = curr, *con = prev;
+        
+        while (right > 0){
+            ListNode* third = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = third;
+            --right;
+        }
+            
+        if (con != nullptr) con -> next = prev;
+        else head = prev;
+        
+        tail -> next = curr;
+        
         return head;
     }
 };
