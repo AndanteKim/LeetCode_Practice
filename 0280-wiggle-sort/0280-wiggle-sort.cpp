3e7c1eb3a9ddd966450cf3add63476d3c2f6eb65
Andrew Kim <ksb0724@utexas.edu>
2023-02-08 01:38:41 -0600

    Time: 7 ms (95.70%), Space: 13.5 MB (75.27%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.cpp
+++ 0280-wiggle-sort/0280-wiggle-sort.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for (int i = 1; i < nums.size()-1; i += 2){
-            swap(nums[i], nums[i+1]);
+        
+        for (int i = 0; i < nums.size() - 1; ++i){
+            if ((i % 2 == 0 && nums[i] > nums[i+1]) || (i % 2 == 1 && nums[i] < nums[i+1])) swap(nums[i], nums[i+1]);
         }
     }
 };
