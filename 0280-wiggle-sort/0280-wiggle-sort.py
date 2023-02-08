a37857d1ad8ff416284779646ee10689f8cd1a94
Andrew Kim <ksb0724@utexas.edu>
2023-02-08 01:31:51 -0600

    Time: 99 ms (61.15%), Space: 15.2 MB (8.61%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.py
+++ 0280-wiggle-sort/0280-wiggle-sort.py
@@ -1,9 +1,12 @@
 class Solution:
+    def swap(self, nums: List[int], i: int, j: int) -> None:
+        nums[i], nums[j] = nums[j], nums[i]
+    
     def wiggleSort(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
-        
-        for i in range(1, len(nums) - 1, 2):
-            nums[i], nums[i+1] = nums[i+1], nums[i]
+        for i in range(len(nums) - 1):
+            if (i % 2 == 0 and nums[i] > nums[i+1]) or (i % 2 == 1 and nums[i] < nums[i+1]):
+                self.swap(nums, i, i + 1)
+            
