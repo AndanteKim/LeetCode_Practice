6d6a1f570d47430c2d4fac98154681e0660c7eca
Andrew Kim <ksb0724@utexas.edu>
2023-02-10 00:33:53 -0600

    Time: 564 ms (79.14%), Space: 14.4 MB (96.76%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -4,21 +4,20 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         
         MAX_DISTANCE = rows + cols + 1
-        
-        dist = [[MAX_DISTANCE for __ in range(cols)] for _ in range(rows)]
-        
+
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j]:
-                    dist[i][j] = 0
+                    grid[i][j] = 0
                 else:
-                    dist[i][j] = min(dist[i][j], min(dist[i-1][j] + 1 if i > 0 else MAX_DISTANCE, dist[i][j-1] + 1 if j > 0 else MAX_DISTANCE))
-                    
+                    grid[i][j] = MAX_DISTANCE
+                    grid[i][j] = min(grid[i][j], min(grid[i-1][j] + 1 if i > 0 else MAX_DISTANCE, grid[i][j-1] + 1 if j > 0 else MAX_DISTANCE))
+        
         ans = float('-inf')
         
         for i in range(rows-1, -1, -1):
             for j in range(cols - 1, -1, -1):
-                dist[i][j] = min(dist[i][j], min(dist[i+1][j] + 1 if i < rows - 1 else MAX_DISTANCE, dist[i][j+1] + 1 if j < cols - 1 else MAX_DISTANCE))
-                ans = max(ans, dist[i][j])
+                grid[i][j] = min(grid[i][j], min(grid[i+1][j] + 1 if i < rows - 1 else MAX_DISTANCE, grid[i][j+1] + 1 if j < cols - 1 else MAX_DISTANCE))
+                ans = max(ans, grid[i][j])
         
         return -1 if (ans == 0 or ans == MAX_DISTANCE) else ans
