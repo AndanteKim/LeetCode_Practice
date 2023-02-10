cdd705e5d557c39e96f0655bb1db3ea87bb98f50
Andrew Kim <ksb0724@utexas.edu>
2023-02-09 23:53:12 -0600

    Time: 558 ms (80.70%), Space: 14.5 MB (91.37%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxDistance(self, grid: List[List[int]]) -> int:
+        direction = [[-1,0], [1,0], [0,-1], [0,1]]
+        r, c = len(grid), len(grid[0])
+        visited = [[0 for __ in range(len(grid[0]))] for _ in range(len(grid))]
+        
+        queue = deque([])
+        for i in range(r):
+            for j in range(c):
+                visited[i][j] = grid[i][j]
+                if grid[i][j]:
+                    queue.append((i, j))
+        
+        distance = -1
+        while queue:
+            len_q = len(queue)
+            
+            while(len_q):
+                landCell = queue.popleft()
+                for direc in direction:
+                    x, y = landCell[0] + direc[0], landCell[1] + direc[1]
+                    
+                    if (x >= 0 and y >= 0 and x < r and y < c and visited[x][y] == 0):
+                        visited[x][y] = 1
+                        queue.append((x,y))
+                len_q -= 1
+            
+            distance += 1
+            
+        return -1 if distance == 0 else distance
