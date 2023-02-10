b2f72d3c95dc35c733693dcfe95179d560d5a0c6
Andrew Kim <ksb0724@utexas.edu>
2023-02-10 01:12:14 -0600

    Time: 59 ms (92.49%), Space: 16.6 MB (97.16%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -6,13 +6,12 @@ public:
         
         const int MAX_DISTANCE = rows + cols + 1;
         
-        vector<vector<int>> dist(rows, vector<int>(cols, MAX_DISTANCE));
-        
         for (int i = 0; i < rows; ++i){
             for (int j = 0; j < cols; ++j){
-                if (grid[i][j]) dist[i][j] = 0;
+                if (grid[i][j]) grid[i][j] = 0;
                 else{
-                    dist[i][j] = min(dist[i][j], min(i > 0? dist[i-1][j] + 1 : MAX_DISTANCE, j > 0? dist[i][j-1] + 1 : MAX_DISTANCE));
+                    grid[i][j] = MAX_DISTANCE;
+                    grid[i][j] = min(grid[i][j], min(i > 0? grid[i-1][j] + 1 : MAX_DISTANCE, j > 0? grid[i][j-1] + 1 : MAX_DISTANCE));
                 }
             }
         }
@@ -20,8 +19,8 @@ public:
         int ans = INT_MIN;
         for (int i = rows - 1; i >= 0; --i){
             for (int j = cols - 1; j >= 0; --j){
-                dist[i][j] = min(dist[i][j], min(i < rows - 1? dist[i+1][j] + 1 : MAX_DISTANCE, j < cols - 1? dist[i][j+1] + 1 : MAX_DISTANCE));
-                ans = max(ans, dist[i][j]);
+                grid[i][j] = min(grid[i][j], min(i < rows - 1? grid[i+1][j] + 1 : MAX_DISTANCE, j < cols - 1? grid[i][j+1] + 1 : MAX_DISTANCE));
+                ans = max(ans, grid[i][j]);
             }
         }
         return ans == 0 || ans == MAX_DISTANCE? -1: ans;
