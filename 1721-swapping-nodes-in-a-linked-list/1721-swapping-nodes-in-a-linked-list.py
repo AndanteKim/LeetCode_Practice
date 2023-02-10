cf83a94c27c81eb68a8849ecbe73e9958f758095
Andrew Kim <ksb0724@utexas.edu>
2023-02-10 08:46:17 -0600

    Time: 977 ms (88.17%), Space: 48.4 MB (47.74%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.py
@@ -4,22 +4,41 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def swapListNodesToNext(self, prevOfFirstK: Optional[ListNode], prevOfLastK: Optional[ListNode]) -> None:
+        if prevOfLastK.next == prevOfFirstK:
+            self.swapListNodesToNext(prevOfLastK, prevOfFirstK)
+        elif prevOfFirstK.next == prevOfLastK:
+            prevOfFirstK.next = prevOfLastK.next
+            prevOfLastK.next = prevOfFirstK.next.next
+            prevOfFirstK.next.next = prevOfLastK
+        else:
+            tempLeftNextNext = prevOfFirstK.next.next
+            tempRightNextNext = prevOfLastK.next.next
+            
+            tempLeftNext = prevOfFirstK.next
+            prevOfFirstK.next = prevOfLastK.next
+            prevOfFirstK.next.next = tempLeftNextNext
+            
+            prevOfLastK.next = tempLeftNext
+            prevOfLastK.next.next = tempRightNextNext
+    
     def swapNodes(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        length = 0
-        frontNode = endNode = None
-        currNode = head
+        sentinel = ListNode(-1)
+        sentinel.next = head
+        last = sentinel
         
-        while currNode:
-            length += 1
-            if endNode != None:
-                endNode = endNode.next
-            
-            if length == k:
-                frontNode = currNode
-                endNode = head
-            
-            currNode = currNode.next
+        for i in range(k-1):
+            last = last.next
         
-        frontNode.val, endNode.val = endNode.val, frontNode.val
+        prevOfFirstK = last
         
-        return head
+        prevOfLastK = sentinel
+        
+        while last.next.next != None:
+            last = last.next
+            prevOfLastK = prevOfLastK.next
+        
+        self.swapListNodesToNext(prevOfFirstK, prevOfLastK)
+        
+        return sentinel.next
+        
