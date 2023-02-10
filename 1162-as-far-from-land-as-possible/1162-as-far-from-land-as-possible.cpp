a0aa4feff0c016304f5c18610bbef1538d4bc563
Andrew Kim <ksb0724@utexas.edu>
2023-02-10 01:02:57 -0600

    Time: 58 ms (93.66%), Space: 18.3 MB (91.76%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -1,43 +1,29 @@
 class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
-        const pair<int, int> direction[4]{{-1,0},{1,0},{0,-1},{0,1}};
+        
         int rows = grid.size(), cols = grid[0].size();
         
-        int visited[rows][cols];
+        const int MAX_DISTANCE = rows + cols + 1;
         
-        queue<pair<int, int>> q;
+        vector<vector<int>> dist(rows, vector<int>(cols, MAX_DISTANCE));
         
         for (int i = 0; i < rows; ++i){
             for (int j = 0; j < cols; ++j){
-                visited[i][j] = grid[i][j];
-                if (grid[i][j]) q.push({i, j});
-            }
-        }
-        
-        int distance = -1;
-        
-        while(!q.empty()){
-            int qSize = q.size();
-            
-            while(qSize--){
-                pair<int, int> landCell = q.front();
-                q.pop();
-                
-                for (const pair<int, int> dir: direction){
-                    int x = landCell.first + dir.first;
-                    int y = landCell.second + dir.second;
-                    
-                    if (x >= 0 && y >= 0 && x < rows && y < cols && visited[x][y] == 0){
-                        visited[x][y] = 1;
-                        q.push({x, y});
-                    }
+                if (grid[i][j]) dist[i][j] = 0;
+                else{
+                    dist[i][j] = min(dist[i][j], min(i > 0? dist[i-1][j] + 1 : MAX_DISTANCE, j > 0? dist[i][j-1] + 1 : MAX_DISTANCE));
                 }
             }
-            
-            ++distance;
         }
         
-        return distance == 0? -1 : distance;
+        int ans = INT_MIN;
+        for (int i = rows - 1; i >= 0; --i){
+            for (int j = cols - 1; j >= 0; --j){
+                dist[i][j] = min(dist[i][j], min(i < rows - 1? dist[i+1][j] + 1 : MAX_DISTANCE, j < cols - 1? dist[i][j+1] + 1 : MAX_DISTANCE));
+                ans = max(ans, dist[i][j]);
+            }
+        }
+        return ans == 0 || ans == MAX_DISTANCE? -1: ans;
     }
 };
