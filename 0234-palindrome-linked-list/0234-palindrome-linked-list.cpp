8d63e32550087f33ce5a1271e3bcea7e6465b4b1
Andrew Kim <ksb0724@utexas.edu>
2023-02-11 12:49:28 -0600

    Time: 261 ms (56.89%), Space: 118.1 MB (60.79%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -10,17 +10,46 @@
  */
 class Solution {
 public:
-    bool isPalindrome(ListNode* head) {
-        ListNode *current_node = head;
-        vector<int> vals, rev_vals;
+    ListNode* end_of_first_half(ListNode* head){
+        ListNode *slow = head, *fast = head;
         
-        while (current_node != nullptr){
-            vals.push_back(current_node -> val);
-            rev_vals.push_back(current_node -> val);
-            current_node = current_node -> next;
+        while (fast -> next != nullptr && fast -> next -> next != nullptr){
+            fast = fast -> next -> next;
+            slow = slow -> next;
         }
         
-        reverse(rev_vals.begin(), rev_vals.end());
-        return rev_vals == vals;
+        return slow;
+    }
+    
+    ListNode* reverse_list(ListNode* head){
+        ListNode *prev = nullptr, *curr = head;
+        
+        while (curr != nullptr){
+            ListNode *next_node = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next_node;
+        }
+        
+        return prev;
+    }
+    
+    bool isPalindrome(ListNode* head) {
+        if (head == nullptr) return true;
+        
+        ListNode* first_half_end = end_of_first_half(head);
+        ListNode* second_half_start = reverse_list(first_half_end -> next);
+        
+        bool result = true;
+        ListNode* first_position = head, * second_position = second_half_start;
+        
+        while (result && second_position != nullptr){
+            if (first_position -> val != second_position -> val) result = false;
+            
+            first_position = first_position -> next;
+            second_position = second_position -> next;
+        }
+        first_half_end -> next = reverse_list(second_half_start);
+        return result;
     }
 };
