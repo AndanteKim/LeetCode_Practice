c0954f12267e1be7048a7e559ed5e9d63143f802
Andrew Kim <ksb0724@utexas.edu>
2023-02-11 10:21:43 -0600

    Time: 927 ms (32.17%), Space: 46.8 MB (35.42%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -4,21 +4,42 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        root = deepcopy(head) 
-        curr, prev = head, None
+        if not head:
+            return True
         
-        while curr:
-            next_node = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next_node
+        first_half_end = self.end_of_first_half(head)
+        second_half_start = self.reverse_list(first_half_end.next)
         
-        while root:
-            if root.val != prev.val:
-                return False
+        result = True
+        first_position = head
+        second_position = second_half_start
+        while result and second_position:
+            if first_position.val != second_position.val:
+                result = False
+            first_position = first_position.next
+            second_position = second_position.next
+        
+        first_half_end.next = self.reverse_list(second_half_start)
+        return result
+    
+    def end_of_first_half(self, head):
+        slow = fast = head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+    
+    def reverse_list(self, head):
+        prev = None
+        current = head
+        
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
             
-            root = root.next
-            prev = prev.next
-        
-        return True
+        return prev
+            
