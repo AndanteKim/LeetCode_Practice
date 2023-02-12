3b043379ab8728caaad29143115b2654c6858519
Andrew Kim <ksb0724@utexas.edu>
2023-02-12 12:18:46 -0600

    Time: 1984 ms (79.75%), Space: 54.8 MB (100.00%) - LeetHub

--- 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
+++ 2477-minimum-fuel-cost-to-report-to-the-capital/2477-minimum-fuel-cost-to-report-to-the-capital.py
@@ -1,27 +1,37 @@
 class Solution:
-    fuel = 0
-    
-    def dfs(self, node: int, parent: int, adj: Dict, seats: int) -> int:
-        representatives = 1
-        for child in adj[node]:
-            if child != parent:
-                representatives += self.dfs(child, node, adj, seats)
+    def bfs(self, n: int, adj: List[List[int]], degree: List[int], seats: int) -> int:
+        q = deque([])
         
-        if node != 0:
-            self.fuel += ceil(representatives / seats)
+        for i in range(1, n):
+            if degree[i] == 1:
+                q.append(i)
+                
+        representatives = [1] * n
+        fuel = 0
         
-        return representatives
+        while q:
+            node = q.popleft()
+            
+            fuel += ceil(representatives[node] / seats)
+            
+            for neighbor in adj[node]:
+                degree[neighbor] -= 1
+                representatives[neighbor] += representatives[node]
+                if degree[neighbor] == 1 and neighbor != 0:
+                    q.append(neighbor)
         
+        return fuel
     
     def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
         n = len(roads) + 1
         adj = defaultdict(list)
+        degree = [0] * n
         
         for road in roads:
             adj[road[0]].append(road[1])
             adj[road[1]].append(road[0])
+            degree[road[0]] += 1
+            degree[road[1]] += 1
         
-        self.dfs(0, -1, adj, seats)
         
-        return self.fuel
-        
+        return self.bfs(n, adj, degree, seats)
