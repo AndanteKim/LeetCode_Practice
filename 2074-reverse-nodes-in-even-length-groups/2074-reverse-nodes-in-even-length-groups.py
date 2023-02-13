848302971df1dd4c7138dc23812d5d3f0cd55d02
Andrew Kim <ksb0724@utexas.edu>
2023-02-13 10:23:53 -0600

    Time: 2509 ms (28.39%), Space: 53.7 MB (29.66%) - LeetHub

---
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseEvenLengthGroups(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, d = head, 2
+        
+        while prev.next:
+            node, n = prev, 0
+            
+            for _ in range(d):
+                if not node.next:
+                    break
+                n += 1
+                node = node.next
+                print(n)
+            if n & 1:
+                prev = node
+            else:
+                node, rev = prev.next, None
+                for _ in range(n):
+                    node.next, node, rev = rev, node.next, node
+                prev.next.next, prev.next, prev = node, rev, prev.next
+            d += 1
+        return head
+        
+        
