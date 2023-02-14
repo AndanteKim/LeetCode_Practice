f593d36ea708d3bb7db72b9a610a1a73133a5856
Andrew Kim <ksb0724@utexas.edu>
2023-02-14 07:06:21 -0600

    Time: 27 ms (95.82%), Space: 13.8 MB (63.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,25 +1,8 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        n = max(len(a), len(b))
-        a, b = a.zfill(n), b.zfill(n)
-        
-        carry = 0
-        answer = []
-        for i in range(n - 1, -1, -1):
-            if a[i] == '1':
-                carry += 1
-            if b[i] == '1':
-                carry += 1
-            
-            if carry % 2 == 1:
-                answer.append('1')
-            else:
-                answer.append('0')
-            
-            carry //= 2
-        
-        if carry == 1:
-            answer.append('1')
-        answer.reverse()
-        
-        return ''.join(answer)
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            ans = x ^ y
+            carry = (x & y) << 1
+            x, y = ans, carry
+        return bin(x)[2:]
