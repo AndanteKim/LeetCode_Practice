6b727f197c2694fdde19c8665e315acf49b01b8d
Andrew Kim <ksb0724@utexas.edu>
2023-02-15 10:47:16 -0600

    Time: 278 ms (92.04%), Space: 15.1 MB (65.15%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.py
@@ -1,12 +1,13 @@
 class Solution:
     def addToArrayForm(self, num: List[int], k: int) -> List[int]:
-        num[-1] += k
         
         for i in range(len(num) - 1, -1, -1):
-            carry, num[i] = divmod(num[i], 10)
-            if i:
-                num[i - 1] += carry
-        
-        if carry:
-            num = list(map(int, str(carry))) + num
+            k, d = divmod(k, 10)
+            carry, num[i] = divmod(num[i] + d, 10)
+            k += carry
+            if not k:
+                break
+                
+        if k:
+            num = list(map(int, str(k))) + num
         return num
