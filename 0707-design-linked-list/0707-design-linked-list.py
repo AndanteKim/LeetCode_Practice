c3673bbbabcabc9297fdbf7d4d6681738e66e629
Andrew Kim <ksb0724@utexas.edu>
2023-02-15 13:07:14 -0600

    Time: 111 ms (95.91%), Space: 15.5 MB (6.32%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.py
+++ 0707-design-linked-list/0707-design-linked-list.py
@@ -2,27 +2,49 @@ class ListNode:
     def __init__(self, x):
         self.val = x
         self.next = None
+        self.prev = None
 
 class MyLinkedList:
 
     def __init__(self):
         self.size = 0
-        self.head = ListNode(0)
+        self.head, self.tail = ListNode(0), ListNode(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
     def get(self, index: int) -> int:
         if index < 0 or index >= self.size:
             return -1
         
-        curr = self.head
-        for _ in range(index + 1):
-            curr = curr.next
+        if index + 1 < self.size - index:
+            curr = self.head
+            for _ in range(index + 1):
+                curr = curr.next
+        else:
+            curr = self.tail
+            for _ in range(self.size - index):
+                curr = curr.prev
+        
         return curr.val
 
     def addAtHead(self, val: int) -> None:
-        self.addAtIndex(0, val)
+        pred, succ = self.head, self.head.next
+        self.size += 1
+        to_add = ListNode(val)
+        to_add.prev = pred
+        to_add.next = succ
+        pred.next = to_add
+        succ.prev = to_add
+        
 
     def addAtTail(self, val: int) -> None:
-        self.addAtIndex(self.size, val)
+        succ, pred = self.tail, self.tail.prev
+        self.size += 1
+        to_add = ListNode(val)
+        to_add.prev = pred
+        to_add.next = succ
+        pred.next = to_add
+        succ.prev = to_add
 
     def addAtIndex(self, index: int, val: int) -> None:
         if index > self.size:
@@ -30,26 +52,43 @@ class MyLinkedList:
         
         if index < 0:
             index = 0
+            
+        if index < self.size - index:
+            pred = self.head
+            for _ in range(index):
+                pred = pred.next
+            succ = pred.next
+        else:
+            succ = self.tail
+            for _ in range(self.size - index):
+                succ = succ.prev
+            pred = succ.prev
         
         self.size += 1
-        pred = self.head
-        for _ in range(index):
-            pred = pred.next
-        
         to_add = ListNode(val)
-        to_add.next = pred.next
+        to_add.prev = pred
+        to_add.next = succ
         pred.next = to_add
+        succ.prev = to_add
         
-
     def deleteAtIndex(self, index: int) -> None:
         if index < 0 or index >= self.size:
             return
         
+        if index < self.size - index:
+            pred = self.head
+            for _ in range(index):
+                pred = pred.next
+            succ = pred.next.next
+        else:
+            succ = self.tail
+            for _ in range(self.size - index - 1):
+                succ = succ.prev
+            pred = succ.prev.prev
+        
         self.size -= 1
-        pred = self.head
-        for _ in range(index):
-            pred = pred.next
-        pred.next = pred.next.next
+        pred.next = succ
+        succ.prev = pred
 
 
 # Your MyLinkedList object will be instantiated and called as such:
