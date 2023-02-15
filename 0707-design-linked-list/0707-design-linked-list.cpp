d05378fe9453afc41c437fb8df04ecbfd33af567
Andrew Kim <ksb0724@utexas.edu>
2023-02-15 13:51:43 -0600

    Time: 42 ms (78.18%), Space: 20.1 MB (11.51%) - LeetHub

--- 0707-design-linked-list/0707-design-linked-list.cpp
+++ 0707-design-linked-list/0707-design-linked-list.cpp
@@ -1,47 +1,99 @@
 class MyLinkedList {
+    struct Node{
+        int val;
+        Node *next, *prev;
+        Node(int x) {
+            val = x;
+            next = prev = nullptr;
+        }
+    };
+    
+    
     int sz;
-    ListNode *head;
+    Node *head, *tail;
 public:
     MyLinkedList() {
         sz = 0;
-        head = new ListNode(0);
-        
+        head = new Node(0);
+        tail = new Node(0);
+        head -> next = tail;
+        tail -> prev = head;
     }
     
     int get(int index) {
         if (index < 0 || index >= sz) return -1;
-        ListNode *curr = head;
-        
-        for (int _ = 0; _ <= index; ++_) curr = curr -> next;
+        Node *curr;
+        if (index + 1 < sz - index){
+            curr = head;
+            for (int i = 0; i <= index; ++i) curr = curr -> next;
+        }
+        else{
+            curr = tail;
+            for (int i = 0; i < sz - index; ++i) curr = curr -> prev;
+        }
         return curr -> val;
     }
     
     void addAtHead(int val) {
-        addAtIndex(0, val);
+        Node *pred = head, *succ = head -> next;
+        ++sz;
+        Node* to_add = new Node(val);
+        to_add -> prev = pred;
+        to_add -> next = succ;
+        pred -> next = to_add;
+        succ -> prev = to_add;
     }
     
     void addAtTail(int val) {
-        addAtIndex(sz, val);
+        Node *succ = tail, *pred = tail -> prev;
+        ++sz;
+        Node* to_add = new Node(val);
+        to_add -> prev = pred;
+        to_add -> next = succ;
+        pred -> next = to_add;
+        succ -> prev = to_add;
     }
     
     void addAtIndex(int index, int val) {
-        if (index > this -> sz) return;
+        if (index > sz) return;
         if (index < 0) index = 0;
-        ++sz;
-        ListNode* pred = head;
-        for (int i = 0; i < index; ++i) pred = pred -> next;
-        ListNode* to_add = new ListNode(val);
-        to_add -> next = pred -> next;
-        pred -> next = to_add;
+        Node *pred, *succ;
+        if (index < sz - index) {
+            pred = head;
+            for (int i = 0; i < index; ++i) pred = pred -> next;
+            succ = pred -> next;
+        }
+        else{
+            succ = tail;
+            for (int i = 0; i < sz - index; ++i) succ = succ -> prev;
+            pred = succ -> prev;
+        }
         
+        ++sz;
+        Node *to_add = new Node(val);
+        to_add -> prev = pred;
+        to_add -> next = succ;
+        pred -> next = to_add;
+        succ -> prev = to_add;
     }
     
     void deleteAtIndex(int index) {
         if (index < 0 || index >= sz) return;
+        
+        Node *pred, *succ;
+        if (index < sz - index){
+            pred = head;
+            for (int i = 0; i < index; ++i) pred = pred -> next;
+            succ = pred -> next -> next;
+        }
+        else{
+            succ = tail;
+            for (int i = 0; i < sz - index - 1; ++i) succ = succ -> prev;
+            pred = succ -> prev -> prev;
+        }
         --sz;
-        ListNode *pred = head;
-        for (int i = 0; i < index; ++i) pred = pred -> next;
-        pred -> next = pred -> next -> next;
+        pred -> next = succ;
+        succ -> prev = pred;
     }
 };
 
