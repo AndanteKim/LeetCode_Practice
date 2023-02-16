269024f886fa332cbeb3eddae476a87b1e28e877
Andrew Kim <ksb0724@utexas.edu>
2023-02-16 11:35:26 -0600

    Time: 79 ms (63.17%), Space: 14.9 MB (11.80%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -1,11 +1,10 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
         stack = []
-        
         for c in s:
-            if not stack or c != stack[-1]:
-                stack.append(c)
-            elif c == stack[-1]:
+            if stack and stack[-1] == c:
                 stack.pop()
-        
+            else:
+                stack.append(c)
+                
         return "".join(stack)
