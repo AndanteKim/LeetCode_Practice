9031333bb93fc7223bf71d000ea144c1e9fd3030
Andrew Kim <ksb0724@utexas.edu>
2023-02-16 12:01:43 -0600

    Time: 4 ms (39.01%), Space: 6.2 MB (77.59%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -1,26 +1,16 @@
 class Solution {
 public:
+    string build(string &s){
+        string new_s = "";
+        for (const char&c : s){
+            if (c != '#') new_s.push_back(c);
+            else if (!new_s.empty()) new_s.pop_back();
+        }
+        
+        return new_s;
+    }
+    
     bool backspaceCompare(string s, string t) {
-        string new_s = "", new_t = "";
-        
-        for (const char& c: s){
-            if (c == '#'){
-                if (!new_s.empty()) new_s.pop_back();
-            }
-            else{
-                new_s.push_back(c);
-            }
-        }
-        
-        for (const char& c: t){
-            if (c == '#') {
-                if (!new_t.empty()) new_t.pop_back();
-            }
-            else{
-                new_t.push_back(c);
-            }
-        }
-        
-        return new_s == new_t;
+        return build(s) == build(t);
     }
 };
