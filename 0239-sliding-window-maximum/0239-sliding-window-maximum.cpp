a7b06ac9d346e42f2ce56b9579211c213ba7f354
Andrew Kim <ksb0724@utexas.edu>
2023-02-18 12:19:38 -0600

    Time: 305 ms (69.27%), Space: 134.5 MB (93.33%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> ans;
+        deque<int> dq;
+        for (int i = 0; i < nums.size(); ++i){
+            while (!dq.empty() && nums[dq.back()] < nums[i]) dq.pop_back();
+            dq.push_back(i);
+            if (dq.front() + k == i) dq.pop_front();
+            if (i >= k - 1) ans.push_back(nums[dq.front()]);
+        }
+        return ans;
+    }
+};
