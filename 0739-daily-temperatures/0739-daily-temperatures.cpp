11cfb2049ac2ffe10a371835bd320ded6f9ab454
Andrew Kim <ksb0724@utexas.edu>
2023-02-18 09:37:04 -0600

    Time: 183 ms (95.74%), Space: 98.6 MB (80.03%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size(), j;
-        stack<int> st;
+        int n = temperatures.size(), hottest = 0;
         vector<int> ans(n, 0);
-        
-        for (int i = 0; i < n; ++i){
-            while ((!st.empty()) && (temperatures[st.top()] < temperatures[i])){
-                j = st.top();
-                st.pop();
-                ans[j] = i - j;
+        for (int curr_day = n - 1; curr_day >= 0; --curr_day){
+            int curr_temp = temperatures[curr_day];
+            if (curr_temp >= hottest) {
+                hottest = curr_temp;
+                continue;
             }
-            st.push(i);
+            int days = 1;
+            while (temperatures[curr_day + days] <= curr_temp){
+                days += ans[curr_day + days];
+            }
+            ans[curr_day] = days;
         }
         return ans;
     }
