657291a43e3cbfc9894678be857a84450900889a
Andrew Kim <ksb0724@utexas.edu>
2023-02-20 06:03:36 -0600

    Time: 3 ms (90.12%), Space: 9.8 MB (31.02%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        return lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int left = 0, right = nums.size() - 1, mid;
+        
+        while (left <= right){
+            mid = ((unsigned int)left + (unsigned int)right) >> 1;
+            if (nums[mid] == target) return mid;
+            if (target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
+        }
+        
+        return left;
     }
 };
