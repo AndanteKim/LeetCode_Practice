d63eaaff46d526dca6b9b390de7e423f5c9e12a3
Andrew Kim <ksb0724@utexas.edu>
2023-02-20 22:39:57 -0600

    Time: 25 ms (82.58%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -2,21 +2,14 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int low = 0, high = nums.size() - 1, mid;
-        bool halves_are_even;
         while (low < high){
             mid = low + (high - low) / 2;
-            halves_are_even = (high - mid) % 2 == 0;
+            if (mid % 2 == 1) --mid;
             
-            if (nums[mid] == nums[mid + 1]){
-                if (halves_are_even) low = mid + 2;
-                else high = mid - 1;
-            }
-            else if (nums[mid - 1] == nums[mid]){
-                if (halves_are_even) high = mid - 2;
-                else low = mid + 1;
-            }
-            else return nums[mid];
+            if (nums[mid] == nums[mid + 1]) low = mid + 2;
+            else high = mid;
         }
+        
         return nums[low];
     }
 };
