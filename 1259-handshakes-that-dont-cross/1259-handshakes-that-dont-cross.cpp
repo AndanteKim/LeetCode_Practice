c310067da11881036e9c65e68843e2134d12cea6
Andrew Kim <ksb0724@utexas.edu>
2023-02-22 08:56:58 -0600

    Time: 2 ms (96.43%), Space: 6.5 MB (62.50%) - LeetHub

--- 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.cpp
+++ 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.cpp
@@ -1,14 +1,19 @@
 class Solution {
+    const int m = 1'000'000'007;
+    int mul(int a, int b) { return (long long) a * b % m;}
 public:
     int numberOfWays(int numPeople) {
-        int m = 1000000007;
-        vector<int> dp(numPeople / 2 + 1, 0);
-        dp[0] = 1;
-        for (int i = 1; i <= numPeople / 2; ++i){
-            for (int j = 0; j < i; ++j){
-                (dp[i] += (long long)dp[j] * dp[i - j - 1] % m) %= m;
-            }
+        int n = numPeople / 2, k, r;
+        vector<int> inv(n+2);
+        inv[1] = 1;
+        for (int i = 2; i <= n+1; ++i){
+            k = m / i;
+            r = m % i;
+            inv[i] = m - mul(k,inv[r]);
         }
-        return dp[numPeople / 2];
+        int C = 1;
+        for (int i = 0; i < n; ++i) C = mul(mul(2 * (2 * i + 1), inv[i + 2]), C);
+        
+        return C;
     }
 };
