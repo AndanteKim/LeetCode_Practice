80a6e5def2ff694fdad27c410df50c201e4941d6
Andrew Kim <ksb0724@utexas.edu>
2023-02-22 08:40:52 -0600

    Time: 47 ms (92.08%), Space: 13.8 MB (98.02%) - LeetHub

--- 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.py
+++ 1259-handshakes-that-dont-cross/1259-handshakes-that-dont-cross.py
@@ -1,10 +1,15 @@
 class Solution:
     def numberOfWays(self, numPeople: int) -> int:
         m = 1000000007
-        dp = [0] * (numPeople // 2 + 1)
-        dp[0] = 1
-        for i in range(1, numPeople // 2 + 1):
-            for j in range(i):
-                dp[i] += dp[j] * dp[i - j - 1]
-                dp[i] %= m
-        return dp[numPeople // 2]
+        n = numPeople // 2
+        inv = [None] * (n+2)
+        inv[1] = 1
+        
+        for i in range(2, n + 2):
+            k = m // i
+            r = m % i
+            inv[i] = m - k * inv[r] % m
+        C = 1
+        for i in range(n):
+            C = 2 * (2 * i + 1) * inv[i + 2] * C % m
+        return C
