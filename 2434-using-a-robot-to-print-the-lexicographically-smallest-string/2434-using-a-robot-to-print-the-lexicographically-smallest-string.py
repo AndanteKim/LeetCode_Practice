690f25f892b21ac9bed42f0003434db493031734
Andrew Kim <ksb0724@utexas.edu>
2023-02-23 08:57:48 -0600

    Time: 1113 ms (54.25%), Space: 17.5 MB (48.11%) - LeetHub

---
+++ 2434-using-a-robot-to-print-the-lexicographically-smallest-string/2434-using-a-robot-to-print-the-lexicographically-smallest-string.py
@@ -0,0 +1,20 @@
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        ans, n = "", len(s)
+        suffixMin = [None] * n
+        
+        for i in range(n-1, -1, -1):
+            if i == n - 1:
+                suffixMin[i] = s[i]
+            else:
+                suffixMin[i] = min(s[i], suffixMin[i+1])
+        
+        t = []
+        for i in range(n):
+            t.append(s[i])
+            
+            while t and (i == n - 1 or t[-1] <= suffixMin[i+1]):
+                ans += t.pop()
+        
+        return ans
+                
