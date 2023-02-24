b5405ac51864b89d13407b37e9a1e79bcb0db30b
Andrew Kim <ksb0724@utexas.edu>
2023-02-24 09:00:18 -0600

    Time: 840 ms (15.25%), Space: 115.8 MB (12.71%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.cpp
@@ -1,31 +1,46 @@
 class Solution {
 public:
     int minimumDeviation(vector<int>& nums) {
-        priority_queue<int> evens;
-        int minimum = INT_MAX;
-        for (int num : nums){
-            if (num % 2 == 0){
-                evens.push(num);
-                minimum = min(minimum, num);
+        int n = nums.size();
+        
+        vector<pair<int, int>> possible;
+        for (int i = 0; i < n; ++i){
+            if (nums[i] % 2 == 0) {
+                int temp = nums[i];
+                possible.push_back({temp, i});
+                while (temp % 2 == 0){
+                    temp /= 2;
+                    possible.push_back({temp, i});
+                }
             }
             else{
-                evens.push(num * 2);
-                minimum = min(minimum, num * 2);
+                possible.push_back({nums[i], i});
+                possible.push_back({nums[i] * 2 , i});
+            }
+        }
+        sort(possible.begin(), possible.end());
+        
+        int min_deviation = INT_MAX, not_included = n, current_start = 0;
+        vector<int> need_include(n, 1);
+        
+        for (auto &it : possible){
+            --need_include[it.second];
+            if (need_include[it.second] == 0) --not_included;
+            
+            if (not_included == 0){
+                while (need_include[possible[current_start].second] < 0){
+                    ++need_include[possible[current_start].second];
+                    ++current_start;
+                }
+                if (min_deviation > it.first - possible[current_start].first)
+                    min_deviation = it.first - possible[current_start].first;
+                
+                ++need_include[possible[current_start].second];
+                ++current_start;
+                ++not_included;
             }
         }
         
-        int minDeviation = INT_MAX;
-        while(!evens.empty()){
-            int currentValue = evens.top();
-            evens.pop();
-            minDeviation = min(minDeviation, currentValue - minimum);
-            if (currentValue % 2 == 0){
-                evens.push(currentValue / 2);
-                minimum = min(minimum, currentValue / 2);
-            }
-            else break;
-        }
-        
-        return minDeviation;
+        return min_deviation;
     }
 };
