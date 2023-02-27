7e46ef3d72be01ae3351bc5c415b53b75d496f1d
Andrew Kim <ksb0724@utexas.edu>
2023-02-27 02:27:29 -0600

    Time: 28 ms (14.49%), Space: 23.1 MB (16.67%) - LeetHub

--- 0427-construct-quad-tree/0427-construct-quad-tree.cpp
+++ 0427-construct-quad-tree/0427-construct-quad-tree.cpp
@@ -40,26 +40,19 @@ public:
 
 class Solution {
 public:
-    bool sameValue(vector<vector<int>>& grid, int x1, int y1, int length){
-        for (int i = x1; i < x1 + length; ++i){
-            for (int j = y1; j < y1 + length; ++j){
-                if (grid[i][j] != grid[x1][y1]) return false;
-            }
-        }
-        
-        return true;
-    }
-    
     Node* solve(vector<vector<int>>& grid, int x1, int y1, int length){
-        if (sameValue(grid, x1, y1, length)) return new Node(grid[x1][y1], true);
-        else{
-            Node* root = new Node(false, false);
-            root -> topLeft = solve(grid, x1, y1, length / 2);
-            root -> topRight = solve(grid, x1, y1 + length / 2, length / 2);
-            root -> bottomLeft = solve(grid, x1 + length / 2, y1, length / 2);
-            root -> bottomRight = solve(grid, x1 + length / 2, y1 + length / 2, length / 2);
-            return root;
-        }
+        if (length == 1)
+            return new Node(grid[x1][y1], true);
+        
+        Node* topLeft = solve(grid, x1, y1, length / 2);
+        Node* topRight = solve(grid, x1, y1 + length / 2, length / 2);
+        Node* bottomLeft = solve(grid, x1 + length / 2, y1, length / 2);
+        Node* bottomRight = solve(grid, x1 + length / 2, y1 + length / 2, length / 2);
+        if (topLeft -> isLeaf && topRight -> isLeaf && bottomLeft -> isLeaf && bottomRight -> isLeaf\
+           && topLeft -> val == topRight -> val && topRight -> val == bottomLeft -> val\
+           && bottomLeft -> val == bottomRight -> val)
+            return new Node(topLeft -> val, true);
+        return new Node(false, false, topLeft, topRight, bottomLeft, bottomRight);
     }
     
     Node* construct(vector<vector<int>>& grid) {
