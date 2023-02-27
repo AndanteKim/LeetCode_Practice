e52e4caa28551045183c5e9f84c8ea91672804ea
Andrew Kim <ksb0724@utexas.edu>
2023-02-27 03:26:09 -0600

    Time: 14 ms (73.62%), Space: 17.5 MB (76.83%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> st;
         for (int asteroid : asteroids){
-            bool flag = false;
+            bool isCollision = false;
             while (!st.empty() && st.back() > 0 && asteroid < 0){
                 if (st.back() < -asteroid) {
                     st.pop_back();
                     continue;
                 }
                 else if (st.back() == -asteroid) st.pop_back();
-                flag = true;
+                isCollision = true;
                 break;
             }
-            if (!flag) st.push_back(asteroid);
+            if (!isCollision) st.push_back(asteroid);
         }
         
         return st;
