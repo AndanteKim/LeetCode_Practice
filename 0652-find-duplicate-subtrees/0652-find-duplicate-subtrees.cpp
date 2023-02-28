947dfd9232eb22cc447151bc81661c9bb4c15949
Andrew Kim <ksb0724@utexas.edu>
2023-02-28 06:02:29 -0600

    Time: 26 ms (82.12%), Space: 25.9 MB (91.12%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.cpp
@@ -10,18 +10,22 @@
  * };
  */
 class Solution {
-    
 public:
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        unordered_map<string, int> cnt;
+        unordered_map<string, int> tripletToID;
+        unordered_map<int, int> cnt;
         vector<TreeNode*> res;
-        function<string(TreeNode*)> traverse = [&cnt, &res, &traverse](TreeNode* node) -> string{
-            if (node == nullptr) return "";
-            string representation = "(" + traverse(node -> left) + ")" + to_string(node -> val) + "(" + \
-                                    traverse(node -> right) + ")";
-            ++cnt[representation];
-            if (cnt[representation] == 2) res.push_back(node);
-            return representation;
+        
+        function<int(TreeNode*)> traverse = [&tripletToID, &cnt, &res,\
+                                            &traverse](TreeNode* node) -> int{
+            if (node == nullptr) return 0;
+            string triplet = to_string(traverse(node -> left)) + "," + to_string(node -> val) + ","+\
+                to_string(traverse(node -> right));
+            if (!tripletToID.count(triplet)) tripletToID[triplet] = tripletToID.size() + 1;
+            int id = tripletToID[triplet];
+            ++cnt[id];
+            if (cnt[id] == 2) res.push_back(node);
+            return id;
         };
         
         traverse(root);
