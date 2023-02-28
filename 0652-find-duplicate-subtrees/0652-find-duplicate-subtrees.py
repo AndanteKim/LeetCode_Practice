3a906fc7e57dccfd0b3f3777fbe13b7fe7a2228a
Andrew Kim <ksb0724@utexas.edu>
2023-02-28 02:54:14 -0600

    Time: 46 ms (91.41%), Space: 16.3 MB (91.96%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.py
@@ -5,22 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    triplet_to_id = dict()
     cnt = defaultdict(int)
     res = []
-    def traverse(self, node: Optional[TreeNode]) -> str:
+    def traverse(self, node: Optional[TreeNode]) -> int:
         if not node:
-            return ""
-        representation = ("(" + self.traverse(node.left) + ")" + str(node.val)\
-                         + "(" + self.traverse(node.right) + ")")
-        self.cnt[representation] += 1
-        if self.cnt[representation] == 2:
+            return 0
+        triplet = (self.traverse(node.left), node.val, self.traverse(node.right))
+        if triplet not in self.triplet_to_id:
+            self.triplet_to_id[triplet] = len(self.triplet_to_id) + 1
+        id = self.triplet_to_id[triplet]
+        self.cnt[id] += 1
+        if self.cnt[id] == 2:
             self.res.append(node)
-        return representation
-        
+        return id
     
     def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        self.triplet_to_id = dict()
         self.cnt = defaultdict(int)
         self.res = []
         self.traverse(root)
         return self.res
-        
