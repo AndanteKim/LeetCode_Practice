897c163b55f5346d1477c78c1261a3ef12ef377b
Andrew Kim <ksb0724@utexas.edu>
2023-03-01 03:08:32 -0600

    Time: 234 ms (47.73%), Space: 89.2 MB (31.58%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,27 +1,23 @@
 class Solution {
-    void heapify(vector<int>& nums, int n, int i){
-        int largest = i, left = 2 * i + 1, right = 2 * i + 2;
-        if (left < n && nums[left] > nums[largest]) largest = left;
-        if (right < n && nums[right] > nums[largest]) largest = right;
+    void counting_sort(vector<int>& nums){
+        unordered_map<int, int> counts;
+        int minVal = *min_element(nums.begin(), nums.end());
+        int maxVal = *max_element(nums.begin(), nums.end());
+        for (int num : nums) ++counts[num];
         
-        if (largest != i){
-            swap(nums[i], nums[largest]);
-            heapify(nums, n, largest);
-        }
-    }
-    
-    void heapSort(vector<int>& nums){
-        int n = nums.size();
-        for (int i = n / 2 - 1; i >= 0; --i) heapify(nums, n, i);
-        for (int i = n - 1; i >= 0; --i) {
-            swap(nums[0], nums[i]);
-            heapify(nums, i, 0);
+        int index = 0;
+        for (int val = minVal; val <= maxVal; ++val){
+            while(counts[val] > 0){
+                nums[index] = val;
+                --counts[val];
+                ++index;
+            }
         }
     }
     
 public:
     vector<int> sortArray(vector<int>& nums) {
-        heapSort(nums);
+        counting_sort(nums);
         return nums;
     }
 };
