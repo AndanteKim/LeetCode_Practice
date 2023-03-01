558c18880a2b86bba91abc096d7e55cfcced50d7
Andrew Kim <ksb0724@utexas.edu>
2023-03-01 03:28:30 -0600

    Time: 149 ms (78.50%), Space: 93.4 MB (30.08%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,23 +1,50 @@
 class Solution {
-    void counting_sort(vector<int>& nums){
-        unordered_map<int, int> counts;
-        int minVal = *min_element(nums.begin(), nums.end());
-        int maxVal = *max_element(nums.begin(), nums.end());
-        for (int num : nums) ++counts[num];
+    void bucketSort(vector<int>& arr, int placeValue){
+        vector<vector<int>> buckets(10, vector<int>());
+        
+        for (int& val : arr){
+            int digit = abs(val) / placeValue;
+            digit %= 10;
+            buckets[digit].push_back(val);
+        }
         
         int index = 0;
-        for (int val = minVal; val <= maxVal; ++val){
-            while(counts[val] > 0){
-                nums[index] = val;
-                --counts[val];
+        for (int digit = 0; digit < 10; ++digit){
+            for (int& val : buckets[digit]){
+                arr[index] = val;
                 ++index;
             }
         }
     }
     
+    void radixSort(vector<int>& arr){
+        int maxElement = arr[0];
+        for (int& val : arr) maxElement = max(abs(val), maxElement);
+        int maxDigits = 0;
+        while (maxElement > 0){
+            ++maxDigits;
+            maxElement /= 10;
+        }
+        
+        int placeValue = 1;
+        for (int digit = 0; digit < maxDigits; ++digit){
+            bucketSort(arr, placeValue);
+            placeValue *= 10;
+        }
+        
+        vector<int> negatives, positives;
+        for (int& val : arr){
+            if (val < 0) negatives.push_back(val);
+            else positives.push_back(val);
+        }
+        reverse(negatives.begin(), negatives.end());
+        merge(negatives.begin(), negatives.end(), positives.begin(), positives.end(), arr.begin());
+    }
+
+    
 public:
     vector<int> sortArray(vector<int>& nums) {
-        counting_sort(nums);
+        radixSort(nums);
         return nums;
     }
 };
