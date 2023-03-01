62112ec8cf4c33f05bc98160595c76f2946a16df
Andrew Kim <ksb0724@utexas.edu>
2023-03-01 06:52:26 -0600

    Time: 18 ms (23.53%), Space: 11.1 MB (5.15%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.cpp
+++ 0422-valid-word-square/0422-valid-word-square.cpp
@@ -3,7 +3,7 @@ public:
     bool validWordSquare(vector<string>& words) {
         int cols = 0, rows = words.size();
         vector<string> new_words;
-        for (string& word: words) cols = max(cols, (int)word.size());
+        for (const string& word: words) cols = max(cols, (int)word.size());
         
         if (cols != words[0].size() || cols != rows) return false;
         
