002f06ad43b3a553d8e2596084d8170ffc7c2c94
Andrew Kim <ksb0724@utexas.edu>
2023-03-03 07:35:07 -0600

    Time: 45 ms (11.58%), Space: 14 MB (11.03%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,41 +1,46 @@
 class Solution:
-    RADIX, MOD, MAX_WEIGHT = 26, 1_000_000_033, 1
+    RADIX_1, MOD_1 = 26, 10 ** 9 + 33
+    RADIX_2, MOD_2 = 27, 2 ** 31 - 1
+    MAX_WEIGHT_1, MAX_WEIGHT_2 = 1, 1
     
-    def hash_value(self, m: int, string: str) -> int:
-        ans, factor = 0, 1
+    def hash_pair(self, m: int, string: str) -> List[int]:
+        hash_1 = hash_2 = 0
+        factor_1 = factor_2 = 1
         
         for i in range(m - 1, -1, -1):
-            ans += ((ord(string[i]) - 97) * (factor)) % self.MOD
-            factor = (factor * self.RADIX) % self.MOD
+            hash_1 += ((ord(string[i]) - 97) * (factor_1)) % self.MOD_1
+            factor_1 = (factor_1 * self.RADIX_1) % self.MOD_1
+            hash_2 += ((ord(string[i]) - 97) * (factor_2)) % self.MOD_2
+            factor_2 = (factor_2 * self.RADIX_2) % self.MOD_2
         
-        return ans % self.MOD
+        return [hash_1 % self.MOD_1, hash_2 % self.MOD_2]
     
     def strStr(self, haystack: str, needle: str) -> int:
         m, n = len(needle), len(haystack)
-        if n < m:
-            return -1
-            
-        for _ in range(m):
-            self.MAX_WEIGHT = (self.MAX_WEIGHT * self.RADIX) % self.MOD
         
-        hash_needle = self.hash_value(m, needle)
+        if n < m: return -1
+        
+        for _ in range(m):
+            self.MAX_WEIGHT_1 = (self.MAX_WEIGHT_1 * self.RADIX_1) % self.MOD_1
+            self.MAX_WEIGHT_2 = (self.MAX_WEIGHT_2 * self.RADIX_2) % self.MOD_2
+        
+        hash_needle = self.hash_pair(m, needle)
         
         for window_start in range(n - m + 1):
             if window_start == 0:
-                hash_hay = self.hash_value(m, haystack)
+                hash_hay = self.hash_pair(m, haystack)
             else:
-                hash_hay = ((hash_hay * self.RADIX) % self.MOD\
-                            - ((ord(haystack[window_start - 1]) - 97)
-                            * self.MAX_WEIGHT) % self.MOD
-                            + (ord(haystack[window_start + m - 1]) - 97)
-                            + self.MOD) % self.MOD
+                hash_hay[0] = (((hash_hay[0] * self.RADIX_1) % self.MOD_1
+                               - ((ord(haystack[window_start - 1]) - 97)
+                                 * (self.MAX_WEIGHT_1)) % self.MOD_1
+                               + (ord(haystack[window_start + m - 1]) - 97))
+                               % self.MOD_1)
+                hash_hay[1] = (((hash_hay[1] * self.RADIX_2) % self.MOD_2
+                               - ((ord(haystack[window_start - 1]) - 97)
+                                 * (self.MAX_WEIGHT_2)) % self.MOD_2
+                               + (ord(haystack[window_start + m - 1]) - 97))
+                               % self.MOD_2)
             
             if hash_needle == hash_hay:
-                for i in range(m):
-                    if needle[i] != haystack[i + window_start]:
-                        break
-                
-                if i == m - 1:
-                    return window_start
-        
+                return window_start
         return -1
