033133506d3c7d0ea4a1f71f01bac848c7d02efa
Andrew Kim <ksb0724@utexas.edu>
2023-03-03 08:00:52 -0600

    Time: 0 ms (100.00%), Space: 6.4 MB (29.81%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,52 +1,42 @@
-#define ll long long
-
 class Solution {
 public:
-    const int RADIX_1 = 26, MOD_1 = 1000000033;
-    const int RADIX_2 = 27, MOD_2 = 2147483647;
-    
-    pair<ll, ll> hashPair(string string, int m){
-        ll hash1 = 0, hash2 = 0;
-        ll factor1 = 1, factor2 = 1;
-        
-        for (int i = m - 1; i >= 0; --i){
-            hash1 += ((int) (string[i] - 'a') * (factor1)) % MOD_1;
-            factor1 = (factor1 * RADIX_1) % MOD_1;
-            hash2 += ((int) (string[i] - 'a') * (factor2)) % MOD_2;
-            factor2 = (factor2 * RADIX_2) % MOD_2;
-        }
-        
-        return make_pair(hash1 % MOD_1, hash2 % MOD_2);
-    }
-    
     int strStr(string haystack, string needle) {
         int m = needle.size(), n = haystack.size();
         
         if (n < m) return -1;
         
-        ll MAX_WEIGHT_1 = 1, MAX_WEIGHT_2 = 1;
-        for (int i = 0; i < m; ++i){
-            MAX_WEIGHT_1 = (MAX_WEIGHT_1 * RADIX_1) % MOD_1;
-            MAX_WEIGHT_2 = (MAX_WEIGHT_2 * RADIX_2) % MOD_2;
-        }
+        vector<int> longest_border(m, 0);
         
-        pair<ll, ll> hashNeedle = hashPair(needle, m);
-        pair<ll, ll> hashHay = {0, 0};
+        int prev = 0, i = 1;
         
-        for (int windowStart = 0; windowStart <= n - m; ++windowStart){
-            if (windowStart == 0) hashHay = hashPair(haystack, m);
-            else{
-                hashHay.first = ((hashHay.first * RADIX_1) % MOD_1
-                        - ((int) (haystack[windowStart - 1] - 'a') * MAX_WEIGHT_1) % MOD_1
-                        + (int) (haystack[windowStart + m - 1] - 'a') + MOD_1) % MOD_1;
-                hashHay.second = ((hashHay.second * RADIX_2) % MOD_2
-                                 - ((int) (haystack[windowStart - 1] - 'a') * MAX_WEIGHT_2) % MOD_2
-                                 + (int) (haystack[windowStart + m - 1] - 'a') + MOD_2) % MOD_2;
+        while (i < m){
+            if (needle[i] == needle[prev]){
+                ++prev;
+                longest_border[i] = prev;
+                ++i;
             }
-            
-            if (hashNeedle.first == hashHay.first && hashNeedle.second == hashHay.second) return windowStart;
+            else{
+                if (prev == 0) {
+                    longest_border[i] = 0;
+                    ++i;
+                }
+                else prev = longest_border[prev - 1];
+            }
         }
         
+        int haystack_pointer = 0, needle_pointer = 0;
+        
+        while (haystack_pointer < n){
+            if (haystack[haystack_pointer] == needle[needle_pointer]){
+                ++needle_pointer;
+                ++haystack_pointer;
+                if (needle_pointer == m) return haystack_pointer - m;
+            }
+            else{
+                if (needle_pointer == 0) ++haystack_pointer;
+                else needle_pointer = longest_border[needle_pointer - 1];
+            }
+        }
         return -1;
     }
 };
