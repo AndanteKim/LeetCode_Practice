0c64328dfbe42cfd51a625ba8b19c6df953db620
Andrew Kim <ksb0724@utexas.edu>
2023-03-03 03:13:48 -0600

    Time: 38 ms (29.11%), Space: 13.8 MB (54.52%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,10 +1,41 @@
 class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        i, m, n = 0, len(haystack), len(needle)
+    RADIX, MOD, MAX_WEIGHT = 26, 1_000_000_033, 1
+    
+    def hash_value(self, m: int, string: str) -> int:
+        ans, factor = 0, 1
         
-        while i <= m - n:
-            if haystack[i:i+n] == needle:
-                return i
-            i += 1
+        for i in range(m - 1, -1, -1):
+            ans += ((ord(string[i]) - 97) * (factor)) % self.MOD
+            factor = (factor * self.RADIX) % self.MOD
+        
+        return ans % self.MOD
+    
+    def strStr(self, haystack: str, needle: str) -> int:
+        m, n = len(needle), len(haystack)
+        if n < m:
+            return -1
+            
+        for _ in range(m):
+            self.MAX_WEIGHT = (self.MAX_WEIGHT * self.RADIX) % self.MOD
+        
+        hash_needle = self.hash_value(m, needle)
+        
+        for window_start in range(n - m + 1):
+            if window_start == 0:
+                hash_hay = self.hash_value(m, haystack)
+            else:
+                hash_hay = ((hash_hay * self.RADIX) % self.MOD\
+                            - ((ord(haystack[window_start - 1]) - 97)
+                            * self.MAX_WEIGHT) % self.MOD
+                            + (ord(haystack[window_start + m - 1]) - 97)
+                            + self.MOD) % self.MOD
+            
+            if hash_needle == hash_hay:
+                for i in range(m):
+                    if needle[i] != haystack[i + window_start]:
+                        break
+                
+                if i == m - 1:
+                    return window_start
         
         return -1
