57a23cdd8e4856679d2f9e528c5329a3400c0e72
Andrew Kim <ksb0724@utexas.edu>
2023-03-05 03:15:55 -0600

    Time: 614 ms (99.35%), Space: 28.6 MB (78.70%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -5,35 +5,39 @@ class Solution:
         
         graph = defaultdict(list)
         for i in range(n):
-            if arr[i] in graph:
-                graph[arr[i]].append(i)
-            else:
-                graph[arr[i]] = [i]
+            graph[arr[i]].append(i)
         
-        curs = [0]
-        visited = {0}
+        curs = set([0])
+        visited = {0, n - 1}
         step = 0
         
+        other = set([n - 1])
+        
         while curs:
-            nex = []
+            if len(curs) > len(other):
+                curs, other = other, curs
+            nex = set()
             
             for node in curs:
-                if node == n - 1:
-                    return step
-            
                 for child in graph[arr[node]]:
+                    if child in other:
+                        return step + 1
+                    
                     if child not in visited:
                         visited.add(child)
-                        nex.append(child)
+                        nex.add(child)
                 
                 graph[arr[node]].clear()
                 
                 for child in [node - 1, node + 1]:
+                    if child in other:
+                        return step + 1
+                    
                     if 0 <= child < len(arr) and child not in visited:
                         visited.add(child)
-                        nex.append(child)
+                        nex.add(child)
             
             curs = nex
             step += 1
-            
+        
         return -1
