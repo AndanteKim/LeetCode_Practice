a9d0275be4e771c1630e5394fd107b7929e4d226
Andrew Kim <ksb0724@utexas.edu>
2023-03-07 07:25:23 -0600

    Time: 789 ms (19.80%), Space: 118.6 MB (68.92%) - LeetHub

--- 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
+++ 2398-maximum-number-of-robots-within-budget/2398-maximum-number-of-robots-within-budget.cpp
@@ -1,22 +1,24 @@
 class Solution {
+    int remove_stale(priority_queue<pair<int, int>>& pq, int& left){
+        while (!pq.empty() && pq.top().second <= left) pq.pop();
+        return !pq.empty()? pq.top().first : 0; 
+    }
+    
 public:
     int maximumRobots(vector<int>& chargeTimes, vector<int>& runningCosts, long long budget) {
-        int left = 0;
-        long long curr = 0;
-        int n = chargeTimes.size();
-        deque<int> *dq = new deque<int>;
+        int ans = 0, left = -1;
+        long long total_run = 0;
+        priority_queue<pair<int, int>> pq;
         
-        for (int right = 0; right < n; ++right){
-            curr += runningCosts[right];
-            while (!dq -> empty() && chargeTimes[dq -> back()] <= chargeTimes[right]) dq -> pop_back();
-            dq -> push_back(right);
-            if ((long long)(chargeTimes[dq -> front()] + (right - left + 1) * curr) > budget){
-                if (dq -> front() == left) dq -> pop_front();
-                curr -= runningCosts[left];
-                ++left;
-            }
+        for (int right = 0; right < runningCosts.size(); ++right){
+            total_run += runningCosts[right];
+            pq.push(make_pair(chargeTimes[right], right));
+            while (total_run * (right - left) + 1LL * remove_stale(pq, left) > budget)
+                total_run -= runningCosts[++left];
+            cout << right - left << endl;
+            ans = max(ans, right - left);
         }
         
-        return n - left;
+        return ans;
     }
 };
