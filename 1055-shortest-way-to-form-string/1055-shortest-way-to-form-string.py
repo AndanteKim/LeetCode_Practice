684fb609a4b66886827eb1dfa96d1828bff542b2
Andrew Kim <ksb0724@utexas.edu>
2023-03-10 01:21:39 +0900

    Time: 43 ms (78.18%), Space: 13.9 MB (75.69%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,24 +1,23 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        source_chars = set(source)
+        char_to_indices = defaultdict(list)
+        
+        for i, c in enumerate(source):
+            char_to_indices[c].append(i)
+        
+        source_iterator = 0
+        count = 1
         
         for char in target:
-            if char not in source_chars:
+            if char not in char_to_indices:
                 return -1
             
-        m = len(source)
-        source_iterator, count = 0, 0
-        
-        for char in target:
-            if source_iterator == 0:
+            index = bisect.bisect_left(char_to_indices[char], source_iterator)
+            
+            if index == len(char_to_indices[char]):
                 count += 1
+                source_iterator = char_to_indices[char][0] + 1
+            else:
+                source_iterator = char_to_indices[char][index] + 1
             
-            while source[source_iterator] != char:
-                source_iterator = (source_iterator + 1) % m
-                
-                if source_iterator == 0:
-                    count += 1
-            
-            source_iterator = (source_iterator + 1) % m
-        
         return count
