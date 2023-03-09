971705962bf07497dd85b23ebe0fd91169ab1723
Andrew Kim <ksb0724@utexas.edu>
2023-03-09 07:01:16 -0600

    Time: 60 ms (44.48%), Space: 13.8 MB (75.69%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,13 +1,4 @@
 class Solution:
-    def is_subsequence(self, to_check: str, in_string: str) -> bool:
-        i = j = 0
-        while i < len(to_check) and j < len(in_string):
-            if to_check[i] == in_string[j]:
-                i += 1
-            j += 1
-        
-        return i == len(to_check)
-    
     def shortestWay(self, source: str, target: str) -> int:
         source_chars = set(source)
         
@@ -15,10 +6,19 @@ class Solution:
             if char not in source_chars:
                 return -1
             
-        concatenated_source = source
-        count = 1
-        while not self.is_subsequence(target, concatenated_source):
-            concatenated_source += source
-            count += 1
+        m = len(source)
+        source_iterator, count = 0, 0
+        
+        for char in target:
+            if source_iterator == 0:
+                count += 1
+            
+            while source[source_iterator] != char:
+                source_iterator = (source_iterator + 1) % m
+                
+                if source_iterator == 0:
+                    count += 1
+            
+            source_iterator = (source_iterator + 1) % m
         
         return count
