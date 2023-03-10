4b8ad467081c1b9a36f9a34521affa08997bef44
Andrew Kim <ksb0724@utexas.edu>
2023-03-11 02:51:31 +0900

    Time: 42 ms (76.27%), Space: 15 MB (11.08%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,23 +1,25 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        char_to_indices = defaultdict(list)
+        source_length = len(source)
+        next_occurrence = [defaultdict(int) for idx in range(source_length)]
         
-        for i, c in enumerate(source):
-            char_to_indices[c].append(i)
+        next_occurrence[source_length - 1][source[source_length - 1]] = source_length - 1
         
-        source_iterator = 0
-        count = 1
+        for idx in range(source_length - 2, -1, -1):
+            next_occurrence[idx] = next_occurrence[idx + 1].copy()
+            next_occurrence[idx][source[idx]] = idx
+            
+        source_iterator, count = 0, 1
         
         for char in target:
-            if char not in char_to_indices:
+            if char not in next_occurrence[0]:
                 return -1
             
-            index = bisect.bisect_left(char_to_indices[char], source_iterator)
-            
-            if index == len(char_to_indices[char]):
+            if (source_iterator == source_length or \
+               char not in next_occurrence[source_iterator]):
                 count += 1
-                source_iterator = char_to_indices[char][0] + 1
-            else:
-                source_iterator = char_to_indices[char][index] + 1
+                source_iterator = 0
             
+            source_iterator = next_occurrence[source_iterator][char] + 1
+        
         return count
