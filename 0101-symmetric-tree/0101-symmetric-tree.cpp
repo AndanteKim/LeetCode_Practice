f6b8cf78e481bf8c54ec9d6ea0986a9fb5c2d90e
Andrew Kim <ksb0724@utexas.edu>
2023-03-13 22:09:17 +0900

    Time: 7 ms (53.98%), Space: 16.7 MB (8.51%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -10,15 +10,27 @@
  * };
  */
 class Solution {
-    bool isMirror(TreeNode* left, TreeNode* right){
-        if (left == nullptr && right == nullptr) return true;
-        if (left == nullptr || right == nullptr) return false;
-        
-        return (left -> val == right -> val) && isMirror(left -> left, right -> right) && isMirror(left -> right, right -> left);
-    }
-    
 public:
     bool isSymmetric(TreeNode* root) {
-        return isMirror(root -> left, root -> right);
+        queue<TreeNode*> *q = new queue<TreeNode*>;
+        q -> push(root);
+        q -> push(root);
+        
+        while(!q -> empty()){
+            TreeNode* t1 = q -> front();
+            q -> pop();
+            TreeNode* t2 = q -> front();
+            q -> pop();
+            
+            if (!t1 && !t2) continue;
+            if (!t1 || !t2) return false;
+            if (t1 -> val != t2 -> val) return false;
+            q -> push(t1 -> left);
+            q -> push(t2 -> right);
+            q -> push(t1 -> right);
+            q -> push(t2 -> left);
+        }
+        
+        return true;
     }
 };
