b2c9779e6e71dc14436dfda453017a43ae3d3804
Andrew Kim <ksb0724@utexas.edu>
2023-03-17 02:01:26 +0900

    Time: 8 ms (73.68%), Space: 18.8 MB (64.59%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -10,35 +10,13 @@
  * };
  */
 class Solution {
-    private:
-    queue<pair<TreeNode*, int>> next_items;
-    int max_depth = 0;
-    
-    int next_maxDepth(){
-        if (next_items.size() == 0) return max_depth;
-        
-        auto next_item = next_items.front();
-        next_items.pop();
-        
-        auto next_node = next_item.first;
-        auto next_level = next_item.second + 1;
-        
-        max_depth = max(max_depth, next_level);
-        
-        if (next_node -> left != NULL) next_items.push(make_pair(next_node -> left, next_level));
-        if (next_node -> right != NULL) next_items.push(make_pair(next_node -> right, next_level));
-        return next_maxDepth();
+    int dfs(TreeNode* root){
+        if (!root) return 0;
+        return max(1 + dfs(root -> left), 1 + dfs(root -> right));
     }
     
-    public:
-        int maxDepth(TreeNode* root) {
-            if (root == NULL) return 0;
-            
-            queue<pair<TreeNode*, int>> empty;
-            //swap(next_items, empty);
-            max_depth = 0;
-            
-            next_items.push(make_pair(root, 0));
-            return next_maxDepth();
-        }
+public:
+    int maxDepth(TreeNode* root) {
+        return dfs(root);
+    }
 };
