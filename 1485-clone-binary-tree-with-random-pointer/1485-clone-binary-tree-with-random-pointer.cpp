2f48992c257dc1eba16d174d3ad2bb6ec9ebcd23
Andrew Kim <ksb0724@utexas.edu>
2023-03-16 22:20:38 +0900

    Time: 269 ms (58.33%), Space: 76.2 MB (30.95%) - LeetHub

--- 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.cpp
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.cpp
@@ -13,24 +13,48 @@
 
 class Solution {
     unordered_map<Node*, NodeCopy*> seen;
-    
-    NodeCopy* dfs(Node* root){
-        if (!root) return NULL;;
+    NodeCopy* bfs(Node* root){
+        if (!root) return nullptr;
+        queue<Node*> *pending = new queue<Node*>;
+        pending -> push(root);
+        seen[root] = new NodeCopy(root -> val);
         
-        if (seen.find(root) != seen.end()) return seen[root];
-        NodeCopy* new_root = new NodeCopy(root -> val);
-        seen[root] = new_root;
-        new_root -> left = dfs(root -> left);
-        new_root -> right = dfs(root -> right);
-        new_root -> random = dfs(root -> random);
+        while (!pending -> empty()){
+            Node* old_node = pending -> front();
+            pending -> pop();
+            NodeCopy* new_node = seen[old_node];
+            
+            if (old_node -> left){
+                if (seen.find(old_node -> left) == seen.end()){
+                    pending -> push(old_node -> left);
+                    seen[old_node -> left] = new NodeCopy(old_node -> left -> val);
+                }
+                new_node -> left = seen[old_node -> left];
+            }
+            
+            if (old_node -> right){
+                if (seen.find(old_node -> right) == seen.end()){
+                    pending -> push(old_node -> right);
+                    seen[old_node -> right] = new NodeCopy(old_node -> right -> val);
+                }
+                new_node -> right = seen[old_node -> right];
+            }
+            
+            if (old_node -> random){
+                if (seen.find(old_node -> random) == seen.end()){
+                    pending -> push(old_node -> random);
+                    seen[old_node -> random] = new NodeCopy(old_node -> random -> val);
+                }
+                new_node -> random = seen[old_node -> random];
+            }
+        }
         
-        return new_root;
+        return seen[root];
     }
     
 public:
     NodeCopy* copyRandomBinaryTree(Node* root) {
-        NodeCopy* new_root = dfs(root);
-        
+        NodeCopy* new_root = bfs(root);
         return new_root;
     }
 };
