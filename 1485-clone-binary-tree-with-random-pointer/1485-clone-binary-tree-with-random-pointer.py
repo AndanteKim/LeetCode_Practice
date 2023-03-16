da60940d92a7669fa062cda03858889880ccb4c7
Andrew Kim <ksb0724@utexas.edu>
2023-03-16 21:35:17 +0900

    Time: 236 ms (71.54%), Space: 19.2 MB (73.17%) - LeetHub

--- 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
+++ 1485-clone-binary-tree-with-random-pointer/1485-clone-binary-tree-with-random-pointer.py
@@ -8,35 +8,23 @@
 
 class Solution:
     def __init__(self):
-        self.new_old_pairs: dict[str, int] = {None: None}
-            
-    def deep_copy(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
+        self.seen: dict[str, int] = {None: None}
+    
+    def dfs(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
         if not root:
             return None
         
+        if self.seen.get(root):
+            return self.seen.get(root)
+        
         new_root = NodeCopy(root.val)
-        new_root.left = self.deep_copy(root.left)
-        new_root.right = self.deep_copy(root.right)
-        self.new_old_pairs[root] = new_root
+        self.seen[root] = new_root
+        
+        new_root.left = self.dfs(root.left)
+        new_root.right = self.dfs(root.right)
+        new_root.random = self.dfs(root.random)
         return new_root
-    
-    def map_random_pointers(self, old_node: 'Optional[Node]') -> None:
-        if not old_node: return
-        new_node = self.new_old_pairs[old_node]
-        old_node_random = old_node.random
-        new_node_random = self.new_old_pairs[old_node_random]
-        
-        new_node.random = new_node_random
-        
-        self.map_random_pointers(old_node.left)
-        self.map_random_pointers(old_node.right)
     
     def copyRandomBinaryTree(self, root: 'Optional[Node]') -> 'Optional[NodeCopy]':
-        new_root = self.deep_copy(root)
-        
-        self.map_random_pointers(root)
+        new_root = self.dfs(root)
         return new_root
-        
-        
-            
-        
