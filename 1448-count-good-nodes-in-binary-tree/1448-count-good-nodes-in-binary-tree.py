2aac5f423ede5c8846e14cb44fa7ffc3fd8777cb
Andrew Kim <ksb0724@utexas.edu>
2023-03-18 01:24:08 +0900

    Time: 271 ms (46.99%), Space: 32.6 MB (13.42%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.py
@@ -5,18 +5,20 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, node: TreeNode, max_so_far: int) -> int:
-        if not node: return 0
-        
-        left = self.dfs(node.left, max(max_so_far, node.val))
-        
-        right = self.dfs(node.right, max(max_so_far, node.val))
-        
-        ans = left + right
-        if node.val >= max_so_far:
-            ans += 1
-        
-        return ans
-    
     def goodNodes(self, root: TreeNode) -> int:
-        return self.dfs(root, float('-inf'))
+        if not root: return 0
+        
+        stack, ans = [(root, float('-inf'))], 0
+        
+        while stack:
+            node, max_so_far = stack.pop()
+            if node.val >= max_so_far:
+                ans += 1
+                
+            if node.left:
+                stack.append((node.left, max(max_so_far, node.val)))
+            
+            if node.right:
+                stack.append((node.right, max(max_so_far, node.val)))
+                
+        return ans
