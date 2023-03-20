18f26959cc30a22139a9a7796420fcbe3f27f444
Andrew Kim <ksb0724@utexas.edu>
2023-03-20 21:41:31 +0900

    Time: 22 ms (40.04%), Space: 20.3 MB (76.11%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        for (int i = 0; i < flowerbed.size(); ++i){
+            if (flowerbed[i] == 0){
+                bool empty_left_plot = (i == 0) || (flowerbed[i - 1] == 0);
+                bool empty_right_plot = (i == flowerbed.size() - 1) || (flowerbed[i + 1] == 0);
+                
+                if (empty_left_plot && empty_right_plot){
+                    flowerbed[i] = 1;
+                    ++count;
+                }
+            }
+        }
+        
+        return count >= n;
+    }
+};
