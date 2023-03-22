96adf96d89994371dcc39670ab870fd98a0f2c28
Andrew Kim <ksb0724@utexas.edu>
2023-03-22 22:04:11 +0900

    Time: 101 ms (36.77%), Space: 33.2 MB (48.39%) - LeetHub

--- 1236-web-crawler/1236-web-crawler.cpp
+++ 1236-web-crawler/1236-web-crawler.cpp
@@ -8,25 +8,24 @@
  */
 
 class Solution {
-    string start_hostname;
-    unordered_set<string> visited;
-    
-    string get_hostname(string url){
-        int pos = min(url.size(), url.find('/', 7));
-        return url.substr(7, pos - 7);
-    }
-    
-    void dfs(string url, HtmlParser htmlParser){
-        visited.insert(url);
-        for (string& nextUrl : htmlParser.getUrls(url)){
-            if (get_hostname(nextUrl) == start_hostname && !visited.count(nextUrl)) dfs(nextUrl, htmlParser);
-        }
-    }
-    
 public:
     vector<string> crawl(string startUrl, HtmlParser htmlParser) {
-        start_hostname = get_hostname(startUrl);
-        dfs(startUrl, htmlParser);
-        return vector<string>(visited.begin(), visited.end());
+        function<string(string)> get_hostname = [](string url) -> string{
+            int pos = min(url.size(), url.find('/', 7));
+            return url.substr(7, pos - 7);
+        };
+        
+        string start_hostname = get_hostname(startUrl);
+        unordered_set<string> *visited = new unordered_set<string>;
+        
+        function<void(string)> dfs = [&](string url) -> void{
+            visited -> insert(url);
+            for (string& next_url : htmlParser.getUrls(url)){
+                if (get_hostname(next_url) == start_hostname && !visited -> count(next_url)) dfs(next_url);
+            }
+        };
+        
+        dfs(startUrl);
+        return vector<string>(visited -> begin(), visited -> end());
     }
 };
