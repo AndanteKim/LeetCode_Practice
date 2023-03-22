5b097e2b6e89dc7a28c850c15efdfe5505fcf78c
Andrew Kim <ksb0724@utexas.edu>
2023-03-22 16:31:41 +0900

    Time: 1854 ms (56.04%), Space: 58.7 MB (96.31%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,21 +1,38 @@
+class UnionFind:
+    parent, rank = [], []
+    
+    def __init__(self, size: int):
+        self.parent, self.rank = [0] * size, [0] * size
+        for i in range(size):
+            self.parent[i] = i
+            
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union_set(self, x: int, y: int) -> None:
+        xset, yset = self.find(x), self.find(y)
+        if xset == yset:
+            return
+        elif self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] += 1
+
 class Solution:
-    answer = float('inf')
-    def dfs(self, node: int, adj: List[List[int]], visit: List[bool]):
-        visit[node] = True
-        for edge in adj[node]:
-            self.answer = min(self.answer, edge[1])
-            if not visit[edge[0]]:
-                self.dfs(edge[0], adj, visit)
-    
-    
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        adj = [[] for _ in range(n+1)]
+        dsu = UnionFind(n + 1)
+        answer = float('inf')
         
         for road in roads:
-            adj[road[0]].append([road[1], road[2]])
-            adj[road[1]].append([road[0], road[2]])
+            dsu.union_set(road[0], road[1])
         
-        visit = [False] * (n + 1)
-        self.dfs(1, adj, visit)
+        for road in roads:
+            if dsu.find(1) == dsu.find(road[0]):
+                answer = min(answer, road[2])
         
-        return self.answer
+        return answer
