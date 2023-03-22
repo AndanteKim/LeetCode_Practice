0695892f92e7f08541abdb8f6be4e9c38654ff13
Andrew Kim <ksb0724@utexas.edu>
2023-03-22 15:53:40 +0900

    Time: 1894 ms (50.67%), Space: 66.9 MB (69.13%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,24 +1,21 @@
 class Solution:
-    def bfs(self, n: int, adj: List[List[int]]) -> int:
-        visit = [False] * (n + 1)
-        queue = deque([1])
-        answer = float('inf')
-        visit[1] = True
-        
-        while queue:
-            node = queue.popleft()
-            for edge in adj[node]:
-                answer = min(answer, edge[1])
-                if not visit[edge[0]]:
-                    visit[edge[0]] = True
-                    queue.append(edge[0])
-        return answer
+    answer = float('inf')
+    def dfs(self, node: int, adj: List[List[int]], visit: List[bool]):
+        visit[node] = True
+        for edge in adj[node]:
+            self.answer = min(self.answer, edge[1])
+            if not visit[edge[0]]:
+                self.dfs(edge[0], adj, visit)
+    
     
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        adj = [[] for i in range(n+1)]
+        adj = [[] for _ in range(n+1)]
         
         for road in roads:
             adj[road[0]].append([road[1], road[2]])
             adj[road[1]].append([road[0], road[2]])
-        print(adj)
-        return self.bfs(n, adj)
+        
+        visit = [False] * (n + 1)
+        self.dfs(1, adj, visit)
+        
+        return self.answer
