b141560cfe74febc3b6778b015adf5a05c09164b
Andrew Kim <ksb0724@utexas.edu>
2023-03-23 16:18:07 +0900

    Time: 461 ms (97.74%), Space: 34.3 MB (55.64%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,24 +1,29 @@
 class Solution:
-    def dfs(self, node: int, adj: List[List[int]], visited: List[bool]) -> None:
-        visited[node] = True
-        for neighbor in adj[node]:
-            if not visited[neighbor]:
-                self.dfs(neighbor, adj, visited)
-    
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections) < n - 1:
             return -1
         
-        adj, visited = [[] for _ in range(n)], [False] * n
-        for connection in connections:
-            adj[connection[0]].append(connection[1])
-            adj[connection[1]].append(connection[0])
+        adj = [[] for _ in range(n)]
         
+        for start, end in connections:
+            adj[start].append(end)
+            adj[end].append(start)
+            
         connected = 0
-        for i in range(n):
-            if not visited[i]:
+        visited = [False] * n
+        
+        for node in range(n):
+            if not visited[node]:
                 connected += 1
-                self.dfs(i, adj, visited)
+                queue = deque([node])
+                visited[node] = True
+                
+                while queue:
+                    curr = queue.popleft()
+                    for neighbor in adj[curr]:
+                        if not visited[neighbor]:
+                            visited[neighbor] = True
+                            queue.append(neighbor)
+                
         
         return connected - 1
-        
