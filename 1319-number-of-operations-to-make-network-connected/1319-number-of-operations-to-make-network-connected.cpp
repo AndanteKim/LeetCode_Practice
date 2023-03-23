c39a85be5a86bf9d5f0a44fd58747389a0e8f50f
Andrew Kim <ksb0724@utexas.edu>
2023-03-23 16:36:11 +0900

    Time: 167 ms (29.61%), Space: 70 MB (5.16%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,13 +1,4 @@
 class Solution {
-    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited){
-        visited[node] = true;
-        for (int adjNode : adj[node]){
-            if (!visited[adjNode]){
-                dfs(adjNode, adj, visited);
-            }
-        }
-    }
-    
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
         if (connections.size() < n - 1) return -1;
@@ -15,7 +6,7 @@ public:
         vector<vector<int>> adj(n);
         vector<bool> visited(n, false);
         
-        for (vector<int>& connection : connections){
+        for (auto& connection : connections){
             adj[connection[0]].push_back(connection[1]);
             adj[connection[1]].push_back(connection[0]);
         }
@@ -24,7 +15,20 @@ public:
         for (int node = 0; node < n; ++node){
             if (!visited[node]){
                 ++connected;
-                dfs(node, adj, visited);
+                visited[node] = true;
+                
+                queue<int> *q = new queue<int>;
+                q -> push(node);
+                while (!q -> empty()){
+                    int curr = q -> front();
+                    q -> pop();
+                    for(int neighbor : adj[curr]){
+                        if (!visited[neighbor]){
+                            visited[neighbor] = true;
+                            q -> push(neighbor);
+                        }
+                    }
+                }
             }
         }
         
