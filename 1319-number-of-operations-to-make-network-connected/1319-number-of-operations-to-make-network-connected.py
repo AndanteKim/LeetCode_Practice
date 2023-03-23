021b2647b302ddb24e42eb52527930edaf8d0f04
Andrew Kim <ksb0724@utexas.edu>
2023-03-23 16:49:28 +0900

    Time: 533 ms (44.83%), Space: 33.9 MB (93.14%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,29 +1,37 @@
+class UnionFind:
+    def __init__(self, size: int):
+        self.parent, self.rank = [], [0] * size
+        for i in range(size):
+            self.parent.append(i)
+            
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union_set(self, x: int, y: int) -> None:
+        xset, yset = self.find(x), self.find(y)
+        if self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] += 1
+        
+
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
         if len(connections) < n - 1:
             return -1
         
-        adj = [[] for _ in range(n)]
+        dsu, connected = UnionFind(n), n
         
-        for start, end in connections:
-            adj[start].append(end)
-            adj[end].append(start)
-            
-        connected = 0
-        visited = [False] * n
-        
-        for node in range(n):
-            if not visited[node]:
-                connected += 1
-                queue = deque([node])
-                visited[node] = True
-                
-                while queue:
-                    curr = queue.popleft()
-                    for neighbor in adj[curr]:
-                        if not visited[neighbor]:
-                            visited[neighbor] = True
-                            queue.append(neighbor)
-                
+        for connection in connections:
+            if dsu.find(connection[0]) != dsu.find(connection[1]):
+                connected -= 1
+                dsu.union_set(connection[0], connection[1])
         
         return connected - 1
+        
+        
