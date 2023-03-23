2e458ae29301a84e2467a2c792d9a357d617b7a7
Andrew Kim <ksb0724@utexas.edu>
2023-03-23 22:07:28 +0900

    Time: 182 ms (99.48%), Space: 17.8 MB (61.86%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        next_level = deque([root])
+        
+        while next_level:
+            curr_level = next_level
+            next_level = deque()
+            
+            for node in curr_level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+        return sum([node.val for node in curr_level])
