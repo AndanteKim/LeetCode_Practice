4070e5f3634f0bc23192e2356dcba4dc39ff62f2
Andrew Kim <ksb0724@utexas.edu>
2023-03-25 01:54:29 +0900

    Time: 7 ms (96.12%), Space: 21.9 MB (13.51%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,18 +9,27 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#define ll long long
+
 class Solution {
-    bool dfs(TreeNode* node, long long small, long long large){
-        if (!node) return true;
-        if (!(small < node -> val && node -> val < large)) return false;
-        
-        bool left = dfs(node -> left, small, node -> val);
-        bool right = dfs(node -> right, node -> val, large);
-        return left && right;
-    }
-    
 public:
     bool isValidBST(TreeNode* root) {
-        return dfs(root, LONG_LONG_MIN, LONG_LONG_MAX);
+        stack<pair<TreeNode*, pair<ll, ll>>> *st = new stack<pair<TreeNode*, pair<ll, ll>>>;
+        st -> push({root, {LONG_LONG_MIN, LONG_LONG_MAX}});
+        
+        while(!st -> empty()){
+            auto [node, range] = st -> top();
+            ll small = range.first;
+            ll large = range.second;
+            st -> pop();
+            
+            if (!(small < (ll)node -> val && (ll)node -> val < large)) return false;
+            
+            if (node -> left) st -> push({node -> left, {small, (ll)node -> val}});
+            if (node -> right) st -> push({node -> right, {(ll)node -> val, large}});
+            
+        }
+        
+        return true;
     }
 };
