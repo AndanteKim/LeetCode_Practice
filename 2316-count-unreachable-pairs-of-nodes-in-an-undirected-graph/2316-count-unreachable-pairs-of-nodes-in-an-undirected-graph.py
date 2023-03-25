13c52e1fb654ee2add66ff2491728f03865116ed
Andrew Kim <ksb0724@utexas.edu>
2023-03-25 16:56:17 +0900

    Time: 2023 ms (95.02%), Space: 75 MB (74.38%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,11 +1,17 @@
 class Solution:
-    def dfs(self, node: int, adj: List[List[int]], visited: List[bool]) -> None:
-        count = 1
-        visited[node] = True
-        for neighbor in adj[node]:
-            if not visited[neighbor]:
-                count += self.dfs(neighbor, adj, visited)
+    def bfs(self, node: int, adj: List[List[int]], visited: List[bool]) -> int:
+        visited[node], count = True, 1
+        queue = deque([node])
+        
+        while queue:
+            curr_node = queue.popleft()
+            for neighbor in adj[curr_node]:
+                if not visited[neighbor]:
+                    visited[neighbor] = True
+                    count += 1
+                    queue.append(neighbor)
         return count
+        
     
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
         adj = [[] for _ in range(n)]
@@ -13,16 +19,14 @@ class Solution:
         for start, end in edges:
             adj[start].append(end)
             adj[end].append(start)
-            
+        
         numberOfPairs, sizeOfComponent, remainingNodes = 0, 0, n
         visited = [False] * n
         
         for node in range(n):
             if not visited[node]:
-                sizeOfComponent = self.dfs(node, adj, visited)
+                sizeOfComponent = self.bfs(node, adj, visited)
                 numberOfPairs += sizeOfComponent * (remainingNodes - sizeOfComponent)
                 remainingNodes -= sizeOfComponent
-            
-        return numberOfPairs
-            
         
+        return numberOfPairs
