a39215be6726dac5bb649e7e38ee40fca0a79195
Andrew Kim <ksb0724@utexas.edu>
2023-03-26 18:14:10 +0900

    Time: 202 ms (72.19%), Space: 92.5 MB (88.87%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,33 +1,42 @@
 class Solution {
-    int ans = -1;
-    
-    void dfs(int node, vector<int>& edges, unordered_map<int, int>& dist, \
-             vector<bool>& visited){
-        visited[node] = true;
-        int neighbor = edges[node];
-        
-        if (neighbor != -1 && !visited[neighbor]){
-            dist[neighbor] = dist[node] + 1;
-            dfs(neighbor, edges, dist, visited);
-        }
-        else if (neighbor != -1 && dist.count(neighbor) > 0){
-            ans = max(ans, dist[node] - dist[neighbor] + 1);
-        }
-    }
-    
 public:
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
-        vector<bool> visited(n, false);
+        vector<bool> visited(n);
+        vector<int> indegree(n);
         
-        for (int node = 0; node < n; ++node){
-            if (!visited[node]){
-                unordered_map<int, int> dist;
-                dist[node] = 1;
-                dfs(node, edges, dist, visited);
+        for (int edge : edges) if (edge != -1) ++indegree[edge];
+        
+        queue<int> *q = new queue<int>;
+        for(int node = 0; node < n; ++node){
+            if (indegree[node] == 0) q -> push(node);
+        }
+        
+        while(!q -> empty()){
+            int node = q -> front();
+            q -> pop();
+            visited[node] = true;
+            int neighbor = edges[node];
+            if (neighbor != -1){
+                --indegree[neighbor];
+                if (indegree[neighbor] == 0) q -> push(neighbor);
             }
         }
         
+        int ans = -1;
+        for (int node = 0; node < n; ++node){
+            if (!visited[node]){
+                int neighbor = edges[node];
+                int count = 1;
+                visited[node] = true;
+                while (neighbor != node){
+                    visited[neighbor] = true;
+                    ++count;
+                    neighbor = edges[neighbor];
+                }
+                ans = max(ans, count);
+            }
+        }
         return ans;
     }
 };
