e25cb1a8f4ffcae1c2e0c87cc210ae90d9dd4299
Andrew Kim <ksb0724@utexas.edu>
2023-03-28 21:57:55 +0900

    Time: 4 ms (69.83%), Space: 9.6 MB (81.02%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -5,7 +5,7 @@ public:
         for (int i = 1; i < dp.size(); ++i){
             if (find(days.begin(), days.end(), i) != days.end()){
                 dp[i] = min(min(dp[max(i-1, 0)] + costs[0], dp[max(i-7, 0)] + costs[1]),\
-                            min(dp[max(i-7, 0)] + costs[1], dp[max(i-30, 0)]) + costs[2]);
+                            min(dp[max(i-7, 0)] + costs[1], dp[max(i-30, 0)] + costs[2]));
             }
             else dp[i] = dp[i-1];
         }
