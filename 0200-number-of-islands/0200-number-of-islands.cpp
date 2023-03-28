6db8eb2fa97c3d09b7037c1a1e62bfdb6f6b285e
Andrew Kim <ksb0724@utexas.edu>
2023-03-29 03:33:03 +0900

    Time: 97 ms (10.88%), Space: 19 MB (19.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,28 +1,36 @@
 class Solution {
-public:
-    int numIslands(vector<vector<char>>& grid) {
-        deque<pair<pair<int, int>, bool>> *dq = new deque<pair<pair<int, int>, bool>>;
-        int rows = grid.size(), cols = grid[0].size(), ans = 0;
-        vector<vector<bool>>visited(rows, vector<bool>(cols, false));
-        for (int i = 0; i < rows; ++i){
-            for (int j = 0; j < cols; ++j)
-                if (grid[i][j] == '1') dq -> push_back({{i, j}, true});
-        }
-        
-        while (!dq -> empty()){
-            auto it = dq -> front();
-            auto& [i,j] = it.first;
-            bool is_origin = it.second;
-            dq -> pop_front();
-            if (!visited[i][j]){
-                visited[i][j] = true;
-                if (j > 0 && grid[i][j - 1] == '1') dq -> push_front({{i, j - 1}, false});
-                if (i > 0 && grid[i - 1][j] == '1') dq -> push_front({{i - 1, j}, false});
-                if (j < cols - 1 && grid[i][j + 1] == '1') dq -> push_front({{i, j + 1}, false});
-                if (i < rows - 1 && grid[i + 1][j] == '1') dq -> push_front({{i + 1, j}, false});
-                if (is_origin) ++ans;
+    vector<pair<int, int>> directions{{0,1}, {1,0}, {0, -1}, {-1, 0}};
+    int m, n;
+    vector<vector<char>> grid;
+    set<pair<int, int>> seen;
+    bool valid(int row, int col){
+        return (0 <= row && row < m) && (0 <= col && col < n) && this -> grid[row][col] == '1'; 
+    }
+    
+    void dfs(int row, int col){
+        for (auto& [dx, dy] : directions){
+            int next_row = row + dy, next_col = col + dx;
+            if (valid(next_row, next_col) && seen.find({next_row, next_col}) == seen.end()){
+                seen.insert({next_row, next_col});
+                dfs(next_row, next_col);
             }
         }
+    }
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        this -> grid = grid;
+        int ans = 0;
+        for (int row = 0; row < m; ++row){
+            for (int col = 0; col < n; ++col){
+                if (grid[row][col] == '1' && seen.find({row, col}) == seen.end()){
+                    ++ans;
+                    seen.insert({row, col});
+                    dfs(row, col);
+                }
+            }
+        }
+        
         return ans;
     }
 };
