1b4057844b11d63fdfde1dae75ad921103e47193
Andrew Kim <ksb0724@utexas.edu>
2023-03-30 00:02:12 +0900

    Time: 44 ms (64.24%), Space: 13.8 MB (98.73%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -1,19 +1,11 @@
 class Solution:
-    def findMaxSatisfaction(self, satisfaction: List[int], memo: List[List[int]], index: int, time: int) -> int:
-        if index == len(satisfaction):
-            return 0
-        
-        if memo[index][time] != -1:
-            return memo[index][time]
-        
-        memo[index][time] = max(satisfaction[index] * time + \
-                                self.findMaxSatisfaction(satisfaction, memo, index + 1, time + 1),\
-                                self.findMaxSatisfaction(satisfaction, memo, index + 1, time))
-        return memo[index][time]
-    
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
         satisfaction.sort()
-        m = len(satisfaction)
-        memo = [[-1] * (m + 1) for _ in range(m+1)]
+        maxSatisfaction, suffixSum = 0, 0
         
-        return self.findMaxSatisfaction(satisfaction, memo, 0, 1)
+        for i in range(len(satisfaction) - 1, -1, -1):
+            if suffixSum + satisfaction[i] > 0:
+                suffixSum += satisfaction[i]
+                maxSatisfaction += suffixSum
+        
+        return maxSatisfaction
