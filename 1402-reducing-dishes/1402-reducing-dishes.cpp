129cbed06e8fd68aab078327f060d33968f96258
Andrew Kim <ksb0724@utexas.edu>
2023-03-30 00:06:30 +0900

    Time: 3 ms (94.52%), Space: 8 MB (89.59%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,18 +1,13 @@
 class Solution {
-    int findMaxSatisfaction(vector<int>& satisfaction, vector<vector<int>>& memo, int index, int time){
-        if (index == satisfaction.size()) return 0;
-        
-        if (memo[index][time] != -1) return memo[index][time];
-        
-        return memo[index][time] = max(satisfaction[index] * time + findMaxSatisfaction(satisfaction, memo, index + 1, time + 1),\
-                                      findMaxSatisfaction(satisfaction, memo, index + 1, time));
-    }
-    
 public:
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(), satisfaction.end());
-        int m = satisfaction.size();
-        vector<vector<int>> memo(m + 1, vector<int>(m+1, -1));
-        return findMaxSatisfaction(satisfaction, memo, 0, 1);
+        int maxSatisfaction = 0, suffixSum = 0;
+        
+        for (int i = satisfaction.size() - 1; i >= 0 && satisfaction[i] + suffixSum > 0; --i){
+            suffixSum += satisfaction[i];
+            maxSatisfaction += suffixSum;
+        }
+        return maxSatisfaction;
     }
 };
