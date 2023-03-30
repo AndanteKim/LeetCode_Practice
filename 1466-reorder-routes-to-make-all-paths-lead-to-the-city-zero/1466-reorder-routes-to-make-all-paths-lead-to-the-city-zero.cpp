39267b692393c17abb989872d8d46b19d4f17ce8
Andrew Kim <ksb0724@utexas.edu>
2023-03-30 17:17:38 +0900

    Time: 348 ms (77.63%), Space: 107.8 MB (56.07%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,34 +1,30 @@
 class Solution {
-    int count = 0;
-    void bfs(int node, int n, vector<vector<pair<int, int>>>& adj){
-        queue<int> *q = new queue<int>;
-        vector<bool> visited(n, false);
-        q -> push(node);
-        visited[node] = true;
-        
-        while (!q -> empty()){
-            int curr_node = q -> front();
-            q -> pop();
-            for (auto& [neighbor, sign] : adj[curr_node]){
-                if (!visited[neighbor]) {
-                    visited[neighbor] = true;
-                    count += sign;
-                    q -> push(neighbor);
-                }
-            }
-        }
-        
-    }
+    int ans = 0;
     
 public:
+    void dfs(int node, vector<vector<pair<int, int>>>& adj, vector<bool>& visited){
+        visited[node] = true;
+        for (const auto& [neighbor, score] : adj[node]){
+            if(!visited[neighbor]){
+                ans += score;
+                dfs(neighbor, adj, visited);
+            }
+        }
+    }
+    
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int, int>>> adj(n);
-        for (vector<int>& connection : connections){
+        vector<bool> visited(n);
+        
+        for (const vector<int>& connection : connections){
             adj[connection[0]].push_back({connection[1], 1});
             adj[connection[1]].push_back({connection[0], 0});
         }
         
-        bfs(0, n, adj);
-        return count;
+        for (int node = 0; node < n; ++node){
+            if(!visited[node]) dfs(node, adj, visited);
+        }
+        
+        return ans;
     }
 };
