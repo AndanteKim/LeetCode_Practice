24aca739a547e18937cea2e39b620b1c1a293b84
Andrew Kim <ksb0724@utexas.edu>
2023-04-01 01:45:13 +0900

    Time: 17 ms (87.61%), Space: 12.1 MB (87.26%) - LeetHub

--- 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
+++ 0323-number-of-connected-components-in-an-undirected-graph/0323-number-of-connected-components-in-an-undirected-graph.cpp
@@ -1,32 +1,47 @@
-class Solution {
-    void dfs(int node, vector<vector<int>>& adj, vector<bool>& visited){
-        visited[node] = true;
-        
-        for (int neighbor : adj[node]){
-            if (!visited[neighbor]){
-                dfs(neighbor, adj, visited);
-            }
-        }
+class UnionFind{
+private:
+    vector<int> parent, rank;
+public:
+    UnionFind(int size){
+        parent.resize(size);
+        rank.resize(size, 0);
+        for (int i = 0; i < size; ++i) parent[i] = i;
     }
     
+    int find(int x){
+        if (parent[x] != x) parent[x] = find(parent[x]);
+        return parent[x];
+    }
+    
+    int UnionSet(int x, int y){
+        int xset = find(x), yset = find(y);
+        if (xset == yset)
+            return 0;
+        else{
+            if (rank[xset] > rank[yset]){
+                rank[xset] += rank[yset];
+                parent[yset] = xset;
+            }
+            else{
+                rank[yset] += rank[xset];
+                parent[xset] = yset;
+            }
+            
+            return 1;
+        }
+    }
+};
+
+class Solution {
 public:
     int countComponents(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> adj(n);
-        vector<bool> visited(n, false);
-        int ans = 0;
+        int components = n;
+        UnionFind uf(n);
         
-        for (const vector<int>& edge: edges){
-            adj[edge[0]].push_back(edge[1]);
-            adj[edge[1]].push_back(edge[0]);
+        for (const vector<int>& edge : edges){
+            components -= uf.UnionSet(edge[0], edge[1]);
         }
         
-        for (int node = 0; node < n; ++node){
-            if (!visited[node]) {
-                dfs(node, adj, visited);
-                ++ans;
-            }
-        }
-        
-        return ans;
+        return components;
     }
 };
