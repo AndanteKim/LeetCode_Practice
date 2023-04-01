e0d18cd8a0ad68b1b35a6c3bb39952b71ace9bfb
Andrew Kim <ksb0724@utexas.edu>
2023-04-01 17:10:39 +0900

    Time: 38 ms (69.66%), Space: 27.4 MB (93.53%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        
-        while (left <= right){
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) left = mid + 1;
-            else right = mid - 1;
-        }
-        return -1;
+        int idx = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+        if (idx > 0 && nums[idx - 1] == target)
+            return idx - 1;
+        else
+            return -1;
     }
 };
