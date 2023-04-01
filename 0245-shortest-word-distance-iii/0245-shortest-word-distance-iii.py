1cd3ed059abbac3adc76dead3afd6207dcbdcaee
Andrew Kim <ksb0724@utexas.edu>
2023-04-01 18:56:01 +0900

    Time: 720 ms (98.76%), Space: 37.2 MB (15.77%) - LeetHub

--- 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.py
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.py
@@ -1,17 +1,12 @@
 class Solution:
     def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
-        ans = float('inf')
-        indices = []
+        ans, prevIndex = float('inf'), -1
         
         for i in range(len(wordsDict)):
-            if wordsDict[i] == word1:
-                indices.append((i, 0))
-            if wordsDict[i] == word2:
-                indices.append((i, 1))
-        
-        for i in range(len(indices) - 1):
-            if indices[i][1] != indices[i + 1][1] and indices[i][0] != indices[i+1][0]:
-                ans = min(ans, indices[i + 1][0] - indices[i][0])
-        
+            if wordsDict[i] == word1 or wordsDict[i] == word2:
+                if prevIndex != -1 and (wordsDict[prevIndex] != wordsDict[i] or word1 == word2):
+                    ans = min(ans, i - prevIndex)
+                prevIndex = i
+            
         return ans
             
