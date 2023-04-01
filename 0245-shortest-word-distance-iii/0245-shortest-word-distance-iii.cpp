b4851a6a74b61bb38b8d94c4af143bd220a56cae
Andrew Kim <ksb0724@utexas.edu>
2023-04-01 19:06:43 +0900

    Time: 169 ms (93.97%), Space: 97.6 MB (67.24%) - LeetHub

--- 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.cpp
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int shortestWordDistance(vector<string>& wordsDict, string word1, string word2) {
-        int ans = INT_MAX;
-        vector<pair<int, int>> indices;
+        int ans = INT_MAX, prevIndex = -1;
         
         for (int i = 0; i < wordsDict.size(); ++i){
-            if (wordsDict[i] == word1){
-                indices.push_back({i, 0});
+            if (wordsDict[i] == word1 || wordsDict[i] == word2){
+                if (prevIndex != -1 && (wordsDict[prevIndex] != wordsDict[i] || word1 == word2)) ans = min(ans, i - prevIndex);
+                prevIndex = i;
             }
-            
-            if (wordsDict[i] == word2){
-                indices.push_back({i, 1});
-            }
-        }
-        
-        for (int i = 0; i < indices.size() - 1; ++i){
-            if (indices[i].second != indices[i + 1].second && indices[i].first != indices[i + 1].first)
-                ans = min(ans, indices[i + 1].first - indices[i].first);
         }
         
         return ans;
