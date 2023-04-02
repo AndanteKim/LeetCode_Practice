3aa1e914f9ad510e90007c4cb3c92ed9a0fdc5b7
Andrew Kim <ksb0724@utexas.edu>
2023-04-03 01:22:39 +0900

    Time: 520 ms (95.97%), Space: 15.4 MB (43.06%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,26 +1,42 @@
 class Solution:
-    def valid(self, x: int, y: int, grid: List[List[int]], visited: List[List[bool]]):
-        return 0 <= x < self.m and 0 <= y < self.n and not grid[x][y] and not visited[x][y]
+    directions = [
+            (-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)
+        ]
     
+    def best_case_estimate(self, x: int, y: int) -> int:
+        return max(self.max_row - x, self.max_col - y)
+    
+    def get_neighbours(self, x: int, y: int) -> Tuple[int, int]:
+        for dx, dy in self.directions:
+            new_x, new_y = x + dx, y + dy
+            if not (0 <= new_x <= self.max_row and 0 <= new_y <= self.max_col):
+                continue
+            if self.grid[new_x][new_y] == 1:
+                continue
+            yield (new_x, new_y)
+            
+            
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == 1:
+        
+        self.max_row, self.max_col, self.grid = len(grid) - 1, len(grid[0]) - 1, grid
+        if grid[0][0] or grid[self.max_row][self.max_col]:
             return -1
-        self.m, self.n = len(grid), len(grid[0])
-        visited = [[False] * self.n for _ in range(self.m)]
-        queue, directions = deque([(0, 0, 1)]), [(1, 0), (-1, 0), (0, -1),\
-                                                (0, 1), (-1, -1), (1, 1),\
-                                                (-1, 1), (1, -1)]
-        while queue:
-            x, y, path = queue.popleft()
-            if x == self.m - 1 and y == self.n - 1:
-                return path
-            
-            for dx, dy in directions:
-                if self.valid(x + dx, y + dy, grid, visited):
-                    visited[x + dx][y + dy] = True
-                    queue.append((x + dx, y + dy, path + 1))
-            
+        visited = set()
+        priority_queue = [(1 + self.best_case_estimate(0, 0), 1, (0, 0))]
+        
+        while priority_queue:
+            estimate, distance, cell = heapq.heappop(priority_queue)
+            if cell in visited:
+                continue
+            if cell == (self.max_row, self.max_col):
+                return distance
+            visited.add(cell)
+            for neighbor in self.get_neighbours(*cell):
+                if neighbor in visited:
+                    continue
+                estimate = self.best_case_estimate(*neighbor) + distance + 1
+                entry = (estimate, distance + 1, neighbor)
+                heapq.heappush(priority_queue, entry)
         return -1
         
         
-            
