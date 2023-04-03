e75c5a73af4a8ea420d4b21b198235f39ac760e6
Andrew Kim <ksb0724@utexas.edu>
2023-04-04 01:56:17 +0900

    Time: 38 ms (67.97%), Space: 14.1 MB (99.89%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -6,25 +6,39 @@
 #         self.right = None
 
 class Solution:
-    def dfs(self, node: TreeNode, parent: TreeNode) -> None:
+    def dfs(self, node: TreeNode, ans: List[int], target: TreeNode) -> int:
+        if not node:
+            return -1
+        elif node == target:
+            self.subtree_add(node, 0, ans)
+            return 1
+        else:
+            L, R = self.dfs(node.left, ans, target), self.dfs(node.right, ans, target)
+            if L != -1:
+                if L == self.k:
+                    ans.append(node.val)
+                self.subtree_add(node.right, L + 1, ans)
+                return L + 1
+            elif R != -1:
+                if R == self.k:
+                    ans.append(node.val)
+                self.subtree_add(node.left, R + 1, ans)
+                return R + 1
+            else:
+                return -1
+            
+    def subtree_add(self, node: TreeNode, dist: int, ans: List[int]) -> None:
         if not node:
             return
-        
-        node.parent = parent
-        self.dfs(node.left, node)
-        self.dfs(node.right, node)
+        elif dist == self.k:
+            ans.append(node.val)
+        else:
+            self.subtree_add(node.left, dist + 1, ans)
+            self.subtree_add(node.right, dist + 1, ans)
     
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        self.dfs(root, None)
-        queue, seen, distance = deque([target]), {target}, 0
+        ans = []
+        self.k = k
+        self.dfs(root, ans, target)
         
-        while queue and distance < k:
-            curr_length = len(queue)
-            for _ in range(curr_length):
-                node = queue.popleft()
-                for neighbor in [node.left, node.right, node.parent]:
-                    if neighbor and neighbor not in seen:
-                        seen.add(neighbor)
-                        queue.append(neighbor)
-            distance += 1
-        return [node.val for node in queue]
+        return ans
