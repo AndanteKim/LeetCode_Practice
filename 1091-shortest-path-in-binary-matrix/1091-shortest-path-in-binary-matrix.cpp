b857b5c06f289ded6e5c32892c4eabb59736ab95
Andrew Kim <ksb0724@utexas.edu>
2023-04-03 17:56:55 +0900

    Time: 59 ms (94.18%), Space: 21.2 MB (58.72%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,33 +1,80 @@
+namespace{
+    const std::array<std::pair<int, int>, 8> neighbours {{
+        {1, 1}, {0, 1}, {1, 0}, {-1, 1}, {1, -1},\
+        {0, -1}, {-1, 0}, {-1, -1}\
+    }};
+
+    struct square {
+        int g_score{INT_MAX};
+        int f_score{INT_MAX};
+        bool in_open_set{false};
+    };
+
+    using square_iterator = std::vector<square>::iterator;
+    auto lowest_fscore_cmp = [](const square_iterator lhs, const square_iterator rhs){
+        return lhs -> f_score > rhs -> f_score;
+    };
+    
+    using p_queue = std::priority_queue<square_iterator, std::vector<square_iterator>,\
+                                    decltype(lowest_fscore_cmp)>;
+}
+
 class Solution {
-    vector<pair<int, int>> directions{{1, 0}, {-1, 0}, {0, -1}, {0, 1},\
-                                      {1, 1}, {-1, -1}, {1, -1}, {-1, 1}};
-    int m, n;
-    bool valid(int x, int y, vector<vector<int>>& grid, vector<vector<bool>>& visited){
-        return (x >= 0 && x < m) && (y >= 0 && y < n) && !grid[x][y] && !visited[x][y];
-    }
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if (grid[0][0] == 1) return -1;
-        m = grid.size(), n = grid[0].size();
-        queue<vector<int>> *q = new queue<vector<int>>;
-        vector<vector<bool>> visited(m, vector<bool>(n));
-        q -> push({0, 0, 1});
+        int n_rows = grid.size();
+        if (n_rows == 0) return -1;
+        int n_cols = grid[0].size();
+        if (n_cols == 0 || grid[0][0] == 1 || grid[n_rows - 1][n_cols - 1] == 1) return -1;
         
-        while (!q -> empty()){
-            auto it = q -> front();
-            int x = it[0], y = it[1], path = it[2];
-            q -> pop();
-            if (x == m - 1 && y == n - 1) return path;
+        auto h = [n_rows, n_cols] (int y, int x){
+            auto dx = (n_cols - 1) - x;
+            auto dy = (n_rows - 1) - y;
+            return max(dx, dy);
+        };
+        
+        vector<square> squares(n_rows * n_cols);
+        auto getSquare = [&squares, n_cols](int y, int x){
+            return squares.begin() + (y * n_cols + x);
+        };
+        
+        auto getXY = [&squares, n_cols](square_iterator it){
+            auto idx = std::distance(squares.begin(), it);
+            return make_pair<int, int> (idx / n_cols, idx % n_cols);
+        };
+        
+        auto start = getSquare(0, 0);
+        start -> g_score = 1;
+        start -> f_score = h(0, 0);
+        auto dest = getSquare(n_rows - 1, n_cols - 1);
+        p_queue OpenSet(lowest_fscore_cmp);
+        start -> in_open_set = true;
+        OpenSet.push(start);
+        
+        while (!OpenSet.empty()){
+            auto current = OpenSet.top();
+            if (current == dest) return dest -> g_score;
+            OpenSet.pop();
+            if (!current -> in_open_set) continue;
+            current -> in_open_set = false;
             
-            for (auto& [dx, dy] : directions){
-                if (valid(x + dx, y + dy, grid, visited)){
-                    visited[x + dx][y + dy] = true;
-                    q -> push({x + dx, y + dy, path + 1});
+            auto [curr_y, curr_x] = getXY(current);
+            
+            for (const auto &n : neighbours){
+                auto y = curr_y + n.first;
+                auto x = curr_x + n.second;
+                if (y >= 0 && y < n_rows && x >= 0 && x < n_cols && grid[y][x] == 0){
+                    auto neighbour = getSquare(y, x);
+                    auto tentative_g_score = current -> g_score + 1;
+                    if (tentative_g_score < neighbour -> g_score){
+                        neighbour -> g_score = tentative_g_score;
+                        neighbour -> f_score = neighbour -> g_score + h(y, x);
+                        neighbour -> in_open_set = true;
+                        OpenSet.push(neighbour);
+                    }
                 }
             }
         }
-        
         return -1;
-        
     }
 };
