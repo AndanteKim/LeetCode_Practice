dfb8cde69a418b53d7da3c43479a35e76a1e1877
Andrew Kim <ksb0724@utexas.edu>
2023-04-03 16:42:55 +0900

    Time: 69 ms (99.37%), Space: 44.8 MB (11.29%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,14 +1,25 @@
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-        sort(people.begin(), people.end());
-        int ans = 0, i = 0, j = people.size() - 1;
+        vector<int> count(limit + 1, 0);
+        for (int p : people) ++count[p];
         
-        while (i <= j){
+        int idx = 0;
+        for (int val = 1; val <= limit; ++val){
+            while(count[val]-- > 0){
+                people[idx++] = val;
+            }
+        }
+        
+        int ans = 0, left = 0, right = people.size() - 1;
+        
+        while (left <= right){
+            if (people[left] + people[right] <= limit){
+                ++left;
+                --right;
+            }
+            else --right;
             ++ans;
-            if (people[i] + people[j] <= limit)
-                ++i;
-            --j;
         }
         return ans;
     }
