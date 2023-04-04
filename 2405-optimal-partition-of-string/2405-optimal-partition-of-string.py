82409c7468b0c1cfad19db05ce23ddd3399656fa
Andrew Kim <ksb0724@utexas.edu>
2023-04-04 21:54:13 +0900

    Time: 157 ms (33.77%), Space: 14.5 MB (99.47%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,12 +1,11 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        lastSeen = [-1] * 26
-        count, substrStart = 1, 0
+        partitions, chars_bitmask = 1, 0
         
-        for i in range(len(s)):
-            if lastSeen[ord(s[i]) - ord('a')] >= substrStart:
-                count += 1
-                substrStart = i
-            lastSeen[ord(s[i]) - ord('a')] = i
-        
-        return count
+        for c in s:
+            set_bit = 1 << (ord(c) - 97)
+            if chars_bitmask & set_bit:
+                partitions += 1
+                chars_bitmask = 0
+            chars_bitmask |= set_bit
+        return partitions
