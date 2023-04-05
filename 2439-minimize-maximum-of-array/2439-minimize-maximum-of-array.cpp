b365df7262f38d874169bd14d5af3b6ffaaa16c8
Andrew Kim <ksb0724@utexas.edu>
2023-04-05 19:16:55 +0900

    Time: 156 ms (53.18%), Space: 71.4 MB (28.09%) - LeetHub

--- 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.cpp
@@ -1,15 +1,27 @@
 #define ll long long
 
 class Solution {
+    bool isValid(int x, vector<int>& nums){
+        ll total = 0;
+        for (int i = 0; i < nums.size(); ++i){
+            total += nums[i];
+            if (total > (ll)x * (i + 1)) return false;
+        }
+        return true;
+    }
+    
 public:
     int minimizeArrayValue(vector<int>& nums) {
-        int ans = 0;
-        ll prefix_sum = 0;
-        for (int i = 0; i < nums.size(); ++i){
-            prefix_sum += nums[i];
-            int curr = ceil((double)prefix_sum / (i + 1));
-            ans = max(ans, curr);
+        int left = 0, right = *max_element(nums.begin(), nums.end());
+        
+        while (left < right){
+            int mid = left + ((right - left) >> 1);
+            if (isValid(mid, nums)){
+                right = mid;
+            }
+            else left = mid + 1;
         }
-        return ans;
+        
+        return left;
     }
 };
