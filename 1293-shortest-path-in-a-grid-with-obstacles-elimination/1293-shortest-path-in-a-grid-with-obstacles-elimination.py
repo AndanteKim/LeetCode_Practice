5c846f99d7158492d17b9135b1b0987d17b9ae6c
Andrew Kim <ksb0724@utexas.edu>
2023-04-05 23:03:45 +0900

    Time: 798 ms (21.23%), Space: 22 MB (26.61%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,30 +1,28 @@
 class Solution:
-    def valid(self, x: int, y: int, available: int) -> bool:
-        return 0 <= x < self.r and 0 <= y < self.c and 0 <= available <= self.k
+    def valid(self, x: int, y:int) -> bool:
+        return 0 <= x < self.r and 0 <= y < self.c
     
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        
-        self.r, self.c, self.k = len(grid), len(grid[0]), k
-        
-        queue, ans = deque([(0,0,0,k)]), float('inf')
-        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
-        visited = [[[False] * (k + 1) for __ in range(self.c)] for _ in range(self.r)]
-        visited[0][0][k - 1] = True
+        self.r, self.c = len(grid), len(grid[0])
+        queue = deque([(0, 0, k, 0)])
+        seen = {(0, 0, k)}
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
         
         while queue:
-            x, y, dist, available = queue.popleft()
+            y, x, remain, steps = queue.popleft()
+            if y == self.r - 1 and x == self.c - 1:
+                return steps
             
-            if (x == self.r - 1 and y == self.c - 1):
-                ans = min(ans, dist)
             for dx, dy in directions:
-                new_rows, new_cols = x + dx, y + dy
-                if self.valid(new_rows, new_cols, available):
-                    if not grid[new_rows][new_cols]:
-                        if not visited[new_rows][new_cols][available]:
-                            visited[new_rows][new_cols][available] = True
-                            queue.append((new_rows, new_cols, dist + 1, available))
-                    elif available and not visited[new_rows][new_cols][available - 1]:
-                        visited[new_rows][new_cols][available - 1] = True
-                        queue.append((new_rows, new_cols, dist + 1, available - 1))
-                        
-        return ans if ans != float('inf') else -1
+                next_row, next_col = y + dy, x + dx
+                
+                if self.valid(next_row, next_col):
+                    if not grid[next_row][next_col]:
+                        if (next_row, next_col, remain) not in seen:
+                            queue.append((next_row, next_col, remain, steps + 1))
+                            seen.add((next_row, next_col, remain))
+                    elif remain and (next_row, next_col, remain - 1) not in seen:
+                        seen.add((next_row, next_col, remain - 1))
+                        queue.append((next_row, next_col, remain - 1, steps + 1))
+        return -1
+        
