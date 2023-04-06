c960a4e34bb829547ae011a0d7b91f643cdf8f7c
Andrew Kim <ksb0724@utexas.edu>
2023-04-06 20:43:59 +0900

    Time: 169 ms (6.89%), Space: 14.6 MB (24.90%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,30 +1,51 @@
-class Solution:
-    def bfs(self, y: int, x: int, visited: List[List[bool]], grid: List[List[int]]) -> bool:
-        queue = deque([(y, x)])
-        visited[y][x], isClosed = True, True
+class UnionFind:
+    def __init__(self, size: int):
+        self.parents, self.ranks = list(range(size)), [0] * size
         
-        while queue:
-            y, x = queue.popleft()
-            for dy, dx in self.directions:
-                new_row, new_col = y + dy, x + dx
-                if new_row < 0 or new_row >= self.rows or new_col < 0 or new_col >= self.cols:
-                    isClosed = False
-                elif not grid[new_row][new_col] and not visited[new_row][new_col]:
-                    queue.append((new_row, new_col))
-                    visited[new_row][new_col] = True
-        
-        return isClosed
+    def find(self, x : int) -> int:
+        if self.parents[x] != x:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
     
+    def union_set(self, x: int, y: int) -> None:
+        xset, yset = self.find(x), self.find(y)
+        if xset == yset:
+            return
+        
+        if self.ranks[xset] < self.ranks[yset]:
+            self.parents[xset] = yset
+        elif self.ranks[xset] > self.ranks[yset]:
+            self.parents[yset] = xset
+        else:
+            self.parents[yset] = xset
+            self.ranks[xset] += 1
+
+class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        self.rows, self.cols, ans = len(grid), len(grid[0]), 0
-        self.directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
-        visited = [[False] * self.cols for _ in range(self.rows)]
+        m, n, islands = len(grid), len(grid[0]), 0
         
-        for y in range(self.rows):
-            for x in range(self.cols):
-                if not grid[y][x] and not visited[y][x] and self.bfs(y, x, visited, grid):
-                    ans += 1
+        uf = UnionFind(m * n)
+        for y in range(m):
+            for x in range(n):
+                if grid[y][x] == 0:
+                    islands += 1
+                    for dy, dx in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+                        new_row, new_col = y + dy, x + dx
+                        if 0 <= new_row < m and 0 <= new_col < n and grid[new_row][new_col] == 0:
+                            if uf.find(y * n + x) != uf.find(new_row * n + new_col):
+                                islands -= 1
+                                uf.union_set(y * n + x, new_row * n + new_col)
         
-        return ans
+        open_islands = set()
+        for y in range(m):
+            if grid[y][0] == 0:
+                open_islands.add(uf.find(y * n))
+            if grid[y][n-1] == 0:
+                open_islands.add(uf.find(y * n + (n - 1)))
         
-        
+        for x in range(n):
+            if grid[0][x] == 0:
+                open_islands.add(uf.find(x))
+            if grid[m - 1][x] == 0:
+                open_islands.add(uf.find((m - 1) * n + x))
+        return islands - len(open_islands)
