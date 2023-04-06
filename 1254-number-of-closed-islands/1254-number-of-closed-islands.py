e6677b64ee8680ea4c416f7093c31a07e8d184f0
Andrew Kim <ksb0724@utexas.edu>
2023-04-06 19:51:31 +0900

    Time: 133 ms (52.28%), Space: 14.4 MB (80.22%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,30 +1,30 @@
 class Solution:
-    def dfs(self, y: int, x: int, visited: List[List[bool]], grid: List[List[int]]) -> bool:
-        if x < 0 or x >= self.cols or y < 0 or y >= self.rows:
-            return False
+    def bfs(self, y: int, x: int, visited: List[List[bool]], grid: List[List[int]]) -> bool:
+        queue = deque([(y, x)])
+        visited[y][x], isClosed = True, True
         
-        if grid[y][x] or visited[y][x]:
-            return True
+        while queue:
+            y, x = queue.popleft()
+            for dy, dx in self.directions:
+                new_row, new_col = y + dy, x + dx
+                if new_row < 0 or new_row >= self.rows or new_col < 0 or new_col >= self.cols:
+                    isClosed = False
+                elif not grid[new_row][new_col] and not visited[new_row][new_col]:
+                    queue.append((new_row, new_col))
+                    visited[new_row][new_col] = True
         
-        visited[y][x] = True
-        isClosed = True
-        
-        for dy, dx in self.directions:
-            new_rows, new_cols = y + dy, x + dx
-            if not self.dfs(new_rows, new_cols, visited, grid):
-                isClosed = False
         return isClosed
-            
-        
+    
     def closedIsland(self, grid: List[List[int]]) -> int:
         self.rows, self.cols, ans = len(grid), len(grid[0]), 0
-        self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        self.directions = ((1, 0), (-1, 0), (0, 1), (0, -1))
         visited = [[False] * self.cols for _ in range(self.rows)]
         
         for y in range(self.rows):
             for x in range(self.cols):
-                if grid[y][x] == 0 and not visited[y][x] and self.dfs(y, x, visited, grid):
+                if not grid[y][x] and not visited[y][x] and self.bfs(y, x, visited, grid):
                     ans += 1
         
         return ans
-                    
+        
+        
