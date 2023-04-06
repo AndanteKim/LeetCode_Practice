1ceb79ffd096f07720d0b8d771232b402d3e7dc0
Andrew Kim <ksb0724@utexas.edu>
2023-04-07 03:55:51 +0900

    Time: 19 ms (87.70%), Space: 9.3 MB (88.93%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,36 +1,45 @@
+struct pathObj{
+    int row, col, g, remain, f;
+};
+
+class Compare{
+    public:
+        bool operator()(pathObj& lhs, pathObj& rhs){
+            return lhs.f > rhs.f;
+        }
+};
+
 class Solution {
-    int m, n;
-    vector<pair<int, int>> directions{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-    bool valid(int nextRow, int nextCol){
-        return (0 <= nextRow && nextRow < m) && (0 <= nextCol && nextCol < n);
-    }
-    
+    int rows, cols;
 public:
     int shortestPath(vector<vector<int>>& grid, int k) {
-        m = grid.size(), n = grid[0].size();
-        queue<vector<int>> *q = new queue<vector<int>>;
-        vector<vector<int>> seen(m, vector<int>(n, -1));
-        q -> push({0, 0, k, 0});
+        rows = grid.size(), cols = grid[0].size();
+        priority_queue<pathObj, vector<pathObj>, Compare> *pathQ = new priority_queue<pathObj, vector<pathObj>, Compare>;
+        vector<vector<int>> visited(rows, vector<int>(cols, -1));
+        pathObj start {0, 0, 0, k, 0};
+        pathQ -> push(start);
+        visited[0][0] = k;
         
-        while (!q -> empty()){
-            vector<int> curr = q -> front();
-            q -> pop();
-            int row = curr[0], col = curr[1], remain = curr[2], steps = curr[3];
-            if (row == m - 1 && col == n - 1) return steps;
+        while (!pathQ -> empty()){
+            pathObj curr = pathQ -> top();
+            pathQ -> pop();
             
-            if (grid[row][col]){
-                if (!remain) continue;
-                else --remain;
-            }
-        
-            if (seen[row][col] >= remain) continue;
-            seen[row][col] = remain;
-        
-            for (auto& [dr, dc] : directions){
-                int nextRow = row + dr, nextCol = col + dc;
-                if (valid(nextRow, nextCol)) q -> push({nextRow, nextCol, remain, steps + 1});
+            if (curr.row == rows - 1 && curr.col == cols - 1) return curr.g;
+            vector<pair<int, int>> directions{{curr.row + 1, curr.col}, {curr.row, curr.col + 1},\
+                                              {curr.row - 1, curr.col}, {curr.row, curr.col - 1}};
+            for (auto& [new_row, new_col] : directions){
+                if (new_row < 0 || new_row >= rows || new_col < 0 || new_col >= cols) continue;
+                    
+                int f = curr.g + 1 + (rows - new_row) + (cols - new_col);
+                pathObj next {new_row, new_col, curr.g + 1, curr.remain - grid[new_row][new_col], f};
+                
+                if (next.remain >= 0 && visited[next.row][next.col] < next.remain){
+                    pathQ -> push(next);
+                    visited[next.row][next.col] = next.remain;
+                }
             }
         }
+        
         return -1;
     }
 };
