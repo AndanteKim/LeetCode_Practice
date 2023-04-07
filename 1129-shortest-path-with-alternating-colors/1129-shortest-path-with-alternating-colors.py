6e100e207b34146a1de367d068a22ee7b6ebddf9
Andrew Kim <ksb0724@utexas.edu>
2023-04-08 02:08:40 +0900

    Time: 82 ms (93.49%), Space: 14.1 MB (69.50%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.py
@@ -1,28 +1,25 @@
 class Solution:
     def shortestAlternatingPaths(self, n: int, redEdges: List[List[int]], blueEdges: List[List[int]]) -> List[int]:
-        adj = defaultdict(list)
-        for start, dest in redEdges:
-            adj[start].append((dest, 0))
+        ans = [-1] * n
+        adj = [[] for _ in range(n)]
         
-        for start, dest in blueEdges:
-            adj[start].append((dest, 1))
+        for start, end in redEdges:
+            adj[start].append((end, 0))
         
-        ans, q = [-1] * n, deque([])
-        visit = [[False] * 2 for _ in range(n)]
-        q.append((0, 0, -1))
-        ans[0] = 0
+        for start, end in blueEdges:
+            adj[start].append((end, 1))
         
-        while q:
-            node, steps, prevColor = q.popleft()
+        queue = deque([(0, 0, -1)])
+        visited = [[False, False] for _ in range(n)]
+        
+        while queue:
+            node, distance, prevColor = queue.popleft()
+            
+            ans[node] = distance if ans[node] == -1 else min(ans[node], distance)
             
             for neighbor, color in adj[node]:
-                if not visit[neighbor][color] and color != prevColor:
-                    visit[neighbor][color] = True
-                    q.append((neighbor, 1 + steps, color))
-                    if ans[neighbor] == -1:
-                        ans[neighbor] = 1 + steps
-                    
-                
+                if color != prevColor and not visited[neighbor][color]:
+                    visited[neighbor][color] = True
+                    queue.append((neighbor, distance + 1, color))    
+        
         return ans
-            
-            
