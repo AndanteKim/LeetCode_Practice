0bfc49bc8ca3112d0a8c8a6f1168abc0622fc78f
Andrew Kim <ksb0724@utexas.edu>
2023-04-07 19:10:42 +0900

    Time: 676 ms (67.52%), Space: 102 MB (9.03%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,27 +1,33 @@
 class Solution:
-    
-    def dfs(self, y: int, x: int, visited: List[List[bool]], grid: List[List[int]]) -> bool:
-        if y < 0 or y >= self.rows or x < 0 or x >= self.cols:
-            return False
+    def dfs(self, x: int, y: int, rows: int, cols: int, grid: List[List[int]], visited: List[List[bool]]) -> None:
+        if x < 0 or x >= rows or y < 0 or y >= cols or not grid[x][y] or visited[x][y]:
+            return
+        visited[x][y] = True
+        directions = ((1, 0), (0, 1), (-1, 0), (0, -1))
         
-        if visited[y][x] or not grid[y][x]:
-            return True
-        
-        visited[y][x], isClosed = True, True
-        for new_row, new_col in ((y, x + 1), (y, x - 1), (y + 1, x), (y - 1, x)):
-            if not self.dfs(new_row, new_col, visited, grid):
-                isClosed = False
-        self.curr = self.curr + 1 if isClosed else 0
-        return isClosed
+        for new_row, new_col in ((x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)):
+            self.dfs(new_row, new_col, rows, cols, grid, visited)
+        return
     
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        self.rows, self.cols, ans, self.curr = len(grid), len(grid[0]), 0, 0
-        visited = [[False] * self.cols for _ in range(self.rows)]
+        rows, cols = len(grid), len(grid[0])
+        visited = [[False] * cols for _ in range(rows)]
         
-        for y in range(self.rows):
-            for x in range(self.cols):
-                if grid[y][x] == 1 and not visited[y][x] and self.dfs(y, x, visited, grid):
-                    ans += self.curr
-                else:
-                    self.curr = 0
+        for i in range(0, rows):
+            if grid[i][0] == 1 and not visited[i][0]:
+                self.dfs(i, 0, rows, cols, grid, visited)
+            if grid[i][cols - 1] == 1 and not visited[i][cols - 1]:
+                self.dfs(i, cols - 1, rows, cols, grid, visited)
+        
+        for i in range(0, cols):
+            if grid[0][i] == 1 and not visited[0][i]:
+                self.dfs(0, i, rows, cols, grid, visited)
+            if grid[rows - 1][i] == 1 and not visited[rows - 1][i]:
+                self.dfs(rows - 1, i, rows, cols, grid, visited)
+        
+        ans = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] and not visited[i][j]:
+                    ans += 1
         return ans
