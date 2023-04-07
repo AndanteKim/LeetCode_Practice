d28f5a516665030dd96273f1977f43b967d5e40f
Andrew Kim <ksb0724@utexas.edu>
2023-04-08 03:24:22 +0900

    Time: 22 ms (58.10%), Space: 15.2 MB (55.76%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -1,31 +1,27 @@
 class Solution {
 public:
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
-        vector<vector<pair<int, int>>> adj(n);
-        for (auto &redEdge : redEdges) adj[redEdge[0]].push_back({redEdge[1], 0});
-        for (auto &blueEdge : blueEdges) adj[blueEdge[0]].push_back({blueEdge[1], 1});
-        
         vector<int> ans(n, -1);
-        vector<vector<bool>> visit(n, vector<bool>(2));
-        queue<vector<int>> q;
-        q.push({0, 0, -1});
-        visit[0][1] = visit[0][0] = true;
-        ans[0] = 0;
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& redEdge : redEdges) adj[redEdge[0]].push_back({redEdge[1], 0});
+        for (auto& blueEdge : blueEdges) adj[blueEdge[0]].push_back({blueEdge[1], 1});
         
-        while (!q.empty()){
-            auto& element = q.front();
-            int node = element[0], steps = element[1], prevColor = element[2];
-            q.pop();
+        vector<vector<bool>> visited(n, vector<bool>(2, false));
+        queue<vector<int>> *q = new queue<vector<int>>;
+        q -> push({0, 0, -1});
+        
+        while (!q -> empty()){
+            int node = q -> front()[0], distance = q -> front()[1], prevColor = q -> front()[2];
+            q -> pop();
+            ans[node] == -1? ans[node] = distance : ans[node] = min(ans[node], distance);
             
-            for (auto&[neighbor, color] : adj[node]){
-                if (!visit[neighbor][color] && prevColor != color){
-                    visit[neighbor][color] = true;
-                    q.push({neighbor, steps + 1, color});
-                    if (ans[neighbor] == -1) ans[neighbor] = 1 + steps;
+            for (auto& [neighbor, color] : adj[node]){
+                if (color != prevColor && !visited[neighbor][color]){
+                    visited[neighbor][color] = true;
+                    q -> push({neighbor, distance + 1, color});
                 }
             }
         }
-        
         
         return ans;
     }
