aeb951fa66a5163d833cbe2d58dd641f0cd23bd6
Andrew Kim <ksb0724@utexas.edu>
2023-04-08 20:59:54 +0900

    Time: 7 ms (23.94%), Space: 8.8 MB (68.31%) - LeetHub

--- 0760-find-anagram-mappings/0760-find-anagram-mappings.cpp
+++ 0760-find-anagram-mappings/0760-find-anagram-mappings.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     vector<int> anagramMappings(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int> M1, M2;
+        const int bitsToShift = 7;
+        const int numsToGetLastBits = (1 << bitsToShift) - 1;
+        
         for (int i = 0; i < nums1.size(); ++i){
-            M1[nums1[i]] = i;
-            M2[nums2[i]] = i;
+            nums1[i] = (nums1[i] << bitsToShift) + i;
+            nums2[i] = (nums2[i] << bitsToShift) + i;
+        }
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        vector<int> mappings(nums1.size(), 0);
+        
+        for (int i = 0; i < nums1.size(); ++i){
+            mappings[nums1[i] & numsToGetLastBits] = (nums2[i] & numsToGetLastBits);
         }
         
-        vector<int> ans;
-        for (int i = 0; i < nums1.size(); ++i) ans.push_back(M2[nums1[i]]);
-        return ans;
+        return mappings;
     }
 };
