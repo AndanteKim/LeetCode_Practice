65150568c017f1ed284bb6e7cd9810cbc31afdf2
Andrew Kim <ksb0724@utexas.edu>
2023-04-08 16:54:14 +0900

    Time: 41 ms (52.94%), Space: 14.7 MB (17.48%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,12 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        root_node = deepcopy(node)
+        return root_node
