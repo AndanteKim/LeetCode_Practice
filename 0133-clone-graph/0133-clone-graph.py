e35c662b5373a6be542e996b65b1556a2df2e9fc
Andrew Kim <ksb0724@utexas.edu>
2023-04-08 18:09:53 +0900

    Time: 48 ms (10.18%), Space: 14.4 MB (65.43%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -7,6 +7,21 @@ class Node:
 """
 
 class Solution:
+    def __init__(self):
+        self.visited = dict()
+    
     def cloneGraph(self, node: 'Node') -> 'Node':
-        root_node = deepcopy(node)
-        return root_node
+        if not node:
+            return node
+        
+        if node in self.visited:
+            return self.visited[node]
+        
+        clone_node = Node(node.val, [])
+        
+        self.visited[node] = clone_node
+        
+        if node.neighbors:
+            clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors]
+        
+        return clone_node
