bbe7f703baa36701d2aaf7c34912c2831ab1a56c
Andrew Kim <ksb0724@utexas.edu>
2023-04-09 21:00:22 +0900

    Time: 557 ms (92.31%), Space: 210.5 MB (18.37%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,34 +1,31 @@
 class Solution {
+    int dfs(int node, string& colors, vector<vector<int>>& adj, vector<vector<int>>& count,\
+           vector<bool>& visited, vector<bool>& inStack){
+        if (inStack[node]) return INT_MAX;
+        if (visited[node])
+            return count[node][colors[node] - 'a'];
+        
+        visited[node] = true;
+        inStack[node] = true;
+        for (int neighbor : adj[node]){
+            if (dfs(neighbor, colors, adj, count, visited, inStack) == INT_MAX) return INT_MAX;
+            for (int i = 0; i < 26; ++i) count[node][i] = max(count[node][i], count[neighbor][i]);
+        }
+        ++count[node][colors[node] - 'a'];
+        inStack[node] = false;
+        return count[node][colors[node] - 'a'];
+    }
+    
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n = colors.size();
+        int n = colors.size(), ans = 0;
         vector<vector<int>> adj(n);
-        vector<int> indegree(n);
-        for (auto& edge : edges){
-            adj[edge[0]].push_back(edge[1]);
-            ++indegree[edge[1]];
-        }
+        for (auto& edge: edges) adj[edge[0]].push_back(edge[1]);
+        vector<vector<int>> count(n, vector<int>(26));
+        vector<bool> visited(n), inStack(n);
         
-        vector<vector<int>> count(n, vector<int>(26, 0));
-        queue<int> *q = new queue<int>;
-        for (int i = 0; i < n; ++i){
-            if (indegree[i] == 0) q -> push(i);
-        }
-        
-        int answer = 0, nodesSeen = 0;
-        while (!q -> empty()){
-            int node = q -> front();
-            q -> pop();
-            answer = max(answer, ++count[node][colors[node] - 'a']);
-            ++nodesSeen;
-            for (int neighbor : adj[node]){
-                for (int i = 0; i < 26; ++i){
-                    count[neighbor][i] = max(count[neighbor][i], count[node][i]);
-                }
-                --indegree[neighbor];
-                if (indegree[neighbor] == 0) q -> push(neighbor);
-            }
-        }
-        return nodesSeen < n? -1 : answer;
+        for (int i = 0; i < n; ++i)
+            ans = max(ans, dfs(i, colors, adj, count, visited, inStack));
+        return ans == INT_MAX? -1 : ans;
     }
 };
