6b2f59f70f7d8d5451c4ba82404cfe7a85830860
Andrew Kim <ksb0724@utexas.edu>
2023-04-10 23:38:03 +0900

    Time: 902 ms (53.64%), Space: 95.1 MB (48.66%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.cpp
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.cpp
@@ -1,7 +1,6 @@
 class Solution {
     int rows, cols;
-    int bfs(vector<vector<int>>& grid, int row, int col, int totalHouses){
-        int distSum = 0, housesReached = 0;
+    void bfs(vector<vector<int>>& grid, vector<vector<vector<int>>>& distances, int row, int col){
         queue<pair<int, int>> *q = new queue<pair<int, int>>;
         q -> push({row, col});
         
@@ -9,22 +8,21 @@ class Solution {
         visited[row][col] = true;
         int steps = 0;
         
-        while (!q -> empty() && housesReached != totalHouses){
+        while (!q -> empty()){
             for (int i = q -> size(); i > 0; --i){
                 auto curr = q -> front();
                 q -> pop();
                 row = curr.first;
                 col = curr.second;
-                if (grid[row][col] == 1){
-                    distSum += steps;
-                    ++housesReached;
-                    continue;
+                if (grid[row][col] == 0){
+                    distances[row][col][0] += steps;
+                    ++distances[row][col][1];
                 }
                 
                 for (auto& [NextRow, NextCol] : {make_pair(row - 1, col), make_pair(row + 1, col), make_pair(row, col - 1)\
                     , make_pair(row, col + 1)}){
                     if (NextRow >= 0 && NextCol >= 0 && NextRow < rows && NextCol < cols){
-                        if (!visited[NextRow][NextCol] && grid[NextRow][NextCol] != 2){
+                        if (!visited[NextRow][NextCol] && grid[NextRow][NextCol] == 0){
                             visited[NextRow][NextCol] = true;
                             q -> push({NextRow, NextCol});
                         }
@@ -34,17 +32,6 @@ class Solution {
             ++steps;
         }
         
-        if (housesReached != totalHouses){
-            for (row = 0; row < rows; ++row){
-                for (col = 0; col < cols; ++col){
-                    if (grid[row][col] == 0 && visited[row][col])
-                        grid[row][col] = 2;
-                }
-            }
-            return INT_MAX;
-        }
-        
-        return distSum;
     }
     
 public:
@@ -52,15 +39,20 @@ public:
         rows = grid.size(), cols = grid[0].size();
         int minDist = INT_MAX, totalHouses = 0;
         
+        vector<vector<vector<int>>> distances (rows, vector<vector<int>> (cols, {0, 0}));
+        
         for (int row = 0; row < rows; ++row){
             for (int col = 0; col < cols; ++col){
-                if (grid[row][col] == 1) ++totalHouses;
+                if (grid[row][col] == 1) {
+                    ++totalHouses;
+                    bfs(grid, distances, row, col);
+                }
             }
         }
         
         for (int row = 0; row < rows; ++row){
             for (int col = 0; col < cols; ++col){
-                if (grid[row][col] == 0) minDist = min(minDist, bfs(grid, row, col, totalHouses));
+                if (distances[row][col][1] == totalHouses) minDist = min(minDist, distances[row][col][0]);
             }
         }
         
