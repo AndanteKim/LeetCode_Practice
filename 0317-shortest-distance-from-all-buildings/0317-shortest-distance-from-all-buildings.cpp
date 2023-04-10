daa6d5c3848779236e82485aa5b739c1e281874a
Andrew Kim <ksb0724@utexas.edu>
2023-04-11 00:37:13 +0900

    Time: 370 ms (81.23%), Space: 45 MB (75.86%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.cpp
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.cpp
@@ -1,62 +1,39 @@
 class Solution {
-    int rows, cols;
-    void bfs(vector<vector<int>>& grid, vector<vector<vector<int>>>& distances, int row, int col){
-        queue<pair<int, int>> *q = new queue<pair<int, int>>;
-        q -> push({row, col});
-        
-        vector<vector<bool>> visited(rows, vector<bool>(cols));
-        visited[row][col] = true;
-        int steps = 0;
-        
-        while (!q -> empty()){
-            for (int i = q -> size(); i > 0; --i){
-                auto curr = q -> front();
-                q -> pop();
-                row = curr.first;
-                col = curr.second;
-                if (grid[row][col] == 0){
-                    distances[row][col][0] += steps;
-                    ++distances[row][col][1];
-                }
-                
-                for (auto& [NextRow, NextCol] : {make_pair(row - 1, col), make_pair(row + 1, col), make_pair(row, col - 1)\
-                    , make_pair(row, col + 1)}){
-                    if (NextRow >= 0 && NextCol >= 0 && NextRow < rows && NextCol < cols){
-                        if (!visited[NextRow][NextCol] && grid[NextRow][NextCol] == 0){
-                            visited[NextRow][NextCol] = true;
-                            q -> push({NextRow, NextCol});
-                        }
-                    }
-                }
-            }
-            ++steps;
-        }
-        
-    }
-    
 public:
     int shortestDistance(vector<vector<int>>& grid) {
-        rows = grid.size(), cols = grid[0].size();
-        int minDist = INT_MAX, totalHouses = 0;
-        
-        vector<vector<vector<int>>> distances (rows, vector<vector<int>> (cols, {0, 0}));
+        int rows = grid.size(), cols = grid[0].size();
+        vector<vector<int>> total(rows, vector<int>(cols, 0));
+        int emptyLandValue = 0, minDist = INT_MAX;
         
         for (int row = 0; row < rows; ++row){
             for (int col = 0; col < cols; ++col){
-                if (grid[row][col] == 1) {
-                    ++totalHouses;
-                    bfs(grid, distances, row, col);
+                if (grid[row][col] == 1){
+                    minDist = INT_MAX;
+                    queue<pair<int, int>> *q = new queue<pair<int, int>>;
+                    int steps = 0;
+                    q -> push({row, col});
+                    
+                    while (!q -> empty()){
+                        ++steps;
+                        for (int level = q -> size(); level > 0; --level){
+                            int x = q -> front().first, y = q -> front().second;
+                            q -> pop();
+                            for (auto& [NextRow, NextCol] : {make_pair(x - 1, y), make_pair(x + 1, y), make_pair(x, y - 1),\
+                                                           make_pair(x, y + 1)}){
+                                if (NextRow >= 0 && NextRow < rows && NextCol >= 0 && NextCol < cols && \
+                                   grid[NextRow][NextCol] == emptyLandValue){
+                                    --grid[NextRow][NextCol];
+                                    total[NextRow][NextCol] += steps;
+                                    q -> push({NextRow, NextCol});
+                                    minDist = min(minDist, total[NextRow][NextCol]);
+                                }
+                            }
+                        }
+                    }
+                    --emptyLandValue;
                 }
             }
         }
-        
-        for (int row = 0; row < rows; ++row){
-            for (int col = 0; col < cols; ++col){
-                if (distances[row][col][1] == totalHouses) minDist = min(minDist, distances[row][col][0]);
-            }
-        }
-        
-        if (minDist == INT_MAX) return -1;
-        return minDist;
+        return minDist == INT_MAX? -1 : minDist;
     }
 };
