4d6df32d017c24c900f2953d62f21ca2c002f61b
Andrew Kim <ksb0724@utexas.edu>
2023-04-15 01:08:58 +0900

    Time: 1322 ms (74.17%), Space: 30.6 MB (71.90%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,18 +1,13 @@
 class Solution:
-    def lps(self, left: int, right: int, s: str) -> int:
-        if (left, right) in self.memo:
-            return self.memo[(left, right)]
-        if left > right:
-            return 0
-        if left == right:
-            return 1
-        
-        if s[left] == s[right]:
-            self.memo[(left, right)] = self.lps(left + 1, right - 1, s) + 2
-        else:
-            self.memo[(left, right)] = max(self.lps(left, right - 1, s), self.lps(left + 1, right, s))
-        return self.memo[(left, right)]
-    
     def longestPalindromeSubseq(self, s: str) -> int:
-        self.memo = dict()
-        return self.lps(0, len(s) - 1, s)
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+        
+        for i in range(n - 1, -1, -1):
+            dp[i][i] = 1
+            for j in range(i + 1, n):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][n - 1]
