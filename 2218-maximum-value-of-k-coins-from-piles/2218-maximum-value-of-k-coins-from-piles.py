2e5e2b99e6c292ae01348a807a5b1cca79f58de8
Andrew Kim <ksb0724@utexas.edu>
2023-04-15 21:37:10 +0900

    Time: 5644 ms (27.68%), Space: 43.8 MB (76.10%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -1,12 +1,18 @@
 class Solution:
+    def f(self, i: int, coins: int, dp: List[List[int]], piles) -> int:
+        if i == 0:
+            return 0
+        if dp[i][coins] != -1:
+            return dp[i][coins]
+        current_sum = 0
+        for current_coins in range(0, min(len(piles[i - 1]), coins) + 1):
+            if current_coins > 0:
+                current_sum += piles[i - 1][current_coins - 1]
+            dp[i][coins] = max(dp[i][coins], self.f(i - 1, coins - current_coins, dp, piles) + current_sum)
+        return dp[i][coins]
+    
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
         n = len(piles)
-        dp = [[0] * (k + 1) for i in range(n + 1)]
-        for i in range(1, n + 1):
-            for coins in range(0, k + 1):
-                current_sum = 0
-                for current_coins in range(0, min(len(piles[i - 1]), coins) + 1):
-                    if current_coins > 0:
-                        current_sum += piles[i - 1][current_coins - 1]
-                    dp[i][coins] = max(dp[i][coins], dp[i - 1][coins - current_coins] + current_sum)
-        return dp[n][k]
+        dp = [[-1] * (k + 1) for i in range(n + 1)]
+        
+        return self.f(n, k, dp, piles)
