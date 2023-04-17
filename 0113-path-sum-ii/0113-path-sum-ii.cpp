5fb695f525a741113e0475079f8b7e36f2f4aaa4
Andrew Kim <ksb0724@utexas.edu>
2023-04-17 18:26:33 +0900

    Time: 21 ms (28.00%), Space: 20 MB (59.98%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -10,35 +10,25 @@
  * };
  */
 class Solution {
+    void path(TreeNode* root, int target, vector<int> &&pth, vector<vector<int>>& ans){
+        if (!root) return;
+        
+        if (root -> val == target && !root -> left && !root -> right){
+            pth.push_back(root -> val);
+            ans.push_back(pth);
+            pth.pop_back();
+            return;
+        }
+        pth.push_back(root -> val);
+        path(root -> left, target - root -> val, move(pth), ans);
+        path(root -> right, target - root -> val, move(pth), ans);
+        pth.pop_back();
+    }
+    
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        if (!root) return {};
-        
-        stack<pair<TreeNode*, pair<vector<int>, int>>> stack;
-        stack.push({root, {{root -> val}, root -> val}});
         vector<vector<int>> ans;
-        
-        while (!stack.empty()){
-            auto [node, values] = stack.top();
-            
-            auto[paths, total] = values;
-            stack.pop();
-            
-            if ((!node -> left && !node -> right) && total == targetSum) ans.push_back(paths);
-            
-            if (node -> left){
-                paths.push_back(node -> left -> val);
-                stack.push({node -> left, {paths, total + node -> left -> val}});
-                paths.pop_back();
-            }
-            
-            if (node -> right){
-                paths.push_back(node -> right -> val);
-                stack.push({node -> right, {paths, total + node -> right -> val}});
-            }
-            
-        }
-        
+        path(root, targetSum, {}, ans);
         return ans;
     }
 };
