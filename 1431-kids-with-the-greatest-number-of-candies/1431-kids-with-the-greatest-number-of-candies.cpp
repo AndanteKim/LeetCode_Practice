b13ff978aa9ada8b24ebe405952ec61a6c1136ef
Andrew Kim <ksb0724@utexas.edu>
2023-04-17 09:14:21 +0900

    Time: 2 ms (71.33%), Space: 9 MB (64.81%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int greatest = *max_element(candies.begin(), candies.end());
+        vector<bool> ans;
+        for (int i = 0; i < candies.size(); ++i){
+            if (candies[i] + extraCandies >= greatest) ans.push_back(true);
+            else ans.push_back(false);
+        }
+        return ans;
+    }
+};
