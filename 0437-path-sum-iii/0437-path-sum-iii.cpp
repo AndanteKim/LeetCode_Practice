9792e53671887d88036fe598f4f2521bb4b42480
Andrew Kim <ksb0724@utexas.edu>
2023-04-19 00:09:10 +0900

    Time: 18 ms (86.47%), Space: 21 MB (26.82%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.cpp
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -12,33 +12,25 @@
 #define ll long long
 
 class Solution {
+    int count = 0, k;
+    unordered_map<ll, int> h;
+    void preOrder(TreeNode* node, ll currSum){
+        if (!node) return;
+        
+        currSum += node -> val;
+        if (currSum == k) ++count;
+        count += h[currSum - k];
+        ++h[currSum];
+        preOrder(node -> left, currSum);
+        preOrder(node -> right, currSum);
+        --h[currSum];
+    }
+    
 public:
     int pathSum(TreeNode* root, int targetSum) {
-        if (!root) return 0;
-        int ans = 0;
-        unordered_map<ll, int> h;
-        stack<pair<TreeNode*, pair<ll, bool>>> stack;
-        stack.push({root, {0, false}});
-        while(!stack.empty()){
-            auto [node, others] = stack.top();
-            auto [curr_sum, visited] = others;
-            stack.pop();
-            
-            if (visited){
-                --h[curr_sum];
-                continue;
-            }
-            
-            curr_sum += node -> val;
-            if (curr_sum == targetSum) ++ans;
-            ans += h[curr_sum - targetSum];
-            ++h[curr_sum];
-            stack.push({node, {curr_sum, true}});
-            
-            if (node -> right) stack.push({node -> right, {curr_sum, false}});
-            if (node -> left) stack.push({node -> left, {curr_sum, false}});
-        }
+        k = targetSum;
+        preOrder(root, 0);
         
-        return ans;
+        return count;
     }
 };
