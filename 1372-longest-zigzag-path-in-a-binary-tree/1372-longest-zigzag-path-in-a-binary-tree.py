671c06e19b51d2cc445250144ffc4f839b6ffda8
Andrew Kim <ksb0724@utexas.edu>
2023-04-19 10:42:01 +0900

    Time: 361 ms (91.58%), Space: 27 MB (94.87%) - LeetHub

--- 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
+++ 1372-longest-zigzag-path-in-a-binary-tree/1372-longest-zigzag-path-in-a-binary-tree.py
@@ -5,21 +5,24 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, node: Optional[TreeNode], goLeft: bool, steps: int) -> None:
-        if node:
-            self.ans = max(self.ans, steps)
-            
-            if goLeft:
-                self.dfs(node.left, False, steps + 1)
-                self.dfs(node.right, True, 1)
-            else:
-                self.dfs(node.left, False, 1)
-                self.dfs(node.right, True, steps + 1)
-    
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
-        self.ans = 0
+        ans, stack = 0, [(root, 0, None)]
         
-        self.dfs(root, False, 0)
-        self.dfs(root, True, 0)
+        while stack:
+            node, zigzag, direction = stack.pop()
+            
+            ans = max(ans, zigzag)
+            
+            if node.left:
+                if direction == 'l':
+                    stack.append((node.left, 1, 'l'))
+                else:
+                    stack.append((node.left, zigzag + 1, 'l'))
+                
+            if node.right:
+                if direction == 'r':
+                    stack.append((node.right, 1, 'r'))
+                else:
+                    stack.append((node.right, zigzag + 1, 'r'))
         
-        return self.ans
+        return ans
