b97bb321a4bafae9910e120a5446ca0bf2e9953d
Andrew Kim <ksb0724@utexas.edu>
2023-04-19 18:25:41 +0900

    Time: 328 ms (27.17%), Space: 18.2 MB (99.84%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.py
@@ -5,26 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def dfs(self, node: Optional[TreeNode], level: int, sum_at_current_level: List) -> None:
+        if not node:
+            return
+        
+        if len(sum_at_current_level) == level:
+            sum_at_current_level.append(node.val)
+        else:
+            sum_at_current_level[level] += node.val
+            
+        self.dfs(node.left, level + 1, sum_at_current_level)
+        self.dfs(node.right, level + 1, sum_at_current_level)
+    
     def maxLevelSum(self, root: Optional[TreeNode]) -> int:
-        ans, max_sum = -1, float('-inf')
-        queue = deque([(root, 1)])
-        
-        while queue:
-            
-            total, curr_level = 0, -1
-            for _ in range(len(queue)):
-                node, level = queue.popleft()
-                total += node.val
-                curr_level = level
-                
-                if node.left:
-                    queue.append((node.left, level + 1))
-                    
-                if node.right:
-                    queue.append((node.right, level + 1))
-            if max_sum < total:
-                max_sum = total
-                ans = curr_level
-            
-        
-        return ans
+        sum_at_current_level = []
+        self.dfs(root, 0, sum_at_current_level)
+        return 1 + sum_at_current_level.index(max(sum_at_current_level))
