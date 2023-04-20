7703871e658980f2add4e1e8bed4bb5a54ac1b41
Andrew Kim <ksb0724@utexas.edu>
2023-04-21 01:30:27 +0900

    Time: 40 ms (75.16%), Space: 34.9 MB (34.82%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.cpp
@@ -12,20 +12,7 @@
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if (!root) return nullptr;
-        
-        stack<TreeNode*> stack;
-        stack.push(root);
-        
-        while (!stack.empty()){
-            TreeNode* node = stack.top();
-            stack.pop();
-            
-            if (node -> val == val) return node;
-        
-            if (node -> right) stack.push(node -> right);
-            if (node -> left) stack.push(node -> left);
-        }
-        return nullptr;
+        if (!root || val == root -> val) return root;
+        return val < root -> val? searchBST(root -> left, val) : searchBST(root -> right, val);
     }
 };
