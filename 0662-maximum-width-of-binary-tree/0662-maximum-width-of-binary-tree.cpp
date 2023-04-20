e76b7ce910b1a4feba8ff53192c0f5b34b7d3f21
Andrew Kim <ksb0724@utexas.edu>
2023-04-21 00:07:52 +0900

    Time: 11 ms (52.46%), Space: 18.5 MB (5.25%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -12,27 +12,21 @@
 #define ull unsigned long long
 
 class Solution {
+    ull ans = 0;
+    unordered_map<ull, ull> firstColIndexTable;
+    
+    void dfs(TreeNode* node, ull depth, ull colIndex){
+        if (!node) return;
+        if (firstColIndexTable.find(depth) == firstColIndexTable.end())
+            firstColIndexTable[depth] = colIndex;
+        ans = max(ans, colIndex - firstColIndexTable[depth] + 1);
+        dfs(node -> left, depth + 1, 2 * colIndex);
+        dfs(node -> right, depth + 1, 2 * colIndex + 1);
+    }
+    
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        ull ans = 0;
-        queue<pair<TreeNode*, ull>> queue;
-        queue.push({root, 0});
-        
-        while (!queue.empty()){
-            int sz = queue.size();
-            auto [_, levelHeadIndex] = queue.front();
-            ull levelLength;
-            
-            for (int i = 0; i < sz; ++i){
-                auto [node, colIndex] = queue.front();
-                queue.pop();
-                levelLength = colIndex;
-                
-                if (node -> left) queue.push({node -> left, 2 * colIndex});
-                if (node -> right) queue.push({node -> right, 2 * colIndex + 1});
-            }
-            ans = max(ans, levelLength - levelHeadIndex + 1);
-        }
+        dfs(root, 0, 0);
         return ans;
     }
 };
