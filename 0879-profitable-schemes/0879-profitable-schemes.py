73beb514d1945d9f1af7137b3f7b9000b3bbc65d
Andrew Kim <ksb0724@utexas.edu>
2023-04-21 16:45:49 +0900

    Time: 3980 ms (24.76%), Space: 43.6 MB (47.62%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -1,14 +1,19 @@
 class Solution:
-    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-        @lru_cache(None)
-        def backtrack(i: int, currProfit: int, people: int) -> None:
-            if i >= len(group):
-                return currProfit >= minProfit
-            ways = 0
-            if people + group[i] <= n:
-                ways += backtrack(i + 1, min(minProfit, currProfit + profit[i]), people + group[i])
-            ways += backtrack(i + 1, currProfit, people)
-            
-            return ways
+    def find(self, pos: int, count: int, profit: int, n: int, minProfit: int, group: List[int], profits: List[int]) -> int:
+        if pos == len(group):
+            return profit >= minProfit
         
-        return backtrack(0, 0, 0) % (10 ** 9 + 7)
+        if self.memo[pos][count][profit] != -1:
+            return self.memo[pos][count][profit]
+        
+        totalWays = self.find(pos + 1, count, profit, n, minProfit, group, profits)
+        if count + group[pos] <= n:
+            totalWays += self.find(pos + 1, count + group[pos], min(minProfit, profit + profits[pos]), n, minProfit, group, profits)
+        self.memo[pos][count][profit] = totalWays % self.mod
+        return self.memo[pos][count][profit]
+    
+    def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
+        self.mod = int(1e9 + 7)
+        self.memo = [[[-1] * 101 for _ in range(101)] for __ in range(101)]
+        
+        return self.find(0, 0, 0, n, minProfit, group, profit)
