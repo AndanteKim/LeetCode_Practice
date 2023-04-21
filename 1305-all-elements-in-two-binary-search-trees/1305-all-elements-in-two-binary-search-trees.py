b2be1fa01543580011f7bdcc4c6884843d1a46e7
Andrew Kim <ksb0724@utexas.edu>
2023-04-21 23:46:18 +0900

    Time: 368 ms (36.12%), Space: 17.9 MB (93.02%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -6,7 +6,26 @@
 #         self.right = right
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
-        def inorder(r: TreeNode) -> List[int]:
-            return inorder(r.left) + [r.val] + inorder(r.right) if r else []
+        stack1, stack2, ans = [], [], []
         
-        return sorted(inorder(root1) + inorder(root2))
+        while root1 or root2 or stack1 or stack2:
+            while root1:
+                stack1.append(root1)
+                root1 = root1.left
+            
+            while root2:
+                stack2.append(root2)
+                root2 = root2.left
+
+            if not stack2 or stack1 and stack1[-1].val <= stack2[-1].val:
+                root1 = stack1.pop()
+                ans.append(root1.val)
+                root1 = root1.right
+            
+            else:
+                root2 = stack2.pop()
+                ans.append(root2.val)
+                root2 = root2.right
+                
+        return ans
+                
