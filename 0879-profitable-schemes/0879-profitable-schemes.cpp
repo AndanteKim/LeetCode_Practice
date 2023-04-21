b5030e422e4d91e9b4cd66216c93576f8184acb9
Andrew Kim <ksb0724@utexas.edu>
2023-04-21 22:40:13 +0900

    Time: 147 ms (76.89%), Space: 12.7 MB (75.00%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,20 +1,25 @@
 class Solution {
-    int memo[101][101][101];
+    int dp[101][101][101];
     int mod = 1e9 + 7;
-    int find(int pos, int count, int profit, int n, int minProfit, vector<int>& group, vector<int>& profits){
-        if (pos == group.size()) return profit >= minProfit;
-        
-        if (memo[pos][count][profit] != -1) return memo[pos][count][profit];
-        
-        int totalWays = find(pos + 1, count, profit, n, minProfit, group, profits);
-        if (count + group[pos] <= n)
-            totalWays += find(pos + 1, count + group[pos], min(minProfit, profit + profits[pos]), n, minProfit, group, profits);
-        return memo[pos][count][profit] = totalWays % mod;
-    }
     
 public:
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        memset(memo, -1, sizeof(memo));
-        return find(0, 0, 0, n, minProfit, group, profit);
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profits) {
+        memset(dp, 0, sizeof(dp));
+        
+        for (int count = 0; count <= n; ++count) dp[group.size()][count][minProfit] = 1;
+        
+        for (int index = group.size() - 1; index >= 0; --index){
+            for (int count = 0; count <= n; ++count){
+                for (int profit = 0; profit <= minProfit; ++profit){
+                    dp[index][count][profit] = dp[index + 1][count][profit];
+                    if (group[index] + count <= n){
+                        dp[index][count][profit] = (dp[index][count][profit] + dp[index + 1][count + group[index]]\
+                                                    [min(minProfit, profit + profits[index])]) % mod;
+                    }
+                }
+            }
+        }
+        
+        return dp[0][0][0];
     }
 };
