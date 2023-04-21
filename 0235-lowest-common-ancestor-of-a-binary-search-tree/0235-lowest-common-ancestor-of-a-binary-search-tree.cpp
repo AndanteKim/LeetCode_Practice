78f2fdbf3c0ee8c2472f76e958105031632422b1
Andrew Kim <ksb0724@utexas.edu>
2023-04-22 01:47:18 +0900

    Time: 24 ms (95.55%), Space: 23.4 MB (27.32%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,32 +11,11 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        unordered_map<TreeNode*, TreeNode*> ancestors;
-        
-        queue<pair<TreeNode*, TreeNode*>> queue;
-        queue.push({root, nullptr});
-        
-        while (!queue.empty()){
-            auto [child, parent] = queue.front();
-            queue.pop();
-            
-            ancestors[child] = parent;
-            
-            if (child -> left)
-                queue.push({child -> left, child});
-            if (child -> right)
-                queue.push({child -> right, child});
-        }
-        
-        unordered_set<TreeNode*> seen;
-        while (p){
-            seen.insert(p);
-            p = ancestors[p];
-        }
-        
-        while (seen.find(q) == seen.end()){
-            q = ancestors[q];
-        }
-        return q;
+        if (p -> val < root -> val && q -> val < root -> val)
+            return lowestCommonAncestor(root -> left, p, q);
+        else if (p -> val > root -> val && q -> val > root -> val)
+            return lowestCommonAncestor(root -> right, p, q);
+        else
+            return root;
     }
 };
