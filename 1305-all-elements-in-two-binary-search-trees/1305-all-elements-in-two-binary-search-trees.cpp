7b3888f32e0f9a85493cbd442783cdfe4f019583
Andrew Kim <ksb0724@utexas.edu>
2023-04-21 23:10:01 +0900

    Time: 173 ms (26.97%), Space: 85.7 MB (35.26%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -10,21 +10,20 @@
  * };
  */
 class Solution {
+    void inorder(TreeNode* node, vector<int>& arr){
+        if (!node) return;
+        inorder(node -> left, arr);
+        arr.push_back(node -> val);
+        inorder(node -> right, arr);
+    }
+    
 public:
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> ans;
-        queue<TreeNode*> queue;
-        queue.push(root1);
-        queue.push(root2);
-        
-        while (!queue.empty()){
-            TreeNode* node = queue.front();
-            queue.pop();
-            if (!node) continue;
-            ans.push_back(node -> val);
-            if (node -> left) queue.push(node -> left);
-            if (node -> right) queue.push(node -> right);
-        }
+        vector<int> r1, r2;
+        inorder(root1, r1);
+        inorder(root2, r2);
+        vector<int> ans = r1;
+        ans.insert(ans.end(), r2.begin(), r2.end());
         sort(ans.begin(), ans.end());
         return ans;
     }
