85856fd30f346a0000e629d31db33ed88a373381
Andrew Kim <ksb0724@utexas.edu>
2023-04-22 18:36:12 +0900

    Time: 80 ms (33.53%), Space: 18.5 MB (15.74%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.py
@@ -5,33 +5,18 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def successor(self, root: Optional[TreeNode]) -> int:
-        root = root.right
-        while root.left:
-            root = root.left
-        return root.val
-    
-    def predecessor(self, root: Optional[TreeNode]) -> int:
-        root = root.left
-        while root.right:
-            root = root.right
-        return root.val
-    
     def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
-        if not root:
-            return None
-        
-        if key > root.val:
-            root.right = self.deleteNode(root.right, key)
-        elif key < root.val:
-            root.left = self.deleteNode(root.left, key)
-        else:
-            if not (root.left or root.right):
-                root = None
-            elif root.right:
-                root.val = self.successor(root)
-                root.right = self.deleteNode(root.right, root.val)
+        if root:
+            if key < root.val:
+                root.left = self.deleteNode(root.left, key)
+            elif key > root.val:
+                root.right = self.deleteNode(root.right, key)
             else:
-                root.val = self.predecessor(root)
+                if not root.left:
+                    return root.right
+                child = root.left
+                while child and child.right:
+                    child = child.right
+                root.val = child.val
                 root.left = self.deleteNode(root.left, root.val)
         return root
