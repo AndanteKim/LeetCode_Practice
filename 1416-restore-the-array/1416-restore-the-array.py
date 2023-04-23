79c865f56893efca41adfb881bedd496c3be1f05
Andrew Kim <ksb0724@utexas.edu>
2023-04-23 17:23:14 +0900

    Time: 1945 ms (31.51%), Space: 15 MB (95.89%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.py
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -2,17 +2,18 @@ class Solution:
     def numberOfArrays(self, s: str, k: int) -> int:
         m, n = len(s), len(str(k))
         mod = 10 ** 9 + 7
-        dp = [1] + [0] * m
+        dp = [1] + [0] * n
         
         for start in range(m):
             if s[start] == '0':
+                dp[start % (n + 1)] = 0
                 continue
-            count = 0
             
             for end in range(start, m):
                 curr_number = s[start: end + 1]
                 if int(curr_number) > k:
                     break
-                dp[end + 1] += dp[start]
-                dp[end + 1] %= mod
-        return dp[-1]
+                dp[(end + 1) % (n + 1)] += dp[start % (n + 1)]
+                dp[(end + 1) % (n + 1)] %= mod
+            dp[start % (n + 1)] = 0
+        return dp[m % (n + 1)]
