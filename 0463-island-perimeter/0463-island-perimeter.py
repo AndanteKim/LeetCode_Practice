f1c28e421ad8896c754d752ee411fd7570648c7a
Andrew Kim <ksb0724@utexas.edu>
2023-04-23 22:58:40 +0900

    Time: 447 ms (96.08%), Space: 14.1 MB (99.21%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,19 +1,15 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        queue = deque()
-        directions = ((-1, 0), (1, 0), (0, -1), (0, 1))
-        m, n = len(grid), len(grid[0])
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    queue.append((i, j))
-        ans = 4 * len(queue)
-        while queue:
-            x, y = queue.popleft()
-            
-            for dx, dy in directions:
-                if 0 <= x + dx < len(grid) and 0 <= y + dy < len(grid[0]) and grid[x + dx][y + dy] == 1:
-                    ans -= 1
-                
-        return ans
+        rows, cols = len(grid), len(grid[0])
         
+        ans = 0
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    ans += 4
+                    
+                    if r > 0 and grid[r - 1][c] == 1:
+                        ans -= 2
+                    if c > 0 and grid[r][c - 1] == 1:
+                        ans -= 2
+        return ans
