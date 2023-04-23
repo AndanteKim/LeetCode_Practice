51357a1b1a912b32c329bf14102aa99c29388219
Andrew Kim <ksb0724@utexas.edu>
2023-04-23 17:14:35 +0900

    Time: 313 ms (12.21%), Space: 11.8 MB (86.05%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.cpp
+++ 1416-restore-the-array/1416-restore-the-array.cpp
@@ -2,26 +2,21 @@ class Solution {
     int m, n;
     int mod = pow(10, 9) + 7;
     
-    int dfs(int start, string& s, int k, vector<int>& dp){
-        if (dp[start] != 0)
-            return dp[start];
-        if (start == m) return 1;
-        if (s[start] == '0') return 0;
-        int count = 0;
-        for (int end = start; end < m; ++end){
-            string currNumber = s.substr(start, end - start + 1);
-            if (stol(currNumber) > k) break;
-            count = (count + dfs(end + 1, s, k, dp)) % mod;
-        }
-        dp[start] = count % mod;
-        return count;
-    }
-    
 public:
     int numberOfArrays(string s, int k) {
-        m = s.size();
-        n = to_string(k).size();
+        int m = s.size(), n = to_string(k).size();
         vector<int> dp(m + 1, 0);
-        return dfs(0, s, k, dp) % mod;
+        dp[0] = 1;
+        for (int start = 0; start < m; ++start){
+            if (s[start] == '0') continue;
+            int count = 0;
+            for (int end = start; end < m; ++end){
+                string currNumber = s.substr(start, end - start + 1);
+                if (stol(currNumber) > k) break;
+                dp[end + 1] = (dp[end + 1] + dp[start]) % mod;
+            }
+        }
+        
+        return dp.back();
     }
 };
