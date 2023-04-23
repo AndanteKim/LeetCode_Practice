4e5e1f52fd64b1a433c9f4dfdaba5dce9c8b180d
Andrew Kim <ksb0724@utexas.edu>
2023-04-23 23:08:38 +0900

    Time: 111 ms (81.07%), Space: 96.1 MB (98.92%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,30 +1,16 @@
 class Solution {
-    int m, n;
-    bool isNext(int x, int y, vector<vector<int>>& grid){
-        if ((x >= 0 && x < m) && (y >= 0 && y < n) && grid[x][y] == 1) return true;
-        return false;
-    }
-    
 public:
     int islandPerimeter(vector<vector<int>>& grid) {
-        queue<pair<int, int>> queue;
-        vector<pair<int, int>> directions{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        m = grid.size(), n = grid[0].size();
+        int rows = grid.size(), cols = grid[0].size(), ans = 0;
         
-        for (int i = 0; i < grid.size(); ++i){
-            for (int j = 0; j < grid[0].size(); ++j){
-                if (grid[i][j] == 1) queue.push({i, j});
+        for (int r = 0; r < rows; ++r){
+            for (int c = 0; c < cols; ++c){
+                if (grid[r][c] == 1){
+                    ans += 4;
+                    if (r > 0 && grid[r - 1][c] == 1) ans -= 2;
+                    if (c > 0 && grid[r][c - 1] == 1) ans -= 2;
+                }
             }
-        }
-        int ans = 4 * queue.size();
-        while (!queue.empty()){
-            auto [x, y] = queue.front();
-            queue.pop();
-            for (auto& [dx, dy] : directions){
-                if (isNext(x + dx, y + dy, grid))
-                    --ans;
-            }
-            
         }
         
         return ans;
