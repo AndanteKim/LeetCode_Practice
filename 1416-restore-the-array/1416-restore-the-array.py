786d6a15abc1dba24fa533a089adcbe63e6ac295
Andrew Kim <ksb0724@utexas.edu>
2023-04-23 17:07:47 +0900

    Time: 1636 ms (58.90%), Space: 18.3 MB (61.64%) - LeetHub

--- 1416-restore-the-array/1416-restore-the-array.py
+++ 1416-restore-the-array/1416-restore-the-array.py
@@ -1,26 +1,18 @@
 class Solution:
-    def dfs(self, start: int, s: str, m: int, n: int, k: int, dp: List[int], mod: int) -> int:
-        if dp[start]:
-            return dp[start]
-        
-        if start == m:
-            return 1
-        
-        if s[start] == '0':
-            return 0
-        
-        count = 0
-        for end in range(start, m):
-            curr_number = s[start: end + 1]
-            if int(curr_number) > k:
-                break
-            count += self.dfs(end + 1, s, m, n, k, dp, mod)
-        dp[start] = count % mod
-        return count
-    
     def numberOfArrays(self, s: str, k: int) -> int:
         m, n = len(s), len(str(k))
         mod = 10 ** 9 + 7
-        dp = [0] * (m + 1)
+        dp = [1] + [0] * m
         
-        return self.dfs(0, s, m, n, k, dp, mod) % mod
+        for start in range(m):
+            if s[start] == '0':
+                continue
+            count = 0
+            
+            for end in range(start, m):
+                curr_number = s[start: end + 1]
+                if int(curr_number) > k:
+                    break
+                dp[end + 1] += dp[start]
+                dp[end + 1] %= mod
+        return dp[-1]
