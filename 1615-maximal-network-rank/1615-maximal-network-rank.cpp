cb17d66e5ea819ca1acc43cbccc01f76dfb38955
Andrew Kim <ksb0724@utexas.edu>
2023-04-24 17:32:46 +0900

    Time: 102 ms (52.43%), Space: 33.9 MB (49.70%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        unordered_map<int, unordered_set<int>> connected;
-        for (auto road : roads){
-            connected[road[0]].insert(road[1]);
-            connected[road[1]].insert(road[0]);
+        vector<int> degrees(n, 0);
+        
+        for (auto road: roads){
+            ++degrees[road[0]];
+            ++degrees[road[1]];
         }
         
-        int ans = 0, curr;
-        for (int i = 0; i < n; ++i){
-            for (int j = i + 1; j < n; ++j){
-                curr = connected[i].size() + connected[j].size()\
-                    - (connected[j].find(i) == connected[j].end()? 0 : 1);
-                ans = max(ans, curr);
+        int first = 0, second = 0;
+        for (int degree : degrees){
+            if (degree > first) {
+                second = first;
+                first = degree;
             }
+            else if (degree == first) continue;
+            else if (degree > second) second = degree;
         }
         
-        return ans;
+        int firstCount = 0, secondCount = 0;
+        for (int degree : degrees){
+            if (degree == first) ++firstCount;
+            else if (degree == second) ++secondCount;
+        }
+        
+        if (firstCount == 1){
+            int count = 0;
+            for (auto road : roads){
+                if (degrees[road[0]] == first && degrees[road[1]] == second) ++count;    
+                else if (degrees[road[1]] == first && degrees[road[0]] == second) ++count;
+            }
+            return first + second - (secondCount > count? 0 : 1);
+        }
+        else{
+            int count = 0;
+            for (auto road : roads){
+                if (degrees[road[0]] == first && degrees[road[1]] == first) ++count;
+            }
+            return 2 * first - (firstCount * (firstCount - 1) / 2 > count? 0 : 1);
+        }
+        
     }
 };
