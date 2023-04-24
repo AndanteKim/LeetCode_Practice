0d3d3abdcfa8b9597483f88d223401f7b5650ec1
Andrew Kim <ksb0724@utexas.edu>
2023-04-24 22:14:57 +0900

    Time: 683 ms (53.66%), Space: 100.8 MB (11.97%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,26 +1,32 @@
 class Solution:
-    def dfs(self, x: int, y: int, visited: List[List[bool]], grid: List[List[int]]) -> None:
-        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
-            return False
+    def dfs(self, x: int, y: int, rows: int, cols: int, visited: List[List[bool]], grid: List[List[int]]) -> None:
+        if x < 0 or x >= rows or y < 0 or y >= cols or visited[x][y] or not grid[x][y]:
+            return
+        visited[x][y] = True
         
-        if visited[x][y] or grid[x][y] == 0:
-            return True
-        
-        visited[x][y], isClosed = True, True
         for new_x, new_y in ((x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)):
-            if not self.dfs(new_x, new_y, visited, grid):
-                isClosed = False
-        self.curr = self.curr + 1 if isClosed else 0
-        return isClosed
+            self.dfs(new_x, new_y, rows, cols, visited, grid)
+        return
     
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        m, n, self.curr = len(grid), len(grid[0]), 0
-        visited, ans = [[False] * n for _ in range(m)], 0
+        m, n, ans = len(grid), len(grid[0]), 0
+        visited = [[False] * n for _ in range(m)]
+        
+        for i in range(m):
+            if grid[i][0] and not visited[i][0]:
+                self.dfs(i, 0, m, n, visited, grid)
+            if grid[i][n - 1] and not visited[i][n - 1]:
+                self.dfs(i, n - 1, m, n, visited, grid)
+        
+        for i in range(n):
+            if grid[0][i] and not visited[0][i]:
+                self.dfs(0, i, m, n, visited, grid)
+            if grid[m - 1][i] and not visited[m - 1][i]:
+                self.dfs(m - 1, i, m, n, visited, grid)
+        
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 1 and not visited[i][j] and self.dfs(i, j, visited, grid):
-                    ans += self.curr
-                else:
-                    self.curr = 0
-        
+                if grid[i][j] and not visited[i][j]:
+                    ans += 1
         return ans
+            
