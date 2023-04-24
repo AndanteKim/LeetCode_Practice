98b8d526fa29d76f7aefe55d9cbfd7b14f09436b
Andrew Kim <ksb0724@utexas.edu>
2023-04-24 16:02:32 +0900

    Time: 35 ms (45.39%), Space: 13.9 MB (47.65%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,13 +1,9 @@
 class Solution:
-    def remove_largest(self, stones: List[int]) -> int:
-        index_of_largest = stones.index(max(stones))
-        stones[index_of_largest], stones[-1] = stones[-1], stones[index_of_largest]
-        return stones.pop()
-    
     def lastStoneWeight(self, stones: List[int]) -> int:
+        stones.sort()
         while len(stones) > 1:
-            stone1 = self.remove_largest(stones)
-            stone2 = self.remove_largest(stones)
+            stone1 = stones.pop()
+            stone2 = stones.pop()
             if stone1 != stone2:
-                stones.append(stone1 - stone2)
+                bisect.insort(stones, stone1 - stone2)
         return stones[0] if stones else 0
