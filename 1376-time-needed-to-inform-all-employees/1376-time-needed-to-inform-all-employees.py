296dc6795f90ad57485ca0d4abf21256bdb3367f
Andrew Kim <ksb0724@utexas.edu>
2023-04-25 00:27:41 +0900

    Time: 1544 ms (16.10%), Space: 36.7 MB (87.90%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,25 +1,11 @@
 class Solution:
-    def bfs(self, boss: int, visited: List[bool], adj: List[List[int]], informTime) -> int:
-        queue, total_time = deque([(boss, 0)]), 0
-        
-        while queue:
-            current, time = queue.popleft()
-            if not visited[current]:
-                visited[current] = True
-                for neighbor in adj[current]:
-                    queue.append((neighbor, time + informTime[current]))
-                total_time = max(total_time, time + informTime[current])
-            
-        return total_time
+    def dfs(self, bossID: int, adj: List[List[int]], informTime: List[int]) -> int:
+        return max([self.dfs(neighbor, adj, informTime) for neighbor in adj[bossID]] or [0]) + informTime[bossID]
     
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
-        visited, ans = [False] * n, 0
         adj = [[] for _ in range(n)]
         
-        for node in range(len(manager)):
-            if manager[node] == -1:
-                continue
-            adj[manager[node]].append(node)
-            
-        ans = self.bfs(headID, visited, adj, informTime)
-        return ans
+        for i in range(len(manager)):
+            if manager[i] >= 0:
+                adj[manager[i]].append(i)
+        return self.dfs(headID, adj, informTime)
