cd0613962986aab3a77ae5441021c4bcfac3d89b
Andrew Kim <ksb0724@utexas.edu>
2023-04-24 21:51:48 +0900

    Time: 944 ms (12.39%), Space: 100.2 MB (25.39%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,39 +1,26 @@
 class Solution:
-    def bfs(self, x: int, y: int, grid: List[List[int]], visited: List[List[bool]]) -> None:
-        queue = deque([(x, y)])
-        visited[x][y] = True
+    def dfs(self, x: int, y: int, visited: List[List[bool]], grid: List[List[int]]) -> None:
+        if x < 0 or x >= len(grid) or y < 0 or y >= len(grid[0]):
+            return False
         
-        while queue:
-            x, y = queue.popleft()
-            
-            for new_row, new_col in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-                if new_row < 0 or new_row >= self.m or new_col < 0 or new_col >= self.n:
-                    continue
-                elif grid[new_row][new_col] and not visited[new_row][new_col]:
-                    queue.append((new_row, new_col))
-                    visited[new_row][new_col] = True
-        return
+        if visited[x][y] or grid[x][y] == 0:
+            return True
+        
+        visited[x][y], isClosed = True, True
+        for new_x, new_y in ((x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)):
+            if not self.dfs(new_x, new_y, visited, grid):
+                isClosed = False
+        self.curr = self.curr + 1 if isClosed else 0
+        return isClosed
     
     def numEnclaves(self, grid: List[List[int]]) -> int:
-        self.m, self.n = len(grid), len(grid[0])
-        visited = [[False] * self.n for _ in range(self.m)]
-        
-        for i in range(self.m):
-            if grid[i][0] and not visited[i][0]:
-                self.bfs(i, 0, grid, visited)
-            if grid[i][self.n - 1] and not visited[i][self.n - 1]:
-                self.bfs(i, self.n - 1, grid, visited)
-            
-        for i in range(self.n):
-            if grid[0][i] and not visited[0][i]:
-                self.bfs(0, i, grid, visited)
-            if grid[self.m - 1][i] and not visited[self.m - 1][i]:
-                self.bfs(self.m - 1, i, grid, visited)
-        
-        ans = 0
-        for i in range(self.m):
-            for j in range(self.n):
-                if grid[i][j] and not visited[i][j]:
-                    ans += 1
+        m, n, self.curr = len(grid), len(grid[0]), 0
+        visited, ans = [[False] * n for _ in range(m)], 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1 and not visited[i][j] and self.dfs(i, j, visited, grid):
+                    ans += self.curr
+                else:
+                    self.curr = 0
         
         return ans
