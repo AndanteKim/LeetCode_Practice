b1f0abd62a72143b27913f17842d93de1514d96d
Andrew Kim <ksb0724@utexas.edu>
2023-04-25 00:38:55 +0900

    Time: 158 ms (98.33%), Space: 90.8 MB (90.97%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,33 +1,19 @@
 class Solution {
-    int bfs(int bossID, vector<bool>& visited, vector<vector<int>>& adj, vector<int>& informTime){
-        int totalTime = 0;
-        queue<pair<int, int>> queue;
-        queue.push({bossID, 0});
-        
-        while (!queue.empty()){
-            auto [current, time] = queue.front();
-            queue.pop();
-            if (!visited[current]){
-                visited[current] = true;
-                for (int neighbor : adj[current]){
-                    queue.push({neighbor, time + informTime[current]});
-                }
-                totalTime = max(totalTime, time + informTime[current]);
-            }
+    int dfs(int i, vector<int>& manager, vector<int>& informTime){
+        if (manager[i] != -1){
+            informTime[i] += dfs(manager[i], manager, informTime);
+            manager[i] = -1;
         }
-        return totalTime;
+        
+        return informTime[i];
     }
     
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<bool> visited(n, false);
-        vector<vector<int>> adj(n);
-        for (int i = 0; i < manager.size(); ++i){
-            if (manager[i] == -1) continue;
-            adj[manager[i]].push_back(i);
+        int ans = 0;
+        for (int i = 0; i < n; ++i){
+            ans = max(ans, dfs(i, manager, informTime));
         }
-        
-        int ans = bfs(headID, visited, adj, informTime);
         return ans;
     }
 };
