891f6dbbe78975656be2cd10c62c71d6e00afeb0
Andrew Kim <ksb0724@utexas.edu>
2023-04-25 17:42:05 +0900

    Time: 572 ms (34.39%), Space: 153.7 MB (23.81%) - LeetHub

--- 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
+++ 2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph/2192-all-ancestors-of-a-node-in-a-directed-acyclic-graph.cpp
@@ -1,21 +1,38 @@
 class Solution {
-    void dfs(int start, int curr, vector<vector<int>>& adj, vector<vector<int>>& ans){
-        for (int neighbor : adj[curr]){
-            if (!ans[neighbor].empty() && ans[neighbor].back() == start) continue;
-            ans[neighbor].push_back(start);
-            dfs(start, neighbor, adj, ans);
-        }
-    }
-    
 public:
     vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
-        vector adj(n, vector<int>()), ans(n, vector<int>());
-        for(auto edge : edges){
+        vector<int> adj[n];
+        vector<int> indegrees(n, 0);
+        for (auto edge: edges){
             adj[edge[0]].push_back(edge[1]);
+            ++indegrees[edge[1]];
         }
         
-        for (int node = 0; node < n; ++node)
-            dfs(node, node, adj, ans);
+        queue<int> queue;
+        for (int i = 0; i < n; ++i) {
+            if (indegrees[i] == 0) queue.push(i);
+        }
+        
+        vector<set<int>> res(n);
+        while (!queue.empty()){
+            int sz = queue.size();
+            while(sz--){
+                int curr = queue.front();
+                for (int i : adj[curr]){
+                    --indegrees[i];
+                    res[i].insert(curr);
+                    if (indegrees[i] == 0) queue.push(i);
+                    for (int node : res[curr]) res[i].insert(node);
+                }
+                queue.pop();
+            }
+        }
+        
+        vector<vector<int>> ans(n);
+        for (int i = 0; i < n; ++i){
+            for (int node : res[i])
+                ans[i].push_back(node);
+        }
         return ans;
     }
 };
