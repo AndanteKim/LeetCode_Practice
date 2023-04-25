94669e0be4359c2f3d7513670ea9a36e95963d12
Andrew Kim <ksb0724@utexas.edu>
2023-04-25 22:52:58 +0900

    Time: 43 ms (81.15%), Space: 14.1 MB (64.70%) - LeetHub

--- 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
+++ 0990-satisfiability-of-equality-equations/0990-satisfiability-of-equality-equations.py
@@ -1,31 +1,25 @@
 class Solution:
-    def dfs(self, node: int, colored: int, color: List[int], graph: List[List[int]]) -> None:
-        if color[node] == -1:
-            color[node] = colored
-            for neighbor in graph[node]:
-                self.dfs(neighbor, colored, color, graph)
     
     def equationsPossible(self, equations: List[str]) -> bool:
-        graph = [[] for _ in range(26)]
+        root = list(range(26))
         
-        for equation in equations:
-            if equation[1] == '=':
-                x = ord(equation[0]) - ord('a')
-                y = ord(equation[3]) - ord('a')
-                graph[x].append(y)
-                graph[y].append(x)
+        def find(x: int) -> int:
+            if x != root[x]:
+                root[x] = find(root[x])
+            return root[x]
         
-        color = [-1] * 26
+        def union(x: int, y: int) -> None:
+            x, y = find(x), find(y)
+            root[x] = y
         
-        for i in range(26):
-            if color[i] == -1:
-                self.dfs(i, i, color, graph)
-                
-        for equation in equations:
-            if equation[1] == '!':
-                x = ord(equation[0]) - ord('a')
-                y = ord(equation[3]) - ord('a')
-                if color[x] == color[y]:
+        for eqn in equations:
+            if eqn[1] == '=':
+                x, y = ord(eqn[0]) - ord('a'), ord(eqn[3]) - ord('a')
+                union(x, y)
+        
+        for eqn in equations:
+            if eqn[1] == '!':
+                x, y = ord(eqn[0]) - ord('a'), ord(eqn[3]) - ord('a')
+                if find(x) == find(y):
                     return False
-            
         return True
