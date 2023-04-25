0df86505395b0e41b58bb0b934188d5d8ec2e3d4
Andrew Kim <ksb0724@utexas.edu>
2023-04-26 01:33:57 +0900

    Time: 191 ms (89.17%), Space: 65.1 MB (53.73%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,17 +1,10 @@
 class Solution {
-    bool bfs(int start, vector<int>& color, vector<vector<int>>& adj){
-        queue<int> queue{{start}};
-        color[start] = 0;
-        
-        while (!queue.empty()){
-            int node = queue.front();
-            queue.pop();
-            for (int neighbor : adj[node]){
-                if (color[neighbor] == color[node]) return false;
-                if (color[neighbor] == -1) {
-                    color[neighbor] = 1 - color[node];
-                    queue.push(neighbor);
-                }
+    bool dfs(int node, int start, vector<int>& color, vector<vector<int>>& adj){
+        color[node] = start;
+        for (int neighbor : adj[node]){
+            if (color[neighbor] == color[node]) return false;
+            if (color[neighbor] == -1){
+                if (!dfs(neighbor, 1 - start, color, adj)) return false;
             }
         }
         return true;
@@ -20,16 +13,16 @@ class Solution {
 public:
     bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
         vector adj(n + 1, vector<int>());
-        vector color(n+1, - 1);
+        vector color(n + 1, -1);
         
-        for (auto dislike : dislikes){
+        for (auto& dislike : dislikes){
             adj[dislike[0]].push_back(dislike[1]);
             adj[dislike[1]].push_back(dislike[0]);
         }
         
         for (int i = 1; i <= n; ++i){
             if (color[i] == -1){
-                if (!bfs(i, color, adj)) return false;
+                if (!dfs(i, 0, color, adj)) return false;
             }
         }
         
