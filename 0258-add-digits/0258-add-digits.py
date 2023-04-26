f98561ec66780d0b35f99c999f83f5aa3666e9db
Andrew Kim <ksb0724@utexas.edu>
2023-04-26 17:10:30 +0900

    Time: 27 ms (93.41%), Space: 13.8 MB (90.82%) - LeetHub

--- 0258-add-digits/0258-add-digits.py
+++ 0258-add-digits/0258-add-digits.py
@@ -1,10 +1,3 @@
 class Solution:
     def addDigits(self, num: int) -> int:
-        
-        if num == 0:
-            return 0
-        
-        if num % 9 == 0:
-            return 9
-        
-        return num % 9
+        return 1 + (num - 1) % 9 if num else 0
