3ebd26e56bf3d6dad06ec841791c1a194f89d6d2
Andrew Kim <ksb0724@utexas.edu>
2023-04-26 17:09:10 +0900

    Time: 0 ms (100.00%), Space: 6 MB (7.98%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,15 +1,8 @@
 class Solution {
 public:
     int addDigits(int num) {
-        while (num >= 10){
-            int curr = 0;
-            while (num > 0){
-                curr += num % 10;
-                num /= 10;
-            }
-            num = curr;
-        }
-        
-        return num;
+        if (num == 0) return 0;
+        if (num % 9 == 0) return 9;
+        return num % 9;
     }
 };
