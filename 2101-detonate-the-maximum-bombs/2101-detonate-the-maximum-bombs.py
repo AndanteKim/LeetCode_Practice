400dfeec0888f17e273b40de9bd17fc7546a96ee
Andrew Kim <ksb0724@utexas.edu>
2023-04-26 22:16:28 +0900

    Time: 8049 ms (5.08%), Space: 16.5 MB (8.59%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isOverlapped(self, s_x: int, s_y: int, s_r: int, n_x: int, n_y: int, n_r: int) -> bool:
+    def isOverlapped(self, s_x: int, s_y: int, s_r: int, n_x: int, n_y: int) -> bool:
         return (n_x - s_x) ** 2 + (n_y - s_y) ** 2 <= s_r ** 2
     
     def dfs(self, n: int, origin: int, start: int, visited: List[int], bombs: List[List[int]]) -> int:
@@ -10,7 +10,7 @@ class Solution:
             start_x, start_y, start_r = bombs[start]
             neighbor_x, neighbor_y, neighbor_r = bombs[neighbor]
             
-            if self.isOverlapped(start_x, start_y, start_r, neighbor_x, neighbor_y, neighbor_r):
+            if self.isOverlapped(start_x, start_y, start_r, neighbor_x, neighbor_y):
                 self.dfs(n, origin, neighbor, visited, bombs)
     
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
