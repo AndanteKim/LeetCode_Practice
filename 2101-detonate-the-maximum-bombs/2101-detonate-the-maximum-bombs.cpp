d568a5565dcf96a3ff2febe95a19cdbf4b6401a9
Andrew Kim <ksb0724@utexas.edu>
2023-04-26 23:49:54 +0900

    Time: 87 ms (90.05%), Space: 29.3 MB (22.87%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,23 +1,43 @@
-class Solution:
-    def isOverlapped(self, s_x: int, s_y: int, s_r: int, n_x: int, n_y: int) -> bool:
-        return (n_x - s_x) ** 2 + (n_y - s_y) ** 2 <= s_r ** 2
-    
-    def dfs(self, n: int, origin: int, start: int, visited: List[int], bombs: List[List[int]]) -> int:
-        visited[start] = True
-        for neighbor in range(n):
-            if visited[neighbor]:
-                continue
-            start_x, start_y, start_r = bombs[start]
-            neighbor_x, neighbor_y, neighbor_r = bombs[neighbor]
+#define ll long long
+class Solution {
+public:
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        int n = bombs.size();
+        vector<vector<int>> adj(n);
+        //sort(bombs.begin(), bombs.end(), [](vector<int>& a, vector<int>& b){return a[2] < b[2];});
+        for (int i = 0; i < n - 1; ++i){
+            for (int j = i + 1; j < n; ++j){
+                int x1 = bombs[i][0], y1 = bombs[i][1], r1 = bombs[i][2];
+                int x2 = bombs[j][0], y2 = bombs[j][1], r2 = bombs[j][2];
+                double d = sqrt(((ll)(x2 - x1) * (x2 - x1) + (ll)(y2 - y1) * (y2 - y1)));
+                if (d <= r1) adj[i].push_back(j);
+                if (d <= r2) adj[j].push_back(i);
+            }
+        }
+        
+        int ans = 0;
+        unordered_set<int> d;
+        for (int b0 = 0; b0 < n; ++b0){
+            if (d.find(b0) != d.end() or d.size() == n) continue;
             
-            if self.isOverlapped(start_x, start_y, start_r, neighbor_x, neighbor_y):
-                self.dfs(n, origin, neighbor, visited, bombs)
-    
-    def maximumDetonation(self, bombs: List[List[int]]) -> int:
-        n, ans = len(bombs), 0
-        for node in range(n):
-            visited = [False] * n
-            visited[node] = True
-            self.dfs(n, node, node, visited, bombs)
-            ans = max(ans, visited.count(True))
-        return ans
+            queue<int> queue{{b0}};
+            unordered_set<int> v{{b0}};
+            while (!queue.empty()){
+                int b1 = queue.front();
+                queue.pop();
+                d.insert(b1);
+                for (int b2 : adj[b1]){
+                    if (v.find(b2) == v.end()){
+                        v.insert(b2);
+                        queue.push(b2);
+                    }
+                }
+            }
+            int cnt = v.size();
+            ans = max(ans, cnt);
+            
+        }
+        
+        return ans;
+    }
+};
