b759682bc5cd95bafbc880251e8decf712e219e2
Andrew Kim <ksb0724@utexas.edu>
2023-04-26 17:11:52 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (63.10%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int addDigits(int num) {
-        if (num == 0) return 0;
-        if (num % 9 == 0) return 9;
-        return num % 9;
+        return num == 0? 0: 1 + (num - 1) % 9;
     }
 };
