84eb601494bf84538f817a6b0c8d09ac2a90a777
Andrew Kim <ksb0724@utexas.edu>
2023-04-28 17:04:25 +0900

    Time: 2076 ms (23.93%), Space: 16.7 MB (10.16%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.py
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -1,30 +1,36 @@
 class Solution:
-    def dfs(self, node: int, adj: List[List[int]], visited: List[bool]) -> None:
-        visited[node] = True
-        for neighbor in adj[node]:
-            if not visited[neighbor]:
-                self.dfs(neighbor, adj, visited)
+    def bfs(self, start: int, adj: List[List[int]], visited: List[bool]) -> None:
+        queue = deque([start])
+        
+        while queue:
+            node = queue.popleft()
+            visited[node] = True
+            for neighbor in adj[node]:
+                if not visited[neighbor]:
+                    queue.append(neighbor)
     
     def isSimilar(self, a: str, b: str) -> bool:
         diff = 0
         for i in range(len(a)):
             if a[i] != b[i]:
                 diff += 1
+        
         return diff == 0 or diff == 2
     
     def numSimilarGroups(self, strs: List[str]) -> int:
         n = len(strs)
         adj = [[] for _ in range(n)]
+        
         for i in range(n):
             for j in range(i + 1, n):
                 if self.isSimilar(strs[i], strs[j]):
                     adj[i].append(j)
                     adj[j].append(i)
                     
-        visited = [False] * n
-        ans = 0
+        visited, ans = [False] * n, 0
+        
         for i in range(n):
             if not visited[i]:
-                self.dfs(i, adj, visited)
+                self.bfs(i, adj, visited)
                 ans += 1
         return ans
