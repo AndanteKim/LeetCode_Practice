cac4f82b31e91a1e8122f45ed20799207529a5bc
Andrew Kim <ksb0724@utexas.edu>
2023-04-28 18:04:34 +0900

    Time: 2086 ms (23.93%), Space: 16.6 MB (10.16%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.py
+++ 0839-similar-string-groups/0839-similar-string-groups.py
@@ -1,36 +1,40 @@
-class Solution:
-    def bfs(self, start: int, adj: List[List[int]], visited: List[bool]) -> None:
-        queue = deque([start])
+class UnionFind:
+    def __init__(self, size: int):
+        self.parent, self.rank = [i for i in range(size)], [0] * size
         
-        while queue:
-            node = queue.popleft()
-            visited[node] = True
-            for neighbor in adj[node]:
-                if not visited[neighbor]:
-                    queue.append(neighbor)
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
     
+    def union_set(self, x: int, y: int) -> None:
+        xset, yset = self.find(x), self.find(y)
+        if self.rank[xset] < self.rank[yset]:
+            self.parent[xset] = yset
+        elif self.rank[xset] > self.rank[yset]:
+            self.parent[yset] = xset
+        else:
+            self.parent[yset] = xset
+            self.rank[xset] += 1
+            
+
+class Solution:
     def isSimilar(self, a: str, b: str) -> bool:
         diff = 0
         for i in range(len(a)):
             if a[i] != b[i]:
                 diff += 1
-        
         return diff == 0 or diff == 2
     
     def numSimilarGroups(self, strs: List[str]) -> int:
         n = len(strs)
-        adj = [[] for _ in range(n)]
+        dsu = UnionFind(n)
+        ans = n
         
         for i in range(n):
             for j in range(i + 1, n):
-                if self.isSimilar(strs[i], strs[j]):
-                    adj[i].append(j)
-                    adj[j].append(i)
-                    
-        visited, ans = [False] * n, 0
+                if self.isSimilar(strs[i], strs[j]) and dsu.find(i) != dsu.find(j):
+                    ans -= 1
+                    dsu.union_set(i, j)
         
-        for i in range(n):
-            if not visited[i]:
-                self.bfs(i, adj, visited)
-                ans += 1
         return ans
