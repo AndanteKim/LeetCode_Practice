b4da1c434d1cc80fe7d48112b9b4da74ba5ef1f3
Andrew Kim <ksb0724@utexas.edu>
2023-05-01 17:18:30 +0900

    Time: 8 ms (63.27%), Space: 9.5 MB (55.10%) - LeetHub

--- 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.cpp
+++ 1065-index-pairs-of-a-string/1065-index-pairs-of-a-string.cpp
@@ -1,13 +1,38 @@
+struct TrieNode {
+    bool flag;
+    map<char, TrieNode*> next;
+};
+
+struct Trie {
+    TrieNode* root;
+    Trie() { root = new TrieNode(); }
+    
+    void insert(string word){
+        TrieNode* p = root;
+        for (int i = 0; i < word.size(); ++i){
+            if ((p -> next).count(word[i]) == 0){
+                (p -> next).insert(make_pair(word[i], new TrieNode()));
+            }
+            p = (p -> next)[word[i]];
+        }
+        p -> flag = true;
+    }
+};
+
+
 class Solution {
 public:
     vector<vector<int>> indexPairs(string text, vector<string>& words) {
-        unordered_set<string> wordsSet(words.begin(), words.end());
+        Trie trie;
+        for (const string& word : words) trie.insert(word);
+        
         vector<vector<int>> ans;
         for (int i = 0; i < text.size(); ++i){
+            TrieNode* p = trie.root;
             for (int j = i; j < text.size(); ++j){
-                if (find(words.begin(), words.end(), text.substr(i, j - i + 1)) != words.end()){
-                    ans.push_back({i, j});
-                }
+                if ((p -> next).count(text[j]) == 0) break;
+                p = (p -> next)[text[j]];
+                if (p -> flag) ans.push_back({i, j});
             }
         }
         return ans;
