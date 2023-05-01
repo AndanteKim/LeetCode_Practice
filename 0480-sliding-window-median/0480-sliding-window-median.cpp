855545f709339cae3ed2a02e0c6c3839a238786f
Andrew Kim <ksb0724@utexas.edu>
2023-05-01 22:11:45 +0900

    Time: 234 ms (22.75%), Space: 56.8 MB (11.98%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -1,58 +1,29 @@
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        vector<double> ans;
-        unordered_map<int, int> hash_table;
-        priority_queue<int> lo;
-        priority_queue<int, vector<int>, greater<int>> hi;
-        
-        int i = 0;
-        while (i < k) lo.push(nums[i++]);
-        for (int j = 0; j < k / 2; ++j){
-            hi.push(lo.top());
-            lo.pop();
-        }
-        
-        while (true){
-            ans.push_back(k & 1? lo.top() : ((double)lo.top() + (double)hi.top()) * 0.5);
-            if (i >= nums.size()) break;
-            
-            int outNum = nums[i - k], inNum = nums[i++], balance = 0;
-            
-            balance += (outNum <= lo.top()? -1 : 1);
-            ++hash_table[outNum];
-            
-            if (!lo.empty() && inNum <= lo.top()){
-                ++balance;
-                lo.push(inNum);
-            }
-            else{
-                --balance;
-                hi.push(inNum);
+        vector<double> medians;
+        multiset<int> lo, hi;
+        for (int i = 0; i < nums.size(); ++i){
+            if (i >= k){
+                if (nums[i - k] <= *lo.rbegin())
+                    lo.erase(lo.find(nums[i - k]));
+                else hi.erase(hi.find(nums[i - k]));
             }
             
-            if (balance < 0) {
-                lo.push(hi.top());
-                hi.pop();
-                ++balance;
+            lo.insert(nums[i]);
+            hi.insert(*lo.rbegin());
+            lo.erase(prev(lo.end()));
+            
+            if (lo.size() < hi.size()){
+                lo.insert(*hi.begin());
+                hi.erase(hi.begin());
             }
             
-            if (balance > 0){
-                hi.push(lo.top());
-                lo.pop();
-                --balance;
-            }
-            
-            while (hash_table[lo.top()]){
-                --hash_table[lo.top()];
-                lo.pop();
-            }
-            
-            while (!hi.empty() && hash_table[hi.top()]){
-                --hash_table[hi.top()];
-                hi.pop();
+            if (i >= k - 1){
+                medians.push_back(k & 1 ? *lo.rbegin() : ((double)(*lo.rbegin()) + (double)(*hi.begin())) * 0.5);
             }
         }
-        return ans;
+        
+        return medians;
     }
 };
