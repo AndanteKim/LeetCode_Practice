7dc15c526715bb3162e982ea6f2baa32fe2f543d
Andrew Kim <ksb0724@utexas.edu>
2023-05-01 22:21:54 +0900

    Time: 145 ms (57.81%), Space: 38 MB (56.08%) - LeetHub

--- 0480-sliding-window-median/0480-sliding-window-median.cpp
+++ 0480-sliding-window-median/0480-sliding-window-median.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
         vector<double> medians;
-        multiset<int> lo, hi;
-        for (int i = 0; i < nums.size(); ++i){
-            if (i >= k){
-                if (nums[i - k] <= *lo.rbegin())
-                    lo.erase(lo.find(nums[i - k]));
-                else hi.erase(hi.find(nums[i - k]));
-            }
+        multiset<int> window(nums.begin(), nums.begin() + k);
+        auto mid = next(window.begin(), k / 2);
+        
+        for (int i = k;; ++i){
+            medians.push_back(((double)(*mid) + *next(mid, k % 2 - 1)) * 0.5);
             
-            lo.insert(nums[i]);
-            hi.insert(*lo.rbegin());
-            lo.erase(prev(lo.end()));
+            if (i == nums.size()) break;
             
-            if (lo.size() < hi.size()){
-                lo.insert(*hi.begin());
-                hi.erase(hi.begin());
-            }
+            window.insert(nums[i]);
+            if (nums[i] < *mid) --mid;
             
-            if (i >= k - 1){
-                medians.push_back(k & 1 ? *lo.rbegin() : ((double)(*lo.rbegin()) + (double)(*hi.begin())) * 0.5);
-            }
+            if (nums[i - k] <= *mid) ++mid;
+            
+            window.erase(window.lower_bound(nums[i - k]));
         }
         
         return medians;
