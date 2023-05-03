f6da7657a75f26f22c2abd525cf5681eba8aae54
Andrew Kim <ksb0724@utexas.edu>
2023-05-03 23:28:59 +0900

    Time: 120 ms (16.18%), Space: 21.1 MB (5.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        hash_table = defaultdict(int)
-        for num in nums:
-            hash_table[num] += 1
+        counts = Counter(nums)
+        heap = []
         
-        ans = []
-        
-        for num in hash_table:
-            ans.append((num, hash_table[num]))
-            
-        ans.sort(key = lambda x: -x[1])
-            
-        return [num for num, freq in ans][:k]
+        for key, val in counts.items():
+            heappush(heap, (val, key));
+            if len(heap) > k:
+                heappop(heap)
+                
+        return [pair[1] for pair in heap]
