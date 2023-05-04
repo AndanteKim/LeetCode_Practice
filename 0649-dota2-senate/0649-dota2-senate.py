3f9783ba38a55cdd75cc7dc58f1eb25695e4b1d1
Andrew Kim <ksb0724@utexas.edu>
2023-05-04 21:57:00 +0900

    Time: 2129 ms (11.01%), Space: 16.5 MB (7.03%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.py
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -1,24 +1,35 @@
 class Solution:
+    def ban(self, to_ban: chr, start_at: int, senate: List[chr]) -> bool:
+        loop_around = False
+        pointer = start_at
+        
+        while True:
+            if pointer == 0:
+                loop_around = True
+            if senate[pointer] == to_ban:
+                senate.pop(pointer)
+                break
+            pointer = (pointer + 1) % len(senate)
+        
+        return loop_around
+    
     def predictPartyVictory(self, senate: str) -> str:
-        queue = deque()
-        people, bans = [0, 0], [0, 0]
+        senate = list(senate)
         
-        for person in senate:
-            x = person == "R"
-            people[x] += 1
-            queue.append(x)
+        r_count = senate.count('R')
+        d_count = len(senate) - r_count
         
-        while all(people):
-            x = queue.popleft()
-            people[x] -= 1
-            if bans[x]:
-                bans[x] -= 1
+        turn = 0
+        while r_count > 0 and d_count > 0:
+            if senate[turn] == 'R':
+                banned_senator_before = self.ban('D', (turn + 1) % len(senate), senate)
+                d_count -= 1
             else:
-                bans[x ^ 1] += 1
-                queue.append(x)
-                people[x] += 1
-        
-        return "Radiant" if people[1] else "Dire"
+                banned_senator_before = self.ban('R', (turn + 1) % len(senate), senate)
+                r_count -= 1
             
+            if banned_senator_before:
+                turn -= 1
+            turn = (turn + 1) % len(senate)
         
-        
+        return 'Radiant' if d_count == 0 else 'Dire'
