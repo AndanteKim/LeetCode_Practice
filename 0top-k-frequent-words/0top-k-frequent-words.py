70a42bdf12a19f194d9e530a97f1925aa9aeeea3
Andrew Kim <ksb0724@utexas.edu>
2023-05-06 00:08:08 +0900

    Time: 114 ms, Memory: 16.6 MB - LeetHub

--- 0top-k-frequent-words/0top-k-frequent-words.py
+++ 0top-k-frequent-words/0top-k-frequent-words.py
@@ -1,18 +1,39 @@
-class Pair:
-    def __init__(self, word: str, freq: int):
-        self.word = word
-        self.freq = freq
-    
-    def __lt__(self, p: 'Pair') -> bool:
-        return self.freq < p.freq or (self.freq == p.freq and self.word > p.word)
-
-
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        n = len(words)
         cnt = Counter(words)
-        h = []
+        bucket = [{} for _ in range(n + 1)]
+        self.k = k
+        
+        def add_word(trie: Mapping, word: str) -> None:
+            root = trie
+            for c in word:
+                if c not in root:
+                    root[c] = {}
+                root = root[c]
+            root['#'] = {}
+        
+        def get_words(trie: Mapping, prefix: str) -> List[str]:
+            if self.k == 0:
+                return []
+            res = []
+            if '#' in trie:
+                self.k -= 1
+                res.append(prefix)
+            
+            for i in range(26):
+                c = chr(ord('a') + i)
+                if c in trie:
+                    res += get_words(trie[c], prefix + c)
+            return res
+        
         for word, freq in cnt.items():
-            heappush(h, Pair(word, freq))
-            if len(h) > k:
-                heappop(h)
-        return [p.word for p in sorted(h, reverse = True)]
+            add_word(bucket[freq], word)
+        
+        ans = []
+        for i in range(n, 0, -1):
+            if self.k == 0:
+                return ans
+            if bucket[i]:
+                ans += get_words(bucket[i], '')
+        return ans
