8c68121852fa36050666878d7eb08f592810a1e2
Andrew Kim <ksb0724@utexas.edu>
2023-05-06 00:21:01 +0900

    Time: 24 ms, Memory: 23.2 MB - LeetHub

--- 0top-k-frequent-words/0top-k-frequent-words.cpp
+++ 0top-k-frequent-words/0top-k-frequent-words.cpp
@@ -1,23 +1,49 @@
 class Solution {
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> frequencies;
-        for (string& word : words) ++frequencies[word];
-        vector<pair<int, string>> candidates;
-        priority_queue<pair<int, string>> pq;
-        
-        for(auto&[key, val] : frequencies){
-            pq.push({-val, key});
-            if (pq.size() > k) pq.pop();
+    int k;
+    struct TrieNode{
+        TrieNode* children[26];
+        bool isWord;
+        TrieNode(){
+            isWord = false;
+            for (int i = 0; i < 26; ++i) children[i] = nullptr;
         }
-        
-        while(!pq.empty()){
-            candidates.push_back(pq.top());
-            pq.pop();
+    };
+    
+    void addWord(TrieNode& root, const string& word){
+        TrieNode* curr = &root;
+        for (int i = 0; i < word.size(); ++i){
+            int idx = word[i] - 'a';
+            if (curr -> children[idx] == nullptr) curr -> children[idx] = new TrieNode();
+            curr = curr -> children[idx];
         }
-        sort(candidates.begin(), candidates.end());
+        curr -> isWord = true;
+    }
+    
+    void getWords(TrieNode& root, const string& prefix, vector<string>& ans){
+        if (k == 0) return;
+        if (root.isWord) {
+            --k;
+            ans.push_back(prefix);
+        }
+        for (int i = 0; i < 26; ++i){
+            if (root.children[i] != nullptr){
+                getWords(*root.children[i], prefix + (char)('a' + i), ans);
+            }
+        }
+    }
+    
+    vector<string> topKFrequent(vector<string>& words, int k){
+        int n = int(words.size());
+        this -> k = k;
+        unordered_map<string, int> cnt;
+        for (string& word: words) ++cnt[word];
         vector<string> ans;
-        for (auto&[val, key] : candidates) ans.push_back(key);
+        vector<TrieNode> bucket(n + 1, TrieNode());
+        for (auto& p : cnt) addWord(bucket[p.second], p.first);
+        
+        for (int i = n; i > 0; --i) getWords(bucket[i], "", ans);
         return ans;
     }
+    
 };
