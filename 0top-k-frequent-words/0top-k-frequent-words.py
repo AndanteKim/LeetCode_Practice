3be62fc328deea7613901c5c3e5feb40c996f246
Andrew Kim <ksb0724@utexas.edu>
2023-05-05 23:25:36 +0900

    Time: 76 ms, Memory: 16.3 MB - LeetHub

--- 0top-k-frequent-words/0top-k-frequent-words.py
+++ 0top-k-frequent-words/0top-k-frequent-words.py
@@ -4,8 +4,7 @@ class Solution:
         for word in words:
             frequencies[word] += 1
         
-        heap = []
-        for key in frequencies:
-            heappush(heap, (frequencies[key], key))
-        heap = sorted(heap, key = lambda x: (-x[0], x[1], len(x)))[:k]
-        return [key for freq, key in heap]
+        heap = [(-frequencies[key], key) for key in frequencies]
+        heapify(heap)
+        
+        return [heappop(heap)[1] for i in range(k)]
