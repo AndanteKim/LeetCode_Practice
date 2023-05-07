485fe45e6d7de03cde46098cd52ac2470d701870
Andrew Kim <ksb0724@utexas.edu>
2023-05-07 18:10:56 +0900

    Time: 99 ms (88.50%), Space: 45.6 MB (74.55%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,19 +1,31 @@
 class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
+    int partition(int left, int right, int pivot_index, vector<int>& nums){
+        int pivot = nums[pivot_index];
+        swap(nums[pivot_index], nums[right]);
         
-        for (int num : nums){
-            if (pq.size() >= k){
-                if (num >= pq.top()){
-                    pq.pop();
-                    pq.push(num);
-                }
-                continue;
+        int store_index = left;
+        for (int i = left; i <= right; ++i){
+            if (nums[i] < pivot){
+                swap(nums[store_index], nums[i]);
+                ++store_index;
             }
-            pq.push(num);
         }
         
-        return pq.top();
+        swap(nums[right], nums[store_index]);
+        return store_index;
+    }
+    
+    int select(int left, int right, int k_smallest, vector<int>& nums){
+        if (left == right) return nums[left];
+        int pivot_index = (rand() % (right - left)) + left;
+        pivot_index = partition(left, right, pivot_index, nums);
+        
+        if (k_smallest == pivot_index) return nums[k_smallest];
+        else if (k_smallest < pivot_index) return select(left, pivot_index - 1, k_smallest, nums);
+        else return select(pivot_index + 1, right, k_smallest, nums);
+    }
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return select(0, nums.size() - 1, nums.size() - k, nums);
     }
 };
