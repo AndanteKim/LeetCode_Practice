c332ed15f5b20f11b4a2b6ff419e01e91379a823
Andrew Kim <ksb0724@utexas.edu>
2023-05-07 17:53:15 +0900

    Time: 523 ms (69.86%), Space: 29.7 MB (29.02%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,12 +1,32 @@
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        heap = []
-        for num in nums:
-            if len(heap) >= k:
-                if num >= heap[0]:
-                    heappop(heap)
-                    heappush(heap, num)
-                continue
-            heappush(heap, num)
+    def partition(self, left: int, right: int, pivot_index: int, nums: List[int]) -> int:
+        pivot = nums[pivot_index]
+        nums[pivot_index], nums[right] = nums[right], nums[pivot_index]
         
-        return heap[0]
+        store_index = left
+        for i in range(left, right):
+            if nums[i] < pivot:
+                nums[store_index], nums[i] = nums[i], nums[store_index]
+                store_index += 1
+        
+        nums[right], nums[store_index] = nums[store_index], nums[right]
+        
+        return store_index
+    
+    def select(self, left: int, right: int, k_smallest: int, nums: List[int]) -> int:
+        if left == right:
+            return nums[left]
+        
+        pivot_index = randint(left, right)
+        
+        pivot_index = self.partition(left, right, pivot_index, nums)
+        
+        if k_smallest == pivot_index:
+            return nums[k_smallest]
+        elif k_smallest < pivot_index:
+            return self.select(left, pivot_index - 1, k_smallest, nums)
+        else:
+            return self.select(pivot_index + 1, right, k_smallest, nums)
+    
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        return self.select(0, len(nums) - 1, len(nums) - k, nums)
