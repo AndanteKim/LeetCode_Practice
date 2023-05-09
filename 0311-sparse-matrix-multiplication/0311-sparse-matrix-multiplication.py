1c695ff8f30a32a029a7dea0c7e30aeaefd03dd1
Andrew Kim <ksb0724@utexas.edu>
2023-05-10 03:54:42 +0900

    Time: 78 ms (39.28%), Space: 16.6 MB (22.65%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -1,11 +1,21 @@
 class Solution:
+    def compress_matrix(self, matrix: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(matrix), len(matrix[0])
+        compressed_matrix = [[] for _ in range(rows)]
+        for row in range(rows):
+            for col in range(cols):
+                if matrix[row][col]:
+                    compressed_matrix[row].append([matrix[row][col], col])
+        return compressed_matrix
+    
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        ans = [[0] * len(mat2[0]) for _ in range(len(mat1))]
+        m, k, n = len(mat1), len(mat1[0]), len(mat2[0])
         
-        for row1 in range(len(mat1)):
-            for col1 in range(len(mat1[0])):
-                if mat1[row1][col1]:
-                    for k in range(len(mat2[col1])):
-                        ans[row1][k] += mat1[row1][col1] * mat2[col1][k]
-                
+        A, B = self.compress_matrix(mat1), self.compress_matrix(mat2)
+        ans = [[0] * n for _ in range(m)]
+        
+        for mat1_row in range(m):
+            for element1, mat1_col in A[mat1_row]:
+                for element2, mat2_col in B[mat1_col]:
+                    ans[mat1_row][mat2_col] += element1 * element2
         return ans
