3dd760e28ce5f13a9d36e1c4bc8505a8ce9ce169
Andrew Kim <ksb0724@utexas.edu>
2023-05-10 03:58:03 +0900

    Time: 59 ms (73.19%), Space: 16.7 MB (16.09%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -1,17 +1,16 @@
 class Solution:
-    def compress_matrix(self, matrix: List[List[int]]) -> List[List[int]]:
-        rows, cols = len(matrix), len(matrix[0])
-        compressed_matrix = [[] for _ in range(rows)]
-        for row in range(rows):
-            for col in range(cols):
-                if matrix[row][col]:
-                    compressed_matrix[row].append([matrix[row][col], col])
-        return compressed_matrix
-    
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        m, k, n = len(mat1), len(mat1[0]), len(mat2[0])
+        def compress_matrix(matrix: List[List[int]]) -> List[List[int]]:
+            rows, cols = len(matrix), len(matrix[0])
+            compressed_matrix = [[] for _ in range(rows)]
+            for row in range(rows):
+                for col in range(cols):
+                    if matrix[row][col]:
+                        compressed_matrix[row].append([matrix[row][col], col])
+            return compressed_matrix
         
-        A, B = self.compress_matrix(mat1), self.compress_matrix(mat2)
+        m, k, n = len(mat1), len(mat1[0]), len(mat2[0])
+        A, B = compress_matrix(mat1), compress_matrix(mat2)
         ans = [[0] * n for _ in range(m)]
         
         for mat1_row in range(m):
