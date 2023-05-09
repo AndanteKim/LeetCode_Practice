acb5cadb8074e0048502e5df25b64c0258837428
Andrew Kim <ksb0724@utexas.edu>
2023-05-10 04:17:58 +0900

    Time: 9 ms (41.91%), Space: 8.2 MB (45.66%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
@@ -1,12 +1,26 @@
 class Solution {
+    vector<vector<pair<int, int>>> compressMatrix(vector<vector<int>>& matrix){
+        int rows = matrix.size(), cols = matrix[0].size();
+        vector<vector<pair<int, int>>> compressedMatrix(rows);
+        for (int row = 0; row < rows; ++row){
+            for (int col = 0; col < cols; ++col){
+                if (matrix[row][col])
+                    compressedMatrix[row].push_back({matrix[row][col], col});
+            }
+        }
+        return compressedMatrix;
+    }
+    
 public:
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
-        vector<vector<int>> ans(mat1.size(), vector<int>(mat2[0].size(), 0));
-        for (int row1 = 0; row1 < mat1.size(); ++row1){
-            for (int col1 = 0; col1 < mat1[0].size(); ++col1){
-                if (mat1[row1][col1]){
-                    for (int k = 0; k < mat2[col1].size(); ++k)
-                        ans[row1][k] += mat1[row1][col1] * mat2[col1][k];
+        int m = mat1.size(), k = mat1[0].size(), n = mat2[0].size();
+        vector<vector<pair<int, int>>> A = compressMatrix(mat1), B = compressMatrix(mat2);
+        vector<vector<int>> ans(m, vector<int>(n, 0));
+        
+        for(int mat1Row = 0; mat1Row < m; ++mat1Row){
+            for (auto [element1, mat1Col] : A[mat1Row]){
+                for (auto [element2, mat2Col] : B[mat1Col]){
+                    ans[mat1Row][mat2Col] += element1 * element2;
                 }
             }
         }
