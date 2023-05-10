91a6d4478a95a766218df1e6d499acc3ca3582aa
Andrew Kim <ksb0724@utexas.edu>
2023-05-10 16:37:50 +0900

    Time: 72 ms (44.24%), Space: 16.7 MB (6.17%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -1,20 +1,54 @@
-class Solution:
+class SparseMatrix:
+    def __init__(self, matrix: List[List[int]], col_wise: bool):
+        self.values, self.row_index, self.col_index = self.compress_matrix(matrix, col_wise)
+        
+    def compress_matrix(self, matrix: List[List[int]], col_wise: bool):
+        return self.compress_col_wise(matrix) if col_wise else self.compress_row_wise(matrix)
+    
+    def compress_row_wise(self, matrix: List[List[int]]) -> Tuple[int, int, int]:
+        values, row_index, col_index = [], [0], []
+        
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col]:
+                    values.append(matrix[row][col])
+                    col_index.append(col)
+            row_index.append(len(values))
+        
+        return values, row_index, col_index
+    
+    def compress_col_wise(self, matrix: List[List[int]]) -> Tuple[int, int, int]:
+        values, row_index, col_index = [], [], [0]
+        
+        for col in range(len(matrix[0])):
+            for row in range(len(matrix)):
+                if matrix[row][col]:
+                    values.append(matrix[row][col])
+                    row_index.append(row)
+            col_index.append(len(values))
+        return values, row_index, col_index
+
+class Solution:    
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        def compress_matrix(matrix: List[List[int]]) -> List[List[int]]:
-            rows, cols = len(matrix), len(matrix[0])
-            compressed_matrix = [[] for _ in range(rows)]
-            for row in range(rows):
-                for col in range(cols):
-                    if matrix[row][col]:
-                        compressed_matrix[row].append([matrix[row][col], col])
-            return compressed_matrix
+        A, B = SparseMatrix(mat1, False), SparseMatrix(mat2, True)
         
-        m, k, n = len(mat1), len(mat1[0]), len(mat2[0])
-        A, B = compress_matrix(mat1), compress_matrix(mat2)
-        ans = [[0] * n for _ in range(m)]
+        ans = [[0] * len(mat2[0]) for _ in range(len(mat1))]
         
-        for mat1_row in range(m):
-            for element1, mat1_col in A[mat1_row]:
-                for element2, mat2_col in B[mat1_col]:
-                    ans[mat1_row][mat2_col] += element1 * element2
+        for row in range(len(ans)):
+            for col in range(len(ans[0])):
+                mat1_row_start = A.row_index[row]
+                mat1_row_end = A.row_index[row + 1]
+            
+                mat2_col_start = B.col_index[col]
+                mat2_col_end = B.col_index[col + 1]
+                
+                while mat1_row_start < mat1_row_end and mat2_col_start < mat2_col_end:
+                    if A.col_index[mat1_row_start] < B.row_index[mat2_col_start]:
+                        mat1_row_start += 1
+                    elif A.col_index[mat1_row_start] > B.row_index[mat2_col_start]:
+                        mat2_col_start += 1
+                    else:
+                        ans[row][col] += A.values[mat1_row_start] * B.values[mat2_col_start]
+                        mat1_row_start += 1
+                        mat2_col_start += 1
         return ans
