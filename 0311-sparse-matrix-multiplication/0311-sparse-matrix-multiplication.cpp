192e36180aec24fefdccc4858071ca4fd81536e5
Andrew Kim <ksb0724@utexas.edu>
2023-05-10 16:48:26 +0900

    Time: 0 ms (100.00%), Space: 8.3 MB (14.16%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.cpp
@@ -1,26 +1,66 @@
-class Solution {
-    vector<vector<pair<int, int>>> compressMatrix(vector<vector<int>>& matrix){
-        int rows = matrix.size(), cols = matrix[0].size();
-        vector<vector<pair<int, int>>> compressedMatrix(rows);
+class SparseMatrix{
+public:
+    int cols = 0, rows = 0;
+    vector<int> values, colIndex, rowIndex;
+    
+    SparseMatrix(vector<vector<int>>& matrix){
+        rows = matrix.size();
+        cols = matrix[0].size();
+        rowIndex.push_back(0);
+        
         for (int row = 0; row < rows; ++row){
             for (int col = 0; col < cols; ++col){
-                if (matrix[row][col])
-                    compressedMatrix[row].push_back({matrix[row][col], col});
+                if (matrix[row][col]){
+                    values.push_back(matrix[row][col]);
+                    colIndex.push_back(col);
+                }
             }
+            rowIndex.push_back(values.size());
         }
-        return compressedMatrix;
     }
     
+    SparseMatrix(vector<vector<int>>& matrix, bool colWise){
+        rows = matrix.size();
+        cols = matrix[0].size();
+        colIndex.push_back(0);
+        
+        for (int col = 0; col < cols; ++col){
+            for (int row = 0; row < rows; ++row){
+                if (matrix[row][col]){
+                    values.push_back(matrix[row][col]);
+                    rowIndex.push_back(row);
+                }
+            }
+            colIndex.push_back(values.size());
+        }
+    }
+};
+
+
+class Solution {
 public:
     vector<vector<int>> multiply(vector<vector<int>>& mat1, vector<vector<int>>& mat2) {
-        int m = mat1.size(), k = mat1[0].size(), n = mat2[0].size();
-        vector<vector<pair<int, int>>> A = compressMatrix(mat1), B = compressMatrix(mat2);
-        vector<vector<int>> ans(m, vector<int>(n, 0));
+        SparseMatrix A(mat1);
+        SparseMatrix B(mat2, true);
         
-        for(int mat1Row = 0; mat1Row < m; ++mat1Row){
-            for (auto [element1, mat1Col] : A[mat1Row]){
-                for (auto [element2, mat2Col] : B[mat1Col]){
-                    ans[mat1Row][mat2Col] += element1 * element2;
+        vector<vector<int>> ans(mat1.size(), vector<int>(mat2[0].size(), 0));
+        
+        for (int row = 0; row < ans.size(); ++row){
+            for (int col = 0; col < ans[0].size(); ++col){
+                int matrixOneRowStart = A.rowIndex[row];
+                int matrixOneRowEnd = A.rowIndex[row + 1];
+                
+                int matrixTwoColStart = B.colIndex[col];
+                int matrixTwoColEnd = B.colIndex[col + 1];
+                
+                while (matrixOneRowStart < matrixOneRowEnd && matrixTwoColStart < matrixTwoColEnd){
+                    if (A.colIndex[matrixOneRowStart] < B.rowIndex[matrixTwoColStart]) ++matrixOneRowStart;
+                    else if (A.colIndex[matrixOneRowStart] > B.rowIndex[matrixTwoColStart]) ++matrixTwoColStart;
+                    else{
+                        ans[row][col] += A.values[matrixOneRowStart] * B.values[matrixTwoColStart];
+                        ++matrixOneRowStart;
+                        ++matrixTwoColStart;
+                    }
                 }
             }
         }
