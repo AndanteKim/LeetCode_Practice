38a116d46978bc6f5782a0064413246f89586711
Andrew Kim <ksb0724@utexas.edu>
2023-05-10 15:41:11 +0900

    Time: 59 ms (5.44%), Space: 16.2 MB (15.26%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.py
@@ -1,29 +1,23 @@
 class Solution:
+    def floorMod(self, x: int, y: int) -> int:
+        return ((x % y) + y) % y
+    
     def generateMatrix(self, n: int) -> List[List[int]]:
         ans = [[0] * n for _ in range(n)]
-        left, right, up, down = 0, n - 1, 0, n - 1
         i = 1
+        directions = ((0, 1), (1, 0), (0, -1), (-1, 0))
+        d, row, col = 0, 0, 0
+        
         while i <= n ** 2:
-            for col in range(left, right + 1):
-                ans[up][col] = i
-                i += 1
+            ans[row][col] = i
+            i += 1
+            r = self.floorMod(row + directions[d][0], n)
+            c = self.floorMod(col + directions[d][1], n)
             
-            for row in range(up + 1, down + 1):
-                ans[row][right] = i
-                i += 1
-            
-            if up != down:
-                for col in range(right - 1, left - 1, -1):
-                    ans[down][col] = i
-                    i += 1
-            
-            if left != right:
-                for row in range(down - 1, left, -1):
-                    ans[row][left] = i
-                    i += 1
-            
-            up += 1
-            down -= 1
-            left += 1
-            right -= 1
+            if ans[r][c] != 0:
+                d = (d + 1) % 4
+            row += directions[d][0]
+            col += directions[d][1]
+        
         return ans
+            
