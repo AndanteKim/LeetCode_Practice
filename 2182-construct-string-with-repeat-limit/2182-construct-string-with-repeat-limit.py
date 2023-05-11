6bef24dc1d355408f1d1206718fbb5f1404b9972
Andrew Kim <ksb0724@utexas.edu>
2023-05-12 00:19:14 +0900

    Time: 2923 ms (5.16%), Space: 18.3 MB (13.55%) - LeetHub

--- 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
+++ 2182-construct-string-with-repeat-limit/2182-construct-string-with-repeat-limit.py
@@ -1,21 +1,32 @@
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        pq = [(-ord(k), v) for k, v in Counter(s).items()]
-        heapify(pq)
-        ans = []
+        cnt = [0] * 26
+        ans = ""
+        for c in s:
+            cnt[ord(c) - ord('a')] += 1
         
-        while pq:
-            k, v = heappop(pq)
-            if ans and ans[-1] == k:
-                if not pq: break
-                kk, vv = heappop(pq)
-                ans.append(kk)
-                if vv - 1:
-                    heappush(pq, (kk, vv - 1))
-                heappush(pq, (k, v))
-            else:
-                m = min(v, repeatLimit)
-                ans.extend([k] * m)
-                if v - m:
-                    heappush(pq, (k, v - m))
-        return "".join(chr(-x) for x in ans)
+        while True:
+            i, onlyOne = 25, False
+            
+            while 0 <= i <= 25:
+                if len(ans) and i == ord(ans[-1]) - ord('a') and cnt[i]:
+                    onlyOne = True
+                    i -= 1
+                    continue
+                
+                if cnt[i]:
+                    break
+                i -= 1
+            
+            if i == -1:
+                break
+            fill = 1 if onlyOne else min(cnt[i], repeatLimit)
+            
+            cnt[i] -= fill
+            
+            ans += fill * chr(97 + i)
+            
+        
+        return ans
+            
+            
