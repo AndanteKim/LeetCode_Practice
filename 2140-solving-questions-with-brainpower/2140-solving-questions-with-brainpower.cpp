c6d29c8add7e8ba3e2ee806d6b3f0a7adda074be
Andrew Kim <ksb0724@utexas.edu>
2023-05-12 15:47:27 +0900

    Time: 386 ms (44.27%), Space: 123 MB (44.72%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -1,20 +1,20 @@
 #define ll long long
 
 class Solution {
+    int n;
+    ll dfs(int i, vector<ll>& dp, vector<vector<int>>& questions){
+        if (i >= n) return 0;
+        if (dp[i]) return dp[i];
+        
+        int pts = questions[i][0], skip = questions[i][1];
+        dp[i] = max(dfs(i + 1, dp, questions), pts + dfs(i + skip + 1, dp, questions));
+        return dp[i];
+    }
+    
 public:
     long long mostPoints(vector<vector<int>>& questions) {
-        int n = questions.size();
+        n = questions.size();
         vector<ll> dp(n, 0);
-        dp[n - 1] = questions.back()[0];
-        
-        for (int i = n - 2; i >= 0; --i){
-            dp[i] = questions[i][0];
-            int skip = questions[i][1];
-            
-            if (i + skip + 1 < n) dp[i] += dp[i + skip + 1];
-            dp[i] = max(dp[i], dp[i + 1]);
-        }
-        
-        return dp[0];
+        return dfs(0, dp, questions);
     }
 };
