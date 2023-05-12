b640fd9b3cd5246f340401ddb8de5006005933cb
Andrew Kim <ksb0724@utexas.edu>
2023-05-12 17:46:57 +0900

    Time: 253 ms (35.36%), Space: 75.7 MB (34.58%) - LeetHub

--- 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -1,29 +1,33 @@
 #define ll long long
 
 class Solution {
+    struct compare{
+        bool operator()(pair<int, int>& a, pair<int, int>& b){
+            if (a.first == b.first)
+                return a.second > b.second;
+            return a.first > b.first;
+        }
+    };
+    
 public:
     long long totalCost(vector<int>& costs, int k, int candidates) {
-        priority_queue<int, vector<int>, greater<int>> headWorkers, tailWorkers;
-        for (int i = 0; i < candidates; ++i) headWorkers.push(costs[i]);
-        for (int i = max(candidates, (int)(costs.size() - candidates)); i < costs.size(); ++i)
-            tailWorkers.push(costs[i]);
-        ll ans = 0;
-        int nextHead = candidates, nextTail = costs.size() - 1 - candidates;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, compare> pq;
+        for (int i = 0; i < candidates; ++i) pq.push({costs[i], 0});
+        for (int i = max(candidates, (int)costs.size() - candidates); i < costs.size(); ++i) pq.push({costs[i], 1});
         
+        ll ans = 0;
+        int nextHead = candidates, nextTail = (int)costs.size() - 1 - candidates;
         for (int i = 0; i < k; ++i){
-            if (tailWorkers.empty() || !headWorkers.empty() && headWorkers.top() <= tailWorkers.top()){
-                ans += headWorkers.top();
-                headWorkers.pop();
-                if (nextHead <= nextTail){
-                    headWorkers.push(costs[nextHead]);
+            auto [curCost, curSectionID] = pq.top();
+            pq.pop();
+            ans += curCost;
+            if (nextHead <= nextTail){
+                if (curSectionID == 0){
+                    pq.push({costs[nextHead], 0});
                     ++nextHead;
                 }
-            }
-            else{
-                ans += tailWorkers.top();
-                tailWorkers.pop();
-                if (nextHead <= nextTail){
-                    tailWorkers.push(costs[nextTail]);
+                else{
+                    pq.push({costs[nextTail], 1});
                     --nextTail;
                 }
             }
