feca633bceb6f7d462d315231eb2feace0726f3f
Andrew Kim <ksb0724@utexas.edu>
2023-05-12 15:38:47 +0900

    Time: 1776 ms (21.81%), Space: 148.2 MB (41.98%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,17 +1,18 @@
 class Solution:
+    def dfs(self, i: int, dp: List[int], questions: List[List[int]]) -> int:
+        if i >= self.n:
+            return 0
+        
+        if dp[i]:
+            return dp[i]
+        points, skip = questions[i]
+        
+        dp[i] = max(self.dfs(i + 1, dp, questions), points + self.dfs(i + skip + 1, dp, questions))
+        
+        return dp[i]
+    
     def mostPoints(self, questions: List[List[int]]) -> int:
-        n = len(questions)
-        dp = [0] * n
-        dp[-1] = questions[-1][0]
+        self.n = len(questions)
+        dp = [0] * self.n
         
-        for i in range(n - 2, -1, -1):
-            dp[i] = questions[i][0]
-            skip = questions[i][1]
-            if i + skip + 1 < n:
-                dp[i] += dp[i + skip + 1]
-            
-            dp[i] = max(dp[i], dp[i + 1])
-        
-        return dp[0]
-            
-            
+        return self.dfs(0, dp, questions)
