7531db03e7814c272bf4ad9377ed6d4cc7c112ed
Andrew Kim <ksb0724@utexas.edu>
2023-05-13 16:04:40 +0900

    Time: 331 ms (54.33%), Space: 86.8 MB (61.42%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -1,14 +1,19 @@
 class Solution:
+    def dfs(self, end: int, dp: List[int], zero: int, one: int) -> int:
+        if dp[end] != -1:
+            return dp[end]
+        count = 0
+        if end >= zero:
+            count += self.dfs(end - zero, dp, zero, one)
+        if end >= one:
+            count += self.dfs(end - one, dp, zero, one)
+        
+        dp[end] = count % self.mod
+        return dp[end]
+        
+    
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
-        dp = [1] + [0] * (high)
-        mod = 10 **  9 + 7
+        dp = [1] + [-1] * high
+        self.mod = 10 ** 9 + 7
         
-        for end in range(1, high + 1):
-            if end >= zero:
-                dp[end] += dp[end - zero]
-            
-            if end >= one:
-                dp[end] += dp[end - one]
-            dp[end] %= mod
-        
-        return sum(dp[low : high + 1]) % mod
+        return sum(self.dfs(end, dp, zero, one) for end in range(low, high + 1)) % self.mod
