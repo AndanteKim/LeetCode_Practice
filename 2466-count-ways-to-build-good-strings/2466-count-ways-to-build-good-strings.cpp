e8b77329721414dae416b6d5135249e08659ed55
Andrew Kim <ksb0724@utexas.edu>
2023-05-13 16:12:45 +0900

    Time: 20 ms (42.12%), Space: 13.3 MB (44.99%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,21 +1,23 @@
 class Solution {
+    int mod = pow(10, 9) + 7;
+    
+    int dfs(int end, vector<int>& dp, int zero, int one){
+        if (dp[end] != -1) return dp[end];
+        int count = 0;
+        if (end >= zero) count = (count + dfs(end - zero, dp, zero, one)) % mod;
+        if (end >= one) count = (count + dfs(end - one, dp, zero, one)) % mod;
+        dp[end] = count % mod;
+        return dp[end];
+    }
+    
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high + 1, 0);
+        vector<int> dp(high + 1, -1);
         dp[0] = 1;
-        int mod = pow(10, 9) + 7;
-        
-        for (int end  = 1; end <= high; ++end){
-            if (end >= zero) dp[end] += dp[end - zero];
-            dp[end] %= mod;
-            if (end >= one) dp[end] += dp[end - one];
-            dp[end] %= mod;
-        }
-        
         int ans = 0;
-        for (int i = low; i <= high; ++i) {
-            ans += dp[i];
-            ans %= mod;
+        
+        for (int end = low; end <= high; ++end){
+            ans = (ans + dfs(end, dp, zero, one)) % mod;
         }
         
         return ans;
