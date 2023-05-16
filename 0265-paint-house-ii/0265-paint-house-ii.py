1c00a9ab660f7dc11e4fc0ff9e06dce8b71e7fee
Andrew Kim <ksb0724@utexas.edu>
2023-05-16 18:00:49 +0900

    Time: 202 ms (29.92%), Space: 16.3 MB (27.99%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.py
+++ 0265-paint-house-ii/0265-paint-house-ii.py
@@ -1,25 +1,17 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
         n = len(costs)
-        
-        if n == 0: return 0
-        
+        if n == 0:
+            return 0
         k = len(costs[0])
         
-        @lru_cache(maxsize = None)
-        def memo_solve(house_num: int, color: int) -> int:
-            if house_num == n - 1:
-                return costs[house_num][color]
-            
-            cost = float('inf')
-            for next_color in range(k):
-                if next_color == color:
-                    continue
-                cost = min(cost, memo_solve(house_num + 1, next_color))
-            return costs[house_num][color] + cost
-            
-        cost = float('inf')
-        for color in range(k):
-            cost = min(cost, memo_solve(0, color))
+        for house in range(1, n):
+            for color in range(k):
+                best = float('inf')
+                for previous_color in range(k):
+                    if color == previous_color:
+                        continue
+                    best = min(best, costs[house - 1][previous_color])
+                costs[house][color] += best
         
-        return cost
+        return min(costs[-1])
