bc1e6898172bfc63733b93ff301ad6cd98e01317
Andrew Kim <ksb0724@utexas.edu>
2023-05-16 18:09:41 +0900

    Time: 21 ms (47.35%), Space: 10.8 MB (90.59%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.cpp
+++ 0265-paint-house-ii/0265-paint-house-ii.cpp
@@ -1,39 +1,21 @@
 class Solution {
-private:
-    int n, k;
-    vector<vector<int>> costs;
-    unordered_map<string, int> memo;
-    
-    int memoSolve(int houseNumber, int color){
-        
-        if (houseNumber == n - 1) return costs[houseNumber][color];
-        if (memo.find(getKey(houseNumber, color)) != memo.end()) return memo[getKey(houseNumber, color)];
-        
-        int minRemainingCost = INT_MAX;
-        for (int nextColor = 0; nextColor < k; ++nextColor){
-            if (color == nextColor) continue;
-            int currentRemainingCost = memoSolve(houseNumber + 1, nextColor);
-            minRemainingCost = min(currentRemainingCost, minRemainingCost);
-        }
-        
-        int totalCost = costs[houseNumber][color] + minRemainingCost;
-        memo[getKey(houseNumber, color)] = totalCost;
-        return totalCost;
-    }
-    
-    string getKey(int n, int color){
-        string ans = to_string(n) + ' ' + to_string(color);
-        return ans;
-    }
-    
 public:
     int minCostII(vector<vector<int>>& costs) {
-        if (costs.size() == 0) return 0;
-        k = costs[0].size(), n = costs.size();
-        this -> costs = costs;
-        int minCost = INT_MAX;
-        for (int color = 0; color < k; ++color) minCost = min(minCost, memoSolve(0, color));
+        int n = costs.size();
+        if (n == 0) return 0;
+        int k = costs[0].size();
         
-        return minCost;
+        for (int house = 1; house < n; ++house){
+            for (int color = 0; color < k; ++color){
+                int best = INT_MAX;
+                for (int prevColor = 0; prevColor < k; ++prevColor){
+                    if (color == prevColor) continue;
+                    best = min(best, costs[house - 1][prevColor]);
+                }
+                costs[house][color] += best;
+            }
+        }
+        
+        return *min_element(costs.back().begin(), costs.back().end());
     }
 };
