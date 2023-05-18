861b435776e53b85f47d1a46b48065cba9a88c07
Andrew Kim <ksb0724@utexas.edu>
2023-05-18 22:09:51 +0900

    Time: 1136 ms (93.46%), Space: 55.1 MB (16.24%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -1,20 +1,13 @@
 class Solution:
-    def dfs(self, node: int, start: int, visited: List[int], adj: List[List[int]]) -> None:
-        if not visited[node]:
-            if node != start:
-                visited[node] = True
-            for neighbor in adj[node]:
-                self.dfs(neighbor, start, visited, adj)
-    
     def findSmallestSetOfVertices(self, n: int, edges: List[List[int]]) -> List[int]:
-        adj = [[] for _ in range(n)]
-        
-        for start, end in edges:
-            adj[start].append(end)
-        
         visited = [False] * n
         
-        for node in range(n):
-            self.dfs(node, node, visited, adj)
+        for start, end in edges:
+            visited[end] = True
         
-        return [node for node in range(n) if not visited[node]]
+        ans = []
+        for node in range(n):
+            if not visited[node]:
+                ans.append(node)
+        
+        return ans
