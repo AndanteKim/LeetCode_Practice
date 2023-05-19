0246770de51b82fb29b89cefd2a9a4589734dfcf
Andrew Kim <ksb0724@utexas.edu>
2023-05-19 18:13:12 +0900

    Time: 27 ms (55.70%), Space: 13.6 MB (41.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,25 +1,29 @@
 class Solution {
-    bool dfs(int node, vector<int>& colors, int color, vector<vector<int>>& graph){
-        if (colors[node] != -1){
-            if (colors[node] != color) return false;
-            return true;
-        }
-        colors[node] = color;
-        for (int neighbor : graph[node]){
-            if (dfs(neighbor, colors, color ^ 1, graph) == false) return false;
-        }
-        return true;
-    }
-    
 public:
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         vector<int> colors(n, -1);
+        
         for (int node = 0; node < n; ++node){
-            if (colors[node] == -1) {
-                if (dfs(node, colors, 0, graph) == false) return false;
+            if (colors[node] != -1) continue;
+            colors[node] = 0;
+            queue<int> queue;
+            queue.push(node);
+            
+            while(!queue.empty()){
+                int curr = queue.front();
+                queue.pop();
+                
+                for (int neighbor : graph[curr]){
+                    if (colors[neighbor] == -1){
+                        colors[neighbor] = colors[curr] ^ 1;
+                        queue.push(neighbor);
+                    }
+                    else if (colors[neighbor] == colors[curr]) return false;
+                }
             }
         }
+        
         return true;
     }
 };
