392cac6d17021b93bc55a433a488cfd77b56f189
Andrew Kim <ksb0724@utexas.edu>
2023-05-19 18:06:18 +0900

    Time: 183 ms (51.93%), Space: 16.7 MB (37.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,25 +1,21 @@
-class Solution {
-    bool dfs(int node, vector<int>& colors, int color, vector<vector<int>>& graph){
-        if (colors[node] != -1){
-            if (colors[node] != color) return false;
-            return true;
-        }
-        colors[node] = color;
-        for (int neighbor : graph[node]){
-            if (dfs(neighbor, colors, color ^ 1, graph) == false) return false;
-        }
-        return true;
-    }
-    
-public:
-    bool isBipartite(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> colors(n, -1);
-        for (int node = 0; node < n; ++node){
-            if (colors[node] == -1) {
-                if (dfs(node, colors, 0, graph) == false) return false;
-            }
-        }
-        return true;
-    }
-};
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        n, colors = len(graph), dict()
+        
+        for node in range(n):
+            if node in colors:
+                continue
+            
+            queue = deque([node])
+            colors[node] = 0
+            
+            while queue:
+                curr = queue.popleft()
+                for neighbor in graph[curr]:
+                    if neighbor not in colors:
+                        colors[neighbor] = colors[curr] ^ 1
+                        queue.append(neighbor)
+                    elif colors[neighbor] == colors[curr]:
+                        return False
+        return True
+        
