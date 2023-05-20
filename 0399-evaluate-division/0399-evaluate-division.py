049a5bfa0dd7553e5379e9b2ede390549bdc4175
Andrew Kim <ksb0724@utexas.edu>
2023-05-20 22:11:22 +0900

    Time: 51 ms (14.24%), Space: 16.4 MB (25.75%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.py
+++ 0399-evaluate-division/0399-evaluate-division.py
@@ -1,36 +1,37 @@
 class Solution:
-    def find(self, node_id: str) -> Tuple[str, float]:
-        if node_id not in self.gid_weight:
-            self.gid_weight[node_id] = (node_id, 1)
-        group_id, node_weight = self.gid_weight[node_id]
-        
-        if group_id != node_id:
-            new_group_id, group_weight = self.find(group_id)
-            self.gid_weight[node_id] = (new_group_id, node_weight * group_weight)
-        return self.gid_weight[node_id]
+    def backtrack_evaluate(self, curr_node: float, target_node: float, acc_product: float, visited: Set[float], graph: Dict[Dict[float, float], float]) -> float:
+        visited.add(curr_node)
+        ret = -1.0
+        neighbors = graph[curr_node]
+        if target_node in neighbors:
+            ret = acc_product * neighbors[target_node]
+        else:
+            for neighbor, value in neighbors.items():
+                if neighbor in visited:
+                    continue
+                ret = self.backtrack_evaluate(neighbor, target_node, acc_product * value, visited, graph)
+                if ret != -1.0:
+                    break
+        visited.remove(curr_node)
+        return ret
     
-    def union(self, dividend: str, divisor: str, value: float) -> None:
-        dividend_gid, dividend_weight = self.find(dividend)
-        divisor_gid, divisor_weight = self.find(divisor)
-        if dividend_gid != divisor_gid:
-            self.gid_weight[dividend_gid] = (divisor_gid, divisor_weight * value / dividend_weight)
-        
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
-        self.gid_weight = dict()
-            
+        graph = defaultdict(defaultdict)
+        
         for (dividend, divisor), value in zip(equations, values):
-            self.union(dividend, divisor, value)
+            graph[dividend][divisor] = value
+            graph[divisor][dividend] = 1 / value
         
         ans = []
         
-        for (dividend, divisor) in queries:
-            if dividend not in self.gid_weight or divisor not in self.gid_weight:
-                ans.append(-1.0)
+        for dividend, divisor in queries:
+            if dividend not in graph or divisor not in graph:
+                ret = -1.0
+            elif dividend == divisor:
+                ret = 1.0
             else:
-                dividend_gid, dividend_weight = self.find(dividend)
-                divisor_gid, divisor_weight = self.find(divisor)
-                if dividend_gid != divisor_gid:
-                    ans.append(-1.0)
-                else:
-                    ans.append(dividend_weight / divisor_weight)
+                visited = set()
+                ret = self.backtrack_evaluate(dividend, divisor, 1, visited, graph)
+            ans.append(ret)
+        
         return ans
