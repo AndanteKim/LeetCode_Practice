3b53e19b06d7eda85fca712c45452680c4cc0cba
Andrew Kim <ksb0724@utexas.edu>
2023-05-21 17:13:40 +0900

    Time: 89 ms (25.16%), Space: 27 MB (16.07%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.cpp
+++ 0934-shortest-bridge/0934-shortest-bridge.cpp
@@ -1,38 +1,40 @@
 class Solution {
-private:
-    int n;
-    
-    void dfs(int x, int y, vector<pair<int, int>>& bfsQueue, vector<vector<int>>& grid){
-        grid[x][y] = 2;
-        bfsQueue.push_back({x, y});
-        for (auto& [curX, curY] : vector<pair<int, int>>{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}){
-            if ((curX >= 0 && curX < n) && (curY >= 0 && curY < n) && grid[curX][curY] == 1)
-                dfs(curX, curY, bfsQueue, grid);
-        }
-    }
-    
 public:
     int shortestBridge(vector<vector<int>>& grid) {
-        n = grid.size();
+        int n = grid.size();
         int firstX = -1, firstY = -1;
         for (int i = 0; i < n; ++i){
             for (int j = 0; j < n; ++j){
-                if (grid[i][j]){
+                if (grid[i][j] == 1){
                     firstX = i, firstY = j;
                     break;
                 }
-            } 
+            }
         }
         
-        vector<pair<int, int>> bfsQueue;
-        dfs(firstX, firstY, bfsQueue, grid);
-        int dist = 0;
+        vector<pair<int, int>> bfsQ{{firstX, firstY}}, secondBFSQ{{firstX, firstY}};
+        grid[firstX][firstY] = 2;
         
-        while (!bfsQueue.empty()){
+        while (!bfsQ.empty()){
             vector<pair<int, int>> newBFS;
-            for (auto& [x, y] : bfsQueue){
+            for (auto& [x, y] : bfsQ){
                 for (auto& [curX, curY] : vector<pair<int, int>>{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}){
-                    if ((curX >= 0 && curX < n) && (curY >= 0 && curY < n)){
+                    if ((curX >= 0 && curX < n) && (curY >= 0 && curY < n) && grid[curX][curY] == 1){
+                        newBFS.push_back({curX, curY});
+                        secondBFSQ.push_back({curX, curY});
+                        grid[curX][curY] = 2;
+                    }
+                }
+            }
+            bfsQ = newBFS;
+        }
+        
+        int dist = 0;
+        while (!secondBFSQ.empty()){
+            vector<pair<int, int>> newBFS;
+            for (auto& [x, y] : secondBFSQ){
+                for (auto& [curX, curY] : vector<pair<int, int>>{{x + 1, y}, {x - 1, y}, {x, y - 1}, {x, y + 1}}){
+                    if ((0 <= curX && curX < n) && (0 <= curY && curY < n)){
                         if (grid[curX][curY] == 1) return dist;
                         else if (grid[curX][curY] == 0){
                             newBFS.push_back({curX, curY});
@@ -41,7 +43,7 @@ public:
                     }
                 }
             }
-            bfsQueue = newBFS;
+            secondBFSQ = newBFS;
             ++dist;
         }
         return dist;
