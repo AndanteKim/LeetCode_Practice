5e9f633bd8b09c316f8913bf9515426536204f0e
Andrew Kim <ksb0724@utexas.edu>
2023-05-22 00:57:02 +0900

    Time: 0 ms, Memory: 5.9 MB - LeetHub

---
+++ maximum-69-number/maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string numString = to_string(num);
+        
+        for (auto& currChar : numString){
+            if (currChar == '6'){
+                currChar = '9';
+                break;
+            }
+        }
+        
+        return stoi(numString);
+    }
+};
