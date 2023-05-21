373d5429156dc055fb7cca508ffceb759812bc48
Andrew Kim <ksb0724@utexas.edu>
2023-05-21 16:49:17 +0900

    Time: 373 ms (87.42%), Space: 17 MB (81.61%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -1,38 +1,38 @@
 class Solution:
-    def dfs(self, x: int, y: int, bfs_queue: List[int], grid: List[List[int]]) -> None:
-        grid[x][y] = 2
-        bfs_queue.append((x, y))
-            
-        for cur_x, cur_y in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-            if 0 <= cur_x < self.n and 0 <= cur_y < self.n and grid[cur_x][cur_y] == 1:
-                self.dfs(cur_x, cur_y, bfs_queue, grid)
-    
     def shortestBridge(self, grid: List[List[int]]) -> int:
-        self.n = len(grid)
+        n = len(grid)
         first_x, first_y = -1, -1
         
-        for i in range(self.n):
-            for j in range(self.n):
-                if grid[i][j]:
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
                     first_x, first_y = i, j
                     break
-            
-        bfs_queue = []
-        self.dfs(first_x, first_y, bfs_queue, grid)
         
-        dist = 0
+        bfs_queue = [(first_x, first_y)]
+        second_bfs_queue = [(first_x, first_y)]
+        grid[first_x][first_y] = 2
         
         while bfs_queue:
             new_bfs = []
-            
             for x, y in bfs_queue:
                 for cur_x, cur_y in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
-                    if 0 <= cur_x < self.n and 0 <= cur_y < self.n:
+                    if 0 <= cur_x < n and 0 <= cur_y < n and grid[cur_x][cur_y] == 1:
+                        new_bfs.append((cur_x, cur_y))
+                        second_bfs_queue.append((cur_x, cur_y));
+                        grid[cur_x][cur_y] = 2
+            bfs_queue = new_bfs
+        
+        dist = 0
+        while second_bfs_queue:
+            new_bfs = []
+            for x, y in second_bfs_queue:
+                for cur_x, cur_y in [(x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)]:
+                    if 0 <= cur_x < n and 0 <= cur_y < n:
                         if grid[cur_x][cur_y] == 1:
                             return dist
                         elif grid[cur_x][cur_y] == 0:
                             new_bfs.append((cur_x, cur_y))
                             grid[cur_x][cur_y] = -1
-            
-            bfs_queue = new_bfs
+            second_bfs_queue = new_bfs
             dist += 1
