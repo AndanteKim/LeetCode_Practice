77c62b5b0e36507e57978f8c88775dcf44743d99
Andrew Kim <ksb0724@utexas.edu>
2023-05-22 16:30:01 +0900

    Time: 77 ms (86.17%), Space: 19.9 MB (32.97%) - LeetHub

---
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        intervals.sort(key = lambda x: x[0])
+        free_rooms = []
+        
+        heappush(free_rooms, intervals[0][1])
+        
+        for interval in intervals[1:]:
+            if free_rooms[0] <= interval[0]:
+                heappop(free_rooms)
+            
+            heappush(free_rooms, interval[1])
+        
+        return len(free_rooms)
