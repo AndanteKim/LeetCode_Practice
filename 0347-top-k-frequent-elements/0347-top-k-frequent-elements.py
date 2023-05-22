f38c8cc0a0cd450885bee8454e2d5fc167dc6df6
Andrew Kim <ksb0724@utexas.edu>
2023-05-22 15:50:08 +0900

    Time: 113 ms (53.07%), Space: 21.2 MB (22.02%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,36 +1,16 @@
 class Solution:
-    def partition(self, left: int, right: int, pivot_index: int) -> int:
-        pivot_frequency = self.counts[self.unique[pivot_index]]
-        
-        self.unique[pivot_index], self.unique[right] = self.unique[right], self.unique[pivot_index]
-        
-        store_index = left
-        for i in range(left, right):
-            if self.counts[self.unique[i]] < pivot_frequency:
-                self.unique[store_index], self.unique[i] = self.unique[i], self.unique[store_index]
-                store_index += 1
-        self.unique[right], self.unique[store_index] = self.unique[store_index], self.unique[right]
-        return store_index
-    
-    def quickselect(self, left: int, right: int, k_smallest: int) -> None:
-        if left == right:
-            return
-        
-        pivot_index = random.randint(left, right)
-        pivot_index = self.partition(left, right, pivot_index)
-        
-        if k_smallest == pivot_index:
-            return
-        
-        elif k_smallest < pivot_index:
-            self.quickselect(left, pivot_index - 1, k_smallest)
-        else:
-            self.quickselect(pivot_index + 1, right, k_smallest)
-    
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        self.counts = Counter(nums)
-        self.unique = list(self.counts.keys())
+        frequencies = defaultdict(int)
+        for num in nums:
+            frequencies[num] += 1
         
-        n = len(self.unique)
-        self.quickselect(0, n - 1, n - k)
-        return self.unique[n - k:]
+        pairs, k_freq = ((value, key) for key, value in frequencies.items()), []
+        
+        for pair in pairs:
+            if len(k_freq) < k:
+                heappush(k_freq, pair)
+            elif len(k_freq) >= k and k_freq[0][0] < pair[0]:
+                heappop(k_freq)
+                heappush(k_freq, pair)
+            
+        return [key for val, key in k_freq]
