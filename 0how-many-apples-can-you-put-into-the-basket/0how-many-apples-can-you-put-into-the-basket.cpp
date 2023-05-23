309ff171fd22ad049bc158870e969e628e4e167a
Andrew Kim <ksb0724@utexas.edu>
2023-05-23 17:14:46 +0900

    Time: 12 ms, Memory: 15.5 MB - LeetHub

--- 0how-many-apples-can-you-put-into-the-basket/0how-many-apples-can-you-put-into-the-basket.cpp
+++ 0how-many-apples-can-you-put-into-the-basket/0how-many-apples-can-you-put-into-the-basket.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int maxNumberOfApples(vector<int>& weight) {
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        for (int w : weight) minHeap.push(w);
-        
+        int size = *max_element(weight.begin(), weight.end()) + 1;
+        vector<int> counting(size);
+        for (int w: weight) ++counting[w];
         int ans = 0, units = 0;
         
-        while (minHeap.size() > 0 && units + minHeap.top() <= 5000){
-            ++ans;
-            units += minHeap.top();
-            minHeap.pop();
+        for (int i = 0; i < size; ++i){
+            if (counting[i]){
+                int take = min(counting[i], (5000 - units) / i);
+                if (take == 0) break;
+                ans += take;
+                units += take * i;
+            }
         }
         
         return ans;
