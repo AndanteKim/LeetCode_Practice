6ea1d162d64d568d0cc32da649cd7d4e8e055cd2
Andrew Kim <ksb0724@utexas.edu>
2023-05-23 15:08:47 +0900

    Time: 39 ms (42.12%), Space: 19.7 MB (86.52%) - LeetHub

--- 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
+++ 0703-kth-largest-element-in-a-stream/0703-kth-largest-element-in-a-stream.cpp
@@ -1,21 +1,21 @@
 class KthLargest {
-    priority_queue<int, vector<int>, greater<int>> pq;
+    priority_queue<int, vector<int>, greater<int>> minHeap;
     int k;
 public:
     KthLargest(int k, vector<int>& nums) {
         this -> k = k;
-        for (int num : nums){
+        for (int num : nums) {
             add(num);
         }
     }
     
     int add(int val) {
-        if (pq.size() >= this -> k && pq.top() <= val){
-            pq.pop();
-            pq.push(val);
+        if (minHeap.size() >= k && minHeap.top() <= val) {
+            minHeap.pop();
+            minHeap.push(val);
         }
-        else if (pq.size() < this -> k) pq.push(val);
-        return pq.top();
+        else if (minHeap.size() < k) minHeap.push(val);
+        return minHeap.top();
     }
 };
 
