809944b66d475ecead72efeca883a75cfa34d38a
Andrew Kim <ksb0724@utexas.edu>
2023-05-26 16:33:18 +0900

    Time: 1025 ms (25.39%), Space: 17.8 MB (23.83%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.py
@@ -1,12 +1,9 @@
 class Solution:
     def getSmallestString(self, n: int, k: int) -> str:
         ans = ['a'] * n
-        k -= n
-        
         for pos in range(n - 1, -1, -1):
-            if k <= 0:
-                break
-            add = min(k, 25)
-            ans[pos] = chr(ord(ans[pos]) + add)
+            add = min(k - pos, 26)
+            ans[pos] = chr(add + ord('a') - 1)
             k -= add
+        
         return "".join(ans)
