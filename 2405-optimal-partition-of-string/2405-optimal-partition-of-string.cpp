1067db9561ea03eed715721d07d9d1677bf5d099
Andrew Kim <ksb0724@utexas.edu>
2023-05-26 20:58:06 +0900

    Time: 45 ms (61.07%), Space: 10.2 MB (96.50%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int partitionString(string s) {
-        int ans = 1, chars_bitmask = 0;
+        int ans = 1;
+        vector<int> substr(26, 0);
         
-        for(const char&c : s){
-            int set_bit = 1 << c - 97;
-            if (chars_bitmask & set_bit){
+        for (char& c: s){
+            if (substr[c - 'a'] == 0)
+                ++substr[c - 'a'];
+            else{
+                for (int i = 0; i < 26; ++i) substr[i] = 0;
+                ++substr[c - 'a'];
                 ++ans;
-                chars_bitmask = 0;
             }
-            chars_bitmask |= set_bit;
         }
         
         return ans;
