661238ef15980539ba843f651d5694ffe0e56a2d
Andrew Kim <ksb0724@utexas.edu>
2023-05-26 16:27:32 +0900

    Time: 34 ms (79.88%), Space: 21.2 MB (72.19%) - LeetHub

--- 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
+++ 1663-smallest-string-with-a-given-numeric-value/1663-smallest-string-with-a-given-numeric-value.cpp
@@ -1,19 +1,13 @@
 class Solution {
 public:
     string getSmallestString(int n, int k) {
-        string ans(n, 0);
+        string ans(n, 'a');
+        k -= n;
         
-        for (int posL = 0; posL < n; ++posL){
-            int posR = n - posL - 1;
-            if (k > posR * 26){
-                int add = k - (posR * 26);
-                ans[posL] = ('a' + add - 1);
-                k -= add;
-            }
-            else{
-                ans[posL] = 'a';
-                --k;
-            }
+        for (int pos = n - 1; pos >= 0 && k > 0; --pos){
+            int add = min(k, 25);
+            ans[pos] = ans[pos] + add;
+            k -= add;
         }
         
         return ans;
