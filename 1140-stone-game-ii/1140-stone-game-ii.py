199ee37661b3a52b4a96e584e0b6738e1648b489
Andrew Kim <ksb0724@utexas.edu>
2023-05-26 15:03:05 +0900

    Time: 821 ms (30.88%), Space: 16.8 MB (59.91%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,20 +1,24 @@
 class Solution:
-    @lru_cache(maxsize = None)
-    def f(self, p: int, i: int, m: int) -> int:
+    def f(self, p: int, i: int, m: int, piles: List[int], dp: List[List[List[int]]]) -> int:
         if i == self.n:
             return 0
+        
+        if dp[p][i][m] != -1:
+            return dp[p][i][m]
         res = 1000000 if p == 1 else -1
         s = 0
         for x in range(1, min(2 * m, self.n - i) + 1):
-            s += self.piles[i + x - 1]
+            s += piles[i + x - 1]
             if p == 0:
-                res = max(res, s + self.f(1, i + x, max(m, x)))
+                res = max(res, s + self.f(1, i + x, max(m, x), piles, dp))
             else:
-                res = min(res, self.f(0, i + x, max(m, x)))
+                res = min(res, self.f(0, i + x, max(m, x), piles, dp))
+        dp[p][i][m] = res
         return res
     
     def stoneGameII(self, piles: List[int]) -> int:
-        self.n, self.piles = len(piles), piles
-        return self.f(0, 0, 1)
+        self.n = len(piles)
+        dp = [[[-1] * (self.n + 1) for i in range(self.n + 1)] for p in range(0, 2)]
         
+        return self.f(0, 0, 1, piles, dp)
         
