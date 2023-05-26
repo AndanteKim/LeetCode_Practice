10702163eb5fe27b50ad91ae46e4cfa29a302aea
Andrew Kim <ksb0724@utexas.edu>
2023-05-26 21:37:12 +0900

    Time: 101 ms (56.76%), Space: 59 MB (83.22%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,10 +4,11 @@ public:
         int i = 0, j = height.size() - 1, ans = 0;
         
         while (i < j){
-            int area = min(height[i], height[j]) * (j - i);
-            ans = max(ans, area);
-            if (height[i] <= height[j]) ++i;
-            else --j;
+            ans = max(ans, min(height[i], height[j]) * (j - i));
+            if (height[i] < height[j])
+                ++i;
+            else
+                --j;
         }
         
         return ans;
