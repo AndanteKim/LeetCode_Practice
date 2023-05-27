d3a17c187e3df33f4d433145b53e226721d08138
Andrew Kim <ksb0724@utexas.edu>
2023-05-27 21:22:01 +0900

    Time: 2853 ms (65.47%), Space: 72.9 MB (49.78%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,18 +1,29 @@
 class Solution:
-    def stoneGameIII(self, stoneValue: List[int]) -> str:
-        n = len(stoneValue)
-        dp = [0] * (n + 1)
-        for i in range(n - 1, -1, -1):
-            dp[i] = stoneValue[i] - dp[i + 1]
-            if i + 2 <= n:
-                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2])
-            
-            if i + 3 <= n:
-                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3])
+    
+    def f(self, i: int, dp: List[int], stoneValue: List[int]) -> int:
+        if i == self.n:
+            return 0
+        if dp[i] != self.not_computed:
+            return dp[i]
+        dp[i] = stoneValue[i] - self.f(i + 1, dp, stoneValue)
+        if i + 2 <= self.n:
+            dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - self.f(i + 2, dp, stoneValue))
         
-        if dp[0] > 0:
+        if i + 3 <= self.n:
+            dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - self.f(i + 3, dp, stoneValue))
+        
+        return dp[i]
+    
+    def stoneGameIII(self, stoneValue: List[int]) -> str:
+        self.n, self.not_computed = len(stoneValue), 10 ** 9
+        dp = [self.not_computed] * (self.n + 1)
+        
+        dif = self.f(0, dp, stoneValue)
+        
+        if dif > 0:
             return "Alice"
-        if dp[0] < 0:
+        if dif < 0:
             return "Bob"
         
         return "Tie"
+        
