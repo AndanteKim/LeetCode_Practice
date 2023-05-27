f3f58b6009c8df07cc8487b7beed4905edb332e3
Andrew Kim <ksb0724@utexas.edu>
2023-05-27 21:44:32 +0900

    Time: 2401 ms (86.55%), Space: 21.1 MB (57.85%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,29 +1,18 @@
 class Solution:
-    
-    def f(self, i: int, dp: List[int], stoneValue: List[int]) -> int:
-        if i == self.n:
-            return 0
-        if dp[i] != self.not_computed:
-            return dp[i]
-        dp[i] = stoneValue[i] - self.f(i + 1, dp, stoneValue)
-        if i + 2 <= self.n:
-            dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - self.f(i + 2, dp, stoneValue))
-        
-        if i + 3 <= self.n:
-            dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - self.f(i + 3, dp, stoneValue))
-        
-        return dp[i]
-    
     def stoneGameIII(self, stoneValue: List[int]) -> str:
-        self.n, self.not_computed = len(stoneValue), 10 ** 9
-        dp = [self.not_computed] * (self.n + 1)
+        n = len(stoneValue)
+        dp = [0] * 4
         
-        dif = self.f(0, dp, stoneValue)
-        
-        if dif > 0:
+        for i in range(n - 1, -1, -1):
+            dp[i % 4] = stoneValue[i] - dp[(i + 1) % 4]
+            if i + 2 <= n:
+                dp[i % 4] = max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] - dp[(i + 2) % 4])
+            if i + 3 <= n:
+                dp[i % 4] = max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[(i + 3) % 4])
+            
+        if dp[0] > 0:
             return "Alice"
-        if dif < 0:
+        if dp[0] < 0:
             return "Bob"
         
         return "Tie"
-        
