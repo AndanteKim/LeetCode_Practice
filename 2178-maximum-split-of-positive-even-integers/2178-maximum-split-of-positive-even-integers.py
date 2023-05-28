8b99c6f3cfde35f235bc889a2f631a191e40621a
Andrew Kim <ksb0724@utexas.edu>
2023-05-28 22:57:45 +0900

    Time: 499 ms (69.55%), Space: 29 MB (30.45%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
@@ -1,12 +1,13 @@
 class Solution:
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
+        if finalSum % 2:
+            return []
         ans, i = [], 2
         
-        if finalSum % 2 == 0:
-            while i <= finalSum:
-                ans.append(i)
-                finalSum -= i
-                i += 2
-            ans[-1] += finalSum
+        while i <= finalSum:
+            ans.append(i)
+            finalSum -= i
+            i += 2
+        ans[-1] += finalSum
         return ans
         
