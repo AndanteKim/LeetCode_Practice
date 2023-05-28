59812c69dad1f007fe3f2c9bfa2fdc8f7078c59c
Andrew Kim <ksb0724@utexas.edu>
2023-05-28 23:31:27 +0900

    Time: 229 ms (10.02%), Space: 83.7 MB (6.38%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.cpp
@@ -1,17 +1,30 @@
 typedef long long ll;
 
 class Solution {
+private:
+    vector<ll> ans;
+    bool backtrack(ll target, ll currSum, vector<ll>& path, ll start){
+        if (currSum == target){
+            if (path.size() > ans.size()) {
+                ans = path;
+            }
+            return true;
+        }
+        
+        for (ll i = start; i <= target; i += 2){
+            if (i + currSum > target) break;
+            path.push_back(i);
+            if (backtrack(target, currSum + i, path, start + 2)) return true;
+            path.pop_back();
+        }
+        return false;
+    }
+    
 public:
     vector<long long> maximumEvenSplit(long long finalSum) {
-        if (finalSum % 2) return {};
-        vector<ll> ans;
-        ll i = 2;
-        while (i <= finalSum){
-            ans.push_back(i);
-            finalSum -= i;
-            i += 2;
-        }
-        ans.back() += finalSum; 
+        if (finalSum & 1) return {};
+        vector<ll> path{};
+        backtrack(finalSum, 0, path, 2);
         return ans;
     }
 };
