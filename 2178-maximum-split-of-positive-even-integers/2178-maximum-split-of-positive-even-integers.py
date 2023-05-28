782272056d0895b3350dd45fafdfab04f3ff7630
Andrew Kim <ksb0724@utexas.edu>
2023-05-28 23:14:42 +0900

    Time: 1075 ms (5.19%), Space: 145.9 MB (5.19%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.py
@@ -1,13 +1,22 @@
 class Solution:
+    def backtrack(self, target: int, currSum: int, path: List[int], start: int) -> bool:
+        if currSum == target:
+            if len(path) > len(self.ans):
+                self.ans = path
+            return True
+        
+        for i in range(start, target + 1, 2):
+            if currSum + i > target:
+                break
+            path.append(i)
+            if self.backtrack(target, currSum + i, path, i + 2):
+                return True
+            path.pop()
+    
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
-        if finalSum % 2:
+        if finalSum & 1:
             return []
-        ans, i = [], 2
         
-        while i <= finalSum:
-            ans.append(i)
-            finalSum -= i
-            i += 2
-        ans[-1] += finalSum
-        return ans
-        
+        self.ans = []
+        self.backtrack(finalSum, 0, [], 2)
+        return self.ans
