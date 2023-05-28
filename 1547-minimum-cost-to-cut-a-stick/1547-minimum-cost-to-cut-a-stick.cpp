39a673080577620befe6b90d5ae6f2ef6b009073
Andrew Kim <ksb0724@utexas.edu>
2023-05-28 20:38:27 +0900

    Time: 64 ms (76.38%), Space: 10.3 MB (60.72%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,31 +1,24 @@
 class Solution {
-private:
-    int cost(int left, int right, vector<int>& cuts, vector<vector<int>>& memo){
-        if (left > right)
-            return 0;
-        
-        if (memo[left][right] != -1)
-            return memo[left][right];
-        
-        int ans = INT_MAX;
-        for (int mid = left; mid <= right; ++mid){
-            int c = cuts[right + 1] - cuts[left - 1] + cost(left, mid - 1, cuts, memo) + cost(mid + 1, right, cuts, memo);
-            ans = min(ans, c);
-        }
-            
-        return memo[left][right] = ans;
-    }
-    
 public:
     int minCost(int n, vector<int>& cuts) {
-        int c = cuts.size();
-        
+        int m = cuts.size();
         cuts.insert(cuts.begin(), 0);
         cuts.push_back(n);
         sort(cuts.begin(), cuts.end());
         
-        vector<vector<int>> memo(c + 1, vector<int>(c + 1, -1));
+        vector<vector<int>> dp(m + 2, vector<int>(m + 2, 0));
         
-        return cost(1, c, cuts, memo);
+        for (int diff = 2; diff <= m + 1; ++diff){
+            for (int left = 0; left <= m + 1 - diff; ++left){
+                int right = left + diff, ans = INT_MAX;
+                
+                for (int mid = left + 1; mid < right; ++mid)
+                    ans = min(ans, dp[left][mid] + dp[mid][right] + cuts[right] - cuts[left]);
+                
+                dp[left][right] = ans;
+            }
+        }
+        
+        return dp[0][m + 1];
     }
 };
