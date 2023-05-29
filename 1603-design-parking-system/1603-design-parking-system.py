6c15101c92cc9fcad550d0b5059dd503d845e6a4
Andrew Kim <ksb0724@utexas.edu>
2023-05-29 14:30:31 +0900

    Time: 142 ms (34.27%), Space: 16.7 MB (38.14%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.py
+++ 1603-design-parking-system/1603-design-parking-system.py
@@ -1,15 +1,11 @@
 class ParkingSystem:
-    
+
     def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small
-        self.carType_capacity = {1:self.big, 2: self.medium, 3: self.small}
-        self.current_status = {1:0, 2:0, 3:0}
+        self.available = [big, medium, small]
 
     def addCar(self, carType: int) -> bool:
-        if self.current_status[carType] < self.carType_capacity[carType]:
-            self.current_status[carType] += 1
+        if self.available[carType - 1] > 0:
+            self.available[carType - 1] -= 1
             return True
         else:
             return False
