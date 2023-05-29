a954f3122a2ed9fc128b2c904771be4c63b45a34
Andrew Kim <ksb0724@utexas.edu>
2023-05-29 16:16:55 +0900

    Time: 28 ms (66.89%), Space: 19.1 MB (95.36%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.cpp
@@ -1,41 +1,26 @@
 class TicTacToe {
 private:
-    vector<vector<int>> board;
-    int n;
+    vector<int> rows, cols;
+    int n, diagonal = 0, antiDiagonal = 0;
+    
 public:
+    
     TicTacToe(int n) {
-        board.resize(n, vector<int>(n, 0));
         this -> n = n;
+        rows.resize(n, 0);
+        cols.resize(n, 0);
     }
     
     int move(int row, int col, int player) {
-        board[row][col] = player;
+        int currentPlayer = (player == 1)? 1 : -1;
+        rows[row] += currentPlayer;
+        cols[col] += currentPlayer;
+        if (row == col) diagonal += currentPlayer;
+        if (row == n - col - 1) antiDiagonal += currentPlayer;
         
-        if (checkRow(row, player) || checkCol(col, player) || (row == col && checkDiagonal(player)) || (row == n - col - 1 &&\
-                                                                                                       checkAntiDiagonal(player))){
-            return player;
-        }
+        if (abs(rows[row]) == n || abs(cols[col]) == n || abs(diagonal) == n || abs(antiDiagonal) == n) return player;
+        
         return 0;
-    }
-    
-    bool checkRow(int row, int player){
-        for (int col = 0; col < n; ++col) if (board[row][col] != player) return false;
-        return true;
-    }
-    
-    bool checkCol(int col, int player){
-        for (int row = 0; row < n; ++row) if (board[row][col] != player) return false;
-        return true;
-    }
-    
-    bool checkDiagonal(int player){
-        for (int row = 0; row < n; ++row) if (board[row][row] != player) return false;
-        return true;
-    }
-    
-    bool checkAntiDiagonal(int player){
-        for (int row = 0; row < n; ++row) if (board[row][n - row - 1] != player) return false;
-        return true;
     }
 };
 
