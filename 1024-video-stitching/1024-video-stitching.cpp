7ffda364707b6e87716ee25d8bd63af23880740b
Andrew Kim <ksb0724@utexas.edu>
2023-05-30 01:06:12 +0900

    Time: 3 ms (67.79%), Space: 7.8 MB (71.17%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.cpp
+++ 1024-video-stitching/1024-video-stitching.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int videoStitching(vector<vector<int>>& clips, int time) {
-        sort(clips.begin(), clips.end());
-        vector<int> dp(101, 101);
+        vector<int> dp(time + 1, time + 1);
         dp[0] = 0;
-        for (vector<int>& clip : clips){
-            for (int i = clip[0] + 1; i <= clip[1]; ++i){
-                dp[i] = min(dp[i], dp[clip[0]] + 1);
+        
+        for (int i = 1; i <= time && dp[i - 1] < time; ++i){
+            for (vector<int>& clip : clips){
+                if (i >= clip[0] && i <= clip[1]){
+                    dp[i] = min(dp[i], dp[clip[0]] + 1);
+                }
             }
         }
-        return (dp[time] >= 100)? -1: dp[time];
+        return (dp[time] == time + 1)? -1: dp[time];
     }
 };
