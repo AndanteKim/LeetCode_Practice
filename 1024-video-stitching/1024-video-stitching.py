58ac4630427563aa065cdcab0881e49e8a7f0a72
Andrew Kim <ksb0724@utexas.edu>
2023-05-29 23:45:05 +0900

    Time: 49 ms (33.76%), Space: 16.2 MB (47.72%) - LeetHub

--- 1024-video-stitching/1024-video-stitching.py
+++ 1024-video-stitching/1024-video-stitching.py
@@ -1,13 +1,11 @@
 class Solution:
     def videoStitching(self, clips: List[List[int]], time: int) -> int:
         clips.sort()
+        dp = [101] * 101
+        dp[0] = 0
         
-        end, end2, ans = -1, 0, 0
-        for i, j in clips:
-            if end2 >= time or i > end2:
-                break
-            elif end < i <= end2:
-                ans, end = ans + 1, end2
-            end2 = max(end2, j)
+        for start, end in clips:
+            for i in range(start + 1, end + 1):
+                dp[i] = min(dp[i], dp[start] + 1)
         
-        return ans if end2 >= time else -1
+        return -1 if dp[time] >= 100 else dp[time]
