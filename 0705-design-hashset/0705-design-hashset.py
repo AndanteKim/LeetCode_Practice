a6f96299f21d71f5d36afefb5761eb1499ef390f
Andrew Kim <ksb0724@utexas.edu>
2023-05-30 19:48:04 +0900

    Time: 216 ms (50.56%), Space: 22.6 MB (21.40%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,19 +1,56 @@
 class MyHashSet:
 
     def __init__(self):
-        self.dictionary = [0] * (10 ** 6 + 1)
+        # set any prime to avoid collision
+        self.keyRange = 769
+        self.bucketArray = [Bucket() for i in range(self.keyRange)]
+    
+    def _hash(self, key):
+        return key % self.keyRange
 
     def add(self, key: int) -> None:
-        if self.dictionary[key] == 0:
-            self.dictionary[key] += 1
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].insert(key)
 
     def remove(self, key: int) -> None:
-        if self.dictionary[key] > 0:
-            self.dictionary[key] -= 1
+        bucketIndex = self._hash(key)
+        self.bucketArray[bucketIndex].delete(key)
 
     def contains(self, key: int) -> bool:
-        return self.dictionary[key] > 0
+        bucketIndex = self._hash(key)
+        return self.bucketArray[bucketIndex].exists(key)
 
+class Node:
+    def __init__(self, value: int, nextNode = None):
+        self.value = value
+        self.next = nextNode
+        
+class Bucket:
+    def __init__(self):
+        self.head = Node(0)
+    
+    def insert(self, newValue: int) -> None:
+        if not self.exists(newValue):
+            newNode = Node(newValue, self.head.next)
+            self.head.next = newNode
+            
+    def delete(self, value: int) -> None:
+        prev = self.head
+        curr = self.head.next
+        while curr:
+            if curr.value == value:
+                prev.next = curr.next
+                return
+            prev = curr
+            curr = curr.next
+    
+    def exists(self, value: int) -> bool:
+        curr = self.head.next
+        while curr:
+            if curr.value == value:
+                return True
+            curr = curr.next
+        return False
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
