608f7507c559261294771525b30a206cf625fc2b
Andrew Kim <ksb0724@utexas.edu>
2023-05-30 20:25:01 +0900

    Time: 376 ms (14.13%), Space: 40.9 MB (79.73%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,24 +1,85 @@
+struct Node{
+    int value;
+    Node* next;
+    Node() : value(0), next(nullptr) {}
+    Node(int value) : value(value), next(nullptr) {}
+    Node(int value, Node* next) : value(value), next(next) {}
+};
+
+class Bucket {
+private:
+    Node* head;
+
+public:
+    Bucket(){
+        head = new Node(0);
+    }
+    
+    void insert(int newValue){
+        if (!exists(newValue)){
+            Node* newNode = new Node(newValue, head -> next);
+            head -> next = newNode;
+        }
+    }
+    
+    void erase(int value){
+        Node* prev = head;
+        Node* curr = head -> next;
+        
+        while (curr){
+            if (curr -> value == value){
+                prev -> next = curr -> next;
+                return;
+            }
+            prev = curr;
+            curr = curr -> next;
+        }
+    }
+    
+    bool exists(int value){
+        Node* curr = head -> next;
+        while (curr){
+            if (curr -> value == value) return true;
+            curr = curr -> next;
+        }
+        
+        return false;
+    }
+};
+
 class MyHashSet {
 private:
-    vector<int> dict;
-    
+    int keyRange;
+    vector<Bucket*> bucketArray;
+protected:
+    int hash(int key){
+        return key % keyRange;
+    }
 public:
     MyHashSet() {
-        dict.resize(pow(10, 6) + 1, 0);
+        keyRange = 769;
+        Bucket* bucket = new Bucket();
+        for (int i = 0; i < 769; ++i) bucketArray.push_back(bucket);
     }
     
     void add(int key) {
-        if (dict[key] == 0) ++dict[key];
+        int bucketIndex = hash(key);
+        bucketArray[bucketIndex] -> insert(key);
     }
     
     void remove(int key) {
-        if (dict[key] > 0) --dict[key];
+        int bucketIndex = hash(key);
+        bucketArray[bucketIndex] -> erase(key);
     }
     
     bool contains(int key) {
-        return dict[key] > 0;
+        int bucketIndex = hash(key);
+        return bucketArray[bucketIndex] -> exists(key);
     }
 };
+
+
+
 
 /**
  * Your MyHashSet object will be instantiated and called as such:
