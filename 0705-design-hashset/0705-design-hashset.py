779246eeaf0c280f194c02166c2cd506ea212ec2
Andrew Kim <ksb0724@utexas.edu>
2023-05-30 20:54:55 +0900

    Time: 233 ms (46.65%), Space: 22.7 MB (19.88%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.py
+++ 0705-design-hashset/0705-design-hashset.py
@@ -1,17 +1,16 @@
 class MyHashSet:
 
     def __init__(self):
-        # set any prime to avoid collision
         self.keyRange = 769
         self.bucketArray = [Bucket() for i in range(self.keyRange)]
-    
-    def _hash(self, key) -> int:
+        
+    def _hash(self, key: int) -> int:
         return key % self.keyRange
 
     def add(self, key: int) -> None:
         bucketIndex = self._hash(key)
         self.bucketArray[bucketIndex].insert(key)
-
+        
     def remove(self, key: int) -> None:
         bucketIndex = self._hash(key)
         self.bucketArray[bucketIndex].delete(key)
@@ -19,38 +18,79 @@ class MyHashSet:
     def contains(self, key: int) -> bool:
         bucketIndex = self._hash(key)
         return self.bucketArray[bucketIndex].exists(key)
-
-class Node:
-    def __init__(self, value: int, nextNode = None):
-        self.value = value
-        self.next = nextNode
-        
+    
 class Bucket:
     def __init__(self):
-        self.head = Node(0)
+        self.tree = BSTree()
     
-    def insert(self, newValue: int) -> None:
-        if not self.exists(newValue):
-            newNode = Node(newValue, self.head.next)
-            self.head.next = newNode
-            
+    def insert(self, value: int) -> None:
+        self.tree.root = self.tree.insertIntoBST(self.tree.root, value)
+    
     def delete(self, value: int) -> None:
-        prev = self.head
-        curr = self.head.next
-        while curr:
-            if curr.value == value:
-                prev.next = curr.next
-                return
-            prev = curr
-            curr = curr.next
+        self.tree.root = self.tree.deleteNode(self.tree.root, value)
     
     def exists(self, value: int) -> bool:
-        curr = self.head.next
-        while curr:
-            if curr.value == value:
-                return True
-            curr = curr.next
-        return False
+        return self.tree.searchBST(self.tree.root, value) is not None
+    
+class TreeNode:
+    def __init__(self, value: int):
+        self.val = value
+        self.left = None
+        self.right = None
+        
+class BSTree:
+    def __init__(self):
+        self.root = None
+    
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root or val == root.val:
+            return root
+        
+        return self.searchBST(root.left, val) if val < root.val else self.searchBST(root.right, val)
+    
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return TreeNode(val)
+        
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        elif val == root.val:
+            return root
+        else:
+            root.left = self.insertIntoBST(root.left, val)
+        return root
+
+    def successor(self, root: Optional[TreeNode]) -> int:
+        root = root.right
+        while root.left:
+            root = root.left
+        return root.val
+    
+    def predecessor(self, root: Optional[TreeNode]) -> int:
+        root = root.left
+        while root.right:
+            root = root.right
+        return root.val
+    
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        
+        if key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        elif key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        else:
+            if not (root.left or root.right):
+                root = None
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.deleteNode(root.right, root.val)
+            else:
+                root.val = self.predecessor(root)
+                root.left = self.deleteNode(root.left, root.val)
+        
+        return root
 
 # Your MyHashSet object will be instantiated and called as such:
 # obj = MyHashSet()
