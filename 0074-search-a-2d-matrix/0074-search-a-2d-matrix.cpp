d73c67551e83e2e09f67ff5ef9c3d5d0d26f37cd
Andrew Kim <ksb0724@utexas.edu>
2023-05-31 21:42:07 +0900

    Time: 0 ms (100.00%), Space: 9.4 MB (95.11%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int row_left = 0, row_right = matrix.size();
-        bool ans = false;
         
         while (row_left < row_right){
             int row_mid = row_left + (row_right - row_left) / 2;
@@ -11,7 +10,7 @@ public:
                 int col_left = 0, col_right = matrix[0].size();
                 while (col_left < col_right){
                     int col_mid = col_left + (col_right - col_left) / 2;
-                    if (target == matrix[row_mid][col_mid]) ans = true;
+                    if (target == matrix[row_mid][col_mid]) return true;
                     if (target < matrix[row_mid][col_mid]) col_right = col_mid;
                     else col_left = col_mid + 1;
                 }
@@ -20,6 +19,6 @@ public:
             else row_left = row_mid + 1;
         }
         
-        return ans;
+        return false;
     }
 };
