d3513874380bca3072478cfc91321762a7403ac7
Andrew Kim <ksb0724@utexas.edu>
2023-06-02 01:41:17 +0900

    Time: 964 ms (66.67%), Space: 39.6 MB (52.99%) - LeetHub

--- 1230-toss-strange-coins/1230-toss-strange-coins.py
+++ 1230-toss-strange-coins/1230-toss-strange-coins.py
@@ -1,22 +1,14 @@
 class Solution:
-    def findProbability(self, index: int, target: int, n: int, prob: List[float], memo: List[List[float]]) -> float:
-        if target < 0:
-            return 0
-        
-        if index == n:
-            if target == 0:
-                return 1
-            else:
-                return 0
-        
-        if memo[index][target] != -1:
-            return memo[index][target]
-        
-        memo[index][target] = self.findProbability(index + 1, target - 1, n, prob, memo) * prob[index] + self.findProbability(index + 1, target, n, prob, memo) * (1 - prob[index])
-        
-        return memo[index][target]
-    
     def probabilityOfHeads(self, prob: List[float], target: int) -> float:
-        memo = [[-1] * (target + 1) for _ in range(len(prob))]
+        n = len(prob)
+        dp = [[0] * (target + 1) for _ in range(n + 1)]
+        dp[0][0] = 1
         
-        return self.findProbability(0, target, len(prob), prob, memo)
+        for i in range(1, n + 1):
+            dp[i][0] = dp[i - 1][0] * (1 - prob[i - 1])
+            for j in range(1, target + 1):
+                if j > i:
+                    break
+                dp[i][j] = dp[i - 1][j - 1] * prob[i - 1] + dp[i - 1][j] * (1 - prob[i - 1])
+        
+        return dp[n][target]
