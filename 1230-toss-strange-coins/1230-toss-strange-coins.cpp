9319d4e89df7706e50d7c2fa0bb2cfecbf1aee84
Andrew Kim <ksb0724@utexas.edu>
2023-06-02 02:11:44 +0900

    Time: 79 ms (85.25%), Space: 8.6 MB (86.89%) - LeetHub

--- 1230-toss-strange-coins/1230-toss-strange-coins.cpp
+++ 1230-toss-strange-coins/1230-toss-strange-coins.cpp
@@ -2,17 +2,16 @@ class Solution {
 public:
     double probabilityOfHeads(vector<double>& prob, int target) {
         int n = prob.size();
-        vector<vector<double>> dp(n + 1, vector<double>(target + 1, 0));
-        dp[0][0] = 1;
+        vector<double> dp(target + 1, 0);
+        dp[0] = 1;
         
         for (int i = 1; i <= n; ++i){
-            dp[i][0] = dp[i - 1][0] * (1 - prob[i - 1]);
-            for (int j = 1; j <= target; ++j){
-                if (j > i) break;
-                dp[i][j] = dp[i - 1][j - 1] * prob[i - 1] + dp[i - 1][j] * (1 - prob[i - 1]);
+            for (int j = target; j > 0; --j){
+                dp[j] = dp[j - 1] * prob[i - 1] + dp[j] * (1 - prob[i - 1]);
             }
+            dp[0] = dp[0] * (1 - prob[i - 1]);
         }
         
-        return dp[n][target];
+        return dp[target];
     }
 };
