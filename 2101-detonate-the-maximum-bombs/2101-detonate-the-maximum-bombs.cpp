3261343ae90fa555983a70174fd0dddce0cdd53e
Andrew Kim <ksb0724@utexas.edu>
2023-06-02 22:12:56 +0900

    Time: 418 ms (32.45%), Space: 42.7 MB (19.33%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -1,41 +1,32 @@
-#define ll long long
+typedef long long ll;
+
 class Solution {
+    int dfs(int curr, unordered_set<int>& visited, unordered_map<int, vector<int>>& graph){
+        visited.insert(curr);
+        
+        for (int neighbor : graph[curr]){
+            if (visited.find(neighbor) == visited.end()) dfs(neighbor, visited, graph);
+        }
+        return visited.size();
+    }
+    
 public:
     int maximumDetonation(vector<vector<int>>& bombs) {
-        int n = bombs.size();
-        vector<vector<int>> adj(n);
-        //sort(bombs.begin(), bombs.end(), [](vector<int>& a, vector<int>& b){return a[2] < b[2];});
-        for (int i = 0; i < n - 1; ++i){
-            for (int j = i + 1; j < n; ++j){
-                int x1 = bombs[i][0], y1 = bombs[i][1], r1 = bombs[i][2];
-                int x2 = bombs[j][0], y2 = bombs[j][1], r2 = bombs[j][2];
-                double d = sqrt(((ll)(x2 - x1) * (x2 - x1) + (ll)(y2 - y1) * (y2 - y1)));
-                if (d <= r1) adj[i].push_back(j);
-                if (d <= r2) adj[j].push_back(i);
+        unordered_map<int, vector<int>> graph;
+        int n = bombs.size(), ans = 0;
+        
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < n; ++j){
+                if (i == j) continue;
+                int x_i = bombs[i][0], y_i = bombs[i][1], r_i = bombs[i][2];
+                int x_j = bombs[j][0], y_j = bombs[j][1];
+                if ((ll)(x_j - x_i) * (x_j - x_i) + (ll)(y_i - y_j) * (y_i - y_j) <= (ll)r_i * r_i) graph[i].push_back(j);
             }
         }
         
-        int ans = 0;
-        unordered_set<int> d;
-        for (int b0 = 0; b0 < n; ++b0){
-            if (d.find(b0) != d.end() or d.size() == n) continue;
-            
-            queue<int> queue{{b0}};
-            unordered_set<int> v{{b0}};
-            while (!queue.empty()){
-                int b1 = queue.front();
-                queue.pop();
-                d.insert(b1);
-                for (int b2 : adj[b1]){
-                    if (v.find(b2) == v.end()){
-                        v.insert(b2);
-                        queue.push(b2);
-                    }
-                }
-            }
-            int cnt = v.size();
-            ans = max(ans, cnt);
-            
+        for (int i = 0; i < n; ++i){
+            unordered_set<int> visited;
+            ans = max(ans, dfs(i, visited, graph));
         }
         
         return ans;
