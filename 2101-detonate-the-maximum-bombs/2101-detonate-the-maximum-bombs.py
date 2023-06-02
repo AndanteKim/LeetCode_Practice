e04a48879c15381a4f67f805ba9e9fef554f8590
Andrew Kim <ksb0724@utexas.edu>
2023-06-02 22:24:46 +0900

    Time: 821 ms (57.30%), Space: 16.6 MB (39.23%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,9 +1,15 @@
 class Solution:
-    def dfs(self, curr: int, visited: Set[int], graph: DefaultDict[int, List[int]]) -> int:
-        visited.add(curr)
-        for neighbor in graph[curr]:
-            if neighbor not in visited:
-                self.dfs(neighbor, visited, graph)
+    def dfs(self, node: int, visited: Set[int], graph: DefaultDict[int, List[int]]) -> int:
+        stack = [node]
+        visited.add(node)
+        
+        while stack:
+            curr = stack.pop()
+            for neighbor in graph[curr]:
+                if neighbor not in visited:
+                    visited.add(neighbor)
+                    stack.append(neighbor)
+                    
         return len(visited)
     
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
@@ -14,9 +20,9 @@ class Solution:
             for j in range(n):
                 if i == j:
                     continue
-                
+                    
                 xi, yi, ri = bombs[i]
-                xj, yj, _ = bombs[j]
+                xj, yj, rj = bombs[j]
                 
                 if (xi - xj) ** 2 + (yi - yj) ** 2 <= ri ** 2:
                     graph[i].append(j)
@@ -25,6 +31,5 @@ class Solution:
         for i in range(n):
             visited = set()
             ans = max(ans, self.dfs(i, visited, graph))
-        
+            
         return ans
-        
