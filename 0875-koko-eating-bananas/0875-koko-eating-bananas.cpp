80628a30e11f8195221d48e930a20c7a0fc4072b
Andrew Kim <ksb0724@utexas.edu>
2023-06-03 22:39:53 +0900

    Time: 49 ms (69.12%), Space: 19 MB (46.17%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,10 +3,11 @@ typedef long long ll;
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        ll left = 1, right = *max_element(piles.begin(), piles.end());
+        int left = 1, right = *max_element(piles.begin(), piles.end());
         
         while (left <= right){
-            ll mid = left + (right - left) / 2, time = 0;
+            int mid = left + (right - left) / 2;
+            ll time = 0;
             for (double pile : piles) time += ceil(pile / mid);
             if (time <= h) right = mid - 1;
             else left = mid + 1;
