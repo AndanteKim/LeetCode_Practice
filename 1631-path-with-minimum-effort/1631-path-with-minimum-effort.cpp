a215c9e12a0e1e4ff74d38e82eaae3429550190a
Andrew Kim <ksb0724@utexas.edu>
2023-06-05 00:00:51 +0900

    Time: 244 ms (29.79%), Space: 35 MB (23.19%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,55 +1,76 @@
-class Cell{
+class Edge{
 public:
-    int x, y;
-    int diff;
-    Cell(int x, int y, int diff){
+    int x, y, diff;
+    
+    Edge(int x, int y, int diff){
         this -> x = x;
         this -> y = y;
         this -> diff = diff;
     }
+    
 };
 
-struct Comparator{
-    bool operator()(Cell const &p1, Cell const &p2){
-        return p2.diff < p1.diff;
+class UnionFind{
+public:
+    vector<int> parent, rank;
+    vector<Edge> edgeList;
+
+    UnionFind(vector<vector<int>>& heights){
+        int row = heights.size(), col = heights[0].size();
+        parent.assign(row * col, 0);
+        rank.assign(row * col, 0);
+        
+        for (int currRow = 0; currRow < row; ++currRow){
+            for (int currCol = 0; currCol < col; ++currCol){
+                if (currRow > 0){
+                    edgeList.push_back(Edge(currRow * col + currCol, (currRow - 1) * col + currCol, abs(heights[currRow][currCol] - heights[currRow - 1][currCol])));
+                }
+                if (currCol > 0){
+                    edgeList.push_back(Edge(currRow * col + currCol, currRow * col + currCol - 1, abs(heights[currRow][currCol] - heights[currRow][currCol - 1])));
+                }
+                
+                parent[currRow * col + currCol] = currRow * col + currCol;
+            }
+        }
+    }
+    
+    int find(int i){
+        if (parent[i] != i) parent[i] = find(parent[i]);
+        return parent[i];
+    }
+    
+    void UnionSet(int x, int y){
+        int parentX = find(x), parentY = find(y);
+        if (parentX != parentY){
+            if (rank[parentX] > rank[parentY]) parent[parentY] = parentX;
+            else if (rank[parentX] < rank[parentY]) parent[parentX] = parentY;
+            else{
+                parent[parentY] = parentX;
+                ++rank[parentX];
+            }
+        }
     }
 };
 
 class Solution {
-private:
-    bool isValidCell(int x, int y, int row, int col){
-        return x >= 0 && x < row && y >= 0 && y < col;
-    }
-    
+    static bool compareInterval(const Edge& p1, const Edge& p2){
+    return (p1.diff < p2.diff);
+}
+
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
         int row = heights.size(), col = heights[0].size();
-        vector<vector<int>> diffMat(row, vector<int>(col, INT_MAX));
-        diffMat[0][0] = 0;
-        vector<vector<bool>> visited(row, vector<bool>(col));
-        priority_queue<Cell, vector<Cell>, Comparator> queue;
-        queue.push(Cell(0, 0, diffMat[0][0]));
+        if (row == 1 && col == 1) return 0;
+        UnionFind unionFind(heights);
+        vector<Edge> edgeList = unionFind.edgeList;
+        sort(edgeList.begin(), edgeList.end(), compareInterval);
         
-        while (!queue.empty()){
-            Cell curr = queue.top();
-            queue.pop();
-            visited[curr.x][curr.y] = true;
-            if (curr.x == row - 1 && curr.y == col - 1) return curr.diff;
-            for (auto& [dx, dy] : vector<pair<int, int>>{{1, 0}, {0, 1}, {-1, 0}, {0, -1}}){
-                int adjX = curr.x + dx, adjY = curr.y + dy;
-                if (isValidCell(adjX, adjY, row, col) && !visited[adjX][adjY]){
-                    int currDiff = abs(heights[adjX][adjY] - heights[curr.x][curr.y]);
-                    int maxDiff = max(currDiff, diffMat[curr.x][curr.y]);
-                    
-                    if (diffMat[adjX][adjY] > maxDiff){
-                        diffMat[adjX][adjY] = maxDiff;
-                        queue.push(Cell(adjX, adjY, maxDiff));
-                    }
-                }
-            }
+        for (int i = 0; i < edgeList.size(); ++i){
+            int x = edgeList[i].x, y = edgeList[i].y;
+            unionFind.UnionSet(x, y);
+            if (unionFind.find(0) == unionFind.find(row * col - 1)) return edgeList[i].diff;
         }
         
-        return diffMat[row - 1][col - 1];
+        return -1;
     }
 };
-
