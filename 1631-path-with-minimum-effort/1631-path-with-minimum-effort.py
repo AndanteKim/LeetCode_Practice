c8bb7fb02af165bedcdd4f8d71690b6d453ab7e2
Andrew Kim <ksb0724@utexas.edu>
2023-06-04 22:52:59 +0900

    Time: 692 ms (80.76%), Space: 17.8 MB (57.72%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,35 +1,23 @@
 class Solution:
-    def valid(self, x: int, y: int) -> bool:
-        return 0 <= x < self.r and 0 <= y < self.c
-    
-    def dfs(self, k: int, heights: List[List[int]]) -> bool:
-        directions = ((1, 0), (-1, 0), (0, -1), (0, 1))
-        stack = [(0, 0)]
-        visited = [[False] * self.c for _ in range(self.r)]
-        
-        while stack:
-            row, col = stack.pop()
-            if row == self.r - 1 and col == self.c - 1:
-                return True
-            
-            for dr, dc in directions:
-                new_r, new_c = row + dr, col + dc
-                if self.valid(new_r, new_c) and not visited[new_r][new_c]:
-                    if (abs(heights[new_r][new_c] - heights[row][col]) <= k):
-                        visited[new_r][new_c] = True
-                        stack.append((new_r, new_c))
-                        
-        return False
-    
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        self.r, self.c = len(heights), len(heights[0])
-        left, right = 0, max(max(row) for row in heights)
+        row, col = len(heights), len(heights[0])
+        diff_mat = [[float('inf')] * col for _ in range(row)]
+        diff_mat[0][0] = 0
+        visited = [[False] * col for _ in range(row)]
         
-        while left <= right:
-            mid = (left + right) // 2
-            if (self.dfs(mid, heights)):
-                right = mid - 1
-            else:
-                left = mid + 1
+        queue = [(0, 0, 0)]
+        while queue:
+            diff, r, c = heappop(queue)
+            visited[r][c] = True
+            for dr, dc in ((0, 1), (1, 0), (0, -1), (-1, 0)):
+                new_r = r + dr
+                new_c = c + dc
+                if 0 <= new_r < row and 0 <= new_c < col and not visited[new_r][new_c]:
+                    curr_diff = abs(heights[new_r][new_c] - heights[r][c])
+                    max_diff = max(curr_diff, diff_mat[r][c])
+                    
+                    if diff_mat[new_r][new_c] > max_diff:
+                        diff_mat[new_r][new_c] = max_diff
+                        heappush(queue, (max_diff, new_r, new_c))
+        return diff_mat[-1][-1]
         
-        return left
