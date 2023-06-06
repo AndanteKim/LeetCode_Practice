d820ab8a757658a10723d601a53372ff28a251f2
Andrew Kim <ksb0724@utexas.edu>
2023-06-07 01:49:40 +0900

    Time: 85 ms, Memory: 7.6 MB - LeetHub

--- split-array-largest-sum/split-array-largest-sum.cpp
+++ split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,30 +1,39 @@
 class Solution {
+private:
+    int memo[1001][51];
+    
 public:
     int splitArray(vector<int>& nums, int k) {
-        int left = *max_element(nums.begin(), nums.end()), right = accumulate(nums.begin(), nums.end(), 0), ans = 0;
+        memset(memo, -1, sizeof(memo));
         
-        while (left <= right){
-            int currSum = 0, maxSplit = 0;
-            int mid = left + (right - left) / 2;
-            for (int num : nums){
-                if (currSum + num <= mid){
-                    currSum += num;
+        int n = nums.size();
+        vector<int> prefixSum(n + 1, 0);
+        for (int i = 0; i < n; ++i){
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        
+        for (int subarrCnt = 1; subarrCnt <= k; ++subarrCnt){
+            for (int currIndex = 0; currIndex < n; ++currIndex){
+                if (subarrCnt == 1) {
+                    memo[currIndex][subarrCnt] = prefixSum[n] - prefixSum[currIndex];
+                    continue;
                 }
-                else{
-                    currSum = num;
-                    ++maxSplit;
+                
+                int minLargestSplitSum = prefixSum[n];
+                for (int i = currIndex; i <= n - subarrCnt; ++i){
+                    int firstSplitSum = prefixSum[i + 1] - prefixSum[currIndex];
+                    int largestSplitSum = max(firstSplitSum, memo[i + 1][subarrCnt - 1]);
+                    minLargestSplitSum = min(minLargestSplitSum, largestSplitSum);
+                    
+                    if (firstSplitSum >= minLargestSplitSum) {
+                        break;
+                    }
                 }
-            }
-            
-            if (maxSplit + 1 <= k){
-                right = mid - 1;
-                ans = mid;
-            }
-            else{
-                left = mid + 1;
+                
+                memo[currIndex][subarrCnt] = minLargestSplitSum;
             }
         }
         
-        return ans;
+        return memo[0][k];
     }
 };
