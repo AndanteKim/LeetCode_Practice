3f01dcd4917db5614b7c016528197c83f901e684
Andrew Kim <ksb0724@utexas.edu>
2023-06-09 01:54:58 +0900

    Time: 1156 ms (10.58%), Space: 85.1 MB (50.79%) - LeetHub

---
+++ 2250-count-number-of-rectangles-containing-each-point/2250-count-number-of-rectangles-containing-each-point.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> countRectangles(vector<vector<int>>& rectangles, vector<vector<int>>& points) {
+        vector<int> ans;
+        int m = rectangles.size(), n = rectangles[0].size();
+        unordered_map<int, vector<int>> rects;
+        
+        for (vector<int>& rectangle : rectangles){
+            rects[rectangle[1]].push_back(rectangle[0]);
+        }
+        
+        for (auto& [height, lengths] : rects){
+            sort(lengths.begin(), lengths.end());
+        }
+        
+        for (vector<int>& pt : points){
+            int curr = 0;
+            int x_i = pt[0], y_i = pt[1];
+            
+            for (auto& [height, lengths] : rects){    
+                if (height >= y_i){
+                    int left = 0, right = lengths.size();
+                    
+                    while (left < right){
+                        int mid = left + (right - left) / 2;
+                        if (x_i <= rects[height][mid]) right = mid;
+                        else left = mid + 1;
+                    }
+                    curr += lengths.size() - left;
+                }
+            }
+            
+            ans.push_back(curr);
+        }
+        
+        return ans;
+    }
+};
