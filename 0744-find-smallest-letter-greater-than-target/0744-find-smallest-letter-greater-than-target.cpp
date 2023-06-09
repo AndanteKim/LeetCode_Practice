754580a3dc156ab393fd3185dd717989e0362857
Andrew Kim <ksb0724@utexas.edu>
2023-06-09 18:23:37 +0900

    Time: 13 ms (84.16%), Space: 15.8 MB (96.78%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        vector<int> alphabets(26, 0);
-        char ans = letters[0];
-        for (char& letter : letters) ++alphabets[letter - 'a'];
-        
-        for (int i = target - 'a' + 1; i < 26; ++i){
-            if (alphabets[i]) return (char) (i + 'a');
+        for (char& letter : letters){
+            if (letter > target) return letter; 
         }
         
-        return ans;
+        return letters[0];
     }
 };
