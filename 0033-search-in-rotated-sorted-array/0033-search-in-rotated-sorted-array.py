7b87754254cd3813770c13e4decba080c92f99fc
Andrew Kim <ksb0724@utexas.edu>
2023-06-11 01:25:12 +0900

    Time: 57 ms (43.87%), Space: 16.7 MB (55.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,47 +1,20 @@
 class Solution:
-    def find_rotate_index(self, left: int, right: int, nums: List[int]) -> int:
-        if nums[left] < nums[right]:
-            return 0
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums) - 1
         
-        while left <= right:
-            mid = (left + right) // 2
-            if nums[mid] > nums[mid + 1]:
-                return mid + 1
-            else:
-                if nums[mid] < nums[left]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-                    
-        return -1
-                    
-    def partial_search(self, left: int, right: int, nums: List[int], target: int) -> int:
-        while left <= right:
-            mid = (left + right) // 2
-            
+        while start <= end:
+            mid = (start + end) // 2
             if nums[mid] == target:
                 return mid
-            
-            else:
-                if target < nums[mid]:
-                    right = mid - 1
+            elif nums[mid] >= nums[start]:
+                if target >= nums[start] and target < nums[mid]:
+                    end = mid - 1
                 else:
-                    left = mid + 1
+                    start = mid + 1
+            else:
+                if target <= nums[end] and target > nums[mid]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        
         return -1
-    
-    def search(self, nums: List[int], target: int) -> int:
-        n = len(nums)
-        
-        if n == 1:
-            return 0 if nums[0] == target else -1
-        
-        rotate_index = self.find_rotate_index(0, n - 1, nums)
-        
-        if nums[rotate_index] == target:
-            return rotate_index
-        
-        if rotate_index == 0:
-            return self.partial_search(0, n - 1, nums, target)
-        if target < nums[0]:
-            return self.partial_search(rotate_index, n - 1, nums, target)
-        return self.partial_search(0, rotate_index, nums, target)
