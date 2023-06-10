ed20ab4f948eadaedfce92c998657b5a7b25c49d
Andrew Kim <ksb0724@utexas.edu>
2023-06-11 01:34:13 +0900

    Time: 3 ms (79.41%), Space: 11.1 MB (58.45%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,45 +1,22 @@
 class Solution {
-    int findRotateIndex(int left, int right, vector<int>& nums){
-        if (nums[left] < nums[right]) return 0;
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
         
-        while (left <= right){
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > nums[mid + 1]) return mid + 1;
-            else{
-                if (nums[mid] < nums[left]) right = mid - 1;
-                else left = mid + 1;
-            }
-        }
-        return 0;
-    }
-    
-    int partialSearch(int left, int right, vector<int>& nums, int target){
         while (left <= right){
             int mid = left + (right - left) / 2;
             
             if (nums[mid] == target) return mid;
-            else{
-                if (target < nums[mid]) right = mid - 1;
+            else if (nums[mid] >= nums[left]){
+                if (target >= nums[left] && target < nums[mid]) right = mid - 1;
                 else left = mid + 1;
+            }
+            else{
+                if (target <= nums[right] && target > nums[mid]) left = mid + 1;
+                else right = mid - 1;
             }
         }
         
         return -1;
-    }
-    
-    
-public:
-    int search(vector<int>& nums, int target) {
-        int n = nums.size();
-        if (n == 1){
-            return (nums[0] == target)? 0 : -1;
-        }
-        
-        int rotateIndex = findRotateIndex(0, n - 1, nums);
-        if (nums[rotateIndex] == target) return rotateIndex;
-        if (rotateIndex == 0) return partialSearch(0, n - 1, nums, target);
-        if (target < nums[0]) return partialSearch(rotateIndex, n - 1, nums, target);
-        return partialSearch(0, rotateIndex, nums, target);
     }
 };
