16adce1dfb79c2816cdd649d162b828c165bc5f3
Andrew Kim <ksb0724@utexas.edu>
2023-06-11 23:30:04 +0900

    Time: 1069 ms (81.42%), Space: 43.6 MB (63.72%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -1,5 +1,29 @@
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        bloom_start, bloom_end = sorted(start for start, end in flowers), sorted(end for start, end in flowers)
+        positions = set()
         
-        return [bisect_right(bloom_start, time) - bisect_left(bloom_end, time) for time in people]
+        for start, end in flowers:
+            positions.add(start)
+            positions.add(end + 1)
+            
+        positions = sorted(list(positions))
+        
+        idx = {positions[i] : i for i in range(len(positions))}
+        tmp = [0] * (1 + len(positions))
+        
+        for start, end in flowers:
+            tmp[idx[start]] += 1
+            tmp[idx[end + 1]] -= 1
+            
+        prefix_sum = [0] * (1 + len(positions))
+        
+        for i in range(len(positions)):
+            prefix_sum[i + 1] = prefix_sum[i] + tmp[i]
+        
+        ans = []
+        
+        for person in people:
+            idx = bisect_right(positions, person)
+            ans.append(prefix_sum[idx])
+        
+        return ans
