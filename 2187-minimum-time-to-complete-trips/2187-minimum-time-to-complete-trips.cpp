bd871b3fac28eb6f188066dd362fbf26aa0a9434
Andrew Kim <ksb0724@utexas.edu>
2023-06-12 00:40:26 +0900

    Time: 393 ms (54.02%), Space: 94.4 MB (98.52%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -1,18 +1,14 @@
+typedef long long ll;
+
 class Solution {
 public:
     long long minimumTime(vector<int>& time, int totalTrips) {
-        if (time.size() == 1) return 1LL * totalTrips * time[0];
-        
-        long long left = 1, right = 1LL * totalTrips * *min_element(time.begin(), time.end()), mid, actual_trips;
+        ll left = 1, right = totalTrips * (ll)*max_element(time.begin(), time.end());
         
         while (left < right){
-            actual_trips = 0;
-            mid = left + (right - left) / 2;
-            for (int t : time){
-                actual_trips += mid / t;
-            }
-            
-            if (actual_trips >= totalTrips) right = mid;
+            ll mid = left + ((right - left) >> 1), trips = 0;
+            for (int t : time) trips += mid / t;
+            if (totalTrips <= trips) right = mid;
             else left = mid + 1;
         }
         
