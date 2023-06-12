5ba6f4c62dfa9480228239ad46df5f1beff199b8
Andrew Kim <ksb0724@utexas.edu>
2023-06-12 23:46:38 +0900

    Time: 1643 ms (50.35%), Space: 28.2 MB (75.35%) - LeetHub

--- 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
+++ 2517-maximum-tastiness-of-candy-basket/2517-maximum-tastiness-of-candy-basket.py
@@ -1,25 +1,19 @@
 class Solution:
-    def check(self, x: int, price: List[int], k: int, n: int) -> bool:
-        last, cnt, i = price[0], 1, 1
-        while cnt < k and i < n:
-            if price[i] - last >= x:
-                last, cnt = price[i], cnt + 1
-            i += 1
-        return cnt == k
-
     def maximumTastiness(self, price: List[int], k: int) -> int:
         price.sort()
-        
-        left, right, n = 0, 10 ** 9, len(price)
+        left, right, n = 0, price[-1] - price[0], len(price)
         
         while left < right:
-            mid = (left + right) >> 1
-            if self.check(mid, price, k, n):
-                left = mid + 1
+            mid = (left + right + 1) >> 1
+            cnt, j = 1, 0
+            for i in range(1, n):
+                if price[i] - price[j] >= mid:
+                    cnt += 1
+                    j = i
+            if cnt >= k:
+                left = mid
             else:
-                right = mid
+                right = mid - 1
         
-        return left - 1
+        return left
             
-            
-                
