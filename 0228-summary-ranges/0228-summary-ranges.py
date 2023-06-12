9e93104174753b5f66b804b7d64db4ef8129137e
Andrew Kim <ksb0724@utexas.edu>
2023-06-12 15:54:31 +0900

    Time: 46 ms (47.11%), Space: 16.2 MB (81.48%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,26 +1,17 @@
 class Solution:
     def summaryRanges(self, nums: List[int]) -> List[str]:
-        if not nums:
-            return []
-        interval, ans = [nums[0], nums[0]], []
-        n, i = len(nums), 1
-        if n == 1:
-            return [f'{nums[0]}']
+        ans, n, i = [], len(nums), 0
         
-        for i in range(1, n):
-            if nums[i] - nums[i - 1] == 1:
-                interval[1] += 1
+        while i < n:
+            start = nums[i]
+            while i + 1 < n and nums[i] + 1 == nums[i + 1]:
+                i += 1
+            
+            if start != nums[i]:
+                ans.append(f'{start}->{nums[i]}')
             else:
-                if interval[0] == interval[1]:
-                    ans.append(f'{interval[0]}')
-                else:
-                    ans.append(f'{interval[0]}->{interval[1]}')
-                interval[0], interval[1] = nums[i], nums[i]
-            
-            if i == n - 1:
-                if interval[0] == interval[1]:
-                    ans.append(f'{interval[0]}')
-                else:
-                    ans.append(f'{interval[0]}->{interval[1]}')
-            
+                ans.append(f'{nums[i]}')
+        
+            i += 1
+        
         return ans
