36324ffbbc1ecb6a2b83c9e7194fbbbf53b5c0c8
Andrew Kim <ksb0724@utexas.edu>
2023-06-14 01:25:42 +0900

    Time: 192 ms (43.86%), Space: 81.1 MB (8.08%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -1,25 +1,50 @@
+class Trie{
+private:
+    struct TrieNode{
+        int count = 0;
+        unordered_map<int, TrieNode*> children;
+    };
+    TrieNode *trie = new TrieNode();
+public:
+    Trie(){}
+    
+    void insert(vector<int>& arr){
+        TrieNode *myTrie = trie;        
+        
+        for (int num : arr){
+            if (myTrie -> children.find(num) == myTrie -> children.end()){
+                myTrie -> children[num] = new TrieNode();
+            }
+            myTrie = myTrie -> children[num];
+        }
+        ++myTrie -> count;
+    }
+    
+    int search(vector<int>& arr){
+        TrieNode *myTrie = trie;
+        
+        for (int num : arr){
+            if (myTrie -> children.find(num) != myTrie -> children.end()) myTrie = myTrie -> children[num];
+            else return 0;
+        }
+        return myTrie -> count;
+    }
+};
+
 class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
-        unordered_map<string, int> cols;
-        int ans = 0;
-        for (int i = 0; i < grid.size(); ++i){
-            string key = "";
-            for (int j = 0; j < grid[0].size(); ++j){
-                key += to_string(grid[j][i]) + " ";
-            }
-            cols[key] += 1;
+        Trie* myTrie = new Trie();
+        int count = 0, n = grid.size();
+        
+        for (vector<int>& row : grid) myTrie -> insert(row);
+        
+        for (int col = 0; col < n;++col){
+            vector<int> cols(n, 0);
+            for (int row = 0; row < n; ++row) cols[row] = grid[row][col];
+            count += myTrie -> search(cols);
         }
         
-        for (vector<int>& row : grid){
-            string key = "";
-            for (int col : row){
-                key += to_string(col) + " ";
-            }
-            
-            ans += cols[key];
-        }
-        
-        return ans;
+        return count;
     }
 };
