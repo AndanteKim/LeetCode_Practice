b1928408d507ec17330eb42aca9dd77060a47be1
Andrew Kim <ksb0724@utexas.edu>
2023-06-13 18:11:22 +0900

    Time: 657 ms (33.78%), Space: 33.1 MB (5.10%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,10 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.count = 0
+        self.children = defaultdict(dict)
+        
+class Trie:
+    def __init__(self):
+        self.trie = TrieNode()
+    
+    def insert(self, arr: List[int]) -> None:
+        my_trie = self.trie
+        for num in arr:
+            if num not in my_trie.children:
+                my_trie.children[num] = TrieNode()
+            my_trie = my_trie.children[num]
+        my_trie.count += 1
+    
+    def search(self, arr: List[int]) -> int:
+        my_trie = self.trie
+        for num in arr:
+            if num in my_trie.children:
+                my_trie = my_trie.children[num]
+            else:
+                return 0
+        return my_trie.count
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        cols, ans = defaultdict(int), 0
-        for col in zip(*grid):
-            cols[col] += 1
+        my_trie, count = Trie(), 0
+        n = len(grid)
         
         for row in grid:
-            ans += cols[tuple(row)]
+            my_trie.insert(row)
         
-        return ans
+        for c in range(n):
+            cols = [grid[i][c] for i in range(n)]
+            count += my_trie.search(cols)
+        
+        return count
+        
