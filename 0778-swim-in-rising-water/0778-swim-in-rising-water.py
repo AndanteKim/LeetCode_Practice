629fc7ba32329710b61874ae38caa75502be2bc8
Andrew Kim <ksb0724@utexas.edu>
2023-06-15 00:41:57 +0900

    Time: 145 ms (27.09%), Space: 16.9 MB (58.06%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,16 +1,40 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.parents = list(range(n ** 2))
+        self.size = [1] * (n ** 2)
+    
+    def find(self, x: int) -> int:
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
+    def union(self, x: int, y: int) -> None:
+        xset, yset = self.find(x), self.find(y)
+        
+        if xset == yset:
+            return
+        if self.size[xset] > self.size[yset]:
+            self.parents[yset] = xset
+        elif self.size[xset] == self.size[yset]:
+            self.parents[yset] = xset
+            self.size[xset] += 1
+        else:
+            self.parents[xset] = yset
+        
+
 class Solution:
     def swimInWater(self, grid: List[List[int]]) -> int:
         n = len(grid)
-        seen = {(0, 0)}
-        pq, ans = [(grid[0][0], 0, 0)], 0
+        visited = [[False] * n for _ in range(n)]
+        positions = sorted([(i, j) for i in range(n) for j in range(n)], key = lambda x: grid[x[0]][x[1]])
         
-        while pq:
-            dist, row, col = heappop(pq)
-            ans = max(ans, dist)
-            if row == col == n - 1:
-                return ans
-            for cr, cc in ((row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)):
-                if 0 <= cr < n and 0 <= cc < n and (cr, cc) not in seen:
-                    heappush(pq, (grid[cr][cc], cr, cc))
-                    seen.add((cr, cc))
+        uf = UnionFind(n);
+        for i, j in positions:
+            for x, y in (i + 1, j), (i - 1, j), (i, j - 1), (i, j + 1):
+                if 0 <= x < n and 0 <= y < n and visited[x][y]:
+                    uf.union(i * n + j, x * n + y)
+            
+            if uf.find(0) == uf.find(n * n - 1):
+                return grid[i][j]
+            visited[i][j] = True
         return -1
