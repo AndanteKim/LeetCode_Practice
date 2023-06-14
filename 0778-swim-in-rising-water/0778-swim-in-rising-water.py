49f66e64e2f55ed3ec56e8163c511f31a9421574
Andrew Kim <ksb0724@utexas.edu>
2023-06-14 23:45:26 +0900

    Time: 133 ms (36.38%), Space: 17 MB (58.06%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.py
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.py
@@ -1,31 +1,16 @@
 class Solution:
-    def valid(self, x: int, y: int, r: int, c: int, grid: List[List[int]], visited: List[List[bool]], threshold: int) -> bool:
-        return 0 <= x < r and 0 <= y < c and grid[x][y] <= threshold and not visited[x][y]
-    
-    def feasible(self, grid: List[List[int]], r:int, c: int, threshold: int) -> bool:
-        visited = [[False] * c for _ in range(r)]
-        queue = deque([(0, 0)])
-        directions = ((1, 0), (-1, 0), (0, -1), (0, 1))
-        
-        while queue:
-            x, y = queue.popleft()
-            for dx, dy in directions:
-                new_x, new_y = x + dx, y + dy
-                if self.valid(new_x, new_y, r, c, grid, visited, threshold):
-                    visited[new_x][new_y] = True
-                    queue.append((new_x, new_y))
-        
-        return visited[0][0] and visited[r - 1][c - 1]
-        
-    
     def swimInWater(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        left, right = 0, max(grid[r][c] for r in range(m) for c in range(n))
+        n = len(grid)
+        seen = {(0, 0)}
+        pq, ans = [(grid[0][0], 0, 0)], 0
         
-        while left < right:
-            mid = (left + right) >> 1
-            if self.feasible(grid, m, n, mid):
-                right = mid
-            else:
-                left = mid + 1
-        return left
+        while pq:
+            dist, row, col = heappop(pq)
+            ans = max(ans, dist)
+            if row == col == n - 1:
+                return ans
+            for cr, cc in ((row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1)):
+                if 0 <= cr < n and 0 <= cc < n and (cr, cc) not in seen:
+                    heappush(pq, (grid[cr][cc], cr, cc))
+                    seen.add((cr, cc))
+        return -1
