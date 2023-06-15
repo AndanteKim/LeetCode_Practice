438cbee8811e4ef8424979cd37256d02a71b7c2c
Andrew Kim <ksb0724@utexas.edu>
2023-06-16 02:14:23 +0900

    Time: 380 ms (78.21%), Space: 107.8 MB (79.33%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -1,46 +1,48 @@
-class Solution {
+class DSU{
 private:
-    int r, c;
-    bool valid(int x, int y, vector<vector<bool>>& visited){
-        return 0 <= x && x < r && 0 <= y && y < c && !visited[x][y];
-    }
-    
-    bool possible(int days, vector<vector<int>>& cells){
-        vector<vector<bool>> visited(r, vector<bool>(c, false));
-        for (int i = 0; i <= days; ++i) visited[cells[i][0] - 1][cells[i][1] - 1] = true;
-        
-        queue<pair<int, int>> queue;
-        for (int i = 0; i < c; ++i){
-            if (!visited[0][i]) queue.push({0, i});   
-        }
-        
-        while (!queue.empty()){
-            auto [x, y] = queue.front();
-            queue.pop();
-            if (x == r - 1) return true;
-            
-            for (auto& [newX, newY] : vector<pair<int, int>>{{x + 1, y}, {x - 1, y}, {x, y + 1}, {x, y - 1}}){
-                if (valid(newX, newY, visited)){
-                    visited[newX][newY] = true;
-                    queue.push({newX, newY});
-                }
-            }
-        }
-        
-        return false;
-    }
+    vector<int> parents, rank;
     
 public:
+    DSU(int n){
+        parents.resize(n);
+        iota(parents.begin(), parents.end(), 0);
+        rank.resize(n, 1);
+    }
+    
+    int find(int x){
+        if (parents[x] != x) parents[x] = find(parents[x]);
+        return parents[x];
+    }
+    
+    void UnionSet(int x, int y){
+        int xset = find(x), yset = find(y);
+        if (xset == yset) return;
+        if (rank[xset] > rank[yset]) swap(xset, yset);
+        parents[xset] = yset;
+        rank[yset] = rank[xset];
+    }
+};
+
+class Solution {
+public:
     int latestDayToCross(int row, int col, vector<vector<int>>& cells) {
-        r = row, c = col;
-        int left = 0, right = cells.size();
-        
-        while (left < right){
-            int mid = left + ((right - left) >> 1);
-            if (possible(mid, cells)) left = mid + 1;
-            else right = mid;
+        DSU dsu = DSU(row * col + 2);
+        vector<vector<int>> grid(row, vector<int>(col, 1));
+        vector<pair<int, int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (int i = cells.size() - 1; i >= 0; --i){
+            int r = cells[i][0] - 1, c = cells[i][1] - 1;
+            grid[r][c] = 0;
+            int idx1 = r * col + c + 1;
+            for (auto& [dr, dc] : directions){
+                int newR = r + dr, newC = c + dc;
+                int idx2 = newR * col + newC + 1;
+                if (0 <= newR && newR < row && 0 <= newC && newC < col && grid[newR][newC] == 0) dsu.UnionSet(idx1, idx2);
+            }
+            if (r == 0) dsu.UnionSet(0, idx1);
+            if (r == row - 1) dsu.UnionSet(row * col + 1, idx1);
+            if (dsu.find(0) == dsu.find(row * col + 1)) return i;
         }
         
-        return left;
+        return -1;
     }
 };
