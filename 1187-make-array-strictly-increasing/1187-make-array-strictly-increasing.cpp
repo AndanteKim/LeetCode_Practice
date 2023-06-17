96461f6820ca7077ee1c1466443442b28c80662f
Andrew Kim <ksb0724@utexas.edu>
2023-06-17 23:40:33 +0900

    Time: 651 ms (81.54%), Space: 16.7 MB (74.62%) - LeetHub

--- 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
+++ 1187-make-array-strictly-increasing/1187-make-array-strictly-increasing.cpp
@@ -1,24 +1,18 @@
-typedef long l;
-
-class Solution {
-private:
-    l dfs(int i, int prev, map<pair<int, int>, l>& dp, vector<int>& arr1, vector<int>& arr2){
-        if (i == arr1.size()) return 0;
-        if (dp.find({i, prev}) != dp.end()) return dp[{i, prev}];
+class Solution:
+    def makeArrayIncreasing(self, arr1: List[int], arr2: List[int]) -> int:
+        dp = {-1: 0}
+        arr2.sort()
+        n = len(arr2)
         
-        long cost = INT_MAX;
-        if (arr1[i] > prev) cost = dfs(i + 1, arr1[i], dp, arr1, arr2);
-        int idx = upper_bound(arr2.begin(), arr2.end(), prev) - arr2.begin();
-        if (idx < arr2.size()) cost = min(cost, 1 + dfs(i + 1, arr2[idx], dp, arr1, arr2));
+        for i in range(len(arr1)):
+            new_dp = defaultdict(lambda: float('inf'))
+            for prev in dp:
+                if arr1[i] > prev:
+                    new_dp[arr1[i]] = min(new_dp[arr1[i]], dp[prev])
+                idx = bisect_right(arr2, prev)
+                if idx < n:
+                    new_dp[arr2[idx]] = min(new_dp[arr2[idx]], 1 + dp[prev])
+            dp = new_dp
         
-        return dp[{i, prev}] = cost;
-    }
-    
-public:
-    int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
-        sort(arr2.begin(), arr2.end());
-        map<pair<int, int>, l> dp;
-        int ans = dfs(0, -1, dp, arr1, arr2);
-        return (ans < INT_MAX)? ans : -1;
-    }
-};
+        return min(dp.values()) if dp else -1
+                
