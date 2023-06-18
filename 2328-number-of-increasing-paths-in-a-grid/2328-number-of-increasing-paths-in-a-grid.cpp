9728d1624f0a3290d95e2de54497f0ad8540403d
Andrew Kim <ksb0724@utexas.edu>
2023-06-18 18:04:46 +0900

    Time: 331 ms (38.59%), Space: 43.3 MB (88.17%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,43 +1,33 @@
 class Solution {
+private:
+    int m, n, mod = 1'000'000'007;
+    int dfs(int i, int j, vector<vector<int>>& dp, vector<vector<int>>& grid){
+        if (dp[i][j]) return dp[i][j];
+        
+        int ans = 1;
+        int d[5] = {-1, 0, 1, 0, -1};
+        for (int k = 0; k < 4; ++k){
+            int ii = i + d[k], jj = j + d[k + 1];
+            if (0 <= ii && ii < m && 0 <= jj && jj < n && grid[ii][jj] < grid[i][j]){
+                ans += dfs(ii, jj, dp, grid);
+                ans %= mod;
+            }
+        }
+        return dp[i][j] = ans;
+    }
+    
 public:
     int countPaths(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size(), mod = 1'000'000'007;
-        int d[5] = {-1, 0, 1, 0, -1};
-        vector<vector<int>> indeg(m, vector<int>(n));
-        for (int i = 0; i < m; ++i){
-            for (int j = 0; j < n; ++j){
-                for (int k = 0; k < 4; ++k){
-                    int ii = i + d[k], jj = j + d[k + 1];
-                    if (0 <= ii && ii < m && 0 <= jj && jj < n && grid[i][j] < grid[ii][jj])
-                        ++indeg[i][j];
-                }
-            }
-        }
-        
-        queue<pair<int, int>> queue;
-        for (int i = 0; i < m; ++i){
-            for (int j = 0; j < n; ++j){
-                if (indeg[i][j] == 0) queue.emplace(i, j);
-            }
-        }
-        
+        m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
         int ans = 0;
-        vector<vector<int>> dp(m, vector<int>(n, 1));
-        while (!queue.empty()){
-            auto [i, j] = queue.front();
-            queue.pop();
-            ans = (ans + dp[i][j]) % mod;
-            for (int k = 0; k < 4; ++k){
-                int ii = i + d[k], jj = j + d[k + 1];
-                if (0 <= ii && ii < m && 0 <= jj && jj < n){
-                    if (grid[i][j] > grid[ii][jj]){
-                        dp[ii][jj] = (dp[i][j] + dp[ii][jj]) % mod;
-                        if (--indeg[ii][jj] == 0) queue.emplace(ii, jj);
-                    }
-                }
+        
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j){
+                ans = (ans + dfs(i, j, dp, grid)) % mod;
             }
         }
-        
+            
         return ans;
     }
 };
