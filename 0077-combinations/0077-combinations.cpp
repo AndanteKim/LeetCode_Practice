d3ce19b1b8ddd5a19234e18eef67d0df547889ab
Andrew Kim <ksb0724@utexas.edu>
2023-06-19 01:06:30 +0900

    Time: 27 ms (75.73%), Space: 8.9 MB (86.06%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,24 @@
+class Solution {
+private:
+    void backtrack(int start, int n, int k, vector<int>& curr, vector<vector<int>>& ans){
+        if (curr.size() == k) {
+            ans.push_back(curr);
+            return;
+        }
+        
+        for (int i = start; i < n; ++i){
+            curr.push_back(i + 1);
+            backtrack(i + 1, n, k, curr, ans);
+            curr.pop_back();
+        }
+    }
+    
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        backtrack(0, n, k, curr, ans);
+        
+        return ans;
+    }
+};
