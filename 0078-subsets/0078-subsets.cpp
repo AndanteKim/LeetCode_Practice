1af31f1e83fa1d1bc3d72ac2c8f2a273c2603d9e
Andrew Kim <ksb0724@utexas.edu>
2023-06-18 23:36:10 +0900

    Time: 0 ms (100.00%), Space: 6.9 MB (93.48%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,22 +1,19 @@
 class Solution {
-private:
-    int n;
-    void backtrack(int start, vector<int>& curr, vector<int>& nums, vector<vector<int>>& ans){
-        if (start > n) return;
-        ans.push_back(curr);
-        for (int i = start; i < n; ++i){
-            curr.push_back(nums[i]);
-            backtrack(i + 1, curr, nums, ans);
-            curr.pop_back();
-        }
-    }
-    
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        n = nums.size();
-        vector<int> curr;
-        vector<vector<int>> ans;
-        backtrack(0, curr, nums, ans);
+        int n = nums.size();
+        vector<vector<int>> ans{{}};
+        if (n == 0) return ans;
+        
+        for (int i = 0; i < n; ++i){
+            int sz = ans.size();
+            for (int j = 0; j < sz; ++j){
+                vector<int> s(ans[j]);
+                s.push_back(nums[i]);
+                ans.push_back(s);
+            }
+        }
+        
         return ans;
     }
 };
