93d994494ad654f1824ca5a446ad3245745e7387
Andrew Kim <ksb0724@utexas.edu>
2023-06-19 01:28:14 +0900

    Time: 4 ms (99.62%), Space: 9.5 MB (58.70%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,24 +1,21 @@
 class Solution {
-private:
-    void backtrack(int start, int n, int k, vector<int>& curr, vector<vector<int>>& ans){
-        if (curr.size() == k) {
-            ans.push_back(curr);
-            return;
-        }
-        
-        for (int i = start; i <= n; ++i){
-            curr.push_back(i);
-            backtrack(i + 1, n, k, curr, ans);
-            curr.pop_back();
-        }
-    }
-    
 public:
     vector<vector<int>> combine(int n, int k) {
+        vector<int> nums;
+        for (int i = 1; i <= k; ++i) nums.push_back(i);
+        nums.push_back(n + 1);
         vector<vector<int>> ans;
-        vector<int> curr;
-        backtrack(1, n, k, curr, ans);
-        
+        int j = 0;
+        while (j < k){
+            vector<int> curr(nums.begin(), nums.begin() + k);
+            ans.push_back(curr);
+            j = 0;
+            while (j < k && nums[j + 1] == nums[j] + 1){
+                nums[j] = j + 1;
+                ++j;
+            }
+            ++nums[j];
+        }
         return ans;
     }
 };
