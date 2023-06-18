ed8b68b0e40c409f97d69a23efd7f09260873f97
Andrew Kim <ksb0724@utexas.edu>
2023-06-19 00:19:06 +0900

    Time: 48 ms (60.57%), Space: 16.5 MB (70.82%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,9 +1,10 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        output = [[]]
+        ans = []
         
-        for num in nums:
-            output += [curr + [num] for curr in output]
+        for i in range(2 ** n, 2 ** (n + 1)):
+            bitmask = bin(i)[3:]
+            ans.append([nums[j] for j in range(n) if bitmask[j] == '1'])
         
-        return output
+        return ans
