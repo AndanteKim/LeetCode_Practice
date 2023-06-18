0a2e8856d604134d20642107d1dd497c8956521b
Andrew Kim <ksb0724@utexas.edu>
2023-06-18 17:46:58 +0900

    Time: 2141 ms (82.53%), Space: 29 MB (100.00%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.py
@@ -1,19 +1,19 @@
 class Solution:
+    def dfs(self, i: int, j: int, dp: List[List[int]], grid: List[List[int]]) -> int:
+        if dp[i][j]:
+            return dp[i][j]
+        
+        ans = 1
+        dirs = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        for di, dj in dirs:
+            ii, jj = i + di, j + dj
+            if 0 <= ii < self.m and 0 <= jj < self.n and grid[ii][jj] < grid[i][j]:
+                ans += self.dfs(ii, jj, dp, grid) % self.mod
+        dp[i][j] = ans
+        return ans
+    
     def countPaths(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        mod = 10 ** 9 + 7
-        dirs = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        self.m, self.n, self.mod = len(grid), len(grid[0]), 10 ** 9 + 7
         
-        dp = [[1] * n for _ in range(m)]
-        cell_list = [(i, j) for i in range(m) for j in range(n)]
-        cell_list.sort(key = lambda x: grid[x[0]][x[1]])
-        
-        for i, j in cell_list:
-            for di, dj in dirs:
-                curr_i, curr_j = i + di, j + dj
-                
-                if 0 <= curr_i < m and 0 <= curr_j < n and grid[curr_i][curr_j] > grid[i][j]:
-                    dp[curr_i][curr_j] += dp[i][j]
-                    dp[curr_i][curr_j] %= mod
-        
-        return sum(sum(row) % mod for row in dp) % mod
+        dp = [[0] * self.n for _ in range(self.m)]
+        return sum(self.dfs(i, j, dp, grid) for i in range(self.m) for j in range(self.n)) % self.mod
