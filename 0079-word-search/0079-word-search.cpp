f8ad1d323ca683e12553e2d732e78e0936de8bb0
Andrew Kim <ksb0724@utexas.edu>
2023-06-21 02:49:26 +0900

    Time: 0 ms (100.00%), Space: 7.9 MB (96.57%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,34 +1,36 @@
 class Solution {
 private:
-    int m, n;
-    bool valid(int r, int c){
-        return 0 <= r && r < m && 0 <= c && c < n;
-    }
-    
-    bool backtrack(int row, int col, int start, vector<vector<bool>>& seen, string& word, vector<vector<char>>& board){
-        if (start == word.size()) return true;
-        
-        for (auto& [n_r, n_c] : vector<pair<int, int>>{{row + 1, col}, {row - 1, col}, {row, col + 1}, {row, col - 1}}){
-            if (valid(n_r, n_c) && !seen[n_r][n_c]){
-                if (board[n_r][n_c] == word[start]){
-                    seen[n_r][n_c] = true;
-                    if (backtrack(n_r, n_c, start + 1, seen, word, board)) return true;
-                    seen[n_r][n_c] = false;
-                }
-            }
-        }
-        return false;
+    bool arr[7][7] = {0};
+    bool ans(vector<vector<char>>& board, int m, int n, string& s, int i, int j, int pos){
+        if (pos == s.size()) return true;
+        if (i >= m || i < 0 || j >= n || j < 0 || board[i][j] != s[pos] || arr[i][j] == 1) return false;
+        arr[i][j] = 1;
+        bool top = ans(board, m, n, s, i - 1, j, pos + 1);
+        bool down = ans(board, m, n, s, i + 1, j, pos + 1);
+        bool left = ans(board, m, n, s, i, j + 1, pos + 1);
+        bool right = ans(board, m, n, s, i, j - 1, pos + 1);
+        arr[i][j] = 0;
+        return top || down || left || right;
     }
     
 public:
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size(), n = board[0].size();
-        vector<vector<bool>> visited(m, vector<bool>(n));
-        for (int row = 0; row < m; ++row){
-            for (int col = 0; col < n; ++col){
-                visited[row][col] = true;
-                if (board[row][col] == word[0] && backtrack(row, col, 1, visited, word, board)) return true;
-                visited[row][col] = false;
+        int m = board.size(), n = board[0].size();
+        int first = 0, last = 0;
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j){
+                if (board[i][j] == word[0]) ++first;
+                else if (board[i][j] == word[word.size() - 1]) ++last; 
+            }
+        }
+        
+        if (first > last) reverse(word.begin(), word.end());
+        
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j){
+                if (board[i][j] == word[0]){
+                    if (ans(board, m, n, word, i, j, 0)) return true;
+                }
             }
         }
         
