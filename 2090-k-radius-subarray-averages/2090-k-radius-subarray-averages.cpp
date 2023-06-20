ab3977e8dfee235c07f63dfaf8d1ee9c10617e72
Andrew Kim <ksb0724@utexas.edu>
2023-06-20 18:07:47 +0900

    Time: 241 ms (85.74%), Space: 130 MB (59.59%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.cpp
@@ -3,17 +3,15 @@ typedef long long ll;
 class Solution {
 public:
     vector<int> getAverages(vector<int>& nums, int k) {
-        int radius = 2 * k + 1, n = nums.size(), left = 0;
-        ll total = 0;
+        int n = nums.size();
         vector<int> ans(n, -1);
+        if (2 * k + 1 > n) return ans;
+        ll windowSum = accumulate(nums.begin(), nums.begin() + 2 * k + 1, 0ll);
+        ans[k] = windowSum / (2 * k + 1);
         
-        for (int right = 0; right < nums.size(); ++right){
-            total += nums[right];
-            while (right - left >= radius - 1){
-                ans[k + left] = total / radius;
-                total -= nums[left];
-                ++left;
-            }
+        for (int i = 2 * k + 1; i < n; ++i){
+            windowSum = windowSum - nums[i - (2 * k + 1)] + nums[i];
+            ans[i - k] = windowSum / (2 * k + 1);
         }
         
         return ans;
