cc5c6d8b394c02d0ecb386ac2d48a11bb7bc97bc
Andrew Kim <ksb0724@utexas.edu>
2023-06-20 18:51:56 +0900

    Time: 59 ms (94.78%), Space: 16.2 MB (90.90%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,17 +1,17 @@
 class Solution:
-    def backtrack(self, prefix: int, curr: List[int], candidates: List[int], target: int, ans: List[List[int]]) -> None:
-        if prefix >= target:
-            if prefix == target:
-                ans.append(curr[:])
+    def backtrack(self, path: List[int], start: int, curr: int, candidates: List[int], target: int, ans: List[List[int]]) -> None:
+        if curr == target:
+            ans.append(path[:])
             return
         
-        for i in range(len(candidates)):
-            if not curr or (curr and curr[-1] <= candidates[i]):
-                curr.append(candidates[i])
-                self.backtrack(prefix + candidates[i], curr, candidates, target, ans)
-                curr.pop()
+        for i in range(start, len(candidates)):
+            if curr + candidates[i] <= target:
+                path.append(candidates[i])
+                self.backtrack(path, i, curr + candidates[i], candidates, target, ans)
+                path.pop()
     
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         ans = []
-        self.backtrack(0, [], candidates, target, ans)
+        self.backtrack([], 0, 0, candidates, target, ans)
+        
         return ans
