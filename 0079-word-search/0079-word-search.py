0fc0fb20b125849132b84bd19e3768a8a9778942
Andrew Kim <ksb0724@utexas.edu>
2023-06-21 00:50:07 +0900

    Time: 8501 ms (27.28%), Space: 16.4 MB (29.51%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,26 +1,24 @@
 class Solution:
-    def valid(self, r: int, c: int) -> bool:
-        return 0 <= r < self.m and 0 <= c < self.n
-    
-    def backtrack(self, row: int, col: int, start: int, seen: Set[Tuple[int, int]], word: str, board: List[List[str]]) -> bool:
-        if start == len(word):
-            return True
-        
-        for n_r, n_c in (row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1):
-            if self.valid(n_r, n_c) and (n_r, n_c) not in seen:
-                if board[n_r][n_c] == word[start]:
-                    seen.add((n_r, n_c))
-                    if self.backtrack(n_r, n_c, start + 1, seen, word, board):
-                        return True
-                    seen.remove((n_r, n_c))
-        
-        return False
-    
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.m, self.n = len(board), len(board[0])
+        def valid(row: int, col: int) -> bool:
+            return 0 <= row < m and 0 <= col < n
         
-        for row in range(self.m):
-            for col in range(self.n):
-                if board[row][col] == word[0] and self.backtrack(row, col, 1, {(row, col)}, word, board):
+        def backtrack(row: int, col: int, i: int, seen: Set[Tuple[int]]) -> bool:
+            if i == len(word):
+                return True
+            
+            for n_r, n_c in (row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1):
+                if valid(n_r, n_c) and (n_r, n_c) not in seen:
+                    if board[n_r][n_c] == word[i]:
+                        seen.add((n_r, n_c))
+                        if backtrack(n_r, n_c, i + 1, seen):
+                            return True
+                        seen.remove((n_r, n_c))
+            return False
+        
+        m, n = len(board), len(board[0])
+        for row in range(m):
+            for col in range(n):
+                if board[row][col] == word[0] and backtrack(row, col, 1, {(row, col)}):
                     return True
         return False
