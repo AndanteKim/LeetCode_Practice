8715ee85b7a6f38c7dd78db4902860009288e31f
Andrew Kim <ksb0724@utexas.edu>
2023-06-21 15:55:01 +0900

    Time: 949 ms (36.76%), Space: 31.9 MB (66.18%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -1,22 +1,20 @@
 class Solution:
+    def getCost(self, base: int, nums: List[int], cost: List[int]) -> int:
+        return sum(abs(base - num) * c for num, c in zip(nums, cost))
+    
     def minCost(self, nums: List[int], cost: List[int]) -> int:
-        num_and_cost = sorted([num, c] for num, c in zip(nums, cost))
-        n = len(cost)
+        left, right = min(nums), max(nums)
+        ans = self.getCost(nums[0], nums, cost)
         
-        prefix_cost = [0] * n
-        prefix_cost[0] = num_and_cost[0][1]
-        for i in range(1, n):
-            prefix_cost[i] = num_and_cost[i][1] + prefix_cost[i - 1]
-        
-        total_cost = 0
-        for i in range(1, n):
-            total_cost += num_and_cost[i][1] * (num_and_cost[i][0] - num_and_cost[0][0])
-        ans = total_cost
-        
-        for i in range(1, n):
-            gap = num_and_cost[i][0] - num_and_cost[i - 1][0]
-            total_cost += prefix_cost[i - 1] * gap
-            total_cost -= gap * (prefix_cost[n - 1] - prefix_cost[i - 1])
-            ans = min(ans, total_cost)
+        while left < right:
+            mid = (left + right) >> 1
+            cost_1 = self.getCost(mid, nums, cost)
+            cost_2 = self.getCost(mid + 1, nums, cost)
+            ans = min(cost_1, cost_2)
+            
+            if cost_1 > cost_2:
+                left = mid + 1
+            else:
+                right = mid
         
         return ans
