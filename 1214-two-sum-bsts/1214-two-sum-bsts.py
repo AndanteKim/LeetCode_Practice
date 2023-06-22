58976d314acc1060ff2ff22a34438fd70cc15516
Andrew Kim <ksb0724@utexas.edu>
2023-06-22 16:52:04 +0900

    Time: 107 ms (36.55%), Space: 22.9 MB (40.34%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.py
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -5,20 +5,22 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def inorder(self, seen: Set[int], root: Optional[TreeNode]) -> None:
-        if not root:
-            return
-        self.inorder(seen, root.left)
-        seen.add(root.val)
-        self.inorder(seen, root.right)
+    def binarySearch(self, root2: Optional[TreeNode], target: int) -> bool:
+        if not root2:
+            return False
+        if root2.val == target:
+            return True
+        elif root2.val > target:
+            return self.binarySearch(root2.left, target)
+        else:
+            return self.binarySearch(root2.right, target)
     
-    def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
-        seen1, seen2 = set(), set()
-        self.inorder(seen1, root1)
-        self.inorder(seen2, root2)
-        for n1 in seen1:
-            if target - n1 in seen2:
-                return True
+    def dfs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
+        if not root1:
+            return False
+        if self.binarySearch(root2, target - root1.val):
+            return True
+        return self.dfs(root1.left, root2, target) or self.dfs(root1.right, root2, target)
         
-        return False
-            
+    def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
+        return self.dfs(root1, root2, target)
