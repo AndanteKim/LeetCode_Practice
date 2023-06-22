b9885ec18440f9c1e5e0d97eddf1a6c7ddeb319d
Andrew Kim <ksb0724@utexas.edu>
2023-06-22 18:19:13 +0900

    Time: 36 ms (79.77%), Space: 28.5 MB (59.54%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.cpp
+++ 1214-two-sum-bsts/1214-two-sum-bsts.cpp
@@ -9,23 +9,114 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+class BSTIterator{
+public:
+    TreeNode* curr;
+    BSTIterator(TreeNode* root){
+        curr = root;
+    }
+    
+    void morris(int flag){
+        while (curr){
+            if (curr -> left){
+                TreeNode* rightMost = curr -> left;
+                while (rightMost -> right && rightMost -> right != curr){
+                    rightMost = rightMost -> right;
+                }
+                
+                if (rightMost -> right == curr){
+                    if (flag) rightMost -> right = NULL;
+                    break;
+                }
+                else{
+                    rightMost -> right = curr;
+                    curr = curr -> left;
+                }
+            }
+            else break;
+        }
+    }
+    
+    int peek(){
+        morris(0);
+        return curr -> val;
+    }
+    
+    void increment(){
+        morris(1);
+        curr = curr -> right;
+    }
+    
+    bool hasNext(){
+        return curr != NULL;
+    }
+    
+};
+
+class rBSTIterator{
+public:
+    TreeNode* curr;
+    rBSTIterator(TreeNode* root){
+        curr = root;
+    }
+    
+    void morris(int flag){
+        while (curr){
+            if (curr -> right){
+                TreeNode* leftMost = curr -> right;
+                while (leftMost -> left && leftMost -> left != curr){
+                    leftMost = leftMost -> left;
+                }
+                
+                if (leftMost -> left == curr){
+                    if (flag) leftMost -> left = NULL;
+                    break;
+                }
+                else{
+                    leftMost -> left = curr;
+                    curr = curr -> right;
+                }
+            }
+            else break;
+        }
+    }
+    
+    int peek(){
+        morris(0);
+        return curr -> val;
+    }
+    
+    void decrement(){
+        morris(1);
+        curr = curr -> left;
+    }
+    
+    bool hasPrev(){
+        return curr != NULL;
+    }
+    
+};
+
 class Solution {
-private:
-    bool binarySearch(TreeNode* root2, int target){
-        if (!root2) return false;
-        if (root2 -> val == target) return true;
-        else if (root2 -> val > target) return binarySearch(root2 -> left, target);
-        else return binarySearch(root2 -> right, target);
-    }
-    
-    bool dfs(TreeNode* root1, TreeNode* root2, int target){
-        if (!root1) return false;
-        if (binarySearch(root2, target - root1 -> val)) return true;
-        return dfs(root1 -> left, root2, target) || dfs(root1 -> right, root2, target);
-    }
-    
 public:
     bool twoSumBSTs(TreeNode* root1, TreeNode* root2, int target) {
-        return dfs(root1, root2, target);
+        BSTIterator it1(root1);
+        rBSTIterator it2(root2);
+        bool found = false;
+        
+        while(it1.hasNext() && it2.hasPrev()){
+            int x = it1.peek(), y = it2.peek();
+            if (x + y == target){
+                found = true;
+                break;
+            }
+            if (x + y < target) it1.increment();
+            else it2.decrement();
+        }
+        
+        while (it1.hasNext()) it1.increment();
+        while (it2.hasPrev()) it2.decrement();
+        
+        return found;
     }
 };
