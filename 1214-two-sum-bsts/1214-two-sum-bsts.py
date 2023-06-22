775c724c8777b20888f2f65ec8a89257c23d17ab
Andrew Kim <ksb0724@utexas.edu>
2023-06-22 17:56:39 +0900

    Time: 87 ms (81.03%), Space: 20.8 MB (74.83%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.py
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -5,22 +5,53 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def binarySearch(self, root2: Optional[TreeNode], target: int) -> bool:
-        if not root2:
-            return False
-        if root2.val == target:
-            return True
-        elif root2.val > target:
-            return self.binarySearch(root2.left, target)
-        else:
-            return self.binarySearch(root2.right, target)
+    def morris_traversal(self, root: Optional[TreeNode]) -> Iterator[int]:
+        current = root
+        while current:
+            if not current.left:
+                yield current.val
+                current = current.right
+            else:
+                pre = current.left
+                while pre.right and pre.right != current:
+                    pre = pre.right
+                if not pre.right:
+                    pre.right = current
+                    current = current.left
+                else:
+                    pre.right = None
+                    yield current.val
+                    current = current.right
+                    
+    def reversed_morris_traversal(self, root: Optional[TreeNode]) -> Iterator[int]:
+        current = root
+        while current:
+            if not current.right:
+                yield current.val
+                current = current.left
+            else:
+                pre = current.right
+                while pre.left and pre.left != current:
+                    pre = pre.left
+                if not pre.left:
+                    pre.left = current
+                    current = current.right
+                else:
+                    pre.left = None
+                    yield current.val
+                    current = current.left
     
-    def dfs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
-        if not root1:
-            return False
-        if self.binarySearch(root2, target - root1.val):
-            return True
-        return self.dfs(root1.left, root2, target) or self.dfs(root1.right, root2, target)
-        
     def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
-        return self.dfs(root1, root2, target)
+        iterator1 = self.morris_traversal(root1)
+        iterator2 = self.reversed_morris_traversal(root2)
+        val1, val2 = next(iterator1, None), next(iterator2, None)
+        
+        while val1 != None and val2 != None:
+            if val1 + val2 == target:
+                return True
+            elif val1 + val2 < target:
+                val1 = next(iterator1, None)
+            else:
+                val2 = next(iterator2, None)
+        
+        return False
