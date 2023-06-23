0407dd9e2f381d5cd08af3914c13b21385ccdd4e
Andrew Kim <ksb0724@utexas.edu>
2023-06-23 22:57:06 +0900

    Time: 1671 ms (37.96%), Space: 16.5 MB (17.49%) - LeetHub

---
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -0,0 +1,23 @@
+class Solution:
+    def backtrack(self, i: int, zero_count: int, curr: List[int], cookies: List[int]) -> int:
+        if self.n - i < zero_count:
+            return float('inf')
+        
+        if i == self.n:
+            return max(curr)
+        
+        ans = float('inf')
+        for j in range(self.k):
+            zero_count -= int(curr[j] == 0)
+            curr[j] += cookies[i]
+            
+            ans = min(ans, self.backtrack(i + 1, zero_count, curr, cookies))
+            curr[j] -= cookies[i]
+            zero_count += int(curr[j] == 0)
+        return ans
+    
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        self.n, self.k = len(cookies), k
+        curr = [0] * k
+        
+        return self.backtrack(0, k, curr, cookies)
