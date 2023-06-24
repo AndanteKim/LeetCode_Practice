35797d37de4865424bb24bb2cca825977f151a04
Andrew Kim <ksb0724@utexas.edu>
2023-06-25 01:23:54 +0900

    Time: 49 ms (72.82%), Space: 16.3 MB (58.71%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,20 +1,19 @@
 class Solution:
-    def valid(self, s: str, start: int, length: int) -> bool:
-        return length == 1 or (s[start] != '0' and (length < 3 or s[start:start+length] <= "255"))
+    def backtrack(self, start: int, addr: str, boundary: int, s: str, ans: List[str]) -> None:
+        if boundary == 0:
+            if start == len(s):
+                ans.append(addr[:-1])
+            return
+        
+        if start + 1 <= len(s):
+            self.backtrack(start + 1, addr + s[start] +'.', boundary - 1, s, ans)
+        if start + 2 <= len(s) and s[start] != '0':
+            self.backtrack(start + 2, addr + s[start] + s[start + 1] + '.', boundary - 1, s, ans)
+        if start + 3 <= len(s) and s[start] != '0' and 0 <= int(s[start:start+3]) <= 255:
+            self.backtrack(start + 3, addr + s[start] + s[start + 1] + s[start + 2] + '.', boundary - 1, s, ans)
     
     def restoreIpAddresses(self, s: str) -> List[str]:
-        ans, length = [], len(s)
-        
-        for len1 in range(max(1, length - 9), min(3, length - 3)+1):
-            if not self.valid(s, 0, len1):
-                continue
-                
-            for len2 in range(max(1, length - len1 - 6), min(3, length - len1 - 2)+1):
-                if not self.valid(s, len1, len2):
-                    continue
-                for len3 in range(max(1, length - len1 - len2 - 3), min(3, length - len1 - len2 - 1)+1):
-                    if self.valid(s, len1 + len2, len3) and self.valid(s, len1 + len2 + len3, length - len1 - len2 - len3):
-                        ans.append(s[0:len1] + '.' + s[len1:len1+len2] + '.' + s[len1 + len2:len1 + len2 + len3] + '.' + s[len1 + len2 + len3:])
-        
+        ans = []
+        self.backtrack(0, "", 4, s, ans)
         
         return ans
