a6e41d15fc4b391c625446d1233e3e9e5ac1da59
Andrew Kim <ksb0724@utexas.edu>
2023-06-24 23:24:51 +0900

    Time: 350 ms (25.55%), Space: 75.8 MB (29.08%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,37 +1,19 @@
 class Solution {
-private:
-    map<int, int> helper(vector<int>& rods, int left, int right){
-        set<pair<int, int>> states;
-        states.insert({0, 0});
-        for (int i = left; i < right; ++i){
-            set<pair<int, int>> newStates;
-            for (auto& [l, r] : states){
-                newStates.insert({l + rods[i], r});
-                newStates.insert({l, r + rods[i]});
-            }
-            
-            for (auto& [l, r] : newStates){
-                states.insert({l, r});
-            }
-            
-        }
-        
-        map<int, int> dp;
-        for (auto& [l, r] : states) dp[l - r] = max(dp[l - r], l);
-        return dp;
-    }
-    
 public:
     int tallestBillboard(vector<int>& rods) {
-        int n = rods.size();
-        
-        map<int, int> firstHalf = helper(rods, 0, n / 2), secondHalf = helper(rods, n / 2, n);
-        int answer = 0;
-        for (auto& [key, diff] : firstHalf){
-            if (secondHalf.find(-key) != secondHalf.end())
-                answer = max(answer, diff + secondHalf[-key]);
+        unordered_map<int, int> dp;
+        dp[0] = 0;
+        for (int r : rods){
+            unordered_map<int, int> newDp(dp);
+            for (auto& [diff, taller] : dp){
+                int shorter = taller - diff;
+                newDp[diff + r] = max(newDp[diff + r], taller + r);
+                int newDiff = abs(shorter + r - taller);
+                int newTaller = max(shorter + r, taller);
+                newDp[newDiff] = max(newDp[newDiff], newTaller);
+            }
+            dp = newDp;
         }
-        return answer;
+        return dp[0];
     }
 };
-    
