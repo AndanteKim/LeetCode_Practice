6e69db0e95a3bd090715b03a8656272a8fdf93e2
Andrew Kim <ksb0724@utexas.edu>
2023-06-24 23:05:53 +0900

    Time: 574 ms (62.50%), Space: 16.9 MB (67.71%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.py
+++ 0956-tallest-billboard/0956-tallest-billboard.py
@@ -1,28 +1,17 @@
 class Solution:
-    def helper(self, half_rods: List[int]) -> Dict[int, int]:
-        states = set()
-        states.add((0, 0))
-        for r in half_rods:
-            new_states = set()
-            for left, right in states:
-                new_states.add((left + r, right))
-                new_states.add((left, right + r))
-            states |= new_states
-        
-        dp = dict()
-        for left, right in states:
-            dp[left - right] = max(dp.get(left - right, 0), left)
-        return dp
-    
     def tallestBillboard(self, rods: List[int]) -> int:
-        n = len(rods)
-        first_half = self.helper(rods[: n // 2])
-        second_half = self.helper(rods[n // 2:])
-        answer = 0
+        dp = {0: 0}
         
-        for diff in first_half:
-            if -diff in second_half:
-                answer = max(answer, first_half[diff] + second_half[-diff])
+        for r in rods:
+            new_dp = dp.copy()
+            for diff, taller in dp.items():
+                shorter = taller - diff
+                new_dp[diff + r] = max(new_dp.get(diff + r, 0), taller + r)
+                
+                new_diff = abs(shorter + r - taller)
+                new_taller = max(shorter + r, taller)
+                new_dp[new_diff] = max(new_dp.get(new_diff, 0), new_taller)
+            
+            dp = new_dp
         
-        return answer
-        
+        return dp.get(0, 0)
