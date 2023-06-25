2c406abb00b5b99b1309f7f5342e862dd449fe9a
Andrew Kim <ksb0724@utexas.edu>
2023-06-25 23:58:17 +0900

    Time: 9 ms (22.66%), Space: 8.3 MB (19.36%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.py
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -1,36 +1,39 @@
-class Solution:
-    def backtrack(self, row: int, col: int, remain: int, grid: List[List[int]]) -> None:
-        if grid[row][col] == 2 and remain == 1:
-            self.ans += 1
-            return
-        
-        if grid[row][col] == 2:
-            return
-        
-        temp = grid[row][col]
-        grid[row][col] = -4
-        remain -= 1
-        
-        for r, c in (row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1):
-            if r < 0 or c < 0 or r >= self.m or c >= self.n:
-                continue
-            if grid[r][c] < 0:
-                continue
-            
-            self.backtrack(r, c, remain, grid)
-            
-        grid[row][col] = temp
+class Solution {
+private:
+    int m, n;
     
-    def uniquePathsIII(self, grid: List[List[int]]) -> int:
-        self.m, self.n = len(grid), len(grid[0])
-        non_obstacles, start_row, start_col = 0, 0, 0
-        for i in range(self.m):
-            for j in range(self.n):
-                if grid[i][j] >= 0:
-                    non_obstacles += 1
-                if grid[i][j] == 1:
-                    start_row, start_col = i, j
+    int backtrack(int row, int col, int remain, vector<vector<int>>& grid){
+        if (grid[row][col] == 2){
+            return remain == 1? 1: 0;
+        }
         
-        self.ans = 0
-        self.backtrack(start_row, start_col, non_obstacles, grid)
-        return self.ans
+        int temp = grid[row][col];
+        grid[row][col] = -4;
+        --remain;
+        int ans = 0;
+        
+        for (auto& [r, c] : vector<pair<int, int>>{{row + 1, col}, {row - 1, col}, {row, col + 1}, {row, col - 1}}){
+            if (r < 0 || c < 0 || r >= m || c >= n) continue;
+            if (grid[r][c] < 0) continue;
+            ans += backtrack(r, c, remain, grid);
+        }
+        grid[row][col] = temp;
+        return ans;
+    }
+    
+public:
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        m = grid.size(), n = grid[0].size();
+        int nonObstacles = 0, startRow, startCol;
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j){
+                if (grid[i][j] >= 0) ++nonObstacles;
+                if (grid[i][j] == 1){
+                    startRow = i, startCol = j;
+                }
+            }
+        }
+        
+        return backtrack(startRow, startCol, nonObstacles, grid);
+    }
+};
