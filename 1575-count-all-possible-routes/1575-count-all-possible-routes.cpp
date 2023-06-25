24c1ee3be0529d8324dc29a7cf057116c221440d
Andrew Kim <ksb0724@utexas.edu>
2023-06-25 16:20:07 +0900

    Time: 718 ms (10.78%), Space: 11.9 MB (27.62%) - LeetHub

--- 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
+++ 1575-count-all-possible-routes/1575-count-all-possible-routes.cpp
@@ -1,25 +1,18 @@
 class Solution {
-private:
-    int n;
-    int solve(vector<int>& locations, int currCity, int finish, int remainingFuel, vector<vector<int>>& memo){
-        if (remainingFuel < 0) return 0;
-        if (memo[currCity][remainingFuel] != -1) return memo[currCity][remainingFuel];
-        
-        int ans = currCity == finish? 1: 0;
-        for (int nextCity = 0; nextCity < n; ++nextCity){
-            if (nextCity != currCity){
-                ans = (ans + solve(locations, nextCity, finish, remainingFuel - abs(locations[currCity] - locations[nextCity]), memo)) % 1'000'000'007;
-            }
-        }
-        
-        return memo[currCity][remainingFuel] = ans;
-    }
-    
 public:
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
-        this -> n = locations.size();
-        vector<vector<int>> memo(n, vector<int>(fuel + 1, -1));
+        int n = locations.size();
+        vector<vector<int>> dp(n, vector<int>(fuel + 1));
+        for (int i = 0; i <= fuel; ++i) dp[finish][i] = 1;
         
-        return solve(locations, start, finish, fuel, memo);
+        for (int j = 0; j <= fuel; ++j){
+            for (int i = 0; i < n; ++i){
+                for (int k = 0; k < n; ++k){
+                    if (k == i) continue;
+                    if (abs(locations[i] - locations[k]) <= j) dp[i][j] = (dp[i][j] + dp[k][j - abs(locations[i] - locations[k])]) % 1'000'000'007;
+                }
+            }
+        }
+        return dp[start][fuel];
     }
 };
