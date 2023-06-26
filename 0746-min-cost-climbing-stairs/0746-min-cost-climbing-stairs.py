23bfd766663879788889442228d5a5d766091d94
Andrew Kim <ksb0724@utexas.edu>
2023-06-26 23:14:03 +0900

    Time: 67 ms (86.63%), Space: 16.5 MB (60.49%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,14 +1,9 @@
 class Solution:
-    def dp(self, i: int, memo: Dict[int, int], cost: List[int]) -> int:
-        if i <= 1:
-            return 0
-        
-        if i in memo:
-            return memo[i]
-        
-        memo[i] = min(self.dp(i - 1, memo, cost) + cost[i - 1], self.dp(i - 2, memo, cost) + cost[i - 2])
-        return memo[i]
-    
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        memo = dict()
-        return self.dp(len(cost), memo, cost)
+        n = len(cost)
+        dp = [0] * (n + 1)
+        
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+        
+        return dp[n]
