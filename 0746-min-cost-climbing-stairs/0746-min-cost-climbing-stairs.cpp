6807cd66f13669748eccf5e008886ca4e82e53f0
Andrew Kim <ksb0724@utexas.edu>
2023-06-26 23:08:10 +0900

    Time: 12 ms (16.92%), Space: 13.8 MB (18.16%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,16 +1,17 @@
 class Solution {
 private:
-    int dp(int i, unordered_map<int, int>& memo, vector<int>& cost){
+    int dp(int i, vector<int>& memo, vector<int>& cost){
         if (i <= 1) return 0;
         
-        if (memo.find(i) != memo.end()) return memo[i];
+        if (memo[i] != -1) return memo[i];
         
         return memo[i] = min(cost[i - 1] + dp(i - 1, memo, cost), cost[i - 2] + dp(i - 2, memo, cost));
     }
     
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        unordered_map<int, int> memo;
+        int n = cost.size();
+        vector<int> memo(n + 1, -1);
         
         return dp(cost.size(), memo, cost);
     }
