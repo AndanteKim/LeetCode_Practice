0459f5c52877b89ab3793a3aebff874c7f1aa427
Andrew Kim <ksb0724@utexas.edu>
2023-06-27 20:33:26 +0900

    Time: 3 ms (99.71%), Space: 10.4 MB (84.99%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, 1);
-        for (int i = 0; i < n; ++i){
-            for (int j = 0; j < i; ++j){
-                if (nums[i] > nums[j]) dp[i] = max(dp[i], dp[j] + 1);
-            }
+        vector<int> sub;
+        for (int num : nums){
+            int idx = lower_bound(sub.begin(), sub.end(), num) - sub.begin();
+            if (idx == sub.size()) sub.push_back(num);
+            else sub[idx] = num;
         }
         
-        return *max_element(dp.begin(), dp.end());
+        return sub.size();
     }
 };
