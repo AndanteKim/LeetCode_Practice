78c62c2b18d02c974c4ef3d2a873f713c53cc975
Andrew Kim <ksb0724@utexas.edu>
2023-06-28 18:34:00 +0900

    Time: 4 ms, Memory: 6.2 MB - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,11 @@
 class Solution {
-private:
-    int dp(int i, vector<int>& memo){
-        if (i < 0) return 0;
-        if (i == 0) return 1;
-        if (memo[i] != -1) return memo[i];
-        return memo[i] = dp(i - 1, memo) + dp(i - 2, memo);
-    }
-    
 public:
     int climbStairs(int n) {
-        vector<int> memo(n + 1, -1);
-        memo[0] = 0, memo[1] = 1;
+        if (n == 1) return n;
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1, dp[2] = 2;
         
-        return dp(n, memo);
+        for (int i = 3; i <= n; ++i) dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
     }
 };
