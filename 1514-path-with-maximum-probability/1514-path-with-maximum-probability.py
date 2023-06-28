241440af16010805c8eefb2c2759c689137096a5
Andrew Kim <ksb0724@utexas.edu>
2023-06-28 17:42:34 +0900

    Time: 692 ms (82.02%), Space: 29.2 MB (21.01%) - LeetHub

--- 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
+++ 1514-path-with-maximum-probability/1514-path-with-maximum-probability.py
@@ -1,20 +1,22 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start: int, end: int) -> float:
         graph = defaultdict(list)
+        for i, (u, v) in enumerate(edges):
+            graph[u].append((v, succProb[i]))
+            graph[v].append((u, succProb[i]))
         
-        for i, (a, b) in enumerate(edges):
-            graph[a].append([b, succProb[i]])
-            graph[b].append([a, succProb[i]])
-        
-        max_prob = [0.0] * n
+        max_prob = [0, 0] * n
         max_prob[start] = 1.0
         
-        queue = deque([start])
-        while queue:
-            curr_node = queue.popleft()
-            for next_node, path_prob in graph[curr_node]:
-                if max_prob[curr_node] * path_prob > max_prob[next_node]:
-                    max_prob[next_node] = max_prob[curr_node] * path_prob
-                    queue.append(next_node)
+        pq = [(-1.0, start)]
         
-        return max_prob[end]
+        while pq:
+            curr_prob, curr_node = heappop(pq)
+            if curr_node == end:
+                return -curr_prob
+            for next_node, path_prob in graph[curr_node]:
+                if -curr_prob * path_prob > max_prob[next_node]:
+                    max_prob[next_node] = -curr_prob * path_prob
+                    heappush(pq, (-max_prob[next_node], next_node))
+        
+        return 0.0
