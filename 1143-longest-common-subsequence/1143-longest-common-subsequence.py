c4fc01dffa0cd5da34fa3cc5974feddbe9f30ef0
Andrew Kim <ksb0724@utexas.edu>
2023-06-30 01:23:36 +0900

    Time: 602 ms (98.65%), Space: 16.5 MB (93.25%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,20 +1,17 @@
 class Solution:
-    def dp(self, i: int, j: int, text1: str, text2: str, memo: Dict[Tuple[int, int], int]) -> int:
-        if i == len(text1) or j == len(text2):
-            return 0
-        
-        if memo[(i, j)] != -1:
-            return memo[(i, j)]
-        
-        if text1[i] == text2[j]:
-            ans = 1 + self.dp(i + 1, j + 1, text1, text2, memo)
-        else:
-            ans = max(self.dp(i + 1, j, text1, text2, memo), self.dp(i, j + 1, text1, text2, memo))
-        
-        memo[(i, j)] = ans
-        return ans
-    
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        memo = defaultdict(lambda: -1)
-        return self.dp(0, 0, text1, text2, memo)
-    
+        if len(text2) < len(text1):
+            text1, text2 = text2, text1
+            
+        previous, current = [0] * (len(text1) + 1), [0] * (len(text1) + 1)
+        
+        for col in reversed(range(len(text2))):
+            for row in reversed(range(len(text1))):
+                if text2[col] == text1[row]:
+                    current[row] = 1 + previous[row + 1]
+                else:
+                    current[row] = max(previous[row], current[row + 1])
+            
+            previous, current = current, previous
+        
+        return previous[0]
