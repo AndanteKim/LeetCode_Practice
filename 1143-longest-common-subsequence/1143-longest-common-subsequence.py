aabd95dce7963e5004a9c25a5a09a1f61718bcc8
Andrew Kim <ksb0724@utexas.edu>
2023-06-29 23:58:38 +0900

    Time: 2675 ms (5.02%), Space: 185.7 MB (16.73%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,13 +1,18 @@
 class Solution:
+    def dp(self, i: int, j: int, text1: str, text2: str, memo: Dict[Tuple[int, int], int]) -> int:
+        if i == len(text1) or j == len(text2):
+            return 0
+        
+        if memo[(i, j)] != -1:
+            return memo[(i, j)]
+        
+        if text1[i] == text2[j]:
+            memo[(i, j)] = 1 + self.dp(i + 1, j + 1, text1, text2, memo)
+            return memo[(i, j)]
+        memo[(i, j)] = max(self.dp(i + 1, j, text1, text2, memo), self.dp(i, j + 1, text1, text2, memo))
+        return memo[(i, j)]
+    
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        m, n = len(text1), len(text2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        
-        for i in range(0, m):
-            for j in range(0, n):
-                if text1[i] == text2[j]:
-                    dp[i + 1][j + 1] = dp[i][j] + 1
-                else:
-                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1])
-        
-        return dp[-1][-1]
+        memo = defaultdict(lambda: -1)
+        return self.dp(0, 0, text1, text2, memo)
+    
