402cb5b613c63aaa1f02d76079aa5249b2fe99ac
Andrew Kim <ksb0724@utexas.edu>
2023-06-29 14:21:07 +0900

    Time: 51 ms (61.64%), Space: 16.6 MB (33.17%) - LeetHub

--- 0250-count-univalue-subtrees/0250-count-univalue-subtrees.py
+++ 0250-count-univalue-subtrees/0250-count-univalue-subtrees.py
@@ -5,25 +5,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, node: Optional[TreeNode]) -> bool:
+    def dfs(self, node: Optional[TreeNode]) -> Tuple[bool, int]:
         if not node:
-            return True
+            return True, 0
         
-        isLeftUniValue = self.dfs(node.left)
-        isRightUniValue = self.dfs(node.right)
+        left = self.dfs(node.left)
+        right = self.dfs(node.right)
+        isLeftUniValue = left[0]
+        isRightUniValue = right[0]
+        count = left[1] + right[1]
         
         if isLeftUniValue and isRightUniValue:
             if node.left and node.val != node.left.val:
-                return False
+                return False, count
             if node.right and node.val != node.right.val:
-                return False
-            
-            self.count += 1
-            return True
-        return False
-        
+                return False, count
+            return True, count + 1
+        return False, count
     
     def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
-        self.count = 0
-        self.dfs(root)
-        return self.count
+        return self.dfs(root)[1]
