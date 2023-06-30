e9954dbf233bc541efc09bf9c2fc3bcd0a08f5ba
Andrew Kim <ksb0724@utexas.edu>
2023-06-30 19:08:02 +0900

    Time: 226 ms (44.45%), Space: 19.4 MB (47.01%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,22 +1,17 @@
 class Solution:
-    def dp(self, i: int, holding: bool, remain: int, prices: List[List[int]], memo: List[List[List[int]]]) -> int:
-        if i == self.n or remain == 0:
-            return 0
-        
-        if memo[i][holding][remain] != -1:
-            return memo[i][holding][remain]
-        
-        ans = self.dp(i + 1, holding, remain, prices, memo)
-        
-        if holding:
-            ans = max(ans, prices[i] + self.dp(i + 1, 0, remain - 1, prices, memo))
-        else:
-            ans = max(ans, -prices[i] + self.dp(i + 1, 1, remain, prices, memo))
-        
-        memo[i][holding][remain] = ans
-        return ans
-    
     def maxProfit(self, k: int, prices: List[int]) -> int:
-        self.n = len(prices)
-        memo = [[[-1] * (k + 1) for _ in range(2)] for __ in range(self.n)]
-        return self.dp(0, False, k, prices, memo)
+        n = len(prices)
+        dp = [[[0] * (k + 1) for _ in range(2)] for __ in range(n + 1)]
+        
+        for i in range(n - 1, -1, -1):
+            for remain in range(1, k + 1):
+                for holding in range(2):
+                    ans = dp[i + 1][holding][remain]
+                    if holding:
+                        ans = max(ans, prices[i] + dp[i + 1][0][remain - 1])
+                    else:
+                        ans = max(ans, -prices[i] + dp[i + 1][1][remain])
+                    
+                    dp[i][holding][remain] = ans
+        
+        return dp[0][0][k]
