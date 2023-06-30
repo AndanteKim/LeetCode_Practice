ea06ce88f61bda48dba083614a2cdd64aed13944
Andrew Kim <ksb0724@utexas.edu>
2023-06-30 19:15:44 +0900

    Time: 20 ms (63.69%), Space: 14.5 MB (30.86%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,19 @@
 class Solution {
-private:
-    int n;
-    int dp(int i, int holding, int remain, vector<int>& prices, vector<vector<vector<int>>>& memo){
-        if (i == n || remain == 0) return 0;
-        if (memo[i][holding][remain] != -1){
-            return memo[i][holding][remain];
-        }
-        
-        int ans = dp(i + 1, holding, remain, prices, memo);
-        if (holding == 1){
-            ans = max(ans, prices[i] + dp(i + 1, 0, remain - 1, prices, memo));
-        }
-        else{
-            ans = max(ans, -prices[i] + dp(i + 1, 1, remain, prices, memo));
-        }
-        
-        memo[i][holding][remain] = ans;
-        return ans;
-    }
-    
 public:
     int maxProfit(int k, vector<int>& prices) {
-        this -> n = prices.size();
-        vector<vector<vector<int>>> memo(n + 1, vector(2, vector(k + 1, -1)));
-        return dp(0, 0, k, prices, memo);
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(n + 1, vector(2, vector(k + 1, 0)));
+        
+        for (int i = n - 1; i >= 0; --i){
+            for (int remain = 1; remain <= k; ++remain){
+                for (int holding = 0; holding < 2; ++holding){
+                    int ans = dp[i + 1][holding][remain];
+                    if (holding) ans = max(ans, prices[i] + dp[i + 1][0][remain - 1]);
+                    else ans = max(ans, -prices[i] + dp[i + 1][1][remain]);
+                    dp[i][holding][remain] = ans;
+                }
+            }
+        }
+        return dp[0][0][k];
     }
 };
