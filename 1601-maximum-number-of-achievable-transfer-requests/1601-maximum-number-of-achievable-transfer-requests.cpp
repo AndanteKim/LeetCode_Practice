723c91c1e01e1fa54bcaf065765a7a6ac2ec9e81
Andrew Kim <ksb0724@utexas.edu>
2023-07-02 16:46:03 +0900

    Time: 592 ms (36.27%), Space: 182.8 MB (28.43%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,28 +1,33 @@
 class Solution {
-private:
-    int ans = 0;
-    void maxRequests(vector<vector<int>>& requests, vector<int>& indegree, int n, int start, int count){
-        if (start == requests.size()){
-            for (int i = 0; i < n; ++i){
-                if (indegree[i]) return;
-            }
-            ans = max(ans, count);
-            return;
-        }
-        
-        --indegree[requests[start][0]];
-        ++indegree[requests[start][1]];
-        maxRequests(requests, indegree, n, start + 1, count + 1);
-        ++indegree[requests[start][0]];
-        --indegree[requests[start][1]];
-        
-        maxRequests(requests, indegree, n, start + 1, count);
-    }
-    
 public:
     int maximumRequests(int n, vector<vector<int>>& requests) {
-        vector<int> indegree(n);
-        maxRequests(requests, indegree, n, 0, 0);
+        int ans = 0;
+        for (int mask = 0; mask < (1 << requests.size()); ++mask){
+            vector<int> indegree(n, 0);
+            int pos = requests.size() - 1;
+            int bitCount = __builtin_popcount(mask);
+            
+            if (bitCount <= ans)
+                continue;
+            
+            for (int curr = mask; curr > 0 ; curr >>= 1, --pos){
+                if (curr & 1){
+                    --indegree[requests[pos][0]];
+                    ++indegree[requests[pos][1]];
+                }
+            }
+            
+            int flag = 1;
+            for (int i = 0; i < n; ++i){
+                if (indegree[i]){
+                    flag = 0;
+                    break;
+                }
+            }
+            
+            if (flag) ans = bitCount;
+        }
+        
         return ans;
     }
 };
