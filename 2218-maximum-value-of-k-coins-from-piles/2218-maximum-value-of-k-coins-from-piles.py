0251cc46d42e5f32e09bc933237469d9df0734c0
Andrew Kim <ksb0724@utexas.edu>
2023-07-03 21:48:20 +0900

    Time: 5563 ms (84.85%), Space: 350.4 MB (5.76%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.py
@@ -1,18 +1,17 @@
 class Solution:
-    def f(self, i: int, coins: int, dp: List[List[int]], piles) -> int:
-        if i == 0:
-            return 0
-        if dp[i][coins] != -1:
-            return dp[i][coins]
-        current_sum = 0
-        for current_coins in range(0, min(len(piles[i - 1]), coins) + 1):
-            if current_coins > 0:
-                current_sum += piles[i - 1][current_coins - 1]
-            dp[i][coins] = max(dp[i][coins], self.f(i - 1, coins - current_coins, dp, piles) + current_sum)
-        return dp[i][coins]
-    
     def maxValueOfCoins(self, piles: List[List[int]], k: int) -> int:
-        n = len(piles)
-        dp = [[-1] * (k + 1) for i in range(n + 1)]
         
-        return self.f(n, k, dp, piles)
+        @lru_cache(maxsize = None)
+        def dp(i: int, remain: int) -> int:
+            if i == len(piles) or remain == 0:
+                return 0
+            
+            ans = dp(i + 1, remain)
+            curr = 0
+            for j in range(min(remain, len(piles[i]))):
+                curr += piles[i][j]
+                ans = max(ans, curr + dp(i + 1, remain - j - 1))
+            return ans
+            
+            
+        return dp(0, k)
