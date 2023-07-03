975ec3519d5fef335ae255aacd9ee8016599dfc9
Andrew Kim <ksb0724@utexas.edu>
2023-07-03 21:57:36 +0900

    Time: 371 ms (72.10%), Space: 29.6 MB (68.22%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,20 +1,24 @@
 class Solution {
+private:
+    int n;
+    int dp(int i, int remain, vector<vector<int>>& memo, vector<vector<int>>& piles){
+        if (i == n || remain == 0) return 0;
+        if (memo[i][remain] != -1) return memo[i][remain];
+        
+        int ans = dp(i + 1, remain, memo, piles);
+        int curr = 0;
+        for (int j = 0; j < min(remain, (int)piles[i].size()); ++j){
+            curr += piles[i][j];
+            ans = max(ans, curr + dp(i + 1, remain - j - 1, memo, piles));
+        }
+        
+        return memo[i][remain] = ans;
+    }
+    
 public:
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
-        int n = piles.size();
-        vector dp(n + 1, vector<int>(k + 1, -1));
-        
-        function<int(int, int)> f = [&](int i, int coins){
-            if (i == 0) return 0;
-            if (dp[i][coins] != -1) return dp[i][coins];
-            int currentSum = 0;
-            for (int currentCoins = 0; currentCoins <= min((int)piles[i - 1].size(), coins); ++currentCoins){
-                if (currentCoins > 0) currentSum += piles[i - 1][currentCoins - 1];
-                dp[i][coins] = max(dp[i][coins], f(i - 1, coins - currentCoins) + currentSum);
-            }
-            return dp[i][coins];
-        };
-        
-        return f(n, k);
+        n = piles.size();
+        vector<vector<int>> memo(n, vector<int>(k + 1, -1));
+        return dp(0, k, memo, piles);
     }
 };
