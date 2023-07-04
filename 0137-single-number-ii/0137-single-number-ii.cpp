3dc59d8fa3120b79a105541e18e9b21bf63de9c2
Andrew Kim <ksb0724@utexas.edu>
2023-07-04 19:37:33 +0900

    Time: 5 ms (85.74%), Space: 9.4 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
+        int seenOnce = 0, seenTwice = 0;
         
-        for (int shift = 0; shift < 32; ++shift){
-            int bitSum = 0;
-            for (int num : nums){
-                bitSum += (num >> shift) & 1;
-            }
-            
-            int lonerBit = bitSum % 3;
-            ans = ans | (lonerBit << shift);
+        for (int num : nums){
+            seenOnce = (seenOnce ^ num) & (~seenTwice);
+            seenTwice = (seenTwice ^ num) & (~seenOnce);
         }
         
-        return ans;
+        return seenOnce;
     }
 };
