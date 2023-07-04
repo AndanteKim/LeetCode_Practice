54d74742cd3e194fa545c736be1978ca1b8915f3
Andrew Kim <ksb0724@utexas.edu>
2023-07-04 19:33:36 +0900

    Time: 75 ms (65.30%), Space: 18.1 MB (86.89%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,16 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans = 0
+        seen_once, seen_twice = 0, 0
         
-        for shift in range(32):
-            bit_sum = 0
-            for num in nums:
-                bit_sum += (num >> shift) & 1
-            
-            loner_bit = bit_sum % 3
-            ans = ans | (loner_bit << shift)
+        for num in nums:
+            seen_once = (seen_once ^ num) & (~seen_twice)
+            seen_twice = (seen_twice ^ num) & (~seen_once)
         
-        if ans >= (1 << 31):
-            ans = ans - (1 << 32)
-        
-        return ans
+        return seen_once
