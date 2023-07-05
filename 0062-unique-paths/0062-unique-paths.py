516f64dc951c2312347c29607bdf475484d9e987
Andrew Kim <ksb0724@utexas.edu>
2023-07-06 01:59:19 +0900

    Time: 51 ms (36.23%), Space: 16.6 MB (12.01%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,9 @@
 class Solution:
+    @lru_cache(maxsize = None)
+    def permutation(self, i: int) -> int:
+        if i <= 1:
+            return 1
+        return i * self.permutation(i - 1)
+    
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n for _ in range(m)]
-        for i in range(n):
-            dp[0][i] = 1
-        for i in range(m):
-            dp[i][0] = 1
-        
-        for i in range(1, m):
-            for j in range(1, n):
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
-        
-        return dp[m - 1][n - 1]
+        return self.permutation(m + n - 2)//(self.permutation(m - 1) * (self.permutation(n - 1)))
