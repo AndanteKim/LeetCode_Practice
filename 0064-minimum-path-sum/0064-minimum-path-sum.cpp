db5845da72f304359b8fa8e1d3e3546e74c16b67
Andrew Kim <ksb0724@utexas.edu>
2023-07-07 02:55:43 +0900

    Time: 10 ms (69.93%), Space: 9.9 MB (69.84%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,19 +1,20 @@
 class Solution {
-private:
-    int dp(int row, int col, vector<vector<int>>& memo, vector<vector<int>>& grid){
-        if (row + col == 0) return grid[0][0];
-        if (memo[row][col] != -1) return memo[row][col];
-        int ans = INT_MAX;
-        if (row > 0) ans = min(ans, dp(row - 1, col, memo, grid));
-        if (col > 0) ans = min(ans, dp(row, col - 1, memo, grid));
-        
-        return memo[row][col] = ans + grid[row][col];
-    }
-    
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> memo(m, vector<int>(n, -1));
-        return dp(m - 1, n - 1, memo, grid);
+        vector<int> dp(n, INT_MAX);
+        dp[0] = 0;
+        
+        for (int row = 0; row < m; ++row){
+            vector<int> nextRow(n, 0);
+            for (int col = 0; col < n; ++col){
+                nextRow[col] = dp[col];
+                if (col > 0) nextRow[col] = min(nextRow[col - 1], nextRow[col]);
+                nextRow[col] += grid[row][col];
+            }
+            dp = nextRow;
+        }
+        
+        return dp[n - 1];
     }
 };
