62139de52e8832b5de139108058a1257403e99af
Andrew Kim <ksb0724@utexas.edu>
2023-07-07 00:23:39 +0900

    Time: 109 ms (71.37%), Space: 17.8 MB (89.15%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,15 +1,17 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        dp = [0 for _ in range(len(grid[0]))]
+        dp = [[0] * len(grid[0]) for _ in range(len(grid))]
+        dp[0][0] = grid[0][0]
         
-        for i in range(len(grid)-1, -1, -1):
-            for j in range(len(grid[0])-1, -1, -1):
-                if i == len(grid) - 1 and j != len(grid[0]) - 1:
-                    dp[j] = grid[i][j] + dp[j+1]
-                elif j == len(grid[0]) - 1 and i != len(grid) - 1:
-                    dp[j] = grid[i][j] + dp[j]
-                elif j != len(grid[0]) - 1 and i != len(grid) - 1:
-                    dp[j] = grid[i][j] + min(dp[j], dp[j+1])
-                else:
-                    dp[j] = grid[i][j]
-        return dp[0]
+        for i in range(1, len(grid[0])):
+            dp[0][i] = dp[0][i-1] + grid[0][i]
+        
+        for i in range(1, len(grid)):
+            dp[i][0] = dp[i - 1][0] + grid[i][0]
+        
+        for r in range(1, len(grid)):
+            for c in range(1, len(grid[0])):
+                dp[r][c] = min(dp[r - 1][c], dp[r][c - 1])+ grid[r][c]
+        
+        return dp[-1][-1]
+        
