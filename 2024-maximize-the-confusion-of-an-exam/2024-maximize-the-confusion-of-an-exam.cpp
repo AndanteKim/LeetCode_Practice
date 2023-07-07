6132a9959bf588fa27effcf9c5265db349234c94
Andrew Kim <ksb0724@utexas.edu>
2023-07-07 17:57:26 +0900

    Time: 94 ms (12.59%), Space: 10 MB (59.24%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,32 +1,17 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        int ans = 0, n = answerKey.size();
+        int ans = k, n = answerKey.size();
+        unordered_map<char, int> count;
+        for (int i = 0; i < k; ++i) ++count[answerKey[i]];
         
-        int left = 0, offsetF = k;
-        for (int right = 0; right < n; ++right){
-            if (answerKey[right] == 'F'){
-                if (offsetF > 0) --offsetF;
-                else{
-                    while (answerKey[left] == 'T') ++left;
-                    ++left;
-                }
+        int left = 0;
+        for (int right = k; right < n; ++right){
+            ++count[answerKey[right]];
+            while (min(count['T'], count['F']) > k){
+                --count[answerKey[left]];
+                ++left;
             }
-            
-            ans = max(ans, right - left + 1);
-        }
-        
-        left = 0;
-        int offsetT = k;
-        for (int right = 0; right < n; ++right){
-            if (answerKey[right] == 'T'){
-                if (offsetT > 0) --offsetT;
-                else{
-                    while (answerKey[left] == 'F') ++left;
-                    ++left;
-                }
-            }
-            
             ans = max(ans, right - left + 1);
         }
         
