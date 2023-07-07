a1222668465b08450159052f0aeb17a6fc7e81a4
Andrew Kim <ksb0724@utexas.edu>
2023-07-08 02:22:02 +0900

    Time: 459 ms (5.08%), Space: 10.1 MB (33.14%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,20 +1,33 @@
 class Solution {
-public:
-    int maxConsecutiveAnswers(string answerKey, int k) {
-        int maxSize = 0, n = answerKey.size();
-        unordered_map<char, int> count;
-        
-        for (int right = 0; right < n; ++right){
-            ++count[answerKey[right]];
-            int minor = min(count['T'], count['F']);
-            if (minor <= k) {
-                ++maxSize;
-            }
-            else{
-                --count[answerKey[right - maxSize]];
-            }
+private:
+    int n, k;
+    bool isValid(int size, string& answerKey){
+        unordered_map<char, int> counter;
+        for (int i = 0; i < size; ++i) ++counter[answerKey[i]];
+        if (min(counter['T'], counter['F']) <= k) return true;
+        for (int i = size; i < n; ++i){
+            ++counter[answerKey[i]];
+            --counter[answerKey[i - size]];
+            
+            if (min(counter['T'], counter['F']) <= k) return true;
         }
         
-        return maxSize;
+        return false;
+    }
+    
+public:
+    int maxConsecutiveAnswers(string answerKey, int k) {
+        n = answerKey.size(), this -> k = k;
+        int left = k, right = n;
+        
+        while (left < right){
+            int mid = left + ((right - left + 1) >> 1);
+            if (isValid(mid, answerKey)){
+                left = mid;
+            }
+            else right = mid - 1;
+        }
+        
+        return left;
     }
 };
