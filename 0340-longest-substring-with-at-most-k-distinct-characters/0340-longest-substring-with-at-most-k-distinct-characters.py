c033c7db5c051302c99130dae2441a2803bc2769
Andrew Kim <ksb0724@utexas.edu>
2023-07-08 17:36:35 +0900

    Time: 101 ms (45.15%), Space: 16.6 MB (38.87%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.py
@@ -1,29 +1,16 @@
 class Solution:
-    def isValid(self, size: int, s: str) -> bool:
-        counter = Counter(s[:size])
-        if len(counter) <= self.k:
-            return True
-        
-        for i in range(size, self.n):
-            counter[s[i]] += 1
-            counter[s[i - size]] -= 1
-            if counter[s[i - size]] == 0:
-                del counter[s[i - size]]
-            if len(counter) <= self.k:
-                return True
-        return False
-    
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        self.n, self.k = len(s), k
-        if k >= self.n:
-            return self.n
-        left, right = k, self.n
+        ans = 0
+        counter = Counter()
         
-        while left < right:
-            mid = left + ((right - left + 1) >> 1)
-            if self.isValid(mid, s):
-                left = mid
+        for right in range(len(s)):
+            counter[s[right]] += 1
+            
+            if len(counter) <= k:
+                ans += 1
             else:
-                right = mid - 1
+                counter[s[right - ans]] -= 1
+                if counter[s[right - ans]] == 0:
+                    del counter[s[right - ans]]
         
-        return left
+        return ans
