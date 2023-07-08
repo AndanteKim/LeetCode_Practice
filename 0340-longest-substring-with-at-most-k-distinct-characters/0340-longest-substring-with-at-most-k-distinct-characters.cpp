e9c0665c491fbf2bee6c30fe0c06e54de2562699
Andrew Kim <ksb0724@utexas.edu>
2023-07-08 17:30:59 +0900

    Time: 106 ms (5.02%), Space: 7.3 MB (65.57%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -1,21 +1,32 @@
 class Solution {
+private:
+    int n, k;
+    bool isValid(int size, string& s){
+        unordered_map<char, int> count;
+        for (int i = 0; i < size; ++i) ++count[s[i]];
+        if (count.size() <= k) return true;
+        for (int i = size; i < n; ++i){
+            ++count[s[i]];
+            --count[s[i - size]];
+            if (count[s[i - size]] == 0) count.erase(s[i - size]);
+            if (count.size() <= k) return true;
+        }
+        return false;
+    }
+    
 public:
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-        int ans = 0, curr = 0, left = 0, n = s.size();
-        unordered_map<char, int> count;
+        n = s.size();
+        this -> k = k;
+        if (k >= n) return n;
         
-        for (int right = 0; right < n; ++right){
-            char add = s[right];
-            if (count.find(add) == count.end()) ++curr;
-            ++count[add];
-            while (count.size() > k){
-                --count[s[left]];
-                if (count[s[left]] == 0) count.erase(s[left]);
-                ++left;
-            }
-            
-            ans = max(ans, right - left + 1);
+        int left = k, right = n;
+        while (left < right){
+            int mid = left + ((right - left + 1) >> 1);
+            if (isValid(mid, s)) left = mid;
+            else right = mid - 1;
         }
-        return ans;
+        
+        return left;
     }
 };
