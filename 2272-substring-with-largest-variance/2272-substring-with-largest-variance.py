1b0d166eab84199090b690f662c168735e51f575
Andrew Kim <ksb0724@utexas.edu>
2023-07-09 22:32:45 +0900

    Time: 1086 ms (99.15%), Space: 16.8 MB (5.98%) - LeetHub

--- 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
+++ 2272-substring-with-largest-variance/2272-substring-with-largest-variance.py
@@ -1,32 +1,28 @@
 class Solution:
     def largestVariance(self, s: str) -> int:
-        counter = [0] * 26
-        for c in s:
-            counter[ord(c) - 97] += 1
+        ans = 0
+        chars_pos = defaultdict(list)
         
-        global_max = 0
+        for i in range(len(s)):
+            chars_pos[s[i]].append(i)
         
-        for i in range(26):
-            for j in range(26):
-                if i == j or counter[i] == 0 or counter[j] == 0:
+        for major in chars_pos.keys():
+            for minor in chars_pos.keys():
+                if major == minor:
                     continue
+                major_idx = minor_idx = major_count = minor_count = 0
+                major_len, minor_len = len(chars_pos[major]), len(chars_pos[minor])
                 
-                major, minor = chr(97 + i), chr(97 + j)
-                major_count, minor_count = 0, 0
-                
-                rest_minor = counter[j]
-                for c in s:
-                    if c == major:
+                while major_idx < major_len or minor_idx < minor_len:
+                    if major_idx < major_len and (minor_idx == minor_len or chars_pos[major][major_idx] < chars_pos[minor][minor_idx]):
                         major_count += 1
-                    if c == minor:
+                        major_idx += 1
+                    elif minor_idx < minor_len:
                         minor_count += 1
-                        rest_minor -= 1
+                        minor_idx += 1
                     
-                    if minor_count > 0:
-                        global_max = max(global_max, major_count - minor_count)
-                    
-                    if major_count < minor_count and rest_minor > 0:
-                        major_count, minor_count = 0, 0
-        
-        return global_max
-                        
+                    if minor_count < major_count and major_idx < major_len:
+                        major_count = minor_count = 0
+                    if major_count > 0 < minor_count:
+                        ans = max(ans, minor_count - major_count)
+        return ans
