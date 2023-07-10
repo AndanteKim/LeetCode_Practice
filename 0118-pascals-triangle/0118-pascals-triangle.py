13c0384c29cc7a4cf982d11c54e5dcb74bdc900c
Andrew Kim <ksb0724@utexas.edu>
2023-07-10 22:55:08 +0900

    Time: 57 ms (12.68%), Space: 16.6 MB (7.60%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,12 +1,18 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows <= 2:
-            return [[1]] if numRows == 1 else [[1], [1,1]]
+        @lru_cache(maxsize = None)
+        def pascal(i: int, j: int) -> int:
+            if j == 1 or i == 1 or i == j:
+                return 1
+            return pascal(i - 1, j - 1) + pascal(i, j - 1)
         
-        dp = [[1], [1, 1]]
-        for i in range(3, numRows + 1):
-            curr = [1] * i
-            for j in range(1, i - 1):
-                curr[j] = dp[i - 2][j - 1] + dp[i - 2][j]
-            dp.append(curr)
+        dp = []
+        
+        for j in range(1, numRows + 1):
+            row = []
+            for i in range(1, j + 1):
+                row.append(pascal(i, j))
+            dp.append(row)
         return dp
+        
+        
