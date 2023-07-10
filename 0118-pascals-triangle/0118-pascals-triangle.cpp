f032f1ea703678554f63c5efa1319b4645d8ba67
Andrew Kim <ksb0724@utexas.edu>
2023-07-10 23:09:10 +0900

    Time: 4 ms (35.48%), Space: 7.1 MB (12.74%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,14 +1,22 @@
 class Solution {
+private:
+    int pascal(int i, int j, map<pair<int, int>, int>& memo){
+        if (j == 1 || i == 1 || j == i) return 1;
+        
+        if (memo.find({i, j}) != memo.end()) return memo[{i, j}];
+        
+        return memo[{i, j}] = pascal(i - 1, j - 1, memo) + pascal(i, j - 1, memo);
+    }
+    
 public:
     vector<vector<int>> generate(int numRows) {
-        if (numRows <= 2) return numRows == 1? vector<vector<int>>{{1}} : vector<vector<int>>{{1}, {1, 1}};
-        vector<vector<int>> dp{{{1}, {1, 1}}};
-        for (int i = 3; i < numRows + 1; ++i){
-            vector<int> curr(i, 1);
-            for (int j = 1; j < i - 1; ++j){
-                curr[j] = dp[i - 2][j - 1] + dp[i - 2][j];
-            }
-            dp.push_back(curr);
+        map<pair<int, int>, int> memo;
+        
+        vector<vector<int>> dp;
+        for (int j = 1; j < numRows + 1; ++j){
+            vector<int> row;
+            for (int i = 1; i <= j; ++i) row.push_back(pascal(i, j, memo));
+            dp.push_back(row);
         }
         
         return dp;
