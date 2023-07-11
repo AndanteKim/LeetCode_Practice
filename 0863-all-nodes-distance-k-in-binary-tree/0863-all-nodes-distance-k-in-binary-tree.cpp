bf7d4d89aa2d0c29dabe61045069fd3bf40720ec
Andrew Kim <ksb0724@utexas.edu>
2023-07-11 17:34:50 +0900

    Time: 11 ms (27.68%), Space: 13.4 MB (23.50%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -8,43 +8,44 @@
  * };
  */
 class Solution {
-    int k;
-    int dfs(TreeNode* node, vector<int>& ans, TreeNode* target){
-        if (!node) return -1;
-        else if (node == target){
-            subtree_add(node, 0, ans);
-            return 1;
-        }
-        else{
-            int L = dfs(node -> left, ans, target), R = dfs(node -> right, ans, target);
-            if (L != -1){
-                if (L == this -> k) ans.push_back(node -> val);
-                subtree_add(node -> right, L + 1, ans);
-                return L + 1;
-            }
-            else if (R != -1){
-                if (R == this -> k) ans.push_back(node -> val);
-                subtree_add(node -> left, R + 1, ans);
-                return R + 1;
-            }
-            else return -1;
-        }
-    }
-    
-    void subtree_add(TreeNode* node, int dist, vector<int>& ans){
-        if (!node) return;
-        else if (dist == this -> k) ans.push_back(node -> val);
-        else{
-            subtree_add(node -> left, dist + 1, ans);
-            subtree_add(node -> right, dist + 1, ans);
-        }
-    }
-    
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        if (k == 0) return vector<int>{target -> val};
+        unordered_map<TreeNode*, vector<TreeNode*>> graph;
+        queue<pair<TreeNode*, TreeNode*>> graphQ;
+        graphQ.push({root, nullptr});
+        unordered_set<int> visited;
+        visited.insert(target -> val);
+        while (!graphQ.empty()){
+            auto [child, parent] = graphQ.front();
+            graphQ.pop();
+            graph[child].push_back(parent);
+            graph[parent].push_back(child);
+            
+            if (child -> left) graphQ.push({child -> left, child});
+            if (child -> right) graphQ.push({child -> right, child});
+        }
+        
         vector<int> ans;
-        this -> k = k;
-        dfs(root, ans, target);
+        queue<pair<TreeNode*, int>> q;
+        q.push({target, 0});
+        while (!q.empty()){
+            auto [node, level] = q.front();
+            q.pop();
+            
+            if (level == k) {
+                ans.push_back(node -> val);
+                continue;
+            }
+            
+            for (TreeNode* neighbor : graph[node]){
+                if (neighbor && visited.find(neighbor -> val) == visited.end()){
+                    visited.insert(neighbor -> val);
+                    q.push({neighbor, level + 1});
+                }
+            }
+        }
+        
         return ans;
     }
 };
