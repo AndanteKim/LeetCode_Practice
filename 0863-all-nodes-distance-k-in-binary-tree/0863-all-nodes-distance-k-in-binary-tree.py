ec03a21cd39437a7c273f93b3fc3bd8da530ccf3
Andrew Kim <ksb0724@utexas.edu>
2023-07-11 17:05:58 +0900

    Time: 38 ms (99.59%), Space: 16.5 MB (99.71%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -6,39 +6,35 @@
 #         self.right = None
 
 class Solution:
-    def dfs(self, node: TreeNode, ans: List[int], target: TreeNode) -> int:
-        if not node:
-            return -1
-        elif node == target:
-            self.subtree_add(node, 0, ans)
-            return 1
-        else:
-            L, R = self.dfs(node.left, ans, target), self.dfs(node.right, ans, target)
-            if L != -1:
-                if L == self.k:
-                    ans.append(node.val)
-                self.subtree_add(node.right, L + 1, ans)
-                return L + 1
-            elif R != -1:
-                if R == self.k:
-                    ans.append(node.val)
-                self.subtree_add(node.left, R + 1, ans)
-                return R + 1
-            else:
-                return -1
-            
-    def subtree_add(self, node: TreeNode, dist: int, ans: List[int]) -> None:
-        if not node:
-            return
-        elif dist == self.k:
-            ans.append(node.val)
-        else:
-            self.subtree_add(node.left, dist + 1, ans)
-            self.subtree_add(node.right, dist + 1, ans)
-    
     def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
-        ans = []
-        self.k = k
-        self.dfs(root, ans, target)
+        if k == 0:
+            return [target.val]
+        
+        graph, ans = defaultdict(list), []
+        
+        queue = deque([(root, None)])
+        while queue:
+            node, parent = queue.popleft()
+            graph[node].append(parent)
+            graph[parent].append(node)
+            
+            if node.left:
+                queue.append((node.left, node))
+            if node.right:
+                queue.append((node.right, node))
+        
+        queue, visited = deque([(target, 0)]), set([target.val])
+        while queue:
+            node, level = queue.popleft()
+            if level == k:
+                ans.append(node.val)
+                continue
+            
+            for neighbor in graph[node]:
+                if neighbor and neighbor.val not in visited:
+                    visited.add(neighbor.val)
+                    queue.append((neighbor, level + 1))
         
         return ans
+        
+        
