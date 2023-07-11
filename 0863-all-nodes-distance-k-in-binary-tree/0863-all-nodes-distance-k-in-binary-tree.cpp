0546e55cb1e740224931fbc96ea49359247f0d40
Andrew Kim <ksb0724@utexas.edu>
2023-07-11 18:54:44 +0900

    Time: 13 ms (13.98%), Space: 13.5 MB (22.08%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -8,44 +8,41 @@
  * };
  */
 class Solution {
+private:
+    int k;
+    void buildGraph(TreeNode* child, TreeNode* parent, unordered_map<int, vector<int>>& graph){
+        if (child && parent){
+            graph[child -> val].push_back(parent -> val);
+            graph[parent -> val].push_back(child -> val);
+        }
+        
+        if (child -> left) buildGraph(child -> left, child, graph);
+        if (child -> right) buildGraph(child -> right, child, graph);
+    }
+    
+    void dfs(int curr, int dist, vector<int>& ans, unordered_set<int>& visited, unordered_map<int, vector<int>>& graph){
+        if (dist == this -> k){
+            ans.push_back(curr);
+            return;
+        }
+        
+        for (int neighbor : graph[curr]){
+            if (visited.find(neighbor) == visited.end()){
+                visited.insert(neighbor);
+                dfs(neighbor, dist + 1, ans, visited, graph);
+            }
+        }
+    }
+    
 public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        if (k == 0) return vector<int>{target -> val};
-        unordered_map<TreeNode*, vector<TreeNode*>> graph;
-        queue<pair<TreeNode*, TreeNode*>> graphQ;
-        graphQ.push({root, nullptr});
-        unordered_set<int> visited;
-        visited.insert(target -> val);
-        while (!graphQ.empty()){
-            auto [child, parent] = graphQ.front();
-            graphQ.pop();
-            graph[child].push_back(parent);
-            graph[parent].push_back(child);
-            
-            if (child -> left) graphQ.push({child -> left, child});
-            if (child -> right) graphQ.push({child -> right, child});
-        }
+        this -> k = k;
+        unordered_map<int, vector<int>> graph;
+        buildGraph(root, nullptr, graph);
         
         vector<int> ans;
-        queue<pair<TreeNode*, int>> q;
-        q.push({target, 0});
-        while (!q.empty()){
-            auto [node, level] = q.front();
-            q.pop();
-            
-            if (level == k) {
-                ans.push_back(node -> val);
-                continue;
-            }
-            
-            for (TreeNode* neighbor : graph[node]){
-                if (neighbor && visited.find(neighbor -> val) == visited.end()){
-                    visited.insert(neighbor -> val);
-                    q.push({neighbor, level + 1});
-                }
-            }
-        }
-        
+        unordered_set<int> visited{{target -> val}};
+        dfs(target -> val, 0, ans, visited, graph);
         return ans;
     }
 };
