bd29ff4e31f0e8da7d37ee25b5f6d9a9ef9a3bfd
Andrew Kim <ksb0724@utexas.edu>
2023-07-12 18:33:58 +0900

    Time: 150 ms (97.90%), Space: 46.7 MB (93.58%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,35 +1,26 @@
 class Solution {
+private:
+    bool dfs(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& inStack){
+        if (inStack[node]) return true;
+        if (visited[node]) return false;
+        
+        inStack[node] = true;
+        visited[node] = true;
+        for (int neighbor : graph[node]){
+            if (dfs(neighbor, graph, visited, inStack)) return true;
+        }
+        inStack[node] = false;
+        return false;
+    }
+    
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> indegree(n);
-        vector<vector<int>> adj(n);
-        
-        for (int i = 0; i < n; ++i){
-            for (int node : graph[i]){
-                adj[node].push_back(i);
-                ++indegree[i];
-            }
-        }
-        
-        queue<int> queue;
-        
-        for (int i = 0; i < n; ++i) if (indegree[i] == 0) queue.push(i);
-        vector<bool> safe(n, false);
-        
-        while (!queue.empty()){
-            int node = queue.front();
-            queue.pop();
-            safe[node] = true;
-            
-            for (int neighbor : adj[node]){
-                --indegree[neighbor];
-                if (indegree[neighbor] == 0) queue.push(neighbor);
-            }
-        }
-        
+        vector<bool> inStack(n, false), visited(n, false);
+        for (int i = 0; i < n; ++i) dfs(i, graph, visited, inStack);
         vector<int> ans;
-        for (int i = 0; i < n; ++i) if (safe[i]) ans.push_back(i);
+        for (int i = 0; i < n; ++i) if (!inStack[i]) ans.push_back(i);
+        
         return ans;
     }
 };
