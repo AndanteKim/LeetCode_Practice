15514e1a57cd157f0d23afbaac33dec5693b4d5a
Andrew Kim <ksb0724@utexas.edu>
2023-07-14 01:39:00 +0900

    Time: 111 ms (78.73%), Space: 18.9 MB (66.71%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,25 +1,26 @@
 class Solution:
+    def dfs(self, node: int, adj: List[List[int]], visited: List[bool], inStack: List[bool]) -> bool:
+        if inStack[node]:
+            return True
+        
+        if visited[node]:
+            return False
+        
+        visited[node], inStack[node] = True, True
+        for neighbor in adj[node]:
+            if self.dfs(neighbor, adj, visited, inStack):
+                return True
+        inStack[node] = False
+        return False
+    
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indegree = [0] * numCourses
         adj = [[] for _ in range(numCourses)]
         
-        for nxt, need in prerequisites:
-            adj[need].append(nxt)
-            indegree[nxt] += 1
+        for curr, need in prerequisites:
+            adj[need].append(curr)
         
-        queue = deque()
+        visited, inStack = [False] * numCourses, [False] * numCourses
         for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
-                
-        nodesVisited = 0
-        while queue:
-            node = queue.popleft()
-            nodesVisited += 1
-            
-            for neighbor in adj[node]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-        
-        return nodesVisited == numCourses
+            if self.dfs(i, adj, visited, inStack):
+                return False
+        return True
