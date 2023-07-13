a3aba34378a5ed05a1d4aa6a5be9fce4bd375b83
Andrew Kim <ksb0724@utexas.edu>
2023-07-14 00:07:27 +0900

    Time: 25 ms (62.83%), Space: 13.3 MB (78.81%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses), required(numCourses);
-        vector<bool> finished(numCourses);
+        vector<vector<int>> adj(numCourses);
+        vector<int> indegree(numCourses);
         
         for (vector<int>& prerequisite : prerequisites){
             adj[prerequisite[1]].push_back(prerequisite[0]);
-            required[prerequisite[0]].push_back(prerequisite[1]);
+            ++indegree[prerequisite[0]];
         }
         
-        for (int numCourse = 0; numCourse < numCourses; ++numCourse){
-            if (!finished[numCourse]){
-                queue<int> queue{{numCourse}};
-                
-                while (!queue.empty()){
-                    int curr = queue.front();
-                    queue.pop();
-                    
-                    int flag = true;
-                    for (int course : required[curr]){
-                        if (!finished[course]) {
-                            flag = false;
-                            break;
-                        }
-                    }
-                    if (flag){
-                        finished[curr] = true;
-                        for (int seq : adj[curr]){
-                            if (!finished[seq]){
-                                queue.push(seq);
-                            }
-                        }
-                    }
-                }
+        queue<int> queue;
+        for (int i = 0; i < numCourses; ++i) if (indegree[i] == 0) queue.push(i);
+        
+        int nodesVisited = 0;
+        while (!queue.empty()){
+            int curr = queue.front();
+            queue.pop();
+            ++nodesVisited;
+            for (int neighbor : adj[curr]){
+                --indegree[neighbor];
+                if (!indegree[neighbor]) queue.push(neighbor);
             }
         }
         
-        return count(finished.begin(), finished.end(), true) == numCourses? true : false;
+        return nodesVisited == numCourses;
     }
 };
