7198b9ceae616a2ec42b009534eeaf914ec18d4a
Andrew Kim <ksb0724@utexas.edu>
2023-07-15 23:24:20 +0900

    Time: 457 ms (11.06%), Space: 58.1 MB (94.47%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.cpp
@@ -1,23 +1,19 @@
 class Solution {
-private:
-    int n;
-    int dfs(int currIndex, int count, vector<int>& starts, vector<vector<int>>& events, vector<vector<int>>& dp){
-        if (currIndex == this -> n || count == 0) return 0;
-        if (dp[count][currIndex] != -1) return dp[count][currIndex];
-        
-        int nextIndex = upper_bound(starts.begin(), starts.end(), events[currIndex][1]) - starts.begin();
-        dp[count][currIndex] = max(dfs(currIndex + 1, count, starts, events, dp), events[currIndex][2] + dfs(nextIndex, count - 1, starts, events, dp));
-        return dp[count][currIndex];
-    }
-    
 public:
     int maxValue(vector<vector<int>>& events, int k) {
+        int n = events.size();
+        vector<vector<int>> dp(k + 1, vector<int>(n + 1, 0));
         sort(events.begin(), events.end());
-        this -> n = events.size();
-        vector<int> starts(this -> n);
+        vector<int> starts(n, 0);
         for (int i = 0; i < n; ++i) starts[i] = events[i][0];
-        vector<vector<int>> dp(k + 1, vector<int>(n, -1));
         
-        return dfs(0, k, starts, events, dp);
+        for (int currIndex = n - 1; currIndex >= 0; --currIndex){
+            for (int count = 1; count <= k; ++count){
+                int nextIndex = upper_bound(starts.begin(), starts.end(), events[currIndex][1]) - starts.begin();
+                dp[count][currIndex] = max(dp[count][currIndex + 1], dp[count - 1][nextIndex] + events[currIndex][2]);
+            }
+        }
+        
+        return dp[k][0];
     }
 };
