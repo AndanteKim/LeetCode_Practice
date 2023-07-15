29d5b8b8c0f3ddfd845473ec14d5d3d453511a48
Andrew Kim <ksb0724@utexas.edu>
2023-07-15 22:45:43 +0900

    Time: 917 ms (80.00%), Space: 159.9 MB (45.93%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
-        events.sort(key = lambda x: x[0])
+        events.sort()
+        n = len(events)
+        starts = [start for start, end, val in events]
+        dp = [[-1] * n for _ in range(k + 1)]
         
-        @lru_cache(maxsize = None)
-        def dp(prev: int, start: int, k: int) -> int:
-            if start >= len(events) or k == 0:
+        def dfs(curr_index: int, count: int) -> int:
+            if count == 0 or curr_index == n:
                 return 0
+            if dp[count][curr_index] != -1:
+                return dp[count][curr_index]
             
-            curr = 0
-            if prev == -1 or events[prev][1] < events[start][0]:
-                curr += max(events[start][2] + dp(start, start + 1, k - 1), dp(prev, start + 1, k))
-            else:
-                curr += dp(prev, start + 1, k)
+            next_index = bisect_right(starts, events[curr_index][1])
+            dp[count][curr_index] = max(dfs(curr_index + 1, count), events[curr_index][2] + dfs(next_index, count - 1))
             
-            return curr
-            
+            return dp[count][curr_index]
         
-        return dp(-1, 0, k)
-        
+        return dfs(0, k)
