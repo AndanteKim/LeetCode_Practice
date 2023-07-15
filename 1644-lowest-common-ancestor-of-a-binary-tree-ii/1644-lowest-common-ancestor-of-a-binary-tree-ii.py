002d2f8416c9dbf1578ae64a616077ac18593481
Andrew Kim <ksb0724@utexas.edu>
2023-07-16 01:30:57 +0900

    Time: 292 ms (72.13%), Space: 30.4 MB (21.52%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -6,49 +6,30 @@
 #         self.right = None
 
 class Solution:
+    def dfs(self, node: 'TreeNode', target: 'TreeNode') -> bool:
+        if node == target:
+            return True
+        if not node:
+            return False
+        return self.dfs(node.left, target) or self.dfs(node.right, target)
+    
+    def LCA(self, node: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not node or node == p or node == q:
+            return node
+        
+        left = self.LCA(node.left, p, q)
+        right = self.LCA(node.right, p, q)
+        if left and right:
+            return node
+        elif left:
+            return left
+        return right
+    
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        parents = defaultdict(lambda: None)
+        ans = self.LCA(root, p, q)
         
-        queue = deque([(root, None)])
-        
-        while queue:
-            node, parent = queue.popleft()
-            parents[node] = parent
-            
-            if node.left:
-                queue.append((node.left, node))
-            
-            if node.right:
-                queue.append((node.right, node))
-            
-        stack = [p]
-        while stack:
-            node = stack.pop()    
-            if node == q:
-                return p
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-        
-        stack = [q]
-        while stack:
-            node = stack.pop()
-            if node == p:
-                return q
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-        
-        p_copy = parents[p]
-        while p_copy:
-            q_copy = parents[q]
-            while q_copy and p_copy != q_copy:
-                q_copy = parents[q_copy]
-            if p_copy == q_copy:
-                break
-            p_copy = parents[p_copy]
-            
-        return p_copy
-            
+        if ans == p:
+            return p if self.dfs(p, q) else None
+        elif ans == q:
+            return q if self.dfs(q, p) else None
+        return ans
