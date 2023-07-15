83fab5e69c2b5a2238132d443bf859b3ea890511
Andrew Kim <ksb0724@utexas.edu>
2023-07-16 01:56:48 +0900

    Time: 307 ms (35.21%), Space: 30.3 MB (52.57%) - LeetHub

--- 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
+++ 1644-lowest-common-ancestor-of-a-binary-tree-ii/1644-lowest-common-ancestor-of-a-binary-tree-ii.py
@@ -6,30 +6,26 @@
 #         self.right = None
 
 class Solution:
-    def dfs(self, node: 'TreeNode', target: 'TreeNode') -> bool:
-        if node == target:
-            return True
+    nodes_found = False
+    def dfs(self, node: 'TreeNode', p, q) -> 'TreeNode':
         if not node:
-            return False
-        return self.dfs(node.left, target) or self.dfs(node.right, target)
-    
-    def LCA(self, node: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if not node or node == p or node == q:
             return node
         
-        left = self.LCA(node.left, p, q)
-        right = self.LCA(node.right, p, q)
-        if left and right:
+        left, right = self.dfs(node.left, p, q), self.dfs(node.right, p, q)
+        conditions = 0
+        if node in (p, q):
+            conditions += 1
+        if left:
+            conditions += 1
+        if right:
+            conditions += 1
+        if conditions == 2:
+            self.nodes_found = True
+        
+        if (left and right) or node in (p, q):
             return node
-        elif left:
-            return left
-        return right
+        return left or right
     
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        ans = self.LCA(root, p, q)
-        
-        if ans == p:
-            return p if self.dfs(p, q) else None
-        elif ans == q:
-            return q if self.dfs(q, p) else None
-        return ans
+        ans = self.dfs(root, p, q)
+        return ans if self.nodes_found else None
