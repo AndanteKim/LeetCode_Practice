28b37c631b1a31d430af2206d5d65f58460bfd2e
Andrew Kim <ksb0724@utexas.edu>
2023-07-16 18:21:51 +0900

    Time: 635 ms (36.71%), Space: 18 MB (67.09%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
@@ -4,27 +4,28 @@ class Solution:
         skill_id = dict()
         for i, skill in enumerate(req_skills):
             skill_id[skill] = i
+            
         skills_mask_of_person = [0] * n
-        
         for i in range(n):
             for skill in people[i]:
                 skills_mask_of_person[i] |= 1 << skill_id[skill]
-        dp = [(1 << n) - 1] * (1 << m)
+        dp = [-1] * (1 << m)
         dp[0] = 0
         
-        for skills_mask in range(1, 1 << m):
+        def f(skills_mask: int) -> int:
+            if dp[skills_mask] != -1:
+                return dp[skills_mask]
             for i in range(n):
-                smaller_skills_mask = skills_mask & ~skills_mask_of_person[i]
-                if smaller_skills_mask != skills_mask:
-                    people_mask = dp[smaller_skills_mask] | (1 << i)
-                    if people_mask.bit_count() < dp[skills_mask].bit_count():
+                new_skills_mask = skills_mask & ~skills_mask_of_person[i]
+                if new_skills_mask != skills_mask:
+                    people_mask = f(new_skills_mask) | (1 << i)
+                    if (dp[skills_mask] == -1 or people_mask.bit_count() < dp[skills_mask].bit_count()):
                         dp[skills_mask] = people_mask
-        
-        answer_mask = dp[(1 << m) - 1]
+            return dp[skills_mask]
+                                                                      
+        answer_mask = f((1 << m) - 1)
         ans = []
-        
         for i in range(n):
             if (answer_mask >> i) & 1:
                 ans.append(i)
-                
         return ans
