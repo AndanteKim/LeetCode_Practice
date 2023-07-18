3928d7f5ecc5f01825c37c81f5251d883715eaac
Andrew Kim <ksb0724@utexas.edu>
2023-07-18 18:58:08 +0900

    Time: 503 ms (62.58%), Space: 169.6 MB (67.25%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,43 +1,61 @@
+struct Node{
+    int key;
+    int val;
+    Node* next;
+    Node* prev;
+    Node(int key, int val) : key(key), val(val), next(nullptr), prev(nullptr){}
+};
+
 class LRUCache {
 private:
     int capacity;
-    unordered_map<int, list<pair<int, int>>::iterator> dic;
-    list<pair<int, int>> lru;
+    unordered_map<int, Node*> dic;
+    Node *head = new Node(-1, -1);
+    Node *tail = new Node(-1, -1);    
     
 public:
     LRUCache(int capacity) {
         this -> capacity = capacity;
+        head -> next = tail;
+        tail -> prev = head;
     }
     
     int get(int key) {
-        auto it = dic.find(key);
-        if (it == dic.end()) return -1;
-        
-        int value = it -> second -> second;
-        lru.erase(it -> second);
-        lru.push_front({key, value});
-        
-        dic.erase(it);
-        dic[key] = lru.begin();
-        return value;
+        if (dic.find(key) == dic.end()) return -1;
+        Node *node = dic[key];
+        remove(node);
+        add(node);
+        return node -> val;
     }
     
     void put(int key, int value) {
-        auto it = dic.find(key);
-        
-        if (it != dic.end()){
-            lru.erase(it -> second);
-            dic.erase(it);
+        if (dic.find(key) != dic.end()){
+            Node *oldNode = dic[key];
+            remove(oldNode);
         }
         
-        lru.push_front({key, value});
-        dic[key] = lru.begin();
+        Node *node = new Node(key, value);
+        dic[key] = node;
+        add(node);
         
         if (dic.size() > capacity){
-            auto it = dic.find(lru.rbegin() -> first);
-            dic.erase(it);
-            lru.pop_back();
+            Node* nodeToDelete = head -> next;
+            remove(nodeToDelete);
+            dic.erase(nodeToDelete -> key);
         }
+    }
+    
+    void add(Node *node){
+        Node *prevEnd = tail -> prev;
+        prevEnd -> next = node;
+        node -> prev = prevEnd;
+        node -> next = tail;
+        tail -> prev = node;
+    }
+    
+    void remove(Node *node){
+        node -> prev -> next = node -> next;
+        node -> next -> prev = node -> prev;
     }
 };
 
