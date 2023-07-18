74a2dc7c0f040afcaff880210054606100c08a02
Andrew Kim <ksb0724@utexas.edu>
2023-07-19 01:20:58 +0900

    Time: 554 ms (34.63%), Space: 30.9 MB (48.71%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,19 +1,20 @@
 class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
+    def dp(self, i: int, remain: int, memo: List[List[int]], coins: List[int]) -> int:
+        if remain == 0:
+            return 1
+        if i == len(coins):
+            return 0
         
-        @lru_cache(maxsize = None)
-        def dp(remain: int, i: int, n: int) -> int:
-            if remain < 0 or i >= n:
-                return 0
-            if remain == 0:
-                return 1
-            
-            ways = 0
-            if coins[i] <= amount:
-                ways = dp(remain - coins[i], i, n) + dp(remain, i + 1, n)
-            else:
-                ways = dp(remain, i + 1, n)
-            return ways
-                    
-        n = len(coins)
-        return dp(amount, 0, n)
+        if memo[i][remain] != -1:
+            return memo[i][remain]
+        
+        if coins[i] > remain:
+            memo[i][remain] = self.dp(i + 1, remain, memo, coins)
+        else:
+            memo[i][remain] = self.dp(i, remain - coins[i], memo, coins) + self.dp(i + 1, remain, memo, coins)
+        return memo[i][remain]
+    
+    def change(self, amount: int, coins: List[int]) -> int:
+        memo = [[-1] * (amount + 1) for _ in range(len(coins))]
+        
+        return self.dp(0, amount, memo, coins)
