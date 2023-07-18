8a8b00e5b0a445be138d8810da3f881e90164b70
Andrew Kim <ksb0724@utexas.edu>
2023-07-19 01:50:51 +0900

    Time: 46 ms (42.50%), Space: 18.4 MB (48.90%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,24 +1,16 @@
 class Solution {
-private:
-    int n;
-    int dp(int i, int remain, vector<vector<int>>& memo, vector<int>& coins){
-        if (remain == 0) return 1;
-        if (i == this -> n) return 0;
-        if (memo[i][remain] != -1) return memo[i][remain];
-        
-        if (coins[i] > remain)
-            memo[i][remain] = dp(i + 1, remain, memo, coins);
-        else
-            memo[i][remain] = dp(i, remain - coins[i], memo, coins) + dp(i + 1, remain, memo, coins);
-            
-        return memo[i][remain];
-    }
-    
 public:
     int change(int amount, vector<int>& coins) {
-        this -> n = coins.size();
-        vector<vector<int>> memo(this -> n, vector<int> (amount + 1, -1));
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, 0));
         
-        return dp(0, amount, memo, coins);
+        for (int i = 0; i < n; ++i) dp[i][0] = 1;
+        for (int i = n - 1; i >= 0; --i){
+            for (int j = 1; j <= amount; ++j){
+                if (coins[i] > j) dp[i][j] = dp[i + 1][j];
+                else dp[i][j] = dp[i + 1][j] + dp[i][j - coins[i]];
+            }
+        }
+        return dp[0][amount];
     }
 };
