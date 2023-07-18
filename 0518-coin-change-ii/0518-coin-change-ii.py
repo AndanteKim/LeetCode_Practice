65f869175df1189ae7083a7cad086d2db7085cd2
Andrew Kim <ksb0724@utexas.edu>
2023-07-19 01:44:16 +0900

    Time: 376 ms (53.65%), Space: 25.9 MB (57.02%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,20 +1,15 @@
 class Solution:
-    def dp(self, i: int, remain: int, memo: List[List[int]], coins: List[int]) -> int:
-        if remain == 0:
-            return 1
-        if i == len(coins):
-            return 0
-        
-        if memo[i][remain] != -1:
-            return memo[i][remain]
-        
-        if coins[i] > remain:
-            memo[i][remain] = self.dp(i + 1, remain, memo, coins)
-        else:
-            memo[i][remain] = self.dp(i, remain - coins[i], memo, coins) + self.dp(i + 1, remain, memo, coins)
-        return memo[i][remain]
-    
     def change(self, amount: int, coins: List[int]) -> int:
-        memo = [[-1] * (amount + 1) for _ in range(len(coins))]
+        n = len(coins)
+        dp = [[0] * (amount + 1) for _ in range(n + 1)]
         
-        return self.dp(0, amount, memo, coins)
+        for i in range(n):
+            dp[i][0] = 1
+            
+        for i in range(n - 1, -1, -1):
+            for j in range(1, amount + 1):
+                if coins[i] > j:
+                    dp[i][j] = dp[i + 1][j]
+                else:
+                    dp[i][j] = dp[i + 1][j] + dp[i][j - coins[i]]
+        return dp[0][amount]
