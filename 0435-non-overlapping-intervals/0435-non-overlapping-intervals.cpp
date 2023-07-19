56cc19253984652b5c3426905135c75a251351a9
Andrew Kim <ksb0724@utexas.edu>
2023-07-19 17:50:50 +0900

    Time: 829 ms (5.03%), Space: 142.7 MB (5.14%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,14 +1,26 @@
 class Solution {
+private:
+    int n;
+    int f(int i, vector<vector<int>>& intervals, vector<int>& dp){
+        if (i == this -> n) return 0;
+        if (dp[i] != -1) return dp[i];
+        
+        int notTake = 0 + f(i + 1, intervals, dp);
+        
+        int currStart = intervals[i][0], currEnd = intervals[i][1];
+        int idx = lower_bound(intervals.begin() + i + 1, intervals.end(), vector<int>{currEnd, currEnd}) - intervals.begin();
+        int take = 1 + f(idx, intervals, dp);
+        
+        return dp[i] = max(take, notTake);
+    }
+    
+    
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end(), [](vector<int>& i1, vector<int>& i2){return i1[1] < i2[1];});
+        this -> n = intervals.size();
+        sort(intervals.begin(), intervals.end());
         
-        int k = INT_MIN, ans = 0;
-        for (vector<int>& interval : intervals){
-            if (interval[0] >= k) k = interval[1];
-            else ++ans;
-        }
-        
-        return ans;
+        vector<int> dp(n, -1);
+        return this -> n - f(0, intervals, dp);
     }
 };
