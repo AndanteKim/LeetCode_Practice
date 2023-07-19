5ea28acceb680be1ef9d369e732a2b382fbdc2fb
Andrew Kim <ksb0724@utexas.edu>
2023-07-20 00:31:15 +0900

    Time: 0 ms (100.00%), Space: 7.8 MB (85.83%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,26 +1,26 @@
 class Solution {
-public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_set<string> words;
-        vector<bool> seen(s.size(), false);
-        for (string& word : wordDict) words.insert(word);
+private:
+    bool dp(int i, string& s, vector<string>& wordDict, vector<int>& memo){
+        if (i < 0) return true;
+        if (memo[i] != -1) return memo[i] == 1;
         
-        queue<int> queue{{0}};
-        
-        while (!queue.empty()){
-            int i = queue.front();
-            queue.pop();
-            if (i == s.size()) return true;
-            
-            for (int end = i + 1; end <= s.size(); ++end){
-                if (seen[end]) continue;
-                if (words.find(s.substr(i, end - i)) != words.end()){
-                    seen[end] = true;
-                    queue.push(end);
-                }
+        for (string& word : wordDict){
+            int currSize = word.size();
+            if (i - currSize + 1 < 0) continue;
+            if (s.substr(i - currSize + 1, currSize) == word && dp(i - currSize, s, wordDict, memo)){
+                memo[i] = 1;
+                return true;
             }
         }
         
+        memo[i] = 0;
         return false;
+    }
+    
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        vector<int> memo(s.size(), -1);
+        return dp(s.size() - 1, s, wordDict, memo);
     }
 };
