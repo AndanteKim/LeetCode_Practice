b94d0a45bb1dec84501f5db4e96b4e6163301c8f
Andrew Kim <ksb0724@utexas.edu>
2023-07-19 17:50:30 +0900

    Time: 2215 ms (5.02%), Space: 142.5 MB (12.86%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -1,14 +1,25 @@
 class Solution:
+    def dfs(self, i: int, intervals: List[List[int]], dp: List[int]) -> int:
+        if i == self.n:
+            return 0
+        
+        if dp[i] != -1:
+            return dp[i]
+        
+        not_take = 0 + self.dfs(i + 1, intervals, dp)
+        start, end = intervals[i]
+        
+        temp = [end, end]
+        idx = bisect.bisect_left(intervals, temp, i + 1, self.n)
+        
+        take = 1 + self.dfs(idx, intervals, dp)
+        dp[i] = max(take, not_take)
+        return max(take, not_take)
+    
+    
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        intervals.sort(key = lambda x: x[1])
+        self.n = len(intervals)
+        intervals.sort()
+        dp = [-1] * self.n
         
-        ans = 0
-        k = float('-inf')
-        
-        for x, y in intervals:
-            if x >= k:
-                k = y
-            else:
-                ans += 1
-        return ans
-        
+        return self.n - self.dfs(0, intervals, dp);
