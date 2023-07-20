bad007cf403e4882448b0b40cfc3615c4f65c3bd
Andrew Kim <ksb0724@utexas.edu>
2023-07-20 21:31:30 +0900

    Time: 12 ms (56.97%), Space: 9.9 MB (56.93%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,16 +1,33 @@
+struct TrieNode{
+    bool isWord;
+    unordered_map<char, TrieNode*> children;
+    TrieNode() : isWord(false), children(unordered_map<char, TrieNode*>()) {}
+};
+
+
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool> dp(s.size(), false);
+        TrieNode* root = new TrieNode();
+        for (string& word : wordDict){
+            TrieNode* curr = root;
+            for (char& c : word){
+                if (curr -> children.find(c) == curr -> children.end()) curr -> children[c] = new TrieNode();
+                curr = curr -> children[c];
+            }
+            curr -> isWord = true;
+        }
         
+        vector<bool> dp(s.size());
         for (int i = 0; i < s.size(); ++i){
-            for (string& word : wordDict){
-                if (i < word.size() - 1) continue;
-                if (i == word.size() - 1 || dp[i - word.size()]){
-                    if (s.substr(i + 1 - word.size(), word.size()) == word){
-                        dp[i] = true;
-                        break;
-                    }
+            if (i == 0 || dp[i - 1]){
+                TrieNode* curr = root;
+                for (int j = i; j < s.size(); ++j){
+                    char c = s[j];
+                    if (curr -> children.find(c) == curr -> children.end()) break;
+                
+                    curr = curr -> children[c];
+                    if (curr -> isWord) dp[j] = true;
                 }
             }
         }
