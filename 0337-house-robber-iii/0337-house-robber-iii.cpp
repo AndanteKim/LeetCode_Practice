a53d51d0c7eda0d7ca437d950e109e8b05fbe310
Andrew Kim <ksb0724@utexas.edu>
2023-07-21 01:26:00 +0900

    Time: 19 ms (63.88%), Space: 25.4 MB (38.17%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.cpp
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -10,29 +10,43 @@
  * };
  */
 class Solution {
-private:
-    int dfs(TreeNode* node, bool parentRobbed, unordered_map<TreeNode*, int>& memo, unordered_map<TreeNode*, int>& notMemo){
-        if(!node) return 0;
-        int result = 0;
-        if (parentRobbed){
-            if (memo.find(node) != memo.end()) return memo[node];
-            result = dfs(node -> left, false, memo, notMemo) + dfs(node -> right, false, memo, notMemo);
-            memo[node] = result;
-        }
-        else{
-            if (notMemo.find(node) != notMemo.end()) return notMemo[node];
-            int rob = node -> val + dfs(node -> left, true, memo, notMemo) + dfs(node -> right, true, memo, notMemo);
-            int notRob = dfs(node -> left, false, memo, notMemo) + dfs(node -> right, false, memo, notMemo);
-            result = max(rob, notRob);
-            notMemo[node] = result;
-        }
-        
-        return result;
-    }
-    
 public:
     int rob(TreeNode* root) {
-        unordered_map<TreeNode*, int> memo, notMemo;
-        return dfs(root, false, memo, notMemo);
+        if (!root) return 0;
+        vector<int> tree;
+        map<int, vector<int>> graph;
+        graph[-1] = {};
+        int index = -1;
+        
+        queue<pair<TreeNode*, int>> queue;
+        queue.push({root, -1});
+        while (!queue.empty()){
+            auto [node, parentIndex] = queue.front();
+            queue.pop();
+            
+            if (node){
+                ++index;
+                tree.push_back(node -> val);
+                graph[index] = {};
+                graph[parentIndex].push_back(index);
+                queue.push({node -> left, index});
+                queue.push({node -> right, index});
+            }
+        }
+        
+        vector<int> dpRob(index + 1), dpNotRob(index + 1);
+        for (int i = index; i >= 0; --i){
+            if (graph[i].empty()){
+                dpRob[i] = tree[i];
+                dpNotRob[i] = 0;
+            }
+            else{
+                dpRob[i] = tree[i];
+                for (int child : graph[i]) dpRob[i] += dpNotRob[child];
+                for (int child : graph[i]) dpNotRob[i] += max(dpRob[child], dpNotRob[child]);
+            }
+        }
+        
+        return max(dpRob[0], dpNotRob[0]);
     }
 };
