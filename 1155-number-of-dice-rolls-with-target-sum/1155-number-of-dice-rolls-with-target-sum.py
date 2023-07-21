4bbe73e9f55aa168f0e1fe9784a190c53f5dc6ea
Andrew Kim <ksb0724@utexas.edu>
2023-07-21 22:52:43 +0900

    Time: 462 ms (57.04%), Space: 16.8 MB (87.73%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.py
@@ -1,18 +1,20 @@
 class Solution:
+    MOD = int(10 ** 9 + 7)
+    def dp(self, memo: List[List[int]], dice_idx: int, n: int, curr_sum: int, target: int, k: int) -> int:
+        if (dice_idx == n):
+            return int(curr_sum == target)
+        
+        if memo[dice_idx][curr_sum] != -1:
+            return memo[dice_idx][curr_sum]
+        
+        ways = 0
+        for i in range(1, min(k, target - curr_sum) + 1):
+            ways = (ways + self.dp(memo, dice_idx + 1, n, curr_sum + i, target, k)) % self.MOD
+        
+        memo[dice_idx][curr_sum] = ways
+        return memo[dice_idx][curr_sum]
+    
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        memo = [[-1] * (target + 1) for _ in range(n + 1)]
+        return self.dp(memo, 0, n, 0, target, k);
         
-        @lru_cache(maxsize = None)
-        def dp(currSum: int, remain: int, dice_max: int) -> int:
-            if remain == 0:
-                if currSum == target:
-                    return 1
-                return 0
-            
-            ways = 0
-            for i in range(1, dice_max):
-                ways = (ways + dp(currSum + i, remain - 1, dice_max)) % (10 ** 9 + 7)
-            
-            return ways
-            
-        
-        return dp(0, n, k + 1) % (10 ** 9 + 7)
