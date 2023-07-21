17e630da2e8ff0dd856cd9e34bd294ae63f79508
Andrew Kim <ksb0724@utexas.edu>
2023-07-21 23:30:12 +0900

    Time: 51 ms (40.13%), Space: 8.4 MB (47.39%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,23 +1,19 @@
 class Solution {
 private:
-    const int MOD = 1e9 + 7;
-    int dp(vector<vector<int>>& memo, int diceIndex, int n, int currSum, int target, int k){
-        if (diceIndex == n){
-            return currSum == target;
-        }
-        
-        if (memo[diceIndex][currSum] != -1) return memo[diceIndex][currSum];
-        
-        int ways = 0;
-        for (int i = 1; i <= min(k, target - currSum); ++i)
-            ways = (ways + dp(memo, diceIndex + 1, n, currSum + i, target, k)) % MOD;
-        
-        return memo[diceIndex][currSum] = ways;
-    }
-    
+    int MOD = 1'000'000'007;
 public:
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> memo(n + 1, vector<int>(target + 1, -1));
-        return dp(memo, 0, n, 0, target, k);
+        vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
+        dp[n][target] = 1;
+        
+        for (int diceIndex = n - 1; diceIndex >= 0; --diceIndex){
+            for (int currSum = 0; currSum <= target; ++currSum){
+                int ways = 0;
+                for (int i = 1; i <= min(k, target - currSum); ++i)
+                    ways = (ways + dp[diceIndex + 1][currSum + i]) % MOD;
+                dp[diceIndex][currSum] = ways;
+            }
+        }
+        return dp[0][0];
     }
 };
