9d4b1e0fa829e7c9887152f0dcdd448f211b8cf4
Andrew Kim <ksb0724@utexas.edu>
2023-07-21 18:20:00 +0900

    Time: 180 ms (78.06%), Space: 13.1 MB (66.65%) - LeetHub

--- 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
+++ 0673-number-of-longest-increasing-subsequence/0673-number-of-longest-increasing-subsequence.cpp
@@ -1,26 +1,38 @@
 class Solution {
-public:
-    int findNumberOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> length(n, 1), count(n, 1);
+private:
+    void calculateDp(int i, vector<int>& nums, vector<int>& length, vector<int>& count){
+        if (length[i]) return;
         
-        for (int i = 0; i < n; ++i){
-            for (int j = 0; j < i; ++j){
-                if (nums[j] < nums[i]){
-                    if (length[j] + 1 > length[i]){
-                        length[i] = length[j] + 1;
-                        count[i] = 0;
-                    }
-                    
-                    if (length[j] + 1 == length[i]) count[i] += count[j];
+        length[i] = 1, count[i] = 1;
+        for (int j = 0; j < i; ++j){
+            if (nums[j] < nums[i]){
+                calculateDp(j, nums, length, count);
+                if (length[j] + 1 > length[i]){
+                    length[i] = length[j] + 1;
+                    count[i] = 0;
+                }
+            
+                if (length[j] + 1 == length[i]){
+                    count[i] += count[j];
                 }
             }
         }
-        
-        int maxLength = *max_element(length.begin(), length.end()), ans = 0;
+    }
+    
+public:
+    int findNumberOfLIS(vector<int>& nums) {
+        int n = nums.size(), ans = 0, maxLength = 0;
+        vector<int> length(n, 0), count(n, 0);
         
         for (int i = 0; i < n; ++i){
-            if (length[i] == maxLength) ans += count[i];
+            calculateDp(i, nums, length, count);
+            maxLength = max(maxLength, length[i]);
+        }
+        
+        for (int i = 0; i < n; ++i){
+            if (length[i] == maxLength){
+                ans += count[i];
+            }
         }
         
         return ans;
