63ed198d47a4b8f287adad4761aac2318295927e
Andrew Kim <ksb0724@utexas.edu>
2023-07-23 02:05:39 +0900

    Time: 57 ms (68.98%), Space: 8.8 MB (58.73%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -3,19 +3,35 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        vector<int> ans;
-        vector<vector<int>> dp(n);
-        
+        vector<int> dp(n);
         for (int i = 0; i < n; ++i){
-            for (int j = 0; j < i; ++j){
-                if (nums[i] % nums[j] == 0 && dp[j].size() > dp[i].size()){
-                    dp[i] = dp[j];
-                }
+            int maxSubsetSize = 0;
+            for (int k = 0; k < i; ++k){
+                if (nums[i] % nums[k] == 0) maxSubsetSize = max(maxSubsetSize, dp[k]);
             }
-            dp[i].push_back(nums[i]);
-            if (ans.size() < dp[i].size()) ans = dp[i];
+            ++maxSubsetSize;
+            dp[i] = maxSubsetSize;
         }
         
+        int maxSize = 0, maxSizeIndex = 0;
+        for (int i = 0; i < n; ++i) {
+            if (dp[i] > maxSize){
+                maxSize = max(maxSize, dp[i]);
+                maxSizeIndex = i;
+            }
+        }
+        
+        vector<int> ans;
+        int currSize = maxSize, currTail = nums[maxSizeIndex];
+        for (int i = maxSizeIndex; i >= 0; --i){
+            if (currSize == dp[i] && currTail % nums[i] == 0){
+                ans.push_back(nums[i]);
+                --currSize;
+                currTail = nums[i];
+            }
+        }
+        
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
