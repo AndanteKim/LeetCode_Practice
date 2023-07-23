aed48ec3f2be4778d6a43f991abc4a52bff986f9
Andrew Kim <ksb0724@utexas.edu>
2023-07-23 23:03:51 +0900

    Time: 98 ms (79.13%), Space: 27.9 MB (76.77%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.cpp
@@ -11,25 +11,23 @@
  */
 class Solution {
 public:
-    unordered_map<int, vector<TreeNode*>> memo;
     vector<TreeNode*> allPossibleFBT(int n) {
         if (n % 2 == 0) return {};
-        if (n == 1) return {new TreeNode()};
+        vector<vector<TreeNode*>> dp(n + 1);
+        dp[1].push_back(new TreeNode());
         
-        if (memo.find(n) != memo.end()) return memo[n];
-        
-        vector<TreeNode*> ans;
-        for (int i = 1; i < n; i += 2){
-            vector<TreeNode*> left = allPossibleFBT(i);
-            vector<TreeNode*> right = allPossibleFBT(n - 1 - i);
-            for (TreeNode *l : left){
-                for (TreeNode *r : right){
-                    TreeNode *root = new TreeNode(0, l, r);
-                    ans.push_back(root);
+        for (int count = 3; count <= n; count += 2){
+            for (int i = 1; i < count - 1; i += 2){
+                int j = count - 1 - i;
+                for (TreeNode* left : dp[i]){
+                    for (TreeNode* right : dp[j]){
+                        TreeNode *root = new TreeNode(0, left, right);
+                        dp[count].push_back(root);
+                    }
                 }
             }
         }
         
-        return ans;
+        return dp[n];
     }
 };
