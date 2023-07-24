5c03a780ed78eab4d3cb3d3b9934a6279b445386
Andrew Kim <ksb0724@utexas.edu>
2023-07-24 18:30:32 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (62.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,15 @@
+typedef long long ll;
+class Solution {
+private:
+    double binaryExp(double x, ll n){
+        if (n == 0) return 1;
+        if (n < 0) return 1.0 / binaryExp(x, -1 * n);
+        if (n % 2) return x * binaryExp(x * x, (n - 1) / 2);
+        else return binaryExp(x * x, n / 2);
+    }
+    
+public:
+    double myPow(double x, int n) {
+        return binaryExp(x, (ll) n);
+    }
+};
