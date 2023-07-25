787da2855f2742c18d4ff80c6d8e16b41d4eaee2
Andrew Kim <ksb0724@utexas.edu>
2023-07-25 21:48:39 +0900

    Time: 112 ms (93.65%), Space: 59.6 MB (88.25%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
-        return max_element(arr.begin(), arr.end()) - arr.begin();
+        int left = 0, right = arr.size();
+        
+        while (left < right){
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] < arr[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        
+        return left;
     }
 };
