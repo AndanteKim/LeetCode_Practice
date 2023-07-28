926b6c1199e87dc8e58ca1dfcd7073a7d2bd49d5
Andrew Kim <ksb0724@utexas.edu>
2023-07-28 23:51:34 +0900

    Time: 42 ms (95.15%), Space: 16.4 MB (70.15%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,14 +1,10 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
         n = len(nums)
-        dp = [[0] * n for _ in range(n)]
-        
-        for i in range(n):
-            dp[i][i] = nums[i]
+        dp = nums[:]
         
         for diff in range(1, n):
             for left in range(n - diff):
                 right = left + diff
-                dp[left][right] = max(nums[left] - dp[left + 1][right], nums[right] - dp[left][right - 1])
-        
-        return dp[0][n - 1] >= 0
+                dp[left] = max(nums[left] - dp[left + 1], nums[right] - dp[left])
+        return dp[0] >= 0
