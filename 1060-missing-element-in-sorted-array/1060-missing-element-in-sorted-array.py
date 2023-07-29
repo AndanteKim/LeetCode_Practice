c970378da851ce6918ceb52de982545c8153c50e
Andrew Kim <ksb0724@utexas.edu>
2023-07-29 22:17:55 +0900

    Time: 260 ms (99.77%), Space: 22.6 MB (85.45%) - LeetHub

--- 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
+++ 1060-missing-element-in-sorted-array/1060-missing-element-in-sorted-array.py
@@ -1,11 +1,13 @@
 class Solution:
     def missingElement(self, nums: List[int], k: int) -> int:
         n = len(nums)
+        left, right = 0, n - 1
         
-        for i in range(1, n):
-            gap = nums[i] - nums[i - 1] - 1
-            if gap >= k:
-                return nums[i - 1] + k
-            k -= gap
+        while left < right:
+            mid = right - ((right - left) >> 1)
+            if (nums[mid] - nums[0]) - mid < k:
+                left = mid
+            else:
+                right = mid - 1
         
-        return nums[n - 1] + k
+        return nums[0] + k + left
