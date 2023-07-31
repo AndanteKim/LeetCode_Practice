7b08eae3a32d5c8d70219673724f80e496686477
Andrew Kim <ksb0724@utexas.edu>
2023-07-31 23:13:05 +0900

    Time: 444 ms (95.11%), Space: 16.2 MB (98.86%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,20 +1,24 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        if len(s1) < len(s2):
+            return self.minimumDeleteSum(s2, s1)
+        
         m, n = len(s1), len(s2)
-        compute_cost = [[0] * (n + 1) for _ in range(m + 1)]
-        
-        for i in range(1, m + 1):
-            compute_cost[i][0] = ord(s1[i - 1]) + compute_cost[i - 1][0]
-        
+        curr_row = [0] * (n + 1)
         for j in range(1, n + 1):
-            compute_cost[0][j] = ord(s2[j - 1]) + compute_cost[0][j - 1]
-        
+            curr_row[j] = curr_row[j - 1] + ord(s2[j - 1])
+            
         for i in range(1, m + 1):
+            diagonal = curr_row[0]
+            curr_row[0] += ord(s1[i - 1])
+            
             for j in range(1, n + 1):
                 if s1[i - 1] == s2[j - 1]:
-                    compute_cost[i][j] = compute_cost[i - 1][j - 1]
+                    answer = diagonal
                 else:
-                    compute_cost[i][j] = min(ord(s1[i - 1]) + compute_cost[i - 1][j],\
-                                            ord(s2[j - 1]) + compute_cost[i][j - 1])
+                    answer = min(ord(s1[i - 1]) + curr_row[j], ord(s2[j - 1]) + curr_row[j - 1])
+                
+                diagonal = curr_row[j]
+                curr_row[j] = answer
+        return curr_row[-1]
         
-        return compute_cost[m][n]
