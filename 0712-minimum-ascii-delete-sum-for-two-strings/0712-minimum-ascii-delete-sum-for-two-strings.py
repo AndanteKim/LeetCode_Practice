7d9a1de7bc3067959cfad86185cd8130db3d0b40
Andrew Kim <ksb0724@utexas.edu>
2023-07-31 22:58:44 +0900

    Time: 502 ms (84.09%), Space: 21.1 MB (47.05%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,26 +1,20 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        m, n = len(s1), len(s2)
+        compute_cost = [[0] * (n + 1) for _ in range(m + 1)]
         
-        def compute_cost(i: int, j: int) -> int:
-            if i < 0 and j < 0:
-                return 0
-            
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            if i < 0:
-                dp[i][j] = ord(s2[j]) + compute_cost(i, j - 1)
-                return dp[i][j]
-            if j < 0:
-                dp[i][j] = ord(s1[i]) + compute_cost(i - 1, j)
-                return dp[i][j]
-            if s1[i] == s2[j]:
-                dp[i][j] = compute_cost(i - 1, j - 1)
-            else:
-                dp[i][j] = min(ord(s1[i]) + compute_cost(i - 1, j),\
-                               ord(s2[j]) + compute_cost(i, j - 1))
-            return dp[i][j]
-            
-        n1, n2 = len(s1), len(s2)
-        dp = [[-1] * (n2 + 1) for _ in range(n1 + 1)]
-        return compute_cost(n1 - 1, n2 - 1)
+        for i in range(1, m + 1):
+            compute_cost[i][0] = ord(s1[i - 1]) + compute_cost[i - 1][0]
+        
+        for j in range(1, n + 1):
+            compute_cost[0][j] = ord(s2[j - 1]) + compute_cost[0][j - 1]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s1[i - 1] == s2[j - 1]:
+                    compute_cost[i][j] = compute_cost[i - 1][j - 1]
+                else:
+                    compute_cost[i][j] = min(ord(s1[i - 1]) + compute_cost[i - 1][j],\
+                                            ord(s2[j - 1]) + compute_cost[i][j - 1])
+        
+        return compute_cost[m][n]
