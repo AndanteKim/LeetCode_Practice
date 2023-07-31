1e78b6b2d2cf1b163be9c97cf910127f3497e73a
Andrew Kim <ksb0724@utexas.edu>
2023-07-31 23:05:21 +0900

    Time: 32 ms (85.55%), Space: 15.2 MB (68.25%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -1,34 +1,20 @@
 class Solution {
-private:
-    int computeCost(vector<vector<int>>& dp, int i, int j, string& s1, string& s2){
-        if (i < 0 && j < 0) return 0;
-        
-        if (i < 0) {
-            int deleteCost = 0;
-            for (int ptr = 0; ptr <= j; ++ptr) deleteCost += s2[ptr];
-            return deleteCost;
-        }
-        
-        if (j < 0) {
-            int deleteCost = 0;
-            for (int ptr = 0; ptr <= i; ++ptr) deleteCost += s1[ptr];
-            return deleteCost;
-        }
-        
-        if (dp[i][j] != -1) return dp[i][j];
-        
-        if (s1[i] == s2[j]) dp[i][j] = computeCost(dp, i - 1, j - 1, s1, s2);
-        else{
-            dp[i][j] = min(s1[i] + computeCost(dp, i - 1, j, s1, s2), s2[j] + computeCost(dp, i, j - 1, s1, s2));
-        }
-        
-        return dp[i][j];
-    }
-    
 public:
     int minimumDeleteSum(string s1, string s2) {
-        int n1 = s1.size(), n2 = s2.size();
-        vector dp(n1 + 1, vector<int>(n2 + 1, -1));
-        return computeCost(dp, n1 - 1, n2 - 1, s1, s2);
+        int m = s1.size(), n = s2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 1; i <= m; ++i) dp[i][0] = dp[i - 1][0] + s1[i - 1];
+        for (int j = 1; j <= n; ++j) dp[0][j] = dp[0][j - 1] + s2[j - 1];
+        
+        for (int i = 1; i <= m; ++i){
+            for (int j = 1; j <= n; ++j){
+                if (s1[i - 1] == s2[j - 1]) dp[i][j] = dp[i - 1][j - 1];
+                else{
+                    dp[i][j] = min(s1[i - 1] + dp[i - 1][j], s2[j - 1] + dp[i][j - 1]);
+                }
+            }
+        }
+        
+        return dp[m][n];
     }
 };
