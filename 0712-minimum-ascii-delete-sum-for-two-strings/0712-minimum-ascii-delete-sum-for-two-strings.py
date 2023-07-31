3179c09ea0e793e021587ed31b61263a69da0bf2
Andrew Kim <ksb0724@utexas.edu>
2023-07-31 22:10:01 +0900

    Time: 688 ms (65.12%), Space: 49.4 MB (24.89%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.py
@@ -1,24 +1,26 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         
-        @lru_cache(maxsize = None)
         def compute_cost(i: int, j: int) -> int:
+            if i < 0 and j < 0:
+                return 0
+            
+            if dp[i][j] != -1:
+                return dp[i][j]
+            
             if i < 0:
-                delete_cost = 0
-                for pointer in range(j + 1):
-                    delete_cost += ord(s2[pointer])
-                return delete_cost
-            
+                dp[i][j] = ord(s2[j]) + compute_cost(i, j - 1)
+                return dp[i][j]
             if j < 0:
-                delete_cost = 0
-                for pointer in range(i + 1):
-                    delete_cost += ord(s1[pointer])
-                return delete_cost
-            
+                dp[i][j] = ord(s1[i]) + compute_cost(i - 1, j)
+                return dp[i][j]
             if s1[i] == s2[j]:
-                return compute_cost(i - 1, j - 1)
+                dp[i][j] = compute_cost(i - 1, j - 1)
             else:
-                return min(ord(s1[i]) + compute_cost(i - 1, j), ord(s2[j]) + compute_cost(i, j - 1), \
-                          ord(s1[i]) + ord(s2[j]) + compute_cost(i - 1, j - 1))
+                dp[i][j] = min(ord(s1[i]) + compute_cost(i - 1, j),\
+                               ord(s2[j]) + compute_cost(i, j - 1))
+            return dp[i][j]
             
-        return compute_cost(len(s1) - 1, len(s2) - 1)
+        n1, n2 = len(s1), len(s2)
+        dp = [[-1] * (n2 + 1) for _ in range(n1 + 1)]
+        return compute_cost(n1 - 1, n2 - 1)
