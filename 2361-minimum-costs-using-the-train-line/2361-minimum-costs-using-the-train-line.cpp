bcc53ecc1a749194f8cc96e12a1ad27240e6e889
Andrew Kim <ksb0724@utexas.edu>
2023-08-01 21:39:12 +0900

    Time: 280 ms (53.54%), Space: 144.5 MB (9.45%) - LeetHub

--- 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.cpp
+++ 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.cpp
@@ -1,25 +1,20 @@
 typedef long long ll;
 
 class Solution {
-private:
-    ll solve(int i, int lane, vector<vector<ll>>& dp, vector<int>& regular, vector<int>& express, int expressCost){
-        if (i < 0) return 0;
-        if (dp[i][lane] != -1) return dp[i][lane];
-        
-        ll regularLane = regular[i] + solve(i - 1, 1, dp, regular, express, expressCost);
-        ll expressLane = (lane? expressCost: 0) + express[i] + solve(i - 1, 0, dp, regular, express, expressCost);
-        dp[i][lane] = min(regularLane, expressLane);
-        return dp[i][lane];
-    }
-        
 public:
     vector<long long> minimumCosts(vector<int>& regular, vector<int>& express, int expressCost) {
-        int n = regular.size(); 
-        vector<vector<ll>> dp(n, vector<ll>(2, -1));
+        int n = regular.size();
+        vector dp(n + 1, vector<ll>(2));
         vector<ll> ans;
-        solve(n - 1, 1, dp, regular, express, expressCost);
-        for (int i = 0; i < n; ++i)
-            ans.push_back(dp[i][1]);
+        dp[0][1] = 0;
+        dp[0][0] = expressCost;
+        
+        // 1: regular lane, 0: express lane
+        for (int i = 1; i <= n; ++i){
+            dp[i][1] = regular[i - 1] + min(dp[i - 1][1], dp[i - 1][0]);
+            dp[i][0] = express[i - 1] + min(dp[i - 1][0], dp[i - 1][1] + expressCost);
+            ans.push_back(min(dp[i][1], dp[i][0]));
+        }
         
         return ans;
     }
