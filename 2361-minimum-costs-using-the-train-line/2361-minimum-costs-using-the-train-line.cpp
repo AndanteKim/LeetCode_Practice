b7d44c45fb9d75bac8a6f54faaaca69217fcc50f
Andrew Kim <ksb0724@utexas.edu>
2023-08-01 21:55:08 +0900

    Time: 189 ms (98.43%), Space: 113.9 MB (63.78%) - LeetHub

--- 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.cpp
+++ 2361-minimum-costs-using-the-train-line/2361-minimum-costs-using-the-train-line.cpp
@@ -4,16 +4,15 @@ class Solution {
 public:
     vector<long long> minimumCosts(vector<int>& regular, vector<int>& express, int expressCost) {
         int n = regular.size();
-        vector dp(n + 1, vector<ll>(2));
         vector<ll> ans;
-        dp[0][1] = 0;
-        dp[0][0] = expressCost;
+        ll prevRegularLane = 0, prevExpressLane = expressCost;
         
-        // 1: regular lane, 0: express lane
         for (int i = 1; i <= n; ++i){
-            dp[i][1] = regular[i - 1] + min(dp[i - 1][1], dp[i - 1][0]);
-            dp[i][0] = express[i - 1] + min(dp[i - 1][0], dp[i - 1][1] + expressCost);
-            ans.push_back(min(dp[i][1], dp[i][0]));
+            ll costRegularLane = regular[i - 1] + min(prevRegularLane, prevExpressLane);
+            ll costExpressLane = express[i - 1] + min(prevRegularLane + expressCost, prevExpressLane);
+            ans.push_back(min(costRegularLane, costExpressLane));
+            prevRegularLane = costRegularLane;
+            prevExpressLane = costExpressLane;
         }
         
         return ans;
