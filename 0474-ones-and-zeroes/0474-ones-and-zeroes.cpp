145ae4d6f36e2a8036dfd54651f5f25899eb322f
Andrew Kim <ksb0724@utexas.edu>
2023-08-02 17:13:33 +0900

    Time: 182 ms (93.22%), Space: 9.7 MB (86.00%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,22 +1,18 @@
 class Solution {
-private:
-    int dp(int i, vector<vector<vector<int>>>& memo, vector<string>& strs, int remM, int remN){
-        if (i < 0) return 0;
-        if (remM == 0 && remN == 0) return 0;
-        if (memo[i][remM][remN] != -1) return memo[i][remM][remN];
-        
-        int countM = count(strs[i].begin(), strs[i].end(), '0'), take = 0, notTake = 0;
-        int countN = strs[i].size() - countM;
-        if (countM <= remM && countN <= remN)
-            take = 1 + dp(i - 1, memo, strs, remM - countM, remN - countN);
-        notTake = dp(i - 1, memo, strs, remM, remN);
-        return memo[i][remM][remN] = max(take, notTake);
-    }
-    
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int length = strs.size();
-        vector memo(length, vector(m + 1, vector<int>(n + 1, -1)));
-        return dp(length - 1, memo, strs, m, n);
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        
+        for (string& s : strs){
+            int count0 = count(s.begin(), s.end(), '0');
+            int count1 = s.size() - count0;
+            
+            for (int i = m; i >= count0; --i){
+                for (int j = n; j >= count1; --j)
+                    dp[i][j] = max(1 + dp[i - count0][j - count1], dp[i][j]);
+            }
+        }
+        
+        return dp[m][n];
     }
 };
