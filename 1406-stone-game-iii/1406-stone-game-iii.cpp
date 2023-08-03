e23b55ceac646b9cf3d197dab4f237b7746f695e
Andrew Kim <ksb0724@utexas.edu>
2023-08-03 23:13:21 +0900

    Time: 175 ms (97.95%), Space: 126.4 MB (93.27%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,22 +1,15 @@
 class Solution {
-private:
-    int n, notComputed = 1'000'000'000;
-    int dp(int i, vector<int>& stoneValue, vector<int>& memo){
-        if (i == this -> n) return 0;
-        
-        if (memo[i] != notComputed) return memo[i];
-        
-        memo[i] = stoneValue[i] - dp(i + 1, stoneValue, memo);
-        if (i + 2 <= n) memo[i] = max(memo[i], stoneValue[i] + stoneValue[i + 1] - dp(i + 2, stoneValue, memo));
-        if (i + 3 <= n) memo[i] = max(memo[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp(i + 3, stoneValue, memo));
-        return memo[i];
-    }
-    
 public:
     string stoneGameIII(vector<int>& stoneValue) {
-        this -> n = stoneValue.size();
-        vector<int> memo(n + 1, notComputed);
-        int diff = dp(0, stoneValue, memo);
-        return diff > 0? "Alice": diff < 0? "Bob" : "Tie";
+        int n = stoneValue.size();
+        vector<int> dp(4);
+        
+        for (int i = n - 1; i >= 0; --i){
+            dp[i % 4] = stoneValue[i] - dp[(i + 1) % 4];
+            if (i + 2 <= n) dp[i % 4] = max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] - dp[(i + 2) % 4]);
+            if (i + 3 <= n) dp[i % 4] = max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[(i + 3) % 4]);
+        }
+        
+        return dp[0] > 0? "Alice" : dp[0] < 0? "Bob" : "Tie";
     }
 };
