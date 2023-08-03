8e6415d2db193e809bc23265b26055aa0a4d5b65
Andrew Kim <ksb0724@utexas.edu>
2023-08-03 22:42:37 +0900

    Time: 183 ms (96.71%), Space: 132.2 MB (82.09%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(4, 0);
+        vector<int> dp(n + 1);
+        
         for (int i = n - 1; i >= 0; --i){
-            dp[i % 4] = stoneValue[i] - dp[(i + 1) % 4];
-            if (i + 2 <= n)
-                dp[i % 4] = max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] - dp[(i + 2) % 4]);
-            if (i + 3 <= n)
-                dp[i % 4] = max(dp[i % 4], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[(i + 3) % 4]);
+            dp[i] = stoneValue[i] - dp[i + 1];
+            if (i + 2 <= n) dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - dp[i + 2]);
+            if (i + 3 <= n) dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - dp[i + 3]);
         }
         
-        return dp[0] > 0? "Alice" : dp[0] < 0? "Bob" : "Tie";
+        return dp[0] > 0? "Alice": dp[0] < 0? "Bob" : "Tie";
     }
 };
