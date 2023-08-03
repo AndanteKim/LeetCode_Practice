18f2f6f72921aa8986b6a02de23322c4abc44ace
Andrew Kim <ksb0724@utexas.edu>
2023-08-03 15:30:51 +0900

    Time: 3 ms (37.56%), Space: 6.4 MB (55.27%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    int n;
+    void backtrack(vector<string>& ans, int start, string& curr, string& digits, unordered_map<char, string>& letters){
+        if (curr.size() == this -> n){
+            ans.push_back(curr);
+            return;
+        }
+        
+        for (int i = start; i < this -> n; ++i){
+            for (char& letter : letters[digits[i]]){
+                curr.push_back(letter);
+                backtrack(ans, i + 1, curr, digits, letters);
+                curr.pop_back();
+            }
+        }
+        
+    }
+    
+public:
+    vector<string> letterCombinations(string digits) {
+        unordered_map<char, string> letters{{'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"},\
+        {'6', "mno"}, {'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}};
+        vector<string> ans;
+        string curr = "";
+        this -> n = digits.size();
+        if (this -> n == 0) return {};
+        backtrack(ans, 0, curr, digits, letters);
+        return ans;
+    }
+};
