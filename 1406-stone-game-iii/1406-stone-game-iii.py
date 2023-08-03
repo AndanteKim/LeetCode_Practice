f37e4227b09a81d84025b51c30bba64ab16c0556
Andrew Kim <ksb0724@utexas.edu>
2023-08-03 22:52:53 +0900

    Time: 2365 ms (82.18%), Space: 110.1 MB (50.11%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.py
+++ 1406-stone-game-iii/1406-stone-game-iii.py
@@ -1,17 +1,21 @@
 class Solution:
     def stoneGameIII(self, stoneValue: List[int]) -> str:
-        @lru_cache(maxsize = None)
         def f(i: int) -> int:
             if i == n:
                 return 0
+            if dp[i] != not_computed:
+                return dp[i]
             
-            ans = stoneValue[i] - f(i + 1)
+            dp[i] = stoneValue[i] - f(i + 1)
             if i + 2 <= n:
-                ans = max(ans, stoneValue[i] + stoneValue[i + 1] - f(i + 2))
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] - f(i + 2))
             if i + 3 <= n:
-                ans = max(ans, stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - f(i + 3))
-            return ans
+                dp[i] = max(dp[i], stoneValue[i] + stoneValue[i + 1] + stoneValue[i + 2] - f(i + 3))
+                
+            return dp[i]
         
         n = len(stoneValue)
+        not_computed = 10 ** 9
+        dp = [not_computed] * (n + 1)
         diff = f(0)
         return "Alice" if diff > 0 else "Bob" if diff < 0 else "Tie"
