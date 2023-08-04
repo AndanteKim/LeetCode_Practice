17116d065328e754b483dbd5b1041f905ad988c8
Andrew Kim <ksb0724@utexas.edu>
2023-08-04 21:57:43 +0900

    Time: 3 ms (91.38%), Space: 13.2 MB (38.95%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,40 +1,19 @@
-struct TrieNode{
-    bool isWord;
-    unordered_map<char, TrieNode*> children;
-    TrieNode() : isWord(false), children(unordered_map<char, TrieNode*>()) {}
-};
-
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n + 1);
+        dp[0] = true;
+        unordered_set<string> words(wordDict.begin(), wordDict.end());
         
-        // build Trie
-        TrieNode* root = new TrieNode();
-        for (string& word : wordDict){
-            TrieNode* curr = root;
-            for (char& c : word){
-                if (curr -> children.find(c) == curr -> children.end()) {
-                    curr -> children[c] = new TrieNode();
-                }
-                curr = curr -> children[c];
-            }
-            curr -> isWord = true;
-        }
-        
-        // find word through Trie
-        vector<bool> dp(s.size());
-        for (int i = 0; i < s.size(); ++i){
-            if (i == 0 || dp[i - 1]){
-                TrieNode* curr = root;
-                for (int j = i; j < s.size(); ++j){
-                    char c = s[j];
-                    if (curr -> children.find(c) == curr -> children.end()) break;
-                    curr = curr -> children[c];
-                    if (curr -> isWord) dp[j] = true;
+        for (int i = 1; i <= n; ++i){
+            for (int j = 0; j < i; ++j){
+                if (dp[j] && words.find(s.substr(j, i - j)) != words.end()){
+                    dp[i] = true;
+                    break;
                 }
             }
         }
-        
         return dp.back();
     }
 };
