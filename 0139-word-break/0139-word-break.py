7cb317645be5cb31764667ca84b653d6a2c30f3b
Andrew Kim <ksb0724@utexas.edu>
2023-08-04 16:32:14 +0900

    Time: 40 ms (96.36%), Space: 16.6 MB (31.53%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,29 +1,16 @@
-class TrieNode:
-    def __init__(self):
-        self.is_word = False
-        self.children = dict()
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        root = TrieNode()
         
-        for word in wordDict:
-            curr = root
-            for c in word:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-            curr.is_word = True
+        @lru_cache(maxsize = None)
+        def dp(i: int) -> bool:
+            if i < 0:
+                return True
+            
+            for word in wordDict:
+                if s[i - len(word) + 1: i + 1] == word and dp(i - len(word)):
+                    return True
+            
+            return False
+        n = len(s)
+        return dp(n - 1)
         
-        dp = [False] * len(s)
-        for i in range(len(s)):
-            if i == 0 or dp[i - 1]:
-                curr = root
-                for j in range(i, len(s)):
-                    c = s[j]
-                    if c not in curr.children:
-                        break
-                    curr = curr.children[c]
-                    if curr.is_word:
-                        dp[j] = True
-        return dp[-1]
