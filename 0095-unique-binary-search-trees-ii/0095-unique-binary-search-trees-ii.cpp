c839dd0c0387e13cc49a55b8f4d542121c053dd6
Andrew Kim <ksb0724@utexas.edu>
2023-08-05 18:20:05 +0900

    Time: 16 ms (75.53%), Space: 12.5 MB (85.13%) - LeetHub

---
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    vector<TreeNode*> allPossibleBST(int start, int end, map<pair<int, int>, vector<TreeNode*>>& memo){
+        vector<TreeNode*> ans;
+        if (start > end){
+            ans.push_back(nullptr);
+            return ans;
+        }
+        
+        if (memo.find(make_pair(start, end)) != memo.end()) return memo[make_pair(start, end)];
+        
+        for (int i = start; i <= end; ++i){
+            vector<TreeNode*> leftSubTrees = allPossibleBST(start, i - 1, memo);
+            vector<TreeNode*> rightSubTrees = allPossibleBST(i + 1, end, memo);
+            
+            for (TreeNode* left : leftSubTrees){
+                for (TreeNode* right : rightSubTrees){
+                    TreeNode* root = new TreeNode(i, left, right);
+                    ans.push_back(root);
+                }
+            }
+        }
+        return memo[make_pair(start, end)] = ans;
+    }
+    
+public:
+    vector<TreeNode*> generateTrees(int n) {
+        map<pair<int, int>, vector<TreeNode*>> memo;
+        return allPossibleBST(1, n, memo);
+    }
+};
