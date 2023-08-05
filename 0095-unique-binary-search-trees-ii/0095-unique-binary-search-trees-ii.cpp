5618150cef1b0367a1409d93fde4fb0c26a7d726
Andrew Kim <ksb0724@utexas.edu>
2023-08-05 22:18:46 +0900

    Time: 12 ms (91.60%), Space: 12.7 MB (84.22%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.cpp
@@ -10,29 +10,32 @@
  * };
  */
 class Solution {
+private:
+    TreeNode* clone(TreeNode* node, int offset){
+        if (!node) return nullptr;
+        TreeNode* clonedNode = new TreeNode(node -> val + offset);
+        clonedNode -> left = clone(node -> left, offset);
+        clonedNode -> right = clone(node -> right, offset);
+        
+        return clonedNode;
+    }
+    
 public:
     vector<TreeNode*> generateTrees(int n) {
-        vector<vector<vector<TreeNode*>>> dp(n + 1, vector(n + 1, vector<TreeNode*>(0)));
+        vector<vector<TreeNode*>> dp(n + 1);
+        dp[0].push_back(nullptr);
         
-        for (int i = 1; i <= n; ++i) dp[i][i].push_back(new TreeNode(i));
-        
-        for (int numberOfNodes = 2; numberOfNodes <= n; ++numberOfNodes){
-            for (int start = 1; start <= n - numberOfNodes + 1; ++start){
-                int end = start + numberOfNodes - 1;
-                for (int i = start; i <= end; ++i){
-                    vector<TreeNode*> leftSubTrees = i != start? dp[start][i - 1] : vector<TreeNode*>({nullptr});
-                    vector<TreeNode*> rightSubTrees = i != end? dp[i + 1][end] : vector<TreeNode*>({nullptr});
-                    
-                    for (TreeNode* left : leftSubTrees){
-                        for (TreeNode* right : rightSubTrees){
-                            TreeNode* root = new TreeNode(i, left, right);
-                            dp[start][end].push_back(root);
-                        }
+        for (int numberOfNodes = 1; numberOfNodes <= n; ++numberOfNodes){
+            for (int i = 1; i <= numberOfNodes; ++i){
+                int j = numberOfNodes - i;
+                for (TreeNode* left : dp[i - 1]){
+                    for (TreeNode* right : dp[j]){
+                        TreeNode* root = new TreeNode(i, left, clone(right, i));
+                        dp[numberOfNodes].push_back(root);
                     }
                 }
             }
         }
-        
-        return dp[1][n];
+        return dp[n];
     }
 };
