90ad506462b992e139dac9413250a747e631ecef
Andrew Kim <ksb0724@utexas.edu>
2023-08-05 22:06:44 +0900

    Time: 56 ms (94.46%), Space: 18.3 MB (20.17%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.py
@@ -6,23 +6,23 @@
 #         self.right = right
 class Solution:
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        dp = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
+        dp = [[] for _ in range(n + 1)]
+        dp[0].append(None)
         
-        for i in range(1, n + 1):
-            dp[i][i] = [TreeNode(i)]
-            
-        for numberOfNodes in range(2, n + 1):
-            for start in range(1, n - numberOfNodes + 2):
-                end = start + numberOfNodes - 1
-                for i in range(start, end + 1):
-                    left_subtrees = dp[start][i - 1] if i != start else [None]
-                    right_subtrees = dp[i + 1][end] if i != end else [None]
-                    
-                    for left in left_subtrees:
-                        for right in right_subtrees:
-                            root = TreeNode(i, left, right)
-                            dp[start][end].append(root)
+        for numberOfNodes in range(1, n + 1):
+            for i in range(1, numberOfNodes + 1):
+                j = numberOfNodes - i
+                for left in dp[i - 1]:
+                    for right in dp[j]:
+                        root = TreeNode(i, left, self.clone(right, i))
+                        dp[numberOfNodes].append(root)
         
-        return dp[1][n]
-        
+        return dp[n]
     
+    def clone(self, node: Optional[TreeNode], offset: int) -> Optional[TreeNode]:
+        if not node:
+            return None
+        cloned_node = TreeNode(node.val + offset)
+        cloned_node.left = self.clone(node.left, offset)
+        cloned_node.right = self.clone(node.right, offset)
+        return cloned_node
