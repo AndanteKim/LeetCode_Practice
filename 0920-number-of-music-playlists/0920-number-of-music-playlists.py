532c8385815eccb7b2a756248cf3f0e7ca1e7891
Andrew Kim <ksb0724@utexas.edu>
2023-08-06 22:47:41 +0900

    Time: 54 ms (84.21%), Space: 16.3 MB (90.98%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.py
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.py
@@ -1,21 +1,30 @@
 class Solution:
-    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
-        MOD = 1_000_000_007
-        dp = [[-1 for _ in range(n + 1)] for _ in range(goal + 1)]
+    MOD = 1_000_000_007
+    def power(self, base: int, exponent: int) -> int:
+        res = 1
+        while exponent > 0:
+            if exponent & 1:
+                res = (res * base) % self.MOD
+            exponent >>= 1
+            base = (base * base) % self.MOD
+        return res
+            
+    def precalculate_factorials(self, n: int) -> None:
+        self.factorial, self.inv_factorial = [1] * (n + 1), [1] * (n + 1)
+        for i in range(1, n + 1):
+            self.factorial[i] = (self.factorial[i - 1] * i) % self.MOD
+            # Fermat's Little Theorem
+            self.inv_factorial[i] = self.power(self.factorial[i], self.MOD - 2)
         
-        def number_of_playlists(i: int, j: int) -> int:
-            if i == 0 and j == 0:\
-                return 1
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        self.precalculate_factorials(n)
+        sign, ans = 1, 0
+        
+        for i in range(n, k - 1, -1):
+            temp = self.power(i - k, goal - k)
+            temp = (temp * self.inv_factorial[n - i]) % self.MOD
+            temp = (temp * self.inv_factorial[i - k]) % self.MOD
             
-            if i == 0 or j == 0:
-                return 0
-            
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            dp[i][j] = (number_of_playlists(i - 1, j - 1) * (n - j + 1)) % MOD
-            if j > k:
-                dp[i][j] += (number_of_playlists(i - 1, j) * (j - k)) % MOD
-                dp[i][j] %= MOD
-            return dp[i][j]
-        return number_of_playlists(goal, n)
+            ans = (ans + sign * temp + self.MOD) % self.MOD
+            sign *= -1
+        return (self.factorial[n] * ans) % self.MOD
