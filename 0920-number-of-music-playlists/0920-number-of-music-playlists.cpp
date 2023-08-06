9a796ad92cb03c05e32cb08168a959c89e1633e2
Andrew Kim <ksb0724@utexas.edu>
2023-08-06 22:35:41 +0900

    Time: 4 ms (75.68%), Space: 9.2 MB (17.12%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -1,20 +1,22 @@
 typedef long long ll;
 
 class Solution {
+private:
+    int MOD = 1'000'000'007, n, k;
+    ll numberOfPlaylists(int i, int j, vector<vector<ll>>& memo){
+        if (i == 0 && j == 0) return 1;
+        if (i == 0 || j == 0) return 0;
+        if (memo[i][j] != -1) return memo[i][j];
+        
+        memo[i][j] = (numberOfPlaylists(i - 1, j - 1, memo) * (n - j + 1)) % MOD;
+        if (j > k) memo[i][j] = (memo[i][j] + numberOfPlaylists(i - 1,j, memo) * (j - k)) % MOD; 
+        return memo[i][j];
+    }
+    
 public:
     int numMusicPlaylists(int n, int goal, int k) {
-        const int MOD = 1'000'000'007;
-        vector<vector<ll>> dp(goal + 1, vector<ll>(n + 1));
-        dp[0][0] = 1;
-        
-        for (int i = 1; i <= goal; ++i){
-            for (int j = 1; j <= min(i, n); ++j){
-                dp[i][j] = dp[i - 1][j - 1] * (n - j + 1) % MOD;
-                if (j > k)
-                    dp[i][j] = (long long)(dp[i][j] + dp[i - 1][j] * (j - k)) % MOD;
-            }
-        }
-        
-        return dp[goal][n];
+        this -> n = n, this -> k = k;
+        vector<vector<ll>> memo(goal + 1, vector<ll>(n + 1, -1));
+        return numberOfPlaylists(goal, n, memo);
     }
 };
