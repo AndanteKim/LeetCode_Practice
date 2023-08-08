05d65d25b33721e64ffccb5e831a1cb2c033f0ff
Andrew Kim <ksb0724@utexas.edu>
2023-08-08 18:21:07 +0900

    Time: 0 ms (100.00%), Space: 11.1 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,22 +1,29 @@
 class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1;
-        
+private:
+    int binarySearch(int left, int right, int target, vector<int>& nums){
         while (left <= right){
-            int mid = left + (right - left) / 2;
+            int mid = left + ((right - left) >> 1);
             
             if (nums[mid] == target) return mid;
-            else if (nums[mid] >= nums[left]){
-                if (target >= nums[left] && target < nums[mid]) right = mid - 1;
-                else left = mid + 1;
-            }
-            else{
-                if (target <= nums[right] && target > nums[mid]) left = mid + 1;
-                else right = mid - 1;
-            }
+            else if (nums[mid] < target) left = mid + 1;
+            else right = mid - 1;
         }
         
         return -1;
+    }
+    
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, n = nums.size();
+        int right = n - 1;
+        while (left <= right){
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] > nums.back()) left = mid + 1;
+            else right = mid - 1;
+        }
+        
+        int ans = binarySearch(0, left - 1, target, nums);
+        
+        return ans != -1? ans : binarySearch(left, n - 1, target, nums);
     }
 };
