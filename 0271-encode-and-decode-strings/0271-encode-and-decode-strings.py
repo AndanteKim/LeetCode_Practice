1bc1ed5132d26f11dc48e69c6a6c3dca109be30a
Andrew Kim <ksb0724@utexas.edu>
2023-08-09 23:09:35 +0900

    Time: 80 ms (67.31%), Space: 16.6 MB (84.52%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.py
@@ -2,30 +2,23 @@ class Codec:
     def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
         """
-        encoded_string = ''
-        
+        encoded_string = ""
         for s in strs:
-            encoded_string += s.replace('/', '//') + '/:'
-        
+            encoded_string += str(len(s)) + '/:' + s
         return encoded_string
 
     def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
         """
         decoded_strings = []
-        current_string, i = "", 0
+        i = 0
         
         while i < len(s):
-            if s[i : i+2] == '/:':
-                decoded_strings.append(current_string)
-                current_string = ""
-                i += 2
-            elif s[i : i + 2] == '//':
-                current_string += '/'
-                i += 2
-            else:
-                current_string += s[i]
-                i += 1
+            delim = s.find('/:', i)
+            length = int(s[i:delim])
+            str_ = s[delim + 2 : delim + 2 + length]
+            decoded_strings.append(str_)
+            i = delim + 2 + length
         return decoded_strings
 
 
