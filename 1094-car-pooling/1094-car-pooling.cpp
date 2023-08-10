dddf3fda6718f90a4c6b4ff003e097b5e0e8e8c9
Andrew Kim <ksb0724@utexas.edu>
2023-08-11 03:52:19 +0900

    Time: 11 ms (86.66%), Space: 10.8 MB (30.89%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        int farthest = 0;
-        for (vector<int>& trip : trips) farthest = max(farthest, trip[2]);
-        vector<int> arr(farthest + 1);
-        for (vector<int>& trip : trips){
-            int value = trip[0], left = trip[1], right = trip[2];
-            arr[left] += value;
-            arr[right] -= value;
-        }
+        map<int, int> m;
         
-        int curr = 0;
-        for (int i = 0; i < arr.size(); ++i){
-            curr += arr[i];
-            if (curr > capacity) return false;
+        for (vector<int>& trip : trips) m[trip[1]] += trip[0], m[trip[2]] -= trip[0];
+        
+        for (auto& [k, v] : m){
+            if ((capacity -= v) < 0) return false;
         }
         
         return true;
