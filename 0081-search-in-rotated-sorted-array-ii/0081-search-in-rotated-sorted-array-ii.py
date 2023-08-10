db0ec12b7ad741dadfcd8e2a52c5ddc50bd0c4d6
Andrew Kim <ksb0724@utexas.edu>
2023-08-11 02:51:47 +0900

    Time: 59 ms (84.78%), Space: 16.9 MB (77.85%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -1,24 +1,33 @@
 class Solution:
+    def isBinarySearchHelpful(self, nums: List[int], start: int, element: int) -> bool:
+        return nums[start] != element
+    
+    def existsInFirst(self, nums: List[int], start: int, element: int) -> bool:
+        return nums[start] <= element
+    
     def search(self, nums: List[int], target: int) -> bool:
         n = len(nums)
         left, right = 0, n - 1
         
         while left <= right:
             mid = left + ((right - left) >> 1)
-            
             if nums[mid] == target:
                 return True
             
-            elif nums[mid] > nums[left]:
-                if target >= nums[left] and target < nums[mid]:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            elif nums[mid] < nums[left]:
-                if target <= nums[right] and target > nums[mid]:
+            if not self.isBinarySearchHelpful(nums, left, nums[mid]):
+                left += 1
+                continue
+            
+            pivotArray = self.existsInFirst(nums, left, nums[mid])
+            targetArray = self.existsInFirst(nums, left, target)
+            if pivotArray ^ targetArray:
+                if pivotArray:
                     left = mid + 1
                 else:
                     right = mid - 1
             else:
-                left += 1
+                if nums[mid] < target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return False
