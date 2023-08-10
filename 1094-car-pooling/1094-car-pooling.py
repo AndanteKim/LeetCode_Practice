855ad6b31270b839091e58b656be9914a73d8487
Andrew Kim <ksb0724@utexas.edu>
2023-08-11 03:48:59 +0900

    Time: 79 ms (80.89%), Space: 16.8 MB (88.96%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,15 +1,8 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        arr = [0] * (max(trip[2] for trip in trips) + 1)
-        
-        for (val, left, right) in trips:
-            arr[left] += val
-            arr[right] -= val
-        
-        curr = 0
-        for i in range(len(arr)):
-            curr += arr[i]
-            if curr > capacity:
+        for i, v in sorted(x for n, i, j in trips for x in [[i, n], [j, -n]]):
+            capacity -= v
+            if capacity < 0:
                 return False
+            
         return True
-        
