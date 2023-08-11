6f9883e5b2cb749cf5d6c3ad42c7aa7635575f3a
Andrew Kim <ksb0724@utexas.edu>
2023-08-12 02:23:46 +0900

    Time: 69 ms (98.31%), Space: 19.3 MB (99.39%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,34 +1,19 @@
-class TrieNode:
-    def __init__(self):
-        self.words = []
-        self.children = dict()
-
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        
-        # build Trie
-        root = TrieNode()
-        for product in products:
-            curr = root
-            for c in product:
-                if c not in curr.children:
-                    curr.children[c] = TrieNode()
-                curr = curr.children[c]
-                curr.words.append(product)
-                curr.words.sort()
-                
-                if len(curr.words) > 3:
-                    curr.words.pop()
-                
-        # search word
-        curr, ans = root, [] 
+        products.sort()
+        ans = []
+        bs_start, n = 0, len(products)
+        prefix = ""
         for c in searchWord:
-            words = []
-            if c in curr.children:
-                curr = curr.children[c]
-                ans.append(curr.words)
-            else:
-                curr.children = {}
-                ans.append([])
-        
+            prefix += c
+            
+            start = bisect_left(products, prefix, bs_start, n)
+            ans.append([])
+            
+            for i in range(start, min(start + 3, n)):
+                if not products[i][:len(prefix)] == prefix:
+                    continue
+                ans[-1].append(products[i])
+            
+            bs_start = start
         return ans
