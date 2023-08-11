23250a93690202b4c12dd3220701d25fdc29b0e5
Andrew Kim <ksb0724@utexas.edu>
2023-08-12 02:31:49 +0900

    Time: 32 ms (95.43%), Space: 23.9 MB (95.53%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.cpp
+++ 1268-search-suggestions-system/1268-search-suggestions-system.cpp
@@ -1,40 +1,21 @@
-struct TrieNode{
-    vector<string> words;
-    unordered_map<char, TrieNode*> children;
-    TrieNode() : words({}), children(unordered_map<char, TrieNode*>()){}
-};
-
 class Solution {
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        // build a trie
-        TrieNode* root = new TrieNode();
-        for (string& product : products){
-            TrieNode* curr = root;
-            for (char& c : product){
-                if (curr -> children.find(c) == curr -> children.end()){
-                    curr -> children[c] = new TrieNode();
-                }
-                curr = curr -> children[c];
-                curr -> words.push_back(product);
-                sort(curr -> words.begin(), curr -> words.end());
-                if (curr -> words.size() > 3) curr -> words.pop_back();
-            }
+        sort(products.begin(), products.end());
+        vector<vector<string>> ans;
+        int start, bsStart = 0, n = products.size();
+        string prefix;
+        
+        for (char& c : searchWord){
+            prefix += c;
+            start = lower_bound(products.begin() + bsStart, products.end(), prefix) - products.begin();
+            ans.push_back({});
+            
+            for (int i = start; i < min(start + 3, n) && !products[i].compare(0, prefix.size(), prefix); ++i)
+                ans.back().push_back(products[i]);
+            bsStart = start;
         }
         
-        // search words
-        vector<vector<string>> ans;
-        TrieNode* curr = root;
-        for (char& c : searchWord){
-            if (curr -> children.find(c) != curr -> children.end()){
-                curr = curr -> children[c];
-                ans.push_back(curr -> words);
-            }
-            else{
-                curr -> children.clear();
-                ans.push_back({});
-            }
-        }
         return ans;
     }
 };
