ad4ef13a148a93396a52c8595e16f62cf3f6609b
Andrew Kim <ksb0724@utexas.edu>
2023-08-13 16:06:08 +0900

    Time: 846 ms (93.08%), Space: 85.5 MB (41.54%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,22 +1,18 @@
 class Solution:
+    def dp(self, i: int, memo: Dict[int, int], nums: List[int]) -> bool:
+        if i in memo:
+            return memo[i]
+        ans = False
+        
+        if i > 0 and nums[i] == nums[i - 1]:
+            ans |= self.dp(i - 2, memo, nums)
+        if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
+            ans |= self.dp(i - 3, memo, nums)
+        if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
+            ans |= self.dp(i - 3, memo, nums)
+        memo[i] = ans
+        return ans
+    
     def validPartition(self, nums: List[int]) -> bool:
-        n = len(nums)
-        memo = {-1: True}
-        
-        def dp(i: int) -> bool:
-            if i in memo:
-                return memo[i]
-            
-            ans = False
-            if i > 0 and nums[i] == nums[i - 1]:
-                ans |= dp(i - 2)
-            if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
-                ans |= dp(i - 3)
-            if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
-                ans |= dp(i - 3)
-            memo[i] = ans
-            return ans
-            
-            
-        return dp(n - 1)
-        
+        n, memo = len(nums), {-1: True}
+        return self.dp(n - 1, memo, nums)
