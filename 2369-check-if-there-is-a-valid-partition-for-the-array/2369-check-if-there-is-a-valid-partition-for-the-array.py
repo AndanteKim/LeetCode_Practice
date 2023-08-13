126c9624ceea7d44b59c40b2ba6947b26de55167
Andrew Kim <ksb0724@utexas.edu>
2023-08-13 16:23:23 +0900

    Time: 914 ms (87.69%), Space: 29.6 MB (100.00%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,18 +1,16 @@
 class Solution:
-    def dp(self, i: int, memo: Dict[int, int], nums: List[int]) -> bool:
-        if i in memo:
-            return memo[i]
-        ans = False
-        
-        if i > 0 and nums[i] == nums[i - 1]:
-            ans |= self.dp(i - 2, memo, nums)
-        if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
-            ans |= self.dp(i - 3, memo, nums)
-        if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
-            ans |= self.dp(i - 3, memo, nums)
-        memo[i] = ans
-        return ans
-    
     def validPartition(self, nums: List[int]) -> bool:
-        n, memo = len(nums), {-1: True}
-        return self.dp(n - 1, memo, nums)
+        n = len(nums)
+        dp = [True] + [False] * n
+        
+        for i in range(n):
+            dp_index = i + 1
+            
+            if i > 0 and nums[i] == nums[i - 1]:
+                dp[dp_index] |= dp[dp_index - 2]
+            if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
+                dp[dp_index] |= dp[dp_index - 3]
+            if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
+                dp[dp_index] |= dp[dp_index - 3]
+        
+        return dp[n]
