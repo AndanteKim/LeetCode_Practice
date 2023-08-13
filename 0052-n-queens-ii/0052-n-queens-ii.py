e1b596d649f537ffd6fe66ec619019cb778ae8b1
Andrew Kim <ksb0724@utexas.edu>
2023-08-14 02:25:05 +0900

    Time: 59 ms (63.91%), Space: 16.3 MB (56.18%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,33 +1,30 @@
 class Solution:
+    def backtrack(self, row: int, diagonals: int, anti_diagonals: int, cols: int) -> int:
+        if row == self.n:
+            return 1
+            
+        # bitmask solution more efficient than hashmap set
+        solutions = 0
+        for col in range(self.n):
+            curr_diagonal = 1 << (row - col + self.n)
+            curr_anti_diagonal = 1 << (row + col)
+            curr_col = 1 << col
+                
+            if curr_col & cols or curr_diagonal & diagonals or curr_anti_diagonal & anti_diagonals:
+                continue
+                
+            cols ^= curr_col
+            diagonals ^= curr_diagonal
+            anti_diagonals ^= curr_anti_diagonal
+                
+            solutions += self.backtrack(row + 1, diagonals, anti_diagonals, cols)
+                
+            cols ^= curr_col
+            diagonals ^= curr_diagonal
+            anti_diagonals ^= curr_anti_diagonal
+                
+        return solutions
+    
     def totalNQueens(self, n: int) -> int:
-        def backtrack(row: int, diagonals: int, anti_diagonals: int, cols: int) -> int:
-            if row == n:
-                return 1
-            
-            # bitmask solution more efficient than hashmap set
-            solutions = 0
-            for col in range(n):
-                curr_diagonal = 1 << (row - col + n)
-                curr_anti_diagonal = 1 << (row + col)
-                curr_col = 1 << col
-                
-                if curr_col & cols or curr_diagonal & diagonals or curr_anti_diagonal & anti_diagonals:
-                    continue
-                
-                cols ^= curr_col
-                diagonals ^= curr_diagonal
-                anti_diagonals ^= curr_anti_diagonal
-                
-                solutions += backtrack(row + 1, diagonals, anti_diagonals, cols)
-                
-                cols ^= curr_col
-                diagonals ^= curr_diagonal
-                anti_diagonals ^= curr_anti_diagonal
-                
-            return solutions
-        
-        return backtrack(0, 0, 0, 0)
-            
-            
-            
-            
+        self.n = n
+        return self.backtrack(0, 0, 0, 0)
