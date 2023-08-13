3c75119c90df408abffc36761a3fc4757c59f074
Andrew Kim <ksb0724@utexas.edu>
2023-08-14 00:30:53 +0900

    Time: 13 ms (25.57%), Space: 7.8 MB (20.50%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,23 +1,31 @@
 class Solution {
+private:
     int n;
-    
     int backtrack(int row, unordered_set<int>& diagonals, unordered_set<int>& antiDiagonals, unordered_set<int>& cols){
         if (row == n) return 1;
-        int ans = 0;
+        
+        int solutions = 0;
         for (int col = 0; col < n; ++col){
             int currDiagonal = row - col, currAntiDiagonal = row + col;
             
+            // If a queen isn't placeable
             if (cols.find(col) != cols.end() || diagonals.find(currDiagonal) != diagonals.end() || antiDiagonals.find(currAntiDiagonal) != antiDiagonals.end()) continue;
+            
+            // deploy a queen
             cols.insert(col);
             diagonals.insert(currDiagonal);
             antiDiagonals.insert(currAntiDiagonal);
-            ans += backtrack(row + 1, diagonals, antiDiagonals, cols);
+            
+            // keep backtrack until it's iterable
+            solutions += backtrack(row + 1, diagonals, antiDiagonals, cols);
+            
+            // draw a queen
             cols.erase(col);
             diagonals.erase(currDiagonal);
             antiDiagonals.erase(currAntiDiagonal);
         }
         
-        return ans;
+        return solutions;
     }
     
 public:
