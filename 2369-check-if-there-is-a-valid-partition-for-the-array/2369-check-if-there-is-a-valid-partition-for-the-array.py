dacfa87eb89a12fb251734ec2ba663b43297924b
Andrew Kim <ksb0724@utexas.edu>
2023-08-13 16:55:20 +0900

    Time: 946 ms (80.00%), Space: 30.1 MB (100.00%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,16 +1,16 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
         n = len(nums)
-        dp = [True] + [False] * n
+        dp = [True] + [False] * 2
         
         for i in range(n):
             dp_index = i + 1
-            
+            ans = False
             if i > 0 and nums[i] == nums[i - 1]:
-                dp[dp_index] |= dp[dp_index - 2]
+                ans |= dp[(dp_index - 2) % 3]
             if i > 1 and nums[i] == nums[i - 1] == nums[i - 2]:
-                dp[dp_index] |= dp[dp_index - 3]
+                ans |= dp[(dp_index - 3) % 3]
             if i > 1 and nums[i] == nums[i - 1] + 1 == nums[i - 2] + 2:
-                dp[dp_index] |= dp[dp_index - 3]
-        
-        return dp[n]
+                ans |= dp[(dp_index - 3) % 3]
+            dp[dp_index % 3] = ans
+        return dp[n % 3]
