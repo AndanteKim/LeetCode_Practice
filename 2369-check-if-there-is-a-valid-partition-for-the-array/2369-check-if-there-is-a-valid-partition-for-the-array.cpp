1e2bb34271d822a28b83596fabaddf9f7aab7b31
Andrew Kim <ksb0724@utexas.edu>
2023-08-13 17:03:18 +0900

    Time: 130 ms (88.00%), Space: 83.8 MB (92.92%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     bool validPartition(vector<int>& nums) {
         int n = nums.size();
-        vector<bool> dp(n + 1);
+        vector<bool> dp(3);
         dp[0] = true;
         
         for (int i = 0; i < n; ++i){
             int dpIndex = i + 1;
-            if (i > 0 && nums[i] == nums[i - 1]) dp[dpIndex] = dp[dpIndex] | dp[dpIndex - 2];
-            if (i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2]) dp[dpIndex] = dp[dpIndex] | dp[dpIndex - 3];
-            if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i - 1] + 1 == nums[i - 2] + 2) dp[dpIndex] = dp[dpIndex] | dp[dpIndex - 3];
+            bool ans = false;
+            if (i > 0 && nums[i] == nums[i - 1])
+                ans |= dp[(dpIndex - 2) % 3];
+            if (i > 1 && nums[i] == nums[i - 1] && nums[i - 1] == nums[i - 2])
+                ans |= dp[(dpIndex - 3) % 3];
+            if (i > 1 && nums[i] == nums[i - 1] + 1 && nums[i] == nums[i - 2] + 2)
+                ans |= dp[(dpIndex - 3) % 3];
+            dp[dpIndex % 3] = ans;
         }
-        
-        return dp[n];
+        return dp[n % 3];
     }
 };
