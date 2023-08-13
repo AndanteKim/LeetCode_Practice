cf9e0e27da72f00b84e07b908ea32273e4fd6ac7
Andrew Kim <ksb0724@utexas.edu>
2023-08-14 00:10:00 +0900

    Time: 51 ms (83.26%), Space: 16.2 MB (81.90%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.py
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -1,26 +1,35 @@
 class Solution:
-    def backtrack(self, row: int, diagonals: Set[int], anti_diagonals: Set[int], cols: Set[int]) -> int:
-        if row == self.n:
-            return 1
-        
-        ans = 0
-        for col in range(self.n):
-            curr_diagonal = row - col
-            curr_anti_diagonal = row + col
-            
-            if col in cols or curr_diagonal in diagonals or curr_anti_diagonal in anti_diagonals:
-                continue
-                
-            cols.add(col)
-            diagonals.add(curr_diagonal)
-            anti_diagonals.add(curr_anti_diagonal)
-            ans += self.backtrack(row + 1, diagonals, anti_diagonals, cols)
-            cols.remove(col)
-            diagonals.remove(curr_diagonal)
-            anti_diagonals.remove(curr_anti_diagonal)
-        
-        return ans
-    
     def totalNQueens(self, n: int) -> int:
-        self.n = n        
-        return self.backtrack(0, set(), set(), set())
+        
+        # review N-Queen question
+        def backtrack(row: int, diagonals: Set[int], anti_diagonals: Set[int], cols: Set[int]) -> int:
+            if row == n:
+                return 1
+            
+            solutions = 0
+            for col in range(n):
+                curr_diagonal = row - col
+                curr_anti_diagonal = row + col
+                
+                # If the queen is not placeable
+                if col in cols or curr_diagonal in diagonals or curr_anti_diagonal in anti_diagonals:
+                    continue
+                
+                # "Add" the queen to the board
+                cols.add(col)
+                diagonals.add(curr_diagonal)
+                anti_diagonals.add(curr_anti_diagonal)
+                
+                # Move on to the next row with the updated board state
+                solutions += backtrack(row + 1, diagonals, anti_diagonals, cols)
+                
+                # "Remove" the queen from the board since we've already
+                # explored all valid paths using the above function call
+                cols.remove(col)
+                diagonals.remove(curr_diagonal)
+                anti_diagonals.remove(curr_anti_diagonal)
+            
+            return solutions
+            
+        
+        return backtrack(0, set(), set(), set())
