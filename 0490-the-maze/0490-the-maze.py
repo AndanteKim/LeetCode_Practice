e193d490724ed30af2c15775a83fbc167c151c7e
Andrew Kim <ksb0724@utexas.edu>
2023-08-16 23:50:39 +0900

    Time: 243 ms (86.29%), Space: 18.4 MB (30.94%) - LeetHub

--- 0490-the-maze/0490-the-maze.py
+++ 0490-the-maze/0490-the-maze.py
@@ -1,29 +1,28 @@
 class Solution:
-    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        rows, cols = len(maze), len(maze[0])
-        if maze[start[0]][start[1]] == 1:
+    def dfs(self, m: int, n: int, maze: List[List[int]], curr: List[int], destination: List[int], visited: List[List[bool]]) -> bool:
+        if visited[curr[0]][curr[1]]:
             return False
         
-        queue, directions, visited = deque([(start[0], start[1])]), ((-1, 0), (1, 0), (0, -1), (0, 1)), [[False] * cols for _ in range(rows)]
-        while queue:
-            x, y = queue.popleft()
+        if curr[0] == destination[0] and curr[1] == destination[1]:
+            return True
+        
+        visited[curr[0]][curr[1]] = True
+        dirX, dirY = [0, 1, 0, -1], [-1, 0, 1, 0]
+        
+        for i in range(4):
+            r, c = curr[0], curr[1]
             
-            if x == destination[0] and y == destination[1]:
+            while 0 <= r and r < m and 0 <= c and c < n and maze[r][c] == 0:
+                r += dirX[i]
+                c += dirY[i]
+            
+            if self.dfs(m, n, maze, [r - dirX[i], c - dirY[i]], destination, visited):
                 return True
-            
-            if visited[x][y]:
-                continue
-            
-            visited[x][y] = True
-            for dx, dy in directions:
-                new_x, new_y = x, y
-                while 0 <= new_x + dx < rows and 0 <= new_y + dy < cols and maze[new_x + dx][new_y + dy] == 0:
-                    new_x += dx
-                    new_y += dy
-                    
-                if dx:
-                    queue.append((new_x, new_y))
-                else:
-                    queue.append((new_x, new_y))
         
         return False
+    
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        m, n = len(maze), len(maze[0])
+        visited = [[False] * n for _ in range(m)]
+        
+        return self.dfs(m, n, maze, start, destination, visited)
