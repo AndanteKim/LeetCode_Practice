9f90eae6e2a70b41c2e0f2747acc6ac8dba540dc
Andrew Kim <ksb0724@utexas.edu>
2023-08-18 05:06:32 +0900

    Time: 55 ms (96.92%), Space: 28.3 MB (93.55%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,34 +1,34 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        vector<vector<int>> dp;
+        for (vector<int>& row : mat) dp.emplace_back(row.begin(), row.end());
         int m = mat.size(), n = mat[0].size();
-        queue<vector<int>> queue;
-        vector<vector<bool>> visited(m, vector<bool>(n));
-        vector<vector<int>> ans(m, vector<int>(n));
         
         for (int i = 0; i < m; ++i){
             for (int j = 0; j < n; ++j){
-                if (!mat[i][j]){
-                    queue.push({i, j, 1});
-                    visited[i][j] = true;
+                int minNeighbor = m * n;
+                if (dp[i][j]){
+                    if (i > 0) minNeighbor = min(minNeighbor, dp[i - 1][j]);
+                    if (j > 0) minNeighbor = min(minNeighbor, dp[i][j - 1]);
+                    dp[i][j] = minNeighbor + 1;
                 }
+                
             }
         }
         
-        while (!queue.empty()){
-            auto it = queue.front();
-            queue.pop();
-            int x = it[0], y = it[1], dist = it[2];
-            
-            for (auto &[newX, newY] : vector<pair<int, int>>{{x + 1, y}, {x - 1, y}, {x, y - 1}, {x, y + 1}}){
-                if (0 <= newX && newX < m && 0 <= newY && newY < n && !visited[newX][newY]){
-                    visited[newX][newY] = true;
-                    queue.push({newX, newY, dist + 1});
-                    ans[newX][newY] = dist;
+        for (int r = m - 1; r >= 0; --r){
+            for (int c = n - 1; c >= 0; --c){
+                int minNeighbor = m * n;
+                if (dp[r][c]){
+                    if (r < m - 1) minNeighbor = min(minNeighbor, dp[r + 1][c]);
+                    if (c < n - 1) minNeighbor = min(minNeighbor, dp[r][c + 1]);
+                    dp[r][c] = min(dp[r][c], minNeighbor + 1);
                 }
+                
             }
         }
         
-        return ans;
+        return dp;
     }
 };
