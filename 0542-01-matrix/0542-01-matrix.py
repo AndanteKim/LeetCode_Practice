0372f3fe02fe98e4c30cb6eb7cb9168cb2a0cd33
Andrew Kim <ksb0724@utexas.edu>
2023-08-18 04:48:21 +0900

    Time: 471 ms (95.29%), Space: 19.6 MB (66.85%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -1,24 +1,28 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        m, n = len(mat), len(mat[0])
-        ans, queue = [[0] * n for _ in range(m)], deque([])
-        visited = [[False] * n for _ in range(m)]
+        dp = [row[:] for row in mat]
+        m, n = len(dp), len(dp[0])
         
-        for i in range(m):
-            for j in range(n):
-                if not mat[i][j]:
-                    queue.append((i, j, 1))
-                    visited[i][j] = True
+        for r in range(m):
+            for c in range(n):
+                min_neighbor = float('inf')
+                if dp[r][c] != 0:
+                    if r > 0:
+                        min_neighbor = min(min_neighbor, dp[r - 1][c])
+                    
+                    if c > 0:
+                        min_neighbor = min(min_neighbor, dp[r][c - 1])
+                    
+                    dp[r][c] = min_neighbor + 1
         
-        while queue:
-            x, y, dist = queue.popleft()
-            
-            for new_x, new_y in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-                if 0 <= new_x < m and 0 <= new_y < n and not visited[new_x][new_y]:
-                    queue.append((new_x, new_y, dist + 1))
-                    ans[new_x][new_y] = dist
-                    visited[new_x][new_y] = True
-                        
-        return ans
-        
-        
+        for r in range(m - 1, -1, -1):
+            for c in range(n - 1, -1, -1):
+                min_neighbor = float('inf')
+                if dp[r][c] != 0:
+                    if r < m - 1:
+                        min_neighbor = min(min_neighbor, dp[r + 1][c])
+                    if c < n - 1:
+                        min_neighbor = min(min_neighbor, dp[r][c + 1])
+                    
+                    dp[r][c] = min(dp[r][c], min_neighbor + 1)
+        return dp
