e11020c40684c2eef8cc1ff9d9eb5297166ef635
Andrew Kim <ksb0724@utexas.edu>
2023-08-21 18:39:44 +0900

    Time: 17 ms (70.36%), Space: 13.5 MB (46.16%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n = s.size();
-        for (int i = 1; i <= n / 2; ++i){
-            if (n % i == 0){
-                string pattern = "";
-                for (int j = 0; j < n / i; ++j) pattern += s.substr(0, i);
-                if (pattern == s) return true;
-            }
-        }
+        string t = s + s;
+        int n = t.size();
         
+        if (t.substr(1, n - 2).find(s) != -1) return true;
         return false;
     }
 };
