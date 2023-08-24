fcef3fd5e8a26f16dcc466d2861f7a272f3d0022
Andrew Kim <ksb0724@utexas.edu>
2023-08-25 02:22:22 +0900

    Time: 17 ms (57.08%), Space: 9.6 MB (15.57%) - LeetHub

--- 0723-candy-crush/0723-candy-crush.cpp
+++ 0723-candy-crush/0723-candy-crush.cpp
@@ -1,57 +1,72 @@
-class Solution:
-    def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
-        m, n = len(board), len(board[0])
+struct hashFunction{
+    size_t operator()(const pair<int, int>& x) const{
+        return x.first ^ x.second;
+    }
+};
+
+class Solution {
+private:
+    int m, n;
+    unordered_set<pair<int, int>, hashFunction> find(vector<vector<int>>& board){
+        unordered_set<pair<int, int>, hashFunction> crushedSet;
+        // Check vertically adjacent candies
+        for (int r = 1; r < m - 1; ++r){
+            for (int c = 0; c < n; ++c){
+                if (!board[r][c]) continue;
+                else if (board[r - 1][c] == board[r][c] && board[r][c] == board[r + 1][c]){
+                    crushedSet.insert(make_pair(r - 1, c));
+                    crushedSet.insert(make_pair(r, c));
+                    crushedSet.insert(make_pair(r + 1, c));
+                }
+            }
+        }
         
-        def find() -> Set[Tuple[int]]:
-            crushed_set = set()
+        // Check horizontally adjacent candies
+        for (int r = 0; r < m; ++r){
+            for (int c = 1; c < n - 1; ++c){
+                if (!board[r][c]) continue;
+                else if (board[r][c - 1] == board[r][c] && board[r][c] == board[r][c + 1]){
+                    crushedSet.insert(make_pair(r, c - 1));
+                    crushedSet.insert(make_pair(r, c));
+                    crushedSet.insert(make_pair(r, c + 1));
+                }
+            }
+        }
+        
+        return crushedSet;
+    }
+    
+    void crush(vector<vector<int>>& board, unordered_set<pair<int, int>, hashFunction>& crushedSet){
+        for (const auto& [r, c] : crushedSet){
+            board[r][c] = 0;
+        }
+    }
+    
+    void drop(vector<vector<int>>& board){
+        for (int c = 0; c < n; ++c){
+            int lowestZero = -1;
             
-            # Check vertically adjacent candies
-            for r in range(1, m - 1):
-                for c in range(n):
-                    if board[r][c] == 0:
-                        continue
-                    
-                    if board[r][c] == board[r - 1][c] == board[r + 1][c]:
-                        crushed_set.add((r, c))
-                        crushed_set.add((r - 1, c))
-                        crushed_set.add((r + 1, c))
-            
-            # Check horizontally adjacent candies
-            for r in range(m):
-                for c in range(1, n - 1):
-                    if board[r][c] == 0:
-                        continue
-                        
-                    if board[r][c] == board[r][c - 1] == board[r][c + 1]:
-                        crushed_set.add((r, c))
-                        crushed_set.add((r, c - 1))
-                        crushed_set.add((r, c + 1))
-            return crushed_set
+            // Iterate over each column
+            for (int r = m - 1; r >= 0; --r){
+                if (!board[r][c]) lowestZero = max(lowestZero, r);
+                else if (lowestZero >= 0) {
+                    swap(board[r][c], board[lowestZero][c]);
+                    --lowestZero;
+                }
+            }
+        }
+    }
+    
+public:
+    vector<vector<int>> candyCrush(vector<vector<int>>& board) {
+        m = board.size(), n = board[0].size();
+        unordered_set<pair<int, int>, hashFunction> crushedSet = find(board);
+        while(!crushedSet.empty()){
+            crush(board, crushedSet);
+            drop(board);
+            crushedSet = find(board);
+        }
         
-        # Set the value of each candies to be crushed as 0
-        def crush(crushed_set: Set[Tuple[int]]) -> None:
-            for (r, c) in crushed_set:
-                board[r][c] = 0
-        
-        def drop() -> None:
-            for c in range(n):
-                lowest_zero = -1
-                
-                # Iterate over each column
-                for r in range(m - 1, -1, -1):
-                    if board[r][c] == 0:
-                        lowest_zero = max(lowest_zero, r)
-                    
-                    # Swap current non-zero candy with the lowest zero.
-                    elif lowest_zero >= 0:
-                        board[r][c], board[lowest_zero][c] = board[lowest_zero][c], board[r][c]
-                        lowest_zero -= 1
-        
-        # Continue with 3 steps until we can no longer find any crushable candies
-        crushed_set = find()
-        while crushed_set:
-            crush(crushed_set)
-            drop()
-            crushed_set = find()
-        
-        return board
+        return board;
+    }
+};
