f9318bbeea193b7b6313494a7f3b5b6241213a92
Andrew Kim <ksb0724@utexas.edu>
2023-08-25 22:58:16 +0900

    Time: 18 ms (54.41%), Space: 11.7 MB (29.10%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.cpp
+++ 0252-meeting-rooms/0252-meeting-rooms.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     bool canAttendMeetings(vector<vector<int>>& intervals) {
-        if (intervals.size() == 0) return true;
         sort(intervals.begin(), intervals.end());
-        vector<int> prev = intervals[0];
         for (int i = 1; i < intervals.size(); ++i){
-            if (prev[1] > intervals[i][0]) return false;
-            prev = intervals[i];
+            if (intervals[i][0] < intervals[i - 1][1]) return false;
         }
+        
         return true;
     }
 };
