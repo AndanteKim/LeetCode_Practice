2ef6c9c5f424478d0ae76d56eec4582c5cc5af50
Andrew Kim <ksb0724@utexas.edu>
2023-08-25 18:40:01 +0900

    Time: 55 ms (53.26%), Space: 16.5 MB (62.86%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,23 +1,21 @@
 class Solution:
-    def dp(self, s1: str, i: int, s2: str, j: int, s3: str, k: int, memo: List[List[int]]) -> bool:
-        if i == self.n1:
-            return s2[j:] == s3[k:]
-        if j == self.n2:
-            return s1[i:] == s3[k:]
-        if memo[i][j] >= 0:
-            return True if memo[i][j] else False
-        ans = False
-        if (s1[i] == s3[k] and self.dp(s1, i + 1, s2, j, s3, k + 1, memo)) or (s2[j] == s3[k] and self.dp(s1, i, s2, j + 1, s3, k + 1, memo)):
-            ans = True
-        memo[i][j] = 1 if ans else 0
-        return ans
-    
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        self.n1, self.n2, self.n3 = len(s1), len(s2), len(s3)
+        n1, n2, n3 = len(s1), len(s2), len(s3)
         
-        if self.n1 + self.n2 != self.n3:
+        if n3 != n1 + n2:
             return False
         
-        memo = [[-1] * self.n2 for _ in range(self.n1)]
+        dp = [[False] * (n2 + 1) for _ in range(n1 + 1)]
         
-        return self.dp(s1, 0, s2, 0, s3, 0, memo)
+        for i in range(n1 + 1):
+            for j in range(n2 + 1):
+                if i == 0 and j == 0:
+                    dp[i][j] = True
+                elif i == 0:
+                    dp[i][j] = dp[i][j - 1] and s2[j - 1] == s3[i + j - 1]
+                elif j == 0:
+                    dp[i][j] = dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]
+                else:
+                    dp[i][j] = (dp[i - 1][j] and s1[i - 1] == s3[i + j - 1]) or (dp[i][j - 1] and s2[j - 1] == s3[i + j - 1])
+        
+        return dp[n1][n2]
