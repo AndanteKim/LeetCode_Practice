9d4a99926806b6f47f00024513ea1fbb98718d07
Andrew Kim <ksb0724@utexas.edu>
2023-08-25 23:18:45 +0900

    Time: 30 ms (79.45%), Space: 19 MB (71.60%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        for (int i = 0; i < intervals.size(); ++i){
+            if (ans.empty() || ans.back()[1] < intervals[i][0]) ans.push_back(intervals[i]);
+            else{
+                ans.back()[0] = min(ans.back()[0], intervals[i][0]);
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+        }
+        
+        return ans;
+    }
+};
