859714bd719c21691ad93c80e69cebf2e776c86a
Andrew Kim <ksb0724@utexas.edu>
2023-08-25 17:38:39 +0900

    Time: 38 ms (97.09%), Space: 16.7 MB (50.30%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,34 +1,23 @@
 class Solution:
+    def dp(self, s1: str, i: int, s2: str, j: int, s3: str, k: int, memo: List[List[int]]) -> bool:
+        if i == self.n1:
+            return s2[j:] == s3[k:]
+        if j == self.n2:
+            return s1[i:] == s3[k:]
+        if memo[i][j] >= 0:
+            return True if memo[i][j] else False
+        ans = False
+        if (s1[i] == s3[k] and self.dp(s1, i + 1, s2, j, s3, k + 1, memo)) or (s2[j] == s3[k] and self.dp(s1, i, s2, j + 1, s3, k + 1, memo)):
+            ans = True
+        memo[i][j] = 1 if ans else 0
+        return ans
+    
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        self.n1, self.n2, self.n3 = len(s1), len(s2), len(s3)
         
-        @lru_cache(maxsize = None)
-        def dp(i: int, j: int, k: int) -> bool:
-            if k == n3:
-                if i == 0 or j == 0:
-                    return False
-                return True
-            
-            if i >= n1 and j >= n2:
-                return False
-            
-            can_interleaving = False
-            if i < n1 and s3[k] == s1[i]:
-                can_interleaving = can_interleaving or dp(i + 1, j, k + 1)
-            
-            if j < n2 and s3[k] == s2[j]:
-                can_interleaving = can_interleaving or dp(i, j + 1, k + 1)
-            
-            return can_interleaving
+        if self.n1 + self.n2 != self.n3:
+            return False
         
-        n1, n2, n3 = len(s1), len(s2), len(s3)
-        if n1 + n2 == 0:
-            if s3:
-                return False
-            return True
-        elif n1 + n2 != n3:
-            return False
-        elif not n1 or not n2:
-            if s1 == s3 or s2 == s3:
-                return True
-            return False
-        return dp(0, 0, 0)
+        memo = [[-1] * self.n2 for _ in range(self.n1)]
+        
+        return self.dp(s1, 0, s2, 0, s3, 0, memo)
