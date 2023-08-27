2168ee79f8c9c36c1890a780192a28a137124928
Andrew Kim <ksb0724@utexas.edu>
2023-08-27 23:19:48 +0900

    Time: 2199 ms (20.53%), Space: 48.6 MB (13.72%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.py
+++ 0403-frog-jump/0403-frog-jump.py
@@ -1,24 +1,23 @@
 class Solution:
-    def solve(self, stones: List[int], n: int, index: int, prev_jump: int, dp: List[List[int]], mark: Dict[int, int]) -> int:
-        if index == n - 1:
-            return 1
-        
-        if dp[index][prev_jump] != -1:
-            return dp[index][prev_jump]
-
-        ans = False
-        for next_jump in range(prev_jump - 1, prev_jump + 2):
-            if next_jump > 0 and stones[index] + next_jump in mark:
-                ans = ans or self.solve(stones, n, mark[stones[index] + next_jump], next_jump, dp, mark)
-        
-        dp[index][prev_jump] = ans
-        return ans
-        
     def canCross(self, stones: List[int]) -> bool:
-        dp = [[-1] * 2001 for _ in range(2001)]
-        mark = dict()
+        mark, dp = defaultdict(int), [[0] * 2001 for _ in range(2001)]
+        n = len(stones)
         
-        for i in range(len(stones)):
+        for i in range(n):
             mark[stones[i]] = i
         
-        return self.solve(stones, len(stones), 0, 0, dp, mark)
+        dp[0][0] = 1
+        for index in range(n):
+            for prev_jump in range(n + 1):
+                if dp[index][prev_jump]:
+                    if mark[stones[index] + prev_jump]:
+                        dp[mark[stones[index] + prev_jump]][prev_jump] = 1
+                    if mark[stones[index] + prev_jump + 1]:
+                        dp[mark[stones[index] + prev_jump + 1]][prev_jump + 1] = 1
+                    if mark[stones[index] + prev_jump - 1]:
+                        dp[mark[stones[index] + prev_jump - 1]][prev_jump - 1] = 1
+        
+        for prev_jump in range(n + 1):
+            if dp[n - 1][prev_jump]:
+                return True
+        return False
