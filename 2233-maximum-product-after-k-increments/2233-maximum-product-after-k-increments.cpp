6150e4b848f8865fc3bb955f50a4f538ea7c8d9a
Andrew Kim <ksb0724@utexas.edu>
2023-08-28 03:15:16 +0900

    Time: 433 ms (68.29%), Space: 90.8 MB (58.84%) - LeetHub

--- 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums, int k) {
-        int ans = 1;
         priority_queue<int, vector<int>, greater<int>> pq;
         
         for (int num : nums) pq.push(num);
-        while (k > 0){
-            int elem = pq.top();
-            pq.pop();
-            --k;
-            ++elem;
-            pq.push(elem);
+        
+        for (int i = 0; i < k; ++i){
+            int num = pq.top(); pq.pop();
+            pq.push(num + 1);
         }
         
+        constexpr int MOD = 1'000'000'007;
+        long long ans = 1;
         while (!pq.empty()){
-            ans = ((long long)ans * pq.top()) % (1'000'000'007);
+            ans = (ans * pq.top()) % MOD;
             pq.pop();
         }
+        
         return ans;
     }
 };
