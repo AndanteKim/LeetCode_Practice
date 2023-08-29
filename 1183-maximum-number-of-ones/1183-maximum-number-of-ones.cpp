ee8e2ebeddb09b6357a935c83582afa17f8dfd82
Andrew Kim <ksb0724@utexas.edu>
2023-08-30 02:21:21 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (100.00%) - LeetHub

--- 1183-maximum-number-of-ones/1183-maximum-number-of-ones.cpp
+++ 1183-maximum-number-of-ones/1183-maximum-number-of-ones.cpp
@@ -1,26 +1,29 @@
-class Solution:
-    def maximumNumberOfOnes(self, width: int, height: int, sideLength: int, maxOnes: int) -> int:
-        m, n = height, width
-        ans = maxOnes * ((m // sideLength) * (n // sideLength))
-        rest = maxOnes
+class Solution {
+public:
+    int maximumNumberOfOnes(int width, int height, int sideLength, int maxOnes) {
+        int m = height, n = width;
+        int ans = maxOnes * (m / sideLength) * (n / sideLength), rest = maxOnes;
         
-        use1 = min((m % sideLength) * (n % sideLength), rest)
-        ans += ((m // sideLength) + (n // sideLength) + 1) * use1
-        rest -= use1
+        int use1 = min((m % sideLength) * (n % sideLength), rest);
+        ans += ((m / sideLength) + (n / sideLength) + 1) * use1;
+        rest -= use1;
+        if (m / sideLength > n / sideLength){
+            int use2 = min((n % sideLength) * sideLength - (m % sideLength) * (n % sideLength), rest);
+            ans += ((m / sideLength) * use2);
+            rest -= use2;
+            int use3 = min((m % sideLength) * sideLength - (m % sideLength) * (n % sideLength), rest);
+            ans += ((n / sideLength) * use3);
+            rest -= use3;
+        }
+        else{
+            int use2 = min((m % sideLength) * sideLength - (m % sideLength) * (n % sideLength), rest);
+            ans += ((n / sideLength) * use2);
+            rest -= use2;
+            int use3 = min((n % sideLength) * sideLength - (m % sideLength) * (n % sideLength), rest);
+            ans += ((m / sideLength) * use3);
+            rest -= use3;
+        }
         
-        if m // sideLength > n // sideLength:
-            use2 = min(((n % sideLength) * sideLength) - ((m % sideLength) * (n % sideLength)), rest)
-            ans += (m // sideLength) * use2
-            rest -= use2
-            use3 = min(((m % sideLength) * sideLength) - ((m % sideLength) * (n % sideLength)), rest)
-            ans += (n // sideLength) * use3
-            rest -= use3
-        else:
-            use2 = min(((m % sideLength) * sideLength) - ((m % sideLength) * (n % sideLength)), rest)
-            ans += (n // sideLength) * use2
-            rest -= use2
-            use3 = min(((n % sideLength) * sideLength) - ((m % sideLength) * (n % sideLength)), rest)
-            ans += (m // sideLength) * use3
-            rest -= use3
-        
-        return ans
+        return ans;
+    }
+};
