ea2db152979223b16aed008060d27b7411595291
Andrew Kim <ksb0724@utexas.edu>
2023-08-29 18:21:04 +0900

    Time: 26 ms (71.04%), Space: 10.7 MB (86.10%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -1,31 +1,19 @@
 class Solution {
 public:
     int bestClosingTime(string customers) {
-        int n = customers.size();
-        
-        // openTime penalty if the shop is kept open
-        // closedTime penalty if the shop kept closed
-        vector<int> openTime(n + 1), closedTime(n + 1);
-        int openCnt = 0, closedCnt = 0;
+        int minPenalty = count(customers.begin(), customers.end(), 'Y'), earliestHours = 0;
+        int currPenalty = minPenalty, n = customers.size();
         
         for (int i = 0; i < n; ++i){
-            openCnt += customers[i] == 'N';
-            openTime[i + 1] = openCnt;
-        }
-        
-        for (int i = n - 1; i >= 0; --i){
-            closedCnt += customers[i] == 'Y';
-            closedTime[i] = closedCnt;
-        }
-        
-        int ans = n - 1, minPenalty = INT_MAX;
-        for (int i = 0; i <= n; ++i){
-            if (minPenalty > openTime[i] + closedTime[i]){
-                minPenalty = openTime[i] + closedTime[i];
-                ans = i;
+            if (customers[i] == 'Y') --currPenalty;
+            else ++currPenalty;
+            
+            if (currPenalty < minPenalty){
+                earliestHours = i + 1;
+                minPenalty = currPenalty;
             }
         }
         
-        return ans;
+        return earliestHours;
     }
 };
