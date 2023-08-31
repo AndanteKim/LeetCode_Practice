2b3f63a4d02d043a9d0ab8fb2db7b0e3ed14a8f5
Andrew Kim <ksb0724@utexas.edu>
2023-08-31 23:48:07 +0900

    Time: 123 ms (87.14%), Space: 16.8 MB (55.71%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -1,10 +1,38 @@
 class Solution:
     def minTaps(self, n: int, ranges: List[int]) -> int:
-        dp = [n + 1] * (n + 1)
-        dp[0] = 0
-    
-        for start_line in range(len(ranges)):
-            for i in range(max(start_line - ranges[start_line] + 1, 0), min(start_line + ranges[start_line], n) + 1):
-                dp[i] = min(dp[i], dp[max(0, start_line - ranges[start_line])] + 1)
+        # Create a list to track the maximum reach for each position
+        max_reach = [0] * (n + 1)
         
-        return -1 if dp[-1] == n + 1 else dp[-1]
+        # Calculate the maximum reach for each tap
+        for i in range(len(ranges)):
+            # Calculate the leftmost position the tap can reach
+            start = max(0, i - ranges[i])
+            # Calculate the rightmost position the tap can reach
+            end = min(n, i + ranges[i])
+            
+            # Update the maximum reacch for the leftmost position
+            max_reach[start] = max(max_reach[start], end)
+            
+        # Number of taps used
+        taps = 0
+        # Current rightmost position reached
+        curr_end = 0
+        # Next rightmost position that can be reached
+        next_end = 0
+        
+        # Iterate through the garden
+        for i in range(n + 1):
+            if i > next_end:
+                # Current position cannot be reached
+                return -1
+            
+            if i > curr_end:
+                # Increment taps when moving to a new tap
+                taps += 1
+                # Move to the rightmost position that can be reached
+                curr_end = next_end
+            
+            # Update the next rightmost position that can be reached
+            next_end = max(next_end, max_reach[i])
+        
+        return taps
