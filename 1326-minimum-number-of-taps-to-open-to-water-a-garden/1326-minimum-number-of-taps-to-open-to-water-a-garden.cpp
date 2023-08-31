e19825cae07cf5d80b0a081cd758b60cbc286fe8
Andrew Kim <ksb0724@utexas.edu>
2023-09-01 00:06:15 +0900

    Time: 7 ms (97.74%), Space: 14.7 MB (55.15%) - LeetHub

--- 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
     int minTaps(int n, vector<int>& ranges) {
-        vector<int> dp(n + 1, n + 1);
-        dp[0] = 0;
+        // create a list to track the maximum reach for each position
+        vector<int> maxReach(n + 1, 0);
         
-        for (int startLine = 0; startLine < ranges.size(); ++startLine){
-            for (int i = max(startLine - ranges[startLine] + 1, 0); i <= min(startLine + ranges[startLine], n); ++i){
-                dp[i] = min(dp[i], dp[max(0, startLine - ranges[startLine])] + 1);
-            }
+        // calculate the maximum reach for each tap
+        for (int i = 0; i < ranges.size(); ++i){
+            // calculate the leftmost and rightmost position, so that each tap can be reached
+            int start = max(0, i - ranges[i]), end = min(n, i + ranges[i]);
+            // update the maximum reach for the leftmost position
+            maxReach[start] = max(maxReach[start], end);
         }
         
-        return dp.back() == n + 1? -1 : dp.back();
+        // number of taps used
+        int taps = 0;
+        // current rightmost position reached
+        int currEnd = 0;
+        // next rightmost position that can be reached
+        int nextEnd = 0;
+        // Iterate through the garden
+        for (int i = 0; i <= n; ++i){
+            if (i > nextEnd) return -1;
+            if (i > currEnd){
+                // increment taps when moving to a new tap
+                ++taps;
+                // move to the rightmost position
+                currEnd = nextEnd;
+            }
+            // Update the next rightmost position can be reached
+            nextEnd = max(nextEnd, maxReach[i]);
+        }
+        
+        return taps;
     }
 };
