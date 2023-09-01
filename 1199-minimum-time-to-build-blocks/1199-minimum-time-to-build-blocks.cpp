1c32b6b34a8506ad513ab235124c053562a0f9f5
Andrew Kim <ksb0724@utexas.edu>
2023-09-02 02:07:23 +0900

    Time: 305 ms (24.14%), Space: 143.3 MB (20.69%) - LeetHub

--- 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
+++ 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
@@ -1,36 +1,35 @@
 class Solution {
-private:
-    int n;
-    int solve(int b, int w, vector<vector<int>>& dp, vector<int>& blocks, int split){
-        // base case
-        if (b == n) return 0;
-        if (w == 0) return 1e9;
-        if (w >= n - b) return blocks[b];
-        
-        // If the subproblem is already solved, return result
-        if (dp[b][w] != -1) return dp[b][w];
-        
-        // 2 choices
-        int workHere = max(blocks[b], solve(b + 1, w - 1, dp, blocks, split));
-        int splitHere = split + solve(b, min(2 * w, n - b), dp, blocks, split);
-        
-        // store the result in dp array
-        dp[b][w] = min(workHere, splitHere);
-        return dp[b][w];
-    }
-    
 public:
     int minBuildTime(vector<int>& blocks, int split) {
-        this -> n = blocks.size();
-        
         // sort the blocks in descending order
+        int N = blocks.size();
         sort(blocks.begin(), blocks.end(), [](int a, int b){return a > b;});
         
-        // dp[i][j] represents the time taken
-        // build blocks[i ~ n - 1] blocks using j workers
-        vector<vector<int>> dp(n, vector<int>(n + 1, -1));
+        // Initialize the array
+        vector<vector<int>> dp(N + 1, vector<int>(N + 1));
         
-        // For block from index 0, with 1 worker
-        return solve(0, 1, dp, blocks, split);
+        // base case 1: If there are no workers, we can't build any block
+        for (int b = 0; b < N; ++b) dp[b][0] = INT_MAX;
+        
+        // base case 2: If there are no blocks, we don't need any time
+        for (int w = 0; w <= N; ++w) dp[N][w] = 0;
+        
+        // fill the array in a bottom-up fashion
+        for (int b = N - 1; b >= 0; --b){
+            for (int w = N; w >= 1; --w){
+                if (w >= N - b){
+                    dp[b][w] = blocks[b];
+                    continue;
+                }
+                
+                // recur relation
+                int workHere = max(blocks[b], dp[b + 1][w - 1]);
+                int splitHere = split + dp[b][min(2 * w, N - b)];
+                dp[b][w] = min(workHere, splitHere);
+            }
+        }
+        
+        // For building all the blocks with initially 1 worker
+        return dp[0][1];
     }
 };
