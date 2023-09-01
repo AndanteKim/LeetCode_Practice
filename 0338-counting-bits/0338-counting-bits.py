672b8eaf77542d79221b50fbf79a678820755c54
Andrew Kim <ksb0724@utexas.edu>
2023-09-01 17:45:18 +0900

    Time: 74 ms (76.62%), Space: 23.1 MB (65.29%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,18 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         ans = [0] * (n + 1)
-        x, b = 0, 1
-        
-        # [0, b) is calculated
-        while b <= n:
-            # generate [b, 2b) or [b, n) from [0, b)
-            while x < b and x + b <= n:
-                ans[x + b] = ans[x] + 1
-                x += 1
-            
-            # reset x
-            x = 0
-            # b = 2b
-            b <<= 1
-        
+        for x in range(1, n + 1):
+            ans[x] = ans[x >> 1] + (x & 1)
         return ans
