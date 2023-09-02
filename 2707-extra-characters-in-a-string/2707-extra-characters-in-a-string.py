002f6da5ded24dad85903cf5767a5e230571612b
Andrew Kim <ksb0724@utexas.edu>
2023-09-02 18:09:13 +0900

    Time: 232 ms (76.98%), Space: 26 MB (5.55%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,13 +1,40 @@
+class TrieNode:
+    def __init__(self):
+        self.children = dict()
+        self.is_word = False
+        
 class Solution:
+    def buildTrie(self, dictionary: List[str]) -> bool:
+        root = TrieNode()
+        
+        for word in dictionary:
+            node = root
+            for char in word:
+                if char not in node.children:
+                    node.children[char] = TrieNode()
+                node = node.children[char]
+            node.is_word = True
+        
+        return root
+    
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        n, dict_set = len(s), set(dictionary)
-        dp = [0] * (len(s) + 1)
+        n, root = len(s), self.buildTrie(dictionary)
         
-        for start in range(n - 1, -1, -1):
-            dp[start] = 1 + dp[start + 1]
+        @lru_cache(maxsize = None)
+        def dp(start: int) -> int:
+            if start == n:
+                return 0
+        
+            # To count this character as a left over character
+            # move to index 'start + 1'
+            ans = dp(start + 1) + 1
+            node = root
             for end in range(start, n):
-                curr = s[start: end + 1]
-                if curr in dict_set:
-                    dp[start] = min(dp[start], dp[end + 1])
+                if s[end] not in node.children:
+                    break
+                node = node.children[s[end]]
+                if node.is_word:
+                    ans = min(ans, dp(end + 1))
+            return ans
         
-        return dp[0]
+        return dp(0)
