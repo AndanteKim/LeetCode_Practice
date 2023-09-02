626f6c401137b200edb798f9d89128a7e24e84fa
Andrew Kim <ksb0724@utexas.edu>
2023-09-02 22:29:03 +0900

    Time: 70 ms (69.57%), Space: 16.4 MB (73.91%) - LeetHub

--- 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.py
+++ 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.py
@@ -1,32 +1,15 @@
 class Solution:
     def minBuildTime(self, blocks: List[int], split: int) -> int:
-        # sort the blocks in descending order
-        N = len(blocks)
-        blocks.sort(reverse = True)
+        # Prepare heap of building time
+        heapify(blocks)
         
-        # Initialize the dp array. When all N blocks
-        # done, we need 0 time
-        dp = [0] * (N + 1)
-        
-        # The case when we have no workers
-        dp[0] = float('inf')
-        
-        # Fill the dp array in a bottom-up fashion
-        for b in range(N - 1, -1, -1):
-            for w in range(N, 0, -1):
-                # If we have more workers than blocks
-                # Then we can build all the blocks
-                if w >= N - b:
-                    dp[w] = blocks[b]
-                    continue
-                
-                # Recurrence relation
-                work_here = max(blocks[b], dp[w - 1])
-                split_here = split + dp[min(2 * w, N - b)]
-                
-                # store the result in the dp array
-                dp[w] = min(work_here, split_here)
-        
-        # For building all the blocks, with
-        # initially 1 worker
-        return dp[1]
+        # Make sibling blocks until we are left with only one root node
+        while len(blocks) > 1:
+            # Pop two minimum The time of the abstracted sub-root will be
+            # split + max(x, y) which is split + y
+            x = heappop(blocks)
+            y = heappop(blocks)
+            heappush(blocks, split + y)
+            
+        # Time of final root node
+        return heappop(blocks)
