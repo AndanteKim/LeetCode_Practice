faeaea1f9b4067c053bf49568f0b90975fcbd465
Andrew Kim <ksb0724@utexas.edu>
2023-09-02 22:13:22 +0900

    Time: 4680 ms (26.09%), Space: 16.6 MB (34.78%) - LeetHub

--- 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.py
+++ 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.py
@@ -4,34 +4,29 @@ class Solution:
         N = len(blocks)
         blocks.sort(reverse = True)
         
-        # Initialize the dp array
-        dp = [[0] * (N + 1) for _ in range(N + 1)]
+        # Initialize the dp array. When all N blocks
+        # done, we need 0 time
+        dp = [0] * (N + 1)
         
-        # base case 1: If there are no workers, then we can't build any block
-        for b in range(N):
-            dp[b][0] = float('inf')
-            
-        # base case 2: If there are no blocks, then we don't need any time.
-        for w in range(N + 1):
-            dp[N][w] = 0
-            
-        # fill the dp array in a bottom-up fashion
+        # The case when we have no workers
+        dp[0] = float('inf')
+        
+        # Fill the dp array in a bottom-up fashion
         for b in range(N - 1, -1, -1):
-            # base case 3: If we have more workers than blocks,
-            # then we can build all the blocks
             for w in range(N, 0, -1):
+                # If we have more workers than blocks
+                # Then we can build all the blocks
                 if w >= N - b:
-                    dp[b][w] = blocks[b]
+                    dp[w] = blocks[b]
                     continue
                 
                 # Recurrence relation
-                workHere = max(blocks[b], dp[b + 1][w - 1])
-                splitHere = split + dp[b][min(2 * w, N - b)]
+                work_here = max(blocks[b], dp[w - 1])
+                split_here = split + dp[min(2 * w, N - b)]
                 
-                # Store the result in the dp array
-                dp[b][w] = min(workHere, splitHere)
+                # store the result in the dp array
+                dp[w] = min(work_here, split_here)
         
-        # For building all the blocks with initially 1 worker
-        return dp[0][1]
-                
-        
+        # For building all the blocks, with
+        # initially 1 worker
+        return dp[1]
