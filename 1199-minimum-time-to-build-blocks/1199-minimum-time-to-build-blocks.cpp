f4bc04c897fbae6222e4be6692e88bd05eb6553d
Andrew Kim <ksb0724@utexas.edu>
2023-09-02 22:54:20 +0900

    Time: 14 ms (68.97%), Space: 9.5 MB (44.83%) - LeetHub

--- 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
+++ 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
     int minBuildTime(vector<int>& blocks, int split) {
-        int N = blocks.size();
-        sort(blocks.begin(), blocks.end(), greater<int>());
+        // heapify heap of building time
+        priority_queue<int, vector<int>, greater<int>> pq(blocks.begin(), blocks.end());
         
-        // Initialize the dp array. When all N blocks done,
-        // we need 0 time
-        vector<int> dp(N + 1);
-        // The case when we have no workers
-        dp[0] = INT_MAX;
-        
-        // Fill the dp array in a bottom-up fashion
-        for (int b = N - 1; b >= 0; --b){
-            for (int w = N; w > 0; --w){
-                // If we have more workers than blocks
-                // Then we can build all the blocks
-                if (w >= N - b){
-                    dp[w] = blocks[b];
-                    continue;
-                }
-                
-                // Recurrence relation
-                int workHere = max(blocks[b], dp[w - 1]);
-                int splitHere = split + dp[min(2 * w, N - b)];
-                
-                // Store the result in the dp array
-                dp[w] = min(workHere, splitHere);
-            }
+        // make sibling blocks until we are left with
+        // only one root node
+        while (pq.size() > 1){
+            // pop two minimum the time of the abstracted sub-root
+            // will be split + max(x, y) which split + y
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+            pq.push(split + y);
         }
-        
-        // For building all the blocks, with
-        // initially 1 worker
-        return dp[1];
+        // Time of final root node
+        return pq.top();
     }
 };
