a86cdfa0d2c1abfba1339f2b3a0b904d78451d12
Andrew Kim <ksb0724@utexas.edu>
2023-09-02 23:17:14 +0900

    Time: 9 ms (86.21%), Space: 9.4 MB (93.10%) - LeetHub

--- 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
+++ 1199-minimum-time-to-build-blocks/1199-minimum-time-to-build-blocks.cpp
@@ -1,21 +1,42 @@
 class Solution {
+private:
+    bool possible(int limit, vector<int>& blocks, int split){
+        int worker = 1;
+        
+        for (int i = 0; i < blocks.size(); ++i){
+            int time = blocks[i];
+            // If no worker or no sufficient time
+            if (worker <= 0 || time > limit) return false;
+            
+            // keep splitting and producing workers as long as
+            // we are within the limit for this block
+            while (time + split <= limit){
+                limit -= split;
+                worker <<= 1;
+                
+                // sufficient worker for remaining block
+                if (worker >= blocks.size() - 1) return true;
+            }
+            --worker;
+        }
+        // all blocks are built
+        return true;
+    }
+    
 public:
     int minBuildTime(vector<int>& blocks, int split) {
-        // heapify heap of building time
-        priority_queue<int, vector<int>, greater<int>> pq(blocks.begin(), blocks.end());
         
-        // make sibling blocks until we are left with
-        // only one root node
-        while (pq.size() > 1){
-            // pop two minimum the time of the abstracted sub-root
-            // will be split + max(x, y) which split + y
-            int x = pq.top();
-            pq.pop();
-            int y = pq.top();
-            pq.pop();
-            pq.push(split + y);
+        // sort in descending order
+        sort(blocks.begin(), blocks.end(), greater<int>());
+        
+        // binary search algorithm
+        int left = blocks[0], right = split * (ceil(log2(blocks.size()))) + blocks[0];
+        while (left < right){
+            int mid = left + ((right - left) >> 1);
+            if (possible(mid, blocks, split)) right = mid;
+            else left = mid + 1;
         }
-        // Time of final root node
-        return pq.top();
+        
+        return right;
     }
 };
