cece91760b0c134b3bb1f4c421dfa4c917bc2a59
Andrew Kim <ksb0724@utexas.edu>
2023-09-03 00:08:17 +0900

    Time: 11 ms (85.72%), Space: 20.3 MB (27.98%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -4,13 +4,12 @@ public:
         int count = 0;
         
         for (int i = 0; i < flowerbed.size(); ++i){
-            if (flowerbed[i] == 0){
-                bool empty_left_plot = ((i == 0) || (flowerbed[i - 1] == 0));
-                bool empty_right_plot = ((i == flowerbed.size() - 1) || flowerbed[i + 1] == 0);
-                if (empty_left_plot && empty_right_plot){
-                    ++count;
+            if (!flowerbed[i]){
+                bool emptyLeftPlot = (i == 0) || !flowerbed[i - 1];
+                bool emptyRightPlot = (i == flowerbed.size() - 1) || !flowerbed[i + 1];
+                if (emptyLeftPlot && emptyRightPlot){
                     flowerbed[i] = 1;
-                    if (count >= n) return true;
+                    ++count;
                 }
             }
         }
