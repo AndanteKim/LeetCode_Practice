899b2127666783cf64813860721b525b24b36f56
Andrew Kim <ksb0724@utexas.edu>
2023-09-02 17:54:23 +0900

    Time: 142 ms (68.26%), Space: 79.9 MB (61.41%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.cpp
@@ -3,23 +3,16 @@ public:
     int minExtraChar(string s, vector<string>& dictionary) {
         int n = s.size();
         unordered_set<string> dictSet(dictionary.begin(), dictionary.end());
-        unordered_map<int, int> memo;
+        vector<int> dp(n + 1);
         
-        function<int(int)> dp = [&](int start){
-            if (start == n) return 0;
-            if (memo.count(start)) return memo[start];
-            
-            // To count this character as a left over character
-            // move to index 'start + 1'
-            int ans = dp(start + 1) + 1;
+        for (int start = n - 1; start >= 0; --start){
+            dp[start] = dp[start + 1] + 1;
             for (int end = start; end < n; ++end){
                 string curr = s.substr(start, end - start + 1);
-                if (dictSet.count(curr)) ans = min(ans, dp(end + 1));
+                if (dictSet.count(curr)) dp[start] = min(dp[start], dp[end + 1]);
             }
-            
-            return memo[start] = ans;
-        };
+        }
         
-        return dp(0);
+        return dp[0];
     }
 };
