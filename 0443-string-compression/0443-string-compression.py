710f4b23510df79d9f1042af472aa6a3b39d197b
Andrew Kim <ksb0724@utexas.edu>
2023-09-04 01:02:39 +0900

    Time: 61 ms (87.52%), Space: 16.4 MB (50.46%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -4,15 +4,14 @@ class Solution:
         
         while i < len(chars):
             group_length = 1
-            while (i + group_length < len(chars)\
-                  and chars[i + group_length] == chars[i]):
+            while i + group_length < len(chars) and chars[i + group_length] == chars[i]:
                 group_length += 1
             chars[res] = chars[i]
             res += 1
             if group_length > 1:
                 str_repr = str(group_length)
-                chars[res: res + len(str_repr)] = list(str_repr)
+                chars[res : res + len(str_repr)] = list(str_repr)
                 res += len(str_repr)
             i += group_length
-        return res
         
+        return res
