239024b4353ac9584dfda74ad193ca36ae416a4a
Andrew Kim <ksb0724@utexas.edu>
2023-09-04 00:15:13 +0900

    Time: 109 ms (47.02%), Space: 116.4 MB (8.96%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        int firstNum = INT_MAX, secondNum = INT_MAX;
+        int n = nums.size();
+        vector<int> dp(n);
+        dp[0] = nums[0];
         
-        for (int num : nums){
-            if (num <= firstNum) firstNum = num;
-            else if (num <= secondNum) secondNum = num;
-            else return true;
+        for (int i = 1; i < n - 1; ++i) dp[i] = min(dp[i - 1], nums[i]);
+        int mx = nums[n - 1];
+        for (int i = n - 2; i > 0; --i){
+            int mn = dp[i - 1];
+            mx = max(mx, nums[i + 1]);
+            if (mn < nums[i] && nums[i] < mx) return true;
         }
         
         return false;
