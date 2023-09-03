4a953f65102a14477dcd55b3095478839dbb9d06
Andrew Kim <ksb0724@utexas.edu>
2023-09-03 22:06:54 +0900

    Time: 196 ms (69.76%), Space: 24.1 MB (49.20%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,22 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        # Time Complexity: O(n)
-        # Space Complexity: O(n) since L, R prefix, suffix product array
-        
         n = len(nums)
-        
-        L, R, ans = [0] * n, [0] * n, [0] * n
-        
-        L[0], R[n - 1] = 1, 1
+        ans = [0] * n
+        ans[0] = 1
         
         for i in range(1, n):
-            L[i] = nums[i - 1] * L[i - 1]
-            
-        for i in range(n - 2, -1, -1):
-            R[i] = nums[i + 1] * R[i + 1]
+            ans[i] = ans[i - 1] * nums[i - 1]
         
-        
-        for i in range(n):
-            ans[i] = L[i] * R[i]
+        R = 1
+        for i in range(n - 1, -1, -1):
+            ans[i] = ans[i] * R
+            R *= nums[i]
         
         return ans
+        
