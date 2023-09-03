2b9a50f9dfc19efbd0c5c016909e97459f75e667
Andrew Kim <ksb0724@utexas.edu>
2023-09-03 22:01:18 +0900

    Time: 14 ms (92.86%), Space: 25 MB (27.42%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,30 +1,14 @@
-typedef long long ll;
-
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        bool exist_0 = false;
         int n = nums.size();
-        ll sum = 1;
-        for (int i = 0; i < n; ++i){
-            if (!nums[i]){
-                if (exist_0) return vector<int>(n);
-                exist_0 = true;
-                continue;
-            }
-            sum *= nums[i];
-        }
+        vector<int> ans(n), L(n), R(n);
+        L[0] = 1, R[n - 1] = 1;
         
-        if (exist_0){
-            vector<int> ans(n, 0);
-            for (int i = 0; i < n; ++i){
-                if(!nums[i]) ans[i] = sum;
-            }
-            return ans;
-        }
-        vector<int> ans(n);
-        for (int i = 0; i < n; ++i) ans[i] = sum / nums[i];
+        for (int i = 1; i < n; ++i) L[i] = L[i - 1] * nums[i - 1];
+        for (int i = n - 2; i >= 0; --i) R[i] = R[i + 1] * nums[i + 1];
         
+        for (int i = 0; i < n; ++i) ans[i] = L[i] * R[i];
         return ans;
     }
 };
