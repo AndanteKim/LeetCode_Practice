ce62f1f5a7c938f22c672c1e8e8b4749624394ef
Andrew Kim <ksb0724@utexas.edu>
2023-09-05 18:07:00 +0900

    Time: 133 ms (49.82%), Space: 72.9 MB (13.37%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size(), i = 0, j = n - 1, ans = 0;
+        unordered_map<int, int> lookup;
+        int ans = 0;
         
-        while (i < j){
-            if (nums[i] + nums[j] == k){
-                ++i;
-                --j;
+        for (int num : nums){
+            int complement = k - num;
+            if (lookup[complement] > 0) {
+                --lookup[complement];
                 ++ans;
             }
-            else if (nums[i] + nums[j] > k) --j;
-            else ++i;
+            else ++lookup[num];
         }
         
         return ans;
