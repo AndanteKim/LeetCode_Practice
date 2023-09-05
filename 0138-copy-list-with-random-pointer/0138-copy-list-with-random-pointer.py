b4f3fd49ef9de5109a209857cb5ff8792381197e
Andrew Kim <ksb0724@utexas.edu>
2023-09-05 17:00:05 +0900

    Time: 44 ms (63.80%), Space: 17.2 MB (92.76%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,44 +8,45 @@ class Node:
 """
 
 class Solution:
-    def __init__(self):
-        # creating a visited dictionary to hold old node reference as key
-        # and new node reference as the "value"
-        self.visited = dict()
-    
-    def getClonedNode(self, node: 'Node') -> 'Node':
-        # if node exists then
-        if node:
-            # check if it's in the visited dictionary
-            if node in self.visited:
-                # If it's in the visited dictionary then return the new node reference
-                # from the dictionary
-                return self.visited[node]
-            else:
-                # Otherwise create a new node, save the reference in the visited dictionary
-                # and return it
-                self.visited[node] = Node(node.val, None, None)
-                return self.visited[node]
-        
-        return None
-    
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
         if not head:
             return head
         
-        old_node = head
-        # create the new head node
-        new_node = Node(old_node.val, None, None)
-        self.visited[old_node] = new_node
+        # creating a new weaved list of original and copied nodes
+        ptr = head
+        while ptr:
+            
+            # cloned node
+            new_node = Node(ptr.val, None, None)
+            
+            # Inserting the cloned node just next to the original node
+            # If A -> B -> C is the original linked list,
+            # Linked list after weaving cloned nodes would be A->A'->B->B'->C->C'
+            new_node.next = ptr.next
+            ptr.next = new_node
+            ptr = new_node.next
         
-        # Iterate on the linked list until all nodes are cloned
-        while old_node:
-            # get the clones of the nodes referenced by random and next pointers
-            new_node.random = self.getClonedNode(old_node.random)
-            new_node.next = self.getClonedNode(old_node.next)
+        ptr = head
+        
+        # Now link the random pointers of the new nodes created
+        # Iterate the newly created list and use the original nodes random pointers
+        # to assign references to random pointers for cloned nodes
+        while ptr:
+            ptr.next.random = ptr.random.next if ptr.random else None
+            ptr = ptr.next.next
             
-            # Move one step ahead in the linked list
-            old_node = old_node.next
-            new_node = new_node.next
-            
-        return self.visited[head]
+        # Unweave the linked list to get back the original linked list
+        # and the cloned list i.e. A->A'->B->B'->C->C' would be broken to A->B->C and
+        # A'->B'->C'
+        ptr_old_list = head # A->B->C
+        ptr_new_list = head.next # A'->B'->C'
+        head_new = head.next # A'->B'->C'
+        while ptr_old_list:
+            ptr_old_list.next = ptr_old_list.next.next
+            ptr_new_list.next = ptr_new_list.next.next if ptr_new_list.next else None
+            ptr_old_list = ptr_old_list.next
+            ptr_new_list = ptr_new_list.next
+        
+        return head_new
+        
+        
