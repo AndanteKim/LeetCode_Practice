92e35851e587a3e2b0038a96aec6e837eb2accf1
Andrew Kim <ksb0724@utexas.edu>
2023-09-05 15:25:20 +0900

    Time: 43 ms (67.74%), Space: 17.7 MB (7.69%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -8,7 +8,29 @@ class Node:
 """
 
 class Solution:
-    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        root = deepcopy(head)
-        return root       
+    def __init__(self):
+        # Dictionary holding old nodes as keys and new nodes as its values
+        self.visitedHash = dict()
         
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        # base case
+        if not head:
+            return None
+        
+        if head in self.visitedHash:
+            return self.visitedHash[head]
+        
+        # create a new node with the value same as old node
+        node = Node(head.val, None, None)
+        
+        # Save it in the hash map to find loops during traversal due to randomness of
+        # random pointers and to avoid it
+        self.visitedHash[head] = node
+
+        # Recursively copy the remaining linked list starting once from the next pointer
+        # and then from the random pointer
+        # Update two independent recursive calls
+        node.next = self.copyRandomList(head.next)
+        node.random = self.copyRandomList(head.random)
+        
+        return node
