ac1630bc0a1d3372b2fa80528d8bd8ac613681ce
Andrew Kim <ksb0724@utexas.edu>
2023-09-09 03:16:42 +0900

    Time: 9 ms (95.42%), Space: 9.1 MB (81.68%) - LeetHub

--- 0358-rearrange-string-k-distance-apart/0358-rearrange-string-k-distance-apart.cpp
+++ 0358-rearrange-string-k-distance-apart/0358-rearrange-string-k-distance-apart.cpp
@@ -1,41 +1,53 @@
 class Solution {
 public:
     string rearrangeString(string s, int k) {
-        int freq[26] = {0};
-        // store the frequency for each character
-        for (char& c : s) ++freq[c - 'a'];
+        unordered_map<char, int> freqs;
+        int maxFreq = 0;
+        // store the frequency and find the highest frequency
+        for (char& c : s){
+            ++freqs[c];
+            maxFreq = max(maxFreq, freqs[c]);
+        }
+        unordered_set<char> mostChars, secondChars;
+        // store all the characters with the highest and second highest frequency - 1
+        for (auto&[c, freq] : freqs){
+            if (freq == maxFreq) mostChars.insert(c);
+            else if (freq == maxFreq - 1) secondChars.insert(c);
+        }
         
-        priority_queue<pair<int, int>> maxHeap;
-        // insert the charaters with their frequencies in the max heap
-        for (int i = 0; i < 26; ++i) if (freq[i] > 0) maxHeap.push(make_pair(freq[i], i));
-        
-        queue<pair<int, int>> busy;
-        string ans = "";
-        // This queue stores the characters that cannot be used now
-        while (ans.size() != s.size()){
-            int idx = ans.size();
-            
-            // insert the character that could be used now into the max heap
-            if (!busy.empty() && idx - busy.front().first >= k){
-                auto[prevIdx, chIdx] = busy.front();
-                busy.pop();
-                maxHeap.push(make_pair(freq[chIdx], chIdx));
-            }
-            
-            // If the max heap is empty, it implies no character can be used at this index
-            if (maxHeap.empty()) return "";
-            
-            int currChIdx = maxHeap.top().second;
-            maxHeap.pop();
-            ans.push_back(currChIdx + 'a');
-            
-            // insert the used character into busy queue with the current index
-            --freq[currChIdx];
-            if (freq[currChIdx] > 0){
-                busy.push(make_pair(idx, currChIdx));
+        // create maxFreq number of different strings
+        string segments[maxFreq];
+        // insert one instance of characters with frequency maxFreq & maxFreq - 1 in each segment
+        for (int i = 0; i < maxFreq; ++i){
+            for (const char& c1 : mostChars) segments[i] += c1;
+            // skip the last segment as the frequency is only maxFreq - 1
+            if (i < maxFreq - 1){
+                for (const char& c2 : secondChars) segments[i] += c2;
             }
         }
         
+        int segmentId = 0;
+        // Iterate over the remaining characters, and for each, distribute the instances over the segments
+        for (auto& [currChar, _] : freqs){
+            // skip characters with maxFreq or maxFreq - 1
+            // frequency as they have already been inserted
+            if (mostChars.find(currChar) != mostChars.end() || secondChars.find(currChar) != secondChars.end()) continue;
+            
+            // distribute the instances of these characters over the segments in a round-robin manner
+            for (int freq = freqs[currChar]; freq > 0; --freq){
+                segments[segmentId] += currChar;
+                segmentId = (segmentId + 1) % (maxFreq - 1);
+            }
+        }
+        
+        // each segment except the last should have exactly k elements; else, return ""
+        for (int i = 0; i < maxFreq - 1; ++i){
+            if (segments[i].size() < k) return "";
+        }
+        
+        string ans;
+        // Join all the segments and return them
+        for (string& s : segments) ans += s;
         return ans;
     }
 };
