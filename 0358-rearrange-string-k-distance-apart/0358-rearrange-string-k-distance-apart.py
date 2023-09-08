03e2f55ab7354a6fdea35e32cc57c75df8b3eeb4
Andrew Kim <ksb0724@utexas.edu>
2023-09-09 02:53:09 +0900

    Time: 66 ms (87.84%), Space: 17.5 MB (55.86%) - LeetHub

--- 0358-rearrange-string-k-distance-apart/0358-rearrange-string-k-distance-apart.py
+++ 0358-rearrange-string-k-distance-apart/0358-rearrange-string-k-distance-apart.py
@@ -1,38 +1,53 @@
 class Solution:
     def rearrangeString(self, s: str, k: int) -> str:
-        freq = [0] * 26
+        freqs, max_freq = dict(), 0
         
-        # store the frequency for each character
+        # store the frequency and find the highest frequency
         for c in s:
-            freq[ord(c) - 97] -= 1
-        
-        max_heap = []
-        
-        # insert the characters with their frequencies in the max heap
-        for i in range(26):
-            if freq[i]:
-                heappush(max_heap, (freq[i], i))
-        
-        ans, busy = "", deque([])
-        while len(ans) != len(s):
-            idx = len(ans)
+            freqs[c] = freqs.get(c, 0) + 1
+            max_freq = max(max_freq, freqs[c])
             
-            # insert the character that could be used now into the free heap
-            if busy and idx - busy[0][0] >= k:
-                prev_idx, ch_idx = busy.popleft()
-                heappush(max_heap, (freq[ch_idx], ch_idx))
+        most_chars, second_chars = set(), set()
+        
+        # store all the characters with the highest and second highest frequency - 1
+        for c, freq in freqs.items():
+            if freq == max_freq:
+                most_chars.add(c)
+            elif freq == max_freq - 1:
+                second_chars.add(c)
+        
+        # create max_freq number of different strings
+        segments = [''] * max_freq
+        # insert one instance of characters with frequency max_freq and max_freq - 1 in each segment
+        for i in range(max_freq):
+            for c in most_chars:
+                segments[i] += c
+        
+            # skip the last segment as the frequency is only max_freq - 1
+            if i < max_freq - 1:
+                for c in second_chars:
+                    segments[i] += c
+        
+        segment_id = 0
+        # iterate over the remaining characters, and for each, distribute the instances over the segments
+        for curr_char, _ in freqs.items():
+            if curr_char in most_chars or curr_char in second_chars:
+                continue
             
-            # if the free heap is empty, it implies no character can be used at this index
-            if not max_heap:
+            # distribute the instances of these characters over the segments in a round-robin manner
+            for freq in range(freqs[curr_char], 0, -1):
+                segments[segment_id] += curr_char
+                segment_id = (segment_id + 1) % (max_freq - 1)
+        
+        # each segment except the last should have exactly k elements; otherwise, return ""
+        for i in range(max_freq - 1):
+            if len(segments[i]) < k:
                 return ""
             
-            f, curr_char_idx = heappop(max_heap)
-            ans += chr(curr_char_idx + 97)
-            
-            # insert the used character into busy queue with the current index.
-            freq[curr_char_idx] += 1
-            if freq[curr_char_idx] < 0:
-                busy.append((idx, curr_char_idx))
+        ans = ""
+        for s in segments:
+            ans += s
         
         return ans
-            
+        
+        
