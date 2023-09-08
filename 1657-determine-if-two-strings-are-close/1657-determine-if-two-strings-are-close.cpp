fa84704b63af9dc3aeae61af20d3b18260893f7e
Andrew Kim <ksb0724@utexas.edu>
2023-09-09 03:36:12 +0900

    Time: 70 ms (79.31%), Space: 20.5 MB (87.36%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if (word1.size() != word2.size()) return false;
-        
-        vector<int> word1Map(26, 0), word2Map(26, 0);
-        int word1Bit = 0, word2Bit = 0;
-        for (const char &c : word1){
-            ++word1Map[c - 'a'];
-            word1Bit = word1Bit | (1 << (c - 'a'));
+        vector<int> count1(26), count2(26);
+        for (const char& c : word1) ++count1[c - 'a'];
+        for (const char& c : word2) {
+            if (!count1[c - 'a']) return false;
+            ++count2[c - 'a'];
         }
         
-        for (const char &c : word2){
-            ++word2Map[c - 'a'];
-            word2Bit = word2Bit | (1 << (c - 'a'));
+        sort(count1.begin(), count1.end(), greater<int>());
+        sort(count2.begin(), count2.end(), greater<int>());
+        for (int i = 0; i < 26; ++i){
+            if (count1[i] != count2[i]) return false;
         }
-        
-        if (word1Bit != word2Bit) return false;
-        sort(word1Map.begin(), word1Map.end());
-        sort(word2Map.begin(), word2Map.end());
-        
-        for (int i = 0; i < 26; ++i) if (word1Map[i] != word2Map[i]) return false;
         return true;
     }
 };
