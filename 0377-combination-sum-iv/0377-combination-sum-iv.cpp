486bf531522d6e2cde55b9067e1b9eb7e7e51e59
Andrew Kim <ksb0724@utexas.edu>
2023-09-09 15:07:41 +0900

    Time: 0 ms (100.00%), Space: 6.8 MB (20.91%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,24 +1,21 @@
+typedef unsigned long long ull;
+
 class Solution {
-private:
-    int n, target;
-    int backtrack(vector<vector<int>>& dp, vector<int>& nums, int curr, int idx){
-        if (idx >= n || curr > target) return 0;
-        if (curr == target) return 1;
-        if (dp[curr][idx] != -1) return dp[curr][idx];
-        int ways = 0;
-        for (int i = 0; i < n; ++i){
-            ways += backtrack(dp, nums, curr + nums[i], i);
-        }
-        
-        return dp[curr][idx] = ways;
-    }
-    
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        this -> n = nums.size();
-        this -> target = target;
-        vector<vector<int>> dp(target + 1, vector<int>(n + 1, -1));
+        sort(nums.begin(), nums.end());
+        vector<ull> dp(target + 1);
+        dp[0] = 1;
         
-        return backtrack(dp, nums, 0, 0);
+        for (int combSum = 0; combSum <= target; ++combSum){
+            for (int num : nums){
+                if (combSum - num >= 0)
+                    dp[combSum] += dp[combSum - num];
+                else
+                    break;
+            }
+        }
+        
+        return dp[target];
     }
 };
