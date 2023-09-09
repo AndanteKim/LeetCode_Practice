6d97662d80401b0b256cab44caa87de4566bc5ad
Andrew Kim <ksb0724@utexas.edu>
2023-09-09 15:27:43 +0900

    Time: 419 ms (90.70%), Space: 21.4 MB (53.73%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,40 +1,19 @@
-class TrieNode:
-    def __init__(self):
-        self.count = 0
-        self.children = defaultdict(TrieNode)
-        
-class Trie:
-    def __init__(self):
-        self.trie = TrieNode()
-    
-    def insert(self, arr: List[int]) -> None:
-        my_trie = self.trie
-        for num in arr:
-            if num not in my_trie.children:
-                my_trie.children[num] = TrieNode()
-            my_trie = my_trie.children[num]
-        my_trie.count += 1
-    
-    def search(self, arr: List[int]) -> int:
-        my_trie = self.trie
-        for num in arr:
-            if num in my_trie.children:
-                my_trie = my_trie.children[num]
-            else:
-                return 0
-        return my_trie.count
-
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
-        my_trie, count = Trie(), 0
-        n = len(grid)
+        rows, cols = dict(), dict()
+        m, n = len(grid), len(grid[0])
         
-        for row in grid:
-            my_trie.insert(row)
+        for i in range(m):
+            rows[tuple(grid[i])] = rows.get(tuple(grid[i]), 0) + 1
+        for i in range(n):
+            curr = []
+            for j in range(m):
+                curr.append(grid[j][i])
+            cols[tuple(curr[:])] = cols.get(tuple(curr[:]), 0) + 1
         
-        for c in range(n):
-            cols = [grid[i][c] for i in range(n)]
-            count += my_trie.search(cols)
-        
-        return count
-        
+        ans = 0
+        for row, freq in rows.items():
+            if row in cols:
+                ans += freq * cols[row]
+                
+        return ans
