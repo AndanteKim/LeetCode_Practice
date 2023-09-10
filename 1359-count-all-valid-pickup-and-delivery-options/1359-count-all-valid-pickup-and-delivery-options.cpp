dbe511ca60656e48762ddb6b0764352f578a0afe
Andrew Kim <ksb0724@utexas.edu>
2023-09-10 15:31:05 +0900

    Time: 2 ms (56.67%), Space: 6 MB (71.30%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -1,28 +1,14 @@
 class Solution {
 public:
     int countOrders(int n) {
-        vector<vector<long>> dp(n + 1, vector<long>(n + 1));
         const int MOD = 1'000'000'007;
-        
-        for (int unpicked = 0; unpicked <= n; ++unpicked){
-            for (int undelivered = unpicked; undelivered <= n; ++undelivered){
-                if (!unpicked && !undelivered){
-                    dp[unpicked][undelivered] = 1;
-                    continue;
-                }
-                
-                if (unpicked > 0){
-                    dp[unpicked][undelivered] = unpicked * dp[unpicked - 1][undelivered];
-                }
-                dp[unpicked][undelivered] %= MOD;
-                if (undelivered > unpicked){
-                    dp[unpicked][undelivered] += (undelivered - unpicked) * dp[unpicked][undelivered - 1];
-                }
-                
-                dp[unpicked][undelivered] %= MOD;
-            }
+        long ans = 1;
+        for (int i = 1; i <= n; ++i){
+            ans *= i;
+            ans *= (2 * i - 1);
+            ans %= MOD;
         }
         
-        return dp[n][n];
+        return ans;
     }
 };
