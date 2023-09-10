30fa1aa69da4cc72eee4f835ad6dcddd94300c73
Andrew Kim <ksb0724@utexas.edu>
2023-09-10 15:28:20 +0900

    Time: 32 ms (93.62%), Space: 16.2 MB (92.77%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,20 +1,12 @@
 class Solution:
     def countOrders(self, n: int) -> int:
         MOD = 1_000_000_007
-        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        ans = 1
         
-        for unpicked in range(n + 1):
-            for undelivered in range(unpicked, n + 1):
-                if not unpicked and not undelivered:
-                    dp[unpicked][undelivered] = 1
-                    continue
-                
-                if unpicked > 0:
-                    dp[unpicked][undelivered] += unpicked * dp[unpicked - 1][undelivered]
-                dp[unpicked][undelivered] %= MOD
-                
-                if undelivered > unpicked:
-                    dp[unpicked][undelivered] += (undelivered - unpicked) * dp[unpicked][undelivered - 1]
-                dp[unpicked][undelivered] %= MOD
+        for i in range(1, n + 1):
+            ans = ans * i
+            
+            ans = ans * (2 * i - 1)
+            ans %= MOD
         
-        return dp[n][n]
+        return ans
