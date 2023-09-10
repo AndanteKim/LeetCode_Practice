45ee6aeb2ada11fbe6cea80583c2c0d3a93a1705
Andrew Kim <ksb0724@utexas.edu>
2023-09-10 15:38:05 +0900

    Time: 27 ms (99.57%), Space: 16.2 MB (92.77%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,12 +1,13 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        MOD = 1_000_000_007
-        ans = 1
+        MOD, ans = 1_000_000_007, 1
         
-        for i in range(1, n + 1):
-            ans = ans * i
+        for i in range(1, 2 * n + 1):
+            ans *= i
             
-            ans = ans * (2 * i - 1)
+            if not i % 2:
+                ans //= 2
             ans %= MOD
-        
+            
         return ans
+        
