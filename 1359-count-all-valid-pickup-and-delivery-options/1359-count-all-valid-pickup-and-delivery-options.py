e51482d741986f184517f759b1dfb197029806f9
Andrew Kim <ksb0724@utexas.edu>
2023-09-10 14:57:00 +0900

    Time: 505 ms (13.19%), Space: 22.1 MB (13.62%) - LeetHub

--- 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.py
@@ -1,25 +1,20 @@
 class Solution:
     def countOrders(self, n: int) -> int:
-        @lru_cache(maxsize = None)
-        def dp(unpicked: int, undelivered: int) -> int:
-            if not unpicked and not undelivered:
-                # completed all orders
-                return 1
-            
-            if unpicked < 0 or undelivered < 0 or undelivered < unpicked:
-                # pick-up should be larger or equal to undelivered since undelivered cannot surpass
-                # pick-up based on the rule
-                return 0
-            
-            # count all choices of picking up an order
-            ans = unpicked * dp(unpicked - 1, undelivered)
-            ans %= MOD
-            
-            # count all choices of delivering a picked order
-            ans += (undelivered - unpicked) * dp(unpicked, undelivered - 1)
-            ans %= MOD
-            
-            return ans
-        
         MOD = 1_000_000_007
-        return dp(n, n)
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
+        
+        for unpicked in range(n + 1):
+            for undelivered in range(unpicked, n + 1):
+                if not unpicked and not undelivered:
+                    dp[unpicked][undelivered] = 1
+                    continue
+                
+                if unpicked > 0:
+                    dp[unpicked][undelivered] += unpicked * dp[unpicked - 1][undelivered]
+                dp[unpicked][undelivered] %= MOD
+                
+                if undelivered > unpicked:
+                    dp[unpicked][undelivered] += (undelivered - unpicked) * dp[unpicked][undelivered - 1]
+                dp[unpicked][undelivered] %= MOD
+        
+        return dp[n][n]
