40f1c25373c5277d580ca286b0d51c57e0a7f8dd
Andrew Kim <ksb0724@utexas.edu>
2023-09-11 14:25:16 +0900

    Time: 8 ms (78.38%), Space: 12.4 MB (96.45%) - LeetHub

--- 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
+++ 1282-group-the-people-given-the-group-size-they-belong-to/1282-group-the-people-given-the-group-size-they-belong-to.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> groupThePeople(vector<int>& groupSizes) {
-        unordered_map<int, vector<int>> m;
-        int n = groupSizes.size();
-        
-        for (int i = 0; i < n; ++i){
-            m[groupSizes[i]].push_back(i);
-        }
-        
-        
         vector<vector<int>> ans;
-        for (auto& [size, indices] : m){
-            vector<int> curr;
-            for (int index: indices){
-                if (curr.size() < size)
-                    curr.push_back(index);
-                else{
-                    ans.push_back(curr);
-                    curr.clear();
-                    curr.push_back(index);
-                }
-            }
+        
+        // A map from group size to the list of indices that are there in the group
+        vector<int> szToGroup[groupSizes.size() + 1];
+        
+        for (int i = 0; i < groupSizes.size(); ++i){
+            szToGroup[groupSizes[i]].push_back(i);
             
-            if (!curr.empty()) {
-                ans.push_back(curr);
-                curr.clear();
+            // When the list size == the group size, empty it and store it in the answer
+            if (szToGroup[groupSizes[i]].size() == groupSizes[i]){
+                ans.push_back(szToGroup[groupSizes[i]]);
+                szToGroup[groupSizes[i]].clear();
             }
         }
         
