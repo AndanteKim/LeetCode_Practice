c831d363b8ec3fa8ecdbeddd3cec5fcd6885c704
Andrew Kim <ksb0724@utexas.edu>
2023-09-12 14:17:12 +0900

    Time: 200 ms (35.10%), Space: 17.2 MB (62.10%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,25 +1,31 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        
         # store the frequency of each character
-        frequencies = [0] * 26
+        frequency = [0] * 26
         for c in s:
-            frequencies[ord(c) - 97] += 1
+            frequency[ord(c) - 97] += 1
+        
+        # Add all non-zero frequencies to max priority queue
+        # Create a max priority queue by flipping the sign of each element
+        pq = []
+        
+        for freq in frequency:
+            if freq:
+                heappush(pq, -freq)
         
         ans = 0
-        # Use a set to store the frequencies we've already seen
-        seen = set()
-        
-        for i in range(26):
-            # Keep decrementing the frequencies we've already seen
-            while frequencies[i] and frequencies[i] in seen:
-                frequencies[i] -= 1
-                ans += 1
+        while len(pq) > 1:
+            # Flip the sign back to positive when removing from the max priority queue
+            top_element = -heappop(pq)
+            
+            # If the top two elements in the priority queue are the same
+            if top_element == -pq[0]:
+                # Decrement the popped value and push it back into the queue
+                if top_element - 1 > 0:
+                    top_element -= 1
+                    heappush(pq, -top_element)
                 
-            # Add the newly occupied frequency to the set
-            seen.add(frequencies[i])
+                ans += 1
         
         return ans
-        
-                    
         
