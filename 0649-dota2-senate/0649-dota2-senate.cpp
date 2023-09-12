0c2c3be8b37210a448953ea860b53f579c7e6ae2
Andrew Kim <ksb0724@utexas.edu>
2023-09-12 19:21:21 +0900

    Time: 9 ms (43.04%), Space: 8 MB (38.25%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.cpp
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -1,39 +1,22 @@
 class Solution {
 public:
     string predictPartyVictory(string senate) {
-        int rCount = count(senate.begin(), senate.end(), 'R');
-        int dCount = senate.size() - rCount;
-        
-        int dFloatingBan = 0, rFloatingBan = 0;
-        queue<char> queue;
-        for (int i = 0; i < senate.size(); ++i) queue.push(senate[i]);
-        
-        while (rCount > 0 && dCount > 0){
-            char curr = queue.front();
-            queue.pop();
-            
-            if (curr == 'D'){
-                if (dFloatingBan > 0){
-                    --dFloatingBan;
-                    --dCount;
-                }
-                else{
-                    ++rFloatingBan;
-                    queue.push(curr);
-                }
-            }
-            else{
-                if (rFloatingBan > 0){
-                    --rFloatingBan;
-                    --rCount;
-                }
-                else {
-                    ++dFloatingBan;
-                    queue.push(curr);
-                }
-            }
+        // Two queues
+        int n = senate.size();
+        queue<int> rQueue, dQueue;
+        for (int i = 0; i < n; ++i) {
+            if (senate[i] == 'R') rQueue.push(i);
+            else dQueue.push(i);
         }
         
-        return dCount == 0? "Radiant" : "Dire";
+        while (!rQueue.empty() && !dQueue.empty()){
+            int rTurn = rQueue.front(), dTurn = dQueue.front();
+            rQueue.pop();
+            dQueue.pop();
+            if (rTurn < dTurn) rQueue.push(rTurn + n);
+            else dQueue.push(dTurn + n);
+        }
+        
+        return !rQueue.empty()? "Radiant" : "Dire";
     }
 };
