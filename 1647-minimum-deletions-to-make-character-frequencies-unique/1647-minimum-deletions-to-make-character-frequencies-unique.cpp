ace4863d0646bfad47855f5dfb12219654a53f89
Andrew Kim <ksb0724@utexas.edu>
2023-09-12 14:34:17 +0900

    Time: 45 ms (89.16%), Space: 17.3 MB (77.52%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int minDeletions(string s) {
+        // store the frequency of each character
+        vector<int> chars(26);
+        for (char& c : s) ++chars[c - 'a'];
         
-        // store the frequency orf each character
-        vector<int> frequencies(26);
-        for (char& c : s){
-            ++frequencies[c - 'a'];
-        }
-        
-        // Use a set to store the frequencies we've already seen
-        unordered_set<int> seen;
+        // add the frequencies to priority queue
         int ans = 0;
-        for (int i = 0; i < 26; ++i){
-            // keep decrementing the frequency until it is unique
-            while (frequencies[i] && seen.find(frequencies[i]) != seen.end()){
-                --frequencies[i];
+        priority_queue<int> pq;
+        for (int i = 0; i < 26; ++i) if (chars[i]) pq.push(chars[i]);
+        
+        while (pq.size() > 1){
+            int topElement = pq.top();
+            pq.pop();
+            
+            // If the top two elements in the priority queue are the same
+            if (topElement == pq.top()){
+                // decrement the popped value and push it back into the queue
+                if (topElement - 1 > 0) {
+                    pq.push(topElement - 1);
+                }
                 ++ans;
             }
-            // add the newly occupied frequency to the set
-            seen.insert(frequencies[i]);
         }
         
         return ans;
