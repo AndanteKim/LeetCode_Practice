1a293eee41c2ce37c576e032289fe460d9d8a9b6
Andrew Kim <ksb0724@utexas.edu>
2023-09-12 15:00:16 +0900

    Time: 51 ms (74.86%), Space: 17.1 MB (92.40%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int minDeletions(string s) {
+        
         // store the frequency of each character
-        vector<int> chars(26);
-        for (char& c : s) ++chars[c - 'a'];
+        vector<int> frequencies(26);
+        for (char& c : s) ++frequencies[c - 'a'];
+        sort(frequencies.begin(), frequencies.end(), greater<int>());
+        // maximum frequency the current character can have
+        int ans = 0, maxFreqAllowed = s.size();
         
-        // add the frequencies to priority queue
-        int ans = 0;
-        priority_queue<int> pq;
-        for (int i = 0; i < 26; ++i) if (chars[i]) pq.push(chars[i]);
-        
-        while (pq.size() > 1){
-            int topElement = pq.top();
-            pq.pop();
-            
-            // If the top two elements in the priority queue are the same
-            if (topElement == pq.top()){
-                // decrement the popped value and push it back into the queue
-                if (topElement - 1 > 0) {
-                    pq.push(topElement - 1);
-                }
-                ++ans;
+        // Iterate over the frequencies in descending order
+        for (int i = 0; i < 26; ++i){
+            // Delete characters to make the frequency equal the maximum frequency allowed
+            if (frequencies[i] > maxFreqAllowed){
+                ans += frequencies[i] - maxFreqAllowed;
+                frequencies[i] = maxFreqAllowed;
             }
+            // Update the maximum allowed frequency
+            maxFreqAllowed = max(0, frequencies[i] - 1);
         }
         
         return ans;
