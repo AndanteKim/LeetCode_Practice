c3d3c4a36f8b08832c1c2884891cd4fe9fcb5ebb
Andrew Kim <ksb0724@utexas.edu>
2023-09-15 20:57:56 +0900

    Time: 331 ms (70.56%), Space: 58.6 MB (54.90%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -33,20 +33,24 @@ public:
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<vector<int>> allEdges;
+        vector<pair<int, pair<int, int>>> allEdges;
         int n = points.size();
+        
+        // store all edges of our complete graph
         for (int currNode = 0; currNode < n; ++currNode){
             for (int nextNode = currNode + 1; nextNode < n; ++nextNode){
                 int weight = abs(points[currNode][0] - points[nextNode][0]) + abs(points[currNode][1] - points[nextNode][1]);
-                allEdges.push_back(vector<int>{weight, currNode, nextNode});
+                allEdges.push_back({weight, {currNode, nextNode}});
             }
         }
-        sort(allEdges.begin(), allEdges.end(), [](vector<int>& a, vector<int>& b) {return a[0] < b[0];});
+        
+        // sort all edges in increasing order
+        sort(allEdges.begin(), allEdges.end());
         
         UnionFind *uf = new UnionFind(n);
         int mstCost = 0, edgesUsed = 0;
-        for (vector<int>& allEdge : allEdges){
-            int weight = allEdge[0], node1 = allEdge[1], node2 = allEdge[2];
+        for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; ++i){
+            int weight = allEdges[i].first, node1 = allEdges[i].second.first, node2 = allEdges[i].second.second;
             
             if (uf -> unite(node1, node2)){
                 mstCost += weight;
