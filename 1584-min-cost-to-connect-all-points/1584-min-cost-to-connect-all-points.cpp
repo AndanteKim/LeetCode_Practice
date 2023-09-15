de9c13678b2e2ce251e7dfc4107745b6a819ccef
Andrew Kim <ksb0724@utexas.edu>
2023-09-15 21:55:07 +0900

    Time: 149 ms (83.16%), Space: 42.3 MB (78.56%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,62 +1,33 @@
-class UnionFind{
-private:
-    vector<int> rank, group;
-
-public:
-    UnionFind(int size){
-        rank.resize(size, 0);
-        group.resize(size);
-        iota(group.begin(), group.end(), 0);
-    }
-    
-    int find(int node){
-        if (group[node] != node) group[node] = find(group[node]);
-        return group[node];
-    }
-    
-    bool unite(int node1, int node2){
-        int group1 = find(node1), group2 = find(node2);
-        
-        if (group1 == group2) return false;
-        
-        if (rank[group1] > rank[group2]) group[group2] = group1;
-        else if (rank[group1] < rank[group2]) group[group1] = group2;
-        else{
-            group[group1] = group2;
-            ++rank[group2];
-        }
-        
-        return true;
-    }
-};
-
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        vector<pair<int, pair<int, int>>> allEdges;
         int n = points.size();
         
-        // store all edges of our complete graph
-        for (int currNode = 0; currNode < n; ++currNode){
-            for (int nextNode = currNode + 1; nextNode < n; ++nextNode){
-                int weight = abs(points[currNode][0] - points[nextNode][0]) + abs(points[currNode][1] - points[nextNode][1]);
-                allEdges.push_back({weight, {currNode, nextNode}});
-            }
-        }
-        
-        // sort all edges in increasing order
-        sort(allEdges.begin(), allEdges.end());
-        
-        UnionFind *uf = new UnionFind(n);
+        // min_heap to store minimum edge at top
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
         int mstCost = 0, edgesUsed = 0;
-        for (int i = 0; i < allEdges.size() && edgesUsed < n - 1; ++i){
-            int weight = allEdges[i].first, node1 = allEdges[i].second.first, node2 = allEdges[i].second.second;
+        
+        // Track nodes which are included in MST
+        vector<bool> InMST(n);
+        
+        while (edgesUsed < n){
+            auto [weight, currNode] = pq.top();
+            pq.pop();
             
-            if (uf -> unite(node1, node2)){
-                mstCost += weight;
-                ++edgesUsed;
-                
-                if (edgesUsed == n - 1) break;
+            // If node was already include in MST we will discard this edge
+            if (InMST[currNode]) continue;
+            InMST[currNode] = true;
+            ++edgesUsed;
+            mstCost += weight;
+            
+            for (int nextNode = 0; nextNode < n; ++nextNode){
+                // If next node is not in MST, then edge from curr node
+                // to next node can be pushed in the priority queue
+                if (!InMST[nextNode]){
+                    int nextWeight = abs(points[currNode][0] - points[nextNode][0]) + abs(points[currNode][1] - points[nextNode][1]);
+                    pq.push({nextWeight, nextNode});
+                }
             }
         }
         
