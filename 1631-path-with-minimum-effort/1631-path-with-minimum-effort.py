ec8d8853ad6e799fdb92d74332be4f05b2e586ff
Andrew Kim <ksb0724@utexas.edu>
2023-09-16 10:15:35 +0900

    Time: 2076 ms (22.27%), Space: 17.5 MB (98.32%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,45 +1,33 @@
-class UnionFind:
-    def __init__(self, size: int):
-        self.parent = [x for x in range(size)]
-        self.rank = [0] * size
-    
-    def find(self, i: int) -> int:
-        if self.parent[i] != i:
-            self.parent[i] = self.find(self.parent[i])
-        return self.parent[i]
-    
-    def union(self, x: int, y: int) -> None:
-        parent_x, parent_y = self.find(x), self.find(y)
-        if parent_x != parent_y:
-            if self.rank[parent_x] > self.rank[parent_y]:
-                self.parent[parent_y] = parent_x
-            elif self.rank[parent_x] < self.rank[parent_y]:
-                self.parent[parent_x] = parent_y
-            else:
-                self.parent[parent_y] = parent_x
-                self.rank[parent_x] += 1
-
 class Solution:
+    def is_possible(self, threshold: int, heights: List[List[int]]) -> bool:
+        queue = deque([(0, 0)])
+        visited = [[False] * self.n for _ in range(self.m)]
+        visited[0][0] = True
+        while queue:
+            x, y = queue.popleft()
+            
+            if x == self.m - 1 and y == self.n - 1:
+                return True
+            
+            for dx, dy in (1, 0), (-1, 0), (0, 1), (0, -1):
+                new_x, new_y = x + dx, y + dy
+                if 0 <= new_x < self.m and 0 <= new_y < self.n and not visited[new_x][new_y] and abs(heights[new_x][new_y] - heights[x][y]) <= threshold:
+                    visited[new_x][new_y] = True
+                    queue.append((new_x, new_y))
+            
+        return False
+    
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
-        row, col = len(heights), len(heights[0])
-        if row == 1 and col == 1:
-            return 0
+        self.m, self.n = len(heights), len(heights[0])
         
-        edge_list = []
-        for curr_row in range(row):
-            for curr_col in range(col):
-                if curr_row > 0:
-                    diff = abs(heights[curr_row][curr_col] - heights[curr_row - 1][curr_col])
-                    edge_list.append((diff, curr_row * col + curr_col, (curr_row - 1) * col + curr_col))
-                if curr_col > 0:
-                    diff = abs(heights[curr_row][curr_col] - heights[curr_row][curr_col - 1])
-                    edge_list.append((diff, curr_row * col + curr_col, curr_row * col + curr_col - 1))
+        left, right = 0, max(max(row) for row in heights)
         
-        edge_list.sort()
-        union_find = UnionFind(row * col)
+        while left <= right:
+            mid = (left + right) >> 1
+            
+            if self.is_possible(mid, heights):
+                right = mid - 1
+            else:
+                left = mid + 1
         
-        for diff, x, y in edge_list:
-            union_find.union(x, y)
-            if union_find.find(0) == union_find.find(row * col - 1):
-                return diff
-        return -1
+        return left
