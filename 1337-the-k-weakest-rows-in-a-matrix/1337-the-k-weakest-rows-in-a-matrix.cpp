5c60454298be748c20a310025fb33623062bc094
Andrew Kim <ksb0724@utexas.edu>
2023-09-18 14:06:16 +0900

    Time: 6 ms (91.97%), Space: 10.7 MB (13.97%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,19 +1,35 @@
 class Solution {
+private:
+    int binarySearch(vector<int>& row){
+        int left = 0, right = row.size();
+        
+        while (left < right){
+            int mid = left + ((right - left) >> 1);
+            if (row[mid] == 1) left = mid + 1;
+            else right = mid;
+        }
+        
+        return left;
+    }
+    
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
         vector<int> ans;
         
         for (int i = 0; i < mat.size(); ++i){
-            pq.push(make_pair(count(mat[i].begin(), mat[i].end(), 1), i));
+            int strength = binarySearch(mat[i]);
+            pair<int, int> entry = make_pair(strength, i);
+            pq.push(entry);
+            if (pq.size() > k) pq.pop();
         }
         
-        for (int i = 0; i < k; ++i){
-            auto [weak, idx] = pq.top();
+        while (!pq.empty()){
+            auto [strength, idx] = pq.top();
             pq.pop();
             ans.push_back(idx);
         }
-        
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
