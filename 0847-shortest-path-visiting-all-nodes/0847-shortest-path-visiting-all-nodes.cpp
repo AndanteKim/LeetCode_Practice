cb9d8f3cd06cdea7b6500946ec06d2893cd5818b
Andrew Kim <ksb0724@utexas.edu>
2023-09-18 14:43:24 +0900

    Time: 11 ms (90.69%), Space: 12.1 MB (76.98%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.cpp
@@ -1,37 +1,38 @@
 class Solution {
-private:
-    int dp(int node, int mask, vector<vector<int>>& cache, vector<vector<int>>& graph){
-        if (cache[node][mask] != 0)
-            return cache[node][mask];
-        
-        if ((mask & (mask - 1)) == 0)
-            // base case: mask only has a single "1", meaning that only one node has
-            // been visited the current node
-            return 0;
-        
-        // avoid infinite loop in recursion
-        cache[node][mask] = INT_MAX - 1;
-        
-        for (int neighbor : graph[node]){
-            if ((mask & (1 << neighbor)) != 0){
-                int alreadyVisited = dp(neighbor, mask, cache, graph);
-                int notVisited = dp(neighbor, mask ^ (1 << node), cache, graph);
-                int betterOption = min(alreadyVisited, notVisited);
-                cache[node][mask] = min(cache[node][mask], 1 + betterOption);
-            }
-        }
-        return cache[node][mask];
-    }
-    
 public:
     int shortestPathLength(vector<vector<int>>& graph) {
-        int n = graph.size(), endingMask = (1 << n) - 1;
-        vector<vector<int>> cache(n + 1, vector<int>(endingMask + 1));
+        if (graph.size() == 1) return 0;
         
-        int ans = INT_MAX;
-        for (int node = 0; node < n; ++node)
-            ans = min(ans, dp(node, endingMask, cache, graph));
+        int n = graph.size(), endingMask = (1 << n) - 1, steps = 0;
+        vector<vector<bool>> seen(n, vector<bool>(endingMask));
+        vector<pair<int, int>> queue;
+        
+        for (int i = 0; i < n; ++i){
+            queue.push_back(make_pair(i, 1 << i));
+            seen[i][1 << i] = true;
+        }
+        
+        while (!queue.empty()){
+            vector<pair<int, int>> nextQueue;
             
-        return ans;
+            for (int i = 0; i < queue.size(); ++i){
+                auto[node, mask] = queue[i];
+                
+                for (int neighbor : graph[node]){
+                    int nextMask = mask | (1 << neighbor);
+                    if (nextMask == endingMask) return 1 + steps;
+                    
+                    if (!seen[neighbor][nextMask]){
+                        seen[neighbor][nextMask] = true;
+                        nextQueue.push_back(make_pair(neighbor, nextMask));
+                    }
+                }
+            }
+            
+            ++steps;
+            queue = nextQueue;
+        }
+        
+        return -1;
     }
 };
