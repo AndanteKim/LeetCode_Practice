d18b5647a7a42b4535afc800be367ac0f78b81b9
Andrew Kim <ksb0724@utexas.edu>
2023-09-18 20:20:22 +0900

    Time: 256 ms (37.22%), Space: 20.3 MB (31.28%) - LeetHub

--- 1136-parallel-courses/1136-parallel-courses.py
+++ 1136-parallel-courses/1136-parallel-courses.py
@@ -1,34 +1,48 @@
 class Solution:
+    def dfs_check_cycle(self, node: int, visited: Dict[int, int], graph: Dict[int, List[int]]) -> bool:
+        # return True if graph has a cycle
+        if node in visited:
+            return visited[node]
+        else:
+            # mark as visiting
+            visited[node] = -1
+        for end_node in graph[node]:
+            if self.dfs_check_cycle(end_node, visited, graph):
+                # we meet a cycle
+                return True
+        # mark as visited
+        visited[node] = False
+        return False
+    
+    def dfs_max_path(self, node: int, visited_length: Dict[int, int], graph: Dict[int, List[int]]) -> int:
+        # return the longest path (inclusive)
+        if node in visited_length:
+            return visited_length[node]
+        max_length = 1
+        for end_node in graph[node]:
+            length = self.dfs_max_path(end_node, visited_length, graph)
+            max_length = max(length + 1, max_length)
+        
+        # store it
+        visited_length[node] = max_length
+        return max_length
+    
     def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
-        graph = {i: [] for i in range(1, n + 1)}
-        indegree = {i: 0 for i in range(1, n + 1)}
+        graph = {i : [] for i in range(1, n + 1)}
         
-        for prev, nxt in relations:
-            graph[prev].append(nxt)
-            indegree[nxt] += 1
+        for start_node, end_node in relations:
+            graph[start_node].append(end_node)
         
-        queue = []
-        # We use list here since we are not poping from
-        # front this code
-        for node in graph:
-            if indegree[node] == 0:
-                queue.append(node)
+        # check if the graph contains a cycle
+        visited = dict()
         
-        step, semesters = 0, 0
+        # If having cycle, return -1
+        for node in graph.keys():
+            if self.dfs_check_cycle(node, visited, graph):
+                return -1
         
-        # start learning with BFS
-        while queue:
-            step += 1
-            next_queue = []
-            for node in queue:
-                semesters += 1
-                end_nodes = graph[node]
-                for end_node in end_nodes:
-                    indegree[end_node] -= 1
-                    
-                    # if all prerequisite courses learned
-                    if indegree[end_node] == 0:
-                        next_queue.append(end_node)
-            queue = next_queue
+        # if no cycle, return the longest path
+        visited_length = dict()
         
-        return step if semesters == n else -1
+        return max(self.dfs_max_path(node, visited_length, graph) for node in graph.keys())
+        
