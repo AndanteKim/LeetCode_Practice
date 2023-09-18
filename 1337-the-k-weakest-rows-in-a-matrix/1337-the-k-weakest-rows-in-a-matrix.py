56dcdb2323f9f53402206edcb3818cb1ffbaa01f
Andrew Kim <ksb0724@utexas.edu>
2023-09-18 13:49:52 +0900

    Time: 105 ms (67.48%), Space: 16.7 MB (70.98%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -1,11 +1,29 @@
 class Solution:
-    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        pq, ans = [], []
-        for i in range(len(mat)):
-            heappush(pq, (mat[i].count(1), i))
+    def binary_search(self, row: List[int]) -> int:
+        low, high = 0, len(row)
         
-        for i in range(k):
-            weak, idx = heappop(pq)
-            ans.append(idx)
-            
-        return ans
+        while low < high:
+            mid = (low + high) >> 1
+            if row[mid] == 1:
+                low = mid + 1
+            else:
+                high = mid
+        return low
+    
+    def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
+        m, n = len(mat), len(mat[0])
+        pq, ans = [], []
+        
+        for i, row in enumerate(mat):
+            strength = self.binary_search(row)
+            entry = (-strength, -i)
+            if len(pq) < k or entry > pq[0]:
+                heappush(pq, entry)
+            if len(pq) > k:
+                heapq.heappop(pq)
+        
+        while pq:
+            strength, i = heappop(pq)
+            ans.append(-i)
+        
+        return ans[::-1]
