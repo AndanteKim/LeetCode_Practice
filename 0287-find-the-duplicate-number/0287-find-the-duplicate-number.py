e5d414581ba6613e3519498961ac530cfeafd6cd
Andrew Kim <ksb0724@utexas.edu>
2023-09-19 20:59:09 +0900

    Time: 1276 ms (5.04%), Space: 30.9 MB (80.88%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -1,9 +1,24 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        nums.sort()
+        duplicate, n = 0, len(nums) - 1
+        bits = n.bit_length()
         
-        for num in nums:
-            if bisect_right(nums, num) - bisect_left(nums, num) > 1:
-                return num
-            
-        return -1
+        for bit in range(bits):
+            mask = 1 << bit
+            base_count, nums_count = 0, 0
+            for i in range(n + 1):
+                # If bit is set in number (i) then add 1 to base_count
+                if i & mask:
+                    base_count += 1
+                
+                # If bit is set in number[i] then add 1 to nums_count
+                if nums[i] & mask:
+                    nums_count += 1
+                
+            # If the current bit is more frequently set in nums than it is in
+            # the range [1, 2, ..., n] then it must also be set in the duplicate number
+            if nums_count - base_count > 0:
+                duplicate |= mask
+        return duplicate
+        
+        
