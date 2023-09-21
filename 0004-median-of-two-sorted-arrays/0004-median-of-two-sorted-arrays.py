58ce9cceb9e81748be3fa075dbb9c1d106540067
Andrew Kim <ksb0724@utexas.edu>
2023-09-21 23:18:43 +0900

    Time: 84 ms (86.87%), Space: 16.5 MB (69.30%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,33 +1,27 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        n1, n2 = len(nums1), len(nums2)
-        n = n1 + n2
+        m, n = len(nums1), len(nums2)
+        if m > n:
+            return self.findMedianSortedArrays(nums2, nums1)
         
-        def solve(k: int, n1_start: int, n1_end: int, n2_start: int, n2_end: int) -> int:
-            # If the segment of an array is empty, it means we have passed all its element
-            #, just return the corresponding element in the other array
-            if n1_start > n1_end:
-                return nums2[k - n1_start]
-            if n2_start > n2_end:
-                return nums1[k - n2_start]
+        left, right = 0, m
+        
+        while left <= right:
+            partA = (left + right) >> 1
+            partB = ((m + n + 1) >> 1) - partA
             
-            # Get the middle indices and middle values of nums1, nums2
-            n1_index, n2_index = (n1_start + n1_end) >> 1, (n2_start + n2_end) >> 1
-            n1_val, n2_val = nums1[n1_index], nums2[n2_index]
+            maxLeftA = float('-inf') if partA == 0 else nums1[partA - 1]
+            minRightA = float('inf') if partA == m else nums1[partA]
+            maxLeftB = float('-inf') if partB == 0 else nums2[partB - 1]
+            minRightB = float('inf') if partB == n else nums2[partB]
             
-            # If k is in the right half of nums1 + nums2, remove the larger right half.
-            if n1_index + n2_index < k:
-                if n1_val > n2_val:
-                    return solve(k, n1_start, n1_end, n2_index + 1, n2_end)
+            if maxLeftA <= minRightB and maxLeftB <= minRightA:
+                if (m + n) % 2 == 0:
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2
                 else:
-                    return solve(k, n1_index + 1, n1_end, n2_start, n2_end)
+                    return max(maxLeftA, maxLeftB)
+            elif maxLeftA > minRightB:
+                right = partA - 1
             else:
-                if n1_val > n2_val:
-                    return solve(k, n1_start, n1_index - 1, n2_start, n2_end)
-                else:
-                    return solve(k, n1_start, n1_end, n2_start, n2_index - 1)
-        
-        if n % 2:
-            return solve(n // 2, 0, n1 - 1, 0, n2 - 1)
-        else:
-            return (solve(n // 2 - 1, 0, n1 - 1, 0, n2 - 1) + solve(n // 2, 0, n1 - 1, 0, n2 - 1)) / 2
+                left = partA + 1
+            
