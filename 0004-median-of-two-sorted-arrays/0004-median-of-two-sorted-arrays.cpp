afec75550f44601f9500e6c049d2ffff277642f4
Andrew Kim <ksb0724@utexas.edu>
2023-09-21 23:34:49 +0900

    Time: 27 ms (59.06%), Space: 89.6 MB (55.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,31 @@
 class Solution {
-private:
-    int solve(vector<int>& nums1, vector<int>& nums2, int k, int n1Start, int n1End, int n2Start, int n2End){
-        if (n1Start > n1End)
-            return nums2[k - n1Start];
-        if (n2Start > n2End)
-            return nums1[k - n2Start];
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size();
+        if (m > n) return findMedianSortedArrays(nums2, nums1);
         
-        int n1Mid = (n1Start + n1End) >> 1, n2Mid = (n2Start + n2End) >> 1;
-        int n1Val = nums1[n1Mid], n2Val = nums2[n2Mid];
-        
-        if (n1Mid + n2Mid < k){
-            if (n1Val > n2Val)
-                return solve(nums1, nums2, k, n1Start, n1End, n2Mid + 1, n2End);
+        int left = 0, right = m;
+        while (left <= right){
+            int partA = (left + right) >> 1;
+            int partB = ((m + n + 1) >> 1) - partA;
+            
+            int maxLeftA = partA == 0? INT_MIN : nums1[partA - 1];
+            int minRightA = partA == m? INT_MAX : nums1[partA];
+            int maxLeftB = partB == 0? INT_MIN : nums2[partB - 1];
+            int minRightB = partB == n? INT_MAX : nums2[partB];
+            
+            if (maxLeftA <= minRightB && maxLeftB <= minRightA){
+                if ((m + n) % 2 == 0)
+                    return (max(maxLeftA, maxLeftB) + min(minRightA, minRightB)) / 2.0;
+                else
+                    return max(maxLeftA, maxLeftB);
+            }
+            else if (maxLeftA > minRightB)
+                right = partA - 1;
             else
-                return solve(nums1, nums2, k, n1Mid + 1, n1End, n2Start, n2End);
-        }
-        else{
-            if (n1Val > n2Val)
-                return solve(nums1, nums2, k, n1Start, n1Mid - 1, n2Start, n2End);
-            else
-                return solve(nums1, nums2, k, n1Start, n1End, n2Start, n2Mid - 1);
+                left = partA + 1;
         }
         
         return -1;
-    }
-    
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size(), n2 = nums2.size();
-        int n = n1 + n2;
-        
-        if (n % 2)
-            return solve(nums1, nums2, n / 2, 0, n1 - 1, 0, n2 - 1);
-        else
-            return (double)(solve(nums1, nums2, n / 2 - 1, 0, n1 - 1, 0, n2 - 1) + solve(nums1, nums2, n / 2, 0, n1 - 1, 0, n2 - 1)) / 2;
     }
 };
