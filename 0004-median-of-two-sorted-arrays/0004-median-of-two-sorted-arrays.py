3c5f54cd4bd097a3d1b5a1d2d2e7112946417bc6
Andrew Kim <ksb0724@utexas.edu>
2023-09-21 16:58:09 +0900

    Time: 87 ms (76.31%), Space: 16.5 MB (69.30%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,19 +1,33 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         i, j, m, n = 0, 0, len(nums1), len(nums2)
-        arr = []
         
-        while i < m and j < n:
-            if nums1[i] <= nums2[j]:
-                arr.append(nums1[i])
+        # Get the smaller value between nums1[p1] and nums2[p2]
+        def get_min():
+            nonlocal i, j
+            if i < m and j < n:
+                if nums1[i] < nums2[j]:
+                    ans = nums1[i]
+                    i += 1
+                else:
+                    ans = nums2[j]
+                    j += 1
+            elif j == n:
+                ans = nums1[i]
                 i += 1
             else:
-                arr.append(nums2[j])
+                ans = nums2[j]
                 j += 1
-                
-        if i < m:
-            arr += nums1[i:]
-        elif j < n:
-            arr += nums2[j:]
+            return ans
+        
+        
+        if (m + n) % 2 == 0:
+            for _ in range((m + n) // 2 - 1):
+                _ = get_min()
             
-        return arr[(m + n) // 2] if (m + n) % 2 else 0.5 * (arr[(m + n) // 2 - 1] + arr[(m + n) // 2])
+            return (get_min() + get_min()) / 2
+        
+        else:
+            for _ in range((m + n) // 2):
+                _ = get_min()
+            return get_min()
