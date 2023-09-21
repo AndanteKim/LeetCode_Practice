304037d9ae0cb00ba9ea23c2830a547fa6487f19
Andrew Kim <ksb0724@utexas.edu>
2023-09-21 23:10:35 +0900

    Time: 22 ms (78.53%), Space: 89.6 MB (55.15%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,35 +1,38 @@
 class Solution {
 private:
-    int i = 0, j = 0, m, n;
-    // Get the smaller value between nums1[i] and nums2[j] and move the pointer forward
-    int getMin(vector<int>& nums1, vector<int>& nums2){
-        if (i < m && j < n)
-            return nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
-        else if (i < m)
-            return nums1[i++];
-        else if (j < n)
-            return nums2[j++];
+    int solve(vector<int>& nums1, vector<int>& nums2, int k, int n1Start, int n1End, int n2Start, int n2End){
+        if (n1Start > n1End)
+            return nums2[k - n1Start];
+        if (n2Start > n2End)
+            return nums1[k - n2Start];
+        
+        int n1Mid = (n1Start + n1End) >> 1, n2Mid = (n2Start + n2End) >> 1;
+        int n1Val = nums1[n1Mid], n2Val = nums2[n2Mid];
+        
+        if (n1Mid + n2Mid < k){
+            if (n1Val > n2Val)
+                return solve(nums1, nums2, k, n1Start, n1End, n2Mid + 1, n2End);
+            else
+                return solve(nums1, nums2, k, n1Mid + 1, n1End, n2Start, n2End);
+        }
+        else{
+            if (n1Val > n2Val)
+                return solve(nums1, nums2, k, n1Start, n1Mid - 1, n2Start, n2End);
+            else
+                return solve(nums1, nums2, k, n1Start, n1End, n2Start, n2Mid - 1);
+        }
+        
         return -1;
     }
     
-    
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        this -> m = nums1.size(), this -> n = nums2.size();
+        int n1 = nums1.size(), n2 = nums2.size();
+        int n = n1 + n2;
         
-        if ((m + n) % 2 == 0){
-            for (int i = 0; i < (m + n) / 2 - 1; ++i){
-                int _ = getMin(nums1, nums2);
-            }
-            return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
-        }
-        else{
-            for (int i = 0; i < (m + n) / 2; ++i){
-                int _ = getMin(nums1, nums2);
-            }
-            return getMin(nums1, nums2);
-        }
-        
-        return -1;
+        if (n % 2)
+            return solve(nums1, nums2, n / 2, 0, n1 - 1, 0, n2 - 1);
+        else
+            return (double)(solve(nums1, nums2, n / 2 - 1, 0, n1 - 1, 0, n2 - 1) + solve(nums1, nums2, n / 2, 0, n1 - 1, 0, n2 - 1)) / 2;
     }
 };
