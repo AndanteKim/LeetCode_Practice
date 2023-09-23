f81c39e43137639949142a717d59c374302f6cbf
Andrew Kim <ksb0724@utexas.edu>
2023-09-23 23:07:25 +0900

    Time: 51 ms (98.11%), Space: 17 MB (51.45%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,33 +1,25 @@
 class Solution {
-private:
-    int dfs(unordered_set<string>& words, unordered_map<string, int>& memo, string& currWord){
-        if (memo.find(currWord) != memo.end())
-            return memo[currWord];
-        
-        int maxLength = 1;
-        for (int i = 0; i < currWord.size(); ++i){
-            string newWord = currWord.substr(0, i) + currWord.substr(i + 1);
-            if (words.find(newWord) != words.end()){
-                maxLength = max(maxLength, 1 + dfs(words, memo, newWord));
-            }
-        }
-        
-        return memo[currWord] = maxLength;
-    }
-    
 public:
     int longestStrChain(vector<string>& words) {
-        unordered_map<string, int> memo;
-        unordered_set<string> wordsPresent;
+        unordered_map<string, int> dp;
+        sort(words.begin(), words.end(), [](const string& w1, const string& w2){return w1.size() < w2.size();});
         
-        for (string& word : words){
-            wordsPresent.insert(word);
+        int ans = 1;
+        
+        for (const string& word : words){
+            int currLength = 1;
+            
+            for (int i = 0; i < word.size(); ++i){
+                string predecessor = word.substr(0, i) + word.substr(i + 1);
+                if (dp.find(predecessor) != dp.end()){
+                    int prevLength = dp[predecessor];
+                    currLength = max(currLength, prevLength + 1);
+                }
+            }
+            dp[word] = currLength;
+            ans = max(ans, currLength);
         }
         
-        int ans = 0;
-        for (string& word : words){
-            ans = max(ans, dfs(wordsPresent, memo, word));
-        }
         return ans;
     }
 };
