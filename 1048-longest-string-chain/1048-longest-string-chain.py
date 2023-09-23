301a84076f6f11405800e7f2eca2a97b873af11a
Andrew Kim <ksb0724@utexas.edu>
2023-09-23 23:01:22 +0900

    Time: 126 ms (75.09%), Space: 16.7 MB (93.85%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.py
+++ 1048-longest-string-chain/1048-longest-string-chain.py
@@ -1,31 +1,20 @@
 class Solution:
-    def dfs(self, words: Set[str], memo: Dict[str, int], current_word: str) -> int:
-        # If the word is encountered previously we just return its value present in the map(memoization)
-        if current_word in memo:
-            return memo[current_word]
-        
-        # This stores the maximum length of word sequence possible with the 'currentWord' as the
-        # creating all possible strings taking out one character at a time from the current_word
-        max_length = 1
-        
-        for i in range(len(current_word)):
-            new_word = current_word[:i] + current_word[i + 1:]
-            # If the new word formed is present in the list, we do a dfs search with this new word
-            if new_word in words:
-                curr_length = 1 + self.dfs(words, memo, new_word)
-                max_length = max(max_length, curr_length)
-        memo[current_word] = max_length
-        return max_length
-    
     def longestStrChain(self, words: List[str]) -> int:
-        memo, words_present = dict(), set()
+        dp = dict()
+        words.sort(key = len)
+        ans = 1
         
         for word in words:
-            words_present.add(word)
-        
-        ans = 0
-        for word in words:
-            ans = max(ans, self.dfs(words_present, memo, word))
+            curr_length = 1
+            
+            # find all possible predecessors for the current word by removing one letter at a time
+            for i in range(len(word)):
+                predecessor = word[:i] + word[i + 1 : len(word) + 1]
+                
+                if predecessor in dp:
+                    prev_length = dp[predecessor]
+                    curr_length = max(curr_length, prev_length + 1)
+            dp[word] = curr_length
+            ans = max(ans, curr_length)
         
         return ans
-        
