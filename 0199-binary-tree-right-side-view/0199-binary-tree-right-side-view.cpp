ca21e44700662304337f6f101495b8dded60fc15
Andrew Kim <ksb0724@utexas.edu>
2023-09-25 18:11:57 +0900

    Time: 0 ms (100.00%), Space: 12.3 MB (19.03%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -10,19 +10,32 @@
  * };
  */
 class Solution {
-    vector<int> rightside;
-    void helper(TreeNode* node, int level){
-        if (level == rightside.size()) rightside.push_back(node -> val);
-        for (TreeNode* child : {node -> right, node -> left}){
-            if (child) helper(child, level + 1);
-        }
-    }
-    
 public:
     vector<int> rightSideView(TreeNode* root) {
-        if (!root) return {};
+        if (!root)
+            return {};
         
-        helper(root, 0);
-        return rightside;
+        queue<TreeNode*> queue;
+        queue.push(root);
+        vector<int> ans;
+        
+        while (!queue.empty()){
+            int n = queue.size();
+            
+            for (int i = 0; i < n; ++i){
+                TreeNode* node = queue.front();
+                queue.pop();
+                
+                if (i == n - 1)
+                    ans.push_back(node -> val);
+                
+                if (node -> left)
+                    queue.push(node -> left);
+                if (node -> right)
+                    queue.push(node -> right);
+            }
+        }
+        
+        return ans;
     }
 };
