c3de3953c13cace847a967820da1b7684066a265
Andrew Kim <ksb0724@utexas.edu>
2023-09-25 10:51:03 +0900

    Time: 39 ms (71.87%), Space: 16.3 MB (76.19%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.py
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -1,11 +1,4 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
-        ch = 0
         
-        for char_ in s:
-            ch ^= ord(char_)
-        
-        for char_ in t:
-            ch ^= ord(char_)
-        
-        return chr(ch)
+        return "".join(c for c in Counter(t) - Counter(s))
