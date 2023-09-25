6c95badd4a08203ccc537e827c3294165304ed5d
Andrew Kim <ksb0724@utexas.edu>
2023-09-25 10:54:48 +0900

    Time: 0 ms (100.00%), Space: 6.9 MB (16.46%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char, int> counter_s;
-        char ans;
-        for (const auto &c: s)++counter_s[c];
+        int ch = 0;
+        for (char& c : s)
+            ch ^= c;
         
-        for (const auto &c: t){
-            if (counter_s.find(c) == counter_s.end() || counter_s[c] == 0) {ans = c; break;}
-            else --counter_s[c];
-        }
-        return ans;
+        for (char& c : t)
+            ch ^= c;
+        
+        return (char)ch;
     }
 };
