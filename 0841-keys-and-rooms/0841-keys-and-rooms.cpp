b6329c737fa12273ae3c24ae6af940490fb64dbd
Andrew Kim <ksb0724@utexas.edu>
2023-09-28 01:37:04 +0900

    Time: 11 ms (27.18%), Space: 10.8 MB (28.38%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,16 +1,18 @@
 class Solution {
-    void dfs(int node, vector<vector<int>>& rooms, vector<bool>& visited){
-        visited[node] = true;
-        for (int room : rooms[node]){
-            if(!visited[room]) dfs(room, rooms, visited);
+private:
+    void dfs(int start, vector<bool>& visited, vector<vector<int>>& rooms){
+        if (!visited[start]){
+            visited[start] = true;
+            for (int neighbor : rooms[start])
+                dfs(neighbor, visited, rooms);
         }
     }
     
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        vector<bool> visited(rooms.size(), false);
-        dfs(0, rooms, visited);
-        
-        return count(visited.begin(), visited.end(), false) == 0;
+        int n = rooms.size();
+        vector<bool> visited(n);
+        dfs(0, visited, rooms);
+        return count(visited.begin(), visited.end(), true) == n;
     }
 };
