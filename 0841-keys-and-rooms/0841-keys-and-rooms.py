1c73801332c7e1fb5b3b613a2b9b6b51ccc8df9e
Andrew Kim <ksb0724@utexas.edu>
2023-09-28 01:35:05 +0900

    Time: 69 ms (70.94%), Space: 17.2 MB (30.98%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.py
+++ 0841-keys-and-rooms/0841-keys-and-rooms.py
@@ -1,15 +1,14 @@
 class Solution:
-    
-    def dfs(self, node: int, rooms: List[List[int]], visited: List[int]) -> None:
-        visited[node] = True
-        for key in rooms[node]:
-            if not visited[key]:
-                self.dfs(key, rooms, visited)
-        
+    def dfs(self, start: int, visited: List[bool], rooms: List[List[int]]) -> None:
+        if not visited[start]:
+            visited[start] = True
+            for neighbor in rooms[start]:
+                self.dfs(neighbor, visited, rooms)
     
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
         n = len(rooms)
         visited = [False] * n
-        self.dfs(0, rooms, visited)
-                
+        
+        self.dfs(0, visited, rooms)
+        
         return all(visited)
