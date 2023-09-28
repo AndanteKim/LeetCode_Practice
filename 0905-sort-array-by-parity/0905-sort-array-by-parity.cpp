70cc607434dbbe668e5180858c3eec7771dd8871
Andrew Kim <ksb0724@utexas.edu>
2023-09-28 21:51:41 +0900

    Time: 7 ms (66.47%), Space: 16.5 MB (25.63%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-        sort(nums.begin(), nums.end(), [](int a, int b){return a % 2 < b % 2;});
+        // two pointer with in-place array
+        int i = 0, j = nums.size() - 1;
+        
+        while (i < j){
+            if (nums[i] % 2 > nums[j] % 2)
+                swap(nums[i], nums[j]);
+            if (nums[i] % 2 == 0)
+                ++i;
+            if (nums[j] % 2)
+                --j;
+        }
+        
         return nums;
     }
 };
