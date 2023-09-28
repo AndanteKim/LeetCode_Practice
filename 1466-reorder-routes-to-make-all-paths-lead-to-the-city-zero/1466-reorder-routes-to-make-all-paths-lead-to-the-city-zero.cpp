b24acd94e07e12d2d0eadabbeb1dc145185e7e4a
Andrew Kim <ksb0724@utexas.edu>
2023-09-29 00:20:22 +0900

    Time: 268 ms (72.87%), Space: 100.3 MB (92.66%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,11 +1,21 @@
 class Solution {
 private:
     int ans = 0;
-    void dfs(int node, int parent, vector<vector<pair<int, int>>>& adj){
-        for (auto& [child, sign] : adj[node]){
-            if (child != parent){
-                ans += sign;
-                dfs(child, node, adj);
+    void bfs(int start, int n, vector<vector<pair<int, int>>>& adj){
+        queue<int> queue;
+        queue.push(start);
+        vector<bool> visited(n);
+        visited[start] = true;
+        
+        while (!queue.empty()){
+            int node = queue.front();
+            queue.pop();
+            for (auto& [neighbor, sign] : adj[node]){
+                if (!visited[neighbor]){
+                    visited[neighbor] = true;
+                    ans += sign;
+                    queue.push(neighbor);
+                }
             }
         }
     }
@@ -13,13 +23,12 @@ private:
 public:
     int minReorder(int n, vector<vector<int>>& connections) {
         vector<vector<pair<int, int>>> adj(n);
-        
-        for (vector<int>& connection : connections){
-            adj[connection[0]].push_back(make_pair(connection[1], 1));
-            adj[connection[1]].push_back(make_pair(connection[0], 0));
+        for(vector<int>& connection : connections){
+            adj[connection[0]].push_back({connection[1], 1});
+            adj[connection[1]].push_back({connection[0], 0});
         }
         
-        dfs(0, -1, adj);
+        bfs(0, n, adj);
         return ans;
     }
 };
