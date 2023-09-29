66570faccaa48d69c0edf3e1f7c9153ce576ef3a
Andrew Kim <ksb0724@utexas.edu>
2023-09-30 01:50:44 +0900

    Time: 129 ms (35.19%), Space: 97 MB (9.17%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.cpp
+++ 0896-monotonic-array/0896-monotonic-array.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
     bool isMonotonic(vector<int>& nums) {
-        if (nums.size() == 1)
-            return true;
+        bool increasing = true, decreasing = true;
         
-        int n = nums.size();
-        bool increasing = false, decreasing = false;
-        
-        for (int i = 0; i < n - 1; ++i){
-            if (nums[i] >= nums[i + 1]){
-                if (i == n - 2)
-                    increasing = true;
-            }
-            else
-                break;
-        }
-        
-        for (int i = 0; i < n - 1; ++i){
-            if (nums[i] <= nums[i + 1]){
-                if (i == n - 2)
-                    decreasing = true;
-            }
-            else
-                break;
+        for (int i = 0; i < nums.size() - 1; ++i){
+            if (nums[i] > nums[i + 1])
+                increasing = false;
+            if (nums[i] < nums[i + 1])
+                decreasing = false;
         }
         
         return increasing || decreasing;
