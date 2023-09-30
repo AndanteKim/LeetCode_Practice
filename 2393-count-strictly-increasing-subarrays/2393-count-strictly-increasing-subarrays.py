725a610203074adbfbd2d8fa28e7c4ba6b3be701
Andrew Kim <ksb0724@utexas.edu>
2023-10-01 00:15:10 +0900

    Time: 683 ms (43.62%), Space: 30.9 MB (59.57%) - LeetHub

--- 2393-count-strictly-increasing-subarrays/2393-count-strictly-increasing-subarrays.py
+++ 2393-count-strictly-increasing-subarrays/2393-count-strictly-increasing-subarrays.py
@@ -1,17 +1,12 @@
 class Solution:
     def countSubarrays(self, nums: List[int]) -> int:
-        # greedy algorithm
-        ans, i, n = 0, 0, len(nums)
-        
-        while i < n:
-            # length of the current subarray
-            curr = 1
-            while i + 1 < n and nums[i] < nums[i + 1]:
-                curr += 1
-                i += 1
-                
-            # add the total number of different subarrays possible from this length
-            ans += (curr * (curr + 1)) // 2
-            i += 1
+        # sliding window
+        left = prev = ans = 0
+        for right in range(len(nums)):
+            if prev >= nums[right]:
+                left = right
+            ans += right - left + 1
+            prev = nums[right]
         
         return ans
+        
