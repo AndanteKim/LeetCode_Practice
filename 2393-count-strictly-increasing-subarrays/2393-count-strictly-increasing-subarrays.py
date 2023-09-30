64c679cedb09a356363acbacf7bb2e4cdf2cae2c
Andrew Kim <ksb0724@utexas.edu>
2023-10-01 00:05:40 +0900

    Time: 682 ms (43.62%), Space: 30.8 MB (84.04%) - LeetHub

--- 2393-count-strictly-increasing-subarrays/2393-count-strictly-increasing-subarrays.py
+++ 2393-count-strictly-increasing-subarrays/2393-count-strictly-increasing-subarrays.py
@@ -1,11 +1,17 @@
 class Solution:
     def countSubarrays(self, nums: List[int]) -> int:
-        # Kadane's algorithm
-        ans = cnt = 0
+        # greedy algorithm
+        ans, i, n = 0, 0, len(nums)
         
-        for i in range(len(nums)):
-            if i and nums[i - 1] >= nums[i]:
-                cnt = 0
-            cnt += 1
-            ans += cnt
+        while i < n:
+            # length of the current subarray
+            curr = 1
+            while i + 1 < n and nums[i] < nums[i + 1]:
+                curr += 1
+                i += 1
+                
+            # add the total number of different subarrays possible from this length
+            ans += (curr * (curr + 1)) // 2
+            i += 1
+        
         return ans
