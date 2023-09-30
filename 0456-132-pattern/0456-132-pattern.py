f09310867053b936144a17fe64bd255852aaf69f
Andrew Kim <ksb0724@utexas.edu>
2023-09-30 23:30:33 +0900

    Time: 439 ms (12.17%), Space: 36.4 MB (80.68%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -2,24 +2,20 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         if len(nums) < 3:
             return False
-        
-        stack, n = [], len(nums)
+        n = len(nums)
         min_arr = [-1] * n
         min_arr[0] = nums[0]
         
         for i in range(1, n):
             min_arr[i] = min(min_arr[i - 1], nums[i])
-            
-        for j in range(n - 1, -1, -1):
+        
+        k = n
+        for j in range(len(nums) - 1, -1, -1):
             if nums[j] <= min_arr[j]:
                 continue
-            
-            # monotonic stack
-            while stack and stack[-1] <= min_arr[j]:
-                stack.pop()
-                
-            if stack and stack[-1] < nums[j]:
+            k = bisect_left(nums, min_arr[j] + 1, k, n)
+            if k < n and nums[k] < nums[j]:
                 return True
-            stack.append(nums[j])
-            
+            k -= 1
+            nums[k] = nums[j]
         return False
