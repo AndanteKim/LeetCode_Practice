dafa7aaa215a9625a8a3853e4eb070ef1624ff30
Andrew Kim <ksb0724@utexas.edu>
2023-09-30 23:45:45 +0900

    Time: 413 ms (43.10%), Space: 36.4 MB (80.68%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -2,18 +2,21 @@ class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
         if len(nums) < 3:
             return False
+        
         n = len(nums)
         min_arr = [-1] * n
         min_arr[0] = nums[0]
-        
         for i in range(1, n):
             min_arr[i] = min(min_arr[i - 1], nums[i])
         
         k = n
-        for j in range(len(nums) - 1, -1, -1):
+        for j in range(n - 1, -1, -1):
             if nums[j] <= min_arr[j]:
                 continue
-            k = bisect_left(nums, min_arr[j] + 1, k, n)
+                
+            while k < n and nums[k] <= min_arr[j]:
+                k += 1
+            
             if k < n and nums[k] < nums[j]:
                 return True
             k -= 1
