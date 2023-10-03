e310d444686948cb95f75a100b37e62098f6d5df
Andrew Kim <ksb0724@utexas.edu>
2023-10-03 15:30:45 +0900

    Time: 50 ms (88.44%), Space: 16.2 MB (95.44%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,35 +1,28 @@
 class Solution:
-    def is_valid(self, x: int, y: int, rows: int, cols: int, visited: List[List[int]], grid: List[List[int]]) -> bool:
-        if x < 0 or x >= rows or y < 0 or y >= cols or visited[x][y] or grid[x][y] != 1:
-            return False
-        visited[x][y] = True
-        grid[x][y] = 2
-        return True
+    def is_valid(self, x: int, y: int, grid: List[List[int]], visited: List[List[bool]]) -> bool:
+        return 0 <= x < len(grid) and 0 <= y < len(grid[0]) and not visited[x][y] and grid[x][y] == 1
     
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        ans, fresh_orange = 0, 0
-        rows, cols = len(grid), len(grid[0])
-        queue = deque()
+        queue, rows, cols, fresh, ans = deque(), len(grid), len(grid[0]), 0, 0
+        visited = [[False] * cols for _ in range(rows)]
+        
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 2:
-                    queue.append((i, j))
-                elif grid[i][j] == 1:
-                    fresh_orange += 1
-        
-        visited = [[False] * cols for _ in range(rows)]
-        while queue:
-            for _ in range(len(queue)):
-                x, y = queue.popleft()
+                if grid[i][j] == 1:
+                    fresh += 1
                 
-                for new_x, new_y in ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)):
-                    if self.is_valid(new_x, new_y, rows, cols, visited, grid):
-                        queue.append((new_x, new_y))
-                        fresh_orange -= 1
-            if queue:
-                ans += 1
-        
-        return ans if fresh_orange == 0 else -1
-        
-        
-        
+                if grid[i][j] == 2:
+                    queue.append((i, j, 0))
+                    
+        while queue:
+            x, y, days = queue.popleft()
+            
+            ans = max(ans, days)
+            for new_x, new_y in (x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1):
+                if self.is_valid(new_x, new_y, grid, visited):
+                    visited[new_x][new_y] = True
+                    grid[new_x][new_y] = 2
+                    fresh -= 1
+                    queue.append((new_x, new_y, days + 1))
+            
+        return -1 if fresh > 0 else ans
