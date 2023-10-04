209e6e90b81d94d4db7cac665405b51beeb1cac3
Andrew Kim <ksb0724@utexas.edu>
2023-10-04 18:20:38 +0900

    Time: 242 ms (39.85%), Space: 20.1 MB (58.72%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.py
+++ 0706-design-hashmap/0706-design-hashmap.py
@@ -1,46 +1,42 @@
-class Bucket:
-    def __init__(self):
-        self.bucket = []
+class ListNode:
+    def __init__(self, key: int = -1, val: int = -1, next: Optional[ListNode] = None):
+        self.key = key
+        self.val = val
+        self.next = next
     
-    def get(self, key: int) -> int:
-        for (k, v) in self.bucket:
-            if k == key:
-                return v
-        return -1
-    
-    def update(self, key: int, value: int) -> None:
-        found = False
-        for i, kv in enumerate(self.bucket):
-            if key == kv[0]:
-                self.bucket[i] = (key, value)
-                found = True
-                break
-        if not found:
-            self.bucket.append((key, value))
-            
-    def remove(self, key: int) -> None:
-        for i, kv in enumerate(self.bucket):
-            if key == kv[0]:
-                del self.bucket[i]
 
 class MyHashMap:
-
     def __init__(self):
         self.key_space = 2069
-        self.hash_table = [Bucket() for _ in range(self.key_space)]
+        self.hash_table = [ListNode() for _ in range(self.key_space)]
 
+    def hash_key(self, key: int) -> int:
+        return key % self.key_space
+    
     def put(self, key: int, value: int) -> None:
-        hash_key = key % self.key_space
-        self.hash_table[hash_key].update(key, value)
-
+        curr = self.hash_table[self.hash_key(key)]
+        while curr.next:
+            if curr.next.key == key:
+                curr.next.val = value
+                return
+            curr = curr.next
+        curr.next = ListNode(key, value)
+        
     def get(self, key: int) -> int:
-        hash_key = key % self.key_space
-        return self.hash_table[hash_key].get(key)
+        curr = self.hash_table[self.hash_key(key)].next
+        while curr:
+            if curr.key == key:
+                return curr.val
+            curr = curr.next
+        return -1
 
     def remove(self, key: int) -> None:
-        hash_key = key % self.key_space
-        self.hash_table[hash_key].remove(key)
-
+        curr = self.hash_table[self.hash_key(key)]
+        while curr and curr.next:
+            if curr.next.key == key:
+                curr.next = curr.next.next
+                return
+            curr = curr.next
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
