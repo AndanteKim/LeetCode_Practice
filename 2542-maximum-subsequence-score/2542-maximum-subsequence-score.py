31f880d1d34b38c984bcd0765558e8de4d99cdf5
Andrew Kim <ksb0724@utexas.edu>
2023-10-04 22:29:16 +0900

    Time: 866 ms (51.83%), Space: 38.6 MB (21.89%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.py
@@ -1,21 +1,26 @@
 class Solution:
     def maxScore(self, nums1: List[int], nums2: List[int], k: int) -> int:
-        pairs = [(n1, n2) for n1, n2 in zip(nums1, nums2)]
+        # sort pair (nums1[i], nums2[i]) in decresing order
+        n = len(nums1)
+        pairs = [(nums1[i], nums2[i]) for i in range(n)]
         pairs.sort(key = lambda x: -x[1])
         
+        # use a min-heap to maintain the top k elements
         top_k_heap = [x[0] for x in pairs[:k]]
         top_k_sum = sum(top_k_heap)
         heapify(top_k_heap)
         
-        answer = top_k_sum * pairs[k - 1][1]
+        # The score of the first k pairs
+        ans = top_k_sum * pairs[k - 1][1]
         
-        for i in range(k, len(nums1)):
-            top_k_sum -= heapq.heappop(top_k_heap)
+        # iterate over every nums2[i] as minimum from nums2
+        for i in range(k, n):
+            # remove the smallest integer from the previous top k elements
+            top_k_sum -= heappop(top_k_heap)
             top_k_sum += pairs[i][0]
             heappush(top_k_heap, pairs[i][0])
             
-            answer = max(answer, top_k_sum * pairs[i][1])
-        
-        return answer
+            # update answer
+            ans = max(ans, top_k_sum * pairs[i][1])
             
-        
+        return ans
