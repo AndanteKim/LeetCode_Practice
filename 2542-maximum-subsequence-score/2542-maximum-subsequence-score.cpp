4332add1dd3310fd72fbb0af8e36f27dca14e318
Andrew Kim <ksb0724@utexas.edu>
2023-10-04 22:51:53 +0900

    Time: 176 ms (77.63%), Space: 92.3 MB (40.62%) - LeetHub

--- 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -4,26 +4,29 @@ class Solution {
 public:
     long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
         vector<pair<int, int>> pairs;
-        for (int i = 0; i < nums1.size(); ++i) pairs.push_back({nums1[i], nums2[i]});
-        sort(pairs.begin(), pairs.end(), [](pair<int, int>& a, pair<int, int>& b){return a.second > b.second;});
+        int n = nums1.size();
+        for (int i = 0; i < n; ++i)
+            pairs.push_back({nums1[i], nums2[i]});
+        sort(pairs.begin(), pairs.end(), [](auto& a, auto& b) {return a.second > b.second;});
         
-        priority_queue<int, vector<int>, greater<int>> top_k_heap;
-        ll top_k_sum = 0;
-        for (int i = 0; i < k; ++i) {
-            top_k_sum += pairs[i].first;
-            top_k_heap.push(pairs[i].first);
-        }
-        ll answer = top_k_sum * pairs[k - 1].second, curr;
-        
-        for (int i = k; i < pairs.size(); ++i){
-            top_k_sum -= top_k_heap.top();
-            top_k_heap.pop();
-            top_k_sum += pairs[i].first;
-            top_k_heap.push(pairs[i].first);
-            curr = top_k_sum * pairs[i].second;
-            answer = max(answer, curr);
+        // set min heap to pop min value of nums1 between 0 ~ k - 1
+        priority_queue<int, vector<int>, greater<int>> topKHeap;
+        ll topKSum = 0, ans = 0;
+        for (int i = 0; i < k; ++i){
+            topKHeap.push(pairs[i].first);
+            topKSum += pairs[i].first;
         }
         
-        return answer;
+        ans = topKSum * pairs[k - 1].second;
+        
+        for (int i = k; i < n; ++i){
+            topKSum -= topKHeap.top();
+            topKHeap.pop();
+            topKSum += pairs[i].first;
+            topKHeap.push(pairs[i].first);
+            ans = max(ans, topKSum * pairs[i].second);
+        }
+        
+        return ans;
     }
 };
