8f8eaf8f079c68f51712218ffd48811440b9e906
Andrew Kim <ksb0724@utexas.edu>
2023-10-05 21:06:28 +0900

    Time: 2 ms (56.28%), Space: 6.3 MB (9.58%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.cpp
@@ -10,14 +10,18 @@
 class Solution {
 public:
     int guessNumber(int n) {
-        int left = 1, right = n;
         
+        int left = 1, right = n;
         while (left <= right){
-            int mid = left + (right - left) / 2;
-            if (!guess(mid)) return mid;
-            else if (guess(mid) == 1) left = mid + 1;
-            else right = mid - 1;
+            int mid = (left + ((right - left) >> 1));
+            if (guess(mid) == 1)
+                left = mid + 1;
+            else if (guess(mid) == -1)
+                right = mid - 1;
+            else
+                return mid;
         }
-        return left;
+        
+        return -1;
     }
 };
