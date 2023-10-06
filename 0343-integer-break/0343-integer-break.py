f5764368b4aadea2bce6cf4799bfe1ecbcd7cf6f
Andrew Kim <ksb0724@utexas.edu>
2023-10-06 17:07:57 +0900

    Time: 42 ms (44.79%), Space: 16.3 MB (27.37%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,19 +1,16 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         @lru_cache(maxsize = None)
-        def dp(remain: int, product: int) -> int:
-            if remain <= 1:
-                return product
+        def dp(num: int) -> int:
+            if num <= 3:
+                return num
             
-            mx = 1
-            for i in range(1, remain + 1):
-                mx = max(mx, dp(remain - i, product * i))
-                    
-            return mx
-        
-        if n == 2:
-            return 1
-        if n == 3:
-            return 2
-        
-        return dp(n, 1)
+            ans = num
+            for i in range(2, num):
+                ans = max(ans, i * dp(num - i))
+            
+            return ans
+                
+        if n <= 3:
+            return n - 1
+        return dp(n)
