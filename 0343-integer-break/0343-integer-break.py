4dc727ae8879ae4bc5bff577fe8e2be4a5458788
Andrew Kim <ksb0724@utexas.edu>
2023-10-06 22:41:05 +0900

    Time: 40 ms (59.64%), Space: 16.2 MB (85.57%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -3,15 +3,9 @@ class Solution:
         if n <= 3:
             return n - 1
         
-        dp = [0] * (n + 1)
+        ans = 1
+        while n > 4:
+            ans *= 3
+            n -= 3
         
-        for i in (1,2,3):
-            dp[i] = i
-            
-        for num in range(4, n + 1):
-            ans = num
-            for i in range(2, num):
-                ans = max(ans, i * dp[num - i])
-            dp[num] = ans
-            
-        return dp[n]
+        return ans * n
