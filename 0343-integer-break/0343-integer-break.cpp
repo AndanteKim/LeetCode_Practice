4f6f21260b2899ee2ed52f24ee1554a8a5b75a77
Andrew Kim <ksb0724@utexas.edu>
2023-10-06 21:32:33 +0900

    Time: 0 ms (100.00%), Space: 6.8 MB (6.83%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,23 +1,20 @@
 class Solution {
-private:
-    int dp(int num, vector<int>& memo){
-        if (num <= 3)
-            return num;
-        if (memo[num] != 0)
-            return memo[num];
-        
-        int ans = num;
-        for (int i = 2; i < num; ++i)
-            ans = max(ans, i * dp(num - i, memo));
-        return memo[num] = ans;
-    }
-    
 public:
     int integerBreak(int n) {
-        vector<int> memo(n + 1);
-        
         if (n <= 3)
             return n - 1;
-        return dp(n, memo);
+        
+        vector<int> dp(n + 1);
+        dp[1] = 1, dp[2] = 2, dp[3] = 3;
+        
+        for (int num = 4; num <= n; ++num){
+            int ans = num;
+            for (int i = 2; i < num; ++i){
+                ans = max(ans, i * dp[num - i]);
+            }
+            dp[num] = ans;
+        }
+        
+        return dp[n];
     }
 };
