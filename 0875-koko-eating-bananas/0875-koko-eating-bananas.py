765e2b2e7114700bc27154f390c6f2ae3cf091ce
Andrew Kim <ksb0724@utexas.edu>
2023-10-07 02:30:42 +0900

    Time: 301 ms (92.47%), Space: 17.8 MB (90.76%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,14 +1,12 @@
 class Solution:
     def is_possible(self, piles: List[int], k: int, h: int) -> bool:
-        if k == 0:
-            return False
         time = 0
         for pile in piles:
             time += ceil(pile / k)
         return time <= h
     
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        left, right = 0, 10 ** 9
+        left, right = 1, 10 ** 9
         
         while left < right:
             mid = (left + right) >> 1
