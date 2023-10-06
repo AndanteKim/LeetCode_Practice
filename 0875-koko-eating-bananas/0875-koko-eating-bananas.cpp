a5c05c611a67e4df5f20a229c8b730169a1eddf8
Andrew Kim <ksb0724@utexas.edu>
2023-10-07 02:33:27 +0900

    Time: 37 ms (56.41%), Space: 19.5 MB (11.70%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,16 +1,23 @@
-typedef long long ll;
-
 class Solution {
+private:
+    bool isPossible(vector<int>& piles, int k, int h){
+        int time = 0;
+        for (int pile : piles){
+            time += ceil((double)pile / k);
+        }
+        return time <= h;
+    }
+    
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int left = 1, right = *max_element(piles.begin(), piles.end());
+        int left = 1, right = 1'000'000'000;
         
-        while (left <= right){
-            int mid = left + (right - left) / 2;
-            ll time = 0;
-            for (double pile : piles) time += ceil(pile / mid);
-            if (time <= h) right = mid - 1;
-            else left = mid + 1;
+        while (left < right){
+            int mid = (left + ((right - left) >> 1));
+            if (isPossible(piles, mid, h))
+                right = mid;
+            else
+                left = mid + 1;
         }
         
         return left;
