1485ed9d230fddaec4ce1c69ca23dab4ec71eeb0
Andrew Kim <ksb0724@utexas.edu>
2023-10-06 22:47:54 +0900

    Time: 2 ms (49.96%), Space: 6.5 MB (29.03%) - LeetHub

--- 0343-integer-break/0343-integer-break.cpp
+++ 0343-integer-break/0343-integer-break.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        if (n <= 3) return n - 1;
-        
-        int ans = 1;
-        
-        while (n > 4){
-            ans *= 3;
-            n -= 3;
-        }
-        
-        return ans * n;
+        if (n <= 3)
+            return n - 1;
+        if (n % 3 == 0)
+            return pow(3, n / 3);
+        if (n % 3 == 1)
+            return pow(3, (n / 3) - 1) * 4;
+        return pow(3, n / 3) * 2;
     }
 };
