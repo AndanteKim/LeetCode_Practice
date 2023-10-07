2ad588831a12e7975d42dcd2477e06805cf74614
Andrew Kim <ksb0724@utexas.edu>
2023-10-07 23:29:10 +0900

    Time: 804 ms (64.60%), Space: 16.6 MB (92.92%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,19 +1,21 @@
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
-        dp = [[[0] * (k + 1) for _ in range(m + 1)] for __ in range(n + 1)]
+        dp = [[0] * (k + 1) for _ in range(m + 1)]
+        prev_dp = [[0] * (k + 1) for _ in range(m + 1)]
         MOD = 10 ** 9 + 7
         
-        for num in range(len(dp[0])):
-            dp[n][num][0] = 1
-        
+        for num in range(len(prev_dp)):
+            prev_dp[num][0] = 1
+            
         for i in range(n - 1, -1, -1):
+            dp = [[0] * (k + 1) for _ in range(m + 1)]
             for max_so_far in range(m, -1, -1):
                 for remain in range(k + 1):
-                    ans = (max_so_far * dp[i + 1][max_so_far][remain]) % MOD
-                    
+                    ans = (max_so_far * prev_dp[max_so_far][remain]) % MOD
                     if remain > 0:
                         for num in range(max_so_far + 1, m + 1):
-                            ans = (ans + dp[i + 1][num][remain - 1]) % MOD
-                    dp[i][max_so_far][remain] = ans
-        return dp[0][0][k]
+                            ans = (ans + prev_dp[num][remain - 1]) % MOD
+                    dp[max_so_far][remain] = ans
+            prev_dp = dp
         
+        return dp[0][k]
