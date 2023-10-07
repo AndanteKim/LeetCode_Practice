f2abb86ced14c5efab950357ea0806788607edfb
Andrew Kim <ksb0724@utexas.edu>
2023-10-08 00:31:07 +0900

    Time: 6 ms (92.17%), Space: 6.4 MB (95.85%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -3,32 +3,44 @@ typedef long long ll;
 class Solution {
 public:
     int numOfArrays(int n, int m, int k) {
-        ll dp[n + 1][m + 1][k + 1];
-        ll prefix[n + 1][m + 1][k + 1];
-        memset(dp, 0, sizeof(dp));
-        memset(prefix, 0, sizeof(prefix));
+        ll dp[m + 1][k + 1], prevDp[m + 1][k + 1], prefix[m + 1][k + 1], prevPrefix[m + 1][k + 1];
+        
         int MOD = 1e9 + 7;
+        memset(dp, 0, sizeof(dp));
+        memset(prevDp, 0, sizeof(prevDp));
+        memset(prefix, 0, sizeof(prefix));
+        memset(prevPrefix, 0, sizeof(prevPrefix));
         
         for (int num = 1; num <= m; ++num){
-            dp[1][num][1] = 1;
-            prefix[1][num][1] = prefix[1][num - 1][1] + 1;
+            dp[num][1] = 1;
         }
         
         for (int i = 1; i <= n; ++i){
+            if (i > 1)
+                memset(dp, 0, sizeof(dp));
+            memset(prefix, 0, sizeof(prefix));
+            
             for (int maxNum = 1; maxNum <= m; ++maxNum){
                 for (int cost = 1; cost <= k; ++cost){
-                    ll ans = (maxNum * dp[i - 1][maxNum][cost]) % MOD;
-                    ans = (ans + prefix[i - 1][maxNum - 1][cost - 1]) % MOD;
+                    ll ans = (maxNum * prevDp[maxNum][cost]) % MOD;
+                    ans = (ans + prevPrefix[maxNum -1][cost - 1]) % MOD;
+                    dp[maxNum][cost] += ans;
+                    dp[maxNum][cost] %= MOD;
                     
-                    dp[i][maxNum][cost] += ans;
-                    dp[i][maxNum][cost] %= MOD;
-                    
-                    prefix[i][maxNum][cost] = (prefix[i][maxNum - 1][cost] + dp[i][maxNum][cost]);
-                    prefix[i][maxNum][cost] %= MOD;
+                    prefix[maxNum][cost] = (prefix[maxNum - 1][cost] + dp[maxNum][cost]);
+                    prefix[maxNum][cost] %= MOD;
+                }
+            }
+            
+            // since unable reassigning long long[][] in C++, need to manually, override
+            for (int maxNum = 0; maxNum <= m; ++maxNum){
+                for (int cost = 0; cost <= k; ++cost){
+                    prevDp[maxNum][cost] = dp[maxNum][cost];
+                    prevPrefix[maxNum][cost] = prefix[maxNum][cost];
                 }
             }
         }
         
-        return prefix[n][m][k];
+        return prefix[m][k];
     }
 };
