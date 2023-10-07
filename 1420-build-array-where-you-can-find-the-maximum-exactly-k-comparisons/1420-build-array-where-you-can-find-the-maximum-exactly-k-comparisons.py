c2b0e7e7f5f7460e54e83787499a821308f7eb4c
Andrew Kim <ksb0724@utexas.edu>
2023-10-07 23:55:17 +0900

    Time: 194 ms (82.30%), Space: 24.2 MB (55.75%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -1,21 +1,22 @@
 class Solution:
     def numOfArrays(self, n: int, m: int, k: int) -> int:
-        dp = [[0] * (k + 1) for _ in range(m + 1)]
-        prev_dp = [[0] * (k + 1) for _ in range(m + 1)]
+        dp = [[[0] * (k + 1) for _ in range(m + 1)] for __ in range(n + 1)]
+        prefix = [[[0] * (k + 1) for _ in range(m + 1)] for __ in range(n + 1)]
         MOD = 10 ** 9 + 7
         
-        for num in range(len(prev_dp)):
-            prev_dp[num][0] = 1
-            
-        for i in range(n - 1, -1, -1):
-            dp = [[0] * (k + 1) for _ in range(m + 1)]
-            for max_so_far in range(m, -1, -1):
-                for remain in range(k + 1):
-                    ans = (max_so_far * prev_dp[max_so_far][remain]) % MOD
-                    if remain > 0:
-                        for num in range(max_so_far + 1, m + 1):
-                            ans = (ans + prev_dp[num][remain - 1]) % MOD
-                    dp[max_so_far][remain] = ans
-            prev_dp = dp
+        for num in range(1, m + 1):
+            dp[1][num][1] = 1
+            prefix[1][num][1] = prefix[1][num - 1][1] + 1
         
-        return dp[0][k]
+        for i in range(1, n + 1):
+            for max_num in range(1, m + 1):
+                for cost in range(1, k + 1):
+                    ans = (max_num * dp[i - 1][max_num][cost]) % MOD
+                    ans = (ans + prefix[i - 1][max_num - 1][cost - 1]) % MOD
+                    
+                    dp[i][max_num][cost] += ans
+                    dp[i][max_num][cost] %= MOD
+                    
+                    prefix[i][max_num][cost] = (prefix[i][max_num - 1][cost] + dp[i][max_num][cost]) % MOD
+        
+        return prefix[n][m][k]
