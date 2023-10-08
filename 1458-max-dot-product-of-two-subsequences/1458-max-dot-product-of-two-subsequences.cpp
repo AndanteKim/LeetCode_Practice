7fdf570da54523a8e5715ff335c78720185ff3bc
Andrew Kim <ksb0724@utexas.edu>
2023-10-08 16:30:09 +0900

    Time: 21 ms (82.52%), Space: 13.8 MB (21.08%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -1,23 +1,8 @@
 class Solution {
-private:
-    int m, n;
-    int dp(vector<vector<int>>& memo, int i, int j, vector<int>& nums1, vector<int>& nums2){
-        if (i == m || j == n)
-            return 0;
-        
-        if (memo[i][j] != 0)
-            return memo[i][j];
-        
-        int use = nums1[i] * nums2[j] + dp(memo, i + 1, j + 1, nums1, nums2);
-        memo[i][j] = max(use, max(dp(memo, i + 1, j, nums1, nums2), dp(memo, i, j + 1, nums1, nums2)));
-        
-        return memo[i][j];
-    }
-    
 public:
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        this -> m = nums1.size(), this -> n = nums2.size();
-        int max1 = INT_MIN, min1 = INT_MAX, max2 = INT_MIN, min2 = INT_MAX;
+        int max1 = INT_MIN, max2 = INT_MIN;
+        int min1 = INT_MAX, min2 = INT_MAX;
         
         for (int num : nums1){
             max1 = max(max1, num);
@@ -31,11 +16,19 @@ public:
         
         if (max1 < 0 && min2 > 0)
             return max1 * min2;
-        
         if (min1 > 0 && max2 < 0)
             return min1 * max2;
         
-        vector memo(m, vector<int>(n));
-        return dp(memo, 0, 0, nums1, nums2);
+        int m = nums1.size(), n = nums2.size();
+        vector dp(m + 1, vector<int>(n + 1));
+        
+        for (int i = m - 1; i >= 0; --i){
+            for (int j = n - 1; j >= 0; --j){
+                int use = nums1[i] * nums2[j] + dp[i + 1][j + 1];
+                dp[i][j] = max(use, max(dp[i + 1][j], dp[i][j + 1]));
+            }
+        }
+        
+        return dp[0][0];
     }
 };
