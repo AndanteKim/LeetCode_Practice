4111f2d15fcb424d557bd74cc172b1d84050f4a6
Andrew Kim <ksb0724@utexas.edu>
2023-10-08 16:50:29 +0900

    Time: 19 ms (84.32%), Space: 13.6 MB (22.88%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -16,19 +16,21 @@ public:
         
         if (max1 < 0 && min2 > 0)
             return max1 * min2;
+        
         if (min1 > 0 && max2 < 0)
             return min1 * max2;
         
-        int m = nums1.size(), n = nums2.size();
-        vector dp(m + 1, vector<int>(n + 1));
+        int m = nums2.size() + 1;
+        vector<int> dp(m), prevDp(m);
         
-        for (int i = m - 1; i >= 0; --i){
-            for (int j = n - 1; j >= 0; --j){
-                int use = nums1[i] * nums2[j] + dp[i + 1][j + 1];
-                dp[i][j] = max(use, max(dp[i + 1][j], dp[i][j + 1]));
+        for (int i = nums1.size() - 1; i >= 0; --i){
+            dp = vector<int>(m);
+            for (int j = nums2.size() - 1; j >= 0; --j){
+                int use = nums1[i] * nums2[j] + prevDp[j + 1];
+                dp[j] = max(use, max(prevDp[j], dp[j + 1]));
             }
+            prevDp = dp;
         }
-        
-        return dp[0][0];
+        return dp[0];
     }
 };
