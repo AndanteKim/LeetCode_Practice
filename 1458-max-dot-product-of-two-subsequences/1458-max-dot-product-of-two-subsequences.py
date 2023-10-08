76ae45b537fb1e114405d5833899cdd37501260e
Andrew Kim <ksb0724@utexas.edu>
2023-10-08 16:37:56 +0900

    Time: 171 ms (99.30%), Space: 16.2 MB (98.59%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.py
@@ -3,16 +3,18 @@ class Solution:
         max1, max2 = max(nums1), max(nums2)
         min1, min2 = min(nums1), min(nums2)
         
+        if max1 < 0 and min2 > 0:
+            return max1 * min2
         if min1 > 0 and max2 < 0:
             return min1 * max2
         
-        if max1 < 0 and min2 > 0:
-            return max1 * min2
+        m = len(nums2) + 1
+        dp, prev_dp = [0] * m, [0] * m
+        for i in range(len(nums1) - 1, -1, -1):
+            dp = [0] * m
+            for j in range(len(nums2) - 1, -1, -1):
+                use = nums1[i] * nums2[j] + prev_dp[j + 1]
+                dp[j] = max(use, prev_dp[j], dp[j + 1])
+            prev_dp = dp
         
-        m, n = len(nums1), len(nums2)
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
-        for i in range(m - 1, -1, -1):
-            for j in range(n - 1, -1, -1):
-                use = nums1[i] * nums2[j] + dp[i + 1][j + 1]
-                dp[i][j] = max(use, dp[i + 1][j] , dp[i][j + 1])
-        return dp[0][0]
+        return dp[0]
