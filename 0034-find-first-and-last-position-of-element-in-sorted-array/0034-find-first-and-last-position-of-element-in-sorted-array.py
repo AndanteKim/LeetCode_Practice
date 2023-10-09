4e8276e531e86a86c137a5d0d56c337e858baf82
Andrew Kim <ksb0724@utexas.edu>
2023-10-09 13:59:12 +0900

    Time: 96 ms (6.42%), Space: 17.7 MB (50.97%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,7 +1,34 @@
 class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if not nums:
-            return [-1, -1]
-        first, last = bisect_left(nums, target), bisect_right(nums, target)
+    def find_bound(self, nums: List[int], target:int, is_first: bool) -> int:
+        n = len(nums)
+        begin, end = 0, n - 1
         
-        return [-1, -1] if first >= len(nums) or nums[first] != target else [first, last - 1]
+        while begin <= end:
+            mid = ((begin + end) >> 1)
+            if nums[mid] == target:
+                if is_first:
+                    # if we find lower bound
+                    if mid == begin or nums[mid - 1] < target:
+                        return mid
+                    # search on the left side for the bound
+                    end = mid - 1
+                else:
+                    # if we find upper bound
+                    if mid == end or nums[mid + 1] > target:
+                        return mid
+                    
+                    # search on the right side for the bound
+                    begin = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                begin = mid + 1
+        return -1
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.find_bound(nums, target, True)
+        if left == -1:
+            return [-1, -1]
+        
+        right = self.find_bound(nums, target, False)
+        return [left, right]
