9b36ddf86525da2b9a0761630e652244ce7797f3
Andrew Kim <ksb0724@utexas.edu>
2023-10-10 16:30:28 +0900

    Time: 377 ms (19.04%), Space: 109.9 MB (7.53%) - LeetHub

---
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        // binary search
+        int n = nums.size(), ans = n;
+        set<int> unique(nums.begin(), nums.end());
+        vector<int> newNums;
+        for (int num : unique)
+            newNums.push_back(num);
+        
+        for (int i = 0; i < newNums.size(); ++i){
+            int left = newNums[i];
+            int right = left + n - 1;
+            int j = upper_bound(newNums.begin(), newNums.end(), right) - newNums.begin();
+            int count = j - i;
+            ans = min(ans, n - count);
+        }
+        
+        return ans;
+    }
+};
