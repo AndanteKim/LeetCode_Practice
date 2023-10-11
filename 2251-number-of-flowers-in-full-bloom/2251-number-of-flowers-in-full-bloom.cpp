2a043d21cb428c091508f2eb7d222e209af37dbd
Andrew Kim <ksb0724@utexas.edu>
2023-10-11 15:43:30 +0900

    Time: 302 ms (24.68%), Space: 88.7 MB (51.00%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     vector<int> fullBloomFlowers(vector<vector<int>>& flowers, vector<int>& people) {
-        map<int, int> m;
-        for (vector<int>& flower : flowers){
-            ++m[flower[0]];
-            --m[flower[1] + 1];
-        }
+        sort(flowers.begin(), flowers.end());
+        vector<int> sortedPeople(people.begin(), people.end());
+        sort(sortedPeople.begin(), sortedPeople.end());
+        vector<int> ans(people.size());
+        priority_queue<int, vector<int>, greater<int>> pq;
+        unordered_map<int, int> m;
         
-        int sum = 0;
-        for (auto& [key, value] : m){
-            sum += value;
-            value = sum;
-        }
-        
-        int n = people.size();
-        vector<int> ans(n, 0);
-        
-        for (int i = 0; i < n; ++i){
-            auto it = m.upper_bound(people[i]);
-            if (it != m.begin()){
-                --it;
-                ans[i] = it -> second;
+        int i = 0;
+        for (int t : sortedPeople){
+            
+            while (i < flowers.size() && flowers[i][0] <= t){
+                pq.push(flowers[i][1]);
+                ++i;
             }
+            
+            while (!pq.empty() && pq.top() < t)
+                pq.pop();
+            
+            m[t] = pq.size();
         }
+        
+        for (int i = 0; i < people.size(); ++i)
+            ans[i] = m[people[i]];
         
         return ans;
     }
