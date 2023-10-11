0ef88c85dbce011c87050b8118b4a024bd3da387
Andrew Kim <ksb0724@utexas.edu>
2023-10-12 02:48:50 +0900

    Time: 4 ms (22.76%), Space: 8 MB (38.51%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
-        if (n <= 1) return nums[0];
+        if (n <= 1)
+            return nums[0];
+        int robNextMinusOne = 0, robNext = nums[0];
         
-        vector<int> dp(n + 1);
-        dp[1] = nums[0];
-        for (int i = 2; i <= n; ++i){
-            dp[i] = max(dp[i - 2] + nums[i - 1], dp[i - 1]);
+        for (int i = 1; i < n; ++i){
+            int curr = max(robNext, robNextMinusOne + nums[i]);
+            robNextMinusOne = robNext;
+            robNext = curr;
         }
         
-        return dp.back();
+        return robNext;
     }
 };
