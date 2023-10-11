e1aa8f497f887cbd5dd2d275db880db2e97d6a7c
Andrew Kim <ksb0724@utexas.edu>
2023-10-11 18:51:51 +0900

    Time: 2 ms (49.73%), Space: 6.5 MB (16.49%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int t_0 = 0, t_1 = 1;
-        if (n == 0) return t_0;
-        if (n == 1) return t_1;
-        if (n == 2) return t_0 + t_1;
-        int t_2 = t_0 + t_1;
-        int t_3, ans;
-        for (int i = 3; i <= n; ++i){
-            t_3 = t_0 + t_1 + t_2;
-            ans = t_3;
-            t_0 = t_1;
-            t_1 = t_2;
-            t_2 = t_3;
+        if (n <= 2)
+            return n == 2? 1 : n == 1? 1 : 0;
+        
+        int t0 = 0, t1 = 1, t2 = 1, t3;
+        for (int i = 0; i <= n - 3; ++i){
+            t3 = t0 + t1 + t2;
+            t0 = t1;
+            t1 = t2;
+            t2 = t3;
         }
-        return ans;
+        
+        return t3;
     }
 };
