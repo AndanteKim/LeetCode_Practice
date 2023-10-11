2487a7072962314e609102aeb49806b7a00fb5f1
Andrew Kim <ksb0724@utexas.edu>
2023-10-11 17:14:27 +0900

    Time: 883 ms (84.55%), Space: 45.1 MB (26.06%) - LeetHub

--- 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
+++ 2251-number-of-flowers-in-full-bloom/2251-number-of-flowers-in-full-bloom.py
@@ -1,22 +1,18 @@
-from sortedcontainers import SortedDict
-
 class Solution:
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        difference = SortedDict({0: 0})
-        for start, end in flowers:
-            difference[start] = difference.get(start, 0) + 1
-            difference[end + 1] = difference.get(end + 1, 0) - 1
-            
-        positions, prefix, curr = [], [], 0
+        starts, ends = [], []
         
-        for key, val in difference.items():
-            positions.append(key)
-            curr += val
-            prefix.append(curr)
-            
+        for start, end in flowers:
+            starts.append(start)
+            ends.append(end + 1)
+        
+        starts.sort()
+        ends.sort()
         ans = []
+        
         for person in people:
-            i = bisect_right(positions, person) - 1
-            ans.append(prefix[i])
+            i = bisect_right(starts, person)
+            j = bisect_right(ends, person)
+            ans.append(i - j)
         
         return ans
