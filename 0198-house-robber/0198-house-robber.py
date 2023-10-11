56528dd96cb783aedcc56a6749b1fe0b3f566768
Andrew Kim <ksb0724@utexas.edu>
2023-10-12 02:36:34 +0900

    Time: 40 ms (49.50%), Space: 16.1 MB (97.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        @lru_cache(maxsize = None)
-        def dp(i: int) -> int:
-            if i >= n:
-                return 0
-            
-            return max(nums[i] + dp(i + 2), dp(i + 1))
-            
-        
         n = len(nums)
-        return dp(0)
+        if n <= 1:
+            return nums[0]
         
+        dp = [0] * (n + 1)
+        dp[1] = nums[0]
+        
+        for i in range(2, n + 1):
+            dp[i] = max(dp[i - 2] + nums[i - 1], dp[i - 1])
+        
+        return dp[-1]
