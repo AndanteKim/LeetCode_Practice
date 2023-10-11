d2f90f903f5c2388f2cbc790d54a155aaa82bbc3
Andrew Kim <ksb0724@utexas.edu>
2023-10-12 00:17:30 +0900

    Time: 3 ms (84.99%), Space: 14.1 MB (15.35%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,14 +1,19 @@
 class Solution {
+private:
+    int dp(int i, vector<int>& cost, vector<int>& memo){
+        if (i <= 1)
+            return 0;
+        if (memo[i] != -1) return memo[i];
+        
+        int oneStep = cost[i - 1] + dp(i - 1, cost, memo);
+        int twoSteps = cost[i - 2] + dp(i - 2, cost, memo);
+        return memo[i] = min(oneStep, twoSteps);
+    }
+    
 public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> minCost(n + 1);
-        for (int i = 2; i <= n; ++i){
-            int oneStep = minCost[i - 1] + cost[i - 1];
-            int twoSteps = minCost[i - 2] + cost[i - 2];
-            minCost[i] = min(oneStep, twoSteps);
-        }
-        
-        return minCost.back();
+        vector<int> memo(n + 1, -1);
+        return dp(n, cost, memo);
     }
 };
