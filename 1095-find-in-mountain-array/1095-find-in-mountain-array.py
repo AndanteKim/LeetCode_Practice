5fdbe0eff896ef0de2dea7bdb239def1e03be842
Andrew Kim <ksb0724@utexas.edu>
2023-10-13 01:43:02 +0900

    Time: 41 ms (41.71%), Space: 17.2 MB (60.00%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -9,43 +9,69 @@
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
         length = mountain_arr.length()
+        cache = dict()
         
         # find the index of the peak element
         low, high = 1, length - 2
-        while low < high:
+        while low != high:
             mid = (low + high) >> 1
-            if mountain_arr.get(mid) < mountain_arr.get(mid + 1):
+            if mid in cache:
+                curr = cache[mid]
+            else:
+                curr = mountain_arr.get(mid)
+                cache[mid] = curr
+            
+            if mid + 1 in cache:
+                nxt = cache[mid + 1]
+            else:
+                nxt = mountain_arr.get(mid + 1)
+                cache[mid + 1] = nxt
+            
+            if curr < nxt:
+                if curr == target:
+                    return mid
+                if nxt == target:
+                    return mid + 1
                 low = mid + 1
             else:
                 high = mid
+        
         peak_index = low
-        
         # search in the strictly increasing part of the array
+        # If found, will be returned in the loop itself
         low, high = 0, peak_index
-        while low < high:
+        while low <= high:
             mid = (low + high) >> 1
-            if mountain_arr.get(mid) < target:
+            if mid in cache:
+                curr = cache[mid]
+            else:
+                curr = mountain_arr.get(mid)
+            
+            if curr == target:
+                return mid
+            elif curr < target:
                 low = mid + 1
             else:
-                high = mid
-        
-        # check if the target is present in the strictly increasing part
-        if mountain_arr.get(low) == target:
-            return low
-        
-        # otherwise, search in the strictly decreasing part
+                high = mid - 1
+            
+        # search in the strictly decreasing part of the array
+        # If found, will be returned in the loop itself
         low, high = peak_index + 1, length - 1
-        while low < high:
+        
+        while low <= high:
             mid = (low + high) >> 1
-            if mountain_arr.get(mid) > target:
+            if mid in cache:
+                curr = cache[mid]
+            else:
+                curr = mountain_arr.get(mid)
+            
+            if curr == target:
+                return mid
+            elif curr > target:
                 low = mid + 1
             else:
-                high = mid
+                high = mid - 1
         
-        # check if the target is present in the strictly decreasing part
-        if mountain_arr.get(low) == target:
-            return low
-        
-        # no target in the mountain array
+        # Target is not present in the mountain array
         return -1
-        
+            
