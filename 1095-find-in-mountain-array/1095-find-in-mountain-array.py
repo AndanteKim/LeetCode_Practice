e67b35da095fdde9a0a833c1d7efe0200364be89
Andrew Kim <ksb0724@utexas.edu>
2023-10-13 01:59:59 +0900

    Time: 39 ms (50.85%), Space: 17 MB (98.91%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -9,23 +9,12 @@
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
         length = mountain_arr.length()
-        cache = dict()
         
-        # find the index of the peak element
         low, high = 1, length - 2
         while low != high:
             mid = (low + high) >> 1
-            if mid in cache:
-                curr = cache[mid]
-            else:
-                curr = mountain_arr.get(mid)
-                cache[mid] = curr
-            
-            if mid + 1 in cache:
-                nxt = cache[mid + 1]
-            else:
-                nxt = mountain_arr.get(mid + 1)
-                cache[mid + 1] = nxt
+            curr = mountain_arr.get(mid)
+            nxt = mountain_arr.get(mid + 1)
             
             if curr < nxt:
                 if curr == target:
@@ -37,33 +26,23 @@ class Solution:
                 high = mid
         
         peak_index = low
-        # search in the strictly increasing part of the array
-        # If found, will be returned in the loop itself
+        
         low, high = 0, peak_index
         while low <= high:
             mid = (low + high) >> 1
-            if mid in cache:
-                curr = cache[mid]
-            else:
-                curr = mountain_arr.get(mid)
+            curr = mountain_arr.get(mid)
             
             if curr == target:
                 return mid
-            elif curr < target:
+            if curr < target:
                 low = mid + 1
             else:
                 high = mid - 1
-            
-        # search in the strictly decreasing part of the array
-        # If found, will be returned in the loop itself
-        low, high = peak_index + 1, length - 1
         
+        low, high = peak_index + 1, length - 1
         while low <= high:
             mid = (low + high) >> 1
-            if mid in cache:
-                curr = cache[mid]
-            else:
-                curr = mountain_arr.get(mid)
+            curr = mountain_arr.get(mid)
             
             if curr == target:
                 return mid
@@ -72,6 +51,4 @@ class Solution:
             else:
                 high = mid - 1
         
-        # Target is not present in the mountain array
         return -1
-            
