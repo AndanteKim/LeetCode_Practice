9aaee8c9cf95786aca6a1992d6a8703205f87697
Andrew Kim <ksb0724@utexas.edu>
2023-10-15 01:25:31 +0900

    Time: 38 ms (80.74%), Space: 16.2 MB (94.01%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,17 +1,15 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        mod = 1_000_000_007
+        MOD = 1_000_000_007
         if n <= 2:
             return n
-        
-        # f[k] := number of ways "fully cover a board" of width k
-        # p[k] := number of ways "partially cover a board" of width k
-        f, p = [0] * (n + 1), [0] * (n + 1)
-        f[1], f[2], p[2] = 1, 2, 1
+        f_prev, f_curr, p_curr = 1, 2, 1
         
         for k in range(3, n + 1):
-            f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % mod
-            p[k] = (p[k - 1] + f[k - 2]) % mod
+            tmp = f_curr
+            f_curr = (f_prev + f_curr + 2 * p_curr) % MOD
+            p_curr = (p_curr + f_prev) % MOD
+            f_prev = tmp
         
-        return f[n]
+        return f_curr
         
