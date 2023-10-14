f2c310ab0f7dced00b6282d8c2271e965781d727
Andrew Kim <ksb0724@utexas.edu>
2023-10-15 01:15:06 +0900

    Time: 0 ms (100.00%), Space: 7.1 MB (14.58%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,32 +1,22 @@
-typedef long l;
 class Solution {
-private:
-    int MOD = 1'000'000'007;
-    unordered_map<int, l> f_cache, p_cache;
-    l p(int k){
-        if (p_cache.find(k) != p_cache.end())
-            return p_cache[k];
-        l val;
-        if (k == 2)
-            val = 1L;
-        else
-            val = (p(k - 1) + f(k - 2)) % MOD;
-        return p_cache[k] = val;
-    }
-    
-    l f(int k){
-        if (f_cache.find(k) != f_cache.end())
-            return f_cache[k];
-        l val;
-        if (k <= 2)
-            val = k;
-        else
-            val = (f(k - 1) + f(k - 2) + 2 * p(k - 1)) % MOD;
-        return f_cache[k] = val;
-    }
-    
 public:
     int numTilings(int n) {
-        return static_cast<int>(f(n));
+        int MOD = 1'000'000'007;
+        
+        if (n <= 2)
+            return n;
+        
+        // f[k] := number of ways "fully cover a board" of width k
+        // p[k] := number of ways "partially cover a board" of width k
+        vector<long> f(n + 1), p(n + 1);
+        
+        // initialize f and p with results for the base case scenarios
+        f[1] = 1L, f[2] = 2L, p[2] = 1L;
+        for (int k = 3; k <= n; ++k){
+            f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % MOD;
+            p[k] = (p[k - 1] + f[k - 2]) % MOD;
+        }
+        
+        return static_cast<int>(f[n]);
     }
 };
