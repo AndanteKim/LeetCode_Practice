cc726b1dab2762460a22039400ff28630f61cca4
Andrew Kim <ksb0724@utexas.edu>
2023-10-15 01:32:22 +0900

    Time: 2 ms (54.46%), Space: 6.3 MB (72.59%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int numTilings(int n) {
-        int MOD = 1'000'000'007;
-        
-        if (n <= 2)
-            return n;
-        
-        // f[k] := number of ways "fully cover a board" of width k
-        // p[k] := number of ways "partially cover a board" of width k
-        vector<long> f(n + 1), p(n + 1);
-        
-        // initialize f and p with results for the base case scenarios
-        f[1] = 1L, f[2] = 2L, p[2] = 1L;
+        int mod = 1'000'000'007;
+        if (n <= 2) return n;
+        long fPrev = 1L, fCurr = 2L, pCurr = 1L;
         for (int k = 3; k <= n; ++k){
-            f[k] = (f[k - 1] + f[k - 2] + 2 * p[k - 1]) % MOD;
-            p[k] = (p[k - 1] + f[k - 2]) % MOD;
+            int tmp = fCurr;
+            fCurr = (fPrev + fCurr + 2 * pCurr) % mod;
+            pCurr = (pCurr + fPrev) % mod;
+            fPrev = tmp;
         }
         
-        return static_cast<int>(f[n]);
+        return fCurr;
     }
 };
