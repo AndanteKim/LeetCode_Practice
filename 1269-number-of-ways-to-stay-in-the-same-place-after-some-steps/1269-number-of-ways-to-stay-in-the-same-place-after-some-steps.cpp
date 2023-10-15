9783114718d3128fde62a43c1f003c6acfd1ba4c
Andrew Kim <ksb0724@utexas.edu>
2023-10-15 16:27:02 +0900

    Time: 14 ms (75.32%), Space: 13.6 MB (42.21%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int numWays(int steps, int arrLen) {
-        int mod = 1e9 + 7;
-        arrLen = min(steps, arrLen);
-        vector dp(arrLen, vector<int>(steps + 1));
-        dp[0][0] = 1;
+        int mod = 1'000'000'007;
+        arrLen = min(arrLen, steps);
+        vector<int> dp(steps + 1), prevDp(steps + 1);
+        prevDp[0] = 1;
         
         for (int remain = 1; remain <= steps; ++remain){
+            dp = vector<int>(steps + 1);
             for (int curr = arrLen - 1; curr >= 0; --curr){
-                int ans = dp[curr][remain - 1];
-                
+                int ans = prevDp[curr];
                 if (curr > 0)
-                    ans = (ans + dp[curr - 1][remain - 1]) % mod;
+                    ans = (ans + prevDp[curr - 1]) % mod;
                 if (curr < arrLen - 1)
-                    ans = (ans + dp[curr + 1][remain - 1]) % mod;
-                dp[curr][remain] = ans;
+                    ans = (ans + prevDp[curr + 1]) % mod;
+                dp[curr] = ans;
             }
+            prevDp = dp;
         }
         
-        return dp[0][steps];
+        return dp[0];
     }
 };
