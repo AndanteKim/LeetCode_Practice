e09bd6c0df437a05b1aec9f22ae6442ae495ec1d
Andrew Kim <ksb0724@utexas.edu>
2023-10-15 15:51:12 +0900

    Time: 329 ms (65.00%), Space: 21.9 MB (70.56%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -1,19 +1,19 @@
 class Solution:
     def numWays(self, steps: int, arrLen: int) -> int:
-        @lru_cache(maxsize = None)
-        def dp(curr: int, remain: int) -> int:
-            nonlocal arrLen
-            if remain == 0:
-                if curr == 0:
-                    return 1
-                return 0
-            
-            ans = dp(curr, remain - 1)
-            if curr > 0:
-                ans = (ans + dp(curr - 1, remain - 1)) % mod
-            if curr < arrLen - 1:
-                ans = (ans + dp(curr + 1, remain - 1)) % mod
-            return ans
-        
         mod = 10 ** 9 + 7
-        return dp(0, steps)
+        arrLen = min(arrLen, steps)
+        dp = [[0] * (steps + 1) for _ in range(arrLen)]
+        dp[0][0] = 1
+        
+        for remain in range(1, steps + 1):
+            for curr in range(arrLen - 1, -1, -1):
+                ans = dp[curr][remain - 1]
+                
+                if curr > 0:
+                    ans = (ans + dp[curr - 1][remain - 1]) % mod
+                if curr < arrLen - 1:
+                    ans = (ans + dp[curr + 1][remain - 1]) % mod
+                
+                dp[curr][remain] = ans
+        
+        return dp[0][steps]
