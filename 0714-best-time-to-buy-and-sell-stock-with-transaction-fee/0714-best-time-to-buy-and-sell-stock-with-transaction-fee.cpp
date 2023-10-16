5d599305a49584be79ea512c79d17a2edf8b45cd
Andrew Kim <ksb0724@utexas.edu>
2023-10-17 01:56:22 +0900

    Time: 84 ms (76.27%), Space: 60.2 MB (62.65%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,26 +1,16 @@
 class Solution {
-private:
-    int n, fee;
-    int dp(int i, int j, vector<int>& prices, vector<vector<int>>& memo){
-        if (i >= n)
-            return 0;
-        
-        if (memo[i][j] != -1)
-           return memo[i][j];
-        
-        int profit = 0;
-        if (j == 0)
-            profit = max(-prices[i] + dp(i + 1, 1, prices, memo), dp(i + 1, 0, prices, memo));
-        else
-            profit = max(prices[i] - fee + dp(i + 1, 0, prices, memo), dp(i + 1, 1, prices, memo));
-            
-        return memo[i][j] = profit;
-    }
-    
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        this -> n = prices.size(), this -> fee = fee;
-        vector memo(n, vector<int>(2, -1));
-        return dp(0, 0, prices, memo);
+        int n = prices.size();
+        vector<int> free(n), hold(n);
+        // In order to hold a stock on day 0, we have no other choice but to buy it for prices[0]
+        hold[0] = -prices[0];
+        
+        for (int i = 1; i < n; ++i){
+            hold[i] = max(hold[i - 1], free[i - 1] - prices[i]);
+            free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee);
+        }
+        
+        return free.back();
     }
 };
