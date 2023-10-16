0f2ff2e5c73374bd995c443d255d60beebc3755d
Andrew Kim <ksb0724@utexas.edu>
2023-10-16 18:10:15 +0900

    Time: 0 ms (100.00%), Space: 6.7 MB (51.35%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.cpp
@@ -1,28 +1,13 @@
 class Solution {
-private:
-    unordered_map<size_t, int> cache;
-    // use a better hashing function like 'boost::hash_combine' in the real world
-    int key(int i, int j) const {
-        size_t hash_i = hash<int>{}(i), hash_j = hash<int>{}(j);
-        int hashed = (int) (hash_i ^ (hash_i >> 32));
-        return (hashed << 5) - 1 + (int)(hash_j ^ (hash_j >> 32));
-    }
-    
-    int getNum(int row, int col){
-        auto rowCol = key(row, col);
-        if (cache.count(rowCol) > 0) return cache[rowCol];
-        
-        if (row == 0 || col == 0 || row == col)
-            return (cache[rowCol] = 1);
-        
-        return (cache[rowCol] = getNum(row - 1, col - 1) + getNum(row - 1, col));
-    }
-    
 public:
     vector<int> getRow(int rowIndex) {
-        vector<int> ans;
-        for (int i = 0; i <= rowIndex; ++i)
-            ans.push_back(getNum(rowIndex, i));
-        return ans;
+        vector<int> curr, prev = {1};
+        for (int i = 1; i <= rowIndex; ++i){
+            curr.assign(i + 1, 1);
+            for (int j = 1; j < i; ++j)
+                curr[j] = prev[j - 1] + prev[j];
+            prev = move(curr);
+        }
+        return prev;
     }
 };
