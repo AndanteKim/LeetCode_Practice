fde9a077d218bd8a29e77dbc4d7ab7ed38b00b59
Andrew Kim <ksb0724@utexas.edu>
2023-10-17 01:59:40 +0900

    Time: 86 ms (73.43%), Space: 55.3 MB (80.53%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
-        int n = prices.size();
-        vector<int> free(n), hold(n);
-        // In order to hold a stock on day 0, we have no other choice but to buy it for prices[0]
-        hold[0] = -prices[0];
+        int n = prices.size(), free = 0, hold = -prices[0];
         
         for (int i = 1; i < n; ++i){
-            hold[i] = max(hold[i - 1], free[i - 1] - prices[i]);
-            free[i] = max(free[i - 1], hold[i - 1] + prices[i] - fee);
+            hold = max(hold, free - prices[i]);
+            free = max(free, hold + prices[i] - fee);
         }
         
-        return free.back();
+        return free;
     }
 };
