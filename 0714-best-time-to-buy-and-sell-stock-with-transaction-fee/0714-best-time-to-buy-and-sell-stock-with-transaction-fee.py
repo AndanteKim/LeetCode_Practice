e4927b756c020b9a6267345a969a020b369768ee
Andrew Kim <ksb0724@utexas.edu>
2023-10-16 23:20:20 +0900

    Time: 1036 ms (19.83%), Space: 260.2 MB (5.11%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,11 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
+        @lru_cache(maxsize = None)
+        def dp(i: int, hold: int) -> int:
+            if i >= n:
+                return 0
+            
+            profit = 0
+            if not hold:
+                profit = max(-prices[i] + dp(i + 1, True), dp(i + 1, hold))
+            else:
+                profit = max(prices[i] - fee + dp(i + 1, False), dp(i + 1, hold))
+                
+            return profit
+        
         n = len(prices)
-        hold, free = -prices[0], 0
-        
-        for i in range(1, n):
-            tmp = hold
-            hold = max(hold, free - prices[i])
-            free = max(free, tmp + prices[i] - fee)
-        
-        return free
+        # -1 := stands for no buying
+        return dp(0, False)
