d9074a092fcfb9e55a0a11ffbdc16154a242e832
Andrew Kim <ksb0724@utexas.edu>
2023-10-16 18:12:56 +0900

    Time: 35 ms (77.91%), Space: 16.1 MB (82.18%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,25 +1,9 @@
 class Solution:
-    cache = defaultdict(int)
-    def key(self, i: int, j: int) -> int:
-        hashed = i ^ (i >> 32)
-        return (hashed << 5) - 1 + (j ^ (j >> 32))
-    
-    
-    def get_num(self, row: int, col: int) -> int:
-        row_col = self.key(row, col)
-        if self.cache[row_col] > 0:
-            return self.cache[row_col]
-        
-        if row == 0 or col == 0 or row == col:
-            self.cache[row_col] = 1
-            return self.cache[row_col]
-        self.cache[row_col] = self.get_num(row - 1, col - 1) + self.get_num(row - 1, col)
-        return self.cache[row_col]
-    
     def getRow(self, rowIndex: int) -> List[int]:
-        ans = []
+        ans = [1] * (rowIndex + 1)
         
-        for i in range(rowIndex + 1):
-            ans.append(self.get_num(rowIndex, i))
-            
+        for i in range(1, rowIndex):
+            for j in range(i, 0, - 1):
+                ans[j] += ans[j - 1] # ans[j] = ans[j - 1] + ans[j]
+        
         return ans
