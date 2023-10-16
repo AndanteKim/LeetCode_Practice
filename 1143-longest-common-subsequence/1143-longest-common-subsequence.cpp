dd482f1540eb9605805be399c4bf78fe83f5a805
Andrew Kim <ksb0724@utexas.edu>
2023-10-16 21:20:34 +0900

    Time: 32 ms (82.30%), Space: 6.9 MB (90.42%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
+        // Space optimized bottom-up dynamic programming
+        // Time complexity: O(n^2), Space Complexity: O(n)
         int n1 = text1.size(), n2 = text2.size();
-        vector dp(n1 + 1, vector<int>(n2 + 1));
+        vector<int> prevDp(n2 + 1), dp(n2 + 1);
         
         for (int i = n1 - 1; i >= 0; --i){
             for (int j = n2 - 1; j >= 0; --j){
                 if (text1[i] == text2[j])
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                    dp[j] = 1 + prevDp[j + 1];
                 else
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j + 1]);
+                    dp[j] = max(prevDp[j], dp[j + 1]);
             }
+            prevDp = dp;
         }
         
-        return dp[0][0];
+        return dp[0];
     }
 };
