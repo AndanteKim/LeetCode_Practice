b0fe3e0ec04191c1b9c60ac21c9c8371b44e7f5d
Andrew Kim <ksb0724@utexas.edu>
2023-10-18 01:49:06 +0900

    Time: 118 ms (68.11%), Space: 20.1 MB (34.89%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,20 +1,23 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        @lru_cache(maxsize = None)
-        def dp(i: int, j: int) -> int:
-            if i == 0:
-                return j
-            if j == 0:
-                return i
-            
-            operations = 0
-            if word1[i - 1] == word2[j - 1]:
-                operations = dp(i - 1, j - 1)
-            else:
-                operations = 1 + min(dp(i - 1, j), dp(i, j - 1), dp(i - 1, j - 1))
-            
-            return operations
+        m, n = len(word1), len(word2)
+        if m == 0:
+            return n
+        if n == 0:
+            return m
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
         
+        for i in range(1, n + 1):
+            dp[0][i] = i
+        for i in range(1, m + 1):
+            dp[i][0] = i
+            
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]) + 1
         
-        n1, n2 = len(word1), len(word2)
-        return dp(n1, n2)
+        return dp[m][n]
+            
