1f3d39a9568292296a3f4ab62289ebf69ddb08b1
Andrew Kim <ksb0724@utexas.edu>
2023-10-17 21:01:34 +0900

    Time: 551 ms (90.19%), Space: 23.6 MB (86.83%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,9 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        n, free, hold = len(prices), 0, -prices[0]
+        # readable space optimized dynamic programming
         
-        for i in range(1, n):
-            hold = max(hold, free - prices[i]);
-            free = max(free, hold + prices[i] - fee);
-            
-        return free
+        n = len(prices)
+        # max_profit if I
+        had_stock_yesterday = -prices[0]
+        # max_profit if I
+        didnt_have_stock_yesterday = 0
+        
+        for price in prices:
+            have_stock_today = max(had_stock_yesterday, didnt_have_stock_yesterday - price)
+            dont_have_stock_today = max(didnt_have_stock_yesterday, had_stock_yesterday + price - fee)
+            had_stock_yesterday = have_stock_today
+            didnt_have_stock_yesterday = dont_have_stock_today
+        
+        return dont_have_stock_today
