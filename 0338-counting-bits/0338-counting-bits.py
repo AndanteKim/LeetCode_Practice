f19054be604f3673875fe8b46e1665f7af83b69b
Andrew Kim <ksb0724@utexas.edu>
2023-10-18 15:48:17 +0900

    Time: 78 ms (45.77%), Space: 23.1 MB (54.54%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -3,6 +3,6 @@ class Solution:
         ans = [0] * (n + 1)
         
         for x in range(1, n + 1):
-            ans[x] = ans[x >> 1] + (x & 1)
+            ans[x] = ans[x & (x - 1)] + 1
         
         return ans
