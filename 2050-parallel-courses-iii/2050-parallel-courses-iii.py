31442a264cdd919158a01229eb2b900f5c4ac41c
Andrew Kim <ksb0724@utexas.edu>
2023-10-18 14:25:39 +0900

    Time: 1451 ms (53.24%), Space: 95.8 MB (9.71%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -1,28 +1,23 @@
 class Solution:
-    
     def minimumTime(self, n: int, relations: List[List[int]], time: List[int]) -> int:
-        graph = defaultdict(list)
-        indegree = [0] * n
+        @lru_cache(maxsize = None)
+        def dfs(node: int):
+            if not graph[node]:
+                return time[node]
+            
+            ans = 0
+            for neighbor in graph[node]:
+                ans = max(ans, dfs(neighbor))
+            
+            return time[node] + ans
         
-        # Kahn's algorithm
+        
+        graph = defaultdict(list)
         for (x, y) in relations:
             graph[x - 1].append(y - 1)
-            indegree[y - 1] += 1
         
-        queue = deque()
-        max_time = [0] * n
-        
+        ans = 0
         for node in range(n):
-            if indegree[node] == 0:
-                queue.append(node)
-                max_time[node] = time[node]
+            ans = max(ans, dfs(node))
         
-        while queue:
-            node = queue.popleft()
-            for neighbor in graph[node]:
-                max_time[neighbor] = max(max_time[neighbor], max_time[node] + time[neighbor])
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-        
-        return max(max_time)
+        return ans
