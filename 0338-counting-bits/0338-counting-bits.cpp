a1a3f72204b516caf242616563501542109269dd
Andrew Kim <ksb0724@utexas.edu>
2023-10-18 15:40:45 +0900

    Time: 0 ms (100.00%), Space: 8.1 MB (51.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,8 +2,17 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> ans(n + 1);
-        for (int x = 1; x <= n; ++x)
-            ans[x] = ans[x & (x - 1)] + 1;
+        int x = 0, b = 1;
+        
+        while (b <= n){
+            while (x < b and x + b <= n){
+                ans[x + b] = ans[x] + 1;
+                ++x;
+            }
+            x = 0;
+            b <<= 1;
+        }
+        
         return ans;
     }
 };
