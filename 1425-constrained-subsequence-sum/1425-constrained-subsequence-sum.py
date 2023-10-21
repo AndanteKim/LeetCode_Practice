8d955b53fc515e5f770cd5ccd15f5815ae2f1b02
Andrew Kim <ksb0724@utexas.edu>
2023-10-21 22:04:04 +0900

    Time: 2271 ms (8.19%), Space: 31.2 MB (31.03%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
@@ -1,14 +1,14 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-        heap = [(-nums[0], 0)]
-        ans = nums[0]
+        window = SortedList([0])
+        dp = [0] * len(nums)
         
-        for i in range(1, len(nums)):
-            while i - heap[0][1] > k:
-                heappop(heap)
-            
-            curr = max(0, -heap[0][0]) + nums[i]
-            ans = max(ans, curr)
-            heappush(heap, (-curr, i))
+        for i in range(len(nums)):
+            dp[i] = nums[i] + window[-1]
+            window.add(dp[i])
+            if i >= k:
+                window.remove(dp[i - k])
         
-        return ans
+        return max(dp)
