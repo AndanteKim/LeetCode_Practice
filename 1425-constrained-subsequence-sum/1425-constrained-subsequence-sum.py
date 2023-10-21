c89601c0faaf4c87a17c0249c7f02d31d0d41ae3
Andrew Kim <ksb0724@utexas.edu>
2023-10-21 22:28:38 +0900

    Time: 1317 ms (76.72%), Space: 29.9 MB (98.71%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
@@ -1,14 +1,17 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-        window = SortedList([0])
+        queue = deque()
         dp = [0] * len(nums)
         
         for i in range(len(nums)):
-            dp[i] = nums[i] + window[-1]
-            window.add(dp[i])
-            if i >= k:
-                window.remove(dp[i - k])
-        
+            if queue and i - queue[0] > k:
+                queue.popleft()
+            
+            dp[i] = (dp[queue[0]] if queue else 0) + nums[i]
+            while queue and dp[queue[-1]] < dp[i]:
+                queue.pop()
+            
+            if dp[i] > 0:
+                queue.append(i)
+                
         return max(dp)
