a249ad43b51cdec7f2b368ec297e27ba2a50a060
Andrew Kim <ksb0724@utexas.edu>
2023-10-21 22:17:24 +0900

    Time: 453 ms (21.22%), Space: 177.1 MB (20.41%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int constrainedSubsetSum(vector<int>& nums, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>> pq;
-        int ans = nums[0];
-        // {current sum, current index}
-        pq.push({nums[0], 0});
+        vector<int> dp(nums.size());
+        map<int, int> window;
+        window[0] = 0;
         
-        for (int i = 1; i < nums.size(); ++i){
-            while (!pq.empty() && i - pq.top().second > k)
-                pq.pop();
+        for (int i = 0; i < nums.size(); ++i){
+            dp[i] = nums[i] + window.rbegin() -> first;
+            ++window[dp[i]];
             
-            int curr = max(0, pq.top().first) + nums[i];
-            ans = max(ans, curr);
-            pq.push({curr, i});
+            if (i >= k){
+                --window[dp[i - k]];
+                if (window[dp[i - k]] == 0)
+                    window.erase(dp[i - k]);
+            }
         }
         
-        return ans;
+        return *max_element(dp.begin(), dp.end());
     }
 };
