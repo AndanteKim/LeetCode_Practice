f718bdd579e9c48dc979958e68e9b4f9c063e5d5
Andrew Kim <ksb0724@utexas.edu>
2023-10-22 17:11:43 +0900

    Time: 1115 ms (25.51%), Space: 26.8 MB (84.18%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -1,28 +1,25 @@
 class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
-        def solve(nums: List[int], k: int) -> int:
-            n = len(nums)
-            left = [0] * k
-            curr_min = float('inf')
-            
-            for i in range(k - 1, -1, -1):
-                curr_min = min(curr_min, nums[i])
-                left[i] = curr_min
-            
-            right = []
-            curr_min = float('inf')
-            
-            for i in range(k, n):
-                curr_min = min(curr_min, nums[i])
-                right.append(curr_min)
-                
-            ans = 0
-            for j in range(len(right)):
-                curr_min = right[j]
-                i = bisect_left(left, curr_min)
-                size = (k + j) - i + 1
-                ans = max(ans, curr_min * size)
-            
-            return ans
+        n = len(nums)
+        left = [-1] * n
+        stack = []
         
-        return max(solve(nums, k), solve(nums[::-1], len(nums) - k - 1))
+        for i in range(n - 1, -1, -1):
+            while stack and nums[stack[-1]] > nums[i]:
+                left[stack.pop()] = i
+            stack.append(i)
+        
+        right = [n] * n
+        stack = []
+        
+        for i in range(n):
+            while stack and nums[stack[-1]] > nums[i]:
+                right[stack.pop()] = i
+            stack.append(i)
+        
+        ans = 0
+        for i in range(n):
+            if left[i] < k and right[i] > k:
+                ans = max(ans, nums[i] * (right[i] - left[i] - 1))
+        return ans
+        
