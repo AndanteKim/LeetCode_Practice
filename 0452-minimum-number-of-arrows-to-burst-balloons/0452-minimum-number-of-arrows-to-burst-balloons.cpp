e46fb66ecf6bbd6bc06a5338d3fa794992daf781
Andrew Kim <ksb0724@utexas.edu>
2023-10-23 20:27:00 +0900

    Time: 349 ms (72.14%), Space: 90 MB (59.50%) - LeetHub

---
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        if (points.size() == 1) return 1;
+        
+        sort(points.begin(), points.end(), [](vector<int>& a, vector<int>& b){return a[1] < b[1];});
+        int ans = 0; 
+        long currPoint = LONG_MIN;
+        
+        for (vector<int>& point : points){
+            if (currPoint < point[0]){
+                ++ans;
+                currPoint = point[1];
+            }
+        }
+        
+        return ans;
+    }
+};
