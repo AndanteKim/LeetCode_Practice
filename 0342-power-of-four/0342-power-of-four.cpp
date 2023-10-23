7c8eb497675dd19d60aae2c5461094bd39464699
Andrew Kim <ksb0724@utexas.edu>
2023-10-23 11:12:34 +0900

    Time: 2 ms (33.82%), Space: 6.2 MB (47.70%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        return n > 0 && ((n & (n - 1)) == 0) && ((n & 0xaaaaaaaa) == 0);
+        return n > 0 && ((n & (n - 1)) == 0) && (n % 3 == 1);
     }
 };
