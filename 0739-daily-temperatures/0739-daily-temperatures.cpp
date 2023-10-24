cd7bebdea04e76786c041b9036a18a5cefc4dd7b
Andrew Kim <ksb0724@utexas.edu>
2023-10-24 10:26:50 +0900

    Time: 103 ms (99.48%), Space: 98.8 MB (84.15%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        stack<int> st;
+        int n = temperatures.size(), hottest = 0;
         vector<int> ans(n, 0);
         
-        for (int i = 0; i < n; ++i){
-            while (!st.empty() && temperatures[st.top()] < temperatures[i]){
-                ans[st.top()] = i - st.top();
-                st.pop();
+        for (int currDay = n - 1; currDay >= 0; --currDay){
+            int currTemperature = temperatures[currDay];
+            if (currTemperature >= hottest){
+                hottest = currTemperature;
+                continue;
             }
-            st.push(i);
+            
+            int days = 1;
+            while (temperatures[currDay + days] <= currTemperature){
+                // use the information from ans to search for the next warmer day
+                days += ans[currDay + days];
+            }
+            ans[currDay] = days;
         }
         
         return ans;
