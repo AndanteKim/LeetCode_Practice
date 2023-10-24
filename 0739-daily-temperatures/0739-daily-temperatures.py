fc5221f11d71c984644f852ab777e678d1438fad
Andrew Kim <ksb0724@utexas.edu>
2023-10-24 10:15:36 +0900

    Time: 1017 ms (96.58%), Space: 30 MB (94.31%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,12 +1,21 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        n, stack = len(temperatures), []
+        n = len(temperatures)
+        hottest = 0
         ans = [0] * n
         
-        for i in range(n):
-            while stack and temperatures[stack[-1]] < temperatures[i]:
-                ans[stack[-1]] = i - stack[-1]
-                stack.pop()
-            stack.append(i)
+        for curr_day in range(n - 1, -1, -1):
+            curr_temp = temperatures[curr_day]
+            if curr_temp >= hottest:
+                hottest = curr_temp
+                continue
+            
+            days = 1
+            
+            while temperatures[curr_day + days] <= curr_temp:
+                # use information from answer to search for the next warmer day
+                days += ans[curr_day + days]
+            ans[curr_day] = days
         
         return ans
+        
