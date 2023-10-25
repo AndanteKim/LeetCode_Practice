962e2309763c6a7954052898ac0a5df6d4777880
Andrew Kim <ksb0724@utexas.edu>
2023-10-26 02:41:58 +0900

    Time: 0 ms (100.00%), Space: 6.3 MB (28.86%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.cpp
@@ -1,23 +1,32 @@
 class Solution {
-private:
-    int dfs(int n, int k, int rootVal){
-        if (n == 1) return rootVal;
-        
-        // Target node will be present in the right half sub-tree of the current root node
-        int totalNodes = 1 << (n - 1);
-        if (k > (totalNodes / 2)){
-            int nextRootVal = (rootVal == 0)? 1 : 0;
-            return dfs(n - 1, k - (totalNodes / 2), nextRootVal);
-        }
-        // Otherwise, the target node is in the left sub-tree of the current root node
-        else{
-            int nextRootVal = (rootVal == 0)? 0 : 1;
-            return dfs(n - 1, k, nextRootVal);
-        }
-    }
-    
 public:
     int kthGrammar(int n, int k) {
-        return dfs(n, k, 0);
+        if (n == 1)
+            return 0;
+        
+        // We assume the symbol at the target position is '1'
+        int symbol = 1;
+        
+        for (int currRow = n; currRow > 1; --currRow){
+            int totalElements = 1 << (currRow - 1);
+            int halfElements = totalElements / 2;
+            
+            // If 'k' lies in the right half symbol, then we flip over to the respective left half symbol
+            if (k > halfElements){
+                // Flip the symbol
+                k -= halfElements;
+                
+                // Change the position after flipping
+                symbol = 1 - symbol;
+            }
+        }
+        
+        // We reached the 1st row; if the symbol is not '0', we started with the wrong symbol
+        if (symbol != 0)
+            // Thus, the start symbol was '0', not '1'
+            return 0;
+            
+        // The start symbol was indeed what we started with a '1'
+        return 1;
     }
 };
