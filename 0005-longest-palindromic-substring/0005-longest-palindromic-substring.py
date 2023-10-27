3b2e12bfd93896a64d4d2225a596bb368e71c5de
Andrew Kim <ksb0724@utexas.edu>
2023-10-27 22:04:17 +0900

    Time: 101 ms (97.66%), Space: 16.1 MB (99.51%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,26 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def expand(i, j):
-            left, right = i, j
-            
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            
-            return right - left - 1
+        s_prime = '#' + '#'.join(s) + '#'
+        n = len(s_prime)
+        palindrome_radii = [0] * n
+        center = radius = 0
         
-        ans = [0, 0]
-        
-        for i in range(len(s)):
-            odd_length = expand(i, i)
-            if odd_length > ans[1] - ans[0] + 1:
-                dist = odd_length // 2
-                ans = [i - dist, i + dist]
-                
-            even_length = expand(i, i + 1)
-            if even_length > ans[1] - ans[0] + 1:
-                dist = (even_length // 2) - 1
-                ans = [i - dist, i + 1 + dist]
+        for i in range(n):
+            mirror = 2 * center - i
             
-        i, j = ans
-        return s[i : j + 1]
+            if i < radius:
+                palindrome_radii[i] = min(radius - i, palindrome_radii[mirror])
+            
+            while (i + 1 + palindrome_radii[i] < n and i - 1 - palindrome_radii[i] >= 0 and \
+                  s_prime[i + 1 + palindrome_radii[i]] == s_prime[i - 1 - palindrome_radii[i]]):
+                palindrome_radii[i] += 1
+            
+            if i + palindrome_radii[i] > radius:
+                center = i
+                radius = i + palindrome_radii[i]
+            
+        max_length = max(palindrome_radii)
+        center_index = palindrome_radii.index(max_length)
+        start_index = (center_index - max_length) // 2
+        longest_palindrome = s[start_index : start_index + max_length]
+        
+        return longest_palindrome
