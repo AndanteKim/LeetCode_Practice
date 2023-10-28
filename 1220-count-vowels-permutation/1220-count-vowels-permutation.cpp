16e83a84a190531007f941fc91182140f9526a05
Andrew Kim <ksb0724@utexas.edu>
2023-10-28 17:09:11 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (84.16%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        vector permutations(5, vector<long>(n, 1));
-        int MOD = 1'000'000'007, ans = 0;
+        long aCnt = 1, eCnt = 1, iCnt = 1, oCnt = 1, uCnt = 1, MOD = 1'000'000'007;
         
         for (int i = 1; i < n; ++i){
-            // a
-            permutations[0][i] = (permutations[1][i - 1] + permutations[2][i - 1] + permutations[4][i - 1]) % MOD;
-            // e
-            permutations[1][i] = (permutations[0][i - 1] + permutations[2][i - 1]) % MOD;
-            // i
-            permutations[2][i] = (permutations[1][i - 1] + permutations[3][i - 1]) % MOD;
-            // o
-            permutations[3][i] = permutations[2][i - 1] % MOD;
-            // u
-            permutations[4][i] = (permutations[2][i - 1] + permutations[3][i - 1]) % MOD;
+            long aNewCnt = (eCnt + iCnt + uCnt) % MOD;
+            long eNewCnt = (aCnt + iCnt) % MOD;
+            long iNewCnt = (eCnt + oCnt) % MOD;
+            long oNewCnt = iCnt % MOD;
+            long uNewCnt = (iCnt + oCnt) % MOD;
+            aCnt = aNewCnt, eCnt = eNewCnt, iCnt = iNewCnt, oCnt = oNewCnt, uCnt = uNewCnt;
         }
         
-        for (int i = 0; i < 5; ++i) ans = (ans + permutations[i].back()) % MOD;
-        
-        return ans;
+        return (aCnt + eCnt + iCnt + oCnt + uCnt) % MOD;
     }
 };
