8be882303c75dab825cde08b6668b00d939e3089
Andrew Kim <ksb0724@utexas.edu>
2023-10-28 16:59:53 +0900

    Time: 91 ms (90.14%), Space: 16 MB (100.00%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -1,18 +1,14 @@
 class Solution:
     def countVowelPermutation(self, n: int) -> int:
-        permutations = [[1] * n for _ in range(5)]
+        a_cnt = e_cnt = i_cnt = o_cnt = u_cnt = 1
         MOD = 1_000_000_007
-        
         for i in range(1, n):
-            # 'a'
-            permutations[0][i] = (permutations[1][i - 1] + permutations[2][i - 1] + permutations[4][i - 1]) % MOD
-            # 'e'
-            permutations[1][i] = (permutations[0][i - 1] + permutations[2][i - 1]) % MOD
-            # 'i'
-            permutations[2][i] = (permutations[1][i - 1] + permutations[3][i - 1]) % MOD
-            # 'o'
-            permutations[3][i] = permutations[2][i - 1] % MOD
-            # 'u'
-            permutations[4][i] = (permutations[2][i - 1] + permutations[3][i - 1]) % MOD
-        
-        return sum(permutations[i][n - 1] for i in range(5)) % MOD
+            a_new_cnt = (e_cnt + i_cnt + u_cnt) % MOD
+            e_new_cnt = (a_cnt + i_cnt) % MOD
+            i_new_cnt = (e_cnt + o_cnt) % MOD
+            o_new_cnt = i_cnt % MOD
+            u_new_cnt = (i_cnt + o_cnt) % MOD
+            
+            a_cnt, e_cnt, i_cnt, o_cnt, u_cnt = a_new_cnt, e_new_cnt, i_new_cnt, o_new_cnt, u_new_cnt
+            
+        return (a_cnt + e_cnt + i_cnt + o_cnt + u_cnt) % MOD
