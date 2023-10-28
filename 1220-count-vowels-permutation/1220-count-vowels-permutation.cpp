f52601ec871b2e51b8f97fae674769c140bff95e
Andrew Kim <ksb0724@utexas.edu>
2023-10-28 16:42:50 +0900

    Time: 12 ms (74.24%), Space: 18.1 MB (54.88%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,37 +1,23 @@
 class Solution {
-private:
-    int n, MOD = 1'000'000'007;
-    // 0: 'a', 1: 'e', 2: 'i', 3: 'o', 4: 'u'
-    map<int, set<int>> followUp{{0, {1}},\
-                                  {1, {0, 2}},\
-                                  {2, {0, 1, 3, 4}},\
-                                  {3, {2, 4}},\
-                                  {4, {0}}
-                                 };
-    
-    int dp(int remain, int prev, vector<vector<int>>& memo){
-        if (remain == 0)
-            return 1;
-        
-        if (memo[remain][prev] != -1)
-            return memo[remain][prev];
-        
-        int ways = 0;
-        for (int ch : followUp[prev]){
-            ways = (ways + dp(remain - 1, ch, memo)) % MOD;
-        }
-        
-        return memo[remain][prev] = ways;
-    }
-    
 public:
     int countVowelPermutation(int n) {
-        this -> n = n;
-        vector memo(n, vector<int>(5, -1));
-        int ans = 0;
-        // 0: 'a', 1: 'e', 2: 'i', 3: 'o', 4: 'u'
-        for (int i = 0; i < 5; ++i)
-            ans = (ans + dp(n - 1, i, memo)) % MOD;
+        vector permutations(5, vector<long>(n, 1));
+        int MOD = 1'000'000'007, ans = 0;
+        
+        for (int i = 1; i < n; ++i){
+            // a
+            permutations[0][i] = (permutations[1][i - 1] + permutations[2][i - 1] + permutations[4][i - 1]) % MOD;
+            // e
+            permutations[1][i] = (permutations[0][i - 1] + permutations[2][i - 1]) % MOD;
+            // i
+            permutations[2][i] = (permutations[1][i - 1] + permutations[3][i - 1]) % MOD;
+            // o
+            permutations[3][i] = permutations[2][i - 1] % MOD;
+            // u
+            permutations[4][i] = (permutations[2][i - 1] + permutations[3][i - 1]) % MOD;
+        }
+        
+        for (int i = 0; i < 5; ++i) ans = (ans + permutations[i].back()) % MOD;
         
         return ans;
     }
