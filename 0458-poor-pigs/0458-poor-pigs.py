0fcb0c58e01a5c7c928c18974f9dcc153722f281
Andrew Kim <ksb0724@utexas.edu>
2023-10-29 15:46:42 +0900

    Time: 38 ms (48.00%), Space: 16.3 MB (36.00%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -0,0 +1,4 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        states = minutesToTest // minutesToDie + 1
+        return ceil(log2(buckets) / log2(states))
