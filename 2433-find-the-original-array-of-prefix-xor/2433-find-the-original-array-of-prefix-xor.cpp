82cd62f1f01363c81e3ebe84b68bbf42ae826a34
Andrew Kim <ksb0724@utexas.edu>
2023-10-31 10:28:09 +0900

    Time: 85 ms (63.55%), Space: 76.3 MB (83.50%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.cpp
@@ -1,12 +1,8 @@
 class Solution {
 public:
     vector<int> findArray(vector<int>& pref) {
-        int n = pref.size();
-        vector<int> ans(n);
-        ans[0] = pref[0];
-        
-        for (int i = 1; i < n; ++i)
-            ans[i] = pref[i] ^ pref[i - 1];
-        return ans;
+        for (int i = pref.size() - 1; i > 0; --i)
+            pref[i] ^= pref[i - 1];
+        return pref;
     }
 };
