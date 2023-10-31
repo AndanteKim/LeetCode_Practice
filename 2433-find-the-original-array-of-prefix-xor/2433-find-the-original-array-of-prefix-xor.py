981501f55815946d1dc3cfdad83939ccb7b7247c
Andrew Kim <ksb0724@utexas.edu>
2023-10-31 10:26:25 +0900

    Time: 669 ms (50.71%), Space: 31.3 MB (91.10%) - LeetHub

--- 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
+++ 2433-find-the-original-array-of-prefix-xor/2433-find-the-original-array-of-prefix-xor.py
@@ -1,10 +1,7 @@
 class Solution:
     def findArray(self, pref: List[int]) -> List[int]:
-        n = len(pref)
-        ans = [0] * n
-        ans[0] = pref[0]
         
-        for i in range(1, n):
-            ans[i] = pref[i] ^ pref[i - 1]
-            
-        return ans
+        for i in range(len(pref) - 1, 0, -1):
+            pref[i] ^= pref[i - 1]
+        
+        return pref
