f9988b31b6c2f29bb498d8cd4cb75340bcf2a7f5
Andrew Kim <ksb0724@utexas.edu>
2023-11-01 16:39:50 +0900

    Time: 53 ms (73.61%), Space: 20.2 MB (94.89%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -6,39 +6,40 @@
 #         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        def add(num: int) -> None:
+            nonlocal max_streak, curr_streak, curr_num, ans
+            if num == curr_num:
+                curr_streak += 1
+            else:
+                curr_streak = 1
+                curr_num = num
+            
+            if curr_streak > max_streak:
+                ans = []
+                max_streak = curr_streak
+            
+            if curr_streak == max_streak:
+                ans.append(num)
+                
         max_streak, curr_streak, curr_num = 0, 0, 0
         ans = []
-        
         curr = root
+        
         while curr:
             if curr.left:
-                # Find the friend
                 friend = curr.left
-                while friend.right:
+                while friend.right and friend.right != curr:
                     friend = friend.right
                 
-                friend.right = curr
-                
-                # Delete the edge after using it
-                left = curr.left
-                curr.left = None
-                curr = left
-            else:
-                # Handle the current node
-                num = curr.val
-                if num == curr_num:
-                    curr_streak += 1
+                if not friend.right:
+                    friend.right = curr
+                    curr = curr.left
                 else:
-                    curr_streak = 1
-                    curr_num = num
-                
-                if curr_streak > max_streak:
-                    ans = []
-                    max_streak = curr_streak
-                
-                if curr_streak == max_streak:
-                    ans.append(num)
-                
+                    friend.right = None
+                    add(curr.val)
+                    curr = curr.right
+            else:
+                add(curr.val)
                 curr = curr.right
-        
+
         return ans
