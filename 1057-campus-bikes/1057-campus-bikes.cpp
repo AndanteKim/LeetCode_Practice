3e0c63d1dc940f58802a10abc7a3dbcdaccf9dc1
Andrew Kim <ksb0724@utexas.edu>
2023-11-01 23:04:31 +0900

    Time: 70 ms (84.31%), Space: 42.6 MB (55.88%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.cpp
+++ 1057-campus-bikes/1057-campus-bikes.cpp
@@ -1,46 +1,45 @@
 class Solution {
 private:
+    // manhattan distance function
     int findDistance(vector<int>& worker, vector<int>& bike){
         return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1]);
     }
     
 public:
     vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        // store the list of (worker, bike) pairs corresponding to its distance
+        vector<pair<int, int>> buckets[1999];
+        int minDistance = INT_MAX;
         
-        // calculate the distance between each worker and bike
-        vector<tuple<int, int, int>> allTriplets;
+        // add the (worker, bike) pairs corresponding to their distance list
         for (int i = 0; i < workers.size(); ++i){
             for (int j = 0; j < bikes.size(); ++j){
-                allTriplets.push_back({findDistance(workers[i], bikes[j]), i, j});
+                int distance = findDistance(workers[i], bikes[j]);
+                buckets[distance].push_back({i, j});
+                minDistance = min(minDistance, distance);
             }
         }
         
-        // sort the triplets. By default, sorting will prioritize the tuple's first value,
-        // then second value, and finally the third value
-        sort(allTriplets.begin(), allTriplets.end());
         
-        // initialize all values to False, to signify no bikes have been taken
-        vector<bool> usedBikes(bikes.size());
-        // initialize all values to -1, to signify no worker has a bike
-        vector<int> ans(workers.size(), -1);
-        // keep track of how many worker-bike pairs have been made
-        int pairCount = 0;
+        int currDistance = minDistance, pairCount = 0;
+        // Initialize all values to false, to signify no bikes have been taken
+        vector<bool> bikeStatus(bikes.size());
+        // Initialize all index to -1, to signify no worker has a bike
+        vector<int> workerStatus(workers.size(), -1);
         
-        for (auto& [dist, worker, bike] : allTriplets){
-            // If both worker and bike are free, assign the bike to the worker
-            // and mark the bike as taken
-            if (!usedBikes[bike] && ans[worker] == -1){
-                usedBikes[bike] = true;
-                ans[worker] = bike;
-                ++pairCount;
-                
-                // If all the workers have the bike assigned, we can stop
-                if (pairCount == workers.size())
-                    return ans;
+        // Until all workers have not been assigned a bike
+        while (pairCount < workers.size()){
+            for (auto& [worker, bike] : buckets[currDistance]){
+                if (!bikeStatus[bike] && workerStatus[worker] == -1){
+                    // If both worker and bike are free, assign them to each other
+                    bikeStatus[bike] = true;
+                    workerStatus[worker] = bike;
+                    ++pairCount;
+                }
             }
-            
+            ++currDistance;
         }
-            
-        return ans;
+        
+        return workerStatus;
     }
 };
