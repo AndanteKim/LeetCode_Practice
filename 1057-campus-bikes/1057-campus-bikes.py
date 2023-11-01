483e71d180f14de38bce28e63dd389e2c48040b0
Andrew Kim <ksb0724@utexas.edu>
2023-11-01 22:30:06 +0900

    Time: 825 ms (59.43%), Space: 112.5 MB (93.40%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -3,36 +3,34 @@ class Solution:
         def find_distance(worker_loc: List[int], bike_loc: List[int]) -> int:
             return abs(worker_loc[0] - bike_loc[0]) + abs(worker_loc[1] - bike_loc[1])
         
-        # calculate the distance between each worker and bike
-        all_triplets = []
+        min_dist = float('inf')
+        dist_to_pairs = defaultdict(list)
+        
         for worker, worker_loc in enumerate(workers):
             for bike, bike_loc in enumerate(bikes):
                 distance = find_distance(worker_loc, bike_loc)
-                all_triplets.append((distance, worker, bike))
+                dist_to_pairs[distance].append((worker, bike))
+                min_dist = min(min_dist, distance)
             
-        # sort the triplets. By default, sorting will prioritize the tuple's first value,
-        # then second value, and finally the third value
-        all_triplets.sort()
+        curr_dist = min_dist
         
-        # initialize all values to False, to signify no bikes have been taken
+        # Init all values to False, to signify no bikes have been taken
         bike_status = [False] * len(bikes)
         
-        # initialize the all values to -1, to signify no worker has a bike
+        # Init all values to -1, to signify no worker has a bike
         worker_status = [-1] * len(workers)
         
         # keep track of how many worker-bike pairs have been made
         pair_count = 0
         
-        for distance, worker, bike in all_triplets:
-            # If both worker and bike are free, assign the bike to the
-            # worker and mark the bike as taken
-            if worker_status[worker] == -1 and not bike_status[bike]:
-                bike_status[bike] = True
-                worker_status[worker] = bike
-                pair_count += 1
-                
-                # If all the workers have the bike assigned, we can stop
-                if pair_count == len(workers):
-                    return worker_status
+        # While all workers have not been assigned a bike
+        while pair_count < len(workers):
+            for worker, bike in dist_to_pairs[curr_dist]:
+                if worker_status[worker] == -1 and not bike_status[bike]:
+                    # If both worker and bike are free, assign them to each other
+                    bike_status[bike] = True
+                    worker_status[worker] = bike
+                    pair_count += 1
+            curr_dist += 1
         
         return worker_status
