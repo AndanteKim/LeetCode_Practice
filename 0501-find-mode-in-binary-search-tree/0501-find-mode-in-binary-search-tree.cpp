3cac97f3a2aa11bbcb4caaf5f4ed6851f3e881c0
Andrew Kim <ksb0724@utexas.edu>
2023-11-01 16:29:15 +0900

    Time: 13 ms (77.45%), Space: 12.7 MB (99.78%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.cpp
@@ -10,37 +10,45 @@
  * };
  */
 class Solution {
-private:
-    void dfs(TreeNode* node, vector<int>& values){
-        if (!node)
-            return;
-        
-        dfs(node -> left, values);
-        values.push_back(node -> val);
-        dfs(node -> right, values);
-    }
-    
 public:
     vector<int> findMode(TreeNode* root) {
-        vector<int> values, ans;
-        dfs(root, values);
+        int maxStreak = 0, currStreak = 0, currNum = 0;
+        TreeNode *curr = root, *left;
+        vector<int> ans;
         
-        int currStreak = 0, maxStreak = 0, currVal = 0;
-        for (int num : values){
-            if (num == currVal)
-                ++currStreak;
+        while (curr){
+            if (curr -> left){
+                // find the friend
+                TreeNode* friendNode = curr -> left;
+                while (friendNode -> right)
+                    friendNode = friendNode -> right;
+                
+                friendNode -> right = curr;
+                
+                // Delete the edge after using it
+                left = curr -> left;
+                curr -> left = nullptr;
+                curr = left;
+            }
             else{
-                currStreak = 1;
-                currVal = num;
+                int num = curr -> val;
+                if (num == currNum)
+                    ++currStreak;
+                else{
+                    currStreak = 1;
+                    currNum = num;
+                }
+                
+                if (currStreak > maxStreak){
+                    ans = {};
+                    maxStreak = currStreak;
+                }
+                
+                if (currStreak == maxStreak)
+                    ans.push_back(num);
+                
+                curr = curr -> right;
             }
-            
-            if (currStreak > maxStreak){
-                maxStreak = currStreak;
-                ans.clear();
-            }
-            
-            if (currStreak == maxStreak)
-                ans.push_back(currVal);
         }
         
         return ans;
