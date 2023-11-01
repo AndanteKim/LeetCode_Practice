2690d5d83d4441c2f5157c284907976ee3570f69
Andrew Kim <ksb0724@utexas.edu>
2023-11-01 23:19:05 +0900

    Time: 540 ms (72.64%), Space: 139.3 MB (58.02%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.py
+++ 1057-campus-bikes/1057-campus-bikes.py
@@ -3,34 +3,40 @@ class Solution:
         def find_distance(worker_loc: List[int], bike_loc: List[int]) -> int:
             return abs(worker_loc[0] - bike_loc[0]) + abs(worker_loc[1] - bike_loc[1])
         
-        min_dist = float('inf')
-        dist_to_pairs = defaultdict(list)
+        # List of triplets (distance, worker index, bike index) for each worker_bike combination
+        worker_to_bike_list = []
+        pq = []
         
         for worker, worker_loc in enumerate(workers):
+            curr_worker_pairs = []
             for bike, bike_loc in enumerate(bikes):
                 distance = find_distance(worker_loc, bike_loc)
-                dist_to_pairs[distance].append((worker, bike))
-                min_dist = min(min_dist, distance)
+                curr_worker_pairs.append((distance, worker, bike))
+                
+            # sort the worker_to_bike_list for the current worker in reverse order
+            curr_worker_pairs.sort(reverse = True)
+            # add the closest bike for this worker to the priority queue
+            heappush(pq, curr_worker_pairs.pop())
+            # store the remaining options for the current worker in worker_to_bike_list
+            worker_to_bike_list.append(curr_worker_pairs)
             
-        curr_dist = min_dist
-        
-        # Init all values to False, to signify no bikes have been taken
+        # initialize all values to False, to signify no bikes have been taken
         bike_status = [False] * len(bikes)
-        
-        # Init all values to -1, to signify no worker has a bike
+        # initialize all values to -1, to signify no worker has a bike
         worker_status = [-1] * len(workers)
         
-        # keep track of how many worker-bike pairs have been made
-        pair_count = 0
-        
-        # While all workers have not been assigned a bike
-        while pair_count < len(workers):
-            for worker, bike in dist_to_pairs[curr_dist]:
-                if worker_status[worker] == -1 and not bike_status[bike]:
-                    # If both worker and bike are free, assign them to each other
-                    bike_status[bike] = True
-                    worker_status[worker] = bike
-                    pair_count += 1
-            curr_dist += 1
-        
+        while pq:
+            # pop the worker-bike pair with smallest distance
+            distance, worker, bike = heappop(pq)
+            
+            if not bike_status[bike]:
+                # If the bike is free, assign the bike to the worker
+                bike_status[bike] = True
+                worker_status[worker] = bike
+            else:
+                # Otherwise, add the next closest bike for the current worker to the priority queue
+                next_closest_bike = worker_to_bike_list[worker].pop()
+                heappush(pq, next_closest_bike)
+                
         return worker_status
+           
