68fd232fdf8e7747c2473ae8effa923a0f1c62ee
Andrew Kim <ksb0724@utexas.edu>
2023-11-01 23:52:04 +0900

    Time: 168 ms (76.47%), Space: 59.9 MB (29.41%) - LeetHub

--- 1057-campus-bikes/1057-campus-bikes.cpp
+++ 1057-campus-bikes/1057-campus-bikes.cpp
@@ -1,43 +1,54 @@
 class Solution {
 private:
-    // manhattan distance function
     int findDistance(vector<int>& worker, vector<int>& bike){
         return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1]);
     }
     
 public:
     vector<int> assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
-        // store the list of (worker, bike) pairs corresponding to its distance
-        vector<pair<int, int>> buckets[1999];
-        int minDistance = INT_MAX;
+        // List of triplets (distance, worker index, bike index) for each worker-bike combination
+        vector<vector<tuple<int, int, int>>> workerToBikeList;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
         
-        // add the (worker, bike) pairs corresponding to their distance list
         for (int i = 0; i < workers.size(); ++i){
+            vector<tuple<int, int, int>> currWorkerPairs;
             for (int j = 0; j < bikes.size(); ++j){
                 int distance = findDistance(workers[i], bikes[j]);
-                buckets[distance].push_back({i, j});
-                minDistance = min(minDistance, distance);
+                currWorkerPairs.push_back({distance, i, j});
             }
+            // sort the workerToBikeList for the current worker in reverse order
+            sort(currWorkerPairs.begin(), currWorkerPairs.end(), greater<tuple<int, int, int>>());
+            
+            // add the closest bike for this worker to the priority queue
+            pq.push(currWorkerPairs.back());
+            
+            // second last is now the closest bike for this worker
+            currWorkerPairs.pop_back();
+            // store the remaining options for the current worker in workerToBikeList
+            workerToBikeList.push_back(currWorkerPairs);
         }
         
-        
-        int currDistance = minDistance, pairCount = 0;
-        // Initialize all values to false, to signify no bikes have been taken
+        // initialize all value to false, to signify no bikes have been taken
         vector<bool> bikeStatus(bikes.size());
-        // Initialize all index to -1, to signify no worker has a bike
+        // initialize all values to -1, to signify no worker has a bike
         vector<int> workerStatus(workers.size(), -1);
         
-        // Until all workers have not been assigned a bike
-        while (pairCount < workers.size()){
-            for (auto& [worker, bike] : buckets[currDistance]){
-                if (!bikeStatus[bike] && workerStatus[worker] == -1){
-                    // If both worker and bike are free, assign them to each other
-                    bikeStatus[bike] = true;
-                    workerStatus[worker] = bike;
-                    ++pairCount;
-                }
+        while (!pq.empty()){
+            // pop the worker-bike pair with smallest distance
+            auto [distance, worker, bike] = pq.top();
+            pq.pop();
+            
+            if (!bikeStatus[bike]){
+                // If the bike is free, assign the bike to the worker
+                bikeStatus[bike] = true;
+                workerStatus[worker] = bike;
             }
-            ++currDistance;
+            else{
+                // otherwise, add the next closest bike for the current worker to the priority queue
+                tuple<int, int, int> nextClosestBike = workerToBikeList[worker].back();
+                workerToBikeList[worker].pop_back();
+                pq.push(nextClosestBike);
+            }
         }
         
         return workerStatus;
