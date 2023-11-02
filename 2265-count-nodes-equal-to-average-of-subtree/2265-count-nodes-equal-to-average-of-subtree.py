562e319129b0b87a985c92fad8d43046f8d111ec
Andrew Kim <ksb0724@utexas.edu>
2023-11-02 11:05:58 +0900

    Time: 47 ms (69.79%), Space: 17.6 MB (58.02%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.py
@@ -6,18 +6,22 @@
 #         self.right = right
 class Solution:
     def averageOfSubtree(self, root: Optional[TreeNode]) -> int:
-        def dfs(node: Optional[TreeNode]) -> Tuple[int]:
+        def post_order(node: Optional[TreeNode]) -> Tuple[int]:
+            nonlocal count
             if not node:
-                return (0, 0, 0)
+                return (0, 0)
             
-            if not node.left and not node.right:
-                return (node.val, 1, 1)
+            left = post_order(node.left)
+            right = post_order(node.right)
             
-            left_total, left_count, left_correct = dfs(node.left)
-            right_total, right_count, right_correct = dfs(node.right)
-            if (left_total + node.val + right_total) // (left_count + right_count + 1) == node.val:
-                return (left_total + node.val + right_total, left_count + right_count + 1, left_correct + right_correct + 1)
-            return (left_total + node.val + right_total, left_count + right_count + 1, left_correct + right_correct)
-                
-        total, count, ans = dfs(root)
-        return ans
+            node_sum = left[0] + right[0] + node.val
+            node_count = left[1] + right[1] + 1
+            
+            # check if the average of the subtree == node value
+            if node.val == node_sum // node_count:
+                count += 1
+            return (node_sum, node_count)
+        
+        count = 0
+        post_order(root)
+        return count
