53cb6d32558f0e4107f82996bf505f8189b9f51e
Andrew Kim <ksb0724@utexas.edu>
2023-11-03 12:06:24 +0900

    Time: 0 ms (100.00%), Space: 8.2 MB (49.67%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
-        int start = 0, targetLength = target.size();
         vector<string> ans;
-        for (int i = 1; i <= n && start < targetLength; ++i){
-            ans.push_back("Push");
-            if (i == target[start])
-                ++start;
-            else
+        int i = 0;
+        
+        for (int num : target){
+            while (i < num - 1){
+                ans.push_back("Push");
                 ans.push_back("Pop");
+                ++i;
+            }
+            ans.push_back("Push");
+            ++i;
         }
         
         return ans;
