743c82fabaa5fbc8317b9006d904664af84f3c70
Andrew Kim <ksb0724@utexas.edu>
2023-11-05 17:55:16 +0900

    Time: 84 ms (68.52%), Space: 63.3 MB (93.06%) - LeetHub

--- 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
+++ 1535-find-the-winner-of-an-array-game/1535-find-the-winner-of-an-array-game.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     int getWinner(vector<int>& arr, int k) {
         int mx = *max_element(arr.begin(), arr.end()), curr = arr[0], winstreak = 0, opponent;
-        queue<int> queue({arr.begin() + 1, arr.end()});
         
-        while (!queue.empty()){
-            opponent = queue.front();
-            queue.pop();
-            
+        for (int i = 1; i < arr.size(); ++i){
+            opponent = arr[i];
             if (curr > opponent){
-                queue.push(opponent);
                 ++winstreak;
             }
             else{
-                queue.push(curr);
                 curr = opponent;
                 winstreak = 1;
             }
