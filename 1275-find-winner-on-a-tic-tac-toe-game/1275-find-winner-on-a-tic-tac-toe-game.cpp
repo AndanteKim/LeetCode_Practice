f9fa13a128f7af2e19b2f9873eaed9809d9a1210
Andrew Kim <ksb0724@utexas.edu>
2023-11-06 21:26:25 +0900

    Time: 0 ms (100.00%), Space: 9 MB (5.45%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.cpp
@@ -1,56 +1,37 @@
 class Solution {
-private:
-    int n;
-    // check if any of 4 winning condittions to see if the current player has won
-    bool checkRow(int row, int playerId, vector<vector<int>>& board){
-        for (int col = 0; col < n; ++col)
-            if (board[row][col] != playerId)
-                return false;
-        return true;
-    }
-    
-    bool checkCol(int col, int playerId, vector<vector<int>>& board){
-        for (int row = 0; row < n; ++row)
-            if (board[row][col] != playerId)
-                return false;
-        return true;
-    }
-    
-    bool checkDiagonal(int playerId, vector<vector<int>>& board){
-        for (int row = 0; row < n; ++row)
-            if (board[row][row] != playerId)
-                return false;
-        return true;
-    }
-    
-    bool checkAntiDiagonal(int playerId, vector<vector<int>>& board){
-        for (int row = 0; row < n; ++row)
-            if (board[row][n - 1 - row] != playerId)
-                return false;
-        return true;
-    }
-    
 public:
     string tictactoe(vector<vector<int>>& moves) {
-        this -> n = 3;
-        int player = 1;
-        vector<vector<int>> board(n, vector<int>(n));
+        // use rows and cols to record the value on each row and each column diag1 and diag2 to record value on diagonal or anti-diagonal
+        int n = 3, player = 1, diagonals = 0, antiDiagonals = 0;
+        vector<int> rows(n), cols(n);
+        
+        // 2 players having value of 1 and -1, player_1 with value = 1 places first
         for (vector<int>& move : moves){
-            int row = move[0] , col = move[1];
-            board[row][col] = player;
+            int row = move[0], col = move[1];
             
-            // If any of the winning conditions is met, return the current player's id
-            if (checkRow(row, player, board) || checkCol(col, player, board) ||\
-               (row == col && checkDiagonal(player, board)) || \
-               (row + col == n - 1 && checkAntiDiagonal(player, board)))
-                return player == 1? "A" : "B";
+            // update the row value and column value
+            rows[row] += player;
+            cols[col] += player;
+            
+            // If this move is placed on diagonal or anti-diagonal, we shall update the relative value as well
+            if (row == col)
+                diagonals += player;
+            
+            if (row + col == n - 1)
+                antiDiagonals += player;
+            
+            // check if this move meets any of the winning conditions
+            for (int line : vector<int>{rows[row], cols[col], diagonals, antiDiagonals})
+                if (abs(line) == n)
+                    return (player == 1)? "A" : "B";
             
             // If no one wins so far, change to the other player alternatively
+            // That is from 1 to -1, from -1 to 1.
             player *= -1;
         }
         
-        //If all moves are completed and there is no still result, we shall check if the grid is\
-        //full or not.        
-        return moves.size() == n * n? "Draw" : "Pending";
+        // If all moves are completed and there is still no result, we shall check if the grid is full or not.
+        // If so, the game ends with draw, otherwise pending.
+        return (moves.size() == n * n)? "Draw" : "Pending";
     }
 };
