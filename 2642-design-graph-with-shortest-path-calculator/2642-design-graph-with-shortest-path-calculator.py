7ca26fdf1029cb4bca8f4bdf5990f2176367f02b
Andrew Kim <ksb0724@utexas.edu>
2023-11-11 22:23:37 +0900

    Time: 4977 ms (5.89%), Space: 19.2 MB (85.29%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
@@ -1,35 +1,31 @@
 class Graph:
 
     def __init__(self, n: int, edges: List[List[int]]):
-        self.graph = [[] for _ in range(n)]
-        
+        self.adj_matrix = [[float('inf')] * n for _ in range(n)]
         for start, end, cost in edges:
-            self.graph[start].append((end, cost))
-
+            self.adj_matrix[start][end] = cost
+        
+        for i in range(n):
+            self.adj_matrix[i][i] = 0
+        
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    self.adj_matrix[j][k] = min(self.adj_matrix[j][k], \
+                                               self.adj_matrix[j][i] +\
+                                               self.adj_matrix[i][k])
+                    
     def addEdge(self, edge: List[int]) -> None:
         start, end, cost = edge
-        self.graph[start].append((end, cost))
+        n = len(self.adj_matrix)
+        for i in range(n):
+            for j in range(n):
+                self.adj_matrix[i][j] = min(self.adj_matrix[i][j],\
+                                           self.adj_matrix[i][start] +\
+                                           self.adj_matrix[end][j] + cost)
 
     def shortestPath(self, node1: int, node2: int) -> int:
-        n, pq = len(self.graph), [(0, node1)]
-        cost_for_node = [float('inf')] * n
-        cost_for_node[node1] = 0
-        
-        while pq:
-            curr_cost, curr_node = heappop(pq)
-            if curr_cost > cost_for_node[curr_node]:
-                continue
-            
-            if curr_node == node2:
-                return curr_cost
-            
-            for neighbor, cost in self.graph[curr_node]:
-                new_cost = curr_cost + cost
-                if new_cost < cost_for_node[neighbor]:
-                    cost_for_node[neighbor] = new_cost
-                    heappush(pq, (new_cost, neighbor))
-        
-        return -1
+        return -1 if self.adj_matrix[node1][node2] == float('inf') else self.adj_matrix[node1][node2]
 
 
 # Your Graph object will be instantiated and called as such:
