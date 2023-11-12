7c0ed9405937d375d38c9d91aae4689f38c6e684
Andrew Kim <ksb0724@utexas.edu>
2023-11-12 18:37:24 +0900

    Time: 2283 ms (16.46%), Space: 32.2 MB (98.36%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,41 +1,63 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        def have_common_node(route1: List[int], route2: List[int]) -> bool:
+            i, j = 0, 0
+            while i < len(route1) and j < len(route2):
+                if route1[i] == route2[j]:
+                    return True
+            
+                if route1[i] < route2[j]:
+                    i += 1
+                else:
+                    j += 1
+            return False
+        
+        # Return true if the given stop is present in the route, False otherwise
+        def is_stop_exist(route: List[int], stop: int) -> bool:
+            for j in range(len(route)):
+                if route[j] == stop:
+                    return True
+            return False
+    
+        
         if source == target:
             return 0
         
-        # key: bus stop, value : route number
-        adj_list = defaultdict(list)
+        for i in range(len(routes)):
+            routes[i].sort()
         
-        # create a map from the bus stop to all the route that include this stop
-        for route in range(len(routes)):
-            for stop in routes[route]:
-                adj_list[stop].append(route)
-                
-        queue, visited = deque(), set()
-        # insert all the routes in the queue having the source stop
-        for route in adj_list[source]:
-            queue.append(route)
-            visited.add(route)
+        adj_list = [[] for _ in range(501)]
+        for i in range(len(routes)):
+            for j in range(i + 1, len(routes)):
+                if have_common_node(routes[i], routes[j]):
+                    adj_list[i].append(j)
+                    adj_list[j].append(i)
         
+        queue = deque()
+        for i in range(len(routes)):
+            if is_stop_exist(routes[i], source):
+                queue.append(i)
+        
+        visited = [False] * len(routes)
         bus_count = 1
+        
         while queue:
             size = len(queue)
             
-            for i in range(size):
-                route = queue.popleft()
+            while size > 0:
+                node = queue.popleft()
                 
-                # iterate over the stops in the current route
-                for stop in routes[route]:
-                    # return the current count if the target is found
-                    if stop == target:
-                        return bus_count
-                    
-                    # iterate over the next possible routes from the current stop
-                    for next_route in adj_list[stop]:
-                        if next_route not in visited:
-                            visited.add(next_route)
-                            queue.append(next_route)
+                # Return bus_count, if the stop target is present in the current route
+                if is_stop_exist(routes[node], target):
+                    return bus_count
             
+                # Add the adjacent routes
+                for adj in adj_list[node]:
+                    if not visited[adj]:
+                        visited[adj] = True
+                        queue.append(adj)
+                size -= 1
+                
             bus_count += 1
-        
         return -1
+        
