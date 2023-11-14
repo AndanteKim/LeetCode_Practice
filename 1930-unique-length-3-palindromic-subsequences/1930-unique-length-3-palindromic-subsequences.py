5ecfaa55492a5a8098198d99fd77336b73ee6fc6
Andrew Kim <ksb0724@utexas.edu>
2023-11-14 10:06:57 +0900

    Time: 138 ms (92.98%), Space: 17.3 MB (43.97%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.py
@@ -1,14 +1,3 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
-        letters = set(s)
-        ans = 0
-        
-        for letter in letters:
-            i, j = s.index(letter), s.rindex(letter)
-            between = set()
-            
-            for k in range(i + 1, j):
-                between.add(s[k])
-            ans += len(between)
-        
-        return ans
+        return sum([len(set(s[s.index(letter) + 1:s.rindex(letter)])) for letter in set(s)])
