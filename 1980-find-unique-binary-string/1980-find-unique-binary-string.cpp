612fa6b403ed3e97b155008b4879435285e8abf9
Andrew Kim <ksb0724@utexas.edu>
2023-11-16 17:03:00 +0900

    Time: 6 ms (42.30%), Space: 10.5 MB (75.23%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     string findDifferentBinaryString(vector<string>& nums) {
-        unordered_set<int> integers;
-        for (string& num : nums)
-            integers.insert(stoi(num, 0, 2));
-        
-        int ans = stoi(nums[0], 0, 2), n = nums.size();
-        
-        while (integers.find(ans) != integers.end())
-            ans = rand() % (int) pow(2, n);
-        
-        return bitset<16>(ans).to_string().substr(16 - n);
+        string ans = "";
+        for (int i = 0; i < nums.size(); ++i){
+            char digit = nums[i][i];
+            ans.push_back(digit == '1'? '0':'1');
+        }
+        return ans;
     }
 };
