217199b0e649e1635b712bc296b7dffdaea3427f
Andrew Kim <ksb0724@utexas.edu>
2023-11-18 22:37:29 +0900

    Time: 287 ms (5.15%), Space: 108.1 MB (5.28%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.cpp
@@ -1,18 +1,37 @@
 class Solution {
+private:
+    int check(int i, vector<long>& prefix, vector<int>& nums, int k){
+        long target = nums[i], left = 0, right = i, best = i;
+        long curr = 0;
+        
+        while (left <= right){
+            int mid = (left + right) >> 1;
+            int count = i - mid + 1;
+            long finalSum = count * target, originalSum = prefix[i] - prefix[mid] + nums[mid];
+            long operationsRequired = finalSum - originalSum;
+            if (operationsRequired > k)
+                left = mid + 1;
+            else{
+                best = mid;
+                right = mid - 1;
+            }
+        }
+        return i - best + 1;
+    }
+    
 public:
     int maxFrequency(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int left = 0;
-        long curr = 0;
+        vector<long> prefix(nums.size());
+        prefix[0] = nums[0];
         
-        for (int right = 0; right < nums.size(); ++right){
-            long target = nums[right];
-            curr += target;
-            
-            if (target * (right - left + 1) - curr > k)
-                curr -= nums[left++];
-        }
+        for (int i = 1; i < nums.size(); ++i)
+            prefix[i] = prefix[i - 1] + nums[i];
         
-        return nums.size() - left;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); ++i)
+            ans = max(ans, check(i, prefix, nums, k));
+        
+        return ans;
     }
 };
