e251a89d729566650f425d9bf889f11b42067528
Andrew Kim <ksb0724@utexas.edu>
2023-11-19 19:05:01 +0900

    Time: 909 ms (20.62%), Space: 23.9 MB (58.25%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.py
@@ -1,20 +1,12 @@
 class Solution:
     def reductionOperations(self, nums: List[int]) -> int:
-        lowest, start, counting = float('inf'), float('-inf'), [0] * (5 * (10 ** 4) + 1)
-        
-        for num in nums:
-            counting[num] += 1
-            lowest = min(lowest, num)
-            start = max(start, num)
-        
-        ans, curr, prev = 0, start - 1, start
-        while counting[lowest] < len(nums):
-            if counting[curr] != 0:
-                ans += counting[prev]
-                counting[curr] += counting[prev]
-                prev = curr
-                
-            curr -= 1
-        
+        nums.sort()
+        ans, up = 0, 0
+    
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                up += 1
+            
+            ans += up
+            
         return ans
-        
