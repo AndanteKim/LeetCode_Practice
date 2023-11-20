9d791f2459b7197ed3a0ea7ffa0990056ee35169
Andrew Kim <ksb0724@utexas.edu>
2023-11-20 18:50:19 +0900

    Time: 847 ms (78.51%), Space: 42.5 MB (5.62%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,27 +1,17 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        prefix_sum = [0] * (len(travel) + 1)
-        prefix_sum[1] = travel[0];
         for i in range(1, len(travel)):
-            prefix_sum[i + 1] = prefix_sum[i] + travel[i]
+            travel[i] = travel[i - 1] + travel[i]
         
-        # map to store garbage type to the last house index
-        garbage_last_pos = dict();
+        garbage_last_pos, ans = dict(), 0
         
-        # map to store the total count of each type of garbage in all houses
-        garbage_count = dict()
         for i in range(len(garbage)):
             for c in garbage[i]:
-                garbage_last_pos[c] = i;
-                garbage_count[c] = garbage_count.get(c, 0) + 1
-                
-        ans = 0
-        for c in ('M', 'P', 'G'):
-            # add only if there is at least one unit of this garbage
-            if garbage_count.get(c, 0) > 0:
-                ans += prefix_sum[garbage_last_pos[c]] + garbage_count.get(c, 0)
+                garbage_last_pos[c] = i
+            ans += len(garbage[i])
+        
+        for c in ('M', 'G', 'P'):
+            # No travel time is required if the last house is at index 0
+            ans += (0 if garbage_last_pos.get(c, 0) == 0 else travel[garbage_last_pos[c] - 1])
         
         return ans
-        
-        
-        
