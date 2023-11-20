93891d99ef25c7ff889834a7f0e3e96acd1043b5
Andrew Kim <ksb0724@utexas.edu>
2023-11-20 18:32:40 +0900

    Time: 944 ms (40.49%), Space: 39.4 MB (15.87%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -1,21 +1,27 @@
 class Solution:
     def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
-        time, count_type = 0, dict()
-        for dummy in garbage:
-            for trash_type in dummy:
-                count_type[trash_type] = count_type.get(trash_type, 0) + 1
+        prefix_sum = [0] * (len(travel) + 1)
+        prefix_sum[1] = travel[0];
+        for i in range(1, len(travel)):
+            prefix_sum[i + 1] = prefix_sum[i] + travel[i]
         
-        for truck in count_type:
-            curr = 0
-            for i in range(len(garbage)):
-                for trash in garbage[i]:
-                    if trash == truck:
-                        curr += 1
-                        count_type[trash] -= 1
-                if count_type[truck] == 0:
-                    break
-                if i < len(travel):
-                    curr += travel[i]
-            time += curr
-            
-        return time
+        # map to store garbage type to the last house index
+        garbage_last_pos = dict();
+        
+        # map to store the total count of each type of garbage in all houses
+        garbage_count = dict()
+        for i in range(len(garbage)):
+            for c in garbage[i]:
+                garbage_last_pos[c] = i;
+                garbage_count[c] = garbage_count.get(c, 0) + 1
+                
+        ans = 0
+        for c in ('M', 'P', 'G'):
+            # add only if there is at least one unit of this garbage
+            if garbage_count.get(c, 0) > 0:
+                ans += prefix_sum[garbage_last_pos[c]] + garbage_count.get(c, 0)
+        
+        return ans
+        
+        
+        
