b089695ed5ede0f8eb823c698cda790005ec6c99
Andrew Kim <ksb0724@utexas.edu>
2023-11-22 14:11:23 +0900

    Time: 1535 ms (22.93%), Space: 41.3 MB (69.33%) - LeetHub

--- 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
+++ 0624-maximum-distance-in-arrays/0624-maximum-distance-in-arrays.py
@@ -1,26 +1,14 @@
 class Solution:
     def maxDistance(self, arrays: List[List[int]]) -> int:
-        min_heap, max_heap = [], []
+        ans = 0
+        n, min_val, max_val = len(arrays[0]), arrays[0][0], arrays[0][len(arrays[0]) - 1]
         
-        for i in range(len(arrays)):
-            mn, mx = arrays[i][0], arrays[i][-1]
-            heappush(min_heap, (mn, i))
-            heappush(max_heap, (-mx, i))
+        for i in range(1, len(arrays)):
+            n = len(arrays[i])
+            ans = max(ans, abs(arrays[i][n - 1] - min_val), abs(max_val - arrays[i][0]))
+            min_val = min(min_val, arrays[i][0])
+            max_val = max(max_val, arrays[i][n - 1])
         
-        mn, mx = float('inf'), float('-inf')
-        idx1, idx2 = -1, -1
-        while min_heap and max_heap:
-            if mn == float('inf') and idx1 == -1:
-                mn, idx1 = heappop(min_heap)
-            if mx == float('-inf') and idx2 == -1:
-                mx, idx2 = heappop(max_heap)
-            
-            if idx1 == idx2:
-                if abs(mn - min_heap[0][0]) < abs(mx - max_heap[0][0]):
-                    mn, idx1 = float('inf'), -1
-                else:
-                    mx, idx2 = float('-inf'), -1
-            else:
-                return abs(mx + mn)
+        return ans
         
-        return 0
+        
