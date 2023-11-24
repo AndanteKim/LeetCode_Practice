127487fd6d98dde95df21acaf11215528a2aca5c
Andrew Kim <ksb0724@utexas.edu>
2023-11-24 17:27:34 +0900

    Time: 101 ms (23.80%), Space: 53.7 MB (54.72%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.cpp
@@ -2,20 +2,10 @@ class Solution {
 public:
     int maxCoins(vector<int>& piles) {
         sort(piles.begin(), piles.end());
-        deque<int> dq(piles.begin(), piles.end());
         int ans = 0;
         
-        while (!dq.empty()){
-            // Alice
-            dq.pop_back();
-            
-            // I
-            ans += dq.back();
-            dq.pop_back();
-            
-            // Bob
-            dq.pop_front();
-        }
+        for (int i = piles.size() / 3; i < piles.size(); i += 2)
+            ans += piles[i];
         
         return ans;
     }
