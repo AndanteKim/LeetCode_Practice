76e44211b97ae79efefffd454d9cb01c689efffc
Andrew Kim <ksb0724@utexas.edu>
2023-11-26 19:09:23 +0900

    Time: 181 ms (22.16%), Space: 84 MB (11.36%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     int largestSubmatrix(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size(), ans = 0;
+        vector<int> prevRow(n);
         
         for (int row = 0; row < m; ++row){
-            for (int col = 0; col < n; ++col){
-                if (matrix[row][col] && row > 0)
-                    matrix[row][col] += matrix[row - 1][col];
-            }
             vector<int> currRow(matrix[row].begin(), matrix[row].end());
-            sort(currRow.begin(), currRow.end(), [](int a, int b) {return a > b;});
+            for (int col = 0; col < n; ++col){
+                if (matrix[row][col])
+                    currRow[col] += prevRow[col];
+                
+            }
+            vector<int> sortedRow(currRow.begin(), currRow.end());
+            sort(sortedRow.begin(), sortedRow.end(), [](int a, int b){return a > b;});
+            for (int i = 0; i < n; ++i){
+                ans = max(ans, sortedRow[i] * (i + 1));
+            }
             
-            for (int i = 0; i < n; ++i)
-                ans = max(ans, currRow[i] * (i + 1));
+            prevRow = currRow;
         }
         
         return ans;
