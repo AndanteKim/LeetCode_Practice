ac9920041055b735a2f916c60e2b0aefdd8b8ebf
Andrew Kim <ksb0724@utexas.edu>
2023-11-27 13:45:26 +0900

    Time: 4 ms (97.08%), Space: 6.5 MB (90.94%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int knightDialer(int n) {
-        vector<vector<int>> jumps{{4,6}, {6,8}, {7,9},\
-        {4,8},{0,3,9},{},{0,1,7},{2,6},{1,3},{2,4}};
-        vector<int> prevDp(10, 1);
-        int MOD = 1'000'000'007;
+        if (n == 1)
+            return 10;
         
-        for (int remain = 1; remain < n; ++remain){
-            vector<int> dp(10);
-            
-            for (int square = 0; square < 10; ++square){
-                int curr = 0;
-                for (int nextSquare : jumps[square])
-                    curr = (curr + prevDp[nextSquare]) % MOD;
-                dp[square] = curr;
-            }
-            prevDp = dp;
+        int A = 4, B = 2, C = 2, D = 1, MOD = 1'000'000'007;
+        
+        for (int i = 0; i < n - 1; ++i){
+            int tempA = A;
+            A = ((2 * B) % MOD + (2 * C) % MOD) % MOD;
+            B = tempA;
+            int tempC = C;
+            C = (tempA + (2 * D) % MOD) % MOD;
+            D = tempC;
         }
         
         int ans = 0;
-        for (int square = 0; square < 10; ++square)
-            ans = (ans + prevDp[square]) % MOD;
-        return ans;
+        ans = (A + B) % MOD;
+        ans = (ans + C) % MOD;
+        
+        return (ans + D) % MOD;
     }
 };
