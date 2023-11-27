cadcfa669f2020913617519155fcd20c37c283b6
Andrew Kim <ksb0724@utexas.edu>
2023-11-27 13:36:39 +0900

    Time: 89 ms (98.03%), Space: 16.1 MB (98.03%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,20 +1,12 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        jumps = ((4,6), (6,8), (7,9), (4,8), (0,3,9), (),\
-                (0,1,7), (2,6), (1,3), (2,4))
+        if n == 1:
+            return 10
         
-        prev_dp, dp, MOD = [1] * 10, [0] * 10, 1_000_000_007
+        A, B, C, D = 4, 2, 2, 1
+        MOD = 10 ** 9 + 7
         
-        for remain in range(1, n):
-            dp = [0] * 10
-            for square in range(10):
-                curr = 0
-                for next_square in jumps[square]:
-                    curr = (curr + prev_dp[next_square]) % MOD
-                dp[square] = curr
-            prev_dp = dp
-        
-        ans = 0
-        for square in range(10):
-            ans = (ans + prev_dp[square]) % MOD
-        return ans
+        for _ in range(n - 1):
+            A, B, C, D = (2 * (B + C)) % MOD, A, (A + 2 * D) % MOD, C
+            
+        return (A + B + C + D) % MOD
