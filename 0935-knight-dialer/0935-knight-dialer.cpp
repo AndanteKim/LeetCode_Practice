47583987c24deb1af8f946140254830da50c8ec8
Andrew Kim <ksb0724@utexas.edu>
2023-11-27 12:54:46 +0900

    Time: 126 ms (59.65%), Space: 37.5 MB (45.18%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,30 +1,26 @@
 class Solution {
-private:
-    unordered_map<int, vector<int>> directions{{0,{4,6}}, {1,{6,8}},\
-    {2,{7,9}}, {3,{4,8}}, {4,{0,3,9}}, {5,{}}, {6,{0,1,7}},\
-    {7,{2,6}}, {8,{1,3}}, {9,{2,4}}};
-    int mod = 1'000'000'007;
-    
-    int dp(vector<vector<int>>& memo, int prev, int remain){
-        if (remain == 0)
-            return 1;
-        
-        if (memo[remain][prev] != -1)
-            return memo[remain][prev];
-        
-        int ways = 0;
-        for (int curr : directions[prev])
-            ways = (ways + dp(memo, curr, remain - 1)) % mod; 
-            
-        return memo[remain][prev] = ways;
-    }
-    
 public:
     int knightDialer(int n) {
-        vector memo(n, vector<int>(10, -1));
+        vector<vector<int>> jumps{{4,6}, {6,8}, {7,9},\
+        {4,8},{0,3,9},{},{0,1,7},{2,6},{1,3},{2,4}};
+        int MOD = 1'000'000'007;
+        vector<vector<int>> dp(n + 1, vector<int>(10));
+        
+        for (int square = 0; square < 10; ++square)
+            dp[0][square] = 1;
+        
+        for (int remain = 1; remain <= n; ++remain){
+            for (int square = 0; square < 10; ++square){
+                int curr = 0;
+                for (int nextSquare : jumps[square])
+                    curr = (curr + dp[remain - 1][nextSquare]) % MOD;
+                dp[remain][square] = curr;
+            }
+        }
+        
         int ans = 0;
-        for (int i = 0; i <= 9; ++i)
-            ans = (ans + dp(memo, i, n - 1)) % mod;
+        for (int i = 0; i < 10; ++i)
+            ans = (ans + dp[n - 1][i]) % MOD;
         
         return ans;
     }
