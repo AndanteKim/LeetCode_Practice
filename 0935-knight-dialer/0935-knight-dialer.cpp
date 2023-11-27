18eb53cf7ac8a79f50a6897a05bbe1fdff79a033
Andrew Kim <ksb0724@utexas.edu>
2023-11-27 14:21:13 +0900

    Time: 35 ms (87.72%), Space: 9.5 MB (65.20%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.cpp
+++ 0935-knight-dialer/0935-knight-dialer.cpp
@@ -1,24 +1,46 @@
 class Solution {
+private:
+    int MOD = 1'000'000'007;
+    vector<vector<long>> multiply(vector<vector<long>>& A, vector<vector<long>>& B){
+        vector<vector<long>> res(A.size(), vector<long>(B[0].size()));
+        for (int i = 0; i < A.size(); ++i){
+            for (int j = 0; j < B[0].size(); ++j){
+                for (int k = 0; k < B.size(); ++k){
+                    res[i][j] = (res[i][j] + (A[i][k] * B[k][j])) % MOD;
+                }
+            }
+        }
+        return res;
+    }
+    
 public:
     int knightDialer(int n) {
         if (n == 1)
             return 10;
         
-        int A = 4, B = 2, C = 2, D = 1, MOD = 1'000'000'007;
+        vector<vector<long>> A = {
+            {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+            {0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
+            {1, 0, 0, 1, 0, 0, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {1, 1, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 1, 0, 0, 0, 1, 0, 0, 0},
+            {0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 1, 0, 0, 0, 0, 0}
+        };
         
-        for (int i = 0; i < n - 1; ++i){
-            int tempA = A;
-            A = ((2 * B) % MOD + (2 * C) % MOD) % MOD;
-            B = tempA;
-            int tempC = C;
-            C = (tempA + (2 * D) % MOD) % MOD;
-            D = tempC;
+        --n;
+        vector<vector<long>> v{{1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+        while (n > 0){
+            if (n & 1)
+                v = multiply(v, A);
+            
+            A = multiply(A, A);
+            n >>= 1;
         }
         
-        int ans = 0;
-        ans = (A + B) % MOD;
-        ans = (ans + C) % MOD;
-        
-        return (ans + D) % MOD;
+        return (accumulate(v[0].begin(), v[0].end(), 0L) % MOD);
     }
 };
