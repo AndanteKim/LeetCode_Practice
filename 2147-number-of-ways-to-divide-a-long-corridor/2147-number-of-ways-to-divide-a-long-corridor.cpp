659af7df9df76d40ebc69c4990b0cfbf0cc29f6c
Andrew Kim <ksb0724@utexas.edu>
2023-11-28 12:13:55 +0900

    Time: 1242 ms (5.14%), Space: 395.6 MB (12.57%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,33 +1,23 @@
 class Solution {
-private:
-    int n, MOD = 1'000'000'007;
-    int count(int i, int seats, vector<vector<int>>& memo, string& corridor){
-        if (i == n)
-            return seats == 2? 1:0;
-        if (memo[i][seats] != -1)
-            return memo[i][seats];
-        
-        int ans = 0;
-        if (seats == 2){
-            if (corridor[i] == 'S')
-                ans = count(i + 1, 1, memo, corridor);
-            else
-                ans = (count(i + 1, 0, memo, corridor) + count(i + 1, 2, memo, corridor)) % MOD;
-        }
-        else{
-            if (corridor[i] == 'S')
-                ans = count(i + 1, seats + 1, memo, corridor);
-            else
-                ans = count(i + 1, seats, memo, corridor);
-        }
-        
-        return memo[i][seats] = ans;
-    }
-    
 public:
     int numberOfWays(string corridor) {
-        n = corridor.size();
-        vector memo(n, vector<int>(3, -1));
-        return count(0, 0, memo, corridor);
+        const int n = corridor.size(), MOD = 1e9 + 7;
+        vector<vector<int>> count(n + 1, vector<int>(3));
+        count[n][2] = 1;
+        
+        for (int i = n - 1; i >= 0; --i){
+            if (corridor[i] == 'S'){
+                count[i][0] = count[i + 1][1];
+                count[i][1] = count[i + 1][2];
+                count[i][2] = count[i + 1][1];
+            }
+            else{
+                count[i][0] = count[i + 1][0];
+                count[i][1] = count[i + 1][1];
+                count[i][2] = (count[i + 1][0] + count[i + 1][2]) % MOD;
+            }
+        }
+        
+        return count[0][0];
     }
 };
