f5cc5ec243ca20908bdf3690476aceb0ae01660b
Andrew Kim <ksb0724@utexas.edu>
2023-11-28 11:55:02 +0900

    Time: 1937 ms (7.14%), Space: 30.6 MB (12.86%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,27 +1,20 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        # Count the number of ways to divide from "index" to the last index with "seats" number of "S" in the current section
-        def count(i: int, seats: int) -> int:
-            if i == len(corridor):
-                return 1 if seats == 2 else 0
-            
-            if memo[i][seats] != -1:
-                return memo[i][seats]
-            
-            if seats == 2:
-                if corridor[i] == "S":
-                    ans = count(i + 1, 1)
-                else:
-                    ans = (count(i + 1, 0) + count(i + 1, 2)) % MOD
-            else:
-                if corridor[i] == "S":
-                    ans = count(i + 1, seats + 1)
-                else:
-                    ans = count(i + 1, seats)
-            
-            memo[i][seats] = ans
-            return memo[i][seats]
+        MOD, n = 1_000_000_007, len(corridor)
         
-        MOD = 1_000_000_007
-        memo = [[-1] * 3 for _ in range(len(corridor))]
-        return count(0, 0)
+        # Base cases
+        count = [[0] * 3 for _ in range(n + 1)]
+        count[n][2] = 1
+        
+        for i in range(n - 1, -1, -1):
+            if corridor[i] == 'S':
+                count[i][0] = count[i + 1][1]
+                count[i][1] = count[i + 1][2]
+                count[i][2] = count[i + 1][1]
+            
+            else:
+                count[i][0] = count[i + 1][0]
+                count[i][1] = count[i + 1][1]
+                count[i][2] = (count[i + 1][0] + count[i + 1][2]) % MOD
+        
+        return count[0][0]
