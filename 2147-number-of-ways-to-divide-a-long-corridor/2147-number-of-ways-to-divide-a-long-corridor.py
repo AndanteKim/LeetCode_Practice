9c54610ec86d7b32802c2c68302cfb403ad047ea
Andrew Kim <ksb0724@utexas.edu>
2023-11-28 12:16:30 +0900

    Time: 813 ms (11.43%), Space: 17.4 MB (52.86%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,20 +1,18 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        MOD, n = 1_000_000_007, len(corridor)
-        
-        # Base cases
-        count = [[0] * 3 for _ in range(n + 1)]
-        count[n][2] = 1
+        n, prev_dp, MOD = len(corridor), [0, 0, 1], 1_000_000_007
         
         for i in range(n - 1, -1, -1):
+            dp = [0, 0, 0]
             if corridor[i] == 'S':
-                count[i][0] = count[i + 1][1]
-                count[i][1] = count[i + 1][2]
-                count[i][2] = count[i + 1][1]
-            
+                dp[0] = prev_dp[1]
+                dp[1] = prev_dp[2]
+                dp[2] = prev_dp[1]
             else:
-                count[i][0] = count[i + 1][0]
-                count[i][1] = count[i + 1][1]
-                count[i][2] = (count[i + 1][0] + count[i + 1][2]) % MOD
+                dp[0] = prev_dp[0]
+                dp[1] = prev_dp[1]
+                dp[2] = (prev_dp[0] + prev_dp[2]) % MOD
+            
+            prev_dp = dp
         
-        return count[0][0]
+        return prev_dp[0]
