d76952b57b69b467d1a9e2578a66e3ef7791f014
Andrew Kim <ksb0724@utexas.edu>
2023-11-28 15:26:09 +0900

    Time: 105 ms (98.29%), Space: 30.9 MB (97.71%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     int numberOfWays(string corridor) {
-        const int n = corridor.size(), MOD = 1e9 + 7;
-        vector<int> indices;
+        const int MOD = 1'000'000'007, n = corridor.size();
+        int seats = 0, prevPairLast = -1;
+        long count = 1;
         
         for (int i = 0; i < n; ++i){
-            if (corridor[i] == 'S')
-                indices.push_back(i);
+            if (corridor[i] == 'S'){
+                ++seats;
+                if (seats == 2){
+                    seats = 0;
+                    prevPairLast = i;
+                }
+                else if (seats == 1 && prevPairLast != -1){
+                    count = (count * (i - prevPairLast)) % MOD;
+                }
+            }
         }
         
-        // When division is not possible
-        if (indices.empty() || indices.size() % 2 == 1)
+        // If odd seats, or zero seats
+        if (seats == 1 || prevPairLast == -1)
             return 0;
-        
-        // Total number of ways
-        long count = 1;
-        int prevFirstLast = 1, currSecondFirst = 2;
-        
-        // Take the product of non-paired neighbors
-        while (currSecondFirst < indices.size()){
-            count = (count * (indices[currSecondFirst] - indices[prevFirstLast])) % MOD;
-            prevFirstLast += 2;
-            currSecondFirst += 2;
-        }
-        
-        return count;
+        return (int)count;
     }
 };
