9e313c9302d3026cd9cec1b41dd24480c65fe032
Andrew Kim <ksb0724@utexas.edu>
2023-11-30 15:19:22 +0900

    Time: 43 ms (44.16%), Space: 16.2 MB (78.57%) - LeetHub

--- 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.py
+++ 1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero.py
@@ -1,11 +1,12 @@
 class Solution:
     def minimumOneBitOperations(self, n: int) -> int:
-        if n == 0:
-            return 0
+        ans, k, mask = 0, 0, 1
         
-        k, curr = 0, 1
-        while curr * 2 <= n:
-            curr *= 2
+        while mask <= n:
+            if n & mask:
+                ans = 2 ** (k + 1) - 1 - ans
+            
+            mask <<= 1
             k += 1
         
-        return 2 ** (k + 1) - 1 - self.minimumOneBitOperations(n ^ curr)
+        return ans
