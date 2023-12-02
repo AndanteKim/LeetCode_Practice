2c77943030dddd93ac3f074c841fac927b9825f2
Andrew Kim <ksb0724@utexas.edu>
2023-12-02 15:56:53 +0900

    Time: 3 ms (71.11%), Space: 11.6 MB (95.22%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -1,17 +1,23 @@
 class Solution {
-private:
-    string combine(vector<string>& word){
-        string concatenated = "";
-        for (string& part : word){
-            for (char& c : part)
-                concatenated.push_back(c);
-        }
-        
-        return concatenated;
-    }
-    
 public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        return combine(word1) == combine(word2);
+        int word1Pointer = 0, word2Pointer = 0, string1Pointer = 0, string2Pointer = 0;
+        
+        while (word1Pointer < word1.size() && word2Pointer < word2.size()){
+            if (word1[word1Pointer][string1Pointer++] != word2[word2Pointer][string2Pointer++])
+                return false;
+            
+            if (word1[word1Pointer].size() == string1Pointer){
+                ++word1Pointer;
+                string1Pointer = 0;
+            }
+            
+            if (word2[word2Pointer].size() == string2Pointer){
+                ++word2Pointer;
+                string2Pointer = 0;
+            }
+        }
+        
+        return word1Pointer == word1.size() && word2Pointer == word2.size();
     }
 };
