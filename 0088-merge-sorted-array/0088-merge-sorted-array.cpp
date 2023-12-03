3d92e2c215522dc26e65b9daed1fc30d6d7a9c7f
Andrew Kim <ksb0724@utexas.edu>
2023-12-04 04:21:19 +0900

    Time: 0 ms (100.00%), Space: 9.3 MB (75.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,8 +1,16 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        for (int i = 0; i < nums2.size(); ++i)
-            nums1[m + i] = nums2[i];
-        sort(nums1.begin(), nums1.end());
+        vector<int> nums1Copy(nums1.begin(), nums1.begin() + m);
+        int p1 = 0, p2 = 0;
+        
+        for (int p = 0; p < m + n; ++p){
+            if (p2 >= n || (p1 < m && nums1Copy[p1] <= nums2[p2])){
+                nums1[p] = nums1Copy[p1++];
+            }
+            else{
+                nums1[p] = nums2[p2++];
+            }
+        }
     }
 };
