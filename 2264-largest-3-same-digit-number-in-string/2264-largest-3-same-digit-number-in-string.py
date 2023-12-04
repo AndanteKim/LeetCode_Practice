6e0c65cc8a8506cf663be8f9dc48b422ae794fe4
Andrew Kim <ksb0724@utexas.edu>
2023-12-04 18:18:24 +0900

    Time: 39 ms (75.56%), Space: 16.2 MB (85.62%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
@@ -1,19 +1,20 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        ans, matching = "", {"000":0,"111":111,"222":222, "333":333,\
-                            "444":444,"555":555,"666":666,\
-                            "777":777,"888":888,"999":999}
-        i, j = 0, 0
-        while j < len(num):
-            if j - i == 2 and num[i] == num[j]:
-                curr = num[i:j+1]
-                if ans == "" or matching[curr] > matching[ans]:
-                    ans = curr
-            
-            if num[i] == num[j]:
-                j += 1
-            else:
-                i = j
-            
-        
-        return ans
+        same_digit_numbers = ["999", "888", "777", "666", "555", "444", "333", "222", "111", "000"]
+
+        # Check whether the 'num' string contains the 'same_digit_number' string or not.
+        def contains(same_digit_number):
+            for index in range(len(num) - 2):
+                if num[index] == same_digit_number[0] and \
+                   num[index + 1] == same_digit_number[1] and \
+                   num[index + 2] == same_digit_number[2]:
+                    return True
+            return False
+
+        # Iterate on all 'same_digit_numbers' and check if the string 'num' contains it.
+        for same_digit_number in same_digit_numbers:
+            if contains(same_digit_number):
+                # Return the current 'same_digit_number'.
+                return same_digit_number
+        # No 3 consecutive same digits are present in the string 'num'.
+        return ""
