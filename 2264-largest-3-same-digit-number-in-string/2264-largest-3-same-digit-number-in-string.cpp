5882991572e068264f3441825f87602c44aaf057
Andrew Kim <ksb0724@utexas.edu>
2023-12-04 18:17:53 +0900

    Time: 4 ms (47.83%), Space: 6.9 MB (59.71%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,25 +1,22 @@
 class Solution {
-public:
-    string largestGoodInteger(string num) {
-        string ans = "", curr;
-        unordered_map<string, int> matching{{"000",0}, {"111",111},\
-        {"222",222}, {"333",333}, {"444",444}, {"555",555},\
-        {"666",666}, {"777",777}, {"888",888}, {"999",999}};
-        int i = 0, j = 0;
-        
-        while (j < num.size()){
-            if (j - i == 2 && num[i] == num[j]){
-                curr = num.substr(i, 3);
-                if (ans == "" || matching[curr] > matching[ans])
-                    ans = curr;
-            }
-            
-            if (num[i] == num[j])
-                ++j;
-            else
-                i = j;
+private:
+    vector<string> sameDigitNumbers{"999","888","777","666","555","444","333","222","111","000"};
+    bool contains(string& s, string& num){
+        for (int i = 0; i < num.size() - 2; ++i){
+            if (num[i] == s[0] && num[i + 1] == s[1] && num[i + 2] == s[2])
+                return true;
         }
         
-        return ans;
+        return false;
+    }
+    
+public:
+    string largestGoodInteger(string num) {
+        for (string& s:sameDigitNumbers){
+            if (contains(s, num))
+                return s;
+        }
+        
+        return "";
     }
 };
