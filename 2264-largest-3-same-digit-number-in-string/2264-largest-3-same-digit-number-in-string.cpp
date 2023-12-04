c2c19f1b0b73c4ac6108c36ddf1b5613c77b5cf8
Andrew Kim <ksb0724@utexas.edu>
2023-12-04 18:29:17 +0900

    Time: 2 ms (53.04%), Space: 6.9 MB (31.59%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.cpp
@@ -1,22 +1,12 @@
 class Solution {
-private:
-    vector<string> sameDigitNumbers{"999","888","777","666","555","444","333","222","111","000"};
-    bool contains(string& s, string& num){
-        for (int i = 0; i < num.size() - 2; ++i){
-            if (num[i] == s[0] && num[i + 1] == s[1] && num[i + 2] == s[2])
-                return true;
-        }
-        
-        return false;
-    }
-    
 public:
     string largestGoodInteger(string num) {
-        for (string& s:sameDigitNumbers){
-            if (contains(s, num))
-                return s;
-        }
+        char maxDigit = '\0';
         
-        return "";
+        for (int i = 0; i < num.size(); ++i)
+            if (num[i] == num[i + 1] && num[i + 1] == num[i + 2])
+                maxDigit = max(num[i], maxDigit);
+        
+        return maxDigit == '\0'? "":string(3, maxDigit);
     }
 };
