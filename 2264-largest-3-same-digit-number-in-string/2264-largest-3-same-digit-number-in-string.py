b4f59336c63339ab8969e4931889352be4b49328
Andrew Kim <ksb0724@utexas.edu>
2023-12-04 18:23:46 +0900

    Time: 45 ms (44.25%), Space: 16.3 MB (59.27%) - LeetHub

--- 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
+++ 2264-largest-3-same-digit-number-in-string/2264-largest-3-same-digit-number-in-string.py
@@ -1,20 +1,9 @@
 class Solution:
     def largestGoodInteger(self, num: str) -> str:
-        same_digit_numbers = ["999", "888", "777", "666", "555", "444", "333", "222", "111", "000"]
-
-        # Check whether the 'num' string contains the 'same_digit_number' string or not.
-        def contains(same_digit_number):
-            for index in range(len(num) - 2):
-                if num[index] == same_digit_number[0] and \
-                   num[index + 1] == same_digit_number[1] and \
-                   num[index + 2] == same_digit_number[2]:
-                    return True
-            return False
-
-        # Iterate on all 'same_digit_numbers' and check if the string 'num' contains it.
-        for same_digit_number in same_digit_numbers:
-            if contains(same_digit_number):
-                # Return the current 'same_digit_number'.
-                return same_digit_number
-        # No 3 consecutive same digits are present in the string 'num'.
-        return ""
+        max_digit = '\0'
+        for index in range(len(num) - 2):
+            if num[index] == num[index + 1] == num[index + 2]:
+                max_digit = max(max_digit, num[index])
+        
+        return '' if max_digit == '\0' else max_digit * 3
+            
