87a96af3a93b52617850ca21baf01402e80e9a7b
Andrew Kim <ksb0724@utexas.edu>
2023-12-05 17:35:52 +0900

    Time: 47 ms (97.18%), Space: 16.2 MB (65.57%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,27 +1,25 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        # Initialize the counter and the array index
-        i, count = 1, 1
+        # Initialize the counte and the second pointer
+        j, count = 1, 1
         
-        # Start from the second element of the array and process elements one by one
-        while i < len(nums):
-            # If the current element is duplicate, increment the count
+        # Start from the second element of the array and process element one by one
+        for i in range(1, len(nums)):
+            # If the current element is a duplicate,
+            # increment the count
             if nums[i] == nums[i - 1]:
                 count += 1
-                
-                # If the count is more than 2, this is an unwanted duplicate element
-                # and hence we remove it from the array
-                if count > 2:
-                    nums.pop(i)
-                    
-                    # Note that we have to decrement the array index value to keep it consistent
-                    # with the size of the array
-                    i -= 1
             else:
-                # Reset the count since we encountered a different element than
-                # the previous one
+                # Reset the count since we encountered a different element
+                # than the previous one
                 count = 1
-            
-            # Move on to the next element in the array
-            i += 1
-        return len(nums)
+        
+            # For a count <= 2, we copy the element over thus
+            # overwriting the element at index "j" in the array
+            if count <= 2:
+                nums[j] = nums[i]
+                j += 1
+        
+        return j
+        
+        
