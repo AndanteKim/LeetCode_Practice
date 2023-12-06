5492cf0746997715d7f24f8b228d3ac714a22fde
Andrew Kim <ksb0724@utexas.edu>
2023-12-06 17:44:34 +0900

    Time: 2 ms (49.74%), Space: 6.3 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int i = 0, ans = 0, start = 1, end = 7;
+        int ans = 0, monday = 1;
         
-        while (i < n){
-            if (i + 7 < n)
-                ans += 7 * (start + end) / 2;
-            else{
-                ans += (n - i) * (2 * start + (n - i - 1)) / 2;
-                break;
-            }
-            i += 7;
-            ++start;
-            ++end;
+        while (n > 0){
+            for (int i = 0; i < min(n, 7); ++i)
+                ans += monday + i;
+            n -= 7;
+            ++monday;
         }
+        
         return ans;
     }
 };
