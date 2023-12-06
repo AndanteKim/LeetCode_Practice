6b189a550b50e19a6661abc0cdf570764dc91c1c
Andrew Kim <ksb0724@utexas.edu>
2023-12-06 17:59:19 +0900

    Time: 30 ms (97.24%), Space: 16.2 MB (73.58%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,11 +1,13 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        ans, monday = 0, 1
+        k, F = n // 7, 28
+        L = 28 + (k - 1) * 7
         
-        while n > 0:
-            for day in range(min(n, 7)):
-                ans += monday + day
-            
-            n -= 7
-            monday += 1
-        return ans
+        arithmetic_sum = k * (F + L) // 2
+        monday, final_week = 1 + k, 0
+        
+        for day in range(n % 7):
+            final_week += monday + day
+        return arithmetic_sum + final_week
+        
+        
