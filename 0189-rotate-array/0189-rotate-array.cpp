db4d4e4214724e7d7bec006e18a4d067fe49559c
Andrew Kim <ksb0724@utexas.edu>
2023-12-07 00:10:35 +0900

    Time: 25 ms (35.10%), Space: 25.9 MB (38.44%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        deque<int> dq(nums.begin(), nums.end());
-        for (int i = 0; i < k; ++i){
-            int elem = dq.back();
-            dq.pop_back();
-            dq.push_front(elem);
-        }
-        
-        for (int i = 0; i < nums.size(); ++i){
-            nums[i] = dq.front();
-            dq.pop_front();
-        }
+        int n = nums.size();
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i)
+            a[(i + k) % n] = nums[i];
+        nums = a;
     }
 };
