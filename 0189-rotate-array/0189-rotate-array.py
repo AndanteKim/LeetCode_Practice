da87a98187366e6a14b9f6bde9f711932a5c563e
Andrew Kim <ksb0724@utexas.edu>
2023-12-07 00:52:11 +0900

    Time: 195 ms (33.76%), Space: 27.7 MB (51.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -4,7 +4,17 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-        a = [0] * n
-        for i in range(n):
-            a[(i + k) % n] = nums[i]
-        nums[:] = a
+        k %= n
+        
+        start = count = 0
+        while count < n:
+            current, prev = start, nums[start]
+            while True:
+                next_idx = (current + k) % n
+                nums[next_idx], prev = prev, nums[next_idx]
+                current = next_idx
+                count += 1
+                
+                if start == current:
+                    break
+            start += 1
