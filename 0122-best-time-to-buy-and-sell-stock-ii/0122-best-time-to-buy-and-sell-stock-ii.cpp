9be552720df9b9212b5deffd87a7f7cffdeee309
Andrew Kim <ksb0724@utexas.edu>
2023-12-08 03:21:20 +0900

    Time: 0 ms (100.00%), Space: 13.5 MB (42.29%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<int> dp(n);
-        for (int i = 1; i < n; ++i)
-            dp[i] = dp[i - 1] + max(0, prices[i] - prices[i - 1]);
-        return dp.back();
+        int i = 0, valley = prices[0], peak = prices[0];
+        int n = prices.size(), ans = 0;
+        
+        for (int i = 0; i < n - 1;){
+            while (i < n - 1 && prices[i] >= prices[i + 1])
+                ++i;
+            valley = prices[i];
+            while (i < n - 1 && prices[i] <= prices[i + 1])
+                ++i;
+            peak = prices[i];
+            ans += peak - valley;
+        }
+        
+        return ans;
     }
 };
