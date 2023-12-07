311f04cc1bec6be4565b7e0b39def27cbbec09c4
Andrew Kim <ksb0724@utexas.edu>
2023-12-08 00:19:23 +0900

    Time: 4 ms (71.43%), Space: 13.6 MB (32.49%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,27 +1,10 @@
 class Solution {
-private:
-    int n;
-    int dp(int i, int bought, vector<vector<int>>& memo, vector<int>& prices){
-        if (i >= n)
-            return 0;
-        if (memo[i][bought] != -1)
-            return memo[i][bought];
-        
-        int profit = 0;
-        if (bought){
-            profit += max(dp(i + 1, 1, memo, prices), prices[i] + dp(i + 1, 0, memo, prices));
-        }
-        else{
-            profit += max(-prices[i] + dp(i + 1, 1, memo, prices), dp(i + 1, 0, memo, prices));
-        }
-        
-        return memo[i][bought] = profit;
-    }
-    
 public:
     int maxProfit(vector<int>& prices) {
-        n = prices.size();
-        vector memo(n, vector<int>(2, -1));
-        return dp(0, 0, memo, prices);
+        int n = prices.size();
+        vector<int> dp(n);
+        for (int i = 1; i < n; ++i)
+            dp[i] = dp[i - 1] + max(0, prices[i] - prices[i - 1]);
+        return dp.back();
     }
 };
