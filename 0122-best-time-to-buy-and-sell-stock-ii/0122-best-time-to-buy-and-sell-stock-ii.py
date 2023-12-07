8cb7ae4bc34609e1e373de45cf54e4dc15ccad97
Andrew Kim <ksb0724@utexas.edu>
2023-12-08 00:15:39 +0900

    Time: 60 ms (80.28%), Space: 17.8 MB (39.58%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        @lru_cache(maxsize = None)
-        def dp(i: int, bought: bool) -> int:
-            if i >= n:
-                return 0
-            
-            profits = 0
-            if bought:
-                profits += max(dp(i + 1, True), prices[i] + dp(i + 1, False))
-            else:
-                profits += max(-prices[i] + dp(i + 1, True), dp(i + 1, False))
-                
-            return profits
-        
         n = len(prices)
-        return dp(0, False)
+        dp = [0] * n
+        
+        for i in range(1, n):
+            dp[i] = dp[i - 1] + max(0, prices[i] - prices[i - 1])
+            
+        return dp[-1]
+        
