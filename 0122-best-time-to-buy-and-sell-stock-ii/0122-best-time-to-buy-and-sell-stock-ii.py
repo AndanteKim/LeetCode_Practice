b60ff8306eb2bbf92af8555d59813c162844a798
Andrew Kim <ksb0724@utexas.edu>
2023-12-08 00:17:05 +0900

    Time: 7 ms (38.46%), Space: 18 MB (6.18%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,27 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        n = len(prices)
-        dp = [0] * n
+class Solution {
+private:
+    int n;
+    int dp(int i, int bought, vector<vector<int>>& memo, vector<int>& prices){
+        if (i >= n)
+            return 0;
+        if (memo[i][bought] != -1)
+            return memo[i][bought];
         
-        for i in range(1, n):
-            dp[i] = dp[i - 1] + max(0, prices[i] - prices[i - 1])
-            
-        return dp[-1]
+        int profit = 0;
+        if (bought){
+            profit += max(dp(i + 1, 1, memo, prices), prices[i] + dp(i + 1, 0, memo, prices));
+        }
+        else{
+            profit += max(-prices[i] + dp(i + 1, 1, memo, prices), dp(i + 1, 0, memo, prices));
+        }
         
+        return memo[i][bought] = profit;
+    }
+    
+public:
+    int maxProfit(vector<int>& prices) {
+        n = prices.size();
+        vector memo(n, vector<int>(2, -1));
+        return dp(0, 0, memo, prices);
+    }
+};
