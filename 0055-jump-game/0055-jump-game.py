0822bd5bff2cfae4cef8434a7d50131f30864344
Andrew Kim <ksb0724@utexas.edu>
2023-12-10 02:52:27 +0900

    Time: 9025 ms (5.01%), Space: 34.3 MB (5.01%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,15 +1,18 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        @lru_cache(maxsize = None)
-        def dp(pos: int) -> bool:
-            if pos >= dest:
-                return True
+        def can_jump_from_position(i: int) -> bool:
+            if memo[i] != -1:
+                return True if memo[i] == 1 else False
             
-            for i in range(1, nums[pos] + 1):
-                if dp(pos + i):
+            furthest_jump = min(n - 1, nums[i] + i)
+            for nxt in range(i + 1, furthest_jump + 1):
+                if can_jump_from_position(nxt):
+                    memo[i] = 1
                     return True
-            
+            memo[i] = 0
             return False
         
-        dest = len(nums) - 1
-        return dp(0)
+        good, bad, unknown, n = 1, 0, -1, len(nums)
+        memo = [unknown] * n
+        memo[n - 1] = good
+        return can_jump_from_position(0)
