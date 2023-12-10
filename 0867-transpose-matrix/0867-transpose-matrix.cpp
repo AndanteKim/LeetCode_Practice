86360b1fdda9bb7d9cba11db763854d136a9e4a3
Andrew Kim <ksb0724@utexas.edu>
2023-12-10 15:58:08 +0900

    Time: 7 ms (87.64%), Space: 10.8 MB (84.60%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -3,9 +3,9 @@ public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
         vector ans(matrix[0].size(), vector<int>(matrix.size()));
         
-        for (int i = 0; i < matrix[0].size(); ++i){
-            for (int j = 0; j < matrix.size(); ++j)
-                ans[i][j] = matrix[j][i];
+        for (int i = 0; i < matrix.size(); ++i){
+            for (int j = 0; j < matrix[0].size(); ++j)
+                ans[j][i] = matrix[i][j];
         }
         return ans;
     }
