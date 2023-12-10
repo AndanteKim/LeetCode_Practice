3042f64ff8b931398a3a7b567741f2edc8e5fc3a
Andrew Kim <ksb0724@utexas.edu>
2023-12-10 16:53:56 +0900

    Time: 5 ms (35.40%), Space: 8.9 MB (72.72%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        int highest = *max_element(citations.begin(), citations.end());
-        vector<int> counting(highest + 1);
+        sort(citations.begin(), citations.end());
+        int n = citations.size(), i = 0;
         
-        for (int citation : citations){
-            for (int i = 0; i <= citation; ++i)
-                ++counting[i];
-        }
-        
-        for (int i = highest; i >= 0; --i)
-            if(counting[i] >= i)
-                return i;
-        return 0;
+        while (i < n && citations[n - 1 - i] > i)
+            ++i;
+        return i;
     }
 };
