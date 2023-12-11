95d3447bacefbd73c6755b7b3d963c52a732e315
Andrew Kim <ksb0724@utexas.edu>
2023-12-11 17:10:19 +0900

    Time: 85 ms (83.51%), Space: 17.7 MB (59.65%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.py
@@ -1,13 +1,13 @@
 class Solution:
     def findSpecialInteger(self, arr: List[int]) -> int:
-        n, cnt, prev = len(arr), 0, -1
+        n = len(arr)
+        candidates = [arr[n // 4], arr[n // 2], arr[(3 * n) // 4]]
+        target = n / 4
         
-        for i in range(n):
-            if prev == -1 or arr[i] == prev:
-                cnt += 1
-            else:
-                cnt = 1
-            prev = arr[i]
-            if cnt / n > 0.25:
-                return arr[i]
+        for candidate in candidates:
+            left = bisect_left(arr, candidate)
+            right = bisect_right(arr, candidate)
+            if right - left > target:
+                return candidate
+        
         return -1
