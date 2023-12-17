a8b03c78d81da95e46d6fd32bcdd586bd13507e4
Andrew Kim <ksb0724@utexas.edu>
2023-12-17 19:00:36 +0900

    Time: 954 ms (30.36%), Space: 46.4 MB (89.88%) - LeetHub

---
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -0,0 +1,26 @@
+from sortedcontainers import SortedList
+from collections import defaultdict
+
+class FoodRatings:
+
+    def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
+        self.food_data = defaultdict(tuple)
+        self.cuisine_data = defaultdict(SortedList)
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.food_data[food] = (cuisine, rating)
+            self.cuisine_data[cuisine].add((-rating, food))
+
+    def changeRating(self, food: str, newRating: int) -> None:
+        cuisine, rating = self.food_data[food]
+        self.food_data[food] = cuisine, newRating
+        self.cuisine_data[cuisine].remove((-rating, food))
+        self.cuisine_data[cuisine].add((-newRating, food))
+
+    def highestRated(self, cuisine: str) -> str:
+        return self.cuisine_data[cuisine][0][1]
+
+
+# Your FoodRatings object will be instantiated and called as such:
+# obj = FoodRatings(foods, cuisines, ratings)
+# obj.changeRating(food,newRating)
+# param_2 = obj.highestRated(cuisine)
