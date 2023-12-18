8c2cb684aade4a57ebcdeafe58ecedd23558b44b
Andrew Kim <ksb0724@utexas.edu>
2023-12-18 19:57:14 +0900

    Time: 175 ms (5.72%), Space: 17.8 MB (8.58%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.py
@@ -1,29 +1,19 @@
 class Solution:
     def maxProductDifference(self, nums: List[int]) -> int:
-        def computing(i: int, count: int) -> int:
-            product = 1
-            if i == 0:
-                while i < len(count_sort) and count < 2:
-                    if count_sort[i]:
-                        product *= i
-                        count_sort[i] -= 1
-                        count += 1
-                    else:
-                        i += 1
-            else:
-                while i >= 0 and count < 2:
-                    if count_sort[i]:
-                        product *= i
-                        count_sort[i] -= 1
-                        count += 1
-                    else:
-                        i -= 1
-            return product
+        biggest, second_biggest = 0, 0
+        smallest, second_smallest = float('inf'), float('inf')
         
-        max_num = max(nums)
-        count_sort = [0] * (max_num + 1)
         for num in nums:
-            count_sort[num] += 1
-        
-        max_pair, min_pair = computing(max_num, 0), computing(0, 0)
-        return max_pair - min_pair
+            if num > biggest:
+                second_biggest = biggest
+                biggest = num
+            else:
+                second_biggest = max(second_biggest, num)
+            
+            if num < smallest:
+                second_smallest = smallest
+                smallest = num
+            else:
+                second_smallest = min(second_smallest, num)
+                
+        return biggest * second_biggest - smallest * second_smallest
