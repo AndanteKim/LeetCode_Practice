70cae1b764398ce0dab2927e42788d66fb9855f8
Andrew Kim <ksb0724@utexas.edu>
2023-12-18 19:55:04 +0900

    Time: 20 ms (74.89%), Space: 23.6 MB (9.65%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.cpp
@@ -1,8 +1,39 @@
 class Solution {
+private:
+    int computing(int start, vector<int>& countSort){
+        int count = 0, product = 1;
+        
+        if (start == 0){
+            for (int i = 0; i < countSort.size() && count < 2; ++i){
+                while (countSort[i] && count < 2){
+                    product *= i;
+                    --countSort[i];
+                    ++count;
+                }
+            }
+        }
+        else{
+            for (int i = countSort.size() - 1; i >= 1 && count < 2; --i){
+                while (countSort[i] && count < 2){
+                    product *= i;
+                    --countSort[i];
+                    ++count;
+                }
+            }
+        }
+        
+        return product;
+    }
+    
 public:
     int maxProductDifference(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        return nums[n - 1] * nums[n - 2] - nums[0] * nums[1];
+        int maxNum = *max_element(nums.begin(), nums.end());
+        vector<int> countSort(maxNum + 1);
+        for (int num : nums)
+            ++countSort[num];
+        int maxPair = computing(maxNum, countSort), minPair = computing(0, countSort);
+        cout << maxPair << endl;
+        cout << minPair << endl;
+        return maxPair - minPair;
     }
 };
