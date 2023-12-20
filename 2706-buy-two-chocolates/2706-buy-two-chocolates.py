16bcc9dadc2559ffc8a9b173a4314d4c2429bc4a
Andrew Kim <ksb0724@utexas.edu>
2023-12-20 23:52:01 +0900

    Time: 65 ms (56.98%), Space: 17.3 MB (23.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.py
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.py
@@ -1,5 +1,18 @@
 class Solution:
     def buyChoco(self, prices: List[int], money: int) -> int:
-        prices.sort(reverse = True)
-        change = money - prices.pop() - prices.pop()
-        return money if change < 0 else change
+        max_price, ans, count = max(prices), money, 0
+        count_sorting = [0] * (max_price + 1)
+        for price in prices:
+            count_sorting[price] += 1
+            
+        for price in range(max_price + 1):
+            if count >= 2 or price > ans:
+                break
+            
+            while count < 2 and count_sorting[price] > 0:
+                ans -= price
+                count += 1
+                count_sorting[price] -= 1
+        
+        return money if count != 2 or ans < 0 else ans 
+        
