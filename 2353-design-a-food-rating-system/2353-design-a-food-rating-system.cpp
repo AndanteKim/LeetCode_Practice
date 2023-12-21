eb33bed73f0500b32c8866deb993f1d2f7262c65
Andrew Kim <ksb0724@utexas.edu>
2023-12-22 00:54:00 +0900

    Time: 434 ms (32.72%), Space: 166.9 MB (32.77%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.cpp
@@ -1,60 +1,43 @@
-class Food{
-public:
-    string foodName;
-    int foodRating;
-    Food(int foodRating, string& foodName){
-        this -> foodRating = foodRating;
-        this -> foodName = foodName;
-    }
-    
-    // Overload the less than operator for comparison
-    bool operator<(const Food& other) const{
-        // If food ratings are same sort on the basis of their name. (lexicographically smaller name food will be on top)
-        if (foodRating == other.foodRating)
-            return foodName > other.foodName;
-        return foodRating < other.foodRating;
-    }
-};
-
-
 class FoodRatings {
 private:
+    // Map food with its raating
     unordered_map<string, int> foodRatingMap;
+    // Map food with the cuisine it belongs to
     unordered_map<string, string> foodCuisineMap;
-    unordered_map<string, priority_queue<Food>> cuisineFoodMap;
+    
+    // Store all food of cuisine in set (to sort them on ratings/name)
+    // Set element -> Pair: (-1 * foodRating, foodName)
+    map<string, set<pair<int, string>>> cuisineFoodMap;
     
 public:
     FoodRatings(vector<string>& foods, vector<string>& cuisines, vector<int>& ratings) {
         for (int i = 0; i < foods.size(); ++i){
-            // Store 'rating' and 'cuisine' of current 'food' in 'foodRatingMap' and 'foodCuisineMap' maps
+            // Store 'rating' and 'cuisine' of current 'food' in 'foodRatingMap' and 'foodCuisineMap' maps.
             foodRatingMap[foods[i]] = ratings[i];
             foodCuisineMap[foods[i]] = cuisines[i];
-            // Insert the '(rating, name)' element in current cuisine's priority queue.
-            cuisineFoodMap[cuisines[i]].push(Food(ratings[i], foods[i]));
+            // Insert the '(-1 * rating, name)' element in current cuisine's set.
+            cuisineFoodMap[cuisines[i]].insert({-ratings[i], foods[i]});
         }
     }
     
     void changeRating(string food, int newRating) {
+        // Fetch cuisine name for food
+        string cuisineName = foodCuisineMap[food];
+        
+        // Find and delete the element from the respective cuisine's set
+        auto oldElementIterator = cuisineFoodMap[cuisineName].find({-foodRatingMap[food], food});
+        cuisineFoodMap[cuisineName].erase(oldElementIterator);
+        
         // Update food's rating in 'foodRating' map
         foodRatingMap[food] = newRating;
-        // Insert the '(new rating, name)' element in current cuisine's priority queue.
-        string cuisineName = foodCuisineMap[food];
-        cuisineFoodMap[cuisineName].push(Food(newRating, food));
+        
+        // Insert the '(-1 * new rating, name)' element in respective cuisine' set
+        cuisineFoodMap[cuisineName].insert({-newRating, food});
     }
     
     string highestRated(string cuisine) {
-        // Get the highest rated 'food' of 'cuisine'.
-        Food highestRated = cuisineFoodMap[cuisine].top();
-        
-        // If the lastest rating of 'food' doesn't match with 'rating' on which it was sorted in priority queue,
-        // then we discard this element of the priority queue.
-        while (foodRatingMap[highestRated.foodName] != highestRated.foodRating){
-            cuisineFoodMap[cuisine].pop();
-            highestRated = cuisineFoodMap[cuisine].top();
-        }
-        
-        // return name of the highest rated 'food' of 'cuisine'
-        return highestRated.foodName;
+        // Return name of the highest rated 'food' of 'cuisine'
+        return cuisineFoodMap[cuisine].begin() -> second;
     }
 };
 
