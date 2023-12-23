745cee37d09ecbed3fc2d483ca6316fc57577eb4
Andrew Kim <ksb0724@utexas.edu>
2023-12-24 00:44:17 +0900

    Time: 233 ms (92.39%), Space: 18 MB (13.70%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.py
@@ -1,32 +1,24 @@
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-        small_chars, capital_chars = [0] * 26, [0] * 26
-        two_chars, n = set(), len(s)
-        left, ans = 0, 0
+        n = len(s)
+        if n < 3:
+            return n
         
-        for right in range(n):
-            if s[right] not in two_chars:
-                while len(two_chars) >= 2 and left < right:
-                    if 0 <= ord(s[left]) - 65 < 26:
-                        capital_chars[ord(s[left]) - 65] -= 1
-                        if capital_chars[ord(s[left]) - 65] == 0:
-                            two_chars.remove(s[left])
-                    else:
-                        small_chars[ord(s[left]) - 97] -= 1
-                        if small_chars[ord(s[left]) - 97] == 0:
-                            two_chars.remove(s[left])
-                    left += 1
-                two_chars.add(s[right])
-            
-            if 0 <= ord(s[right]) - 65 < 26:
-                capital_chars[ord(s[right]) - 65] += 1
-            else:
-                small_chars[ord(s[right]) - 97] += 1
-            
-            longest = 0
-            for i in range(26):
-                longest += small_chars[i]
-                longest += capital_chars[i]
-            ans = max(ans, longest)
+        left, right = 0, 0
+        hashmap = defaultdict()
+        max_len = 2
         
-        return ans
+        while right < n:
+            hashmap[s[right]] = right
+            right += 1
+            
+            if len(hashmap) == 3:
+                del_idx = min(hashmap.values())
+                del hashmap[s[del_idx]]
+                
+                # move left pointer of the sliding window
+                left = del_idx + 1
+            
+            max_len = max(max_len, right - left)
+        
+        return max_len
