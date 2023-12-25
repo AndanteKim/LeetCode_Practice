fbe108504b317d4a1dbcefd072e7d485f13a4490
Andrew Kim <ksb0724@utexas.edu>
2023-12-26 03:38:43 +0900

    Time: 38 ms (70.49%), Space: 17.2 MB (5.57%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,21 +1,22 @@
 class Solution:
-    
     def numDecodings(self, s: str) -> int:
-        @lru_cache(maxsize = None)
-        def dp(i: int) -> int:
-            if i == n:
-                return 1
-            
-            if s[i] == '0':
-                return 0
-            
-            if i == n - 1:
-                return 1
-            
-            ans = dp(i + 1)
-            if int(s[i:i + 2]) <= 26:
-                ans += dp(i + 2)
-            return ans
-        
         n = len(s)
-        return dp(0)
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        
+        # Ways to decode a string of size 1 is 1. Unless the string is '0'.
+        # '0' doesn't have a single digit decode
+        dp[1] = 0 if s[0] == '0' else 1
+        
+        for i in range(2, n + 1):
+            
+            # Check if successful single digit decode is possible
+            if s[i - 1] != '0':
+                dp[i] = dp[i - 1]
+            
+            # Check if successful two digit decode is possible
+            two_digit = int(s[i - 2 : i])
+            if 10 <= two_digit <= 26:
+                dp[i] += dp[i - 2]
+        
+        return dp[n]
