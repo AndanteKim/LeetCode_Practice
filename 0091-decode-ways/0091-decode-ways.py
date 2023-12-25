08876118481fad7cb7d6ff63da18d5fd3222bd35
Andrew Kim <ksb0724@utexas.edu>
2023-12-26 04:01:27 +0900

    Time: 34 ms (88.04%), Space: 17.2 MB (5.57%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,22 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        n = len(s)
-        dp = [0] * (n + 1)
-        dp[0] = 1
+        if s[0] == '0':
+            return 0
         
-        # Ways to decode a string of size 1 is 1. Unless the string is '0'.
-        # '0' doesn't have a single digit decode
-        dp[1] = 0 if s[0] == '0' else 1
-        
-        for i in range(2, n + 1):
-            
-            # Check if successful single digit decode is possible
-            if s[i - 1] != '0':
-                dp[i] = dp[i - 1]
-            
-            # Check if successful two digit decode is possible
-            two_digit = int(s[i - 2 : i])
+        two_back, one_back = 1, 1
+        for i in range(1, len(s)):
+            curr = 0
+            if s[i] != '0':
+                curr = one_back
+            two_digit = int(s[i - 1 : i + 1])
             if 10 <= two_digit <= 26:
-                dp[i] += dp[i - 2]
-        
-        return dp[n]
+                curr += two_back
+            two_back, one_back = one_back, curr
+            
+        return one_back
