9522b0f4d9102e2a96a25b691dee07453a8a1c97
Andrew Kim <ksb0724@utexas.edu>
2023-12-26 04:06:21 +0900

    Time: 3 ms (47.64%), Space: 6.3 MB (98.44%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        // DP array to store the subproblem results
-        int n = s.size();
-        vector<int> dp(n + 1, 0);
-        dp[0] = 1;
+        if (s[0] == '0')
+            return 0;
         
-        // Ways to decode a string of size 1 is 1. Unless the stirng is '0'.
-        // '0' doesn't have a single digit decode
-        dp[1] = (s[0] == '0')? 0:1;
+        int oneBack = 1, twoBack = 1;
         
-        for (int i = 2; i <= n; ++i){
-            // Check if successful single digit decode is possible.
-            if (s[i - 1] != '0')
-                dp[i] = dp[i - 1];
+        for (int i = 1; i < s.size(); ++i){
+            int curr = 0;
+            if (s[i] != '0')
+                curr = oneBack;
             
-            // Check if successful two digit decode is possible.
-            int twoDigits = stoi(s.substr(i - 2, 2));
-            if (twoDigits >= 10 && twoDigits <= 26)
-                dp[i] += dp[i - 2];
+            int twoDigit = stoi(s.substr(i - 1, 2));
+            if (twoDigit >= 10 && twoDigit <= 26)
+                curr += twoBack;
+            twoBack = oneBack;
+            oneBack = curr;
         }
         
-        return dp[n];
+        return oneBack;
     }
 };
