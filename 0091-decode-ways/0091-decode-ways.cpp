165887c6ad8ecf52d7387e7ecb53382062516890
Andrew Kim <ksb0724@utexas.edu>
2023-12-26 03:49:09 +0900

    Time: 3 ms (47.64%), Space: 6.5 MB (87.76%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,30 +1,26 @@
 class Solution {
-private:
-    int n;
-    int recursiveWithMemo(int i, string& s, vector<int>& memo){
-        if (i >= n){
-            return (i == n)? 1:0;
-        }
-        
-        if (s[i] == '0')
-            return 0;
-        
-        if (i == n - 1)
-            return 1;
-        
-        if (memo[i] != -1)
-            return memo[i];
-        
-        int ans = recursiveWithMemo(i + 1, s, memo);
-        if (stoi(s.substr(i, 2)) <= 26)
-            ans += recursiveWithMemo(i + 2, s, memo);
-        return memo[i] = ans;
-    }
-    
 public:
     int numDecodings(string s) {
-        this -> n = s.size();
-        vector<int> memo(n, -1);
-        return recursiveWithMemo(0, s, memo);
+        // DP array to store the subproblem results
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        
+        // Ways to decode a string of size 1 is 1. Unless the stirng is '0'.
+        // '0' doesn't have a single digit decode
+        dp[1] = (s[0] == '0')? 0:1;
+        
+        for (int i = 2; i <= n; ++i){
+            // Check if successful single digit decode is possible.
+            if (s[i - 1] != '0')
+                dp[i] = dp[i - 1];
+            
+            // Check if successful two digit decode is possible.
+            int twoDigits = stoi(s.substr(i - 2, 2));
+            if (twoDigits >= 10 && twoDigits <= 26)
+                dp[i] += dp[i - 2];
+        }
+        
+        return dp[n];
     }
 };
