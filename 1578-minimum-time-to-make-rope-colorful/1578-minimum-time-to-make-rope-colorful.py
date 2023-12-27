64b7252f53a8766fc50f237fdeb49997644da1df
Andrew Kim <ksb0724@utexas.edu>
2023-12-27 19:07:35 +0900

    Time: 884 ms (72.46%), Space: 28.2 MB (9.18%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.py
@@ -1,24 +1,20 @@
 class Solution:
     def minCost(self, colors: str, neededTime: List[int]) -> int:
-        # Init two pointers i, j
-        total_time = 0
-        i, j = 0, 0
+        # total_time: total time needed to make rope colorful;
+        # curr_max_time: maximum time of a balloon needed in this group.
+        total_time, curr_max_time = 0, 0
         
-        while i < len(neededTime) and j < len(neededTime):
-            curr_total, curr_max = 0, 0
-            
-            # Find all the balloons having the same color as the balloon indexed at i,
-            # record the total removal time and the maximum removal time.
-            while j < len(neededTime) and colors[i] == colors[j]:
-                curr_total += neededTime[j]
-                curr_max = max(curr_max, neededTime[j])
-                j += 1
-            
-            # Once we reach the end of the current group, add the cost of
-            # this group to total_Time, and reset two pointers
-            total_time += curr_total - curr_max
-            i = j
-            
+        # For each balloon in the array:
+        for i in range(len(colors)):
+            # If this balloon is the first balloon of a new group
+            # set the curr_max_time as 0.
+            if i > 0 and colors[i] != colors[i - 1]:
+                curr_max_time = 0
+                
+            # Increment total_time by the smaller one.
+            # Update curr_max_time as the larger one.
+            total_time += min(curr_max_time, neededTime[i])
+            curr_max_time = max(curr_max_time, neededTime[i])
+        
+        # Return total_time as the minimum removal time
         return total_time
-            
-        
