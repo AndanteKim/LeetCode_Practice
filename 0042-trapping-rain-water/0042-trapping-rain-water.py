1049a66b0711774d82b97e63c6e73f51a9cdc95e
Andrew Kim <ksb0724@utexas.edu>
2023-12-28 00:59:03 +0900

    Time: 100 ms (94.18%), Space: 19.1 MB (11.38%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,21 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        if not height:
-            return 0
+        ans, stack = 0, []
         
-        ans, n = 0, len(height)
-        left_max, right_max = [0] * n, [0] * n
-        left_max[0], right_max[-1] = height[0], height[-1]
-        
-        # Find the left side of area
-        for i in range(1, n):
-            left_max[i] = max(height[i], left_max[i - 1])
-        
-        # Find the right side of area
-        for i in range(n - 2, -1, -1):
-            right_max[i] = max(height[i], right_max[i + 1])
-        
-        for i in range(1, n - 1):
-            ans += min(left_max[i], right_max[i]) - height[i]
+        for curr in range(len(height)):
+            while stack and height[curr] > height[stack[-1]]:
+                right = stack.pop()
+                if not stack:
+                    break
+                dist = curr - stack[-1] - 1
+                bounded_height = min(height[curr], height[stack[-1]]) - height[right]
+                ans += dist * bounded_height
             
+            stack.append(curr)
+        
         return ans
