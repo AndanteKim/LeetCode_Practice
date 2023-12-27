f1fa0c5ab1a1b1d54c2dc199d28cae73ca962dc9
Andrew Kim <ksb0724@utexas.edu>
2023-12-28 01:15:38 +0900

    Time: 13 ms (42.53%), Space: 20.7 MB (37.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if (height.empty())
-            return 0;
-        
         int ans = 0, n = height.size();
-        vector<int> leftMax(n), rightMax(n);
-        leftMax[0] = height[0];
-        rightMax.back() = height.back();
+        stack<int> st;
         
-        // Find the left max height for each index of elevation map
-        for (int i = 1; i < n; ++i)
-            leftMax[i] = max(leftMax[i - 1], height[i]);
-        
-        // Find the right max height for each index of elevation map
-        for (int i = n - 2; i >= 0; --i)
-            rightMax[i] = max(rightMax[i + 1], height[i]);
-        
-        // Find the intersection of area between leftMax[i] and rightMax[i]
-        for (int i = 1; i < n - 1; ++i)
-            ans += min(leftMax[i], rightMax[i]) - height[i];
+        for (int curr = 0; curr < n; ++curr){
+            while (!st.empty() && height[curr] > height[st.top()]){
+                int right = st.top();
+                st.pop();
+                if (st.empty())
+                    break;
+                
+                int dist = curr - st.top() - 1;
+                int boundedHeight = min(height[curr], height[st.top()]) - height[right];
+                
+                ans += dist * boundedHeight;
+            }
+            st.push(curr);
+        }
         
         return ans;
     }
