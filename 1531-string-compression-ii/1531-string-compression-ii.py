547738d2a5b7a63c5d4a51a1a15171bad53c3cec
Andrew Kim <ksb0724@utexas.edu>
2023-12-28 18:14:03 +0900

    Time: 2860 ms (5.00%), Space: 17.6 MB (95.00%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.py
+++ 1531-string-compression-ii/1531-string-compression-ii.py
@@ -1,20 +1,27 @@
 class Solution:
     def getLengthOfOptimalCompression(self, s: str, k: int) -> int:
-        @lru_cache(maxsize = None)
-        def dp(i: int, last_char: chr, last_char_count: int, remain: int) -> int:
-            if remain < 0:
-                return float('inf')
-            
-            if i == n:
-                return 0
-            
-            delete_char = dp(i + 1, last_char, last_char_count, remain - 1)
-            if s[i] == last_char:
-                keep_char = dp(i + 1, last_char, last_char_count + 1, remain) + (last_char_count in [1, 9, 99])
-            else:
-                keep_char = dp(i + 1, s[i], 1, remain) + 1
-            
-            return min(keep_char, delete_char)
+        def digits(x: int) -> int:
+            return 1 if x == 1 else 2 if 1 < x < 10 else 3 if 10 <= x < 100 else 4
         
         n = len(s)
-        return dp(0, '', 0, k)
+        dp = [[float('inf')] * (k + 1) for _ in range(n + 1)]
+        dp[0][0] = 0
+        
+        for i in range(1, n + 1):
+            for j in range(min(i, k) + 1):
+                if j > 0:
+                    dp[i][j] = dp[i - 1][j - 1]
+                
+                remove = count = 0
+                for left in range(i, 0, -1):
+                    if s[left - 1] == s[i - 1]:
+                        count += 1
+                    else:
+                        remove += 1
+                        if remove > j:
+                            break
+                    
+                    dp[i][j] = min(dp[i][j], dp[left - 1][j - remove] + digits(count))
+        
+        return dp[-1][-1]
+                
