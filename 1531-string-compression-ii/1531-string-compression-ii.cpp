961fec449d889b5032da44ee47fb4bd4abecf855
Andrew Kim <ksb0724@utexas.edu>
2023-12-28 18:33:30 +0900

    Time: 243 ms (62.92%), Space: 8.9 MB (69.66%) - LeetHub

--- 1531-string-compression-ii/1531-string-compression-ii.cpp
+++ 1531-string-compression-ii/1531-string-compression-ii.cpp
@@ -1,30 +1,34 @@
 class Solution {
 private:
-    unordered_map<int, int> memo;
-    set<int> add{1, 9, 99};
-    int dp(string& s, int i, char lastChar, int lastCharCount, int k){
-        if (k < 0)
-            return INT_MAX / 2;
-        
-        if (i == s.size())
-            return 0;
-        
-        int key = i * 101 * 27 * 101 + (lastChar - 'a') * 101 * 101 + lastCharCount * 101 + k;
-        if (memo.find(key) != memo.end())
-            return memo[key];
-        
-        int keepChar;
-        int deleteChar = dp(s, i + 1, lastChar, lastCharCount, k - 1);
-        if (s[i] == lastChar)
-            keepChar = dp(s, i + 1, lastChar, lastCharCount + 1, k) + add.count(lastCharCount);
-        else
-            keepChar = dp(s, i + 1, s[i], 1, k) + 1;
-        int ans = min(keepChar, deleteChar);
-        return memo[key] = ans;
+    int digits(int x){
+        return (x == 1)? 1 : (1 < x && x < 10)? 2 : (10 <= x && x < 100)? 3 : 4;
     }
     
 public:
     int getLengthOfOptimalCompression(string s, int k) {
-        return dp(s, 0, (char)('a' + 26), 0, k);
+        int n = s.size();
+        vector dp(n + 1, vector<int>(k + 1, 1e9));
+        dp[0][0] = 0;
+        
+        for (int i = 1; i <= n; ++i){
+            for (int j = 0; j <= min(i, k); ++j){
+                if (j > 0)
+                    dp[i][j] = dp[i - 1][j - 1];
+                int count = 0, remove = 0;
+                for (int left = i; left > 0; --left){
+                    if (s[left - 1] == s[i - 1])
+                        ++count;
+                    else{
+                        ++remove;
+                        if (remove > j)
+                            break;
+                    }
+                
+                    dp[i][j] = min(dp[i][j], dp[left - 1][j - remove] + digits(count));
+                }
+            }
+        }
+        
+        return dp.back().back();
     }
 };
