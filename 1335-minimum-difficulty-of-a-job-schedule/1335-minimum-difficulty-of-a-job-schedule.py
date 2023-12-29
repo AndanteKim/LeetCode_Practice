356db4f44b0c443102470b803d451300140beb1a
Andrew Kim <ksb0724@utexas.edu>
2023-12-30 01:52:01 +0900

    Time: 1038 ms (23.51%), Space: 17.5 MB (60.80%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -4,17 +4,19 @@ class Solution:
         if n < d:
             return -1
         
-        # Initialize the min_diff matrix to record the minimum difficulty of the job schedule
-        min_diff = [[float('inf')] * n + [0] for _ in range(d + 1)]
+        # Initialize min_diff, prev_diff matrix to record the minimum difficulty of the job schedule
+        min_diff_next_day = [float('inf')] * (n + 1)
+        min_diff_next_day[-1] = 0
         
         for days_remaining in range(1, d + 1):
+            min_diff_curr_day = [float('inf')] * (n + 1)
+            min_diff_curr_day[-1] = 0
             for i in range(n - days_remaining + 1):
                 daily_max_job_diff = 0
-                
                 for j in range(i + 1, n - days_remaining + 2):
                     # Use daily_max_job_diff to record maximum job difficulty
                     daily_max_job_diff = max(daily_max_job_diff, jobDifficulty[j - 1])
-                    min_diff[days_remaining][i] = min(min_diff[days_remaining][i], daily_max_job_diff + \
-                                                      min_diff[days_remaining - 1][j])
+                    min_diff_curr_day[i] = min(min_diff_curr_day[i], daily_max_job_diff + min_diff_next_day[j])
+            min_diff_next_day = min_diff_curr_day
         
-        return -1 if min_diff[d][0] == float('inf') else min_diff[d][0]
+        return -1 if min_diff_next_day[0] == float('inf') else min_diff_next_day[0]
