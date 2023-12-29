f00d60d304869e6c1f21d078ce063252aaecdbd0
Andrew Kim <ksb0724@utexas.edu>
2023-12-30 01:01:18 +0900

    Time: 1689 ms (18.42%), Space: 17.7 MB (41.68%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -1,33 +1,22 @@
 class Solution:
     def minDifficulty(self, jobDifficulty: List[int], d: int) -> int:
-        
-        # Use memoization to avoid repeated computation of min_diff states
-        @lru_cache(maxsize = None)
-        def min_diff(i: int, remain: int) -> int:
-            # Base case: finish all remaining jobs in the last day
-            if remain == 1:
-                return max_job_remaining[i]
-        
-            ans = float('inf')
-            daily_max_job_diff = 0 # keep track of the maximum difficulty for today
-            
-            # Iterate through possible starting index for the next day
-            # and ensure we have at least one job for each remaining day
-            
-            for j in range(i, n - remain + 1):
-                daily_max_job_diff = max(daily_max_job_diff, jobDifficulty[j])
-                ans = min(ans, daily_max_job_diff + min_diff(j + 1, remain - 1))
-                
-            return ans
-        
         n = len(jobDifficulty)
-        # Edge case: make sure there is at least one job per day
         if n < d:
             return -1
         
-        # Precompute the maximum job difficulty for remaining jobs
-        max_job_remaining = jobDifficulty[:]
-        for i in range(n - 2, -1, -1):
-            max_job_remaining[i] = max(max_job_remaining[i], max_job_remaining[i + 1])
+        # Initialize the min_diff matrix to record the minimum difficulty of the job schedule
+        min_diff = [[float('inf')] * (n + 1) for _ in range(d + 1)]
+        for i in range(d + 1):
+            min_diff[i][-1] = 0
         
-        return min_diff(0, d)
+        for days_remaining in range(1, d + 1):
+            for i in range(n - days_remaining + 1):
+                daily_max_job_diff = 0
+                
+                for j in range(i + 1, n - days_remaining + 2):
+                    # Use daily_max_job_diff to record maximum job difficulty
+                    daily_max_job_diff = max(daily_max_job_diff, jobDifficulty[j - 1])
+                    min_diff[days_remaining][i] = min(min_diff[days_remaining][i], daily_max_job_diff + \
+                                                      min_diff[days_remaining - 1][j])
+        
+        return -1 if min_diff[d][0] == float('inf') else min_diff[d][0]
