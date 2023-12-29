b41d943c19623ac1660816e1069ae7294ea4ca6f
Andrew Kim <ksb0724@utexas.edu>
2023-12-30 02:10:38 +0900

    Time: 58 ms (38.31%), Space: 7.8 MB (66.51%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -2,26 +2,28 @@ class Solution {
 public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n = jobDifficulty.size();
-        if (n < d)
-            return -1;
+        if (n < d) return -1;
         
-        // Initialize the minDiff matrix to record the minimum difficulty of the job schedule
-        vector minDiff(d + 1, vector<int>(n + 1, INT_MAX));
-        for (int i = 0; i <= d; ++i)
-            minDiff[i].back() = 0;
+        // Initialize maxDiffNextDay matrix to record the minimum difficulty of the job schedule
+        vector<int> minDiffNextDay(n + 1, INT_MAX);
+        minDiffNextDay.back() = 0;
         
         for (int daysRemaining = 1; daysRemaining <= d; ++daysRemaining){
+            // Initialize maxCurrNextDay matrix to record the minimum difficulty of the job schedule
+            vector<int> minDiffCurrDay(n + 1, INT_MAX);
+            minDiffCurrDay.back() = 0;
             for (int i = 0; i <= n - daysRemaining; ++i){
                 int dailyMaxJobDiff = 0;
                 for (int j = i + 1; j <= n - daysRemaining + 1; ++j){
                     // Use dailyMaxJobDiff to record maximum job difficulty
                     dailyMaxJobDiff = max(dailyMaxJobDiff, jobDifficulty[j - 1]);
-                    if (minDiff[daysRemaining - 1][j] != INT_MAX)
-                        minDiff[daysRemaining][i] = min(minDiff[daysRemaining][i], dailyMaxJobDiff + minDiff[daysRemaining - 1][j]);
+                    if (minDiffNextDay[j] != INT_MAX)
+                        minDiffCurrDay[i] = min(minDiffCurrDay[i], dailyMaxJobDiff + minDiffNextDay[j]);
                 }
             }
+            minDiffNextDay = minDiffCurrDay;
         }
         
-        return (minDiff[d][0] == INT_MAX)? -1: minDiff[d][0];
+        return (minDiffNextDay[0] == INT_MAX)? -1: minDiffNextDay[0];
     }
 };
