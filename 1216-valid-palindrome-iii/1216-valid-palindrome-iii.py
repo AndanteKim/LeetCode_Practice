e07a319b23616bfb87bfda8bae77afb0e2964674
Andrew Kim <ksb0724@utexas.edu>
2023-12-31 02:52:22 +0900

    Time: 159 ms (98.46%), Space: 19.2 MB (68.21%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -1,36 +1,22 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
-        def dp(i: int, j: int) -> int:
-            
-            # Base case, only 1 letter remaining
-            if i == j:
-                return 0
-            
-            # Base case 2, only 2 letters remaining
-            if i == j - 1:
-                return s[i] != s[j]
-            
-            if memo[i][j] != -1:
-                return memo[i][j]
-        
-            # Case 1: Character at 'i' equals character at 'j'
-            if s[i] == s[j]:
-                memo[i][j] = dp(i + 1, j - 1)
-                return memo[i][j]
-            
-            # Case 2: Character at 'i' does not equal character at 'j'.
-            # Either delete character at 'i' or delete character at 'j'
-            # and try to match the two poijnters using recursion
-            # We need to take the minimum of two results and add 1
-            # representing the cost of deletion
-            memo[i][j] = 1 + min(dp(i + 1, j), dp(i, j - 1))
-            return memo[i][j]
-        
-        
         n = len(s)
-        memo = [[-1] * n for _ in range(n)]
+        dp = [[0] * n for _ in range(n)]
         
-        # Return true if the minimum cost to create a palindrome by only deleting the letters
-        # is less than or equal to 'k'
-        
-        return dp(0, n - 1) <= k
+        # Generate all combinations of 'i' and 'j' in the correct order
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                
+                # Case 1: Character at 'i' equals character at 'j'
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1]
+                    
+                # Case 2: Character at 'i' doesn't equal character at 'j'.
+                # Either delete character at 'i' or delete character at 'j'
+                # and try to match the two pointers using recursion.
+                # We need to take the minimum of the two results and add 1
+                # representing the cost of deletion.
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1])
+                
+        return dp[0][n - 1] <= k
