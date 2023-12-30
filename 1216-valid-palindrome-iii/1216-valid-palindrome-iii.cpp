8e011f92509f6cd1cdedff8b7a1c953db2642ec4
Andrew Kim <ksb0724@utexas.edu>
2023-12-31 03:39:59 +0900

    Time: 22 ms (49.07%), Space: 6.6 MB (99.47%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -2,25 +2,41 @@ class Solution {
 public:
     bool isValidPalindrome(string s, int k) {
         int n = s.size();
-        vector<int> prevDp(n);
+        vector<int> dp(n);
         
-        // Generate all combinations of 'i' and 'j' in the correct order
+        // To store the previous required values from dp.
+        int prev, temp;
+        // Generate all combinations 'i' and 'j' in the correct order.
         for (int i = n - 2; i >= 0; --i){
-            vector<int> dp(n);
-            
+            // 'prev' stores the value at dp[i + 1][j - 1];
+            prev = 0;
             for (int j = i + 1; j < n; ++j){
-                // Case 1: Character at 'i' == character at 'j'
+                // Store the value of memo[i + 1][j] temporarily
+                temp = dp[j];
+                
+                // Case 1: Character at 'i' equals character at 'j'
                 if (s[i] == s[j])
-                    dp[j] = prevDp[j - 1];
-                // Case 2: Character at 'i' != character at 'j'
-                // Either delete character 'i' or 'j' and try to match two pointers
-                // We try to take two minimum of results and add 1 representating the cost of deletion 
+                    dp[j] = prev;
+                
+                // Case 2: Character at 'i' doesn't equal character at 'j'.
+                // Either delete character at 'i' or delete character at 'j'
+                // and try to match the two pointers.
+                // We need to take the minimum of the two results and add 1
+                // representating the cost of deletion.
                 else
-                    dp[j] = 1 + min(prevDp[j], dp[j - 1]);
+                    // dp[j] := contain the value for dp[i + 1][j]
+                    // dp[j - 1] := contain the value for dp[i][j - 1]
+                    dp[j] = 1 + min(dp[j], dp[j - 1]);
+            
+                // Copy the value of dp[i + 1][j] to 'prev'
+                // For the next iteration when j = j + 1
+                // 'prev' will hold the value dp[i + 1][j - 1]
+                prev = temp;
             }
-            prevDp = dp;
         }
         
-        return prevDp[n - 1] <= k;
+        // Return true if the minimum cost to create a palindrome by only deleting the letters
+        // is less than equal to 'k'.
+        return dp[n - 1] <= k;
     }
 };
