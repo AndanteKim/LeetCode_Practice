1496f7edffe1cb289c4f1b7e5d62fbe8a19618a4
Andrew Kim <ksb0724@utexas.edu>
2023-12-31 03:05:00 +0900

    Time: 39 ms (17.51%), Space: 17.1 MB (68.17%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.cpp
@@ -1,40 +1,24 @@
 class Solution {
-    int n;
-    // Used overloading
-    int isValidPalindrome(string& s, int i, int j, vector<vector<int>>& memo){
-        // Base case: only 1 character remaining
-        if (i == j)
-            return 0;
-        
-        // Base case 2: only 2 characters remaining
-        if (i == j - 1)
-            return s[i] != s[j];
-        
-        // Return the precomputed value if exists as memoization
-        if (memo[i][j] != -1)
-            return memo[i][j];
-        
-        
-        // Case 1: If character at 'i' equals character at 'j'
-        if (s[i] == s[j])
-            return memo[i][j] = isValidPalindrome(s, i + 1, j - 1, memo);
-        
-        // Case 2: If character at 'i' does not equal to character at 'j'.
-        // Either delete character at 'i' or delete character at 'j'
-        // and try to match the two pointers using recursion.
-        // We need to take the minimum of the two results and add 1
-        // representing the cost of deletion.
-        
-        return memo[i][j] = 1 + min(isValidPalindrome(s, i + 1, j, memo), isValidPalindrome(s, i, j - 1, memo));
-    }
-    
 public:
     bool isValidPalindrome(string s, int k) {
-        this -> n = s.size();
-        vector memo(n, vector<int>(n, -1));
+        int n = s.size();
+        vector dp(n, vector<int>(n));
         
-        // Return true if the minimum cost to create a palindrome by only deleting the letters
-        // is less than or equal to 'k'.
-        return isValidPalindrome(s, 0, n - 1, memo) <= k;
+        // Generate all combinations of 'i' and 'j' in the correct order
+        for (int i = n - 2; i >= 0; --i){
+            for (int j = i + 1; j < n; ++j){
+                // Case 1: If character at 'i' equals character at 'j'
+                if (s[i] == s[j])
+                    dp[i][j] = dp[i + 1][j - 1];
+                
+                // Case 2: If character at 'i' doesn't equal character at 'j'
+                // Either delete characters at 'i' or 'j' and try to match two pointers
+                // We need to take the minimum of the two results and add 1 representating the cost of deletion
+                else
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        
+        return dp[0][n - 1] <= k;
     }
 };
