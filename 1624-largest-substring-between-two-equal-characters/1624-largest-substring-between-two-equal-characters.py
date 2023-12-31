0db5de4adfa3ec74dfd35a0b785f30ada24d3539
Andrew Kim <ksb0724@utexas.edu>
2023-12-31 17:03:05 +0900

    Time: 76 ms (6.83%), Space: 17.3 MB (10.73%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxLengthBetweenEqualCharacters(self, s: str) -> int:
-        ans, n, idx_list = -1, len(s), defaultdict(list)
+        first_index, ans = dict(), -1
         
-        for i in range(n):
-            idx_list[s[i]].append(i)
-        
-        for char, indices in idx_list.items():
-            for i in range(1, len(indices)):
-                ans = max(ans, indices[-1] - indices[0] - 1)
+        for i in range(len(s)):
+            if s[i] in first_index:
+                ans = max(ans, i - first_index[s[i]] - 1)
+            else:
+                first_index[s[i]] = i
         
         return ans
