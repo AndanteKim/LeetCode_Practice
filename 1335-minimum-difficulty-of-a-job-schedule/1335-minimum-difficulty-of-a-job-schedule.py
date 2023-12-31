ba80088a05a3c9dc0c443dcced326e3b72966743
Andrew Kim <ksb0724@utexas.edu>
2023-12-31 18:10:42 +0900

    Time: 97 ms (95.89%), Space: 17.4 MB (90.80%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.py
@@ -4,20 +4,44 @@ class Solution:
         if n < d:
             return -1
         
-        # Initialize min_diff_next_day matrix to record the minimum difficulty of the job schedule
-        min_diff_next_day = [float('inf')] * (n + 1)
-        min_diff_next_day[-1] = 0
+        # min_diff_curr_day and min_diff_prev_day record the minimum total job difficulty
+        # for the current day and previous day, respectively
+        min_diff_prev_day, min_diff_curr_day = [float('inf')] * n, [float('inf')] * n
         
-        for days_remaining in range(1, d + 1):
-            # Initialize min_diff_curr_day matrix to record the minimum difficulty of the job schedule
-            min_diff_curr_day = [float('inf')] * (n + 1)
-            min_diff_curr_day[-1] = 0
-            for i in range(n - days_remaining + 1):
-                daily_max_job_diff = 0
-                for j in range(i + 1, n - days_remaining + 2):
-                    # Use daily_max_job_diff to record maximum job difficulty
-                    daily_max_job_diff = max(daily_max_job_diff, jobDifficulty[j - 1])
-                    min_diff_curr_day[i] = min(min_diff_curr_day[i], daily_max_job_diff + min_diff_next_day[j])
-            min_diff_next_day = min_diff_curr_day
+        for day in range(d):
+            # Use a monotonically decreasing stack to record job difficulties
+            stack = []
+            
+            # The number of jobs needs to be no less than number of days passed
+            for i in range(day, n):
+                
+                # We initialize min_diff_curr_day[i] as only performing 1 job at the i-th index.
+                # At day 0, the minimum total job difficulty is to complete the 0th job only.
+                if i == 0:
+                    min_diff_curr_day[i] = jobDifficulty[0]
+                # Otherwise, we increment min_diff_prev_day[i - 1] by the i-th job difficulty
+                else:
+                    min_diff_curr_day[i] = min_diff_prev_day[i - 1] + jobDifficulty[i]
+                    
+                # When we find the last element in the stack is smaller than or equal to current job
+                # we need to pop out the element to maintain a monotonic decreasing stack
+                while stack and jobDifficulty[stack[-1]] <= jobDifficulty[i]:
+                    
+                    # If we include all jobs with index j + 1 to i to the current day,
+                    # total job difficulty of the current day will be increased.
+                    # by the amount of jobDifficulty[i] - jobDifficulty[j]
+                    j = stack.pop()
+                    diff_incr = jobDifficulty[i] - jobDifficulty[j]
+                    min_diff_curr_day[i] = min(min_diff_curr_day[i], min_diff_curr_day[j] + diff_incr)
+                    
+                # When the last element in the stack is larger than current element,
+                # If we include all jobs with index j + 1 to the i to the current day,
+                # the overall job difficulty will not change.
+                if stack:
+                    min_diff_curr_day[i] = min(min_diff_curr_day[i], min_diff_curr_day[stack[-1]])
+                    
+                # Update the monotonic stack by adding in the current index
+                stack.append(i)
+            min_diff_prev_day, min_diff_curr_day = min_diff_curr_day, min_diff_prev_day
         
-        return -1 if min_diff_next_day[0] == float('inf') else min_diff_next_day[0]
+        return min_diff_prev_day[-1]
