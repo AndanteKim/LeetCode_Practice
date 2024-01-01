fe101941acb087835579526d1fd8a8b73f276154
Andrew Kim <ksb0724@utexas.edu>
2024-01-02 02:34:24 +0900

    Time: 6 ms (75.61%), Space: 8.6 MB (70.73%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.cpp
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.cpp
@@ -1,38 +1,41 @@
 class Solution {
 private:
-    // Maximum number of bikes is 10
-    int smallestDistSum = INT_MAX;
-    int visited[10];
+    // Maximum value of mask will be 2 ^ (Number of bikes)
+    // And number of bikes can be 10 at max
+    int memo[1024];
     
-    // Manhattan distance
+    // Manhattan Distance
     int findDistance(vector<int>& worker, vector<int>& bike){
         return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1]);
     }
     
-    void minDistSum(vector<vector<int>>& workers, int workerIdx, vector<vector<int>>& bikes, int currDistSum){
-        if (workerIdx >= workers.size()){
-            smallestDistSum = min(smallestDistSum, currDistSum);
-            return;
-        }
+    // 
+    int minDistSum(vector<vector<int>>& workers, vector<vector<int>>& bikes, int workerIdx, int mask){
+        if (workerIdx >= workers.size())
+            return 0;
         
-        // If the current distance sum is greater than the smallest result
-        // found then stop exploring this combination of workers and bikes
-        if (currDistSum >= smallestDistSum)
-            return;
+        // If result is already calculated, return it no recursion needed
+        if (memo[mask] != -1)
+            return memo[mask];
         
+        int smallestDistSum = INT_MAX;
         for (int bikeIdx = 0; bikeIdx < bikes.size(); ++bikeIdx){
-            // If bike is available
-            if (!visited[bikeIdx]){
-                visited[bikeIdx] = true;
-                minDistSum(workers, workerIdx + 1, bikes, currDistSum + findDistance(workers[workerIdx], bikes[bikeIdx]));
-                visited[bikeIdx] = false;
-            }
+            // Check if the bike at bikeIndex is available
+            if ((mask & (1 << bikeIdx)) == 0)
+                // Adding the current distance and repeat the process for next worker
+                // also changing the bit at index bikeIndex to 1 to show the bike there is assigned
+                smallestDistSum = min(smallestDistSum, findDistance(workers[workerIdx], bikes[bikeIdx]) + minDistSum(workers, bikes, workerIdx + 1, mask | (1 << bikeIdx)));
         }
+        
+        // Memoizing the result corresponding to mask
+        return memo[mask] = smallestDistSum;
     }
     
 public:
     int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
-        minDistSum(workers, 0, bikes, 0);
-        return smallestDistSum;
+        // Marking all postions to -1 that signifies result has not been calculated yet for this week
+        memset(memo, -1, sizeof(memo));
+        
+        return minDistSum(workers, bikes, 0, 0);
     }
 };
