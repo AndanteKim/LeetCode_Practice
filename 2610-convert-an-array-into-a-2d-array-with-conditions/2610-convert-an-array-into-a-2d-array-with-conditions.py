853e38f82238be1d86de0c43664f8b5373203074
Andrew Kim <ksb0724@utexas.edu>
2024-01-02 10:28:01 +0900

    Time: 53 ms (80.78%), Space: 17.4 MB (5.17%) - LeetHub

---
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMatrix(self, nums: List[int]) -> List[List[int]]:
+        n = max(nums)
+        hashmap, ans = defaultdict(int), []
+        
+        for num in nums:
+            hashmap[num] += 1
+        
+        for num, height in hashmap.items():
+            for i in range(height):
+                if len(ans) < i + 1:
+                    ans.append([num])
+                else:
+                    ans[i].append(num)
+        
+        return ans
