52c59e7351b7d1091812933459bb11fa8ba9fa9d
Andrew Kim <ksb0724@utexas.edu>
2024-01-02 17:29:20 +0900

    Time: 153 ms (18.58%), Space: 17.6 MB (26.55%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.py
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.py
@@ -1,34 +1,45 @@
 class Solution:
+    # Count the number of ones using Brian Kernighan's Algorithm
+    def count_num_of_ones(self, mask: int) -> int:
+        cnt = 0
+        while mask != 0:
+            mask &= (mask - 1)
+            cnt += 1
+        return cnt
     
-    # Manhattan distance
+    # Manhanttan Distance
     def find_dist(self, worker: List[int], bike: List[int]) -> int:
-        return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1]);
+        return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1])
     
-    def min_dist_sum(self, workers: List[List[int]], bikes: List[List[int]], worker_idx: int, mask: int) -> int:
-        if worker_idx >= len(workers):
-            return 0
+    def min_dist_sum(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
+        m, n, smallest_dist_sum = len(bikes), len(workers), float('inf')
         
-        # If result is already calculated, return it no recursion needed
-        if self.memo[mask] != -1:
-            return self.memo[mask]
+        # 0 signifies that no bike has been assigned and distance sum for no assigning any bike is equal to 0
+        self.dp[0] = 0
         
-        smallest_dist_sum = float('inf')
-        for bike_idx in range(len(bikes)):
-            # Check if the bike at bike_idx is available
-            if mask & (1 << bike_idx) == 0:
-                # Adding the current distance and repeat the process for next worker
-                # also changing the bit at index bike_idx to 1 to show the bike there is assigned
-                smallest_dist_sum = min(smallest_dist_sum, self.find_dist(workers[worker_idx], bikes[bike_idx]) + self.min_dist_sum(workers, bikes, worker_idx + 1, mask | (1 << bike_idx)))
+        # Traverse over all the possible values of mask
+        for mask in range((1 << m)):
+            next_worker_idx = self.count_num_of_ones(mask)
+            
+            # If mask has more number of 1's than the number of workers
+            # Then, we can update our anser accordingly
+            if next_worker_idx >= n:
+                smallest_dist_sum = min(smallest_dist_sum, self.dp[mask])
+                continue
+            
+            for bike_idx in range(m):
+                # Checking if the bike at bike_idx has already been assigned
+                if (mask & (1 << bike_idx)) == 0:
+                    new_mask = (1 << bike_idx) | mask
+                    
+                    # Updating the distance sum for new mask
+                    self.dp[new_mask] = min(self.dp[new_mask], self.dp[mask] + self.find_dist(workers[next_worker_idx], bikes[bike_idx]))
         
-        # Memoizing the result corresponding to mask
-        self.memo[mask] = smallest_dist_sum
-        return self.memo[mask]
-    
+        return smallest_dist_sum
+     
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
         # Maximum value of mask will be 2 ^ (Number of bikes)
-        # and number of bikes can be 10 at max
-        # Marking all positions to -1 that signifies result
-        # has not been calculated yet for this mask
-        self.memo = [-1] * 1024
+        # And number of bikes can be 10 at max
+        self.dp = [float('inf')] * 1024
+        return self.min_dist_sum(workers, bikes)
         
-        return self.min_dist_sum(workers, bikes, 0, 0)
