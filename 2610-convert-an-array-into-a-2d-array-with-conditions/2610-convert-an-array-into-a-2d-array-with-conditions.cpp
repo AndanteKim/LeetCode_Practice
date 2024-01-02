85d5766c3ea90a2ca575e1b846e4faf093e8c77a
Andrew Kim <ksb0724@utexas.edu>
2024-01-02 11:36:42 +0900

    Time: 40 ms (5.35%), Space: 29.8 MB (83.60%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> findMatrix(vector<int>& nums) {
-        unordered_map<int, int> mapping;
+        vector<int> freq(nums.size() + 1);
         vector<vector<int>> ans;
         
-        for (int num: nums)
-            ++mapping[num];
-        
-        for (auto& [num, height]: mapping){
-            for (int i = 0; i < height; ++i){
-                if (ans.size() <= i)
-                    ans.push_back({num});
-                else
-                    ans[i].push_back(num);
-            }
+        for (int n: nums){
+            if (freq[n] >= ans.size())
+                ans.push_back({});
+            
+            // Store the integer in the list corresponding to its current frequency
+            ans[freq[n]].push_back(n);
+            ++freq[n];
         }
         
         return ans;
