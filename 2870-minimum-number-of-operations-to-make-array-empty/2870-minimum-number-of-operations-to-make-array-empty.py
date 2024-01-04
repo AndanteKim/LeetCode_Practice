302c4706b89d907c3a3480f9b0b3c238b14ba34f
Andrew Kim <ksb0724@utexas.edu>
2024-01-04 17:47:10 +0900

    Time: 543 ms (95.76%), Space: 32.4 MB (6.06%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -1,22 +1,11 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        idx_table, ans = defaultdict(list), 0
+        counter = Counter(nums)
+        ans = 0
         
-        for i in range(len(nums)):
-            idx_table[nums[i]].append(i)
-            
-        for num, l in idx_table.items():
-            n, curr = len(l), float('inf')
-            max_3 = n // 3
-            
-            for i in range(max_3 + 1):
-                remain = n - (3 * i)
-                if remain % 2 == 0:
-                    curr = min(curr, i + (remain // 2))
-            
-            if curr == float('inf'):
+        for c in counter.values():
+            if c == 1:
                 return -1
-            ans += curr
-       
+            ans += ceil(c / 3)
         return ans
-            
+        
