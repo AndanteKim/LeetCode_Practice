7ec07e34d7902760ce0829405bad671405ddb730
Andrew Kim <ksb0724@utexas.edu>
2024-01-08 01:07:51 +0900

    Time: 604 ms (60.79%), Space: 122.9 MB (16.19%) - LeetHub

--- 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
+++ 0446-arithmetic-slices-ii-subsequence/0446-arithmetic-slices-ii-subsequence.py
@@ -1,17 +1,30 @@
 class Solution:
-    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        n, ans = len(nums), 0
-        cnt = [defaultdict(int) for _ in range(n)]
+    def dp(self, nums: List[int], i: int, diff: int, cnt: int, counter: DefaultDict[int, List[int]], memo: Dict[int, Tuple[int]]) -> int:
+        if (i, diff, cnt) in memo:
+            return memo[(i, diff, cnt)]
         
-        for i in range(1, n):
-            for j in range(i):
-                delta = nums[i] - nums[j]
-                total = 0
-                if delta in cnt[j]:
-                    total = cnt[j][delta]
-                cnt[i][delta] += total + 1
-                ans += total
-                
-                
+        ans = 0
+        if cnt >= 3:
+            ans += 1
+        
+        if nums[i] + diff in counter:
+            for j in counter[nums[i] + diff]:
+                if j > i:
+                    ans += self.dp(nums, j, diff, cnt + 1, counter, memo)
+        
+        memo[(i, diff, cnt)] = ans
         return ans
+    
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        n, counter = len(nums), defaultdict(list)
         
+        for i in range(n):
+            counter[nums[i]].append(i)
+            
+        ans, memo = 0, dict()
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                ans += self.dp(nums, j, nums[j] - nums[i], 2, counter, memo)
+        
+        return ans
