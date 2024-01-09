135ac4ac22ece939f81d341b8f8b744fc3f844cd
Andrew Kim <ksb0724@utexas.edu>
2024-01-09 18:44:04 +0900

    Time: 89 ms (94.99%), Space: 17.5 MB (29.67%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.py
@@ -9,24 +9,16 @@
 class Solution:
     def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
         rows, cols = binaryMatrix.dimensions()
-        smallest_idx = cols
         
-        for row in range(rows):
-            # Binary Search for the first 1 in the row.
-            lo, hi = 0, cols - 1
-            
-            while lo < hi:
-                mid = (lo + hi) >> 1
-                if binaryMatrix.get(row, mid) == 0:
-                    lo = mid + 1
-                else:
-                    hi = mid
-            
-            # If the last element in the search space is a 1, then this row
-            # contained a 1.
-            if binaryMatrix.get(row, lo) == 1:
-                smallest_idx = min(smallest_idx, lo)
-                
-        # If smallest_idx is the still set to cols, then there were no 1's in the grid
-        return -1 if smallest_idx == cols else smallest_idx
+        # Set pointers to the top-right corner.
+        current_row, current_col = 0, cols - 1
         
+        # Repeat the search until it goews off the grid.
+        while current_row < rows and current_col >= 0:
+            if binaryMatrix.get(current_row, current_col) == 0:
+                current_row += 1
+            else:
+                current_col -= 1
+        
+        # If we never left the last column, it must have been all 0's.
+        return current_col + 1 if current_col != cols - 1 else -1
