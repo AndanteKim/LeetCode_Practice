14fb98cc37658544124d973ce08db76eba6961e7
Andrew Kim <ksb0724@utexas.edu>
2024-01-10 01:05:42 +0900

    Time: 508 ms (92.30%), Space: 29.6 MB (76.40%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.py
@@ -1,35 +1,43 @@
 class Solution:
-    def dp(self, jobs: List[List[int]], start: List[List[int]], n: int, pos: int) -> int:
-        # 0 profit if we have already iterated over all the jobs
-        if pos == n:
-            return 0
-        
-        # we have already calculated the answer, so no need to go into recursion
-        if self.memo[pos] != -1:
-            return self.memo[pos]
-        
-        # next_idx is the index of next non-conflicting job
-        # bisect_left will return the iterator to the first element which is equal to or greater than the element at jobs[pos][1]
-        next_idx = bisect_left(start, jobs[pos][1])
-        
-        # find the maximum profit of our two options: skipping or scheduling the current job
-        max_profit = max(self.dp(jobs, start, n, pos + 1), jobs[pos][2] + self.dp(jobs, start, n, next_idx))
-        self.memo[pos] = max_profit
-        return max_profit
-    
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        # marking all values to -1, so that we can differentiate
-        # if we have already calculated the answer or not
-        n, self.memo, jobs = len(startTime), [-1] * 50001, []
         
-        # Storing job's details into one list
-        # this will help in sorting the jobs while maintaining the other parameters
+        def find_max_profit(n: int) -> int:
+            for pos in range(n - 1, -1, -1):
+                # It is the profit made by scheduling the current job
+                curr_profit = 0
+                
+                # next_idx is the index of next non-conflicting job
+                # bisect_left will return the iterator to the first element
+                # which is equal to or greater than the element at 'jobs[pos][1]'
+                next_idx = bisect_left(startTime, jobs[pos][1])
+                
+                # If there is a non-conflicting job possible add it's profit
+                # else just consider the current job profit
+                if next_idx != n:
+                    curr_profit = jobs[pos][2] + dp[next_idx]
+                else:
+                    curr_profit = jobs[pos][2]
+                
+                # storing the maximum profit we can achieve by scheduling
+                # non-conflicting jobs from index i to the end of array
+                if pos == n - 1:
+                    dp[pos] = curr_profit
+                else:
+                    dp[pos] = max(curr_profit, dp[pos + 1])
+            
+            return dp[0]
+        
+        jobs, n, dp = [], len(profit), [0] * 50001
+        
+        # storing job's details into one list
+        # this will help in sorting the jobs while maintaining the other parameters\
         for i in range(n):
-            jobs.append([startTime[i], endTime[i], profit[i]])
+            jobs.append((startTime[i], endTime[i], profit[i]))
         
         jobs.sort()
         
-        # binary search will be used in startTime so store it as separate list
+        # binary search will be used in startTime, so store it as separate list
         for i in range(n):
-            startTime[i] = jobs[i][0]
-        return self.dp(jobs, startTime, n, 0)
+            startTime[i] = jobs[i][0];
+            
+        return find_max_profit(n)
