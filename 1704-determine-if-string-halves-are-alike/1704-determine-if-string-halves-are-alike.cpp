1d33960a7d8a3b86ee0abbcbb85c06a80c6862ba
Andrew Kim <ksb0724@utexas.edu>
2024-01-12 15:34:49 +0900

    Time: 2 ms (64.23%), Space: 7 MB (72.68%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -1,20 +1,20 @@
 class Solution {
+private:
+    int countVowels(int start, int end, string& s){
+        string vowels = "aeiouAEIOU";
+        int ans = 0;
+        
+        for (int i = start; i < end; ++i){
+            if (vowels.find(s[i]) != string::npos)
+                ++ans;
+        }
+        
+        return ans;
+    }
+    
 public:
     bool halvesAreAlike(string s) {
-        int n = s.size(), aVowelCount = 0;
-        string vowels = "aeiouAEIOU";
-        
-        for (int i = 0; i < n / 2; ++i){
-            if (vowels.find(s[i]) != string::npos)
-                ++aVowelCount;
-        }
-        
-        int bVowelCount = 0;
-        for (int i = n / 2; i < n; ++i){
-            if (vowels.find(s[i]) != string::npos)
-                ++bVowelCount;
-        }
-        
-        return aVowelCount == bVowelCount;
+        int n = s.size();
+        return countVowels(0, n / 2, s) == countVowels(n / 2, n, s);
     }
 };
