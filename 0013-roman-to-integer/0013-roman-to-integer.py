2d347218b2143427b3c18bceb8173691c3a4d470
Andrew Kim <ksb0724@utexas.edu>
2024-01-14 19:05:07 +0900

    Time: 53 ms (46.76%), Space: 17.2 MB (34.57%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,22 +1,22 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        mapping = {
-            "I":1, "II":2, "III":3, "IV":4, "V":5,
-            "VI":6, "VII":7, "VIII":8, "IX":9, "X":10,
-            "XL":40, "L":50, "XC":90, "C":100, "CD":400,
-            "D":500, "CM":900, "M":1000 
+        values = {
+            'I':1,'V':5,'X':10,\
+            'L':50,'C':100,'D':500,\
+            'M':1000
         }
         
-        i, n = 0, len(s)
-        ans, curr = 0, ""
-        while i < n:
-            if curr + s[i] in mapping:
-                curr += s[i]
+        total, i = 0, 0
+        while i < len(s):
+            # If this is the subractive case.
+            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
+                total += values[s[i + 1]] - values[s[i]]
+                i += 2
+                
+            # Else this is NOT the subractive case.
             else:
-                ans += mapping[curr]
-                curr = s[i]
-            i += 1
+                total += values[s[i]]
+                i += 1
         
-        if curr:
-            ans += mapping[curr]
-        return ans
+        return total
+        
