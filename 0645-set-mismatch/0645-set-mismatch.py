48ffc4bfa72ebd220a47396743023dde2d8e8896
Andrew Kim <ksb0724@utexas.edu>
2024-01-22 19:10:57 +0900

    Time: 169 ms (60.26%), Space: 18.4 MB (64.05%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,19 +1,16 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        mx = max(nums)
-        count = [0] * (mx + 1)
+        freqs = dict()
+        dup, missing = -1, 1
         
         for n in nums:
-            count[n] += 1
+            freqs[n] = freqs.get(n, 0) + 1
             
-        ans = [-1, -1]
-        for i in range(1, mx + 1):
-            if count[i] == 2:
-                ans[0] = i
-            
-            if count[i] == 0:
-                ans[1] = i
-            
-        return [ans[0], mx + 1] if ans[1] == -1 else ans
-        
+        for i in range(1, len(nums) + 1):
+            if i in freqs:
+                if freqs[i] == 2:
+                    dup = i
+            else:
+                missing = i
+                
+        return [dup, missing]
