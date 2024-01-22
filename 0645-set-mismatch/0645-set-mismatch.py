f2b41b828856e75058c09717ea94b66921ce91fe
Andrew Kim <ksb0724@utexas.edu>
2024-01-22 20:45:39 +0900

    Time: 171 ms (54.31%), Space: 17.9 MB (89.31%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -1,31 +1,12 @@
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
-        # xor0 := missing, xor1: duplicate
-        xor, xor0, xor1 = 0, 0, 0
+        diff, sq_diff = 0, 0
         
-        for n in nums:
-            xor ^= n
-            
-        for i in range(1, len(nums) + 1):
-            xor ^= i
-            
-        rightmostbit = xor & ~(xor - 1)
-                                     
-        for n in nums:
-            if (n & rightmostbit) != 0:
-                xor1 ^= n
-            else:
-                xor0 ^= n
-                
-        for i in range(1, len(nums) + 1):
-            if (i & rightmostbit) != 0:
-                xor1 ^= i
-            else:
-                xor0 ^= i
-                
         for i in range(0, len(nums)):
-            if nums[i] == xor0:
-                return [xor0, xor1]
+            diff += (i + 1) - nums[i]
             
-        return [xor1, xor0]
+            sq_diff += (i + 1) ** 2 - nums[i] ** 2
+            
+        sum = sq_diff // diff
         
+        return [(sum - diff) >> 1, (sum + diff) >> 1]
