e1657c69a0d57a0eaf53b6a1412ea85a3cc09999
Andrew Kim <ksb0724@utexas.edu>
2024-01-22 20:48:18 +0900

    Time: 25 ms (64.92%), Space: 23.8 MB (35.68%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,35 +1,14 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int op = 0, op0 = 0, op1 = 0;
+        long diff = 0, sqDiff = 0;
         
-        for (int n:nums)
-            op ^= n;
-        
-        for (int i = 1; i <= nums.size(); ++i)
-            op ^= i;
-        
-        int rightBitMost = op & ~(op - 1);
-        
-        for (int n:nums){
-            if ((n & rightBitMost) != 0)
-                op1 ^= n;
-            else
-                op0 ^= n;
+        for (int i = 0; i < nums.size(); ++i){
+            diff += (i + 1) - nums[i];
+            sqDiff += (i + 1) * (i + 1) - nums[i] * nums[i];
         }
         
-        for (int i = 1; i <= nums.size(); ++i){
-            if ((i & rightBitMost) != 0)
-                op1 ^= i;
-            else
-                op0 ^= i;
-        }
-        
-        for (int n:nums){
-            if (n == op0)
-                return vector<int>{op0, op1};
-        }
-        
-        return vector<int>{op1, op0};
+        int sum = sqDiff / diff;
+        return vector<int>{(int)(sum - diff) >> 1, (int)(sum + diff) >> 1};
     }
 };
