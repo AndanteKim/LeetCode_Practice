9aab28ccac90990aba4a71565f700e4de23bf9be
Andrew Kim <ksb0724@utexas.edu>
2024-01-22 20:29:45 +0900

    Time: 5 ms (100.00%), Space: 23.8 MB (35.68%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int missing = 1, dup = -1;
+        int op = 0, op0 = 0, op1 = 0;
+        
+        for (int n:nums)
+            op ^= n;
+        
+        for (int i = 1; i <= nums.size(); ++i)
+            op ^= i;
+        
+        int rightBitMost = op & ~(op - 1);
         
         for (int n:nums){
-            if (nums[abs(n) - 1] < 0)
-                dup = abs(n);
+            if ((n & rightBitMost) != 0)
+                op1 ^= n;
             else
-                nums[abs(n) - 1] *= -1;
+                op0 ^= n;
         }
         
-        for (int i = 1; i < nums.size(); ++i){
-            if (nums[i] > 0)
-                missing = i + 1;
+        for (int i = 1; i <= nums.size(); ++i){
+            if ((i & rightBitMost) != 0)
+                op1 ^= i;
+            else
+                op0 ^= i;
         }
         
-        return vector<int>{dup, missing};
+        for (int n:nums){
+            if (n == op0)
+                return vector<int>{op0, op1};
+        }
+        
+        return vector<int>{op1, op0};
     }
 };
