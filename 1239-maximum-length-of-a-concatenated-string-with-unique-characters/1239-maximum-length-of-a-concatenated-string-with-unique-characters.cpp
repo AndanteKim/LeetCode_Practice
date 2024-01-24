a6901371ac73adce7bcd9e8a88d10a83473b04b2
Andrew Kim <ksb0724@utexas.edu>
2024-01-24 23:13:28 +0900

    Time: 26 ms (71.50%), Space: 20.1 MB (64.82%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -1,27 +1,59 @@
 class Solution {
+private:
+    int addWord(string& word, unordered_set<int>& results){
+        // Initialize an int used as a character set
+        int charBitset = 0, ans = 0;
+        
+        for (char& c:word){
+            // Define character mask for current char
+            int mask = 1 << (c - 'a');
+            
+            // Bitwise AND check using chaaracter mask
+            // to see if char already found and if so, exit
+            if ((charBitset & mask) > 0)
+                return 0;
+            
+            // Mark char as seen in charBitset
+            charBitset += mask;
+        }
+        
+        // If the initial bitset is already a known results,
+        // then any possible new results will have already been found
+        if (results.find(charBitset + (word.size() << 26)) != results.end())
+            return 0;
+        
+        // Iterate through previous results only
+        // and store new results in newResults
+        unordered_set<int> newResults;
+        for (int res:results){
+            // If the two bitsets overlap, skip to the next result
+            if (res & charBitset)
+                continue;
+            
+            // Build the new entry with bit manipulation
+            int newResLen = (res >> 26) + word.size();
+            int newCharBitset = (charBitset + res) & ((1 << 26) - 1);
+            
+            // Merge the two into one, add it to results,
+            // and keep track of the longest so far
+            newResults.insert((newResLen << 26) + newCharBitset);
+            ans = max(ans, newResLen);
+        }
+        
+        // Merge newResults into results
+        results.insert(newResults.begin(), newResults.end());
+        return ans;
+    }
+    
 public:
     int maxLength(vector<string>& arr) {
-        // Initialize results with an empty string
-        // from which to build all future results
-        vector<string> results{""};
+        // Results initialized as a set to prevent duplicates
+        unordered_set<int> results({0});
         int ans = 0;
         
-        for (string& word: arr){
-            // We only want to iterate through results
-            // that existed prior to this loop
-            for (int i = 0; i < results.size(); ++i){
-                // Form a new result combination and
-                // use a set to check for duplicate characters
-                string newResults = results[i] + word;
-                unordered_set<int> chars(newResults.begin(), newResults.end());
-                if (newResults.size() != chars.size())
-                    continue;
-                
-                // Add valid options to results and
-                // keep track of the longest so far
-                results.push_back(newResults);
-                ans = max(ans, (int)newResults.size());
-            }
+        // check each string in arr and find the best length
+        for (string& word:arr){
+            ans = max(ans, addWord(word, results));
         }
         
         return ans;
