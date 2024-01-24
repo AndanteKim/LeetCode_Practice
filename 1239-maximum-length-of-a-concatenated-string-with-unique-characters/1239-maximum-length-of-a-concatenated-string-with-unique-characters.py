da03efde7f73f717770b5515838053f614b4e5cc
Andrew Kim <ksb0724@utexas.edu>
2024-01-24 22:45:35 +0900

    Time: 49 ms (99.28%), Space: 20.6 MB (29.31%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -1,24 +1,45 @@
 class Solution:
     def maxLength(self, arr: List[str]) -> int:
-        # Initialize results with an empty string
-        # from which to build all future results
-        results, ans = [""], 0
+        # Results initialized as a Set to prevent duplicates
+        results, ans = set([0]), 0
         
+        # Check eeach string in arr and find the best length
         for word in arr:
-            # We only want to iterate through results
-            # that exisited prior this loop
-            
-            for i in range(len(results)):
-                # Form a new result combination and
-                # use a set to check for duplicate characters
-                new_res = results[i] + word
-                if len(new_res) != len(set(new_res)):
-                    continue
-                
-                # Add valid options results and
-                # keep track of the longest so far
-                results.append(new_res)
-                ans = max(ans, len(new_res))
-       
+            ans = max(ans, self.add_word(word, results))
         return ans
+
+    def add_word(self, word: str, results: List[str]) -> int:
+        # Initialize an int used as a character set
+        char_bitset, ans = 0, 0
         
+        for ch in word:
+            # Define character mask for current chaar
+            mask = 1 << ord(ch) - 97
+            
+            # Bitwise AND check using character mask to see
+            # if char already found and if so, exit
+            if char_bitset & mask > 0:
+                return 0
+            
+            # Mark char as seen in char_bitset
+            char_bitset += mask
+        
+        # If the initial bitset is already a known result
+        # then any possible new results will have already been found
+        if char_bitset + (len(word) << 26) in results:
+            return 0
+        
+        # Iterate through previous results only
+        for res in list(results):
+            # If the two bitsets overlap, skip to the next result
+            if res & char_bitset:
+                continue
+                
+            # Build the new entry with bit manipulation
+            new_res_len = (res >> 26) + len(word)
+            new_char_bitset = char_bitset + res & ((1 << 26) - 1)
+            
+            # Merge the two into one, add it to results and keep track of the longest so far
+            results.add((new_res_len << 26) + new_char_bitset)
+            ans = max(ans, new_res_len)
+        return ans
