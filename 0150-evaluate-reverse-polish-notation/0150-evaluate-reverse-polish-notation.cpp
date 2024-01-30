7861a8587d4237c90dfc8f7071323074f13409f2
Andrew Kim <ksb0724@utexas.edu>
2024-01-31 04:21:39 +0900

    Time: 472 ms (5.08%), Space: 15.4 MB (15.06%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,32 +1,47 @@
 class Solution {
+private:
+    void delete2AtIndex(vector<string>& tokens, int d, int length){
+        for (int i = d; i < length - 2; ++i)
+            tokens[i] = tokens[i + 2];
+    }
+    
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<string> s;
-        int ans;
-        unordered_set<string> ops4{"+", "-", "*", "/"};
+        unordered_map<string, function<int(int, int)>> operations;
+        operations["+"] = [](int a, int b) -> int{
+            return a + b;
+        };
+        operations["-"] = [](int a, int b) -> int{
+            return a - b;
+        };
+        operations["*"] = [](int a, int b) -> int{
+            return a * b;
+        };
+        operations["/"] = [](int a, int b) -> int{
+            return a / b;
+        };
         
-        for (string& token:tokens){
-            if (ops4.find(token) != ops4.end()){
-                int second = stoi(s.top());
-                s.pop();
-                int first = stoi(s.top()), ops = 0;
-                s.pop();
-                
-                if (token == "+")
-                    ops += first + second;
-                else if (token == "-")
-                    ops += first - second;
-                else if (token == "*")
-                    ops += first * second;
-                else
-                    ops += first / second;
-                s.push(to_string(ops));
-            }
-            else{
-                s.push(token);
-            }
+        int currPos = 0, length = tokens.size();
+        while (length > 1){
+            
+            // Move the current position and numbers from the list
+            while (operations.find(tokens[currPos]) == operations.end())
+                ++currPos;
+            
+            // Extract the operator and numbers from the list.
+            string opr = tokens[currPos];
+            int n1 = stoi(tokens[currPos - 2]), n2 = stoi(tokens[currPos - 1]);
+            
+            // Calculate the result to overwrite the operator with.
+            tokens[currPos] = to_string(operations[opr](n1, n2));
+            
+            // Remove the numbers and move the pointer to the position
+            // after the new number we just added.
+            delete2AtIndex(tokens, currPos - 2, length);
+            --currPos;
+            length -= 2;
         }
-        ans = stoi(s.top());
-        return ans;
+        
+        return stoi(tokens[0]);
     }
 };
