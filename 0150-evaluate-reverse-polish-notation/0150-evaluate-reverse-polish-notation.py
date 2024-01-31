b5144d2a4b228f9c34af3a9c503d4aa44f8b69ae
Andrew Kim <ksb0724@utexas.edu>
2024-01-31 18:09:48 +0900

    Time: 67 ms (64.17%), Space: 17.1 MB (72.33%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -7,25 +7,14 @@ class Solution:
             "/": lambda a, b: int(a / b)
         }
         
-        curr_pos = 0
+        stack = []
+        for token in tokens:
+            if token in operations:
+                n2 = stack.pop()
+                n1 = stack.pop()
+                opr = operations[token]
+                stack.append(opr(n1, n2))
+            else:
+                stack.append(int(token))
         
-        while len(tokens) > 1:
-            # Move the current position pointer to the next operator
-            while tokens[curr_pos] not in "+-*/":
-                curr_pos += 1
-            
-            # Extract the operator and numbers from the list
-            operator = tokens[curr_pos]
-            n1, n2 = int(tokens[curr_pos - 2]), int(tokens[curr_pos - 1])
-            
-            # Calculate the result to overwrite the operator with
-            operation = operations[operator]
-            tokens[curr_pos] = operation(n1, n2)
-            
-            # Remove the numbers and move the pointer to the position
-            # after the new number we just added.
-            tokens.pop(curr_pos - 2)
-            tokens.pop(curr_pos - 2)
-            curr_pos -= 1
-            
-        return int(tokens[0])
+        return stack.pop()
