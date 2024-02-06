0dfa64d4cf9556ab38f74cdde55719da3933da6a
Andrew Kim <ksb0724@utexas.edu>
2024-02-06 19:40:47 +0900

    Time: 107 ms (58.43%), Space: 27.7 MB (5.15%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -3,51 +3,45 @@ class Solution:
         if not t or not s:
             return ""
         
-        # Dictionary keeping a count of all the unique characters in t.
         dict_t = Counter(t)
-        
-        # Number of unique characters in t, needing to be present in the desired window.
         required = len(dict_t)
         
-        # left and right pointer
-        l, r = 0, 0
+        # Filter all the characters from s into a new list along with their index.
+        # The filtering criteria is that the character should be present in t.
+        filtered_s = []
         
-        # Formed is used to keep track of how many unique characters in t are present in the current window in its desired frequency
-        # e.g. If t is "AABC", then the window must have two A's, one B and one C. Thus, formed would be 3 when all these conditions are met.
-        formed = 0
-        
-        # Dictionary which keeps a count of all the unique characters in the current window
+        for i, c in enumerate(s):
+            if c in dict_t:
+                filtered_s.append((i, c))
+                
+        left, formed = 0, 0
         window_counts = dict()
+        ans = float("inf"), None, None
         
-        # ans tuple of the form (window length, left, right)
-        ans = float('inf'), None, None
+        # Look for the characters only in the filtered list instead of entire s. This helps to reduce our search.
+        # Hence, we follow the sliding window approach on as small list.
         
-        while r < len(s):
-            # Add one character from the right to the window
-            character = s[r]
-            window_counts[character] = window_counts.get(character, 0) + 1
+        for right in range(len(filtered_s)):
+            ch = filtered_s[right][1]
+            window_counts[ch] = window_counts.get(ch, 0) + 1
             
-            # If the frequency of the current character added equals to the desired count in t then increment the formed count by 1.
-            if character in dict_t and window_counts[character] == dict_t[character]:
+            if window_counts[ch] == dict_t[ch]:
                 formed += 1
-            
-            # Try and contract the window till the point where it ceases to be 'desirable'.
-            while l <= r and formed == required:
-                character = s[l]
                 
-                # Save the smallest window until now
-                if r - l + 1 < ans[0]:
-                    ans = (r - l + 1, l, r)
+            # If the current window has all the characters in desired frequencies i.e. t is present in the window
+            while left <= right and formed == required:
+                ch = filtered_s[left][1]
+                
+                # Save the smallest window until now.
+                end, start = filtered_s[right][0], filtered_s[left][0]
+                if end - start + 1 < ans[0]:
+                    ans = (end - start + 1, start, end)
                     
-                # The character at the position pointed by the 'left' pointer is no longer a part of the window.
-                window_counts[character] -= 1
-                if character in dict_t and window_counts[character] < dict_t[character]:
+                window_counts[ch] -= 1
+                if window_counts[ch] < dict_t[ch]:
                     formed -= 1
-                
-                # Move the left pointer ahead, this would help to look for a new window.
-                l += 1
-           
-            # Keep expanding the window once we're dong contracting
-            r += 1
+                left += 1
+         
+        return "" if ans[0] == float("inf") else s[ans[1] : ans[2] + 1]
         
-        return "" if ans[0] == float("inf") else s[ans[1]: ans[2] + 1]
+                
