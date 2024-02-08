bcf60d8f884a05a80e2bbe74c8026643ae048bc6
Andrew Kim <ksb0724@utexas.edu>
2024-02-08 18:21:09 +0900

    Time: 5 ms (97.77%), Space: 9.6 MB (62.10%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,20 +1,34 @@
 class Solution {
-public:
-    int numSquares(int n) {
-        vector<int> sqNums, dp(n + 1, 10'001);
-        for (int i = 0; i <= (int)sqrt(n); ++i)
-            sqNums.push_back(i * i);
+private:
+    bool isDividedBy(int n, int count, unordered_set<int>& squareNums){
+        /*
+        return: true if "n" can be decomposed into "count" number of perfect square nums
+        e.g. n = 12, count = 3: true
+             n = 12, count = 2: false
+        */
         
-        dp[0] = 0;
-        int ans = 0;
-        for (int i = 1; i <= n; ++i){
-            for (int sq:sqNums){
-                if (i < sq)
-                    break;
-                dp[i] = min(dp[i], dp[i - sq] + 1);
-            }
+        if (count == 1)
+            return squareNums.find(n) != squareNums.end();
+        
+        for (int k : squareNums){
+            if (isDividedBy(n - k, count - 1, squareNums))
+                return true;
         }
         
-        return dp.back();
+        return false;
+    }
+    
+public:
+    int numSquares(int n) {
+        unordered_set<int> squareNums;
+        
+        for (int i = 1; i <= (int)sqrt(n); ++i)
+            squareNums.insert(i * i);
+        
+        for (int count = 1; count <= n; ++count)
+            if (isDividedBy(n, count, squareNums))
+                return count;
+        
+        return -1;
     }
 };
