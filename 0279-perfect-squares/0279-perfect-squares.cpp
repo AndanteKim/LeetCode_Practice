8ce08013722eb7a48ee7561d0297779cd522c0e1
Andrew Kim <ksb0724@utexas.edu>
2024-02-08 18:53:21 +0900

    Time: 111 ms (62.43%), Space: 40.5 MB (9.63%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,34 +1,34 @@
 class Solution {
-private:
-    bool isDividedBy(int n, int count, unordered_set<int>& squareNums){
-        /*
-        return: true if "n" can be decomposed into "count" number of perfect square nums
-        e.g. n = 12, count = 3: true
-             n = 12, count = 2: false
-        */
-        
-        if (count == 1)
-            return squareNums.find(n) != squareNums.end();
-        
-        for (int k : squareNums){
-            if (isDividedBy(n - k, count - 1, squareNums))
-                return true;
-        }
-        
-        return false;
-    }
-    
 public:
     int numSquares(int n) {
-        unordered_set<int> squareNums;
-        
+        // List of square numbers that are less than 'n'
+        vector<int> squareNumbers;
         for (int i = 1; i <= (int)sqrt(n); ++i)
-            squareNums.insert(i * i);
+            squareNumbers.push_back(i * i);
         
-        for (int count = 1; count <= n; ++count)
-            if (isDividedBy(n, count, squareNums))
-                return count;
+        unordered_set<int> queue{n};
+        int level = 0;
         
-        return -1;
+        while (!queue.empty()){
+            ++level;
+            /*! important: use set() instead of list() to eliminate the redundancy
+            which would even produce a 5-times speedup, 200ms vs 1000ms.
+            */
+            unordered_set<int> nextQueue;
+            // construct the queue for the next level
+            for (int remainder:queue){
+                for (int squareNum:squareNumbers){
+                    if (remainder == squareNum)
+                        return level; // find the node
+                    else if (remainder < squareNum)
+                        break;
+                    else
+                        nextQueue.insert(remainder - squareNum);
+                }
+            }
+            queue = nextQueue;
+        }
+        
+        return level;
     }
 };
