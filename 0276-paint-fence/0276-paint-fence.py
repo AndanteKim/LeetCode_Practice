261e55757d8581828e8ca7d6b028311e6f49a3b9
Andrew Kim <ksb0724@utexas.edu>
2024-02-11 02:23:16 +0900

    Time: 42 ms (23.05%), Space: 16.7 MB (43.07%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.py
+++ 0276-paint-fence/0276-paint-fence.py
@@ -1,13 +1,15 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        @lru_cache(maxsize = None)
-        def total_ways(i: int) -> int:
-            if i == 1:
-                return k
-            
-            if i == 2:
-                return k * k
-            
-            return (k - 1) * (total_ways(i - 1) + total_ways(i - 2))
+        if n == 1:
+            return k
         
-        return total_ways(n)
+        if n == 2:
+            return k * k
+        
+        total_ways = [0] * (n + 1)
+        total_ways[1], total_ways[2] = k, k * k
+        
+        for i in range(3, n + 1):
+            total_ways[i] = (k - 1) * (total_ways[i - 1] + total_ways[i - 2])
+        
+        return total_ways[n]
