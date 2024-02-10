1828d4b79683c94c6acf79d8086e96468913eb53
Andrew Kim <ksb0724@utexas.edu>
2024-02-11 02:32:06 +0900

    Time: 0 ms (100.00%), Space: 7.2 MB (36.09%) - LeetHub

--- 0276-paint-fence/0276-paint-fence.cpp
+++ 0276-paint-fence/0276-paint-fence.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int numWays(int n, int k) {
+        // Space optimization with tabulation
         if (n == 1) return k;
         if (n == 2) return k * k;
         
-        vector<int> totalWays(n + 1);
-        totalWays[1] = k, totalWays[2] = k * k;
+        int twoPostsBack = k, onePostBack = k * k;
         for (int i = 3; i <= n; ++i){
-            totalWays[i] = (k - 1) * (totalWays[i - 1] + totalWays[i - 2]);
+            int curr = (k - 1) * (twoPostsBack + onePostBack);
+            twoPostsBack = onePostBack;
+            onePostBack = curr;
         }
         
-        return totalWays[n];
+        return onePostBack;
     }
 };
