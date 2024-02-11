295a032b859b5cfea96ad5b0844dafef48654921
Andrew Kim <ksb0724@utexas.edu>
2024-02-12 01:09:51 +0900

    Time: 1645 ms (26.07%), Space: 24.2 MB (72.91%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.py
@@ -1,21 +1,28 @@
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
-        @lru_cache(maxsize = None)
-        def dp(row: int, col1: int, col2: int) -> int:
-            if col1 < 0 or col1 >= n or col2 < 0 or col2 >= n:
-                return float('-inf')
-            
-            # Current cell
-            ans = 0
-            ans += grid[row][col1]
-            if col1 != col2:
-                ans += grid[row][col2]
-            
-            # transition
-            if row != m - 1:
-                ans += max(dp(row + 1, new_col1, new_col2) for new_col1 in [col1, col1 + 1, col1 - 1] for new_col2 in [col2, col2 + 1, col2 - 1])
-            
-            return ans
-        
         m, n = len(grid), len(grid[0])
-        return dp(0, 0, n - 1)
+        dp = [[[0] * n for __ in range(n)] for _ in range(m)]
+        
+        for row in range(m - 1, -1, -1):
+            for col1 in range(n):
+                for col2 in range(n):
+                    ans = 0
+                    # Current cell
+                    ans += grid[row][col1]
+                    
+                    if col1 != col2:
+                        ans += grid[row][col2]
+                    
+                    # transition
+                    if row != m - 1:
+                        curr = 0
+                        for new_col1 in range(col1 - 1, col1 + 2):
+                            for new_col2 in range(col2 - 1, col2 + 2):
+                                if new_col1 >= 0 and new_col1 < n and new_col2 >= 0 and new_col2 < n:
+                                    curr = max(curr, dp[row + 1][new_col1][new_col2])
+                        
+                        ans += curr
+                    
+                    dp[row][col1][col2] = ans
+                    
+        return dp[0][0][n - 1]
