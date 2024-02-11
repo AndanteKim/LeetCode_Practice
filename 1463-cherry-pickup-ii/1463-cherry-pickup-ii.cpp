8eafe66c5aa2ada2a7dd4786fcab542b84eba5b9
Andrew Kim <ksb0724@utexas.edu>
2024-02-12 01:19:36 +0900

    Time: 58 ms (73.59%), Space: 19.4 MB (11.09%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,36 +1,33 @@
 class Solution {
-private:
-    int m, n;
-    int dp(int row, int col1, int col2, vector<vector<vector<int>>>& memo, vector<vector<int>>& grid){
-        if (col1 < 0 || col1 >= n || col2 < 0 || col2 >= n)
-            return 0;
-        // check cache
-        if (memo[row][col1][col2] != -1)
-            return memo[row][col1][col2];
-        
-        // Current cell
-        int ans = grid[row][col1];
-        if (col1 != col2)
-            ans += grid[row][col2];
-        
-        // transition
-        if (row != m - 1){
-            int mx = 0;
-            for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; ++newCol1){
-                for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; ++newCol2)
-                    mx = max(mx, dp(row + 1, newCol1, newCol2, memo, grid));
-            }
-            ans += mx;
-        }
-        memo[row][col1][col2] = ans;
-        return ans;
-    }
-    
 public:
     int cherryPickup(vector<vector<int>>& grid) {
-        this -> m = grid.size(), this -> n = grid[0].size();
-        // initial all elements to -1 to mark unseen
-        vector memo(m, vector(n, vector<int>(n, -1)));
-        return dp(0, 0, n - 1, memo, grid);
+        int m = grid.size(), n = grid[0].size();
+        vector dp(m, vector(n, vector<int>(n, 0)));
+        
+        for (int row = m - 1; row >= 0; --row){
+            for (int col1 = 0; col1 < n; ++col1){
+                for (int col2 = 0; col2 < n; ++col2){
+                    // current cell
+                    int ans = grid[row][col1];
+                    
+                    if (col1 != col2)
+                        ans += grid[row][col2];
+                    // transition
+                    if (row != m - 1){
+                        int curr = 0;
+                        for (int newCol1 = col1 - 1; newCol1 <= col1 + 1; ++newCol1){
+                            for (int newCol2 = col2 - 1; newCol2 <= col2 + 1; ++newCol2){
+                                if (newCol1 >= 0 && newCol1 < n && newCol2 >= 0 && newCol2 < n)
+                                    curr = max(curr, dp[row + 1][newCol1][newCol2]);
+                            }
+                        }
+                        ans += curr;
+                    }
+                    dp[row][col1][col2] = ans;
+                }
+            }
+        }
+        
+        return dp[0][0][n - 1];
     }
 };
