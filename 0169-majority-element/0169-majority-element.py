1df007603d9627998e9c64caca37b1bb17b52371
Andrew Kim <ksb0724@utexas.edu>
2024-02-12 15:05:06 +0900

    Time: 133 ms (73.51%), Space: 17.8 MB (99.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,13 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        ans, count = float('inf'), 0
+        ans, count = 0, 0
         
         for num in nums:
-            if ans != num:
-                if ans == float('inf') or count == 0:
-                    ans = num
-                    count += 1
-                else:
-                    count -= 1
+            if count == 0:
+                ans, count = num, 1
+            elif ans != num:
+                count -= 1
             else:
                 count += 1
-            
+                
         return ans
