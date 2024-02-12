4eb0e4ae2980cd01f374b755151fb3cb6567e339
Andrew Kim <ksb0724@utexas.edu>
2024-02-12 15:07:17 +0900

    Time: 16 ms (42.65%), Space: 22.1 MB (24.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int ans = INT_MAX, count = 0;
+        int ans = 0, count = 0;
         
-        for (int num : nums){
+        for (int num:nums){
             if (ans != num){
                 if (count == 0){
                     ans = num;
                     ++count;
                 }
-                else
-                    --count;
+                else --count;
             }
             else
                 ++count;
+                
         }
         
         return ans;
