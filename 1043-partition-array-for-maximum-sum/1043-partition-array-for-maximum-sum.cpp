1b5649e503b4f45488aa9ef2639345d260840c5c
Andrew Kim <ksb0724@utexas.edu>
2024-02-13 18:08:40 +0900

    Time: 11 ms (40.23%), Space: 10.9 MB (50.12%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+private:
+    int n;
+    int dp(int start, int k, vector<int>& memo, vector<int>& arr){
+        if (start >= n)
+            return 0;
+        
+        // Return the already calculated answer
+        if (memo[start] != -1)
+            return memo[start];
+        
+        int currMax = 0, ans = 0;
+        int end = min(n, start + k);
+        for (int i = start; i < end; ++i){
+            currMax = max(currMax, arr[i]);
+            // Store the maximum of all options for the current subarray
+            ans = max(ans, currMax * (i - start + 1) + dp(i + 1, k, memo, arr));
+        }
+        
+        // Store the answer to be reused
+        return memo[start] = ans;
+    }
+    
+public:
+    int maxSumAfterPartitioning(vector<int>& arr, int k) {
+        this -> n = arr.size();
+        vector<int> memo(n, -1);
+        
+        return dp(0, k, memo, arr);
+    }
+};
