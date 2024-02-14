5c7b0d07767e4f29a7e4f8d50aaec56a4c258183
Andrew Kim <ksb0724@utexas.edu>
2024-02-15 01:11:07 +0900

    Time: 38 ms (30.24%), Space: 16.5 MB (44.15%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.py
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -1,29 +1,12 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        def backtrack(curr: int, start: int, digit: int) -> None:
-            if digit == 0:
-                curr += start
-                if low <= curr <= high:
-                    ans.append(curr)
-                return
-            
-            backtrack(curr + (start * (10 ** digit)), start + 1, digit - 1)
-            
-        ans, low_digit, high_digit = [], 0, 0
-        lowest, highest = low, high
+        sample = "123456789"
+        n, nums = 10, []
         
-        while lowest >= 10:
-            lowest //= 10
-            low_digit += 1
-            
-            
-        while highest >= 10:
-            highest //= 10
-            high_digit += 1
-            
-        for digit in range(low_digit, high_digit + 1):
-            for i in range(1, 10 - digit):
-                curr = i * (10 ** digit)
-                backtrack(curr, i + 1, digit - 1)
-        
-        return ans
+        for length in range(len(str(low)), len(str(high)) + 1):
+            for start in range(n - length):
+                num = int(sample[start:start+length])
+                if num >= low and num <= high:
+                    nums.append(num)
+                    
+        return nums
