aeaf717943f2d5f15326d43568822cb9ae169de9
Andrew Kim <ksb0724@utexas.edu>
2024-02-15 01:11:01 +0900

    Time: 0 ms (100.00%), Space: 7.5 MB (31.48%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,37 +1,16 @@
 class Solution {
-private:
-    int low, high;
-    void backtrack(int curr, int start, int digit, vector<int>& ans){
-        if (digit == 0){
-            curr += start;
-            if (low <= curr && curr <= high)
-                ans.push_back(curr);
-            return;
-        }
-        
-        backtrack(curr + (start * pow(10, digit)), start + 1, digit - 1, ans);
-    }
-    
 public:
     vector<int> sequentialDigits(int low, int high) {
+        string sample = "123456789";
+        int n = 10, lowSz = to_string(low).size(), highSz = to_string(high).size();
         vector<int> ans;
-        this -> low = low, this -> high = high;
-        int lowDigit = 0, highDigit = 0, lowest = low, highest = high;
         
-        while (lowest >= 10){
-            lowest /= 10;
-            ++lowDigit;
-        }
         
-        while (highest >= 10){
-            highest /= 10;
-            ++highDigit;
-        }
-        
-        for (int digit = lowDigit; digit <= highDigit; ++digit){
-            for (int i = 1; i < 10 - digit; ++i){
-                int curr = i * pow(10, digit);
-                backtrack(curr, i + 1, digit - 1, ans);
+        for (int length = lowSz; length <= highSz; ++length){
+            for (int start = 0; start < n - length; ++start){
+                int num = stoi(sample.substr(start, length));
+                if (num >= low && num <= high)
+                    ans.push_back(num);
             }
         }
         
