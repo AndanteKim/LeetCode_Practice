997fc9d7f673a8445deec4f91d03e3743fcf2d5a
Andrew Kim <ksb0724@utexas.edu>
2024-02-14 17:29:15 +0900

    Time: 984 ms (96.01%), Space: 47.6 MB (57.40%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -1,21 +1,24 @@
 class Solution:
     def rearrangeArray(self, nums: List[int]) -> List[int]:
-        ans, pos, neg, n = [], [], [], len(nums)
+        n = len(nums)
+        
+        # Initialize an answer array of size n
+        ans = [0] * n
+        
+        # Initializing two pointers to track even and 
+        # odd indices for positive and negative integers respectively
+        pos_idx, neg_idx = 0, 1
         
         for i in range(n):
             if nums[i] > 0:
-                pos.append(i)
+                # Placing the positive integer at the
+                # desired index in ans and incrementing pos_idx by 2
+                ans[pos_idx] = nums[i]
+                pos_idx += 2
             else:
-                neg.append(i)
-        
-        idx = 0
-        while idx < max(len(pos), len(neg)):
-            if idx < len(pos):
-                ans.append(nums[pos[idx]])
-            
-            if idx < len(neg):
-                ans.append(nums[neg[idx]])
-            
-            idx += 1
+                # Placing the negative integer at the
+                # desired index in ans and incrementing neg_idx by 2
+                ans[neg_idx] = nums[i]
+                neg_idx += 2
             
         return ans
