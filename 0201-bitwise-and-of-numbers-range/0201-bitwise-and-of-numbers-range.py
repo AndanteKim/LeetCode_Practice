b2c4df867b2eb8076052ef8f49f39b47f9d7a873
Andrew Kim <ksb0724@utexas.edu>
2024-02-22 01:49:39 +0900

    Time: 59 ms (31.03%), Space: 16.5 MB (81.69%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -1,11 +1,7 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        shift = 0
-        
-        # Find the common 1-bits
         while left < right:
-            left >>= 1
-            right >>= 1
-            shift += 1
-        
-        return left << shift     
+            # turn off the rightmost 1-bit
+            right = right & (right - 1)
+            
+        return left & right
