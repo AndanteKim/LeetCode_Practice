0b56ec26cd13bd20e72ec1136eaf57630d4a9b79
Andrew Kim <ksb0724@utexas.edu>
2024-02-26 18:54:42 +0900

    Time: 652 ms (56.36%), Space: 129.6 MB (56.36%) - LeetHub

--- 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
@@ -1,79 +1,80 @@
-class DSU{
+class Graph{
 private:
-    vector<int> dsu, size;
+    int n;
+    vector<int> parent, compSize;
+    
+    int getParent(int x){
+        if (parent[x] == x) return x;
+        return parent[x] = getParent(parent[x]);
+    }
+    
+    void unionSet(int x, int y){
+        int parx = getParent(x), pary = getParent(y);
+        if (parx != pary){
+            if (compSize[parx] < compSize[pary]){
+                swap(parx, pary);
+            }
+            
+            parent[pary] = parx;
+            compSize[parx] += compSize[pary];
+        }
+    }
     
 public:
-    DSU(int N){
-        dsu.resize(N + 1);
-        size.resize(N + 1);
-        
-        for (int i = 0; i <= N; ++i){
-            dsu[i] = i;
-            size[i] = 1;
-        }
+    Graph(int n = 0) : n(n){
+        parent.resize(n);
+        compSize.resize(n, 1);
+        iota(parent.begin(), parent.end(), 0);
     }
     
-    int find(int x){
-        return (dsu[x] == x)? x : dsu[x] = find(dsu[x]);
+    void addEdge(int x, int y){
+        unionSet(x, y);
     }
     
-    void merge(int x, int y){
-        int fx = find(x), fy = find(y);
-        
-        if (fx == fy) return;
-        
-        if (size[fx] > size[fy]){
-            int temp = fx;
-            fx = fy;
-            fy = temp;
-        }
-        
-        dsu[fx] = fy;
-        size[fy] += size[fx];
+    bool isConnected(){
+        return compSize[getParent(0)] == n;
     }
 };
 
+
+
 class Solution {
+private:
+    vector<int> getPrimeFactors(int x){
+        vector<int> primeFactors;
+        
+        for (int i = 2; i * i <= x; ++i){
+            if (x % i == 0){
+                primeFactors.push_back(i);
+                while (x % i == 0)
+                    x /= i;
+            }
+        }
+        
+        if (x != 1) primeFactors.push_back(x);
+        
+        return primeFactors;
+    }
+    
 public:
     bool canTraverseAllPairs(vector<int>& nums) {
-        int MAX = 100'000, N = nums.size();
-        vector<bool> has(MAX + 1);
+        int n = nums.size();
+        if (n == 1) return true;
         
-        for (int num:nums) has[num] = true;
-        
-        // edge cases
-        if (N == 1) return true;
-        
-        if (has[1]) return false;
-        
-        // the general solution
-        vector<int> sieve(MAX + 1);
-        
-        for (int d = 2; d <= MAX; ++d){
-            if (sieve[d] == 0){
-                for (int v = d; v <= MAX; v += d)
-                    sieve[v] = d;
+        Graph g(n);
+        unordered_map<int, int> seen;
+        for (int i = 0; i < n; ++i){
+            if (nums[i] == 1) return false;
+            
+            vector<int> primeFactors = getPrimeFactors(nums[i]);
+            for (int prime : primeFactors){
+                if (seen.find(prime) != seen.end())
+                    g.addEdge(i, seen[prime]);
+                else
+                    seen[prime] = i;
             }
         }
         
-        DSU uf = DSU(2 * MAX + 1);
-        for (int x:nums){
-            int val = x;
-            while (val > 1){
-                int prime = sieve[val], root = prime + MAX;
-                
-                if (uf.find(root) != uf.find(x))
-                    uf.merge(root, x);
-                
-                while (val % prime == 0) val /= prime;
-            }
-        }
-        
-        int cnt = 0;
-        for (int i = 2; i <= MAX; ++i){
-            if (has[i] && uf.find(i) == i) ++cnt;
-        }
-        
-        return cnt == 1;
+        return g.isConnected();
     }
 };
