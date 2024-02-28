b7cdc4015e72c74c3face288f9f664f869e76da5
Andrew Kim <ksb0724@utexas.edu>
2024-02-28 17:01:36 +0900

    Time: 6 ms (83.03%), Space: 20.1 MB (88.43%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -10,24 +10,25 @@
  * };
  */
 class Solution {
-public:
-    int findBottomLeftValue(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        int ans = 0;
+private:
+    int maxDepth = -1, bottomLeftValue = 0;
+    void dfs(TreeNode* current, int depth){
+        if (!current) return;
         
-        while (!q.empty()){
-            int sz = q.size();
-            ans = q.front() -> val;
-            
-            while (sz-- > 0){
-                TreeNode* node = q.front();
-                q.pop();
-                if (node -> left) q.push(node -> left);
-                if (node -> right) q.push(node -> right);
-            }
+        // If true, we discovered a new level
+        if (depth > maxDepth){
+            maxDepth = depth;
+            bottomLeftValue = current -> val;
         }
         
-        return ans;
+        dfs(current -> left, depth + 1);
+        dfs(current -> right, depth + 1);
+    }
+    
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        dfs(root, 0);
+        
+        return bottomLeftValue;
     }
 };
