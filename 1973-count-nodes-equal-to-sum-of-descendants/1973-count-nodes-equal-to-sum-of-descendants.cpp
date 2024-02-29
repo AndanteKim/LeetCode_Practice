280bb1ff5f90c9b279efed656e403d0356ee5f41
Andrew Kim <ksb0724@utexas.edu>
2024-03-01 05:35:38 +0900

    Time: 235 ms (95.95%), Space: 198.4 MB (95.95%) - LeetHub

---
+++ 1973-count-nodes-equal-to-sum-of-descendants/1973-count-nodes-equal-to-sum-of-descendants.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int count = 0;
+    long dfs(TreeNode* curr){
+        if (!curr) return 0;
+        
+        long left = dfs(curr -> left), right = dfs(curr -> right);
+        
+        if (curr -> val == (long)left + (long)right) ++count;
+        
+        return left + right + curr -> val;
+    }
+    
+public:
+    int equalToDescendants(TreeNode* root) {
+        dfs(root);
+        
+        return count;
+    }
+};
