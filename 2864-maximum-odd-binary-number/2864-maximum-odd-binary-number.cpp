08bd484d9ed24acf401d0275fdb060d017ec4998
Andrew Kim <ksb0724@utexas.edu>
2024-03-04 19:12:10 +0900

    Time: 4 ms (59.38%), Space: 8.4 MB (76.99%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        int zeros = count(s.begin(), s.end(), '0'), n = s.size();
-        int ones = n - zeros;
-        string ans = "";
+        sort(s.begin(), s.end());
+        int secondLast = s.size() - 2;
         
-        if (ones > 1) ans += string(ones - 1, '1');
-        ans += string(zeros, '0');
-        return ans + '1';
+        for (int i = 0; i < s.size() >> 1; ++i){
+            swap(s[i], s[secondLast - i]);
+        }
+        
+        return s;
     }
 };
