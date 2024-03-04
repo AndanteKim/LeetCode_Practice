8663af38dc6015a77b73eda63c09d009e6ae0060
Andrew Kim <ksb0724@utexas.edu>
2024-03-04 23:02:25 +0900

    Time: 4 ms (59.38%), Space: 8.1 MB (96.59%) - LeetHub

--- 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
+++ 2864-maximum-odd-binary-number/2864-maximum-odd-binary-number.cpp
@@ -1,13 +1,24 @@
 class Solution {
 public:
     string maximumOddBinaryNumber(string s) {
-        sort(s.begin(), s.end());
-        int secondLast = s.size() - 2;
+        // Get n
+        int n = s.size(), left = 0, right = n - 1;
         
-        for (int i = 0; i < s.size() >> 1; ++i){
-            swap(s[i], s[secondLast - i]);
+        while (left <= right){
+            // Increment left if equals '1'.
+            if (s[left] == '1') ++left;
+            
+            // Decrement right if equals '0'.
+            if (s[right] == '0') --right;
+            
+            // Swap if neither pointer can be iterated
+            if (left <= right && s[left] == '0' && s[right] == '1'){
+                swap(s[left], s[right]);
+            }
         }
         
+        // Swap rightmost 1 bit to the end
+        swap(s[left - 1], s[n - 1]);
         return s;
     }
 };
