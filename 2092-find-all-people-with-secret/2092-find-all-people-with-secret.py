9e0376a9893eaa9a10cdf172f5800f7b5b965d93
Andrew Kim <ksb0724@utexas.edu>
2024-03-05 01:45:53 +0900

    Time: 1542 ms (87.49%), Space: 66.4 MB (30.39%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -2,31 +2,29 @@ class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
         # For every person, store the time and label of the person met.
         graph = defaultdict(list)
-        
         for x, y, t in meetings:
             graph[x].append((t, y))
             graph[y].append((t, x))
-        
-        # Earliest time at which a person learned the secret
+            
+        # Earliest time at which a person learnt the secret
         # as per current state of knowledge. If due to some new information,
-        # the earliest time fo knowing the secret changes, we'll update it
+        # the earliest time of knowing the secret changes, we'll update it
         # and again process all the people whom he/she meets after the time
-        # at which he/she learned the secret.
+        # at which he/she learnt the secret.
         earliest = [float('inf')] * n
-        earliest[0] = 0
-        earliest[firstPerson] = 0
+        earliest[0], earliest[firstPerson] = 0, 0
         
-        # Queue for BFS. It will store (person, time of knowing the secret)
-        q = deque([(0, 0), (firstPerson, 0)])
+        # Stack for DFS. It'll store (person, time of knowing the secret)
+        stack = [(0, 0), (firstPerson, 0)]
         
-        # Do BFS
-        while q:
-            person, time = q.popleft()
+        # Do DFS
+        while stack:
+            person, time = stack.pop()
             for t, next_person in graph[person]:
                 if t >= time and earliest[next_person] > t:
                     earliest[next_person] = t
-                    q.append((next_person, t))
-        
-        # Since we visited only those people who know the secret,
-        # we need to return indices of all visited people.
+                    stack.append((next_person, t))
+                    
+        # Since we visited only those people who know the secret
+        # we need to return indices of all visited people
         return [i for i in range(n) if earliest[i] != float('inf')]
