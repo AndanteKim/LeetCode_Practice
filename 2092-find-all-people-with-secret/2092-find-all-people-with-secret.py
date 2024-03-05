81ff3b3df9fc99771089f747041d098b004e1287
Andrew Kim <ksb0724@utexas.edu>
2024-03-05 21:22:58 +0900

    Time: 1578 ms (69.86%), Space: 70.2 MB (19.93%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -1,5 +1,11 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        def dfs(person: int, time: int) -> None:
+            for t, next_person in graph[person]:
+                if t >= time and earliest[next_person] > t:
+                    earliest[next_person] = t
+                    dfs(next_person, t)
+        
         # For every person, store the time and label of the person met.
         graph = defaultdict(list)
         for x, y, t in meetings:
@@ -14,17 +20,9 @@ class Solution:
         earliest = [float('inf')] * n
         earliest[0], earliest[firstPerson] = 0, 0
         
-        # Stack for DFS. It'll store (person, time of knowing the secret)
-        stack = [(0, 0), (firstPerson, 0)]
-        
-        # Do DFS
-        while stack:
-            person, time = stack.pop()
-            for t, next_person in graph[person]:
-                if t >= time and earliest[next_person] > t:
-                    earliest[next_person] = t
-                    stack.append((next_person, t))
-                    
+        dfs(0, 0)
+        dfs(firstPerson, 0)
+            
         # Since we visited only those people who know the secret
         # we need to return indices of all visited people
         return [i for i in range(n) if earliest[i] != float('inf')]
