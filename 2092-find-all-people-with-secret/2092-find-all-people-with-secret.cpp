08d74fb127a6a5603e7f79b766ac1065d8c364d7
Andrew Kim <ksb0724@utexas.edu>
2024-03-05 22:02:30 +0900

    Time: 477 ms (47.68%), Space: 184.7 MB (46.84%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,41 +1,45 @@
 class Solution {
-private:
-    void dfs(int person, int time, auto& earliest, auto& graph){
-        for (auto [t, nextPerson] : graph[person]){
-            if (t >= time && earliest[nextPerson] > t){
-                earliest[nextPerson] = t;
-                dfs(nextPerson, t, earliest, graph);
-            }
-        }
-    }       
-
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int firstPerson) {
-        // For every person, we store the meeting time and label of the person met.
+        // For every person, store the time and label of the person met.
         unordered_map<int, vector<pair<int, int>>> graph;
         
-        for(vector<int>& meeting:meetings){
+        for (auto& meeting : meetings){
             int x = meeting[0], y = meeting[1], t = meeting[2];
             graph[x].push_back({t, y});
             graph[y].push_back({t, x});
         }
         
-        // Earliest time at which a person learnt the secret
-        // as per current state of knowledge. If due to some new information,
-        // the earliest time of knowing the secret changes, we'll update it
-        // and again process all the people whom he/she meets after the time
-        // at which he/she learned the secret.
-        vector<int> earliest(n, INT_MAX);
-        earliest[0] = 0, earliest[firstPerson] = 0;
+        // Priority Queue for BFS. It stores (time secret learned, person)
+        // It pops the person with the minimum time of knowing the secret.
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+        pq.push({0, 0});
+        pq.push({0, firstPerson});
         
-        dfs(0, 0, earliest, graph);
-        dfs(firstPerson, 0, earliest, graph);
+        // Visited array to mark if a person is visited or not.
+        // We'll mark a person as visited after it's dequeued
+        // from the queue.
+        vector<bool> visited(n);
+        
+        // Do BFS, but pop minimum.
+        while(!pq.empty()){
+            auto [t, person] = pq.top();
+            pq.pop();
+            
+            if (visited[person])
+                continue;
+            visited[person] = true;
+            for (auto& [time, nextPerson] : graph[person]){
+                if (time >= t && !visited[nextPerson]){
+                    pq.push({time, nextPerson});
+                }
+            }
+        }
         
         // Since we visited only those people who know the secret
-        // we need to return indices of all visited people.
+        // we need to return indices of all visited people
         vector<int> ans;
-        for (int i = 0; i < n; ++i)
-            if (earliest[i] != INT_MAX) ans.push_back(i);
+        for (int i = 0; i < n; ++i) if (visited[i]) ans.push_back(i);
         return ans;
     }
 };
