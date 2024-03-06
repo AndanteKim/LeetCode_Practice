b465c7e2315a78e6fae8da650d15e263f420440e
Andrew Kim <ksb0724@utexas.edu>
2024-03-07 02:05:52 +0900

    Time: 54 ms (19.36%), Space: 16.4 MB (98.14%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,22 +1,21 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         values = {
-            'I':1,'V':5,'X':10,\
-            'L':50,'C':100,'D':500,\
-            'M':1000
+            "I": 1, "V": 5, "X": 10, "L": 50,\
+            "C": 100, "D": 500, "M": 1000, "IV": 4,\
+            "IX": 9, "XL": 40, "XC": 90, "CD": 400,\
+            "CM": 900
         }
         
         total, i = 0, 0
+        
         while i < len(s):
-            # If this is the subractive case.
-            if i + 1 < len(s) and values[s[i]] < values[s[i + 1]]:
-                total += values[s[i + 1]] - values[s[i]]
+            # This is the subtractive case.
+            if i < len(s) - 1 and s[i:i+2] in values:
+                total += values[s[i:i+2]]
                 i += 2
-                
-            # Else this is NOT the subractive case.
             else:
                 total += values[s[i]]
                 i += 1
-        
         return total
-        
+    
