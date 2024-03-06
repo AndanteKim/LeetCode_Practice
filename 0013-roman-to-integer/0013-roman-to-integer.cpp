ebb90478a140f10b5ac1b51dc20df9aaef904b20
Andrew Kim <ksb0724@utexas.edu>
2024-03-06 21:45:29 +0900

    Time: 16 ms (24.25%), Space: 12.7 MB (45.21%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<string, int> mapping{
-            {"I",1}, {"II",2}, {"III",3}, {"IV",4}, {"V",5},
-            {"VI",6}, {"VII",7}, {"VIII",8}, {"IX",9}, {"X",10},
-            {"XL",40}, {"L",50}, {"XC",90}, {"C",100}, {"CD",400},
-            {"D",500}, {"CM",900}, {"M",1000}
+        unordered_map<char, int> values{
+            {'I', 1}, {'V', 5}, {'X', 10}, \
+            {'L', 50}, {'C', 100}, {'D', 500}, \
+            {'M', 1000}
         };
-        
         int i = 0, n = s.size(), ans = 0;
-        string curr = "";
+        
         while (i < n){
-            if (!mapping.count(curr + s[i])){
-                ans += mapping[curr];
-                curr.clear();
+            // If this is subtractive case
+            if (i + 1 < n && values[s[i]] < values[s[i + 1]]){
+                ans += values[s[i + 1]] - values[s[i]];
+                i += 2;
             }
-            curr.push_back(s[i]);
-            ++i;
-        }
-        if (!curr.empty()){
-            ans += mapping[curr];
+            else{
+                // If this is not subtractive case
+                ans += values[s[i]];
+                ++i;
+            }
         }
         
         return ans;
