91d20f31b140029597ad36f1da7ff5c2c2af1d9a
Andrew Kim <ksb0724@utexas.edu>
2024-03-07 21:32:42 +0900

    Time: 58 ms (11.40%), Space: 16.5 MB (71.65%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,20 +1,21 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        values = [
-            (1, "I"), (4, "IV"), (5, "V"),\
-            (9, "IX"), (10, "X"), (40, "XL"),\
-            (50, "L"), (90, "XC"), (100, "C"),\
-            (400, "CD"), (500, 'D'), (900, "CM"),\
-            (1000, 'M')
+        digits = [
+            (1000, "M"), (900, "CM"), (500, "D"),\
+            (400, "CD"), (100, "C"), (90, "XC"),\
+            (50, "L"), (40, "XL"), (10, "X"),\
+            (9, "IX"), (5, "V"), (4, "IV"), (1, "I")
         ]
         
-        ans, n = "", len(values) - 1
-        while num > 0:
-            i = n
-            for i in range(n, -1, -1):
-                val, roman = values[i]
-                while num >= val:
-                    ans += roman
-                    num -= val
+        roman_digits = []
         
-        return ans
+        # Loop through each symbol.
+        for val, symbol in digits:
+            # We don't want to continue looping if we're done
+            if num == 0:
+                break
+            count, num = divmod(num, val)
+            # Append "count" copies of "symbol" to roman_digits.
+            roman_digits.append(symbol * count)
+        
+        return "".join(roman_digits)
