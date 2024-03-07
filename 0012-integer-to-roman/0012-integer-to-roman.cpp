68601bc366c3a33b54332db3f096a7856d60cd2a
Andrew Kim <ksb0724@utexas.edu>
2024-03-07 22:29:02 +0900

    Time: 10 ms (30.51%), Space: 13.3 MB (26.31%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        vector<pair<int, string>> values{
-            {1, "I"}, {4, "IV"}, {5, "V"},\
-            {9, "IX"}, {10, "X"}, {40, "XL"},\
-            {50, "L"}, {90, "XC"}, {100, "C"},\
-            {400, "CD"}, {500, "D"}, {900, "CM"},\
-            {1000, "M"}
+        vector<pair<int, string>> digits{
+            {1000, "M"}, {900, "CM"}, {500, "D"},\
+            {400, "CD"}, {100, "C"}, {90, "XC"},\
+            {50, "L"}, {40, "XL"}, {10, "X"},\
+            {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
         };
         
-        int n = values.size() - 1;
         string ans = "";
         
-        while (num > 0){
-            for (int i = n; i >= 0; --i){
-                auto [val, roman] = values[i];
-                while (num >= val){
-                    ans += roman;
-                    num -= val;
-                }
-            }
+        // Loop through each symbol
+        for (auto& [val, symbol]:digits){
+            // We don't want to continue looping if we're done.
+            if (num == 0) break;
+            
+            int count = num / val;
+            num %= val;
+            
+            // Append "count" copies of "symbol" to roman_digits
+            for (int i = 0; i < count; ++i)
+                ans.append(symbol);
         }
         
         return ans;
