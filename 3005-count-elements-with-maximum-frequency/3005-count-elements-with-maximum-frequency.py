45f194d7ccfa560d1c59f28d49e04336b52bafd2
Andrew Kim <ksb0724@utexas.edu>
2024-03-08 16:09:59 +0900

    Time: 46 ms (41.26%), Space: 16.5 MB (97.35%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.py
@@ -1,10 +1,23 @@
 class Solution:
     def maxFrequencyElements(self, nums: List[int]) -> int:
-        freqs, ans = Counter(nums), 0
-        mx = max(val for _, val in freqs.items())
+        freqs, max_freq, total_freq = dict(), 0, 0
         
-        for _, val in freqs.items():
-            if val == mx:
-                ans += val
-                
-        return ans
+        # Find the frequency of each element
+        # Determine the maximum frequency
+        # Calculate the total frequencies of elements with the maximum frequency
+        for num in nums:
+            freqs[num] = freqs.get(num, 0) + 1
+            freq = freqs[num]
+            
+            # If we discover a higher frequency element
+            # Update max_frequency
+            # Re-set total_freqs to the new max_freq
+            if freq > max_freq:
+                max_freq = freq
+                total_freq = freq
+            # If we find an element with the max frequency, add it to the total
+            elif freq == max_freq:
+                total_freq += freq
+            
+        return total_freq
+            
