b55b9b83f800f916e4eee97725b47d166adfcdca
Andrew Kim <ksb0724@utexas.edu>
2024-03-09 17:56:31 +0900

    Time: 70 ms (63.19%), Space: 52.9 MB (51.54%) - LeetHub

---
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0, j = 0, n1 = nums1.size(), n2 = nums2.size();
+        
+        while (i < n1 && j < n2){
+            if (nums1[i] < nums2[j])
+                ++i;
+            else if (nums1[i] > nums2[j])
+                ++j;
+            else
+                return nums1[i];
+        }
+        
+        return -1;
+    }
+};
