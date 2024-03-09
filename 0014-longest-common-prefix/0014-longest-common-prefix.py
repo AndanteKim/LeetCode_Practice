bba9da868ea514bb55a3520e145eec431d14a8a2
Andrew Kim <ksb0724@utexas.edu>
2024-03-09 23:04:52 +0900

    Time: 39 ms (50.29%), Space: 16.6 MB (92.19%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,22 +1,26 @@
 class Solution:
-    def commonPrefix(self, left: str, right: str) -> str:
-        mn = min(len(left), len(right))
-        for i in range(mn):
-            if left[i] != right[i]:
-                return left[:i]
-        
-        return left[:mn]
-    
-    def _longestCommonPrefix(self, s: List[str], l: int, r: int) -> str:
-        if l == r:
-            return s[l]
-        else:
-            mid = (l + r) >> 1
-            lcp_left = self._longestCommonPrefix(s, l, mid)
-            lcp_right = self._longestCommonPrefix(s, mid + 1, r)
-            return self.commonPrefix(lcp_left, lcp_right)
+    def is_common_prefix(self, strs: List[str], l: int) -> bool:
+        str1 = strs[0][:l]
+        for i in range(1, len(strs)):
+            if not strs[i].startswith(str1):
+                return False
+        return True
     
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        if not strs or len(strs) == 0:
+        if len(strs) == 0:
             return ""
-        return self._longestCommonPrefix(strs, 0, len(strs) - 1)
+        
+        min_len = float('inf')
+        for s in strs:
+            min_len = min(min_len, len(s))
+            
+        low, high = 1, min_len
+        
+        while low <= high:
+            mid = (low + high) >> 1
+            if self.is_common_prefix(strs, mid):
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return strs[0][0: (low + high) >> 1]
