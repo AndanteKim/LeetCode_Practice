367e1a4b8799664d5582ba42c4c3fea5b9865ec1
Andrew Kim <ksb0724@utexas.edu>
2024-03-09 18:57:18 +0900

    Time: 42 ms (29.32%), Space: 16.7 MB (55.24%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,13 +1,22 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        if len(strs) == 0:
-            return ""
+    def commonPrefix(self, left: str, right: str) -> str:
+        mn = min(len(left), len(right))
+        for i in range(mn):
+            if left[i] != right[i]:
+                return left[:i]
         
-        prefix = strs[0]
-        for i in range(1, len(strs)):
-            while not strs[i].startswith(prefix):
-                prefix = prefix[:-1]
-                if len(prefix) == 0:
-                    return ""
-                
-        return prefix
+        return left[:mn]
+    
+    def _longestCommonPrefix(self, s: List[str], l: int, r: int) -> str:
+        if l == r:
+            return s[l]
+        else:
+            mid = (l + r) >> 1
+            lcp_left = self._longestCommonPrefix(s, l, mid)
+            lcp_right = self._longestCommonPrefix(s, mid + 1, r)
+            return self.commonPrefix(lcp_left, lcp_right)
+    
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if not strs or len(strs) == 0:
+            return ""
+        return self._longestCommonPrefix(strs, 0, len(strs) - 1)
