05f2c56900d6180a190b791bdc33d9fab9183bed
Andrew Kim <ksb0724@utexas.edu>
2024-03-13 18:35:11 +0900

    Time: 47 ms (57.20%), Space: 16.5 MB (59.17%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,15 +1,23 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        sigma, s = dict(), 0
+        left_value, right_value = 1, n
+        sum_left, sum_right = left_value, right_value
         
-        for i in range(n + 1):
-            s += i
-            sigma[i] = s
-            
-        s = 0
-        for pivot in range(1, n + 1):
-            s += pivot
-            if s == (sigma[n] - sigma[pivot - 1]):
-                return pivot
-            
-        return -1
+        if n == 1:
+            return n
+        
+        # Iterate until the pointers meet
+        while left_value < right_value:
+            # Adjust sums and pointers based on comparison
+            if sum_left < sum_right:
+                sum_left += left_value + 1
+                left_value += 1
+            else:
+                sum_right += right_value - 1
+                right_value -= 1
+                
+            # Check for pivot condition
+            if sum_left == sum_right and left_value + 1 == right_value - 1:
+                return left_value + 1
+        
+        return -1 # Return -1 if no pivot is found
