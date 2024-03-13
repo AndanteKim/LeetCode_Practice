d757ab6d03449b0cb2882a3b72634ff7e6dcadf1
Andrew Kim <ksb0724@utexas.edu>
2024-03-13 19:18:09 +0900

    Time: 41 ms (76.39%), Space: 16.5 MB (59.17%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.py
@@ -1,21 +1,7 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        # Initialize left and right pointers for binary search
-        left, right = 1, n
+        s = (n * (n + 1) >> 1)
+        pivot = int(s ** 0.5)
         
-        # Calculate the total sum of the sequence
-        total_sum = n * (n + 1) >> 1
-        
-        # Binary search for the pivot point
-        while left <right:
-            # Calculate the mid-point
-            mid = (left + right) >> 1
-            
-            # Check if the difference between the square of mid and the total sum is negative
-            if mid * mid - total_sum < 0:
-                left = mid + 1  # Adjust the left bound if the sum is smaller
-            else:
-                right = mid  # Adjust the right bound if the sum is equal or greater
-       
-        # Check if the square of the left pointer minus the total sum is zero
-        return left if left * left - total_sum == 0 else -1
+        # If pivot * pivot is equal to sum (pivot found) return pivot, else return -1
+        return pivot if pivot * pivot == s else -1
