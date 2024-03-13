cea3a2f33f0a0fc17f859eac3b4f65ba134f0992
Andrew Kim <ksb0724@utexas.edu>
2024-03-13 19:22:30 +0900

    Time: 0 ms (100.00%), Space: 7.2 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,26 +1,10 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        // Initialize left and right pointers for binary search
-        int left = 1, right = n;
+        const int sum = n * (n + 1) >> 1;
+        const int pivot = sqrt(sum);
         
-        // Calculate the total sum of the sequence.
-        int sum = n * (n + 1) >> 1;
-        
-        // Binary search for the pivot point
-        while (left < right){
-            // Calculate the mid-point
-            int mid = (left + ((right - left) >> 1));
-            
-            // Check if the difference between the square of mid and the total sum is
-            // negative
-            if ((mid * mid) - sum < 0)
-                left = mid + 1;
-            else
-                right = mid; // Adjust the right bound if sum is equal or greater.
-        }
-        
-        // Check if the square of the left pointer minus the total sum is zero.
-        return (left * left - sum == 0)? left: -1; 
+        // If pivot * pivot is equal to sum(pivot found) return pivot, else return -1
+        return (pivot * pivot == sum)? pivot:-1;
     }
 };
