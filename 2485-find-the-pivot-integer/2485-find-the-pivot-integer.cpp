c3ce993f1ad4583b768d5a86614dd5976d18eb52
Andrew Kim <ksb0724@utexas.edu>
2024-03-13 18:47:46 +0900

    Time: 0 ms (100.00%), Space: 7.3 MB (41.86%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,21 +1,28 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        unordered_map<int, long> sigma;
-        int sum = 0;
+        int leftValue = 1, rightValue = n;
+        long leftSum = leftValue, rightSum = rightValue;
         
-        for (int i = 0; i <= n; ++i){
-            sum += i;
-            sigma[i] = sum;
+        if (n == 1) return n;
+        
+        // Iterate until the pointers meet
+        while (leftValue < rightValue){
+            // Adjust sums and pointers based on comparison
+            if (leftSum < rightSum){
+                leftSum += leftValue + 1;
+                ++leftValue;
+            }
+            else{
+                rightSum += rightValue - 1;
+                --rightValue;
+            }
+            
+            // Check for pivot condition
+            if (leftSum == rightSum && leftValue + 1 == rightValue - 1)
+                return leftValue + 1;
         }
         
-        sum = 0;
-        for (int pivot = 1; pivot <= n; ++pivot){
-            sum += pivot;
-            if (sum == (sigma[n] - sigma[pivot - 1]))
-                return pivot;
-        }
-        
-        return -1;
+        return -1; // Return -1 if no pivot is found
     }
 };
