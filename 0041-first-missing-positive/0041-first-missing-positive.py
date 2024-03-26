aa5bc9c6bf73f07d86a12f9329f1a100c3280535
Andrew Kim <ksb0724@utexas.edu>
2024-03-27 02:28:30 +0900

    Time: 295 ms (56.14%), Space: 30.4 MB (37.59%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -1,40 +1,25 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        n, contains1 = len(nums), False
+        n = len(nums)
         
-        # Replace negative numbers, zeros,
-        # and numbers larger than n with 1s.
-        # After this nums contains only positive numbers.
-        for i in range(n):
-            # Check whether 1 is in the original array
-            if nums[i] == 1:
-                contains1 = True
-            if nums[i] <= 0 or nums[i] > n:
-                nums[i] = 1
-                
-        if not contains1:
-            return 1
-        
-        # Mark whether integer 1 to n are in nums
-        # Use index as a hash key and negative sign as a presence detector.
-        for i in range(n):
-            value = abs(nums[i])
-            # If you meet number a in the array, - change the sign of a-th element
-            # Be careful with duplicates : do it only once.
-            if value == n:
-                nums[0] = -abs(nums[0])
+        # Use cycle sort to place positive elements smaller than n
+        # at the correct index
+        i = 0
+        while i < n:
+            correct_idx = nums[i] - 1
+            if 0 < nums[i] <= n and nums[i] != nums[correct_idx]:
+                # swap
+                nums[i], nums[correct_idx] = nums[correct_idx], nums[i]
             else:
-                nums[value] = -abs(nums[value])
+                i += 1
         
-        # First positive in nums is the smallest missing positive integer
-        for i in range(1, n):
-            if nums[i] > 0:
-                return i
-        
-        # nums[0] stores whether n is in nums
-        if nums[0] > 0:
-            return n
-        
-        # If nums contains all elements 1 to n
+        # Iterate through nums
+        # return the smallest missing positive integer
+        for i in range(n):
+            if nums[i] != i + 1:
+                return i + 1
+            
+        # If all elements are at the correct index
         # the smallest missing positive number is n + 1
         return n + 1
+        
