4e353ef1668e65a249073a9e04ca02ee8aa22ee0
Andrew Kim <ksb0724@utexas.edu>
2024-03-29 06:11:19 +0900

    Time: 39 ms (85.91%), Space: 17 MB (63.28%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -3,9 +3,9 @@ class Solution:
         i, j = 0, len(s) - 1
         
         while i < j:
-            while i < j and not (48 <= ord(s[i]) < 58 or 65 <= ord(s[i]) < 91 or 97 <= ord(s[i]) < 123):
+            while i < j and not s[i].isalnum():
                 i += 1
-            while i < j and not (48 <= ord(s[j]) < 58 or 65 <= ord(s[j]) < 91 or 97 <= ord(s[j]) < 123):
+            while i < j and not s[j].isalnum():
                 j -= 1
             if s[i].lower() != s[j].lower():
                 return False
