29d49ab4caef5c9acfc81017a4e08f715b465f87
Andrew Kim <ksb0724@utexas.edu>
2024-03-28 21:19:59 +0900

    Time: 218 ms (48.36%), Space: 148.3 MB (86.57%) - LeetHub

--- 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
+++ 2958-length-of-longest-subarray-with-at-most-k-frequency/2958-length-of-longest-subarray-with-at-most-k-frequency.cpp
@@ -5,17 +5,15 @@ public:
         if (k <= 0) return 0;
         
         unordered_map<int, int> count;
-        int n = nums.size(), left = 0, curr = 0, ans = 0;
+        int n = nums.size(), left = 0, ans = 0;
         
         for (int right = 0; right < n; ++right){
             while (count[nums[right]] == k && left < right){
                 --count[nums[left++]];
-                --curr;
             }
             
             ++count[nums[right]];
-            ++curr;
-            ans = max(ans, curr);
+            ans = max(ans, right - left + 1);
         }
         
         return ans;
