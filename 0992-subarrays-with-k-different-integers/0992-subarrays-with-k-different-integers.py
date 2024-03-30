eef1b9aa92a4992792f680684b991373d4b4ee38
Andrew Kim <ksb0724@utexas.edu>
2024-03-30 23:04:30 +0900

    Time: 290 ms (92.31%), Space: 20.6 MB (17.75%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,26 +1,38 @@
 class Solution:
-    # Helper function to count the number of subarrays with at most k distinct elements
-    def slidingWindowAtMost(self, nums: List[int], distinctK: int) -> int:
-        # To store the occurrences of each element
-        freq = defaultdict(int)
-        left, total_count = 0, 0
-        
-        # Right pointer of the sliding window iterates through the array.
-        for right in range(len(nums)):
-            freq[nums[right]] += 1
-            
-            # If the number of distinct elements in the window exceeds k,
-            # we shrink the window from the left until we have at most k distinct elements.
-            while len(freq) > distinctK:
-                freq[nums[left]] -= 1
-                if freq[nums[left]] == 0:
-                    del freq[nums[left]]
-                left += 1
-            
-            # Update the total count by adding the length of the current subarray.
-            total_count += right - left + 1
-        
-        return total_count
-    
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        return self.slidingWindowAtMost(nums, k) - self.slidingWindowAtMost(nums, k - 1)
+        # Dictionary to store the count of distinct values encountered
+        distinct_count = defaultdict(int)
+        
+        total_count, left, right, curr_count = 0, 0, 0, 0
+        
+        while right < len(nums):
+            # Increment the count of the current element in the window
+            distinct_count[nums[right]] += 1
+            
+            # If encountering a new distinct element, decrement k
+            if distinct_count[nums[right]] == 1:
+                k -= 1
+                
+            # If k becomes negative, adjust the window from the left
+            if k < 0:
+                # Move the left pointer until the count of distinct elements becomes valid again
+                distinct_count[nums[left]] -= 1
+                if distinct_count[nums[left]] == 0:
+                    k += 1
+                left += 1
+                curr_count = 0
+                
+            # If k becomes zero, calculate subarrays
+            if k == 0:
+                # While the count of left remains greater than 1, keep shrinking the window from the left
+                while distinct_count[nums[left]] > 1:
+                    distinct_count[nums[left]] -= 1
+                    left += 1
+                    curr_count += 1
+                # Add the count of subarrays with k distinct elements to the total count
+                total_count += curr_count + 1
+            
+            # Move the right pointer to expand the window
+            right += 1
+            
+        return total_count
