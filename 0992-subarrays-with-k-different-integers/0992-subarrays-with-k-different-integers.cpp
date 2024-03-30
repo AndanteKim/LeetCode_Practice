772ece5d4e6b41b84ade01198a2820c224dfe8f4
Andrew Kim <ksb0724@utexas.edu>
2024-03-30 23:00:23 +0900

    Time: 23 ms (99.34%), Space: 36.5 MB (97.66%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,32 +1,41 @@
 class Solution {
-private:
-    // Helper function to count the number of subarrays with at most k distinct elements.
-    int slidingWindowAtMostK(vector<int>& nums, int distinctK){
-        // To store the occurrences of each element.
-        int left = 0, ans = 0;
-        unordered_map<int, int> freqMap;
-        
-        // Right pointer of the sliding window iterates through the array.
-        for (int right = 0; right < nums.size(); ++right){
-            ++freqMap[nums[right]];
-            
-            // If the number of distinct elements in the window exceeds k,
-            // we shrink the window from the left until we have at most k distinct elements.
-            while (freqMap.size() > distinctK){
-                --freqMap[nums[left]];
-                if (freqMap[nums[left]] == 0) freqMap.erase(nums[left]);
-                ++left;
-            }
-            
-            // Update the total count by adding the length of the current subarray
-            ans += (right - left + 1);
-        }
-        
-        return ans;
-    }
-    
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-        return slidingWindowAtMostK(nums, k) - slidingWindowAtMostK(nums, k - 1);
+        // Array to store the count of distinct values encountered
+        vector<int> distinctCount(nums.size() + 1, 0);
+        int left = 0, currCount = 0, totalCount = 0;
+        
+        for (int right = 0; right < nums.size(); ++right){
+            // Increment the count of the current element in the window
+            ++distinctCount[nums[right]];
+            
+            // If encountering a new distinct element, decrement K
+            if (distinctCount[nums[right]] == 1) --k;
+            
+            // If k becomes negative, adjust the window from the left
+            if (k < 0){
+                // Move the left pointer until the count of distinct elements
+                // becomes valid again
+                --distinctCount[nums[left++]];
+                ++k;
+                currCount = 0;
+            }
+            
+            // If k becomes zero, calculate the subarrays
+            if (k == 0){
+                // While the count of left remains greater than 1, keep
+                // shrinking the window from the left
+                while (distinctCount[nums[left]] > 1){
+                    --distinctCount[nums[left++]];
+                    ++currCount;
+                }
+                
+                // Add the count of subarrays with k distinct elements to the
+                // total count
+                totalCount += currCount + 1;
+            }
+        }
+        
+        return totalCount;
     }
 };
