ce06a3bb09c97ef82224cc7cee21927910a525de
Andrew Kim <ksb0724@utexas.edu>
2024-04-01 09:31:54 +0900

    Time: 146 ms (45.65%), Space: 37.1 MB (24.76%) - LeetHub

--- 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cpp
+++ 0159-longest-substring-with-at-most-two-distinct-characters/0159-longest-substring-with-at-most-two-distinct-characters.cpp
@@ -1,32 +1,21 @@
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(string s) {
-        int n = s.size();
-        if (n < 3)
-            return n;
+        unordered_map<char, int> freq;
+        int left = 0, ans = 0;
         
-        // sliding window left and right pointers
-        int left = 0, right = 0, maxLen = 2;
-        // hashmap character -> its rightmost position
-        // in the sliding window
-        unordered_map<char, int> hashmap;
-        
-        while (right < n){
-            // when the sliding window contains less than 3 characters
-            hashmap[s[right]] = right;
-            ++right;
+        for (int right = 0; right < s.size(); ++right){
+            ++freq[s[right]];
             
-            // sliding window contains 3 characters
-            if (hashmap.size() == 3){
-                auto[_, delIdx] = *min_element(hashmap.begin(), hashmap.end(), [](auto& l, auto& r){return l.second < r.second;});
-                // delete the leftmost character
-                hashmap.erase(s[delIdx]);
-                left = delIdx + 1;
+            while (freq.size() > 2 && left < right){
+                --freq[s[left]];
+                if (freq[s[left]] == 0) freq.erase(s[left]);
+                ++left;
             }
             
-            maxLen = max(maxLen, right - left);
+            ans = max(ans, right - left + 1);
         }
         
-        return maxLen;
+        return ans;
     }
 };
