9a6d6c64fe89879890ada40bdd6d8d523251719d
Andrew Kim <ksb0724@utexas.edu>
2024-04-01 09:21:32 +0900

    Time: 30 ms (84.88%), Space: 16.8 MB (6.34%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,4 +1,16 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        s = s.split()
-        return len(s[-1])
+        i, ans, count_start = len(s) - 1, 0, False
+        while i >= 0:
+            if not count_start and s[i] == ' ':
+                i -= 1
+                continue
+            
+            if count_start and s[i] == ' ':
+                break
+            
+            count_start = True
+            ans += 1
+            i -= 1
+        
+        return ans
