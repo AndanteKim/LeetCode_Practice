b1da9cfc209c2c07f243361bf9d7415011bf21a6
Andrew Kim <ksb0724@utexas.edu>
2024-04-04 20:40:01 +0900

    Time: 553 ms (34.66%), Space: 42 MB (35.10%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,32 +1,26 @@
 class Solution {
-private:
-    int n;
-    void twoSum(vector<int>& nums, int i, vector<vector<int>>& ans){
-        int j = i + 1;
-        unordered_set<int> seen;
-        
-        for (int j = i + 1; j < n; ++j){
-            int complement = -(nums[i] + nums[j]);
-            if (seen.count(complement)){
-                ans.push_back({nums[i], complement, nums[j]});
-                while (j + 1 < n && nums[j] == nums[j + 1]) ++j;
-            }
-            seen.insert(nums[j]);
-        }
-    }
-    
-    
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        n = nums.size();
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
+        set<vector<int>> ans;
+        unordered_set<int> dups;
+        unordered_map<int, int> seen;
         
-        for (int i = 0; i < n && nums[i] <= 0; ++i){
-            if (i == 0 || nums[i - 1] != nums[i])
-                twoSum(nums, i, ans);
+        for (int i = 0; i < nums.size(); ++i){
+            if (!dups.count(nums[i])){
+                dups.insert(nums[i]);
+                for (int j = i + 1; j < nums.size(); ++j){
+                    int complement = -(nums[i] + nums[j]);
+                    if (seen.count(complement) && seen[complement] == i){
+                        vector<int> triplet = {nums[i], nums[j], complement};
+                        sort(triplet.begin(), triplet.end());
+                        ans.insert(triplet);
+                    }
+                    seen[nums[j]] = i;
+                }
+            }
+            
         }
         
-        return ans;
+        return vector<vector<int>>(ans.begin(), ans.end());
     }
 };
