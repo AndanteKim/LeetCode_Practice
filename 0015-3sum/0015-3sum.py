e1210aae98dc321379d7f3bbc1a5ab1aa41d1f5e
Andrew Kim <ksb0724@utexas.edu>
2024-04-04 15:56:07 +0900

    Time: 807 ms (40.87%), Space: 21 MB (20.44%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,29 +1,26 @@
 class Solution:
-    def twoSumII(self, nums: List[int], i: int, ans: List[List[int]]) -> None:
-        lo, hi = i + 1, self.n - 1
-        
-        while lo < hi:
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0: lo += 1
-            elif sum > 0: hi -= 1
-            else:
-                ans.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                
-                while lo < hi and nums[lo - 1] == nums[lo]:
-                    lo += 1
-                
-        
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         ans, self.n = [], len(nums)
         nums.sort()
-        
         for i in range(self.n):
             if nums[i] > 0:
                 break
             
-            if i == 0 or nums[i] != nums[i - 1]:
-                self.twoSumII(nums, i, ans)
-                
+            if i == 0 or nums[i - 1] != nums[i]:
+                self.twoSum(nums, i, ans)
+       
         return ans
+    
+    def twoSum(self, nums: List[int], i: int, ans: List[List[int]]) -> None:
+        seen = set()
+        j = i + 1
+        
+        while j < self.n:
+            complement = -nums[i] - nums[j]
+            if complement in seen:
+                ans.append([nums[i], nums[j], complement])
+                while j + 1 < self.n and nums[j] == nums[j + 1]:
+                    j += 1
+            seen.add(nums[j])
+            j += 1
+            
