16c5ffe0900a38b587623ea7f488b45e9200b805
Andrew Kim <ksb0724@utexas.edu>
2024-04-04 16:15:59 +0900

    Time: 1524 ms (6.03%), Space: 448.4 MB (5.08%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,37 +1,33 @@
 class Solution {
 private:
     int n;
-    void twoSumII(vector<int>& nums, int i, vector<vector<int>>& ans){
-        int lo = i + 1, hi = n - 1, sum;
+    void twoSum(vector<int>& nums, int i, vector<vector<int>>& ans){
+        int j = i + 1;
+        unordered_set<int> seen;
         
-        while (lo < hi){
-            sum = nums[lo] + nums[hi] + nums[i];
+        while (j < n){
+            int complement = -(nums[i] + nums[j]);
             
-            if (sum < 0)
-                ++lo;
-            else if (sum > 0)
-                --hi;
-            else{
-                ans.push_back({nums[i], nums[lo], nums[hi]});
-                ++lo;
-                --hi;
-                while (lo < hi && nums[lo - 1] == nums[lo]) ++lo;
+            if (seen.find(complement) != seen.end()){
+                ans.push_back({nums[i], nums[j], complement});
+                while (j + 1 < n && nums[j] == nums[j + 1]) ++j;
             }
+            seen.insert(nums[j]);
+            ++j;
         }
     }
+    
     
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         n = nums.size();
         vector<vector<int>> ans;
-        
         sort(nums.begin(), nums.end());
         
         for (int i = 0; i < n; ++i){
-            if (nums[i] > 0)
-                break;
+            if (nums[i] > 0) break;
             if (i == 0 || nums[i - 1] != nums[i])
-                twoSumII(nums, i, ans);
+                twoSum(nums, i, ans);
         }
         
         return ans;
