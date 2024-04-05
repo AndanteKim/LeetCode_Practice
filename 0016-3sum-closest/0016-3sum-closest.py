f9d525bfe161e6cbfb9a03d970e5e5d6475b4493
Andrew Kim <ksb0724@utexas.edu>
2024-04-05 21:28:33 +0900

    Time: 655 ms (8.58%), Space: 16.7 MB (60.05%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -4,16 +4,17 @@ class Solution:
         nums.sort()
         
         for i in range(len(nums)):
-            lo, hi = i + 1, len(nums) - 1
-            while lo < hi:
-                sum = nums[i] + nums[lo] + nums[hi]
-                if abs(target - sum) < abs(diff):
-                    diff = target - sum
-                if sum < target:
-                    lo += 1
-                else:
-                    hi -= 1
-            
+            for j in range(i + 1, len(nums)):
+                complement = target - nums[i] - nums[j]
+                hi = bisect_right(nums, complement, j + 1);
+                lo = hi - 1
+                if hi < len(nums) and abs(complement - nums[hi]) < abs(diff):
+                    diff = complement - nums[hi]
+                
+                if lo > j and abs(complement - nums[lo]) < abs(diff):
+                    diff = complement - nums[lo]
+                    
             if diff == 0:
                 break
+        
         return target - diff
