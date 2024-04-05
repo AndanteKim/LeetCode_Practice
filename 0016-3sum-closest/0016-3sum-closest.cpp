7f84c914dd1994d086e99b87fba389f067aa4ddc
Andrew Kim <ksb0724@utexas.edu>
2024-04-05 21:20:48 +0900

    Time: 24 ms (14.16%), Space: 12.9 MB (19.12%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,37 +1,25 @@
 class Solution {
-private:
-    int n;
-    int twoSum(vector<int>& nums, int startIndex, int target){
-        int left = startIndex, right = n - 1, curr = nums[left] + nums[right];
-        
-        while (left < right){
-            if (nums[left] + nums[right] == target) return target;
-            else if (nums[left] + nums[right] < target){
-                if (abs(curr - target) > abs(nums[left] + nums[right] - target))
-                    curr = nums[left] + nums[right];
-                ++left;
-            }
-            else{
-                if (abs(curr - target) > abs(nums[left] + nums[right] - target))
-                    curr = nums[left] + nums[right];
-                --right;
-            }
-        }
-        
-        return curr;
-    }
-    
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        int ans = 100'000;
-        this -> n = nums.size();
+        int diff = INT_MAX;
         sort(nums.begin(), nums.end());
         
-        for (int i = 0; i < n - 2; ++i){
-            int curr = nums[i] + twoSum(nums, i + 1, target - nums[i]);
-            if (abs(ans - target) > abs(curr - target)) ans = curr;
+        for (int i = 0; i < nums.size(); ++i){
+            int lo = i + 1, hi = nums.size() - 1;
+            
+            while (lo < hi){
+                int sum = nums[i] + nums[lo] + nums[hi];
+                
+                if (abs(target - sum) < abs(diff)) diff = target - sum;
+                
+                if (sum < target) ++lo;
+                else --hi;
+            }
+            
+            // If there is the exact target by summation of three vars.
+            if (diff == 0) break;
         }
         
-        return ans;
+        return target - diff;
     }
 };
