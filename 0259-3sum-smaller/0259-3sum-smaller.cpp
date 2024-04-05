d72bca358c79774f5c88c592e170d72533f5e870
Andrew Kim <ksb0724@utexas.edu>
2024-04-05 15:47:57 +0900

    Time: 32 ms (24.83%), Space: 16.4 MB (90.49%) - LeetHub

--- 0259-3sum-smaller/0259-3sum-smaller.cpp
+++ 0259-3sum-smaller/0259-3sum-smaller.cpp
@@ -1,24 +1,14 @@
 class Solution {
 private:
     int n;
-    
-    int binarySearch(vector<int>& nums, int startIndex, int target){
-        int left = startIndex, right = n - 1;
-        
-        while (left < right){
-            int mid = (left + right + 1) >> 1;
-            if (nums[mid] < target) left = mid;
-            else right = mid - 1;
-        }
-        
-        return left;
-    }
-    
     int twoSumSmaller(vector<int>& nums, int startIndex, int target){
-        int sum = 0;
-        for (int i = startIndex; i < n - 1; ++i){
-            int j = binarySearch(nums, i, target - nums[i]);
-            sum += j - i;
+        int sum = 0, left = startIndex, right = n - 1;
+        while (left < right){
+            if (nums[left] + nums[right] < target){
+                sum += right - left;
+                ++left;
+            }
+            else --right;
         }
         
         return sum;
@@ -26,9 +16,9 @@ private:
     
 public:
     int threeSumSmaller(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
         n = nums.size();
         int ans = 0;
-        sort(nums.begin(), nums.end());
         
         for (int i = 0; i < n - 2; ++i)
             ans += twoSumSmaller(nums, i + 1, target - nums[i]);
