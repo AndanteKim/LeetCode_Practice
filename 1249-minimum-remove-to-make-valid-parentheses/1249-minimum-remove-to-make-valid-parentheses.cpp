98a464fe46b4509f9ecbfc53087c834c4cc567d5
Andrew Kim <ksb0724@utexas.edu>
2024-04-07 08:13:36 +0900

    Time: 29 ms (16.06%), Space: 13.4 MB (38.76%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> st;
+        set<int> indicesToRemove;
+        
+        for (int i = 0; i < s.size(); ++i){
+            if (s[i] == '(') st.push(i);
+            else if (s[i] == ')'){
+                if (st.empty()) indicesToRemove.insert(i);
+                else st.pop();
+            }
+        }
+        
+        while (!st.empty()){
+            indicesToRemove.insert(st.top());
+            st.pop();
+        }
+        
+        string ans = "";
+        for (int i = 0; i < s.size(); ++i){
+            if (indicesToRemove.find(i) != indicesToRemove.end())
+                continue;
+            ans.push_back(s[i]);
+        }
+        
+        return ans;
+    }
+};
