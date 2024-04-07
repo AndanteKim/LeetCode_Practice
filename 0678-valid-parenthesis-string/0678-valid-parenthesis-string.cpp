95f643d290f532fed74f11be057aa7255062355d
Andrew Kim <ksb0724@utexas.edu>
2024-04-07 12:57:20 +0900

    Time: 0 ms (100.00%), Space: 7.3 MB (75.32%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,44 +1,27 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int n = s.size();
+        int openCount = 0, closeCount = 0, n = s.size() - 1;
         
-        // dp[i][j] represents the substring starting index i is valid with opening brackets.
-        vector<vector<bool>> dp(n + 1, vector<bool>(n + 1));
-        // base case: an empty string with 0 opening brackets is valid
-        dp[n][0] = true;
-        
-        for (int index = n - 1; index >= 0; --index){
-            for (int openCount = 0; openCount < n; ++openCount){
-                bool isValid = false;
-                
-                // '*' can represent '(', ')' or (empty)
-                if (s[index] == '*'){
-                    if (index < n)
-                        // try * as '('
-                        isValid |= dp[index + 1][openCount + 1];
-                    
-                    // opening brackets to use '*' as ')'
-                    if (openCount > 0)
-                        // try * as ')'
-                        isValid |= dp[index + 1][openCount - 1];
-                    // ignore '*'
-                    isValid |= dp[index + 1][openCount];
-                }
-                else{
-                    // If the character is not '*', it can be '(' or ')'
-                    if (s[index] == '(')
-                        // try '('
-                        isValid |= dp[index + 1][openCount + 1];
-                    else if (openCount > 0)
-                        // try ')'
-                        isValid |= dp[index + 1][openCount - 1];
-                }
-                dp[index][openCount] = isValid;
+        // Traverse the string from both ends simultaneously
+        for (int i = 0; i <= n; ++i){
+            // Count open parentheses or asterisks
+            if (s[i] == '(' || s[i] == '*'){
+                ++openCount;
             }
+            else --openCount;
+            
+            // count close parentheses or asterisks
+            if (s[n - i] == ')' || s[n - i] == '*'){
+                ++closeCount;
+            }
+            else --closeCount;
+            
+            // If at any point open count or close count goes negative, the string is invalid
+            if (openCount < 0 || closeCount < 0) return false;
         }
         
-        // check if the entire string is valid with no excess opening brackets
-        return dp[0][0];
+        // If open count and close count are both non-negative, the string is valid.
+        return true;
     }
 };
