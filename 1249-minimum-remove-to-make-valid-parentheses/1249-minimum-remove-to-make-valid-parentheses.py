02df56ce1eeaaf6e6cb28f72cbb027f55b6a1d6a
Andrew Kim <ksb0724@utexas.edu>
2024-04-07 16:21:36 +0900

    Time: 83 ms (47.77%), Space: 19.1 MB (15.16%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -1,22 +1,28 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        def delete_invalid_closing(string: str, open_symbol: chr, close_symbol: chr) -> str:
-            # sb := string builder
-            sb, balance = [], 0
-            for c in string:
-                if c == open_symbol:
-                    balance += 1
-                if c == close_symbol:
-                    if balance == 0:
-                        continue
-                    balance -= 1
-                sb.append(c)
-                
-            return "".join(sb)
+        # Pass 1: Remove all invalid ")"
+        first_pass_chars = []
+        balance, open_seen = 0, 0
+        
+        for c in s:
+            if c == '(':
+                balance += 1
+                open_seen += 1
             
+            if c == ')':
+                if balance == 0:
+                    continue
+                balance -= 1
+            first_pass_chars.append(c)
+            
+        # Pass 2: Remove the rightmost ')'
+        result = []
+        open_to_keep = open_seen - balance
+        for c in first_pass_chars:
+            if c == '(':
+                open_to_keep -= 1
+                if open_to_keep < 0:
+                    continue
+            result.append(c)
         
-        # Note that s[::-1] gets the reverse of s.
-        s = delete_invalid_closing(s, '(', ')')
-        s = delete_invalid_closing(s[::-1], ')', '(')
-        return s[::-1]
-        
+        return "".join(result)
