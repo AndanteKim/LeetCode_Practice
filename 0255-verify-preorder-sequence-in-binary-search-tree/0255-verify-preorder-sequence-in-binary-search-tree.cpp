b682c42b64022d957cbccefcf7383f8cf67ecd0d
Andrew Kim <ksb0724@utexas.edu>
2024-04-09 06:49:53 +0900

    Time: 25 ms (55.00%), Space: 24.9 MB (20.56%) - LeetHub

--- 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.cpp
+++ 0255-verify-preorder-sequence-in-binary-search-tree/0255-verify-preorder-sequence-in-binary-search-tree.cpp
@@ -1,20 +1,19 @@
 class Solution {
 private:
-    bool helper(int &i, vector<int>& preorder, int minLimit, int maxLimit){
-        if (i == preorder.size())
-            return true;
+    int i = 0;
+    bool helper(vector<int>& preorder, int minLimit, int maxLimit){
+        if (i == preorder.size()) return true;
         
         int root = preorder[i];
-        if (preorder[i] <= minLimit || preorder[i] >= maxLimit) return false;
+        if (root < minLimit || root > maxLimit) return false;
         ++i;
-        bool left = helper(i, preorder, minLimit, root);
-        bool right = helper(i, preorder, root, maxLimit);
+        bool left = helper(preorder, minLimit, root);
+        bool right = helper(preorder, root, maxLimit);
         return left || right;
     }
     
 public:
     bool verifyPreorder(vector<int>& preorder) {
-        int i = 0;
-        return helper(i, preorder, INT_MIN, INT_MAX);
+        return helper(preorder, INT_MIN, INT_MAX);
     }
 };
