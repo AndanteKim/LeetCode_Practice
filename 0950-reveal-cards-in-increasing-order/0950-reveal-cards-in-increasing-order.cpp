04cb92e908fdbd68f3416bdd1ea5664176ddff68
Andrew Kim <ksb0724@utexas.edu>
2024-04-10 13:06:05 +0900

    Time: 0 ms (100.00%), Space: 10.9 MB (67.85%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.cpp
@@ -2,28 +2,27 @@ class Solution {
 public:
     vector<int> deckRevealedIncreasing(vector<int>& deck) {
         int N = deck.size();
-        vector<int> result(N);
-        int indexInResult = 0, indexInDeck = 0;
-        bool skip = false;
-        sort(deck.begin(), deck.end());
+        queue<int> q;
         
-        // 
-        while (indexInDeck < N){
-            // There is an available gap in result
-            if (result[indexInResult] == 0){
-                // Add a card to result
-                if (!skip){
-                    result[indexInResult] = deck[indexInDeck++];
-                }
-                
-                // Toggle skip to alternate between adding and skipping cards
-                skip = !skip;
+        // create queue of indices
+        for (int i = 0; i < N; ++i) q.push(i);
+        
+        sort(deck.begin(), deck.end());
+        vector<int> ans(N, 0);
+        
+        // Put cards at correct index in ans 
+        for (int card:deck){
+            // Reveal card
+            ans[q.front()] = card;
+            q.pop();
+                            
+            // Move next card to bottom
+            if (!q.empty()){
+                q.push(q.front());
+                q.pop();
             }
-            
-            // Progress to the next index of the result array
-            indexInResult = (indexInResult + 1) % N;
         }
         
-        return result;
+        return ans;
     }
 };
