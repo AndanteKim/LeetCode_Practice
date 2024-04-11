7e83130b868024452442d415a79ee162c8407aa1
Andrew Kim <ksb0724@utexas.edu>
2024-04-11 17:58:10 +0900

    Time: 12 ms (46.22%), Space: 10.4 MB (15.77%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.py
+++ 0402-remove-k-digits/0402-remove-k-digits.py
@@ -1,18 +1,33 @@
-class Solution:
-    def removeKdigits(self, num: str, k: int) -> str:
-        ans, stack = "", []
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        vector<char> st;
         
-        # Construct a monotone increasing sequence of digits 
-        for digit in num:
-            while k > 0 and stack and digit < stack[-1]:
-                stack.pop()
-                k -= 1
-                
-            stack.append(digit)
+        // Construct a monotone increasing sequence of digits
+        for (char& c : num){
+            while (k > 0 && !st.empty() && c < st.back()){
+                st.pop_back();
+                --k;
+            }
+            
+            st.push_back(c);
+        }
         
-        # Trunk the remaining k digits at the end
-        # in the case k == 0: return the entire list
-        final_stack = stack[:-k] if k else stack
+        // Trunk the remaining K digits at the end
+        // in the case k == 0: return the entire list
+        while (k-- > 0) st.pop_back();
         
-        # Trip the leading zeros
-        return "".join(final_stack).lstrip('0') or "0"
+        string ans = "";
+        // trip the leading zeros
+        bool leadingZero = true;
+        for (char& c : st) {
+            if (leadingZero && c == '0')
+                continue;
+            
+            leadingZero = false;
+            ans.push_back(c);
+        }
+            
+        return ans.empty()? "0":ans;
+    }
+};
