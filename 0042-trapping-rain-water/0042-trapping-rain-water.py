c26de937e11398e77b20d764879dc5b02997c422
Andrew Kim <ksb0724@utexas.edu>
2024-04-12 14:59:28 +0900

    Time: 108 ms (39.25%), Space: 18.7 MB (16.56%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,21 +1,20 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
-        ans, left_max, right_max = 0, 0, 0
+        ans, size = 0, len(height)
+        left_max, right_max = [0] * size, [0] * size
         
-        while left < right:
-            if height[left] < height[right]:
-                if height[left] >= left_max:
-                    left_max = height[left]
-                else:
-                    ans += left_max - height[left]
-                left += 1
-            else:
-                if height[right] >= right_max:
-                    right_max = height[right]
-                else:
-                    ans += right_max - height[right]
-                right -= 1
+        # Search the left part for max bar size
+        left_max[0] = height[0]
+        for i in range(1, size):
+            left_max[i] = max(left_max[i - 1], height[i])
+            
+        # Search the right side for max bar size
+        right_max[-1] = height[-1]
+        for i in range(size - 2, -1, -1):
+            right_max[i] = max(right_max[i + 1], height[i])
+            
+        # Find the overlap of both sides
+        for i in range(1, size - 1):
+            ans += min(left_max[i], right_max[i]) - height[i]
         
         return ans
-        
