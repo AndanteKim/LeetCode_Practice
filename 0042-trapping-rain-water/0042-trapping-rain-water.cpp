346b442f6b50d8f95de91a277f931a00a6a378f5
Andrew Kim <ksb0724@utexas.edu>
2024-04-12 16:22:18 +0900

    Time: 6 ms (89.80%), Space: 23 MB (34.69%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int size = height.size(), ans = 0;
-        vector<int> leftMax(size), rightMax(size);
+        int ans = 0;
+        stack<int> stack;
         
-        leftMax[0] = height[0];
-        // Search the left part for max bar size
-        for (int i = 1; i < size; ++i)
-            leftMax[i] = max(leftMax[i - 1], height[i]);
-            
-        rightMax.back() = height.back();
-        // Search the right part for max bar size
-        for (int i = size - 2; i >= 0; --i)
-            rightMax[i] = max(rightMax[i + 1], height[i]);
-        
-        // find the overlap of both sides
-        for (int i = 1; i < size - 1; ++i)
-            ans += min(leftMax[i], rightMax[i]) - height[i];
+        for (int curr = 0; curr < height.size(); ++curr){
+            while (!stack.empty() && height[curr] > height[stack.top()]){
+                int top = stack.top(); stack.pop();
+                if (stack.empty()) break;
+                
+                int dist = curr - stack.top() - 1, boundedHeight = min(height[curr], height[stack.top()]) - height[top];
+                ans += dist * boundedHeight;
+            }
+            stack.push(curr);
+        }
         
         return ans;
     }
