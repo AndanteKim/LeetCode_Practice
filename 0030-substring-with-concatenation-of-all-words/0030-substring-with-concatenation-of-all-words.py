48f1f2ed34602ff9d3da6b2e457def0ad7de5d03
Andrew Kim <ksb0724@utexas.edu>
2024-04-14 08:58:53 +0900

    Time: 70 ms (90.43%), Space: 17.3 MB (39.95%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,29 +1,52 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
-        n, k = len(s), len(words)
-        word_length = len(words[0])
-        substr_size = word_length * k
-        word_count = Counter(words)
-        
-        def check(i: int) -> bool:
-            # Copy the original dictionary to use for this index
-            remaining = word_count.copy()
-            words_used = 0
+        def sliding_window(left: int) -> None:
+            words_found = defaultdict(int)
+            words_used, excess_word = 0, False
             
-            # Each iteration will check for a match in words
-            for j in range(i, i + substr_size, word_length):
-                sub = s[j : j + word_length]
-                if remaining[sub] > 0:
-                    remaining[sub] -= 1
-                    words_used += 1
-                else:
+            # Do the same iteration pattern as the previous approach - iterate
+            # word_length at a time, and at each iteration we focus on one word
+            for right in range(left, n, word_length):
+                if right + word_length > n:
                     break
-            
-            # Valid if we used all the words
-            return words_used == k
+                
+                sub = s[right : right + word_length]
+                if sub not in word_count:
+                    # Mismatched word - reset the window
+                    words_found = defaultdict(int)
+                    words_used, excess_word = 0, False
+                    left = right + word_length # Retry at the next index
+                else:
+                    # If we reached max window size or have an excess word
+                    while right - left == substr_size or excess_word:
+                        # Move the left bound over continuously
+                        leftmost_word = s[left : left + word_length]
+                        left += word_length
+                        words_found[leftmost_word] -= 1
+                        
+                        if words_found[leftmost_word] == word_count[leftmost_word]:
+                            # This word was the excess word
+                            excess_word = False
+                        else:
+                            # Otherwise we actually needed it
+                            words_used -= 1
+                    
+                    # Keep track of how many times this word occurs in the window
+                    words_found[sub] += 1
+                    if words_found[sub] <= word_count[sub]:
+                        words_used += 1
+                    else:
+                        # Found too many instances already
+                        excess_word = True
+                    
+                    if words_used == k and not excess_word:
+                        # Found a valid substring
+                        ans.append(left)
         
+        n, k, word_length = len(s), len(words), len(words[0])
+        substr_size, word_count = k * word_length, Counter(words)
         ans = []
-        for i in range(n - substr_size + 1):
-            if check(i):
-                ans.append(i)
+        
+        for i in range(word_length):
+            sliding_window(i)
         return ans
