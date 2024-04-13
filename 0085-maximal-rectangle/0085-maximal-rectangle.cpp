52f00c6026530543ae3136ae491723a81a0cc530
Andrew Kim <ksb0724@utexas.edu>
2024-04-13 14:23:26 +0900

    Time: 37 ms (37.80%), Space: 17.8 MB (58.28%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,23 +1,39 @@
 class Solution {
+private:
+    int monotone(vector<int>& heights){
+        int maxArea = 0;
+        stack<int> st;
+        st.push(-1);
+        
+        for (int i = 0; i < heights.size(); ++i){
+            while (st.top() != -1 && heights[i] <= heights[st.top()]){
+                int height = heights[st.top()]; st.pop();
+                maxArea = max(maxArea, height * (i - st.top() - 1));
+            }
+            st.push(i);
+        }
+        
+        while (st.top() != -1){
+            int height = heights[st.top()]; st.pop();
+            maxArea = max(maxArea, height * ((int)heights.size() - st.top() - 1));
+        }
+        
+        return maxArea;
+    }
+    
 public:
     int maximalRectangle(vector<vector<char>>& matrix) {
         int maxArea = 0, m = matrix.size(), n = matrix[0].size();
-        vector dp(m, vector<int>(n, 0));
+        vector<int> dp(n);
         
         for (int i = 0; i < m; ++i){
             for (int j = 0; j < n; ++j){
-                if (matrix[i][j] == '0') continue;
-                
-                // Compute the maximum width and update dp with it
-                dp[i][j] = (j > 0)? dp[i][j - 1] + 1:1;
-                int width = dp[i][j];
-                
-                // Compute the maximum area rectangle with a lower right corner at [i, j]
-                for (int k = i; k >= 0; --k){
-                    width = min(width, dp[k][j]);
-                    maxArea = max(maxArea, width * (i - k + 1));
-                }
+                // Update the state of this rows histogram using the last row's histogram
+                // by keeping track of the number of consecutive ones
+                dp[j] = (matrix[i][j] == '1')? ++dp[j] : 0;
             }
+            // Update maxArea with the maximum area from the row's histogram
+            maxArea = max(maxArea, monotone(dp));
         }
         
         return maxArea;
