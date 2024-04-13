6f3c6a37f9835b9cee31360b28b19211f26c7ab5
Andrew Kim <ksb0724@utexas.edu>
2024-04-13 13:54:27 +0900

    Time: 204 ms (96.41%), Space: 17.8 MB (76.17%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,19 +1,29 @@
 class Solution:
-    def maximalRectangle(self, matrix: List[List[str]]) -> int:
-        max_area, m, n = 0, len(matrix), len(matrix[0])
+    def monotone(self, heights: List[int]) -> int:
+        stack, max_area = [-1], 0
         
-        dp = [[0] * n for _ in range(m)]
+        for i in range(len(heights)):
+            while stack[-1] != -1 and heights[stack[-1]] >= heights[i]:
+                max_area = max(max_area, heights[stack.pop()] * (i - stack[-1] - 1))
+            stack.append(i)
+        
+        while stack[-1] != -1:
+            max_area = max(max_area, heights[stack.pop()] * (len(heights) - stack[-1] - 1))
+
+        return max_area
+    
+    def maximalRectangle(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+        
+        max_area, m, n = 0, len(matrix), len(matrix[0])
+        dp = [0] * n
         for i in range(m):
             for j in range(n):
-                if matrix[i][j] == '0':
-                    continue
-                
-                # compute the maximum width and update dp with it
-                width = dp[i][j] = dp[i][j - 1] + 1 if j else 1
-                
-                # compute the maximum area rectangle with ad lower right corner at [i, j]
-                for k in range(i, -1, -1):
-                    width = min(width, dp[k][j])
-                    max_area = max(max_area, width * (i - k + 1))
-        
+                # Update the state of this row's histogram using the last row's histogram
+                # by keeping track of the number of consecutive ones
+                dp[j] = dp[j] + 1 if matrix[i][j] == '1' else 0
+            
+            # update max_area with the maximum area from this row's historgram
+            max_area = max(max_area, self.monotone(dp))
         return max_area
