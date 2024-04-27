4ec27ca5e6e360b75f5a0bf8587df7c4f9837d59
Andrew Kim <ksb0724@utexas.edu>
2024-04-28 03:47:59 +0900

    Time: 211 ms (41.17%), Space: 17.3 MB (43.14%) - LeetHub

--- 0514-freedom-trail/0514-freedom-trail.py
+++ 0514-freedom-trail/0514-freedom-trail.py
@@ -1,26 +1,32 @@
 class Solution:
     def findRotateSteps(self, ring: str, key: str) -> int:
-        # Find the minimum steps between two indices of ring
         def count_steps(curr: int, next: int) -> int:
             steps_between = abs(curr - next)
             steps_around = ring_len - steps_between
             return min(steps_between, steps_around)
         
-        # Find the minimum number of steps to spell the keyword
-        @lru_cache(maxsize = None)
-        def try_lock(ring_i: int, key_i: int, min_steps: int) -> int:
-            # If we reach the end of the key, it has been spelled
+        def try_locks(ring_i: int, key_i: int) -> int:
+            # If we have already calculated this sub_problem, return result
+            if (ring_i, key_i) in best_steps:
+                return best_steps[(ring_i, key_i)]
+            
+            # If we reach the end of the key_word, it has been spelled
             if key_i == key_len:
+                best_steps[(ring_i, key_i)] = 0
                 return 0
             
-            # For each occurrence of the character k[key_i] in ring
-            # calculate the minimum steps from the ring_i of ring
+            # For each occurrence of the chracter k[key_index] in ring
+            # calculate the minimum steps from the ring_index of ring
+            min_steps = float('inf')
             for i in range(ring_len):
                 if ring[i] == key[key_i]:
-                    total_steps = count_steps(ring_i, i) + 1 + \
-                    try_lock(i, key_i + 1, float('inf'))
-                    min_steps = min(min_steps, total_steps)
+                    min_steps = min(min_steps, count_steps(ring_i, i) + 1\
+                                   + try_locks(i, key_i + 1))
+            
+            best_steps[(ring_i, key_i)] = min_steps
             return min_steps
         
         ring_len, key_len = len(ring), len(key)
-        return try_lock(0, 0, float('inf'))
+        best_steps = dict()
+        
+        return try_locks(0, 0)
