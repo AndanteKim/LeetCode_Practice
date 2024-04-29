a319c912157f28ada4ff9f804dd8371ce4356739
Andrew Kim <ksb0724@utexas.edu>
2024-04-29 20:46:52 +0900

    Time: 90 ms (95.99%), Space: 91.2 MB (85.86%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,21 +1,9 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int finalXor = 0, count = 0;
+        int finalXor = 0;
         for (int n:nums) finalXor ^= n;
         
-        // Keep iterating until any of k or finalXor becomes zero.
-        while (k > 0 || finalXor > 0){
-            // k % 2 returns the rightmost bit in k.
-            // finalXor % 2 returns the rightmost bit in finalXor.
-            if ((finalXor % 2) != (k % 2))
-                ++count;
-            
-            // Remove the rightmost bit
-            k >>= 1;
-            finalXor >>= 1;
-        }
-        
-        return count;
+        return __builtin_popcount(finalXor ^ k);
     }
 };
