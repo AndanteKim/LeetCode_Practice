8758010180a2dd93d82de0a03ad80db18e7f6f33
Andrew Kim <ksb0724@utexas.edu>
2024-04-30 17:23:34 +0900

    Time: 341 ms (53.89%), Space: 19.1 MB (46.11%) - LeetHub

---
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        unordered_map<long long, int> freq;
+        // Create the frequency map
+        // key = bitmask, value = frequency of bitmask key
+        freq[0] = 1;
+        long long ans = 0, mask = 0;
+        
+        // The empty prefix can be the smaller prefix, which is handled like this.
+        for (char& c : word){
+            int bit = c - 'a';
+            
+            // Flip the parity of the c-th bit in the running prefix mask
+            mask ^= (1 << bit);
+            
+            // Count smaller prefixes creating substrings with no odd occurring letters.
+            if (freq.find(mask) != freq.end()){
+                ans += freq[mask];
+                ++freq[mask];
+            }
+            else
+                freq[mask] = 1;
+        
+        
+            // Loop through every possible letter that can appear an odd number of times in a substring.
+            for (int oddC = 0; oddC < 10; ++oddC){
+                if (freq.find(mask ^ (1 << oddC)) != freq.end())
+                    ans += freq[mask ^ (1 << oddC)];
+            }
+        }
+        
+        return ans;
+    }
+};
