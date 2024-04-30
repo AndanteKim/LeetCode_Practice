058c15bb41188011c94cb850dd0fd9ff49995231
Andrew Kim <ksb0724@utexas.edu>
2024-05-01 03:13:07 +0900

    Time: 388 ms (51.85%), Space: 28.4 MB (11.11%) - LeetHub

--- 2505-bitwise-or-of-all-subsequence-sums/2505-bitwise-or-of-all-subsequence-sums.py
+++ 2505-bitwise-or-of-all-subsequence-sums/2505-bitwise-or-of-all-subsequence-sums.py
@@ -1,29 +1,12 @@
 class Solution:
     def subsequenceSumOr(self, nums: List[int]) -> int:
-        # Array to store the count of bits set at each position (0 to 63)
-        bit_set_counts, ans = [0] * 64, 0
+        ans, prefix_sum = 0, 0
         
-        # Loop through each number in the array
+        # Iterate through each element in the input array
         for num in nums:
-            # Loop through each bit position (0 to 30)
-            # since the input numbers are in the range of int
-            for i in range(31):
-                # Check if the ith bit of current number is set
-                if num & (1 << i):
-                    # If yes, increment the count of set bits at that position
-                    bit_set_counts[i] += 1
-            
-        # Loop through each bit position (0 to 62)
-        # since we're calculating the OR of subsequences
-        for i in range(63):
-            # If there are any set bits at the current position
-            if bit_set_counts[i] > 0:
-                # Set the corresponding bit in the result
-                ans |= 1 << i
-            
-            # Carry over any excess bits to the next position
-            bit_set_counts[i + 1] += bit_set_counts[i] >> 1
-            
+            # Update the cumulative sum by adding the current element
+            prefix_sum += num
+            # Update the result by performing bitwise OR with the current element and the cumulative sum
+            ans |= num | prefix_sum
+        
         return ans
-        
-        
