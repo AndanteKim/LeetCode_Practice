964db7448188dcd9003593f6abc2bbd927cdda71
Andrew Kim <ksb0724@utexas.edu>
2024-05-07 05:04:24 +0900

    Time: 151 ms (38.80%), Space: 101.5 MB (20.87%) - LeetHub

--- 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.cpp
+++ 1885-count-pairs-in-two-arrays/1885-count-pairs-in-two-arrays.cpp
@@ -2,39 +2,27 @@ class Solution {
 public:
     long long countPairs(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
-        long long ans = 0;
-        vector<long long> diff(n);
         
-        // diff[i] stores nums1[i] - nums2[i]
-        for (int i = 0; i < n; ++i) diff[i] = static_cast<long>(nums1[i]) - nums2[i];
+        vector<int> diff;
+        // Diff stores nums1[i] - nums2[i]
+        for (int i = 0; i < n; ++i) diff.push_back(nums1[i] - nums2[i]);
         
         sort(diff.begin(), diff.end());
         
         // Count the number of valid pairs
-        for (int i = 0; i < n; ++i){
-            // All indices j following i make a valid pair
-            if (diff[i] > 0)
-                ans += n - i - 1;
-                
-            else{
-                // Binary search to find the first index j
-                // that makes aa valid pair with i
-                int left = i + 1, right = n - 1;
-                
-                while (left <= right){
-                    int mid = left + ((right - left) >> 1);
-                    // If diff[mid] doesn't make a valid pair, search in right half
-                    if (diff[i] + diff[mid] <= 0) left = mid + 1;
-                    // If diff[mid] is a valid pair, search in left half
-                    else right = mid - 1;
-                }
-                
-                // After the search left points to the first index j that makes
-                // a valid pair with i, so we count that and all following indices
-                ans += n - left;
+        long long ans = 0;
+        int left = 0, right = n - 1;
+        
+        while (left < right){
+            // Left makes a valid pair with right
+            // Right makes a valid pair with indices between the pointers
+            if (diff[left] + diff[right] > 0){
+                ans += (right-- - left);
             }
-            
-            
+            // Left and right doesn't make a valid pair
+            else{
+                ++left;
+            }
         }
         
         return ans;
