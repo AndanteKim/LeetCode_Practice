6c758a1d4159b89257a907b83d1a40696b864807
Andrew Kim <ksb0724@utexas.edu>
2024-05-10 00:51:25 +0900

    Time: 934 ms (5.80%), Space: 42.7 MB (84.50%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.py
@@ -1,14 +1,15 @@
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        # Sort in descending order
-        happiness.sort(reverse = True)
+        # Convert the list into a max heap ny inverting the happiness values
+        # Pythion's default heap data structure is a min heap
+        max_heap = [-h for h in happiness]
+        heapq.heapify(max_heap)
         
         ans, turns = 0, 0
         
-        # Calculate the maximum happiness sum
         for i in range(k):
-            # Adjust happiness and ensure it's not negative
-            ans += max(happiness[i] - turns, 0)
+            # Invert again to get the original value
+            ans += max(-heapq.heappop(max_heap) - turns, 0)
             
             # Increment turns for the next iteration
             turns += 1
