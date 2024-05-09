e26d2140f9104fd5b15dbc0ab953f8950a361584
Andrew Kim <ksb0724@utexas.edu>
2024-05-10 00:24:41 +0900

    Time: 168 ms (40.89%), Space: 107.2 MB (60.15%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
-        sort(happiness.begin(), happiness.end(), [](int a, int b){return a > b;});
+        // sort in descending order
+        sort(happiness.begin(), happiness.end(), greater<int>());
         
-        long long ans = 0, curr = 0;
+        long long ans = 0;
+        int turns = 0;
         
+        // Calculate the maximum happiness sum
         for (int i = 0; i < k; ++i){
-            if (curr > happiness[i]) break;
-            ans += happiness[i] - (curr++);
+            // Adjust happiness and ensure it's not negative
+            ans += max(happiness[i] - turns, 0);
+            
+            // Increment turns for the next iteration
+            ++turns;
         }
         
         return ans;
