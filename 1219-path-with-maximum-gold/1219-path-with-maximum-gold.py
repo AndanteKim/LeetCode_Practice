7f996ee3eb4c4b71347e8a454707d6382b569b16
Andrew Kim <ksb0724@utexas.edu>
2024-05-14 22:45:54 +0900

    Time: 1454 ms (92.38%), Space: 49.2 MB (7.80%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.py
@@ -1,30 +1,47 @@
 class Solution:
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        def backtrack(r: int, c: int) -> int:
-            # Base case: This is not in the matrix or has no gold
-            if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] == 0:
-                return 0
+        def bfs_backtrack(r: int, c: int) -> int:
+            queue, visited = deque(), set()
+            max_g = 0
+            visited.add((r, c))
+            queue.append((r, c, grid[r][c], visited))
             
-            max_gold = 0
+            while queue:
+                curr_r, curr_c, curr_g, curr_vis = queue.popleft()
+                max_g = max(max_g, curr_g)
+                
+                # Search for gold in each of the 4 neighbor cells
+                for i in range(4):
+                    next_r, next_c = curr_r + dirs[i], curr_c + dirs[i + 1]
+                    
+                    # If the next cell is in the matrix, has gold,
+                    # and has not been visited, add it to the queue
+                    if 0 <= next_r < m and 0 <= next_c < n and \
+                    grid[next_r][next_c] > 0 and \
+                    (next_r, next_c) not in curr_vis:
+                        curr_vis.add((next_r, next_c))
+                        queue.append((next_r, next_c,\
+                                     curr_g + grid[next_r][next_c],
+                                     curr_vis.copy()))
+                        curr_vis.remove((next_r, next_c))
             
-            # Mark the cell as visited and save the value
-            original_val = grid[r][c]
-            grid[r][c] = 0
-            
-            # Backtrack in each of the four directions
-            for i in range(4):
-                max_gold = max(max_gold, backtrack(r + dirs[i], c + dirs[i + 1]))
-            
-            # Set the cell back to its original value
-            grid[r][c] = original_val
-            return max_gold + original_val
+            return max_g
         
-        m, n, ans = len(grid), len(grid[0]), 0
-        dirs = (0, -1, 0, 1, 0)
+        
+        dirs, m, n = [0, 1, 0, -1, 0], len(grid), len(grid[0])
+        
+        # Find the total amount of gold in the grid
+        total_gold = sum(sum(row) for row in grid)
         
         # Search for the path with the maximum gold starting from each cell
-        for i in range(m):
-            for j in range(n):
-                ans = max(ans, backtrack(i, j))
-            
+        ans = 0
+        for r in range(m):
+            for c in range(n):
+                if grid[r][c] > 0:
+                    ans = max(ans, bfs_backtrack(r, c))
+                    
+                    # If we found a path with the total gold, it's the max gold
+                    if ans == total_gold:
+                        return total_gold
         return ans
+        
