b9a27499cbbdb78531c1ea4f04c86fc00290dff4
Andrew Kim <ksb0724@utexas.edu>
2024-05-17 06:49:25 +0900

    Time: 23 ms (6.24%), Space: 21.6 MB (24.03%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.cpp
@@ -12,16 +12,39 @@
 class Solution {
 public:
     bool evaluateTree(TreeNode* root) {
-        // Handle the case for leaf nodes.
-        if (!(root -> left || root -> right)) return root -> val != 0;
+        stack<TreeNode*> stack;
+        unordered_map<TreeNode*, bool> evaluated;
+        stack.push(root);
         
-        // Store evaluations for the left and right subtree.
-        bool evaluateRoot;
-        bool evaluateLeftSubtree = evaluateTree(root -> left), evaluateRightSubtree = evaluateTree(root -> right);
-        if (root -> val == 2)
-            evaluateRoot = evaluateLeftSubtree | evaluateRightSubtree;
-        else
-            evaluateRoot = evaluateLeftSubtree & evaluateRightSubtree;
-        return evaluateRoot;
+        while(!stack.empty()){
+            TreeNode* topNode = stack.top();
+            
+            // If the node is a leaf node, store its value in the evaluated, 
+            if (!(topNode -> left || topNode -> right)){
+                stack.pop();
+                evaluated[topNode] = topNode -> val == 1;
+                continue;
+            }
+            
+            // If both the children have already been evaluated, use their
+            // values to evaluate the current node.
+            if (evaluated.find(topNode -> left) != evaluated.end() && evaluated.find(topNode -> right) != evaluated.end()){
+                stack.pop();
+                if (topNode -> val == 2)
+                    evaluated[topNode] = evaluated[topNode -> left] | evaluated[topNode -> right];
+                else
+                    evaluated[topNode] = evaluated[topNode -> left] & evaluated[topNode -> right];
+            }
+            else{
+                // If both the children are not leaf nodes, push the current
+                // node along with its left and right child back into the stack.
+                if (topNode -> left)
+                    stack.push(topNode -> left);
+                if (topNode -> right)
+                    stack.push(topNode -> right);
+            }
+        }
+        
+        return evaluated[root];
     }
 };
