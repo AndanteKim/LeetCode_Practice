3b643af4897c119dd220037cfb5e59cd52e646fe
Andrew Kim <ksb0724@utexas.edu>
2024-05-19 08:50:22 +0900

    Time: 4 ms (63.31%), Space: 16.3 MB (6.59%) - LeetHub

--- 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -10,27 +10,33 @@
  * };
  */
 class Solution {
-private:
-    int ans = 0;
-    int dfs(TreeNode* curr){
-        // base case
-        if (!curr) return 0;
-        
-        // Calculate the number of coins each subtree has available to exchange
-        int leftCoins = dfs(curr -> left), rightCoins = dfs(curr -> right);
-        
-        
-        // Add the total number of exchanges to move
-        ans += abs(leftCoins) + abs(rightCoins);
-        
-        // The number of coins current has available to exchange
-        return curr -> val - 1 + leftCoins + rightCoins;
-    }
-    
 public:
     int distributeCoins(TreeNode* root) {
+        stack<TreeNode*> stack{{root}}, visited;
         
-        dfs(root);
+        while (!stack.empty()){
+            TreeNode* node = stack.top(); stack.pop();
+            
+            visited.push(node);
+            if (node -> left) stack.push(node -> left);
+            if (node -> right) stack.push(node -> right);
+        }
+        
+        int ans = 0;
+        while (!visited.empty()){
+            TreeNode* curr = visited.top(); visited.pop();
+            
+            if (curr -> left){
+                ans += abs(curr -> left -> val - 1);
+                curr -> val += curr -> left -> val - 1;
+            }
+            
+            if (curr -> right){
+                ans += abs(curr -> right -> val - 1);
+                curr -> val += curr -> right -> val - 1;
+            }
+        }
+        
         return ans;
     }
 };
