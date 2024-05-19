94e6dde1ed550e5a7abc71b70ff3179a233145c8
Andrew Kim <ksb0724@utexas.edu>
2024-05-20 08:26:43 +0900

    Time: 942 ms (91.67%), Space: 28.2 MB (39.29%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,21 +1,25 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        n = len(nums)
-        net_change = [(nums[i] ^ k) - nums[i] for i in range(n)]
-        node_sum = sum(nums)
+        sum_val, count = 0, 0
+        pos_min = 1 << 30
+        neg_max = -1 * (1 << 30)
         
-        net_change.sort(reverse = True)
-        
-        for i in range(0, n, 2):
-            # If net_change contains odd number of elements break the loop
-            if i + 1 == n:
-                break
-                
-            pair_sum = net_change[i] + net_change[i + 1]
+        for node_val in nums:
+            operated = node_val ^ k
+            sum_val += node_val
+            net_change = operated - node_val
             
-            # Include in node_sum if pair_sum is positive
-            if pair_sum > 0:
-                node_sum += pair_sum
+            if net_change > 0:
+                pos_min = min(pos_min, net_change)
+                sum_val += net_change
+                count += 1
                 
-        return node_sum
+            else:
+                neg_max = max(neg_max, net_change)
+                
+        # If the number of postive net_change values is even, return the sum.
+        if count % 2 == 0:
+            return sum_val
         
+        # Otherwise return the maximum of both discussed cases.
+        return max(sum_val - pos_min, sum_val + neg_max)
