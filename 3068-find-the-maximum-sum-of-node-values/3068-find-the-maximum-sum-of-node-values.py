8da9e70842aeb45fa95525e757d95c2e7225ca1d
Andrew Kim <ksb0724@utexas.edu>
2024-05-20 05:21:40 +0900

    Time: 1051 ms (29.76%), Space: 28.4 MB (22.02%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,24 +1,18 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        def max_sum_of_nodes(i: int, is_even: int) -> int:
-            if i == n:
-                # If the operation is performed on an odd number of elements 
-                return 0 if is_even else float('-inf')
-            
-            if memo[i][is_even] != -1:
-                return memo[i][is_even]
-            
-            # Xor is applied on the element
-            no_xor_done = nums[i] + max_sum_of_nodes(i + 1, is_even)
-            
-            # Xor is perfomed on the element
-            xor_done = (nums[i] ^ k) + max_sum_of_nodes(i + 1, is_even ^ 1)
-            
-            # Memoize and return the result
-            memo[i][is_even] = max(no_xor_done, xor_done)
-            return memo[i][is_even]
-        
         n = len(nums)
-        memo = [[-1] * 2 for _ in range(n)]
+        dp = [[0] * 2 for _ in range(n + 1)]
         
-        return max_sum_of_nodes(0, 1)
+        dp[n][1], dp[n][0] = 0, float('-inf')
+        
+        for i in range(n - 1, -1, -1):
+            for is_even in range(2):
+                # Case 1: We perform an operation on this element
+                xor_done = dp[i + 1][is_even ^ 1] + (nums[i] ^ k)
+                
+                # Case 2: We don't perform an operation on this element.
+                non_xor_done = dp[i + 1][is_even] + nums[i]
+                dp[i][is_even] = max(non_xor_done, xor_done)
+        
+        return dp[0][1]
+        
