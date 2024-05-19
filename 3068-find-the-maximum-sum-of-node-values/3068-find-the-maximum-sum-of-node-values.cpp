46503ab99270462667a3ec61eba6ccc6a49162f7
Andrew Kim <ksb0724@utexas.edu>
2024-05-20 05:55:12 +0900

    Time: 229 ms (22.52%), Space: 143.1 MB (22.97%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -1,29 +1,22 @@
 class Solution {
-private:
-    int n, k;
-    long long maxSumOfNodes(int i, int isEven, vector<int>& nums, vector<vector<long long>>& memo){
-        if (i == n)
-            // If the operation is performed on an odd number of elements retuurn INT_MIN
-            return isEven? 0 : INT_MIN;
-        
-        if (memo[i][isEven] != -1)
-            return memo[i][isEven];
-            
-        // No operation is performed on the element
-        long long noXorDone = nums[i] + maxSumOfNodes(i + 1, isEven, nums, memo);
-        
-        // Operation is performed on the element
-        long long xorDone = (nums[i] ^ k) + maxSumOfNodes(i + 1, isEven ^ 1, nums, memo);
-            
-        // Memoize the maximum of result
-        return memo[i][isEven] = max(noXorDone, xorDone);
-    }
-    
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-        this -> n = nums.size(), this -> k = k;
-        vector<vector<long long>> memo(n, vector<long long>(2, -1));
+        int n = nums.size();
+        vector dp(n + 1, vector<long long> (2, 0));
+        // Base case
+        dp[n][1] = 0, dp[n][0] = LLONG_MIN;
         
-        return maxSumOfNodes(0, 1, nums, memo);
+        for (int i = n - 1; i >= 0; --i){
+            for (int isEven = 0; isEven < 2; ++isEven){
+                // Case 1: We performed an operation on this element
+                long long xorDone = (nums[i] ^ k) + dp[i + 1][isEven ^ 1];
+                
+                // Case 2: We don't performed an operation on this element
+                long long noXorDone = nums[i] + dp[i + 1][isEven];
+                dp[i][isEven] = max(xorDone, noXorDone);
+            }
+        }
+        
+        return dp[0][1];
     }
 };
