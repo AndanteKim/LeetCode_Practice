9ccf30865fc2f1b3f257ca2eefdb5455ad0fdfff
Andrew Kim <ksb0724@utexas.edu>
2024-05-20 00:51:22 +0900

    Time: 1093 ms (25.00%), Space: 34 MB (9.09%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.py
@@ -1,16 +1,24 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        @lru_cache(maxsize = None)
         def max_sum_of_nodes(i: int, is_even: int) -> int:
             if i == n:
-                # If the operation is performed on an odd number of elements return float('-inf')
-                return 0 if is_even == 1 else float('-inf')
+                # If the operation is performed on an odd number of elements 
+                return 0 if is_even else float('-inf')
             
-            # No operation performed on the element
-            no_xor = nums[i] + max_sum_of_nodes(i + 1, is_even)
-            xor = (nums[i] ^ k) + max_sum_of_nodes(i + 1, is_even ^ 1)
+            if memo[i][is_even] != -1:
+                return memo[i][is_even]
             
-            return max(no_xor, xor)
+            # Xor is applied on the element
+            no_xor_done = nums[i] + max_sum_of_nodes(i + 1, is_even)
+            
+            # Xor is perfomed on the element
+            xor_done = (nums[i] ^ k) + max_sum_of_nodes(i + 1, is_even ^ 1)
+            
+            # Memoize and return the result
+            memo[i][is_even] = max(no_xor_done, xor_done)
+            return memo[i][is_even]
         
         n = len(nums)
+        memo = [[-1] * 2 for _ in range(n)]
+        
         return max_sum_of_nodes(0, 1)
