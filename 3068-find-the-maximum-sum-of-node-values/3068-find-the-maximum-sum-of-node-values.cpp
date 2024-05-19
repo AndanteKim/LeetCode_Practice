e902232a46d1caa32777bfaf5e175ac35a18d482
Andrew Kim <ksb0724@utexas.edu>
2024-05-20 06:39:02 +0900

    Time: 191 ms (59.91%), Space: 127.2 MB (46.17%) - LeetHub

--- 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
+++ 3068-find-the-maximum-sum-of-node-values/3068-find-the-maximum-sum-of-node-values.cpp
@@ -2,21 +2,23 @@ class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
         int n = nums.size();
-        vector dp(n + 1, vector<long long> (2, 0));
-        // Base case
-        dp[n][1] = 0, dp[n][0] = LLONG_MIN;
+        vector<int> netChange(n);
+        long long nodeSum = 0;
+        for (int i = 0; i < n; ++i){
+            netChange[i] = (nums[i] ^ k) - nums[i];
+            nodeSum += 1LL * nums[i];
+        }
+        sort(netChange.begin(), netChange.end(), greater<int>());
         
-        for (int i = n - 1; i >= 0; --i){
-            for (int isEven = 0; isEven < 2; ++isEven){
-                // Case 1: We performed an operation on this element
-                long long xorDone = (nums[i] ^ k) + dp[i + 1][isEven ^ 1];
-                
-                // Case 2: We don't performed an operation on this element
-                long long noXorDone = nums[i] + dp[i + 1][isEven];
-                dp[i][isEven] = max(xorDone, noXorDone);
-            }
+        for (int i = 0; i < n; i += 2){
+            // If netChange contains odd number of elements, break the loop
+            if (i + 1 == n) break;
+            long long pairSum = netChange[i] + netChange[i + 1];
+            
+            // If pair of sum is positive, 
+            if (pairSum > 0) nodeSum += pairSum;
         }
         
-        return dp[0][1];
+        return nodeSum;
     }
 };
