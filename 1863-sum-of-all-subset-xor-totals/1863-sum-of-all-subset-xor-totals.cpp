19428e0979c0d1b1a9522c9dae8f560b2017e4fa
Andrew Kim <ksb0724@utexas.edu>
2024-05-21 06:03:55 +0900

    Time: 0 ms (100.00%), Space: 8.4 MB (65.21%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,40 +1,24 @@
 class Solution {
 private:
     int n;
-    
-    void generateSubsets(vector<int>& nums, int i, vector<int> subset, vector<vector<int>>& subsets){
-        // Base case: index reached end of nums
-        // Add the current subset to subsets
-        if (i == n){
-            subsets.push_back(subset);
-            return;
-        }
-        subset.push_back(nums[i]);
-        // Generate subsets with nums[i]
-        generateSubsets(nums, i + 1, subset, subsets);
-        subset.pop_back();
-        // Generate subsets without nums[i]
-        generateSubsets(nums, i + 1, subset, subsets);
+    int generateSubsets(vector<int>& nums, int i, int currXor){
+        // Return the sum of subsets when all elements are already considered
+        if (i == n)
+            return currXor;
         
+        // Calculate sum of subset with current element
+        int withElement = generateSubsets(nums, i + 1, currXor ^ nums[i]);
+        
+        // Calculate sum of subset without current element
+        int withoutElement = generateSubsets(nums, i + 1, currXor);
+        
+        // Return sum of xor totals
+        return withElement + withoutElement;
     }
     
 public:
     int subsetXORSum(vector<int>& nums) {
         this -> n = nums.size();
-        vector<vector<int>> subsets;
-        // Generate all of the subsets
-        generateSubsets(nums, 0, {}, subsets);
-        int ans = 0;
-        
-        // Compute the XOR total for each subset and add to the result
-        for (auto& subset:subsets){
-            int xorTotal = 0;
-            for (int num : subset){
-                xorTotal ^= num;
-            }
-            ans += xorTotal;
-        }
-        
-        return ans;
+        return generateSubsets(nums, 0, 0);
     }
 };
