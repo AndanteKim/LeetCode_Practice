b52f4b7652d9d513e12dc54254a12a21889f1ebe
Andrew Kim <ksb0724@utexas.edu>
2024-05-21 05:50:20 +0900

    Time: 50 ms (63.24%), Space: 16.5 MB (48.32%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,31 +1,18 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def generate_subsets(i: int, subset: List[int], subsets: List[List[int]]) -> None:
-            # Base case: index reached end of nums
-            # Add the current subset to subsets
-            if i == len(nums):
-                subsets.append(subset[:])
-                return
+        def generate_subsets(i: int, curr_xor: int) -> int:
+            # Return current_xor when all elements in nums are already considered
+            if i == n:
+                return curr_xor
             
-            # Generate subsets with nums[i]
-            subset.append(nums[i])
-            generate_subsets(i + 1, subset, subsets)
-            subset.pop()
+            # Calculate sum of subset with current element
+            with_element = generate_subsets(i + 1, curr_xor ^ nums[i])
             
-            # Generate subsets without nums[i]
-            generate_subsets(i + 1, subset, subsets)
-            
-        # Generate all of the subsets
-        subsets = []
-        generate_subsets(0, [], subsets)
+            # Calculate sum of subset without current element
+            without_element = generate_subsets(i + 1, curr_xor)
         
-        # Compute the XOR total for each subset and add to the result
-        ans = 0
+            # Return sum of xor totals
+            return with_element + without_element
         
-        for subset in subsets:
-            subset_XOR_total = 0
-            for num in subset:
-                subset_XOR_total ^= num
-            ans += subset_XOR_total
-            
-        return ans
+        n = len(nums)
+        return generate_subsets(0, 0)
