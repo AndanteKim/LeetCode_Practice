1109f3c2b64f557d9e749bf072e7f18a34a725e9
Andrew Kim <ksb0724@utexas.edu>
2024-05-21 05:39:27 +0900

    Time: 98 ms (6.00%), Space: 46.9 MB (5.32%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,23 +1,40 @@
 class Solution {
 private:
-    int ans = 0, n;
+    int n;
     
-    void backtrack(int i, int curr, vector<int>& nums){
-        // base case
+    void generateSubsets(vector<int>& nums, int i, vector<int> subset, vector<vector<int>>& subsets){
+        // Base case: index reached end of nums
+        // Add the current subset to subsets
         if (i == n){
-            ans += curr;
+            subsets.push_back(subset);
             return;
         }
+        subset.push_back(nums[i]);
+        // Generate subsets with nums[i]
+        generateSubsets(nums, i + 1, subset, subsets);
+        subset.pop_back();
+        // Generate subsets without nums[i]
+        generateSubsets(nums, i + 1, subset, subsets);
         
-        backtrack(i + 1, curr ^ nums[i], nums);
-        backtrack(i + 1, curr, nums);
     }
     
 public:
     int subsetXORSum(vector<int>& nums) {
         this -> n = nums.size();
-        backtrack(0, 0, nums);
-     
+        vector<vector<int>> subsets;
+        // Generate all of the subsets
+        generateSubsets(nums, 0, {}, subsets);
+        int ans = 0;
+        
+        // Compute the XOR total for each subset and add to the result
+        for (auto& subset:subsets){
+            int xorTotal = 0;
+            for (int num : subset){
+                xorTotal ^= num;
+            }
+            ans += xorTotal;
+        }
+        
         return ans;
-    }       
+    }
 };
