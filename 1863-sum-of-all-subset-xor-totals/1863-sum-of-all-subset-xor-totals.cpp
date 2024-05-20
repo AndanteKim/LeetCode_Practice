fa3eece7899f081f2cbc50a4dcf509567dde7a82
Andrew Kim <ksb0724@utexas.edu>
2024-05-21 06:28:40 +0900

    Time: 4 ms (55.12%), Space: 8.2 MB (98.57%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,24 +1,13 @@
 class Solution {
-private:
-    int n;
-    int generateSubsets(vector<int>& nums, int i, int currXor){
-        // Return the sum of subsets when all elements are already considered
-        if (i == n)
-            return currXor;
-        
-        // Calculate sum of subset with current element
-        int withElement = generateSubsets(nums, i + 1, currXor ^ nums[i]);
-        
-        // Calculate sum of subset without current element
-        int withoutElement = generateSubsets(nums, i + 1, currXor);
-        
-        // Return sum of xor totals
-        return withElement + withoutElement;
-    }
-    
 public:
     int subsetXORSum(vector<int>& nums) {
-        this -> n = nums.size();
-        return generateSubsets(nums, 0, 0);
+        int ans = 0;
+        
+        // Capture each bit that is set in any of the elements 
+        for (int num:nums)
+            ans |= num;
+        
+        // Multiply the number of subset XOR totals that will have each bit set
+        return ans << (nums.size() - 1);
     }
 };
