e2dfd81e55ed908be994f80ade6a71c988ff2cd5
Andrew Kim <ksb0724@utexas.edu>
2024-05-21 04:00:05 +0900

    Time: 60 ms (47.54%), Space: 17 MB (23.04%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,16 +1,31 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        def backtrack(i: int, curr: int) -> None:
-            nonlocal ans
-            if i == n:
-                ans += curr
+        def generate_subsets(i: int, subset: List[int], subsets: List[List[int]]) -> None:
+            # Base case: index reached end of nums
+            # Add the current subset to subsets
+            if i == len(nums):
+                subsets.append(subset[:])
                 return
             
-            backtrack(i + 1, curr ^ nums[i])
-            backtrack(i + 1, curr)
+            # Generate subsets with nums[i]
+            subset.append(nums[i])
+            generate_subsets(i + 1, subset, subsets)
+            subset.pop()
             
+            # Generate subsets without nums[i]
+            generate_subsets(i + 1, subset, subsets)
+            
+        # Generate all of the subsets
+        subsets = []
+        generate_subsets(0, [], subsets)
         
-        n, ans = len(nums), 0
-        backtrack(0, 0)
+        # Compute the XOR total for each subset and add to the result
+        ans = 0
         
+        for subset in subsets:
+            subset_XOR_total = 0
+            for num in subset:
+                subset_XOR_total ^= num
+            ans += subset_XOR_total
+            
         return ans
