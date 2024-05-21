96c3e9c171a878780be746bbcb1a68e7d4055b61
Andrew Kim <ksb0724@utexas.edu>
2024-05-22 06:08:57 +0900

    Time: 32 ms (72.52%), Space: 25.1 MB (93.69%) - LeetHub

--- 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.cpp
+++ 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.cpp
@@ -1,32 +1,32 @@
 class Solution {
-private:
-    const vector<int> directions{0, 1, 0, -1, 0};
-    int rows, cols;
-    int clean(vector<vector<int>>& room, int row, int col, int direction, unordered_set<string>& visited, unordered_set<string>& cleaned){
-        // If the robot already visited this space facing this direction
-        // Return the number of spaces cleaned
-        if (visited.find(to_string(row) + to_string(col) + to_string(direction)) != visited.end())
-            return cleaned.size();
-        
-        // Mark the space as visited facing this direction and cleaned
-        visited.insert(to_string(row) + to_string(col) + to_string(direction));
-        cleaned.insert(to_string(row) + to_string(col));
-        
-        // Clean the next space straight ahead if it's empty and in the room
-        int nextRow = row + directions[direction], nextCol = col + directions[direction + 1];
-        
-        if (0 <= nextRow && nextRow < rows && 0 <= nextCol && nextCol < cols && room[nextRow][nextCol] == 0)
-            return clean(room, nextRow, nextCol, direction, visited, cleaned);
-        
-        // Otherwise turn right and clean the current space
-        return clean(room, row, col, (direction + 1) % 4, visited, cleaned);
-    }
-    
 public:
     int numberOfCleanRooms(vector<vector<int>>& room) {
-        this -> rows = room.size(), this -> cols = room[0].size();
-        unordered_set<string> visited, cleaned;
+        const vector<int> directions{0, 1, 0, -1, 0};
+        int row = 0, col = 0, cleaned = 0, direction = 0;
+        int rows = room.size(), cols = room[0].size();
         
-        return clean(room, 0, 0, 0, visited, cleaned);
+        // Clean until we revisit a space facing the same direction
+        while (!(room[row][col] >> (direction + 1) & 1)){
+            // If the robot hasn't cleaned this space yet, increment cleaned
+            if (room[row][col] == 0)
+                ++cleaned;
+            
+            // Mark the room as visited facing this direction
+            room[row][col] |= 1 << (direction + 1);
+            
+            // Clean the next space straight ahead if it's empty and in the room.
+            int nextRow = row + directions[direction], nextCol = col + directions[direction + 1];
+            
+            if (0 <= nextRow && nextRow < rows && 0 <= nextCol && nextCol < cols && room[nextRow][nextCol] != 1){
+                row = nextRow; col = nextCol;
+            }
+            else{
+                // Otherwise turn right and clean the current space
+                direction = (direction + 1) % 4;
+            }
+            
+        }
+        
+        return cleaned;
     }
 };
