61ea7a9bfaf4ad5d58e351263b93181f73d0e987
Andrew Kim <ksb0724@utexas.edu>
2024-05-22 05:16:36 +0900

    Time: 330 ms (15.35%), Space: 19.2 MB (94.93%) - LeetHub

--- 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.py
+++ 2061-number-of-spaces-cleaning-robot-cleaned/2061-number-of-spaces-cleaning-robot-cleaned.py
@@ -2,26 +2,28 @@ class Solution:
     def numberOfCleanRooms(self, room: List[List[int]]) -> int:
         directions = (0, 1, 0, -1, 0)
         rows, cols = len(room), len(room[0])
-        visited, cleaned = set(), set()
+        cleaned = 0
         
-        def clean(row: int, col: int, direction: int) -> int:
-            # If the robot already visited this space facing this direction
-            # Return the number of spaces cleaned
-            if (row, col, direction) in visited:
-                return len(cleaned)
+        row, col, direction = 0, 0, 0
+        
+        # Clean until we revisit a space facing the same direction
+        while not room[row][col] >> (direction + 1) & 1:
+            # If the robot hasn't cleaned this space yet, increment cleaned
+            if room[row][col] == 0:
+                cleaned += 1
             
-            # Mark the space as visited facing this direction and cleaned
-            visited.add((row, col, direction))
-            cleaned.add((row, col))
+            # Mark the space as visited facing this direction
+            room[row][col] |= 1 << (direction + 1)
             
             # Clean the next space straight ahead if it's empty and in the room
             next_row = row + directions[direction]
             next_col = col + directions[direction + 1]
             
-            if 0 <= next_row < rows and 0 <= next_col < cols and not room[next_row][next_col]:
-                return clean(next_row, next_col, direction)
-            
+            if 0 <= next_row < rows and 0 <= next_col < cols and room[next_row][next_col] != 1:
+                row, col = next_row, next_col
+                
             # Otherwise turn right and clean the current space
-            return clean(row, col, (direction + 1) % 4)
-        
-        return clean(0, 0, 0)        
+            else:
+                direction = (direction + 1) % 4
+                
+        return cleaned
