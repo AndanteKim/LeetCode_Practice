77c73c9ffd73010fbbf99c1dc2858298b0aee105
Andrew Kim <ksb0724@utexas.edu>
2024-05-23 23:37:41 +0900

    Time: 10284 ms (5.07%), Space: 16.5 MB (84.32%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.py
@@ -1,19 +1,28 @@
 class Solution:
     def beautifulSubsets(self, nums: List[int], k: int) -> int:
-        def backtrack(i: int, curr: List[int]) -> None:
-            if i == n:
-                if len(curr) > 0:
-                    ans.append(curr[:])
-                return
-            
-            if not curr or (nums[i] - k not in curr and nums[i] + k not in curr):
-                curr.append(nums[i])
-                backtrack(i + 1, curr)
-                curr.pop()
-                backtrack(i + 1, curr)
-            else:
-                backtrack(i + 1, curr)
+        self.n = len(nums)
+        return self._count_beautiful_subsets(nums, k, 0, 0)
+    
+    def _count_beautiful_subsets(self, nums: List[int], diff: int, i: int, mask: int) -> int:
+        # Base case: Return 1 if mask is greater than 0 (non-empty subset)
+        if i == self.n:
+            return 1 if mask > 0 else 0
         
-        ans, n = [], len(nums)
-        backtrack(0, [])
-        return len(ans)
+        # Flag to check if the current subset is beautiful
+        is_beautiful = True
+        
+        # Check if the current number forms a beautiful pair with any
+        # previous number in the subset
+        for j in range(i):
+            if ((1 << j) & mask) == 0 or abs(nums[j] - nums[i]) != diff:
+                continue
+            else:
+                is_beautiful = False
+                break
+        
+        # Recursively calculate beautiful subsets including and excluding
+        skip = self._count_beautiful_subsets(nums, diff, i + 1, mask)
+        take = (self._count_beautiful_subsets(nums, diff, i + 1, mask + (1 << i)) if is_beautiful else 0)
+        
+        return skip + take
+        
