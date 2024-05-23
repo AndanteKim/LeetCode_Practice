26ccee50cb53f0cb86e44115bb2c11733e3f8d4c
Andrew Kim <ksb0724@utexas.edu>
2024-05-24 00:06:43 +0900

    Time: 400 ms (45.46%), Space: 35.6 MB (99.38%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.cpp
@@ -2,23 +2,24 @@ class Solution {
 private:
     int n;
     int countBeautifulSubsets(vector<int>& nums, int diff, int i, int mask){
+        // Base case: Return 1 if mask is greater than 0 (non-empty subset)
         if (i == n)
             return (mask > 0)? 1 : 0;
         
+        // Flag to check if the current subset is beautiful
         bool isBeautiful = true;
-        int count = 0;
-        for (int j = 0; j < i; ++j){
-            if (((1 << j) & mask) == 0 || abs(nums[j] - nums[i]) != diff)
-                continue;
-            else{
-                isBeautiful = false;
-                break;
-            }
-        }
-        if (isBeautiful) count = countBeautifulSubsets(nums, diff, i + 1, mask | (1 << i));
+        int take = 0;
         
+        // Check if the current number forms a beautiful pair with any previous
+        // number in the subset
+        for (int j = 0; j < i && isBeautiful; ++j){
+            isBeautiful = (((1 << j) & mask) == 0 || abs(nums[j] - nums[i]) != diff);
+        }
+        
+        // Recursively calculate beautiful subsets including and excluding the current number
+        if (isBeautiful) take = countBeautifulSubsets(nums, diff, i + 1, mask | (1 << i));
         int skip = countBeautifulSubsets(nums, diff, i + 1, mask);
-        return skip + count;
+        return skip + take;
     }
     
 public:
