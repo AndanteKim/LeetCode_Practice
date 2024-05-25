00b485b32d7b547d600d98b16e264fe18d4c49de
Andrew Kim <ksb0724@utexas.edu>
2024-05-26 06:37:56 +0900

    Time: 0 ms (100.00%), Space: 8.6 MB (94.27%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -1,28 +1,40 @@
 class Solution {
 private:
     int n;
-    
-    void backtrack(int i, string curr, string& s, vector<string>& ans, unordered_set<string>& words){
-        if (i >= n){
-            if (i == n) ans.push_back(curr.substr(0, curr.size() - 1));
+    void backtrack(const string& s, const unordered_set<string>& wordSet, string& curr, vector<string>& ans, int start){
+        // If we've reached the end of the string, add the current sentence to ans
+        if (start == n){
+            ans.push_back(curr);
             return;
         }
         
-        for (int j = i + 1; j <= n; ++j){
-            string str = s.substr(i, j - i);
-            if (words.find(str) != words.end()){
-                backtrack(j, curr + str + " ", s, ans, words);
+        // Iterate over possible end indices
+        for (int end = start + 1; end <= n; ++end){
+            string word = s.substr(start, end - start);
+            
+            // If the word is in the set, proceed with backtracking
+            if (wordSet.find(word) != wordSet.end()){
+                string original = curr;
+                if (!curr.empty()) curr += " ";
+                curr += word;
+                // Recursively call backtrack with the new end index
+                backtrack(s, wordSet, curr, ans, end);
+                curr = original;
             }
         }
     }
     
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
+        // Convert wordDict to a set for O(1) lookups
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
         this -> n = s.size();
-        unordered_set<string> words(wordDict.begin(), wordDict.end());
         vector<string> ans;
+        string curr;
         
-        backtrack(0, "", s, ans, words);
+        // Start the backtracking process
+        backtrack(s, wordSet, curr, ans, 0);
+        
         return ans;
     }
 };
