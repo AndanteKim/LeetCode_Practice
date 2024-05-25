bc9f122049732c7a28f6a3ff8d8ed566c2a53cda
Andrew Kim <ksb0724@utexas.edu>
2024-05-25 16:32:38 +0900

    Time: 40 ms (19.82%), Space: 16.4 MB (99.01%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.py
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -1,21 +1,30 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        def backtrack(i: int, curr: List[str]) -> None:
-            if i >= n:
-                if i == n:
-                    ans.append(" ".join(curr))
-                return
-            
-            for j in range(i + 1, n + 1):
-                substr = s[i:j]
-
-                if substr in word_dict:
-                    curr.append(substr)
-                    backtrack(j, curr)
-                    curr.pop()
-        
-        n, word_dict = len(s), {word for word in wordDict}
+        # Convert wordDict to a set for O(1) lookups
+        word_set, self.n = set(wordDict), len(s)
         ans = []
-        backtrack(0, [])
         
+        # Start the backtracking process
+        self._backtrack(s, word_set, [], ans, 0)
+
         return ans
+    
+    def _backtrack(self, s: str, word_set: set, curr: List[str], ans: List[str], start: int) -> None:
+        # If we've reached the end of the string, add the current sentence to results
+        if start == self.n:
+            ans.append(" ".join(curr))
+            return
+        
+        # Iterate over possible end indices
+        for end in range(start + 1, self.n + 1):
+            word = s[start:end]
+            
+            # If the word is in the set, proceed with backtracking
+            if word in word_set:
+                curr.append(word)
+                # Recursively call backtrack with the new end index
+                self._backtrack(s, word_set, curr, ans, end)
+                
+                # Remove the last word to backtrack
+                curr.pop()
+                
