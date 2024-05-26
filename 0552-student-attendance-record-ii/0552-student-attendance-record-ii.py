6dc381eb2337ef26d146140c5cf9f5acf2934150
Andrew Kim <ksb0724@utexas.edu>
2024-05-27 02:57:45 +0900

    Time: 5847 ms (20.38%), Space: 76.8 MB (31.75%) - LeetHub

--- 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
+++ 0552-student-attendance-record-ii/0552-student-attendance-record-ii.py
@@ -1,29 +1,40 @@
 class Solution:
     def checkRecord(self, n: int) -> int:
-        def dp(day: int, absent: int, late: int) -> int:
-            if day == n:
-                return 1 if absent < 2 and late < 3 else 0
-            
-            if absent >= 2 or late >= 3:
+        
+        # Recursive function to return the count of combinations
+        # of length 'n' eligible for the award.
+        def eligible_combinations(n: int, total_absences: int, consecutive_lates: int) -> int:
+            # If the combination has become not eligible for the award.
+            # then we'll not count any combinations that can be made using it.
+            if total_absences >= 2 or consecutive_lates >= 3:
                 return 0
             
-            if memo[day][absent][late] != -1:
-                return memo[day][absent][late]
+            # If we'vve generated a combination of length 'n', we'll count it.
+            if n == 0:
+                return 1
             
-            # If today is present
-            ways = dp(day + 1, absent, 0)
+            # If we have already seen this sub-problem earlier,
+            # we return the stored result.
+            if memo[n][total_absences][consecutive_lates] != -1:
+                return memo[n][total_absences][consecutive_lates]
             
-            # If today is late
-            ways += dp(day + 1, absent, late + 1)
+            # We choose 'P' for the current position.
+            count = eligible_combinations(n - 1, total_absences, 0)
             
-            # If today is absent
-            ways += dp(day + 1, absent + 1, 0)
+            # We choose 'A' for the current position.
+            count = (count + eligible_combinations(n - 1, total_absences + 1, 0)) % MOD
             
-            memo[day][absent][late] = ways % mod
-            return memo[day][absent][late]
+            # We choose 'L' for the current position.
+            count = (count + eligible_combinations(n - 1, total_absences, consecutive_lates + 1)) % MOD
+            
+            # Return and store the current sub-problem result in the cache.
+            memo[n][total_absences][consecutive_lates] = count
+            
+            return count
         
-        memo = [[[-1] * 3 for _ in range(4)] for _ in range(n + 1)]
+        MOD = 1_000_000_007
+        # Initialize the cache to store sub-problem results.
+        memo = [[[-1] * 3 for _ in range(2)] for _ in range(n + 1)]
         
-        mod = 10 ** 9 + 7
-        return dp(0, 0, 0)
-        
+        # Return count of combinations of length 'n' eligible for teh award.
+        return eligible_combinations(n, 0, 0)
