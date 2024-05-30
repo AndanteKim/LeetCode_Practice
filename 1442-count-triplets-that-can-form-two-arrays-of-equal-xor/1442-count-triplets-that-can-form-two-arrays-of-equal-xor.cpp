c6dc445ddf6db06f389e1058ff09d4896c3e6e5a
Andrew Kim <ksb0724@utexas.edu>
2024-05-31 02:24:56 +0900

    Time: 5 ms (38.75%), Space: 9.3 MB (36.30%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int countTriplets(vector<int>& arr) {
-        int count = 0, n = arr.size();
+        int n = arr.size() + 1, count = 0;
+        vector<int> prefixXor(n);
         
-        // Iterate over every possible starting index i
-        for (int start = 0; start <= n - 2; ++start){
-            // Initialize XOR value for the subarray from start to mid - 1
-            int xorA = 0;
-            
-            //Iterate over every possible middle index j
-            for (int mid = start + 1; mid <= n - 1; ++mid){
-                // Update xorA to include arr[mid - 1]
-                xorA ^= arr[mid - 1];
-                
-                // Initialize XOR value for the subarray from mid to end
-                int xorB = 0;
-                
-                // Iterate over each possible ending index k (starting from mid)
-                for (int end = mid; end <= n - 1; ++end){
-                    // Update xorB to include arr[end]
-                    xorB ^= arr[end];
-                    
-                    // Found a valid triplet (start, mid, end)
-                    if (xorA == xorB)
-                        ++count;
+        // Iterate through consecutive element in the modified array
+        for (int i = 1; i < n; ++i)
+            prefixXor[i] = prefixXor[i - 1] ^ arr[i - 1]; 
+        
+        // Iterate through the modified array to count triplets
+        for (int start = 0; start < n; ++start){
+            for (int end = start + 1; end < n; ++end){
+                if (prefixXor[start] == prefixXor[end]){
+                    // Increment the result by the count of valid triplets
+                    count += end - start - 1;
                 }
             }
         }
