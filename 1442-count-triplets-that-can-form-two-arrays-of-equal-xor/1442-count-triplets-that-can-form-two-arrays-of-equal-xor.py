580f0ca0f43ed602440e65931a5a1a90bcd76efe
Andrew Kim <ksb0724@utexas.edu>
2024-05-31 02:38:57 +0900

    Time: 48 ms (64.37%), Space: 16.5 MB (93.68%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,19 +1,23 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
-        prefix_xor = [0] + arr[:]
-        n = len(prefix_xor)
+        prefix_xor = [0] + arr
+        size, count = len(prefix_xor), 0
         
-        # Perform XOR on consecutive elements in the modified array
-        for i in range(1, n):
+        # Performing XOR operation on the array elements
+        for i in range(1, size):
             prefix_xor[i] ^= prefix_xor[i - 1]
             
-        count = 0
+        # Dictionaries to store counts and totals of XOR values encountered
+        count_map, total_map = defaultdict(int), defaultdict(int)
         
-        # Iterate through the modified array to count triplets
-        for start in range(n):
-            for end in range(start + 1, n):
-                if prefix_xor[start] == prefix_xor[end]:
-                    # Increment the result by the count of valid triplets
-                    count += end - start - 1
-        
+        # Iterate through the array
+        for i in range(size):
+            # Calculating contribution of current element to the result
+            count += (count_map[prefix_xor[i]] * (i - 1) - total_map[prefix_xor[i]])
+            
+            # Updating total count of current XOR value
+            total_map[prefix_xor[i]] += i
+            count_map[prefix_xor[i]] += 1
+            
         return count
+        
