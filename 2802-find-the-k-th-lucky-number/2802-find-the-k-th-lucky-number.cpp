e93b5560fa8b9e7ea5d6994f3431322d0a3bf385
Andrew Kim <ksb0724@utexas.edu>
2024-06-01 00:16:56 +0900

    Time: 15 ms (47.48%), Space: 10.4 MB (30.58%) - LeetHub

--- 2802-find-the-k-th-lucky-number/2802-find-the-k-th-lucky-number.cpp
+++ 2802-find-the-k-th-lucky-number/2802-find-the-k-th-lucky-number.cpp
@@ -2,16 +2,18 @@ class Solution {
 public:
     string kthLuckyNumber(int k) {
         // Increment k to account for 1-based indexing
-        ++k;
+        k = k + 1;
         
-        // For each digit in the binary representation of k except the most
-        // significant pretend 4 to the result if the digit is 0 and 7 otherwise
-        string kthLuckyNumber = "";
+        int digits = log2(k);
+        
+        // Place '4' for '0' and '1' for '7'
+        string kthLuckyNum(digits, ' ');
         while (k > 1){
-            kthLuckyNumber.insert(0, ((k & 1)? "7":"4"));
+            --digits;
+            kthLuckyNum[digits] = ((k & 1)? '7' : '4');
             k >>= 1;
         }
         
-        return kthLuckyNumber;
+        return kthLuckyNum;
     }
 };
