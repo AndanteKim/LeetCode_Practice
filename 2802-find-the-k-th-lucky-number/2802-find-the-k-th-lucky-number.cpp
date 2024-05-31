9328134046e3c6684a80f46a04f7d835831e30fd
Andrew Kim <ksb0724@utexas.edu>
2024-06-01 00:05:17 +0900

    Time: 13 ms (52.52%), Space: 10.1 MB (89.93%) - LeetHub

--- 2802-find-the-k-th-lucky-number/2802-find-the-k-th-lucky-number.cpp
+++ 2802-find-the-k-th-lucky-number/2802-find-the-k-th-lucky-number.cpp
@@ -1,27 +1,15 @@
 class Solution {
 public:
     string kthLuckyNumber(int k) {
-        // c := the number of digits
-        // numCount := the number of lucky numbers with c or fewer digits
-        int c = 0, numCount = 0;
+        // Increment k to account for 1-based indexing
+        ++k;
         
-        while (numCount < k){
-            ++c;
-            numCount += (1 << c);
-        }
-        
-        // Calculate the number of lucky numbers with c digits before the kth
-        // lucky number
-        int x = k - 1 - (numCount - (1 << c));
-        
-        // Build result using x by pretending 0 for 4 or 1 for 7
+        // For each digit in the binary representation of k except the most
+        // significant pretend 4 to the result if the digit is 0 and 7 otherwise
         string kthLuckyNumber = "";
-        for (int i = 0; i < c; ++i){
-            if (x % 2 == 1)
-                kthLuckyNumber.insert(0, "7");
-            else
-                kthLuckyNumber.insert(0, "4");
-            x >>= 1;
+        while (k > 1){
+            kthLuckyNumber.insert(0, ((k & 1)? "7":"4"));
+            k >>= 1;
         }
         
         return kthLuckyNumber;
