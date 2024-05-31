98ef3c8e1c74809461dd254ff1221060e93850f4
Andrew Kim <ksb0724@utexas.edu>
2024-05-31 23:34:12 +0900

    Time: 61 ms (61.90%), Space: 16.6 MB (9.72%) - LeetHub

--- 2802-find-the-k-th-lucky-number/2802-find-the-k-th-lucky-number.py
+++ 2802-find-the-k-th-lucky-number/2802-find-the-k-th-lucky-number.py
@@ -1,23 +1,15 @@
 class Solution:
     def kthLuckyNumber(self, k: int) -> str:
-        c = 0 # The number of digits in the kth lucky number
-        num_count = 0 # The number of lucky numbers with c or fewer digits
-        while num_count < k:
-            c += 1
-            num_count += 2 ** c
-            
-        # Calculate the number of lucky numbers with c digits before the kth lucky number
-        x = k - 1 - (num_count - (2 ** c))
+        # Increment k to account for 1-based indexing
+        k += 1;
         
-        # Build result using x by pretending 4 for 0 and 1 for 7
+        # For each digit in the binary representation of k except the most significant
+        # Pretend 4 to the result if the digit is 0 and 7 otherwise
         kth_lucky_num = ""
-        for i in range(c):
-            if x % 2 == 1:
-                digit = "7"
-            else:
-                digit = "4"
-                
-            kth_lucky_num = "".join((digit, kth_lucky_num))
-            x >>= 1
+        
+        while k > 1:
+            kth_lucky_num = "".join((("7" if k & 1 else "4"), kth_lucky_num))
+            k >>= 1
             
         return kth_lucky_num
+        
