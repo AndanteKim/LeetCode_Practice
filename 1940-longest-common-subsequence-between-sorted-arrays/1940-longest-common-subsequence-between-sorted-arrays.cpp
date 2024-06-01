54411160dfa78b9570fe30223df22d1223e1084f
Andrew Kim <ksb0724@utexas.edu>
2024-06-01 20:44:02 +0900

    Time: 22 ms (86.32%), Space: 19.9 MB (27.37%) - LeetHub

--- 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.cpp
+++ 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.cpp
@@ -1,17 +1,37 @@
 class Solution {
-public:
-    vector<int> longestCommonSubsequence(vector<vector<int>>& arrays) {
-        int rows = arrays.size();
-        vector<int> intersect(101), ans;
+private:
+    vector<int> longestSeq(vector<int>& nums1, vector<int>& nums2){
+        vector<int> longestCommonSeq;
+        int first = 0, second = 0;
         
-        for (vector<int>& arr : arrays){
-            for (int num : arr){
-                ++intersect[num];
-                if (intersect[num] == rows)
-                    ans.push_back(num);
+        // Traverse through both arrays with two pointers
+        // Increment the pointer with a small value at that index
+        // when the values are equal, add to the longest common subsequence.
+        while (first < nums1.size() && second < nums2.size()){
+            if (nums1[first] < nums2[second])
+                ++first;
+            else if (nums1[first] > nums2[second])
+                ++second;
+            else{
+                longestCommonSeq.push_back(nums1[first++]);
+                ++second;
             }
         }
+        return longestCommonSeq;
+    }
+    
+public:
+    vector<int> longestCommonSubsequence(vector<vector<int>>& arrays) {
         
-        return ans;
+        // Iterate through the rest of the arrays and
+        // If the common subsequence is empty, return immediately 
+        // Else update the longest common subsequence.
+        vector<int> longestCommonSubseq = arrays[0];
+        
+        for (int i = 1; i < arrays.size(); ++i){
+            longestCommonSubseq = longestSeq(longestCommonSubseq, arrays[i]);
+        }
+        
+        return longestCommonSubseq;
     }
 };
