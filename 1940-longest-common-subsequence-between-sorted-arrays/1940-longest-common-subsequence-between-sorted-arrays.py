412e1ca00ec0574fed6f323f0a14fbe5cad017fa
Andrew Kim <ksb0724@utexas.edu>
2024-06-01 20:26:17 +0900

    Time: 216 ms (25.00%), Space: 17 MB (76.92%) - LeetHub

--- 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.py
+++ 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.py
@@ -1,13 +1,31 @@
 class Solution:
     def longestCommonSubsequence(self, arrays: List[List[int]]) -> List[int]:
-        intersect, rows, ans = [0] * 101, len(arrays), []
+        def longest_seq(nums1: List[int], nums2: List[int]) -> List[int]:
+            longest_common_seq = []
+            first, second = 0, 0
+            
+            # Traverse through both arrays with two pointers
+            # Increment the pointer with a smaller value at that index
+            # When the values are equal, add to the longest common subsequence
+            while first < len(nums1) and second < len(nums2):
+                if nums1[first] < nums2[second]:
+                    first += 1
+                elif nums1[first] > nums2[second]:
+                    second += 1
+                else:
+                    longest_common_seq.append(nums1[first])
+                    first += 1
+                    second += 1
+            return longest_common_seq
+         
+        # Iterate through the rest of the arrays and
+        # If the common subsequence is empty, return immediately
+        # Else update the longest common subsequence
+        longest_common_subseq = arrays[0]
         
-        for arr in arrays:
-            for num in arr:
-                intersect[num] += 1
-        
-        for i in range(1, 101):
-            if intersect[i] == rows:
-                ans.append(i)
-                
-        return ans
+        for i in range(1, len(arrays)):
+            if len(longest_common_subseq) == 0:
+                return longest_common_subseq
+            longest_common_subseq = longest_seq(longest_common_subseq, arrays[i])
+            
+        return longest_common_subseq
