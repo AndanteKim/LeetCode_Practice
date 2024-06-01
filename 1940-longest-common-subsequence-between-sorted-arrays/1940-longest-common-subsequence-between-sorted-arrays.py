0df7df3a4aac119e733761d82b27d43837a09072
Andrew Kim <ksb0724@utexas.edu>
2024-06-01 15:03:55 +0900

    Time: 199 ms (81.41%), Space: 16.9 MB (96.15%) - LeetHub

--- 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.py
+++ 1940-longest-common-subsequence-between-sorted-arrays/1940-longest-common-subsequence-between-sorted-arrays.py
@@ -1,8 +1,13 @@
 class Solution:
     def longestCommonSubsequence(self, arrays: List[List[int]]) -> List[int]:
-        intersect = set(arrays[0])
+        intersect, rows, ans = [0] * 101, len(arrays), []
         
-        for arr in arrays[1:]:
-            intersect = intersect.intersection(set(arr))
+        for arr in arrays:
+            for num in arr:
+                intersect[num] += 1
+        
+        for i in range(1, 101):
+            if intersect[i] == rows:
+                ans.append(i)
                 
-        return sorted(list(intersect))
+        return ans
