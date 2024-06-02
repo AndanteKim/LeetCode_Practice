25f616dfdaefd92731ed691661a6069799e21691
Andrew Kim <ksb0724@utexas.edu>
2024-06-02 11:58:36 +0900

    Time: 172 ms (38.96%), Space: 22.7 MB (5.16%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,9 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        i, j = 0, len(s) - 1
+        def reverse(left: int, right: int) -> None:
+            if left > right:
+                return
+            s[left], s[right] = s[right], s[left]
+            reverse(left + 1, right - 1)
         
-        while i < j:
-            s[i], s[j] = s[j], s[i]
-            i += 1
-            j -= 1
+        reverse(0, len(s) - 1)
