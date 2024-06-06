44a7414a63835d37cacc698a0ce8852d0c15449c
Andrew Kim <ksb0724@utexas.edu>
2024-06-06 21:30:36 +0900

    Time: 154 ms (86.35%), Space: 18.4 MB (50.76%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.py
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -1,27 +1,27 @@
 class Solution:
     def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
-        hand_size = len(hand)
-        
-        if hand_size % groupSize != 0:
+        if len(hand) % groupSize != 0:
             return False
         
         # Counter to store the count of each card value
         card_count = Counter(hand)
         
-        # Min-heap to process the cards in sorted order
-        min_heap = list(card_count.keys())
-        heapify(min_heap)
-        
-        # Process the cards until the heap is empty
-        while min_heap:
-            curr_card = min_heap[0] # Get the smallest card value
-            # Check each consecutive sequence of groupSize cards
-            for i in range(groupSize):
-                if card_count[curr_card + i] == 0:
-                    return False
-                card_count[curr_card + i] -= 1
-                if card_count[curr_card + i] == 0:
-                    if curr_card + i != heappop(min_heap):
-                        return False
-        
+        for card in hand:
+            start_card = card
+            
+            # Find the start of the potential straight sequence
+            while card_count[start_card - 1]:
+                start_card -= 1
+            
+            # Process the sequence starting from start_card
+            while start_card <= card:
+                while card_count[start_card]:
+                    # Check if we can form a consecutive sequence
+                    # of groupSize cards
+                    for next_card in range(start_card, start_card + groupSize):
+                        if not card_count[next_card]:
+                            return False
+                        card_count[next_card] -= 1
+                start_card += 1
+                
         return True
