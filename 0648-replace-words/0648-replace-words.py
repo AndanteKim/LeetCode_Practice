b903418ce338859f45ecdacc4024d40f90906401
Andrew Kim <ksb0724@utexas.edu>
2024-06-07 16:41:48 +0900

    Time: 128 ms (52.87%), Space: 36.8 MB (15.63%) - LeetHub

--- 0648-replace-words/0648-replace-words.py
+++ 0648-replace-words/0648-replace-words.py
@@ -1,18 +1,44 @@
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = [None] * 26
+        
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for c in word:
+            if curr.children[ord(c) - 97] is None:
+                curr.children[ord(c) - 97] = TrieNode()
+            curr = curr.children[ord(c) - 97]
+        curr.is_end = True
+        
+    # Find the shortest root of the word in the trie
+    def shortest_root(self, word: str) -> str:
+        curr = self.root
+        for i in range(len(word)):
+            c = word[i]
+            if curr.children[ord(c) - 97] is None:
+                # There is not a corresponding root in the trie
+                return word
+            curr = curr.children[ord(c) - 97]
+            if curr.is_end:
+                return word[: i + 1]
+        
+        # There is not a corresponding root in the trie
+        return word
+
 class Solution:
     def replaceWords(self, dictionary: List[str], sentence: str) -> str:
-        def shortest_root(word: str, dict_set: Set[str]) -> str:
-            # Find the shortest root of the word in the dictionary
-            for i in range(len(word)):
-                root = word[0:i]
-                if root in dict_set:
-                    return root
+        words = sentence.split()
+        dict_trie = Trie()
+        for word in dictionary:
+            dict_trie.insert(word)
             
-            # There is not a corresponding root in the dictionary
-            return word
-        
-        words, dict_set = sentence.split(), set(dictionary)
-        # Replace each word in sentence with the corresponding shortest root
+        # Replace each word in the sentence with the corresponding shortest root
         for word in range(len(words)):
-            words[word] = shortest_root(words[word], dict_set)
-        
+            words[word] = dict_trie.shortest_root(words[word])
+            
         return " ".join(words)
