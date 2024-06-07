ec5820e8782c991b6960d5da6eb21f72f14f0434
Andrew Kim <ksb0724@utexas.edu>
2024-06-07 17:16:34 +0900

    Time: 83 ms (65.00%), Space: 81.1 MB (41.51%) - LeetHub

--- 0648-replace-words/0648-replace-words.cpp
+++ 0648-replace-words/0648-replace-words.cpp
@@ -1,28 +1,62 @@
-class Solution {
-private:
-    string shortestRoot(string& word, unordered_set<string>& mapSet){
-        // Find the shortest root of the word in the dictionary
-        for (int i = 1; i <= word.size(); ++i){
-            string root = word.substr(0, i);
-            if (mapSet.contains(root))
-                return root;
-        }
-        
-        // There is not a corresponding root in the dictionary
-        return word;
+class TrieNode{
+public:
+    bool isEnd;
+    vector<TrieNode*> children;
+    TrieNode() : children(26, nullptr){
+        isEnd = false;
     }
+};
+
+class Trie{
+private:
+    TrieNode *root;
     
 public:
-    string replaceWords(vector<string>& dictionary, string sentence) {
-        unordered_set<string> mapSet(dictionary.begin(), dictionary.end());
-        istringstream sStream(sentence);
+    Trie() {root = new TrieNode();}
+    void insert(string& s){
+        TrieNode *curr = root;
+        for (char& c:s){
+            if (curr -> children[c - 'a'] == nullptr)
+                curr -> children[c - 'a'] = new TrieNode();
+            curr = curr -> children[c - 'a'];
+        }
+        curr -> isEnd = true;
+    }
+    
+    // Find the shortest root of the word in the trie
+    string findShortest(string& word){
+        TrieNode *curr = root;
+        for (int i = 0; i < word.size(); ++i){
+            if (curr -> children[word[i] - 'a'] == nullptr)
+                // There is not a corresponding root in the trie
+                return word;
+            curr = curr -> children[word[i] - 'a'];
+            if (curr -> isEnd)
+                return word.substr(0, i + 1);
+        }
         
-        // Replace each word in sentence with the corresponding shortest root
-        string word, newSentence;
-        while (getline(sStream, word, ' '))
-            newSentence += shortestRoot(word, mapSet) + " ";
+        // There is not a corresponding root in the trie
+        return word;
+    }
+};
+
+class Solution {
+public:
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        Trie *dictTrie = new Trie();
+        for (string& s:dictionary)
+            dictTrie -> insert(s);
+        
+        // Replace eaach word in sentence with the corresponding shortest root
+        string word, newSentence = "";
+        istringstream isStream(sentence);
+        
+        while (getline(isStream, word, ' ')){
+            newSentence += dictTrie -> findShortest(word) + " ";
+        }
         
         newSentence.pop_back(); // remove extra space
+        
         return newSentence;
     }
 };
