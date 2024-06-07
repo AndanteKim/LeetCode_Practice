5fce81e4257f3603950ac9e872c325c4c48c66ff
Andrew Kim <ksb0724@utexas.edu>
2024-06-08 01:33:05 +0900

    Time: 114 ms (80.51%), Space: 53.4 MB (61.29%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.cpp
@@ -2,20 +2,27 @@ class Solution {
 public:
     bool isPossibleDivide(vector<int>& nums, int k) {
         if (nums.size() % k != 0) return false;
-        map<int, int> counter;
-        for (int num:nums)
-            ++counter[num];
         
-        while (!counter.empty()){
-            int curr = counter.begin() -> first;
+        // Count to store the count of each frequency
+        unordered_map<int, int> count;
+        for (int n:nums) ++count[n];
+        
+        for (int num:nums){
+            int start = num;
             
-            for (int next = curr; next < curr + k; ++next){
-                if (!counter.contains(next) || counter[next] == 0)
-                    return false;
-                --counter[next];
-                
-                if (counter[next] == 0)
-                    counter.erase(next);
+            // Find the start of the potential straight sequence
+            while (count[start - 1] != 0) --start;
+            
+            // Process the sequence starting from start
+            while (start <= num){
+                while (count[start] != 0){
+                    // Check if we can form a consecutive sequence of k
+                    for (int next = start; next < start + k; ++next){
+                        if (count[next] == 0) return false;
+                        --count[next];
+                    }
+                }
+                ++start;
             }
         }
         
