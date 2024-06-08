336306b386458c832d6e595eccd632b202e75d61
Andrew Kim <ksb0724@utexas.edu>
2024-06-08 23:18:50 +0900

    Time: 32 ms (26.61%), Space: 15.6 MB (5.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -12,6 +12,8 @@ public:
         for (char& c:t) ++dictT[c];
         int required = dictT.size(), formed = 0;
         
+        // Filter all the characters from s into a new list along with their index.
+        // The filtering criteria is that the character should be present in t.
         vector<element> filteredS;
         for (int i = 0; i < s.size(); ++i){
             if (dictT.contains(s[i]))
@@ -21,6 +23,9 @@ public:
         int l = 0;
         int ans[3]{-1, 0, 0};
         unordered_map<char, int> windowsCount;
+        
+        // Look for the characters only in the filtered list instead of entire s. This helps to reduce
+        // our search. Hence, we follow the sliding window approach on as small list.
         for (int r = 0; r < filteredS.size(); ++r){
             char ch = filteredS[r].charVal;
             ++windowsCount[ch];
@@ -28,8 +33,12 @@ public:
             if (windowsCount[ch] == dictT[ch])
                 ++formed;
             
+            // If the current window has all the characters in desired frequencies i.e. t is present in the
+            // window.
             while (l <= r && formed == required){
                 ch = filteredS[l].charVal;
+                
+                // Save the smallest window until now.
                 int end = filteredS[r].index, start = filteredS[l].index;
                 
                 if (ans[0] == -1 || end - start + 1 < ans[0]){
