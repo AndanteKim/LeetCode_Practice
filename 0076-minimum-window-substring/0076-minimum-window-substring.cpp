8d163f32073ce926360970165d90597c977b2461
Andrew Kim <ksb0724@utexas.edu>
2024-06-08 23:15:59 +0900

    Time: 32 ms (26.61%), Space: 15.7 MB (5.18%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        struct element{
+            int index;
+            char charVal;
+        };
+        
         if (s.empty() || t.empty()) return "";
         
         unordered_map<char, int> dictT;
         for (char& c:t) ++dictT[c];
         int required = dictT.size(), formed = 0;
         
-        vector<pair<int, char>> filteredS;
+        vector<element> filteredS;
         for (int i = 0; i < s.size(); ++i){
             if (dictT.contains(s[i]))
                 filteredS.push_back({i, s[i]});
@@ -17,15 +22,15 @@ public:
         int ans[3]{-1, 0, 0};
         unordered_map<char, int> windowsCount;
         for (int r = 0; r < filteredS.size(); ++r){
-            char ch = filteredS[r].second;
+            char ch = filteredS[r].charVal;
             ++windowsCount[ch];
             
             if (windowsCount[ch] == dictT[ch])
                 ++formed;
             
             while (l <= r && formed == required){
-                ch = filteredS[l].second;
-                int end = filteredS[r].first, start = filteredS[l].first;
+                ch = filteredS[l].charVal;
+                int end = filteredS[r].index, start = filteredS[l].index;
                 
                 if (ans[0] == -1 || end - start + 1 < ans[0]){
                     ans[0] = end - start + 1;
@@ -39,6 +44,6 @@ public:
                 ++l;
             }
         }
-        return ans[0] == -1? "" : s.substr(ans[1], ans[2] - ans[1] + 1);
+        return ans[0] == -1? "" : s.substr(ans[1], ans[0]);
     }
 };
