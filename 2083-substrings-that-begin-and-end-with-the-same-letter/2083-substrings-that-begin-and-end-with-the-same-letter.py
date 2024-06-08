fcf7ad95bd3cb70b4b3f6f83be462d031d6a0679
Andrew Kim <ksb0724@utexas.edu>
2024-06-08 18:27:01 +0900

    Time: 100 ms (26.45%), Space: 17.2 MB (90.91%) - LeetHub

--- 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
+++ 2083-substrings-that-begin-and-end-with-the-same-letter/2083-substrings-that-begin-and-end-with-the-same-letter.py
@@ -1,11 +1,14 @@
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
-        ans, counts = 0, Counter()
+        n = len(s)
+        ans, prefix_count = 0, [0] * 26
         
-        for c in s:
-            counts[c] += 1
+        for i in range(len(s)):
+            # Increment the number of times we encountered the current letter so far.
+            prefix_count[ord(s[i]) - 97] += 1
             
-        for c in counts:
-            ans += (counts[c] * (counts[c] + 1) >> 1)
-            
+            # Current letter can be paired with all the occurrences of it that
+            # comes before, including itself, to form a valid substring.
+            ans += prefix_count[ord(s[i]) - 97]
+        
         return ans
