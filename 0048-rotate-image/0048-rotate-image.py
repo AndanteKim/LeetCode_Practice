7ac80674b0032a22bd8fb834b2741ecd5de56381
Andrew Kim <ksb0724@utexas.edu>
2024-06-11 18:34:45 +0900

    Time: 42 ms (30.63%), Space: 16.6 MB (16.27%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -3,12 +3,20 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        self.transpose(matrix)
+        self.reflect(matrix)
+        
+    def transpose(self, matrix: List[List[int]]) -> None:
         n = len(matrix)
         
-        for i in range(n // 2 + n % 2):
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+                
+    def reflect(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        
+        for i in range(n):
             for j in range(n // 2):
-                tmp = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j]
-                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i]
-                matrix[j][n - 1 - i] = matrix[i][j]
-                matrix[i][j] = tmp
+                matrix[i][j], matrix[i][-j - 1] = matrix[i][-j - 1], matrix[i][j]
+    
