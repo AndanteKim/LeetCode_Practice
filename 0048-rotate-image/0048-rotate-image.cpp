4d396306242cea564cb20072ff587807c254152f
Andrew Kim <ksb0724@utexas.edu>
2024-06-11 18:49:40 +0900

    Time: 4 ms (38.41%), Space: 8.6 MB (61.04%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,17 +1,27 @@
 class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
+private:
+    void transpose(vector<vector<int>>& matrix){
         int n = matrix.size();
-        
-        for (int i = 0; i < n / 2 + n % 2; ++i){
-            for (int j = 0; j < n / 2; ++j){
-                int tmp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
-                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = tmp;
+        for (int i = 0; i < n; ++i){
+            for (int j = i + 1; j < n; ++j){
+                swap(matrix[i][j], matrix[j][i]);
             }
         }
-        
+    }
+    
+    void reflect(vector<vector<int>>& matrix){
+        int n = matrix.size();
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < (n >> 1); ++j){
+                swap(matrix[i][j], matrix[i][n - 1 - j]);
+            }
+        }
+    }
+    
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // rotate pi / 2 (= 90 degree) := transpose + reverse(= reflect)
+        transpose(matrix);
+        reflect(matrix);
     }
 };
