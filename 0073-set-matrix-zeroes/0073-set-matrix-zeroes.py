f8c2c663c50bfe321a76c3eedecf530dd225b7f6
Andrew Kim <ksb0724@utexas.edu>
2024-06-12 00:24:34 +0900

    Time: 102 ms (60.30%), Space: 17.4 MB (93.49%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,22 +1,37 @@
-class Solution {
-public:
-    void setZeroes(vector<vector<int>>& matrix) {
-        int R = matrix.size(), C = matrix[0].size();set<int> rows, cols;
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        is_col = False
+        R, C = len(matrix), len(matrix[0])
         
-        // Essentially, we mark the rows and columns that are to be made zero
-        for (int i = 0; i < R; ++i){
-            for (int j = 0; j < C; ++j)
-                if (matrix[i][j] == 0){
-                    rows.insert(i);
-                    cols.insert(j);
-                }
-        }
-        
-        // Iterate over the array once again, and using rows and cols sets, update the elements
-        for (int i = 0; i < R; ++i){
-            for (int j = 0; j < C; ++j)
-                if (rows.contains(i) || cols.contains(j))
-                    matrix[i][j] = 0;
-        }
-    }
-};
+        for i in range(R):
+            # Since first cell for both first row and first column is the same i.e. matrix[0][0]
+            # We can use an additional variable for either the first row/column.
+            # For this solution, we're using an additional variable for the first column
+            # and using matrix[0][0] for the first row.
+            if matrix[i][0] == 0:
+                is_col = True
+            
+            for j in range(1, C):
+                # If an element is zero, we set the first element of the corresponding row and column to 0
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+                    
+        # Iterate over the array once again and using the first row and first column, update the elements
+        for i in range(1, R):
+            for j in range(1, C):
+                if not matrix[i][0] or not matrix[0][j]:
+                    matrix[i][j] = 0
+                    
+        # See if the first row needs to be set to zero as well
+        if matrix[0][0] == 0:
+            for j in range(C):
+                matrix[0][j] = 0
+                
+        # See if the first column needs to be set to zero as well
+        if is_col:
+            for i in range(R):
+                matrix[i][0] = 0
