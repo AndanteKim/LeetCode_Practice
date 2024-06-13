bba28e3fefbcbea7921f17aa8704d39a2ca581b9
Andrew Kim <ksb0724@utexas.edu>
2024-06-13 15:59:58 +0900

    Time: 55 ms (81.76%), Space: 16.5 MB (82.54%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.py
@@ -1,10 +1,24 @@
 class Solution:
     def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-        seats.sort()
-        students.sort()
+        # Find the maximum position in the arrays
+        max_pos = max(max(seats), max(students))
         
-        ans = 0
-        for i in range(len(seats)):
-            ans += abs(seats[i] - students[i])
+        # Stores difference between number of seats and students at each position
+        diff = [0] * max_pos
+        
+        # Count the available seats at each position
+        for pos in seats:
+            diff[pos - 1] += 1
+        
+        # Remove a seat for each student at that position
+        for pos in students:
+            diff[pos - 1] -= 1
+        
+        # Calculate the number of moves needed to seat the students
+        moves, unmatched = 0, 0
+        
+        for d in diff:
+            moves += abs(unmatched)
+            unmatched += d
             
-        return ans
+        return moves
