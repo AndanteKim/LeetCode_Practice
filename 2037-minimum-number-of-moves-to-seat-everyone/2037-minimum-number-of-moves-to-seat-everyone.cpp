cbd2087b460df4b3794335c9e89e32e5842f7436
Andrew Kim <ksb0724@utexas.edu>
2024-06-13 16:12:31 +0900

    Time: 7 ms (60.18%), Space: 21 MB (17.35%) - LeetHub

--- 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(), seats.end());
-        sort(students.begin(), students.end());
+        // Find the max position in the arrays
+        int mx = max(*max_element(seats.begin(), seats.end()), *max_element(students.begin(), students.end()));
         
-        int ans = 0;
-        for (int i = 0; i < seats.size(); ++i)
-            ans += abs(seats[i] - students[i]);
+        // Stores difference between number of seats and students at each
+        // position
+        vector<int> diff(mx);
+        
+        // Count the available seats at each position
+        for (int pos : seats)
+            ++diff[pos - 1];
+        
+        // Remove the seats from students at that position
+        for (int pos : students)
+            --diff[pos - 1];
+        
+        // Calculate the number of moves needed to seat the students
+        int ans = 0, unmatched = 0;
+        for (int d : diff){
+            ans += abs(unmatched);
+            unmatched -= d;
+        }
         
         return ans;
     }
