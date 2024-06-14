1f4c746c9edff13050ac898f917095df43868ebe
Andrew Kim <ksb0724@utexas.edu>
2024-06-14 23:21:41 +0900

    Time: 124 ms (46.26%), Space: 69.2 MB (24.61%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,30 +1,14 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        int n = nums.size(), maxVal = *max_element(nums.begin(), nums.end()), minIncrements = 0;
+        sort(nums.begin(), nums.end());
+        int ans = 0, need = 0;
         
-        // Create a freqCounts vector to store the frequency of each value
-        // in nums
-        vector<int> freqCounts(n + maxVal + 1);
-        
-        // Populate freqCounts vector with the frequency of each value in nums
-        for (int num : nums)
-            ++freqCounts[num];
-        
-        // Iterate over the freqCounts vector to make all values unique
-        for (int i = 0; i < freqCounts.size(); ++i){
-            if (freqCounts[i] <= 1)
-                continue;
-            
-            // Determine excess occurrences, carry them over to the next value,
-            // ensure single occurrence for current value, and update
-            // minIncrements
-            int duplicates = freqCounts[i] - 1;
-            freqCounts[i + 1] += duplicates;
-            freqCounts[i] = 1;
-            minIncrements += duplicates;
+        for (int n:nums){
+            ans += max(need - n, 0);
+            need = max(need + 1, n + 1);
         }
         
-        return minIncrements;
+        return ans;
     }
 };
