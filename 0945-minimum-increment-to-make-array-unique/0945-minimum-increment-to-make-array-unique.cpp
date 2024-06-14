cff54c14851212c322a8d71b4a1abd8d40a1aa7f
Andrew Kim <ksb0724@utexas.edu>
2024-06-14 23:29:32 +0900

    Time: 285 ms (14.37%), Space: 101.8 MB (9.64%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int ans = 0, need = 0;
+        map<int, int> c;
         
-        for (int n:nums){
-            ans += max(need - n, 0);
-            need = max(need + 1, n + 1);
+        for (int n:nums) ++c[n];
+        
+        int ans = 0, need = 0;
+        for (auto& [x, freq] : c){
+            ans += freq * max(need - x, 0) + ((freq * (freq - 1)) >> 1);
+            need = max(need, x) + freq;
         }
         
         return ans;
