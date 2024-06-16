69420053ab5f0bd09fb9f0157d7e8035ce38a26e
Andrew Kim <ksb0724@utexas.edu>
2024-06-16 21:09:55 +0900

    Time: 106 ms (77.14%), Space: 63.9 MB (71.43%) - LeetHub

--- 1580-put-boxes-into-the-warehouse-ii/1580-put-boxes-into-the-warehouse-ii.cpp
+++ 1580-put-boxes-into-the-warehouse-ii/1580-put-boxes-into-the-warehouse-ii.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
-        int n = warehouse.size();
-        int minHeight = 1'000'000'001;
-        vector<int> effectiveHeights(n, minHeight);
-        
-        // Preprocess the height of the warehouse rooms to
-        // get usable heights from the left end.
-        for (int i = 0; i < n; ++i){
-            minHeight = min(minHeight, warehouse[i]);
-            effectiveHeights[i] = minHeight;
-        }
-        
-        minHeight = 1'000'000'001;
-        // Update the effective heights considering the right end
-        for (int i = n - 1; i >= 0; --i){
-            minHeight = min(minHeight, warehouse[i]);
-            effectiveHeights[i] = max(minHeight, effectiveHeights[i]);
-        }
-        
         // sort the boxes by height
         sort(boxes.begin(), boxes.end());
-        // sort the effective heights of the warehouse rooms
-        sort(effectiveHeights.begin(), effectiveHeights.end());
-        int ans = 0, boxIdx = 0;
+        int leftIndex = 0, rightIndex = warehouse.size() - 1;
+        int boxesCount = 0, boxIndex = boxes.size() - 1;
         
-        // Try to place each box in the warehouse
-        // from the smallest room to the largest
-        for (int i = 0; i < n; ++i){
-            if (boxIdx < boxes.size() && boxes[boxIdx] <= effectiveHeights[i]){
-                // Place the box and move to the next one
-                ++ans;
-                ++boxIdx;
+        // Iterate through the boxes from the largest to the smallest
+        while (leftIndex <= rightIndex && boxIndex >= 0){
+            // Check if the current box can fit in the
+            // leftmost available room
+            if (boxes[boxIndex] <= warehouse[leftIndex]){
+                ++boxesCount;
+                ++leftIndex;
             }
+            // Check if the current box can fit in the
+            // rightmost available room
+            else if (boxes[boxIndex] <= warehouse[rightIndex]){
+                ++boxesCount;
+                --rightIndex;
+            }
+            
+            --boxIndex;
         }
         
-        return ans;
+        return boxesCount;
     }
 };
