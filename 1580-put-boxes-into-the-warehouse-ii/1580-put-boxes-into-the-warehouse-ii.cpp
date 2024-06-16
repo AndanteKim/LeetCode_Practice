5a6a689171f23a2ca182f30dab4adac3817cbe16
Andrew Kim <ksb0724@utexas.edu>
2024-06-16 18:08:03 +0900

    Time: 104 ms (82.86%), Space: 65.2 MB (57.14%) - LeetHub

---
+++ 1580-put-boxes-into-the-warehouse-ii/1580-put-boxes-into-the-warehouse-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
+        int n = warehouse.size();
+        int minHeight = 1'000'000'001;
+        vector<int> effectiveHeights(n, minHeight);
+        
+        // Preprocess the height of the warehouse rooms to
+        // get usable heights from the left end.
+        for (int i = 0; i < n; ++i){
+            minHeight = min(minHeight, warehouse[i]);
+            effectiveHeights[i] = minHeight;
+        }
+        
+        minHeight = 1'000'000'001;
+        // Update the effective heights considering the right end
+        for (int i = n - 1; i >= 0; --i){
+            minHeight = min(minHeight, warehouse[i]);
+            effectiveHeights[i] = max(minHeight, effectiveHeights[i]);
+        }
+        
+        // sort the boxes by height
+        sort(boxes.begin(), boxes.end());
+        // sort the effective heights of the warehouse rooms
+        sort(effectiveHeights.begin(), effectiveHeights.end());
+        int ans = 0, boxIdx = 0;
+        
+        for (int i = 0; i < n; ++i){
+            if (boxIdx < boxes.size() && boxes[boxIdx] <= effectiveHeights[i]){
+                ++ans;
+                ++boxIdx;
+            }
+        }
+        
+        return ans;
+    }
+};
