3754f60719b38fa47dea62b8d15fe4c848a8124e
Andrew Kim <ksb0724@utexas.edu>
2024-06-17 17:41:32 +0900

    Time: 36 ms (9.50%), Space: 7 MB (61.35%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,13 +1,24 @@
 class Solution {
+private:
+    bool binarySearch(int start, int end, int target){
+        if (start > end) return false;
+        
+        int mid = start + ((end - start) >> 1);
+        if ((long long)mid * mid == target) return true;
+        
+        if ((long long)mid * mid > target)
+            return binarySearch(start, mid - 1, target);
+        
+        return binarySearch(mid + 1, end, target);
+    }
+    
 public:
     bool judgeSquareSum(int c) {
         
-        int b = INT_MAX;
-        for (int a = 0; a <= c && b >= a * a; ++a){
-            b = c - (a * a);
-            
-            double sqrtB = sqrt(b);
-            if (sqrtB == (int)sqrtB) return true;
+        for (long long a = 0; a * a <= c; ++a){
+            int b = c - (int)a * a;
+            if (binarySearch(0, b, b))
+                return true;
         }
         
         return false;
