472d6b9f2ad45551a49e4661d582d32767049408
Andrew Kim <ksb0724@utexas.edu>
2024-06-17 17:35:16 +0900

    Time: 2237 ms (5.00%), Space: 16.2 MB (99.89%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,12 +1,23 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        
-        for i in range(c + 1):
-            j = c - (i ** 2)
-            if j < 0:
-                break
-            sq_j = j ** 0.5
-            if sq_j == int(sq_j):
+        # s:= start, e:= end, t:= target
+        def binary_search(s: int, e: int, t: int) -> bool:
+            if s > e:
+                return False
+            
+            mid = s + ((e - s) >> 1)
+            if mid * mid == t:
                 return True
+            
+            if mid * mid > t:
+                return binary_search(s, mid - 1, t)
+            return binary_search(mid + 1, e, t)
         
+        for a in range(c + 1):
+            if a * a > c:
+                break
+            b = c - a * a
+            if binary_search(0, b, b):
+                return True
+            
         return False
