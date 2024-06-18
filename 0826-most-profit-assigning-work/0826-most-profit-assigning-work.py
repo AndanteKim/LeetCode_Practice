66cf168f436c524959091013a1b4ff56479fa19e
Andrew Kim <ksb0724@utexas.edu>
2024-06-18 17:08:15 +0900

    Time: 426 ms (11.50%), Space: 19.8 MB (33.39%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -3,30 +3,29 @@ class Solution:
         job_profile = [(0, 0)]
         
         for i in range(len(difficulty)):
-            job_profile.append((difficulty[i], profit[i]))
-            
-        # Sort by difficulty values in increasing order
-        job_profile.sort()
-        
+            job_profile.append((profit[i], difficulty[i]))
+       
+        # sort by decreasing order of profit
+        job_profile.sort(reverse = True)
         for i in range(len(job_profile) - 1):
-            job_profile[i + 1] = (job_profile[i + 1][0], max(job_profile[i][1], job_profile[i + 1][1]),)
-        
-        net_profit = 0
-        for i in range(len(worker)):
-            ability = worker[i]
+            job_profile[i + 1] = (job_profile[i + 1][0], min(job_profile[i][1], job_profile[i + 1][1]),)
             
-            # Find the job with just smaller or equal difficulty than ability
+        net_profit = 0
+        
+        for ability in worker:
+            # Maximize profit using binary search
             l, r = 0, len(job_profile) - 1
             job_profit = 0
+            
             while l <= r:
                 mid = (l + r) >> 1
-                if job_profile[mid][0] <= ability:
-                    job_profit = max(job_profit, job_profile[mid][1])
-                    l = mid + 1
-                else:
+                if job_profile[mid][1] <= ability:
+                    job_profit = max(job_profit, job_profile[mid][0])
                     r = mid - 1
+                else:
+                    l = mid + 1
             
-            # Increment profit of current worker to total profit
+            # Add profit of each worker to total profit
             net_profit += job_profit
         
         return net_profit
