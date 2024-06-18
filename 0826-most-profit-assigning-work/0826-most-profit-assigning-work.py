772dfd7d4676184acf356707a4fcd070faef7a78
Andrew Kim <ksb0724@utexas.edu>
2024-06-18 17:24:26 +0900

    Time: 264 ms (85.28%), Space: 19.5 MB (74.98%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.py
@@ -1,31 +1,18 @@
 class Solution:
     def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
-        job_profile = [(0, 0)]
+        job_profile = [(difficulty[i], profit[i]) for i in range(len(difficulty))]
         
-        for i in range(len(difficulty)):
-            job_profile.append((profit[i], difficulty[i]))
-       
-        # sort by decreasing order of profit
-        job_profile.sort(reverse = True)
-        for i in range(len(job_profile) - 1):
-            job_profile[i + 1] = (job_profile[i + 1][0], min(job_profile[i][1], job_profile[i + 1][1]),)
-            
-        net_profit = 0
+        # Sort both worker and job_profile arrays
+        worker.sort()
+        job_profile.sort()
         
+        net_profit, max_profit, idx = 0, 0, 0
         for ability in worker:
-            # Maximize profit using binary search
-            l, r = 0, len(job_profile) - 1
-            job_profit = 0
+            # While the index has not reached the end and worker can pick a job
+            # with greater difficulty move ahead.
+            while idx < len(difficulty) and ability >= job_profile[idx][0]:
+                max_profit = max(max_profit, job_profile[idx][1])
+                idx += 1
+            net_profit += max_profit
             
-            while l <= r:
-                mid = (l + r) >> 1
-                if job_profile[mid][1] <= ability:
-                    job_profit = max(job_profit, job_profile[mid][0])
-                    r = mid - 1
-                else:
-                    l = mid + 1
-            
-            # Add profit of each worker to total profit
-            net_profit += job_profit
-        
         return net_profit
