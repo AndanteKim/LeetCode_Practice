6ff8e9a985828530125d5fd151ec9b1a89e7955b
Andrew Kim <ksb0724@utexas.edu>
2024-06-19 10:08:31 +0900

    Time: 390 ms (25.62%), Space: 124.2 MB (5.02%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,21 +1,73 @@
+class UnionFind{
+private:
+    unordered_map<int, int> root, rank, size;
+    
+    int find(int x){
+        if (root[x] != x)
+            root[x] = find(root[x]);
+        return root[x];
+    }
+    
+    void unionSet(int x, int y){
+        int rootX = find(x), rootY = find(y);
+        if (rank[rootX] > rank[rootY]){
+            root[rootY] = rootX;
+            size[rootX] += size[rootY];
+        }
+        else if (rank[rootX] < rank[rootY]){
+            root[rootX] = rootY;
+            size[rootY] += size[rootX];
+        }
+        else{
+            root[rootY] = rootX;
+            ++rank[rootX];
+            size[rootX] += size[rootY];
+        }
+    }
+
+public:
+    UnionFind(vector<int>& nums){
+        for (int num : nums){
+            root[num] = num;
+            rank[num] = 0;
+            size[num] = 1;
+        }
+    }
+    
+    void unionSets(void){
+        for (const auto& num : root){
+            if (root.find(num.first + 1) != root.end())
+                unionSet(num.first, num.first + 1);
+        }
+    }
+    
+    int getMaxsize(void){
+        int maxSize = 0;
+        for (const auto& sz : size)
+            maxSize = max(maxSize, sz.second);
+        
+        return maxSize;
+    }
+};
+
+
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        int longestStreak = 0;
-        unordered_set<int> numSets(nums.begin(), nums.end());
+        /*
+        Each num is initially an island, we want to find out
+        the largest continent -> Disjoint Set
+        1. Build disjoint set for each num.
+        2. Try connect each set with its consecutive number
+        3. Return the largest set size
         
-        for (int num : nums){
-            if (!numSets.count(num - 1)){
-                int currentStreak = 1, currentNum = num; 
-                
-                while (numSets.count((currentNum++) + 1))
-                    ++currentStreak;
-                
-                longestStreak = max(longestStreak, currentStreak);
-            }
-            
-        }
+        Complexity:
+        - time: O(n * alpha(n)) where n is the size of nums
+        - space: O(n)
+        */
         
-        return longestStreak;
+        UnionFind uf(nums);     // O(n)
+        uf.unionSets();         // O(n * alpha(n))
+        return uf.getMaxsize();
     }
 };
