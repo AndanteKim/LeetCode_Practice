7274177d3ff403e4668e067c905c02b23335b3ad
Andrew Kim <ksb0724@utexas.edu>
2024-06-21 19:27:11 +0900

    Time: 16 ms (93.07%), Space: 34.3 MB (82.09%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+        int unrealizedCustomer = 0;
+        
+        // Calculate initial number of unrealized customers in the first 'minutes'
+        // window
+        for (int i = 0; i < minutes; ++i)
+            unrealizedCustomer += customers[i] * grumpy[i];
+        
+        int maxUnrealizedCustomer = unrealizedCustomer;
+        
+        // Slide the 'minutes' window across the rest of the customers array 
+        for (int i = minutes; i < customers.size(); ++i){
+            // Add the current 'minute's unsatisfied customers if the owner is
+            // grumpy and remove the customers that are out oof the current
+            // window
+            unrealizedCustomer += customers[i] * grumpy[i];
+            unrealizedCustomer -= customers[i - minutes] * grumpy[i - minutes];
+            
+            // Update the maximuum unrealized customers
+            maxUnrealizedCustomer = max(maxUnrealizedCustomer, unrealizedCustomer);
+        }
+        
+        // Start with the maximum possible satisfied customers due to the secret
+        // technique
+        int ans = maxUnrealizedCustomer;
+        
+        // Add the satisfied customers during non-grumpy minutes
+        for (int i = 0; i < customers.size(); ++i)
+            ans += customers[i] * (1 - grumpy[i]);
+        
+        // Return the maximum nuumber of satisfied customers
+        return ans;
+    }
+};
