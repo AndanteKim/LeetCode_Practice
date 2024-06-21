767af14a632de493940dcdecbf98f3e1a8aae979
Andrew Kim <ksb0724@utexas.edu>
2024-06-21 19:52:45 +0900

    Time: 19 ms (86.78%), Space: 34.4 MB (16.52%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -1,36 +1,23 @@
 class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
-        int unrealizedCustomer = 0;
+        int happy = 0;
         
-        // Calculate initial number of unrealized customers in the first 'minutes'
-        // window
-        for (int i = 0; i < minutes; ++i)
-            unrealizedCustomer += customers[i] * grumpy[i];
+        for (int i = 0; i < customers.size(); ++i)
+            happy += customers[i] * (grumpy[i] ^ 1);
         
-        int maxUnrealizedCustomer = unrealizedCustomer;
+        int maxHappy = happy;
         
-        // Slide the 'minutes' window across the rest of the customers array 
-        for (int i = minutes; i < customers.size(); ++i){
-            // Add the current 'minute's unsatisfied customers if the owner is
-            // grumpy and remove the customers that are out of the current
-            // window
-            unrealizedCustomer += customers[i] * grumpy[i];
-            unrealizedCustomer -= customers[i - minutes] * grumpy[i - minutes];
+        for (int i = 0; i < customers.size(); ++i){
+            if (i >= minutes && grumpy[i - minutes] == 1)
+                happy -= customers[i - minutes];
             
-            // Update the maximum unrealized customers
-            maxUnrealizedCustomer = max(maxUnrealizedCustomer, unrealizedCustomer);
+            if (grumpy[i] == 1)
+                happy += customers[i];
+            
+            maxHappy = max(maxHappy, happy);
         }
         
-        // Start with the maximum possible satisfied customers due to the secret
-        // technique
-        int ans = maxUnrealizedCustomer;
-        
-        // Add the satisfied customers during non-grumpy minutes
-        for (int i = 0; i < customers.size(); ++i)
-            ans += customers[i] * (1 - grumpy[i]);
-        
-        // Return the maximum nuumber of satisfied customers
-        return ans;
+        return maxHappy;
     }
 };
