ac9054df86c6494fee25b47fc4f9cb747d7b51fb
Andrew Kim <ksb0724@utexas.edu>
2024-06-21 18:31:11 +0900

    Time: 217 ms (54.69%), Space: 18.8 MB (64.93%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        n, unrealized_customers = len(customers), 0
+        
+        # Calculate initial number of unrealized customers in the first 'minutes' window
+        for i in range(minutes):
+            unrealized_customers += customers[i] * grumpy[i]
+        
+        max_unrealized_customers = unrealized_customers
+        
+        # Slide the 'minutes' window across the rest of the customers array
+        for i in range(minutes, n):
+            # Add current minute's unsatisfied customers if the owner is grumpy
+            # and remove the customers that are out of the current window
+            unrealized_customers += customers[i] * grumpy[i]
+            unrealized_customers -= customers[i - minutes] * grumpy[i - minutes]
+            
+            # Update the maximuum unrealized customers
+            max_unrealized_customers = max(max_unrealized_customers, unrealized_customers)
+            
+        # Start with maximum possible satisfied customers due to secret technique
+        total_customers = max_unrealized_customers
+        
+        # Add the satisfied customers during non-grumpy minutes
+        for i in range(n):
+            total_customers += customers[i] * (1 - grumpy[i])
+            
+        # Return the maximum number of satisfied customers
+        return total_customers
