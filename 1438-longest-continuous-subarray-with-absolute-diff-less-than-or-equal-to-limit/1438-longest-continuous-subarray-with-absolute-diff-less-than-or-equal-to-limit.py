cba23e2d16d06d9a9b50759a3bbc854e1300b6d9
Andrew Kim <ksb0724@utexas.edu>
2024-06-25 03:43:45 +0900

    Time: 1032 ms (7.80%), Space: 26.9 MB (20.47%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,27 +1,27 @@
+from sortedcontainers import SortedDict
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        max_heap, min_heap = [], []
-        
+        # SortedDict to maintain the elements within the current window
+        window = SortedDict()
         left, max_length = 0, 0
         
         for right in range(len(nums)):
-            heappush(max_heap, (-nums[right], right))
-            heappush(min_heap, (nums[right], right))
+            if nums[right] in window:
+                window[nums[right]] += 1
+            else:
+                window[nums[right]] = 1
             
-            # Check if the absolutue difference between the maximum and minimum values in the current window
-            # exceeds the limit
-            while -max_heap[0][0] - min_heap[0][0] > limit:
-                # Move the left pointer to the right until the condition is satisfied.
-                # This ensures we remove the element causing the violation
-                left = min(max_heap[0][1], min_heap[0][1]) + 1
-                
-                # Remove elements from the heaps that are outside the current window
-                while max_heap[0][1] < left:
-                    heappop(max_heap)
-                while min_heap[0][1] < left:
-                    heappop(min_heap)
+            # Check if the absolute difference between the maximum and minimum values in the current window exceed the limit
+            while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
+                # Remove the element at the left pointer from the SortedDict
+                window[nums[left]] -= 1
+                if window[nums[left]] == 0:
+                    window.pop(nums[left])
                     
-            # Update max_length with the length of the current valid window
+                # Move the left pointer to the right to exclude the element causing the violaation
+                left += 1
+                
+            # Update maxLength with the length of the current valid window
             max_length = max(max_length, right - left + 1)
         
         return max_length
