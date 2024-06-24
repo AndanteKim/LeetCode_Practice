e442b2887df8033c0465ab792530567c0d12e2bd
Andrew Kim <ksb0724@utexas.edu>
2024-06-25 02:51:13 +0900

    Time: 92 ms (53.07%), Space: 108.6 MB (88.93%) - LeetHub

--- 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 0995-minimum-number-of-k-consecutive-bit-flips/0995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -2,35 +2,30 @@ class Solution {
 public:
     int minKBitFlips(vector<int>& nums, int k) {
         int n = nums.size();
-        // Queue to keep track of flips
-        // Current flip state
-        int res = 0, flipped = 0;
-        // Total number of flips
-        deque<int> flipQueue;
+        // Track the current number of flips
+        int currFlip = 0;
+        // Track the total number of flips
+        int totalFlip = 0;
         
         for (int i = 0; i < n; ++i){
-            // Remove the effect of the oldest flip if it's out of the current
-            // window
-            if (i >= k){
-                flipped ^= flipQueue.front();
-                flipQueue.pop_front();
+            // If the window slides out of the range and the leftmost element is marked as flipped (2)
+            // decrement the current flip by 1
+            if (i >= k && nums[i - k] == 2){
+                --currFlip;
             }
             
-            // If the current bit is 0, it needs to be flipped
-            if (flipped == nums[i]){
-                // If we cannot flip a subarray starting at index i
-                if (i + k > n)
-                    return -1;
-                // Add a flip at this position
-                flipQueue.push_back(1);
-                // Toggle the flipped state
-                flipped ^= 1;
-                // Increment the flip count
-                ++res;
+            // Check if the current bit needs to be flipped
+            if (currFlip % 2 == nums[i]){
+                // If flipping would exceed array bounds, return -1
+                if (i + k > n) return -1;
+                
+                // Mark the current bit as flipped
+                nums[i] = 2;
+                ++currFlip;
+                ++totalFlip;
             }
-            else flipQueue.push_back(0);
         }
         
-        return res;
+        return totalFlip;
     }
 };
