3d5d292fe75cd004537131243abdea011cf7c3cb
Andrew Kim <ksb0724@utexas.edu>
2024-06-25 10:57:32 +0900

    Time: 41 ms (21.43%), Space: 16.5 MB (74.62%) - LeetHub

---
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: TreeNode) -> TreeNode:
+        # Store the inorder traversal in an array.
+        self.inorder_traversal = []
+        self.inorder(root)
+        
+        # Reverse the array to get descending order.
+        self.inorder_traversal.reverse()
+        
+        # Modify the values in the tree.
+        self.replace_values(root)
+        
+        return root
+    
+    def inorder(self, root: TreeNode) -> None:
+        if not root:
+            return
+            
+        self.inorder(root.left)
+        self.inorder_traversal.append(root.val)
+        self.inorder(root.right)
+        
+    # Function to modify the values in the tree.
+    def replace_values(self, root: TreeNode) -> None:
+        if not root:
+            return
+        
+        self.replace_values(root.left)
+        self.replace_values(root.right)
+        
+        # Replace node with values greater than the current value.
+        node_sum = 0
+        for i in self.inorder_traversal:
+            if i > root.val:
+                node_sum += i
+            else:
+                break
+                
+        root.val += node_sum
