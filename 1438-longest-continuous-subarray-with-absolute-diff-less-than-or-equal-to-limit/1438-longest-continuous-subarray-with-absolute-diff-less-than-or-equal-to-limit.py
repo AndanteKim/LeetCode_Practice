042b68d2b43766926082522970f2828370b1529c
Andrew Kim <ksb0724@utexas.edu>
2024-06-25 09:48:20 +0900

    Time: 435 ms (79.70%), Space: 26.4 MB (41.10%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -1,27 +1,31 @@
-from sortedcontainers import SortedDict
 class Solution:
     def longestSubarray(self, nums: List[int], limit: int) -> int:
-        # SortedDict to maintain the elements within the current window
-        window = SortedDict()
+        max_dq, min_dq = deque(), deque()
         left, max_length = 0, 0
         
         for right in range(len(nums)):
-            if nums[right] in window:
-                window[nums[right]] += 1
-            else:
-                window[nums[right]] = 1
+            # Maintain the max_dq in decreasing order
+            while max_dq and max_dq[-1] < nums[right]:
+                max_dq.pop()
+            max_dq.append(nums[right])
             
-            # Check if the absolute difference between the maximum and minimum values in the current window exceed the limit
-            while window.peekitem(-1)[0] - window.peekitem(0)[0] > limit:
-                # Remove the element at the left pointer from the SortedDict
-                window[nums[left]] -= 1
-                if window[nums[left]] == 0:
-                    window.pop(nums[left])
+            # Maintain the min_dq in increasing order
+            while min_dq and min_dq[-1] > nums[right]:
+                min_dq.pop()
+            min_dq.append(nums[right])
+            
+            # Check if the current window exceeds the limit
+            while max_dq[0] - min_dq[0] > limit:
+                # Remove the elements that are out of the current window
+                if max_dq[0] == nums[left]:
+                    max_dq.popleft()
                     
-                # Move the left pointer to the right to exclude the element causing the violaation
+                if min_dq[0] == nums[left]:
+                    min_dq.popleft()
+                    
                 left += 1
-                
-            # Update maxLength with the length of the current valid window
+            
             max_length = max(max_length, right - left + 1)
-        
+            
         return max_length
+        
