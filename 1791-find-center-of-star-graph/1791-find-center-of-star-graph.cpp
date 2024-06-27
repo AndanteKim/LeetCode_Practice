874c3e7e7c272a3377e7737fba4957db9d15e556
Andrew Kim <ksb0724@utexas.edu>
2024-06-27 10:18:27 +0900

    Time: 151 ms (38.89%), Space: 70.9 MB (45.39%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.cpp
@@ -3,6 +3,6 @@ public:
     int findCenter(vector<vector<int>>& edges) {
         vector<int> firstEdge = edges[0], secondEdge = edges[1];
         
-        return find(secondEdge.begin(), secondEdge.end(), firstEdge[0]) != secondEdge.end()? firstEdge[0] : firstEdge[1];
+        return (firstEdge[0] == secondEdge[0] || firstEdge[0] == secondEdge[1])? firstEdge[0] : firstEdge[1];
     }
 };
