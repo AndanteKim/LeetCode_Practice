2dd0b51118b29e54d3130a8c3e70efad9869e0d3
Andrew Kim <ksb0724@utexas.edu>
2024-06-28 20:34:49 +0900

    Time: 69 ms (55.38%), Space: 18.1 MB (54.21%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,56 @@
+class Solution:
+    def evaluate_expr(self, stack: List[int]) -> int:
+        # If stack is empty or the expression starts with
+        # a symbol, then append 0 to the stack.
+        # i.e. [1, '-', 2, '-'] becomes [1, '-', 2, '-', 0]
+        if not stack or type(stack[-1]) == str:
+            stack.append(0)
+            
+        res = stack.pop()
+        
+        # Evaluate the expression til we get corresponding ')'
+        while stack and stack[-1] != ')':
+            sign = stack.pop()
+            if sign == '+':
+                res += stack.pop()
+            else:
+                res -= stack.pop()
+                
+        return res
+    
+    def calculate(self, s: str) -> int:
+        stack = []
+        n, operand = 0, 0
+        
+        for i in range(len(s) - 1, -1, -1):
+            ch = s[i]
+            
+            if ch.isdigit():
+                # Forming the operand - in reverse order.
+                operand = (10 ** n * int(ch)) + operand
+                n += 1
+            
+            elif ch != " ":
+                if n:
+                    # Save the operand on the stack
+                    # As we encounter some non-digit
+                    stack.append(operand)
+                    n, operand = 0, 0
+                    
+                if ch == '(':
+                    res = self.evaluate_expr(stack)
+                    stack.pop()
+                    
+                    # Append the evaluated result to the stack.
+                    # This result could be of a sub-expression within the parenthesis.
+                    stack.append(res)
+                # For other non-digits just push onto the stack.
+                else:
+                    stack.append(ch)
+                    
+        # Push the last operand to stack, if any.
+        if n:
+            stack.append(operand)
+        
+        # Evaluate any left overs in the stack.
+        return self.evaluate_expr(stack)
