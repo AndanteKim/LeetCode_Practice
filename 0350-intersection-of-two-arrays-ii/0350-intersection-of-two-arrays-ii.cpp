1aa28a0ecfed29b3f3cd17dafbdf99e14f79173a
Andrew Kim <ksb0724@utexas.edu>
2024-07-02 19:02:14 +0900

    Time: 8 ms (19.74%), Space: 12.7 MB (59.70%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -4,16 +4,9 @@ public:
         sort(begin(nums1), end(nums1));
         sort(begin(nums2), end(nums2));
         
-        int i = 0, j = 0, k = 0;
-        while (i < nums1.size() && j < nums2.size()){
-            if (nums1[i] < nums2[j]) ++i;
-            else if (nums1[i] > nums2[j]) ++j;
-            else{
-                nums1[k++] = nums1[i++];
-                ++j;
-            }   
-        }
+        nums1.erase(set_intersection(begin(nums1), end(nums1),
+                   begin(nums2), end(nums2), begin(nums1)), end(nums1));
         
-        return vector<int>(begin(nums1), begin(nums1) + k);
+        return nums1;
     }
 };
