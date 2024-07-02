dd52bb5126632d954144d29f8b96f251bda52a06
Andrew Kim <ksb0724@utexas.edu>
2024-07-02 19:20:49 +0900

    Time: 44 ms (81.60%), Space: 16.7 MB (31.35%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.py
@@ -1,9 +1,27 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        common = Counter(nums1) & Counter(nums2)
-        
         ans = []
-        for c, v in common.items():
-            ans.extend([c] * v)
+        
+        nums1.sort()
+        nums2.sort()
+        
+        i, j = 0, 0
+        
+        while i < len(nums1):
+            idx = bisect_left(nums2, nums1[i])
+            cnt2 = 0
+            
+            while idx < len(nums2) and nums2[idx] == nums1[i]:
+                cnt2 += 1
+                idx += 1
+            
+            cnt1 = 0
+            while j < len(nums1) and nums1[j] == nums1[i]:
+                j += 1
+                cnt1 += 1
+                
+            ans.extend([nums1[i]] * min(cnt1, cnt2))
+            i = j
             
         return ans
+        
