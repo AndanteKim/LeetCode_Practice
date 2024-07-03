bf6e943336293085b694ccb6f2d4a25818672bfd
Andrew Kim <ksb0724@utexas.edu>
2024-07-03 20:51:53 +0900

    Time: 59 ms (90.30%), Space: 37.8 MB (95.34%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        int n = nums.size();
-        
-        // If the array has 34 or fewer elements, return 0
+        int n = nums.size(), minDiff = INT_MAX;
         if (n <= 4) return 0;
         
-        sort(nums.begin(), nums.end());
-        int minDiff = INT_MAX;
+        // Partially sort the first four elements
+        partial_sort(nums.begin(), nums.begin() + 4, nums.end());
+        
+        // Find the 4th largest element
+        nth_element(nums.begin() + 4, nums.begin() + (n - 4), nums.end());
+        
+        // Sort the last four elements
+        sort(nums.begin() + (n - 4), nums.end());
         
         // Four scenarios to compute the minimum difference
-        for (int left = 0, right = n - 4; left < 4; ++left, ++right){
+        for (int left = 0, right = n - 4; left < 4; ++left, ++right)
             minDiff = min(minDiff, nums[right] - nums[left]);
-        }
         
         return minDiff;
     }
