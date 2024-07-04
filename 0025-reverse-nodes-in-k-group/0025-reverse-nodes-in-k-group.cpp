6b7970b38f9280c4981d64fcfa7e2f1706cb128e
Andrew Kim <ksb0724@utexas.edu>
2024-07-04 23:50:59 +0900

    Time: 3 ms (98.87%), Space: 15.1 MB (39.25%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -9,44 +9,59 @@
  * };
  */
 class Solution {
+private:
+    ListNode* reverseLL(ListNode* head, int k){
+        // Reverse k nodes of the given linked list.
+        // This function assumes that the list contains
+        // at least k nodes.
+        ListNode* prev = nullptr, *ptr = head;
+        
+        // Decrement the count of nodes to be reversed by 1.
+        while (k-- > 0){
+            // Keep track of the next node to process in the
+            // original list
+            ListNode* nextNode = ptr -> next;
+            
+            // Insert the node pointed to by "ptr"
+            // at the beginning of the reversed list
+            ptr -> next = prev;
+            prev = ptr;
+            
+            // Move on to the next node
+            ptr = nextNode;
+        }
+        
+        // Return the head of the reversed list
+        return prev;
+    }
+    
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *sentinel = new ListNode();
-        sentinel -> next = head;
-        ListNode *curr = head, *prev = sentinel;
-        stack<ListNode*> stk;
+        int count = 0;
+        ListNode* ptr = head;
         
-        while (curr){
-            if (stk.size() == k){
-                prev -> next = stk.top();
-                ListNode* nextNode = stk.top() -> next, *node;
-                while (stk.size() > 1){
-                    node = stk.top(); stk.pop();
-                    node -> next = stk.top();
-                }
-                
-                node = stk.top(); stk.pop();
-                prev = node;
-                node -> next = nextNode;
-            }
-            
-            stk.push(curr);
-            curr = curr -> next;
+        // First, see if there are at least k nodes
+        // left in the linked list.
+        while (ptr && count < k){
+            ++count;
+            ptr = ptr -> next;
         }
         
-        if (stk.size() == k){
-            prev -> next = stk.top();
-            ListNode *nextNode = stk.top() -> next, *node;
-            while (stk.size() > 1){
-                node = stk.top(), stk.pop();
-                node -> next = stk.top();
-            }
+        // If we have k nodes, then we reverse them.
+        if (count == k){
             
-            node = stk.top(); stk.pop();
-            prev = node;
-            node -> next = nextNode;
+            // Reverse the first k nodes of the list and
+            // get the reversed list's head.
+            ListNode* reverseHead = reverseLL(head, k);
+            
+            // Now, recurse on the remaaining linked list. Since
+            // our recursion returns the head of the overall processed
+            // list, we use that and the "original" head of the "k" nodes
+            // to re-wire the connections.
+            head -> next = reverseKGroup(ptr, k);
+            return reverseHead;
         }
         
-        return sentinel -> next;
+        return head;
     }
 };
