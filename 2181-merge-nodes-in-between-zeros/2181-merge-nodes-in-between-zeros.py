2e6efa37808f4db9e7bcf69be8b163a68aa8d0ee
Andrew Kim <ksb0724@utexas.edu>
2024-07-04 18:13:40 +0900

    Time: 931 ms (62.73%), Space: 68.8 MB (59.10%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
@@ -5,25 +5,23 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # Initialize a sentinel/dummy node with the first non-zero value.
-        modify = head.next
-        next_sum = modify
+        # Staart with the first non-zero value.
+        head = head.next
+        if not head:
+            return head
         
-        while next_sum:
-            total = 0
+        # Initialize a dummy head node.
+        temp = head
+        total = 0
+        
+        while temp.val != 0:
+            total += temp.val
+            temp = temp.next
             
-            # Find the sum oof aall nodes until you encounter a 0.
-            while next_sum.val != 0:
-                total += next_sum.val
-                next_sum = next_sum.next
-            
-            # Assign the sum to the current node's value.
-            modify.val = total
-            # Move next_sum to the first non-zero value of the next block.
-            next_sum = next_sum.next
-            
-            # Move modify also to this node.
-            modify.next = next_sum
-            modify = modify.next
-            
-        return head.next
+        # Store the sum in head's value.
+        head.val = total
+        
+        # Store head's next node as the recursive result for temp node.
+        head.next = self.mergeNodes(temp)
+        
+        return head
