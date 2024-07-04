99c39d1269449a52a97e0b32c6f27b48e39b57ac
Andrew Kim <ksb0724@utexas.edu>
2024-07-04 22:37:57 +0900

    Time: 39 ms (78.82%), Space: 17.1 MB (99.83%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -4,33 +4,50 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+    def reverseLL(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        # Reverse k nodes of the given linked list.
+        # This function assumes thaat the list contains
+        # at least k nodes.
+        new_head, ptr = None, head
+        while k:
+            # Keep track of the next node to process in 
+            # the original list
+            next_node = ptr.next
+            
+            # Insert the node pointed to by "ptr"
+            # at the beginning of the reversed list
+            ptr.next = new_head
+            new_head = ptr
+            
+            # Move on to the next node
+            ptr = next_node
+            
+            # Decrement the count of nodes to be reversed by 1
+            k -= 1
+            
+        # Return the head of the reversed list
+        return new_head
+    
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        sentinel = ListNode()
-        sentinel.next, curr = head, head
-        stack, prev = [], sentinel
+        cnt, ptr = 0, head
         
-        while curr:
-            if len(stack) == k:
-                prev.next = stack[-1]
-                next_node = stack[-1].next
-                while len(stack) > 1:
-                    node = stack.pop()
-                    node.next = stack[-1]
-                node = stack.pop()
-                prev = node
-                node.next = next_node
+        # First, see if there are at least k nodes left
+        # in the linked list.
+        while cnt < k and ptr:
+            ptr = ptr.next
+            cnt += 1
             
-            stack.append(curr)
-            curr = curr.next
+        # If we have k nodes, then we reverse them.
+        if cnt == k:
+            # Reverse the first k nodes of the list and 
+            # get the reversedd list's head
+            reverse_head = self.reverseLL(head, k)
             
-        if len(stack) == k:
-            prev.next = stack[-1]
-            next_node = stack[-1].next
-            while len(stack) > 1:
-                node = stack.pop()
-                node.next = stack[-1]
-            node = stack.pop()
-            prev = node
-            node.next = next_node
+            # Now, recurse on the remaining linked list.
+            # Since our recursion returns the head of the overall processed
+            # list, we use that and the "original" head of the "k" nodes
+            # to re-wire the connections.
+            head.next = self.reverseKGroup(ptr, k)
+            return reverse_head
         
-        return sentinel.next
+        return head
