8584c7515a1b8a3a77aa04ded1fbb00d750770a5
Andrew Kim <ksb0724@utexas.edu>
2024-07-06 21:15:44 +0900

    Time: 41 ms (16.71%), Space: 16.4 MB (97.72%) - LeetHub

--- 2582-pass-the-pillow/2582-pass-the-pillow.py
+++ 2582-pass-the-pillow/2582-pass-the-pillow.py
@@ -1,14 +1,16 @@
 class Solution:
     def passThePillow(self, n: int, time: int) -> int:
+        # Calculate the number of complete rounds of pillow passing
+        full_rounds = time // (n - 1)
         
-        curr, sign = 1, 1
-        while time > 0:
-            # Pass the pillow
-            curr += sign
-            
-            if curr == 1 or curr == n:
-                sign *= -1
-            
-            time -= 1
-            
-        return curr
+        # Calculate the remaining time after complete rounds
+        extraTime = time % (n - 1)
+        
+        # Determine the position of the person holding the pillow
+        # If full_rounds is even, the pillow is moving forward
+        # If full_rounds is odd, the pillow is moving backward
+        if full_rounds % 2 == 0:
+            return extraTime + 1 # Position when moving forward
+        else:
+            return n - extraTime # Position when moving backward
+        
