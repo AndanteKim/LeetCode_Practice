1ced0b1ca329b31727dee74820046d1dbc6cc77a
Andrew Kim <ksb0724@utexas.edu>
2024-07-07 09:57:57 +0900

    Time: 2 ms (53.84%), Space: 7.1 MB (72.30%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int ans = 0, empty = 0, recycle;
-        
-        while (numBottles > 0){
-            ans += numBottles;
-            empty += numBottles;
-            numBottles = empty / numExchange;
-            empty %= numExchange;
+        int consumedBottles = 0;
+
+        while (numBottles >= numExchange){
+            // Maximum number of times we can consume numExchange
+            // number of bottles using numBottles.
+            int K = numBottles / numExchange;
+            consumedBottles += numExchange * K;
+            numBottles -= numExchange * K;
+            
+            numBottles += K;
         }
         
-        return ans;
+        return consumedBottles + numBottles;
     }
 };
