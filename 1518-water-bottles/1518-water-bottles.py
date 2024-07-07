8b05c8a83d054f7326102c20bdd844aa9f0341cd
Andrew Kim <ksb0724@utexas.edu>
2024-07-07 10:00:21 +0900

    Time: 38 ms (30.42%), Space: 16.6 MB (32.78%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.py
+++ 1518-water-bottles/1518-water-bottles.py
@@ -1,15 +1,4 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        consumed_bottles = 0
-
-        while numBottles >= numExchange:
-            # Maximum number of times we can consume numExchange
-            # number of bottles using numBottles
-            K = numBottles // numExchange
-            
-            consumed_bottles += numExchange * K
-            numBottles -= numExchange * K
-            
-            numBottles += K
-        
-        return consumed_bottles + numBottles
+        # Mathematical formula
+        return numBottles + ((numBottles - 1) // (numExchange - 1))
