5a7a60322177f33e60a080342b350d718972326c
Andrew Kim <ksb0724@utexas.edu>
2024-07-10 20:42:14 +0900

    Time: 48 ms (93.12%), Space: 17.6 MB (98.29%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,28 +6,29 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def array_to_tree(left: int, right: int) -> Optional[TreeNode]:
-            nonlocal preorder_index
-            # base case: if there is no elements to construct the tree
-            if left > right:
-                return None
-            
-            # select the preorder_index element as the root and increment it
-            root_val = preorder[preorder_index]
-            root = TreeNode(root_val)
-            preorder_index += 1
-            
-            # build left and right subtree
-            # excluding inorder_index_maa[root_val] element because it's the root
-            root.left = array_to_tree(left, inorder_index_map[root_val] - 1)
-            root.right = array_to_tree(inorder_index_map[root_val] + 1, right)
-            
-            return root
+        # Iterative
+        if not preorder:
+            return None
         
-        inorder_index_map = dict()
-        # Build a hash map to store value -> its index relations
-        for idx, val in enumerate(inorder):
-            inorder_index_map[val] = idx
-        preorder_index = 0
+        root = TreeNode(preorder[0])
+        pre, ino = 1, 0
+        stack = []
+        stack.append(root)
         
-        return array_to_tree(0, len(preorder) - 1)
+        while pre < len(preorder):
+            curr = TreeNode(preorder[pre])
+            pre += 1
+            prev = None
+            
+            while stack and stack[-1].val == inorder[ino]:
+                prev = stack.pop()
+                ino += 1
+            
+            if prev:
+                prev.right = curr
+            else:
+                stack[-1].left = curr
+        
+            stack.append(curr)
+            
+        return root
