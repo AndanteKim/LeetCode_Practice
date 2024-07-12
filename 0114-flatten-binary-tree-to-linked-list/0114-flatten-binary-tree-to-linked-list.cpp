4d24c2cb0c1acd3c9902b89fdb59a0adb4fc5fd6
Andrew Kim <ksb0724@utexas.edu>
2024-07-13 04:27:29 +0900

    Time: 10 ms (12.49%), Space: 16.9 MB (5.56%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,28 +11,37 @@
  */
 class Solution {
 private:
-    void preorder(TreeNode* root, vector<int>& flatten){
-        if (!root) return;
+    TreeNode* flattenTree(TreeNode* node){
+        // Handle the null scenario
+        if (!node) return nullptr;
         
-        flatten.push_back(root -> val);
-        preorder(root -> left, flatten);
-        preorder(root -> right, flatten);
+        // For a leaf node, we simply return the node as is.
+        if (!(node -> left || node -> right))
+            return node;
+        
+        // Recursively flatten the left subtree
+        TreeNode* leftTail = flattenTree(node -> left);
+        
+        // Recursively flatten the right subtree
+        TreeNode* rightTail = flattenTree(node -> right);
+        
+        // If there was a left subtree, we shuffle the connections
+        // around, so that there is nothing on the left side
+        // anymore.
+        if (leftTail){
+            leftTail -> right = node -> right;
+            node -> right = node -> left;
+            node -> left = nullptr;
+        }
+        
+        // We need to return the "rightmost" node after we are
+        // done wiring the new connections.
+        return (rightTail)? rightTail : leftTail;
     }
+    
     
 public:
     void flatten(TreeNode* root) {
-        if (!root) return;
-
-        vector<int> flatten;
-        preorder(root, flatten);
-        root -> left = nullptr;
-        root -> right = nullptr;
-        TreeNode* curr = root;
-        
-        for (int i = 1; i < flatten.size(); ++i){
-            curr -> right = new TreeNode(flatten[i]);
-            curr = curr -> right;
-        }
-        
+        this -> flattenTree(root);
     }
 };
