3c15f6c07884beb1ddcd428673673fb97cfbe367
Andrew Kim <ksb0724@utexas.edu>
2024-07-13 22:20:19 +0900

    Time: 60 ms (35.94%), Space: 21.7 MB (73.91%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.py
@@ -5,29 +5,58 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def compute_depth(self, node: TreeNode) -> int:
+        """
+        Return tree depth in O(d) time.
+        """
+        
+        d = 0
+        while node.left:
+            node = node.left
+            d += 1
+            
+        return d
+    
+    def exists(self, idx: int, d: int, node: TreeNode) -> bool:
+        """
+        Last level nodes are enumerated from 0 to 2 ** d - 1 (left -> right)
+        Return True if last level node idx exists.
+        Binary search with O(d) complexity
+        """
+        left, right = 0, 2 ** d - 1
+        for _ in range(d):
+            pivot = left + ((right - left) >> 1)
+            if idx <= pivot:
+                node = node.left
+                right = pivot
+            else:
+                node = node.right
+                left = pivot + 1
+                
+        return True if node else False
+        
+    
     def countNodes(self, root: Optional[TreeNode]) -> int:
+        # If the tree node is empty
         if not root:
             return 0
         
-        ans = 0
+        d = self.compute_depth(root)
+        # If the tree contains 1 node
+        if d == 0:
+            return 1
         
-        while root:
-            if root.left:
-                predecessor = root.left
-                while predecessor.right and predecessor.right != root:
-                    predecessor = predecessor.right
-                    
-                if not predecessor.right:
-                    predecessor.right = root
-                    root = root.left
-                    ans += 1
-                else:
-                    predecessor.right = None
-                    root = root.right
-                
+        # Last level node are enumerated from to 2 ** d - 1 (left -> right)
+        # Perform binary search to check how many nodes exist.
+        left, right = 1, 2 ** d - 1
+        
+        while left <= right:
+            pivot = left + ((right - left) >> 1)
+            if self.exists(pivot, d, root):
+                left = pivot + 1
             else:
-                ans += 1
-                root = root.right
-        
-        return ans
-        
+                right = pivot - 1
+                
+        # The tree contains 2 ** d - 1 nodes on the first (d - 1) levels
+        # and left nodes on the last level.
+        return (2 ** d - 1) + left
