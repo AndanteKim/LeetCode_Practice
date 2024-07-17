d9d3e8b300c0189bc190e6f18e9aa5a9e974f592
Andrew Kim <ksb0724@utexas.edu>
2024-07-18 03:10:50 +0900

    Time: 18 ms (41.04%), Space: 27.4 MB (20.14%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -10,36 +10,45 @@
  * };
  */
 class Solution {
-private:
-    TreeNode* processNode(TreeNode* node, unordered_set<int>& toDeleteSet, vector<TreeNode*>& forest){
-        if (!node)
-            return nullptr;
-        
-        node -> left = processNode(node -> left, toDeleteSet, forest);
-        node -> right = processNode(node -> right, toDeleteSet, forest);
-        
-        // Node evaluation: Check if the current node needs to be deleted
-        if (toDeleteSet.count(node -> val)){
-            if (node -> left)
-                forest.push_back(node -> left);
-            if (node -> right)
-                forest.push_back(node -> right);
-            
-            // Delete the current node by returning nullptr to its parent
-            return nullptr;
-        }
-        
-        return node;
-    }
-    
 public:
     vector<TreeNode*> delNodes(TreeNode* root, vector<int>& to_delete) {
+        if (!root) return {};
+        
         vector<TreeNode*> forest;
         unordered_set<int> toDeleteSet(to_delete.begin(), to_delete.end());
-        root = processNode(root, toDeleteSet, forest);
+        queue<TreeNode*> queue;
+        queue.push(root);
         
-        // IF the root is not deleted, add it to the forest
-        if (root)
+        while (!queue.empty()){
+            TreeNode* curr = queue.front(); queue.pop();
+            
+            if (curr -> left){
+                queue.push(curr -> left);
+                
+                // Disconnect the left child if it needs to be deleted
+                if (toDeleteSet.count(curr -> left -> val))
+                    curr -> left = nullptr;
+            }
+            
+            if (curr -> right){
+                queue.push(curr -> right);
+                
+                // Disconnect the right child if it needs to be deleted
+                if (toDeleteSet.count(curr -> right -> val))
+                    curr -> right = nullptr;
+            }
+            
+            // If the current node needs to be deleted, add its non-null children to the forest
+            if (toDeleteSet.count(curr -> val)){
+                if (curr -> left)
+                    forest.push_back(curr -> left);
+                if (curr -> right)
+                    forest.push_back(curr -> right);
+            }
+        }
+        
+        // Ensure the root is added to the forest if it's not to be deleted
+        if (!toDeleteSet.count(root -> val))
             forest.push_back(root);
         
         return forest;
