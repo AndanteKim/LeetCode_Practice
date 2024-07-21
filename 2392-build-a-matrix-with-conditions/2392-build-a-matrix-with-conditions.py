c570c7a9d70445f7ef0b6989fdf28498d815b898
Andrew Kim <ksb0724@utexas.edu>
2024-07-22 00:02:11 +0900

    Time: 596 ms (19.85%), Space: 25.4 MB (45.80%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.py
@@ -1,63 +1,44 @@
 class Solution:
-    def __topoSort(self, edges: List[List[int]], n: int) -> List[int]:
-        adj = defaultdict(list)
-        order = []
-        visited, has_cycle = [0] * (n + 1), [False]
+    def __topo_sort(self, edges: List[List[int]], n: int) -> List[int]:
+        adj, order = [[] for _ in range(n + 1)], []
+        deg = [0] * (n + 1)
         
-        # Build adjacency list
-        for x, y in edges:
-            adj[x].append(y)
-            
-        # Perform DFS for each node
+        for x in edges:
+            adj[x[0]].append(x[1])
+            deg[x[1]] += 1
+        
+        q = deque()
+        
         for i in range(1, n + 1):
-            if visited[i] == 0:
-                self.__dfs(i, adj, visited, order, has_cycle)
+            if deg[i] == 0:
+                q.append(i)
                 
-                # Return empty if cycle detected
-                if has_cycle[0]:
-                    return []
-                
-        # Reverse to get the correct order
-        order.reverse()
-        return order
-    
-    def __dfs(self, node: int, adj: DefaultDict[int, List[int]], visited: List[int], \
-             order: List[int], has_cycle: List[bool]) -> None:
-        # Mark node as visiting
-        visited[node] = 1
-        for neighbor in adj[node]:
-            if visited[neighbor] == 0:
-                self.__dfs(neighbor, adj, visited, order, has_cycle)
-                # Early exit if a cycle is detected
-                if has_cycle[0]:
-                    return
-                
-            elif visited[neighbor] == 1:
-                # Cycle detected
-                has_cycle[0] = True
-                return
+        while q:
+            f = q.popleft()
+            order.append(f)
+            n -= 1
             
-        # Mark node as visited
-        visited[node] = 2
+            for v in adj[f]:
+                deg[v] -= 1
+                if deg[v] == 0:
+                    q.append(v)
         
-        # Add node to the order
-        order.append(node)
+        return [] if n != 0 else order
+        
     
     def buildMatrix(self, k: int, rowConditions: List[List[int]], colConditions: List[List[int]]) -> List[List[int]]:
-        # Store the topologically sorted sequences.
-        order_rows = self.__topoSort(rowConditions, k)
-        order_cols = self.__topoSort(colConditions, k)
+        order_rows = self.__topo_sort(rowConditions, k)
+        order_cols = self.__topo_sort(colConditions, k)
         
-        # If no topological sort exists, return empty array.
         if not (order_rows and order_cols):
             return []
         
         matrix = [[0] * k for _ in range(k)]
-        pos_row = {num: i for i, num in enumerate(order_rows)}
-        pos_col = {num: i for i, num in enumerate(order_cols)}
-        
-        for num in range(1, k + 1):
-            if num in pos_row and num in pos_col:
-                matrix[pos_row[num]][pos_col[num]] = num
-                
+        for i in range(k):
+            for j in range(k):
+                if order_rows[i] == order_cols[j]:
+                    matrix[i][j] = order_rows[i]
+                    
         return matrix
+        
+        
