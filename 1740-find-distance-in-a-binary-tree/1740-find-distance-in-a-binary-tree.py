9b23d489bbd67b0326dcf42bb980015c6346050b
Andrew Kim <ksb0724@utexas.edu>
2024-07-22 04:19:59 +0900

    Time: 57 ms (72.35%), Space: 20.7 MB (74.20%) - LeetHub

--- 1740-find-distance-in-a-binary-tree/1740-find-distance-in-a-binary-tree.py
+++ 1740-find-distance-in-a-binary-tree/1740-find-distance-in-a-binary-tree.py
@@ -5,39 +5,44 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findDistance(self, root: Optional[TreeNode], p: int, q: int) -> int:
-        # Find the lowest common ancestor of p and q
-        lca = self.__find_LCA(root, p, q)
-        return self.__depth(lca, p) + self.__depth(lca, q)
-    
-    # Function to find the LCA of the given nodes
-    def __find_LCA(self, root: Optional[TreeNode], p: int, q: int) -> TreeNode:
+    def find_LCA(self, root: TreeNode, p: int, q: int) -> TreeNode:
         if not root or root.val == p or root.val == q:
             return root
         
-        left = self.__find_LCA(root.left, p, q)
-        right = self.__find_LCA(root.right, p, q)
+        left = self.find_LCA(root.left, p, q)
+        right = self.find_LCA(root.right, p, q)
         
         if left and right:
             return root
         
         return left if left else right
     
-    # Function to find the depth of the node with respect to LCA.
-    def __depth(self, root: TreeNode, target: int, curr_depth = 0):
-        # Node not found
-        if not root:
-            return -1
-        
-        if root.val == target:
-            return curr_depth
-        
-        # Check left subtree
-        left_depth = self.__depth(root.left, target, curr_depth + 1)
-        
-        if left_depth != -1:
-            return left_depth
-        
-        # If not in left subtree, it's guaranteed to be in the right subtree
-        return self.__depth(root.right, target, curr_depth + 1)
     
+    def findDistance(self, root: Optional[TreeNode], p: int, q: int) -> int:
+        lca = self.find_LCA(root, p, q)
+        bfs = deque([lca])
+        dist, depth = 0, 0
+        found_p, found_q = False, False
+        
+        while bfs and not ( found_p and found_q):
+            sz = len(bfs)
+            for _ in range(sz):
+                # Dequeue the node
+                node = bfs.popleft()
+                if node.val == p:
+                    dist += depth
+                    found_p = True
+                    
+                if node.val == q:
+                    dist += depth
+                    found_q = True
+                    
+                if node.left:
+                    bfs.append(node.left) # Enqueue left child
+                    
+                if node.right:
+                    bfs.append(node.right) # Enqueue right child
+                    
+            depth += 1
+            
+        return dist
