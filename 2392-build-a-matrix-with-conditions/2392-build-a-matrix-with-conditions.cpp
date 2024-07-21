ae9f8cfe6b0067da0fb8c42509de04acb24b441f
Andrew Kim <ksb0724@utexas.edu>
2024-07-22 00:22:06 +0900

    Time: 98 ms (67.61%), Space: 60.4 MB (78.87%) - LeetHub

--- 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
+++ 2392-build-a-matrix-with-conditions/2392-build-a-matrix-with-conditions.cpp
@@ -1,77 +1,56 @@
 class Solution {
 private:
+    // Find the topologically sorted sequence using Kahn's algorithm
     vector<int> topoSort(vector<vector<int>>& edges, int n){
+        // Create an adjacency list to store the edges.
         vector<vector<int>> adj(n + 1);
+        vector<int> deg(n + 1);
         
-        // Build adjacency list
-        for (vector<int>& e: edges){
-            int x = e[0], y = e[1];
-            adj[x].push_back(y);
+        for (vector<int>& e : edges){
+            adj[e[0]].push_back(e[1]);
+            ++deg[e[1]];
         }
         
-        // 0: not visited, 1: visiting, 2: visited
-        vector<int> visited(n + 1);
+        queue<int> q;
+        
+        // Push all integers with in-degree 0 in the queue.
+        for (int i = 1; i <= n; ++i)
+            if (deg[i] == 0)
+                q.push(i);
+        
         vector<int> order;
-        bool hasCycle = false;
-        
-        // Perform DFS for each node
-        for (int node = 1; node <= n; ++node){
-            if (!visited[node]){
-                dfs(node, adj, visited, order, hasCycle);
+        while (!q.empty()){
+            int f = q.front(); q.pop();
+            order.push_back(f);
+            --n;
+            
+            for (int v : adj[f]){
+                --deg[v];
                 
-                // Return empty if cycle detected
-                if (hasCycle)
-                    return vector<int>{};
+                if (deg[v] == 0)
+                    q.push(v);
             }
         }
         
-        // Reverse to get the correct order
-        reverse(order.begin(), order.end());
-        return order;
-    }
-    
-    void dfs(int node, vector<vector<int>>& adj, vector<int>& visited, vector<int>& order, \
-            bool& hasCycle){
-        // Mark node as visiting
-        visited[node] = 1;
-        
-        for (int neighbor : adj[node]){
-            if (!visited[neighbor]){
-                dfs(neighbor, adj, visited, order, hasCycle);
-                
-                // Early exit if a cycle is detected
-                if (hasCycle)
-                    return;
-            }
-            else if (visited[neighbor] == 1){
-                // Cycle detected
-                hasCycle = true;
-                return;
-            }
-        }
-        
-        // Mark node as visited
-        visited[node] = 2;
-        
-        // Add node to the order
-        order.push_back(node);
+        // If we have not visited all integers, return empty array.
+        return (n != 0)? vector<int>{} : order;
     }
     
 public:
     vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
-        // Store the topologically sorted sequences.
+        // Store the topologically sorted sequences
         vector<int> orderRows = topoSort(rowConditions, k);
         vector<int> orderCols = topoSort(colConditions, k);
         
-        // If no topological sort exists, return empty array.
-        if (orderRows.empty() || orderCols.empty()) return vector<vector<int>> {};
+        // If no topological sort exists, return empty array
+        if (orderRows.empty() || orderCols.empty())
+            return vector<vector<int>>{};
         
         vector matrix(k, vector<int>(k));
         for (int i = 0; i < k; ++i){
             for (int j = 0; j < k; ++j)
-                if (orderRows[i] == orderCols[j]){
+                if (orderRows[i] == orderCols[j])
                     matrix[i][j] = orderRows[i];
-                }
         }
         
         return matrix;
