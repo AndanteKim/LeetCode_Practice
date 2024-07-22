3fdacda7473ef81aa53ccbeb23919a4752de47a9
Andrew Kim <ksb0724@utexas.edu>
2024-07-22 22:08:47 +0900

    Time: 1532 ms (15.94%), Space: 44.1 MB (50.72%) - LeetHub

--- 2093-minimum-cost-to-reach-city-with-discounts/2093-minimum-cost-to-reach-city-with-discounts.py
+++ 2093-minimum-cost-to-reach-city-with-discounts/2093-minimum-cost-to-reach-city-with-discounts.py
@@ -7,40 +7,35 @@ class Solution:
             adj[u].append((v, toll))
             adj[v].append((u, toll))
             
-        # Min-heap priority queue to store tuples of (cost, city, discounts used)
-        pq = [(0, 0, 0)]
+        # Min-heap priority queue to store tuples of (cost, city, used_discounts)
+        pq = [(0, 0, 0)] # Start from city 0 with cost 0 and 0 discounts used
         
         # 2D array to track minimum distance to each city with a given number of discounts used
         dist = [[float("inf")] * (discounts + 1) for _ in range(n)]
         dist[0][0] = 0
         
-        visited = [[False] * (discounts + 1) for _ in range(n)]
-        
         while pq:
-            curr_cost, city, used = heappop(pq)
+            curr_cost, city, used_discounts = heappop(pq)
             
-            # Skip processing if already visited with the same number of discounts used
-            if visited[city][used]:
+            # If this cost is already higher than the known minimum, skip it
+            if curr_cost > dist[city][used_discounts]:
                 continue
-            
-            visited[city][used] = True
-            
+                
             # Explore all neighbors of the current city
             for neighbor, toll in adj[city]:
                 # Case 1: Move to the neighbor without using a discount
-                if curr_cost + toll < dist[neighbor][used]:
-                    dist[neighbor][used] = curr_cost + toll
-                    heappush(pq, (dist[neighbor][used], neighbor, used))
+                if curr_cost + toll < dist[neighbor][used_discounts]:
+                    dist[neighbor][used_discounts] = curr_cost + toll
+                    heappush(pq, (dist[neighbor][used_discounts], neighbor, used_discounts))
                     
                 # Case 2: Move to the neighbor using a discount if available
-                if used < discounts:
-                    discounted_cost  = curr_cost + (toll >> 1)
-                    if discounted_cost < dist[neighbor][used + 1]:
-                        dist[neighbor][used + 1] = discounted_cost
-                        heappush(pq, (discounted_cost, neighbor, used + 1))
-                
+                if used_discounts < discounts:
+                    new_cost = curr_cost + (toll >> 1)
+                    
+                    if new_cost < dist[neighbor][used_discounts + 1]:
+                        dist[neighbor][used_discounts + 1] = new_cost
+                        heappush(pq, (new_cost, neighbor, used_discounts + 1))
+            
         # Find the minimum cost to reach city n - 1 with any number of discounts used
         min_cost = min(dist[n - 1])
-        
         return -1 if min_cost == float("inf") else min_cost
-        
