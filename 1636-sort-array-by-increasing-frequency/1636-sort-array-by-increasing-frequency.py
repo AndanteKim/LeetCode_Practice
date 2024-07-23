07364c39c5fccb5bbb02e3895324ea24641ffce5
Andrew Kim <ksb0724@utexas.edu>
2024-07-23 10:01:24 +0900

    Time: 56 ms (19.25%), Space: 16.5 MB (76.93%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.py
@@ -1,12 +1,4 @@
 class Solution:
     def frequencySort(self, nums: List[int]) -> List[int]:
-        counter, pq = Counter(nums), []
-        for num, freq in counter.items():
-            heappush(pq, (freq, -num))
-        
-        ans = []
-        while pq:
-            freq, n = heappop(pq)
-            ans.extend([-n] * freq)
-        
-        return ans
+        freq = Counter(nums)
+        return sorted(nums, key = lambda x: (freq[x], -x))
