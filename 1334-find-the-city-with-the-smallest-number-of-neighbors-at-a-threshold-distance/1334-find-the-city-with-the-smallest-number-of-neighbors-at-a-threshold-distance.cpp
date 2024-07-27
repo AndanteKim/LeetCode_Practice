75cfb833c87cb1ac7874ff51957ce10d5dce7adf
Andrew Kim <ksb0724@utexas.edu>
2024-07-28 01:38:55 +0900

    Time: 28 ms (49.25%), Space: 15.6 MB (75.72%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,85 +1,66 @@
 class Solution {
 private:
-    int n;
-    
-    // SPFA algorithm to find the shortest paths from a source city
-    void spfa(vector<vector<pair<int, int>>>& adj, vector<int>& distances, int source){
-        // Queue to process nodes with updated shortest path distances
-        queue<int> queue;
-        queue.push(source);
-        vector<int> updateCount(n, 0);
-        
-        // Process nodes in queue
-        while (!queue.empty()){
-            int currCity = queue.front(); queue.pop();
-            
-            for (auto& [neighbor, weight] : adj[currCity]){
-                if (distances[neighbor] > distances[currCity] + weight){
-                    distances[neighbor] = distances[currCity] + weight;
-                    queue.push(neighbor);
-                    ++updateCount[neighbor];
-                    
-                    // Detect negative weight cycles (not expected in this problem).
-                    if (updateCount[neighbor] > n)
-                        cout << "Negative cycle detected" << "\n";
+    // Floyd-Warshall algorithm to compute shortest paths between all pairs of
+    // cities
+    void floyd(int n, vector<vector<int>>& distances){
+        // Update distances for each intermediate city
+        for (int k = 0; k < n; ++k){
+            for (int i = 0; i < n; ++i){
+                for (int j = 0; j < n; ++j){
+                    // Update shortest paths from i to j through k
+                    distances[i][j] = min(distances[i][j], distances[i][k] + distances[k][j]);
                 }
             }
         }
     }
     
-    // Determine the city with the fewest number of reachable cities within the
-    // distances threshold
-    int getCityWithFewestReachable(vector<vector<int>>& matrix, int threshold){
-        int cityWithFewestReachable = -1;
-        int fewestReachableCount = n;
+    // Determine the city with the fewest number of reachable cites within the
+    // distance threshold
+    int getCityWithFewestReachable(int n, vector<vector<int>>& distances, int threshold){
+        int cityWithFewestReachable = -1, fewestReachableCount = n;
         
         // Count number of cities reachable within the distance threshold for
         // each city
         for (int i = 0; i < n; ++i){
             int reachableCount = 0;
             for (int j = 0; j < n; ++j){
-                if (i != j && matrix[i][j] <= threshold)
+                if (distances[i][j] <= threshold)
                     ++reachableCount;
             }
             
             // Update the city with the fewest reachable cities
             if (reachableCount <= fewestReachableCount){
-                fewestReachableCount = reachableCount;
                 cityWithFewestReachable = i;
+                fewestReachableCount = reachableCount;
             }
         }
         
         return cityWithFewestReachable;
     }
     
-    
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        // Adjacency list to store the graph
-        this -> n = n;
-        vector adj(n, vector<pair<int, int>>());
+        // Larger value to represent infinity
+        const int INF = 1e9 + 7;
         
-        // Matrix to store the shortest path distances from each city
-        vector matrix(n, vector<int>(n, 1e9 + 7));
+        // Distance matrix to store shortest paths between all pairs of cities
+        vector matrix(n, vector<int> (n, INF));
         
-        // Intialize adjacency list and the shortest path
-        for (int i = 0; i < n; ++i)
-            matrix[i][i] = 0;   // Distance to itself is zero
+        // Initialize distance matrix
+        for (int i = 0; i < n; ++i) matrix[i][i] = 0;   // Distance to itself is zero
         
-        // Populate the adjacency list with edges
+        // Populate the distance matrix with initial edge weights
         for (vector<int>& edge : edges){
             int start = edge[0], end = edge[1], weight = edge[2];
-            adj[start].push_back({end, weight});
-            adj[end].push_back({start, weight});    // For Undirected graph
+            matrix[start][end] = weight;
+            matrix[end][start] = weight;    // For Undirected graph
         }
         
-        
-        // Compute the shortest paths from each city using SPFA algorithm
-        for (int i = 0; i < n; ++i)
-            spfa(adj, matrix[i], i);
+        // Compute shortest paths using Floyd-Warshall algorithm
+        floyd(n, matrix);
         
         // Find the city with the fewest number of reachable cities within the
         // distance threshold
-        return getCityWithFewestReachable(matrix, distanceThreshold);
+        return getCityWithFewestReachable(n, matrix, distanceThreshold);
     }
 };
