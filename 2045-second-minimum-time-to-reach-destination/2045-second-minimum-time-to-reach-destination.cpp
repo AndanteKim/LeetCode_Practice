c2b92e3849dc2eade670675d4249e54a82c57a05
Andrew Kim <ksb0724@utexas.edu>
2024-07-28 23:29:55 +0900

    Time: 419 ms (84.12%), Space: 185.4 MB (83.62%) - LeetHub

--- 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
+++ 2045-second-minimum-time-to-reach-destination/2045-second-minimum-time-to-reach-destination.cpp
@@ -3,48 +3,38 @@ public:
     int secondMinimum(int n, vector<vector<int>>& edges, int time, int change) {
         vector<vector<int>> adj(n + 1);
         
+        // Build the adjacency list.
         for (vector<int>& edge : edges){
-            int start = edge[0], end = edge[1];
-            adj[start].push_back(end);
-            adj[end].push_back(start);
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
         }
         
-        // Dist1[i] stores the minimum time taken to reach node i from node1. dist2[i] stores the
-        // second minimum time taken to reach node from node1. Freq[i] stores the number of times
-        // node is popped out of the heap.
-        vector<int> dist1(n + 1, INT_MAX), dist2(n + 1, INT_MAX), freq(n + 1);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
-        minHeap.push({0, 1});
+        vector<int> dist1(n + 1, -1), dist2(n + 1, -1);
+        queue<pair<int, int>> queue;
+        // Start with node 1, with its minimum distance.
+        queue.push({1, 1});
         dist1[1] = 0;
         
-        while (!minHeap.empty()){
-            auto [timeTaken, node] = minHeap.top(); minHeap.pop();
-            ++freq[node];
+        while (!queue.empty()){
+            auto [node, freq] = queue.front(); queue.pop();
             
-            // If the node is being visited for the second time and is 'n', return the answer.
-            if (node == n && freq[node] == 2)
-                return timeTaken;
+            int timeTaken = (freq == 1)? dist1[node] : dist2[node];
             
-            // If the current light is read, wait till the path turns green.
+            // If timeTaken is in the red bracket
             if ((timeTaken / change) % 2)
                 timeTaken = change * (timeTaken / change + 1) + time;
             else
                 timeTaken += time;
             
             for (int neighbor : adj[node]){
-                // Ignore nodes that have already popped out twice.
-                if (freq[neighbor] == 2) continue;
-                
-                // Update dist1 if it's more than the current timeTaken and store its value in
-                // dist2 since that becomes the second minimum value now.
-                if (dist1[neighbor] > timeTaken){
-                    dist2[neighbor] = dist1[neighbor];
+                if (dist1[neighbor] == -1){
                     dist1[neighbor] = timeTaken;
-                    minHeap.push({timeTaken, neighbor});
+                    queue.push({neighbor, 1});
                 }
-                else if (dist2[neighbor] > timeTaken && timeTaken != dist1[neighbor]){
+                else if (dist2[neighbor] == -1 && timeTaken != dist1[neighbor]){
+                    if (neighbor == n) return timeTaken;
                     dist2[neighbor] = timeTaken;
-                    minHeap.push({timeTaken, neighbor});
+                    queue.push({neighbor, 2});
                 }
             }
         }
