6cb72e314bf64c16e06973e8afc87fe2a28b46d1
Andrew Kim <ksb0724@utexas.edu>
2024-07-30 14:02:28 +0900

    Time: 106 ms (37.00%), Space: 53.1 MB (18.50%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,32 +1,32 @@
 class Solution {
-private:
-    int n;
-    int dp(int i, int prev, int length, string& s, vector<vector<int>>& memo){
-        // Base case
-        if (i == n)
-            return 0;
-        
-        if (memo[i][s[prev] - 97] != -1)
-            return memo[i][s[prev] - 97];
-        
-        int minDelete = INT_MAX;
-        // If s[prev] = 'b' and s[i] = 'a' with prev < i.
-        if (s[prev] > s[i])
-            // Delete current character to find the equivalent or length with new start
-            minDelete = min(1 + dp(i + 1, prev, length, s, memo), length + dp(i + 1, i, 0, s, memo));
-        else
-            // Delete current character to find the equivalent or just keep searching
-            minDelete = min(dp(i + 1, i, length + 1, s, memo), 1 + dp(i + 1, prev, length, s, memo));
-        
-        return memo[i][s[prev] - 97] = minDelete;
-    }
-    
-    
 public:
     int minimumDeletions(string s) {
-        this -> n = s.size();
-        vector memo(n + 1, vector<int>(2, -1));
+        int n = s.size();
+        vector<int> countA(n), countB(n);
         
-        return dp(0, 0, 0, s, memo);
+        int bCount = 0;
+        // First pass: Compute bCount storing the number of 
+        // 'b' characters to the left of current position.
+        for (int i = 0; i < n; ++i){
+            countB[i] = bCount;
+            if (s[i] == 'b')
+                ++bCount;
+        }
+        
+        int aCount = 0;
+        // Second pass: Compute aCount storing the number of 
+        // 'a' characters to the right of current position.
+        for (int i = n - 1; i >= 0; --i){
+            countA[i] = aCount;
+            if (s[i] == 'a')
+                ++aCount;
+        }
+        
+        int minDeletions = n;
+        // Third pass: Iterate through the string to find the minimum deletions
+        for (int i = 0; i < n; ++i)
+            minDeletions = min(minDeletions, countA[i] + countB[i]);
+        
+        return minDeletions;
     }
 };
