361efceccea2b5823c4febe42349ea16d9509133
Andrew Kim <ksb0724@utexas.edu>
2024-07-30 14:24:56 +0900

    Time: 100 ms (44.98%), Space: 38.5 MB (35.25%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -2,30 +2,23 @@ class Solution {
 public:
     int minimumDeletions(string s) {
         int n = s.size();
-        vector<int> countA(n), countB(n);
-        
-        int bCount = 0;
-        // First pass: Compute bCount storing the number of 
-        // 'b' characters to the left of current position.
-        for (int i = 0; i < n; ++i){
-            countB[i] = bCount;
-            if (s[i] == 'b')
-                ++bCount;
-        }
-        
+        vector<int> countA(n);
         int aCount = 0;
-        // Second pass: Compute aCount storing the number of 
-        // 'a' characters to the right of current position.
+        
+        // First pass: Compute countA storing the number of 
+        // 'a' character to the right of the current position
         for (int i = n - 1; i >= 0; --i){
             countA[i] = aCount;
             if (s[i] == 'a')
                 ++aCount;
         }
         
-        int minDeletions = n;
-        // Third pass: Iterate through the string to find the minimum deletions
-        for (int i = 0; i < n; ++i)
-            minDeletions = min(minDeletions, countA[i] + countB[i]);
+        // Second pass: Compute minimum deletions on the fly
+        int bCount = 0, minDeletions = n;
+        for (int i = 0; i < n; ++i){
+            minDeletions = min(minDeletions, countA[i] + bCount);
+            if (s[i] == 'b') ++bCount;
+        }
         
         return minDeletions;
     }
