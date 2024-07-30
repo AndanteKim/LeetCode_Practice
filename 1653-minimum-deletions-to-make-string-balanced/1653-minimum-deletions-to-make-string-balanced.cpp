83cdcc97351d067e8025e8433497433222ac6f33
Andrew Kim <ksb0724@utexas.edu>
2024-07-30 15:18:00 +0900

    Time: 77 ms (87.56%), Space: 23.7 MB (73.37%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.size(), deleteCount = 0;
-        stack<int> st;
+        int n = s.size(), bCount = 0;
         
-        // Iterate through each character in the string
+        // MinDeletions variable represents dp[i]
+        int minDeletions = 0;
+        
         for (int i = 0; i < n; ++i){
-            // If stack is not empty, top of stack is 'b',
-            // current character is 'a'.
-            if (!st.empty() && s[st.top()] == 'b' && s[i] == 'a'){
-                st.pop();   // Remove 'b' from stack
-                ++deleteCount;  // Increment deletion count
-            }
+            if (s[i] == 'b')
+                ++bCount;
             else
-                st.push(i);     // Append current character to stack
+                // Two cases: Remove 'a' or keep 'a'
+                minDeletions = min(minDeletions + 1, bCount);
         }
         
-        return deleteCount;
+        return minDeletions;
     }
 };
