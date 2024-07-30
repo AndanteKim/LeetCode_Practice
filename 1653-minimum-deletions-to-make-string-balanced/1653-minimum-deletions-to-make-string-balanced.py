cf2e54f59acd5bb7521660dd73c80fbeeef2114d
Andrew Kim <ksb0724@utexas.edu>
2024-07-30 14:34:28 +0900

    Time: 301 ms (58.67%), Space: 17.8 MB (52.00%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,16 +1,15 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
-        n, a_count, b_count = len(s), s.count('a'), 0
-        min_deletions = n
+        n, stack, delete_count = len(s), [], 0
         
-        # Second pass: Iterate through the string to compute minimum deletions
+        # Iterate through each character in the string
         for i in range(n):
-            if s[i] == 'a':
-                a_count -= 1
-            
-            min_deletions = min(min_deletions, a_count + b_count)
-            if s[i] == 'b':
-                b_count += 1
-                
-        return min_deletions
+            # If the stack is not empty, top of stack is 'b',
+            # and current char is 'a'
+            if stack and s[stack[-1]] == 'b' and s[i] == 'a':
+                stack.pop()     # Remove 'b' from stack
+                delete_count += 1   # Increment deletion count
+            else:
+                stack.append(i)     # Append current character to stack
         
+        return delete_count
