16ebbf3b8fee97c41c1b96c2b5e6f6fe66861f7e
Andrew Kim <ksb0724@utexas.edu>
2024-07-30 14:12:38 +0900

    Time: 578 ms (25.34%), Space: 20.1 MB (28.00%) - LeetHub

--- 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -1,28 +1,20 @@
 class Solution:
     def minimumDeletions(self, s: str) -> int:
         n = len(s)
-        cnt_a, cnt_b = [0] * n, [0] * n
-        b_cnt = 0
+        count_a, a_cnt = [0] * n, 0
         
-        # First pass: Compute cnt_b storing the number of
-        # 'b' characters to the left of the current position
+        # First pass: compute count_a storing the number of
+        # 'a' characters to the right of the current position
+        for i in range(n - 1, -1, -1):
+            count_a[i] = a_cnt
+            if s[i] == 'a':
+                a_cnt += 1
+        
+        min_deletions, b_cnt = n, 0
+        # Second pass: compute minimum deletions on the fly
         for i in range(n):
-            cnt_b[i] = b_cnt
+            min_deletions = min(count_a[i] + b_cnt, min_deletions)
             if s[i] == 'b':
                 b_cnt += 1
                 
-        a_cnt = 0
-        # Second pass: Compute cnt_a storing the number of
-        # 'a' characters to the right of the current position
-        for i in range(n - 1, -1, -1):
-            cnt_a[i] = a_cnt
-            if s[i] == 'a':
-                a_cnt += 1
-                
-        min_deletions = n
-        
-        # Third pass: Iterate through the string to find the minimum deletions
-        for i in range(n):
-            min_deletions = min(min_deletions, cnt_a[i] + cnt_b[i])
-            
         return min_deletions
