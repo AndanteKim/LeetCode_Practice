f7a90787167f47bb6bfd223b9d36f4fa239c3484
Andrew Kim <ksb0724@utexas.edu>
2024-07-30 22:25:00 +0900

    Time: 20 ms (41.68%), Space: 26.4 MB (28.20%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -1,33 +1,40 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        # Post order traversal of subtree rooted at 'node'
-        def gain_from_subtree(node: TreeNode) -> int:
-            nonlocal max_sum
-            
-            if not node:
-                return 0
-            
-            # Add the gain from the left subtree. Note that if the
-            # gain is negative, we can ignore it, or count it as 0.
-            # This is the reason we use 'max' here.
-            gain_from_left = max(gain_from_subtree(node.left), 0)
-            
-            # add the gain / path sum from the right subtree. 0 if negative
-            gain_from_right = max(gain_from_subtree(node.right), 0)
-            
-            # If left or right gain are negative, they are counted as 0,
-            # so this statement takes care of all four scenarios.
-            max_sum = max(max_sum, gain_from_left + gain_from_right + node.val)
-            
-            # Return the max sum for a path starting at the root of subtree.
-            return max(gain_from_left + node.val, gain_from_right + node.val)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+private:
+    int maxSum;
+    // Post order traversal of subtree rooted at 'root'
+    int gainFromSubtree(const TreeNode* node){
+        if (!node) return 0;
         
-        max_sum = float('-inf')
-        gain_from_subtree(root)
-        return max_sum
+        // Add the path sum from the left subtree. Note that if the path
+        // sum is negative, we can ignore it, or count it as 0.
+        int gainFromLeft = max(gainFromSubtree(node -> left), 0);
+        
+        // Add the path sum from the right subtree. 0 if negative
+        int gainFromRight = max(gainFromSubtree(node -> right), 0);
+        
+        // If left or right path sum are negative, they're counted
+        // as 0, so this statement takes care of all 4 scenarios
+        maxSum = max(maxSum, gainFromLeft + gainFromRight + node -> val);
+        
+        // Return the max sum for a path starting at the root of subtree
+        return max(gainFromLeft + node -> val, gainFromRight + node -> val);
+    }
+    
+public:
+    int maxPathSum(TreeNode* root) {
+        maxSum = INT_MIN;
+        gainFromSubtree(root);
+        return maxSum;
+    }
+};
