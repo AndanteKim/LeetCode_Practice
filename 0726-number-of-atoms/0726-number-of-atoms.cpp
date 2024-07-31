58fa38bf4dbc76a698d537162598f1d80612720d
Andrew Kim <ksb0724@utexas.edu>
2024-08-01 02:41:35 +0900

    Time: 0 ms (100.00%), Space: 9 MB (88.41%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.cpp
+++ 0726-number-of-atoms/0726-number-of-atoms.cpp
@@ -1,53 +1,72 @@
 class Solution {
 public:
     string countOfAtoms(string formula) {
-        // For multiplier
+        // For every index, store the valid multiplier
         int runningMul = 1;
+        vector<int> muls(formula.size());
+        
+        // Stack to take care of nested formula
         stack<int> st;
         st.push(1);
         
-        // Map to store the count of atoms
-        map<string, int> finalMap;
-        
-        // Strings to take care of current atom and count
-        string currAtom = "", currCount = "";
-        
-        // Index to traverse the formula in reverse and parse the formula
+        // Preprocess the formula and extract all multipliers
+        string currNum = "";
         for (int i = formula.size() - 1; i >= 0; --i){
-            // If digit, update the count
-            if (isdigit(formula[i]))
-                currCount = formula[i] + currCount;
+            if (isdigit(formula[i])) currNum = formula[i] + currNum;
             
-            // If lowercase letter, prepend to the currAtom
-            else if (islower(formula[i]))
-                currAtom = formula[i] + currAtom;
+            // If we encountered a letter, then the scanned
+            // number was count and not a multiplier. Discard it.
+            else if (isalpha(formula[i])) currNum = "";
             
-            // If uppercase letter, prepend and update the finalMap
-            else if (isupper(formula[i])){
-                currAtom = formula[i] + currAtom;
-                if (!currCount.empty())
-                    finalMap[currAtom] += stoi(currCount) * runningMul;
-                else
-                    finalMap[currAtom] += 1 * runningMul;
-                
-                currAtom.clear(), currCount.clear();
-            }
-            
-            // If the right parenthesis, the currCount if any
-            // will be considered a multiplier
+            // If we encounter a right parenthesis, then the
+            // scanned number was multiplier. Store it.
             else if (formula[i] == ')'){
-                int currMultiplier = (!currCount.empty())? stoi(currCount) : 1;
-                st.push(currMultiplier);
+                int currMultiplier = (!currNum.empty())? stoi(currNum) : 1;
                 runningMul *= currMultiplier;
-                currCount.clear();
+                st.push(currMultiplier);
+                currNum.clear();
             }
             
-            // If the left parenthesis, update the runningMul
+            // If we encounter a left parenthesis, then the
+            // most recent multiplier will cease to exist.
             else if (formula[i] == '('){
                 runningMul /= st.top();
                 st.pop();
+                currNum.clear();
             }
-            
+            muls[i] = runningMul;
+        }
+        
+        
+        int i = 0;
+        // Map to store the count of atoms
+        map<string, int> finalMap;
+        
+        // Traverse left to right in the formula
+        while (i < formula.size()){
+            // If upper case letter, extract the entire atom
+            if (isupper(formula[i])){
+                string currAtom = "", currCount = "";
+                currAtom.push_back(formula[i]);
+                ++i;
+                
+                while (i < formula.size() && islower(formula[i])){
+                    currAtom += formula[i];
+                    ++i;
+                }
+                
+                // Extract the count
+                while (i < formula.size() && isdigit(formula[i])){
+                    currCount += formula[i];
+                    ++i;
+                }
+                
+                // Update the final map
+                int count = currCount.empty()? 1: stoi(currCount);
+                finalMap[currAtom] += count * muls[i - 1];
+            }
+            else
+                ++i;
         }
         
         // Generate the answer string
