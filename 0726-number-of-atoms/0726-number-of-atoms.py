029046543561005a76e910841cd1de0d22751bcd
Andrew Kim <ksb0724@utexas.edu>
2024-08-01 02:06:01 +0900

    Time: 41 ms (22.06%), Space: 16.6 MB (50.77%) - LeetHub

--- 0726-number-of-atoms/0726-number-of-atoms.py
+++ 0726-number-of-atoms/0726-number-of-atoms.py
@@ -1,53 +1,80 @@
 class Solution:
     def countOfAtoms(self, formula: str) -> str:
-        # For multipliers
-        stack, running_mul = [1], 1
+        # For every index, store the valid multiplier
+        muls, running_mul = [], 1
+        
+        # Stack to take care of nested formula
+        stack = [1]
+        
+        # Preprocess the formula and extract all multipliers
+        curr_num = ""
+        for i in range(len(formula) - 1, -1, -1):
+            if formula[i].isdigit():
+                curr_num += formula[i]
+                
+            # If we encountered a letter, then the scanned
+            # number was count and not multiplier. Discard it.
+            elif formula[i].isalpha():
+                curr_num = ""
+                
+            # If we encounter a right parenthesis, then the
+            # scanned number was a multiplier. Store it.
+            elif formula[i] == ')':
+                curr_multiplier = int(curr_num[::-1]) if curr_num else 1
+                running_mul *= curr_multiplier
+                stack.append(curr_multiplier)
+                curr_num = ""
+                
+            # If we encounter a left parenthesis, then the
+            # most recent multiplier will cease to exist.
+            elif formula[i] == '(':
+                running_mul //= stack.pop()
+                curr_num = ""
+                
+            # For every index, store the valid multiplier
+            muls.append(running_mul)
+            
+        # Reverse the muls
+        muls = muls[::-1]
         
         # Map to store the count of atoms
         final_map = defaultdict(int)
         
-        # Strings to take care of current atom and count
-        curr_atom, curr_count = "", ""
-        
-        # Index to traverse the formula in reverse and parse the formula
-        for index in range(len(formula) - 1, -1, -1):
-            # If digit, update the count
-            if formula[index].isdigit():
-                curr_count = formula[index] + curr_count
-                
-            # If lowercase letter, prepend to the curr_atom
-            elif formula[index].islower():
-                curr_atom = formula[index] + curr_atom
-                
-            # If uppercase letter, prepend and update the finalMap
-            elif formula[index].isupper():
-                curr_atom = formula[index] + curr_atom
-                
-                if curr_count:
-                    final_map[curr_atom] += int(curr_count) * running_mul
-                else:
-                    final_map[curr_atom] += 1 * running_mul
-                    
-                curr_atom, curr_count = "", ""
-                
-            # If the right parenthesis, the curr_count if any
-            # will be considered a multiplier
-            elif formula[index] == ')':
-                curr_multiplier = int(curr_count) if curr_count else 1
-                stack.append(curr_multiplier)
-                running_mul *= curr_multiplier
+        # Traverse left to right in the formula
+        i = 0
+        while i < len(formula):
+            # If upper case letter, extract the entire atom
+            if formula[i].isupper():
+                curr_atom = formula[i]
                 curr_count = ""
+                i += 1
                 
-            # If left parenthesis, update the running_mul
-            elif formula[index] == '(':
-                running_mul //= stack.pop()
+                while i < len(formula) and formula[i].islower():
+                    curr_atom += formula[i]
+                    i += 1
+                  
+                # Extract the count
+                while i < len(formula) and formula[i].isdigit():
+                    curr_count += formula[i]
+                    i += 1
                 
+                # Update the final map
+                if curr_count:
+                    final_map[curr_atom] += int(curr_count) * muls[i - 1]
+                else:
+                    final_map[curr_atom] += 1 * muls[i - 1]
+                    
+            else:
+                i += 1
+                
+        # Sort the final map
         final_map = dict(sorted(final_map.items()))
         
+        # Generate the answer string
         ans = ""
-        for atom, val in final_map.items():
+        for atom in final_map:
             ans += atom
-            if val > 1:
-                ans += str(val)
-        
+            if final_map[atom] > 1:
+                ans += str(final_map[atom])
+                
         return ans
