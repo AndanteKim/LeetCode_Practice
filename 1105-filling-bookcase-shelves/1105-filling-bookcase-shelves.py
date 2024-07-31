9ed60da1b643fb5d5dc004f1a7c1aa3b13feb72e
Andrew Kim <ksb0724@utexas.edu>
2024-07-31 16:48:10 +0900

    Time: 56 ms (28.81%), Space: 16.9 MB (85.88%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -1,23 +1,22 @@
 class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
-        def dp(i: int, remain: int, max_height_prev: int, memo: List[List[int]]) -> int:
-            # Base case
-            if i == n:
-                return max_height_prev
-            
-            if memo[i][remain] != -1:
-                return memo[i][remain]
-            
-            min_height = float('inf')
-            # Check the remain of shelfwidth whether it's ok to put on
-            if remain >= books[i][0]:
-                min_height = min(dp(i + 1, remain - books[i][0], max(max_height_prev, books[i][1]), memo), max_height_prev + dp(i + 1, shelfWidth - books[i][0], books[i][1], memo))
-            else:
-                min_height = max_height_prev + dp(i + 1, shelfWidth - books[i][0], books[i][1], memo)
-                
-            memo[i][remain] = min_height
-            return min_height
-        
         n = len(books)
-        memo = [[-1] * (shelfWidth + 1) for _ in range(n + 1)]
-        return dp(0, shelfWidth, 0, memo)
+        # dp[i] will store the minimum height of the bookcase containing all
+        # books up to and excluding i
+        dp = [0] * (n + 1)
+        dp[1] = books[0][1]
+        
+        for i in range(2, n + 1):
+            # new shelf built to hold current book
+            remain, max_height = shelfWidth - books[i - 1][0], books[i - 1][1]
+            dp[i] = books[i - 1][1] + dp[i - 1]
+            
+            j = i - 1
+            # Calculate the height when previous books are added to new shelf
+            while j > 0 and remain - books[j - 1][0] >= 0:
+                max_height = max(max_height, books[j - 1][1])
+                remain -= books[j - 1][0]
+                dp[i] = min(dp[i], max_height + dp[j - 1])
+                j -= 1
+            
+        return dp[n]
