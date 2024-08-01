528beaec0f13cdbc432ac257ee19e30df95a47fa
Andrew Kim <ksb0724@utexas.edu>
2024-08-01 23:15:18 +0900

    Time: 62 ms (37.22%), Space: 22 MB (76.65%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -7,24 +7,35 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        self.pq = []
-        stack = [root]
-        while stack:
-            node = stack.pop()
-            heappush(self.pq, node.val)
-            
-            if node.left:
-                stack.append(node.left)
-                
-            if node.right:
-                stack.append(node.right)
+        # Array containing all the nodes in the sorted order
+        self.nodes_sorted = []
+        
+        # Pointer to the next smallest element in the BST
+        self.idx = -1
+        
+        # Call to flatten the input binary search tree.
+        self._inorder(root)
 
+    def _inorder(self, root: TreeNode) -> None:
+        if not root:
+            return
+        self._inorder(root.left)
+        self.nodes_sorted.append(root.val)
+        self._inorder(root.right)
+    
     def next(self) -> int:
-        return heappop(self.pq) if self.hasNext else -1
+        """
+        @return the next smallest number
+        """
+        self.idx += 1
+        return self.nodes_sorted[self.idx]
+        
 
     def hasNext(self) -> bool:
-        return len(self.pq) > 0
-
+        """
+        @return whether we have a next smallest number
+        """
+        return self.idx + 1 < len(self.nodes_sorted)
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
