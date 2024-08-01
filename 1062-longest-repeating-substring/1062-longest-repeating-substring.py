284d1b082a76d95b5156582e791f20360d70ca25
Andrew Kim <ksb0724@utexas.edu>
2024-08-01 16:39:33 +0900

    Time: 285 ms (37.37%), Space: 32.8 MB (24.20%) - LeetHub

--- 1062-longest-repeating-substring/1062-longest-repeating-substring.py
+++ 1062-longest-repeating-substring/1062-longest-repeating-substring.py
@@ -1,28 +1,17 @@
 class Solution:
     def longestRepeatingSubstring(self, s: str) -> int:
-        start, end = 1, len(s) - 1
+        n, max_len = len(s), 0
+        dp = [[0] * (n + 1) for _ in range(n + 1)]
         
-        while start <= end:
-            mid = (start + end) >> 1
-            
-            # Check if there's a repeating substring of length mid
-            if self._has_repeating_substr(s, mid):
-                start = mid + 1     # Try longer substrings
-            else:
-                end = mid - 1       # Try shorter substrings
-                
-        return start - 1
-    
-    def _has_repeating_substr(self, s: str, length: int) -> bool:
-        seen = set()
-        for i in range(len(s) - length + 1):
-            # Extract a substring of the given length
-            substr = s[i : i + length]
-            
-            # Check if the substring has been seen before
-            if substr in seen:
-                return True
-            seen.add(substr)
-            
-        return False
-            
+        # Populate the DP array
+        for i in range(1, n + 1):
+            for j in range(i + 1, n + 1):
+                # Check if the characters match and
+                # update the DP value
+                if s[i - 1] == s[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                    
+                    # Update max_len
+                    max_len = max(max_len, dp[i][j])
+                    
+        return max_len
