64c8f47dee871d73ea92b89f728abab2a906bb0b
Andrew Kim <ksb0724@utexas.edu>
2024-08-01 23:26:22 +0900

    Time: 63 ms (31.42%), Space: 22.5 MB (13.31%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.py
@@ -7,35 +7,40 @@
 class BSTIterator:
 
     def __init__(self, root: Optional[TreeNode]):
-        # Array containing all the nodes in the sorted order
-        self.nodes_sorted = []
+        # Stack for the recursion simulation
+        self.stack = []
         
-        # Pointer to the next smallest element in the BST
-        self.idx = -1
+        # Remember that the algorithm starts with a call to the helper function
+        # with the root node as the input
+        self._leftmost_inorder(root)
         
-        # Call to flatten the input binary search tree.
-        self._inorder(root)
+    def _leftmost_inorder(self, root: TreeNode) -> None:
+        # For a given node, add all the elements in the leftmost branch of the tree
+        # under it to the stack.
+        while root:
+            self.stack.append(root)
+            root = root.left
 
-    def _inorder(self, root: TreeNode) -> None:
-        if not root:
-            return
-        self._inorder(root.left)
-        self.nodes_sorted.append(root.val)
-        self._inorder(root.right)
-    
     def next(self) -> int:
         """
         @return the next smallest number
         """
-        self.idx += 1
-        return self.nodes_sorted[self.idx]
+        # Node at the top of the stack is the next smallest element
+        topmost_node = self.stack.pop()
+        
+        # Need to maintain the invariant. If the node has a right child, call the
+        # helper function for the right child.
+        if topmost_node.right:
+            self._leftmost_inorder(topmost_node.right)
+            
+        return topmost_node.val
         
 
     def hasNext(self) -> bool:
         """
         @return whether we have a next smallest number
         """
-        return self.idx + 1 < len(self.nodes_sorted)
+        return len(self.stack) > 0
 
 # Your BSTIterator object will be instantiated and called as such:
 # obj = BSTIterator(root)
