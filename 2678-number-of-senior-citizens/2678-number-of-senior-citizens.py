75a415963c518f3917debddf37fd12a124d27532
Andrew Kim <ksb0724@utexas.edu>
2024-08-01 10:38:04 +0900

    Time: 42 ms (83.17%), Space: 16.6 MB (7.17%) - LeetHub

--- 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -1,3 +1,11 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        return sum((s[11:13] > '60') for s in details)
+        ans = 0
+        
+        for d in details:
+            if d[11] in ('7', '8', '9'):
+                ans += 1
+            elif d[11] == '6' and d[12] != '0':
+                ans += 1
+                
+        return ans
