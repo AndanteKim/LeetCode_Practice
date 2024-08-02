df2d45eb04e28f95319f65a8e779baa9a5b30e59
Andrew Kim <ksb0724@utexas.edu>
2024-08-02 17:13:48 +0900

    Time: 69 ms (53.24%), Space: 90 MB (21.22%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -1,20 +1,41 @@
 class Solution {
-public:
-    int minSwaps(vector<int>& nums) {
-        int ans = 0, counts1 = count(nums.begin(), nums.end(), 1), n = nums.size();
+private:
+    int minSwapsHelper(vector<int>& data, int val){
+        int n = data.size();
+        vector<int> rightSuffixSum(n + 1);
         
-        for (int i = 0; i < counts1; ++i)
-            if (nums[i] == 0) ++ans;
-        
-        int windowLength = ans;
-        for (int left = 0; left < n; ++left){
-            if (nums[(left + counts1) % n] == 0)
-                ++windowLength;
-            if (nums[left] == 0) --windowLength;
-            
-            ans = min(ans, windowLength);
+        // Construct the suffix sum array for counting opposite values (val ^ 1)
+        for (int i = n - 1; i >= 0; --i){
+            rightSuffixSum[i] = rightSuffixSum[i + 1];
+            if (data[i] == (val ^ 1))
+                ++rightSuffixSum[i];
         }
         
-        return ans;
+        // Total zeros in the array if 'val' is 1 (or vice versa)
+        int totalSwaps = rightSuffixSum[0];
+        
+        // Track current number of required swaps in the current segment
+        int currSwap = 0;
+        int minSwap = totalSwaps - rightSuffixSum[n - totalSwaps];
+        
+        // Iterate to find the minimum swaps by sliding
+        // the potential block of grouped 'val'
+        for (int i = 0; i < totalSwaps; ++i){
+            if (data[i] == (val ^ 1))
+                ++currSwap;
+            int remaining = totalSwaps - i - 1;
+            int requiredSwaps = ((i + 1) - currSwap) + (remaining - rightSuffixSum[n - remaining]);
+            minSwap = min(minSwap, requiredSwaps);
+        }
+        
+        return minSwap;
+    }
+    
+public:
+    int minSwaps(vector<int>& nums) {
+        int op1 = minSwapsHelper(nums, 0);  // Groups 0's altogether
+        int op2 = minSwapsHelper(nums, 1);  // Groups 1's altogether
+        
+        return min(op1, op2);
     }
 };
