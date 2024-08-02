ad3c74cb9cbc835f338ccdeba31680e96954d80e
Andrew Kim <ksb0724@utexas.edu>
2024-08-02 16:56:24 +0900

    Time: 797 ms (12.61%), Space: 23.5 MB (5.18%) - LeetHub

--- 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -1,18 +1,38 @@
 class Solution:
     def minSwaps(self, nums: List[int]) -> int:
-        ans, counts1, n = 0, nums.count(1), len(nums)
-        for i in range(counts1):
-            if nums[i] == 0:
-                ans += 1
+        op1 = self.min_swaps(nums, 0)   # Grouping all 0s together
+        op2 = self.min_swaps(nums, 1)   # Grouping all 1s together
+        return min(op1, op2)
+    
+    def min_swaps(self, data: List[int], val: int) -> int:
+        n = len(data)
+        right_suffix_sum = [0] * (n + 1)
         
-        window = ans
-        for left in range(n):
-            if nums[(left + counts1) % n] == 0:
-                window += 1
-            
-            if nums[left] == 0:
-                window -= 1
-            
-            ans = min(ans, window)
+        # Construct the suffix sum array for counting opposite values
+        # (val ^ 1)
+        for i in range(n - 1, -1, -1):
+            right_suffix_sum[i] = right_suffix_sum[i + 1]
+            if data[i] == (val ^ 1):
+                right_suffix_sum[i] += 1
                 
-        return ans
+        # Total zeros in the array if 'val' is 1 (or vice versa)
+        total_swaps = right_suffix_sum[0]
+        
+        # Track current number of required swaps in the current segment
+        curr_swap = 0
+        min_swaps = total_swaps - right_suffix_sum[n - total_swaps]
+        
+        # Iterate to find the minimum swaps by sliding
+        # the potential block of grouped 'val'
+        for i in range(total_swaps):
+            if data[i] == val ^ 1:
+                curr_swap += 1
+                
+            remaining = total_swaps - i - 1
+            required_swaps = ((i + 1) - curr_swap) + (remaining - right_suffix_sum[n - remaining])
+            
+            min_swaps = min(min_swaps, required_swaps)
+            
+        return min_swaps
+                
+    
