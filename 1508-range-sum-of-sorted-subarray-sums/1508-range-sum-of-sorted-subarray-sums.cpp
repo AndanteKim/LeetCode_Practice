93513a3cf30a5916ec04cca8c516cce2673d17a2
Andrew Kim <ksb0724@utexas.edu>
2024-08-04 21:17:21 +0900

    Time: 0 ms (100.00%), Space: 10.1 MB (92.81%) - LeetHub

--- 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -1,20 +1,50 @@
 typedef long long ll;
 
 class Solution {
+private:
+    int mod = 1'000'000'007;
+    // Helper function to count subarrays with sum <= target and their total sum.
+    pair<int, ll> countAndSum(vector<int>& nums, int n, int target){
+        ll currSum = 0, windowSum = 0, totalSum = 0;
+        int count = 0;
+        
+        for (int j = 0, i = 0; j < n; ++j){
+            currSum += nums[j];
+            windowSum += nums[j] * (j - i + 1);
+            
+            while (currSum > target){
+                windowSum -= currSum;
+                currSum -= nums[i++];
+            }
+            
+            count += (j - i + 1);
+            totalSum += windowSum;
+        }
+        
+        return make_pair(count, totalSum);
+    }
+    
+    // Helper function to find the sum of the first k smallest subarray sums
+    ll sumOfFirstK(vector<int>& nums, int n, int k){
+        int left = *min_element(nums.begin(), nums.end()), right = accumulate(nums.begin(), nums.end(), 0LL);
+        
+        while (left <= right){
+            ll mid = (left + right) >> 1;
+            if (countAndSum(nums, n, mid).first >= k)
+                right = mid - 1;
+            else
+                left = mid + 1;
+        }
+        
+        auto [count, totalSum] = countAndSum(nums, n, left);
+        // There can be more subarrays with the same sum of left.
+        return totalSum - (left * (count - k));
+    }
+    
+    
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
-        vector<ll> sumSubarr(n * (n + 1) >> 1);
-        int k = 0, rem = 1e9 + 7;
-        
-        for (int i = 0; i < n; ++i){
-            ll partial = 0;
-            for (int j = i; j < n; ++j){
-                partial = (partial + nums[j]) % rem;
-                sumSubarr[k++] = partial;
-            }
-        }
-        sort(sumSubarr.begin(), sumSubarr.end());
-        
-        return accumulate(sumSubarr.begin() + left - 1, sumSubarr.begin() + right, 0LL) % rem;
+        ll ans = (sumOfFirstK(nums, n, right) - sumOfFirstK(nums, n, left - 1)) % mod;
+        return (ans + mod) % mod;
     }
 };
