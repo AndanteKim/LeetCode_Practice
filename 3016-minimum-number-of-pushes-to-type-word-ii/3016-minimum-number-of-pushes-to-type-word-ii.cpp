2420bc8ebb4e5d87aabaf4c898596fed4eef70b3
Andrew Kim <ksb0724@utexas.edu>
2024-08-06 19:05:26 +0900

    Time: 68 ms (79.47%), Space: 24.9 MB (62.15%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        // Frequency vector to store the number for each characters
+        // Frequency vector to store count of each letter
         vector<int> freq(26);
         
+        // Count occurrences of each letter
         for (char &c : word)
             ++freq[c - 'a'];
         
-        // Sort the frequency in descending order
+        // Sort frequencies in descending order
         sort(freq.begin(), freq.end(), [](int a, int b){return a > b;});
         
         int ans = 0;
         
-        // Calculate the total number of presses
+        // Calculate total number of presses
         for (int i = 0; i < 26; ++i){
             if (freq[i] == 0)
                 break;
