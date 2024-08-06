87992d90a8034fc001e88eff3cc62f3a5472ab14
Andrew Kim <ksb0724@utexas.edu>
2024-08-06 19:11:36 +0900

    Time: 120 ms (26.08%), Space: 27.2 MB (39.51%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        // Frequency vector to store count of each letter
-        vector<int> freq(26);
+        // Frequency map to store count of each letter
+        unordered_map<char, int> freqMap;
+        for (char& c : word) ++freqMap[c];
         
-        // Count occurrences of each letter
-        for (char &c : word)
-            ++freq[c - 'a'];
+        // Priority queue to store frequencies in descending order
+        priority_queue<int, vector<int>> pq;
+        for (auto& [_, val] : freqMap)
+            pq.push(val);
         
-        // Sort frequencies in descending order
-        sort(freq.begin(), freq.end(), [](int a, int b){return a > b;});
-        
-        int ans = 0;
+        int ans = 0, i = 0;
         
         // Calculate total number of presses
-        for (int i = 0; i < 26; ++i){
-            if (freq[i] == 0)
-                break;
-            ans += ((i / 8) + 1) * freq[i];
+        while (!pq.empty()){
+            ans += ((i++ / 8) + 1) * pq.top();
+            pq.pop();
         }
         
         return ans;
