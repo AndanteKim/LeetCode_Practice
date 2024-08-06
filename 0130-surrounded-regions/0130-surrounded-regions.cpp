a1ead4354d0976f039adb55e8bea238e32f34e0a
Andrew Kim <ksb0724@utexas.edu>
2024-08-06 21:56:55 +0900

    Time: 8 ms (74.69%), Space: 14.3 MB (30.41%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,45 +1,31 @@
 class Solution {
 private:
     int rows, cols;
-    void BFS(vector<vector<char>>& board, int row, int col){
-        queue<pair<int, int>> queue;
-        queue.push({row, col});
+    void DFS(vector<vector<char>>& board, int row, int col){
+        if (row < 0 || row >= rows || col < 0 || col >= cols)
+            return;
         
-        while (!queue.empty()){
-            auto [r, c] = queue.front(); queue.pop();
-            
-            if (board[r][c] != 'O')
-                continue;
-            
-            // Mark this cell as escaped
-            board[r][c] = 'E';
-            // Check its neighbor cells
-            if (c < cols - 1)
-                queue.push({r, c + 1});
-            
-            if (r < rows - 1)
-                queue.push({r + 1, c});
-            
-            if (c > 0)
-                queue.push({r, c - 1});
-            
-            if (r > 0)
-                queue.push({r - 1, c});
-        }
+        if (board[row][col] != 'O')
+            return;
+        board[row][col] = 'E';
+        
+        // Jump to the neighbors without boundary checks
+        for (auto& [ro, co] : vector<pair<int, int>>{make_pair(0, 1), make_pair(1, 0), make_pair(0, -1), make_pair(-1, 0)})
+            DFS(board, row + ro, col + co);
     }
     
 public:
     void solve(vector<vector<char>>& board) {
-        // Base case
-        if (board.empty() || board[0].empty()) return;
+        if (board.empty() || board[0].empty())
+            return;
         
-        this -> rows = board.size(), this -> cols = board[0].size();
+        rows = board.size(), cols = board[0].size();
         vector<pair<int, int>> borders;
         
-        // Step1). retrive all border cells
+        // Step1). retrieve all border cells
         for (int i = 0; i < rows; ++i){
             borders.push_back({i, 0});
-            borders.push_back({i, cols - 1});    
+            borders.push_back({i, cols - 1});
         }
         
         for (int j = 0; j < cols; ++j){
@@ -47,9 +33,9 @@ public:
             borders.push_back({rows - 1, j});
         }
         
-        // Step2). mark the "escaped" cells, with any placeholder, e.g. 'E'
+        // Step2). mark the 'escaped' cell, with any placeholder, e.g. 'E'
         for (auto& [r, c] : borders)
-            BFS(board, r, c);
+            DFS(board, r, c);
         
         // Step3). flip the captured cells ('O' -> 'X') and the escaped one ('E' -> 'O')
         for (int i = 0; i < rows; ++i){
@@ -61,6 +47,5 @@ public:
                     board[i][j] = 'O';  // escaped
             }
         }
-        
     }
 };
