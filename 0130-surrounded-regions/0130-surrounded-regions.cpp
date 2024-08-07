95e532418c592e9128b65a734dd7b6ad621272c3
Andrew Kim <ksb0724@utexas.edu>
2024-08-08 06:39:31 +0900

    Time: 11 ms (57.77%), Space: 14.3 MB (31.16%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,51 +1,84 @@
-class Solution {
+class UnionFind{
 private:
-    int rows, cols;
-    void DFS(vector<vector<char>>& board, int row, int col){
-        if (row < 0 || row >= rows || col < 0 || col >= cols)
-            return;
-        
-        if (board[row][col] != 'O')
-            return;
-        board[row][col] = 'E';
-        
-        // Jump to the neighbors without boundary checks
-        for (auto& [ro, co] : vector<pair<int, int>>{make_pair(0, 1), make_pair(1, 0), make_pair(0, -1), make_pair(-1, 0)})
-            DFS(board, row + ro, col + co);
+    vector<int> parent, rank;
+    int count;
+    
+public:
+    UnionFind(int n){
+        this -> count = n;
+        parent.resize(n);
+        rank.resize(n);
+        iota(parent.begin(), parent.end(), 0);
     }
     
+    int find(int x){
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        
+        return parent[x];
+    }
+    
+    void disjoint(int x, int y){
+        int rootX = find(x), rootY= find(y);
+        
+        if (rootX != rootY){
+            if (rank[rootX] > rank[rootY])
+                parent[rootY] = rootX;
+            else if (rank[rootX] < rank[rootY])
+                parent[rootX] = rootY;
+            else{
+                parent[rootY] = rootX;
+                ++rank[rootX];
+            }
+            --count;
+        }
+    }
+    
+    int getCount(){
+        return count;
+    }
+    
+    bool connected(int x, int y){
+        return find(x) == find(y);
+    }
+};
+
+class Solution {
 public:
     void solve(vector<vector<char>>& board) {
         if (board.empty() || board[0].empty())
             return;
         
-        rows = board.size(), cols = board[0].size();
-        vector<pair<int, int>> borders;
+        int m = board.size(), n = board[0].size();
+        UnionFind uf = UnionFind(m * n + 1);
         
-        // Step1). retrieve all border cells
-        for (int i = 0; i < rows; ++i){
-            borders.push_back({i, 0});
-            borders.push_back({i, cols - 1});
-        }
-        
-        for (int j = 0; j < cols; ++j){
-            borders.push_back({0, j});
-            borders.push_back({rows - 1, j});
-        }
-        
-        // Step2). mark the 'escaped' cell, with any placeholder, e.g. 'E'
-        for (auto& [r, c] : borders)
-            DFS(board, r, c);
-        
-        // Step3). flip the captured cells ('O' -> 'X') and the escaped one ('E' -> 'O')
-        for (int i = 0; i < rows; ++i){
-            for (int j = 0; j < cols; ++j){
-                if (board[i][j] == 'O')
-                    board[i][j] = 'X';  // captured
-                
-                if (board[i][j] == 'E')
-                    board[i][j] = 'O';  // escaped
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j){
+                // If 'O' node is on the boundry, connect it to the dummy node
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1 && board[i][j] == 'O')
+                    uf.disjoint(i * n + j, m * n);
+                // Connect 'O' node to its neighbor 'O' nodes
+                else if (board[i][j] == 'O'){
+                    if (board[i - 1][j] == 'O')
+                        uf.disjoint(i * n + j, (i - 1) * n + j);
+                    
+                    if (board[i + 1][j] == 'O')
+                        uf.disjoint(i * n + j, (i + 1) * n + j);
+                    
+                    if (board[i][j - 1] == 'O')
+                        uf.disjoint(i * n + j, i * n + j - 1);
+                    
+                    if (board[i][j + 1] == 'O')
+                        uf.disjoint(i * n + j, i * n + j + 1);
+                }
             }
+        }
+        
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j)
+                // If a 'O' node is not connected to the dummy node, it's captured.
+                if (!uf.connected(i * n + j, m * n))
+                    board[i][j] = 'X';
         }
     }
 };
