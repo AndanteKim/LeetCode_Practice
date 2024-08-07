0f9cff0398684f8542c9c06a656439f5e80111b5
Andrew Kim <ksb0724@utexas.edu>
2024-08-08 06:13:32 +0900

    Time: 196 ms (5.89%), Space: 19.3 MB (20.40%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -1,44 +1,68 @@
+class UnionFind:
+    def __init__(self, n: int):
+        self.count = n
+        self.parent, self.rank = [i for i in range(n)], [0] * n
+        
+    def find(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        
+        return self.parent[x]
+    
+    def union(self, x: int, y: int) -> None:
+        root_x, root_y = self.find(x), self.find(y)
+        
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+            self.count -= 1
+        
+    def connected(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+        
+    def get_count(self) -> int:
+        return self.count
+
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
-        if not board and not board[0]:
+        if not board or not board[0]:
             return
         
-        self.m, self.n = len(board), len(board[0])
+        m, n = len(board), len(board[0])
+        uf = UnionFind(m * n + 1)
         
-        # Step1). retrieve all border cells
-        borders = list(product(range(self.m), [0, self.n - 1])) + list(product([0, self.m - 1], range(self.n)))
+        for i in range(m):
+            for j in range(n):
+                # If 'O' node is on the boundry, connect it to the dummy node
+                if i == 0 or i == m - 1 or j == 0 or j == n - 1 and board[i][j] == 'O':
+                    uf.union(i * n + j, n * m)
+                elif board[i][j] == 'O':
+                    if board[i - 1][j] == 'O':
+                        uf.union(i * n + j, (i - 1) * n + j)
+                        
+                    if board[i + 1][j] == 'O':
+                        uf.union(i * n + j, (i + 1) * n + j)
+                        
+                    if board[i][j - 1] == 'O':
+                        uf.union(i * n + j, i * n + j - 1)
+                    
+                    if board[i][j + 1] == 'O':
+                        uf.union(i * n + j, i * n + j + 1)
+                        
+        for i in range(m):
+            for j in range(n):
+                # If the dummy node is not connected to the dummy node, it's captured
+                if not uf.connected(i * n + j, n * m):
+                    board[i][j] = 'X'
         
-        # Step2). mark the "escaped" cells, with any placeholder, e.g. 'E'
-        for row, col in borders:
-            self.DFS(board, row, col)
-            
-        # Step3). flip the captured cells ('O' -> 'X') and the escaped one ('E' -> 'O')
-        for i in range(self.m):
-            for j in range(self.n):
-                if board[i][j] == 'O':
-                    board[i][j] = 'X'   # captured
+                    
                 
-                elif board[i][j] == 'E':
-                    board[i][j] = 'O'   # escaped
-                    
-                    
-    def DFS(self, board: List[List[int]], row: int, col: int) -> None:
-        if (board[row][col] != 'O'):
-            return
-        board[row][col] = 'E'
         
-        if col < self.n - 1:
-            self.DFS(board, row, col + 1)
-        
-        if row < self.m - 1:
-            self.DFS(board, row + 1, col)
-            
-        if col > 0:
-            self.DFS(board, row, col - 1)
-            
-        if row > 0:
-            self.DFS(board, row - 1, col)
-       
