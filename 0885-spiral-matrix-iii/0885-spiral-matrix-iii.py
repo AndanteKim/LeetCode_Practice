81924a8b7b5a83e229d877d08e7ee5bc5f1c8471
Andrew Kim <ksb0724@utexas.edu>
2024-08-08 10:38:48 +0900

    Time: 90 ms (23.83%), Space: 17.9 MB (38.67%) - LeetHub

--- 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
+++ 0885-spiral-matrix-iii/0885-spiral-matrix-iii.py
@@ -1,46 +1,28 @@
 class Solution:
     def spiralMatrixIII(self, rows: int, cols: int, rStart: int, cStart: int) -> List[List[int]]:
-        count, magnitude, ans = 0, 1, []
-        row, col = rStart, cStart
+        # Store all possible directions in an array.
+        dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        ans = []
         
-        while count < rows * cols:
-            # Right
-            c = 0
-            while c < magnitude and count < rows * cols:
-                if 0 <= row < rows and 0 <= col < cols:
-                    ans.append([row, col])
-                    count += 1
-                col += 1
-                c += 1
-            
-            # Down
-            r = 0
-            while r < magnitude and count < rows * cols:
-                if 0 <= row < rows and 0 <= col < cols:
-                    ans.append([row, col])
-                    count += 1
-                row += 1
-                r += 1
-            magnitude += 1
-            
-            # Left
-            c = 0
-            while c < magnitude and count < rows * cols:
-                if 0 <= row < rows and 0 <= col < cols:
-                    ans.append([row, col])
-                    count += 1
-                col -= 1
-                c += 1
-            
-            # Up
-            r = 0
-            while r < magnitude and count < rows * cols:
-                if 0 <= row < rows and 0 <= col < cols:
-                    ans.append([row, col])
-                    count += 1
-                row -= 1
-                r += 1
+        # Initial step size is 1, value of d represents the current direction.
+        step, direction = 1, 0
+        
+        while len(ans) < rows * cols:
+            # direction = 0 -> East, direction = 1 -> South
+            # direction = 2 -> West, direction = 3 -> North
+            for _ in range(2):
+                for _ in range(step):
+                    # Validate the current position
+                    if 0 <= rStart < rows and 0 <= cStart < cols:
+                        ans.append([rStart, cStart])
+                        
+                    # Make changes to the current position
+                    rStart += dir[direction][0]
+                    cStart += dir[direction][1]
+                    
+                direction = (direction + 1) % 4
                 
-            magnitude += 1
+            step += 1
             
         return ans
+            
