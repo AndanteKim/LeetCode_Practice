bbeedc3c8c743d685b623676da7a09c50da7a387
Andrew Kim <ksb0724@utexas.edu>
2024-08-12 19:51:38 +0900

    Time: 88 ms (70.82%), Space: 18.4 MB (39.73%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,28 +1,44 @@
 class Solution:
+    WHITE, GRAY, BLACK = 1, 2, 3
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # Create the adjacency list representation of the graph
         adj = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
         
+        # A pair [a, b] in the input represents edges from b -> a
         for end, start in prerequisites:
-            indegree[end] += 1
             adj[start].append(end)
             
-        queue = deque()
+        # Topological order
+        ans, is_possible = [], True
         
-        for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
+        # By default all vertices are WHITE
+        color = {k : self.WHITE for k in range(numCourses)}
         
-        ans = []
-        while queue:
-            curr = queue.popleft()
-            ans.append(curr)
+        def dfs(node: int) -> None:
+            nonlocal is_possible
             
-            for neighbor in adj[curr]:
-                indegree[neighbor] -= 1
-                
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-                
-                
-        return ans if len(ans) == numCourses else []
+            # Don't recurse further if we found a cycle already
+            if not is_possible:
+                return
+            
+            # Start the recursion
+            color[node] = self.GRAY
+            
+            # Traverse on neighboring vertices
+            for neighbor in adj[node]:
+                if color[neighbor] == self.WHITE:
+                    dfs(neighbor)
+                        
+                elif color[neighbor] == self.GRAY:
+                    # An edge to a GRAY vertex represents a cycle
+                    is_possible = False
+                        
+            # Recursion ends. We mark it as black
+            color[node] = self.BLACK
+            ans.append(node)
+            
+        for vertex in range(numCourses):
+            # If the node is unprocessed, then call dfs on it.
+            if color[vertex] == self.WHITE:
+                dfs(vertex)
+        return ans[::-1] if is_possible else []
