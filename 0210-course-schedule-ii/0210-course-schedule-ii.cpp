6a3d5f279610d0b9a66b5f912343dca30be0a375
Andrew Kim <ksb0724@utexas.edu>
2024-08-12 20:40:14 +0900

    Time: 15 ms (68.25%), Space: 18.2 MB (25.03%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,32 +1,59 @@
 class Solution {
-public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<vector<int>> adj(numCourses);
-        vector<int> indegree(numCourses);
+private:
+    enum status {WHITE, GRAY, BLACK};
+    bool isPossible = true;
+    
+    void dfs(int node, vector<vector<int>>& adj, unordered_map<int, status>& color, vector<int>& ans){
+        // Don't recurse further if we found a cycle already.
+        if (!isPossible)
+            return;
         
-        for (auto& prerequisite : prerequisites){
-            adj[prerequisite[1]].push_back(prerequisite[0]);
-            ++indegree[prerequisite[0]];
-        }
+        // Start the recursion
+        color[node] = status::GRAY;
         
-        queue<int> queue;
-        
-        for (int i = 0; i < numCourses; ++i)
-            if (indegree[i] == 0)
-                queue.push(i);
-        
-        vector<int> ans;
-        while (!queue.empty()){
-            int curr = queue.front(); queue.pop();
-            ans.push_back(curr);
-            
-            for (int neighbor : adj[curr]){
-                --indegree[neighbor];
-                if (indegree[neighbor] == 0)
-                    queue.push(neighbor);
+        // Traverse on neighboring vertices.
+        for (int neighbor : adj[node]){
+            if (color[neighbor] == status::WHITE)
+                dfs(neighbor, adj, color, ans);
+            else if (color[neighbor] == status::GRAY){
+                // An edge to a GRAY vertex represents a cycle.
+                isPossible = false;
             }
         }
         
-        return ans.size() == numCourses? ans : vector<int>{};
+        // Recursion ends. We mark it as black.
+        color[node] = status::BLACK;
+        ans.push_back(node);
+    }
+    
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<vector<int>> adj(numCourses);
+        
+        // Create the adjacency list representation of the graph.
+        for (auto& prerequisite : prerequisites){
+            adj[prerequisite[1]].push_back(prerequisite[0]);
+        }
+        
+        // By default, all vertices are WHITE.
+        unordered_map<int, status> color;
+        for (int k = 0; k < numCourses; ++k)
+            color[k] = status::WHITE;
+        
+        vector<int> ans;
+        
+        // If the node is unprocessed, then call dfs on it.
+        for (int i = 0; i < numCourses && isPossible; ++i){
+            if (color[i] == status::WHITE)
+                dfs(i, adj, color, ans);
+        }
+        
+        
+        if (isPossible){
+            reverse(ans.begin(), ans.end());
+            return ans;
+        }
+        
+        return vector<int>{};
     }
 };
