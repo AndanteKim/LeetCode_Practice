2a64e2ba548292d1875680d11d8bec05cc93bbde
Andrew Kim <ksb0724@utexas.edu>
2024-08-15 01:21:12 +0900

    Time: 3 ms (41.22%), Space: 8.6 MB (6.61%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
-        int ans = INT_MAX, n = startGene.size();
-        unordered_set<string> validBank(bank.begin(), bank.end()), visited;
         queue<pair<string, int>> queue;
         queue.push({startGene, 0});
+        int n = startGene.size();
+        unordered_set<string> visited;
         visited.insert(startGene);
-        const vector<int> geneChar{'A', 'C', 'G', 'T'};
+        const string geneChar{"ACGT"};
         
         while (!queue.empty()){
             auto [curr, steps] = queue.front(); queue.pop();
             
             if (curr == endGene)
-                ans = min(ans, steps);
+                return steps;
             
-            for (int i = 0; i < n; ++i){
-                char temp = curr[i];
-                for (const auto& c : geneChar){
-                    if (curr[i] != c){
-                        curr[i] = c;
-                        if (validBank.count(curr) && !visited.count(curr)){
-                            queue.push({curr, steps + 1});
-                            visited.insert(curr);
-                        }
-                        curr[i] = temp;
+            for (const char& c : geneChar){
+                for (int i = 0; i < n; ++i){
+                    string neighbor = curr.substr(0, i) + c + curr.substr(i + 1);
+                    if (find(bank.begin(), bank.end(), neighbor) != bank.end() && !visited.count(neighbor)){
+                        visited.insert(neighbor);
+                        queue.push({neighbor, steps + 1});
                     }
                 }
             }
+            
         }
         
-        return ans == INT_MAX? -1 : ans;
+        return -1;
     }
 };
