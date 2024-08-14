cf70689a29f362eb94ad11db17ab0f6600e26187
Andrew Kim <ksb0724@utexas.edu>
2024-08-15 01:22:28 +0900

    Time: 3 ms (41.22%), Space: 8.6 MB (8.39%) - LeetHub

--- 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int minMutation(string startGene, string endGene, vector<string>& bank) {
+        
         queue<pair<string, int>> queue;
         queue.push({startGene, 0});
         int n = startGene.size();
-        unordered_set<string> visited;
+        unordered_set<string> visited, bankGenes(bank.begin(), bank.end());
         visited.insert(startGene);
         const string geneChar{"ACGT"};
         
@@ -17,7 +18,7 @@ public:
             for (const char& c : geneChar){
                 for (int i = 0; i < n; ++i){
                     string neighbor = curr.substr(0, i) + c + curr.substr(i + 1);
-                    if (find(bank.begin(), bank.end(), neighbor) != bank.end() && !visited.count(neighbor)){
+                    if (bankGenes.count(neighbor) && !visited.count(neighbor)){
                         visited.insert(neighbor);
                         queue.push({neighbor, steps + 1});
                     }
