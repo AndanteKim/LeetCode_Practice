bdc1140507d505406b4146e92c00b722817381d2
Andrew Kim <ksb0724@utexas.edu>
2024-08-15 15:53:47 +0900

    Time: 207 ms (82.39%), Space: 16.7 MB (73.30%) - LeetHub

--- 3189-minimum-moves-to-get-a-peaceful-board/3189-minimum-moves-to-get-a-peaceful-board.py
+++ 3189-minimum-moves-to-get-a-peaceful-board/3189-minimum-moves-to-get-a-peaceful-board.py
@@ -1,15 +1,21 @@
 class Solution:
     def minMoves(self, rooks: List[List[int]]) -> int:
-        n, ans = len(rooks), 0
+        ans, n = 0, len(rooks)
         
-        rooks.sort()
-        # Moves required to place rooks in each row
-        for i in range(n):
-            ans += abs(i - rooks[i][0])
+        # Store the count of rooks in each row and colimn
+        row, col = [0] * n, [0] * n
+        for r in rooks:
+            row[r[0]] += 1
+            col[r[1]] += 1
+            
+        row_min_moves, col_min_moves = 0, 0
         
-        rooks.sort(key = lambda x: x[1])
-        # Moves required to place rooks in each column
         for i in range(n):
-            ans += abs(i - rooks[i][1])
+            # Difference between the rooks count at row and column and one.
+            row_min_moves += row[i] - 1
+            col_min_moves += col[i] - 1
+            
+            # Moves required for row and column constraints
+            ans += abs(row_min_moves) + abs(col_min_moves)
             
         return ans
