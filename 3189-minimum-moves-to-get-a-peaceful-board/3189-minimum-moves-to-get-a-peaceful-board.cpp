459a8e56a4620e3558a2aa5636530cf042c7adad
Andrew Kim <ksb0724@utexas.edu>
2024-08-15 15:44:27 +0900

    Time: 59 ms (7.63%), Space: 39.1 MB (64.41%) - LeetHub

--- 3189-minimum-moves-to-get-a-peaceful-board/3189-minimum-moves-to-get-a-peaceful-board.cpp
+++ 3189-minimum-moves-to-get-a-peaceful-board/3189-minimum-moves-to-get-a-peaceful-board.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int minMoves(vector<vector<int>>& rooks) {
-        int ans = 0, n = rooks.size(), row = 0;
+        int ans = 0, n = rooks.size();
         
         sort(rooks.begin(), rooks.end(), [](vector<int>& a, vector<int>& b){return a[0] < b[0];});
-        for (int i = 0; i < n; ++i){
-            ans += abs(rooks[i][0] - row++);
-        }
-        
-        sort(rooks.begin(), rooks.end(), [](vector<int>& a, vector<int>& b){return a[1] < b[1];});
-        int col = 0;
+        // Moves required to place rooks in each row 
         for (int i = 0; i < n; ++i)
-            ans += abs(rooks[i][1] - col++);
+            ans += abs(rooks[i][0] - i);
+            
+        sort(rooks.begin(), rooks.end(), [](vector<int>& a, vector<int>& b){return a[1] < b[1];});
+        // Moves required to place rooks in each column
+        for (int i = 0; i < n; ++i)
+            ans += abs(rooks[i][1] - i);
         
         return ans;
     }
