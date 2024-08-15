94c820287319a48c88ff56382701fc1b7ded4cac
Andrew Kim <ksb0724@utexas.edu>
2024-08-15 10:33:42 +0900

    Time: 617 ms (62.64%), Space: 21.2 MB (58.06%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,20 +1,21 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        # Index 0: $5 ~ Index 2: $20
-        currencies, mapping_idx_to_dollar = [0] * 3, {0 : 5, 1 : 10, 2 : 20}
-        mapping_dollar_to_idx = {5 : 0, 10 : 1, 20: 2}
+        # Concise simulation
+        five, ten = 0, 0
         
-        for i in range(len(bills)):
-            change = bills[i] - 5
-            for currency in range(2, -1, -1):
-                if change == 0:
-                    break
-                units = min(change // mapping_idx_to_dollar[currency], currencies[currency])
-                currencies[currency] -= units
-                change -= mapping_idx_to_dollar[currency] * units
+        for bill in bills:
+            if bill == 5:
+                five += 1
+            elif bill == 10:
+                five -= 1
+                ten += 1
+            elif ten > 0:
+                five -= 1
+                ten -= 1
+            else:
+                five -= 3
             
-            if change > 0:
+            if five < 0:
                 return False
-            currencies[mapping_dollar_to_idx[bills[i]]] += 1
             
         return True
