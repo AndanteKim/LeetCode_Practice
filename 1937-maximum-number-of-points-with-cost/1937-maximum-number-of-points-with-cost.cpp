75a1e38c2996c11e4984d4afe9d2bab38b8ee1ac
Andrew Kim <ksb0724@utexas.edu>
2024-08-18 00:49:58 +0900

    Time: 169 ms (84.47%), Space: 87.6 MB (85.56%) - LeetHub

--- 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
+++ 1937-maximum-number-of-points-with-cost/1937-maximum-number-of-points-with-cost.cpp
@@ -3,36 +3,32 @@ typedef long long ll;
 class Solution {
 public:
     long long maxPoints(vector<vector<int>>& points) {
-        int m = points.size(), n = points[0].size();
-        vector<ll> prevRow(n);
+        int n = points[0].size();
+        vector<ll> currRow(n), prevRow(n);
+        ll runningMax;
         
-        // Intiailize the first row
-        for (int c = 0; c < n; ++c)
-            prevRow[c] = points[0][c];
-        
-        // Process each row
-        for (int r = 1; r < m; ++r){
-            vector<ll> currRow(n), leftMax(n), rightMax(n);
+        for (auto& row : points){
+            // RunningMax holds the maximum value generated in the previous
+            // iteration of each loop
+            runningMax = 0;
             
-            // Calculate left-to-right maximum
-            leftMax[0] = prevRow[0];
-            for (int c = 1; c < n; ++c)
-                leftMax[c] = max(leftMax[c - 1] - 1, prevRow[c]);
+            // Left to right pass
+            for (int col = 0; col < row.size(); ++col){
+                runningMax = max(runningMax - 1, prevRow[col]);
+                currRow[col] = runningMax;
+            }
             
-            // Calculate right-to-left maximum
-            rightMax.back() = prevRow.back();
-            for (int c = n - 2; c >= 0; --c)
-                rightMax[c] = max(rightMax[c + 1] - 1, prevRow[c]);
+            runningMax = 0;
+            // Right to left pass
+            for (int col = n - 1; col >= 0; --col){
+                runningMax = max(runningMax - 1, prevRow[col]);
+                currRow[col] = max(currRow[col], runningMax) + row[col];
+            }
             
-            // Calculate the current row's maximum points
-            for (int c = 0; c < n; ++c)
-                currRow[c] = points[r][c] + max(leftMax[c], rightMax[c]);
-            
-            // Update the previousRow for the next iteration
+            // Update previousRow for the next iteration
             prevRow = currRow;
         }
         
-        // Find the maximum value in the last processed row
         return *max_element(prevRow.begin(), prevRow.end());
     }
 };
