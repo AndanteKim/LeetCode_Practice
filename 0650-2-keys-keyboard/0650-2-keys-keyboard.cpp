e8b8d00f8c6c965f2b862ea7ac781e32db979fbe
Andrew Kim <ksb0724@utexas.edu>
2024-08-19 19:33:09 +0900

    Time: 13 ms (39.85%), Space: 8.2 MB (30.87%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,30 +1,20 @@
 class Solution {
-private:
-    int n;
-    
-    int minStepsHelper(int currLen, int pasteLen, vector<vector<int>>& memo){
-        if (currLen == n)
-            return 0;
-        
-        if (currLen > n)
-            return 1000;
-        
-        // return the result if it has been already calculated
-        if (memo[currLen][pasteLen] != -1)
-            return memo[currLen][pasteLen];
-        
-        int opt1 = 1 + minStepsHelper(currLen + pasteLen, pasteLen, memo);
-        int opt2 = 2 + minStepsHelper(currLen * 2, currLen, memo);
-        return memo[currLen][pasteLen] = min(opt1, opt2);
-    }
-    
 public:
     int minSteps(int n) {
-        if (n == 1) return 0;
+        vector<int> dp(n + 1, 1000);
         
-        this -> n = n;
-        vector<vector<int>> memo(n + 1, vector<int>((n >> 1) + 1, -1));
+        // base case
+        dp[1] = 0;
+        for (int i = 2; i <= n; ++i){
+            for (int j = 1; j <= (i >> 1); ++j){
+                if (i % j == 0){
+                    // Copy all and paste (i - j) / j times
+                    // for all valid j's
+                    dp[i] = min(dp[i], dp[j] + i / j);
+                }
+            }
+        }
         
-        return 1 + minStepsHelper(1, 1, memo);
+        return dp[n];
     }
 };
