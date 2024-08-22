9f62ecbb1a22bd4026d13b991b35108e2797ccdc
Andrew Kim <ksb0724@utexas.edu>
2024-08-22 16:30:21 +0900

    Time: 33 ms (32.00%), Space: 21 MB (96.80%) - LeetHub

--- 0320-generalized-abbreviation/0320-generalized-abbreviation.cpp
+++ 0320-generalized-abbreviation/0320-generalized-abbreviation.cpp
@@ -1,31 +1,32 @@
 class Solution {
-private:
-    int n;
-    void abbreviated(vector<string>& abbreviations, const string& word, string currWord, int index, int abbreviatedCount){
-        // Base case
-        if (index == n){
-            // If the length of the last abbreviated substring is 0, add an empty string.
+public:
+    vector<string> generateAbbreviations(string word) {
+        int n = word.size();
+        vector<string> abbreviations;
+        
+        for (int mask = 0; mask < (1 << n); ++mask){
+            string currWord = "";
+            int abbreviatedCount = 0;
+            for (int index = 0; index < n; ++index){
+                // If the bit at the position index is 1, increment the abbreviated substring.
+                if (mask & (1 << index))
+                    ++abbreviatedCount;
+                
+                else{
+                    // Append the last substring and then the current character.
+                    if (abbreviatedCount > 0){
+                        currWord += to_string(abbreviatedCount);
+                        abbreviatedCount = 0;
+                    }
+                    currWord.push_back(word[index]);
+                }
+            }
+            
             if (abbreviatedCount > 0)
                 currWord += to_string(abbreviatedCount);
             abbreviations.push_back(currWord);
-            return;
         }
         
-        // Option 1: Keep the current character 
-        string abbreviatedString = (abbreviatedCount > 0)? to_string(abbreviatedCount) : "";
-        abbreviated(abbreviations, word, currWord + abbreviatedString + word[index], index + 1, 0);
-        
-        // Option 2: Abbreviate the current character
-        abbreviated(abbreviations, word, currWord, index + 1, abbreviatedCount + 1);
-    }
-    
-    
-public:
-    vector<string> generateAbbreviations(string word) {
-        this -> n = word.size();
-        vector<string> abbreviations;
-        
-        abbreviated(abbreviations, word, "", 0, 0);
         return abbreviations;
     }
 };
