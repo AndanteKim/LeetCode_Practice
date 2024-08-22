328450450319d3f37d251df643a992c46de630f6
Andrew Kim <ksb0724@utexas.edu>
2024-08-22 12:15:58 +0900

    Time: 38 ms (30.88%), Space: 16.5 MB (58.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,13 +1,10 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        todo, bit = num, 1
+        # n is a length of num in binary representation
+        n = floor(log2(num)) + 1
         
-        while todo:
-            # flip the current bit
-            num ^= bit
-            
-            # prepare for the next run
-            bit <<= 1
-            todo >>= 1
-            
-        return num
+        # The bitmask has the same length as num and contains only ones 1...1
+        bitmask = (1 << n) - 1
+        
+        # Flip all bits
+        return bitmask ^ num
