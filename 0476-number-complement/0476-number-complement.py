143f0f28cebb28549b1bd5340ae6d0c5e778b3cb
Andrew Kim <ksb0724@utexas.edu>
2024-08-22 12:40:24 +0900

    Time: 28 ms (90.35%), Space: 16.5 MB (14.64%) - LeetHub

--- 0476-number-complement/0476-number-complement.py
+++ 0476-number-complement/0476-number-complement.py
@@ -1,3 +1,11 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        return (1 << num.bit_length()) - 1 - num
+        bitmask = num
+        bitmask |= (bitmask >> 1)
+        bitmask |= (bitmask >> 2)
+        bitmask |= (bitmask >> 4)
+        bitmask |= (bitmask >> 8)
+        bitmask |= (bitmask >> 16)
+        
+        # flip all bits
+        return bitmask ^ num
