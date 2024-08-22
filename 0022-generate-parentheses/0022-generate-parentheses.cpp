200d5336508d268836afe41e058a9a7081798381
Andrew Kim <ksb0724@utexas.edu>
2024-08-23 03:13:02 +0900

    Time: 9 ms (10.43%), Space: 15.5 MB (33.07%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,38 +1,17 @@
 class Solution {
-private:
-    bool isValid(string& pString){
-        int leftCount = 0;
-        
-        for (const char& p : pString){
-            if (p == '(')
-                ++leftCount;
-            else
-                --leftCount;
-            
-            if (leftCount < 0) return false;
-        }
-        
-        return leftCount == 0;
-    }
-    
 public:
     vector<string> generateParenthesis(int n) {
-        queue<string> queue;
-        queue.push("");
-        vector<string> ans;
+        if (n == 0)
+            return vector<string>{""};
         
-        while (!queue.empty()){
-            auto currStr = queue.front(); queue.pop();
-            
-            // If the length of current string is 2 * n, add it to 'answer' if it's valid.
-            if (currStr.size() == 2 * n){
-                if (isValid(currStr))
-                    ans.push_back(currStr);
-                continue;
+        vector<string> ans;
+        for (int leftCount = 0; leftCount < n; ++leftCount){
+            string curr = "";
+            for (const string& leftString : generateParenthesis(leftCount)){
+                for (const string& rightString : generateParenthesis(n - 1 - leftCount)){
+                    ans.push_back("(" + leftString + ")" + rightString);
+                }
             }
-            
-            queue.push(currStr + "(");
-            queue.push(currStr + ")");
         }
         
         return ans;
