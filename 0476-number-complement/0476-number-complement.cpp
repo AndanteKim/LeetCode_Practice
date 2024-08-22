78df3b70b21e3464109ba3b1bb8342662437e0ca
Andrew Kim <ksb0724@utexas.edu>
2024-08-22 12:13:15 +0900

    Time: 0 ms (100.00%), Space: 7.3 MB (65.77%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int findComplement(int num) {
-        long ans = 0, bitwise = 1;
+        int todo = num, flip = 1;
         
-        while (bitwise < num){
-            if (!(num & bitwise))
-                ans += bitwise;
+        while (todo){
+            // flip the current bit
+            num ^= flip;
             
-            bitwise <<= 1;
+            // prepare for the next run
+            flip <<= 1;
+            todo >>= 1;
         }
         
-        return static_cast<int>(ans);
+        return num;
     }
 };
