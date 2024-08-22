b783082405c9e5b871f36a961de3f128031c5f37
Andrew Kim <ksb0724@utexas.edu>
2024-08-23 03:00:55 +0900

    Time: 47 ms (8.46%), Space: 16.7 MB (97.31%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,33 +1,13 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        def is_valid(p_str: str) -> bool:
-            left_count = 0
-            
-            for p in p_str:
-                if p == '(':
-                    left_count += 1
-                else:    
-                    left_count -= 1
-                    
-                if left_count < 0:
-                    return False
-                
-            return left_count == 0
+        if n == 0:
+            return [""]
         
         ans = []
-        queue = deque([""])
         
-        while queue:
-            curr_str = queue.popleft()
-            
-            # If the length of curr_str is 2 * n, add it to 'answer' if
-            # it's valid.
-            if len(curr_str) == 2 * n:
-                if is_valid(curr_str):
-                    ans.append(curr_str)
-                continue
-            
-            queue.append(curr_str + '(')
-            queue.append(curr_str + ')')
-    
+        for left_count in range(n):
+            for left_string in self.generateParenthesis(left_count):
+                for right_string in self.generateParenthesis(n - 1 - left_count):
+                    ans.append("(" + left_string + ")" + right_string)
+                    
         return ans
