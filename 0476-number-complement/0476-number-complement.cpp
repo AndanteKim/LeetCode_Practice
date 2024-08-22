961698c8f478bc7f905f46cb0446d7c077b20750
Andrew Kim <ksb0724@utexas.edu>
2024-08-22 12:38:41 +0900

    Time: 0 ms (100.00%), Space: 7.6 MB (33.08%) - LeetHub

--- 0476-number-complement/0476-number-complement.cpp
+++ 0476-number-complement/0476-number-complement.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int findComplement(int num) {
-        // n is a length of num in binary representation
-        int n = log2(num) + 1;
-        
-        // The bitmask has the same length as num and contains only ones 1...1
-        int bitmask = (long)(1 << n) - 1;
-        
-        // Flip all bits
-        return static_cast<int>(bitmask ^ num);
+        int n = log2(num);
+        return (long)(1 << (n + 1)) - 1 - num;
     }
 };
