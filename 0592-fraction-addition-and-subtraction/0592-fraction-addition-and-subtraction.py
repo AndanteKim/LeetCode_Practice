11176bd767c96a6590fced8b8ce1c84db25dcfbd
Andrew Kim <ksb0724@utexas.edu>
2024-08-23 14:08:31 +0900

    Time: 28 ms (93.02%), Space: 16.5 MB (59.69%) - LeetHub

--- 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
+++ 0592-fraction-addition-and-subtraction/0592-fraction-addition-and-subtraction.py
@@ -1,3 +1,5 @@
+import re
+
 class Solution:
     def _find_gcd(self, a: int, b: int) -> int:
         if a == 0:
@@ -8,44 +10,19 @@ class Solution:
     def fractionAddition(self, expression: str) -> str:
         num, denom = 0, 1
         
-        i, n = 0, len(expression)
-        while i < n:
-            curr_num, curr_denom = 0, 0
-            is_negative = False
+        # Separate expression into signed numbers
+        nums = re.split("/|(?=[-+])", expression)
+        nums = list(filter(None, nums))
+        
+        for i in range(0, len(nums), 2):
+            curr_num = int(nums[i])
+            curr_denom = int(nums[i + 1])
             
-            # Check for sign
-            if expression[i] == '-' or expression[i] == '+':
-                if expression[i] == '-':
-                    is_negative = True
-                # Move to next character
-                i += 1
-                
-            # Build numerator
-            while i < n and expression[i].isdigit():
-                val = int(expression[i])
-                curr_num = curr_num * 10 + val
-                i += 1
-                
-            if is_negative:
-                curr_num *= -1
-                
-            # Skip divisor
-            i += 1
-            
-            # Build denominator
-            while i < n and expression[i].isdigit():
-                val = int(expression[i])
-                curr_denom = curr_denom * 10 + val
-                i += 1
-                
-            # Add fractions together using common denominator
             num = num * curr_denom + curr_num * denom
-            denom = denom * curr_denom
+            denom *= curr_denom
             
         gcd = abs(self._find_gcd(num, denom))
-        
-        # Reduce fractions
         num //= gcd
         denom //= gcd
         
-        return f"{num}/{denom}"
+        return str(num) + "/" + str(denom)
