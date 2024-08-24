eacaf1a14bb8a8ac7256c80058dc202ff85c8edc
Andrew Kim <ksb0724@utexas.edu>
2024-08-25 08:59:39 +0900

    Time: 162 ms (13.28%), Space: 75.1 MB (10.15%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -9,23 +9,53 @@
  * };
  */
 class Solution {
-public:
-    ListNode* sortList(ListNode* head) {
-        ListNode* root = head;
-        vector<int> flatten;
-        ListNode* dummy = new ListNode(), *curr = dummy;
+private:
+    ListNode* getMid(ListNode* head){
+        ListNode* midPrev = nullptr;
         
-        while (root){
-            flatten.push_back(root -> val);
-            root = root -> next;
+        while (head && head -> next){
+            midPrev = (midPrev == nullptr)? head : midPrev -> next;
+            head = head -> next -> next; 
         }
         
-        sort(flatten.begin(), flatten.end());
-        for (int n : flatten){
-            curr -> next = new ListNode(n);
+        ListNode* mid = midPrev -> next;
+        midPrev -> next = nullptr;
+        return mid;
+    }
+    
+    ListNode* merge(ListNode* list1, ListNode* list2){
+        ListNode *dummy = new ListNode(), *curr = dummy;
+        while (list1 && list2){
+            if (list1 -> val < list2 -> val){
+                curr -> next = list1;
+                list1 = list1 -> next;
+            }
+            else{
+                curr -> next = list2;
+                list2 = list2 -> next;
+            }
+            
             curr = curr -> next;
         }
+        curr -> next = list1? list1 : list2;
         
         return dummy -> next;
+    }
+    
+public:
+    ListNode* sortList(ListNode* head) {
+        // Top down style
+        // Base case: If the head or the entire list is none, return the head.
+        if (!head || !head -> next)
+            return head;
+        
+        // Get the middle node
+        ListNode* mid = getMid(head);
+        
+        // Split the list to left and right and sort them
+        ListNode* left = sortList(head), *right = sortList(mid);
+        
+        // Merge the sorted lists
+        return merge(left, right);
     }
 };
