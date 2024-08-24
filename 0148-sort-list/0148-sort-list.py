7336548926a22d805f3d59ea91fa729ac593a438
Andrew Kim <ksb0724@utexas.edu>
2024-08-25 08:31:06 +0900

    Time: 290 ms (46.90%), Space: 32.4 MB (64.53%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -5,16 +5,43 @@
 #         self.next = next
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        root, flatten = head, []
-        dummy = curr = ListNode()
+        # If the head or the entire list is none, return the head
+        if not head or not head.next:
+            return head
         
-        while root:
-            flatten.append(root.val)
-            root = root.next
-            
-        flatten.sort()
-        for n in flatten:
-            curr.next = ListNode(n)
-            curr = curr.next
+        # Get the middle node
+        mid = self.get_mid(head)
         
+        # Split the list to left and right and sort them
+        left = self.sortList(head)
+        right = self.sortList(mid)
+        
+        # Merge the sorted lists
+        return self.merge(left, right)
+    
+    def merge(self, list1: ListNode, list2: ListNode) -> ListNode:
+        dummy = ListNode()
+        tail = dummy
+        
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        tail.next = list1 if list1 else list2
         return dummy.next
+    
+    def get_mid(self, head: ListNode) -> ListNode:
+        mid_prev = None
+        
+        while head and head.next:
+            mid_prev = head if not mid_prev else mid_prev.next
+            head = head.next.next
+            
+        mid = mid_prev.next
+        mid_prev.next = None
+        
+        return mid 
