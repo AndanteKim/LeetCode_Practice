500f079895665141f121244386efb32db332b1be
Andrew Kim <ksb0724@utexas.edu>
2024-08-24 16:25:56 +0900

    Time: 56 ms (6.61%), Space: 16.5 MB (92.31%) - LeetHub

--- 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
@@ -1,47 +1,53 @@
 class Solution:
-    def nearestPalindromic(self, n: str) -> str:
-        len_n = len(n)
-        i = (len_n >> 1) - 1 if len_n % 2 == 0 else (len_n >> 1)
+    def convert(self, num: int) -> int:
+        s = str(num)
+        n = len(s)
+        l, r = (n - 1) >> 1, n >> 1
         
-        first_half = int(n[: i + 1])
-        """
-        Generate possible palindromic candidates:
-        1. Create a palindrome by mirroring the first half.
-        2. Create a palindrome by mirroring the first half incremented by 1.
-        3. Create a palindrome by mirroring the first half decremented by 1.
-        4. Handle edge cases by considering palindromes of the form 999...
-           and 100...001 (smallest and largest n-digit palindromes).
-        """
-        possibilities = []
-        possibilities.append(self.half_to_palindrome(first_half, len_n % 2 == 0))
-        possibilities.append(self.half_to_palindrome(first_half + 1, len_n % 2 == 0))
-        possibilities.append(self.half_to_palindrome(first_half - 1, len_n % 2 == 0))
-        possibilities.append(10 ** (len_n - 1) - 1)
-        possibilities.append(10 ** len_n + 1)
-        
-        diff = float("inf")
-        ans = 0
-        num = int(n)
-        
-        for cand in possibilities:
-            if cand == num:
-                continue
-            if abs(cand - num) < diff:
-                diff = abs(cand - num)
-                ans = cand
-            elif abs(cand - num) == diff:
-                ans = min(ans, cand)
-        
-        return str(ans)
-    
-    def half_to_palindrome(self, left: int, even: bool) -> int:
-        ans = left
-        if not even:
-            left //= 10
-        
-        while left > 0:
-            ans = ans * 10 + left % 10
-            left //= 10
+        s_list = list(s)
+        while l >= 0:
+            s_list[r] = s_list[l]
+            r += 1
+            l -= 1
             
-        return ans
+        return int("".join(s_list))
+    
+    def next_palindrome(self, num: int) -> int:
+        left, right = 0, num
+        ans = float("-inf")
         
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            palin = self.convert(mid)
+            
+            if palin < num:
+                ans = palin
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return ans
+    
+    def prev_palindrome(self, num: int) -> int:
+        left, right = num, int(1e18)
+        ans = float("-inf")
+        
+        while left <= right:
+            mid = left + ((right - left) >> 1)
+            palin = self.convert(mid)
+            if palin > num:
+                ans = palin
+                right = mid - 1
+            else:
+                left = mid + 1
+                
+        return ans
+    
+    def nearestPalindromic(self, n: str) -> str:
+        num = int(n)
+        a = self.next_palindrome(num)
+        b = self.prev_palindrome(num)
+        
+        if abs(a - num) <= abs(b - num):
+            return str(a)
+        return str(b)
