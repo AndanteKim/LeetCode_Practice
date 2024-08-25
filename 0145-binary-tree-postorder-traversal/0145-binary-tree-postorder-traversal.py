e00b0ae2fd981e5fbf635cdfb3f53c10da523635
Andrew Kim <ksb0724@utexas.edu>
2024-08-25 09:32:10 +0900

    Time: 37 ms (43.44%), Space: 16.6 MB (20.02%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -6,14 +6,30 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        def post_order(curr: TreeNode) -> None:
-            if not curr:
-                return
-            
-            post_order(curr.left)
-            post_order(curr.right)
-            ans.append(curr.val)
-            
+        # List to store the result of postorder traversal
         ans = []
-        post_order(root)
+        
+        # Stack to facilitate the traversal of nodes
+        traversal_stack = []
+        curr_node = root
+        
+        # Traverse the tree while there are nodes to process
+        while curr_node or traversal_stack:
+            if curr_node:
+                # Add current node's value to result list before going to its children
+                ans.append(curr_node.val)
+                
+                # Push current node onto the stack
+                traversal_stack.append(curr_node)
+                
+                # Move to the right child
+                curr_node = curr_node.right
+                
+            else:
+                # Pop the node from the stack and move to its left child
+                curr_node = traversal_stack.pop()
+                curr_node = curr_node.left
+                
+        # Reverse the answer list to get the correct postorder sequence
+        ans.reverse()
         return ans
