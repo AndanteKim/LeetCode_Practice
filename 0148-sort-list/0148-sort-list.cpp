0a7fabb55c827387d766e756db9fc073edbda7e9
Andrew Kim <ksb0724@utexas.edu>
2024-08-26 02:35:02 +0900

    Time: 141 ms (47.16%), Space: 62.9 MB (62.46%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,52 +10,96 @@
  */
 class Solution {
 private:
-    ListNode* getMid(ListNode* head){
-        ListNode* midPrev = nullptr;
+    ListNode* tail = new ListNode();
+    ListNode* nextSublist = new ListNode();
+    
+    int getCount(ListNode* head){
+        int count = 0;
         
-        while (head && head -> next){
-            midPrev = (midPrev == nullptr)? head : midPrev -> next;
-            head = head -> next -> next; 
+        while (head){
+            ++count;
+            head = head -> next;
+        }
+        
+        return count;
+    }
+    
+    ListNode* split(ListNode* start, int size){
+        ListNode* midPrev = start;
+        ListNode* end = start -> next;
+        
+        // Use fast and slow approach to find middle and end of the second linked list
+        for (int index = 1; index < size && (midPrev -> next || end -> next); ++index){
+            if (end -> next)
+                end = (end -> next -> next)? end -> next -> next : end -> next;
+            
+            if (midPrev -> next)
+                midPrev = midPrev -> next;
         }
         
         ListNode* mid = midPrev -> next;
+        nextSublist = end -> next;
         midPrev -> next = nullptr;
+        end -> next = nullptr;
+        
+        // return the start of the second linked list
         return mid;
     }
     
-    ListNode* merge(ListNode* list1, ListNode* list2){
-        ListNode *dummy = new ListNode(), *curr = dummy;
+    void merge(ListNode* list1, ListNode* list2){
+        ListNode* dummy = new ListNode();
+        ListNode* newTail = dummy;
+        
         while (list1 && list2){
             if (list1 -> val < list2 -> val){
-                curr -> next = list1;
+                newTail -> next = list1;
                 list1 = list1 -> next;
             }
             else{
-                curr -> next = list2;
+                newTail -> next = list2;
                 list2 = list2 -> next;
             }
             
-            curr = curr -> next;
+            newTail = newTail -> next;
         }
-        curr -> next = list1? list1 : list2;
         
-        return dummy -> next;
+        newTail -> next = (list1)? list1 : list2;
+        
+        // Traverse till the end of merged list to get the newTail
+        while (newTail -> next)
+            newTail = newTail -> next;
+        
+        // Link the old tail with the head of merged list
+        tail -> next = dummy -> next;
+        
+        // Update the old tail with the new tail of merged list
+        tail = newTail;
     }
     
 public:
     ListNode* sortList(ListNode* head) {
-        // Top down style
-        // Base case: If the head or the entire list is none, return the head.
-        if (!head || !head -> next)
-            return head;
+        if (!(head && head -> next)) return head;
         
-        // Get the middle node
-        ListNode* mid = getMid(head);
+        int n = getCount(head);
+        ListNode* dummy = new ListNode(), *start = head;
         
-        // Split the list to left and right and sort them
-        ListNode* left = sortList(head), *right = sortList(mid);
+        for (int size = 1; size < n; size *= 2){
+            tail = dummy;
+            
+            while (start){
+                if (!start -> next){
+                    tail -> next = start;
+                    break;
+                }
+                
+                ListNode* mid = split(start, size);
+                merge(start, mid);
+                start = nextSublist;
+            }
+            
+            start = dummy -> next;
+        }
         
-        // Merge the sorted lists
-        return merge(left, right);
+        return dummy -> next;
     }
 };
