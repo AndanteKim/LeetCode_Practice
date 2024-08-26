9c01d86c8a89f607a0334b15c92789ca07fb1e7a
Andrew Kim <ksb0724@utexas.edu>
2024-08-26 10:11:14 +0900

    Time: 45 ms (58.87%), Space: 18.2 MB (77.39%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -8,16 +8,26 @@ class Node:
 
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        def dfs(curr: 'Node') -> List[int]:
-            if not curr:
-                return []
+        # Base case: If the root is None, return the empthy list
+        if not root:
+            return []
+        
+        # Stack for traversal and stack to reverse the order
+        node_stack, reverse_stack = [root], []
+        
+        # Traverse the tree using the node_stack
+        while node_stack:
+            curr = node_stack.pop()
+            reverse_stack.append(curr)
             
-            ans = []
+            # Push all the children of the current node to node_stack
             for child in curr.children:
-                ans += dfs(child)
-            
+                node_stack.append(child)
+                
+        ans = []
+        # Pop nodes from reverse_stack and add their values to the answer list
+        while reverse_stack:
+            curr = reverse_stack.pop()
             ans.append(curr.val)
-            return ans
-        
-        
-        return dfs(root)
+                
+        return ans
