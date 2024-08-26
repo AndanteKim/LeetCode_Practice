0a58e7ae370e8c590ef28b5903676f9be3432ee2
Andrew Kim <ksb0724@utexas.edu>
2024-08-26 10:25:38 +0900

    Time: 39 ms (89.82%), Space: 18.1 MB (77.39%) - LeetHub

--- 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.py
@@ -8,26 +8,26 @@ class Node:
 
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        # Base case: If the root is None, return the empthy list
-        if not root:
-            return []
-        
-        # Stack for traversal and stack to reverse the order
-        node_stack, reverse_stack = [root], []
-        
-        # Traverse the tree using the node_stack
-        while node_stack:
-            curr = node_stack.pop()
-            reverse_stack.append(curr)
-            
-            # Push all the children of the current node to node_stack
-            for child in curr.children:
-                node_stack.append(child)
-                
         ans = []
-        # Pop nodes from reverse_stack and add their values to the answer list
-        while reverse_stack:
-            curr = reverse_stack.pop()
-            ans.append(curr.val)
+        
+        # If the root is None, return the empty list
+        if not root:
+            return ans
+        
+        node_stack = [(root, False)]
+        
+        while node_stack:
+            curr_node, is_visited = node_stack.pop()
+            
+            if is_visited:
+                # If the node has been visited, add its value to the result
+                ans.append(curr_node.val)
+            else:
+                # Mark the current node as visited and push it back to the stack
+                node_stack.append((curr_node, True))
                 
+                # Push all children to the stack in reverse order
+                for i in range(len(curr_node.children) - 1, -1, -1):
+                    node_stack.append((curr_node.children[i], False))
+                    
         return ans
