d46de2ff0c3aa4417c308d5c5d75ad926024f776
Andrew Kim <ksb0724@utexas.edu>
2024-08-29 16:26:03 +0900

    Time: 3 ms (52.78%), Space: 12.6 MB (27.78%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.cpp
+++ 0666-path-sum-iv/0666-path-sum-iv.cpp
@@ -11,6 +11,7 @@ public:
         for (int num : nums)
             tree[num / 10] = num % 10;
         
+        // Initialize the BFS queue and start with the root node.
         queue<pair<int, int>> queue;
         queue.push({nums[0] / 10, 0});
         int ans = 0;
@@ -19,16 +20,21 @@ public:
             auto [rootPosition, prevSum] = queue.front(); queue.pop();
             int level = rootPosition / 10, position = rootPosition % 10;
             int currSum = prevSum + tree[rootPosition];
+            
+            // Find the left and right child coordinates
             int left = (level + 1) * 10 + 2 * position - 1, right = (level + 1) * 10 + 2 * position;
             
+            // If it's a leaf node (no left and right children), add current sum to answer
             if (!(tree.count(left) || tree.count(right))){
                 ans += currSum;
                 continue;
             }
             
+            // Add the left child to the queue if it exists
             if (tree.count(left))
                 queue.push({left, currSum});
             
+            // Add the right child to the queue if it exists
             if (tree.count(right))
                 queue.push({right, currSum});
         }
