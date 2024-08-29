ecb81fbbb3abcc9d36556e67b9333a8557076da9
Andrew Kim <ksb0724@utexas.edu>
2024-08-29 16:24:28 +0900

    Time: 6 ms (14.81%), Space: 12.5 MB (31.48%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.cpp
+++ 0666-path-sum-iv/0666-path-sum-iv.cpp
@@ -1,37 +1,38 @@
 class Solution {
-private:
-    int dfs(int rootPosition, int preSum, unordered_map<int, int>& tree){
-        // Find the level and position values from the root position.
-        int level = rootPosition / 10, position = rootPosition % 10;
-        
-        // Find out the left child and right child positions of the tree.
-        int left = (level + 1) * 10 + 2 * position - 1, right = (level + 1) * 10 + 2 * position;
-        int currSum = preSum + tree[rootPosition];
-        
-        // If the both left and right child don't exist, return.
-        if (!(tree.count(left) || tree.count(right)))
-            return currSum;
-        
-        // Otherwise, iterate through the left and right children recursively
-        // using depth first search
-        int leftSum = tree.count(left)? dfs(left, currSum, tree) : 0;
-        int rightSum = tree.count(right)? dfs(right, currSum, tree) : 0;
-        
-        return leftSum + rightSum;
-    }
-    
 public:
     int pathSum(vector<int>& nums) {
+        // Base case
         if (nums.empty())
             return 0;
         
+        // Store the data in a hashmap, with the coordinates as the key and the node value as the value.
         unordered_map<int, int> tree;
         
-        // Store the data in a hashmap, with the coordinates as the key and the
-        // node value as the value.
         for (int num : nums)
-            tree[num / 10] = num % 10; 
+            tree[num / 10] = num % 10;
         
-        return dfs(nums[0] / 10, 0, tree);
+        queue<pair<int, int>> queue;
+        queue.push({nums[0] / 10, 0});
+        int ans = 0;
+        
+        while (!queue.empty()){
+            auto [rootPosition, prevSum] = queue.front(); queue.pop();
+            int level = rootPosition / 10, position = rootPosition % 10;
+            int currSum = prevSum + tree[rootPosition];
+            int left = (level + 1) * 10 + 2 * position - 1, right = (level + 1) * 10 + 2 * position;
+            
+            if (!(tree.count(left) || tree.count(right))){
+                ans += currSum;
+                continue;
+            }
+            
+            if (tree.count(left))
+                queue.push({left, currSum});
+            
+            if (tree.count(right))
+                queue.push({right, currSum});
+        }
+        
+        return ans;
     }
 };
