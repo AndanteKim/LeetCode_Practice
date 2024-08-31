901691f5aa53d12eb18e4c02fc79e937a035bf06
Andrew Kim <ksb0724@utexas.edu>
2024-08-31 22:13:29 +0900

    Time: 2886 ms (5.42%), Space: 37.7 MB (37.32%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,57 +1,76 @@
+# Union-Find class
+class UnionFind:
+    def __init__(self, n: int):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+        
+    # Find the root of element 'u', using the path-compression technique.
+    def find(self, u: int) -> int:
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+    
+    # Union two components of elements 'u' and 'v' respectively based on their ranks.
+    def union(self, u: int, v: int) -> None:
+        root_u, root_v = self.find(u), self.find(v)
+        
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+                
 class Solution:
+    # Directions in which we can traverse inside the grids
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    
     # Helper method to check if the cell at the position (x, y) in the 'grid'
     # is a land cell.
     def is_cell_land(self, x: int, y: int, grid: List[List[int]]) -> bool:
         return grid[x][y] == 1
     
-    # Traverse all cells of island starting at position (x, y) in 'grid2',
-    # and check this island is a sub-island in 'grid1'.
-    def is_sub_island(self, x: int, y: int, grid1: List[List[int]], grid2: List[List[int]], visited: List[List[bool]]) -> bool:
-        is_sub_island = True
-        
-        # Push the starting cell in the queue and mark it as visited.
-        queue = deque([(x, y)])
-        visited[x][y] = True
-        
-        # Traverse on all cells using the bread-first search method.
-        while queue:
-            curr_x, curr_y = queue.popleft()
-            
-            # If the current position cell isn't a land cell in 'grid1',
-            # then the current island can't be a sub-island.
-            if not self.is_cell_land(curr_x, curr_y, grid1):
-                is_sub_island = False
-            
-            for dx, dy in self.directions:
-                next_x, next_y = curr_x + dx, curr_y + dy
-                
-                # If the next cell is inside 'grid2', is never visited and
-                # is a land cell, then we traverse to the next cell.
-                if 0 <= next_x < self.m and 0 <= next_y < self.n and not visited[next_x][next_y] and \
-                self.is_cell_land(next_x, next_y, grid2):
-                    # Push the next cell in the queue and mark it as visited.
-                    visited[next_x][next_y] = True
-                    queue.append((next_x, next_y))
-                    
-        return is_sub_island
+    # Helper method to convert (x, y) position to a 1-dimensional index.
+    def convert_to_index(self, x: int, y: int) -> int:
+        return x * self.cols + y
     
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        self.m, self.n, ans = len(grid2), len(grid2[0]), 0
+        self.rows, self.cols = len(grid2), len(grid2[0])
+        uf = UnionFind(self.rows * self.cols)
         
-        # Directions in which we can traverse inside the grids
-        self.directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        visited = [[False] * self.n for _ in range(self.m)] 
+        # Traverse each land cell of 'grid2'.
+        for x in range(self.rows):
+            for y in range(self.cols):
+                if self.is_cell_land(x, y, grid2):
+                    # Union adjacent land cells with the current land cell.
+                    for dx, dy in self.directions:
+                        next_x, next_y = x + dx, y + dy
+                        if 0 <= next_x < self.rows and 0 <= next_y < self.cols and self.is_cell_land(next_x, next_y, grid2):
+                            uf.union(self.convert_to_index(x, y), self.convert_to_index(next_x, next_y))
+                            
+        # Traverse 'grid2' land cells and mark that cell's root as not a sub-island
+        # if the land cell isn't present at the respective position in 'grid1'.
+        is_sub_island = [True] * (self.rows * self.cols)
         
-        # Iterate on each cell in 'grid2'
-        for x in range(self.m):
-            for y in range(self.n):
-                # If cell at the position (x, y) in the 'grid2' isn't visited,
-                # is a land cell in 'grid2', and the island
-                # starting from this cell is a sub-island in 'grid1', then we
-                # increment the count of sub-islands.
-                if not visited[x][y] and self.is_cell_land(x, y, grid2) and\
-                self.is_sub_island(x, y, grid1, grid2, visited):
-                        ans += 1
-        
-        # Return total count of sub-islands
-        return ans
+        for x in range(self.rows):
+            for y in range(self.cols):
+                if self.is_cell_land(x, y, grid2) and not self.is_cell_land(x, y, grid1):
+                    root = uf.find(self.convert_to_index(x, y))
+                    is_sub_island[root] = False
+                    
+        # Count all the sub-islands.
+        sub_island_counts = 0
+        for x in range(self.rows):
+            for y in range(self.cols):
+                if self.is_cell_land(x, y, grid2):
+                    root = uf.find(self.convert_to_index(x, y))
+                    if is_sub_island[root]:
+                        sub_island_counts += 1
+                        
+                        # One cell can be the root of multiple land cells, so to
+                        # avoid counting the same island multiple times, mark it as false.
+                        is_sub_island[root] = False
+                        
+        return sub_island_counts
