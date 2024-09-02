fcca9148a8fe0ce4308ef2ecc27922f4ed788147
Andrew Kim <ksb0724@utexas.edu>
2024-09-02 20:00:42 +0900

    Time: 578 ms (51.45%), Space: 30.7 MB (19.92%) - LeetHub

--- 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
+++ 1894-find-the-student-that-will-replace-the-chalk/1894-find-the-student-that-will-replace-the-chalk.py
@@ -1,18 +1,19 @@
 class Solution:
     def chalkReplacer(self, chalk: List[int], k: int) -> int:
-        if not chalk:
-            return 0
+        # Find the sum of all elements.
+        sum = 0
         
-        remain, i, n = k, 0, len(chalk)
-        prefix_sum = [0] * (n + 1)
-
-        for i in range(1, n + 1):
-            prefix_sum[i] = prefix_sum[i - 1] + chalk[i - 1]
+        for i in range(len(chalk)):
+            sum += chalk[i]
+            if sum > k:
+                break
+                
+        # Find modulo of k with sum
+        k %= sum
         
-        remain %= prefix_sum[-1]
-        
-        for i in range(n + 1):
-            if 0 <= remain < prefix_sum[i]:
-                return i - 1
-        
-        return -1
+        for i in range(len(chalk)):
+            if k < chalk[i]:
+                return i
+            k -= chalk[i]
+            
+        return 0
