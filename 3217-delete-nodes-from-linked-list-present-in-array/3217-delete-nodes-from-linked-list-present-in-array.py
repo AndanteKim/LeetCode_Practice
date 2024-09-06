37ab2ac141dbb3c9832fb13dd98eaa3f790aa865
Andrew Kim <ksb0724@utexas.edu>
2024-09-06 13:14:56 +0900

    Time: 739 ms (30.73%), Space: 55 MB (70.63%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.py
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.py
@@ -5,18 +5,15 @@
 #         self.next = next
 class Solution:
     def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
-        # Set hashset and dummy nodes
         nums = set(nums)
-        dummy = curr = ListNode()
-        curr.next = head
+        dummy = ListNode(next = head)
+        curr = dummy
         
-        while curr:
-            # Delete nodes if the next val is in nums
-            while curr.next and curr.next.val in nums:
+        while curr.next:
+            if curr.next.val in nums:
                 curr.next = curr.next.next
-            
-            curr = curr.next
-            
+            else:
+                curr = curr.next
         
         return dummy.next
         
