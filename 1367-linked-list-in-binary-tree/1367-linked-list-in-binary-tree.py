4f5e058b985a898e62da0e8282fbf3437bc1f03e
Andrew Kim <ksb0724@utexas.edu>
2024-09-07 18:21:28 +0900

    Time: 54 ms (96.36%), Space: 17.3 MB (63.38%) - LeetHub

--- 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
+++ 1367-linked-list-in-binary-tree/1367-linked-list-in-binary-tree.py
@@ -11,27 +11,37 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        if not root:
+        # Build the pattern and prefix table from the linked list
+        pattern, prefix_table = [head.val], [0]
+        pattern_idx = 0
+        head = head.next
+        
+        while head:
+            while pattern_idx > 0 and head.val != pattern[pattern_idx]:
+                pattern_idx = prefix_table[pattern_idx - 1]
+                
+            pattern_idx += 1 if head.val == pattern[pattern_idx] else 0
+            pattern.append(head.val)
+            prefix_table.append(pattern_idx)
+            head = head.next
+            
+        # Perform DFS to search for the pattern in the tree
+        return self._search_in_tree(root, 0, pattern, prefix_table)
+    
+    def _search_in_tree(self, node: TreeNode, pattern_idx, pattern: List[int], prefix_table: List[int]) -> bool:
+        if not node:
             return False
         
-        # Check the current node and all its descendants
-        main = self.dfs(root, head)
+        while pattern_idx > 0 and node.val != pattern[pattern_idx]:
+            pattern_idx = prefix_table[pattern_idx - 1]
+        pattern_idx += 1 if node.val == pattern[pattern_idx] else 0
         
-        # Check all paths from the left and right children of the root
-        left = self.isSubPath(head, root.left)
-        right = self.isSubPath(head, root.right)
-        
-        return main or left or right
-    
-    def dfs(self, node: TreeNode, head: ListNode) -> bool:
-        if not head:
+        # Check if the pattern is fully matched
+        if pattern_idx == len(pattern):
             return True
         
-        if not node or node.val != head.val:
-            return False
+        # Recursively search left and right subtrees
+        return self._search_in_tree(node.left, pattern_idx, pattern, prefix_table) or \
+    self._search_in_tree(node.right, pattern_idx, pattern, prefix_table)
         
-        # Continue searching in both left and right subtrees
-        left = self.dfs(node.left, head.next)
-        right = self.dfs(node.right, head.next)
         
-        return left or right
