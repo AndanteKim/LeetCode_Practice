38ecd4516156ceaf969ca9c949c5d90655156d38
Andrew Kim <ksb0724@utexas.edu>
2024-09-08 17:31:42 +0900

    Time: 408 ms (90.50%), Space: 24.4 MB (13.54%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.py
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -1,23 +1,14 @@
 class Solution:
     def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
-        slots1.sort()
-        slots2.sort()
+        # Build up a heap containing time slots last longer than duration
+        timeslots = list(filter(lambda x: x[1] - x[0] >= duration, slots1 + slots2))
+        heapify(timeslots)
         
-        i, j = 0, 0
-        m, n = len(slots1), len(slots2)
-        
-        while i < m and j < n:
-            # Find the boundaries of the intersection, or the common slot
-            intersect_r = min(slots1[i][1], slots2[j][1])
-            intersect_l = max(slots1[i][0], slots2[j][0])
+        while len(timeslots) > 1:
+            s1, e1 = heappop(timeslots)
+            s2, e2 = timeslots[0]
             
-            if intersect_r - intersect_l >= duration:
-                return [intersect_l, intersect_l + duration]
+            if e1 >= s2 + duration:
+                return [s2, s2 + duration]
             
-            # Always move the one ending earlier
-            if slots1[i][1] < slots2[j][1]:
-                i += 1
-            else:
-                j += 1
-                
         return []
