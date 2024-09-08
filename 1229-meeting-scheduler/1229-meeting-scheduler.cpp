1fd59ed028684d35fa2b3ef5025b8747875a6207
Andrew Kim <ksb0724@utexas.edu>
2024-09-08 17:36:04 +0900

    Time: 90 ms (96.83%), Space: 44 MB (66.67%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.cpp
+++ 1229-meeting-scheduler/1229-meeting-scheduler.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
     vector<int> minAvailableDuration(vector<vector<int>>& slots1, vector<vector<int>>& slots2, int duration) {
-        sort(slots1.begin(), slots1.end());
-        sort(slots2.begin(), slots2.end());
+        // Build up a min heap containing time slots lasting longer than duration
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
         
-        int i = 0, j = 0, m = slots1.size(), n = slots2.size();
+        for (const auto& slot1 : slots1){
+            if (slot1[1] - slot1[0] >= duration)
+                minHeap.push({slot1[0], slot1[1]});
+        }
         
-        while (i < m && j < n){
-            int intersectRight = min(slots1[i][1], slots2[j][1]);
-            int intersectLeft = max(slots1[i][0], slots2[j][0]);
+        for (const auto& slot2 : slots2){
+            if (slot2[1] - slot2[0] >= duration)
+                minHeap.push({slot2[0], slot2[1]});
+        }
+        
+        while (minHeap.size() > 1){
+            auto [s1, e1] = minHeap.top(); minHeap.pop();
+            auto [s2, e2] = minHeap.top();
             
-            // Find the boundaries of the intersection, or the common slot
-            if (intersectRight - intersectLeft >= duration)
-                return vector<int>{intersectLeft, intersectLeft + duration};
-            
-            // Always move the one ending earlier
-            if (slots1[i][1] < slots2[j][1])
-                ++i;
-            else
-                ++j;
+            if (e1 >= s2 + duration)
+                return vector<int>{s2, s2 + duration};
         }
         
         return vector<int>{};
