46f17fca701ff4c7df2e51f0b1b96e781dce175b
Andrew Kim <ksb0724@utexas.edu>
2024-09-10 12:39:52 +0900

    Time: 38 ms (64.33%), Space: 35.4 MB (82.75%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.cpp
@@ -9,27 +9,42 @@
  * };
  */
 class Solution {
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) {
+        // If the list contains only one node, return the head as no insertion
+        // is needed
+        if (head -> next == nullptr) return head;
+
+        // Initialize pointers to traverse the list
+        ListNode* node1 = head;
+        ListNode* node2 = head -> next;
+
+        // Traverse the linked list
+        while (node2 != nullptr) {
+            int gcdValue = calculateGCD(node1->val, node2->val);
+            ListNode* gcdNode = new ListNode(gcdValue);
+
+            // Insert the GCD node between node1 and node2
+            node1 -> next = gcdNode;
+            gcdNode -> next = node2;
+
+            // Move to the next pair of nodes
+            node1 = node2;
+            node2 = node2 -> next;
+        }
+
+        return head;
+    }
+
 private:
-    int gcd(int a, int b){
-        while (b > 0){
+    // Helper method to calculate the greatest common divisor using the
+    // Euclidean algorithm
+    int calculateGCD(int a, int b) {
+        while (b > 0) {
             int temp = b;
             b = a % b;
             a = temp;
         }
-        
         return a;
-    }
-    
-public:
-    ListNode* insertGreatestCommonDivisors(ListNode* head) {
-        ListNode* root = head;
-        
-        while (root && root -> next){
-            root -> next = new ListNode(gcd(max(root -> val, root -> next -> val), min(root -> val, root -> next -> val)), \
-                                       root -> next);
-            root = root -> next -> next;
-        }
-        
-        return head;
     }
 };
