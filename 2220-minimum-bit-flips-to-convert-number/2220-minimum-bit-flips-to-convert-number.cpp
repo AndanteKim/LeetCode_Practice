33b4261a23f59634b65507037aa3f0a8816d1b55
Andrew Kim <ksb0724@utexas.edu>
2024-09-11 13:18:44 +0900

    Time: 0 ms (100.00%), Space: 7.7 MB (44.87%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -1,18 +1,6 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        // XOR to find differing bits
-        int xorRes = start ^ goal, ans = 0;
-        
-        // Count the number of 1s in xorRes (differing bits)
-        while (xorRes > 0){
-            // Increment if the last bit is 1
-            ans += xorRes & 1;
-            
-            // Shift right to process the next bit
-            xorRes >>= 1;
-        }
-        
-        return ans;
+        return __builtin_popcount(start ^ goal);
     }
 };
