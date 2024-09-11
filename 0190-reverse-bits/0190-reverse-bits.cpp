ce2c5cb1a2f956a2d943d5346f6fb50cc9842b86
Andrew Kim <ksb0724@utexas.edu>
2024-09-11 20:09:16 +0900

    Time: 0 ms (100.00%), Space: 8 MB (6.54%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,12 +1,22 @@
 class Solution {
+private:
+    int reverseByte(int byte, map<uint32_t, uint32_t>& cache){
+        if (cache.count(byte))
+            return cache[byte];
+        
+        return cache[byte] = (byte * 0x0202020202 & 0x010884422010) % 1023;
+    }
+    
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
-        int power = 31;
+        int power = 24;
+        map<uint32_t, uint32_t> cache;
         
         while (n > 0){
-            ans += (n & 1) * pow(2, power--);
-            n >>= 1;
+            ans += reverseByte(n & (0xFF), cache) << power;
+            n >>= 8;
+            power -= 8;
         }
         
         return ans;
