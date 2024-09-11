6f1ae7b2101fc3ffea82909c4cf1a8782936de5d
Andrew Kim <ksb0724@utexas.edu>
2024-09-11 13:18:06 +0900

    Time: 35 ms (57.21%), Space: 16.6 MB (26.96%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.py
@@ -1,12 +1,3 @@
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        # Xor to find differing bits
-        ans, xor_res = 0, start ^ goal
-        
-        # Brian Kernighans algorithm to count 1s
-        while xor_res:
-            # Clear the lowest set bit
-            xor_res &= xor_res - 1
-            ans += 1
-            
-        return ans
+        return bin(start ^ goal).count('1')
