f1622bbea54837e9c25f1f89cd3019fe914c7f9e
Andrew Kim <ksb0724@utexas.edu>
2024-09-11 19:41:34 +0900

    Time: 0 ms (100.00%), Space: 7.5 MB (18.06%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,14 +2,11 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
+        int power = 31;
         
         for (int i = 0; i < 32; ++i){
-            if (n & 1)
-                ans |= 1;
-            
+            ans += (n & 1) * pow(2, power--);
             n >>= 1;
-            if (i < 31)
-                ans <<= 1;
         }
         
         return ans;
