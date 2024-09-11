d84d1ec81e80243cb6a6f28dd852af007c534c22
Andrew Kim <ksb0724@utexas.edu>
2024-09-11 20:50:07 +0900

    Time: 36 ms (58.22%), Space: 16.5 MB (81.93%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,16 +1,9 @@
-import functools
-
 class Solution:
     def reverseBits(self, n: int) -> int:
-        ans, power = 0, 24
-        while n:
-            ans += self.reverseByte(n & 0xFF) << power
-            n = n >> 8
-            power -= 8
-            
-        return ans
-    
-    # memoization with decorator
-    @functools.lru_cache(maxsize = 256)
-    def reverseByte(self, byte: int) -> int:
-        return (byte * 0x0202020202 & 0x010884422010) % 1023
+        n = (n >> 16) | (n << 16)
+        n = ((n & 0xFF00FF00) >> 8) | ((n & 0x00FF00FF) << 8)
+        n = ((n & 0xF0F0F0F0) >> 4) | ((n & 0x0F0F0F0F) << 4)
+        n = ((n & 0xCCCCCCCC) >> 2) | ((n & 0x33333333) << 2)
+        n = ((n & 0xAAAAAAAA) >> 1) | ((n & 0x55555555) << 1)
+        
+        return n
