f6fc2c0a630815952bcf305eff2beff80d0b302f
Andrew Kim <ksb0724@utexas.edu>
2024-09-12 17:12:14 +0900

    Time: 40 ms (84.99%), Space: 33.9 MB (87.14%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
-        int ans = 0;
-        unordered_set<char> allowedChars(allowed.begin(), allowed.end());
+        // Create a bitmask representing the allowed characters
+        int allowedBits = 0, consistentCount = 0;
         
+        // Set the corresponding bit for each character in allowed
+        for (const char& c : allowed)
+            allowedBits |= (1 << (c - 97));
+        
+        // Iterate through each word in the words list
         for (const string& word : words){
-            bool flag = true;
+            bool isConsistent = true;
+            
+            // Check each character in the word
             for (const char& c : word){
-                if (!allowedChars.count(c)){
-                    flag = false;
+                // Calculate the bit position for the curretn character
+                bool bit = (allowedBits >> (c - 97)) & 1;
+                
+                // If the bit is 0, the character isn't allowed.
+                if (!bit){
+                    isConsistent = false;
                     break;
                 }
             }
             
-            if (flag) ++ans;
+            // If the word is consistent, increment the count
+            if (isConsistent)
+                ++consistentCount;
         }
         
-        return ans;
+        return consistentCount;
     }
 };
