c0ff1ce7149ba9c93ef410592a1e4b226392ffdd
Andrew Kim <ksb0724@utexas.edu>
2024-09-12 21:04:11 +0900

    Time: 8 ms (50.63%), Space: 10.3 MB (63.63%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -3,7 +3,7 @@ public:
     int rangeBitwiseAnd(int left, int right) {
         // Brian Kernighan's algorithm
         while (left < right){
-            // Turn off the rightmost bits
+            // Turn off the rightmost 1-bit
             right &= (right - 1);
         }
         
