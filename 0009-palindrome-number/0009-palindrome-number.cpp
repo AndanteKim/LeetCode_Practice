779e83e728e1c0b3ba5e6b18e773400c0708af98
Andrew Kim <ksb0724@utexas.edu>
2024-09-13 16:08:21 +0900

    Time: 0 ms (100.00%), Space: 8.5 MB (37.98%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         // Special cases:
-        // When x < 0.  x isn't a palindrome. 
+        // When x < 0, x isn't a palindrome. 
         // Also, if the last digit of the number is 0, in order to be a palindrome,
         // the first digit of the number also needs to be 0. Only 0 satisfy this property.
         if (x < 0 || (x % 10 == 0 && x != 0))
