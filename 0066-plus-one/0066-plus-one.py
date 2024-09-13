1780444182a2b9d855e17e8d6938fce9d08cde0c
Andrew Kim <ksb0724@utexas.edu>
2024-09-13 16:29:29 +0900

    Time: 35 ms (68.34%), Space: 16.4 MB (91.89%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,10 +1,13 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        # In-place method and use carry for flipping digits
         carry = 1
         
+        # Move along the input array starting from the end
         for i in range(len(digits) - 1, -1, -1):
             carry, digits[i] = (digits[i] + carry) // 10, (digits[i] + carry) % 10
-            
+        
+        # If carry is larger than 0, then insert 1 at the digits' begin.
         if carry:
             digits = [1] + digits
             
