cd8be8c584e8182783997dbe393f3afc9f614c8a
Andrew Kim <ksb0724@utexas.edu>
2024-09-13 16:26:23 +0900

    Time: 0 ms (100.00%), Space: 10.5 MB (50.30%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -4,12 +4,14 @@ public:
         // In-place
         int carry = 1;
         
+        // Move along the input array starting from the end
         for (int i = digits.size() - 1; i >= 0; --i){
             int temp = (digits[i] + carry) % 10;
             carry = (digits[i] + carry) / 10;
             digits[i] = temp;
         }
         
+        // If carry is larger than 0, then insert 1 at the digits' begin
         if (carry)
             digits.insert(digits.begin(), 1);
         
