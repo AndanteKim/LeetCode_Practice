cd379cd5d7ba5bb57486880a4d021ccb38dfe768
Andrew Kim <ksb0724@utexas.edu>
2024-09-13 10:40:04 +0900

    Time: 60 ms (61.22%), Space: 42.1 MB (53.29%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
-        // Prefix Sum
         int n = arr.size();
-        vector<int> prefixXor(n + 1);
-        
-        // Store its prefix xor sum
-        for (int i = 0; i < n; ++i)
-            prefixXor[i + 1] = prefixXor[i] ^ arr[i];
-            
-        vector<int> ans;
-        // sum of Xor = prefixXor[right + 1] ^ prefixXor[left]
-        for (int i = 0; i < queries.size(); ++i){
-            int left = queries[i][0], right = queries[i][1];
-            ans.push_back(prefixXor[right + 1] ^ prefixXor[left]);
+        vector<int> prefixXOR(n + 1, 0);
+
+        // Build prefix XOR array
+        for (int i = 0; i < n; i++) {
+            prefixXOR[i + 1] = prefixXOR[i] ^ arr[i];
         }
-        
-        return ans;
+
+        vector<int> result;
+        // Process each query using prefix XOR
+        for (const auto& q : queries) {
+            result.push_back(prefixXOR[q[1] + 1] ^ prefixXOR[q[0]]);
+        }
+        return result;
     }
 };
