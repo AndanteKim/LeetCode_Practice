241dbfa017cd25a96211a27c33e0407d1a26ae9a
Andrew Kim <ksb0724@utexas.edu>
2024-09-13 15:51:19 +0900

    Time: 47 ms (83.48%), Space: 16.6 MB (5.83%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,11 +1,20 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
+        # Special cases:
+        # 1 ) When x < 0, x isn't a palindrome.
+        # 2 ) If the last digit of the number is 0, in order to be a palindrome,
+        # the first digit of the number also needs to be 0.
+        # Only 0 satisfy this property.
+        if x < 0 or (x % 10 == 0 and x != 0):
             return False
         
-        elems = []
-        while x > 0:
-            elems.append(x % 10)
+        reverted_num = 0
+        
+        while x > reverted_num:
+            reverted_num = reverted_num * 10 + x % 10
             x //= 10
             
-        return elems == elems[::-1]
+        # When the length an odd number, we can get rid of the middle digit by reverted_num // 10
+        # For example, when the input is 12321, at the end of the while loop we get x = 12, reverted_num = 123,
+        # since the middle digit doesn't matter in palindrome(it'll always equal to itself), we can simply get rid of it.
+        return x == reverted_num or x == reverted_num // 10
