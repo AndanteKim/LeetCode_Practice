04bf9fea8cfb767b04c8efde05ee9694457ab972
Andrew Kim <ksb0724@utexas.edu>
2024-09-14 17:06:49 +0900

    Time: 108 ms (46.49%), Space: 85 MB (34.21%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int longestSubarray(vector<int>& nums) {
-        int ans = 0, k = 0, n = nums.size(), left = 0;
-        unordered_map<int, int> seen;
+        // Longest consecutive sequence of the maximum value
+        // k := maximum number
+        int ans = 0, k = 0, n = nums.size(), currentStreak = 0;
         
-        for (int right = 0; right < n; ++right){
-            k = max(k, nums[right]);
-            while (left < right && (nums[left] & nums[right]) < k)
-                ++left;
-            
-            if (k == nums[right]){
-                ans = max(seen[k], right - left + 1);
-                seen[k] = max(seen[k], right - left + 1);
+        for (int num : nums){
+            if (k < num){
+                k = num;
+                ans = currentStreak = 0;
             }
+                
+            if (k == num)
+                ++currentStreak;
+            else
+                currentStreak = 0;
+                
+            ans = max(ans, currentStreak);
         }
         
         return ans;
