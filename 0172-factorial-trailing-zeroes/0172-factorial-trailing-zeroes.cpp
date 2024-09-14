1259e8887e39cf3883500bfa6eb9a52987035e13
Andrew Kim <ksb0724@utexas.edu>
2024-09-14 22:33:03 +0900

    Time: 4 ms (31.10%), Space: 7.9 MB (38.58%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
-        // Intialize zero count
-        int zeroCount = 0;
+        int currentMultiple = 5, zeroCount = 0;
         
-        // Start from 5 until n with an increment of 5 at each iteration
-        for (int i = 5; i <= n; i += 5){
-            // Set current factor as i at each iteration
-            int curr = i;
-            // Until the current factor is divisible by 5, keep incrementing the zero count
-            while (curr % 5 == 0){
-                ++zeroCount;
-                curr /= 5;
-            }
+        while (n >= currentMultiple){
+            zeroCount += n / currentMultiple;
+            currentMultiple *= 5;
         }
         
         return zeroCount;
