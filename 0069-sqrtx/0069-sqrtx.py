96f27560106e3fc9e09440a6788d7b7bd642b245
Andrew Kim <ksb0724@utexas.edu>
2024-09-16 04:23:50 +0900

    Time: 37 ms (72.05%), Space: 16.7 MB (19.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,19 +1,10 @@
+from math import e, log
+
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # Binary search
         if x < 2:
             return x
         
-        left, right = 0, x
-        
-        while left <= right:
-            mid = (left + right) >> 1
-            
-            if mid * mid < x:
-                left = mid + 1
-            elif mid * mid > x:
-                right = mid - 1
-            else:
-                return mid
-                
-        return right
+        left = int(e ** (0.5 * log(x)))
+        right = left + 1
+        return left if right * right > x else right
