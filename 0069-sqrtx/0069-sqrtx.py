b77b7ad74f4d3846ef096aae89f04417f66ae15d
Andrew Kim <ksb0724@utexas.edu>
2024-09-16 04:20:52 +0900

    Time: 30 ms (95.24%), Space: 16.4 MB (65.65%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,14 +1,19 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
         # Binary search
+        if x < 2:
+            return x
+        
         left, right = 0, x
         
         while left <= right:
             mid = (left + right) >> 1
             
-            if mid * mid <= x:
+            if mid * mid < x:
                 left = mid + 1
-            else:
+            elif mid * mid > x:
                 right = mid - 1
+            else:
+                return mid
                 
         return right
