4d0b986a78dfa574329327ed9756ac06d8d7ff89
Andrew Kim <ksb0724@utexas.edu>
2024-09-16 04:02:49 +0900

    Time: 51 ms (100.00%), Space: 39 MB (92.05%) - LeetHub

--- 1257-smallest-common-region/1257-smallest-common-region.cpp
+++ 1257-smallest-common-region/1257-smallest-common-region.cpp
@@ -1,24 +1,52 @@
 class Solution {
+private:
+    // Function to return an array representing the path from the root node
+    // to the current node.
+    void fetchToPath(string& currNode, unordered_map<string, string>& childParentMap, vector<string>& path){
+        // Start by adding the current node to the path.
+        path.push_back(currNode);
+        
+        // Traverse upwards through the tree by finding the parent of the current node.
+        // Continue until the root node is reached.
+        while (childParentMap.count(currNode)){
+            string parentNode = childParentMap[currNode];
+            path.push_back(parentNode);
+            currNode = parentNode;
+        }
+        
+        reverse(path.begin(), path.end());
+    }
+    
 public:
     string findSmallestRegion(vector<vector<string>>& regions, string region1, string region2) {
-        // Simplify LCA with time complexity: O(n) where n is the total regions, space complexity: O(n)
-        unordered_map<string, string> parents;
-        for (const auto& region : regions){
-            for (int i = 1; i < region.size(); ++i)
-                parents[region[i]] = region[0];
+        // Hash map to store (child -> parent) relationships for each region.
+        unordered_map<string, string> childParentMap;
+        
+        // Populate the 'childParentMap' using the provided 'regions' list.
+        for (const auto& currRegion : regions){
+            for (int i = 1; i < currRegion.size(); ++i){
+                childParentMap[currRegion[i]] = currRegion[0];
+            }
         }
         
-        string r1 = region1;
-        unordered_set<string> ancestryHistory;
-        ancestryHistory.insert(region1);
-        while (parents.count(r1)){
-            r1 = parents[r1];
-            ancestryHistory.insert(r1);
+        // Store the paths from the root node to 'region1' and 'region2'
+        // nodes in their respective arrays.
+        vector<string> path1, path2;
+        fetchToPath(region1, childParentMap, path1);
+        fetchToPath(region2, childParentMap, path2);
+            
+        int i = 0, j = 0;
+        string lowestCommonAncestor = "";
+        
+        // Traverse both paths simultansiously until the paths diverge.
+        // The last common node is the lowest common ancestor.
+        while (i < path1.size() && j < path2.size() && path1[i] == path2[j]){
+            lowestCommonAncestor = path1[i];
+            ++i;
+            ++j;
         }
         
-        string r2 = region2;
-        while (!ancestryHistory.count(r2))
-            r2 = parents[r2];
-        return r2;
+        // Reuturn the lowest common ancestor of 'region1' and 'region2'.
+        return lowestCommonAncestor;
     }
 };
