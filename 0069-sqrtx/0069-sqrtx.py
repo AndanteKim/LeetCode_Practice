dfbb07b2594e2a7bf508295cd47af5085ea5c1cf
Andrew Kim <ksb0724@utexas.edu>
2024-09-16 04:32:33 +0900

    Time: 37 ms (72.05%), Space: 16.5 MB (19.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,10 +1,8 @@
-from math import e, log
-
 class Solution:
     def mySqrt(self, x: int) -> int:
         if x < 2:
             return x
         
-        left = int(e ** (0.5 * log(x)))
+        left = self.mySqrt(x >> 2) << 1
         right = left + 1
         return left if right * right > x else right
