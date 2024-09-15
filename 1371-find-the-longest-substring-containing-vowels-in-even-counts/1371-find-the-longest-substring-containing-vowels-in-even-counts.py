12234e5c90d921bf1ecf495c8ab497f2e13725f5
Andrew Kim <ksb0724@utexas.edu>
2024-09-15 20:43:12 +0900

    Time: 305 ms (51.53%), Space: 22 MB (79.11%) - LeetHub

---
+++ 1371-find-the-longest-substring-containing-vowels-in-even-counts/1371-find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -0,0 +1,20 @@
+class Solution:
+    def findTheLongestSubstring(self, s: str) -> int:
+        prefixXOR = 0
+        char_map = [0] * 26
+        char_map[ord('a') - 97] = 1
+        char_map[ord('e') - 97] = 2
+        char_map[ord('i') - 97] = 4
+        char_map[ord('o') - 97] = 8
+        char_map[ord('u') - 97] = 16
+        
+        mp, ans = [-1] * 32, 0
+        
+        for i in range(len(s)):
+            prefixXOR ^= char_map[ord(s[i]) - 97]
+            if mp[prefixXOR] == -1 and prefixXOR != 0:
+                mp[prefixXOR] = i
+                
+            ans = max(ans, i - mp[prefixXOR])
+            
+        return ans
