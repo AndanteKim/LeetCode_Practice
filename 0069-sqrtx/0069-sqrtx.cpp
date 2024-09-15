4cb89e9e56643c7082f00dc0f9ca24766e0b2ef1
Andrew Kim <ksb0724@utexas.edu>
2024-09-16 04:26:42 +0900

    Time: 3 ms (42.63%), Space: 8.6 MB (19.86%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,23 +1,9 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        // Binary search
-        if (x < 2)
-            return x;
-        
-        int left = 0, right = x;
-        
-        while (left <= right){
-            int mid = (left + right) >> 1;
-            
-            if ((long)mid * mid > x)
-                right = mid - 1;
-            else if ((long)mid * mid < x)
-                left = mid + 1;
-            else
-                return mid;
-        }
-        
-        return right;
+        if (x < 2) return x;
+        long left = static_cast<long>(exp(0.5 * log(x)));
+        long right = left + 1;
+        return right * right > x ? left : right;
     }
 };
