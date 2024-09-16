fcd857fa7a5da35b9701dfc20e20d8664ab67d24
Andrew Kim <ksb0724@utexas.edu>
2024-09-16 17:14:19 +0900

    Time: 7 ms (95.56%), Space: 17.3 MB (95.56%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int findMinDifference(vector<string>& timePoints) {
-        // Convert input to minutes
-        vector<int> minutes;
+        // Create a bucket array for the times converted to minutes
+        vector<bool> minutes(24 * 60, false);
         
         for (const auto& time : timePoints){
             int h = stoi(time.substr(0, 2)), m = stoi(time.substr(3));
-            minutes.push_back(60 * h + m);
+            int minTime = 60 * h + m;
+            
+            if (minutes[minTime])
+                return 0;
+            
+            minutes[minTime] = true;
+        }
+            
+        int prevIndex = INT_MAX, firstIndex = INT_MAX;
+        int lastIndex = INT_MAX, ans = INT_MAX;
+        
+        // Find differences between adjacent elements in sorted array
+        for (int i = 0; i < 24 * 60; ++i){
+            if (minutes[i]){
+                if (prevIndex != INT_MAX)
+                    ans = min(ans, i - prevIndex);
+                
+                prevIndex = i;
+                if (firstIndex == INT_MAX)
+                    firstIndex = i;
+                lastIndex = i;
+            }
         }
         
-        // Sort times in ascending order
-        sort(minutes.begin(), minutes.end());
-        int n = minutes.size(), ans = INT_MAX;
-        
-        // Find the minimum difference across adjacent elements
-        for (int i = 0; i < n - 1; ++i)
-            ans = min(ans, minutes[i + 1] - minutes[i]);
-        
-        // Consider difference between the last and first element
-        return min(ans, 60 * 24 - minutes.back() + minutes[0]);
+        return min(ans, 60 * 24 - lastIndex + firstIndex);
     }
 };
