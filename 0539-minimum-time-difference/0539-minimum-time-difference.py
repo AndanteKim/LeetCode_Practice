7dd3bfcbaa98906febe6a6a315273ee1635bbec8
Andrew Kim <ksb0724@utexas.edu>
2024-09-16 16:57:56 +0900

    Time: 65 ms (80.36%), Space: 19.6 MB (36.73%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.py
+++ 0539-minimum-time-difference/0539-minimum-time-difference.py
@@ -1,19 +1,27 @@
 class Solution:
     def findMinDifference(self, timePoints: List[str]) -> int:
-        # Convert input to minutes
-        minutes = []
+        # Create buckets array for the times converted to minutes
+        minutes = [False] * (24 * 60)
         
         for time in timePoints:
-            h, m = int(time[:2]), int(time[3:])
-            minutes.append(60 * h + m)
-        
-        # sort times in ascending order
-        minutes.sort()
-        ans, n = float('inf'), len(minutes)
-        
-        # Find the minimum difference across adjacent elements
-        for i in range(n - 1):
-            ans = min(ans, minutes[i + 1] - minutes[i])
+            h, m = map(int, time.split(":"))
+            min_time = 60 * h + m
             
-        # Consider difference between last and first element
-        return min(ans, 24 * 60 - minutes[n - 1] + minutes[0])
+            if minutes[min_time]:
+                return 0
+            minutes[min_time] = True
+            
+        prev_idx, ans = float('inf'), float('inf')
+        first_idx, last_idx = float('inf'), float('inf')
+        
+        # Find differences between adjacent elements in sorted array
+        for i in range(24 * 60):
+            if minutes[i]:
+                if prev_idx != float('inf'):
+                    ans = min(ans, i - prev_idx)
+                prev_idx = i
+                if first_idx == float('inf'):
+                    first_idx = i
+                last_idx = i
+                
+        return min(ans, 24 * 60 - last_idx + first_idx)
