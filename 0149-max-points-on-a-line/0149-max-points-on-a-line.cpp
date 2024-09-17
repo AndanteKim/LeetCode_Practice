9c84e2afc955c0fafed8afc05a9042e2dc61dc0c
Andrew Kim <ksb0724@utexas.edu>
2024-09-17 23:07:25 +0900

    Time: 22 ms (78.81%), Space: 17.4 MB (53.59%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,23 +1,34 @@
 class Solution {
+private:
+    double calculateSlope(vector<int>& p1, vector<int>& p2){
+        int x1 = p1[0], x2 = p2[0];
+        int y1 = p1[1], y2 = p2[1];
+        
+        if (x1 == x2) return DBL_MAX;
+        if (y1 == y2) return 0;
+        
+        return (double) (y2 - y1) / (x2 - x1);
+    }
+    
 public:
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
+        
         if (n == 1)
             return 1;
         
-        int ans = 2;
+        int ans = 0;
         
         for (int i = 0; i < n; ++i){
-            unordered_map<double, int> cnt;
-            for (int j = 0; j < n; ++j){
-                if (i != j)
-                    ++cnt[atan2(points[j][1] - points[i][1], points[j][0] - points[i][0])];
+            unordered_map<double, int> count;
+            for (int j = i + 1; j < n; ++j){
+                double slope = calculateSlope(points[i], points[j]);
+                ++count[slope];
+                ans = max(ans, count[slope]);
             }
-            
-            for (auto& [_, v] : cnt)
-                ans = max(ans, v + 1);
         }
         
-        return ans;
+        // Since points[i] also lies on the line
+        return ans + 1;
     }
 };
