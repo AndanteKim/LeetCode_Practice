a2337fb8a61cc7994e0757d1c323d648f2680060
Andrew Kim <ksb0724@utexas.edu>
2024-09-17 22:59:52 +0900

    Time: 66 ms (76.01%), Space: 16.8 MB (45.39%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,16 +1,29 @@
 class Solution:
+    def calculate_slope(self, p1: List[int], p2: List[int]) -> float:
+        x1, x2 = p1[0], p2[0]
+        y1, y2 = p1[1], p2[1]
+        
+        if x1 == x2:
+            return float('inf')
+        if y1 == y2:
+            return 0
+        
+        return (float) (y2 - y1) / (x2 - x1)
+    
     def maxPoints(self, points: List[List[int]]) -> int:
+        # Geometry + hash map
         n = len(points)
         if n == 1:
             return 1
         
-        ans = 2
+        ans = 0
+        
         for i in range(n):
-            cnt = defaultdict(int)
-            for j in range(n):
-                if i != j:
-                    cnt[math.atan2(points[j][1] - points[i][1], points[j][0] - points[i][0])] += 1
-            
-            ans = max(ans, max(cnt.values()) + 1)
-            
-        return ans
+            count = defaultdict(int)
+            for j in range(i + 1, n):
+                slope = self.calculate_slope(points[i], points[j])
+                count[slope] += 1
+                ans = max(ans, count[slope])
+                
+        # Since points[i] also lies on the line
+        return ans + 1
