d6129fd807679da4b571154024848c33efc8003d
Andrew Kim <ksb0724@utexas.edu>
2024-09-19 03:36:09 +0900

    Time: 30 ms (83.41%), Space: 16.5 MB (61.43%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,16 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Bottom-up, space-optimized
+        sqrt5 = 5 ** 0.5
+        phi, psi = (1 + sqrt5) / 2, (1 - sqrt5) / 2
         
-        # Base case
-        if n <= 2:
-            return max(n, 0)
-        
-        first, second, ans = 1, 2, 0
-        
-        for _ in range(3, n + 1):
-            ans = first + second
-            first = second
-            second = ans
-            
-        return ans
+        return int((phi ** (n + 1) - psi ** (n + 1)) / sqrt5)
