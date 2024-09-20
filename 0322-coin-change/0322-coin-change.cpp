643d017bad85275cd6375bc456b4a2ebafd37162
Andrew Kim <ksb0724@utexas.edu>
2024-09-21 02:05:24 +0900

    Time: 13 ms (97.34%), Space: 16.4 MB (75.46%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,15 @@
 class Solution {
-private:
-    int dp(vector<int>& coins, vector<int>& count, int remain){
-        // Base case
-        if (remain < 0) return -1;
-        if (remain == 0) return 0;
-        if (count[remain] != 0) return count[remain];
-        
-        int minCoins = INT_MAX;
-        for (int coin : coins){
-            int res = dp(coins, count, remain - coin);
-            if (0 <= res && res < minCoins)
-                minCoins = res + 1;
-        }
-        count[remain] = (minCoins == INT_MAX)? -1 : minCoins;
-        
-        return count[remain];
-    }
-    
 public:
     int coinChange(vector<int>& coins, int amount) {
-        // Base case
-        if (amount < 1) return 0;
+        // Bottom-up Dynamic programming
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
         
-        // Top-down Dynamic Programming
-        vector<int> count(amount + 1, 0);
-        return dp(coins, count, amount);
+        for (int coin : coins){
+            for (int curr = coin; curr <= amount; ++curr)
+                dp[curr] = min(dp[curr], dp[curr - coin] + 1);
+        }
+        
+        return (dp[amount] == amount + 1)? -1 : dp[amount];
     }
 };
