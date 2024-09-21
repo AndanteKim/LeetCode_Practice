46398f1634df8756cd15df42d41cc58d4167171a
Andrew Kim <ksb0724@utexas.edu>
2024-09-21 13:20:01 +0900

    Time: 73 ms (68.42%), Space: 22.5 MB (74.04%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,15 +1,18 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        def backtrack(digit: int) -> None:
-            if digit > n:
-                return
-                
-            ans.append(digit)
-            digit *= 10
-            for i in range(0, 10):
-                backtrack(digit + i)
+        ans, curr = [], 1
         
-        ans = []
-        for i in range(1, 10):
-            backtrack(i)
+        # Generate numbers from 1 to n
+        for _ in range(n):
+            ans.append(curr)
+            
+            # If multiplying the current number by 10 is within the limit, do it.
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                # Adjust the current number by moving up one digit
+                while curr % 10 == 9 or curr >= n:
+                    curr //= 10     # Remove the last digit
+                curr += 1   # Increment the number
+                
         return ans
