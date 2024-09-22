c4906b45a71adc77a7fe9235b8b1292e1ebe44a2
Andrew Kim <ksb0724@utexas.edu>
2024-09-22 22:20:17 +0900

    Time: 443 ms (15.00%), Space: 45.8 MB (35.00%) - LeetHub

--- 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.py
+++ 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.py
@@ -1,22 +1,43 @@
+class TrieNode:
+    def __init__(self):
+        self.is_end = False
+        self.children = dict()
+
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        
+    # Insert a word into the trie
+    def _insert(self, s: str) -> None:
+        curr = self.root
+        for c in s:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+        curr.is_end = True
+    
+    # Check if all prefixes of the word exists in the trie
+    def _has_all_prefixes(self, s: str) -> bool:
+        curr = self.root
+        for c in s:
+            if c not in curr.children or not curr.children[c].is_end:
+                return False
+            curr = curr.children[c]
+        return True
+
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        # Sort the words lexicographically
-        words.sort()
+        trie, longest_valid_word = Trie(), ""
         
-        # Set to store valid words
-        valid_words = set()
-        longest_valid_word = ""
-        
-        # Iterate through each word
+        # Insert all words into the trie
         for word in words:
-            # Check if the word of length 1 or if its prefix exists in the set
-            if len(word) == 1 or word[:-1] in valid_words:
-                # Add the current word to the set of valid words
-                valid_words.add(word)
+            trie._insert(word)
             
-                # Update the longest valid word if necessary
-                if len(word) > len(longest_valid_word):
+        # Check each word and update the longest valid word
+        for word in words:
+            if trie._has_all_prefixes(word):
+                if len(word) > len(longest_valid_word) or \
+                (len(word) == len(longest_valid_word) and word < longest_valid_word):
                     longest_valid_word = word
-                
-        # Return the longest valid word found
         return longest_valid_word
