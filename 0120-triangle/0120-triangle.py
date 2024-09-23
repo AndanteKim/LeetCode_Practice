a2ea4d9d8a81348f05a1586ac34065e3df35476d
Andrew Kim <ksb0724@utexas.edu>
2024-09-23 22:50:55 +0900

    Time: 60 ms (66.81%), Space: 17.6 MB (64.55%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,16 +1,11 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        prev_row, n = triangle[0], len(triangle)
-        for row in range(1, n):
+        below_row, n = triangle[-1], len(triangle)
+        
+        for row in range(n - 2, -1, -1):
             curr_row = []
             for col in range(row + 1):
-                smallest = float('inf')
-                if col > 0:
-                    smallest = prev_row[col - 1]
-                
-                if col < row:
-                    smallest = min(smallest, prev_row[col])
+                smallest = min(below_row[col], below_row[col + 1])
                 curr_row.append(triangle[row][col] + smallest)
-                
-            prev_row = curr_row
-        return min(prev_row)
+            below_row = curr_row
+        return below_row[0]
