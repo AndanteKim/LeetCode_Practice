7a30c5b52666dea443a8a1a2590fff4935ac8637
Andrew Kim <ksb0724@utexas.edu>
2024-09-23 22:42:44 +0900

    Time: 6 ms (35.53%), Space: 11.9 MB (8.41%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
+        vector<int> prevRow = triangle[0];
         int m = triangle.size();
         
         for (int row = 1; row < m; ++row){
+            vector<int> currRow;
             for (int col = 0; col <= row; ++col){
                 int smallest = 1e5;
-                if (col > 0) smallest = triangle[row - 1][col - 1];
-                
-                if (col < row) smallest = min(smallest, triangle[row - 1][col]);
-            
-                triangle[row][col] += smallest;
+                if (col > 0) smallest = prevRow[col - 1];
+                if (col < row) smallest = min(smallest, prevRow[col]);
+                currRow.push_back(triangle[row][col] + smallest);
             }
+            
+            prevRow = currRow;
         }
         
-        return *min_element(triangle.back().begin(), triangle.back().end());
+        return *min_element(prevRow.begin(), prevRow.end());
     }
 };
