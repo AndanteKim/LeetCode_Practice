8d13a6b86c3dc844a9566abbfe32b7f12fff2039
Andrew Kim <ksb0724@utexas.edu>
2024-09-23 22:07:36 +0900

    Time: 69 ms (20.26%), Space: 17.5 MB (86.16%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,16 +1,16 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        # Top-down DP
-        def min_path(row: int, col: int) -> int:
-            path = triangle[row][col]
-            if memo[row][col] != -1:
-                return memo[row][col]
-            
-            if row < len(triangle) - 1:
-                path += min(min_path(row + 1, col), min_path(row + 1, col + 1))
-            memo[row][col] = path
-            return path
-        
         m = len(triangle)
-        memo = [[-1] * len(triangle[i]) for i in range(m)]
-        return min_path(0, 0)
+        
+        for row in range(1, m):
+            for col in range(row + 1):
+                smallest = float('inf')
+                if col > 0:
+                    smallest = triangle[row - 1][col - 1]
+                
+                if col < row:
+                    smallest = min(smallest, triangle[row - 1][col])
+                    
+                triangle[row][col] += smallest
+                
+        return min(triangle[-1])
