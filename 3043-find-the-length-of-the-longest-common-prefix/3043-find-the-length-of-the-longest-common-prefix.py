eebd60f52e3c74b31a8e21fe9513d0112479997e
Andrew Kim <ksb0724@utexas.edu>
2024-09-24 13:26:23 +0900

    Time: 670 ms (96.15%), Space: 28.8 MB (65.78%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,46 +1,25 @@
-class TrieNode:
-    def __init__(self):
-        self.is_end = False
-        self.children = dict()
-        
-class Trie:
-    def __init__(self):
-        self.root = TrieNode()
-        
-    def insert(self, s:str) -> None:
-        curr = self.root
-        for c in s:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        curr.is_end = True
-    
-    def search_longest_prefix(self, s: str) -> int:
-        length, curr = 0, self.root
-        
-        for c in s:
-            if c not in curr.children:
-                break
-            curr = curr.children[c]
-            length += 1
-        return length
-
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        t1, t2 = Trie(), Trie()
+        # Set to store all prefixes from arr1
+        arr1_prefixes = set()
         
-        str1, str2 = [str(n1) for n1 in arr1], [str(n2) for n2 in arr2]
-        for s1 in str1:
-            t1.insert(s1)
-            
-        for s2 in str2:
-            t2.insert(s2)
+        # Step 1: Build all possible prefixes from arr1
+        for val in arr1:
+            while val not in arr1_prefixes and val > 0:
+                # Insert current value as a prefix
+                arr1_prefixes.add(val)
+                
+                # Generate the next shorter prefix by removing the last digit
+                val //= 10
         
-        ans = 0
-        for s1 in str1:
-            ans = max(ans, t2.search_longest_prefix(s1))
-            
-        for s2 in str2:
-            ans = max(ans, t1.search_longest_prefix(s2))
-        
-        return ans
+        # Step2: Check each number in arr2 for the longest matching prefix
+        longest_prefix = 0
+        for val in arr2:
+            while val not in arr1_prefixes and val > 0:
+                # Reduce val by removing the last digit if not found in the prefix set
+                val //= 10
+                
+            if val > 0:
+                # Length of the matched prefix using log10 to determine the number of digits
+                longest_prefix = max(longest_prefix, int(log10(val)) + 1)
+        return longest_prefix
