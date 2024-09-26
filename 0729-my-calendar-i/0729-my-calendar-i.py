5818d1573738059af91650ca32ad1f4e144ef135
Andrew Kim <ksb0724@utexas.edu>
2024-09-26 12:07:07 +0900

    Time: 192 ms (56.73%), Space: 17.8 MB (10.74%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,25 +1,17 @@
-class MyCalendar {
-private:
-    vector<pair<int, int>> calendar;
+from sortedcontainers import SortedList
 
-public:
-    MyCalendar() {}
-    
-    // Brute Force
-    bool book(int start, int end) {
-        for (auto& [s, e] : calendar){
-            // No overlapping: end2 <= start1 or end1 <= start2
-            if (!(end <= s || e <= start))
-                return false;
-        }
-        
-        calendar.push_back({start, end});
-        return true;
-    }
-};
+class MyCalendar:
 
-/**
- * Your MyCalendar object will be instantiated and called as such:
- * MyCalendar* obj = new MyCalendar();
- * bool param_1 = obj->book(start,end);
- */
+    def __init__(self):
+        self.calendar = SortedList()
+
+    def book(self, start: int, end: int) -> bool:
+        idx = self.calendar.bisect_right((start, end))
+        if (idx > 0 and self.calendar[idx - 1][1] > start) or (idx < len(self.calendar) and self.calendar[idx][0] < end):
+            return False
+        self.calendar.add((start, end))
+        return True
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
