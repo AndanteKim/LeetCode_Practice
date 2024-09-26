6a11c0107772cd609e0715662ef51e298c15038e
Andrew Kim <ksb0724@utexas.edu>
2024-09-26 11:58:38 +0900

    Time: 81 ms (55.69%), Space: 41.6 MB (86.63%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,17 +1,25 @@
-class MyCalendar:
+class MyCalendar {
+private:
+    vector<pair<int, int>> calendar;
 
-    def __init__(self):
-        self.calendar = []
+public:
+    MyCalendar() {}
+    
+    // Brute Force
+    bool book(int start, int end) {
+        for (auto& [s, e] : calendar){
+            // No overlapping: end2 <= start1 or end1 <= start2
+            if (!(end <= s || e <= start))
+                return false;
+        }
+        
+        calendar.push_back({start, end});
+        return true;
+    }
+};
 
-    def book(self, start: int, end: int) -> bool:
-        for s, e in self.calendar:
-            if start < e and s < end:
-                return False
-            
-        self.calendar.append((start, end))
-        return True
-
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
