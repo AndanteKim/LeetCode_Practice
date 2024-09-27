04b6257b09c1ce7233164045a41b43291ac09fc0
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 22:58:19 +0900

    Time: 199 ms (65.68%), Space: 138.2 MB (62.08%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,33 +1,30 @@
 class Solution {
-private:
-    int n;
-    int dp(int i, int left, vector<int>& prices, vector<vector<int>>& memo){
-        // Base case
-        if (i == n || left == 0) return 0;
-        
-        // If we saved the subproblem result
-        if (memo[i][left] != -1) return memo[i][left];
-        
-        // start choosing no transaction
-        int profit1 = dp(i + 1, left, prices, memo);
-        int profit2;
-        
-        // Choose buying or selling it
-        bool buy = (left % 2 == 0);
-        if (buy)
-            profit2 = -prices[i] + dp(i + 1, left - 1, prices, memo);
-        else
-            profit2 = prices[i] + dp(i + 1, left - 1, prices, memo);
-        
-        return memo[i][left] = max(profit1, profit2);
-    }
-    
 public:
     int maxProfit(vector<int>& prices) {
-        // Top-Down dynamic programming
-        this -> n = prices.size();
-        vector memo(n, vector<int>(5, -1));
+        // Bottom-up dynamic programming
+        int n = prices.size();
+        vector<vector<int>> dp(n + 1, vector<int>(5));
         
-        return dp(0, 4, prices, memo);
+        for (int i = n; i >= 0; --i){
+            for (int left = 0; left <= 4; ++left){
+                // Base case
+                if (left == 0 || i == n)
+                    dp[i][left] = 0;
+                else{
+                    // Choice 1: No transaction day
+                    int profit1 = dp[i + 1][left], profit2 = 0;
+                    
+                    // Choice 2: transaction day
+                    bool buy = (left % 2 == 0);
+                    if (buy)
+                        profit2 = -prices[i] + dp[i + 1][left - 1];
+                    else
+                        profit2 = prices[i] + dp[i + 1][left - 1];
+                    dp[i][left] = max(profit1, profit2);
+                }
+            }
+        }
+        
+        return dp[0][4];
     }
 };
