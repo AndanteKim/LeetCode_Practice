4fc9df8c7b036d1065d5e940fe69d1b8b6e61476
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 23:06:40 +0900

    Time: 1120 ms (31.28%), Space: 30.8 MB (44.72%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,27 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Bottom-up dynamic programming
+        # Bottom-up space-optimized dp
         n = len(prices)
-        dp = [[0] * 5 for _ in range(n + 1)]
+        prev_dp = [0] * 5
         
-        for i in range(n, -1, -1):
+        for i in range(n - 1, -1, -1):
+            dp = [0] * 5
             for left in range(0, 5):
+                # Base case
                 if i == n or left == 0:
-                    dp[i][left] = 0
+                    continue
+                
+                # Choice 1: No transaction day
+                profit1, profit2 = prev_dp[left], 0
+                
+                # Choice 2: Transaction day
+                buy = (left % 2 == 0)
+                if buy:
+                    profit2 = -prices[i] + prev_dp[left - 1]
                 else:
-                    # Choice 1: No transaction day
-                    profit1 = dp[i + 1][left]
-                    
-                    # Choice 2: Transaction day
-                    profit2 = 0
-                    buy = left % 2 == 0
-                    
-                    if buy:
-                        profit2 = -prices[i] + dp[i + 1][left - 1]
-                    else:
-                        profit2 = prices[i] + dp[i + 1][left - 1]
-                    
-                    dp[i][left] = max(profit1, profit2)
-                    
-        return dp[0][4]
-        
+                    profit2 = prices[i] + prev_dp[left - 1]
+                
+                dp[left] = max(profit1, profit2)
+            prev_dp = dp
+        return prev_dp[4]
