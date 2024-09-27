547e3582bebeda8f2f2e6adc3e00e8c3ee513cfc
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 13:52:59 +0900

    Time: 1464 ms (28.18%), Space: 18.1 MB (7.66%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,29 +1,35 @@
+from sortedcontainers import SortedDict
+
 class MyCalendarTwo:
 
     def __init__(self):
-        self.bookings, self.overlap_bookings = [], []
-        
-    # Return True if the booking [start1, end1) & [start2, end2) overlaps.
-    def does_overlap(self, start1: int, end1: int, start2: int, end2: int) -> bool:
-        return max(start1, start2) < min(end1, end2)
+        # Store the number of bookings at each point.
+        self.max_overlapped_booking = 2
+        # The maximum number of overlapped bookings allowed. 
+        self.booking_count = SortedDict(int)
 
-    # Return the overlapping booking between [start1, end1) & [start2, end2).
-    def get_overlapped(self, start1: int, end1: int, start2: int, end2: int) -> Tuple[int]:
-        return (max(start1, start2), min(end1, end2))
-    
     def book(self, start: int, end: int) -> bool:
-        # Check if the new booking overlaps with any double-booked booking.
-        for s, e in self.overlap_bookings:
-            if self.does_overlap(s, e, start, end):
+        # Increase and decrease the booking count at the start and end respectively.
+        self.booking_count[start] = self.booking_count.get(start, 0) + 1
+        self.booking_count[end] = self.booking_count.get(end, 0) - 1
+        overlapped_booking = 0
+        
+        # Calculate the prefix sum of bookings
+        for count in self.booking_count.values():
+            overlapped_booking += count
+            
+            # If the number of overlaps exceeds the allowed limit
+            # rollback and return False
+            if overlapped_booking > self.max_overlapped_booking:
+                # Rollback changes
+                self.booking_count[start] -= 1
+                self.booking_count[end] += 1
+                
+                # Remove entries if their count becomes zero to clean up the SortedDict.
+                if self.booking_count[start] == 0:
+                    del self.booking_count[start]
                 return False
         
-        # Add any new double overlaps that the current booking creates
-        for s, e in self.bookings:
-            if self.does_overlap(s, e, start, end):
-                self.overlap_bookings.append(self.get_overlapped(s, e, start, end))
-        
-        # Add the new booking to the list of bookings
-        self.bookings.append((start, end))
         return True
 
 
