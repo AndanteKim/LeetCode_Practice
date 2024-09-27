ecc036276b40cc4dae6ae35b5aa02c90627d8b57
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 22:49:25 +0900

    Time: 1288 ms (28.41%), Space: 39.5 MB (29.83%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,29 +1,27 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # Top-Down Dynamic programming
-        def dp(i: int, left: int) -> int:
-            if i == n or left == 0:
-                return 0
-            
-            # If we saved the subproblem result
-            if memo[i][left] != -1:
-                return memo[i][left]
-            
-            # start choosing no transaction
-            profit1 = dp(i + 1, left)
-            
-            # choose buying or selling it.
-            profit2, bought = 0, (left % 2 == 0)
-            if bought:
-                profit2 = -prices[i] + dp(i + 1, left - 1)
-            else:
-                profit2 = prices[i] + dp(i + 1, left - 1)
-                
-            memo[i][left] = max(profit1, profit2)
-            return memo[i][left]
-                
-        
+        # Bottom-up dynamic programming
         n = len(prices)
-        memo = [[-1] * 5 for i in range(n + 1)]
-        # We set 4 times left due to at most two transactions(buy -> sell -> buy -> sell)
-        return dp(0, 4)
+        dp = [[0] * 5 for _ in range(n + 1)]
+        
+        for i in range(n, -1, -1):
+            for left in range(0, 5):
+                if i == n or left == 0:
+                    dp[i][left] = 0
+                else:
+                    # Choice 1: No transaction day
+                    profit1 = dp[i + 1][left]
+                    
+                    # Choice 2: Transaction day
+                    profit2 = 0
+                    buy = left % 2 == 0
+                    
+                    if buy:
+                        profit2 = -prices[i] + dp[i + 1][left - 1]
+                    else:
+                        profit2 = prices[i] + dp[i + 1][left - 1]
+                    
+                    dp[i][left] = max(profit1, profit2)
+                    
+        return dp[0][4]
+        
