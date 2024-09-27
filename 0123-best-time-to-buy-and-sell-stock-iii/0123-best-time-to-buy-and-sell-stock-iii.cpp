56835f0f21d8eb8ee91e3d7beeb7c47fd796c3d0
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 18:30:01 +0900

    Time: 81 ms (98.18%), Space: 78.1 MB (94.46%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // Base case
-        if (prices.size() <= 1) return 0;
+        // One-pass simulation
+        int t1Cost = INT_MAX, t2Cost = INT_MAX;
+        int t1Profit = 0, t2Profit = 0;
         
-        int n = prices.size();
-        int leftMin = prices[0], rightMax = prices.back();
-        
-        // Pad the right DP array with an additional zero for the convenience
-        vector<int> leftProfit(n), rightProfit(n + 1);
-        
-        // Construct the bidirectional DP array
-        for (int left = 1; left < n; ++left){
-            leftProfit[left] = max(leftProfit[left - 1], prices[left] - leftMin);
-            leftMin = min(leftMin, prices[left]);
+        for (int price : prices){
+            // The maximum profit if only one transaction is allowed
+            t1Cost = min(t1Cost, price);
+            t1Profit = max(t1Profit, price - t1Cost);
             
-            int right = n - 1 - left;
-            rightProfit[right] = max(rightProfit[right + 1], rightMax - prices[right]);
-            rightMax = max(rightMax, prices[right]);
+            // reinvest the gained profit in the second transaction 
+            t2Cost = min(t2Cost, price - t1Profit);
+            t2Profit = max(t2Profit, price - t2Cost);
         }
         
-        int maxProfit = 0;
-        for (int i = 0; i < n; ++i)
-            maxProfit = max(maxProfit, leftProfit[i] + rightProfit[i + 1]);
-        
-        return maxProfit;
+        return t2Profit;
     }
 };
