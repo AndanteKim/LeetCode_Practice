1511be1f40426e6da2f6fd6686f452c6abfb7af2
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 18:12:57 +0900

    Time: 725 ms (93.41%), Space: 30.6 MB (49.59%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,25 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 1:
-            return 0
+        t1_cost, t2_cost = float("inf"), float("inf")
+        t1_profit, t2_profit = 0, 0
         
-        left_min, right_max = prices[0], prices[-1]
-        n = len(prices)
-        
-        # Pad the right DP array with an additional zero for convenience
-        left_profits, right_profits = [0] * n, [0] * (n + 1)
-        
-        # Construct the bidirectional DP array
-        for left in range(1, n):
-            left_profits[left] = max(left_profits[left - 1], prices[left] - left_min)
-            left_min = min(left_min, prices[left])
+        for price in prices:
+            # The maximum profit if only one transaction is allowed
+            t1_cost = min(t1_cost, price)
+            t1_profit = max(t1_profit, price - t1_cost)
             
-            right = n - 1 - left
-            right_profits[right] = max(right_profits[right + 1], right_max - prices[right])
-            right_max = max(right_max, prices[right])
+            # reinvest the gained profit in the second transaction
+            t2_cost = min(t2_cost, price - t1_profit)
+            t2_profit = max(t2_profit, price - t2_cost)
             
-        max_profit = 0
-        for i in range(0, n):
-            max_profit = max(max_profit, left_profits[i] + right_profits[i + 1])
-            
-        return max_profit
+        return t2_profit
