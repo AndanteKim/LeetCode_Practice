893bd912912a2b033a704790e433354ad8b8cf2d
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 22:27:51 +0900

    Time: 1531 ms (25.96%), Space: 58.4 MB (22.85%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,15 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        t1_cost, t2_cost = float("inf"), float("inf")
-        t1_profit, t2_profit = 0, 0
+        # Top-Down Dynamic programming
+        def dp(i: int, left: int) -> int:
+            if i == n or left == 0:
+                return 0
+            
+            # If we saved the subproblem result
+            if memo[i][left] != -1:
+                return memo[i][left]
+            
+            # start choosing no transaction
+            profit1 = dp(i + 1, left)
+            
+            # choose buying or selling it.
+            profit2, bought = 0, (left % 2 == 0)
+            if bought:
+                profit2 = -prices[i] + dp(i + 1, left - 1)
+            else:
+                profit2 = prices[i] + dp(i + 1, left - 1)
+                
+            memo[i][left] = max(profit1, profit2)
+            return memo[i][left]
+                
         
-        for price in prices:
-            # The maximum profit if only one transaction is allowed
-            t1_cost = min(t1_cost, price)
-            t1_profit = max(t1_profit, price - t1_cost)
-            
-            # reinvest the gained profit in the second transaction
-            t2_cost = min(t2_cost, price - t1_profit)
-            t2_profit = max(t2_profit, price - t2_cost)
-            
-        return t2_profit
+        n = len(prices)
+        memo = [[-1] * 5 for i in range(n + 1)]
+        # We set 4 times left due to at most two transactions(buy -> sell -> buy -> sell)
+        return dp(0, 4)
