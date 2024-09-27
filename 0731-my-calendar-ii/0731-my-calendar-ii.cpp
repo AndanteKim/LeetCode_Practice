dd1ce1f2759993900bdd8fb3a06e06757df170a2
Andrew Kim <ksb0724@utexas.edu>
2024-09-27 14:11:45 +0900

    Time: 170 ms (16.79%), Space: 42.6 MB (46.51%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,34 +1,36 @@
 class MyCalendarTwo {
 private:
-    vector<pair<int, int>> bookings, overlappedBooking;
+    // Store the number of bookings at each point.
+    map<int, int> bookingCount;
     
-    // Return true if the booking [start1, end1) & [start2, end2) overlaps
-    bool doesOverlapped(int start1, int end1, int start2, int end2) const{
-        return !(end1 <= start2 || end2 <= start1); 
-    }
-    
-    // Return overlapping booking between [start1, end1) & [start2, end2).
-    pair<int, int> getOverlapped(int start1, int end1, int start2, int end2) const{
-        return make_pair(max(start1, start2), min(end1, end2));
-    }
+    // The maximum number of overlapped bookings allowed.
+    int maxOverlappedBookings = 2;
     
 public:
     MyCalendarTwo() {}
     
     bool book(int start, int end) {
-        // Base case: If the new booking overlaps with the existing double-booked bookings, return false
-        for (const auto& [s, e] : overlappedBooking){
-            if (doesOverlapped(s, e, start, end)) return false;
+        // Increase and decrease the booking count at the start and end respectively.
+        ++bookingCount[start];
+        --bookingCount[end];
+        
+        int overlappedBookings = 0;
+        
+        // Calculate the prefix sum of bookings
+        for (auto& [_, count] : bookingCount){
+            overlappedBookings += count;
+            
+            // If the numbrer of overlaps exceeds the allowed limit
+            // rollback and return false.
+            if (overlappedBookings > maxOverlappedBookings){
+                // Rollback changes.
+                --bookingCount[start];
+                ++bookingCount[end];
+                
+                return false;
+            }
         }
         
-        // Add the double overlapping if any with the new booking.
-        for (const auto& [s, e] : bookings){
-            if (doesOverlapped(s, e, start, end))
-                overlappedBooking.push_back(getOverlapped(s, e, start, end));
-        }
-        
-        // Add the booking to the list of bookings.
-        bookings.push_back({start, end});
         return true;
     }
 };
