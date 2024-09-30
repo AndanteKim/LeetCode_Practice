d44cad4971b81c13c5b2959f9661fef71d327bb7
Andrew Kim <ksb0724@utexas.edu>
2024-09-30 15:50:26 +0900

    Time: 120 ms (8.31%), Space: 17.6 MB (58.60%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
@@ -1,21 +1,23 @@
 class CustomStack:
 
     def __init__(self, maxSize: int):
-        # Array to store stack elements
-        self._stack = []
-        # Index of the top element in the stack
-        self._max_size = maxSize
+        # Initialize the stack as a deque for efficient add/remove operations
+        self.stack = deque()
+        self.max_size = maxSize
 
     def push(self, x: int) -> None:
-        if len(self._stack) < self._max_size:
-            self._stack.append(x)
+        # Add the element to the top of the stack if it hasn't reached max_size
+        if len(self.stack) < self.max_size:
+            self.stack.append(x)
 
     def pop(self) -> int:
-        return self._stack.pop() if self._stack else -1
+        # Return -1 if the stack is empty, otherwise remove and return the top element
+        return self.stack.pop() if self.stack else -1
 
     def increment(self, k: int, val: int) -> None:
-        for i in range(min(k, len(self._stack))):
-            self._stack[i] += val
+        # Increment the bottom k elements (or all elements if k > the size of stack)
+        for i, _ in zip(range(k), self.stack):
+            self.stack[i] += val
 
 
 # Your CustomStack object will be instantiated and called as such:
