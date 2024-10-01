4d018ba3ef1f3d8c580f0135bfb4a580ea2aaaf4
Andrew Kim <ksb0724@utexas.edu>
2024-10-01 18:20:50 +0900

    Time: 128 ms (48.94%), Space: 73.8 MB (30.82%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        
-        // Store the count of remainders in a map.
         unordered_map<int, int> freq;
-        for (int num : arr){
-            int target = (k + (num % k)) % k;
-            ++freq[target];
-        }
+        int remain = ((int)arr.size()) >> 1;
         
         for (int num : arr){
-            int target = (k + (num % k)) % k;
-
-            // If the remainder for an element is 0, then the count of
-            // numbers that give this remainder must be even.
-            if (target == 0){
-                if (freq[target] % 2) return false;
+            int target = ((-num % k) + k) % k;
+            
+            if (freq[target] > 0){
+                --freq[target];
+                --remain;
             }
-            // If the remainder target and k - target don't have the same
-            // count then pairs cannot be made.
-            else if (freq[target] != freq[k - target])
-                return false;
+            else{
+                ++freq[((num % k) + k) % k];
+            }
         }
         
-        return true;
+        return remain == 0;
     }
 };
