af25cd00c8f54791bfd3562aed6f090aad4848cc
Andrew Kim <ksb0724@utexas.edu>
2024-10-01 17:24:31 +0900

    Time: 543 ms (13.13%), Space: 30.2 MB (91.94%) - LeetHub

--- 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
+++ 1497-check-if-array-pairs-are-divisible-by-k/1497-check-if-array-pairs-are-divisible-by-k.py
@@ -1,22 +1,25 @@
 class Solution:
     def canArrange(self, arr: List[int], k: int) -> bool:
-        freq = dict()
+        # Custom comparator to sort based on mod values
+        arr.sort(key = lambda x: (k + x % k) % k)
         
-        # Store the count of remainders in a map
-        for num in arr:
-            target = ((num % k) + k) % k
-            freq[target] = freq.get(target, 0) + 1
-            
-        for num in arr:
-            target = ((num % k) + k) % k
-            
-            # If the remainder for an element is 0, then the count of numbers that give this remainder must be even.
-            if target == 0:
-                if freq[target] % 2 == 1:
-                    return False
-            
-            # If the remainder target and k - rem don't have the same count then pairs cannot be made.
-            elif freq[target] != freq.get(k - target, 0):
+        # Assign the pairs with modulo first.
+        start, end = 0, len(arr) - 1
+        
+        while start < end:
+            if arr[start] % k != 0:
+                break
+                
+            if arr[start + 1] % k != 0:
                 return False
             
+            start += 2
+        
+        # Now, pick one element from the beginning and one element from the end.
+        while start < end:
+            if (arr[start] + arr[end]) % k != 0:
+                return False
+            start += 1
+            end -= 1
+        
         return True
