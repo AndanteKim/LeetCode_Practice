8a5d2fd37c5d1b3b1bd60f506fdcc55ad0a18fbd
Andrew Kim <ksb0724@utexas.edu>
2024-10-02 16:42:46 +0900

    Time: 101 ms (29.99%), Space: 51.7 MB (5.75%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     vector<int> arrayRankTransform(vector<int>& arr) {
-        vector<int> rank = arr;
-        unordered_map<int, int> mapping;
-        int i = 1;
-        sort(rank.begin(), rank.end());
+        // Store the rank for each number in arr.
+        map<int, vector<int>> numToIndices;
         
-        for (int num : rank){
-            if (mapping.count(num))
-                continue;
-            mapping[num] = i++;
+        for (int i = 0; i < arr.size(); ++i)
+            numToIndices[arr[i]].push_back(i);
+        
+        int rank = 1;
+        for (auto& pair: numToIndices){
+            for (int index : pair.second)
+                arr[index] = rank;
+            ++rank;
         }
         
-        vector<int> ans(arr.size());
-        i = 0;
-        for (int num : arr)
-            ans[i++] = mapping[num];
-        
-        return ans;
+        return arr;
     }
 };
