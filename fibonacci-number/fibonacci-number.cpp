ed709cbec3f08514ee422d00b026747298b4e7bf
Andrew Kim <ksb0724@utexas.edu>
2024-10-03 18:29:14 +0900

    Time: 3 ms, Memory: 7.6 MB - LeetHub

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,16 +1,16 @@
 class Solution {
-private:
-    int dp(int remain, vector<int>& memo){
-        // Base case
-        if (remain < 2) return remain;
-        if (memo[remain] != -1) return memo[remain];
-        
-        return memo[remain] = dp(remain - 1, memo) + dp(remain - 2, memo);
-    }
-    
 public:
     int fib(int n) {
-        vector<int> memo(n + 1, -1);
-        return dp(n, memo);
+        // Base case
+        if (n <= 1) return n;
+        
+        vector<int> dp(n + 1, 0);
+        
+        // Bottom-up
+        dp[1] = 1;
+        for (int i = 2; i <= n; ++i)
+            dp[i] = dp[i - 2] + dp[i - 1];
+        
+        return dp[n];
     }
 };
