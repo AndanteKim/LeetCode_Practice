c11a0b14303f166bf44622fc7d120e61078d185f
Andrew Kim <ksb0724@utexas.edu>
2024-10-04 17:45:41 +0900

    Time: 0 ms, Memory: 7.3 MB - LeetHub

--- 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.cpp
+++ 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.cpp
@@ -1,25 +1,22 @@
 class Solution {
 private:
-    int dfs(int remain, int position, int rootVal){
-        // Base case
-        if (remain == 1) return rootVal;
+    int recursion(int remain, int position){
+        // Base case: First row will only have one symbol '0'.
+        if (remain == 1) return 0;
         
         int totalNodes = 1 << (remain - 1);
+        int halfNodes = totalNodes >> 1;
         
-        // If the position is the half side of right of tree
-        if (position > (totalNodes >> 1)){
-            int nextRootVal = rootVal ^ 1;
-            return dfs(remain - 1, position - (totalNodes >> 1), nextRootVal);
-        }
-        // Left tree
-        else{
-            return dfs(remain - 1, position, rootVal);
-        }
+        // If the target is present in the right half, we switch to the respective left half symbol.
+        if (position > halfNodes)
+            return 1 - recursion(remain, position - halfNodes);
+        
+        // Otherwise, we switch to the previous one.
+        return recursion(remain - 1, position);
     }
-    
     
 public:
     int kthGrammar(int n, int k) {
-        return dfs(n, k, 0);
+        return recursion(n, k);
     }
 };
