282ee3c3d1d6a5e2fedf3c286014bcfd71a31032
Andrew Kim <ksb0724@utexas.edu>
2024-10-05 00:03:33 +0900

    Time: 0 ms, Memory: 7.3 MB - LeetHub

--- 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.cpp
+++ 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.cpp
@@ -1,22 +1,8 @@
 class Solution {
-private:
-    int recursion(int remain, int position){
-        // Base case: First row will only have one symbol '0'.
-        if (remain == 1) return 0;
-        
-        int totalNodes = 1 << (remain - 1);
-        int halfNodes = totalNodes >> 1;
-        
-        // If the target is present in the right half, we switch to the respective left half symbol.
-        if (position > halfNodes)
-            return 1 - recursion(remain, position - halfNodes);
-        
-        // Otherwise, we switch to the previous one.
-        return recursion(remain - 1, position);
-    }
-    
 public:
     int kthGrammar(int n, int k) {
-        return recursion(n, k);
+        int count = __builtin_popcount(k - 1);
+        
+        return count % 2;
     }
 };
