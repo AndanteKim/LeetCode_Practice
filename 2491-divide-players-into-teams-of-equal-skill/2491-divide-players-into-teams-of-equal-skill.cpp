592a5a9b96a84821d3a7d484fbf68f75b991aecb
Andrew Kim <ksb0724@utexas.edu>
2024-10-04 11:15:46 +0900

    Time: 70 ms (27.20%), Space: 57.3 MB (14.60%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.cpp
@@ -5,19 +5,21 @@ public:
     long long dividePlayers(vector<int>& skill) {
         int n = skill.size(), pairsRemain = n >> 1;
         unordered_map<int, int> freq;
-        int average = static_cast<int>(accumulate(skill.begin(), skill.end(), 0LL) / pairsRemain);
+        int targetSkill = static_cast<int>(accumulate(skill.begin(), skill.end(), 0LL) / pairsRemain);
         ll ans = 0;
         
-        for (int s : skill){
-            if (freq[average - s] > 0){
-                ans += s * (average - s);
-                --freq[average - s];
+        for (int currentSkill : skill){
+            int partnerSkill = targetSkill - currentSkill;
+            if (freq[partnerSkill] > 0){
+                ans += currentSkill * partnerSkill;
+                --freq[partnerSkill];
                 --pairsRemain;
             }
             else
-                ++freq[s];
+                ++freq[currentSkill];
         }
         
+        // If all pairs match
         return (pairsRemain == 0)? ans : -1;
     }
 };
