20679d55ad3a4cfa3499428c9f53cae237acca24
Andrew Kim <ksb0724@utexas.edu>
2024-10-04 16:59:20 +0900

    Time: 26 ms, Memory: 16.5 MB - LeetHub

--- 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.py
+++ 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.py
@@ -1,18 +1,18 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        def dfs(remain: int, pos: int, root_val: int) -> int:
-            # Base case: If the row reached the target row
+        def recursion(remain: int, pos: int) -> int:
+            # Base case: First row will only have one symbol '0'
             if remain == 1:
-                return root_val
+                return 0
             
             total_nodes = 2 ** (remain - 1)
+            half_nodes = (total_nodes) >> 1
             
-            # Target node will be present in the right subtree of the current root node.
-            if pos > (total_nodes >> 1):
-                next_root_val = root_val ^ 1
-                return dfs(remain - 1, pos - (total_nodes >> 1), next_root_val)
-            # Otherwise, the target node is in the left subtree of the current root node.
-            else:
-                return dfs(remain - 1, pos, root_val)
+            # If the target is present in the right half, we switch to the respective left half symbol
+            if pos > half_nodes:
+                return 1 - recursion(remain, pos - half_nodes)
+            
+            # Otherwise, we switch to the previous row
+            return recursion(remain - 1, pos)
         
-        return dfs(n, k, 0)
+        return recursion(n, k)
