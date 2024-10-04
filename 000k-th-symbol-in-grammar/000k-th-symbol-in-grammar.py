56e4927b698fa5a2b7f1e63c510a169bc7144c70
Andrew Kim <ksb0724@utexas.edu>
2024-10-04 23:29:37 +0900

    Time: 32 ms, Memory: 16.6 MB - LeetHub

--- 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.py
+++ 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.py
@@ -1,18 +1,27 @@
 class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
-        def recursion(remain: int, pos: int) -> int:
-            # Base case: First row will only have one symbol '0'
-            if remain == 1:
-                return 0
-            
-            total_nodes = 2 ** (remain - 1)
-            half_nodes = (total_nodes) >> 1
-            
-            # If the target is present in the right half, we switch to the respective left half symbol
-            if pos > half_nodes:
-                return 1 - recursion(remain, pos - half_nodes)
-            
-            # Otherwise, we switch to the previous row
-            return recursion(remain - 1, pos)
+        # Base case
+        if n == 1:
+            return 0
         
-        return recursion(n, k)
+        symbol = 1
+        
+        for curr_row in range(n, 1, -1):
+            total_nodes = 1 << (curr_row - 1)
+            half_nodes = total_nodes >> 1
+            
+            # If 'k' lies in the right half symbol 
+            if k > half_nodes:
+                # Flip the symbol
+                symbol ^= 1
+                                
+                # Change the position after flipping
+                k -= half_nodes
+        
+        # We reached the 1st row; If the symbol isn't '0', we started with the wrong symbol.
+        if symbol != 0:
+            # Thus, the start symbol was '0', not '1'.
+            return 0
+        
+        # The start symbol was indeed what we started with a '1'.
+        return 1
