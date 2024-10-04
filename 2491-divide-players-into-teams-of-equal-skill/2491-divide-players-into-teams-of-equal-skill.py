5c3cfbce094eaf13ddadaca3207ed6f0003bab83
Andrew Kim <ksb0724@utexas.edu>
2024-10-04 11:17:49 +0900

    Time: 414 ms (29.91%), Space: 29.5 MB (68.50%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.py
@@ -1,19 +1,17 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
-        # Base case
-        # if len(skill) == 2:
-        #    return skill[0] * skill[1]
-        
-        n, seen = len(skill), defaultdict(int) 
+        n, skill_seen = len(skill), defaultdict(int) 
         pair_remain = (n >> 1)
-        avg, ans = sum(skill) // pair_remain, 0
+        target_skill, ans = sum(skill) // pair_remain, 0
         
-        for s in skill:
-            if seen[avg - s] > 0:
-                ans += s * (avg - s)
-                seen[avg - s] -= 1
+        for curr_skill in skill:
+            partner_skill = target_skill - curr_skill
+            if skill_seen[partner_skill] > 0:
+                ans += curr_skill * partner_skill
+                skill_seen[partner_skill] -= 1
                 pair_remain -= 1
             else:
-                seen[s] += 1
+                skill_seen[curr_skill] += 1
         
+        # If all the pairs match, 
         return ans if pair_remain == 0 else -1
