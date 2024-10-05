8f2835ce38cbfbb97d7408f395673f17e1eb0bce
Andrew Kim <ksb0724@utexas.edu>
2024-10-06 01:51:47 +0900

    Time: 50 ms, Memory: 18 MB - LeetHub

--- unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
+++ unique-binary-search-trees-ii/unique-binary-search-trees-ii.py
@@ -5,22 +5,27 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def clone(self, node: 'TreeNode', offset: int) -> 'TreeNode':
+        # Base case
+        if not node:
+            return None
+        
+        cloned_node = TreeNode(node.val + offset)
+        cloned_node.left = self.clone(node.left, offset)
+        cloned_node.right = self.clone(node.right, offset)
+        return cloned_node
+    
     def generateTrees(self, n: int) -> List[Optional[TreeNode]]:
-        dp = [[[] for _ in range(n + 1)] for _ in range(n + 1)]
+        dp = [[] for _ in range(n + 1)]
+        dp[0].append(None)
         
-        for i in range(n + 1):
-            dp[i][i] = [TreeNode(i)]
-            
-        for num_of_nodes in range(2, n + 1):
-            for start in range(1, n - num_of_nodes + 2):
-                end = start + num_of_nodes - 1
-                for i in range(start, end + 1):
-                    left_subtrees = dp[start][i - 1] if i != start else [None]
-                    right_subtrees = dp[i + 1][end] if i != end else [None]
-                    
-                    for left in left_subtrees:
-                        for right in right_subtrees:
-                            root = TreeNode(i, left, right)
-                            dp[start][end].append(root)
-        
-        return dp[1][n]
+        for number_of_nodes in range(1, n + 1):
+            for i in range(1, number_of_nodes + 1):
+                j = number_of_nodes - i
+                
+                for left in dp[i - 1]:
+                    for right in dp[j]:
+                        root = TreeNode(i, left, self.clone(right, i))
+                        dp[number_of_nodes].append(root)
+                        
+        return dp[n]
