4050534a097b439588fe9d7a000581e965e80ad7
Andrew Kim <ksb0724@utexas.edu>
2024-10-08 12:52:09 +0900

    Time: 186 ms (77.55%), Space: 29.3 MB (19.60%) - LeetHub

---
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSwaps(self, s: str) -> int:
+        stack, unbalanced = [], 0
+        
+        for c in s:
+            # If an opening bracket is encountered, push it in the deque.
+            if c == '[':
+                stack.append(c)
+            else:
+                # If the deque isn't empty, pop it.
+                if stack:
+                    stack.pop()
+                
+                # Otherwise increase the count of unbalanced brackets
+                else:
+                    unbalanced += 1
+        
+        return (unbalanced + 1) >> 1
+            
