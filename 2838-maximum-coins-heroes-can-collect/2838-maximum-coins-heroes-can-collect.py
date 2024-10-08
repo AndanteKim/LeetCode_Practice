ce0b3d8f1e6c2347efbae09bbaf1d1adde08aaf8
Andrew Kim <ksb0724@utexas.edu>
2024-10-08 15:29:39 +0900

    Time: 712 ms (43.33%), Space: 40.4 MB (10.00%) - LeetHub

--- 2838-maximum-coins-heroes-can-collect/2838-maximum-coins-heroes-can-collect.py
+++ 2838-maximum-coins-heroes-can-collect/2838-maximum-coins-heroes-can-collect.py
@@ -1,24 +1,24 @@
 class Solution:
     def maximumCoins(self, heroes: List[int], monsters: List[int], coins: List[int]) -> List[int]:
         m, n = len(coins), len(heroes)
-        pairs = []
+        monsters_and_coins = []
         
         for i in range(m):
-            pairs.append((monsters[i], coins[i]))
+            monsters_and_coins.append((monsters[i], coins[i]))
             
         ans = [0] * n
-        pairs.sort()
-        monsters.sort()
+        # Sort by ascending value of monster power
+        monsters_and_coins.sort()
         prefix_coins = [0] * (m + 1)
         
         for i in range(m):
-            prefix_coins[i + 1] += prefix_coins[i] + pairs[i][1]
+            prefix_coins[i + 1] += prefix_coins[i] + monsters_and_coins[i][1]
         
         inf = 10 ** 10
         for i in range(n):
             hero = heroes[i]
             
-            limit = bisect_left(pairs, (hero, inf))
+            limit = bisect_left(monsters_and_coins, (hero, inf))
             ans[i] = prefix_coins[limit]
                     
         return ans
