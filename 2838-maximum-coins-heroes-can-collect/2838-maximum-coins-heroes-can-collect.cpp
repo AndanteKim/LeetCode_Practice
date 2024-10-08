282b05665d67b8a1192d20f19653c6bc6eadeaf6
Andrew Kim <ksb0724@utexas.edu>
2024-10-08 15:31:57 +0900

    Time: 147 ms (86.05%), Space: 114.7 MB (62.79%) - LeetHub

--- 2838-maximum-coins-heroes-can-collect/2838-maximum-coins-heroes-can-collect.cpp
+++ 2838-maximum-coins-heroes-can-collect/2838-maximum-coins-heroes-can-collect.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     vector<long long> maximumCoins(vector<int>& heroes, vector<int>& monsters, vector<int>& coins) {
         int m = coins.size(), n = heroes.size();
-        vector<pair<int, int>> pairs;
+        vector<pair<int, int>> monstersAndCoins;
         
         for (int i = 0; i < m; ++i)
-            pairs.push_back({monsters[i], coins[i]});
+            monstersAndCoins.push_back({monsters[i], coins[i]});
         
-        sort(pairs.begin(), pairs.end());
+        // Sort by ascending value of monster power
+        sort(monstersAndCoins.begin(), monstersAndCoins.end());
         
         int inf = INT_MAX;
         vector<long long> prefixCoins(m + 1);
         for (int i = 0; i < m; ++i){
-            prefixCoins[i + 1] = prefixCoins[i] + pairs[i].second;
+            prefixCoins[i + 1] = prefixCoins[i] + monstersAndCoins[i].second;
         }
         
         vector<long long> ans(n);
         for (int i = 0; i < n; ++i){
             pair<int, int> hero = {heroes[i], inf};
             
-            int limit = lower_bound(pairs.begin(), pairs.end(), hero) - pairs.begin();
+            int limit = lower_bound(monstersAndCoins.begin(), monstersAndCoins.end(), hero) - monstersAndCoins.begin();
             ans[i] = prefixCoins[limit];
         }
         
