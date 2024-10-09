93f7fbcb15c3e7628989b82ddc7eda2c64ff83f8
Andrew Kim <ksb0724@utexas.edu>
2024-10-09 16:54:17 +0900

    Time: 27 ms (93.74%), Space: 16.6 MB (30.64%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -1,19 +1,16 @@
 class Solution:
     def minAddToMakeValid(self, s: str) -> int:
-        mismatch, stack = 0, []
+        open_brackets = 0
+        min_adds_required = 0
         
         for c in s:
-            if c == '(':
-                stack.append(c)
-                continue
+            if c == "(":
+                open_brackets += 1
             else:
-                if stack:
-                    stack.pop()
+                if open_brackets > 0:
+                    open_brackets -= 1
                 else:
-                    mismatch += 1
-        
-        while stack:
-            stack.pop()
-            mismatch += 1
-
-        return mismatch
+                    min_adds_required += 1
+                
+        # Add the remaining open brackets as closing brackets would be required
+        return min_adds_required + open_brackets
