c4b6d023e066f90c9435fc99ec504fa99dd8539f
Andrew Kim <ksb0724@utexas.edu>
2024-10-10 04:19:55 +0900

    Time: 115 ms, Memory: 16.8 MB - LeetHub

--- snakes-and-ladders/snakes-and-ladders.py
+++ snakes-and-ladders/snakes-and-ladders.py
@@ -1,28 +1,29 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n = len(board)
-        cells, label = [None] * (n ** 2 + 1), 1
-        columns = list(range(0, n))
+        n, label = len(board), 1
+        columns, cells = list(range(0, n)), [None] * (n * n + 1)
         
         for row in range(n - 1, -1, -1):
             for col in columns:
                 cells[label] = (row, col)
                 label += 1
             columns.reverse()
-            
-        dist = [-1] * (n * n + 1)
-        queue = deque([1])
-        dist[1] = 0
         
-        while queue:
-            curr = queue.popleft()
+        dist = [-1] * (n * n + 1)
+        dist[1] = 0
+        min_heap = [(0, 1)]
+        
+        while min_heap:
+            d, curr = heappop(min_heap)
+            
+            if d != dist[curr]:
+                continue
             
             for next in range(curr + 1, min(curr + 6, n * n) + 1):
                 row, col = cells[next]
                 dest = board[row][col] if board[row][col] != -1 else next
-                
-                if dist[dest] == -1:
+                if dist[dest] == -1 or dist[curr] + 1 < dist[dest]:
                     dist[dest] = dist[curr] + 1
-                    queue.append(dest)
-                    
+                    heappush(min_heap, (dist[dest], dest))
+        
         return dist[n * n]
