0e5a28ce5df61c0c3b6b385681449afe81fefb54
Andrew Kim <ksb0724@utexas.edu>
2024-10-11 13:03:28 +0900

    Time: 574 ms (56.16%), Space: 22.7 MB (76.28%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.py
@@ -1,17 +1,42 @@
 class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
-        target_time = times[targetFriend]
-        times.sort()
+        # To store both arrival and leave events
+        n, events = len(times), []
         
-        n = len(times)
-        chair_time = [0] * n
+        # Populate events with arrival and leave times
+        for i in range(n):
+            # Arrival and leave
+            events.append((times[i][0], i))
+            events.append((times[i][1], ~i))
         
-        for time in times:
-            for i in range(n):
-                if chair_time[i] <= time[0]:
-                    chair_time[i] = time[1]
-                    if time == target_time:
-                        return i
-                    break
-                    
-        return 0
+        # Sort events by time
+        events.sort()
+        
+        # Tracking chairs that are free.
+        available_chairs = [i for i in range(n)]
+        
+        # When each chair will be free
+        occupied_chairs = []
+        
+        for event in events:
+            time, friend = event
+            
+            # free up chairs if friends leave
+            while occupied_chairs and occupied_chairs[0][0] <= time:
+                # Pop chair that becomes empty
+                _, chair = heappop(occupied_chairs)
+                
+                # Available chairs
+                heappush(available_chairs, chair)
+            
+            # If friend arrives
+            if friend >= 0:
+                chair = heappop(available_chairs)
+                if friend == targetFriend:
+                    return chair
+                
+                # Chair will be occupied till this time
+                heappush(occupied_chairs, (times[friend][1], chair))
+                
+        # Should not come to this point 
+        return -1
