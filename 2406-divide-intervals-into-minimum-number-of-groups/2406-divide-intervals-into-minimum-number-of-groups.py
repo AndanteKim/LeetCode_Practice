2710124e5c103cfff9ea8d1b5d30a18b649ff269
Andrew Kim <ksb0724@utexas.edu>
2024-10-12 17:20:35 +0900

    Time: 3435 ms (5.06%), Space: 61.5 MB (11.39%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,18 +1,24 @@
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
-        # Use a dictionary to store the points and their counts
-        point_to_count = defaultdict(int)
+        # Find the minimum and maximum value in the intervals
+        range_start, range_end = float('inf'), float('-inf')
         
-        # Mark the starting and ending points in the dictionary
         for start, end in intervals:
-            point_to_count[start] += 1      # Start of an interval
-            point_to_count[end + 1] -= 1    # End of an interval (interval[1] + 1)
+            range_start = min(range_start, start)
+            range_end = max(range_end, end)
+            
+        # Initialize the list with all zeroes
+        point_to_count = [0] * (range_end + 2)
+        
+        for start, end in intervals:
+            point_to_count[start] += 1    # Increment at the start of the interval
+            point_to_count[end + 1] -= 1    # Decrement right after the end of the interval
             
         concurrent, max_concurrent = 0, 0
         
-        # Iterate over the sorted keys of the dictionary
-        for point in sorted(point_to_count.keys()):
-            concurrent += point_to_count[point]    # Update currently active intervals
-            max_concurrent = max(max_concurrent, concurrent)    # Update max intervals
+        for i in range(range_start, range_end + 1):
+            # Update currently active intervals
+            concurrent += point_to_count[i]
+            max_concurrent = max(max_concurrent, concurrent)
             
         return max_concurrent
