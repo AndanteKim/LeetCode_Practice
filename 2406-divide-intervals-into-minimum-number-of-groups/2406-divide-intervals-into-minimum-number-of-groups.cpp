f874353d28bbd49a74d74cc6d7d4bc173da780ff
Andrew Kim <ksb0724@utexas.edu>
2024-10-12 17:24:35 +0900

    Time: 232 ms (92.57%), Space: 214.6 MB (14.41%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        // Use a map to store the points and their counts
-        map<int, int> pointToCount;
-        
-        // Mark the starting and ending points in the dictionary
+        // Find the minimum and maximum value in the interval
+        int rangeStart = INT_MAX, rangeEnd = INT_MIN;
         for (const auto& interval : intervals){
-            ++pointToCount[interval[0]];    // Start of an interval
-            --pointToCount[interval[1] + 1];    // End of an interval (interval[1] + 1)
+            rangeStart = min(rangeStart, interval[0]);
+            rangeEnd = max(rangeEnd, interval[1]);
+        }
+        
+        // Initialize the list with all zeroes
+        vector<int> pointToCount(rangeEnd + 2);
+        for (const auto& interval : intervals){
+            ++pointToCount[interval[0]];
+            --pointToCount[interval[1] + 1];
         }
         
         int concurrent = 0, maxConcurrent = 0;
-        
-        // Iterate over the sorted keys of the dictionary
-        for (const auto& [time, count] : pointToCount){
-            concurrent += count;    // Update currently active intervals
-            maxConcurrent = max(maxConcurrent, concurrent); // Update max intervals
+        for (int i = rangeStart; i <= rangeEnd; ++i){
+            // Update currently active intervals
+            concurrent += pointToCount[i];
+            maxConcurrent = max(maxConcurrent, concurrent);
         }
         
         return maxConcurrent;
