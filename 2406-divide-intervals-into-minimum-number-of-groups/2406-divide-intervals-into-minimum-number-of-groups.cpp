a86ea29d2df6f3d3be91a98e46ad296829d8dd7b
Andrew Kim <ksb0724@utexas.edu>
2024-10-12 17:16:12 +0900

    Time: 416 ms (25.67%), Space: 121.9 MB (43.92%) - LeetHub

--- 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2406-divide-intervals-into-minimum-number-of-groups/2406-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int minGroups(vector<vector<int>>& intervals) {
-        // Convert the intervals to two events
-        // start as (start, 1) and end as (end + 1, -1)
-        vector<pair<int, int>> events;
+        // Use a map to store the points and their counts
+        map<int, int> pointToCount;
         
+        // Mark the starting and ending points in the dictionary
         for (const auto& interval : intervals){
-            events.push_back({interval[0], 1});     // Start event
-            events.push_back({interval[1] + 1, -1});    // end event (interval[1] + 1);
+            ++pointToCount[interval[0]];    // Start of an interval
+            --pointToCount[interval[1] + 1];    // End of an interval (interval[1] + 1)
         }
-        
-        // Sort the events first by time, and then by type (1 for start. -1 for end)
-        sort(events.begin(), events.end(), [](pair<int, int> &a, pair<int, int>& b){
-            return (a.first == b.first)? a.second < b.second : a.first < b.first;
-        });
         
         int concurrent = 0, maxConcurrent = 0;
         
-        // Sweep through the event
-        for (auto& [time, check] : events) {
-            concurrent += check;    // Track currently active intervals
-            maxConcurrent = max(maxConcurrent, concurrent);
+        // Iterate over the sorted keys of the dictionary
+        for (const auto& [time, count] : pointToCount){
+            concurrent += count;    // Update currently active intervals
+            maxConcurrent = max(maxConcurrent, concurrent); // Update max intervals
         }
         
         return maxConcurrent;
