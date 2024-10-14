db6f2281340bfca195f58c52580304e6d63c2d95
Andrew Kim <ksb0724@utexas.edu>
2024-10-14 09:47:24 +0900

    Time: 767 ms (18.35%), Space: 31.6 MB (8.23%) - LeetHub

--- 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
+++ 2530-maximal-score-after-applying-k-operations/2530-maximal-score-after-applying-k-operations.py
@@ -1,14 +1,17 @@
 class Solution:
     def maxKelements(self, nums: List[int], k: int) -> int:
+        # Define a max-heap by using a min-heap with negative values
         pq = []
         for num in nums:
             heappush(pq, -num)
         
         ans = 0
         while pq and k > 0:
-            num = heappop(pq)
-            num *= -1
+            # Retrieve the max element (invert the sign because it's stored as negative.)
+            num = -heappop(pq)
             ans += num
+            
+            # Add one-third of the max element back to the heap. Rounded up using integer division.
             num = num // 3 + 1 if (num // 3) != num / 3 else num // 3
             k -= 1
             if num > 0:
