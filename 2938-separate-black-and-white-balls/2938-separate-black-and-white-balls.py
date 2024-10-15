0cc6f2e0d6b39e7b1ee576165eaa8ec813eba034
Andrew Kim <ksb0724@utexas.edu>
2024-10-15 12:48:08 +0900

    Time: 83 ms (77.91%), Space: 17.5 MB (54.03%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.py
@@ -1,14 +1,12 @@
 class Solution:
     def minimumSteps(self, s: str) -> int:
-        white_pos, ans = 0, 0
+        ans, black = 0, 0
         
         # Iterate through each ball in the string
-        for curr_pos, ch in enumerate(s):
-            if ch == '0':
-                # Calculate the number of swaps needed
-                ans += curr_pos - white_pos
-                
-                # Move the next available position for a white ball one step to the right
-                white_pos += 1
+        for c in s:
+            if c == '0':
+                ans += black
+            else:
+                black += 1
                 
         return ans
