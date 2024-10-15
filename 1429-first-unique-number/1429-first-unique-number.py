8b61a35deea7683fbc14a5b4dbf479a536f8dd5b
Andrew Kim <ksb0724@utexas.edu>
2024-10-16 02:18:45 +0900

    Time: 543 ms (67.81%), Space: 74.9 MB (18.28%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.py
+++ 1429-first-unique-number/1429-first-unique-number.py
@@ -1,20 +1,36 @@
+from collections import OrderedDict
+
 class FirstUnique:
 
     def __init__(self, nums: List[int]):
-        self.queue, self.seen = deque(), dict()
+        self._queue = OrderedDict()
+        self._is_unique = dict()
+        
         for num in nums:
+            # Notice that we're calling the "add" method of FirstUnique; not of the queue.
             self.add(num)
 
     def showFirstUnique(self) -> int:
-        while self.queue and self.seen[self.queue[0]] > 1:
-            self.queue.popleft()
-        
-        return -1 if not self.queue else self.queue[0]
+        # Check if there's still a value left in the queue. There might be no uniques.
+        if self._queue:
+            # We don't want to actually remove the value.
+            # Seeing as OrderedDict has no "get first" method, the way that we can get
+            # the first value is to create an iterator, and then get the "next" value
+            # from that. Note that this is O(1).
+            return next(iter(self._queue))
+        return -1
 
     def add(self, value: int) -> None:
-        if value not in self.seen:
-            self.queue.append(value)
-        self.seen[value] = self.seen.get(value, 0) + 1
+        # Case 1: We need to add the number to the queue and mark it as unique.
+        if value not in self._is_unique:
+            self._is_unique[value] = True
+            self._queue[value] = None
+        # Case 2: We need to mark the value as no longer unique and then
+        # remove it from the queue.
+        elif self._is_unique[value]:
+            self._is_unique[value] = False
+            self._queue.pop(value)
+            
 
 
 # Your FirstUnique object will be instantiated and called as such:
