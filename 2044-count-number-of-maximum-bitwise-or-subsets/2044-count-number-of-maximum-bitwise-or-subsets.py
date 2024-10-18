1c6ae655c51bc150011ce1b3acc3c8b0830e0a8f
Andrew Kim <ksb0724@utexas.edu>
2024-10-19 05:01:42 +0900

    Time: 231 ms (78.57%), Space: 335.1 MB (5.41%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,9 +1,13 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        def backtrack(i: int, curr_or: int, target_or: int) -> int:
+        def backtrack(i: int, curr_or:int, target_or: int) -> int:
             # Base case: Reached the end of the array
             if i == n:
                 return 1 if curr_or == target_or else 0
+            
+            # Check if the result for this state is already memoized
+            if memo[i][curr_or] != -1:
+                return memo[i][curr_or]
             
             # Don't include the current number
             count_without = backtrack(i + 1, curr_or, target_or)
@@ -11,13 +15,18 @@ class Solution:
             # Include the current number
             count_with = backtrack(i + 1, curr_or | nums[i], target_or)
             
-            # Return the sum of both cases
-            return count_without + count_with
+            # Memoize and return the result
+            memo[i][curr_or] = count_without + count_with
+            return memo[i][curr_or]
         
-        maximum = 0
+        max_or_val = 0
         
+        # Caculate the maximum OR value
         for num in nums:
-            maximum |= num
-        
+            max_or_val |= num
+            
         n = len(nums)
-        return backtrack(0, 0, maximum)
+        
+        # Initialize memo with -1
+        memo = [[-1] * (max_or_val + 1) for _ in range(n)]
+        return backtrack(0, 0, max_or_val)
