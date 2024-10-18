5e1aff2de95bd2d86d74fa85338ea276e81c8aa1
Andrew Kim <ksb0724@utexas.edu>
2024-10-19 05:21:47 +0900

    Time: 114 ms (12.52%), Space: 308.9 MB (9.83%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,25 +1,34 @@
 class Solution {
 private:
     int n;
-    int backtrack(int i, int currOr, int targetOr, vector<int>& nums){
+    
+    int backtrack(int i, int currOr, int maxOr, vector<int>& nums, vector<vector<int>>& memo){
         // Base case: Reached the end of the array
-        if (i == n) return (currOr == targetOr)? 1 : 0;
+        if (i == n) return (currOr == maxOr)? 1 : 0;
+        
+        // Check if the result for this state is already memoized
+        if (memo[i][currOr] != -1) return memo[i][currOr];
         
         // Don't include the current number
-        int countWithout = backtrack(i + 1, currOr, targetOr, nums);
+        int countWithout = backtrack(i + 1, currOr, maxOr, nums, memo);
         
         // Include the current number
-        int countWith = backtrack(i + 1, currOr | nums[i], targetOr, nums);
+        int countWith = backtrack(i + 1, currOr | nums[i], maxOr, nums, memo);
         
-        // Return the sum of both cases
-        return countWithout + countWith;
+        // Memoize and return the result
+        return memo[i][currOr] = countWithout + countWith;
     }
     
 public:
     int countMaxOrSubsets(vector<int>& nums) {
         this -> n = nums.size();
-        int maxOrValue = 0;
-        for (int num : nums) maxOrValue |= num;
-        return backtrack(0, 0, maxOrValue, nums);
+        
+        int maxOrVal = 0;
+        
+        // Calculate the maximum OR value
+        for (int num : nums) maxOrVal |= num;
+        
+        vector memo(n, vector<int>(maxOrVal + 1, -1));
+        return backtrack(0, 0, maxOrVal, nums, memo);
     }
 };
