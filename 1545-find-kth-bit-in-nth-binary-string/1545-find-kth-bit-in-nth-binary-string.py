c9321ab5cd07d3cc3d8d668039c26a20f672f2f0
Andrew Kim <ksb0724@utexas.edu>
2024-10-19 19:27:11 +0900

    Time: 242 ms (46.35%), Space: 24.8 MB (28.60%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,14 +1,17 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
-        @lru_cache(maxsize = None)
-        def recursive(i: int) -> int:
-            # Base case
-            if i <= 1:
-                return "0"
-            
-            half = recursive(i - 1)
-            mid = '1'
-            rest = "".join(['1' if i == '0' else '0' for i in half][::-1])
-            return half + mid + rest
+        sequence = "0"
         
-        return recursive(n)[k - 1]
+        # Generate sequence until we've enough elements or reach the nth iteration
+        for i in range(1, n):
+            if k <= len(sequence):
+                break
+            sequence += "1"
+            
+            # Append the inverted and reversed part of the existing sequence
+            inverted = "".join("1" if bit == "0" else "0" for bit in sequence[:-1])
+            
+            sequence += inverted[::-1]
+            
+        # Return the kth bit
+        return sequence[k - 1]
