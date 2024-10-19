135072d368de86e8ae43c2130a30a805e06738cf
Andrew Kim <ksb0724@utexas.edu>
2024-10-20 03:22:26 +0900

    Time: 2818 ms (5.18%), Space: 17.7 MB (22.73%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -1,24 +1,17 @@
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        max_or_val, n = 0, len(nums)
+        max_or_val, dp = 0, [0] * (1 << 17)
         
+        # Initialize the empty subset
+        dp[0] = 1
+        
+        # Iterate through each number in the input array
         for num in nums:
+            for i in range(max_or_val, -1, -1):
+                # For each existing subset, create a new subset by including the current number
+                dp[i | num] += dp[i]
+            
+            # Update the maximum OR value
             max_or_val |= num
-            
-        # 2^n subsets
-        total_subsets, subsets_with_max_or = 1 << n, 0
-        
-        # Iterate through all possible subsets
-        for subset_mask in range(total_subsets):
-            curr_or_val = 0
-            
-            # Calculate OR value for the current subset
-            for i in range(n):
-                if (subset_mask >> i) & 1:
-                    curr_or_val |= nums[i]
-                    
-            # If current subset's OR equals max_or_val, increment count
-            if curr_or_val == max_or_val:
-                subsets_with_max_or += 1
-                
-        return subsets_with_max_or
+
+        return dp[max_or_val]
