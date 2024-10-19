c480246d1bf558bbb734be4e8b84ab897abf6309
Andrew Kim <ksb0724@utexas.edu>
2024-10-20 02:18:02 +0900

    Time: 78 ms (23.50%), Space: 9.9 MB (93.49%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,34 +1,28 @@
 class Solution {
-private:
-    int n;
-    
-    int backtrack(int i, int currOr, int maxOr, vector<int>& nums, vector<vector<int>>& memo){
-        // Base case: Reached the end of the array
-        if (i == n) return (currOr == maxOr)? 1 : 0;
-        
-        // Check if the result for this state is already memoized
-        if (memo[i][currOr] != -1) return memo[i][currOr];
-        
-        // Don't include the current number
-        int countWithout = backtrack(i + 1, currOr, maxOr, nums, memo);
-        
-        // Include the current number
-        int countWith = backtrack(i + 1, currOr | nums[i], maxOr, nums, memo);
-        
-        // Memoize and return the result
-        return memo[i][currOr] = countWithout + countWith;
-    }
-    
 public:
     int countMaxOrSubsets(vector<int>& nums) {
-        this -> n = nums.size();
+        // Calculate the maximum possible OR value
+        int n = nums.size(), maxOrVal = 0;
         
-        int maxOrVal = 0;
+        for (int num : nums)
+            maxOrVal |= num;
         
-        // Calculate the maximum OR value
-        for (int num : nums) maxOrVal |= num;
+        int totalSubsets = (1 << n);    // 2^n subsets
+        int subsetsWithMaxOr = 0;
         
-        vector memo(n, vector<int>(maxOrVal + 1, -1));
-        return backtrack(0, 0, maxOrVal, nums, memo);
+        // Iterate through all possible subsets
+        for (int subsetMask = 0; subsetMask < totalSubsets; ++subsetMask){
+            int currOrVal = 0;
+            
+            // Calculate OR value for the current subset
+            for (int i = 0; i < n; ++i)
+                if (((subsetMask >> i) & 1) == 1)
+                    currOrVal |= nums[i];
+                    
+            // If current subset's OR equals maxOrVal, increment count
+            if (currOrVal == maxOrVal) ++subsetsWithMaxOr;
+        }
+        
+        return subsetsWithMaxOr;
     }
 };
