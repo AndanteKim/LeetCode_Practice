e4bb1de07839efa659a176a596c60956cf7893b6
Andrew Kim <ksb0724@utexas.edu>
2024-10-21 00:37:51 +0900

    Time: 0 ms (100.00%), Space: 9.5 MB (84.54%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.cpp
@@ -2,55 +2,43 @@ class Solution {
 public:
     bool parseBoolExpr(string expression) {
         stack<char> st;
+        string ops{"&|!"}, types{"&|!tf"};
         
-        // Traverse the entire expression
-        for (const char& ch : expression){
-            if (ch == ')'){
-                vector<char> values;
-                
-                // Gather all values inside the parentheses
-                while (st.top() != '('){
-                    values.push_back(st.top());
-                    st.pop();   
-                }
-                st.pop();   // Remove '('
-                char op = st.top();
-                st.pop();   // Remove the operator
-                
-                // Evaluate the subexpression and push the result back
-                char res = evalSubExpr(op, values);
-                st.push(res);
-            }
-            else if (ch != ',')
-                // Push non-comma characters into the stack
-                st.push(ch);
-        }
-        
-        // Final result is on the top of the stack
-        return st.top() == 't';
-    }
-    
-private:
-    // Evaluates a subexpression based on the operator and list of values
-    char evalSubExpr(const char& op, const vector<char>& values){
-        if (op == '!')
-            return (values[0] == 't')? 'f' : 't';
-        
-        // AND: return 'f' if any value is 'f', otherwise return 't'
-        if (op == '&'){
-            for (const char& val : values)
-                if (val == 'f') return 'f';
-            return 't';
-        }
-        
-        // OR: return 't' if any value is 't', otherwise return 'f'
-        if (op == '|'){
-            for (const char& val : values)
-                if (val == 't') return 't';
-            return 'f';
-        }
+        // Traverse through the expression
+        for (const char& c : expression){
+            if (c == ',' || c == '(')
+                continue;   // Skip commas and open parentheses
             
-        // This point should never be reached
-        return 'f';
+            // Push operators and boolean values to the stack
+            if (types.find(c) != string::npos)
+                st.push(c);
+            
+            // Handle closing parentheses and evaluate the subexpression
+            if (c == ')'){
+                bool hasTrue = false, hasFalse = false;
+                
+                // Process the values insdie the parentheses
+                while (ops.find(st.top()) == string::npos){
+                    if (st.top() == 't')
+                        hasTrue = true;
+                    
+                    if (st.top() == 'f')
+                        hasFalse = true;
+                    st.pop();
+                }
+                
+                // Pop the operator and evaluate the subexpression
+                char op = st.top(); st.pop();
+                if (op == '!')
+                    st.push(hasTrue? 'f' : 't');
+                else if (op == '&')
+                    st.push(hasFalse? 'f' : 't');
+                else
+                    st.push(hasTrue? 't' : 'f');
+            }
+        }
+        
+        // The final result is at the top of the stack
+        return st.top() == 't';
     }
 };
