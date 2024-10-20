cec49e35c935e6f9e775b899724e3bbade12932e
Andrew Kim <ksb0724@utexas.edu>
2024-10-21 00:26:35 +0900

    Time: 11 ms (100.00%), Space: 16.8 MB (50.60%) - LeetHub

--- 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
+++ 1106-parsing-a-boolean-expression/1106-parsing-a-boolean-expression.py
@@ -1,44 +1,39 @@
 class Solution:
-    # Evaluates a subexpression based on the operator and list of values
-    def _eval_sub_expr(self, op: chr, values: List[chr]) -> chr:
-        if op == '!':
-            return 'f' if values[0] == 't' else 't'
-        
-        # AND: return 'f' if any value is 'f', otherwise return 't'
-        if op == '&':
-            for val in values:
-                if val == 'f':
-                    return 'f'
-            return 't'
-            
-        # OR: return 't' if any value is 't', otherwise return 'f'
-        if op == '|':
-            for val in values:
-                if val == 't':
-                    return 't'
-            return 'f'
-            
-        return 'f'      # This point should never be reached
-    
     def parseBoolExpr(self, expression: str) -> bool:
         st = []
         
-        # Traverse the entire expression
+        # Traverse through the expression
         for c in expression:
+            if c == ',' or c == '(':
+                continue    # Skip commas and open parentheses
+                
+            # Push operators and boolean values to the stack
+            if c in "tf!&|":
+                st.append(c)
+                
+            # Handle closing parentheses and evaluate the subexpression
             if c == ')':
-                values = []
+                has_true, has_false = False, False
                 
-                # Gather all values inside the parentheses
-                while st[-1] != '(':
-                    values.append(st.pop())
-                st.pop()    # Remove '('
-                op = st.pop()   # Remove the operator
+                # Process the values inside the parenthesis
+                while st[-1] not in '!&|':
+                    val = st.pop()
+                    if val == 't':
+                        has_true = True
+                    
+                    if val == 'f':
+                        has_false = True
                 
-                # Evaluate the subexpression and push the result back
-                res = self._eval_sub_expr(op, values)
-                st.append(res)
-            elif c != ',':
-                st.append(c)    # Push non-comma characters into the stack
+                # Pop the operator and evaluate the subexpression
+                op = st.pop()
+                if op == '!':
+                    st.append('t' if not has_true else 'f')
                 
-        # Final result is on the top of the stack
+                elif op == '&':
+                    st.append('f' if has_false else 't')
+                else:
+                    st.append('t' if has_true else 'f')
+        
+        # The final result is at the top of the stack
         return st[-1] == 't'
+        
