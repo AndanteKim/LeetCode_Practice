945fa540c6c068eee3757b521d405c1307591c8d
Andrew Kim <ksb0724@utexas.edu>
2024-10-25 04:41:44 +0900

    Time: 0 ms (100.00%), Space: 14.5 MB (83.88%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.cpp
@@ -10,23 +10,44 @@
  * };
  */
 class Solution {
+private:
+    // Checks whether the given pair of nodes should be examined be
+    // pushed into the stack
+    bool checkNodeValues(TreeNode* node1, TreeNode* node2){
+        if (!(node1 || node2)) return true;
+        
+        if (node1 && node2 && node1 -> val == node2 -> val) return true;
+        
+        return false;
+    }
+    
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        // Both trees are empty
-        if (!(root1 || root2)) return true;
+        // Initialize stack to store pairs of nodes
+        stack<pair<TreeNode*, TreeNode*>> s;
+        s.push({root1, root2});
         
-        // Just one of trees is empty
-        if (!(root1 && root2)) return false;
+        // While the stack isn't empty
+        while (!s.empty()){
+            auto [node1, node2] = s.top(); s.pop();
+            
+            if (!(node1 || node2)) continue;
+            if (!(node1 && node2)) return false;
+            if (node1 -> val != node2 -> val) return false;
+            
+            // Check both configurations: no swap and swap
+            if (checkNodeValues(node1 -> left, node2 -> left) && checkNodeValues(node1 -> right, node2 -> right)){
+                s.push({node1 -> left, node2 -> left});
+                s.push({node1 -> right, node2 -> right});
+            }
+            else if (checkNodeValues(node1 -> left, node2 -> right) && checkNodeValues(node1 -> right, node2 -> left)){
+                s.push({node1 -> left, node2 -> right});
+                s.push({node1 -> right, node2 -> left});
+            }
+            else
+                return false;
+        }
         
-        // Corresponding values differ
-        if (root1 -> val != root2 -> val) return false;
-        
-        // Check if corresponding subtrees are flip equivalent
-        bool noSwap = flipEquiv(root1 -> left, root2 -> left) && flipEquiv(root1 -> right, root2 -> right);
-        
-        // Check if opposite subtrees are flip equivalent
-        bool swap = flipEquiv(root1 -> left, root2 -> right) && flipEquiv(root1 -> right, root2 -> left);
-        
-        return noSwap || swap;
+        return true;
     }
 };
