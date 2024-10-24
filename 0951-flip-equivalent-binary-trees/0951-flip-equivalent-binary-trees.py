14adc8491e4b55b14418d7a0f1d95d195f94c9bf
Andrew Kim <ksb0724@utexas.edu>
2024-10-25 04:35:32 +0900

    Time: 0 ms (100.00%), Space: 16.6 MB (58.31%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
@@ -6,22 +6,41 @@
 #         self.right = right
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        # Both trees are empty
-        if not (root1 or root2):
-            return True
-        
-        # Just one of the trees is empty
-        if not (root1 and root2):
+        # Checks whether the given pair of nodes should be examined -
+        # be pushed into the stack
+        def check_node_values(node1: TreeNode, node2: TreeNode) -> bool:
+            if not (node1 or node2):
+                return True
+            
+            if node1 and node2 and node1.val == node2.val:
+                return True
+            
             return False
         
-        # Corresponding values differ
-        if root1.val != root2.val:
-            return False
+        # Initialize stack to store pairs of nodes
+        s = [(root1, root2)]
         
-        # Check if corresponding subtrees are flip equivalent
-        no_swap = self.flipEquiv(root1.left, root2.left) and self.flipEquiv(root1.right, root2.right)
-        
-        # Check if opposite subtrees are flip equivalent
-        swap = self.flipEquiv(root1.left, root2.right) and self.flipEquiv(root1.right, root2.left)
-        
-        return no_swap or swap
+        while s:
+            node1, node2 = s.pop()
+            
+            if not (node1 or node2):
+                continue
+                
+            if not (node1 and node2):
+                return False
+            
+            if node1.val != node2.val:
+                return False
+            
+            # Check both configurations: no swap and swap
+            if check_node_values(node1.left, node2.left) and check_node_values(node1.right, node2.right):
+                s.append((node1.left, node2.left))
+                s.append((node1.right, node2.right))
+            elif check_node_values(node1.left, node2.right) and check_node_values(node1.right, node2.left):
+                s.append((node1.left, node2.right))
+                s.append((node1.right, node2.left))
+            else:
+                return False
+            
+        return True
+            
