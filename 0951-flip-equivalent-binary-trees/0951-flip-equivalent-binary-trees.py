6fda42b8b7640730a11c2231a7f354b482e4d08f
Andrew Kim <ksb0724@utexas.edu>
2024-10-25 04:48:50 +0900

    Time: 0 ms (100.00%), Space: 16.6 MB (58.31%) - LeetHub

--- 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
+++ 0951-flip-equivalent-binary-trees/0951-flip-equivalent-binary-trees.py
@@ -5,42 +5,44 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        # Checks whether the given pair of nodes should be examined -
-        # be pushed into the stack
-        def check_node_values(node1: TreeNode, node2: TreeNode) -> bool:
-            if not (node1 or node2):
-                return True
-            
-            if node1 and node2 and node1.val == node2.val:
-                return True
-            
+    def find_canonical_form(self, node: TreeNode) -> None:
+        if not node:
+            return
+        
+        # Post-order traversal: first bring subtrees into their canonical form
+        self.find_canonical_form(node.left)
+        self.find_canonical_form(node.right)
+        
+        if not node.right:
+            return
+        
+        # Swap subtrees, so that left is non-empty
+        if not node.left:
+            node.left = node.right
+            node.right = None
+            return
+        
+        left = node.left
+        right = node.right
+        
+        # Swap subtrees
+        if left.val > right.val:
+            node.left, node.right = node.right, node.left
+    
+    def are_equivalent(self, node1: TreeNode, node2: TreeNode) -> bool:
+        if not (node1 or node2):
+            return True
+        
+        if not (node1 and node2):
             return False
         
-        # Initialize stack to store pairs of nodes
-        s = [(root1, root2)]
+        if node1.val != node2.val:
+            return False
         
-        while s:
-            node1, node2 = s.pop()
-            
-            if not (node1 or node2):
-                continue
-                
-            if not (node1 and node2):
-                return False
-            
-            if node1.val != node2.val:
-                return False
-            
-            # Check both configurations: no swap and swap
-            if check_node_values(node1.left, node2.left) and check_node_values(node1.right, node2.right):
-                s.append((node1.left, node2.left))
-                s.append((node1.right, node2.right))
-            elif check_node_values(node1.left, node2.right) and check_node_values(node1.right, node2.left):
-                s.append((node1.left, node2.right))
-                s.append((node1.right, node2.left))
-            else:
-                return False
-            
-        return True
-            
+        return self.are_equivalent(node1.left, node2.left) and self.are_equivalent(node1.right, node2.right)
+    
+    def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        self.find_canonical_form(root1)
+        self.find_canonical_form(root2)
+        
+        return self.are_equivalent(root1, root2)
