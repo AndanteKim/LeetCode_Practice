ccddcaedc3c1a302e168dad3dd147a124791506c
Andrew Kim <ksb0724@utexas.edu>
2024-10-26 01:05:44 +0900

    Time: 42 ms (88.79%), Space: 29.6 MB (93.10%) - LeetHub

---
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,30 @@
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        # Create a set to store all folder paths for fast lookup
+        folder_set = set(folder)
+        ans = []
+        
+        # Iterate through each folder to check if it's a sub-folder
+        for f in folder:
+            is_sub_folder = False
+            prefix = f
+            
+            # Check all prefixes of the current folder path
+            while not prefix == "":
+                pos = prefix.rfind("/")
+                if pos == -1:
+                    break
+                    
+                # Reduce the prefix to its parent folder
+                prefix = prefix[0:pos]
+                
+                # If the parent folder exists in the set, mark as sub-folder
+                if prefix in folder_set:
+                    is_sub_folder = True
+                    break
+                
+            # If not a sub-folder, add it to the result
+            if not is_sub_folder: 
+                ans.append(f)
+            
+        return ans
