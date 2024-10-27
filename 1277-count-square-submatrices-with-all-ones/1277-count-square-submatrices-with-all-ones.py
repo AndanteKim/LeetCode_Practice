b10546cec2b9b9287c783add076e6a51ab3eec2d
Andrew Kim <ksb0724@utexas.edu>
2024-10-27 16:57:47 +0900

    Time: 59 ms (72.46%), Space: 19.4 MB (15.31%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -1,33 +1,12 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
-        def solve(i: int, j: int) -> int:
-            # If the cell lies outside the grid, return 0
-            if i >= m or j >= n:
-                return 0
-            
-            if not matrix[i][j]:
-                return 0
-            
-            # If we've already visited this cell, return the memoized value.
-            if dp[i][j] != -1:
-                return dp[i][j]
-            
-            # Find the answer for the cell to the right of the current cell.
-            right = solve(i, j + 1)
-            
-            # Find the answer for the cell to the diagonal of the current cell.
-            diagonal = solve(i + 1, j + 1)
-            
-            # Find the answer for the cell to the below of the current cell.
-            below = solve(i + 1, j)
-            dp[i][j] = 1 + min(right, diagonal, below)
-            return dp[i][j]
-            
         m, n, ans = len(matrix), len(matrix[0]), 0
-        dp = [[-1] * n for _ in range(m)]
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
         
         for i in range(m):
             for j in range(n):
-                ans += solve(i, j)
-                
+                if matrix[i][j]:
+                    # min(right, diagonal, left) + 1
+                    dp[i + 1][j + 1] = 1 + min(dp[i][j + 1], dp[i][j], dp[i + 1][j])
+                    ans += dp[i + 1][j + 1]
         return ans
