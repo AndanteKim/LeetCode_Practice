c69563d0f8d025eb5d154046f4f922c408e137c3
Andrew Kim <ksb0724@utexas.edu>
2024-11-02 00:49:07 +0900

    Time: 2250 ms (5.63%), Space: 48.1 MB (44.38%) - LeetHub

--- 2955-number-of-same-end-substrings/2955-number-of-same-end-substrings.py
+++ 2955-number-of-same-end-substrings/2955-number-of-same-end-substrings.py
@@ -1,29 +1,33 @@
 class Solution:
     def sameEndSubstringCount(self, s: str, queries: List[List[int]]) -> List[int]:
-        # Dictionary to store each character and its positions in the string 's'
-        n, char_pos_map = len(s), defaultdict(list)
+        n = len(s)
+        # 2D list to store prefix sum of character frequencies for each character 'a' to 'z'
+        char_freq_prefix_sum = [[0] * n for _ in range(26)]
         
-        # Traverse the string and store the index of each character in the dictionary
+        # Fill the frequency array
         for i in range(n):
-            char_pos_map[s[i]].append(i)
+            char_freq_prefix_sum[ord(s[i]) - 97][i] += 1
         
+        # Convert the frequency array into a prefix sum array
+        for freq in char_freq_prefix_sum:
+            for j in range(1, n):
+                freq[j] += freq[j - 1]
+                
         ans = []
         
         # Process each query
-        for left_idx, right_idx in queries:
+        for left_index, right_index in queries:
             count_same_end_substrings = 0
             
-            # For each unique character in the string, calculate the number of same-end substrings
-            for positions in char_pos_map.values():
-                # Get the number of occurrences of the character within the range [left_index, right_index]
-                left_bound = bisect_left(positions, left_idx)
-                right_bound = bisect_right(positions, right_idx)
-                num_occurrences = right_bound - left_bound
+            # For each character, calculate the frequency of occurrences within the query range
+            for freq in char_freq_prefix_sum:
+                left_freq = 0 if left_index == 0 else freq[left_index - 1]
+                right_freq = freq[right_index]
+                freq_in_range = right_freq - left_freq
                 
                 # Calculate the number of same-end substrings for this character
-                count_same_end_substrings += ((num_occurrences * (num_occurrences + 1)) >> 1)
+                count_same_end_substrings += (freq_in_range * (freq_in_range + 1) >> 1)
             
-            # Store the result for this query
             ans.append(count_same_end_substrings)
-            
+        
         return ans
