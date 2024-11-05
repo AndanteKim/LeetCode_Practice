9cbe3f97f96d20d59527a51acb9aed4eee8cd1f0
Andrew Kim <ksb0724@utexas.edu>
2024-11-05 18:56:23 +0900

    Time: 21 ms (55.41%), Space: 16.9 MB (74.80%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.py
@@ -1,25 +1,4 @@
 class Solution:
     def minChanges(self, s: str) -> int:
-        # Initialize with first character
-        curr_char, consec_cnt = s[0], 0
-        min_changes_req = 0
-        
-        # Iterate through each character
-        for ch in s:
-            # If current character matches the previous sequence
-            if ch == curr_char:
-                consec_cnt += 1
-                continue
-            
-            # If we have even count of characters, start new sequence
-            if consec_cnt % 2 == 0:
-                consec_cnt = 1
-            # If odd count, we need to change the current character
-            else: 
-                consec_cnt = 0
-                min_changes_req += 1
-            
-            # Update the current character for next iteration
-            curr_char = ch
-            
-        return min_changes_req
+        # Count changes needed for each unmatched pair
+        return sum(s[i] != s[i + 1] for i in range(0, len(s), 2))
