b44c0d30884c35d63c463ab39790470db648dc63
Andrew Kim <ksb0724@utexas.edu>
2024-11-05 19:00:41 +0900

    Time: 0 ms (100.00%), Space: 12.3 MB (16.88%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -4,7 +4,7 @@ public:
         int minChangesRequired = 0;
         
         // Check pairs of characters (i, i + 1) with step size 2
-        for (int i = 0; i < s.size() - 1; i += 2){
+        for (int i = 0; i < s.size(); i += 2){
             // If characters in current pair don't match
             // we need one change to make them equal.
             if (s[i] != s[i + 1])
