0d9cca44c46d7572444633e0d2188354e80132f5
Andrew Kim <ksb0724@utexas.edu>
2024-11-06 17:25:36 +0900

    Time: 0 ms (100.00%), Space: 31.3 MB (63.03%) - LeetHub

--- 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
+++ 3011-find-if-array-can-be-sorted/3011-find-if-array-can-be-sorted.cpp
@@ -1,24 +1,38 @@
 class Solution {
 public:
     bool canSortArray(vector<int>& nums) {
+        // Number of set bits of the elements in the current segment
         int n = nums.size();
+        int maxOfSegment = nums[0], minOfSegment = nums[0];
+        int numOfSetBits = __builtin_popcount(nums[0]);
         
-        // Avoid modifying the input directly
-        // Create a copy of input array
-        for (int i = 0; i < n; ++i){
-            for (int j = 0; j < n - i - 1; ++j){
-                if (nums[j] <= nums[j + 1])
-                    // No swap needed
-                    continue;
-                else{
-                    if (__builtin_popcount(nums[j]) == __builtin_popcount(nums[j + 1]))
-                        // Swap the elements
-                        swap(nums[j], nums[j + 1]);
-                    else
-                        return false;
-                }
+        // Initialize max of the previous segment to the smallest possible integer
+        int maxOfPrevSegment = std::numeric_limits<int>::min();
+        
+        for (int i = 1; i < n; ++i){
+            if (numOfSetBits == __builtin_popcount(nums[i])){
+                // Element belongs to the same segment
+                // Update min and max values of the segment
+                maxOfSegment = max(maxOfSegment, nums[i]);
+                minOfSegment = min(minOfSegment, nums[i]);
+            }
+            else{   // Element belongs to a new segment
+                // Check if the segments are arranged properly
+                if (minOfSegment < maxOfPrevSegment)
+                    return false;
+                
+                // Update the previous segment's max
+                maxOfPrevSegment = maxOfSegment;
+                
+                // Start a new segment with the current element
+                maxOfSegment = nums[i];
+                minOfSegment = nums[i];
+                numOfSetBits = __builtin_popcount(nums[i]);
             }
         }
+        
+        // Final check for proper segment arrangement
+        if (minOfSegment < maxOfPrevSegment) return false;
         
         return true;
     }
