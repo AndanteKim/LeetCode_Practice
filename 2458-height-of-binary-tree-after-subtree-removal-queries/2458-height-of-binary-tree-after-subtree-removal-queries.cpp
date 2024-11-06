a6918939323ef94fd321c647bda87fb5bd670053
Andrew Kim <ksb0724@utexas.edu>
2024-11-06 21:30:53 +0900

    Time: 595 ms (25.05%), Space: 479.5 MB (5.55%) - LeetHub

--- 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
+++ 2458-height-of-binary-tree-after-subtree-removal-queries/2458-height-of-binary-tree-after-subtree-removal-queries.cpp
@@ -11,66 +11,54 @@
  */
 class Solution {
 private:
-    // Depth-first search to populate nodeIndexMap and nodeDepths
-    void dfs(TreeNode* node, int depth, unordered_map<int, int>& nodeIndexMap, vector<int>& nodeDepths){
+    // Depth-First Search to build the Euler tour and store node information
+    void dfs(TreeNode* node, int height, vector<int>& eulerTour, unordered_map<int, int>& nodeHeights, unordered_map<int, int>& firstOccurrence, unordered_map<int, int>& lastOccurrence){
         if (!node) return;
         
-        nodeIndexMap[node -> val] = nodeDepths.size();
-        nodeDepths.push_back(depth);
-        dfs(node -> left, depth + 1, nodeIndexMap, nodeDepths);
-        dfs(node -> right, depth + 1, nodeIndexMap, nodeDepths);
-    }
-    
-    // Calculate the size of the subtree for each node
-    int calculateSubtreeSize(TreeNode* node, unordered_map<int, int>& subtreeSize){
-        if (!node) return 0;
+        nodeHeights[node -> val] = height;
+        firstOccurrence[node -> val] = eulerTour.size();
+        eulerTour.push_back(node -> val);
         
-        int leftSize = calculateSubtreeSize(node -> left, subtreeSize);
-        int rightSize = calculateSubtreeSize(node -> right, subtreeSize);
-        int totalSize = leftSize + rightSize + 1;
-        subtreeSize[node -> val] = totalSize;
-        return totalSize;
+        dfs(node -> left, height + 1, eulerTour, nodeHeights, firstOccurrence, lastOccurrence);
+        dfs(node -> right, height + 1, eulerTour, nodeHeights, firstOccurrence, lastOccurrence);
+        
+        lastOccurrence[node -> val] = eulerTour.size();
+        eulerTour.push_back(node -> val);
     }
     
 public:
     vector<int> treeQueries(TreeNode* root, vector<int>& queries) {
-        // Map to store the index of each node value and
-        // the number of nodes in the subtree for each node
-        unordered_map<int, int> nodeIndexMap, subtreeSize;
+        // Vectors and maps to store tree information
+        vector<int> eulerTour;
+        unordered_map<int, int> nodeHeights, firstOccurrence, lastOccurrence;
         
-        // Vectors to store node depths and maximum depths from left and right
-        vector<int> nodeDepths, maxDepthFromLeft, maxDepthFromRight;
+        // Perform DFS to build Euler tour and node information
+        dfs(root, 0, eulerTour, nodeHeights, firstOccurrence, lastOccurrence);
         
-        // Perform DFS to populate nodeIndexMap and nodeDepths
-        dfs(root, 0, nodeIndexMap, nodeDepths);
+        int tourSize = eulerTour.size();
+        vector<int> maxDepthLeft(tourSize, 0), maxDepthRight(tourSize, 0);
         
-        int totalNodes = nodeDepths.size();
+        // Initialize the first and last elements of max height arrays
+        maxDepthLeft[0] = maxDepthRight.back() = nodeHeights[root -> val];
         
-        // Calculate subtree sizes
-        calculateSubtreeSize(root, subtreeSize);
-        
-        // Calculate maximum depths from left and right
-        maxDepthFromLeft.push_back(nodeDepths[0]);
-        maxDepthFromRight.push_back(nodeDepths.back());
-        
-        for (int i = 1; i < totalNodes; ++i){
-            maxDepthFromLeft.push_back(max(maxDepthFromLeft[i - 1], nodeDepths[i]));
-            maxDepthFromRight.push_back(max(maxDepthFromRight[i - 1], nodeDepths[totalNodes - i - 1]));
+        // Build maxDepthLeft and maxDepthRight arrays
+        for (int i = 1; i < tourSize; ++i){
+            maxDepthLeft[i] = max(maxDepthLeft[i - 1], nodeHeights[eulerTour[i]]);
         }
         
-        reverse(maxDepthFromRight.begin(), maxDepthFromRight.end());
+        for (int i = tourSize - 2; i >= 0; --i){
+            maxDepthRight[i] = max(maxDepthRight[i + 1], nodeHeights[eulerTour[i]]);
+        }
         
         // Process queries
-        vector<int> ans(queries.size());
-        for (int i = 0; i < queries.size(); ++i){
-            int queryNode = queries[i];
-            int startIndex = nodeIndexMap[queryNode] - 1;
-            int endIndex = startIndex + 1 + subtreeSize[queryNode];
-                
-            int maxDepth = maxDepthFromLeft[startIndex];
-            if (endIndex < totalNodes)
-                maxDepth = max(maxDepth, maxDepthFromRight[endIndex]);
-            ans[i] = maxDepth;
+        int querySize = queries.size();
+        vector<int> ans(querySize);
+        
+        for (int i = 0; i < querySize; ++i){
+            int query = queries[i];
+            int leftMax = (maxDepthLeft[firstOccurrence[query]] > 0)? maxDepthLeft[firstOccurrence[query] - 1]: 0;
+            int rightMax = (maxDepthRight[lastOccurrence[query]] < tourSize - 1)? maxDepthRight[lastOccurrence[query] + 1] : 0;
+            ans[i] = max(leftMax, rightMax);
         }
         
         return ans;
