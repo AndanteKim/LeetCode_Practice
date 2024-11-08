10034557b3d81f07d2f9bd18969753c481e1658c
Andrew Kim <ksb0724@utexas.edu>
2024-11-08 22:29:04 +0900

    Time: 0 ms (100.00%), Space: 14.5 MB (55.03%) - LeetHub

--- 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
+++ 1213-intersection-of-three-sorted-arrays/1213-intersection-of-three-sorted-arrays.cpp
@@ -2,16 +2,26 @@ class Solution {
 public:
     vector<int> arraysIntersection(vector<int>& arr1, vector<int>& arr2, vector<int>& arr3) {
         vector<int> ans;
-        map<int, int> counter;
         
-        // Concatenate all vectors together
-        for (int num : arr1) ++counter[num];
-        for (int num : arr2) ++counter[num];
-        for (int num : arr3) ++counter[num];
+        // Prepare three pointers to iterate through three arrays
+        // p1, p2, and p3 point to the beginning of arr1, arr2, and arr3 accordingly
+        int p1 = 0, p2 = 0, p3 = 0;
         
-        for (auto& [num, freq] : counter){
-            if (freq == 3) ans.push_back(num);
+        while (p1 < arr1.size() && p2 < arr2.size() && p3 < arr3.size()){
+            if (arr1[p1] == arr2[p2] && arr2[p2] == arr3[p3]){
+                ans.push_back(arr1[p1]);
+                ++p1; ++p2; ++p3;
+            }
+            else{
+                if (arr1[p1] < arr2[p2])
+                    ++p1;
+                else if (arr2[p2] < arr3[p3])
+                    ++p2;
+                else
+                    ++p3;
+            }
         }
+        
         return ans;
     }
 };
