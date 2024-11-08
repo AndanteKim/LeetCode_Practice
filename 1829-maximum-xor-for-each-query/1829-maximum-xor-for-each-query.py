d7c801af956314be2c15db35c5b160ddaf861dc2
Andrew Kim <ksb0724@utexas.edu>
2024-11-08 19:44:46 +0900

    Time: 73 ms (29.46%), Space: 35.4 MB (24.13%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -1,15 +1,17 @@
 class Solution:
     def getMaximumXor(self, nums: List[int], maximumBit: int) -> List[int]:
-        n, max_num = len(nums), 2 ** maximumBit - 1
-        start_nums = 0
-        for num in nums:
-            start_nums ^= num
-            
-        ans = [0] * n
-        for i in range(n - 1, -1, -1):
-            k = max_num ^ start_nums
-            start_nums ^= nums[i]
-            ans[i] = k
-        ans.reverse()
+        n = len(nums)
+        prefixXOR = [0] * n
+        prefixXOR[0] = nums[0]
         
+        for i in range(1, n):
+            prefixXOR[i] = prefixXOR[i - 1] ^ nums[i]
+            
+        mask = (1 << maximumBit) - 1
+        ans = [0] * n
+        for i in range(n):
+            # Find k to maximize value
+            product = prefixXOR[n - 1 - i]
+            ans[i] = product ^ mask
+            
         return ans
