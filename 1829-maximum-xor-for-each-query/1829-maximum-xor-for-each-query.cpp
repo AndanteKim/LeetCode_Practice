1c0f17f81fbf4ab8ac1cf291f4222f59ab7440e2
Andrew Kim <ksb0724@utexas.edu>
2024-11-08 19:50:43 +0900

    Time: 1 ms (74.29%), Space: 98.3 MB (43.73%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
-        int n = nums.size(), startNum = 0;
-        int maxNum = (1 << maximumBit) - 1;
+        int n = nums.size();
+        vector<int> prefixXOR(n);
+        prefixXOR[0] = nums[0];
+        
+        for (int i = 1; i < n; ++i)
+            prefixXOR[i] = prefixXOR[i - 1] ^ nums[i];
+        
+        int mask = (1 << maximumBit) - 1;
         vector<int> ans(n);
-        
         for (int i = 0; i < n; ++i){
-            startNum ^= nums[i];
-            ans[i] = maxNum ^ startNum;
+            // Find k to maximize value
+            int product = prefixXOR[n - 1 - i];
+            ans[i] = product ^ mask;
         }
-        
-        reverse(ans.begin(), ans.end());
         
         return ans;
     }
