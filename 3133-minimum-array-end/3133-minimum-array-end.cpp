fe70bd5a41383c8cd9f222bea0efe743786aa086
Andrew Kim <ksb0724@utexas.edu>
2024-11-09 19:17:38 +0900

    Time: 1765 ms (5.36%), Space: 8.3 MB (92.66%) - LeetHub

---
+++ 3133-minimum-array-end/3133-minimum-array-end.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long minEnd(int n, int x) {
+        long long ans = x;
+        
+        // Step 1: Iterate n - 1 times (since we already initialized result with x)
+        while (n-- > 1){
+            // Step 2: Increment result and perform bitwise OR with x
+            ans = (ans + 1) | x;
+        }
+        
+        return ans;
+    }
+};
