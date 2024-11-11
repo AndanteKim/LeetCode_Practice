8a72997680a391699d3c4443285084e516d15132
Andrew Kim <ksb0724@utexas.edu>
2024-11-11 11:51:00 +0900

    Time: 4 ms (55.96%), Space: 28.5 MB (61.98%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.cpp
@@ -1,39 +1,42 @@
 class Solution {
 private:
-    bool checkPrime(int x){
+    bool isPrime(int x){
         for (int i = 2; i <= sqrt(x); ++i){
             if (x % i == 0) return false;
         }
+        
         return true;
     }
     
 public:
     bool primeSubOperation(vector<int>& nums) {
-        int n = nums.size(), bound;
+        int n = nums.size(), maxElement = *max_element(nums.begin(), nums.end());
         
-        for (int i = 0; i < n; ++i){
-            // In case of first index, we need to find the largest prime
-            // less than nums[0].
-            if (i == 0){
-                bound = nums[i];
-            }
+        // Store the previousPrime array.
+        vector<int> previousPrime(maxElement + 1);
+        
+        for (int i = 2; i <= maxElement; ++i){
+            if (isPrime(i))
+                previousPrime[i] = i;
             else
-                // Otherwise, we need to find the largest prime, that makes
-                // the current element closest to the previous element.
+                previousPrime[i] = previousPrime[i - 1];
+        }
+        
+        int bound;
+        for (int i = 0; i < n; ++i){
+            // In case of first index, we need to find the largest prime less than nums[0].
+            if (i == 0) bound = nums[i];
+            else
+                // Otherwise, we need to find the largest prime, that makes the
+                // current element closest to the previous element.
                 bound = nums[i] - nums[i - 1];
             
-            // If the bound is less than or equal to 0, then the array cannot be
+            // If the bound is less than or equal to 0, then the array cannot be 
             // made strictly increasing.
             if (bound <= 0) return false;
             
             // Find the largest prime less than bound.
-            int largestPrime = 0;
-            for (int j = bound - 1; j >= 2; --j){
-                if (checkPrime(j)){
-                    largestPrime = j;
-                    break;
-                }
-            }
+            int largestPrime = previousPrime[bound - 1];
             
             // Subtract this value from nums[i].
             nums[i] -= largestPrime;
