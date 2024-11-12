0bfcf50504af89e601839416a3d72a9893294273
Andrew Kim <ksb0724@utexas.edu>
2024-11-13 03:59:34 +0900

    Time: 43 ms (95.90%), Space: 93.9 MB (64.86%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -1,39 +1,24 @@
 class Solution {
-private:
-    int n;
-    int binarySearch(vector<vector<int>>& items, int targetPrice){
-        int left = 0, right = n - 1, maxBeauty = 0, mid;
-        
-        while (left <= right){
-            mid = (left + right) >> 1;
-            
-            if (targetPrice < items[mid][0])
-                right = mid - 1;
-            else{
-                // Found viable price. Keep moving to right
-                maxBeauty = max(maxBeauty, items[mid][1]);
-                left = mid + 1;
-            }
-        }
-        
-        return maxBeauty;
-    }
-    
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        // Sort and store max beauty
+        // sort both items and queries in ascending order
         sort(items.begin(), items.end());
-        int mx = items[0][1];
-        this -> n = items.size();
-        for (int i = 0; i < n; ++i){
-            mx = max(mx, items[i][1]);
-            items[i][1] = mx;
-        }
+        vector<pair<int, int>> queriesWithIndex;
+        for (int i = 0; i < queries.size(); ++i)
+            queriesWithIndex.push_back({queries[i], i});
         
-        int qSize = queries.size();
-        vector<int> ans(qSize);
-        for (int i = 0; i < qSize; ++i)
-            ans[i] = binarySearch(items, queries[i]);
+        sort(queriesWithIndex.begin(), queriesWithIndex.end());
+        int n = items.size(), itemIndex = 0, maxBeauty = 0;
+        vector<int> ans(queries.size());
+        for (int i = 0; i < queries.size(); ++i){
+            auto [query, originalIndex] = queriesWithIndex[i];
+            
+            while (itemIndex < n && items[itemIndex][0] <= query){
+                maxBeauty = max(maxBeauty, items[itemIndex][1]);
+                ++itemIndex;
+            }
+            ans[originalIndex] = maxBeauty;
+        }
         
         return ans;
     }
