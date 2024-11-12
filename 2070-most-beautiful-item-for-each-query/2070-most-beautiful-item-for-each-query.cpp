fb02b138b8dee4c674db4c356fb79b22d19e4163
Andrew Kim <ksb0724@utexas.edu>
2024-11-13 03:24:09 +0900

    Time: 64 ms (42.62%), Space: 91.5 MB (88.51%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.cpp
@@ -1,28 +1,39 @@
 class Solution {
+private:
+    int n;
+    int binarySearch(vector<vector<int>>& items, int targetPrice){
+        int left = 0, right = n - 1, maxBeauty = 0, mid;
+        
+        while (left <= right){
+            mid = (left + right) >> 1;
+            
+            if (targetPrice < items[mid][0])
+                right = mid - 1;
+            else{
+                // Found viable price. Keep moving to right
+                maxBeauty = max(maxBeauty, items[mid][1]);
+                left = mid + 1;
+            }
+        }
+        
+        return maxBeauty;
+    }
+    
 public:
     vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-        map<int, int> reducedItems;
-        for (const auto& item : items){
-            reducedItems[item[0]] = max(reducedItems[item[0]], item[1]);
-        }
-        
-        vector<pair<int, int>> maxItems;
-        int maxBeauty = 0;
-        for (const auto& [price, beauty] : reducedItems){
-            maxItems.push_back({price, max(maxBeauty, beauty)});
-            maxBeauty = max(maxBeauty, beauty);
-        }
-        sort(maxItems.begin(), maxItems.end());
-        
-        int n = queries.size();
-        vector<int> ans(n);
+        // Sort and store max beauty
+        sort(items.begin(), items.end());
+        int mx = items[0][1];
+        this -> n = items.size();
         for (int i = 0; i < n; ++i){
-            if (queries[i] < maxItems[0].first)
-                continue;
-            
-            int maxItemsIndex = upper_bound(maxItems.begin(), maxItems.end(), make_pair(queries[i], INT_MAX)) - maxItems.begin();
-            ans[i] = maxItems[maxItemsIndex - 1].second;
+            mx = max(mx, items[i][1]);
+            items[i][1] = mx;
         }
+        
+        int qSize = queries.size();
+        vector<int> ans(qSize);
+        for (int i = 0; i < qSize; ++i)
+            ans[i] = binarySearch(items, queries[i]);
         
         return ans;
     }
