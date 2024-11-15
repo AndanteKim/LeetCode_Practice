d6d255e5a4ef89dd4d34e250f44ece2712b084f2
Andrew Kim <ksb0724@utexas.edu>
2024-11-16 08:26:39 +0900

    Time: 225 ms (12.25%), Space: 42.8 MB (6.90%) - LeetHub

--- 2107-number-of-unique-flavors-after-sharing-k-candies/2107-number-of-unique-flavors-after-sharing-k-candies.py
+++ 2107-number-of-unique-flavors-after-sharing-k-candies/2107-number-of-unique-flavors-after-sharing-k-candies.py
@@ -1,36 +1,16 @@
 class Solution:
     def shareCandies(self, candies: List[int], k: int) -> int:
-        uniq_flav = 0
+        ans, d = 0, Counter(candies)
         
-        # Store the total number of unique flavors in the array.
-        flav_freq = defaultdict(int)
-        for c in candies:
-            flav_freq[c] += 1
-            if flav_freq[c] == 1:
-                uniq_flav += 1
-        
-        # Get the flavors used completely in the window.
-        used_in_window = 0
-        for i in range(k):
-            flav_freq[candies[i]] -= 1
-            if flav_freq[candies[i]] == 0:
-                used_in_window += 1
-                
-        # Get the flavors in the remaining array currently
-        max_flav, n = uniq_flav - used_in_window, len(candies)
-        
-        # Slide the window to the right.
-        for i in range(k, n):
-            # Remove the candy on the left end from the window.
-            flav_freq[candies[i - k]] += 1
-            if flav_freq[candies[i - k]] == 1:
-                used_in_window -= 1
-                
-            # Add the candy on the right end at index i.
-            flav_freq[candies[i]] -= 1
-            if flav_freq[candies[i]] == 0:
-                used_in_window += 1
-                
-            max_flav = max(max_flav, uniq_flav - used_in_window)
+        for i, c in enumerate(candies):
+            d[c] -= 1
+            if d[c] == 0:
+                del d[c]
             
-        return max_flav
+            if i >= k:
+                d[candies[i - k]] += 1
+            
+            if i >= k - 1:
+                ans = max(ans, len(d))
+                
+        return ans
