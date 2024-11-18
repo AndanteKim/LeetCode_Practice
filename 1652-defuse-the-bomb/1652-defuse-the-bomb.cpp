74db675f7c6700905c6beb5db6d737c05e5510d3
Andrew Kim <ksb0724@utexas.edu>
2024-11-18 16:40:35 +0900

    Time: 0 ms (100.00%), Space: 10.4 MB (60.07%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.cpp
@@ -3,18 +3,25 @@ public:
     vector<int> decrypt(vector<int>& code, int k) {
         int n = code.size();
         vector<int> ans(n);
+        
+        // Define the initial window and initial sum
+        int start = 1, end = k;
+        long currSum = 0;
+        
+        // If k < 0, the starting point will be the end of the array.
+        if (k < 0){
+            start = n - abs(k);
+            end = n - 1;
+        }
+        
+        for (int i = start; i <= end; ++i) currSum += code[i];
+        
+        // Scan through the code array as i moving to the right, update the window sum.
         for (int i = 0; i < n; ++i){
-            int curr = 0;
-            if (k > 0){
-                for (int di = 1; di <= k; ++di)
-                    curr += code[(i + di) % n];
-            }
-            else{
-                for (int di = 1; di <= abs(k); ++di)
-                    curr += code[(i - di + n) % n];
-            }
-            
-            ans[i] = curr;
+            ans[i] = currSum;
+            currSum = currSum - code[start % n] + code[(end + 1) % n];
+            ++start;
+            ++end;
         }
         
         return ans;
