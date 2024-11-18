4625e7b3cfa9c7275452f59320d5bc09221d40c5
Andrew Kim <ksb0724@utexas.edu>
2024-11-18 16:27:33 +0900

    Time: 0 ms (100.00%), Space: 16.7 MB (62.03%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.py
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -3,14 +3,22 @@ class Solution:
         n = len(code)
         ans = [0] * n
         
-        for i in range(n):
-            curr = 0
-            if k > 0:
-                for di in range(1, k + 1):
-                    curr += code[(i + di) % n]
-            else:
-                for di in range(1, abs(k) + 1):
-                    curr += code[(i - di + n) % n]
-            ans[i] = curr
+        if k == 0:
+            return ans
         
+        # Define the initial window and initial sum
+        start, end, curr_sum = 1, k, 0
+        if k < 0:
+            start, end = n - abs(k), n - 1
+        
+        for i in range(start, end + 1):
+            curr_sum += code[i]
+        
+        # Scan through the code array as i moving to the right, update the window sum.
+        for i in range(n):
+            ans[i] = curr_sum
+            curr_sum = curr_sum - code[start % n] + code[(end + 1) % n]
+            start += 1
+            end += 1
+            
         return ans
