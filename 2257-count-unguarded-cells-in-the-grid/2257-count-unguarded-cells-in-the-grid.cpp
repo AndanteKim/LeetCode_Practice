839bc597e34dbe7e334393b5ea29563857f69919
Andrew Kim <ksb0724@utexas.edu>
2024-11-21 23:49:57 +0900

    Time: 52 ms (77.32%), Space: 172.1 MB (78.43%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.cpp
@@ -1,64 +1,53 @@
 class Solution {
 private:
-    int m, n;
     const int UNGUARDED = 0, GUARDED = 1, GUARD = 2, WALL = 3;
+    int m, n;
     
-    void markGuarded(int row, int col, vector<vector<int>>& grid){
-        // Traverse upwards
-        for (int r = row - 1; r >= 0; --r){
-            if (grid[r][col] == WALL || grid[r][col] == GUARD)
-                break;
-            grid[r][col] = GUARDED;
-        }
+    void dfs(int row, int col, vector<vector<int>>& grid, char direction){
+        // Base case
+        if (row < 0 || row >= m || col < 0 || col >= n || grid[row][col] == GUARD || grid[row][col] == WALL)
+            return;
         
-        // Traverse downwards
-        for (int r = row + 1; r < m; ++r){
-            if (grid[r][col] == WALL || grid[r][col] == GUARD)
-                break;
-            grid[r][col] = GUARDED;
-        }
+        // Mark cell as guarded
+        grid[row][col] = GUARDED;
         
-        // Traverse leftwards
-        for (int c = col - 1; c >= 0; --c){
-            if (grid[row][c] == WALL || grid[row][c] == GUARD)
-                break;
-            grid[row][c] = GUARDED;
-        }
-        
-        // Traverse rightwards
-        for (int c = col + 1; c < n; ++c){
-            if (grid[row][c] == WALL || grid[row][c] == GUARD)
-                break;
-            grid[row][c] = GUARDED;
-        }
+        if (direction == 'U')
+            dfs(row - 1, col, grid, direction);
+        else if (direction == 'D')
+            dfs(row + 1, col, grid, direction);
+        else if (direction == 'L')
+            dfs(row, col - 1, grid, direction);
+        else
+            dfs(row, col + 1, grid, direction);
     }
+        
     
 public:
     int countUnguarded(int m, int n, vector<vector<int>>& guards, vector<vector<int>>& walls) {
         this -> m = m, this -> n = n;
         vector<vector<int>> grid(m, vector<int>(n));
         
-        // Mark guards' positions
-        for (const auto& guard : guards){
+        // Mark the guards' position
+        for (const auto& guard: guards)
             grid[guard[0]][guard[1]] = GUARD;
-        }
         
-        // Mark walls' positions
-        for (const auto& wall : walls){
+        // Mark the walls' position
+        for (const auto& wall : walls)
             grid[wall[0]][wall[1]] = WALL;
-        }   
         
         // Mark cells as guarded by traversing from each guard
         for (const auto& guard : guards){
-            markGuarded(guard[0], guard[1], grid);
+            dfs(guard[0] - 1, guard[1], grid, 'U');  // Up
+            dfs(guard[0] + 1, guard[1], grid, 'D');  // Down
+            dfs(guard[0], guard[1] - 1, grid, 'L');  // Left
+            dfs(guard[0], guard[1] + 1, grid, 'R');  // Right
         }
         
         // Count unguarded cells
         int count = 0;
         for (const auto& row : grid){
-            for (int cell : row){
+            for (const int cell : row)
                 if (cell == UNGUARDED) ++count;
-            }
         }
         
         return count;
