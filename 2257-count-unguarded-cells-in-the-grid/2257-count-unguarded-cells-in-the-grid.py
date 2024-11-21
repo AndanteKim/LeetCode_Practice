587c293b939a018b9d0982908b226c117b35cbb6
Andrew Kim <ksb0724@utexas.edu>
2024-11-21 15:37:11 +0900

    Time: 379 ms (63.00%), Space: 38.3 MB (77.78%) - LeetHub

--- 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
+++ 2257-count-unguarded-cells-in-the-grid/2257-count-unguarded-cells-in-the-grid.py
@@ -4,51 +4,43 @@ class Solution:
     GUARD = 2
     WALL = 3
     
-    def _mark_guarded(self, row: int, col: int, grid: List[List[int]]) -> None:
-        # Traverse upwards
-        for r in range(row - 1, -1, -1):
-            if grid[r][col] == self.WALL or grid[r][col] == self.GUARD:
-                break
-            grid[r][col] = self.GUARDED
-            
-        # Traverse downwards
-        for r in range(row + 1, len(grid)):
-            if grid[r][col] == self.WALL or grid[r][col] == self.GUARD:
-                break
-            grid[r][col] = self.GUARDED
-            
-        # Traverse leftwards
-        for c in range(col - 1, -1, -1):
-            if grid[row][c] == self.WALL or grid[row][c] == self.GUARD:
-                break
-            grid[row][c] = self.GUARDED
-            
-        # Traverse rightwards
-        for c in range(col + 1, len(grid[0])):
-            if grid[row][c] == self.WALL or grid[row][c] == self.GUARD:
-                break
-            grid[row][c] = self.GUARDED
+    # Depth-First Search to mark guarded cells
+    def _dfs(self, row: int, col: int, grid: List[List[int]], direction: chr) -> None:
+        if row < 0 or row >= self.m or col < 0 or col >= self.n or grid[row][col] == self.WALL \
+        or grid[row][col] == self.GUARD:
+            return
+        
+        # Mark cell as guarded
+        grid[row][col] = self.GUARDED
+        if direction == 'U':
+            self._dfs(row - 1, col, grid, 'U')   # Up
+        elif direction == 'D':
+            self._dfs(row + 1, col, grid, 'D')   # Down
+        elif direction == 'L':
+            self._dfs(row, col - 1, grid, 'L')   # Left
+        else:
+            self._dfs(row, col + 1, grid, 'R')   # Right
+           
     
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
-        grid = [[self.UNGUARDED] * n for _ in range(m)]
+        self.m, self.n = m, n
+        grid = [[self.UNGUARDED] * self.n for _ in range(self.m)]
         
-        # Mark guards' positions
+        # Mark the guards' positions
         for x, y in guards:
             grid[x][y] = self.GUARD
-            
-        # Mark walls' positions
+        
+        # Mark the walls' positions
         for x, y in walls:
             grid[x][y] = self.WALL
-            
+        
         # Mark cells as guarded by traversing from each guard
         for x, y in guards:
-            self._mark_guarded(x, y, grid)
+            self._dfs(x - 1, y, grid, 'U')   # Up
+            self._dfs(x + 1, y, grid, 'D')   # Down
+            self._dfs(x, y - 1, grid, 'L')   # Left
+            self._dfs(x, y + 1, grid, 'R')   # Right
         
-        # Count unguarded cells
-        count = 0
-        for row in grid:
-            for cell in row:
-                if cell == self.UNGUARDED:
-                    count += 1
-        
+        # Count ungraded cells
+        count = sum(row.count(self.UNGUARDED) for row in grid)
         return count
