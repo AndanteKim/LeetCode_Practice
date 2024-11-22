8496a65f12b43b34bac7732ace6566651fa95ec4
Andrew Kim <ksb0724@utexas.edu>
2024-11-23 07:00:15 +0900

    Time: 95 ms (41.43%), Space: 49.4 MB (84.37%) - LeetHub

--- 2371-minimize-maximum-value-in-a-grid/2371-minimize-maximum-value-in-a-grid.py
+++ 2371-minimize-maximum-value-in-a-grid/2371-minimize-maximum-value-in-a-grid.py
@@ -1,25 +1,32 @@
-class Solution:
-    def minScore(self, grid: List[List[int]]) -> List[List[int]]:
-        row, col = len(grid), len(grid[0])
+class Solution {
+public:
+    vector<vector<int>> minScore(vector<vector<int>>& grid) {
+        int row = grid.size(), col = grid[0].size();
         
-        # Min-heap to store elements with their values and coordinates
-        # Initialize rows and cols arrays to keep track of maximum values assigned
-        min_heap, rows, cols = [], [1] * row, [1] * col
+        // Min-heap to store elements with their values and coordinates
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> minHeap;
+        // Initialize rows and cols arrays to keep track of maximum values assigned
+        vector<int> rows(row, 1), cols(col, 1);
         
-        # Populate the min-heap with all elements of the grid
-        for i in range(row):
-            for j in range(col):
-                heappush(min_heap, (grid[i][j], i, j))
-                
-        # Process elements in ascending order of their values
-        while min_heap:
-            _, i, j = heappop(min_heap)
+        // Populate the min-heap with all elements of the grid
+        for (int i = 0; i < row; ++i){
+            for (int j = 0; j < col; ++j)
+                minHeap.push({grid[i][j], i, j});
+        }
+        
+        // Process elements in ascending order of their values
+        while (!minHeap.empty()){
+            auto [_, i, j] = minHeap.top();
+            minHeap.pop();
             
-            # Determine the smallest assignable value based on rows and cols constraints
-            val = max(rows[i], cols[j])
-            grid[i][j] = val
+            // Determine the smallest assignable value based on rows and cols constraints
+            int val = max(rows[i], cols[j]);
+            grid[i][j] = val;
             
-            # Update rows and cols arrays with the next possibile value for each row and column
-            rows[i], cols[j] = val + 1, val + 1
-            
-        return grid
+            // Update rows and cols arrays with the next possible value for each row and column
+            rows[i] = cols[j] = val + 1;
+        }
+        
+        return grid;
+    }
+};
