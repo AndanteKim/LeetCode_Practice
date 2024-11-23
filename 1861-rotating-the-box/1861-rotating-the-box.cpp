f0b099b714a42fd1a351ae6a920c752a539d196e
Andrew Kim <ksb0724@utexas.edu>
2024-11-23 11:18:57 +0900

    Time: 234 ms (15.83%), Space: 56.3 MB (63.41%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size(), n = box[0].size();
+        
+        vector<vector<char>> rotatedBox(n, vector<char>(m));
+        
+        // Create the transpose of the input grid in 'result'
+        for (int i = 0; i < n; ++i){
+            for (int j = 0; j < m; ++j)
+                rotatedBox[i][j] = box[j][i];
+        }
+        
+        // Reverse each row in the transpose grid to complete the 90° rotation
+        for (int i = 0; i < n; ++i) reverse(rotatedBox[i].begin(), rotatedBox[i].end());
+        
+        // Apply gravity to let stones fall to the lowest possible empty cell in
+        // each column
+        for (int j = 0; j < m; ++j){
+            
+            // Process each cell in column 'j' from bottome to top
+            for (int i = n - 1; i >= 0; --i){
+                if (rotatedBox[i][j] == '.'){   // Found an empty cell; check if a stone can fall into it
+                    int nextEmptyWithStone = -1;
+                    
+                    // Look for a stone directly above the empty cell
+                    // 'rotatedBox[i][j]'
+                    for (int k = i - 1; k >= 0; --k){
+                        if (rotatedBox[k][j] == '*')
+                            break;  // Obstacle blocks any stones above
+                        
+                        if (rotatedBox[k][j] == '#'){   // Stone foudn with no obstacles in between
+                            nextEmptyWithStone = k;
+                            break;
+                        }
+                    }
+                    
+                    // If a stone was found above, let it fall into the empty cell 'rotatedBox[i][j]'
+                    if (nextEmptyWithStone != -1){
+                        swap(rotatedBox[nextEmptyWithStone][j], rotatedBox[i][j]);
+                    }
+                }
+            }
+        }
+        
+        return rotatedBox;
+    }
+};
