5b56178e26967b7d9d7219a55d5cc9bddf5a620d
Andrew Kim <ksb0724@utexas.edu>
2024-11-23 12:28:42 +0900

    Time: 192 ms (60.00%), Space: 56.3 MB (63.41%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.cpp
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -2,47 +2,35 @@ class Solution {
 public:
     vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
         int m = box.size(), n = box[0].size();
+        vector ans(n, vector<char>(m));
         
-        vector<vector<char>> rotatedBox(n, vector<char>(m));
-        
-        // Create the transpose of the input grid in 'rotatedBox'
-        for (int i = 0; i < n; ++i){
-            for (int j = 0; j < m; ++j)
-                rotatedBox[i][j] = box[j][i];
-        }
-        
-        // Reverse each row in the transpose grid to complete the 90° rotation
-        for (int i = 0; i < n; ++i) reverse(rotatedBox[i].begin(), rotatedBox[i].end());
-        
-        // Apply gravity to let stones fall to the lowest possible empty cell in
-        // each column
-        for (int j = 0; j < m; ++j){
-            
-            // Process each cell in column 'j' from bottom to top
-            for (int i = n - 1; i >= 0; --i){
-                if (rotatedBox[i][j] == '.'){   // Found an empty cell; check if a stone can fall into it
-                    int nextRowWithStone = -1;
-                    
-                    // Look for a stone directly above the empty cell
-                    // 'rotatedBox[i][j]'
-                    for (int k = i - 1; k >= 0; --k){
-                        if (rotatedBox[k][j] == '*')
-                            break;  // Obstacle blocks any stones above
-                        
-                        if (rotatedBox[k][j] == '#'){   // Stone found with no obstacles in between
-                            nextRowWithStone = k;
-                            break;
-                        }
-                    }
-                    
-                    // If a stone was found above, let it fall into the empty cell 'rotatedBox[i][j]'
-                    if (nextRowWithStone != -1){
-                        swap(rotatedBox[nextRowWithStone][j], rotatedBox[i][j]);
-                    }
-                }
+        // Create the transpose of the input grid in 'ans'
+        for (int i = 0; i < m; ++i){
+            for (int j = 0; j < n; ++j){
+                ans[j][i] = box[i][j];
             }
         }
         
-        return rotatedBox;
+        // Reverse each row in the transpose grid to complete the 90° rotation
+        for (int i = 0; i < n; ++i)
+            reverse(begin(ans[i]), end(ans[i]));
+        
+        // Apply gravity to let stones fall to the lowest possible empty cell in each column
+        for (int j = 0; j < m; ++j){
+            int lowestRowWithEmptyCell = n - 1;
+            
+            // Process each cell in column 'j' from bottom to top
+            for (int i = n - 1; i >= 0; --i){
+                // Found a stone - let it fall to the lowest empty cell
+                if (ans[i][j] == '#')
+                    swap(ans[i][j], ans[lowestRowWithEmptyCell--][j]);
+                
+                // Found an obstacle - reset 'lowestRowWithEmptyCell' to the row directly above it
+                if (ans[i][j] == '*')
+                    lowestRowWithEmptyCell = i - 1;
+            }
+        }
+        
+        return ans;
     }
 };
