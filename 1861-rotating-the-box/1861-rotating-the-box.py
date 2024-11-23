7e33e375289a3d1230391a8dec72de6b09923fbd
Andrew Kim <ksb0724@utexas.edu>
2024-11-23 12:14:03 +0900

    Time: 1879 ms (74.37%), Space: 28.9 MB (65.69%) - LeetHub

--- 1861-rotating-the-box/1861-rotating-the-box.py
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -1,38 +1,30 @@
 class Solution:
     def rotateTheBox(self, box: List[List[str]]) -> List[List[str]]:
         m, n = len(box), len(box[0])
-        rotated_box = [[""] * m for _ in range(n)]
+        ans = [[""] * m for _ in range(n)]
         
-        # Create the transpose of the input grid in 'rotated_box'
-        for i in range(n):
-            for j in range(m):
-                rotated_box[i][j] = box[j][i]
+        # Create the transpose of the input grid in 'result'
+        for i in range(m):
+            for j in range(n):
+                ans[j][i] = box[i][j]
         
         # Reverse each row in the transpose grid to complete the 90° rotation
         for i in range(n):
-            rotated_box[i].reverse()
-        
+            ans[i] = ans[i][::-1]
+            
         # Apply gravity to let stones fall to the lowest possible empty cell in each column
         for j in range(m):
+            lowest_row_with_empty_cell = n - 1
+            
             # Process each cell in column 'j' from bottom to top
             for i in range(n - 1, -1, -1):
-                if rotated_box[i][j] == ".":
-                    # Found an empty cell; check if a stone can fall into it
-                    next_row_with_stone = -1
-                    
-                    # Look for a stone directly above the empty cell 'rotated_box[i][j]'
-                    for k in range(i - 1, -1, -1):
-                        if rotated_box[k][j] == "*":
-                            break   # Obstacle blocks any stones above
-                        
-                        if rotated_box[k][j] == "#":
-                            # Stone found with no obstacles in between
-                            next_row_with_stone = k
-                            break
-                    
-                    # If a stone was found above, let it fall into the empty cell 'rotated_box[i][j]'
-                    if next_row_with_stone != -1:
-                        rotated_box[next_row_with_stone][j] = "."
-                        rotated_box[i][j] = "#"
+                # Found a stone - let it fall to the lowest empty cell
+                if ans[i][j] == "#":
+                    ans[i][j], ans[lowest_row_with_empty_cell][j] = ans[lowest_row_with_empty_cell][j], ans[i][j]
+                    lowest_row_with_empty_cell -= 1
                 
-        return rotated_box
+                # Found an obstacle - reset 'lowest_row_with_empty_cell' to the row directly above it
+                if ans[i][j] == "*":
+                    lowest_row_with_empty_cell = i - 1
+                    
+        return ans
