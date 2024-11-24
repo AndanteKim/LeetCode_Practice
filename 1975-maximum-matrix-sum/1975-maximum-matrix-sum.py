6493744b5241f5c0e76109feebcc5ed0452cef1d
Andrew Kim <ksb0724@utexas.edu>
2024-11-24 11:08:00 +0900

    Time: 0 ms (100.00%), Space: 38.4 MB (30.74%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -1,13 +1,17 @@
-class Solution:
-    def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        total_sum, min_val = 0, float('inf')
-        neg_cnt = 0
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+        long long totalSum = 0;
+        int minVal = std::numeric_limits<int>::max(), negativeCount = 0;
         
-        for row in matrix:
-            for val in row:
-                total_sum += abs(val)
-                min_val = min(min_val, abs(val))
-                neg_cnt += (val >> 31) & 1;
-                
-            
-        return total_sum - (min_val << 1) if neg_cnt & 1 else total_sum
+        for (const auto& row : matrix){
+            for (int val : row){
+                totalSum += abs(val);
+                minVal = min(minVal, abs(val));
+                negativeCount += (val >> 31) & 1;
+            }
+        }
+        
+        return (negativeCount & 1)? totalSum - (minVal << 1) : totalSum;
+    }
+};
