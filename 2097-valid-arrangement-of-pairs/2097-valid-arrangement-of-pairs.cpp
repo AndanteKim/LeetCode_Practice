953f06db2ad34082e4726b5c24752309689d3107
Andrew Kim <ksb0724@utexas.edu>
2024-12-01 03:30:44 +0900

    Time: 452 ms (72.86%), Space: 433.7 MB (23.05%) - LeetHub

--- 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
+++ 2097-valid-arrangement-of-pairs/2097-valid-arrangement-of-pairs.cpp
@@ -2,53 +2,56 @@ class Solution {
 public:
     vector<vector<int>> validArrangement(vector<vector<int>>& pairs) {
         unordered_map<int, deque<int>> adjacencyMatrix;
-        unordered_map<int, int> indegree, outdegree;
+        unordered_map<int, int> inDegree, outDegree;
         
         // Build the adjacency list and track in-degrees and out-degrees
         for (const auto& p : pairs){
             int start = p[0], end = p[1];
             adjacencyMatrix[start].push_back(end);
-            ++outdegree[start];
-            ++indegree[end];
+            ++outDegree[start];
+            ++inDegree[end];
         }
         
-        vector<int> processes;
-        
-        // Helper lambda function for DFS traversal
-        function<void(int)> visit = [&](int node){
-            while (!adjacencyMatrix[node].empty()){
-                int nextNode = adjacencyMatrix[node].front();
-                adjacencyMatrix[node].pop_front();
-                visit(nextNode);
-            }
-            processes.push_back(node);
-        };
-        
-        
-        // Find the start node (outdegree == 1 + indegree)
+        // Find the start node (outDegree = inDegree + 1)
         int startNode = -1;
-        for (const auto& [node, count] : outdegree){
-            if (count == indegree[node] + 1){
+        for (const auto& [node, outCount] : outDegree){
+            if (outCount == inDegree[node] + 1){
                 startNode = node;
                 break;
             }
         }
         
         // If no such node exists, start from the first pair's first element
-        if (startNode == -1)
-            startNode = pairs[0][0];
+        if (startNode == -1) startNode = pairs[0][0];
         
-        // Start DFS traversal
-        visit(startNode);
+        stack<int> nodeStack; nodeStack.push(startNode);
+        vector<int> result;
         
-        // Reverse the result since DFS gives us the path in reverse
-        reverse(processes.begin(), processes.end());
+        // Iterative DFS using stack
+        while (!nodeStack.empty()){
+            int node = nodeStack.top();
+            
+            if (!adjacencyMatrix[node].empty()){
+                // Visit the next node
+                int nextNode = adjacencyMatrix[node].front();
+                adjacencyMatrix[node].pop_front();
+                nodeStack.push(nextNode);
+            }
+            else{
+                // No more neighbors to visit, add node to result
+                result.push_back(node);
+                nodeStack.pop();
+            }
+        }
         
-        vector<vector<int>> ans;
+        // Reverse the result since we collected nodes in reverse order
+        reverse(result.begin(), result.end());
+        
         // Construct the result pairs
-        for (int i = 1; i < processes.size(); ++i)
-            ans.push_back(vector<int>{processes[i - 1], processes[i]});
+        vector<vector<int>> pairedResult;
+        for (int i = 1; i < result.size(); ++i)
+            pairedResult.push_back({result[i - 1], result[i]});
         
-        return ans;
+        return pairedResult;
     }
 };
