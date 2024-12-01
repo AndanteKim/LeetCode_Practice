3899269852200fa23b726e369e70c4c0e41a1c8e
Andrew Kim <ksb0724@utexas.edu>
2024-12-02 03:29:51 +0900

    Time: 0 ms (100.00%), Space: 14 MB (14.55%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
-        unordered_set<double> seen;
+        unordered_set<int> seen;
         
         for (int num : arr){
-            if (seen.count(num / 2.0) || seen.count(num * 2))
+            // Check if 2 * num or num / 2 exists in the set
+            if (seen.count(num * 2) || (num % 2 == 0 && seen.count(num >> 1)))
                 return true;
+            
+            // Add the current number to the set
             seen.insert(num);
         }
         
+        // No valid pair found
         return false;
     }
 };
