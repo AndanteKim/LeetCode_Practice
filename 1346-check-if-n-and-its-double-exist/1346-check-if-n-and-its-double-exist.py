b85eb985bfc2324c2b0eb7ff52cc4c1f3178c5a2
Andrew Kim <ksb0724@utexas.edu>
2024-12-02 03:28:13 +0900

    Time: 0 ms (100.00%), Space: 17.3 MB (5.25%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -3,8 +3,12 @@ class Solution:
         seen = set()
         
         for num in arr:
-            if (num / 2) in seen or num * 2 in seen:
+            # Check if 2 * num or num / 2 exists in the set
+            if 2 * num in seen or (num % 2 == 0 and (num >> 1) in seen):
                 return True
+            
+            # Add the current number to the set
             seen.add(num)
             
+        # No valid pair found
         return False
