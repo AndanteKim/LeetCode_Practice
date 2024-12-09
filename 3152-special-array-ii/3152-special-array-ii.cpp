dad9102162478c429f53b66c60d7afd240e9b0f0
Andrew Kim <ksb0724@utexas.edu>
2024-12-10 06:52:35 +0900

    Time: 4 ms (88.15%), Space: 126.2 MB (79.50%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.cpp
+++ 3152-special-array-ii/3152-special-array-ii.cpp
@@ -1,55 +1,52 @@
 class Solution {
-public:
-    vector<bool> isArraySpecial(vector<int>& nums,
-                                vector<vector<int>>& queries) {
-        vector<bool> ans(queries.size());
-        vector<int> violatingIndices;
-
-        for (int i = 1; i < nums.size(); i++) {
-            // same parity, found violating index
-            if (nums[i] % 2 == nums[i - 1] % 2) {
-                violatingIndices.push_back(i);
-            }
-        }
-
-        for (int i = 0; i < queries.size(); i++) {
-            vector<int> query = queries[i];
-            int start = query[0];
-            int end = query[1];
-
-            bool foundViolatingIndex =
-                binarySearch(start + 1, end, violatingIndices);
-
-            if (foundViolatingIndex) {
-                ans[i] = false;
-            } else {
-                ans[i] = true;
-            }
-        }
-
-        return ans;
-    }
-
 private:
-    bool binarySearch(int start, int end, vector<int>& violatingIndices) {
-        int left = 0;
-        int right = violatingIndices.size() - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+    bool binarySearch(int start, int end, vector<int>& violatingIndices){
+        int left = 0, right = violatingIndices.size() - 1;
+        
+        while (left <= right){
+            int mid = left + ((right - left) >> 1);
             int violatingIndex = violatingIndices[mid];
-
-            if (violatingIndex < start) {
-                // check right half
+            
+            if (violatingIndex < start)
+                // Check the right half
                 left = mid + 1;
-            } else if (violatingIndex > end) {
-                // check left half
+            
+            else if (violatingIndex > end)
+                // Check the left half
                 right = mid - 1;
-            } else {
+            
+            else
                 // violatingIndex falls in between start and end
                 return true;
-            }
+            
         }
-
+        
         return false;
+    }
+    
+public:
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        int n = nums.size(), q = queries.size();
+        vector<int> violatingIndices;
+        
+        for (int i = 1; i < n; ++i)
+            // same parity, found violating index
+            if (nums[i] % 2 == nums[i - 1] % 2)
+                violatingIndices.push_back(i);
+        
+        
+        vector<bool> ans(q, false);
+        for (int i = 0; i < q; ++i){
+            int start = queries[i][0], end = queries[i][1];
+            
+            bool foundViolatingIndex = binarySearch(start + 1, end, violatingIndices);
+            
+            if (foundViolatingIndex)
+                ans[i] = false;
+            else
+                ans[i] = true;
+        }
+        
+        return ans;
     }
 };
