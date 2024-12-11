687b234b5b9429e99c2a16c50e258244321f2e3c
Andrew Kim <ksb0724@utexas.edu>
2024-12-12 01:58:04 +0900

    Time: 1166 ms (8.11%), Space: 28.3 MB (31.74%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -1,12 +1,27 @@
 class Solution:
     def maximumBeauty(self, nums: List[int], k: int) -> int:
-        ans, n = 0, len(nums)
+        def find_upper_bound(target: int) -> int:
+            lo, hi, res = 0, n - 1, 0
+            
+            # Perform binary search to find the upper bound
+            while lo <= hi:
+                mid = lo + ((hi - lo) >> 1)
+                if nums[mid] <= target:
+                    res = mid   # Update the result and move to the right half
+                    lo = mid + 1
+                else:
+                    hi = mid - 1    # Move to the left half
+            
+            return res
+        
+        max_beauty, n = 0, len(nums)
         nums.sort()
-        left = 0
         
-        for right in range(n):
-            while left <= right and nums[right] - nums[left] > 2 * k:
-                left += 1
-            ans = max(ans, right - left + 1)
-        
-        return ans
+        for i in range(n):
+            # Find the farthest index where the value is within the range [num, num + 2 * k]
+            upper_bound = find_upper_bound(nums[i] + 2 * k)
+            
+            # Update the maximum beauty based on the current range
+            max_beauty = max(max_beauty, upper_bound - i + 1)
+            
+        return max_beauty
