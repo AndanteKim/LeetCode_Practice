43907299ad5571617fd722355fd6fde270ea6252
Andrew Kim <ksb0724@utexas.edu>
2024-12-12 02:05:37 +0900

    Time: 85 ms (45.32%), Space: 105 MB (84.24%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,15 +1,41 @@
 class Solution {
-public:
-    int maximumBeauty(vector<int>& nums, int k) {
-        int ans = 0, left = 0, n = nums.size();
-        sort(nums.begin(), nums.end());
+private:
+    int n;
+    
+    // Helper function to find the largest index where arr[index] <= val
+    int findUpperBound(vector<int>& nums, int target){
+        int low = 0, high = n - 1, result = 0, mid;
         
-        for (int right = 0; right < n; ++right){
-            while (left <= right && nums[right] - nums[left] > 2 * k)
-                ++left;
-            ans = max(ans, right - left + 1);
+        // Perform binary search to find the upper bound
+        while (low <= high){
+            mid = low + ((high - low) >> 1);
+            
+            if (nums[mid] <= target){
+                result = mid;   // Update the result and move to the right half
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;     // Move to the left half
+            }
         }
         
-        return ans;
+        return result;
+    }
+    
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        this -> n = nums.size();
+        sort(nums.begin(), nums.end());
+        int maxBeauty = 0, upperBound;
+        
+        for (int i = 0; i < n; ++i){
+            // Find the farthest index where the value is within the range [num, num + 2 * k] 
+            upperBound = findUpperBound(nums, nums[i] + 2 * k);
+            
+            // Update the maximum beauty based on the current range
+            maxBeauty = max(maxBeauty, upperBound - i + 1);
+        }
+        
+        return maxBeauty;
     }
 };
