7b823834e604fe4e2821da6a0f06652f761f299a
Andrew Kim <ksb0724@utexas.edu>
2024-12-27 00:35:46 +0900

    Time: 137 ms (72.09%), Space: 45.1 MB (5.57%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,17 +1,12 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total = sum(nums)
-        dp = [0] * (2 * total + 1)
-        dp[nums[0] + total] = 1
-        dp[-nums[0] + total] += 1
-        
-        for i in range(1, len(nums)):
-            nxt = [0] * (2 * total + 1)
-            for s in range(-total, total + 1):
-                if dp[s + total] > 0:
-                    nxt[s + total + nums[i]] += dp[s + total]
-                    nxt[s + total - nums[i]] += dp[s + total]
-            dp = nxt
-        
-        return 0 if abs(target) > total else dp[target + total]
-        
+        @lru_cache(maxsize = None)
+        def dp(i: int, curr: int) -> int:
+            if i == n:
+                return 1 if curr == target else 0
+
+            curr = dp(i + 1, curr + nums[i]) + dp(i + 1, curr - nums[i])
+            return curr
+
+        n = len(nums)
+        return dp(0, 0)
