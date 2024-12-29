b488b521a71ffeb85e5e9ebae9b65a1a6c12b958
Andrew Kim <ksb0724@utexas.edu>
2024-12-30 02:21:22 +0900

    Time: 950 ms (75.29%), Space: 23.6 MB (74.36%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,35 +1,33 @@
 class Solution:
     def numWays(self, words: List[str], target: str) -> int:
-        mod, char_freq = 10 ** 9 + 7, [[0] * 26 for _ in range(len(words[0]))]
         word_length, target_length = len(words[0]), len(target)
+        char_freq, mod = [[0] * 26 for _ in range(word_length)], 1_000_000_007
 
-        # Step 1: Calculate frequency of each character at every index in "words".
-        for i in range(len(words)):
+        # Step 1: Calculate the frequency of each charactersat every index in "words".
+        for word in words:
             for j in range(word_length):
-                char_freq[j][ord(words[i][j]) - 97] += 1
+                char_freq[j][ord(word[j]) - 97] += 1
 
-        # Step 2: Initialize a DP table.
-        dp = [[0] * (len(target) + 1) for _ in range(word_length + 1)]
-        
-        # Base case: There is one way to form an empty target string/
-        for curr_word in range(word_length + 1):
-            dp[curr_word][0] = 1
+        # Step 2: Initialize two DP arrays: prev_dp and dp
+        prev_dp, dp = [0] * (target_length + 1), [0] * (target_length + 1)
 
-        # Step 3: Fill the DP table
+        # Base case: There is one way to form an empty target string
+        prev_dp[0], dp[0] = 1, 1
+
+        # Step 3: Fill the dp table
         for curr_word in range(1, word_length + 1):
+            # Carry over the previous one (not using this index of "words").
+            dp = prev_dp[:]
             for curr_target in range(1, target_length + 1):
-                # Carry over the previous value (not using this index of "words").
-                dp[curr_word][curr_target] = dp[curr_word - 1][curr_target]
-
-                # Add ways using the current index of "words" if the characters match.
+                # Add ways using the current index of "words" if the characters match
                 curr_pos = ord(target[curr_target - 1]) - 97
-                dp[curr_word][curr_target] += (
-                    char_freq[curr_word - 1][curr_pos]\
-                     * dp[curr_word - 1][curr_target - 1]
-                ) % mod
-                dp[curr_word][curr_target] %= mod
+                dp[curr_target] += char_freq[curr_word - 1][curr_pos] * prev_dp[curr_target - 1]
+                dp[curr_target] %= mod
+
+            # Move current row to previous row for the next iteration.
+            prev_dp = dp[:]
+
+        # Step 4: The result is in prev_dp[target_length]
+        return prev_dp[target_length]
+
         
-        # Step 4: The result is in dp[word_length][target_length]
-        return dp[word_length][target_length]
-
-
