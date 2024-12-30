35f91e2a43dbb6b8e38e68ecdb9f892074e3c1cd
Andrew Kim <ksb0724@utexas.edu>
2024-12-31 00:50:28 +0900

    Time: 113 ms (68.04%), Space: 44.3 MB (59.18%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.py
@@ -1,18 +1,21 @@
 class Solution:
     def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        def dfs(end: int) -> int:
+            # Base case
+            if dp[end] != -1:
+                return dp[end]
+            ans = 0
+            if end >= zero:
+                ans += dfs(end - zero)
+            
+            if end >= one:
+                ans += dfs(end - one)
+            dp[end] = ans % mod
+            return dp[end]
+        
         # Use dp[i] to record to number of good strings of length i.
-        dp, mod = [0] * (high + 1), 1_000_000_007
+        dp, mod = [-1] * (high + 1), 1_000_000_007
         dp[0] = 1
 
-        # Iterate over each length 'end'.
-        for end in range(1, high + 1):
-            # Check if the current string can be made by append zero '0's or one '1's
-            if end >= zero:
-                dp[end] += dp[end - zero]
-
-            if end >= one:
-                dp[end] += dp[end - one]
-            dp[end] % mod
-
-        # Add up the number of strings with each valid length [low ~ high].
-        return sum(dp[low : high + 1]) % mod 
+        # Add up the number of strings with each valid length [low ~ high]
+        return sum(dfs(end) for end in range(low, high + 1)) % mod
