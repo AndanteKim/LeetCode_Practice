190a2fae9ed579bb033bf86d1adda96c64051559
Andrew Kim <ksb0724@utexas.edu>
2024-12-31 00:40:45 +0900

    Time: 3 ms (96.09%), Space: 14.7 MB (52.05%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,25 +1,23 @@
 class Solution {
-    int mod = pow(10, 9) + 7;
-    
-    int dfs(int end, vector<int>& dp, int zero, int one){
-        if (dp[end] != -1) return dp[end];
-        int count = 0;
-        if (end >= zero) count = (count + dfs(end - zero, dp, zero, one)) % mod;
-        if (end >= one) count = (count + dfs(end - one, dp, zero, one)) % mod;
-        dp[end] = count % mod;
-        return dp[end];
-    }
-    
 public:
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int> dp(high + 1, -1);
+        // Use dp[i] to record to number of good strings of length i.
+        const int mod = 1'000'000'007;
+        vector<long> dp(high + 1);
         dp[0] = 1;
-        int ans = 0;
-        
-        for (int end = low; end <= high; ++end){
-            ans = (ans + dfs(end, dp, zero, one)) % mod;
+
+        // Iterate over each length 'end'.
+        for (int end = 1; end <= high; ++end){
+            // Check if the current string can be made by append zero '0's or one '1's.
+            if (end >= zero)
+                dp[end] += dp[end - zero] % mod;
+            if (end >= one)
+                dp[end] += dp[end - one] % mod;
+
+            dp[end] %= mod;
         }
-        
-        return ans;
+
+        // Add up to the number of strings with each valid length [low ~ high].
+        return accumulate(dp.begin() + low, dp.begin() + high + 1, 0L) % mod;
     }
 };
