78c0bba3c664f051eb43f28789b4462adb01701e
Andrew Kim <ksb0724@utexas.edu>
2025-01-01 03:42:13 +0900

    Time: 0 ms (100%), Space: 13.3 MB (32.35%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,15 +1,38 @@
 class Solution {
+private:
+    unordered_set<int> isTravelNeeded;
+
+    int solve(vector<int>& days, vector<int>& costs, vector<int>& dp, int currDay){
+        // Base case: If we've iterated over travel days, return 0.
+        if (currDay > days.back())
+            return 0;
+
+        // If already calculated, return from here with the stored answer.
+        if (dp[currDay] != -1)
+            return dp[currDay];
+
+        // If we don't need to travel on this day, move on to next day.
+        if (!isTravelNeeded.count(currDay))
+            return solve(days, costs, dp, currDay + 1);
+        
+        int oneDay = costs[0] + solve(days, costs, dp, currDay + 1);
+        int sevenDays = costs[1] + solve(days, costs, dp, currDay + 7);
+        int thirtyDays = costs[2] + solve(days, costs, dp, currDay + 30);
+
+        // Store the cost with the minimum of the three options.
+        return dp[currDay] = min(oneDay, min(sevenDays, thirtyDays));
+    }
+
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
-        vector<int> dp(days.back() + 1, 0);
-        for (int i = 1; i < dp.size(); ++i){
-            if (find(days.begin(), days.end(), i) != days.end()){
-                dp[i] = min(min(dp[max(i-1, 0)] + costs[0], dp[max(i-7, 0)] + costs[1]),\
-                            min(dp[max(i-7, 0)] + costs[1], dp[max(i-30, 0)] + costs[2]));
-            }
-            else dp[i] = dp[i-1];
-        }
-        
-        return dp.back();
+        // The last day on which we need to travel.
+        int lastDay = days.back();
+        vector<int> dp(lastDay + 1, -1);
+
+        // Mark the days on which we need to travel.
+        for (int day : days)
+            isTravelNeeded.insert(day);
+
+        return solve(days, costs, dp, 1);
     }
 };
