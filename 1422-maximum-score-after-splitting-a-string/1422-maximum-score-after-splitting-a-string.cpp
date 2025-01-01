a9c95b7b7bf836339b0519304e6ce13eba97143e
Andrew Kim <ksb0724@utexas.edu>
2025-01-01 11:16:28 +0900

    Time: 0 ms (100%), Space: 8.6 MB (52.6%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     int maxScore(string s) {
-        int best = INT_MIN, zeros = 0, ones = 0;
-        
-        for (int i = 0; i < s.size() - 1; ++i){
+        int leftZeros = (s[0] == '0')? 1 : 0, rightOnes = (s[0] == '1')? count(s.begin(), s.end(), '1') - 1 :\
+        count(s.begin(), s.end(), '1');
+        int n = s.size(), ans = leftZeros + rightOnes;
+
+        for (int i = 1; i < n - 1; ++i){
+            if (s[i] == '0')
+                ++leftZeros;
+
             if (s[i] == '1')
-                ++ones;
-            else
-                ++zeros;
-            best = max(best, zeros - ones);
+                --rightOnes;
+            
+            ans = max(ans, leftZeros + rightOnes);
         }
-        
-        if (s.back() == '1')
-            ++ones;
-        
-        return best + ones;
+
+        return ans;
     }
 };
