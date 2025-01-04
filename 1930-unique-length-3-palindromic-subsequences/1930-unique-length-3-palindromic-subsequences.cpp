dfe6322a53cf6ef773f4b09feb93675ec180038b
Andrew Kim <ksb0724@utexas.edu>
2025-01-05 08:06:01 +0900

    Time: 159 ms (69.97%), Space: 16 MB (47.99%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -2,26 +2,23 @@ class Solution {
 public:
     int countPalindromicSubsequence(string s) {
         vector<int> first(26, -1), last(26, -1);
-        
+
         for (int i = 0; i < s.size(); ++i){
-            int curr = s[i] - 'a';
-            if (first[curr] == -1)
-                first[curr] = i;
-            last[curr] = i;
+            if (first[s[i] - 97] == -1)
+                first[s[i] - 97] = i;
+            last[s[i] - 97] = i;
         }
-        
+
         int ans = 0;
         for (int i = 0; i < 26; ++i){
-            if (first[i] == -1)
-                continue;
-            
+            if (first[i] == -1) continue;
+
             unordered_set<char> between;
             for (int j = first[i] + 1; j < last[i]; ++j)
                 between.insert(s[j]);
-            
             ans += between.size();
         }
-        
+
         return ans;
     }
 };
