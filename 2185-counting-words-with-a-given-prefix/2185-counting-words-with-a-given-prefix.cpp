ccb05408e203bdd109a214623cc6956c8da6ae6c
Andrew Kim <ksb0724@utexas.edu>
2025-01-10 00:46:06 +0900

    Time: 0 ms (100%), Space: 13 MB (85.23%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
     int prefixCount(vector<string>& words, string pref) {
-        int p = pref.size(), ans = 0;
+        int count = 0;
 
         for (const string& word : words){
-            if (p > word.size())
-                continue;
-
-            int j = 0;
-            for (int i = 0; i < p; ++i)
-                if (pref[i] == word[i])
-                    ++j;
-            if (j == p)
-                ++ans;
+            if (word.find(pref) == 0)
+                ++count;    // Found a string with pref as prefix
         }
 
-        return ans;
+        return count;
     }
 };
