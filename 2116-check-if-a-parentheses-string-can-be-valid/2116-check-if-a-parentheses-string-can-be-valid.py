4853f66357c558dabd2207ba47a1ed6e5cda70ef
Andrew Kim <ksb0724@utexas.edu>
2025-01-12 17:57:11 +0900

    Time: 118 ms (34.29%), Space: 18.6 MB (45.53%) - LeetHub

--- 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
+++ 2116-check-if-a-parentheses-string-can-be-valid/2116-check-if-a-parentheses-string-can-be-valid.py
@@ -1,28 +1,40 @@
 class Solution:
     def canBeValid(self, s: str, locked: str) -> bool:
-        # Base case
-        if len(s) == 0 or len(s) % 2:
+        n = len(s)
+        # Base case: If the length of string is odd, return False
+        if n == 0 or n % 2:
             return False
 
-        open_brackets, unlocked = [], []
+        open_brackets, unlocked = 0, 0
 
         # Iterate through the string to handle '(' and ')'
         for i, c in enumerate(s):
             if locked[i] == '0':
-                unlocked.append(i)
-            elif c == '(':
-                open_brackets.append(i)
+                unlocked += 1
+            elif s[i] == '(':
+                open_brackets += 1
             else:
-                if open_brackets:
-                    open_brackets.pop()
-                elif unlocked:
-                    unlocked.pop()
+                if open_brackets > 0:
+                    open_brackets -= 1
+                elif unlocked > 0:
+                    unlocked -= 1
                 else:
                     return False
         
-        # Match remaining open brackets and the unlocked characters
-        while open_brackets and unlocked and open_brackets[-1] < unlocked[-1]:
-            open_brackets.pop()
-            unlocked.pop()
-
-        return False if open_brackets else True
+        # Match remaining open brackets with unlocked characters
+        balance = 0
+        for i in range(len(s) - 1, -1, -1):
+            if locked[i] == '0':
+                balance -= 1
+                unlocked -= 1
+            elif s[i] == '(':
+                balance += 1
+                open_brackets -= 1
+            elif s[i] == ')':
+                balance -= 1
+            if balance > 0:
+                return False
+            if unlocked == 0 and open_brackets == 0:
+                break
+        
+        return False if open_brackets > 0 else True
