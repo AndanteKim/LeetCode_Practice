a9e490a28aa6bfe0b508ff635d276b4266c24278
Andrew Kim <ksb0724@utexas.edu>
2025-01-14 00:33:06 +0900

    Time: 206 ms (32.06%), Space: 18.8 MB (52.38%) - LeetHub

--- 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
+++ 3223-minimum-length-of-string-after-operations/3223-minimum-length-of-string-after-operations.py
@@ -1,15 +1,21 @@
 class Solution:
     def minimumLength(self, s: str) -> int:
-        freq = Counter(s)
+        # Step 1: Initialize a frequency array to count occurrences of each character
+        total_length, char_freq = 0, [0] * 26
 
-        for c, val in freq.items():
-            if val % 2 == 0:
-                freq[c] = 2
+        # Step 2: Count the frequency of each character in the string
+        for curr_char in s:
+            char_freq[ord(curr_char) - 97] += 1
+        
+        # Step 3: Calculate the total length after deletions count
+        for freq in char_freq:
+            if freq == 0:
+                continue    # Skip characters that don't appear
+            
+            if freq % 2:
+                total_length += 1   # If frequency is odd, add 1 character
             else:
-                freq[c] = 1
-        
-        ans = 0
-        for c, val in freq.items():
-            ans += val
-        
-        return ans
+                total_length += 2   # If frequency is even, add 2 characters
+
+        # Step 4: Return the minimum length after deletions count
+        return total_length
