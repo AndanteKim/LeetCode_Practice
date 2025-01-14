990abb7df1048909645f49b5e0a38cd6b1154d3c
Andrew Kim <ksb0724@utexas.edu>
2025-01-15 02:13:39 +0900

    Time: 15 ms (47.92%), Space: 17.7 MB (45.05%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -1,23 +1,24 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
         n = len(A)
-        prefix_common_arr = [0] * n
+        freq, prefix_common_arr = [0] * (n + 1), [0] * n
+        common_count = 0
 
-        # Loop through each index to calculate common elements for each prefix
+        # Iterate through the elements of both arrays
         for curr_idx in range(n):
-            common_cnt = 0
+            # Increment frequency of current elements in A and B
+            # Check if the element in A has appeared before (common in prefix)
+            freq[A[curr_idx]] += 1
+            if freq[A[curr_idx]] == 2:
+                common_count += 1
 
-            # Compare elements in A and B within the range of current prefix
-            for a_idx in range(curr_idx + 1):
-                for b_idx in range(curr_idx + 1):
+            # Check the element in B has appeared before (common in prefix)
+            freq[B[curr_idx]] += 1
+            if freq[B[curr_idx]] == 2:
+                common_count += 1
 
-                    # Check if elements match, and count if they do
-                    if A[a_idx] == B[b_idx]:
-                        common_cnt += 1
-                        break   # Prevent counting duplicates
-            
             # Store the count of common elements for the current prefix
-            prefix_common_arr[curr_idx] = common_cnt
-        
-        # Return the final list with counts of common elements in each prefix
+            prefix_common_arr[curr_idx] = common_count
+
+        # Return the final array with counts of common elements in each prefix
         return prefix_common_arr
