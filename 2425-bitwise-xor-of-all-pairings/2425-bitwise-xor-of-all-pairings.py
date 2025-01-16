554f5f3d6d9eb9a5c43d67a922167477d48bbe33
Andrew Kim <ksb0724@utexas.edu>
2025-01-16 21:39:42 +0900

    Time: 2 ms (76.88%), Space: 36.4 MB (60.69%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -4,14 +4,18 @@ class Solution:
         if len(nums1) % 2 == 0 and len(nums2) % 2 == 0:
             return 0
         
+        # Initialize XOR results for both lists
         num1_xors, num2_xors = 0, 0
         
+        # If the length of nums2 is odd, each element in nums1 appears odd times in final result.
         if len(nums2) % 2 == 1:
             for n1 in nums1:
                 num1_xors ^= n1
         
+        # If the length of nums1 is odd, each element in nums2 appears odd times in final result
         if len(nums1) % 2 == 1:
             for n2 in nums2:
                 num2_xors ^= n2
 
+        # Return XOR of both results
         return num1_xors ^ num2_xors
