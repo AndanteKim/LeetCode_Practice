e435ff0f4188d7097db3123f009fd583d683a197
Andrew Kim <ksb0724@utexas.edu>
2025-01-17 22:44:06 +0900

    Time: 47 ms (58.39%), Space: 22.2 MB (70.07%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -1,15 +1,8 @@
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        # Create an original array initialized with 0.
-        original, n = [0], len(derived)
-        for i in range(n):
-            original.append(derived[i] ^ original[i])
+        XOR = 0
+        
+        for num in derived:
+            XOR ^= num
 
-        # Store the validation results in check_for_zero and check_for_one respectively
-        check_for_zero = original[0] == original[-1]
-        original = [1]
-        for i in range(n):
-            original.append(derived[i] ^ original[i])
-        check_for_one = original[0] == original[-1]
-
-        return check_for_zero or check_for_one
+        return True if XOR == 0 else False
