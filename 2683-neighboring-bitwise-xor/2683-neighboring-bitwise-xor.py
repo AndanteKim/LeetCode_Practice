df7b956cd91dcef0e97900a9659804169ff08d05
Andrew Kim <ksb0724@utexas.edu>
2025-01-17 23:08:17 +0900

    Time: 21 ms (96.35%), Space: 22.4 MB (24.82%) - LeetHub

--- 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
+++ 2683-neighboring-bitwise-xor/2683-neighboring-bitwise-xor.py
@@ -1,8 +1,3 @@
 class Solution:
     def doesValidArrayExist(self, derived: List[int]) -> bool:
-        XOR = 0
-        
-        for num in derived:
-            XOR ^= num
-
-        return True if XOR == 0 else False
+        return sum(derived) % 2 == 0
