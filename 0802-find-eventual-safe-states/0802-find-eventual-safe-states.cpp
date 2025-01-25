8c0febbe8b15bfe8e01c39e753388323f0a59b5a
Andrew Kim <ksb0724@utexas.edu>
2025-01-26 03:12:13 +0900

    Time: 50 ms (46.08%), Space: 66.2 MB (43.05%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,26 +1,42 @@
 class Solution {
-private:
-    bool dfs(int node, vector<vector<int>>& graph, vector<bool>& visited, vector<bool>& inStack){
-        if (inStack[node]) return true;
-        if (visited[node]) return false;
-        
-        inStack[node] = true;
-        visited[node] = true;
-        for (int neighbor : graph[node]){
-            if (dfs(neighbor, graph, visited, inStack)) return true;
-        }
-        inStack[node] = false;
-        return false;
-    }
-    
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> inStack(n, false), visited(n, false);
-        for (int i = 0; i < n; ++i) dfs(i, graph, visited, inStack);
+        vector<int> indegree(n);
+        vector adj(n, vector<int>());
+
+        for (int start = 0; start < n; ++start){
+            for (int end : graph[start]){
+                adj[end].push_back(start);
+                ++indegree[start];
+            }
+        }
+
+        queue<int> q;
+        // Push all the nodes with indegree zero in the queue.
+        for (int node = 0; node < n; ++node)
+            if (indegree[node] == 0)
+                q.push(node);
+
+        vector<bool> safe(n);
+        while (!q.empty()){
+            int node = q.front(); q.pop();
+            safe[node] = true;
+
+            // Delete the edge "node -> neighbor".
+            for (int neighbor : adj[node]){
+                --indegree[neighbor];
+                if (indegree[neighbor] == 0){
+                    q.push(neighbor);
+                }
+            }
+        }
+
         vector<int> ans;
-        for (int i = 0; i < n; ++i) if (!inStack[i]) ans.push_back(i);
-        
+        for (int node = 0; node < n; ++node)
+            if (safe[node])
+                ans.push_back(node);
+
         return ans;
     }
 };
