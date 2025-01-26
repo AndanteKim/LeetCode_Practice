dcfd170e2970dce174e5b201f0e0d790814352ba
Andrew Kim <ksb0724@utexas.edu>
2025-01-27 03:35:11 +0900

    Time: 393 ms (29.1%), Space: 247.2 MB (13.53%) - LeetHub

--- 2127-maximum-employees-to-be-invited-to-a-meeting/2127-maximum-employees-to-be-invited-to-a-meeting.cpp
+++ 2127-maximum-employees-to-be-invited-to-a-meeting/2127-maximum-employees-to-be-invited-to-a-meeting.cpp
@@ -1,51 +1,68 @@
 class Solution {
+private:
+    int n;
+
+    // Helper function for BFS
+    int bfs(int startNode, unordered_set<int>& visitedNodes, vector<vector<int>>& reversedGraph){
+        queue<pair<int, int>> q;
+        q.push({startNode, 0});
+        int maxDistance = 0;
+
+        while (!q.empty()){
+            auto [currPerson, currDistance] = q.front(); q.pop();
+            for (int neighbor : reversedGraph[currPerson]){
+                if (!visitedNodes.count(neighbor)){
+                    visitedNodes.insert(neighbor);
+                    q.push({neighbor, currDistance + 1});
+                    maxDistance = max(maxDistance, currDistance + 1);
+                }
+            }
+        }
+        return maxDistance;
+    }
+
 public:
     int maximumInvitations(vector<int>& favorite) {
-        int n = favorite.size();
-        vector<int> indegree(n);
+        this -> n = favorite.size();
+        vector reversedGraph(n, vector<int>());
         
-        // Calculate in-degree for each node
-        for (int p = 0; p < n; ++p)
-            ++indegree[favorite[p]];
+        for (int person = 0; person < n; ++person)
+            reversedGraph[favorite[person]].push_back(person);
 
-        // Topological sorting to remove non-cycle nodes
-        queue<int> q;
-        for (int p = 0; p < n; ++p)
-            if (indegree[p] == 0)
-                q.push(p);
+        int longestCycle = 0, twoCyclesInvitations = 0;
+        vector<bool> visited(n, false);
 
-        vector<int> depth(n, 1);    // Depth of each node
-        while (!q.empty()){
-            int currNode = q.front(); q.pop();
-            int nextNode = favorite[currNode];
-            depth[nextNode] = max(depth[nextNode], depth[currNode] + 1);
-
-            --indegree[nextNode];
-            if (indegree[nextNode] == 0)
-                q.push(nextNode);
-        }
-
-        int longestCycle = 0, twoCycleInvitations = 0;
-
-        // Detect cycles
+        // Find all cycles
         for (int person = 0; person < n; ++person){
-            if (indegree[person] == 0)   // Already processed
-                continue;
-            
-            int cycleLength = 0, current = person;
-            while (indegree[current] != 0){
-                indegree[current] = 0;  // Mark as visited
-                ++cycleLength;
-                current = favorite[current];
+            if (!visited[person]){
+                int currPerson = person, dist = 0;
+                unordered_map<int, int> visitedPeople;
+                while (true){
+                    if (visited[currPerson])
+                        break;
+                    
+                    visited[currPerson] = true;
+                    visitedPeople[currPerson] = dist++;
+                    int nextPerson = favorite[currPerson];
+
+                    // Cycle detected
+                    if (visitedPeople.count(nextPerson)){
+                        int cycleLength = dist - visitedPeople[nextPerson];
+                        longestCycle = max(longestCycle, cycleLength);
+                        
+                        // Handle cycles of length 2
+                        if (cycleLength == 2){
+                            unordered_set<int> visitedNodes{currPerson, nextPerson};
+                            twoCyclesInvitations += 2 + bfs(nextPerson, visitedNodes, reversedGraph)\
+                             + bfs(currPerson, visitedNodes, reversedGraph);
+                        }
+                        break;
+                    }
+                    currPerson = nextPerson;
+                }
             }
-            
-            if (cycleLength == 2)
-                // For 2-cycles, add the depth of both nodes
-                twoCycleInvitations += depth[person] + depth[favorite[person]];
-            else
-                longestCycle = max(longestCycle, cycleLength);
         }
 
-        return max(longestCycle, twoCycleInvitations);
+        return max(longestCycle, twoCyclesInvitations);
     }
 };
