c413434dc7d28daf717dbe5e891582d03dca8e40
Andrew Kim <ksb0724@utexas.edu>
2025-01-27 16:35:57 +0900

    Time: 70 ms (44.87%), Space: 64 MB (93.96%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,42 +1,27 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector adj(numCourses, vector<int>());
-        vector<int> indegree(numCourses);
+        vector isPrerequisite(numCourses, vector<bool>(numCourses));
 
-        for (int i = 0; i < prerequisites.size(); ++i){
-            int start = prerequisites[i][0], end = prerequisites[i][1];
-            adj[start].push_back(end);
-            ++indegree[end];
+        for (const auto& prerequisite : prerequisites){
+            int start = prerequisite[0], end = prerequisite[1];
+            isPrerequisite[start][end] = true;
         }
 
-        queue<int> q;
-        for (int node = 0; node < numCourses; ++node){
-            if (indegree[node] == 0)
-                q.push(node);
-        }
-
-        // Map from the node as key to the set of prerequisite nodes.
-        unordered_map<int, unordered_set<int>> nodePrerequisites;
-        while (!q.empty()){
-            int node = q.front(); q.pop();
-
-            for (int neighbor : adj[node]){
-                // Add node and prerequisite of node to the prerequisites of adj
-                nodePrerequisites[neighbor].insert(node);
-                for (int prerequisite : nodePrerequisites[node])
-                    nodePrerequisites[neighbor].insert(prerequisite);
-            
-                --indegree[neighbor];
-                if (indegree[neighbor] == 0)
-                    q.push(neighbor);
+        for (int stopOver = 0; stopOver < numCourses; ++stopOver){
+            for (int start = 0; start < numCourses; ++start){
+                for (int end = 0; end < numCourses; ++end){
+                    // If there's a path start -> stopOver and stopOver -> end, then start -> end exists as well.
+                    isPrerequisite[start][end] = isPrerequisite[start][end] || \
+                    (isPrerequisite[start][stopOver] && isPrerequisite[stopOver][end]);
+                }
             }
         }
-
-        vector<bool> ans;
+        
+        vector<bool> ans(queries.size());
         for (int i = 0; i < queries.size(); ++i){
             int start = queries[i][0], end = queries[i][1];
-            ans.push_back(nodePrerequisites[end].count(start)? true : false);
+            ans[i] = isPrerequisite[start][end];
         }
 
         return ans;
