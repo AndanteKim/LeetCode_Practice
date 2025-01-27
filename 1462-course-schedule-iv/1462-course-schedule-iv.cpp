900834ba686586b64a94cf5448a6943681a775c5
Andrew Kim <ksb0724@utexas.edu>
2025-01-27 14:25:27 +0900

    Time: 53 ms (55.61%), Space: 72.9 MB (42.09%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,35 +1,42 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<int>> graph(numCourses);
-        vector<bool> ans(queries.size(), false);
+        vector adj(numCourses, vector<int>());
+        vector<int> indegree(numCourses);
 
         for (int i = 0; i < prerequisites.size(); ++i){
             int start = prerequisites[i][0], end = prerequisites[i][1];
-            graph[start].push_back(end);
+            adj[start].push_back(end);
+            ++indegree[end];
         }
 
+        queue<int> q;
+        for (int node = 0; node < numCourses; ++node){
+            if (indegree[node] == 0)
+                q.push(node);
+        }
+
+        // Map from the node as key to the set of prerequisite nodes.
+        unordered_map<int, unordered_set<int>> nodePrerequisites;
+        while (!q.empty()){
+            int node = q.front(); q.pop();
+
+            for (int neighbor : adj[node]){
+                // Add node and prerequisite of node to the prerequisites of adj
+                nodePrerequisites[neighbor].insert(node);
+                for (int prerequisite : nodePrerequisites[node])
+                    nodePrerequisites[neighbor].insert(prerequisite);
+            
+                --indegree[neighbor];
+                if (indegree[neighbor] == 0)
+                    q.push(neighbor);
+            }
+        }
+
+        vector<bool> ans;
         for (int i = 0; i < queries.size(); ++i){
             int start = queries[i][0], end = queries[i][1];
-            queue<int> q;
-            q.push(start);
-            vector<bool> visited(numCourses);
-
-            while (!q.empty()){
-                int curr = q.front(); q.pop();
-        
-                visited[curr] = true;
-                if (curr == end){
-                    ans[i] = true;
-                    break;
-                }
-
-                for (int neighbor : graph[curr]){
-                    if (!visited[neighbor]){
-                        q.push(neighbor);
-                    }
-                }
-            }
+            ans.push_back(nodePrerequisites[end].count(start)? true : false);
         }
 
         return ans;
