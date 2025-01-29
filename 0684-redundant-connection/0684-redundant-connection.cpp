eb1c2ac14cc8120b56e40abfcaed5cdca3194a8f
Andrew Kim <ksb0724@utexas.edu>
2025-01-30 03:17:15 +0900

    Time: 2 ms (40.15%), Space: 13.9 MB (26.44%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,39 +1,57 @@
 class Solution {
 private:
-    int n;
-    
-    // Performs DFS and returns True if there's a path between src and target
-    bool isConnected(int src, int target, vector<bool>& visited, vector<vector<int>>& adjList){
+    int n, startCycle = -1;
+
+    // Perform the DFS and store a node in the cycle as cycleStart
+    void dfs(int src, vector<bool>& visited, vector<int>& parent, vector<vector<int>>& graph){
         visited[src] = true;
 
-        if (src == target)
-            return true;
+        for (int adj : graph[src]){
+            if (!visited[adj]){
+                parent[adj] = src;
+                dfs(adj, visited, parent, graph);
+            }
 
-        bool isFound = false;
-
-        for (int adj : adjList[src]){
-            if (!visited[adj])
-                isFound |= isConnected(adj, target, visited, adjList);
-            
+            // If the node is visited and the parent is different then
+            // the node is part of the cycle
+            else if (parent[src] != adj && startCycle == -1){
+                parent[adj] = src;
+                startCycle = adj;
+            }
         }
 
-        return isFound;
     }
 
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         this -> n = edges.size();
-        vector<vector<int>> adjList(n);
+        vector graph(n, vector<int>{});
         
         for (const vector<int>& edge : edges){
-            vector<bool> visited(n, false);
-            
-            // If DFS returns True, we'll return the edge.
-            if (isConnected(edge[0] - 1, edge[1] - 1, visited, adjList))
-                return edge;
+            int u = edge[0], v = edge[1];
+            graph[u - 1].push_back(v - 1);
+            graph[v - 1].push_back(u - 1);
+        }
 
-            adjList[edge[0] - 1].push_back(edge[1] - 1);
-            adjList[edge[1] - 1].push_back(edge[0] - 1);
+        vector<bool> visited(n, false);
+        vector<int> parent(n, -1);
+        dfs(0, visited, parent, graph);
+        
+        unordered_set<int> isConnected;
+        int node = startCycle;
+
+        // Start from the startCycle node and backtrack to get all the nodes
+        // in the cycle. Mark them all in the map.
+        do {
+            node = parent[node];
+            isConnected.insert(node);
+        } while(node != startCycle);
+
+        // If both nodes of the edge were marked as cycle nodes then this edge
+        // can be removed.
+        for (int i = n - 1; i >= 0; --i){
+            if (isConnected.count(edges[i][0] - 1) && isConnected.count(edges[i][1] - 1))
+                return edges[i];
         }
 
         return vector<int>{};
