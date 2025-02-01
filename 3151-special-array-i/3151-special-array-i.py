cf136d3535b6a426cafb9458afc028602a89af72
Andrew Kim <ksb0724@utexas.edu>
2025-02-01 16:36:46 +0900

    Time: 0 ms (100%), Space: 17.9 MB (22.9%) - LeetHub

--- 3151-special-array-i/3151-special-array-i.py
+++ 3151-special-array-i/3151-special-array-i.py
@@ -1,14 +1,11 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        if len(nums) == 1:
-            return True
-
-        if len(nums) == 2:
-            return nums[0] % 2 != nums[1] % 2
-
-        n = len(nums)
-        for i in range(1, n - 1):
-            if (nums[i - 1] % 2 == nums[i] % 2) or (nums[i] % 2 == nums[i + 1] % 2):
+        # Iterate  through indics 0 to n - 1
+        for i in range(len(nums) - 1):
+            # Compare the parities of the current and next number
+            if nums[i] % 2 == nums[i + 1] % 2:
+                # If the two adjacent numbers have the same parity, return False
                 return False
-        
+
+        # Return True if no pair of adjacent numbers with the same parity are found
         return True
