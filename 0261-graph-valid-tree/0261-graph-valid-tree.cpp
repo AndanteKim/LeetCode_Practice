d64a7f9914421bf655d0354fe0cde0c85f55e551
Andrew Kim <ksb0724@utexas.edu>
2025-02-03 01:01:20 +0900

    Time: 0 ms (100%), Space: 16.1 MB (84.23%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.cpp
+++ 0261-graph-valid-tree/0261-graph-valid-tree.cpp
@@ -1,31 +1,83 @@
+class UnionFind{
+private:
+    // For efficiency, we aren't using makeset, but instead initializing
+    // all the sets at the same time in the constructor.
+    vector<int> parent, rootSize;
+
+public:
+    UnionFind(int n){
+        parent.resize(n);
+        iota(parent.begin(), parent.end(), 0);
+        // We use this to keep track of the size of each set.
+        rootSize.resize(n, 1);
+    }
+
+    // The find method, with path compression. There are ways of implementing
+    // this elegantly with recursion, but the iterative version is easier ofr
+    // most people to understand!
+    int find(int node){
+        // Step 1: Find the root.
+        int root = parent[node];
+
+        while (root != parent[root])
+            root = find(parent[root]);
+
+        // Step 2: Do a second traversal, this time setting each node to point
+        // directly at node as we go.
+        while (node != root){
+            int oldNode = parent[node];
+            parent[node] = root;
+            node = parent[oldNode];
+        }
+
+        return root;
+    }
+
+    // The unite method, with optimization union by size. It returns true if a merge happend,
+    // false if otherwise.
+    bool unite(int u, int v){
+        // Find the roots for u and v.
+        int rootU = find(u), rootV = find(v);
+        
+        // Check if u and v already in the same set.
+        if (rootU == rootV) return false;
+
+        // We want to ensure the larger set remains the root.
+        if (rootSize[rootU] < rootSize[rootV]){
+            // Make rootV the overall root.
+            parent[rootU] = rootV;
+            // The size of the set rooted at v is the sum of the 2.
+            rootSize[rootV] += rootSize[rootU];
+        }
+        else{
+            // Make rootU the overall root.
+            parent[rootV] = rootU;
+            // The size of the set rooted at u is the sum of the 2.
+            rootSize[rootU] += rootSize[rootV];
+        }
+
+        return true;
+    }
+
+};
+
 class Solution {
 public:
     bool validTree(int n, vector<vector<int>>& edges) {
-        if (n - 1 != edges.size()) return false;
+        // Base case: The graph must contain n - 1 edges
+        if (edges.size() != n - 1) return false;
 
-        vector graph(n, vector<int>());
+        // Create a new UnionFind object with n nodes.
+        UnionFind *uf = new UnionFind(n);
 
+        // Add each edge. Check if a merge happened because if it didn't
+        //, there must be a cycle.
         for (const vector<int>& edge : edges){
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+            int u = edge[0], v = edge[1];
+            if (!uf -> unite(u, v)) return false;
         }
 
-        unordered_map<int, int> parent{{0, -1}};
-        queue<int> q;
-        q.push(0);
-
-        while (!q.empty()){
-            int node = q.front(); q.pop();
-
-            for (int neighbor : graph[node]){
-                if (neighbor == parent[node]) continue;
-
-                if (parent.count(neighbor)) return false;
-                parent[neighbor] = node;
-                q.push(neighbor);
-            }
-        }
-        
-        return parent.size() == n;
+        // If we got this far, there's no cycles!
+        return true;
     }
 };
