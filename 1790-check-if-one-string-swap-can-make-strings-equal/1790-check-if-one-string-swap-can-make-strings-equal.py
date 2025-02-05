6b24c8b3ad54c07b0eb3f75f362cf901742335bc
Andrew Kim <ksb0724@utexas.edu>
2025-02-05 22:07:08 +0900

    Time: 0 ms (100%), Space: 17.9 MB (27.73%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.py
@@ -1,14 +1,15 @@
 class Solution:
     def areAlmostEqual(self, s1: str, s2: str) -> bool:
-        
-        if s1 == s2: return True
-        c1, c2, cnt = Counter(s1), Counter(s2), 0
-        
-        for i in range(len(s1)):
-            cnt += s1[i] != s2[i]
-            if cnt > 2:
+        n, freq1, freq2, unmatch_cnt = len(s1), dict(), dict(), 0
+
+        for i in range(n):
+            if s1[i] != s2[i]:
+                unmatch_cnt += 1
+            
+            if unmatch_cnt > 2:
                 return False
-            if c1[s1[i]] != c2[s1[i]]:
-                return False
-        return True
-    
+
+            freq1[s1[i]] = freq1.get(s1[i], 0) + 1
+            freq2[s2[i]] = freq2.get(s2[i], 0) + 1
+
+        return freq1 == freq2
