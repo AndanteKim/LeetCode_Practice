4a3c1850b11bec0428cb36500a4ba8906ce39292
Andrew Kim <ksb0724@utexas.edu>
2025-02-09 03:40:17 +0900

    Time: 96 ms (92.65%), Space: 76.4 MB (88.24%) - LeetHub

--- 2349-design-a-number-container-system/2349-design-a-number-container-system.py
+++ 2349-design-a-number-container-system/2349-design-a-number-container-system.py
@@ -1,28 +1,34 @@
 class NumberContainers:
 
     def __init__(self):
-        # Initializing the defaultdict with SortedSet and the regular dictionary
-        # Map from number to set of indices
-        self.num_to_idx = defaultdict(SortedSet)
-        # Map from index to number
+        # Map to store number -> min heap of indices
+        self.num_to_idx = defaultdict(list)
+        # Map to store index -> number
         self.idx_to_num = dict()
 
     def change(self, index: int, number: int) -> None:
-        # If index already has a number, remove it from the old number's index set
-        if index in self.idx_to_num:
-            prev_num = self.idx_to_num[index]
-            self.num_to_idx[prev_num].remove(index)
-            if not self.num_to_idx[prev_num]:
-                del self.num_to_idx[prev_num]
-
-        # Update the number and add the index to the new number's set
+        # Update index to number mapping
         self.idx_to_num[index] = number
-        self.num_to_idx[number].add(index)
+
+        # Add index to the min heap for this number
+        heappush(self.num_to_idx[number], index)
 
     def find(self, number: int) -> int:
-        # Return the smallest index for the given number, or -1 if not found
-        if number in self.num_to_idx and self.num_to_idx[number]:
-            return self.num_to_idx[number][0]
+        # If number doesn't exist in our map
+        if not self.num_to_idx[number]:
+            return -1
+
+        # Keep checking top element until we find valid index
+        while self.num_to_idx[number]:
+            idx = self.num_to_idx[number][0]
+
+            # If index still maps to our target numbers, return it
+            if self.idx_to_num.get(idx) == number:
+                return idx        
+
+            # Otherwise remove this stale index
+            heappop(self.num_to_idx[number])
+
         return -1
 
 # Your NumberContainers object will be instantiated and called as such:
