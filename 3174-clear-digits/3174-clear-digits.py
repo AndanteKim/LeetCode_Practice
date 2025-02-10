bf9b4bb1981ce4680519cbf3c30675c02f50a75f
Andrew Kim <ksb0724@utexas.edu>
2025-02-10 19:21:28 +0900

    Time: 0 ms (100%), Space: 17.9 MB (27.31%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.py
+++ 3174-clear-digits/3174-clear-digits.py
@@ -1,13 +1,20 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
-        stack = []
+        # This variable keeps track of the actual length of the resulting string
+        ans_len, s = 0, list(s)
 
-        for c in s:
-            if 48 <= ord(c) <= 57:
-                if stack:
-                    stack.pop()
-
+        # Iterate through each character in the input string
+        for chr_idx in range(len(s)):
+            # If the current character is a digit
+            if s[chr_idx].isdigit():
+                # Decrement ans_len to remove the last character from the result
+                ans_len = max(ans_len - 1, 0)
             else:
-                stack.append(c)
+                # Place the character in the "answer" portion of the string
+                s[ans_len] = s[chr_idx]
+                ans_len += 1
+        
+        # Resize the string to match the actual length of the answer
+        s = s[:ans_len]
 
-        return "".join(stack)
+        return "".join(s)
