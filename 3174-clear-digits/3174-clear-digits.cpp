90cbc6ea0f4a51b33574a3e4cb5b0703431b4267
Andrew Kim <ksb0724@utexas.edu>
2025-02-10 19:23:17 +0900

    Time: 0 ms (100%), Space: 8.6 MB (56.73%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.cpp
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        deque<char> st;
-
-        for (const char& c : s){
-            if (48 <= c && c <= 57){
-                if (!st.empty()) st.pop_back();
-            }
-            else
-                st.push_back(c);
-        }
-
         string ans = "";
-        while (!st.empty()){
-            ans.push_back(st.front()); st.pop_front();
+
+        // Iterate over each character in the input string
+        for (int charIndex = 0; charIndex < s.size(); ++charIndex){
+            // If the current character is a digit
+            if (isdigit(s[charIndex]) && !ans.empty())
+                // If the answer string isn't empty, remove the last character
+                ans.pop_back();
+            else
+                // If the character is not a digi, add it to the answer string
+                ans.push_back(s[charIndex]);
         }
 
         return ans;
