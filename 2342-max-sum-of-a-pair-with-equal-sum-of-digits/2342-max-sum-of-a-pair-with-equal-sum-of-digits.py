a0ebe77df0942cdbd78bc67dc963c08018e7aaa2
Andrew Kim <ksb0724@utexas.edu>
2025-02-13 02:04:00 +0900

    Time: 540 ms (19.87%), Space: 39.5 MB (5.46%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -1,20 +1,35 @@
 class Solution:
+    # Helper function to compute the sum digits of a number
+    def calculate_digit_sum(self, num: int) -> int:
+        digit_sum = 0
+        while num > 0:
+            digit_sum += num % 10
+            num //= 10
+        
+        return digit_sum
+    
     def maximumSum(self, nums: List[int]) -> int:
-        def find_sum_digits(num: int) -> int:
-            sum_digits = 0
-            while num > 0:
-                sum_digits += num % 10
-                num //= 10
+        digit_sum_pairs = []
 
-            return sum_digits
-
-        seen = defaultdict(list)
+        # Store number with their digit sums as pairs
         for num in nums:
-            heappush(seen[find_sum_digits(num)], -num)
-        
+            digit_sum = self.calculate_digit_sum(num)
+            digit_sum_pairs.append((digit_sum, num))
+
+        # Sort based on digit sums, and if equal, by number value
+        digit_sum_pairs.sort()
+
         ans = -1
-        for key in seen:
-            if len(seen[key]) >= 2:
-                ans = max(ans, -(heappop(seen[key]) + heappop(seen[key])))
-        
+
+        # Iterate through the sorted array to find the maximum sum of pairs
+        for idx in range(1, len(digit_sum_pairs)):
+            curr_digit_sum, prev_digit_sum = digit_sum_pairs[idx][0], digit_sum_pairs[idx - 1][0]
+
+            # If two consecutive numbers have the same digit sum
+            if curr_digit_sum == prev_digit_sum:
+                curr_sum = digit_sum_pairs[idx][1] + digit_sum_pairs[idx - 1][1]
+                ans = max(ans, curr_sum)
+
         return ans
+
+        
