ce78e04bc5ba85c5e4f2c8f7ea86e6a2c84f6570
Andrew Kim <ksb0724@utexas.edu>
2025-02-12 21:27:47 +0900

    Time: 23 ms (64.47%), Space: 71.7 MB (35.53%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -1,22 +1,32 @@
 class Solution {
-public:
-    int get_digit_sum(int num){
-        int digit_sum = 0;
+private:
+    int findSumDigits(int num){
+        int ans = 0;
+
         while (num > 0){
-            digit_sum += num % 10;
+            ans += num % 10;
             num /= 10;
         }
-        return digit_sum;
+
+        return ans;
     }
-    
+
+public:
     int maximumSum(vector<int>& nums) {
-        unordered_map<int, int> dic;
-        int ans = -1, digit_sum;
-        for (const int &num : nums){
-            digit_sum = get_digit_sum(num);
-            if (dic.find(digit_sum) != dic.end()) ans = max(ans, num + dic[digit_sum]);
-            dic[digit_sum] = max(dic[digit_sum], num);
+        unordered_map<int, priority_queue<int, vector<int>>> seen;
+        for (int num : nums){
+            seen[findSumDigits(num)].push(num);
         }
+
+        int ans = -1;
+        for (auto& [num, maxHeap] : seen){
+            if (maxHeap.size() >= 2){
+                int pairSum = maxHeap.top(); maxHeap.pop();
+                pairSum += maxHeap.top(); maxHeap.pop();
+                ans = max(ans, pairSum);
+            }
+        }
+
         return ans;
     }
 };
