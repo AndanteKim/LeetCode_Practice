2b6b000b435f8ea32bf1c8c8d416d2c66daf07c1
Andrew Kim <ksb0724@utexas.edu>
2025-02-19 22:33:45 +0900

    Time: 49 ms (36.76%), Space: 22.6 MB (8.97%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.py
@@ -1,30 +1,14 @@
-class Solution {
-private:
-    int n, k;
-    void backtrack(int start, string& word, vector<string>& ansList){
-        if (start == n){
-            ansList.push_back(word);
-            return;
-        }
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        def backtrack(i: int, curr: str):
+            if i == n:
+                words.append(curr)
+                return
+
+            for ch in ('a', 'b', 'c'):
+                if curr == "" or ch != curr[-1]:
+                    backtrack(i + 1, curr + ch)
         
-        for (const char& c: vector<char>{'a','b','c'}){
-            if (!word.empty() && word.back() == c) continue;
-            else{
-                word.push_back(c);
-                backtrack(start + 1, word, ansList);
-                word.pop_back();
-            }
-        }
-        
-    }
-    
-public:
-    string getHappyString(int n, int k) {
-        vector<string> ansList;
-        string word = "";
-        this -> n = n, this -> k = k;
-        backtrack(0, word, ansList);
-        
-        return ansList.size() < k? "" : ansList[k - 1];
-    }
-};
+        words = []
+        backtrack(0, "")
+        return "" if k > len(words) else words[k - 1]
