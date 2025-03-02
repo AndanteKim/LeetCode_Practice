60cd5271e2076d1fc2599ac01e13ea00990701bc
Andrew Kim <ksb0724@utexas.edu>
2025-03-02 21:44:34 +0900

    Time: 0 ms (100%), Space: 14.7 MB (64.62%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,36 +1,41 @@
 class Solution {
-private:
-    int countNums(vector<vector<int>>& nums){
-        int count = 0;
-        for (const auto& num : nums)
-            count = max(count, num[0]);
-        
-        return count + 1;
-    }
-
-    void merge(vector<int>& rows, vector<vector<int>>& nums){
-        for (const auto& num : nums){
-            int id = num[0], val = num[1];
-            rows[id] += val;
-        }
-    }
-
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        int maxCount = countNums(nums1);
-        maxCount = max(maxCount, countNums(nums2));
+        int n1 = nums1.size(), n2 = nums2.size();
+        int ptr1 = 0, ptr2 = 0;
 
-        vector<int> rows(maxCount);
-
-        merge(rows, nums1);
-        merge(rows, nums2);
-        
         vector<vector<int>> ans;
 
-        for (int i = 0; i < rows.size(); ++i){
-            if (rows[i] > 0)
-                ans.push_back({i, rows[i]});
+        while (ptr1 < n1 && ptr2 < n2){
+            // If the id is same, add the values and insert to the result.
+            // increment both pointeres.
+            if (nums1[ptr1][0] == nums2[ptr2][0]){
+                ans.push_back(
+                    {nums1[ptr1][0], nums1[ptr1][1] + nums2[ptr2][1]}
+                );
+                ++ptr1;
+                ++ptr2;
+            }
+            else if (nums1[ptr1][0] < nums2[ptr2][0]){
+                // If the id in nums1 is smaller, add it to result and increment the pointer
+                ans.push_back(nums1[ptr1++]);
+            }
+            else{
+                // If the id in nums2 is smaller, add it to result and increment the pointer
+                ans.push_back(nums2[ptr2++]);
+            }
         }
+
+        // If pairs are remaining in the nums1, then add them to the result.
+        while (ptr1 < n1){
+           ans.push_back(nums1[ptr1++]);
+        }
+
+        // If pairs are remaining in the nums2, then add them to the result.
+        while (ptr2 < n2){
+            ans.push_back(nums2[ptr2++]);
+        }
+
         return ans;
     }
 };
