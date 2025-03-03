b925ddd4683e7ea0d5f8858202ad41205db92b9b
Andrew Kim <ksb0724@utexas.edu>
2025-03-03 21:10:58 +0900

    Time: 67 ms (14.74%), Space: 34.8 MB (23.77%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.py
@@ -1,22 +1,19 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        def merge(ans: List[int], elems: Deque[int]) -> None:
-            while elems:
-                ans.append(elems.popleft())
+        ans = [0] * len(nums)
+        less_i, greater_i = 0, len(nums) - 1
+
+        for i, j in zip(range(len(nums)), range(len(nums) - 1, -1, -1)):
+            if nums[i] < pivot:
+                ans[less_i] = nums[i]
+                less_i += 1
+            
+            if nums[j] > pivot:
+                ans[greater_i] = nums[j]
+                greater_i -= 1
         
-        left_side, center, right_side = deque(), deque(), deque()
-
-        for num in nums:
-            if num == pivot:
-                center.append(num)
-            elif num < pivot:
-                left_side.append(num)
-            else:
-                right_side.append(num)
-
-        ans = []
-        merge(ans, left_side)
-        merge(ans, center)
-        merge(ans, right_side)
-
+        while less_i <= greater_i:
+            ans[less_i] = pivot
+            less_i += 1
+        
         return ans
