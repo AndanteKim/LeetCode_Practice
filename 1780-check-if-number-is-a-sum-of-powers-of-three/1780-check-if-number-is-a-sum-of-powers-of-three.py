193672ee9a4cceb232217f1204b1f37e9570e4fa
Andrew Kim <ksb0724@utexas.edu>
2025-03-05 02:18:38 +0900

    Time: 0 ms (100%), Space: 18 MB (18.33%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,22 +1,12 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
-        power = 0
-
-        # Find the largest poewr that is smaller or equal to n
-        while 3 ** power <= n:
-            power += 1
-
-        while n > 0:
-            # Subtract current power from n
-            if n >= 3 ** power:
-                n -= 3 ** power
-
-            # We cannot use the same power twice
-            if n >= 3 ** power:
+        while n >0:
+            # Check if this power should be used twice
+            if n % 3 == 2:
                 return False
 
-            # Move to the next lower power
-            power -= 1
+            # Divide n by 3 to move to the next greater power
+            n //= 3
 
-        # n has reached 0
+        # The ternary representation of n consists only of 0s and 1s
         return True
