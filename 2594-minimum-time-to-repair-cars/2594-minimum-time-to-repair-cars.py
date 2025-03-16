09a67ba72b1bcfd48d9feb61e9bc0598e8ecfb3d
Andrew Kim <ksb0724@utexas.edu>
2025-03-16 23:29:32 +0900

    Time: 1149 ms (20.49%), Space: 21.9 MB (84.79%) - LeetHub

--- 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
+++ 2594-minimum-time-to-repair-cars/2594-minimum-time-to-repair-cars.py
@@ -1,32 +1,21 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        n, min_rank, max_rank = len(ranks), ranks[0], ranks[0]
-        
-        # Frequency list to count meachanics with each rank
-        freq = [0] * 101
-
-        for rank in ranks:
-            freq[rank] += 1
-
-            # Find min and max rank dynamically
-            min_rank = min(min_rank, rank)
-            max_rank = max(max_rank, rank)
-
-        # Minimum possible time, Maximum possible time
-        low, high, ans = 1, min_rank * cars * cars, float('inf')
+        # The minimum possible time is 1,
+        # and the maximum possible time is when the slowest mechanic(highest rank) repairs all cars.
+        low, high = 1, ranks[0] * cars * cars
 
         # Perform binary search to find the minimum time required
         while low < high:
-            mid, cars_repaired = low + ((high - low) >> 1), 0
+            mid = low + ((high - low) >> 1)
+            cars_repaired = 0
 
-            # Calculate the total number of cars that can be repaired in 'mid' time
-            for rank in range(1, max_rank + 1):
-                cars_repaired += freq[rank] * int((mid / rank) ** 0.5)
+            for rank in ranks:
+                cars_repaired += int((mid / rank) ** 0.5)
             
-            # Adjust the search boundaries based on the number of cars repaired
-            if cars_repaired >= cars:
-                high = mid  # Try to find a smaller time
+            # If the total cars repaired is less than required, increase the time.
+            if cars_repaired < cars:
+                low = mid + 1
             else:
-                low = mid + 1   # Need more time
-
+                high = mid  # Otherwise, try a smaller time.
+        
         return low
