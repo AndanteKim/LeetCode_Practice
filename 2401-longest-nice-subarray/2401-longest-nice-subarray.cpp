7e139786ab20261964acd04707b6478c47f142ef
Andrew Kim <ksb0724@utexas.edu>
2025-03-19 00:03:44 +0900

    Time: 0 ms (100%), Space: 61 MB (64.15%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
-        // Bit manipulation and sliding window
-        int left = 0, used = 0, ans = 0;
-        
-        for (int right = 0; right < nums.size(); ++right){
-            while (left < right && (nums[right] & used)){
-                used ^= nums[left++];
+        int n = nums.size(), ans = 0;   // Length of longest nice subarray found
+
+        // Tracks bits used in current winbdow
+        // Start position of current window
+        int usedBits = 0, left = 0;
+        for (int right = 0; right < n; ++right){
+            while ((usedBits & nums[right]) != 0){
+                usedBits ^= nums[left++];   // Remove leftmost element's bits and shrink window from left
             }
-            used |= nums[right];
+            
+            // Add current number to the window
+            usedBits ^= nums[right];
+            
+            // Update max length if current window is longer
             ans = max(ans, right - left + 1);
         }
-        
+
+
         return ans;
     }
 };
