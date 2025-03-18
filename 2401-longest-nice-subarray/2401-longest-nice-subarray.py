e9652d3e45a7033153d14d2427872db8f5601e32
Andrew Kim <ksb0724@utexas.edu>
2025-03-19 00:03:34 +0900

    Time: 91 ms (61.28%), Space: 31.8 MB (82.62%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.py
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -1,12 +1,21 @@
 class Solution:
     def longestNiceSubarray(self, nums: List[int]) -> int:
-        left, ans, used = 0, 0, 0
-        
-        for right in range(len(nums)):
-            while left < right and (used & nums[right]):
-                used ^= nums[left]
-                left += 1
-            used |= nums[right]
-            ans = max(ans, right - left + 1)
+        # used_bits := Tracks bits used in current window
+        # left := Start position of current window
+        used_bits, left = 0, 0
+        ans, n = 0, len(nums)   # Length of longest nice subarray found
+
+        for right in range(n):
+            # If the current number shares bits with window, shrink window from left
+            # until there's no bit conflict
+            while used_bits & nums[right] != 0:
+                used_bits ^= nums[left] # Remove leftmost element's bits
+                left += 1   # Shrink window from left
             
+            # Add current number to the window
+            used_bits ^= nums[right]
+
+            # Update max length if current window is longer
+            ans = max(ans, right - left + 1)
+
         return ans
