e64f4cba55af08c76d5642e846b29bf41b85c4c5
Andrew Kim <ksb0724@utexas.edu>
2025-03-27 22:10:08 +0900

    Time: 111 ms (30.69%), Space: 34.7 MB (24.26%) - LeetHub

--- 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
+++ 2780-minimum-index-of-a-valid-split/2780-minimum-index-of-a-valid-split.py
@@ -3,14 +3,18 @@ class Solution:
         n = len(nums)
         first_map, second_map = defaultdict(int), defaultdict(int)
 
+        # Add all elements of nums to second_map
         for num in nums:
             second_map[num] += 1
         
         for i, num in enumerate(nums):
+            # Create split at current index
             second_map[num] -= 1
             first_map[num] += 1
 
+            # Check if valid split
             if first_map[num] * 2 > i + 1 and second_map[num] * 2 > n - i - 1:
                 return i
 
+        # No valid split exists
         return -1
