1fbb5b62a6dc71bdc93f10989c129583efe9001a
Andrew Kim <ksb0724@utexas.edu>
2025-03-31 01:32:26 +0900

    Time: 247 ms (5.03%), Space: 17.8 MB (69.95%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -1,25 +1,31 @@
 class Solution:
     def shortestWay(self, source: str, target: str) -> int:
-        source_length = len(source)
-        next_occurrence = [defaultdict(int) for idx in range(source_length)]
-        
-        next_occurrence[source_length - 1][source[source_length - 1]] = source_length - 1
-        
-        for idx in range(source_length - 2, -1, -1):
-            next_occurrence[idx] = next_occurrence[idx + 1].copy()
-            next_occurrence[idx][source[idx]] = idx
+        # To check it to_check is subsequence of in_string
+        def is_subsequence(to_check: str, in_string: str) -> bool:
+            i = j = 0
+            while i < len(to_check) and j < len(in_string):
+                if to_check[i] == in_string[j]:
+                    i += 1
+                j += 1
             
-        source_iterator, count = 0, 1
-        
-        for char in target:
-            if char not in next_occurrence[0]:
+            return i == len(to_check)
+
+        # Set of all characters of the source. We could use a boolean array as well.
+        source_chars = set(source)
+
+        # Check if all characters of the target are present in the source
+        # If any character is not present, return -1
+        for ch in target:
+            if ch not in source_chars:
                 return -1
-            
-            if (source_iterator == source_length or \
-               char not in next_occurrence[source_iterator]):
-                count += 1
-                source_iterator = 0
-            
-            source_iterator = next_occurrence[source_iterator][char] + 1
+
+        # Concatenate source until the target is a subsequence of the concatenated string
+        concatenated_source = source
+        ans = 1
+
+        while not is_subsequence(target, concatenated_source):
+            concatenated_source += source
+            ans += 1
         
-        return count
+        # Number of concatenations done
+        return ans
