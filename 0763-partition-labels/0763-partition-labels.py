66e06104037d9f1b949191128396d43db063711c
Andrew Kim <ksb0724@utexas.edu>
2025-03-31 00:20:06 +0900

    Time: 7 ms (41.22%), Space: 17.8 MB (57.51%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,20 +1,30 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        # Stores the last index of each character in 's'
-        last_occurrence = [0] * 26
+        ans = []
+        first_occurrence, last_occurrence = [-1] * 26, [0] * 26
+        partition_start, partition_end = 0, 0
 
+        # Store the last occurrence index of each character
         for i, ch in enumerate(s):
             last_occurrence[ord(ch) - 97] = i
 
-        partition_end, partition_start = 0, 0
-        ans = []
-
         for i, ch in enumerate(s):
-            partition_end = max(partition_end, last_occurrence[ord(ch) - 97])
+            idx = ord(ch) - 97
 
-            # End of the current partition
-            if i == partition_end:
-                ans.append(i - partition_start + 1)
-                partition_start = i + 1
+            # Store the first occurrence index of each character (if not set)
+            if first_occurrence[idx] == -1:
+                first_occurrence[idx] = i
+
+            # If we find a new partition start
+            if partition_end < first_occurrence[idx]:
+                ans.append(partition_end - partition_start + 1)
+                partition_start, partition_end = i, i
+
+            # Update partition end boundary
+            partition_end = max(partition_end, last_occurrence[idx])
+
+        # Add the last partition if it exists
+        if partition_end - partition_start + 1 > 0:
+            ans.append(partition_end - partition_start + 1)
 
         return ans
