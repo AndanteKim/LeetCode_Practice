2789b545b70474a0b5f57efcb9a6dc7e4e26df70
Andrew Kim <ksb0724@utexas.edu>
2025-04-05 19:30:34 +0900

    Time: 0 ms (100%), Space: 9.1 MB (71.99%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,19 +1,14 @@
 class Solution {
-private:
-    int ans = 0, n;
-    void backtrack(int i, int prev, vector<int>& nums){
-        if (i == n) return;
-
-        int curr = prev ^ nums[i];
-        ans += curr;
-        backtrack(i + 1, prev, nums);
-        backtrack(i + 1, curr, nums);
-    }
-
 public:
     int subsetXORSum(vector<int>& nums) {
-        this -> n = nums.size();
-        backtrack(0, 0, nums);
-        return ans;
+        int ans = 0;
+
+        // Capture each bit that is set in any of the elements
+        for (int num : nums){
+            ans |= num;
+        }
+
+        // Multiply by the number of subset XOR totals that will have each bit set
+        return ans << ((int)nums.size() - 1);
     }
 };
