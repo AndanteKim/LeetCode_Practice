7c26478804a83a0b0494f34326d7c250aba09136
Andrew Kim <ksb0724@utexas.edu>
2025-04-05 19:25:08 +0900

    Time: 0 ms (100%), Space: 17.9 MB (23.27%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,10 +1,10 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
         ans = 0
-        
+
         # Capture each bit that is set in any of the elements
         for num in nums:
             ans |= num
-            
+
         # Multiply by the number of subset XOR totals that will have each bit set
         return ans << (len(nums) - 1)
