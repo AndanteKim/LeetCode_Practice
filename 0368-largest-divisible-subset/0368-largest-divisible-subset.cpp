297390532fb77608abbd0caecae9aae33c0dec80
Andrew Kim <ksb0724@utexas.edu>
2025-04-06 22:48:45 +0900

    Time: 9 ms (79.63%), Space: 12.3 MB (99.72%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -2,34 +2,50 @@ class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         // Base case
-        int n = nums.size();
-        if (n == 0) return vector<int>{};
+        if (nums.size() == 0) return {};
 
-        // Container to keep the largest divisible subset
-        // ending with each of the nums
-        map<int, vector<int>> EDS;
-        for (int num : nums) EDS[num] = vector<int>{};
-        
-        // Sort the original list in ascending order
+        // Sort the nums
         sort(nums.begin(), nums.end());
 
+        // The container keeping the size of the largest divisible subset ending with X_i
+        // dp[i] corresponds to the length of EDS(X_i)
+        int n = nums.size(), maxSubsetSize = 0;
+        vector<int> dp(n);
+
+        // Build the dynamic programming matrix/vector
         for (int i = 0; i < n; ++i){
-            vector<int> maxCurr;
-
-            // Find the largest divisible subset of previous elements
+            int maxSubsetSize = 0;
             for (int k = 0; k < i; ++k){
-                if (nums[i] % nums[k] == 0 && maxCurr.size() < EDS[k].size())
-                    maxCurr = EDS[k];
+                if (nums[i] % nums[k] == 0)
+                    maxSubsetSize = max(maxSubsetSize, dp[k]);
             }
-
-            EDS[i] = maxCurr;
-            EDS[i].push_back(nums[i]);
+            ++maxSubsetSize;
+            dp[i] = maxSubsetSize;
         }
 
-        // Find the largest of EDS values
-        vector<int> ans;
-        for (int i = 0; i < n; ++i) if (ans.size() < EDS[i].size()) ans = EDS[i];
+        // Find both the size of largest divisible set and its index
+        int maxSize = 0, maxSizeIndex = 0;
+        for (int i = 0; i < n; ++i){
+            if (maxSize < dp[i]){
+                maxSize = dp[i];
+                maxSizeIndex = i;
+            }
+        }
 
+        // Reconstruct the largest divisible subset
+        // currSize: the size of the current subset
+        // currTail: the last element in the current subset
+        int currSize = maxSize, currTail = nums[maxSizeIndex];
+        vector<int> ans;
+        for (int i = maxSizeIndex; i >= 0; --i){
+            if (currSize == dp[i] && currTail % nums[i] == 0){
+                ans.push_back(nums[i]);
+                --currSize;
+                currTail = nums[i];
+            }
+        }
+
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
