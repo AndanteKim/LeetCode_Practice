6b9e184a34407bc2f308589643234d27c733d480
Andrew Kim <ksb0724@utexas.edu>
2025-04-07 01:04:10 +0900

    Time: 1 ms (24.08%), Space: 23.6 MB (19.24%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -11,22 +11,47 @@
  */
 class Solution {
 private:
-    pair<int, TreeNode*> dfs(TreeNode* root){
-        // Base case
-        if (!root) return {0, nullptr};
+    vector<pair<int, int>> bfs(TreeNode* root){
+        vector<vector<pair<int, int>>> treeSet;
+        int depth = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            int sz = q.size();
+            vector<pair<int, int>> interSet;
+            for (int i = 0; i < sz; ++i){
+                TreeNode* node = q.front(); q.pop();
 
-        pair<int, TreeNode*> left = dfs(root -> left);
-        pair<int, TreeNode*> right = dfs(root -> right);
-        if (left.first < right.first)
-            return {right.first + 1, right.second};
-        else if (left.first > right.first)
-            return {left.first + 1, left.second};
-        else
-            return {left.first + 1, root};
-    } 
+                interSet.push_back({depth, node -> val});
+                if (node -> left)
+                    q.push(node -> left);
+                
+                if (node -> right)
+                    q.push(node -> right);
+            }
+
+            treeSet.push_back(interSet);
+            ++depth;
+        }
+
+        return treeSet.back();
+    }
+    
+    TreeNode* lca(TreeNode* node, int a, int b){
+        if (!node || node -> val == a || node -> val == b) return node;
+
+        TreeNode* left = lca(node -> left, a, b);
+        TreeNode* right = lca(node -> right, a, b);
+        
+        if (!left) return right;
+        else if (!right) return left;
+        return node;
+    }
 
 public:
     TreeNode* lcaDeepestLeaves(TreeNode* root) {
-        return dfs(root).second;
+        vector<pair<int, int>> lastDeepest = bfs(root);
+        int a = lastDeepest[0].second, b = lastDeepest.back().second;
+        return lca(root, a, b);
     }
 };
