4b6d5eb7edf83f88c31b9c5d26d50c929bf77bcc
Andrew Kim <ksb0724@utexas.edu>
2025-04-08 00:41:20 +0900

    Time: 100 ms (70.15%), Space: 17 MB (69.09%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,31 +1,27 @@
 class Solution {
-private:
-    int n;
-    bool dp(int i, int subsetSum, vector<int>& nums, vector<vector<int>>& memo){
-        // Base cases: If subsetSum is splitted
-        if (subsetSum == 0) return true;
-
-        if (subsetSum < 0 || i == 0) return false;
-
-        // Check if subsetSum for given n is already computed and stored in memo
-        if (memo[i][subsetSum] != -1) return memo[i][subsetSum];
-
-        return memo[i][subsetSum] = dp(i - 1, subsetSum - nums[i], nums, memo) || dp(i - 1, subsetSum, nums, memo);
-    }
-
 public:
     bool canPartition(vector<int>& nums) {
-        this -> n = nums.size();
+        // Find sum of array elements
+        int total = accumulate(nums.begin(), nums.end(), 0);
 
-        // Find the sum of all array elements
-        int totalSum = accumulate(nums.begin(), nums.end(), 0);
+        // If total_sum is odd, it cannot be partitioned into equal sum subsets
+        if (total % 2 != 0) return false;
+        int n = nums.size(), subsetSum = total >> 1;
 
-        // If totalSum is odd, it cannot be partitioned into equal sum subset
-        if (totalSum % 2 != 0) return false;
-        
-        int subsetSum = totalSum >> 1;
-        vector memo(n + 1, vector<int>(subsetSum + 1, -1));
+        // construct a dp table of size (n + 1) x (subsetSum + 1)
+        vector dp(n + 1, vector<bool>(subsetSum + 1));
+        dp[0][0] = true;
+        for (int i = 1; i <= n; ++i){
+            int curr = nums[i - 1];
 
-        return dp(n - 1, subsetSum, nums, memo);
+            for (int j = 0; j <= subsetSum; ++j){
+                if (j < curr)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - curr];
+            }
+        }
+
+        return dp[n][subsetSum];
     }
 };
