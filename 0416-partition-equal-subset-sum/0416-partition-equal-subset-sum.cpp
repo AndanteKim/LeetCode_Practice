5baf8c8a2ba88ec08e6aed51aec5a9dc1ec2d8f6
Andrew Kim <ksb0724@utexas.edu>
2025-04-08 00:56:24 +0900

    Time: 74 ms (81.38%), Space: 13.4 MB (86.66%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,27 +1,21 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        // Find sum of array elements
+        // Find the sum of elements of array
         int total = accumulate(nums.begin(), nums.end(), 0);
 
-        // If total_sum is odd, it cannot be partitioned into equal sum subsets
+        // Base case: If totalSum is odd, it cannot be partitioned into equal sum subset
         if (total % 2 != 0) return false;
-        int n = nums.size(), subsetSum = total >> 1;
 
-        // construct a dp table of size (n + 1) x (subsetSum + 1)
-        vector dp(n + 1, vector<bool>(subsetSum + 1));
-        dp[0][0] = true;
-        for (int i = 1; i <= n; ++i){
-            int curr = nums[i - 1];
-
-            for (int j = 0; j <= subsetSum; ++j){
-                if (j < curr)
-                    dp[i][j] = dp[i - 1][j];
-                else
-                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - curr];
+        int subsetSum = total >> 1;
+        vector<bool> dp(subsetSum + 1, false);
+        dp[0] = true;
+        
+        for (int num : nums){
+            for (int j = subsetSum; j >= num; --j){
+                dp[j] = dp[j] || dp[j - num];
             }
         }
-
-        return dp[n][subsetSum];
+        return dp[subsetSum];
     }
 };
