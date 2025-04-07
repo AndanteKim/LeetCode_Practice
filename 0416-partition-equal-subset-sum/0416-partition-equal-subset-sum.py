71bdf9d366a3e712ed67d3727388dd1fe250aabc
Andrew Kim <ksb0724@utexas.edu>
2025-04-08 00:48:14 +0900

    Time: 483 ms (74.33%), Space: 17.9 MB (84.54%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,27 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        def dp(i: int, subset_sum: int) -> bool:
-            # Base cases
-            if subset_sum == 0:
-                return True
-            
-            if i == 0 or subset_sum < 0:
-                return False
-
-            if memo[i][subset_sum] != -1:
-                return True if memo[i][subset_sum] else False
-            
-            memo[i][subset_sum] = dp(i - 1, subset_sum - nums[i - 1]) or dp(i - 1, subset_sum)
-            return memo[i][subset_sum]
-
         # Find sum of array elements
         total = sum(nums)
 
-        # If total sum is odd, it cannot be partitioned into equal sum subsets
+        # If total_sum is odd, it cannot be partitioned into equal sum subsets
         if total % 2 != 0:
             return False
         
+        # Construct a dp talbe of size (subset_sum + 1)
         n, subset_sum = len(nums), total >> 1
-        memo = [[-1] * (subset_sum + 1) for _ in range(n + 1)]
+        dp = [False] * (subset_sum + 1)
+        dp[0] = True
 
-        return dp(n - 1, subset_sum)
+        for num in nums:
+            for j in range(subset_sum, num - 1, -1):
+                dp[j] = dp[j] or dp[j - num]
+
+        return dp[subset_sum]
