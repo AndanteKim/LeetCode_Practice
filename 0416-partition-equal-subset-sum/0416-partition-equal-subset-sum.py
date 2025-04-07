fbbeabda835601bcd3ec2e8320d60e762a9f0315
Andrew Kim <ksb0724@utexas.edu>
2025-04-08 00:19:37 +0900

    Time: 183 ms (95.13%), Space: 54.5 MB (24.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,6 +1,5 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        @lru_cache(maxsize = None)
         def dp(i: int, subset_sum: int) -> bool:
             # Base cases
             if subset_sum == 0:
@@ -8,9 +7,12 @@ class Solution:
             
             if i == 0 or subset_sum < 0:
                 return False
+
+            if memo[i][subset_sum] != -1:
+                return True if memo[i][subset_sum] else False
             
-            ans = dp(i - 1, subset_sum - nums[i - 1]) or dp(i - 1, subset_sum)
-            return ans
+            memo[i][subset_sum] = dp(i - 1, subset_sum - nums[i - 1]) or dp(i - 1, subset_sum)
+            return memo[i][subset_sum]
 
         # Find sum of array elements
         total = sum(nums)
@@ -20,5 +22,6 @@ class Solution:
             return False
         
         n, subset_sum = len(nums), total >> 1
+        memo = [[-1] * (subset_sum + 1) for _ in range(n + 1)]
 
         return dp(n - 1, subset_sum)
