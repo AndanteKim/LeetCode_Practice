4eec44a54867059575725f67c455807bda9f2464
Andrew Kim <ksb0724@utexas.edu>
2025-04-08 21:35:52 +0900

    Time: 0 ms (100%), Space: 26.9 MB (99.66%) - LeetHub

--- 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3396-minimum-number-of-operations-to-make-elements-in-array-distinct/3396-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,26 +1,13 @@
 class Solution {
-private:
-    int n;
-    bool checkUnique(int start, vector<int>& nums){
-        unordered_set<int> seen;
-
-        for (int i = start; i < n; ++i){
-            if (seen.count(nums[i])) return false;
-            seen.insert(nums[i]);
-        }
-        return true;
-    }
-
 public:
     int minimumOperations(vector<int>& nums) {
-        this -> n = nums.size();
-        int ans = 0;
+        int n = nums.size();
+        vector<bool> seen(128, false);
 
-        for (int i = 0; i < n; i += 3){
-            if (checkUnique(i, nums)) return ans;
-            ++ans;
+        for (int i = n - 1; i >= 0; --i){
+            if (seen[nums[i]]) return i / 3 + 1;
+            seen[nums[i]] = true;
         }
-
-        return ans;
+        return 0;
     }
 };
