cb7de0fad6e28729395ea80190108ee9eababbdd
Andrew Kim <ksb0724@utexas.edu>
2025-04-09 20:08:21 +0900

    Time: 4 ms (95.6%), Space: 32.4 MB (42.14%) - LeetHub

--- 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
+++ 3375-minimum-operations-to-make-array-values-equal-to-k/3375-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        int ans = 0;
-        unordered_set<int> seen;
+        unordered_set<int> st;
 
         for (int num : nums){
-            if (seen.count(num)) continue;
-
-            if (num < k) return -1;
-            else if (num > k) ++ans;
-            seen.insert(num);
+            if (num < k)
+                return -1;
+            else if (num > k)
+                st.insert(num);
         }
 
-        return ans;
+        return st.size();
     }
 };
