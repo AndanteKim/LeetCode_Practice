ee18d8f54291875695ca6e73acbc4ceb7a1b0a24
Andrew Kim <ksb0724@utexas.edu>
2025-04-12 03:48:10 +0900

    Time: 6 ms (72.12%), Space: 17.8 MB (87.5%) - LeetHub

--- 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.py
+++ 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.py
@@ -1,27 +1,25 @@
 class Solution:
     def numberOfPowerfulInt(self, start: int, finish: int, limit: int, s: str) -> int:
-        @lru_cache(maxsize = None)
-        def dfs(i: int, limit_low: bool, limit_high: bool) -> int:
-            # Recursive boundary
-            if i == n:
-                return 1
-            lo = int(low[i]) if limit_low else 0
-            hi = int(high[i]) if limit_high else 9
+        def calculate(x: str) -> int:
+            if len(x) < len(s):
+                return 0
 
-            ans = 0
+            if len(x) == len(s):
+                return 1 if x >= s else 0
+            
+            count, suffix = 0, x[len(x) - len(s) :]
+            pre_len = len(x) - len(s)
 
-            if i < pre_len:
-                for digit in range(lo, min(hi, limit) + 1):
-                    ans += dfs(i + 1, limit_low and digit == lo, limit_high and digit == hi)
-            else:
-                x = int(s[i - pre_len])
-                if lo <= x <= min(hi, limit):
-                    ans = dfs(i + 1, limit_low and x == lo, limit_high and x == hi)
+            for i in range(pre_len):
+                if limit < int(x[i]):
+                    count += (limit + 1) ** (pre_len - i)
+                    return count
+                count += int(x[i]) * (limit + 1) ** (pre_len - 1 - i)
 
-            return ans
+            if suffix >= s:
+                count += 1
+            return count
 
-        low, high = str(start), str(finish)
-        n = len(high)
-        low = low.zfill(n)  # align digits
-        pre_len = n - len(s)    # prefix length
-        return dfs(0, True, True)
+        start_ = str(start - 1)
+        finish_ = str(finish)
+        return calculate(finish_) - calculate(start_)
