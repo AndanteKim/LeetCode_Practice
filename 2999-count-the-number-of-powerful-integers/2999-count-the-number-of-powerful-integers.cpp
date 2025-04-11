cb4509b9efe37d79320be6a3242bef8f2792c7db
Andrew Kim <ksb0724@utexas.edu>
2025-04-12 04:11:26 +0900

    Time: 1 ms (59.68%), Space: 10.2 MB (48.92%) - LeetHub

--- 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
+++ 2999-count-the-number-of-powerful-integers/2999-count-the-number-of-powerful-integers.cpp
@@ -1,39 +1,33 @@
 class Solution {
+private:
+    int limit;
+    long long calculate(string& x, string& s){
+        if (x.size() < s.size()) return 0;
+
+        if (x.size() == s.size()) return (x >= s)? 1 : 0;
+
+        long long count = 0;
+        int preLen = x.size() - s.size();
+        string suffix = x.substr(x.size() - s.size());
+
+        for (int i = 0; i < preLen; ++i){
+            if (limit < (x[i] - '0')){
+                count += pow(limit + 1, preLen - i);
+                return count;
+            }
+            count += (x[i] - '0') * (long)pow(limit + 1, preLen - i - 1);
+        }
+
+        if (suffix >= s) ++count;
+
+        return count;
+    }
+
+
 public:
     long long numberOfPowerfulInt(long long start, long long finish, int limit, string s) {
-        string low = to_string(start), high = to_string(finish);
-        int n = high.size();
-        low = string(n - low.size(), '0') + low;    // Align digits
-        int preLen = n - s.size();                  // prefix length
-
-        vector<long long> memo(n, -1);
-        function<long long(int, bool, bool)> dfs = 
-            [&](int i, bool limitLow, bool limitHigh) -> long long{
-                // Recursive boundary
-                if (i == n) return 1;
-
-                if (!limitLow && !limitHigh && memo[i] != -1) return memo[i];
-
-                int lo = limitLow? low[i] - '0' : 0;
-                int hi = limitHigh? high[i] - '0' : 9;
-
-                long long ans = 0;
-                if (i < preLen){
-                    for (int digit = lo; digit <= min(hi, limit); ++digit){
-                        ans += dfs(i + 1, limitLow && digit == lo, limitHigh && digit == hi);
-                    }
-                }
-                else{
-                    int x = s[i - preLen] - '0';
-                    if (lo <= x && x <= min(hi, limit))
-                    ans = dfs(i + 1, limitLow && x == lo, limitHigh && x == hi);
-                }
-
-                if (!limitLow && !limitHigh) memo[i] = ans;
-
-                return ans;
-        };
-
-        return dfs(0, true, true);
+        string start_ = to_string(start - 1), finish_ = to_string(finish);
+        this -> limit = limit;
+        return calculate(finish_, s) - calculate(start_, s);
     }
 };
