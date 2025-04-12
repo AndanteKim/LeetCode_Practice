49efb7aa289cae6f782efa4f0c695c9e57e0ec79
Andrew Kim <ksb0724@utexas.edu>
2025-04-12 22:11:59 +0900

    Time: 878 ms (88.71%), Space: 19.1 MB (61.29%) - LeetHub

---
+++ 3272-find-the-count-of-good-integers/3272-find-the-count-of-good-integers.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countGoodIntegers(self, n: int, k: int) -> int:
+        dictionary = set()
+        base = 10 ** ((n - 1) >> 1)
+        skip = n & 1
+
+        # Enumerate the number of palindrome numbers of n digits
+        for i in range(base, base * 10):
+            s = str(i)
+            s += s[::-1][skip:]
+            palindrome = int(s)
+
+            # If the current palindrome number is a k-palindromic integer
+            if palindrome % k == 0:
+                sorted_s = "".join(sorted(s))
+                dictionary.add(sorted_s)
+
+        fac, ans = [factorial(i) for i in range(n + 1)], 0
+
+        for s in dictionary:
+            cnt = [0] * 10
+            for c in s:
+                cnt[int(c)] += 1
+            
+            # Calculate permutations and combinations
+            tot = (n - cnt[0]) * fac[n - 1]
+            for x in cnt:
+                tot //= fac[x]
+            ans += tot
+
+        return ans
+            
