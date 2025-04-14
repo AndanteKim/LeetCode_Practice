5ce9e68f777380576bd4bb1c0913513b21a54eb6
Andrew Kim <ksb0724@utexas.edu>
2025-04-14 22:21:04 +0900

    Time: 0 ms (100%), Space: 11.6 MB (5.91%) - LeetHub

--- 1534-count-good-triplets/1534-count-good-triplets.cpp
+++ 1534-count-good-triplets/1534-count-good-triplets.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
     int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
-        int ans = 0, n = arr.size();
+        int n = arr.size(), ans = 0;
+        vector<int> total(1001);
 
-        for (int i = 0; i < n - 2; ++i){
-            for (int j = i + 1; j < n - 1; ++j){
-                for (int k = j + 1; k < n; ++k){
-                    if (abs(arr[i] - arr[j]) <= a && abs(arr[j] - arr[k]) <= b && abs(arr[k] - arr[i]) <= c) ++ans;
+        for (int j = 0; j < n; ++j){
+            for (int k = j + 1; k < n; ++k){
+                if (abs(arr[j] - arr[k]) <= b){
+                    int lj = arr[j] - a, rj = arr[j] + a;
+                    int lk = arr[k] - c, rk = arr[k] + c;
+                    int l = max(0, max(lj, lk)), r = min(1000, min(rj, rk));
+                    if (l <= r)
+                        ans += (l == 0)? total[r] : total[r] - total[l - 1]; 
                 }
             }
+
+            for (int k = arr[j]; k <= 1000; ++k)
+                ++total[k];
         }
 
         return ans;
