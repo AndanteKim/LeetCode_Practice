74171c4dfc59ef7e4ae752355e28f514e9f7711a
Andrew Kim <ksb0724@utexas.edu>
2025-04-20 03:21:19 +0900

    Time: 57 ms (52.8%), Space: 60.5 MB (7.2%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.cpp
@@ -1,27 +1,16 @@
 class Solution {
-private:
-    // Calculate the number of pairs with sum less than 'value'.
-    long long boundSearch(vector<int>& nums, int target){
-        int left = 0, right = nums.size() - 1;
-        long long ans = 0;
-        
-        while (left < right){
-            int sum = nums[left] + nums[right];
-            // If sum is less than value, add the size of window to result and move to the next index.
-            if (sum < target){
-                ans += right - left++;
-            }
-            else
-                // Otherwise, shift the right pointer backwards, until we get a valid window.
-                --right;
-        }
-        
-        return ans;
-    }
-    
 public:
     long long countFairPairs(vector<int>& nums, int lower, int upper) {
+        int n = nums.size();
         sort(nums.begin(), nums.end());
-        return boundSearch(nums, upper + 1) - boundSearch(nums, lower);
+        long long ans = 0;
+
+        for (int i = 0; i < n; ++i){
+            int left = lower_bound(nums.begin() + i + 1, nums.end(), lower - nums[i]) - nums.begin();
+            int right = upper_bound(nums.begin() + i + 1, nums.end(), upper - nums[i]) - nums.begin();
+            ans += right - left;
+        }
+
+        return ans;
     }
 };
