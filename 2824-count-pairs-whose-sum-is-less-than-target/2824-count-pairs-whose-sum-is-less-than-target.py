2beae0f1b723393c7351b934878e1b5cae39ae86
Andrew Kim <ksb0724@utexas.edu>
2025-04-20 02:55:19 +0900

    Time: 3 ms (69.6%), Space: 17.9 MB (15.11%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.py
@@ -1,10 +1,21 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
+        def binary_search(num: int, left: int, right: int) -> int:
+            while left <= right:
+                mid = left + ((right - left) >> 1)
+                if nums[mid] < target - num:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+            
+            return right
+        
+        nums.sort()
         ans, n = 0, len(nums)
 
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] < target:
-                    ans += 1
+        for left in range(n - 1):
+            right = binary_search(nums[left], left + 1, n - 1)
+            
+            ans += right - left
 
         return ans
