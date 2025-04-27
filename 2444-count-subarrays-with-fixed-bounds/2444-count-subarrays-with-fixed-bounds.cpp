d3ab34146bdca17d572dfd4b01ff1512f8b4a225
Andrew Kim <ksb0724@utexas.edu>
2025-04-27 10:55:15 +0900

    Time: 0 ms (100%), Space: 93.7 MB (94.46%) - LeetHub

--- 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
+++ 2444-count-subarrays-with-fixed-bounds/2444-count-subarrays-with-fixed-bounds.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int minK, int maxK) {
-        long long answer = 0;
-        int min_pos = -1, max_pos = -1, left_bound = -1;
-        
-        for (int i = 0; i < nums.size(); ++i){
-            if (nums[i] < minK || nums[i] > maxK) left_bound = i;
-            
-            if (nums[i] == minK) min_pos = i;
-            if (nums[i] == maxK) max_pos = i;
-            
-            answer += max(0, min(min_pos, max_pos) - left_bound); 
+        // minPos, maxPos: the most recent positions of minK and maxK
+        // left_bound: the most recent value outside the range [minK, maxK].
+        long long ans = 0;
+        int minPos = -1, maxPos = -1, leftBound = -1;
+
+        // Iterate over nums, for each number at index i:
+        for (int i = 0; i < (int)nums.size(); ++i){
+            // If the number is outside the range [minK, maxK], update the most recent leftBound.
+            if (nums[i] < minK || nums[i] > maxK) leftBound = i;
+
+            // If the number is minK or maxK, update the most recent position.
+            if (nums[i] == minK) minPos = i;
+
+            if (nums[i] == maxK) maxPos = i;
+
+            // The number of valid subarrays equals the number of elements between leftBound and
+            // the smaller of the two most recent positions.
+            ans += max(0, min(minPos, maxPos) - leftBound);
         }
-        
-        return answer;
+
+        return ans;
     }
 };
