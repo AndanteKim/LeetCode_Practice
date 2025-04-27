088cd5e678c44ed5019ffb6e5639f4ebadd71ade
Andrew Kim <ksb0724@utexas.edu>
2025-04-28 02:10:50 +0900

    Time: 0 ms (100%), Space: 19.7 MB (38.76%) - LeetHub

--- 0302-smallest-rectangle-enclosing-black-pixels/0302-smallest-rectangle-enclosing-black-pixels.py
+++ 0302-smallest-rectangle-enclosing-black-pixels/0302-smallest-rectangle-enclosing-black-pixels.py
@@ -1,23 +1,35 @@
 class Solution:
     def minArea(self, image: List[List[str]], x: int, y: int) -> int:
-        def dfs(r: int, c: int) -> None:
-            if r < 0 or r >= len(image) or c < 0 or c >= len(image[0]) or image[r][c] == '0': return
-        
-            image[r][c] = '0'
-            self.top = min(self.top, r)
-            self.bot = max(self.bot, r + 1)
-            self.left = min(self.left, c)
-            self.right = max(self.right, c + 1)
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c - 1)
-            dfs(r, c + 1)
-        
-        # Base case
-        if len(image) == 0 or len(image[0]) == 0:
-            return 0
+        def _search_col(i: int, j: int, top: int, bot: int, white2black: bool) -> int:
+            while i != j:
+                k, mid = top, (i + j) >> 1
 
-        self.top = self.bot = x
-        self.left = self.right = y
-        dfs(x, y)
-        return (self.right - self.left) * (self.bot - self.top)
+                while k < bot and image[k][mid] == '0':
+                    k += 1
+                # k < bot means the column mid has black pixel
+                if (k < bot) == white2black:
+                    j = mid # Search the boundary in the smaller half
+                else:
+                    i = mid + 1
+            return i
+        
+        def _search_row(i: int, j: int, left: int, right: int, white2black: bool) -> int:
+            while i != j:
+                k, mid = left, (i + j) >> 1
+
+                while k < right and image[mid][k] == '0':
+                    k += 1
+                
+                # k < right means the row mid has black pixel
+                if (k < right) == white2black:
+                    j = mid
+                else:
+                    i = mid + 1
+            return i
+
+        m, n = len(image), len(image[0])
+        left = _search_col(0, y, 0, m, True)
+        right = _search_col(y + 1, n, 0, m, False)
+        top = _search_row(0, x, left, right, True)
+        bot = _search_row(x + 1, m, left, right, False)
+        return (right - left) * (bot - top)
