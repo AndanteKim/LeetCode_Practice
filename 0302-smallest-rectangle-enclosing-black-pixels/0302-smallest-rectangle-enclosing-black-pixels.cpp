75df1e7ea94145b789bfd297ba1701b4eb127750
Andrew Kim <ksb0724@utexas.edu>
2025-04-28 01:03:56 +0900

    Time: 4 ms (33.92%), Space: 20.4 MB (55.24%) - LeetHub

--- 0302-smallest-rectangle-enclosing-black-pixels/0302-smallest-rectangle-enclosing-black-pixels.cpp
+++ 0302-smallest-rectangle-enclosing-black-pixels/0302-smallest-rectangle-enclosing-black-pixels.cpp
@@ -1,20 +1,34 @@
 class Solution {
+private:
+    int top, bot;
+    int left, right;
+
+    void dfs(vector<vector<char>>& image, int r, int c){
+        // Base case
+        if (r < 0 || r >= image.size() || c < 0 || c >= image[0].size() || image[r][c] == '0') return;
+
+        image[r][c] = '0';
+        top = min(top, r);
+        bot = max(bot, r + 1);
+        left = min(left, c);
+        right = max(right, c + 1);
+
+        dfs(image, r - 1 , c);
+        dfs(image, r + 1, c);
+        dfs(image, r, c - 1);
+        dfs(image, r, c + 1);
+    }
+
+
 public:
     int minArea(vector<vector<char>>& image, int x, int y) {
-        int ans = 0;
-        int left = y, right = y;
-        int top = x, bot = x;
-        for (int r = 0; r < image.size(); ++r){
-            for (int c = 0; c < image[0].size(); ++c){
-                if (image[r][c] == '1'){
-                    top = min(top, r);
-                    bot = max(bot, r + 1);
-                    left = min(left, c);
-                    right = max(right, c + 1);
-                }
-            }
-        }
+        // Base case
+        if (image.size() == 0 || image[0].size() == 0) return 0;
+        
+        this -> top = x; this -> bot = x;
+        this -> left = y; this -> right = y;
 
-        return (right - left) * (bot - top);
+        dfs(image, x, y);
+        return (bot - top) * (right - left);    
     }
 };
