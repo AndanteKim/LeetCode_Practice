091c910a58d5087e8a59371b520c151b009b297a
Andrew Kim <ksb0724@utexas.edu>
2025-04-28 02:33:26 +0900

    Time: 0 ms (100%), Space: 20.1 MB (99.65%) - LeetHub

--- 0302-smallest-rectangle-enclosing-black-pixels/0302-smallest-rectangle-enclosing-black-pixels.cpp
+++ 0302-smallest-rectangle-enclosing-black-pixels/0302-smallest-rectangle-enclosing-black-pixels.cpp
@@ -1,34 +1,42 @@
 class Solution {
 private:
-    int top, bot;
-    int left, right;
+    int searchColumns(vector<vector<char>>& image, int i, int j, int top, int bottom, bool white2black){
+        while (i != j){
+            int k = top, mid = (i + j) >> 1;
 
-    void dfs(vector<vector<char>>& image, int r, int c){
-        // Base case
-        if (r < 0 || r >= image.size() || c < 0 || c >= image[0].size() || image[r][c] == '0') return;
+            while (k < bottom && image[k][mid] == '0') ++k;
 
-        image[r][c] = '0';
-        top = min(top, r);
-        bot = max(bot, r + 1);
-        left = min(left, c);
-        right = max(right, c + 1);
+            if (k < bottom == white2black)  // k < bottom means the column mid has black pixel
+                j = mid;    // search the boundary in the smaller half
+            else
+                i = mid + 1;    // search the boundary in the greater half
+        }
 
-        dfs(image, r - 1 , c);
-        dfs(image, r + 1, c);
-        dfs(image, r, c - 1);
-        dfs(image, r, c + 1);
+        return i;
     }
 
+    int searchRows(vector<vector<char>>& image, int i, int j, int left, int right, bool white2black){
+        while (i != j){
+            int k = left, mid = (i + j) >> 1;
+
+            while (k < right && image[mid][k] == '0') ++k;
+
+            if (k < right == white2black) j = mid;  // k < right means the row mid has black pixel
+            else
+                i = mid + 1;
+        }
+
+        return i;
+    }
 
 public:
     int minArea(vector<vector<char>>& image, int x, int y) {
-        // Base case
-        if (image.size() == 0 || image[0].size() == 0) return 0;
+        int m = image.size(), n = image[0].size();
+        int left = searchColumns(image, 0, y, 0, m, true);
+        int right = searchColumns(image, y + 1, n, 0, m, false);
+        int top = searchRows(image, 0, x, left, right, true);
+        int bottom = searchRows(image, x + 1, m, left, right, false);
         
-        this -> top = x; this -> bot = x;
-        this -> left = y; this -> right = y;
-
-        dfs(image, x, y);
-        return (bot - top) * (right - left);    
+        return (right - left) * (bottom - top);
     }
 };
