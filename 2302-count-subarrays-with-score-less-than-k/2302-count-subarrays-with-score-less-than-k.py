deb83cb06a84c8536e8d9b00421e487359f05430
Andrew Kim <ksb0724@utexas.edu>
2025-04-29 00:28:04 +0900

    Time: 1770 ms (5.01%), Space: 30.7 MB (31.97%) - LeetHub

--- 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.py
+++ 2302-count-subarrays-with-score-less-than-k/2302-count-subarrays-with-score-less-than-k.py
@@ -1,15 +1,28 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        ans, n = 0, len(nums)
-        curr, left = 0, 0
-        for right in range(n):
-            curr += nums[right]
-            
-            while left <= right and curr * (right - left + 1) >= k:
-                curr -= nums[left]
-                left += 1
+        n = len(nums)
+        prefix = [0] * (n + 1)
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
 
-            length = right - left + 1
-            ans += length
+        ans = 0
+        for j in range(n):
+            lo, hi, i = 0, j, j
+            while lo <= hi:
+                mid = lo + ((hi - lo) >> 1)
+                length = j - mid + 1
+                total = prefix[j + 1] - prefix[mid]
+                score = total * length
+                if score < k:
+                    i = mid
+                    hi = mid - 1
+                else:
+                    lo = mid + 1
+            
+            # If length 1 and score is greater than equal to k, skip it
+            if i == j and nums[j] >= k:
+                continue
+            # else this will be valid because of binary search
+            ans += j - i + 1
 
         return ans
