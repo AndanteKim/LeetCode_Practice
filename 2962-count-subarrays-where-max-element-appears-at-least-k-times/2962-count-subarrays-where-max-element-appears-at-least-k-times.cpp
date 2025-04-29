2ebc8a90b000fb2449173acd9a0f2eace267661c
Andrew Kim <ksb0724@utexas.edu>
2025-04-30 07:38:53 +0900

    Time: 4 ms (70.14%), Space: 121.8 MB (48.45%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     long long countSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        int maxElementInWindow = 0, maxElement = *max_element(nums.begin(), nums.end()), start = 0;
         long long ans = 0;
-        int freq, maxElement = *max_element(nums.begin(), nums.end()), n = nums.size();
-        vector<int> indicesOfMaxElements;
         
-        for (int i = 0; i < n; ++i){
-            if (nums[i] == maxElement)
-                indicesOfMaxElements.push_back(i);
-            
-            freq = indicesOfMaxElements.size();
-            if (freq >= k){
-                ans += indicesOfMaxElements[freq - k] + 1;
+        for (int end = 0; end < n; ++end){
+            if (nums[end] == maxElement) ++maxElementInWindow;
+
+            while (maxElementInWindow == k){
+                if (nums[start] == maxElement) --maxElementInWindow;
+                ++start;
             }
+
+            ans += start;
         }
-        
+
         return ans;
     }
 };
