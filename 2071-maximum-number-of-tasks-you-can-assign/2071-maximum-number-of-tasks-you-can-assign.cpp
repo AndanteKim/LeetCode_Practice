aeedc536023a2c8a50ebcdf8a5276e289986df7c
Andrew Kim <ksb0724@utexas.edu>
2025-05-01 18:17:34 +0900

    Time: 834 ms (32.95%), Space: 286 MB (77.91%) - LeetHub

---
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.cpp
@@ -0,0 +1,46 @@
+class Solution {
+private:
+
+public:
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        int m = workers.size(), n = tasks.size();
+        sort(tasks.begin(), tasks.end());
+        sort(workers.begin(), workers.end());
+
+        auto check = [&](int mid) -> bool{
+            int p = pills;
+            // Ordered set of workers
+            multiset<int> ws;
+            for (int i = m - mid; i < m; ++i) ws.insert(workers[i]);
+
+            // Enumerate each task from largest to smallest
+            for (int i = mid - 1; i >= 0; --i){
+                // If the largest element in the ordered set is greater than or
+                // equal to tasks[i]
+                if (auto it = prev(ws.end()); *it >= tasks[i]) ws.erase(it);
+                else{
+                    if (!p) return false;
+                    auto rep = ws.lower_bound(tasks[i] - strength);
+                    if (rep == ws.end()) return false;
+                    --p;
+                    ws.erase(rep);
+                }
+            }
+
+            return true;
+        };
+
+        int left = 1, right = min(m, n), ans = 0; 
+        while (left <= right){
+            int mid = left + ((right - left) >> 1);
+            if (check(mid)){
+                ans = mid;
+                left = mid + 1;
+            }
+            else
+                right = mid - 1;
+        }
+
+        return ans;
+    }
+};
