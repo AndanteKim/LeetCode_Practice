82af2c4baafdf4aefae924df52baa77c337cb7c5
Andrew Kim <ksb0724@utexas.edu>
2025-05-01 19:56:03 +0900

    Time: 495 ms (28.53%), Space: 26.1 MB (70.22%) - LeetHub

--- 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.py
+++ 2071-maximum-number-of-tasks-you-can-assign/2071-maximum-number-of-tasks-you-can-assign.py
@@ -1,39 +1,40 @@
-from sortedcontainers import SortedList
-
 class Solution:
     def maxTaskAssign(self, tasks: List[int], workers: List[int], pills: int, strength: int) -> int:
+        n, m = len(tasks), len(workers)
+        tasks.sort()
+        workers.sort()
+
         def check(mid: int) -> bool:
-            p = pills
-            # Ordered set of workers
-            ws = SortedList(workers[m - mid : ])
+            p, ptr = pills, m - 1
+            ws = deque()
+            
             # Enumerate each task from largest to smallest
             for i in range(mid - 1, -1, -1):
-                # If the largest element in the ordered set is greater than or equal to tasks[i]
-                if ws[-1] >= tasks[i]:
+                while ptr >= m - mid and workers[ptr] + strength >= tasks[i]:
+                    ws.appendleft(workers[ptr])
+                    ptr -= 1
+                if not ws:
+                    return False
+
+                # If the largest element in the deque is greater than or equal to tasks[i]
+                elif ws[-1] >= tasks[i]:
                     ws.pop()
                 else:
                     if p == 0:
                         return False
-                    rep = ws.bisect_left(tasks[i] - strength)
-                    if rep == len(ws):
-                        return False
                     p -= 1
-                    ws.pop(rep)
+                    ws.popleft()
             
             return True
-        
-        workers.sort()
-        tasks.sort()
 
-        ans, n, m = 0, len(tasks), len(workers)
-        left, right = 1, min(m, n)
-
+        left, right, ans = 1, min(m, n), 0
         while left <= right:
-            mid = left + ((right - left) >> 1)
+            mid = (left + right) >> 1
             if check(mid):
-                ans = mid
-                left = mid + 1
+                ans, left = mid, mid + 1
             else:
                 right = mid - 1
-
+        
         return ans
+        
+            
