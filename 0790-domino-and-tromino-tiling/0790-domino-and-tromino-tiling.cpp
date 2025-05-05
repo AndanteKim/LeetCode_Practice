1c9bd3bcff35829c9d2d67b98a01f5cf923f8b36
Andrew Kim <ksb0724@utexas.edu>
2025-05-05 20:38:11 +0900

    Time: 3 ms (17.99%), Space: 9.5 MB (16.61%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,16 +1,27 @@
+typedef long long ll;
+
 class Solution {
+private:
+    int mod = 1'000'000'007;
+    ll p(int n, vector<ll>& fCache, vector<ll>& pCache){
+        if (n == 2) return 1;
+
+        if (pCache[n] != -1) return pCache[n];
+
+        return pCache[n] = (p(n - 1, fCache, pCache) + f(n - 2, fCache, pCache)) % mod;
+    }
+
+    ll f(int n, vector<ll>& fCache, vector<ll>& pCache){
+        if (n <= 2) return n;
+
+        if (fCache[n] != -1) return fCache[n];
+
+        return fCache[n] = (f(n - 1, fCache, pCache) + f(n - 2, fCache, pCache) + 2 * p(n - 1, fCache, pCache)) % mod;
+    }
+
 public:
     int numTilings(int n) {
-        int mod = 1'000'000'007;
-        if (n <= 2) return n;
-        long fPrev = 1L, fCurr = 2L, pCurr = 1L;
-        for (int k = 3; k <= n; ++k){
-            long tmp = fCurr;
-            fCurr = (fPrev + fCurr + 2 * pCurr) % mod;
-            pCurr = (pCurr + fPrev) % mod;
-            fPrev = tmp;
-        }
-        
-        return fCurr;
+        vector<ll> fCache(n + 1, -1), pCache(n + 1, -1);
+        return static_cast<int>(f(n, fCache, pCache));
     }
 };
