8d90f0b1b3d724a28b81514a1e38bd865a82095f
Andrew Kim <ksb0724@utexas.edu>
2025-05-05 20:14:46 +0900

    Time: 8 ms (15.28%), Space: 19.3 MB (13.74%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,15 +1,17 @@
 class Solution:
     def numTilings(self, n: int) -> int:
-        MOD = 1_000_000_007
-        if n <= 2:
-            return n
-        f_prev, f_curr, p_curr = 1, 2, 1
+        @lru_cache(maxsize = None)
+        def p(n):
+            if n == 2:
+                return 1
+            
+            return (p(n - 1) + f(n - 2)) % mod
+
+        @lru_cache(maxsize = None)
+        def f(n):
+            if n <= 2:
+                return n
+            return (f(n - 1) + f(n - 2) + 2 * p(n - 1)) % mod
         
-        for k in range(3, n + 1):
-            tmp = f_curr
-            f_curr = (f_prev + f_curr + 2 * p_curr) % MOD
-            p_curr = (p_curr + f_prev) % MOD
-            f_prev = tmp
-        
-        return f_curr
-        
+        mod = 1_000_000_007
+        return f(n)
