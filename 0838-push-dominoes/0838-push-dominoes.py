1e3eeb64dd17ec4b24cc7658ce014cb3f258d5f5
Andrew Kim <ksb0724@utexas.edu>
2025-05-06 03:07:28 +0900

    Time: 265 ms (26.95%), Space: 22.7 MB (30.98%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.py
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -1,16 +1,28 @@
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
-        symbols = [(i, x) for i, x in enumerate(dominoes) if x != '.']
-        symbols = [(-1, 'L')] + symbols + [(len(dominoes), 'R')]
+        n = len(dominoes)
+        force = [0] * n
 
-        ans = list(dominoes)
+        # Populate forces going from left to right
+        f = 0
+        for i in range(n):
+            if dominoes[i] == 'R':
+                f = n
+            elif dominoes[i] == 'L':
+                f = 0
+            else:
+                f = max(f - 1, 0)
+            force[i] += f
 
-        for (i, x), (j, y) in zip(symbols, symbols[1:]):
-            if x == y:
-                for k in range(i + 1, j):
-                    ans[k] = x
-            elif x > y: #RL
-                for k in range(i + 1, j):
-                    ans[k] = ".LR"[((k - i) > (j - k)) - ((k - i) < (j - k))]
-
-        return "".join(ans)
+        # Populate forces going from right to left
+        f = 0
+        for i in range(n - 1, -1, -1):
+            if dominoes[i] == 'L':
+                f = n
+            elif dominoes[i] == 'R':
+                f = 0
+            else:
+                f = max(f - 1, 0)
+            force[i] -= f
+        
+        return "".join('.' if f == 0 else 'R' if f > 0 else 'L' for f in force)
