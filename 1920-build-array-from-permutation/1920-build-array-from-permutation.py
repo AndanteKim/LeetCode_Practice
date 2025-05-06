b0b81ee2a26bfd754eb7568a4cd9a252c6915d45
Andrew Kim <ksb0724@utexas.edu>
2025-05-06 19:55:32 +0900

    Time: 12 ms (5.86%), Space: 18 MB (34.1%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,4 +1,13 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        return [nums[nums[_]] for _ in range(n)]
+
+        # Build the final value on the first iteration
+        for i in range(n):
+            nums[i] += 1000 * (nums[nums[i]] % 1000)
+
+        # Modified to final value on the second iteration
+        for i in range(n):
+            nums[i] //= 1000
+        
+        return nums
