cbefaa10f193aab62f1766bd6de054e81fd0ff98
Andrew Kim <ksb0724@utexas.edu>
2025-05-06 20:06:22 +0900

    Time: 3 ms (12.49%), Space: 20.4 MB (58.11%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans = nums, temp = nums;
         int n = nums.size();
-        for (int i = 0; i < n; ++i){
-            temp[i] = nums[i];
-            ans[i] = nums[temp[i]];
-        }
 
-        return ans;
+        for (int i = 0; i < n; ++i)
+            nums[i] += 1000 * (nums[nums[i]] % 1000);
+        
+        for (int i = 0; i < n; ++i) nums[i] /= 1000;
+        return nums;
     }
 };
