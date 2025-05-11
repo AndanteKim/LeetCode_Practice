57366367bd1f7edaa30f561c91d7a12e15486924
Andrew Kim <ksb0724@utexas.edu>
2025-05-11 21:31:06 +0900

    Time: 2800 ms (25.54%), Space: 93 MB (64.34%) - LeetHub

--- 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.py
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.py
@@ -1,41 +1,36 @@
 class State:
-    def __init__(self, x, y, dis):
-        self.x = x
-        self.y = y
-        self.dis = dis
+    def __init__(self, x: int, y: int, dis: int):
+        self.x, self.y, self.dis = x, y, dis
 
-    def __lt__(self, other):
+    def __lt__(self, other: 'State'):
         return self.dis < other.dis
 
-
 class Solution:
-    def minTimeToReach(self, moveTime):
-        n = len(moveTime)
-        m = len(moveTime[0])
-        inf = float("inf")
-        d = [[inf] * m for _ in range(n)]
+    def minTimeToReach(self, moveTime: List[List[int]]) -> int:
+        n, m = len(moveTime), len(moveTime[0])
+        d = [[float('inf')] * m for _ in range(n)]
         v = [[0] * m for _ in range(n)]
-
-        dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        d[0][0] = 0
-        q = []
-        heapq.heappush(q, State(0, 0, 0))
+        d[0][0], q = 0, []
+        heappush(q, State(0, 0, 0))
 
         while q:
-            s = heapq.heappop(q)
+            s = heappop(q)
             if v[s.x][s.y]:
                 continue
+            
             if s.x == n - 1 and s.y == m - 1:
                 break
+            
             v[s.x][s.y] = 1
-            for dx, dy in dirs:
+
+            for dx, dy in ((1, 0), (-1, 0), (0, -1), (0, 1)):
                 nx, ny = s.x + dx, s.y + dy
                 if not (0 <= nx < n and 0 <= ny < m):
                     continue
                 dist = max(d[s.x][s.y], moveTime[nx][ny]) + (s.x + s.y) % 2 + 1
+
                 if d[nx][ny] > dist:
                     d[nx][ny] = dist
-                    heapq.heappush(q, State(nx, ny, dist))
+                    heappush(q, State(nx, ny, dist))
 
         return d[n - 1][m - 1]
