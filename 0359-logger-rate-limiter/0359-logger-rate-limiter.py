7c51c232c2d4df9352f99343a12eff793031b277
Andrew Kim <ksb0724@utexas.edu>
2025-05-11 13:12:38 +0900

    Time: 7 ms (81.02%), Space: 24.8 MB (81.75%) - LeetHub

--- 0359-logger-rate-limiter/0359-logger-rate-limiter.py
+++ 0359-logger-rate-limiter/0359-logger-rate-limiter.py
@@ -1,24 +1,14 @@
 class Logger:
 
     def __init__(self):
-        self._msg_set = set()
-        self._msg_queue = deque()
+        self.logs = dict()
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        while self._msg_queue:
-            msg, ts = self._msg_queue[0]
-            if timestamp - ts >= 10:
-                self._msg_queue.popleft()
-                self._msg_set.remove(msg)
-            else:
-                break
-        
-        if message not in self._msg_set:
-            self._msg_set.add(message)
-            self._msg_queue.append((message, timestamp))
-            return True
-        else:
+        if message in self.logs and self.logs[message] + 10 > timestamp:
             return False
+        
+        self.logs[message] = timestamp
+        return True
 
 
 # Your Logger object will be instantiated and called as such:
