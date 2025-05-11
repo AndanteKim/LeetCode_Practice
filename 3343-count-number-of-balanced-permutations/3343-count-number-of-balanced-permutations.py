e46d0c23cc344fc405e603dbc0107f85e48a2140
Andrew Kim <ksb0724@utexas.edu>
2025-05-12 03:50:18 +0900

    Time: 1937 ms (15.43%), Space: 18.2 MB (89.21%) - LeetHub

--- 3343-count-number-of-balanced-permutations/3343-count-number-of-balanced-permutations.py
+++ 3343-count-number-of-balanced-permutations/3343-count-number-of-balanced-permutations.py
@@ -1,39 +1,38 @@
 class Solution:
     def countBalancedPermutations(self, num: str) -> int:
-        mod, num = 1_000_000_007, list(map(int, num))
-        tot = sum(num)
+        mod = 10 ** 9 + 7
+        tot, n = 0, len(num)
+        cnt = [0] * 10
+        for ch in num:
+            d = int(ch)
+            cnt[d] += 1
+            tot += d
 
         # Base case
         if tot % 2 != 0:
             return 0
-
-        target, n = tot >> 1, len(num)
-        cnt = Counter(num)
-        max_odd = (n + 1) >> 1
-        psum = [0] * 11
-
-        for i in range(9, -1, -1):
-            psum[i] = psum[i + 1] + cnt[i]
         
-        @cache
-        def dfs(pos: int, curr: int, odd_cnt: int) -> int:
-            # If the remaining positions cannot complete a legal placement, or the sum of the elements\
-            # in the current odd positions is greater than the target value
-            if odd_cnt < 0 or psum[pos] < odd_cnt or curr > target:
-                return 0
+        target, max_odd = tot >> 1, (n + 1) >> 1
+        f = [[0] * (max_odd + 1) for _ in range(target + 1)]
+        f[0][0] = 1
+        psum = tot_sum = 0
+        for i in range(10):
+            # Sum of the number of the first i digits
+            psum += cnt[i]
+            # Sum of the first i numbers
+            tot_sum += i * cnt[i]
+            for odd_cnt in range(min(psum, max_odd), max(0, psum - (n - max_odd)) - 1, -1):
+                # The number of bits that need to be filled in even numbered positions
+                even_cnt = psum - odd_cnt
+                for curr in range(min(tot_sum, target), max(0, tot_sum - target) - 1, -1):
+                    ans = 0
+                    for j in range(max(0, cnt[i] - even_cnt), min(cnt[i], odd_cnt) + 1):
+                        if i * j > curr:
+                            break
+                        # The current digit is filled with j positions at odd positions, and cnt[i] - j positions at even positions
+                        ways = (comb(odd_cnt, j) * comb(even_cnt, cnt[i] - j) % mod)
+                        ans = (ans + ways * f[curr - i * j][odd_cnt - j] % mod) % mod
+                    f[curr][odd_cnt] = ans % mod
             
-            if pos > 9:
-                return int(curr == target and odd_cnt == 0)
-            
-            # Even-numbered positions remaining to be filled
-            even_cnt = (psum[pos] - odd_cnt)
-            ans = 0
 
-            for i in range(max(0, cnt[pos] - even_cnt), min(cnt[pos], odd_cnt) + 1):
-                # Place i of the current number at odd positions, and cnt[pos] - i\
-                # at even positions
-                ways = comb(odd_cnt, i) * comb(even_cnt, cnt[pos] - i) % mod
-                ans += ways * dfs(pos + 1, curr + i * pos, odd_cnt - i)
-            return ans % mod
-        
-        return dfs(0, 0, max_odd)
+        return f[target][max_odd] 
