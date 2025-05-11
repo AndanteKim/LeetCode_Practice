c2759665ab615340c03652fa0fd851ed539bc01a
Andrew Kim <ksb0724@utexas.edu>
2025-05-11 13:08:50 +0900

    Time: 0 ms (100%), Space: 11.8 MB (59.6%) - LeetHub

--- 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        int n = arr.size();
-        
-        for (int i = 0; i < n - 2; ++i){
-            int product = arr[i] * arr[i + 1] * arr[i + 2];
-            if (product % 2) return true;
+        int count = 0;
+
+        for (int num : arr){
+            if (num % 2) ++count;
+            else count = 0;
+
+            if (count == 3) return true;
         }
-        
+
         return false;
     }
 };
