2d3410e507963616366279f2dc94889eb489073e
Andrew Kim <ksb0724@utexas.edu>
2025-05-18 12:52:04 +0900

    Time: 0 ms (100%), Space: 17.9 MB (11.76%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,18 +3,24 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count, n = {0:0, 1:0, 2:0}, len(nums)
+        n = len(nums)
+        
+        # For all idx < p0 : nums[idx < p0] = 0
+        # curr is an index of elements under consideration
+        p0 = curr = 0
+        
+        # For all idx > p2: nums[idx > p2] = 2
+        p2 = n - 1
 
-        for num in nums:
-            count[num] += 1
-
-        for i in range(n):
-            if count[0] > 0:
-                nums[i] = 0
-                count[0] -= 1
-            elif count[1] > 0:
-                nums[i] = 1
-                count[1] -= 1
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
             else:
-                nums[i] = 2
-                count[2] -= 1
+                curr += 1
+
+        
