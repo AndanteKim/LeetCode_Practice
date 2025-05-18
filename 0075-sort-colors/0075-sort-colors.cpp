bb58239d22b0b436c1e204b6847585418052aaa9
Andrew Kim <ksb0724@utexas.edu>
2025-05-18 12:54:23 +0900

    Time: 0 ms (100%), Space: 11.6 MB (78.57%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        unordered_map<int, int> count{{0, 0}, {1, 0}, {2, 0}};
         int n = nums.size();
-        for (int num : nums) ++count[num];
 
-        for (int i = 0; i < n; ++i){
-            if (count[0] > 0){
-                --count[0];
-                nums[i] = 0;
+        // For all idx < p0 : nums[idx < p0] = 0
+        // curr is an index of elements under consideration
+        int curr = 0, p0 = curr;
+
+        // For all idx > p2 : nums[idx > p2] = 2
+        int p2 = n - 1;
+
+        while (curr <= p2){
+            if (nums[curr] == 0){
+                swap(nums[curr++], nums[p0++]);
             }
-            else if (count[1] > 0){
-                --count[1];
-                nums[i] = 1;
+            else if (nums[curr] == 2){
+                swap(nums[curr], nums[p2--]);
             }
-            else{
-                nums[i] = 2;
-                --count[2];
-            }
+            else
+                ++curr;
         }
-        
     }
 };
