2b59d05b3a3c0796cb9e2309d7bf6818064231ae
Andrew Kim <ksb0724@utexas.edu>
2025-05-21 19:48:26 +0900

    Time: 3 ms (11.48%), Space: 18.9 MB (12.91%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,42 +1,20 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int R = matrix.size(), C = matrix[0].size();
-        bool isCol = false;
-        
-        for (int i = 0; i < R; ++i){
-            // Since first cell for both first row and first column is the same i.e. matrix[0][0]
-            // We can use an additional variable for either the first row/column.
-            // For this solution we're using an additional variable for the first column
-            // and using matrix[0][0] for the first row.
-            if (matrix[i][0] == 0) isCol = true;
-            for (int j = 1; j < C; ++j)
-                // If an element is zero, we set the first element of the corresponding row and column to 0
-                if (matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
-                }
+        queue<pair<int, int>> q;
+        int m = matrix.size(), n = matrix[0].size();
+
+        for (int i = 0; i < m; ++ i){
+            for (int j = 0; j < n; ++j)
+                if (matrix[i][j] == 0) q.push({i, j});
         }
-        
-        // Iterate over the aray once again and using the first row and first column, update the elements.
-        for (int i = 1; i < R; ++i){
-            for (int j = 1; j < C; ++j){
-                if (matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        
-        // See if the first row needs to be set to zero as well
-        if (matrix[0][0] == 0){
-            for (int j = 0; j < C; ++j)
-                matrix[0][j] = 0;
-        }
-        
-        // See if the first column needs to be set to zero as well
-        if (isCol){
-            for (int i = 0; i < R; ++i)
-                matrix[i][0] = 0;
+
+        while (!q.empty()){
+            auto [currRow, currCol] = q.front(); q.pop();
+
+            for (int i = 0; i < n; ++i) matrix[currRow][i] = 0;
+
+            for (int i = 0; i < m; ++i) matrix[i][currCol] = 0;
         }
     }
 };
