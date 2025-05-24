59f0a73fddeafdeb61c8b4a1588f4a212699282e
Andrew Kim <ksb0724@utexas.edu>
2025-05-24 23:05:22 +0900

    Time: 211 ms (56.43%), Space: 128.3 MB (92.86%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.cpp
+++ 1429-first-unique-number/1429-first-unique-number.cpp
@@ -1,61 +1,33 @@
 class FirstUnique {
 private:
-    struct Node{
-        int val;
-        Node *prev, *next;
-        Node(int x, Node* p, Node* n): val(x), prev(p), next(n){}
-    };
-    unordered_map<int, Node*> root;
-    unordered_set<int> duplicates;
-    Node *head, *tail;
-    
+    queue<int> q;
+    unordered_map<int, int> frequency;
+
 public:
-    FirstUnique(vector<int>& nums): head(nullptr), tail(nullptr) {
-        for (int num : nums)
-            // Notice that we're calling the "add" method of FirstUnique; not of the queue.
-            add(num);
+    FirstUnique(vector<int>& nums) {
+        for (int num : nums){
+            if (frequency[num] == 0)
+                q.push(num);
+            
+            ++frequency[num];
+            while (!q.empty() && frequency[q.front()] > 1)
+                q.pop();
+        }
     }
     
     int showFirstUnique() {
-        // Check if there's still a value left in the queue. There might be no uniques.
-        if (head) return head -> val;
-        return -1;
+        return !q.empty()? q.front() : -1;
     }
     
     void add(int value) {
-        if (duplicates.count(value)) return;
-        
-        auto iter = root.find(value);
-        if (iter == root.end()){
-            if (tail){
-                tail -> next = new Node(value, tail, nullptr);
-                tail = tail -> next;
-            }
-            else{
-                head = new Node(value, nullptr, nullptr);
-                tail = head;
-            }
-            root.insert({value, tail});
-        }
+        ++frequency[value];
+
+        if (frequency[value] == 1)
+            q.push(value);
         else{
-            duplicates.insert(value);
-            Node* node = iter -> second;
-            
-            if (head == node)
-                head = node -> next;
-            
-            if (tail == node)
-                tail = tail -> prev;
-            
-            if (node -> next)
-                node -> next -> prev = node -> prev;
-            
-            if (node -> prev)
-                node -> prev -> next = node -> next;
-            root.erase(iter);
-            delete node;
+            while (!q.empty() && frequency[q.front()] > 1)
+                q.pop();
         }
-        
     }
 };
 
