105792b9f0031d61892cf22d243708104206db59
Andrew Kim <ksb0724@utexas.edu>
2025-05-24 22:59:40 +0900

    Time: 593 ms (36.42%), Space: 60.3 MB (78.55%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.py
+++ 1429-first-unique-number/1429-first-unique-number.py
@@ -1,37 +1,25 @@
-from collections import OrderedDict
-
 class FirstUnique:
 
     def __init__(self, nums: List[int]):
-        self._queue = OrderedDict()
-        self._is_unique = dict()
-        
+        self.queue, self.count = deque(), defaultdict(int)
         for num in nums:
-            # Notice that we're calling the "add" method of FirstUnique; not of the queue.
-            self.add(num)
+            if num not in self.count:
+                self.queue.append(num)
+            self.count[num] += 1
+
+            while self.queue and self.count[self.queue[0]] > 1:
+                self.queue.popleft()
 
     def showFirstUnique(self) -> int:
-        # Check if there's still a value left in the queue. There might be no uniques.
-        if self._queue:
-            # We don't want to actually remove the value.
-            # Seeing as OrderedDict has no "get first" method, the way that we can get
-            # the first value is to create an iterator, and then get the "next" value
-            # from that. Note that this is O(1).
-            return next(iter(self._queue))
-        return -1
+        return -1 if not self.queue else self.queue[0]
 
     def add(self, value: int) -> None:
-        # Case 1: We need to add the number to the queue and mark it as unique.
-        if value not in self._is_unique:
-            self._is_unique[value] = True
-            self._queue[value] = None
-        # Case 2: We need to mark the value as no longer unique and then
-        # remove it from the queue.
-        elif self._is_unique[value]:
-            self._is_unique[value] = False
-            self._queue.pop(value)
-            
+        if value not in self.count:
+            self.queue.append(value)
 
+        self.count[value] += 1
+        while self.queue and self.count[self.queue[0]] > 1:
+            self.queue.popleft()
 
 # Your FirstUnique object will be instantiated and called as such:
 # obj = FirstUnique(nums)
