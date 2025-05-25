89f3e85fc63238946109a9320d874d421ae32e81
Andrew Kim <ksb0724@utexas.edu>
2025-05-25 17:19:21 +0900

    Time: 2 ms (99.18%), Space: 172.2 MB (18.72%) - LeetHub

---
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        int alphabetSize = 26;
+        vector count(26, vector<int>(26, 0));
+
+        for (const string& word : words){
+            ++count[word[0] - 97][word[1] - 97];
+        }
+
+        int ans = 0;
+        bool central = false;
+        for (int i = 0; i < 26; ++i){
+            if (count[i][i] % 2 == 0)
+                ans += count[i][i];
+            else{
+                ans += count[i][i] - 1;
+                central = true;
+            }
+
+            for (int j = i + 1; j < 26; ++j)
+                ans += 2 * min(count[i][j], count[j][i]);
+        }
+
+        if (central) ++ans;
+
+        return 2 * ans;
+    }
+};
