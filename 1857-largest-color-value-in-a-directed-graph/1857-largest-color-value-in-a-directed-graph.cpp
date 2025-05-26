4dbb98c2dcddf7b814f7b121d60529bc468acae5
Andrew Kim <ksb0724@utexas.edu>
2025-05-27 02:52:47 +0900

    Time: 339 ms (91.77%), Space: 174.7 MB (57.88%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,31 +1,40 @@
 class Solution {
-    int dfs(int node, string& colors, vector<vector<int>>& adj, vector<vector<int>>& count,\
-           vector<bool>& visited, vector<bool>& inStack){
-        if (inStack[node]) return INT_MAX;
-        if (visited[node])
-            return count[node][colors[node] - 'a'];
-        
-        visited[node] = true;
-        inStack[node] = true;
-        for (int neighbor : adj[node]){
-            if (dfs(neighbor, colors, adj, count, visited, inStack) == INT_MAX) return INT_MAX;
-            for (int i = 0; i < 26; ++i) count[node][i] = max(count[node][i], count[neighbor][i]);
-        }
-        ++count[node][colors[node] - 'a'];
-        inStack[node] = false;
-        return count[node][colors[node] - 'a'];
-    }
-    
 public:
     int largestPathValue(string colors, vector<vector<int>>& edges) {
-        int n = colors.size(), ans = 0;
+        int n = colors.size();
+        vector count(n, vector<int>(26, 0));
         vector<vector<int>> adj(n);
-        for (auto& edge: edges) adj[edge[0]].push_back(edge[1]);
-        vector<vector<int>> count(n, vector<int>(26));
-        vector<bool> visited(n), inStack(n);
+        vector<int> indegree(n);
+
+        for (const auto& edge : edges){
+            adj[edge[0]].push_back(edge[1]);
+            ++indegree[edge[1]];
+        }
         
-        for (int i = 0; i < n; ++i)
-            ans = max(ans, dfs(i, colors, adj, count, visited, inStack));
-        return ans == INT_MAX? -1 : ans;
+        queue<int> q;
+        // Push all the nodes with indegree zero in the queue.
+        for (int i = 0; i < n; ++i){
+            if (indegree[i] == 0) q.push(i);
+        }
+
+        int ans = 0, nodesSeen = 0;
+        while (!q.empty()){
+            int node = q.front(); q.pop();
+            ++nodesSeen;
+            ans = max(ans, ++count[node][colors[node] - 97]);
+
+            for (int neighbor : adj[node]){
+                // Try to update the frequency of colors for neighbor to include paths
+                // that use node -> neighbor edge
+                for (int i = 0; i < 26; ++i){
+                    count[neighbor][i] = max(count[neighbor][i], count[node][i]);
+                }
+                --indegree[neighbor];
+                if (indegree[neighbor] == 0)
+                    q.push(neighbor);
+            }
+        }
+
+        return (nodesSeen < n)? -1 : ans;
     }
 };
