078caed5e0078993201f04d063bb7d2986512ead
Andrew Kim <ksb0724@utexas.edu>
2025-05-27 21:06:03 +0900

    Time: 0 ms (100%), Space: 8.5 MB (36.04%) - LeetHub

--- 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
+++ 2894-divisible-and-non-divisible-sums-difference/2894-divisible-and-non-divisible-sums-difference.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int num1 = 0, num2 = 0;
-
-        for (int i = 1; i <= n; ++i){
-            if (i % m != 0) num1 += i;
-            else num2 += i;
-        }
-
-        return num1 - num2;
+        int k = n / m;
+        // num1 - num2 = n(n + 1) / 2 - 2 num2 = n(n + 1) / 2 - k (k + 1) * m
+        return (n * (n + 1) >> 1) - k * (k + 1) * m;
     }
 };
