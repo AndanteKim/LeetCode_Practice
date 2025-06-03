aca4b1946c6b909052c4a2ad70c08b1eafeade4c
Andrew Kim <ksb0724@utexas.edu>
2025-06-04 01:44:03 +0900

    Time: 15 ms (77.78%), Space: 28.2 MB (65.13%) - LeetHub

---
+++ 1298-maximum-candies-you-can-get-from-boxes/1298-maximum-candies-you-can-get-from-boxes.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
+        n = len(status)
+        can_open = [status[i] == 1 for i in range(n)]
+        has_box, used = [False] * n, [False] * n
+
+        q, ans = deque(), 0
+
+        for box in initialBoxes:
+            has_box[box] = True
+            if can_open[box]:
+                q.append(box)
+                used[box] = True
+                ans += candies[box]
+
+        while q:
+            big_box = q.popleft()
+            for key in keys[big_box]:
+                can_open[key] = True
+                if not used[key] and has_box[key]:
+                    q.append(key)
+                    used[key] = True
+                    ans += candies[key]
+            
+            for box in containedBoxes[big_box]:
+                has_box[box] = True
+                if not used[box] and can_open[box]:
+                    q.append(box)
+                    used[box] = True
+                    ans += candies[box]
+            
+        return ans
