442e853eed81564b8fb136fe65a6c4f685b7fe08
Andrew Kim <ksb0724@utexas.edu>
2025-06-06 08:57:48 +0900

    Time: 7 ms (50.12%), Space: 17.8 MB (87.65%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.py
@@ -1,32 +1,33 @@
 class Solution:
-    representative = [0] * 26
-    
-    def find(self, x: int) -> int:
-            if (self.representative[x] == x): return x
-            return self.find(self.representative[x])
+    minChar = 2**31 - 1
+    def DFS(self, src: int, adjMatrix: List[List[int]], visited: List[int], component: List[int]):
+        visited[src] = 1
+        component.append(src)
+        self.minChar = min(self.minChar, src)
         
-    def performUnion(self, x: int, y: int) -> None:
-        x = self.find(x)
-        y = self.find(y)
-        if x == y: return
-            
-        if (x < y):
-            self.representative[y] = x
-        else:
-            self.representative[x] = y
+        for i in range(26):
+            if (adjMatrix[src][i] != None and visited[i] == None):
+                self.DFS(i, adjMatrix, visited, component)
     
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
-        ans = ""
-        for i in range(26):
-            self.representative[i] = i
-        
+        adjMatrix = [[None] * 26 for _ in range(26)]
         for i in range(len(s1)):
-            self.performUnion(ord(s1[i]) - ord('a'), ord(s2[i]) - ord('a'))
+            adjMatrix[ord(s1[i]) - ord('a')][ord(s2[i]) - ord('a')] = 1
+            adjMatrix[ord(s2[i]) - ord('a')][ord(s1[i]) - ord('a')] = 1
         
+        mappingChar = [i for i in range(26)]
+        visited = [None] *26
+        for c in range(26):
+            if visited[c] == None:
+                component = []
+                self.minChar = 27
+                self.DFS(c, adjMatrix, visited, component)
+                
+                for vertex in component:
+                    mappingChar[vertex] = self.minChar
+                    
+        ans = ""
         for c in baseStr:
-            ans += chr(self.find(ord(c)-ord('a')) + ord('a'))
+            ans += chr(mappingChar[ord(c) - ord('a')] + ord('a'))
         
         return ans
-            
-        
-        
