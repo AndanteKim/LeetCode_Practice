e824ce9fc3a37503b98e1329e4690c3ce5ea9140
Andrew Kim <ksb0724@utexas.edu>
2025-06-07 22:53:38 +0900

    Time: 55 ms (82.47%), Space: 38 MB (30.52%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     string clearStars(string s) {
-        vector cnt(26, vector<int>{});
+        vector<stack<int>> cnt(26);
         string ans = "";
 
         for (int i = 0; i < s.size(); ++i){
             if (s[i] != '*'){
-                cnt[s[i] - 97].push_back(i);
+                cnt[s[i] - 97].push(i);
             }
             else{
                 for (int j = 0; j < 26; ++j){
                     if (!cnt[j].empty()){
-                        s[cnt[j].back()] = '*';
-                        cnt[j].pop_back();
+                        s[cnt[j].top()] = '*';
+                        cnt[j].pop();
                         break;
                     }
                 }
             }
         }
 
-        for (int i = 0; i < s.size(); ++i){
+        for (int i = 0; i < s.size(); ++i)
             if (s[i] != '*') ans.push_back(s[i]);
-        }
 
         return ans;
     }
