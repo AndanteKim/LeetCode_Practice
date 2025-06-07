4eba238fd31ec4619cf7b3cbdc3411d962360708
Andrew Kim <ksb0724@utexas.edu>
2025-06-08 03:54:02 +0900

    Time: 729 ms (31.66%), Space: 34.2 MB (5.04%) - LeetHub

--- 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.py
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.py
@@ -1,14 +1,13 @@
 class Solution:
     def clearStars(self, s: str) -> str:
-        cnt, arr = [[] for _ in range(26)], list(s)
-        
-        for i, c in enumerate(s):
-            if c != '*':
-                cnt[ord(c) - 97].append(i)
-            else:
-                for j in range(26):
-                    if cnt[j]:
-                        arr[cnt[j].pop()] = '*'
-                        break
+        remove = set()
+        min_heap = []
 
-        return "".join(c for c in arr if c != '*')
+        for i, c in enumerate(s):
+            if c == '*':
+                remove.add(i)
+                remove.add(-heappop(min_heap)[1])
+            else:
+                heappush(min_heap, (c, -i))
+        
+        return "".join(c for i, c in enumerate(s) if i not in remove)
