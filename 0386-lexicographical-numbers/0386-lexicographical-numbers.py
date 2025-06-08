7a1dc232dcbb9dbc9e32c95141c953cb9491c486
Andrew Kim <ksb0724@utexas.edu>
2025-06-08 18:38:22 +0900

    Time: 83 ms (17.39%), Space: 22.8 MB (32.49%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.py
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -1,56 +1,15 @@
-class Node:
-    def __init__(self):
-        self.links = [None] * 10
-        self.flag = False
-        
-    def contain_keys(self, ch: chr) -> bool:
-        return self.links[ord(ch) - 48] != None
-    
-    def put(self, ch: chr, node: 'Node') -> None:
-        self.links[ord(ch) - 48] = node
-    
-    def get(self, ch: chr) -> 'Node':
-        return self.links[ord(ch) - 48]
-    
-    def set_end(self):
-        self.flag = True
-        
-    def get_end(self) -> bool:
-        return self.flag
-        
-class Trie:
-    def __init__(self):
-        self.root = Node()
-        
-    def insert(self, num: str) -> None:
-        temp = self.root
-        for i in range(len(num)):
-            if not temp.contain_keys(num[i]):
-                temp.put(num[i], Node())
-            temp = temp.get(num[i])
-        temp.set_end()
-
 class Solution:
-    def dfs(self, root: 'Node', curr: int, ans: List[int]) -> None:
-        # Base case
-        if not root:
-            return
-        
-        if root.get_end():
-            ans.append(curr)
-        for i in range(0, 10):
-            if root.contain_keys(chr(48 + i)):
-                self.dfs(root.get(chr(48 + i)), (curr * 10) + i, ans)
-    
     def lexicalOrder(self, n: int) -> List[int]:
-        # Trie
-        trie = Trie()
-        
-        for i in range(1, n + 1):
-            s = str(i)
-            trie.insert(s)
-            
+        def backtrack(curr: int) -> None:
+            if curr > n:
+                return
+
+            ans.append(curr)
+            for i in range(0, 10):
+                backtrack(curr * 10 + i)
+
         ans = []
-        self.dfs(trie.root, 0, ans)
+        for i in range(1,10):
+            backtrack(i)
+
         return ans
-        
