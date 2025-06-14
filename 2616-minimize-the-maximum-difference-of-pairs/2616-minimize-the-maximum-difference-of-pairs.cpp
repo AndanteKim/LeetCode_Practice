568e91395cf553839efa4f2c353009e829c4b536
Andrew Kim <ksb0724@utexas.edu>
2025-06-14 09:05:55 +0900

    Time: 22 ms (80.79%), Space: 86.9 MB (17.36%) - LeetHub

--- 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.cpp
@@ -1,31 +1,39 @@
 class Solution {
 private:
     int n;
-    int countValidPairs(int threshold, vector<int>& nums){
-        int count = 0, index = 0;
+    int countValidPairs(vector<int>& nums, int threshold){
+        int index = 0, count = 0;
+
+        // Find the number of valid pairs by greedy approach.
         while (index < n - 1){
+            // If a valid pair is found, skip both numbers.
             if (nums[index + 1] - nums[index] <= threshold){
                 ++count;
                 ++index;
             }
             ++index;
         }
+
         return count;
     }
-    
+
 public:
     int minimizeMax(vector<int>& nums, int p) {
-        this -> n = nums.size();
+        n = nums.size();
         sort(nums.begin(), nums.end());
-        int left = 0, right = nums.back();
+        int left = 0, right = nums.back() - nums[0];
+
         while (left < right){
             int mid = left + ((right - left) >> 1);
-            if (countValidPairs(mid, nums) >= p)
+
+            // If there are enough pairs, look for a smaller threshold.
+            // Otherwise, look for a larger threshold.
+            if (countValidPairs(nums, mid) >= p)
                 right = mid;
             else
                 left = mid + 1;
         }
-        
+
         return left;
     }
 };
