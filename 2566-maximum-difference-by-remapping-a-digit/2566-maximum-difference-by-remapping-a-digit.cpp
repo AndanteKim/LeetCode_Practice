f4b778dcf440b2a9fb5333da0ff8e673ea169b7a
Andrew Kim <ksb0724@utexas.edu>
2025-06-16 04:13:09 +0900

    Time: 0 ms (100%), Space: 8.1 MB (85.96%) - LeetHub

--- 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
+++ 2566-maximum-difference-by-remapping-a-digit/2566-maximum-difference-by-remapping-a-digit.cpp
@@ -1,35 +1,15 @@
 class Solution {
 public:
     int minMaxDifference(int num) {
-        int n = num, mx = std::numeric_limits<int>::min(), mn = std::numeric_limits<int>::max();
-        unordered_map<int, int> count;
-        while (n > 0){
-            ++count[n % 10];
-            n /= 10;
+        string s = to_string(num), t = s;
+        size_t pos = s.find_first_not_of('9');
+        if (pos != string::npos){
+            char a = s[pos];
+            replace(s.begin(), s.end(), a, '9');
         }
 
-        for (const auto& [digit, _] : count){
-            n = num;
-            int currMax = 0, currMin = 0, decimal = 1;
-
-            while (n > 0){
-                int curr = n % 10;
-                if (curr == digit){
-                    currMax += 9 * decimal;
-                }
-                else{
-                    currMax += curr * decimal;
-                    currMin += curr * decimal;
-                }
-                decimal *= 10;
-                n /= 10;
-            }
-            
-            mx = max(mx, currMax);
-            mn = min(mn, currMin);
-        }
-        
-
-        return mx - mn;
+        char b = t[0];
+        replace(t.begin(), t.end(), b, '0');
+        return stoi(s) - stoi(t);
     }
 };
