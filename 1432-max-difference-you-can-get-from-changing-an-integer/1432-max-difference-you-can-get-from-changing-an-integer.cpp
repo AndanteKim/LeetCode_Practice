5e3e10ae25ebe37572e12efce469da0b8426b9bc
Andrew Kim <ksb0724@utexas.edu>
2025-06-15 19:46:24 +0900

    Time: 0 ms (100%), Space: 8.2 MB (43.13%) - LeetHub

--- 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1432-max-difference-you-can-get-from-changing-an-integer/1432-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     int maxDiff(int num) {
-        auto change = [&](int x, int y){
-            string numS = to_string(num);
-            for (char& digit : numS){
-                if (digit - '0' == x)
-                    digit = '0' + y;
+        auto replace = [](string& s, char x, char y){
+            for (char& digit : s){
+                if (digit == x) digit = y;
             }
-            return numS;
         };
 
-        int minNum = num, maxNum = num;
 
-        for (int x = 0; x < 10; ++x){
-            for (int y = 0; y < 10; ++y){
-                string res = change(x, y);
+        string minNum = to_string(num), maxNum = to_string(num);
+        int n = minNum.size();
 
-                // Check if there are leading zeros
-                if (res[0] != '0'){
-                    int resInt = stoi(res);
-                    minNum = min(minNum, resInt);
-                    maxNum = max(maxNum, resInt);
-                }
+        // Find a high position and replace it with 9.
+        for (char digit : maxNum){
+            if (digit != '9'){
+                replace(maxNum, digit, '9');
+                break;
             }
         }
 
-        return maxNum - minNum;
+        // Replace the most significant bit with 1
+        // Or find a high-order digit that is not equal to the highest digit and replace it with 0.
+        for (int i = 0; i < n; ++i){
+            char digit = minNum[i];
+            if (i == 0){
+                if (digit != '1'){
+                    replace(minNum, digit, '1');
+                    break;
+                }
+            }
+            else{
+                if (digit != '0' && digit != minNum[0]){
+                    replace(minNum, digit, '0');
+                    break;
+                }
+            }
+        }
+        return stoi(maxNum) - stoi(minNum);
     }
 };
