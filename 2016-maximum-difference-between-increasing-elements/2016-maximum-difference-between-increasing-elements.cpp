dd5f640541bd80d571241e9ccbd64ba7f49038e9
Andrew Kim <ksb0724@utexas.edu>
2025-06-17 02:56:57 +0900

    Time: 0 ms (100%), Space: 12 MB (38.11%) - LeetHub

--- 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
+++ 2016-maximum-difference-between-increasing-elements/2016-maximum-difference-between-increasing-elements.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maximumDifference(vector<int>& nums) {
         int ans = -1, n = nums.size();
+        vector<int> increasing;
 
-        for (int i = 0; i < n - 1; ++i){
-            for (int j = i + 1; j < n; ++j){
-                if (nums[i] < nums[j])
-                    ans = max(ans, nums[j] - nums[i]);
-            }
+        for (int num : nums){
+            while (!increasing.empty() && increasing.back() > num) increasing.pop_back();
+            if (!increasing.empty() && increasing[0] < num) ans = max(ans, num - increasing[0]);
+            increasing.push_back(num);
         }
-
+        
         return ans;
     }
 };
