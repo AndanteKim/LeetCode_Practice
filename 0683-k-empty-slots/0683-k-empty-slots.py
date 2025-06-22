cc469c647480fe8758145f58c3e2a62d44f657b1
Andrew Kim <ksb0724@utexas.edu>
2025-06-23 00:26:04 +0900

    Time: 367 ms (17.88%), Space: 20 MB (18.54%) - LeetHub

--- 0683-k-empty-slots/0683-k-empty-slots.py
+++ 0683-k-empty-slots/0683-k-empty-slots.py
@@ -1,13 +1,36 @@
+class MinQueue(deque):
+    def __init__(self):
+        deque.__init__(self)
+        self.mins = deque()
+
+    def append(self, x: int) -> None:
+        deque.append(self, x)
+        while self.mins and x < self.mins[-1]:
+            self.mins.pop()
+        self.mins.append(x)
+
+    def popleft(self):
+        x = deque.popleft(self)
+        if self.mins[0] == x:
+            self.mins.popleft()
+        return x
+    
+    def min(self):
+        return self.mins[0]
+
 class Solution:
     def kEmptySlots(self, bulbs: List[int], k: int) -> int:
-        active = []
-
-        for day, bulb in enumerate(bulbs, 1):
-            i = bisect.bisect(active, bulb)
-
-            for neighbor in active[i - (i > 0) : i + 1]:
-                if abs(neighbor - bulb) - 1 == k:
-                    return day
-            active.insert(i, bulb)
+        days = [0] * len(bulbs)
+        for day, position in enumerate(bulbs, 1):
+            days[position - 1] = day
         
-        return -1
+        window, ans = MinQueue(), len(days)
+
+        for i, day in enumerate(days):
+            window.append(day)
+            if k <= i and i < len(days) - 1:
+                window.popleft()
+                if k == 0 or days[i - k] < window.min() and window.min() > days[i + 1]:
+                    ans = min(ans, max(days[i - k], days[i + 1]))
+
+        return ans if ans < len(days) else -1
