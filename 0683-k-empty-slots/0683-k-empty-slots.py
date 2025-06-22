12e41e056ea6d753b5d5707fcb09e63be605dc17
Andrew Kim <ksb0724@utexas.edu>
2025-06-22 21:38:05 +0900

    Time: 183 ms (56.29%), Space: 19.8 MB (25.83%) - LeetHub

---
+++ 0683-k-empty-slots/0683-k-empty-slots.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kEmptySlots(self, bulbs: List[int], k: int) -> int:
+        active = []
+
+        for day, bulb in enumerate(bulbs, 1):
+            i = bisect.bisect(active, bulb)
+
+            for neighbor in active[i - (i > 0) : i + 1]:
+                if abs(neighbor - bulb) - 1 == k:
+                    return day
+            active.insert(i, bulb)
+        
+        return -1
