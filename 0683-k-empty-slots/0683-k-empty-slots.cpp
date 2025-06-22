3c9669c950a567f8587af0c24a91774d204b9e23
Andrew Kim <ksb0724@utexas.edu>
2025-06-23 01:18:20 +0900

    Time: 3 ms (91.67%), Space: 88.3 MB (83.33%) - LeetHub

--- 0683-k-empty-slots/0683-k-empty-slots.cpp
+++ 0683-k-empty-slots/0683-k-empty-slots.cpp
@@ -1,47 +1,29 @@
-class MinQueue{
-private:
-    deque<int> mins, q;
-
-public:
-    void push(int x){
-        q.push_back(x);
-        while (!mins.empty() && x < mins.back()) mins.pop_back();
-        mins.push_back(x);
-    }
-
-    void pop(){
-        if (!q.empty()){
-            int x = q.front(); q.pop_front();
-            if (mins.front() == x) mins.pop_front();
-        }
-    }
-
-    int getMin() const{
-        return mins.front();
-    }
-};
-
 class Solution {
 public:
     int kEmptySlots(vector<int>& bulbs, int k) {
         int n = bulbs.size();
+        int ans = std::numeric_limits<int>::max();
         vector<int> days(n);
 
-        // Map to bulbs position
-        for (int day = 1; day < n; ++day) days[bulbs[day] - 1] = day + 1;
+        for (int day = 0; day < n; ++day) days[bulbs[day] - 1] = day + 1;
 
-        MinQueue window;
-        int ans = n;
-
-        for (int i = 0; i < n; ++i){
-            window.push(days[i]);
-            if (k <= i && i < n - 1){
-                window.pop();
-                if (k == 0 || (days[i - k] < window.getMin() && window.getMin() > days[i + 1]))
-                    ans = min(ans, max(days[i - k], days[i + 1]));
+        int left = 0, right = k + 1;
+        while (right < n){
+            bool found = true;
+            for (int i = left + 1; i < right; ++i){
+                if (days[i] < days[left] || days[i] < days[right]){
+                    left = i; right = i + k + 1;
+                    found = false;
+                    break;
+                }
+            }
+            
+            if (found){
+                ans = min(ans, max(days[left], days[right]));
+                left = right; right = left + k + 1;
             }
         }
 
-        return (ans < n)? ans : -1;
+        return ans != std::numeric_limits<int>::max()? ans : -1;
     }
 };
