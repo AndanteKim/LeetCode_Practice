15f653b8e9a21e08f28755d6b8fb80cb3a0adf0c
Andrew Kim <ksb0724@utexas.edu>
2025-06-29 12:44:21 +0900

    Time: 50 ms (11.59%), Space: 54.2 MB (56%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,21 +1,20 @@
-#define ull unsigned long long
-
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
+        int n = nums.size(), mod = 1'000'000'007, ans = 0;
         sort(nums.begin(), nums.end());
-        int n = nums.size();
-        int ans = 0, left = 0, right = n - 1, mod = pow(10, 9) + 7;
-        vector<int> pows(n, 1);
-        for (int i = 1; i < n; ++i) pows[i] = pows[i - 1] * 2 % mod;
-        while (left <= right){
-            if (nums[left] + nums[right] <= target){
-                ans = (ans + pows[right - left]) % mod;
-                ++left;
-            }
-            else --right;
+
+        vector<int> power(n);
+        power[0] = 1;
+        for (int i = 1; i < n; ++i) power[i] = (power[i - 1] * 2) % mod;
+
+        for (int left = 0; left < n; ++left){
+            // Find the insertion position for 'target - nums[left]'
+            // 'right' equals the insertion index minus - 1.
+            int right = upper_bound(nums.begin(), nums.end(), target - nums[left]) - nums.begin() - 1;
+            if (right >= left) ans = (ans + (long long)power[right - left]) % mod;
         }
-        
-        return ans;
+
+        return ans % mod;
     }
 };
