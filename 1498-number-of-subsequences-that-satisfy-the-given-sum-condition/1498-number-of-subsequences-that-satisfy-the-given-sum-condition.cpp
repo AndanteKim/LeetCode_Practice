337506ecf277face8c51dc82621d4c09c56f4ed8
Andrew Kim <ksb0724@utexas.edu>
2025-06-29 12:56:52 +0900

    Time: 28 ms (76.96%), Space: 54.2 MB (56%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
-        int n = nums.size(), mod = 1'000'000'007, ans = 0;
-        sort(nums.begin(), nums.end());
-
+        int n = nums.size(), ans = 0, mod = 1'000'000'007;
         vector<int> power(n);
         power[0] = 1;
-        for (int i = 1; i < n; ++i) power[i] = (power[i - 1] * 2) % mod;
+        for (int i = 1; i < n; ++i) power[i] = power[i - 1] * 2 % mod;
 
-        for (int left = 0; left < n; ++left){
-            // Find the insertion position for 'target - nums[left]'
-            // 'right' equals the insertion index minus - 1.
-            int right = upper_bound(nums.begin(), nums.end(), target - nums[left]) - nums.begin() - 1;
-            if (right >= left) ans = (ans + (long long)power[right - left]) % mod;
+        sort(nums.begin(), nums.end());
+        int left = 0, right = n - 1;
+
+        while (left <= right){
+            if (nums[left] + nums[right] <= target){
+                ans = (ans + power[right - left]) % mod;
+                ++left;
+            }
+            else --right;
         }
 
-        return ans % mod;
+        return ans;
     }
 };
