e29588e84a29916186591efa238b626e32a8028d
Andrew Kim <ksb0724@utexas.edu>
2025-06-29 12:52:15 +0900

    Time: 6285 ms (20.94%), Space: 28.2 MB (51.42%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,14 +1,14 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
+        n, mod = len(nums), 1_000_000_007
         nums.sort()
-        ans, n, mod = 0, len(nums), 10 ** 9 + 7
+        ans, left, right = 0, 0, n - 1
 
-        for left in range(n):
-            # Find the insertion position for 'target - nums[left]'
-            # 'right' equals the insertion index minus - 1
-            right = bisect_right(nums, target - nums[left]) - 1
-
-            if right >= left:
-                ans += 2 ** (right - left) % mod
-
-        return ans % mod
+        while left <= right:
+            if nums[left] + nums[right] <= target:
+                ans = (ans + pow(2, right - left)) % mod
+                left += 1
+            else:
+                right -= 1
+            
+        return ans
