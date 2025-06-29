0550a78207767ca4ba42dbdfcef0d2537751ff93
Andrew Kim <ksb0724@utexas.edu>
2025-06-29 12:53:39 +0900

    Time: 111 ms (97.51%), Space: 28.2 MB (40.09%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -3,10 +3,15 @@ class Solution:
         n, mod = len(nums), 1_000_000_007
         nums.sort()
         ans, left, right = 0, 0, n - 1
+        power = [0] * n
+        power[0] = 1
+        for i in range(1, n):
+            power[i] = power[i - 1] * 2 % mod
+        
 
         while left <= right:
             if nums[left] + nums[right] <= target:
-                ans = (ans + pow(2, right - left)) % mod
+                ans = (ans + power[right - left]) % mod
                 left += 1
             else:
                 right -= 1
