20de03e7c6b57a0de4a51c17b9f4705445842088
Andrew Kim <ksb0724@utexas.edu>
2025-07-03 08:50:01 +0900

    Time: 4028 ms (60.44%), Space: 26.2 MB (52.75%) - LeetHub

---
+++ 3333-find-the-original-typed-string-ii/3333-find-the-original-typed-string-ii.py
@@ -0,0 +1,33 @@
+class Solution:
+    def possibleStringCount(self, word: str, k: int) -> int:
+        mod = 10**9 + 7
+        n, cnt = len(word), 1
+        freq = list()
+
+        for i in range(1, n):
+            if word[i] == word[i - 1]:
+                cnt += 1
+            else:
+                freq.append(cnt)
+                cnt = 1
+        freq.append(cnt)
+
+        ans = 1
+        for o in freq:
+            ans = ans * o % mod
+
+        if len(freq) >= k:
+            return ans
+
+        f, g = [1] + [0] * (k - 1), [1] * k
+        for i in range(len(freq)):
+            f_new = [0] * k
+            for j in range(1, k):
+                f_new[j] = g[j - 1]
+                if j - freq[i] - 1 >= 0:
+                    f_new[j] = (f_new[j] - g[j - freq[i] - 1]) % mod
+            g_new = [f_new[0]] + [0] * (k - 1)
+            for j in range(1, k):
+                g_new[j] = (g_new[j - 1] + f_new[j]) % mod
+            f, g = f_new, g_new
+        return (ans - g[k - 1]) % mod
