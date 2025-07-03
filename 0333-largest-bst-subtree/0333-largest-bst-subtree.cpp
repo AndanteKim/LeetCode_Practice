eb1528cb83f0de66d2dcff0a3198cfec88760145
Andrew Kim <ksb0724@utexas.edu>
2025-07-03 23:48:16 +0900

    Time: 1 ms (43.45%), Space: 20.9 MB (99.66%) - LeetHub

--- 0333-largest-bst-subtree/0333-largest-bst-subtree.cpp
+++ 0333-largest-bst-subtree/0333-largest-bst-subtree.cpp
@@ -11,63 +11,49 @@
  */
 class Solution {
 private:
+    TreeNode* prev;
     bool isValidBST(TreeNode* node){
-        // Check if given tree is a valid Binary Search Tree.
-        // An empty tree is a valid Binary Search Tree.
+        /*
+        Check if given tree is a valid BST using in-order traversal.
+        
+        An empty tree is a valid Binary Search Tree.
+        */
         if (!node) return true;
 
-        // Find the max node in the left subtree of current node.
-        int leftMax = findMax(node -> left);
+        // If left subtree isn't a valid BST return false.
+        if (!isValidBST(node -> left)) return false;
 
-        // If the left subtree has a node greater than or equal to the current node,
-        // then it's not a valid Binary Search Tree.
-        if (leftMax >= node -> val) return false;
+        // If current node's value isn't greater than the previous node's value
+        // in the in-order traversal return false.
+        if (prev && prev -> val >= node -> val) return false;
 
-        // Find the min node in the right subtree of current node.
-        int rightMin = findMin(node -> right);
-        
-        // If the right subtree has a value less than or equal to the current node,
-        // then it's not a valid Binary Search Tree.
-        if (rightMin <= node -> val) return false;
+        // Update previous node to current node.
+        prev = node;
 
-        // If the left and right subtrees of current tree are also valid BST.
-        // then the whole tree is a BST.
-        return isValidBST(node -> left) && isValidBST(node -> right);
+        // If the right subtree isn't a valid BST return false
+        return isValidBST(node -> right);
     }
 
-    int findMax(TreeNode* node){
-        // Max node in a empty tree should smaller than parent.
-        if (!node) return std::numeric_limits<int>::min();
-
-        // Check the maximum node from the current node, left and right subtree of the current tree.
-        return max(node -> val, max(findMax(node -> left), findMax(node -> right)));
-    }
-
-    int findMin(TreeNode* node){
-        // Min node in a empty tree should be larger than parent.
-        if (!node) return std::numeric_limits<int>::max();
-        
-        // Check the minimum node from the current node, left and right subtree of the current tree
-        return min(node -> val, min(findMin(node -> left), findMin(node -> right)));
-    }
-    
-
+    // Count nodes in current tree.
     int countNodes(TreeNode* node){
         if (!node) return 0;
 
         // Add nodes in left and right subtree.
         // Add 1 and return total size.
         return 1 + countNodes(node -> left) + countNodes(node -> right);
-    }
+    } 
 
 public:
     int largestBSTSubtree(TreeNode* root) {
         if (!root) return 0;
 
-        // If current subtree is a validBST, its children will have smaller size BST.
+        // Previous node is initially null.
+        prev = nullptr;
+
+        // If the current subtree is a valid BST, its children will have smaller size BST.
         if (isValidBST(root)) return countNodes(root);
 
         // Find BST in left and right subtrees of current nodes.
-        return max(largestBSTSubtree(root -> left), largestBSTSubtree(root -> right));    
+        return max(largestBSTSubtree(root -> left), largestBSTSubtree(root -> right));
     }
 };
