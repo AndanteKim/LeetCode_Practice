01338e116f478b87327ec16bf31b0a5d2df22fbc
Andrew Kim <ksb0724@utexas.edu>
2025-07-08 23:45:42 +0900

    Time: 759 ms (66.48%), Space: 93.5 MB (59.89%) - LeetHub

--- 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
+++ 1751-maximum-number-of-events-that-can-be-attended-ii/1751-maximum-number-of-events-that-can-be-attended-ii.py
@@ -1,13 +1,22 @@
 class Solution:
     def maxValue(self, events: List[List[int]], k: int) -> int:
-        n = len(events)
-        dp = [[0] * (n + 1) for _ in range(k + 1)]
-        events.sort()
-        starts = [start for start, end, value in events]
+        def dp(curr_idx: int, cnt: int) -> int:
+            # Base case
+            if cnt == 0 or curr_idx == n:
+                return 0
+
+            if memo[curr_idx][cnt] != -1:
+                return memo[curr_idx][cnt]
+            
+            # Find the nearest available events after attending event 0.
+            next_idx = bisect_right(starts, events[curr_idx][1])
+            memo[curr_idx][cnt] = max(dp(curr_idx + 1, cnt), events[curr_idx][2] + dp(next_idx, cnt - 1))
+            return memo[curr_idx][cnt]
         
-        for curr_index in range(n - 1, -1, -1):
-            for count in range(1, k + 1):
-                next_index = bisect_right(starts, events[curr_index][1])
-                dp[count][curr_index] = max(dp[count][curr_index + 1], events[curr_index][2] + dp[count - 1][next_index])
-                
-        return dp[k][0]
+        
+        events.sort()
+        n = len(events)
+        starts = [start for start, end, val in events]
+        memo = [[-1] * (k + 1) for _ in range(n)]
+        
+        return dp(0, k)
