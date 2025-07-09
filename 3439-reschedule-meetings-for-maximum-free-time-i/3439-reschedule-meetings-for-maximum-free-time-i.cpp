66e11a857e1b7b716eee56a50de3a798b1fd2383
Andrew Kim <ksb0724@utexas.edu>
2025-07-09 23:56:17 +0900

    Time: 0 ms (100%), Space: 121.1 MB (91.9%) - LeetHub

--- 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.cpp
+++ 3439-reschedule-meetings-for-maximum-free-time-i/3439-reschedule-meetings-for-maximum-free-time-i.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
-        int n = startTime.size(), ans = 0;
-        vector<int> total(n + 1, 0);
+        int n = startTime.size(), ans = 0, t = 0;
 
-        for (int i = 0; i < n; ++i) total[i + 1] = total[i] + endTime[i] - startTime[i];
-
-        for (int i = k - 1; i < n; ++i){
-            int right = (i == n - 1)? eventTime : startTime[i + 1];
-            int left = (i == k - 1)? 0 : endTime[i - k];
-
-            ans = max(ans, right - left - (total[i + 1] - total[i - k + 1]));
+        for (int i = 0; i < n; ++i){
+            t += endTime[i] - startTime[i];
+            int right = i == n - 1? eventTime : startTime[i + 1];
+            int left = i <= k - 1? 0 : endTime[i - k];
+            
+            ans = max(ans, right - left - t);
+            if (i >= k - 1) t -= endTime[i - k + 1] - startTime[i - k + 1];
         }
+
         return ans;
     }
 };
