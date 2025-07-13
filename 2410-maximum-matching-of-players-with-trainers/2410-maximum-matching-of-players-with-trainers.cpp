d7812bde6c1d4043de2ce007c9d04723220ca363
Andrew Kim <ksb0724@utexas.edu>
2025-07-14 01:57:34 +0900

    Time: 25 ms (86.69%), Space: 80.2 MB (60.41%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.cpp
@@ -2,21 +2,20 @@ class Solution {
 public:
     int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
         sort(players.begin(), players.end());
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        int ans = 0;
+        sort(trainers.begin(), trainers.end());
 
-        for (int trainer : trainers) minHeap.push(trainer);
+        int m = players.size(), n = trainers.size(), ans = 0;
+        int i = 0, j = i; 
+        while (i < m && j < n){
+            while (j < n && players[i] > trainers[j])
+                ++j;
 
-        for (int player : players){
-            while (!minHeap.empty()){
-                int capacity = minHeap.top(); minHeap.pop();
-                if (capacity >= player){
-                    ++ans;
-                    break;
-                }
-            }
+            if (j < n) ++ans;
+
+            ++i;
+            ++j;
         }
-
-        return ans;  
+        
+        return ans;
     }
 };
