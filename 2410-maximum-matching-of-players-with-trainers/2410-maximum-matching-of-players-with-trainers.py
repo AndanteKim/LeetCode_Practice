a750589a1042a82f56ba6c8907074a7b47df734c
Andrew Kim <ksb0724@utexas.edu>
2025-07-13 23:39:33 +0900

    Time: 70 ms (85.26%), Space: 34.1 MB (28.68%) - LeetHub

--- 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
+++ 2410-maximum-matching-of-players-with-trainers/2410-maximum-matching-of-players-with-trainers.py
@@ -1,16 +1,17 @@
 class Solution:
     def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
         players.sort()
-        pq = []
-        for trainer in trainers:
-            heappush(pq, trainer)
+        trainers.sort()
+        m, n = len(players), len(trainers)
+        i = j = ans = 0
 
-        ans = 0
-        for player in players:
-            while pq:
-                capacity = heappop(pq)
-                if capacity >= player:
-                    ans += 1
-                    break
+        while i < m and j < n:
+            while j < n and players[i] > trainers[j]:
+                j += 1
+            
+            if j < n:
+                ans += 1
+            i += 1
+            j += 1
         
         return ans
