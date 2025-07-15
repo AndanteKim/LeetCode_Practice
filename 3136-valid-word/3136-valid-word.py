aa9f460c67a644c61067e49f4e1ca6f0abc518b3
Andrew Kim <ksb0724@utexas.edu>
2025-07-15 20:54:23 +0900

    Time: 0 ms (100%), Space: 17.7 MB (54.18%) - LeetHub

--- 3136-valid-word/3136-valid-word.py
+++ 3136-valid-word/3136-valid-word.py
@@ -1,22 +1,17 @@
 class Solution:
     def isValid(self, word: str) -> bool:
-        
         if len(word) < 3:
             return False
+        
+        has_vowel, has_consonant = False, False
 
-        chars, vowel_exist, consonant_exist = set(word), False, False
-        vowels_list = ('A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u')
-
-        for c in chars:
-            order = ord(c)
-            # If the character is not digits or a English letter
-            if not (ord('0') <= order <= ord('9') or 65 <= order <= 90 or 97 <= order <= 122):
+        for c in word:
+            if c.isalpha():
+                if c.lower() in 'aeiou':
+                    has_vowel = True
+                else:
+                    has_consonant = True
+            elif not c.isdigit():
                 return False
             
-            if c in vowels_list:
-                vowel_exist = True
-            
-            if (65 <= order <= 90 or 97 <= order <= 122) and c not in vowels_list:
-                consonant_exist = True
-        
-        return vowel_exist and consonant_exist
+        return has_vowel and has_consonant
