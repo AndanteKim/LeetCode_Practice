a8a58e458f7c4f0ad9f0db3fdae3f7a462dddfa5
Andrew Kim <ksb0724@utexas.edu>
2025-08-04 20:44:17 +0900

    Time: 45 ms (39.56%), Space: 75 MB (57.1%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        
-        // key : fruit type number, value: quantity of fruits for each type(within max 2 types)
-        unordered_map<int, int> basket_2;
-        int left = 0, quantity = 0;
-        
-        for (int right = 0; right < fruits.size(); ++right){
-            ++basket_2[fruits[right]];
-            
-            while (left < right && basket_2.size() > 2){
-                --basket_2[fruits[left]];
-                if (basket_2[fruits[left]] == 0) basket_2.erase(fruits[left]);
+        int left = 0, n = fruits.size(), ans = 0;
+        unordered_map<int, int> seen;
+        for (int right = 0; right < n; ++right){
+            while (seen.count(fruits[right]) == 0 && seen.size() >= 2){
+                --seen[fruits[left]];
+                if (seen[fruits[left]] == 0) seen.erase(fruits[left]);
                 ++left;
             }
-            
-            
-            quantity = max(quantity, right - left + 1);
+
+            ++seen[fruits[right]];
+            ans = max(ans, right - left + 1);
         }
-        
-        return quantity;
+
+        return ans;
     }
 };
