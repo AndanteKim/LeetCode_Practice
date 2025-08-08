83275911f2a46c35552db7eb726ae8bd1cafd87d
Andrew Kim <ksb0724@utexas.edu>
2025-08-09 08:27:28 +0900

    Time: 459 ms (5.16%), Space: 20 MB (48.39%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.py
+++ 0808-soup-servings/0808-soup-servings.py
@@ -1,11 +1,13 @@
 class Solution:
     def soupServings(self, n: int) -> float:
         m = ceil(n / 25)
-        dp = dict()
-        
-        def calculate_dp(i: int, j: int) -> float:
-            return 0.25 * (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] + dp[max(0, i - 2)][max(0, j - 2)] + dp[i - 1][max(0, j - 3)])
-        
+        dp = {}
+
+        def calculate_dp(i, j):
+            return (dp[max(0, i - 4)][j] + dp[max(0, i - 3)][j - 1] +
+                    dp[max(0, i - 2)][max(0, j - 2)]
+                    + dp[i - 1][max(0, j - 3)]) / 4
+
         dp[0] = {0: 0.5}
         for k in range(1, m + 1):
             dp[0][k] = 1
@@ -14,6 +16,5 @@ class Solution:
                 dp[j][k] = calculate_dp(j, k)
                 dp[k][j] = calculate_dp(k, j)
             if dp[k][k] > 1 - 1e-5:
-                return 1.0
+                return 1
         return dp[m][m]
-        
