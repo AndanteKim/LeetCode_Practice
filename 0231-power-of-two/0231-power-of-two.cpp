6220b6c8640e6c6d523d0b4028cec8071e35b8ba
Andrew Kim <ksb0724@utexas.edu>
2025-08-09 20:05:56 +0900

    Time: 0 ms (100%), Space: 7.9 MB (17.71%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if (n == 0) return false;
-        long x = n;
-        return (x & (x - 1)) == 0;
+        long curr = 1;
+
+        while (curr <= n){
+            if (curr == n) return true;
+            curr <<= 1;
+        }
+
+        return false;
     }
 };
