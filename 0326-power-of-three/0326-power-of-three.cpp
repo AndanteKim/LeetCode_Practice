a29f738dade950832b6a75e82d773c6de01e456a
Andrew Kim <ksb0724@utexas.edu>
2025-08-14 02:09:20 +0900

    Time: 11 ms (7.8%), Space: 9.2 MB (5.34%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        // Base case
         if (n <= 0) return false;
 
-        while (n % 3 == 0) n /= 3;
-
-        return n == 1;
+        int x = log2(n) / log2(3);
+        return pow(3, x) == n;
     }
 };
