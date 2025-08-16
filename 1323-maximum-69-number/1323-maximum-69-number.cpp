32617365f81f80d873feec410fc90503987fd857
Andrew Kim <ksb0724@utexas.edu>
2025-08-16 18:16:17 +0900

    Time: 0 ms (100%), Space: 8 MB (71.54%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.cpp
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        vector<int> digits;
-        int n = num;
+        // Since we start with the lowest digit, initialize currDigit = 0.
+        int numCopy = num, indexFirstSix = -1, currDigit = 0;
 
-        while (n > 0){
-            digits.push_back(n % 10);
-            n /= 10;
+        // Check every digit of 'numCopy' from low to high.
+        while (numCopy > 0){
+            // If the current digit is '6', record it as the highest digit of 6.
+            if (numCopy % 10 == 6) indexFirstSix = currDigit;
+
+            // Move on to the next digit.
+            numCopy /= 10;
+            ++currDigit;
         }
 
-        reverse(digits.begin(), digits.end());
-        int digit = digits.size() - 1, ans = 0;
-        bool changeable = true;
-
-        for (int d : digits){
-            if (changeable && d != 9){
-                changeable = false;
-                ans += 9 * pow(10, digit--);
-            }
-            else
-                ans += d * pow(10, digit--);
-        }
-
-        return ans;
+        // If we don't find any digit of '6', return the original number,
+        // otherwise, increment 'num' by the difference made by the first '6'.
+        // There might be precision loss in pow function if 'indexFirstSix' is large,
+        // we can write a for loop to avoid the precision loss.
+        return indexFirstSix == -1? num : num + 3 * (int)pow(10, indexFirstSix);
     }
 };
