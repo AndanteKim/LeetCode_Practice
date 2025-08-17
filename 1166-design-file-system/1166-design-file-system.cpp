1b6c842a4c0f69571ad754da5dd3450eb3bee7c6
Andrew Kim <ksb0724@utexas.edu>
2025-08-17 19:10:01 +0900

    Time: 43 ms (59.42%), Space: 71.9 MB (72.68%) - LeetHub

--- 1166-design-file-system/1166-design-file-system.cpp
+++ 1166-design-file-system/1166-design-file-system.cpp
@@ -6,12 +6,14 @@ public:
     FileSystem() {}
     
     bool createPath(string path, int value) {
+        // Step-1: Basic path validations
         if (path == "/" || path.size() == 0 || paths.contains(path)) return false;
 
+        // Step-2: If the parent doesn't exist. Note that "/" is a valid parent.
         string parent = path.substr(0, path.rfind('/'));
-
         if (parent.size() > 1 && !paths.contains(parent)) return false;
 
+        // Step-3: add this new path and return true.
         paths[path] = value;
         return true;
     }
