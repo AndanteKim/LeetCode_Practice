8ab4bf83b8041ebb7f29a6c38a31ac26c54e99fe
Andrew Kim <ksb0724@utexas.edu>
2025-08-21 03:18:13 +0900

    Time: 0 ms (100%), Space: 27.8 MB (86.7%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -1,28 +1,20 @@
 class Solution {
-private:
-    int row, col;
-    int solve(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp){
-        if (i >= row || j >= col) return 0;
-        if (matrix[i][j] == 0) return 0;
-
-        // Memoized
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int right = solve(i, j + 1, matrix, dp), diagonal = solve(i + 1, j + 1, matrix, dp), below = solve(i + 1, j, matrix, dp);
-        dp[i][j] = min(right, min(diagonal, below)) + 1;
-        
-        return dp[i][j];
-    }
-
 public:
     int countSquares(vector<vector<int>>& matrix) {
-        int ans = 0;
-        row = matrix.size(), col = matrix[0].size();
-        vector dp(row, vector<int>(col, -1));
+        int ans = 0, row = matrix.size(), col = matrix[0].size();
+        int prev = 0;
+        vector<int> dp(col + 1);
 
-        for (int i = 0; i < row; ++i){
-            for (int j = 0; j < col; ++j)
-                ans += solve(i, j, matrix, dp);
+        for (int i = 1; i <= row; ++i){
+            for (int j = 1; j <= col; ++j){
+                if (matrix[i - 1][j - 1]){
+                    int temp = dp[j];
+                    dp[j] = min(prev, min(dp[j], dp[j - 1])) + 1;
+                    prev = temp;
+                    ans += dp[j];
+                }
+                else dp[j] = 0;
+            }
         }
 
         return ans;
