8b30f1609b3768cb305d930d14ec26ebdf1e20d4
Andrew Kim <ksb0724@utexas.edu>
2025-08-29 23:08:43 +0900

    Time: 0 ms (100%), Space: 17.6 MB (87.5%) - LeetHub

--- 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.py
@@ -1,3 +1,6 @@
 class Solution:
     def flowerGame(self, n: int, m: int) -> int:
-        return (n * m) >> 1
+        n_even, m_even = n >> 1, m >> 1
+        n_odd, m_odd = n - n_even, m - m_even
+
+        return n_even * m_odd + n_odd * m_even
