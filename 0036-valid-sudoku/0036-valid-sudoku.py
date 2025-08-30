c5f20085ad05b64c1ab2294fd5bdfdad83de8952
Andrew Kim <ksb0724@utexas.edu>
2025-08-30 20:10:59 +0900

    Time: 1 ms (88.77%), Space: 18 MB (24.98%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,32 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        n = 9
-        # Use binary number to check previous occurrence
-        rows, cols, boxes = [0] * n, [0] * n, [0] * n
-        
-        for i in range(n):
+        rows, cols = defaultdict(set), defaultdict(set)
+        m, n = len(board), len(board[0])
+
+        for i in range(m):
             for j in range(n):
-                # Check if the position is filled with number
-                if board[i][j] == '.':
-                    continue
-                    
-                pos = int(board[i][j]) - 1
-                
-                # Check the row
-                if rows[i] & (1 << pos):
-                    return False
-                rows[i] |= (1 << pos)
-                
-                # Check the col
-                if cols[j] & (1 << pos):
-                    return False
-                cols[j] |= (1 << pos)
-                
-                # Check the box
-                boxes_idx = (i // 3) * 3 + (j // 3)
-                
-                if boxes[boxes_idx] & (1 << pos):
-                    return False
-                boxes[boxes_idx] |= (1 << pos)
-                    
+                if board[i][j] != '.':
+                    if board[i][j] in rows[i] or board[i][j] in cols[j]:
+                        return False
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+
+        for curr_cube in range(9):
+            start_row, start_col, seen = (curr_cube // 3) * 3, (3 * curr_cube) % 9, set()
+            for row in range(start_row, start_row + 3):
+                for col in range(start_col, start_col + 3):
+                    if board[row][col] != '.':
+                        if board[row][col] in seen:
+                            return False
+                        seen.add(board[row][col])
+
         return True
