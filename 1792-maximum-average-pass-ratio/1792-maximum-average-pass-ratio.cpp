e8d89ab0a32f6679ce3fe6455f4ba5f3cb4cdf65
Andrew Kim <ksb0724@utexas.edu>
2025-09-01 22:15:10 +0900

    Time: 369 ms (29.82%), Space: 99.2 MB (28.95%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -1,35 +1,31 @@
 class Solution {
-private:
-    // Lambda to calculate the gain of adding an extra student
-    double calculateGain(int passes, int totalStudents){
-        return (double)(passes + 1) / (totalStudents + 1) - ((double)passes / totalStudents);
-    }
-    
 public:
     double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
-        // Max heap to store (-gain, passes, total_students)
-        priority_queue<tuple<double, int, int>> maxHeap;
-        int n = classes.size();
+        int n = classes.size(), numerator, denominator;
+        double ratio, nextRatio;
+        priority_queue<tuple<double, int, int>, vector<tuple<double, int, int>>> minHeap;
+
         for (int i = 0; i < n; ++i){
-            int passes = classes[i][0], totalStudents = classes[i][1];
-            double gain = calculateGain(passes, totalStudents);
-            maxHeap.push({gain, passes, totalStudents});
+            numerator = classes[i][0], denominator = classes[i][1];
+            nextRatio = (double)(numerator + 1) / (denominator + 1);
+            ratio = (double)numerator / denominator;
+            minHeap.push({nextRatio - ratio, numerator, denominator});
         }
-        
-        // Distribute extra students
+
         for (int i = 0; i < extraStudents; ++i){
-            auto [_, passes, totalStudents] = maxHeap.top(); maxHeap.pop();
-            double gain = calculateGain(passes + 1, totalStudents + 1);
-            maxHeap.push({gain, passes + 1, totalStudents + 1});
+            auto [_, numerator, denominator] = minHeap.top(); minHeap.pop();
+            ++numerator, ++denominator;
+            nextRatio = (double)(numerator + 1) / (denominator + 1);
+            ratio = (double)numerator / denominator;
+            minHeap.push({nextRatio - ratio, numerator, denominator});
         }
-        
-        // Calculate the final average pass ratio
-        double totalPassRatio = 0.0;
-        while (!maxHeap.empty()){
-            auto [_, passes, totalStudents] = maxHeap.top(); maxHeap.pop();
-            totalPassRatio += (double)passes / totalStudents;
+
+        double totalAverage = 0.0;
+        while (!minHeap.empty()){
+            auto [_, numerator, denominator] = minHeap.top(); minHeap.pop();
+            totalAverage += (double)numerator / denominator;
         }
-        
-        return totalPassRatio / n;
+
+        return totalAverage / n;
     }
 };
