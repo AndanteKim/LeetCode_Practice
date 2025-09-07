0d0555480515f8e7771ee16e6aea93bf2f940e7c
Andrew Kim <ksb0724@utexas.edu>
2025-09-07 11:42:57 +0900

    Time: 0 ms (100%), Space: 9.8 MB (13.58%) - LeetHub

---
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int> ans;
+        for (int i = 1; i <= (n >> 1); ++i){
+            ans.push_back(i);
+            ans.push_back(-i);
+        }
+
+        if (n % 2) ans.push_back(0);
+
+        return ans;
+    }
+};
