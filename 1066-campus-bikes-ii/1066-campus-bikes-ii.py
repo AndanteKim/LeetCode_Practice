e6e3709b9c1fd3ffdc4ea2cd2564bc9042587975
Andrew Kim <ksb0724@utexas.edu>
2025-09-07 23:48:25 +0900

    Time: 8499 ms (5.07%), Space: 17.8 MB (91.47%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.py
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.py
@@ -1,45 +1,25 @@
 class Solution:
-    # Manhattan Distance
-    def find_distance(self, worker: List[int], bike: List[int]) -> int:
-        return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1]);
-    
-    # Count the number of ones using Brian Kernighan's Algorithm
-    def count_num_of_ones(self, mask: int) -> int:
-        count = 0
-        while mask != 0:
-            mask &= mask - 1
-            count += 1
-        return count
-    
     def assignBikes(self, workers: List[List[int]], bikes: List[List[int]]) -> int:
-        m, n = len(bikes), len(workers)
-        pq, visited = [], set()
-        heappush(pq, (0, 0))
+        # Manhattan distance
+        def find_dist(worker: List[int], bike: List[int]) -> int:
+            return abs(worker[0] - bike[0]) + abs(worker[1] - bike[1])
+
+        def min_dist_sum(worker_idx: int, curr_dist_sum: int) -> None:
+            if worker_idx >= m:
+                self.ans = min(self.ans, curr_dist_sum)
+                return
+            
+            # If the current distance sum is greater than the smallest result
+            # foudn then stop exploring this combination of workers and bikes
+            if curr_dist_sum >= self.ans:
+                return
+            
+            for bike_idx in range(n):
+                if not visited[bike_idx]:
+                    visited[bike_idx] = True
+                    min_dist_sum(worker_idx + 1, curr_dist_sum + find_dist(workers[worker_idx], bikes[bike_idx]))
+                    visited[bike_idx] = False
         
-        while pq:
-            current_dist_sum, current_mask = heappop(pq)
-            
-            # Continue if the mask is already traversed
-            if current_mask in visited:
-                continue
-                
-            # Marking the mask as visited
-            visited.add(current_mask)
-            # Next worker index would be equal to the number of 1's in curent_mask
-            worker_index = self.count_num_of_ones(current_mask)
-            
-            # Return the current distance sum if all workers are covered
-            if worker_index == n:
-                return current_dist_sum
-            
-            for bike_index in range(m):
-                # Checking if the bike at bike_index has been assigned or not
-                if (current_mask & (1 << bike_index)) == 0:
-                    next_state_distance_sum = current_dist_sum + self.find_distance(workers[worker_index], bikes[bike_index])
-                    
-                    # Put the next state pair into the priority queue
-                    next_state_mask = current_mask | (1 << bike_index)
-                    heappush(pq, (next_state_distance_sum, next_state_mask))
-       
-        # This statement will never be executed provided there is at least one bike per worker
-        return -1;
+        self.ans, visited, m, n = float('inf'), [0] * 10, len(workers), len(bikes)
+        min_dist_sum(0, 0)
+        return self.ans
