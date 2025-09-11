0112984f2cd1349a62f0e80fb26489205e48fc17
Andrew Kim <ksb0724@utexas.edu>
2025-09-12 08:23:57 +0900

    Time: 135 ms (20.42%), Space: 20 MB (84.42%) - LeetHub

--- 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
+++ 2785-sort-vowels-in-a-string/2785-sort-vowels-in-a-string.py
@@ -1,25 +1,15 @@
 class Solution:
     def sortVowels(self, s: str) -> str:
-        def is_vowel(ch: chr) -> bool:
-            return ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U' or\
-        ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u'
-        
-        count = defaultdict(int)
+        min_heap, vowels = [], "aeiouAEIOU"
+
         for c in s:
-            if is_vowel(c):
-                count[c] += 1
-        
-        sorted_vowel = "AEIOUaeiou"
-        ans, j = "", 0
-        
-        for i in range(len(s)):
-            if is_vowel(s[i]):
-                while count[sorted_vowel[j]] == 0:
-                    j += 1
-                ans += sorted_vowel[j]
-                count[sorted_vowel[j]] -= 1
-        
-            else:
-                ans += s[i]
-        
-        return ans
+            if c in vowels:
+                heappush(min_heap, c)
+
+        arr, n = [c for c in s], len(s)
+
+        for i in range(n):
+            if arr[i] in vowels:
+                arr[i] = heappop(min_heap)
+
+        return "".join(arr)
