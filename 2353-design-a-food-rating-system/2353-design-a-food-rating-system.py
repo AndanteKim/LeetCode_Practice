9b9df09791cad76507a374b0698a867c5dda5bc6
Andrew Kim <ksb0724@utexas.edu>
2025-09-17 22:57:05 +0900

    Time: 274 ms (24.27%), Space: 51.4 MB (72.56%) - LeetHub

--- 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
+++ 2353-design-a-food-rating-system/2353-design-a-food-rating-system.py
@@ -1,43 +1,19 @@
-from sortedcontainers import SortedSet
-
 class FoodRatings:
 
     def __init__(self, foods: List[str], cuisines: List[str], ratings: List[int]):
-        # Map food with its rating
-        self.food_rating_map = dict()
-        
-        # Map food with cuisine it belongs to.
-        self.food_cuisine_map = dict()
-        
-        # Store all food of a cuisine in a set (to sort them on ratings/name)
-        self.cuisine_food_map = defaultdict(SortedSet)
-        
-        for i in range(len(foods)):
-            # Store 'rating' and 'cuisine' of the current 'food' in 'food_rating_map' and 'food_cuisine_map' maps
-            self.food_rating_map[foods[i]] = ratings[i]
-            self.food_cuisine_map[foods[i]] = cuisines[i]
-            
-            # Insert the '(-1 * rating, name)' element in the current cuisine's set.
-            self.cuisine_food_map[cuisines[i]].add((-ratings[i], foods[i]))
+        self.food_data, self.cuisine_data = dict(), defaultdict(SortedList)
+        for food, cuisine, rating in zip(foods, cuisines, ratings):
+            self.food_data[food] = (cuisine, rating)
+            self.cuisine_data[cuisine].add((-rating, food))
 
     def changeRating(self, food: str, newRating: int) -> None:
-        # Fetch cuisine name for food
-        cuisine_name = self.food_cuisine_map[food]
-        
-        # Find and delete the element from the respective cuisine's set
-        old_element = (-self.food_rating_map[food], food)
-        self.cuisine_food_map[cuisine_name].remove(old_element)
-        
-        # Update food's rating in 'food_rating' map
-        self.food_rating_map[food] = newRating
-        
-        # Insert the '(-1 * new rating, name)' element in the respective cuisine's set.
-        self.cuisine_food_map[cuisine_name].add((-newRating, food))
+        cuisine, rating = self.food_data[food]
+        self.food_data[food] = cuisine, newRating
+        self.cuisine_data[cuisine].remove((-rating, food))
+        self.cuisine_data[cuisine].add((-newRating, food))
 
     def highestRated(self, cuisine: str) -> str:
-        highest_rated = self.cuisine_food_map[cuisine][0]
-        # Return name of the highest-rated 'food' of 'cuisine'
-        return highest_rated[1]
+        return self.cuisine_data[cuisine][0][1]
 
 
 # Your FoodRatings object will be instantiated and called as such:
