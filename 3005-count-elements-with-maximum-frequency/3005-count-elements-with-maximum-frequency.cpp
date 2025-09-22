0c0c889354b6988e52b841d491e637f0b0cd1ccf
Andrew Kim <ksb0724@utexas.edu>
2025-09-22 22:24:11 +0900

    Time: 0 ms (100%), Space: 23.3 MB (9.87%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        unordered_map<int, int> freqs;
-        
-        int ans = 0, maxFreq = 0, freq;
-        for (int num:nums){
-            ++freqs[num];
-            freq = freqs[num];
-            
-            // If we discover a higher frequency element
-            // Update mx_freq
-            // Reset ans to the new maxFreq
-            if (freq > maxFreq){
-                maxFreq = freq;
-                ans = freq;
-            }
-            // If we find an element with the max frequency, add it to the total
-            else if (freq == maxFreq)
-                ans += freq;
+        int ans = 0, maxFreq = 0;
+        unordered_map<int, int> freq;
+
+        for (int num : nums){
+            ++freq[num];
+            maxFreq = max(maxFreq, freq[num]);
         }
+
+        for (const auto& [_, f] : freq)
+            if (maxFreq == f) ans += f;
         
         return ans;
     }
