7647c3fd19a6e76dddf86ff8e4fe69a56f3abb1e
Andrew Kim <ksb0724@utexas.edu>
2025-09-24 08:41:50 +0900

    Time: 0 ms (100%), Space: 18 MB (17.18%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.py
+++ 0165-compare-version-numbers/0165-compare-version-numbers.py
@@ -1,14 +1,15 @@
 class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
-        v1, v2 = version1.split("."), version2.split(".")
-        n1, n2 = len(v1), len(v2)
-        
+        nums1 = version1.split(".")
+        nums2 = version2.split(".")
+        n1, n2 = len(nums1), len(nums2)
+
+        # compare versions
         for i in range(max(n1, n2)):
-            i1 = int(v1[i]) if i < n1 else 0
-            i2 = int(v2[i]) if i < n2 else 0
-            
+            i1 = int(nums1[i]) if i < n1 else 0
+            i2 = int(nums2[i]) if i < n2 else 0
             if i1 != i2:
                 return 1 if i1 > i2 else -1
-        
+
+        # The versions are equal
         return 0
-        
