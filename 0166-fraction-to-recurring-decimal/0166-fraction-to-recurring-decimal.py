12bc0ea5258ec3d6e76508f9ad7f38d1af80d6b5
Andrew Kim <ksb0724@utexas.edu>
2025-09-24 21:20:01 +0900

    Time: 1 ms (19.65%), Space: 17.9 MB (90.77%) - LeetHub

---
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
@@ -0,0 +1,31 @@
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if numerator == 0:
+            return "0"
+
+        fraction = []
+        if (numerator < 0) != (denominator < 0):
+            fraction.append('-')
+
+        dividend, divisor = abs(numerator), abs(denominator)
+
+        fraction.append(str(dividend // divisor))
+        remainder = dividend % divisor
+        if remainder == 0:
+            return "".join(fraction)
+        
+        fraction.append('.')
+        lookup = dict()
+
+        while remainder != 0:
+            if remainder in lookup:
+                fraction.insert(lookup[remainder], '(')
+                fraction.append(')')
+                break
+            
+            lookup[remainder] = len(fraction)
+            remainder *= 10
+            fraction.append(str(remainder // divisor))
+            remainder %= divisor
+        
+        return "".join(fraction)
