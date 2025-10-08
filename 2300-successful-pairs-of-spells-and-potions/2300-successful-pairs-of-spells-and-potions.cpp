9748bf106379e124119ce58eeea3d015bf924c93
Andrew Kim <ksb0724@utexas.edu>
2025-10-08 16:40:46 +0900

    Time: 45 ms (47.82%), Space: 129.4 MB (5.42%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.cpp
@@ -1,21 +1,13 @@
 class Solution {
 public:
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        vector<pair<int, int>> sortedSpells;
-        int n = spells.size(), m = potions.size();
-        for (int i = 0; i < n; ++i)
-            sortedSpells.push_back({spells[i], i});
-        
-        sort(sortedSpells.begin(), sortedSpells.end());
+        vector<int> ans(spells.size());
+        vector<long long> multiple(potions.size());
         sort(potions.begin(), potions.end());
-        
-        int pointIndex = m - 1;
-        vector<int> ans(n);
-        for (auto& [spell, i] : sortedSpells){
-            while (pointIndex >= 0 && success <= (long long) spell * potions[pointIndex]){
-                --pointIndex;
-            }
-            ans[i] = m - (pointIndex + 1);
+
+        for (int i = 0; i < spells.size(); ++i){
+            int count = (int)(potions.size()) - static_cast<int>(lower_bound(potions.begin(), potions.end(), (double)success / spells[i]) - potions.begin()); 
+            ans[i] = count;
         }
         
         return ans;
