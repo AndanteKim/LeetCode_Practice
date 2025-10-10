890530f00f6aecd77feb5f56fc0630167305e566
Andrew Kim <ksb0724@utexas.edu>
2025-10-11 00:19:59 +0900

    Time: 1220 ms (23.75%), Space: 60.7 MB (11.25%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.py
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.py
@@ -1,11 +1,16 @@
 class Solution:
     def maximumEnergy(self, energy: List[int], k: int) -> int:
-        @lru_cache(maxsize = None)
-        def dp(i: int) -> int:
+        #@lru_cache(maxsize = None)
+        def dp(i: int, memo: List[int]) -> int:
             if i >= n:
                 return 0
             
-            return energy[i] + dp(i + k)
+            if memo[i] != -1:
+                return memo[i]
+
+            memo[i] = energy[i] + dp(i + k, memo)
+            return memo[i]
 
         n = len(energy)
-        return max(dp(i) for i in range(n)) 
+        memo = [-1] * n
+        return max(dp(i, memo) for i in range(n)) 
