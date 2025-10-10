2f66d54b049ddd3a6abde46e940e9303307f0923
Andrew Kim <ksb0724@utexas.edu>
2025-10-11 00:48:06 +0900

    Time: 135 ms (75.35%), Space: 145.5 MB (77.46%) - LeetHub

--- 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -1,26 +1,16 @@
 class Solution {
-private:
-    int n, k;
-
-    int dp(int index, vector<int>& energy, vector<int>& memo){
-        if (index >= n) return 0;
-
-        if (memo[index] != -1) return memo[index];
-
-        return memo[index] = energy[index] + dp(index + k, energy, memo);
-    }
-
 public:
     int maximumEnergy(vector<int>& energy, int k) {
-        this -> k = k;
-        this -> n = energy.size();
-        int maxEnergy = std::numeric_limits<int>::min();
-        vector<int> memo(n, -1);
+        int ans = std::numeric_limits<int>::min(), n = energy.size(), total;
 
-        for (int i = 0; i < n; ++i){
-            maxEnergy = max(maxEnergy, dp(i, energy, memo));
+        for (int i = n - k; i < n; ++i){
+            total = 0;
+            for (int j = i; j >= 0; j -= k){
+                total += energy[j];
+                ans = max(ans, total);
+            }
         }
 
-        return maxEnergy;
+        return ans;
     }
 };
