2cae9b15f43a8b7a9673a9d512ed5b71152a7a12
Andrew Kim <ksb0724@utexas.edu>
2025-11-08 19:10:13 +0900

    Time: 67 ms (65.43%), Space: 34.8 MB (44.44%) - LeetHub

---
+++ LeetCode/Easy/0170-two-sum-iii-data-structure-design/0170-two-sum-iii-data-structure-design-11-08-2025-19-10-15.cpp
@@ -0,0 +1,33 @@
+class TwoSum {
+private:
+    unordered_map<long, int> freqs;
+    unordered_set<int> cache;
+
+public:
+    TwoSum() {}
+    
+    void add(int number) {
+        ++freqs[number];
+    }
+    
+    bool find(int value) {
+        if (cache.find(value) != cache.end()) return true;
+
+        for (const auto& [elem, f] : freqs){
+            if (freqs.find((long)value - elem) != freqs.end())
+                if (elem != (value >> 1) || (elem == (value >> 1) && freqs[elem] > 1)){
+                    cache.insert(value);
+                    return true;
+                }
+        }
+
+        return false;
+    }
+};
+
+/**
+ * Your TwoSum object will be instantiated and called as such:
+ * TwoSum* obj = new TwoSum();
+ * obj->add(number);
+ * bool param_2 = obj->find(value);
+ */
