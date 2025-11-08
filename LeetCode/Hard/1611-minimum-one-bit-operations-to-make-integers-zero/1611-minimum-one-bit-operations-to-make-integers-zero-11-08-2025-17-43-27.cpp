7decb7b259ab27f75f6c39f7782d878d1aa13256
Andrew Kim <ksb0724@utexas.edu>
2025-11-08 17:43:26 +0900

    Time: 0 ms (100%), Space: 8 MB (55.27%) - LeetHub

---
+++ LeetCode/Hard/1611-minimum-one-bit-operations-to-make-integers-zero/1611-minimum-one-bit-operations-to-make-integers-zero-11-08-2025-17-43-27.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        int ans = n;
+
+        for (int bin = 16; bin >= 1; bin /= 2) {
+            ans ^= (ans >> bin);
+        }
+
+        return ans;
+    }
+};
