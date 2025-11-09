27c546bf98528117ad2ec72f8cd6506c909959b5
Andrew Kim <ksb0724@utexas.edu>
2025-11-09 10:58:00 +0900

    Time: 3 ms (13.72%), Space: 7.8 MB (71.74%) - LeetHub

---
+++ LeetCode/Easy/2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero-11-09-2025-10-58-00.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int ops = 0, n1 = num1, n2 = num2;
+
+        while (n1 != 0 and n2 != 0) {
+            if (n1 >= n2) n1 -= n2;
+            else n2 -= n1;
+
+            ++ops;
+        }
+
+        return ops;
+    }
+};
