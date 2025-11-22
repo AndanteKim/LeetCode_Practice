5f3c5ec1e047e8748602938cf98c2b1b1aa5f399
Andrew Kim <ksb0724@utexas.edu>
2025-11-22 21:10:29 +0900

    Time: 0 ms (100%), Space: 17.9 MB (66.12%) - LeetHub

---
+++ LeetCode/Medium/0364-nested-list-weight-sum-ii/0364-nested-list-weight-sum-ii-11-22-2025-21-10-29.py
@@ -0,0 +1,67 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        q = deque([nested_int for nested_int in nestedList])
+
+        max_depth, depth = 0, 1
+        sum_elems, sum_prods = 0, 0
+
+        while q:
+            sz = len(q)
+            max_depth = max(max_depth, depth)
+
+            for i in range(sz):
+                nested = q.popleft()
+
+                if nested.isInteger():
+                    sum_elems += nested.getInteger()
+                    sum_prods += nested.getInteger() * depth
+                else:
+                    for nxt in nested.getList():
+                        q.append(nxt)
+
+            depth += 1
+        
+        return (max_depth + 1) * sum_elems - sum_prods
