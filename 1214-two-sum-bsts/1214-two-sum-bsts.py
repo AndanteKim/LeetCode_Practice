803450924c63f838707c4d050af7e8ab231d841b
Andrew Kim <ksb0724@utexas.edu>
2025-12-03 01:14:12 +0900

    Time: 6 ms (55.43%), Space: 21.2 MB (26.29%) - LeetHub

--- 1214-two-sum-bsts/1214-two-sum-bsts.py
+++ 1214-two-sum-bsts/1214-two-sum-bsts.py
@@ -5,53 +5,21 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def morris_traversal(self, root: Optional[TreeNode]) -> Iterator[int]:
-        current = root
-        while current:
-            if not current.left:
-                yield current.val
-                current = current.right
-            else:
-                pre = current.left
-                while pre.right and pre.right != current:
-                    pre = pre.right
-                if not pre.right:
-                    pre.right = current
-                    current = current.left
-                else:
-                    pre.right = None
-                    yield current.val
-                    current = current.right
-                    
-    def reversed_morris_traversal(self, root: Optional[TreeNode]) -> Iterator[int]:
-        current = root
-        while current:
-            if not current.right:
-                yield current.val
-                current = current.left
-            else:
-                pre = current.right
-                while pre.left and pre.left != current:
-                    pre = pre.left
-                if not pre.left:
-                    pre.left = current
-                    current = current.right
-                else:
-                    pre.left = None
-                    yield current.val
-                    current = current.left
-    
     def twoSumBSTs(self, root1: Optional[TreeNode], root2: Optional[TreeNode], target: int) -> bool:
-        iterator1 = self.morris_traversal(root1)
-        iterator2 = self.reversed_morris_traversal(root2)
-        val1, val2 = next(iterator1, None), next(iterator2, None)
+        def inorder(node: Optional[TreeNode], seen: Set[int]) -> None:
+            if not node:
+                return
+            
+            inorder(node.left, seen)
+            seen.add(node.val)
+            inorder(node.right, seen)
         
-        while val1 != None and val2 != None:
-            if val1 + val2 == target:
+        seen1, seen2 = set(), set()
+        inorder(root1, seen1)
+        inorder(root2, seen2)
+
+        for node1 in seen1:
+            if target - node1 in seen2:
                 return True
-            elif val1 + val2 < target:
-                val1 = next(iterator1, None)
-            else:
-                val2 = next(iterator2, None)
-        
+
         return False
