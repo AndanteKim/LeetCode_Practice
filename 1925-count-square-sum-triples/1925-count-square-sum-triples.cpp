1456e950dd6f1d0b8aa9d9d6c2533dd128c0affc
Andrew Kim <ksb0724@utexas.edu>
2025-12-09 08:58:30 +0900

    Time: 7 ms (62.34%), Space: 7.7 MB (84.89%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countTriples(int n) {
+        int ans = 0;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < n; ++j) {
+                double sum = sqrt(i * i + j * j);
+                if ((int)sum == sum && sum * sum <= n * n) ++ans;
+            }
+        }
+
+        return ans;
+    }
+};
