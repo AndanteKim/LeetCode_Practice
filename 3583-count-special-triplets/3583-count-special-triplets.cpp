81f53348711abcea212e4a60ee5c6cc45f294950
Andrew Kim <ksb0724@utexas.edu>
2025-12-09 23:34:43 +0900

    Time: 528 ms (82.48%), Space: 362.7 MB (71.9%) - LeetHub

--- 3583-count-special-triplets/3583-count-special-triplets.cpp
+++ 3583-count-special-triplets/3583-count-special-triplets.cpp
@@ -1,18 +1,33 @@
 class Solution {
+private:
+    pair<int, int> upperBound(vector<int>& arr, int i) {
+        int mid, left = 0, right = arr.size() - 1;
+
+        while (left < right) {
+            mid = left + ((right - left + 1) >> 1);
+            if (i >= arr[mid]) left = mid;
+            else right = mid - 1;
+        }
+
+        return pair<int, int>{left + 1, arr.size() - left - 1};
+    }
+
 public:
     int specialTriplets(vector<int>& nums) {
-        int ans = 0, mod = 1'000'000'007;
-        unordered_map<int, int> numsCnt, numsPartialCnt;
+        int mod = 1'000'000'007, ans = 0, n = nums.size();
+        unordered_map<int, vector<int>> pos;
+        for (int i = 0; i < n; ++i) pos[nums[i]].push_back(i);
 
-        for (int num : nums) ++numsCnt[num];
+        int target;
+        for (int i = 1; i < n - 1; ++i) {
+            target = nums[i] * 2;
+            if (pos.count(target) && (pos[target].size() > 1) && (pos[target][0] < i)) {
+                auto [l, r] = upperBound(pos[target], i);
 
-        int target, leftCnt, rightCnt;
-        for (int num : nums) {
-            target = num * 2;
-            leftCnt = numsPartialCnt[target];
-            ++numsPartialCnt[num];
-            rightCnt = numsCnt[target] - numsPartialCnt[target];
-            ans = static_cast<int>((ans + (long long)leftCnt * rightCnt) % mod);
+                if (nums[i] == 0)
+                    --l;
+                ans = static_cast<int>((ans + (long long)l * r) % mod);
+            }
         }
 
         return ans;
