161ba84130c04ff7f624f09c658029a61fdd92b4
Andrew Kim <ksb0724@utexas.edu>
2026-01-01 09:29:00 +0900

    Time: 0 ms (100%), Space: 11.5 MB (77.72%) - LeetHub

---
+++ LeetCode/Easy/0066-plus-one/0066-plus-one-01-01-2026-09-28-59.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int carry = 1;
+
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            int digit = digits[i] + carry;
+            digits[i] = digit % 10;
+            carry = digit / 10;
+        }
+
+        if (carry) digits.insert(digits.begin(), carry); 
+
+        return digits;
+    }
+};
