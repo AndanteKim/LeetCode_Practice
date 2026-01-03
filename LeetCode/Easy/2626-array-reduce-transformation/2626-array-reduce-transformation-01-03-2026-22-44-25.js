edb2ee9f43a6e5549dec92e35ce5ecb7196de4ab
Andrew Kim <ksb0724@utexas.edu>
2026-01-03 22:44:26 +0900

    Time: 42 ms (83.36%), Space: 56.1 MB (40.98%) - LeetHub

---
+++ LeetCode/Easy/2626-array-reduce-transformation/2626-array-reduce-transformation-01-03-2026-22-44-25.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if (nums.length == 0) return init;
+    var ans = init;
+    for (let i = 0; i < nums.length; ++i) {
+        ans = fn(ans, nums[i]);
+    }
+
+    return ans;
+};
