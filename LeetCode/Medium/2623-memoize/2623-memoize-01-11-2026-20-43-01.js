ebf37921584591753b6fe5d44650678d18ec5e75
Andrew Kim <ksb0724@utexas.edu>
2026-01-11 20:43:02 +0900

    Time: 262 ms (60.64%), Space: 93.8 MB (94.5%) - LeetHub

---
+++ LeetCode/Medium/2623-memoize/2623-memoize-01-11-2026-20-43-01.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+        const key = JSON.stringify(args);
+        if (key in cache) {
+            return cache[key];
+        }
+
+        const funcOutput = fn(...args);
+        cache[key] = funcOutput;
+        return funcOutput;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
