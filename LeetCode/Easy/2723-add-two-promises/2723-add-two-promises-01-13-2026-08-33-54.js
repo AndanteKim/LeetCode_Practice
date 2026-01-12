fddf991554ee1fa32cd8abb63553f1b6bf4123d8
Andrew Kim <ksb0724@utexas.edu>
2026-01-13 08:33:54 +0900

    Time: 57 ms (44.91%), Space: 54.5 MB (54.78%) - LeetHub

---
+++ LeetCode/Easy/2723-add-two-promises/2723-add-two-promises-01-13-2026-08-33-54.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    try {
+        const [res1, res2] = await Promise.all([promise1, promise2]);
+        return res1 + res2;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
