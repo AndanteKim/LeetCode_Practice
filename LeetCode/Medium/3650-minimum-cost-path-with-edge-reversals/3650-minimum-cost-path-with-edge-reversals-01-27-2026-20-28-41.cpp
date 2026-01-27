947a5f508902a412eebc060dc27cedce176089ce
Andrew Kim <ksb0724@utexas.edu>
2026-01-27 20:28:41 +0900

    Time: 225 ms (78.61%), Space: 290 MB (84.13%) - LeetHub

---
+++ LeetCode/Medium/3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals-01-27-2026-20-28-41.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+        vector graph(n, vector<pair<int, int>>{});
+        
+        for (const auto& e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            graph[u].push_back({v, w});
+            graph[v].push_back({u, 2 * w});
+        }
+
+        vector<int> dist(n, std::numeric_limits<int>::max());
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> minHeap;
+        minHeap.push({0, 0});
+
+        while (!minHeap.empty()) {
+            const auto [cost, u] = minHeap.top(); minHeap.pop();
+
+            if (u == n - 1) return cost;
+
+            for (const auto [v, w] : graph[u]) {
+                int newCost = cost + w;
+
+                if (newCost < dist[v]) {
+                    dist[v] = newCost;
+                    minHeap.push({newCost, v});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
