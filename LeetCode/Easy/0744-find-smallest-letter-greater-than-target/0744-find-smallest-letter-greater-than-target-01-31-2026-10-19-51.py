e2234adb07f573f0eb1e9a69772eee33f41a19dd
Andrew Kim <ksb0724@utexas.edu>
2026-01-31 10:19:51 +0900

    Time: 0 ms (100%), Space: 20.8 MB (25.32%) - LeetHub

---
+++ LeetCode/Easy/0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target-01-31-2026-10-19-51.py
@@ -0,0 +1,9 @@
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        ans = ''
+
+        for c in letters:
+            if c > target:
+                return c
+
+        return letters[0]
