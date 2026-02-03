fdf3644c4273fb7bd567768847f9f3e347170b22
Andrew Kim <ksb0724@utexas.edu>
2026-02-03 20:06:25 +0900

    Time: 0 ms (100%), Space: 19.4 MB (19.11%) - LeetHub

---
+++ LeetCode/Easy/3637-trionic-array-i/3637-trionic-array-i-02-03-2026-20-06-26.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isTrionic(self, nums: List[int]) -> bool:
+        n, i = len(nums), 1
+
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        p = i - 1
+
+        while i < n and nums[i - 1] > nums[i]:
+            i += 1
+        q = i - 1
+
+        while i < n and nums[i - 1] < nums[i]:
+            i += 1
+        flag = i - 1
+
+        return p != 0 and q != p and (flag == n - 1 and flag != q)
