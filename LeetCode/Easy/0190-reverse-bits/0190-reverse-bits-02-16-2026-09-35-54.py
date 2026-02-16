bcc25546635de0491ca404ea9047f9cd4beab598
Andrew Kim <ksb0724@utexas.edu>
2026-02-16 09:35:54 +0900

    Time: 46 ms (43.06%), Space: 19.2 MB (65.93%) - LeetHub

---
+++ LeetCode/Easy/0190-reverse-bits/0190-reverse-bits-02-16-2026-09-35-54.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans, power = 0, 31
+
+        while n > 0:
+            ans += (n & 1) << power
+            n >>= 1
+            power -= 1
+
+        return ans
