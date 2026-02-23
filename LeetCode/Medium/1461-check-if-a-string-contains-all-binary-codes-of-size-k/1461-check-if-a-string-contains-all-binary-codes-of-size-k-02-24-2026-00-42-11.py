a8cb6c3681944f1143d94e39d8c6e9fcbefcf1aa
Andrew Kim <ksb0724@utexas.edu>
2026-02-24 00:42:11 +0900

    Time: 722 ms (9.34%), Space: 52.4 MB (26%) - LeetHub

---
+++ LeetCode/Medium/1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k-02-24-2026-00-42-11.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        if len(s) < k:
+            return False
+        
+        ans, n = {s[:k]}, len(s)
+        curr = deque(s[:k])
+
+        for i in range(k, n):
+            curr.popleft()
+            curr.append(s[i])
+            ans.add("".join(curr))
+
+        return len(ans) == (2 ** k)
