b5df56dc397179b53924964b7da3e0e9d60fabd4
Andrew Kim <ksb0724@utexas.edu>
2026-03-01 14:22:00 +0900

    Time: 0 ms (100%), Space: 16.5 MB (81.74%) - LeetHub

---
+++ LeetCode/Medium/1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers-03-01-2026-14-21-59.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+        int ans = 0;
+
+        for (const char& c : n) {
+            ans = max(ans, c - '0');
+        }
+
+        return ans;
+    }
+};
