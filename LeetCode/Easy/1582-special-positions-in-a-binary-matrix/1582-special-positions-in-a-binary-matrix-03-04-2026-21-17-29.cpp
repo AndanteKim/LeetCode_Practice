effebe78083d4b9a9b99fa59945ceb392f0628f0
Andrew Kim <ksb0724@utexas.edu>
2026-03-04 21:17:29 +0900

    Time: 0 ms (100%), Space: 17.2 MB (20.57%) - LeetHub

---
+++ LeetCode/Easy/1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix-03-04-2026-21-17-29.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numSpecial(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<int> rows(m), cols(n);
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j]) {
+                    ++rows[i];
+                    ++cols[j];
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (mat[i][j] && rows[i] == 1 && cols[j] == 1) ++ans;
+            } 
+        }
+        
+        return ans;
+    }
+};
