419b134fed32b48895da50d2e477154c50069ee6
Andrew Kim <ksb0724@utexas.edu>
2026-03-11 02:17:09 +0900

    Time: 107 ms (83.74%), Space: 51.2 MB (34.15%) - LeetHub

---
+++ LeetCode/Medium/3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i-03-11-2026-02-17-09.cpp
@@ -0,0 +1,45 @@
+typedef long long ll;
+
+
+class Solution {
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+        vector dp(zero + 1, vector(one + 1, vector<ll>(2)));
+        int mod = 1'000'000'007;
+
+        for (int i = 0; i <= min(zero, limit); ++i) dp[i][0][0] = 1;
+        for (int j = 0; j <= min(one, limit); ++j) dp[0][j][1] = 1;
+
+        for (int i = 1; i <= zero; ++i) {
+            for (int j = 1; j <= one; ++j) {
+                if (i > limit) {
+                    dp[i][j][0] = (
+                        dp[i - 1][j][0]
+                        + dp[i - 1][j][1]
+                        - dp[i - limit - 1][j][1]
+                    );
+                }
+                else {
+                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                }
+
+                dp[i][j][0] = (dp[i][j][0] % mod + mod) % mod;
+
+                if (j > limit) {
+                    dp[i][j][1] = (
+                        dp[i][j - 1][0]
+                        + dp[i][j - 1][1]
+                        - dp[i][j - limit - 1][0]
+                    );
+                }
+                else {
+                    dp[i][j][1] = dp[i][j - 1][0] + dp[i][j - 1][1];
+                }
+
+                dp[i][j][1] = (dp[i][j][1] % mod + mod) % mod;
+            }
+        }
+
+        return (dp[zero][one][0] + dp[zero][one][1]) % mod;
+    }
+};
