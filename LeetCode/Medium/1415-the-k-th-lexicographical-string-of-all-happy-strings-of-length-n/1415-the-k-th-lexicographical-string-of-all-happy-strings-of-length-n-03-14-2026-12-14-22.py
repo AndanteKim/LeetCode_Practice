5a8464dbddf6c47f73d8e718da64d99496e6d7ac
Andrew Kim <ksb0724@utexas.edu>
2026-03-14 12:14:22 +0900

    Time: 51 ms (26.73%), Space: 29.8 MB (15.37%) - LeetHub

---
+++ LeetCode/Medium/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n-03-14-2026-12-14-22.py
@@ -0,0 +1,16 @@
+class Solution:
+    def getHappyString(self, n: int, k: int) -> str:
+        def backtrack(remain: int, curr: str) -> None:
+            if remain == 0:
+                candidates.append(curr)
+                return
+            
+            for ch in ['a', 'b', 'c']:
+                if len(curr) == 0 or ch != curr[-1]:
+                    backtrack(remain - 1, curr + ch)
+
+        candidates = []
+        backtrack(n, "")
+
+
+        return "" if len(candidates) < k else candidates[k - 1]
