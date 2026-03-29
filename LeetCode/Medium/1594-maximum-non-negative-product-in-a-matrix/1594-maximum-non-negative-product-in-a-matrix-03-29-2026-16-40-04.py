fd8d95ebe2ed1bc6c8ee7e70700d71556dbf5a2d
Andrew Kim <ksb0724@utexas.edu>
2026-03-29 16:40:05 +0900

    Time: 5 ms (34.07%), Space: 19.5 MB (30.9%) - LeetHub

---
+++ LeetCode/Medium/1594-maximum-non-negative-product-in-a-matrix/1594-maximum-non-negative-product-in-a-matrix-03-29-2026-16-40-04.py
@@ -0,0 +1,35 @@
+class Solution:
+    def maxProductPath(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        MOD = 1_000_000_007
+        maxgt, minlt = [[0] * n for _ in range(m)], [[0] * n for _ in range(m)]
+        maxgt[0][0] = minlt[0][0] = grid[0][0]
+
+        for i in range(1, n):
+            maxgt[0][i] = minlt[0][i] = maxgt[0][i - 1] * grid[0][i]
+            
+        for i in range(1, m):
+            maxgt[i][0] = minlt[i][0] = maxgt[i - 1][0] * grid[i][0]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                if grid[i][j] >= 0:
+                    maxgt[i][j] = (
+                        max(maxgt[i][j - 1], maxgt[i - 1][j]) * grid[i][j]
+                    )
+                    minlt[i][j] = (
+                        min(minlt[i][j - 1], minlt[i - 1][j]) * grid[i][j]
+                    )
+                else:
+                    maxgt[i][j] = (
+                        min(minlt[i][j - 1], minlt[i - 1][j]) * grid[i][j]
+                    )
+                    minlt[i][j] = (
+                        max(maxgt[i][j - 1], maxgt[i - 1][j]) * grid[i][j]
+                    )
+        
+        if maxgt[m - 1][n - 1] < 0:
+            return -1
+        return maxgt[m - 1][n - 1] % MOD
+
+
