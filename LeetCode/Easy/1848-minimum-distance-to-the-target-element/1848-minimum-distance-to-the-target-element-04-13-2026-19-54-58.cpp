d085d3413442faab03e4bf5000adba20f99fc91d
Andrew Kim <ksb0724@utexas.edu>
2026-04-13 19:54:58 +0900

    Time: 0 ms (100%), Space: 13.1 MB (99.57%) - LeetHub

---
+++ LeetCode/Easy/1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element-04-13-2026-19-54-58.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int ans = nums.size();
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] == target) {
+                ans = min(ans, abs(i - start));
+            }
+        }
+
+        return ans;
+    }
+};
