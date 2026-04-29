c4f095292726d2bd7c699ae3311c93d768ff172a
Andrew Kim <ksb0724@utexas.edu>
2026-04-30 02:36:41 +0900

    Time: 18 ms (38.13%), Space: 38.8 MB (30.22%) - LeetHub

---
+++ LeetCode/Medium/1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination-04-30-2026-02-36-41.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool leadsToDestination(int n, vector<vector<int>>& edges, int source, int destination) {
+        unordered_map<int, bool> visited;
+        unordered_map<int, vector<int>> adj;
+
+        for (const auto& edge : edges) {
+            adj[edge[0]].push_back(edge[1]);
+        }
+
+        function<bool(int)> dfs = [&](int node) -> bool {
+            if (node == destination) return true;
+
+            if (visited.contains(node)) return visited[node];
+
+            if (adj[node].size() == 0) return false;
+            visited[node] = false;
+
+            for (int next : adj[node]) {
+                if (!dfs(next)) return false;
+            }
+
+            visited[node] = true;
+
+            return visited[node];
+        };
+
+
+        if (adj[destination].size() > 0) return false;
+        return dfs(source);
+    }
+};
