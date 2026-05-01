9af1eb7395d6e84eb6343338b864313a624ee29e
Andrew Kim <ksb0724@utexas.edu>
2026-05-01 17:00:22 +0900

    Time: 66 ms (86.15%), Space: 20.6 MB (95.19%) - LeetHub

---
+++ LeetCode/Hard/0759-employee-free-time/0759-employee-free-time-05-01-2026-17-00-22.py
@@ -0,0 +1,25 @@
+"""
+# Definition for an Interval.
+class Interval:
+    def __init__(self, start: int = None, end: int = None):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
+        times = []
+        for s in schedule:
+            for it in s:
+                times.append([it.start, it.end])
+
+        times.sort()
+        ans, prev_e = [], -1
+        
+        for s, e in times:
+            if prev_e != -1 and prev_e < s:
+                ans.append(Interval(prev_e, s))
+            
+            prev_e = max(prev_e, e)
+
+        return ans
