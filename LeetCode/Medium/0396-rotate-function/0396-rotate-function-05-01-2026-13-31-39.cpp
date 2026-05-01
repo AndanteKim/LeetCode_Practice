1d70d14d6384dfe77d9d99fad11823b5281cf590
Andrew Kim <ksb0724@utexas.edu>
2026-05-01 13:31:39 +0900

    Time: 4 ms (40.21%), Space: 99.5 MB (79.08%) - LeetHub

---
+++ LeetCode/Medium/0396-rotate-function/0396-rotate-function-05-01-2026-13-31-39.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxRotateFunction(vector<int>& nums) {
+        int f = 0, n = nums.size();
+        int numSum = accumulate(nums.begin(), nums.end(), 0);
+
+        for (int i = 0; i < n; ++i) f += i * nums[i];
+
+        int ans = f;
+        for (int i = n - 1; i > 0; --i) {
+            f += numSum - n * nums[i];
+            ans = max(ans, f);
+        }
+
+        return ans;
+    }
+};
