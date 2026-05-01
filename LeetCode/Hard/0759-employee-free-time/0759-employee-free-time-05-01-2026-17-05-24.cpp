da4bcdacb9622a12c0626bc4cc55b6c344d842e2
Andrew Kim <ksb0724@utexas.edu>
2026-05-01 17:05:24 +0900

    Time: 25 ms (28.39%), Space: 15.6 MB (72.9%) - LeetHub

---
+++ LeetCode/Hard/0759-employee-free-time/0759-employee-free-time-05-01-2026-17-05-24.cpp
@@ -0,0 +1,39 @@
+/*
+// Definition for an Interval.
+class Interval {
+public:
+    int start;
+    int end;
+
+    Interval() {}
+
+    Interval(int _start, int _end) {
+        start = _start;
+        end = _end;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
+        vector<pair<int, int>> times;
+
+        for (const auto& s : schedule) {
+            for (const auto& it : s) {
+                times.push_back({it.start, it.end});
+            }
+        }
+
+        sort(times.begin(), times.end());
+
+        vector<Interval> ans;
+        int prevE = -1;
+        for (const auto& [s, e] : times) {
+            if (prevE != -1 && prevE < s) ans.push_back(Interval(prevE, s));
+            prevE = max(prevE, e);
+        }
+        
+        return ans;
+    }
+};
