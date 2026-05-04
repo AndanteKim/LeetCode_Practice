f4d5d800db2092d5fff11e7d18f52eec90bcff67
Andrew Kim <ksb0724@utexas.edu>
2026-05-04 14:43:31 +0900

    Time: 0 ms (100%), Space: 19.2 MB (68.02%) - LeetHub

---
+++ LeetCode/Medium/0048-rotate-image/0048-rotate-image-05-04-2026-14-43-31.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        self.transpose(matrix)
+        self.reflect(matrix)
+
+    def transpose(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    def reflect(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        for i in range(n):
+            for j in range(n >> 1):
+                matrix[i][j], matrix[i][n - j - 1] = matrix[i][n - j - 1], matrix[i][j]
