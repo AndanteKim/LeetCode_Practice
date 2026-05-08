817c0c965c636d99a26c752d3388f52c1468bc8f
Andrew Kim <ksb0724@utexas.edu>
2026-05-09 02:01:50 +0900

    Time: 803 ms (91.51%), Space: 147.9 MB (14.46%) - LeetHub

---
+++ LeetCode/Medium/3629-minimum-jumps-to-reach-end-via-prime-teleportation/3629-minimum-jumps-to-reach-end-via-prime-teleportation-05-09-2026-02-01-50.py
@@ -0,0 +1,45 @@
+MX = 1_000_001
+factors = [[] for _ in range(MX)]
+for i in range(2, MX):
+    if not factors[i]:
+        for j in range(i, MX, i):
+            factors[j].append(i)
+
+class Solution:
+    def minJumps(self, nums: List[int]) -> int:
+        n = len(nums)
+        edges = defaultdict(list)
+        for i, a in enumerate(nums):
+            for p in factors[a]:
+                edges[p].append(i)
+
+        ans, seen = 0, [False] * n
+        q, seen[0] = [0], True
+
+        while True:
+            q2 = []
+
+            for i in q:
+                if i == n - 1:
+                    return ans
+
+                if i > 0 and not seen[i - 1]:
+                    seen[i - 1] = True
+                    q2.append(i - 1)
+
+                if i < n - 1 and not seen[i + 1]:
+                    seen[i + 1] = True
+                    q2.append(i + 1)
+
+                if len(factors[nums[i]]) == 1:
+                    p = nums[i]
+                    for j in edges[p]:
+                        if not seen[j]:
+                            seen[j] = True
+                            q2.append(j)
+                    edges[p].clear()
+
+            q = q2
+            ans += 1
+        
+        return ans
