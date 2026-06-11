e3f38aee0c78781c036898e75a20acadf3ec1413
Andrew Kim <ksb0724@utexas.edu>
2026-06-11 23:36:03 +0900

    Time: 384 ms (53.76%), Space: 114.8 MB (34.41%) - LeetHub

---
+++ LeetCode/Medium/3558-number-of-ways-to-assign-edge-weights-i/3558-number-of-ways-to-assign-edge-weights-i-06-11-2026-23-36-03.py
@@ -0,0 +1,21 @@
+class Solution:
+    MOD = 10 ** 9 + 7
+
+    def assignEdgeWeights(self, edges: List[List[int]]) -> int:
+        def dfs(x: int, f: int) -> int:
+            max_dep = 0
+            for y in g[x]:
+                if y == f:
+                    continue
+                max_dep = max(max_dep, dfs(y, x) + 1)
+            
+            return max_dep
+        
+        n = len(edges) + 1
+        g = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+
+        max_dep = dfs(1, 0)
+        return pow(2, max_dep - 1, self.MOD)
