d4a7602804da98619f366a130058308990175ceb
Andrew Kim <ksb0724@utexas.edu>
2026-06-22 00:27:34 +0900

    Time: 386 ms (44.94%), Space: 0B (100%) - LeetHub

---
+++ LeetCode/Easy/1050-actors-and-directors-who-cooperated-at-least-three-times/1050-actors-and-directors-who-cooperated-at-least-three-times-06-22-2026-00-27-32.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+/*SELECT actor_id, director_id
+    FROM (
+    SELECT COUNT(*) AS Coop
+        actor_id, director_id
+        FROM ActorDirector;
+    )
+    WHERE Coop >= 3
+;*/
+
+SELECT 
+    actor_id, director_id
+    FROM ActorDirector
+    GROUP BY actor_id, director_id
+    HAVING COUNT(timestamp) >= 3;
