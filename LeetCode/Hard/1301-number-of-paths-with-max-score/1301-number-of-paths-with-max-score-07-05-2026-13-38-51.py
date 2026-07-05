2eb7f6ea719c255bebd7afc1b9efc9b44f79bdc8
Andrew Kim <ksb0724@utexas.edu>
2026-07-05 13:38:53 +0900

    Time: 101 ms (77.06%), Space: 20.5 MB (43.12%) - LeetHub

---
+++ LeetCode/Hard/1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score-07-05-2026-13-38-51.py
@@ -0,0 +1,32 @@
+class Solution:
+    def pathsWithMaxScore(self, board: List[str]) -> List[int]:
+        def update(x: int, y: int, u: int, v: int) -> None:
+            if u >= n or v >= n or dp[u][v][0] == -1:
+                return
+            
+            if dp[u][v][0] > dp[x][y][0]:
+                dp[x][y] = dp[u][v][:]
+            elif dp[u][v][0] == dp[x][y][0]:
+                dp[x][y][1] += dp[u][v][1]
+        
+        n = len(board)
+        dp = [[[-1, 0]] * n for _ in range(n)]
+        dp[n - 1][n - 1] = [0, 1]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+                if not (i == n - 1 and j == n - 1) and board[i][j] != 'X':
+                    update(i, j, i + 1, j)
+                    update(i, j, i, j + 1)
+                    update(i, j, i + 1, j + 1)
+
+                    if dp[i][j][0] != -1:
+                        dp[i][j][0] += (
+                            0 if board[i][j] == 'E' else ord(board[i][j]) - 48
+                        )
+        
+        return (
+            [dp[0][0][0], dp[0][0][1] % (10 ** 9 + 7)]
+            if dp[0][0][0] != -1
+            else [0, 0]
+        )
