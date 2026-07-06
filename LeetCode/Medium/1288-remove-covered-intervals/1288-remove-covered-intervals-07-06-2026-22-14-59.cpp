d231ebbf8ccfeba5d9ec992004f280b580e76c9a
Andrew Kim <ksb0724@utexas.edu>
2026-07-06 22:14:59 +0900

    Time: 12 ms (12.85%), Space: 15.3 MB (30.6%) - LeetHub

---
+++ LeetCode/Medium/1288-remove-covered-intervals/1288-remove-covered-intervals-07-06-2026-22-14-59.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeCoveredIntervals(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        sort(intervals.begin(), intervals.end(), [](vector<int>& a, vector<int>& b) {
+            return a[0] != b[0]? a[0] < b[0] : b[1] < a[1];
+        });
+
+        int ans = 0, prevEnd = 0;
+        for (vector<int>& interval : intervals) {
+            int end = interval[1];
+
+            if (end > prevEnd) {
+                ++ans;
+                prevEnd = end;
+            }
+        }
+
+        return ans;
+    }
+};
