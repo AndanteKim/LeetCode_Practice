813b924a6cad562155cb3c86835a576d0dc33e7d
Andrew Kim <ksb0724@utexas.edu>
2026-07-23 20:14:36 +0900

    Time: 0 ms (100%), Space: 34 MB (35.59%) - LeetHub

---
+++ LeetCode/Medium/3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i-07-23-2026-20-14-35.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniqueXorTriplets(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n <= 2:
+            return n
+        
+        ans = 1
+        while ans <= n:
+            ans <<= 1
+
+        return ans
