786041614cb31d2c76d4294c41738f74de0ec12e
Andrew Kim <ksb0724@utexas.edu>
2026-07-30 04:27:20 +0900

    Time: 27 ms (74.4%), Space: 54.2 MB (91.76%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -2,11 +2,21 @@ class Solution {
 public:
     string smallestPalindrome(string s) {
         int n = s.size(), partition = n >> 1;
-        sort(s.begin(), s.begin() + partition);
-        
-        for (int i = 0; i < partition; ++i)
-            s[n - 1 - i] = s[i];
-        
+        vector<int> bucket(26);
+
+        for (int i = 0; i < partition; ++i) ++bucket[s[i] - 'a'];
+
+        int left = 0, right = n - 1;
+
+        for (int i = 0; i < 26; ++i) {
+            while (bucket[i]-- > 0) {
+                char c = i + 'a';
+                s[left++] = c;
+                s[right--] = c;
+            }
+            
+        }
+
         return s;
     }
 };
