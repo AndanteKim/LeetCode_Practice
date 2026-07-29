bce2ce418bc0127fc5b4ff98c57031a0ce1f2c66
Andrew Kim <ksb0724@utexas.edu>
2026-07-30 05:53:31 +0900

    Time: 197 ms (61.36%), Space: 61.3 MB (29.55%) - LeetHub

--- 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.py
+++ 1858-longest-word-with-all-prefixes/1858-longest-word-with-all-prefixes.py
@@ -1,43 +1,43 @@
 class TrieNode:
     def __init__(self):
-        self.is_end = False
+        self.is_word = False
         self.children = dict()
-
 
 class Trie:
     def __init__(self):
         self.root = TrieNode()
-        
-    # Insert a word into the trie
-    def _insert(self, s: str) -> None:
+
+    def insert(self, word: str) -> None:
         curr = self.root
-        for c in s:
+        curr.is_word = True
+        for c in word:
             if c not in curr.children:
                 curr.children[c] = TrieNode()
             curr = curr.children[c]
-        curr.is_end = True
-    
-    # Check if all prefixes of the word exists in the trie
-    def _has_all_prefixes(self, s: str) -> bool:
+        curr.is_word = True
+
+    def search(self, word: str) -> bool:
         curr = self.root
-        for c in s:
-            if c not in curr.children or not curr.children[c].is_end:
+
+        for c in word:
+            if not curr.is_word:
                 return False
             curr = curr.children[c]
+
         return True
 
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        trie, longest_valid_word = Trie(), ""
+        root = Trie()
+        for word in words:
+            curr = root
+            curr.insert(word)
+
+        words.sort(key = lambda x: (-len(x), x))
+
+        for word in words:
+            if root.search(word):
+                return word
+
+        return ""
         
-        # Insert all words into the trie
-        for word in words:
-            trie._insert(word)
-            
-        # Check each word and update the longest valid word
-        for word in words:
-            if trie._has_all_prefixes(word):
-                if len(word) > len(longest_valid_word) or \
-                (len(word) == len(longest_valid_word) and word < longest_valid_word):
-                    longest_valid_word = word
-        return longest_valid_word
