cad59a14fdc1593d0a47dc516632de31b27b2b17
Andrew Kim <ksb0724@utexas.edu>
2026-08-01 13:15:12 +0900

    Time: 0 ms (100%), Space: 9.8 MB (56.39%) - LeetHub

---
+++ LeetCode/Medium/0486-predict-the-winner/0486-predict-the-winner-08-01-2026-13-15-11.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp = nums;
+
+        for (int diff = 1; diff < n; ++diff) {
+            for (int left = 0; left < n - diff; ++left){
+                int right = left + diff;
+
+                dp[left] = max(nums[left] - dp[left + 1], nums[right] - dp[left]);
+            }
+        }
+
+        return dp[0] >= 0;
+    }
+};
