d898e2a24afd378a2c6339a6f01a301fda8854fa
Andrew Kim <ksb0724@utexas.edu>
2026-08-02 11:30:35 +0900

    Time: 3 ms (38.73%), Space: 19.7 MB (20.08%) - LeetHub

---
+++ LeetCode/Medium/0877-stone-game/0877-stone-game-08-02-2026-11-30-35.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector dp(n, vector<int>(n));
+
+        for (int i = 0; i < n; ++i) dp[i][i] = piles[i];
+
+        for (int i = n - 2; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j){
+                int takeLeft = piles[i] - dp[i + 1][j];
+                int takeRight = piles[j] - dp[i][j - 1];
+
+                dp[i][j] = max(takeLeft, takeRight);
+            }
+        }
+
+        return dp[0][n - 1] > 0;
+    }
+};
