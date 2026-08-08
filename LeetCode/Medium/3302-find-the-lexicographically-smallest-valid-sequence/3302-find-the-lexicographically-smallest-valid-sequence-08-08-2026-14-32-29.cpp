fb91670858613fbe721448a3d87d407692d88841
Andrew Kim <ksb0724@utexas.edu>
2026-08-08 14:32:29 +0900

    Time: 44 ms (55.32%), Space: 109.3 MB (46.81%) - LeetHub

---
+++ LeetCode/Medium/3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence-08-08-2026-14-32-29.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> validSequence(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+
+        int j = m - 1;
+        vector<int> last(m, -1);
+        for (int i = n - 1; i >= 0; --i) {
+            if (j >= 0 && word1[i] == word2[j]) {
+                last[j--] = i;
+            }
+        }
+
+        vector<int> ans;
+        j = 0;
+        int skip = 0;
+
+        for (int i = 0; i < n; ++i) {
+            
+            if (j == m) break;
+
+            if (word1[i] == word2[j] || (skip == 0 && (j == m - 1 || i < last[j + 1]))) {
+                skip += (int)(word1[i] != word2[j++]);
+                ans.push_back(i);
+            }
+
+        }
+
+        return j == m? ans : vector<int>{};
+    }
+};
