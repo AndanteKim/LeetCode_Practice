5a871a200d979e1437f8392ecb66a998937a4d7d
Andrew Kim <ksb0724@utexas.edu>
2026-08-09 18:41:18 +0900

    Time: 289 ms (64.18%), Space: 33.5 MB (76%) - LeetHub

---
+++ LeetCode/Hard/3348-smallest-divisible-digit-product-ii/3348-smallest-divisible-digit-product-ii-08-09-2026-18-41-17.py
@@ -0,0 +1,53 @@
+class Solution:
+    def smallestNumber(self, num: str, t: int) -> str:
+        temp = t
+        for i in range(2, 10):
+            while temp % i == 0:
+                temp //= i
+
+        # Base case
+        if temp > 1:
+            return "-1"
+
+        n = len(num)
+        rem = [0] * (n + 1)
+        rem[0], pos = t, n - 1
+
+        num_list = list(num)
+        for i in range(n):
+            if num_list[i] == '0':
+                pos = i
+                break
+            rem[i + 1] = rem[i] // math.gcd(rem[i], int(num_list[i]))
+        
+        if rem[n] == 1:
+            return num
+        
+        for i in range(pos, -1, -1):
+            while True:
+                num_list[i] = chr(ord(num_list[i]) + 1)
+                if num_list[i] > '9':
+                    break
+                k, t_now = 9, rem[i] // math.gcd(rem[i], int(num_list[i]))
+                
+                for j in range(n - 1, i, -1):
+                    while t_now % k != 0:
+                        k -= 1
+                    t_now //= k
+                    num_list[j] = str(k)
+
+                if t_now == 1:
+                    return "".join(num_list)
+        
+        ans, original_t = [], t
+
+        for i in range(9, 1, -1):
+            while original_t % i == 0:
+                ans.append(str(i))
+                original_t //= i
+            
+        ans_str = "".join(ans)
+        padding = max(n + 1 - len(ans_str), 0)
+        ans_str += '1' * padding
+
+        return ans_str[::-1]
