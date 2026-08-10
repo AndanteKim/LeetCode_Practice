941ec4e52af3a3a2f4a989c4ef80ddccc4f968c6
Andrew Kim <ksb0724@utexas.edu>
2026-08-11 00:13:50 +0900

    Time: 1539 ms (21.22%), Space: 54.9 MB (24.55%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.py
+++ 1510-stone-game-iv/1510-stone-game-iv.py
@@ -1,15 +1,16 @@
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        @lru_cache(maxsize = None)
         def dp(remain: int) -> int:
-            if remain == 0:
-                return False
+            if remain in memo:
+                return memo[remain]
 
             for i in range(1, int(remain ** 0.5) + 1):
                 if not dp(remain - i * i):
+                    memo[remain] = True
                     return True
-
-
-            return False
+            
+            memo[remain] = False
+            return False 
         
+        memo = {0 : False}
         return dp(n)
