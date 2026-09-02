7132733eca0ea43136a7c7f030ed0073dff8df1a
Andrew Kim <ksb0724@utexas.edu>
2026-09-03 08:25:28 +0900

    Time: 192 ms (33.79%), Space: 32.3 MB (21.38%) - LeetHub

--- 2355-maximum-number-of-books-you-can-take/2355-maximum-number-of-books-you-can-take.py
+++ 2355-maximum-number-of-books-you-can-take/2355-maximum-number-of-books-you-can-take.py
@@ -1,29 +1,23 @@
 class Solution:
     def maximumBooks(self, books: List[int]) -> int:
-        n = len(books)
-        
-        # calculate the sum of books in a given range [l, r]
-        def calculate_sum(l: int, r: int) -> int:
+        def cal_sum(l: int, r: int) -> int:
             cnt = min(books[r], r - l + 1)
-            return (2 * books[r] - (cnt - 1)) * cnt // 2
+            return (2 * books[r] - (cnt - 1)) * cnt >> 1
         
-        stack = []
+        n, st = len(books), []
         dp = [0] * n
-        
+
         for i in range(n):
-            # While we cannot push i, we pop from the stack
-            while stack and books[stack[-1]] - stack[-1] >= books[i] - i:
-                stack.pop()
-                
-            # compute dp[i]
-            if not stack:
-                dp[i] = calculate_sum(0, i)
-            else:
-                j = stack[-1]
-                dp[i] = dp[j] + calculate_sum(j + 1, i)
+            curr = 0
+            while st and books[st[-1]] - st[-1] >= books[i] - i:
+                st.pop()
             
-            # push the current index onto the stack
-            stack.append(i)
-        
-        # return the maximum element in the dp array
+            if not st:
+                dp[i] = cal_sum(0, i)
+            else:
+                j = st[-1]
+                dp[i] = dp[j] + cal_sum(j + 1, i)
+
+            st.append(i)
+
         return max(dp)
