6b594e0fae6b97397f91ad3136bc14ccaaa92b26
Andrew Kim <ksb0724@utexas.edu>
2026-09-06 12:11:07 +0900

    Time: 11 ms (87.61%), Space: 8.8 MB (95.82%) - LeetHub

---
+++ LeetCode/Hard/0115-distinct-subsequences/0115-distinct-subsequences-09-06-2026-12-11-06.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = s.size(), n = t.size();
+        vector<unsigned int> dp(n);
+
+        for (int i = m - 1; i >= 0; --i) {
+            unsigned int prev = 1;
+
+            for (int j = n - 1; j >= 0; --j) {
+                unsigned int oldDp = dp[j];
+                if (s[i] == t[j]) dp[j] += prev;
+
+                prev = oldDp;
+            }
+        }
+
+        return dp[0];
+    }
+};
