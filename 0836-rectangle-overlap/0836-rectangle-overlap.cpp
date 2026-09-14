f41fd0e99b7a9ea2aec3f4504c53d41e17ead609
Andrew Kim <ksb0724@utexas.edu>
2026-09-15 07:47:41 +0900

    Time: 0 ms (100%), Space: 9.7 MB (77.15%) - LeetHub

--- 0836-rectangle-overlap/0836-rectangle-overlap.cpp
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -1,6 +1,14 @@
 class Solution {
 public:
     bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
-        return (min(rec1[3], rec2[3]) > max(rec1[1], rec2[1])) && (min(rec1[2], rec2[2]) > max(rec1[0], rec2[0]));
+        if (rec1[0] == rec1[2] || rec1[1] == rec1[3] || rec2[0] == rec2[2] || rec2[1] == rec2[3])
+            return false; 
+    
+        return !(
+            rec1[2] <= rec2[0] ||
+            rec1[3] <= rec2[1] ||
+            rec1[0] >= rec2[2] ||
+            rec1[1] >= rec2[3]
+        );
     }
 };
