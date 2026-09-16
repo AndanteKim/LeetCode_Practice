a0014a149b7d7bf3fa3aee06099fb4eb31f279fb
Andrew Kim <ksb0724@utexas.edu>
2026-09-16 23:17:26 +0900

    Time: 0 ms (100%), Space: 7.9 MB (88.79%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.cpp
@@ -1,17 +1,27 @@
 class Solution {
-public:
-    int numberOfSets(int n, int k) {
-        vector<int> dp(n, 1), prefix(n + 1);
-        int MOD = 1'000'000'007;
-
-        for (int i = 0; i < n; ++i) prefix[i + 1] = (prefix[i] + dp[i]) % MOD;
-
-        for (int i = 0; i < k; ++i) {
-            dp[0] = 0;
-            for (int j = 1; j < n; ++j) dp[j] = (dp[j - 1] + prefix[j]) % MOD;
-            for (int j = 0; j < n; ++j) prefix[j + 1] = (prefix[j] + dp[j]) % MOD;
+private:
+    const int MOD = 1'000'000'007;
+    long long quickPow(long long a, long long e) {
+        long long res = 1;
+        while (e > 0) {
+            if (e & 1) res = res * a % MOD;
+            a = a * a % MOD;
+            e >>= 1;
         }
 
-        return dp.back();
+        return res;
+    }
+
+public:
+    int numberOfSets(int n, int k) {
+        int m = 2 * k;
+        long long numerator = 1, denominator = 1;
+
+        for (int i = 1; i <= m; ++i) {
+            numerator = numerator * (n + k - i) % MOD;
+            denominator = denominator * i % MOD;
+        }
+
+        return numerator * quickPow(denominator, MOD - 2) % MOD;
     }
 };
