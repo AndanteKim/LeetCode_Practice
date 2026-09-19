cb29f93893af8eb0936d196c7e658d99fcb18d2d
Andrew Kim <ksb0724@utexas.edu>
2026-09-19 16:33:43 +0900

    Time: 5 ms (35.51%), Space: 73.1 MB (34.83%) - LeetHub

---
+++ LeetCode/Medium/1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view-09-19-2026-16-33-42.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+        vector<int> ans;
+
+        for (int i = 0; i < heights.size(); ++i) {
+            while (!ans.empty() && heights[ans.back()] <= heights[i]) ans.pop_back();
+
+            ans.push_back(i);
+        }
+
+        return ans;
+    }
+};
